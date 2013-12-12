32b68285eaeacb926086c282221bf4e73a1fe846
Tomas Olvecky <tolvecky@cisco.com>
2013-12-12 11:45:56 +0100

    Add ModuleQName annotation.
    Add ModuleQName annotation so that generated code can maintain description of yang file from which it was derived.
    
    Change-Id: I6d4469acae62fcb4e9b80a814244f1a60e2ad983
    Signed-off-by: Tomas Olvecky <tolvecky@cisco.com>

--- yang/yang-binding/src/main/java/org/opendaylight/yangtools/yang/binding/annotations/QName.java
+++ yang/yang-binding/src/main/java/org/opendaylight/yangtools/yang/binding/annotations/QName.java
@@ -1,9 +1,14 @@
 package org.opendaylight.yangtools.yang.binding.annotations;
 
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface QName {
 
     String namespace();
     String revision();
     String name();
-    
+
 }
