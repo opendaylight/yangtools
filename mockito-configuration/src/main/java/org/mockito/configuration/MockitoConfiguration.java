dad10e30c4aaba6b7a3489edf3e60cf8a1e9f3a4
Robert Varga <rovarga@cisco.com>
2014-01-16 16:33:58 +0100

    Introduce mockito-configuration from bgpcep
    Change-Id: Id454e3ae9d121f2f75f7c9036c0042280ab6c6c9
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ mockito-configuration/src/main/java/org/mockito/configuration/MockitoConfiguration.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.mockito.configuration;
+
+import org.mockito.stubbing.Answer;
+
+/**
+ * Configuration customization for Mockito. Change default answer to {@link ThrowsUnstubbedMethodException}.
+ */
+public class MockitoConfiguration extends DefaultMockitoConfiguration {
+
+	@Override
+	public Answer<Object> getDefaultAnswer() {
+		return new ThrowsUnstubbedMethodException();
+	}
+}
---
+++ mockito-configuration/src/main/java/org/mockito/configuration/ThrowsUnstubbedMethodException.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.mockito.configuration;
+
+import java.io.Serializable;
+
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+/**
+ * Answer that throws {@link UnstubbedMethodException}.
+ */
+public class ThrowsUnstubbedMethodException implements Answer<Object>, Serializable {
+	private static final long serialVersionUID = 1L;
+
+    public ThrowsUnstubbedMethodException() {
+    }
+
+    @Override
+	public Object answer(InvocationOnMock invocation) throws Throwable {
+        Throwable t = new UnstubbedMethodException(invocation.toString() + " was not stubbed");
+        throw t;
+    }
+}
