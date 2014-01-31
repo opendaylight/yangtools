9ae3359f45a643b562b837a00ebfab942236c167
Martin Sunal <msunal@cisco.com>
2014-01-31 22:07:24 +0100

    Added ietf-restconf yang module
    Signed-off-by: Martin Sunal <msunal@cisco.com>

---
+++ model/ietf/ietf-restconf/src/main/java/org/opendaylight/yang/gen/v1/urn/ietf/params/xml/ns/yang/ietf/restconf/rev131019/restconf/restconf/modules/RevisionBuilder.java
@@ -0,0 +1,13 @@
+package org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.restconf.rev131019.restconf.restconf.modules;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.restconf.rev131019.restconf.restconf.modules.Module.Revision;
+
+
+/**
+**/
+public class RevisionBuilder {
+
+    public static Revision getDefaultInstance(String defaultValue) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
