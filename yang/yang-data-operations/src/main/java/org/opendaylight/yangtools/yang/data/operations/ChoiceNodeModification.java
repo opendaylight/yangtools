27ca746531f6562eb78661d844c926f5713d3cdd
Maros Marsalek <mmarsale@cisco.com>
2014-03-19 09:54:15 +0100

    Implement edit-config operations for NormalizedNode yang-data-api
    Placed in a new module yang-data-operations
    
    Supported operations: all defined in http://tools.ietf.org/html/rfc6241
    Multiple positive and negative test cases added. Testing yang schema can be found under src/test/resources
    
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>
    Change-Id: I897c6479cf14e7073a6fb954565cfe2b376a6c40

---
+++ yang/yang-data-operations/src/main/java/org/opendaylight/yangtools/yang/data/operations/ChoiceNodeModification.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.operations;
+
+import java.util.Set;
+
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.AugmentationNode;
+import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
+import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.DataContainerNodeBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.transform.base.SchemaUtils;
+import org.opendaylight.yangtools.yang.model.api.ChoiceCaseNode;
+import org.opendaylight.yangtools.yang.model.api.ChoiceNode;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.Sets;
+
+final class ChoiceNodeModification extends
+        AbstractContainerNodeModification<ChoiceNode, org.opendaylight.yangtools.yang.data.api.schema.ChoiceNode> {
+
+    @Override
+    protected QName getQName(ChoiceNode schema) {
+        return schema.getQName();
+    }
+
+    @Override
+    protected Object findSchemaForChild(ChoiceNode schema, QName nodeType) {
+        return SchemaUtils.findSchemaForChild(schema, nodeType);
+    }
+
+    @Override
+    protected Set<InstanceIdentifier.PathArgument> getChildrenToProcess(ChoiceNode schema,
+            Optional<org.opendaylight.yangtools.yang.data.api.schema.ChoiceNode> actual,
+            Optional<org.opendaylight.yangtools.yang.data.api.schema.ChoiceNode> modification)
+            throws DataModificationException {
+        Set<InstanceIdentifier.PathArgument> childrenToProcess = super.getChildrenToProcess(schema, actual,
+                modification);
+
+        if (modification.isPresent() == false) {
+            return childrenToProcess;
+        }
+
+        // Detect case node from modification
+        ChoiceCaseNode detectedCase = null;
+        for (DataContainerChild<? extends InstanceIdentifier.PathArgument, ?> child : modification.get().getValue()) {
+            Optional<ChoiceCaseNode> detectedCaseForChild = SchemaUtils.detectCase(schema, child);
+
+            if(detectedCaseForChild.isPresent() == false) {
+                DataModificationException.IllegalChoiceValuesException.throwUnknownChild(schema.getQName(),
+                        child.getNodeType());
+            }
+
+            if (detectedCase != null && detectedCase.equals(detectedCaseForChild.get()) == false) {
+                DataModificationException.IllegalChoiceValuesException.throwMultipleCasesReferenced(schema.getQName(),
+                        modification.get(), detectedCase.getQName(), detectedCaseForChild.get().getQName());
+            }
+            detectedCase = detectedCaseForChild.get();
+        }
+
+        if (detectedCase == null)
+            return childrenToProcess;
+
+        // Filter out child nodes that do not belong to detected case =
+        // Nodes from other cases present in actual
+        Set<InstanceIdentifier.PathArgument> childrenToProcessFiltered = Sets.newLinkedHashSet();
+        for (InstanceIdentifier.PathArgument childToProcess : childrenToProcess) {
+            // child from other cases, skip
+            if (childToProcess instanceof AugmentationNode
+                    && SchemaUtils.belongsToCaseAugment(detectedCase,
+                            (InstanceIdentifier.AugmentationIdentifier) childToProcess) == false) {
+                continue;
+            } else if (belongsToCase(detectedCase, childToProcess) == false) {
+                continue;
+            }
+
+            childrenToProcessFiltered.add(childToProcess);
+        }
+
+        return childrenToProcessFiltered;
+    }
+
+    private boolean belongsToCase(ChoiceCaseNode detectedCase, InstanceIdentifier.PathArgument childToProcess) {
+        return detectedCase.getDataChildByName(childToProcess.getNodeType()) != null;
+    }
+
+    @Override
+    protected Object findSchemaForAugment(ChoiceNode schema, InstanceIdentifier.AugmentationIdentifier childToProcessId) {
+        return SchemaUtils.findSchemaForAugment(schema, childToProcessId.getPossibleChildNames());
+    }
+
+    @Override
+    protected DataContainerNodeBuilder<?, org.opendaylight.yangtools.yang.data.api.schema.ChoiceNode> getBuilder(
+            ChoiceNode schema) {
+        return Builders.choiceBuilder(schema);
+    }
+}
