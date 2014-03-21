7390e5824dd1d090a1ef18258e3b6727cfc2d08e
Jozef Gloncak <jgloncak@cisco.com>
2014-03-21 09:58:00 +0100

    Parsing and serialization of composite and simple node to normalized nodes
    Added implementation of base classes from yang-data-impl artifact for
    serializer and for parser
    + simple test
    
    Change-Id: Ic7353e1be7ef6e00546595304c23b205f7580cee
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

---
+++ yang/yang-data-json/src/main/java/org/opendaylight/yangtools/yang/data/json/schema/cnsn/parser/CnSnToNormalizedNodeParserFactory.java
@@ -0,0 +1,92 @@
+package org.opendaylight.yangtools.yang.data.json.schema.cnsn.parser;
+
+import org.opendaylight.yangtools.yang.data.api.Node;
+import org.opendaylight.yangtools.yang.data.api.schema.AugmentationNode;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafSetEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafSetNode;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.transform.ToNormalizedNodeParser;
+import org.opendaylight.yangtools.yang.data.impl.schema.transform.ToNormalizedNodeParserFactory;
+import org.opendaylight.yangtools.yang.data.impl.schema.transform.base.parser.NodeParserDispatcher;
+import org.opendaylight.yangtools.yang.model.api.AugmentationSchema;
+import org.opendaylight.yangtools.yang.model.api.ChoiceNode;
+import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.LeafListSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.ListSchemaNode;
+
+public class CnSnToNormalizedNodeParserFactory implements ToNormalizedNodeParserFactory<Node<?>> {
+
+    private final ContainerNodeCnSnParser containerNodeCnSnParser;
+    private final LeafNodeCnSnParser leafNodeCnSnParser;
+    private final LeafSetEntryNodeCnSnParser leafSetEntryNodeCnSnParser;
+    private final LeafSetNodeCnSnParser leafSetNodeCnSnParser;
+    private final MapNodeCnSnParser mapNodeCnSnParser;
+    private final MapEntryNodeCnSnParser mapEntryNodeCnSnParser;
+    private final ChoiceNodeCnSnParser choiceNodeCnSnParser;
+    private final AugmentationNodeCnSnParser augmentationNodeCnSnParser;
+
+    private CnSnToNormalizedNodeParserFactory() {
+        leafNodeCnSnParser = new LeafNodeCnSnParser();
+        leafSetEntryNodeCnSnParser = new LeafSetEntryNodeCnSnParser();
+        leafSetNodeCnSnParser = new LeafSetNodeCnSnParser(leafSetEntryNodeCnSnParser);
+
+        final NodeParserDispatcher<Node<?>> dispatcher = new NodeParserDispatcher.BaseNodeParserDispatcher<Node<?>>(
+                this) {
+
+        };
+
+        containerNodeCnSnParser = new ContainerNodeCnSnParser(dispatcher);
+        mapEntryNodeCnSnParser = new MapEntryNodeCnSnParser(dispatcher);
+        mapNodeCnSnParser = new MapNodeCnSnParser(mapEntryNodeCnSnParser);
+        choiceNodeCnSnParser = new ChoiceNodeCnSnParser(dispatcher);
+        augmentationNodeCnSnParser = new AugmentationNodeCnSnParser(dispatcher);
+    }
+
+    public static CnSnToNormalizedNodeParserFactory getInstance() {
+        return new CnSnToNormalizedNodeParserFactory();
+    }
+
+    @Override
+    public ToNormalizedNodeParser<Node<?>, ContainerNode, ContainerSchemaNode> getContainerNodeParser() {
+        return containerNodeCnSnParser;
+    }
+
+    @Override
+    public ToNormalizedNodeParser<Node<?>, LeafNode<?>, LeafSchemaNode> getLeafNodeParser() {
+        return leafNodeCnSnParser;
+    }
+
+    @Override
+    public ToNormalizedNodeParser<Node<?>, LeafSetNode<?>, LeafListSchemaNode> getLeafSetNodeParser() {
+        return leafSetNodeCnSnParser;
+    }
+
+    @Override
+    public ToNormalizedNodeParser<Node<?>, MapNode, ListSchemaNode> getMapNodeParser() {
+        return mapNodeCnSnParser;
+    }
+
+    @Override
+    public ToNormalizedNodeParser<Node<?>, org.opendaylight.yangtools.yang.data.api.schema.ChoiceNode, ChoiceNode> getChoiceNodeParser() {
+        return choiceNodeCnSnParser;
+    }
+
+    @Override
+    public ToNormalizedNodeParser<Node<?>, AugmentationNode, AugmentationSchema> getAugmentationNodeParser() {
+        return augmentationNodeCnSnParser;
+    }
+
+    @Override
+    public ToNormalizedNodeParser<Node<?>, LeafSetEntryNode<?>, LeafListSchemaNode> getLeafSetEntryNodeParser() {
+        return leafSetEntryNodeCnSnParser;
+    }
+
+    @Override
+    public ToNormalizedNodeParser<Node<?>, MapEntryNode, ListSchemaNode> getMapEntryNodeParser() {
+        return mapEntryNodeCnSnParser;
+    }
+}
