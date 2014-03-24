18330105b7d568ec339b693c3c86eecbbaa6a244
Robert Varga <rovarga@cisco.com>
2014-03-24 20:41:01 +0100

    Add guava-based object cache
    This object cache is bound by the garbage collector, as it uses soft
    references internally. It can be grown later to implement a caching
    policies based on object type.
    
    Change-Id: I16a5683bdc587dd5fe7a3c9c8e1e5a14cb85d09b
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ common/object-cache-guava/src/main/java/org/opendaylight/yangtools/objcache/guava/GuavaObjectCache.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.objcache.guava;
+
+import org.opendaylight.yangtools.objcache.spi.AbstractObjectCache;
+
+import com.google.common.base.FinalizableReferenceQueue;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheBuilderSpec;
+
+final class GuavaObjectCache extends AbstractObjectCache {
+	public GuavaObjectCache(final FinalizableReferenceQueue queue, final CacheBuilderSpec spec) {
+		super(CacheBuilder.from(spec).softValues().build(), queue);
+	}
+}
