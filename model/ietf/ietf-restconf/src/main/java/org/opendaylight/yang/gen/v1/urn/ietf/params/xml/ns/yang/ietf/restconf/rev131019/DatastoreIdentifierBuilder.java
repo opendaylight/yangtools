7f64697ee43e37453143f8749ea99952529695ed
Devin Avery <devin.avery@brocade.com>
2014-04-25 11:06:50 -0400

    Bug 735 - Part 1: Update ietf-restconf and ietf-yangtypes to newer versions
    Implemented RevisionBuilder.java and RevisionBuilderTest.java classes.
    
    Change-Id: I8824580f37869ff128fe33ea862c90261bd743a6
    Signed-off-by: Devin Avery <devin.avery@brocade.com>

---
+++ model/ietf/ietf-restconf/src/main/java/org/opendaylight/yang/gen/v1/urn/ietf/params/xml/ns/yang/ietf/restconf/rev131019/DatastoreIdentifierBuilder.java
@@ -0,0 +1,13 @@
+package org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.restconf.rev131019;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.restconf.rev131019.DatastoreIdentifier;
+
+
+/**
+**/
+public class DatastoreIdentifierBuilder {
+
+    public static DatastoreIdentifier getDefaultInstance(String defaultValue) {
+        throw new UnsupportedOperationException("The ietf draft does not currently use this type.");
+    }
+
+}
