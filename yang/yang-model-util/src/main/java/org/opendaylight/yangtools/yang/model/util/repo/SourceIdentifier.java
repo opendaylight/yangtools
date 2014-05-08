edb5446f0f917b58f3aab2b42d444d3cc2248417
Robert Varga <rovarga@cisco.com>
2014-05-08 08:11:01 +0200

    BUG-731: add curly braces
    Automated addition of curly braces.
    
    Change-Id: I475657dc778601124610d0415cdf05e6a4495cc5
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-model-util/src/main/java/org/opendaylight/yangtools/yang/model/util/repo/SourceIdentifier.java
+++ yang/yang-model-util/src/main/java/org/opendaylight/yangtools/yang/model/util/repo/SourceIdentifier.java
@@ -14,7 +14,7 @@ public final class SourceIdentifier {
     private final String name;
     private final String revision;
 
-    public SourceIdentifier(String name, Optional<String> formattedRevision) {
+    public SourceIdentifier(final String name, final Optional<String> formattedRevision) {
         super();
         this.name = name;
         this.revision = formattedRevision.orNull();
@@ -30,24 +30,31 @@ public final class SourceIdentifier {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
+    public boolean equals(final Object obj) {
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()) {
             return false;
+        }
         SourceIdentifier other = (SourceIdentifier) obj;
         if (name == null) {
-            if (other.name != null)
+            if (other.name != null) {
                 return false;
-        } else if (!name.equals(other.name))
+            }
+        } else if (!name.equals(other.name)) {
             return false;
+        }
         if (revision == null) {
-            if (other.revision != null)
+            if (other.revision != null) {
                 return false;
-        } else if (!revision.equals(other.revision))
+            }
+        } else if (!revision.equals(other.revision)) {
             return false;
+        }
         return true;
     }
 
@@ -59,20 +66,20 @@ public final class SourceIdentifier {
         return revision;
     }
 
-    public static SourceIdentifier create(String moduleName, Optional<String> revision) {
+    public static SourceIdentifier create(final String moduleName, final Optional<String> revision) {
         return new SourceIdentifier(moduleName, revision);
     }
-    
+
     public String toYangFilename() {
         return toYangFileName(name, Optional.fromNullable(revision));
     }
-    
+
     @Override
     public String toString() {
         return "SourceIdentifier [name=" + name + "@" + revision + "]";
     }
 
-    public static final String toYangFileName(String moduleName, Optional<String> revision) {
+    public static final String toYangFileName(final String moduleName, final Optional<String> revision) {
         StringBuilder filename = new StringBuilder(moduleName);
         if (revision.isPresent()) {
             filename.append("@");
