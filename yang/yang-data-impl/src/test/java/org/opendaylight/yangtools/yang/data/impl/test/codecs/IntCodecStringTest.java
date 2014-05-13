480f9090a404736235ae3b3c328c37e60a50b4fe
tpantelis <tpanteli@brocade.com>
2014-05-12 22:46:00 -0400

    [DO NOT MERGE]Bug 762: Verify input in typedef codecs
    This can't be merged until https://git.opendaylight.org/gerrit/#/c/7372/
    is merged on he controller side.
    
    - Modified the Bits, Union, Enum, and Empty TypeDefinitionAwareCodecs to verify
      the input in the deserialize method and throw IllegalArgEx if invalid.
    
    - Modified IdentityCompositeCodec to throw IllegalArgEx if input not
      found in identity map.
    
    - Added unit tests to cover all the TypeDefinitionAwareCodec-derived
      classes.
    
    Change-Id: I10e963b83bfb9e4200f3ea0e77cd41bd48d04474
    Signed-off-by: tpantelis <tpanteli@brocade.com>

--- yang/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/test/codecs/IntCodecStringTest.java
+++ yang/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/test/codecs/IntCodecStringTest.java
@@ -12,56 +12,99 @@ import static org.junit.Assert.assertEquals;
 import java.math.BigInteger;
 
 import org.junit.Test;
-import org.opendaylight.yangtools.yang.data.impl.codec.TypeDefinitionAwareCodec;
+import org.opendaylight.yangtools.yang.data.api.codec.Int16Codec;
+import org.opendaylight.yangtools.yang.data.api.codec.Int32Codec;
+import org.opendaylight.yangtools.yang.data.api.codec.Int64Codec;
+import org.opendaylight.yangtools.yang.data.api.codec.Int8Codec;
+import org.opendaylight.yangtools.yang.data.api.codec.Uint16Codec;
+import org.opendaylight.yangtools.yang.data.api.codec.Uint32Codec;
+import org.opendaylight.yangtools.yang.data.api.codec.Uint64Codec;
+import org.opendaylight.yangtools.yang.data.api.codec.Uint8Codec;
+import org.opendaylight.yangtools.yang.model.util.Int16;
+import org.opendaylight.yangtools.yang.model.util.Int32;
+import org.opendaylight.yangtools.yang.model.util.Int64;
+import org.opendaylight.yangtools.yang.model.util.Int8;
+import org.opendaylight.yangtools.yang.model.util.Uint16;
+import org.opendaylight.yangtools.yang.model.util.Uint32;
+import org.opendaylight.yangtools.yang.model.util.Uint64;
+import org.opendaylight.yangtools.yang.model.util.Uint8;
+
+import static org.opendaylight.yangtools.yang.data.impl.test.codecs.TypeDefinitionAwareCodecTest.getCodec;
+import static org.opendaylight.yangtools.yang.data.impl.test.codecs.TypeDefinitionAwareCodecTest.deserializeWithExpectedIllegalArgEx;
 
 public class IntCodecStringTest {
 
+    @SuppressWarnings({ "unchecked" })
     @Test
     public void uint8CodecTest() {
         final String hexa = "0x40";
         final String octal = "0100";
         final String integer = "64";
 
-        assertEquals(TypeDefinitionAwareCodec.UINT8_DEFAULT_CODEC.deserialize(hexa), Short.valueOf("040", 16));
-        assertEquals(TypeDefinitionAwareCodec.UINT8_DEFAULT_CODEC.deserialize(octal), Short.valueOf(octal, 8));
-        assertEquals(TypeDefinitionAwareCodec.UINT8_DEFAULT_CODEC.deserialize(integer), Short.valueOf(integer, 10));
-        assertEquals(TypeDefinitionAwareCodec.UINT8_DEFAULT_CODEC.deserialize("0"), Short.valueOf("0", 10));
+        Uint8Codec<String> codec = getCodec(Uint8.getInstance(), Uint8Codec.class);
+        assertEquals(codec.deserialize(hexa), Short.valueOf("040", 16));
+        assertEquals(codec.deserialize(octal), Short.valueOf(octal, 8));
+        assertEquals(codec.deserialize(integer), Short.valueOf(integer, 10));
+        assertEquals(codec.deserialize("0"), Short.valueOf("0", 10));
+
+        deserializeWithExpectedIllegalArgEx(codec, "1o");
+        deserializeWithExpectedIllegalArgEx(codec, "");
+        deserializeWithExpectedIllegalArgEx(codec, null);
     }
 
+    @SuppressWarnings("unchecked")
     @Test
     public void uint16CodecTest() {
         final String hexa = "0X45c";
         final String octal = "02134";
         final String integer = "1116";
 
-        assertEquals(TypeDefinitionAwareCodec.UINT16_DEFAULT_CODEC.deserialize(hexa), Integer.valueOf("045c", 16));
-        assertEquals(TypeDefinitionAwareCodec.UINT16_DEFAULT_CODEC.deserialize(octal), Integer.valueOf(octal, 8));
-        assertEquals(TypeDefinitionAwareCodec.UINT16_DEFAULT_CODEC.deserialize(integer), Integer.valueOf(integer, 10));
+        Uint16Codec<String> codec = getCodec(Uint16.getInstance(), Uint16Codec.class);
+        assertEquals(codec.deserialize(hexa), Integer.valueOf("045c", 16));
+        assertEquals(codec.deserialize(octal), Integer.valueOf(octal, 8));
+        assertEquals(codec.deserialize(integer), Integer.valueOf(integer, 10));
+
+        deserializeWithExpectedIllegalArgEx(codec, "1o");
+        deserializeWithExpectedIllegalArgEx(codec, "");
+        deserializeWithExpectedIllegalArgEx(codec, null);
     }
 
+    @SuppressWarnings("unchecked")
     @Test
     public void uint32CodecTest() {
         final String hexa = "0x45FFFCDE";
         final String octal = "010577776336";
         final String integer = "1174404318";
 
-        assertEquals(TypeDefinitionAwareCodec.UINT32_DEFAULT_CODEC.deserialize(hexa), Long.valueOf("45FFFCDE", 16));
-        assertEquals(TypeDefinitionAwareCodec.UINT32_DEFAULT_CODEC.deserialize(octal), Long.valueOf(octal, 8));
-        assertEquals(TypeDefinitionAwareCodec.UINT32_DEFAULT_CODEC.deserialize(integer), Long.valueOf(integer, 10));
+        Uint32Codec<String> codec = getCodec(Uint32.getInstance(), Uint32Codec.class);
+        assertEquals(codec.deserialize(hexa), Long.valueOf("45FFFCDE", 16));
+        assertEquals(codec.deserialize(octal), Long.valueOf(octal, 8));
+        assertEquals(codec.deserialize(integer), Long.valueOf(integer, 10));
+
+        deserializeWithExpectedIllegalArgEx(codec, "1o");
+        deserializeWithExpectedIllegalArgEx(codec, "");
+        deserializeWithExpectedIllegalArgEx(codec, null);
     }
 
+    @SuppressWarnings("unchecked")
     @Test
     public void uint64CodecTest() {
         final String hexa = "0X75EDC78edCBA";
         final String octal = "03536670743556272";
         final String integer = "129664115727546";
 
-        assertEquals(TypeDefinitionAwareCodec.UINT64_DEFAULT_CODEC.deserialize(hexa),
+        Uint64Codec<String> codec = getCodec(Uint64.getInstance(), Uint64Codec.class);
+        assertEquals(codec.deserialize(hexa),
                 new BigInteger("75EDC78edCBA", 16));
-        assertEquals(TypeDefinitionAwareCodec.UINT64_DEFAULT_CODEC.deserialize(octal), new BigInteger(octal, 8));
-        assertEquals(TypeDefinitionAwareCodec.UINT64_DEFAULT_CODEC.deserialize(integer), new BigInteger(integer, 10));
+        assertEquals(codec.deserialize(octal), new BigInteger(octal, 8));
+        assertEquals(codec.deserialize(integer), new BigInteger(integer, 10));
+
+        deserializeWithExpectedIllegalArgEx(codec, "12345o");
+        deserializeWithExpectedIllegalArgEx(codec, "");
+        deserializeWithExpectedIllegalArgEx(codec, null);
     }
 
+    @SuppressWarnings("unchecked")
     @Test
     public void int8CodecTest() {
         final String hexa = "0x40";
@@ -71,14 +114,20 @@ public class IntCodecStringTest {
         final String integer = "64";
         final String negInteger = "-64";
 
-        assertEquals(TypeDefinitionAwareCodec.INT8_DEFAULT_CODEC.deserialize(hexa), Byte.valueOf("040", 16));
-        assertEquals(TypeDefinitionAwareCodec.INT8_DEFAULT_CODEC.deserialize(negHexa), Byte.valueOf("-040", 16));
-        assertEquals(TypeDefinitionAwareCodec.INT8_DEFAULT_CODEC.deserialize(octal), Byte.valueOf(octal, 8));
-        assertEquals(TypeDefinitionAwareCodec.INT8_DEFAULT_CODEC.deserialize(negOctal), Byte.valueOf(negOctal, 8));
-        assertEquals(TypeDefinitionAwareCodec.INT8_DEFAULT_CODEC.deserialize(integer), Byte.valueOf(integer, 10));
-        assertEquals(TypeDefinitionAwareCodec.INT8_DEFAULT_CODEC.deserialize(negInteger), Byte.valueOf(negInteger, 10));
+        Int8Codec<String> codec = getCodec(Int8.getInstance(), Int8Codec.class);
+        assertEquals(codec.deserialize(hexa), Byte.valueOf("040", 16));
+        assertEquals(codec.deserialize(negHexa), Byte.valueOf("-040", 16));
+        assertEquals(codec.deserialize(octal), Byte.valueOf(octal, 8));
+        assertEquals(codec.deserialize(negOctal), Byte.valueOf(negOctal, 8));
+        assertEquals(codec.deserialize(integer), Byte.valueOf(integer, 10));
+        assertEquals(codec.deserialize(negInteger), Byte.valueOf(negInteger, 10));
+
+        deserializeWithExpectedIllegalArgEx(codec, "1o");
+        deserializeWithExpectedIllegalArgEx(codec, "");
+        deserializeWithExpectedIllegalArgEx(codec, null);
     }
 
+    @SuppressWarnings("unchecked")
     @Test
     public void int16CodecTest() {
         final String hexa = "+0X45c";
@@ -88,15 +137,21 @@ public class IntCodecStringTest {
         final String integer = "+1116";
         final String negInteger = "-1116";
 
-        assertEquals(TypeDefinitionAwareCodec.INT16_DEFAULT_CODEC.deserialize(hexa), Short.valueOf("+045c", 16));
-        assertEquals(TypeDefinitionAwareCodec.INT16_DEFAULT_CODEC.deserialize(negHexa), Short.valueOf("-045c", 16));
-        assertEquals(TypeDefinitionAwareCodec.INT16_DEFAULT_CODEC.deserialize(octal), Short.valueOf(octal, 8));
-        assertEquals(TypeDefinitionAwareCodec.INT16_DEFAULT_CODEC.deserialize(negOctal), Short.valueOf(negOctal, 8));
-        assertEquals(TypeDefinitionAwareCodec.INT16_DEFAULT_CODEC.deserialize(integer), Short.valueOf(integer, 10));
-        assertEquals(TypeDefinitionAwareCodec.INT16_DEFAULT_CODEC.deserialize(negInteger),
+        Int16Codec<String> codec = getCodec(Int16.getInstance(), Int16Codec.class);
+        assertEquals(codec.deserialize(hexa), Short.valueOf("+045c", 16));
+        assertEquals(codec.deserialize(negHexa), Short.valueOf("-045c", 16));
+        assertEquals(codec.deserialize(octal), Short.valueOf(octal, 8));
+        assertEquals(codec.deserialize(negOctal), Short.valueOf(negOctal, 8));
+        assertEquals(codec.deserialize(integer), Short.valueOf(integer, 10));
+        assertEquals(codec.deserialize(negInteger),
                 Short.valueOf(negInteger, 10));
+
+        deserializeWithExpectedIllegalArgEx(codec, "1o");
+        deserializeWithExpectedIllegalArgEx(codec, "");
+        deserializeWithExpectedIllegalArgEx(codec, null);
     }
 
+    @SuppressWarnings("unchecked")
     @Test
     public void int32CodecTest() {
         final String hexa = "0x45FFFCDE";
@@ -106,16 +161,22 @@ public class IntCodecStringTest {
         final String integer = "1174404318";
         final String negInteger = "-1174404318";
 
-        assertEquals(TypeDefinitionAwareCodec.INT32_DEFAULT_CODEC.deserialize(hexa), Integer.valueOf("+045FFFCDE", 16));
-        assertEquals(TypeDefinitionAwareCodec.INT32_DEFAULT_CODEC.deserialize(negHexa),
+        Int32Codec<String> codec = getCodec(Int32.getInstance(), Int32Codec.class);
+        assertEquals(codec.deserialize(hexa), Integer.valueOf("+045FFFCDE", 16));
+        assertEquals(codec.deserialize(negHexa),
                 Integer.valueOf("-045FFFCDE", 16));
-        assertEquals(TypeDefinitionAwareCodec.INT32_DEFAULT_CODEC.deserialize(octal), Integer.valueOf(octal, 8));
-        assertEquals(TypeDefinitionAwareCodec.INT32_DEFAULT_CODEC.deserialize(negOctal), Integer.valueOf(negOctal, 8));
-        assertEquals(TypeDefinitionAwareCodec.INT32_DEFAULT_CODEC.deserialize(integer), Integer.valueOf(integer, 10));
-        assertEquals(TypeDefinitionAwareCodec.INT32_DEFAULT_CODEC.deserialize(negInteger),
+        assertEquals(codec.deserialize(octal), Integer.valueOf(octal, 8));
+        assertEquals(codec.deserialize(negOctal), Integer.valueOf(negOctal, 8));
+        assertEquals(codec.deserialize(integer), Integer.valueOf(integer, 10));
+        assertEquals(codec.deserialize(negInteger),
                 Integer.valueOf(negInteger, 10));
+
+        deserializeWithExpectedIllegalArgEx(codec, "1o");
+        deserializeWithExpectedIllegalArgEx(codec, "");
+        deserializeWithExpectedIllegalArgEx(codec, null);
     }
 
+    @SuppressWarnings("unchecked")
     @Test
     public void int64CodecTest() {
         final String hexa = "0X75EDC78edCBA";
@@ -125,12 +186,17 @@ public class IntCodecStringTest {
         final String integer = "+129664115727546";
         final String negInteger = "-129664115727546";
 
-        assertEquals(TypeDefinitionAwareCodec.INT64_DEFAULT_CODEC.deserialize(hexa), Long.valueOf("075EDC78edCBA", 16));
-        assertEquals(TypeDefinitionAwareCodec.INT64_DEFAULT_CODEC.deserialize(negHexa),
+        Int64Codec<String> codec = getCodec(Int64.getInstance(), Int64Codec.class);
+        assertEquals(codec.deserialize(hexa), Long.valueOf("075EDC78edCBA", 16));
+        assertEquals(codec.deserialize(negHexa),
                 Long.valueOf("-075EDC78edCBA", 16));
-        assertEquals(TypeDefinitionAwareCodec.INT64_DEFAULT_CODEC.deserialize(octal), Long.valueOf(octal, 8));
-        assertEquals(TypeDefinitionAwareCodec.INT64_DEFAULT_CODEC.deserialize(negOctal), Long.valueOf(negOctal, 8));
-        assertEquals(TypeDefinitionAwareCodec.INT64_DEFAULT_CODEC.deserialize(integer), Long.valueOf(integer, 10));
-        assertEquals(TypeDefinitionAwareCodec.INT64_DEFAULT_CODEC.deserialize(negInteger), Long.valueOf(negInteger, 10));
+        assertEquals(codec.deserialize(octal), Long.valueOf(octal, 8));
+        assertEquals(codec.deserialize(negOctal), Long.valueOf(negOctal, 8));
+        assertEquals(codec.deserialize(integer), Long.valueOf(integer, 10));
+        assertEquals(codec.deserialize(negInteger), Long.valueOf(negInteger, 10));
+
+        deserializeWithExpectedIllegalArgEx(codec, "1234o");
+        deserializeWithExpectedIllegalArgEx(codec, "");
+        deserializeWithExpectedIllegalArgEx(codec, null);
     }
 }
