ec14b35ea14b9c9fe6583687b268696d640e98b4
Tony Tkacik <ttkacik@cisco.com>
2014-05-29 17:48:39 +0200

    API Clarity: Documented YANG Common.
    Change-Id: I7b789ba7c180c5b63409ad44000d944e01c26b6f
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- yang/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/RpcResult.java
+++ yang/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/RpcResult.java
@@ -9,10 +9,37 @@ package org.opendaylight.yangtools.yang.common;
 
 import java.util.Collection;
 
+/**
+ *
+ * Result of call to YANG enabled system.
+ *
+ *
+ * @param <T> Return type
+ */
 public interface RpcResult<T> {
+
+    /**
+     * True if processing of request was successful
+     *
+     * @return true if processing was successful.
+     */
     boolean isSuccessful();
 
+    /**
+     *
+     * Returns result of call or null if no result is available.
+     *
+     * @return result of call or null if no result is available.
+     *
+     */
     T getResult();
 
+    /**
+     *
+     * Returns set of errors and warnings which occured during processing
+     * the request.
+     *
+     * @return
+     */
     Collection<RpcError> getErrors();
 }
