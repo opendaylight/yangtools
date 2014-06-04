ca8ee9d83e6c34519696ee49f10ab2f95bff143c
Tony Tkacik <ttkacik@cisco.com>
2014-06-04 13:47:37 +0200

    API Clarity: Documented o.o.y.yang.perser.builder.api
    Change-Id: I75c524c7d4d41a4d65004a0ed86065af6a2ae523
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/builder/api/DocumentedNodeBuilder.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.builder.api;
+
+import org.opendaylight.yangtools.yang.model.api.Status;
+
+/**
+ * Mixin-style builder interfac for nodes, which may have documentation attached.
+ *
+ **/
+public interface DocumentedNodeBuilder {
+
+    /**
+     * Returns description of resulting schema node
+     * as was defined by description statement.
+     *
+     * @return description statement
+     */
+    String getDescription();
+
+    /**
+     * Set description to this node.
+     *
+     * @param description
+     */
+    void setDescription(String description);
+
+    /**
+     * Get reference of this node.
+     *
+     * @return reference statement
+     */
+    String getReference();
+
+    /**
+     * Set reference to this node.
+     *
+     * @param reference
+     */
+    void setReference(String reference);
+
+    /**
+     * Get status of this node.
+     *
+     * @return status statement
+     */
+    Status getStatus();
+
+    /**
+     * Set status to this node.
+     *
+     * @param status
+     */
+    void setStatus(Status status);
+}
--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/builder/api/UsesNodeBuilder.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/builder/api/UsesNodeBuilder.java
@@ -16,7 +16,7 @@ import org.opendaylight.yangtools.yang.model.api.UsesNode;
 import org.opendaylight.yangtools.yang.parser.util.RefineHolder;
 
 /**
- * Interface for builders of 'uses' statement.
+ * Builder for  'uses' statement.
  */
 public interface UsesNodeBuilder extends GroupingMember {
 
@@ -25,6 +25,7 @@ public interface UsesNodeBuilder extends GroupingMember {
      * module, container, list, case, grouping, input, output, notification or
      * augment, return type is DataNodeContainerBuilder.
      */
+    @Override
     DataNodeContainerBuilder getParent();
 
     /**
@@ -134,10 +135,27 @@ public interface UsesNodeBuilder extends GroupingMember {
     /**
      * Build new UsesNode object.
      */
+    @Override
     UsesNode build();
 
+    /**
+     * 
+     * Returns true if uses node was resolved and {@link #getGroupingBuilder()}
+     * was instantiated for parent done of this node.
+     * 
+     * @return
+     */
     boolean isResolved();
 
+    /**
+     * 
+     * Sets state of instantiation of {@link #getGroupingBuilder()}
+     * into parent node of this node.
+     * 
+     * @deprecated Do not use this, this should be internal to the implementation
+     *  and public API contract.
+     */
+    @Deprecated
     void setResolved(boolean resolved);
 
 }
