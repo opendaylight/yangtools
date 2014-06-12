8447f01b7cc2fa8b520b261644425d9648ed12da
Martin Vitez <mvitez@cisco.com>
2014-06-12 12:46:14 +0200

    BUG-597: removed dependency on GeneratedTOBuilderImpl from BindingGeneratorUtil to break a package cycle.
    Change-Id: I6dfbf217982e1128ab22db70af261ade250642c3
    Signed-off-by: Martin Vitez <mvitez@cisco.com>

--- code-generator/binding-generator-util/src/main/java/org/opendaylight/yangtools/binding/generator/util/BindingGeneratorUtil.java
+++ code-generator/binding-generator-util/src/main/java/org/opendaylight/yangtools/binding/generator/util/BindingGeneratorUtil.java
@@ -29,6 +29,7 @@ import org.opendaylight.yangtools.sal.binding.model.api.AccessModifier;
 import org.opendaylight.yangtools.sal.binding.model.api.Restrictions;
 import org.opendaylight.yangtools.sal.binding.model.api.Type;
 import org.opendaylight.yangtools.sal.binding.model.api.type.builder.GeneratedPropertyBuilder;
+import org.opendaylight.yangtools.sal.binding.model.api.type.builder.GeneratedTypeBuilderBase;
 import org.opendaylight.yangtools.sal.binding.model.api.type.builder.MethodSignatureBuilder;
 import org.opendaylight.yangtools.sal.binding.model.api.type.builder.TypeMemberBuilder;
 import org.opendaylight.yangtools.yang.binding.BindingMapping;
@@ -391,7 +392,7 @@ public final class BindingGeneratorUtil {
         gto.setSUID(prop);
     }
 
-    public static long computeDefaultSUID(final GeneratedTOBuilderImpl to) {
+    public static long computeDefaultSUID(final GeneratedTypeBuilderBase<?> to) {
         try {
             ByteArrayOutputStream bout = new ByteArrayOutputStream();
             DataOutputStream dout = new DataOutputStream(bout);
