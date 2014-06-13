b9703600251283441f1f26b8459d7bd564add0c8
Martin Vitez <mvitez@cisco.com>
2014-06-13 12:02:38 +0200

    BUG-1143: refactored AugmentRelativeXPathTest.
    Removed test sources which are irrelevant for relative path augment generated type testing.
    
    Signed-off-by: Martin Vitez <mvitez@cisco.com>

--- code-generator/binding-generator-impl/src/test/java/org/opendaylight/yangtools/sal/binding/generator/impl/AugmentRelativeXPathTest.java
+++ code-generator/binding-generator-impl/src/test/java/org/opendaylight/yangtools/sal/binding/generator/impl/AugmentRelativeXPathTest.java
@@ -11,13 +11,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.opendaylight.yangtools.sal.binding.generator.api.BindingGenerator;
 import org.opendaylight.yangtools.sal.binding.model.api.GeneratedProperty;
@@ -29,27 +24,16 @@ import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.parser.api.YangContextParser;
 import org.opendaylight.yangtools.yang.parser.impl.YangParserImpl;
 
-public class AugmentRelativeXPathTest {
+public class AugmentRelativeXPathTest extends AbstractTypesTest {
 
-    private final static List<File> augmentModels = new ArrayList<>();
-    private final static URL augmentFolderPath = AugmentedTypeTest.class
-            .getResource("/augment-relative-xpath-models");
-
-    @BeforeClass
-    public static void loadTestResources() throws URISyntaxException {
-        final File augFolder = new File(augmentFolderPath.toURI());
-
-        for (final File fileEntry : augFolder.listFiles()) {
-            if (fileEntry.isFile()) {
-                augmentModels.add(fileEntry);
-            }
-        }
+    public AugmentRelativeXPathTest() {
+        super(AugmentRelativeXPathTest.class.getResource("/augment-relative-xpath-models"));
     }
 
     @Test
     public void AugmentationWithRelativeXPathTest() throws IOException {
         final YangContextParser parser = new YangParserImpl();
-        final SchemaContext context = parser.parseFiles(augmentModels);
+        final SchemaContext context = parser.parseFiles(testModels);
 
         assertNotNull("context is null", context);
         final BindingGenerator bindingGen = new BindingGeneratorImpl();
@@ -66,19 +50,21 @@ public class AugmentRelativeXPathTest {
         for (final Type type : genTypes) {
             if (type.getName().equals("InterfaceKey") && type.getPackageName().contains("augment._abstract.topology")) {
                 gtInterfaceKey = (GeneratedTransferObject) type;
-            } else if (type.getName().equals("Interface") && type.getPackageName().contains("augment._abstract.topology")) {
+            } else if (type.getName().equals("Interface")
+                    && type.getPackageName().contains("augment._abstract.topology")) {
                 gtInterface = (GeneratedType) type;
             } else if (type.getName().equals("Tunnel") && type.getPackageName().contains("augment._abstract.topology")) {
                 gtTunnel = (GeneratedType) type;
-            } else if (type.getName().equals("TunnelKey") && type.getPackageName().contains("augment._abstract.topology")) {
+            } else if (type.getName().equals("TunnelKey")
+                    && type.getPackageName().contains("augment._abstract.topology")) {
                 gtTunnelKey = (GeneratedTransferObject) type;
             }
         }
 
         // 'Interface
-        assertNotNull("gtInterface is null", gtInterface);
+        assertNotNull("Interface is null", gtInterface);
         final List<MethodSignature> gtInterfaceMethods = gtInterface.getMethodDefinitions();
-        assertNotNull("gtInterfaceMethods is null", gtInterfaceMethods);
+        assertNotNull("Interface methods are null", gtInterfaceMethods);
         MethodSignature getIfcKeyMethod = null;
         for (final MethodSignature method : gtInterfaceMethods) {
             if (method.getName().equals("getKey")) {
@@ -86,17 +72,15 @@ public class AugmentRelativeXPathTest {
                 break;
             }
         }
-        assertNotNull("getIfcKeyMethod is null", getIfcKeyMethod);
-        assertNotNull("getIfcKeyMethod.getReturnType() is null", getIfcKeyMethod.getReturnType());
-        assertFalse("getIfcKeyMethod.getReturnType() should not be Void",
-                getIfcKeyMethod.getReturnType().equals("java.lang.Void"));
-        assertTrue("getIfcKeyMethod.getReturnType().getName() must be InterfaceKey",
-                getIfcKeyMethod.getReturnType().getName().equals("InterfaceKey"));
+        assertNotNull("getKey method is null", getIfcKeyMethod);
+        assertNotNull("getKey method return type is null", getIfcKeyMethod.getReturnType());
+        assertTrue("getKey method return type name must be InterfaceKey", getIfcKeyMethod.getReturnType().getName()
+                .equals("InterfaceKey"));
 
         // 'InterfaceKey'
-        assertNotNull("gtInterfaceKey is null", gtInterfaceKey);
+        assertNotNull("InterfaceKey is null", gtInterfaceKey);
         final List<GeneratedProperty> properties = gtInterfaceKey.getProperties();
-        assertNotNull("properties is null", properties);
+        assertNotNull("InterfaceKey properties are null", properties);
         GeneratedProperty gtInterfaceId = null;
         for (final GeneratedProperty property : properties) {
             if (property.getName().equals("interfaceId")) {
@@ -104,17 +88,15 @@ public class AugmentRelativeXPathTest {
                 break;
             }
         }
-        assertNotNull("gtInterfaceId is null", gtInterfaceId);
-        assertNotNull("gtInterfaceId.getReturnType() is null", gtInterfaceId.getReturnType());
-        assertFalse("gtInterfaceId.getReturnType() should not be Void",
-                gtInterfaceId.getReturnType().equals("java.lang.Void"));
-        assertTrue("gtInterfaceId.getReturnType().getName() must be String",
+        assertNotNull("interfaceId is null", gtInterfaceId);
+        assertNotNull("interfaceId return type is null", gtInterfaceId.getReturnType());
+        assertTrue("interfaceId return type name must be String",
                 gtInterfaceId.getReturnType().getName().equals("String"));
 
         // 'Tunnel'
-        assertNotNull("gtTunnel is null", gtTunnel);
+        assertNotNull("Tunnel is null", gtTunnel);
         final List<MethodSignature> tunnelMethods = gtTunnel.getMethodDefinitions();
-        assertNotNull("tunnelMethods is null", tunnelMethods);
+        assertNotNull("Tunnel methods are null", tunnelMethods);
         MethodSignature getTunnelKeyMethod = null;
         for (MethodSignature method : tunnelMethods) {
             if (method.getName().equals("getKey")) {
@@ -122,18 +104,15 @@ public class AugmentRelativeXPathTest {
                 break;
             }
         }
-        assertNotNull("getTunnelKeyMethod is null", getTunnelKeyMethod);
-        assertNotNull("getTunnelKeyMethod.getReturnType()",
-                getTunnelKeyMethod.getReturnType());
-        assertFalse("getTunnelKeyMethod.getReturnType() should not be Void",
-                getTunnelKeyMethod.getReturnType().equals("java.lang.Void"));
-        assertTrue("getTunnelKeyMethod.getReturnType().getName() must be TunnelKey",
-                getTunnelKeyMethod.getReturnType().getName().equals("TunnelKey"));
+        assertNotNull("getKey method is null", getTunnelKeyMethod);
+        assertNotNull("getKey method return type", getTunnelKeyMethod.getReturnType());
+        assertTrue("getKey method return type name must be TunnelKey", getTunnelKeyMethod.getReturnType().getName()
+                .equals("TunnelKey"));
 
         // 'TunnelKey'
-        assertNotNull("gtTunnelKey is null", gtTunnelKey);
+        assertNotNull("TunnelKey is null", gtTunnelKey);
         final List<GeneratedProperty> tunnelKeyProperties = gtTunnelKey.getProperties();
-        assertNotNull("tunnelKeyProperties is null", tunnelKeyProperties);
+        assertNotNull("TunnelKey properties are null", tunnelKeyProperties);
 
         GeneratedProperty gtTunnelId = null;
         for (final GeneratedProperty property : tunnelKeyProperties) {
@@ -141,13 +120,9 @@ public class AugmentRelativeXPathTest {
                 gtTunnelId = property;
             }
         }
-        assertNotNull("gtTunnelId is null", gtTunnelId);
-        assertNotNull("gtTunnelId.getReturnType() is null",
-                gtTunnelId.getReturnType());
-        assertFalse("gtTunnelId.getReturnType() should not be Void",
-                gtTunnelId.getReturnType().equals("java.lang.Void"));
-        assertTrue("gtTunnelId.getReturnType().getName() must be Uri",
-                gtTunnelId.getReturnType().getName().equals("Uri"));
+        assertNotNull("tunnelId is null", gtTunnelId);
+        assertNotNull("tunnelId return type is null", gtTunnelId.getReturnType());
+        assertTrue("tunnelId return type name must be Uri", gtTunnelId.getReturnType().getName().equals("Uri"));
     }
 
 }
