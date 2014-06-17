9033b9a5a228a552a4abc6230d6e5ce63d49fc78
Jozef Gloncak <jgloncak@cisco.com>
2014-06-17 09:11:42 +0200

    AnyXmlNode builders
    Add builders to yang-data-impl to be able to implement
    DataNormalizationOperation.AnyXmlNormalization.normalize() from sal-common-impl
    
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

---
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/builder/impl/ImmutableAnyXmlNodeBuilder.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.impl.schema.builder.impl;
+
+import java.util.Map;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.Node;
+import org.opendaylight.yangtools.yang.data.api.schema.AnyXmlNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.NormalizedNodeAttrBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.nodes.AbstractImmutableNormalizedValueAttrNode;
+
+public class ImmutableAnyXmlNodeBuilder extends AbstractImmutableNormalizedNodeBuilder<InstanceIdentifier.NodeIdentifier, Node<?>, AnyXmlNode> {
+
+    public static NormalizedNodeAttrBuilder<InstanceIdentifier.NodeIdentifier, Node<?>, AnyXmlNode> create() {
+        return new ImmutableAnyXmlNodeBuilder();
+    }
+
+    @Override
+    public AnyXmlNode build() {
+        return new ImmutableXmlNode(getNodeIdentifier(), getValue(), getAttributes());
+    }
+
+    private static final class ImmutableXmlNode extends AbstractImmutableNormalizedValueAttrNode<InstanceIdentifier.NodeIdentifier, Node<?>> implements AnyXmlNode {
+
+        ImmutableXmlNode(final InstanceIdentifier.NodeIdentifier nodeIdentifier, final Node<?> value, final Map<QName, String> attributes) {
+            super(nodeIdentifier, value, attributes);
+        }
+    }
+}
