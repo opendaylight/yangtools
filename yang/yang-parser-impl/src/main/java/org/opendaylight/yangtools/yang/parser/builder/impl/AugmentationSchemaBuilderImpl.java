9390d90d1ba083ce3c9f16ab79f111efbdf3300a
Tomas Olvecky <tolvecky@cisco.com>
2014-06-25 14:22:30 +0200

    BUG 1131: Removed duplicate code in builders.
    Sonar Analysis showed that concrete implementations of builders have
    number of similar code blocks, which deals with creating instance
    of schema node.
    
    This functionality should have only one implementiation (this
    increases readibility and reduces potential places for bugs)
    moved common shared functionality  to Abstract*Builder and
    Abstract implementaitons of SchemaNodes.
    
    Change-Id: Ie9d87eaa728612090f9ad3740381df1bf970d1fe
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>
    Signed-off-by: Tomas Olvecky <tolvecky@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/builder/impl/AugmentationSchemaBuilderImpl.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/builder/impl/AugmentationSchemaBuilderImpl.java
@@ -7,51 +7,35 @@
  */
 package org.opendaylight.yangtools.yang.parser.builder.impl;
 
-import com.google.common.base.Optional;
-import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
-
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.model.api.AugmentationSchema;
-import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
-import org.opendaylight.yangtools.yang.model.api.GroupingDefinition;
 import org.opendaylight.yangtools.yang.model.api.NamespaceRevisionAware;
 import org.opendaylight.yangtools.yang.model.api.RevisionAwareXPath;
 import org.opendaylight.yangtools.yang.model.api.SchemaPath;
-import org.opendaylight.yangtools.yang.model.api.Status;
-import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
 import org.opendaylight.yangtools.yang.model.api.UnknownSchemaNode;
-import org.opendaylight.yangtools.yang.model.api.UsesNode;
 import org.opendaylight.yangtools.yang.model.util.RevisionAwareXPathImpl;
 import org.opendaylight.yangtools.yang.parser.builder.api.AugmentationSchemaBuilder;
 import org.opendaylight.yangtools.yang.parser.builder.api.Builder;
-import org.opendaylight.yangtools.yang.parser.builder.api.DataSchemaNodeBuilder;
-import org.opendaylight.yangtools.yang.parser.builder.api.GroupingBuilder;
 import org.opendaylight.yangtools.yang.parser.builder.api.UnknownSchemaNodeBuilder;
-import org.opendaylight.yangtools.yang.parser.builder.api.TypeDefinitionBuilder;
 import org.opendaylight.yangtools.yang.parser.builder.api.UsesNodeBuilder;
-import org.opendaylight.yangtools.yang.parser.builder.util.AbstractDataNodeContainerBuilder;
-import org.opendaylight.yangtools.yang.parser.util.YangParseException;
+import org.opendaylight.yangtools.yang.parser.builder.util.AbstractDocumentedDataNodeContainer;
+import org.opendaylight.yangtools.yang.parser.builder.util.AbstractDocumentedDataNodeContainerBuilder;
 
-public final class AugmentationSchemaBuilderImpl extends AbstractDataNodeContainerBuilder implements
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
+
+public final class AugmentationSchemaBuilderImpl extends AbstractDocumentedDataNodeContainerBuilder implements
         AugmentationSchemaBuilder {
     private final int order;
     private AugmentationSchemaImpl instance;
     private String whenCondition;
-
-    private String description;
-    private String reference;
-    private Status status = Status.CURRENT;
 
     private final String augmentTargetStr;
     private final SchemaPath targetPath;
@@ -68,18 +52,8 @@ public final class AugmentationSchemaBuilderImpl extends AbstractDataNodeContain
     }
 
     @Override
-    public Set<GroupingDefinition> getGroupings() {
-        return Collections.emptySet();
-    }
-
-    @Override
-    public Set<GroupingBuilder> getGroupingBuilders() {
-        return Collections.emptySet();
-    }
-
-    @Override
-    public void addGrouping(final GroupingBuilder grouping) {
-        throw new YangParseException(getModuleName(), grouping.getLine(), "augment can not contains grouping statement");
+    protected String getStatementName() {
+        return "augment";
     }
 
     @Override
@@ -93,11 +67,8 @@ public final class AugmentationSchemaBuilderImpl extends AbstractDataNodeContain
             return instance;
         }
 
-        instance = new AugmentationSchemaImpl(targetPath, order);
-
-        instance.description = description;
-        instance.reference = reference;
-        instance.status = status;
+        buildChildren();
+        instance = new AugmentationSchemaImpl(targetPath, order,this);
 
         Builder parent = getParent();
         if (parent instanceof ModuleBuilder) {
@@ -131,18 +102,6 @@ public final class AugmentationSchemaBuilderImpl extends AbstractDataNodeContain
         }
         instance.whenCondition = whenStmt;
 
-        // CHILD NODES
-        for (DataSchemaNodeBuilder node : addedChildNodes) {
-            childNodes.put(node.getQName(), node.build());
-        }
-        instance.childNodes = ImmutableSet.copyOf(childNodes.values());
-
-        // USES
-        for (UsesNodeBuilder builder : addedUsesNodes) {
-            usesNodes.add(builder.build());
-        }
-        instance.uses = ImmutableSet.copyOf(usesNodes);
-
         // UNKNOWN NODES
         for (UnknownSchemaNodeBuilder b : addedUnknownNodes) {
             unknownNodes.add(b.build());
@@ -170,46 +129,6 @@ public final class AugmentationSchemaBuilderImpl extends AbstractDataNodeContain
     @Override
     public void addWhenCondition(final String whenCondition) {
         this.whenCondition = whenCondition;
-    }
-
-    @Override
-    public Set<TypeDefinitionBuilder> getTypeDefinitionBuilders() {
-        return Collections.emptySet();
-    }
-
-    @Override
-    public void addTypedef(final TypeDefinitionBuilder type) {
-        throw new YangParseException(getModuleName(), type.getLine(), "Augmentation can not contains typedef statement.");
-    }
-
-    @Override
-    public String getDescription() {
-        return description;
-    }
-
-    @Override
-    public void setDescription(final String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String getReference() {
-        return reference;
-    }
-
-    @Override
-    public void setReference(final String reference) {
-        this.reference = reference;
-    }
-
-    @Override
-    public Status getStatus() {
-        return status;
-    }
-
-    @Override
-    public void setStatus(final Status status) {
-        this.status = Preconditions.checkNotNull(status, "status cannot be null");
     }
 
     @Override
@@ -243,7 +162,7 @@ public final class AugmentationSchemaBuilderImpl extends AbstractDataNodeContain
         int result = 1;
         result = prime * result + ((augmentTargetStr == null) ? 0 : augmentTargetStr.hashCode());
         result = prime * result + ((whenCondition == null) ? 0 : whenCondition.hashCode());
-        result = prime * result + ((addedChildNodes == null) ? 0 : addedChildNodes.hashCode());
+        result = prime * result + getChildNodeBuilders().hashCode();
         return result;
     }
 
@@ -273,11 +192,11 @@ public final class AugmentationSchemaBuilderImpl extends AbstractDataNodeContain
         } else if (!whenCondition.equals(other.whenCondition)) {
             return false;
         }
-        if (addedChildNodes == null) {
-            if (other.addedChildNodes != null) {
+        if (other.getChildNodeBuilders() == null) {
+            if (other.getChildNodeBuilders() != null) {
                 return false;
             }
-        } else if (!addedChildNodes.equals(other.addedChildNodes)) {
+        } else if (!getChildNodeBuilders().equals(other.getChildNodeBuilders())) {
             return false;
         }
         return true;
@@ -292,23 +211,19 @@ public final class AugmentationSchemaBuilderImpl extends AbstractDataNodeContain
         copyOf = old;
     }
 
-    private static final class AugmentationSchemaImpl implements AugmentationSchema, NamespaceRevisionAware,
+    private static final class AugmentationSchemaImpl extends AbstractDocumentedDataNodeContainer implements AugmentationSchema, NamespaceRevisionAware,
             Comparable<AugmentationSchemaImpl> {
         private final int order;
         private SchemaPath targetPath;
         private RevisionAwareXPath whenCondition;
-        private ImmutableSet<DataSchemaNode> childNodes;
-        private ImmutableSet<UsesNode> uses;
-        private String description;
-        private String reference;
-        private Status status;
 
         private URI namespace;
         private Date revision;
         private ImmutableList<UnknownSchemaNode> unknownNodes;
         private AugmentationSchema copyOf;
 
-        public AugmentationSchemaImpl(final SchemaPath targetPath, final int order) {
+        public AugmentationSchemaImpl(final SchemaPath targetPath, final int order, final AugmentationSchemaBuilderImpl builder) {
+            super(builder);
             this.targetPath = targetPath;
             this.order = order;
         }
@@ -333,61 +248,8 @@ public final class AugmentationSchemaBuilderImpl extends AbstractDataNodeContain
         }
 
         @Override
-        public Set<DataSchemaNode> getChildNodes() {
-            return childNodes;
-        }
-
-        /**
-         * Always returns an empty set, because augment can not contains
-         * grouping statement.
-         */
-        @Override
-        public Set<GroupingDefinition> getGroupings() {
-            return Collections.emptySet();
-        }
-
-        @Override
-        public Set<UsesNode> getUses() {
-            return uses;
-        }
-
-        /**
-         * Always returns an empty set, because augment can not contains type
-         * definitions.
-         */
-        @Override
-        public Set<TypeDefinition<?>> getTypeDefinitions() {
-            return Collections.emptySet();
-        }
-
-        @Override
-        public String getDescription() {
-            return description;
-        }
-
-        @Override
-        public String getReference() {
-            return reference;
-        }
-
-        @Override
-        public Status getStatus() {
-            return status;
-        }
-
-        @Override
         public List<UnknownSchemaNode> getUnknownSchemaNodes() {
             return unknownNodes;
-        }
-
-        @Override
-        public DataSchemaNode getDataChildByName(final QName name) {
-            return getChildNode(childNodes, name);
-        }
-
-        @Override
-        public DataSchemaNode getDataChildByName(final String name) {
-            return getChildNode(childNodes, name);
         }
 
         @Override
@@ -406,7 +268,7 @@ public final class AugmentationSchemaBuilderImpl extends AbstractDataNodeContain
             int result = 1;
             result = prime * result + ((targetPath == null) ? 0 : targetPath.hashCode());
             result = prime * result + ((whenCondition == null) ? 0 : whenCondition.hashCode());
-            result = prime * result + ((childNodes == null) ? 0 : childNodes.hashCode());
+            result = getChildNodes().hashCode();
             return result;
         }
 
@@ -436,11 +298,7 @@ public final class AugmentationSchemaBuilderImpl extends AbstractDataNodeContain
             } else if (!whenCondition.equals(other.whenCondition)) {
                 return false;
             }
-            if (childNodes == null) {
-                if (other.childNodes != null) {
-                    return false;
-                }
-            } else if (!childNodes.equals(other.childNodes)) {
+            if (!getChildNodes().equals(other.getChildNodes())) {
                 return false;
             }
             return true;
@@ -476,5 +334,4 @@ public final class AugmentationSchemaBuilderImpl extends AbstractDataNodeContain
             return this.order - o.order;
         }
     }
-
 }
