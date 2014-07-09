9c3212674301e0d247f70546fb78b5d001f4a085
tpantelis <tpanteli@brocade.com>
2014-07-09 11:29:02 -0400

    Added RpcResultBuilder class
    Added a DSL-like builder class for creating RpcResult instances. This class will
    deprecate the static Rpcs and RpcErrors classes in the controller.
    
    Also added more javadocs to the RpcResult and RpcError interfaces.
    
    Change-Id: I30cffc68419ed493035ba5fef466096bcec9df70
    Signed-off-by: tpantelis <tpanteli@brocade.com>

---
+++ yang/yang-common/src/test/java/org/opendaylight/yangtools/yang/common/RpcResultBuilderTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2014 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.common;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.RpcError.ErrorSeverity;
+import org.opendaylight.yangtools.yang.common.RpcError.ErrorType;
+
+/**
+ * Unit tests for RpcResultBuilder.
+ *
+ * @author Thomas Pantelis
+ */
+public class RpcResultBuilderTest {
+
+    @Test
+    public void testSuccess() {
+        RpcResult<String> result = RpcResultBuilder.<String>success().withResult( "foo" ).build();
+        verifyRpcResult( result, true, "foo" );
+        assertNotNull( "getErrors returned null", result.getErrors() );
+        assertEquals( "getErrors size", 0, result.getErrors().size() );
+
+        result = RpcResultBuilder.<String>success( "bar" ).build();
+        verifyRpcResult( result, true, "bar" );
+    }
+
+    @Test
+    public void testFailed() {
+        Throwable cause = new Throwable( "mock cause" );
+        RpcResult<String> result = RpcResultBuilder.<String>failed()
+                  .withError( ErrorType.PROTOCOL, "error message 1" )
+                  .withError( ErrorType.APPLICATION, "lock_denied", "error message 2" )
+                  .withError( ErrorType.RPC, "in-use", "error message 3", "my-app-tag", "my-info", cause )
+                  .build();
+        verifyRpcResult( result, false, null );
+        verifyRpcError( result, 0, ErrorSeverity.ERROR, ErrorType.PROTOCOL, "operation-failed",
+                        "error message 1", null, null, null );
+        verifyRpcError( result, 1, ErrorSeverity.ERROR, ErrorType.APPLICATION, "lock_denied",
+                        "error message 2", null, null, null );
+        verifyRpcError( result, 2, ErrorSeverity.ERROR, ErrorType.RPC, "in-use",
+                        "error message 3", "my-app-tag", "my-info", cause );
+        assertEquals( "getErrors size", 3, result.getErrors().size() );
+    }
+
+    @Test
+    public void testWithWarnings() {
+        Throwable cause = new Throwable( "mock cause" );
+        RpcResult<String> result = RpcResultBuilder.<String>success()
+                  .withWarning( ErrorType.APPLICATION, "lock_denied", "message 1" )
+                  .withWarning( ErrorType.RPC, "in-use", "message 2", "my-app-tag", "my-info", cause )
+                  .build();
+        verifyRpcResult( result, true, null );
+        verifyRpcError( result, 0, ErrorSeverity.WARNING, ErrorType.APPLICATION, "lock_denied",
+                        "message 1", null, null, null );
+        verifyRpcError( result, 1, ErrorSeverity.WARNING, ErrorType.RPC, "in-use",
+                        "message 2", "my-app-tag", "my-info", cause );
+        assertEquals( "getErrors size", 2, result.getErrors().size() );
+    }
+
+    void verifyRpcError( RpcResult<?> result, int errorIndex, ErrorSeverity expSeverity,
+            ErrorType expErrorType, String expTag, String expMessage, String expAppTag,
+            String expInfo, Throwable expCause ) {
+
+        List<RpcError> errors = new ArrayList<>( result.getErrors() );
+        assertTrue( "Expected error at index " + errorIndex + " not found",
+                    errorIndex < errors.size() );
+        RpcError error = errors.get( errorIndex );
+        assertEquals( "getSeverity", expSeverity, error.getSeverity() );
+        assertEquals( "getErrorType", expErrorType, error.getErrorType() );
+        assertEquals( "getTag", expTag, error.getTag() );
+        assertEquals( "getMessage", expMessage, error.getMessage() );
+        assertEquals( "getApplicationTag", expAppTag, error.getApplicationTag() );
+        assertEquals( "getInfo", expInfo, error.getInfo() );
+        assertEquals( "getCause", expCause, error.getCause() );
+    }
+
+    void verifyRpcResult( RpcResult<?> result, boolean expSuccess, Object expValue ) {
+        assertEquals( "isSuccessful", expSuccess, result.isSuccessful() );
+        assertEquals( "getResult", expValue, result.getResult() );
+    }
+}
