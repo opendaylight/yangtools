082aa80b9bb452462ea458e773fee17db18717c9
Robert Varga <rovarga@cisco.com>
2014-07-09 20:39:04 +0200

    Move XmlDocumentUtils.writeValueByType()
    Now that we're using XMLStreamWriter, the only user lives in DomUtils,
    so let's move the functionality over there.
    
    Change-Id: Ia3cb69f5222ffb9c645bc91f79d1aa908b4fe971
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/transform/dom/DomUtils.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/transform/dom/DomUtils.java
@@ -7,6 +7,9 @@
  */
 package org.opendaylight.yangtools.yang.data.impl.schema.transform.dom;
 
+import com.google.common.collect.LinkedListMultimap;
+import com.google.common.collect.Lists;
+
 import java.net.URI;
 import java.util.HashMap;
 import java.util.List;
@@ -15,36 +18,40 @@ import java.util.Map;
 import javax.xml.XMLConstants;
 
 import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.impl.codec.TypeDefinitionAwareCodec;
+import org.opendaylight.yangtools.yang.data.impl.codec.xml.InstanceIdentifierForXmlCodec;
 import org.opendaylight.yangtools.yang.data.impl.codec.xml.XmlCodecProvider;
 import org.opendaylight.yangtools.yang.data.impl.codec.xml.XmlDocumentUtils;
+import org.opendaylight.yangtools.yang.data.impl.codec.xml.XmlUtils;
 import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.IdentityrefTypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.InstanceIdentifierTypeDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import com.google.common.collect.LinkedListMultimap;
-import com.google.common.collect.Lists;
+public final class DomUtils {
 
-public class DomUtils {
-
-    private DomUtils() {
-    }
-
+    private static final Logger LOG = LoggerFactory.getLogger(DomUtils.class);
     private static final XmlCodecProvider DEFAULT_XML_VALUE_CODEC_PROVIDER = new XmlCodecProvider() {
-
         @Override
-        public TypeDefinitionAwareCodec<Object, ? extends TypeDefinition<?>> codecFor(TypeDefinition<?> baseType) {
+        public TypeDefinitionAwareCodec<Object, ? extends TypeDefinition<?>> codecFor(final TypeDefinition<?> baseType) {
             return TypeDefinitionAwareCodec.from(baseType);
         }
     };
+
+    private DomUtils() {
+    }
 
     public static XmlCodecProvider defaultValueCodecProvider() {
         return DEFAULT_XML_VALUE_CODEC_PROVIDER;
     }
 
-    public static Object parseXmlValue(Element xml, XmlCodecProvider codecProvider, TypeDefinition<?> type) {
+    public static Object parseXmlValue(final Element xml, final XmlCodecProvider codecProvider, final TypeDefinition<?> type) {
         TypeDefinitionAwareCodec<Object, ? extends TypeDefinition<?>> codec = codecProvider.codecFor(type);
 
         String text = xml.getTextContent();
@@ -60,11 +67,55 @@ public class DomUtils {
         return value;
     }
 
-    public static void serializeXmlValue(Element itemEl, TypeDefinition<? extends TypeDefinition<?>> type, XmlCodecProvider codecProvider, Object value) {
-        XmlDocumentUtils.writeValueByType(itemEl, type, codecProvider, value);
+    public static void serializeXmlValue(final Element element, final TypeDefinition<? extends TypeDefinition<?>> type, final XmlCodecProvider codecProvider, final Object value) {
+        TypeDefinition<?> baseType = XmlUtils.resolveBaseTypeFrom(type);
+        if (baseType instanceof IdentityrefTypeDefinition) {
+            if (value instanceof QName) {
+                QName qname = (QName) value;
+                String prefix = "x";
+                if (qname.getPrefix() != null && !qname.getPrefix().isEmpty()) {
+                    prefix = qname.getPrefix();
+                }
+                element.setAttribute("xmlns:" + prefix, qname.getNamespace().toString());
+                element.setTextContent(prefix + ":" + qname.getLocalName());
+            } else {
+                LOG.debug("Value of {}:{} is not instance of QName but is {}", baseType.getQName().getNamespace(),
+                        baseType.getQName().getLocalName(), value != null ? value.getClass() : "null");
+                if (value != null) {
+                    element.setTextContent(String.valueOf(value));
+                }
+            }
+        } else if (baseType instanceof InstanceIdentifierTypeDefinition) {
+            if (value instanceof InstanceIdentifier) {
+                InstanceIdentifierForXmlCodec.serialize((InstanceIdentifier)value, element);
+            } else {
+                LOG.debug("Value of {}:{} is not instance of InstanceIdentifier but is {}", baseType.getQName()
+                        .getNamespace(), //
+                        baseType.getQName().getLocalName(), value != null ? value.getClass() : "null");
+                if (value != null) {
+                    element.setTextContent(String.valueOf(value));
+                }
+            }
+        } else {
+            if (value != null) {
+                final TypeDefinitionAwareCodec<Object, ?> codec = codecProvider.codecFor(baseType);
+                if (codec != null) {
+                    try {
+                        final String text = codec.serialize(value);
+                        element.setTextContent(text);
+                    } catch (ClassCastException e) {
+                        LOG.error("Provided node value {} did not have type {} required by mapping. Using stream instead.", value, baseType, e);
+                        element.setTextContent(String.valueOf(value));
+                    }
+                } else {
+                    LOG.error("Failed to find codec for {}, falling back to using stream", baseType);
+                    element.setTextContent(String.valueOf(value));
+                }
+            }
+        }
     }
 
-    public static LinkedListMultimap<QName, Element> mapChildElementsForSingletonNode(Element node) {
+    public static LinkedListMultimap<QName, Element> mapChildElementsForSingletonNode(final Element node) {
         List<Element> childNodesCollection = Lists.newArrayList();
         NodeList childNodes = node.getChildNodes();
         for (int i = 0; i < childNodes.getLength(); i++) {
@@ -76,7 +127,7 @@ public class DomUtils {
         return mapChildElements(childNodesCollection);
     }
 
-    public static LinkedListMultimap<QName, Element> mapChildElements(Iterable<Element> childNodesCollection) {
+    public static LinkedListMultimap<QName, Element> mapChildElements(final Iterable<Element> childNodesCollection) {
         LinkedListMultimap<QName, Element> mappedChildElements = LinkedListMultimap.create();
 
         for (Element element : childNodesCollection) {
@@ -88,7 +139,7 @@ public class DomUtils {
     }
 
 
-    public static Map<QName, String> toAttributes(NamedNodeMap xmlAttributes) {
+    public static Map<QName, String> toAttributes(final NamedNodeMap xmlAttributes) {
         Map<QName, String> attributes = new HashMap<>();
 
         for (int i = 0; i < xmlAttributes.getLength(); i++) {
