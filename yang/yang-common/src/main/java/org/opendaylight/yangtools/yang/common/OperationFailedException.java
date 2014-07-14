701adc7aee3280f46b819a62f12324e09b2bc418
Thomas Pantelis <tpanteli@brocade.com>
2014-07-14 12:13:08 -0400

    Bug 1392: New yang-common/util classes
    Needed for https://git.opendaylight.org/gerrit/#/c/9178/
    
    OperationFailedException:
    
       Generalized exception class that contains an RpcError list.
    
    MappingCheckedFuture:
    
       Futures#addCallback and Futures#transform call Future#get but
       CheckedFuture doesn't override the get methods to translate exceptions
       to the checked type. Thus you don't get the checked exception when using
       those Futures methods. This kind of defeats the purpose of CheckedFuture.
    
       The new MappingCheckedFuture class overrides the get methods to translate exceptions.
    
    ExceptionMapper:
    
       Generalized class for use with CheckedFuture to translate exceptions to a
       specified exception type.
    
    Change-Id: Idb4c7e8b5c2cf17795de2f3af4278989ac7bd116
    Signed-off-by: tpantelis <tpanteli@brocade.com>
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/OperationFailedException.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2014 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.common;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.opendaylight.yangtools.yang.common.RpcError;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
+import org.opendaylight.yangtools.yang.common.RpcError.ErrorType;
+
+import com.google.common.base.Objects;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+
+/**
+ * A general base exception for an operation failure.
+ *
+ * @author Thomas Pantelis
+ */
+public class OperationFailedException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    private final List<RpcError> errorList;
+
+    /**
+     * Constructs a new instance with the specified detail message and errors.
+     *
+     * @param message the detail message
+     * @param errors {@link RpcError} instances that provide additional error information about
+     *               this exception
+     */
+    public OperationFailedException(final String message, final RpcError... errors) {
+        this(message, null, errors);
+    }
+
+    /**
+     * Constructs a new instance with the specified detail message, cause and errors.
+     *
+     * @param message the detail message
+     * @param cause the cause
+     * @param errors {@link RpcError} instances that provide additional error information about
+     *               this exception
+     */
+    public OperationFailedException(final String message, final Throwable cause,
+                                    final RpcError... errors) {
+        super(Preconditions.checkNotNull(message), cause);
+
+        if( errors != null && errors.length > 0 ) {
+            errorList = ImmutableList.<RpcError>copyOf( Arrays.asList( errors ) );
+        }
+        else {
+            // Add a default RpcError.
+            errorList = ImmutableList.of(RpcResultBuilder.newError(ErrorType.APPLICATION, null,
+                    getMessage(), null, null, getCause()));
+        }
+    }
+
+    /**
+     * Returns additional error information about this exception.
+     *
+     * @return a List of RpcErrors. There is always at least one RpcError.
+     */
+    public List<RpcError> getErrorList() {
+        return errorList;
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper( this ).add( "message", getMessage() )
+                .add( "errorList", errorList ).toString();
+    }
+}
