69c16eadc81bfbb07287ba9878f6685347359bd8
tpantelis <tpanteli@brocade.com>
2014-07-26 20:11:33 -0400

    Bug 1430: New common/util concurrent classes
    AsyncNotifyingListeningExecutorService:
        A ListeningExecutorService that executes ListenableFuture callback
        Runnables on another executor to off-load them.
    
    AsyncNotifyingListenableFutureTask
        Listenable FutureTask used by AsyncNotifyingListeningExecutorService.
    
    QueuedNotificationManager
        Manages queuing and dispatching notifications for multiple listeners
        concurrently. Notifications are queued on a per-listener basis and
        dispatched serially to each listener via an Executor.
    
    FastThreadPoolExecutor
        Executor with a bounded queue that provides fast task execution time
        by favoring creating threads over queuing.
    
    CachedThreadPoolExecutor
        Executor with a bounded queue that favors reusing existing idle
        threads over creating new threads at the expense of slower task
        execution time.
    
    SpecialExecutors
        Factory methods for creating Fast/CachedThreadPoolExecutor
        instances.
    
    Change-Id: Ie0d2f4d4e0561976c6956d551e386da78631aafa
    Signed-off-by: tpantelis <tpanteli@brocade.com>

---
+++ common/util/src/main/java/org/opendaylight/yangtools/util/concurrent/QueuedNotificationManager.java
@@ -0,0 +1,275 @@
+/*
+ * Copyright (c) 2014 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.util.concurrent;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.Executor;
+
+import javax.annotation.concurrent.GuardedBy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Preconditions;
+
+/**
+ * This class manages queuing and dispatching notifications for multiple listeners concurrently.
+ * Notifications are queued on a per-listener basis and dispatched serially to each listener via an
+ * {@link Executor}.
+ * <p>
+ * This class optimizes its memory footprint by only allocating and maintaining a queue and executor
+ * task for a listener when there are pending notifications. On the first notification(s), a queue
+ * is created and a task is submitted to the executor to dispatch the queue to the associated
+ * listener. Any subsequent notifications that occur before all previous notifications have been
+ * dispatched are appended to the existing queue. When all notifications have been dispatched, the
+ * queue and task are discarded.
+ *
+ * @author Thomas Pantelis
+ *
+ * @param <L> the listener type
+ * @param <N> the notification type
+ */
+public class QueuedNotificationManager<L,N> implements NotificationManager<L,N> {
+
+    /**
+     * Interface implemented by clients that does the work of invoking listeners with notifications.
+     *
+     * @author Thomas Pantelis
+     *
+     * @param <L> the listener type
+     * @param <N> the notification type
+     */
+    public interface Invoker<L,N> {
+
+        /**
+         * Called to invoke a listener with a notification.
+         *
+         * @param listener the listener to invoke
+         * @param notification the notification to send
+         */
+        void invokeListener( L listener, N notification );
+    }
+
+    private static final Logger LOG = LoggerFactory.getLogger( QueuedNotificationManager.class );
+
+    private final Executor executor;
+    private final Invoker<L,N> listenerInvoker;
+
+    private final ConcurrentMap<ListenerKey<L>,NotificationTask<L,N>>
+                                                          listenerCache = new ConcurrentHashMap<>();
+
+    /**
+     * Constructor.
+     *
+     * @param executor the {@link Executor} to use for notification tasks
+     * @param listenerInvoker the {@link Invoker} to use for invokings listeners
+     */
+    public QueuedNotificationManager( Executor executor, Invoker<L,N> listenerInvoker ) {
+        this.executor = Preconditions.checkNotNull( executor );
+        this.listenerInvoker = Preconditions.checkNotNull( listenerInvoker );
+    }
+
+    /* (non-Javadoc)
+     * @see org.opendaylight.yangtools.util.concurrent.NotificationManager#addNotification(L, N)
+     */
+    @Override
+    public void addNotification( final L listener, final N notification ) {
+        if( notification == null ) {
+            return;
+        }
+
+        addNotifications( listener, Arrays.asList( notification ) );
+    }
+
+    /* (non-Javadoc)
+     * @see org.opendaylight.yangtools.util.concurrent.NotificationManager#addNotifications(L, java.util.Collection)
+     */
+    @Override
+    public void addNotifications( final L listener, final Collection<N> notifications ) {
+
+        if( notifications == null || listener == null ) {
+            return;
+        }
+
+        ListenerKey<L> key = new ListenerKey<>( listener );
+        NotificationTask<L,N> newNotificationTask = null;
+
+        // Keep looping until we are either able to add a new NotificationTask or are able to
+        // add our notifications to an existing NotificationTask. Eventually one or the other
+        // will occur.
+
+        while( true ) {
+            NotificationTask<L,N> existingTask = listenerCache.get( key );
+
+            if( existingTask == null || !existingTask.addNotifications( notifications ) ) {
+
+                // Either there's no existing task or we couldn't add our notifications to the
+                // existing one because it's in the process of exiting and removing itself from the
+                // cache. Either way try to put a new task in the cache. If we can't put then either
+                // the existing one is still there and hasn't removed itself quite yet or some other
+                // concurrent thread beat us to the put. Either way we'll loop back up and try again.
+
+                if( newNotificationTask == null ) {
+                    newNotificationTask = new NotificationTask<>(
+                                               key, notifications, listenerCache, listenerInvoker );
+                }
+
+                existingTask = listenerCache.putIfAbsent( key, newNotificationTask );
+                if( existingTask == null ) {
+
+                    // We were able to put our new task - now submit it to the executor and
+                    // we're done.
+
+                    executor.execute( newNotificationTask );
+                    break;
+                }
+            } else {
+
+                // We were able to add our notifications to an existing task so we're done.
+
+                break;
+            }
+        }
+    }
+
+    /**
+     * Used as the listenerCache map key. We key by listener reference identity hashCode/equals.
+     * Since we don't know anything about the listener class implementations and we're mixing
+     * multiple listener class instances in the same map, this avoids any potential issue with an
+     * equals implementation that just blindly casts the other Object to compare instead of checking
+     * for instanceof.
+     */
+    private static class ListenerKey<L> {
+
+        private final L listener;
+
+        public ListenerKey( L listener ) {
+            this.listener = listener;
+        }
+
+        L getListener() {
+            return listener;
+        }
+
+        @Override
+        public int hashCode() {
+            return System.identityHashCode( listener );
+        }
+
+        @Override
+        public boolean equals( Object obj ) {
+            ListenerKey<?> other = (ListenerKey<?>) obj;
+            return listener == other.listener;
+        }
+    }
+
+    /**
+     * Executor task for a single listener that queues notifications and sends them serially to the
+     * listener.
+     */
+    private static class NotificationTask<L,N> implements Runnable {
+
+        @GuardedBy("notificationQueue")
+        private final Queue<N> notificationQueue;
+
+        private volatile boolean done = false;
+
+        private final Invoker<L,N> listenerInvoker;
+        private final ListenerKey<L> listenerKey;
+        private final ConcurrentMap<ListenerKey<L>,NotificationTask<L,N>> listenerCache;
+
+        NotificationTask( ListenerKey<L> listenerKey, Collection<N> notifications,
+                ConcurrentMap<ListenerKey<L>,NotificationTask<L,N>> listenerCache,
+                Invoker<L, N> listenerInvoker) {
+
+            this.listenerKey = listenerKey;
+            this.notificationQueue = new LinkedList<>( notifications );
+            this.listenerInvoker = listenerInvoker;
+            this.listenerCache = listenerCache;
+        }
+
+        boolean addNotifications( Collection<N> notifications ) {
+
+            boolean result = false;
+            synchronized( notificationQueue ) {
+
+                // Check the done flag - if true then #run is in the process of exiting so return
+                // false to indicate such. Otherwise, add to the queue. We're thread-safe here
+                // because we hold the notificationQueue lock.
+
+                if( !done ) {
+                    notificationQueue.addAll( notifications );
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                // Loop until we've dispatched all the notifications in the queue.
+
+                while( true ) {
+                    N notification;
+                    synchronized( notificationQueue ) {
+
+                        // Get the notification at the head of the queue. If there's none, set done
+                        // to true and exit. Once done is set and we leave the sync block,
+                        // calls to #addNotifications will fail.
+
+                        notification = notificationQueue.poll();
+                        if( notification == null ) {
+                            done = true;
+                            break;
+                        }
+                    }
+
+                    notifyListener( notification );
+                }
+            } finally {
+
+                // We're exiting, gracefully or not - either way make sure we always remove
+                // ourselves from  the cache.
+
+                listenerCache.remove( listenerKey );
+            }
+        }
+
+        private void notifyListener( N notification ) {
+
+            try {
+
+                listenerInvoker.invokeListener( listenerKey.getListener(), notification );
+
+            } catch( RuntimeException e ) {
+
+                // We'll let a RuntimeException from the listener slide and keep sending any
+                // remaining notifications.
+
+                LOG.error( "Error notifying listener " + listenerKey.getListener().getClass(), e );
+
+            } catch( Error e ) {
+
+                // A JVM Error is severe - best practice is to throw them up the chain. Set done to
+                // true so no new notifications can be added to this task as we're about to bail.
+
+                done = true;
+                throw e;
+            }
+        }
+    }
+}
---
+++ common/util/src/main/java/org/opendaylight/yangtools/util/concurrent/SpecialExecutors.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2014 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.util.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.opendaylight.yangtools.util.ExecutorServiceUtil;
+
+/**
+ * Factory methods for creating {@link ExecutorService} instances with specific configurations.
+
+ * @author Thomas Pantelis
+ */
+public class SpecialExecutors {
+
+    /**
+     * Creates an ExecutorService with a specified bounded queue capacity that favors creating new
+     * threads over queuing, as the former is faster, so threads will only be reused when the thread
+     * limit is exceeded and tasks are queued. If the maximum queue capacity is reached, subsequent
+     * tasks will be rejected.
+     * <p>
+     * For example, if the maximum number of threads is 100 and 100 short-lived tasks are submitted
+     * within say 10 seconds, then 100 threads will be created and used - previously constructed
+     * idle threads will not be reused. This provides the fastest execution of the 100 tasks at the
+     * expense of memory and thread resource overhead. Therefore it is advisable to specify a
+     * relatively small thread limit (probably no more than 50).
+     * <p>
+     * Threads that have not been used for 15 seconds are terminated and removed from the pool.
+     * Thus, a pool that remains idle for long enough will not consume any resources.
+     * <p>
+     * If you need an executor with less memory and thread resource overhead where slower execution
+     * time is acceptable, consider using {@link #newBoundedCachedThreadPool }.
+     *
+     * @param maximumPoolSize
+     *            the maximum number of threads to allow in the pool. Threads will terminate after
+     *            being idle for 15 seconds.
+     * @param maximumQueueSize
+     *            the capacity of the queue.
+     * @param threadPrefix
+     *            the name prefix for threads created by this executor.
+     * @return a new ExecutorService with the specified configuration.
+     */
+    public static ExecutorService newBoundedFastThreadPool( int maximumPoolSize,
+            int maximumQueueSize, String threadPrefix ) {
+        return new FastThreadPoolExecutor( maximumPoolSize, maximumQueueSize, threadPrefix );
+    }
+
+    /**
+     * Creates an ExecutorService similar to {@link #newBoundedFastThreadPool } except that it
+     * handles rejected tasks by performing a blocking put to the queue. Therefore if the queue is
+     * full, the caller submitting the task will be blocked until space becomes available on the
+     * queue. In this manner, tasks are never rejected.
+     *
+     * @param maximumPoolSize
+     *            the maximum number of threads to allow in the pool. Threads will terminate after
+     *            being idle for 15 seconds.
+     * @param maximumQueueSize
+     *            the capacity of the queue.
+     * @param threadPrefix
+     *            the name prefix for threads created by this executor.
+     * @return a new ExecutorService with the specified configuration.
+     */
+    public static ExecutorService newBlockingBoundedFastThreadPool( int maximumPoolSize,
+            int maximumQueueSize, String threadPrefix ) {
+
+        FastThreadPoolExecutor executor =
+                new FastThreadPoolExecutor( maximumPoolSize, maximumQueueSize, threadPrefix );
+        executor.setRejectedExecutionHandler( ExecutorServiceUtil.waitInQueueExecutionHandler() );
+        return executor;
+    }
+
+    /**
+     * Creates an ExecutorService with a specified bounded queue capacity that favors reusing
+     * previously constructed threads, when they are available, over creating new threads. When a
+     * task is submitted, if no existing thread is available, a new thread will be created and added
+     * to the pool. If there is an existing idle thread available, the task will be handed to that
+     * thread to execute. If the specified maximum thread limit is reached, subsequent tasks will be
+     * queued and will execute as threads become available. If the maximum queue capacity is
+     * reached, subsequent tasks will be rejected.
+     * <p>
+     * Threads that have not been used for sixty seconds are terminated and removed from the pool.
+     * Thus, a pool that remains idle for long enough will not consume any resources.
+     * <p>
+     * By reusing threads when possible, this executor optimizes for reduced memory and thread
+     * resource overhead at the expense of execution time.
+     * <p>
+     * If you need an executor with faster execution time where increased memory and thread resource
+     * overhead is acceptable, consider using {@link #newBoundedFastThreadPool }.
+     *
+     * @param maximumPoolSize
+     *            the maximum number of threads to allow in the pool. Threads will terminate after
+     *            being idle for 60 seconds.
+     * @param maximumQueueSize
+     *            the capacity of the queue.
+     * @param threadPrefix
+     *            the name prefix for threads created by this executor.
+     * @return a new ExecutorService with the specified configuration.
+     */
+    public static ExecutorService newBoundedCachedThreadPool( int maximumPoolSize,
+            int maximumQueueSize, String threadPrefix ) {
+        return new CachedThreadPoolExecutor( maximumPoolSize, maximumQueueSize, threadPrefix );
+    }
+
+    /**
+     * Creates an ExecutorService similar to {@link #newBoundedCachedThreadPool } except that it
+     * handles rejected tasks by performing a blocking put to the queue. Therefore if the queue is
+     * full, the caller submitting the task will be blocked until space becomes available on the
+     * queue. In this manner, tasks are never rejected.
+     *
+     * @param maximumPoolSize
+     *            the maximum number of threads to allow in the pool. Threads will terminate after
+     *            being idle for 60 seconds.
+     * @param maximumQueueSize
+     *            the capacity of the queue.
+     * @param threadPrefix
+     *            the name prefix for threads created by this executor.
+     * @return a new ExecutorService with the specified configuration.
+     */
+    public static ExecutorService newBlockingBoundedCachedThreadPool( int maximumPoolSize,
+            int maximumQueueSize, String threadPrefix ) {
+
+        CachedThreadPoolExecutor executor =
+                new CachedThreadPoolExecutor( maximumPoolSize, maximumQueueSize, threadPrefix );
+        executor.setRejectedExecutionHandler( ExecutorServiceUtil.waitInQueueExecutionHandler() );
+        return executor;
+    }
+
+    /**
+     * Creates an ExecutorService that uses a single worker thread operating off a bounded queue
+     * with the specified capacity. Tasks are guaranteed to execute sequentially, and no more than
+     * one task will be active at any given time. If the maximum queue capacity is reached,
+     * subsequent tasks will be rejected.
+     *
+     * @param maximumQueueSize
+     *            the capacity of the queue.
+     * @param threadPrefix
+     *            the name prefix for the thread created by this executor.
+     * @return a new ExecutorService with the specified configuration.
+     */
+    public static ExecutorService newBoundedSingleThreadExecutor( int maximumQueueSize,
+            String threadPrefix ) {
+        return new FastThreadPoolExecutor( 1, maximumQueueSize, Long.MAX_VALUE, TimeUnit.SECONDS,
+                threadPrefix );
+    }
+}
