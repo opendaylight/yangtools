735ccf62473117b9bc2b19470f5db408ff44dfb2
Lukas Sedlak <lsedlak@cisco.com>
2014-07-29 10:39:43 +0200

    Fixed incorrect test location.
    Fixed incorrect test file location.
    Fixed Ignored test in ClassCodeGeneraterTest to test negative case scenario instead of being ingored.
    
    Signed-off-by: Lukas Sedlak <lsedlak@cisco.com>
    Change-Id: Ia059c1578f0d61e97cf98f6dd4b0b191d2273dc1

--- code-generator/binding-generator-impl/src/test/java/org/opendaylight/yangtools/sal/binding/generator/impl/GenerateInnerClassForBitsAndUnionInLeavesTest.java
+++ code-generator/binding-generator-impl/src/test/java/org/opendaylight/yangtools/sal/binding/generator/impl/GenerateInnerClassForBitsAndUnionInLeavesTest.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.yangtools.sal.java.api.generator.test;
+package org.opendaylight.yangtools.sal.binding.generator.impl;
 
 import static org.junit.Assert.assertTrue;
 
