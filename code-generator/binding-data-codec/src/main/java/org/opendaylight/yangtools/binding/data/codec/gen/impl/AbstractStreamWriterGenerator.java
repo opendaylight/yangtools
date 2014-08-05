7da29caaf567b04323fa69da5be01c3383573f39
Robert Varga <rovarga@cisco.com>
2014-08-05 18:30:05 +0200

    BUG-1487: refactor StreamWriterGenerator
    This is a reorganization of the StreamWriterGenerator such that it is
    properly split up into classes, with contracts clarified. It also
    prevents outside subclassing.
    
    Change-Id: I492d73a082c24c03b8d17f3868e4a497c8f64b7b
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- code-generator/binding-data-codec/src/main/java/org/opendaylight/yangtools/binding/data/codec/gen/impl/AbstractStreamWriterGenerator.java
+++ code-generator/binding-data-codec/src/main/java/org/opendaylight/yangtools/binding/data/codec/gen/impl/AbstractStreamWriterGenerator.java
@@ -22,17 +22,13 @@ import javassist.CtMethod;
 import javassist.Modifier;
 import javassist.NotFoundException;
 
-import org.opendaylight.yangtools.binding.data.codec.gen.spi.AbstractSource;
 import org.opendaylight.yangtools.binding.data.codec.gen.spi.StaticConstantDefinition;
 import org.opendaylight.yangtools.binding.data.codec.util.AugmentableDispatchSerializer;
 import org.opendaylight.yangtools.binding.generator.util.Types;
-import org.opendaylight.yangtools.sal.binding.generator.api.ClassLoadingStrategy;
-import org.opendaylight.yangtools.sal.binding.generator.impl.GeneratedClassLoadingStrategy;
 import org.opendaylight.yangtools.sal.binding.generator.util.BindingRuntimeContext;
 import org.opendaylight.yangtools.sal.binding.generator.util.ClassCustomizer;
 import org.opendaylight.yangtools.sal.binding.generator.util.JavassistUtils;
 import org.opendaylight.yangtools.sal.binding.model.api.GeneratedType;
-import org.opendaylight.yangtools.sal.binding.model.api.Type;
 import org.opendaylight.yangtools.util.ClassLoaderUtils;
 import org.opendaylight.yangtools.yang.binding.BindingStreamEventWriter;
 import org.opendaylight.yangtools.yang.binding.DataContainer;
@@ -47,10 +43,8 @@ import org.opendaylight.yangtools.yang.model.api.ListSchemaNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-abstract class AbstractStreamWriterGenerator implements DataObjectSerializerGenerator {
+abstract class AbstractStreamWriterGenerator extends AbstractGenerator implements DataObjectSerializerGenerator {
     private static final Logger LOG = LoggerFactory.getLogger(AbstractStreamWriterGenerator.class);
-    private static final ClassLoadingStrategy STRATEGY = GeneratedClassLoadingStrategy.getTCCLClassLoadingStrategy();
-    private static final String SERIALIZER_SUFFIX = "$StreamWriter";
 
     protected static final String SERIALIZE_METHOD_NAME = "serialize";
     protected static final AugmentableDispatchSerializer AUGMENTABLE = new AugmentableDispatchSerializer();
@@ -86,8 +80,17 @@ abstract class AbstractStreamWriterGenerator implements DataObjectSerializerGene
         this.context = runtime;
     }
 
-    private final class SerializerImplementationLoader extends
-            CacheLoader<Class<?>, Class<? extends DataObjectSerializerImplementation>> {
+    @Override
+    protected final String loadSerializerFor(final Class<?> cls) {
+        return implementations.getUnchecked(cls).getName();
+    }
+
+    private final class SerializerImplementationLoader extends CacheLoader<Class<?>, Class<? extends DataObjectSerializerImplementation>> {
+        private static final String SERIALIZER_SUFFIX = "$StreamWriter";
+
+        private String getSerializerName(final Class<?> type) {
+            return type.getName() + SERIALIZER_SUFFIX;
+        }
 
         @Override
         public Class<? extends DataObjectSerializerImplementation> load(final Class<?> type) throws Exception {
@@ -107,28 +110,17 @@ abstract class AbstractStreamWriterGenerator implements DataObjectSerializerGene
 
         private Class<? extends DataObjectSerializerImplementation> loadFromClassPoolOrGenerate(final Class<?> type,
                 final String serializerName) throws CannotCompileException {
-            CtClass poolClass;
-            DataObjectSerializerSource source = generateEmitterSource(type, serializerName);
-            poolClass = generateEmitter0(source, serializerName);
+            final DataObjectSerializerSource source = generateEmitterSource(type, serializerName);
+            final CtClass poolClass = generateEmitter0(source, serializerName);
             @SuppressWarnings("unchecked")
-            Class<? extends DataObjectSerializerImplementation> cls = poolClass.toClass(type.getClassLoader(), type.getProtectionDomain());
+            final Class<? extends DataObjectSerializerImplementation> cls = poolClass.toClass(type.getClassLoader(), type.getProtectionDomain());
+
             initStaticConstants(cls,source);
             return cls;
         }
-
-        private void initStaticConstants(final Class<? extends DataObjectSerializerImplementation> cls, final DataObjectSerializerSource source) {
-            for(StaticConstantDefinition constant : source.getStaticConstants()) {
-                try {
-                    Field field = cls.getDeclaredField(constant.getName());
-                    field.set(null, constant.getValue());
-                } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-                    throw new IllegalStateException("Could not initialize expected constant",e);
-                }
-            }
-        }
     }
 
-    protected DataObjectSerializerSource generateEmitterSource(final Class<?> type, final String serializerName) {
+    private DataObjectSerializerSource generateEmitterSource(final Class<?> type, final String serializerName) {
         Types.typeForClass(type);
         Entry<GeneratedType, Object> typeWithSchema = context.getTypeWithSchema(type);
         GeneratedType generatedType = typeWithSchema.getKey();
@@ -181,12 +173,14 @@ abstract class AbstractStreamWriterGenerator implements DataObjectSerializerGene
         return product;
     }
 
-    @SuppressWarnings("unchecked")
-    protected Class<? extends DataContainer> loadClass(final Type childType) {
-        try {
-            return (Class<? extends DataContainer>) STRATEGY.loadClass(childType);
-        } catch (ClassNotFoundException e) {
-            throw new IllegalStateException("Could not load referenced class ",e);
+    private static void initStaticConstants(final Class<? extends DataObjectSerializerImplementation> cls, final DataObjectSerializerSource source) {
+        for(StaticConstantDefinition constant : source.getStaticConstants()) {
+            try {
+                Field field = cls.getDeclaredField(constant.getName());
+                field.set(null, constant.getValue());
+            } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+                throw new IllegalStateException("Could not initialize expected constant",e);
+            }
         }
     }
 
@@ -202,7 +196,7 @@ abstract class AbstractStreamWriterGenerator implements DataObjectSerializerGene
      * @param node Schema of container
      * @return Source for container node writer
      */
-    abstract DataObjectSerializerSource generateContainerSerializer(GeneratedType type, ContainerSchemaNode node);
+    protected abstract DataObjectSerializerSource generateContainerSerializer(GeneratedType type, ContainerSchemaNode node);
 
     /**
      * Generates serializer source for supplied case node,
@@ -216,7 +210,7 @@ abstract class AbstractStreamWriterGenerator implements DataObjectSerializerGene
      * @param node Schema of case
      * @return Source for case node writer
      */
-    abstract DataObjectSerializerSource generateCaseSerializer(GeneratedType type, ChoiceCaseNode node);
+    protected abstract DataObjectSerializerSource generateCaseSerializer(GeneratedType type, ChoiceCaseNode node);
 
     /**
      * Generates serializer source for supplied list node,
@@ -230,7 +224,7 @@ abstract class AbstractStreamWriterGenerator implements DataObjectSerializerGene
      * @param node Schema of list
      * @return Source for list node writer
      */
-    abstract DataObjectSerializerSource generateMapEntrySerializer(GeneratedType type, ListSchemaNode node);
+    protected abstract DataObjectSerializerSource generateMapEntrySerializer(GeneratedType type, ListSchemaNode node);
 
     /**
      * Generates serializer source for supplied list node,
@@ -244,7 +238,7 @@ abstract class AbstractStreamWriterGenerator implements DataObjectSerializerGene
      * @param node Schema of list
      * @return Source for list node writer
      */
-    abstract DataObjectSerializerSource generateUnkeyedListEntrySerializer(GeneratedType type, ListSchemaNode node);
+    protected abstract DataObjectSerializerSource generateUnkeyedListEntrySerializer(GeneratedType type, ListSchemaNode node);
 
     /**
      * Generates serializer source for supplied augmentation node,
@@ -258,115 +252,6 @@ abstract class AbstractStreamWriterGenerator implements DataObjectSerializerGene
      * @param node Schema of augmentation
      * @return Source for augmentation node writer
      */
-    abstract DataObjectSerializerSource generateSerializer(GeneratedType type, AugmentationSchema schema);
-
-    protected static String getSerializerName(final Class<?> type) {
-        return type.getName() + SERIALIZER_SUFFIX;
-    }
-
-    protected abstract class DataObjectSerializerSource extends AbstractSource {
-
-        protected static final String STREAM = "_stream";
-        protected static final String ITERATOR = "_iterator";
-        protected static final String CURRENT = "_current";
-        protected static final String REGISTRY = "_registry";
-
-        /**
-         * Returns body of static serialize method.
-         *
-         * <ul>
-         * <li> {@link DataObjectSerializerRegistry} - registry of serializers
-         * <li> {@link DataObject} - object to be serialized
-         * <li> {@link BindingStreamEventWriter} - writer to which events should be serialized.
-         * </ul>
-         *
-         * @return Valid javassist code describing static serialization body.
-         */
-        protected abstract CharSequence getSerializerBody();
-
-        protected final CharSequence leafNode(final String localName, final CharSequence value) {
-            return invoke(STREAM, "leafNode", escape(localName), value);
-        }
-
-        protected final CharSequence startLeafSet(final String localName,final CharSequence expected) {
-            return invoke(STREAM, "startLeafSet", escape(localName),expected);
-        }
-
-        protected final CharSequence leafSetEntryNode(final CharSequence value) {
-            return invoke(STREAM, "leafSetEntryNode", value);
-
-        }
-
-        protected final CharSequence startContainerNode(final CharSequence type, final CharSequence expected) {
-            return invoke(STREAM, "startContainerNode", (type),expected);
-        }
-
-        protected final  CharSequence escape(final String localName) {
-            return '"'+localName+'"';
-        }
-
-        protected final CharSequence startUnkeyedList(final CharSequence type, final CharSequence expected) {
-            return invoke(STREAM, "startUnkeyedList", (type),expected);
-        }
-
-        protected final CharSequence startUnkeyedListItem(final CharSequence expected) {
-            return invoke(STREAM, "startUnkeyedListItem",expected);
-        }
-
-        protected final CharSequence startMapNode(final CharSequence type,final CharSequence expected) {
-            return invoke(STREAM, "startMapNode", (type),expected);
-        }
-
-        protected final CharSequence startOrderedMapNode(final CharSequence type,final CharSequence expected) {
-            return invoke(STREAM, "startOrderedMapNode", (type),expected);
-        }
-
-        protected final CharSequence startMapEntryNode(final CharSequence key, final CharSequence expected) {
-            return invoke(STREAM,"startMapEntryNode",key,expected);
-
-        }
-
-        protected final CharSequence startAugmentationNode(final CharSequence key) {
-            return invoke(STREAM,"startAugmentationNode",key);
-
-        }
-
-        protected final CharSequence startChoiceNode(final CharSequence localName,final CharSequence expected) {
-            return invoke(STREAM, "startChoiceNode", (localName),expected);
-        }
-
-        protected final CharSequence startCaseNode(final CharSequence localName,final CharSequence expected) {
-            return invoke(STREAM, "startCase", (localName),expected);
-        }
-
-
-        protected final CharSequence anyxmlNode(final String name, final String value) throws IllegalArgumentException {
-            return invoke(STREAM, "anyxmlNode", escape(name),name);
-        }
-
-        protected final CharSequence endNode() {
-            return invoke(STREAM, "endNode");
-        }
-
-        protected final CharSequence forEach(final String iterable,final Type valueType,final CharSequence body) {
-            return forEach(iterable,ITERATOR,valueType.getFullyQualifiedName(),CURRENT,body);
-        }
-
-        protected final CharSequence classReference(final Type type) {
-            return new StringBuilder().append(type.getFullyQualifiedName()).append(".class");
-        }
-
-        protected final CharSequence staticInvokeEmitter(final Type childType, final String name) {
-            final Class<?> cls;
-            try {
-                cls = STRATEGY.loadClass(childType);
-            } catch (ClassNotFoundException e) {
-                throw new IllegalStateException("Failed to invoke emitter", e);
-            }
-
-            String className = implementations.getUnchecked(cls).getName() + ".getInstance()";
-            return invoke(className, SERIALIZE_METHOD_NAME, REGISTRY, name, STREAM);
-        }
-    }
+    protected abstract DataObjectSerializerSource generateSerializer(GeneratedType type, AugmentationSchema schema);
 
 }
