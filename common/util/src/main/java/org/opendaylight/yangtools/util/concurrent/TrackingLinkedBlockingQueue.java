fcf5d7d9835dfd11cb9d2f251bf2eeb3ab473cec
tpantelis <tpanteli@brocade.com>
2014-08-07 17:32:09 -0400

    Bug 1446: Add new concurrent classes for tracking stats
    TrackingLinkedBlockingQueue
        A LinkedBlockingQueue that tracks the largest queue size foridebugging.
    
    CountingRejectedExecutionHandler
        A RejectedExecutionHandler that delegates to a backing RejectedExecutionHandler
        and counts the number of rejected tasks.
    
    DurationStatsTracker
        Class that calculates and tracks time duration statistics.
    
    ThreadExecutorStatsMXBean/Impl
        JXM bean that retrieves statistics from a backing ThreadPoolExecutor.
    
    Change-Id: I1b7bbb45301d8cf682a5de5d51cc64727e3139dc
    Signed-off-by: tpantelis <tpanteli@brocade.com>

---
+++ common/util/src/main/java/org/opendaylight/yangtools/util/concurrent/TrackingLinkedBlockingQueue.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2014 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.util.concurrent;
+
+import java.util.Collection;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * A {@link LinkedBlockingQueue} that tracks the largest queue size for debugging.
+ *
+ * @author Thomas Pantelis
+ *
+ * @param <E> the element t.ype
+ */
+public class TrackingLinkedBlockingQueue<E> extends LinkedBlockingQueue<E> {
+
+    private static final long serialVersionUID = 1L;
+
+    private final AtomicLong largestQueueSize = new AtomicLong( 0 );
+
+    /**
+     * @see LinkedBlockingQueue#LinkedBlockingQueue
+     */
+    public TrackingLinkedBlockingQueue() {
+        super();
+    }
+
+    /**
+     * @see LinkedBlockingQueue#LinkedBlockingQueue(Collection)
+     */
+    public TrackingLinkedBlockingQueue( Collection<? extends E> c ) {
+        super(c);
+    }
+
+    /**
+     * @see LinkedBlockingQueue#LinkedBlockingQueue(int)
+     */
+    public TrackingLinkedBlockingQueue( int capacity ) {
+        super(capacity);
+    }
+
+    /**
+     * Returns the largest queue size.
+     */
+    public long getLargestQueueSize(){
+        return largestQueueSize.get();
+    }
+
+    @Override
+    public boolean offer( E e, long timeout, TimeUnit unit ) throws InterruptedException {
+        if( super.offer( e, timeout, unit ) ) {
+            updateLargestQueueSize();
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean offer( E e ) {
+        if( super.offer( e ) ) {
+            updateLargestQueueSize();
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public void put( E e ) throws InterruptedException {
+        super.put( e );
+        updateLargestQueueSize();
+    }
+
+    @Override
+    public boolean add( E e ) {
+        boolean result = super.add( e );
+        updateLargestQueueSize();
+        return result;
+    }
+
+    @Override
+    public boolean addAll( Collection<? extends E> c ) {
+        try {
+            return super.addAll( c );
+        } finally {
+            updateLargestQueueSize();
+        }
+    }
+
+    private void updateLargestQueueSize() {
+        long size = size();
+        long largest = largestQueueSize.get();
+        if( size > largest ) {
+            largestQueueSize.compareAndSet( largest, size );
+        }
+    }
+}
