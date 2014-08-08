de17630f6c72bfc17684dd152ecf748bf2275ad2
Ladislav Borak <lborak@cisco.com>
2014-08-08 15:38:38 +0200

    Fix ordering fields in generated type builder
    Change-Id: Ie1171a78a17e6eab71b629411f0b0a1bb1807d7d
    Signed-off-by: Ladislav Borak <lborak@cisco.com>

---
+++ code-generator/binding-java-api-generator/src/main/java/org/opendaylight/yangtools/sal/java/api/generator/AlphabeticallyTypeMemberComparator.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.sal.java.api.generator;
+
+import java.util.Comparator;
+import org.opendaylight.yangtools.sal.binding.model.api.TypeMember;
+
+public class AlphabeticallyTypeMemberComparator<T extends TypeMember> implements Comparator<T>{
+
+    @Override
+    public int compare(T member1, T member2) {
+        return member1.getName().compareTo(member2.getName());
+    }
+}
