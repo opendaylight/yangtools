422f36175573c8280e86dac057db1b6d5d8d9728
Jozef Gloncak <jgloncak@cisco.com>
2014-08-21 15:00:10 +0200

    BUG 1440 - json stream to normalized node stream writer
    JsonParserStream contains implementation of read method in which there
    is gradually constructed structure of NodeDataWithSchema nodes.
    
    NodeDataWithSchema is similar to SchemaNode but contains only reference to
    children which really exists in JSON input and concrete values for nodes
    of type leaf or leaf list.
    
    It was necessary to firstly load JSON to NodeDataWitchSchema structure
    because for some types (composite keys of list, all nodes belonging to one
    augment, all nodes belonging to one choice) it isn't possible to do
    sequential processing (it means to call concrete opening and closing
    methods of NormalizedNodeStreamWriter interface).
    
    JsonParserStream constructor requires schema context as input parameter.
    If data behind mount point should be parsed then schema context of mount
    point is required.
    
    Change-Id: I28c1d3193792feb875ec2ceda0035ca56bfa5d42
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/AbstractNodeDataWithSchema.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.codec.gson;
+
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+
+abstract class AbstractNodeDataWithSchema {
+
+    private final DataSchemaNode schema;
+
+    public AbstractNodeDataWithSchema(final DataSchemaNode schema) {
+        this.schema = schema;
+    }
+
+    public final DataSchemaNode getSchema() {
+        return schema;
+    }
+
+    protected abstract void writeToStream(final NormalizedNodeStreamWriter nnStreamWriter);
+
+    protected NodeIdentifier provideNodeIdentifier() {
+        return new NodeIdentifier(schema.getQName());
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((schema == null) ? 0 : schema.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        AbstractNodeDataWithSchema other = (AbstractNodeDataWithSchema) obj;
+        if (schema == null) {
+            if (other.schema != null) {
+                return false;
+            }
+        } else if (!schema.equals(other.schema)) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
---
+++ yang/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/AnyXmlNodeDataWithSchema.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.codec.gson;
+
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+
+class AnyXmlNodeDataWithSchema extends SimpleNodeDataWithSchema {
+
+    public AnyXmlNodeDataWithSchema(final DataSchemaNode dataSchemaNode) {
+        super(dataSchemaNode);
+    }
+
+    @Override
+    public void writeToStream(final NormalizedNodeStreamWriter nnStreamWriter) {
+//      FIXME: should be changed according to format of value
+        nnStreamWriter.anyxmlNode(provideNodeIdentifier(), getValue());
+    }
+}
