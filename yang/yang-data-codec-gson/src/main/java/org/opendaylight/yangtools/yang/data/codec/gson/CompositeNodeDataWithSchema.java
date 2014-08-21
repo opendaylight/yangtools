355fbcc72b923a60052cc85adb3b3bec0c16f3be
Jozef Gloncak <jgloncak@cisco.com>
2014-08-21 15:00:10 +0200

    BUG 1440 - json stream to normalized node stream writer
    JsonParserStream contains implementation of read method in which there
    is gradually constructed structure of NodeDataWithSchema nodes.
    
    NodeDataWithSchema is similar to SchemaNode but contains only reference to
    children which really exists in JSON input and concrete values for nodes
    of type leaf or leaf list.
    
    It was necessary to firstly load JSON to NodeDataWitchSchema structure
    because for some types (composite keys of list, all nodes belonging to one
    augment, all nodes belonging to one choice) it isn't possible to do
    sequential processing (it means to call concrete opening and closing
    methods of NormalizedNodeStreamWriter interface).
    
    JsonParserStream constructor requires schema context as input parameter.
    If data behind mount point should be parsed then schema context of mount
    point is required.
    
    Change-Id: I28c1d3193792feb875ec2ceda0035ca56bfa5d42
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/CompositeNodeDataWithSchema.java
@@ -0,0 +1,233 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.codec.gson;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.AugmentationIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.model.api.AnyXmlSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.AugmentationSchema;
+import org.opendaylight.yangtools.yang.model.api.AugmentationTarget;
+import org.opendaylight.yangtools.yang.model.api.ChoiceCaseNode;
+import org.opendaylight.yangtools.yang.model.api.ChoiceNode;
+import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.LeafListSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.ListSchemaNode;
+
+class CompositeNodeDataWithSchema extends AbstractNodeDataWithSchema {
+
+    /**
+     * nodes which were added to schema via augmentation and are present in data input
+     */
+    protected Map<AugmentationSchema, List<AbstractNodeDataWithSchema>> augmentationsToChild = new HashMap<>();
+
+    /**
+     * remaining data nodes (which aren't added via augment). Every of them should have the same QName
+     */
+    protected List<AbstractNodeDataWithSchema> childs = new ArrayList<>();
+
+    public CompositeNodeDataWithSchema(final DataSchemaNode schema) {
+        super(schema);
+    }
+
+    public AbstractNodeDataWithSchema addSimpleChild(final DataSchemaNode schema) {
+        SimpleNodeDataWithSchema newChild = null;
+        if (schema instanceof LeafSchemaNode) {
+            newChild = new LeafNodeDataWithSchema(schema);
+        } else if (schema instanceof AnyXmlSchemaNode) {
+            newChild = new AnyXmlNodeDataWithSchema(schema);
+        }
+
+        if (newChild != null) {
+
+            AugmentationSchema augSchema = null;
+            if (schema.isAugmenting()) {
+                augSchema = findCorrespondingAugment(getSchema(), schema);
+            }
+            if (augSchema != null) {
+                addChildToAugmentation(augSchema, newChild);
+            } else {
+                addChild(newChild);
+            }
+            return newChild;
+        }
+        return null;
+    }
+
+    private void addChildToAugmentation(final AugmentationSchema augSchema, final AbstractNodeDataWithSchema newChild) {
+        List<AbstractNodeDataWithSchema> childsInAugment = augmentationsToChild.get(augSchema);
+        if (childsInAugment == null) {
+            childsInAugment = new ArrayList<>();
+            augmentationsToChild.put(augSchema, childsInAugment);
+        }
+        childsInAugment.add(newChild);
+    }
+
+    public AbstractNodeDataWithSchema addChild(final Deque<DataSchemaNode> schemas) {
+        if (schemas.size() == 1) {
+            final DataSchemaNode childDataSchemaNode = schemas.pop();
+            return addChild(childDataSchemaNode);
+        } else {
+            DataSchemaNode choiceCandidate = schemas.pop();
+            DataSchemaNode caseCandidate = schemas.pop();
+            ChoiceNode choiceNode = null;
+            ChoiceCaseNode caseNode = null;
+            if (choiceCandidate instanceof ChoiceNode) {
+                choiceNode = (ChoiceNode) choiceCandidate;
+            } else {
+                throw new IllegalArgumentException("Awaited node of type ChoiceNode but was "
+                        + choiceCandidate.getClass().getSimpleName());
+            }
+
+            if (caseCandidate instanceof ChoiceCaseNode) {
+                caseNode = (ChoiceCaseNode) caseCandidate;
+            } else {
+                throw new IllegalArgumentException("Awaited node of type ChoiceCaseNode but was "
+                        + caseCandidate.getClass().getSimpleName());
+            }
+
+            AugmentationSchema augSchema = null;
+            if (choiceCandidate.isAugmenting()) {
+                augSchema = findCorrespondingAugment(getSchema(), choiceCandidate);
+            }
+
+            // looking for existing choice
+            List<AbstractNodeDataWithSchema> childNodes = Collections.emptyList();
+            if (augSchema != null) {
+                childNodes = augmentationsToChild.get(augSchema);
+            } else {
+                childNodes = childs;
+            }
+
+            CompositeNodeDataWithSchema caseNodeDataWithSchema = findChoice(childNodes, choiceCandidate, caseCandidate);
+            if (caseNodeDataWithSchema == null) {
+                ChoiceNodeDataWithSchema choiceNodeDataWithSchema = new ChoiceNodeDataWithSchema(choiceNode);
+                addChild(choiceNodeDataWithSchema);
+                caseNodeDataWithSchema = choiceNodeDataWithSchema.addCompositeChild(caseNode);
+            }
+
+            return caseNodeDataWithSchema.addChild(schemas);
+        }
+
+    }
+
+    private CaseNodeDataWithSchema findChoice(final List<AbstractNodeDataWithSchema> childNodes, final DataSchemaNode choiceCandidate,
+            final DataSchemaNode caseCandidate) {
+        if (childNodes == null) {
+            return null;
+        }
+        for (AbstractNodeDataWithSchema nodeDataWithSchema : childNodes) {
+            if (nodeDataWithSchema instanceof ChoiceNodeDataWithSchema
+                    && nodeDataWithSchema.getSchema().getQName().equals(choiceCandidate.getQName())) {
+                CaseNodeDataWithSchema casePrevious = ((ChoiceNodeDataWithSchema) nodeDataWithSchema).getCase();
+                if (casePrevious.getSchema().getQName() != caseCandidate.getQName()) {
+                    throw new IllegalArgumentException("Data from case " + caseCandidate.getQName()
+                            + " are specified but other data from case " + casePrevious.getSchema().getQName()
+                            + " were specified erlier. Data aren't from the same case.");
+                }
+                return casePrevious;
+            }
+        }
+        return null;
+    }
+
+    public AbstractNodeDataWithSchema addCompositeChild(final DataSchemaNode schema) {
+        CompositeNodeDataWithSchema newChild;
+        if (schema instanceof ListSchemaNode) {
+            newChild = new ListNodeDataWithSchema(schema);
+        } else if (schema instanceof LeafListSchemaNode) {
+            newChild = new LeafListNodeDataWithSchema(schema);
+        } else if (schema instanceof ContainerSchemaNode) {
+            newChild = new ContainerNodeDataWithSchema(schema);
+        } else {
+            newChild = new CompositeNodeDataWithSchema(schema);
+        }
+        addCompositeChild(newChild);
+        return newChild;
+    }
+
+    public void addCompositeChild(final CompositeNodeDataWithSchema newChild) {
+        AugmentationSchema augSchema = findCorrespondingAugment(getSchema(), newChild.getSchema());
+        if (augSchema != null) {
+            addChildToAugmentation(augSchema, newChild);
+        } else {
+            addChild(newChild);
+        }
+    }
+
+    private AbstractNodeDataWithSchema addChild(final DataSchemaNode schema) {
+        AbstractNodeDataWithSchema newChild = addSimpleChild(schema);
+        return newChild == null ? addCompositeChild(schema) : newChild;
+    }
+
+    public void addChild(final AbstractNodeDataWithSchema newChild) {
+        childs.add(newChild);
+    }
+
+    /**
+     * Tries to find in {@code parent} which is dealed as augmentation target node with QName as {@code child}. If such
+     * node is found then it is returned, else null.
+     */
+    protected AugmentationSchema findCorrespondingAugment(final DataSchemaNode parent, final DataSchemaNode child) {
+        if (parent instanceof AugmentationTarget) {
+            for (AugmentationSchema augmentation : ((AugmentationTarget) parent).getAvailableAugmentations()) {
+                DataSchemaNode childInAugmentation = augmentation.getDataChildByName(child.getQName());
+                if (childInAugmentation != null) {
+                    return augmentation;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void writeToStream(final NormalizedNodeStreamWriter nnStreamWriter) {
+        for (AbstractNodeDataWithSchema child : childs) {
+            child.writeToStream(nnStreamWriter);
+        }
+        for (Entry<AugmentationSchema, List<AbstractNodeDataWithSchema>> augmentationToChild : augmentationsToChild.entrySet()) {
+
+            final List<AbstractNodeDataWithSchema> childsFromAgumentation = augmentationToChild.getValue();
+
+            if (!childsFromAgumentation.isEmpty()) {
+                nnStreamWriter.startAugmentationNode(toAugmentationIdentifier(augmentationToChild));
+
+                for (AbstractNodeDataWithSchema nodeDataWithSchema : childsFromAgumentation) {
+                    nodeDataWithSchema.writeToStream(nnStreamWriter);
+                }
+
+                nnStreamWriter.endNode();
+            }
+        }
+    }
+
+    private static AugmentationIdentifier toAugmentationIdentifier(
+            final Entry<AugmentationSchema, List<AbstractNodeDataWithSchema>> augmentationToChild) {
+        Collection<DataSchemaNode> nodes = augmentationToChild.getKey().getChildNodes();
+        Set<QName> nodesQNames = new HashSet<>();
+        for (DataSchemaNode node : nodes) {
+            nodesQNames.add(node.getQName());
+        }
+
+        return new AugmentationIdentifier(nodesQNames);
+    }
+
+}
---
+++ yang/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/helpers/RestCodec.java
@@ -0,0 +1,336 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.codec.gson.helpers;
+
+import com.google.common.annotations.Beta;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.opendaylight.yangtools.concepts.Codec;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeWithValue;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.codec.IdentityrefCodec;
+import org.opendaylight.yangtools.yang.data.api.codec.InstanceIdentifierCodec;
+import org.opendaylight.yangtools.yang.data.api.codec.LeafrefCodec;
+import org.opendaylight.yangtools.yang.data.codec.gson.helpers.IdentityValuesDTO.IdentityValue;
+import org.opendaylight.yangtools.yang.data.codec.gson.helpers.IdentityValuesDTO.Predicate;
+import org.opendaylight.yangtools.yang.data.impl.codec.TypeDefinitionAwareCodec;
+import org.opendaylight.yangtools.yang.model.api.DataNodeContainer;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.LeafListSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.ListSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.IdentityrefTypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.InstanceIdentifierTypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.LeafrefTypeDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class is implementation-internal and subject to change. Please do not use it.
+ */
+@Beta
+public final class RestCodec {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RestCodec.class);
+
+    private RestCodec() {
+    }
+
+    public static final Codec<Object, Object> from(final TypeDefinition<?> typeDefinition) {
+        return new ObjectCodec(typeDefinition);
+    }
+
+    @SuppressWarnings("rawtypes")
+    public static final class ObjectCodec implements Codec<Object, Object> {
+
+        private final Logger logger = LoggerFactory.getLogger(RestCodec.class);
+
+        public static final Codec LEAFREF_DEFAULT_CODEC = new LeafrefCodecImpl();
+        private final Codec instanceIdentifier;
+        private final Codec identityrefCodec;
+
+        private final TypeDefinition<?> type;
+
+        private ObjectCodec(final TypeDefinition<?> typeDefinition) {
+            type = RestUtil.resolveBaseTypeFrom(typeDefinition);
+            if (type instanceof IdentityrefTypeDefinition) {
+                identityrefCodec = new IdentityrefCodecImpl();
+            } else {
+                identityrefCodec = null;
+            }
+            if (type instanceof InstanceIdentifierTypeDefinition) {
+                instanceIdentifier = new InstanceIdentifierCodecImpl();
+            } else {
+                instanceIdentifier = null;
+            }
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public Object deserialize(final Object input) {
+            try {
+                if (type instanceof IdentityrefTypeDefinition) {
+                    if (input instanceof IdentityValuesDTO) {
+                        return identityrefCodec.deserialize(input);
+                    }
+                    logger.debug(
+                            "Value is not instance of IdentityrefTypeDefinition but is {}. Therefore NULL is used as translation of  - {}",
+                            input == null ? "null" : input.getClass(), String.valueOf(input));
+                    return null;
+                } else if (type instanceof LeafrefTypeDefinition) {
+                    if (input instanceof IdentityValuesDTO) {
+                        return LEAFREF_DEFAULT_CODEC.deserialize(((IdentityValuesDTO) input).getOriginValue());
+                    }
+                    return LEAFREF_DEFAULT_CODEC.deserialize(input);
+                } else if (type instanceof InstanceIdentifierTypeDefinition) {
+                    if (input instanceof IdentityValuesDTO) {
+                        return instanceIdentifier.deserialize(input);
+                    }
+                    logger.info(
+                            "Value is not instance of InstanceIdentifierTypeDefinition but is {}. Therefore NULL is used as translation of  - {}",
+                            input == null ? "null" : input.getClass(), String.valueOf(input));
+                    return null;
+                } else {
+                    TypeDefinitionAwareCodec<Object, ? extends TypeDefinition<?>> typeAwarecodec = TypeDefinitionAwareCodec
+                            .from(type);
+                    if (typeAwarecodec != null) {
+                        if (input instanceof IdentityValuesDTO) {
+                            return typeAwarecodec.deserialize(((IdentityValuesDTO) input).getOriginValue());
+                        }
+                        return typeAwarecodec.deserialize(String.valueOf(input));
+                    } else {
+                        logger.debug("Codec for type \"" + type.getQName().getLocalName()
+                                + "\" is not implemented yet.");
+                        return null;
+                    }
+                }
+            } catch (ClassCastException e) { // TODO remove this catch when everyone use codecs
+                logger.error(
+                        "ClassCastException was thrown when codec is invoked with parameter " + String.valueOf(input),
+                        e);
+                return null;
+            }
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public Object serialize(final Object input) {
+            try {
+                if (type instanceof IdentityrefTypeDefinition) {
+                    return identityrefCodec.serialize(input);
+                } else if (type instanceof LeafrefTypeDefinition) {
+                    return LEAFREF_DEFAULT_CODEC.serialize(input);
+                } else if (type instanceof InstanceIdentifierTypeDefinition) {
+                    return instanceIdentifier.serialize(input);
+                } else {
+                    TypeDefinitionAwareCodec<Object, ? extends TypeDefinition<?>> typeAwarecodec = TypeDefinitionAwareCodec
+                            .from(type);
+                    if (typeAwarecodec != null) {
+                        return typeAwarecodec.serialize(input);
+                    } else {
+                        logger.debug("Codec for type \"" + type.getQName().getLocalName()
+                                + "\" is not implemented yet.");
+                        return null;
+                    }
+                }
+            } catch (ClassCastException e) { // TODO remove this catch when everyone use codecs
+                logger.error(
+                        "ClassCastException was thrown when codec is invoked with parameter " + String.valueOf(input),
+                        e);
+                return input;
+            }
+        }
+
+    }
+
+    public static class IdentityrefCodecImpl implements IdentityrefCodec<IdentityValuesDTO> {
+
+        private final Logger logger = LoggerFactory.getLogger(IdentityrefCodecImpl.class);
+
+        @Override
+        public IdentityValuesDTO serialize(final QName data) {
+            return new IdentityValuesDTO(data.getNamespace().toString(), data.getLocalName(), data.getPrefix(), null);
+        }
+
+        @Override
+        public QName deserialize(final IdentityValuesDTO data) {
+            IdentityValue valueWithNamespace = data.getValuesWithNamespaces().get(0);
+            Module module = getModuleByNamespace(valueWithNamespace.getNamespace());
+            if (module == null) {
+                logger.info("Module was not found for namespace {}", valueWithNamespace.getNamespace());
+                logger.info("Idenetityref will be translated as NULL for data - {}", String.valueOf(valueWithNamespace));
+                return null;
+            }
+
+            return QName.create(module.getNamespace(), module.getRevision(), valueWithNamespace.getValue());
+        }
+
+    }
+
+    public static class LeafrefCodecImpl implements LeafrefCodec<String> {
+
+        @Override
+        public String serialize(final Object data) {
+            return String.valueOf(data);
+        }
+
+        @Override
+        public Object deserialize(final String data) {
+            return data;
+        }
+
+    }
+
+    public static class InstanceIdentifierCodecImpl implements InstanceIdentifierCodec<IdentityValuesDTO> {
+        private final Logger logger = LoggerFactory.getLogger(InstanceIdentifierCodecImpl.class);
+
+        @Override
+        public IdentityValuesDTO serialize(final YangInstanceIdentifier data) {
+            IdentityValuesDTO identityValuesDTO = new IdentityValuesDTO();
+            for (PathArgument pathArgument : data.getPathArguments()) {
+                IdentityValue identityValue = qNameToIdentityValue(pathArgument.getNodeType());
+                if (pathArgument instanceof NodeIdentifierWithPredicates && identityValue != null) {
+                    List<Predicate> predicates = keyValuesToPredicateList(((NodeIdentifierWithPredicates) pathArgument)
+                            .getKeyValues());
+                    identityValue.setPredicates(predicates);
+                } else if (pathArgument instanceof NodeWithValue && identityValue != null) {
+                    List<Predicate> predicates = new ArrayList<>();
+                    String value = String.valueOf(((NodeWithValue) pathArgument).getValue());
+                    predicates.add(new Predicate(null, value));
+                    identityValue.setPredicates(predicates);
+                }
+                identityValuesDTO.add(identityValue);
+            }
+            return identityValuesDTO;
+        }
+
+        @Override
+        public YangInstanceIdentifier deserialize(final IdentityValuesDTO data) {
+            List<PathArgument> result = new ArrayList<PathArgument>();
+            IdentityValue valueWithNamespace = data.getValuesWithNamespaces().get(0);
+            Module module = getModuleByNamespace(valueWithNamespace.getNamespace());
+            if (module == null) {
+                logger.info("Module by namespace '{}' of first node in instance-identiefier was not found.",
+                        valueWithNamespace.getNamespace());
+                logger.info("Instance-identifier will be translated as NULL for data - {}",
+                        String.valueOf(valueWithNamespace.getValue()));
+                return null;
+            }
+
+            DataNodeContainer parentContainer = module;
+            List<IdentityValue> identities = data.getValuesWithNamespaces();
+            for (int i = 0; i < identities.size(); i++) {
+                IdentityValue identityValue = identities.get(i);
+                URI validNamespace = resolveValidNamespace(identityValue.getNamespace());
+                DataSchemaNode node = SchemaContextUtils.getInstance().findInstanceDataChildByNameAndNamespace(
+                        parentContainer, identityValue.getValue(), validNamespace);
+                if (node == null) {
+                    logger.info("'{}' node was not found in {}", identityValue, parentContainer.getChildNodes());
+                    logger.info("Instance-identifier will be translated as NULL for data - {}",
+                            String.valueOf(identityValue.getValue()));
+                    return null;
+                }
+                QName qName = node.getQName();
+                PathArgument pathArgument = null;
+                if (identityValue.getPredicates().isEmpty()) {
+                    pathArgument = new NodeIdentifier(qName);
+                } else {
+                    if (node instanceof LeafListSchemaNode) { // predicate is value of leaf-list entry
+                        Predicate leafListPredicate = identityValue.getPredicates().get(0);
+                        if (!leafListPredicate.isLeafList()) {
+                            logger.info("Predicate's data is not type of leaf-list. It should be in format \".='value'\"");
+                            logger.info("Instance-identifier will be translated as NULL for data - {}",
+                                    String.valueOf(identityValue.getValue()));
+                            return null;
+                        }
+                        pathArgument = new NodeWithValue(qName, leafListPredicate.getValue());
+                    } else if (node instanceof ListSchemaNode) { // predicates are keys of list
+                        DataNodeContainer listNode = (DataNodeContainer) node;
+                        Map<QName, Object> predicatesMap = new HashMap<>();
+                        for (Predicate predicate : identityValue.getPredicates()) {
+                            validNamespace = resolveValidNamespace(predicate.getName().getNamespace());
+                            DataSchemaNode listKey = SchemaContextUtils.getInstance()
+                                    .findInstanceDataChildByNameAndNamespace(listNode, predicate.getName().getValue(),
+                                            validNamespace);
+                            predicatesMap.put(listKey.getQName(), predicate.getValue());
+                        }
+                        pathArgument = new NodeIdentifierWithPredicates(qName, predicatesMap);
+                    } else {
+                        logger.info("Node {} is not List or Leaf-list.", node);
+                        logger.info("Instance-identifier will be translated as NULL for data - {}",
+                                String.valueOf(identityValue.getValue()));
+                        return null;
+                    }
+                }
+                result.add(pathArgument);
+                if (i < identities.size() - 1) { // last element in instance-identifier can be other than
+                    // DataNodeContainer
+                    if (node instanceof DataNodeContainer) {
+                        parentContainer = (DataNodeContainer) node;
+                    } else {
+                        logger.info("Node {} isn't instance of DataNodeContainer", node);
+                        logger.info("Instance-identifier will be translated as NULL for data - {}",
+                                String.valueOf(identityValue.getValue()));
+                        return null;
+                    }
+                }
+            }
+
+            return result.isEmpty() ? null : YangInstanceIdentifier.create(result);
+        }
+
+        private List<Predicate> keyValuesToPredicateList(final Map<QName, Object> keyValues) {
+            List<Predicate> result = new ArrayList<>();
+            for (QName qName : keyValues.keySet()) {
+                Object value = keyValues.get(qName);
+                result.add(new Predicate(qNameToIdentityValue(qName), String.valueOf(value)));
+            }
+            return result;
+        }
+
+        private IdentityValue qNameToIdentityValue(final QName qName) {
+            if (qName != null) {
+                return new IdentityValue(qName.getNamespace().toString(), qName.getLocalName(), qName.getPrefix());
+            }
+            return null;
+        }
+    }
+
+    private static Module getModuleByNamespace(final String namespace) {
+        URI validNamespace = resolveValidNamespace(namespace);
+
+        Module module = null;
+        module = SchemaContextUtils.getInstance().findModuleByNamespace(validNamespace);
+        if (module == null) {
+            LOG.info("Module for namespace " + validNamespace + " wasn't found.");
+            return null;
+        }
+        return module;
+    }
+
+    private static URI resolveValidNamespace(final String namespace) {
+        URI validNamespace;
+        validNamespace = SchemaContextUtils.getInstance().findNamespaceByModuleName(namespace);
+        if (validNamespace == null) {
+            validNamespace = URI.create(namespace);
+        }
+
+        return validNamespace;
+    }
+
+}
