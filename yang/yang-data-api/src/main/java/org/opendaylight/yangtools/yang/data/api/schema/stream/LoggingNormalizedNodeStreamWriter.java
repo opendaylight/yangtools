6384692476e8dc9594beff1e61dd7d5553417945
Robert Varga <rovarga@cisco.com>
2014-08-30 23:04:28 +0200

    BUG-1440: speed up LoggingNormalizedNodeStreamWriter
    Precompute indent string as we go, saving some object instantiations.
    
    Change-Id: Ie591417d28066140e99d7718d19d926d1e863d1f
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/stream/LoggingNormalizedNodeStreamWriter.java
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/stream/LoggingNormalizedNodeStreamWriter.java
@@ -1,10 +1,10 @@
 package org.opendaylight.yangtools.yang.data.api.schema.stream;
 
 import com.google.common.annotations.Beta;
-import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 
-import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.AugmentationIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
@@ -12,87 +12,98 @@ import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdent
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * A {@link NormalizedNodeStreamWriter} which logs the events into a {@link Logger}.
+ */
 @Beta
-public class LoggingNormalizedNodeStreamWriter implements NormalizedNodeStreamWriter {
+public final class LoggingNormalizedNodeStreamWriter implements NormalizedNodeStreamWriter {
     private static final Logger LOG = LoggerFactory.getLogger(LoggingNormalizedNodeStreamWriter.class);
     private static final int DEFAULT_INDENT_SIZE = 2;
-    private final int indentSize = DEFAULT_INDENT_SIZE;
-    private int currentIndent = 0;
+
+    private final Deque<String> indent = new ArrayDeque<>();
+    private final String indentStr;
+
+    public LoggingNormalizedNodeStreamWriter() {
+        this(DEFAULT_INDENT_SIZE);
+    }
+
+    public LoggingNormalizedNodeStreamWriter(final int indentSize) {
+        this.indentStr = Strings.repeat(" ", indentSize);
+        indent.push("");
+    }
 
     private String ind() {
-        return Strings.repeat(" ", currentIndent);
+        return indent.peek();
     }
 
     private void decIndent() {
-        Preconditions.checkState(currentIndent >= 0, "Unexpected indentation %s", currentIndent);
-        currentIndent -= indentSize;
+        indent.pop();
     }
 
     private void incIndent() {
-        currentIndent += indentSize;
+        indent.push(ind() + indentStr);
     }
 
     @Override
-    public void startUnkeyedListItem(final NodeIdentifier name, final int childSizeHint) throws IllegalStateException {
+    public void startUnkeyedListItem(final NodeIdentifier name, final int childSizeHint) {
         LOG.debug("{}{}[](no key)", ind(), name);
         incIndent();
     }
 
     @Override
-    public void startUnkeyedList(final NodeIdentifier name, final int childSizeHint) throws IllegalArgumentException {
+    public void startUnkeyedList(final NodeIdentifier name, final int childSizeHint) {
         LOG.debug("{}{}(no key)", ind(), name);
         incIndent();
     }
 
     @Override
-    public void startOrderedMapNode(final NodeIdentifier name, final int childSizeHint) throws IllegalArgumentException {
+    public void startOrderedMapNode(final NodeIdentifier name, final int childSizeHint) {
 
     }
 
     @Override
-    public void startMapNode(final NodeIdentifier name, final int childSizeHint) throws IllegalArgumentException {
+    public void startMapNode(final NodeIdentifier name, final int childSizeHint) {
         LOG.debug("{}{}(key)", ind(), name);
         incIndent();
     }
 
     @Override
-    public void startMapEntryNode(final NodeIdentifierWithPredicates identifier, final int childSizeHint)
-            throws IllegalArgumentException {
+    public void startMapEntryNode(final NodeIdentifierWithPredicates identifier, final int childSizeHint) {
         LOG.debug("{}{}[](key)", ind(), identifier);
         incIndent();
     }
 
     @Override
-    public void startLeafSet(final NodeIdentifier name, final int childSizeHint) throws IllegalArgumentException {
+    public void startLeafSet(final NodeIdentifier name, final int childSizeHint) {
         LOG.debug("{}{}(leaf-list)", ind(), name);
         incIndent();
     }
 
     @Override
-    public void startContainerNode(final NodeIdentifier name, final int childSizeHint) throws IllegalArgumentException {
+    public void startContainerNode(final NodeIdentifier name, final int childSizeHint) {
         LOG.debug("{}{}(container)", ind(), name);
         incIndent();
     }
 
     @Override
-    public void startChoiceNode(final NodeIdentifier name, final int childSizeHint) throws IllegalArgumentException {
+    public void startChoiceNode(final NodeIdentifier name, final int childSizeHint) {
         LOG.debug("{}{}(choice)", ind(), name);
         incIndent();
     }
 
     @Override
-    public void startAugmentationNode(final AugmentationIdentifier identifier) throws IllegalArgumentException {
+    public void startAugmentationNode(final AugmentationIdentifier identifier) {
         LOG.debug("{}{}(augmentation)", ind(), identifier);
         incIndent();
     }
 
     @Override
-    public void leafSetEntryNode(final Object value) throws IllegalArgumentException {
+    public void leafSetEntryNode(final Object value) {
         LOG.debug("{}{}({}) ", ind(), value, value.getClass().getSimpleName());
     }
 
     @Override
-    public void leafNode(final NodeIdentifier name, final Object value) throws IllegalArgumentException {
+    public void leafNode(final NodeIdentifier name, final Object value) {
         LOG.debug("{}{}(leaf({}))=", ind(), name, value.getClass().getSimpleName(), value);
     }
 
@@ -103,17 +114,17 @@ public class LoggingNormalizedNodeStreamWriter implements NormalizedNodeStreamWr
     }
 
     @Override
-    public void anyxmlNode(final NodeIdentifier name, final Object value) throws IllegalArgumentException {
+    public void anyxmlNode(final NodeIdentifier name, final Object value) {
         LOG.debug("{}{}(anyxml)=", ind(), name, value);
     }
 
     @Override
-    public void flush() throws IOException {
+    public void flush() {
         LOG.trace("<<FLUSH>>");
     }
 
     @Override
-    public void close() throws IOException {
+    public void close() {
         LOG.debug("<<END-OF-STREAM>>");
     }
 }
