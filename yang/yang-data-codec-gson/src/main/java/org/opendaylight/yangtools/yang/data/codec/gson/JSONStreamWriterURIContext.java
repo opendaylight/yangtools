073fbd1db9b78e35813d9d9297e2e8c13643fde7
Robert Varga <rovarga@cisco.com>
2014-09-05 15:06:03 +0200

    BUG-1704: rework state tracking
    In order to understand when a list (or container) is necessary, the
    state tracking machinery needs to be reworked. This patch does exactly
    that, adding a single-linked stack tracking nodes and their respective
    output.
    
    Change-Id: Iaf6ec6b25c05e0505a1dec6faad5fde2ecb791a0
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/JSONStreamWriterURIContext.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.codec.gson;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.net.URI;
+
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+/**
+ * Abstract class tracking a virtual level of {@link JSONNormalizedNodeStreamWriter}
+ * recursion. It only tracks the namespace associated with this node.
+ */
+abstract class JSONStreamWriterURIContext extends JSONStreamWriterContext {
+    private final URI namespace;
+
+    protected JSONStreamWriterURIContext(final JSONStreamWriterContext parent, final URI namespace) {
+        super(parent, false);
+        this.namespace = namespace;
+    }
+
+    @Override
+    protected final URI getNamespace() {
+        return namespace;
+    }
+
+    @Override
+    protected final void emitStart(final SchemaContext schema, final Writer writer) throws IOException {
+        // No-op
+    }
+}
