08c0f78336e6d48fc33ff5ab02c8e03ed975f134
Robert Varga <rovarga@cisco.com>
2014-09-05 15:06:03 +0200

    BUG-1704: rework state tracking
    In order to understand when a list (or container) is necessary, the
    state tracking machinery needs to be reworked. This patch does exactly
    that, adding a single-linked stack tracking nodes and their respective
    output.
    
    Change-Id: Iaf6ec6b25c05e0505a1dec6faad5fde2ecb791a0
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/JSONStreamWriterRootContext.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.codec.gson;
+
+import java.io.Writer;
+import java.net.URI;
+
+/**
+ * The root node of a particular {@link JSONNormalizedNodeStreamWriter} instance.
+ * It holds the base namespace and can never be removed from the stack.
+ */
+final class JSONStreamWriterRootContext extends JSONStreamWriterURIContext {
+    JSONStreamWriterRootContext(final URI namespace) {
+        super(null, namespace);
+    }
+
+    @Override
+    protected void emitEnd(final Writer writer) {
+        throw new IllegalArgumentException("Top-level node reached");
+    }
+}
