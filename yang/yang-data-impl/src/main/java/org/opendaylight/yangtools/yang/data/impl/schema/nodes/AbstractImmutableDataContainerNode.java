opendaylight/yangtools

commit 9bcc82d107075da35c8cd01422481eaea395ba29

BUG-648: expose copy builders
This patch exposes the ability to have builders pre-seeded from existing
nodes. Also adds the ability to remove children, such that the users can
be simplified.

The end result of this patch is to be able to completely elide copying
data when it's not necessary, e.g. the implementations can choose to
switch to a persistent data structure and optimize the copy away.

Change-Id: I37d3540038bf2cbde8fb4ab456e9eb03f18a66f6
Signed-off-by: Robert Varga <rovarga@cisco.com>

yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/nodes/AbstractImmutableDataContainerNode.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.yangtools.yang.data.impl.schema.nodes;
 
+import java.util.Collections;
 import java.util.Map;
 
 import org.opendaylight.yangtools.concepts.Immutable;
@@ -23,7 +24,6 @@ public abstract class AbstractImmutableDataContainerNode<K extends PathArgument>
 
     protected final Map<PathArgument, DataContainerChild<? extends PathArgument, ?>> children;
 
-
     public AbstractImmutableDataContainerNode(
             final Map<PathArgument, DataContainerChild<? extends PathArgument, ?>> children, final K nodeIdentifier) {
         super(nodeIdentifier, Iterables.unmodifiableIterable(children.values()));
@@ -38,6 +38,11 @@ public abstract class AbstractImmutableDataContainerNode<K extends PathArgument>
     @Override
     protected int valueHashCode() {
         return children.hashCode();
+    }
+
+    public final Map<PathArgument, DataContainerChild<? extends PathArgument, ?>> getChildren() {
+        // Make sure we do not leak a mutable view
+        return Collections.unmodifiableMap(children);
     }
 
     @Override

yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/Builders.java
@@ -13,6 +13,7 @@ import org.opendaylight.yangtools.yang.data.api.schema.ChoiceNode;
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
 import org.opendaylight.yangtools.yang.data.api.schema.LeafSetEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafSetNode;
 import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
 import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
 import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.CollectionNodeBuilder;
@@ -53,7 +54,7 @@ public final class Builders {
     }
 
     public static <T> NormalizedNodeAttrBuilder<InstanceIdentifier.NodeIdentifier, T, LeafNode<T>> leafBuilder(
-            LeafSchemaNode schema) {
+            final LeafSchemaNode schema) {
         return ImmutableLeafNodeSchemaAwareBuilder.create(schema);
     }
 
@@ -62,7 +63,7 @@ public final class Builders {
     }
 
     public static <T> NormalizedNodeAttrBuilder<InstanceIdentifier.NodeWithValue, T, LeafSetEntryNode<T>> leafSetEntryBuilder(
-            LeafListSchemaNode schema) {
+            final LeafListSchemaNode schema) {
         return ImmutableLeafSetEntryNodeSchemaAwareBuilder.create(schema);
     }
 
@@ -70,17 +71,34 @@ public final class Builders {
         return ImmutableLeafSetNodeBuilder.create();
     }
 
+    public static <T> ListNodeBuilder<T,LeafSetEntryNode<T>> leafSetBuilder(final LeafSetNode<T> node) {
+        return ImmutableLeafSetNodeBuilder.create(node);
+    }
+
     public static <T> ListNodeBuilder<T,LeafSetEntryNode<T>> leafSetBuilder(final LeafListSchemaNode schema) {
         return ImmutableLeafSetNodeSchemaAwareBuilder.<T>create(schema);
+    }
+
+    public static <T> ListNodeBuilder<T,LeafSetEntryNode<T>> leafSetBuilder(final LeafListSchemaNode schema, final LeafSetNode<T> node) {
+        return ImmutableLeafSetNodeSchemaAwareBuilder.<T>create(schema, node);
     }
 
     public static DataContainerNodeAttrBuilder<InstanceIdentifier.NodeIdentifier, ContainerNode> containerBuilder() {
         return ImmutableContainerNodeBuilder.create();
     }
 
+    public static DataContainerNodeAttrBuilder<InstanceIdentifier.NodeIdentifier, ContainerNode> containerBuilder(final ContainerNode node) {
+        return ImmutableContainerNodeBuilder.create(node);
+    }
+
     public static DataContainerNodeAttrBuilder<InstanceIdentifier.NodeIdentifier, ContainerNode> containerBuilder(
-            ContainerSchemaNode schema) {
+            final ContainerSchemaNode schema) {
         return ImmutableContainerNodeSchemaAwareBuilder.create(schema);
+    }
+
+    public static DataContainerNodeAttrBuilder<InstanceIdentifier.NodeIdentifier, ContainerNode> containerBuilder(
+            final ContainerSchemaNode schema, final ContainerNode node) {
+        return ImmutableContainerNodeSchemaAwareBuilder.create(schema, node);
     }
 
     public static DataContainerNodeAttrBuilder<InstanceIdentifier.NodeIdentifierWithPredicates, MapEntryNode> mapEntryBuilder() {
@@ -88,7 +106,7 @@ public final class Builders {
     }
 
     public static DataContainerNodeAttrBuilder<InstanceIdentifier.NodeIdentifierWithPredicates, MapEntryNode> mapEntryBuilder(
-            ListSchemaNode schema) {
+            final ListSchemaNode schema) {
         return ImmutableMapEntryNodeSchemaAwareBuilder.create(schema);
     }
 
@@ -96,8 +114,16 @@ public final class Builders {
         return ImmutableMapNodeBuilder.create();
     }
 
+    public static CollectionNodeBuilder<MapEntryNode, MapNode> mapBuilder(final MapNode node) {
+        return ImmutableMapNodeBuilder.create(node);
+    }
+
     public static CollectionNodeBuilder<MapEntryNode, MapNode> mapBuilder(final ListSchemaNode schema) {
         return ImmutableMapNodeSchemaAwareBuilder.create(schema);
+    }
+
+    public static CollectionNodeBuilder<MapEntryNode, MapNode> mapBuilder(final ListSchemaNode schema, final MapNode node) {
+        return ImmutableMapNodeSchemaAwareBuilder.create(schema, node);
     }
 
     public static DataContainerNodeBuilder<InstanceIdentifier.AugmentationIdentifier, AugmentationNode> augmentationBuilder() {

yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/builder/api/CollectionNodeBuilder.java
@@ -24,4 +24,5 @@ public interface CollectionNodeBuilder<V extends NormalizedNode<?, ?>, R extends
     CollectionNodeBuilder<V, R> withNodeIdentifier(InstanceIdentifier.NodeIdentifier nodeIdentifier);
 
     CollectionNodeBuilder<V, R> withChild(V child);
+    CollectionNodeBuilder<V, R> withoutChild(InstanceIdentifier.PathArgument key);
 }

yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/builder/api/DataContainerNodeBuilder.java
@@ -24,4 +24,5 @@ public interface DataContainerNodeBuilder<I extends InstanceIdentifier.PathArgum
     DataContainerNodeBuilder<I, R> withNodeIdentifier(I nodeIdentifier);
 
     DataContainerNodeBuilder<I, R> withChild(DataContainerChild<?, ?> child);
+    DataContainerNodeBuilder<I, R> withoutChild(PathArgument key);
 }

yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/builder/api/ListNodeBuilder.java
@@ -7,13 +7,13 @@
  */
 package org.opendaylight.yangtools.yang.data.impl.schema.builder.api;
 
+import java.util.List;
+import java.util.Map;
+
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.LeafSetEntryNode;
 import org.opendaylight.yangtools.yang.data.api.schema.LeafSetNode;
-
-import java.util.List;
-import java.util.Map;
 
 public interface ListNodeBuilder<T, V>
         extends CollectionNodeBuilder<LeafSetEntryNode<T>, LeafSetNode<T>> {
@@ -26,6 +26,9 @@ public interface ListNodeBuilder<T, V>
 
     @Override
     ListNodeBuilder<T, V> withChild(LeafSetEntryNode<T> child);
+
+    @Override
+    ListNodeBuilder<T, V> withoutChild(InstanceIdentifier.PathArgument key);
 
     ListNodeBuilder<T, V> withChildValue(T child);
 

yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/builder/api/NormalizedNodeContainerBuilder.java
@@ -9,10 +9,11 @@ public interface NormalizedNodeContainerBuilder<K extends PathArgument,CK extend
 extends NormalizedNodeBuilder<K,List<CV>,P>{
 
     @Override
-    public NormalizedNodeContainerBuilder<K,CK,CV,P> withNodeIdentifier(K nodeIdentifier);
+    NormalizedNodeContainerBuilder<K,CK,CV,P> withNodeIdentifier(K nodeIdentifier);
 
     @Override
-    public NormalizedNodeContainerBuilder<K,CK,CV,P> withValue(List<CV> value);
+    NormalizedNodeContainerBuilder<K,CK,CV,P> withValue(List<CV> value);
 
-    public NormalizedNodeContainerBuilder<K,CK,CV,P> addChild(CV child);
+    NormalizedNodeContainerBuilder<K,CK,CV,P> addChild(CV child);
+    NormalizedNodeContainerBuilder<K,CK,CV,P> removeChild(CK key);
 }

yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/builder/impl/AbstractImmutableDataContainerNodeAttrBuilder.java
@@ -16,31 +16,45 @@ import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
 import org.opendaylight.yangtools.yang.data.api.schema.DataContainerNode;
 import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.DataContainerNodeAttrBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.nodes.AbstractImmutableDataContainerAttrNode;
 
 abstract class AbstractImmutableDataContainerNodeAttrBuilder<I extends InstanceIdentifier.PathArgument, R extends DataContainerNode<I>>
         extends AbstractImmutableDataContainerNodeBuilder<I, R>
         implements DataContainerNodeAttrBuilder<I, R> {
 
-    protected Map<QName, String> attributes = Collections.emptyMap();
+    private Map<QName, String> attributes;
+
+    protected AbstractImmutableDataContainerNodeAttrBuilder() {
+        this.attributes = Collections.emptyMap();
+    }
+
+    protected AbstractImmutableDataContainerNodeAttrBuilder(final AbstractImmutableDataContainerAttrNode<I> node) {
+        super(node);
+        this.attributes = node.getAttributes();
+    }
+
+    protected final Map<QName, String> getAttributes() {
+        return attributes;
+    }
 
     @Override
-    public DataContainerNodeAttrBuilder<I, R> withAttributes(Map<QName, String> attributes){
+    public DataContainerNodeAttrBuilder<I, R> withAttributes(final Map<QName, String> attributes){
         this.attributes = attributes;
         return this;
     }
 
     @Override
-    public DataContainerNodeAttrBuilder<I, R> withValue(List<DataContainerChild<? extends InstanceIdentifier.PathArgument, ?>> value) {
+    public DataContainerNodeAttrBuilder<I, R> withValue(final List<DataContainerChild<? extends InstanceIdentifier.PathArgument, ?>> value) {
         return (DataContainerNodeAttrBuilder<I, R>) super.withValue(value);
     }
 
     @Override
-    public DataContainerNodeAttrBuilder<I, R> withChild(DataContainerChild<?, ?> child) {
+    public DataContainerNodeAttrBuilder<I, R> withChild(final DataContainerChild<?, ?> child) {
         return (DataContainerNodeAttrBuilder<I, R>) super.withChild(child);
     }
 
     @Override
-    public DataContainerNodeAttrBuilder<I, R> withNodeIdentifier(I nodeIdentifier) {
+    public DataContainerNodeAttrBuilder<I, R> withNodeIdentifier(final I nodeIdentifier) {
         return (DataContainerNodeAttrBuilder<I, R>) super.withNodeIdentifier(nodeIdentifier);
     }
 }

yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/builder/impl/AbstractImmutableDataContainerNodeBuilder.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.yangtools.yang.data.impl.schema.builder.impl;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,8 +16,8 @@ import org.opendaylight.yangtools.yang.data.api.InstanceIdentifier.PathArgument;
 import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
 import org.opendaylight.yangtools.yang.data.api.schema.DataContainerNode;
 import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.DataContainerNodeBuilder;
-
-import com.google.common.collect.Maps;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.NormalizedNodeContainerBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.nodes.AbstractImmutableDataContainerNode;
 
 abstract class AbstractImmutableDataContainerNodeBuilder<I extends InstanceIdentifier.PathArgument, R extends DataContainerNode<I>>
         implements DataContainerNodeBuilder<I, R> {
@@ -33,7 +34,13 @@ abstract class AbstractImmutableDataContainerNodeBuilder<I extends InstanceIdent
     private boolean dirty;
 
     protected AbstractImmutableDataContainerNodeBuilder() {
-        this.value = Maps.newHashMap();
+        this.value = new HashMap<>();
+        this.dirty = false;
+    }
+
+    protected AbstractImmutableDataContainerNodeBuilder(final AbstractImmutableDataContainerNode<I> node) {
+        this.value = node.getChildren();
+        this.dirty = true;
     }
 
     protected final I getNodeIdentifier() {
@@ -51,7 +58,7 @@ abstract class AbstractImmutableDataContainerNodeBuilder<I extends InstanceIdent
 
     private void checkDirty() {
         if (dirty) {
-            value = Maps.newHashMap(value);
+            value = new HashMap<>(value);
             dirty = false;
         }
     }
@@ -73,6 +80,13 @@ abstract class AbstractImmutableDataContainerNodeBuilder<I extends InstanceIdent
     }
 
     @Override
+    public DataContainerNodeBuilder<I, R> withoutChild(final PathArgument key) {
+        checkDirty();
+        this.value.remove(key);
+        return this;
+    }
+
+    @Override
     public DataContainerNodeBuilder<I, R> withNodeIdentifier(final I nodeIdentifier) {
         this.nodeIdentifier = nodeIdentifier;
         return this;
@@ -82,5 +96,10 @@ abstract class AbstractImmutableDataContainerNodeBuilder<I extends InstanceIdent
     public DataContainerNodeBuilder<I, R> addChild(
             final DataContainerChild<? extends PathArgument, ?> child) {
         return withChild(child);
+    }
+
+    @Override
+    public NormalizedNodeContainerBuilder<I, PathArgument, DataContainerChild<? extends PathArgument, ?>, R> removeChild(final PathArgument key) {
+        return withoutChild(key);
     }
 }

commit 0d71cb8296652be2da49b50d0ac38e376abb87b2

BUG-1790: detect existing unmodifiableMap encapsulation
Commit 9bcc82d107075da35c8cd01422481eaea395ba29 introduced a way to
reuse children map by builders. Unfortunately there is a codepath which
could see us receiving the returned collection in our constructor, and
the subsequent operation would wrap it again. If you stack these
operations, at some point the nesting becomes unmanageable.

The fix is to detect a previously-encapsulated collection and reusing
it. Analysis also shows that the children field is not needed, so remove
it as well.

Change-Id: I42d604d290326afca4564565760cbcf4586c2908
Signed-off-by: Robert Varga <rovarga@cisco.com>

yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/nodes/AbstractImmutableDataContainerNode.java
@@ -16,18 +16,41 @@ import org.opendaylight.yangtools.concepts.Immutable;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
 import org.opendaylight.yangtools.yang.data.api.schema.DataContainerNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public abstract class AbstractImmutableDataContainerNode<K extends PathArgument> extends AbstractImmutableNormalizedNode<K, Iterable<DataContainerChild<? extends PathArgument, ?>>>
-implements Immutable, DataContainerNode<K> {
-
-    protected final Map<PathArgument, DataContainerChild<? extends PathArgument, ?>> children;
-    private final Map<PathArgument, DataContainerChild<? extends PathArgument, ?>> publicChildren;
+public abstract class AbstractImmutableDataContainerNode<K extends PathArgument> extends AbstractImmutableNormalizedNode<K, Iterable<DataContainerChild<? extends PathArgument, ?>>> implements Immutable, DataContainerNode<K> {
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractImmutableDataContainerNode.class);
+    private final Map<PathArgument, DataContainerChild<? extends PathArgument, ?>> children;
 
     public AbstractImmutableDataContainerNode(
             final Map<PathArgument, DataContainerChild<? extends PathArgument, ?>> children, final K nodeIdentifier) {
         super(nodeIdentifier);
-        this.children = children;
-        this.publicChildren = Collections.unmodifiableMap(children);
+
+        /*
+         * There is a code path where AbstractImmutableDataContainerNodeBuilder can reflect
+         * the collection acquired via getChildren() back to us. This is typically the case
+         * in the datastore where transactions cancel each other out, leaving an unmodified
+         * node. In that case we want to skip wrapping the map again (and again and again).
+         *
+         * In a perfect world, Collection.unmodifiableMap() would be doing the instanceof
+         * check which would stop the proliferation. Unfortunately this not the case and the
+         * 'unmodifiable' trait is not exposed by anything we can query. Furthermore the API
+         * contract there is sufficiently vague so an implementation may actually return a
+         * different implementation based on input map -- for example
+         * Collections.unmodifiableMap(Collections.emptyMap()) returning the same thing as
+         * Collections.emptyMap().
+         *
+         * This means that we have to perform the instantiation here (as opposed to once at
+         * class load time) and then compare the classes.
+         */
+        final Map<PathArgument, DataContainerChild<? extends PathArgument, ?>> pub = Collections.unmodifiableMap(children);
+        if (children.getClass().equals(pub.getClass())) {
+            LOG.trace("Reusing already-unmodifiable children {}", children);
+            this.children = children;
+        } else {
+            this.children = pub;
+        }
     }
 
     @Override
@@ -37,7 +60,7 @@ implements Immutable, DataContainerNode<K> {
 
     @Override
     public final Iterable<DataContainerChild<? extends PathArgument, ?>> getValue() {
-        return publicChildren.values();
+        return children.values();
     }
 
     @Override
@@ -46,7 +69,7 @@ implements Immutable, DataContainerNode<K> {
     }
 
     public final Map<PathArgument, DataContainerChild<? extends PathArgument, ?>> getChildren() {
-        return publicChildren;
+        return children;
     }
 
     @Override