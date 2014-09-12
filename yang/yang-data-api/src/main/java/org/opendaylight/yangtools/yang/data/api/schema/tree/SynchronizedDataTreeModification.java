2ff3d8442e0996681953cf73f5172f90c2507b4a
Robert Varga <rovarga@cisco.com>
2014-09-12 23:47:17 +0200

    BUG-650: improve modification sealing performance
    The seal operation is on a fast path, so performing synchronization is
    not really advised. Downgrade synchronized blocks to volatile
    modifications, which have better effects on the optimizer. We introduce
    a SynchronizedDataTreeModification which the users can use when they
    require a serialized thread-safe access.
    
    Change-Id: Ieffcf1f5fef4d4df32fcdc30f992672d79dcf7bf
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/tree/SynchronizedDataTreeModification.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.api.schema.tree;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+/**
+ * A {@link DataTreeModification} implementation which delegates all calls to
+ * another instance, making sure no method is being invoked from multiple threads
+ * concurrently.
+ */
+public final class SynchronizedDataTreeModification implements DataTreeModification {
+    private final DataTreeModification delegate;
+
+    private SynchronizedDataTreeModification(final DataTreeModification delegate) {
+        this.delegate = Preconditions.checkNotNull(delegate);
+    }
+
+    public DataTreeModification create(final DataTreeModification delegate) {
+        return new SynchronizedDataTreeModification(delegate);
+    }
+
+    @Override
+    public synchronized Optional<NormalizedNode<?, ?>> readNode(final YangInstanceIdentifier path) {
+        return delegate.readNode(path);
+    }
+
+    @Override
+    public synchronized DataTreeModification newModification() {
+        return delegate.newModification();
+    }
+
+    @Override
+    public synchronized void delete(final YangInstanceIdentifier path) {
+        delegate.delete(path);
+    }
+
+    @Override
+    public synchronized void merge(final YangInstanceIdentifier path, final NormalizedNode<?, ?> data) {
+        delegate.merge(path, data);
+    }
+
+    @Override
+    public synchronized void write(final YangInstanceIdentifier path, final NormalizedNode<?, ?> data) {
+        delegate.write(path, data);
+    }
+
+    @Override
+    public synchronized void ready() {
+        delegate.ready();
+    }
+}
