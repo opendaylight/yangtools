2cc112d3702e19e9c90b1d62f33ea6800042b12e
Jozef Gloncak <jgloncak@cisco.com>
2014-09-23 15:21:21 +0200

    BUG 1975 - yang unkeyed list is transformed to map node
    Parsers and serializer for base and for concrete dom type were
    added/modified to make it possible parse|serialize unkeyed list to|from
    UnkeyedListNode.
    
    Change-Id: Ia3a9554dea00bb9d424495a2b6ed1f96765be738
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/transform/base/parser/ListEntryNodeBaseParser.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/transform/base/parser/ListEntryNodeBaseParser.java
@@ -9,12 +9,8 @@ package org.opendaylight.yangtools.yang.data.impl.schema.transform.base.parser;
 
 import java.util.Map;
 import java.util.Set;
-
 import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
-import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
-import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
-import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.DataContainerNodeBuilder;
+import org.opendaylight.yangtools.yang.data.api.schema.DataContainerNode;
 import org.opendaylight.yangtools.yang.data.impl.schema.SchemaUtils;
 import org.opendaylight.yangtools.yang.model.api.AugmentationSchema;
 import org.opendaylight.yangtools.yang.model.api.AugmentationTarget;
@@ -23,23 +19,13 @@ import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.ListSchemaNode;
 
 /**
- * Abstract(base) parser for MapEntryNodes, parses elements of type E.
+ * Abstract(base) parser for ListNodes (MapNode, UnkeyedListNode), parses elements of type E.
  *
- * @param <E> type of elements to be parsed
+ * @param <E>
+ *            type of elements to be parsed
  */
-public abstract class MapEntryNodeBaseParser<E> extends BaseDispatcherParser<E, MapEntryNode, ListSchemaNode> {
-
-    @Override
-    protected final DataContainerNodeBuilder<YangInstanceIdentifier.NodeIdentifierWithPredicates, MapEntryNode> getBuilder(
-            ListSchemaNode schema) {
-        return Builders.mapEntryBuilder(schema);
-    }
-
-    @Override
-    public final MapEntryNode parse(Iterable<E> elements, ListSchemaNode schema) {
-        checkOnlyOneNode(schema, elements);
-        return super.parse(elements, schema);
-    }
+public abstract class ListEntryNodeBaseParser<E, N extends DataContainerNode<?>> extends
+        BaseDispatcherParser<E, N, ListSchemaNode> {
 
     @Override
     protected final Set<DataSchemaNode> getRealSchemasForAugment(ListSchemaNode schema, AugmentationSchema augmentSchema) {
@@ -59,6 +45,12 @@ public abstract class MapEntryNodeBaseParser<E> extends BaseDispatcherParser<E, 
     @Override
     protected final Map<QName, AugmentationSchema> mapChildElementsFromAugments(ListSchemaNode schema) {
         return SchemaUtils.mapChildElementsFromAugments(schema);
+    }
+
+    @Override
+    public final N parse(Iterable<E> elements, ListSchemaNode schema) {
+        checkOnlyOneNode(schema, elements);
+        return super.parse(elements, schema);
     }
 
     @Override
