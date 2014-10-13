99c9c667948278f01fe31d210f0e24025d6692f3
Jozef Gloncak <jgloncak@cisco.com>
2014-10-13 13:23:46 +0200

    BUG 2153 - pretty print support in yang json output
    Support of pretty pringint of JSON output
    
    Change-Id: Ie352d87627e2f2a7d86a25a5610c21a2a9f30912
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

--- yang/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/JSONStreamWriterInvisibleContext.java
+++ yang/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/JSONStreamWriterInvisibleContext.java
@@ -8,20 +8,37 @@
 package org.opendaylight.yangtools.yang.data.codec.gson;
 
 import com.google.common.base.Preconditions;
-
+import java.io.IOException;
 import java.io.Writer;
 
 /**
- * A virtual recursion level in {@link JSONNormalizedNodeStreamWriter}, used for nodes
- * which are not emitted in the JSON representation.
+ * A virtual recursion level in {@link JSONNormalizedNodeStreamWriter}, used for nodes which are not emitted in the JSON
+ * representation.
  */
 final class JSONStreamWriterInvisibleContext extends JSONStreamWriterURIContext {
+    boolean firstChild = true;
+
     JSONStreamWriterInvisibleContext(final JSONStreamWriterContext parent) {
-        super(Preconditions.checkNotNull(parent), parent.getNamespace());
+        super(Preconditions.checkNotNull(parent), parent.getNamespace(), parent.getIndentLevel());
     }
 
     @Override
-    protected void emitEnd(final Writer writer) {
+    protected void emitEnd(final Writer writer, final String indent) {
         // No-op
     }
+
+    /**
+     * White spaces (new line + indentation) is for invisible node printed only if leafNode, leafSetEntryNode,
+     * anyXmlNode methods are called in context of invisible node (it means that doesn't exist context which parent
+     * would be this invisible context) and it isn't first child of this node (because this indentation is prepared by
+     * parent context).
+     */
+    @Override
+    protected void writeWhiteSpaces(final Writer writer, final String indent) throws IOException {
+        if (!hasChildContext() && !firstChild) {
+            super.writeWhiteSpaces(writer, indent);
+        }
+        firstChild = false;
+    }
+
 }
