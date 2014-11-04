ce857d1b20697596f1a14dd8b3a5ab18eb5a63ae
Ladislav Borak <lborak@cisco.com>
2014-11-04 08:16:32 +0100

    Bug 2297: Standalone model validation tool
    - initial version
    
    Change-Id: I22da7a2c51578eab1e91d0c7cca17a68ea879e7d
    Signed-off-by: Ladislav Borak <lborak@cisco.com>

---
+++ yang-validation-tool/src/main/java/org/opendaylight/yangtools/yang/validation/tool/Main.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.validation.tool;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.annotation.Arg;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import org.opendaylight.yangtools.yang.parser.impl.YangParserImpl;
+
+public class Main {
+
+    static class Params {
+
+        @Arg(dest = "yang-source-dir")
+        public File yangSourceDir;
+
+        static ArgumentParser getParser() throws URISyntaxException {
+            final ArgumentParser parser = ArgumentParsers.newArgumentParser("yang validation tool");
+            parser.description("Yang models validation tool.");
+
+            parser.addArgument("--yang-source-dir")
+                .type(File.class)
+                .help("Directory containing yang models which will be parsed.").dest("yang-source-dir")
+                .dest("yang-source-dir")
+                .setDefault(getDefaultSourceFile());
+
+            return parser;
+        }
+
+        void validate() {
+            if (yangSourceDir != null) {
+                checkArgument(yangSourceDir.exists(), "Yang source directory has to exist");
+                checkArgument(yangSourceDir.isDirectory(), "Yang source directory has to be a directory");
+                checkArgument(yangSourceDir.canRead(), "Yang source directory has to be readable");
+                //checkArgument(Iterables.isEmpty(Arrays.asList(yangSourceDir.listFiles())), "Yang source directory must not be emtpy");
+            }
+        }
+
+        static File getDefaultSourceFile() throws URISyntaxException {
+            return new File(Main.class.getResource("/yang-sources").toURI());
+        }
+    }
+
+    public static void main(String[] args) throws URISyntaxException {
+        final Params params = parseArgs(args, Params.getParser());
+        params.validate();
+
+        final YangParserImpl yangParser = new YangParserImpl();
+        final File[] yangModels = params.yangSourceDir.listFiles();
+
+        yangParser.parseFiles(Arrays.asList(yangModels));
+    }
+
+    private static Params parseArgs(final String[] args, final ArgumentParser parser) {
+        final Params opt = new Params();
+        try {
+            parser.parseArgs(args, opt);
+            return opt;
+        } catch (final ArgumentParserException e) {
+            parser.handleError(e);
+        }
+        System.exit(1);
+        return null;
+    }
+}
