a8f86bde0241a6e8cb1fda6120cd9f79482fb7f4
pkajsa <pkajsa@cisco.com>
2014-11-11 08:08:38 +0100

    Bug 2326 - NormalizeNode equals fails when NormalizeNodes being compared contain binary data which is identical.
    Change-Id: If049e215156f5338eb3ff0ab95d19eecd622defd
    Signed-off-by: pkajsa <pkajsa@cisco.com>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/nodes/AbstractImmutableNormalizedValueAttrNode.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/nodes/AbstractImmutableNormalizedValueAttrNode.java
@@ -10,7 +10,10 @@ package org.opendaylight.yangtools.yang.data.impl.schema.nodes;
 import com.google.common.base.Objects;
 import com.google.common.base.Objects.ToStringHelper;
 import com.google.common.collect.ImmutableMap;
+
+import java.util.Arrays;
 import java.util.Map;
+
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.AttributesContainer;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
@@ -53,17 +56,61 @@ public abstract class AbstractImmutableNormalizedValueAttrNode<K extends YangIns
 
     @Override
     protected boolean valueEquals(final AbstractImmutableNormalizedNode<?, ?> other) {
-        // We can not call directly getValue.equals because of Empty Type Definition leaves
+        // We can not call directly getValue.equals because of Empty Type
+        // Definition leaves
         // which allways have NULL value
-        if (!Objects.equal(getValue(), other.getValue())) {
+
+        Object thisValue = getValue();
+        Object otherValue = other.getValue();
+
+        if (thisValue != null && otherValue != null && thisValue.getClass().isArray()
+                && otherValue.getClass().isArray()) {
+            return arrayEquals(thisValue, otherValue);
+        }
+
+        if (!Objects.equal(thisValue, otherValue)) {
             return false;
         }
 
-// FIXME: are attributes part of hashCode/equals?
-//        final Set<Entry<QName, String>> tas = getAttributes().entrySet();
-//        final Set<Entry<QName, String>> oas = container.getAttributes().entrySet();
-//
-//        return tas.containsAll(oas) && oas.containsAll(tas);
+        // FIXME: are attributes part of hashCode/equals?
+        // final Set<Entry<QName, String>> tas = getAttributes().entrySet();
+        // final Set<Entry<QName, String>> oas =
+        // container.getAttributes().entrySet();
+        //
+        // return tas.containsAll(oas) && oas.containsAll(tas);
         return true;
     }
+
+    private boolean arrayEquals(Object thisValue, Object otherValue) {
+
+        if (thisValue instanceof Object[] && otherValue instanceof Object[])
+            return Arrays.deepEquals((Object[]) thisValue, (Object[]) otherValue);
+
+        else if (thisValue instanceof byte[] && otherValue instanceof byte[])
+            return Arrays.equals((byte[]) thisValue, (byte[]) otherValue);
+
+        else if (thisValue instanceof boolean[] && otherValue instanceof boolean[])
+            return Arrays.equals((boolean[]) thisValue, (boolean[]) otherValue);
+
+        else if (thisValue instanceof char[] && otherValue instanceof char[])
+            return Arrays.equals((char[]) thisValue, (char[]) otherValue);
+
+        else if (thisValue instanceof int[] && otherValue instanceof int[])
+            return Arrays.equals((int[]) thisValue, (int[]) otherValue);
+
+        else if (thisValue instanceof float[] && otherValue instanceof float[])
+            return Arrays.equals((float[]) thisValue, (float[]) otherValue);
+
+        else if (thisValue instanceof double[] && otherValue instanceof double[])
+            return Arrays.equals((double[]) thisValue, (double[]) otherValue);
+
+        else if (thisValue instanceof short[] && otherValue instanceof short[])
+            return Arrays.equals((short[]) thisValue, (short[]) otherValue);
+
+        else if (thisValue instanceof long[] && otherValue instanceof long[])
+            return Arrays.equals((long[]) thisValue, (long[]) otherValue);
+
+        return false;
+    }
+
 }
