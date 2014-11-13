725f4c4f12a3fc32118f388b693db7416c8c91f7
Robert Varga <rovarga@cisco.com>
2014-11-13 14:47:26 +0100

    BUG-2377: introduce internal UnmodifiableChildrenMap
    This new class acts a replacement of java.util.Collection's version of
    the same. It uses a proper instanceof check to elide instantiating
    objects if the delegate is a known-immutable class. It also squashes
    empty maps to Collections.emptyMap().
    
    Change-Id: I84c8ac428d6b207fafccf8cc898f0555b104a4ee
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/nodes/UnmodifiableChildrenMap.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.impl.schema.nodes;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Internal equivalent of {@link Collections}' unmodifiable Map. It does not retain
+ * keySet/entrySet references, thus lowering the memory overhead.
+ */
+final class UnmodifiableChildrenMap implements Map<PathArgument, DataContainerChild<? extends PathArgument, ?>>, Serializable {
+    private static final Logger LOG = LoggerFactory.getLogger(UnmodifiableChildrenMap.class);
+    private static final long serialVersionUID = 1L;
+    private final Map<PathArgument, DataContainerChild<? extends PathArgument, ?>> delegate;
+    private transient Collection<DataContainerChild<? extends PathArgument, ?>> values;
+
+    private UnmodifiableChildrenMap(final Map<PathArgument, DataContainerChild<? extends PathArgument, ?>> delegate) {
+        this.delegate = Preconditions.checkNotNull(delegate);
+    }
+
+    /**
+     * Create an unmodifiable view of a particular map. Does not perform unnecessary
+     * encapsulation if the map is known to be already unmodifiable.
+     *
+     * @param map Backing map
+     * @return Unmodifiable view
+     */
+    static Map<PathArgument, DataContainerChild<? extends PathArgument, ?>> create(final Map<PathArgument, DataContainerChild<? extends PathArgument, ?>> map) {
+        if (map instanceof UnmodifiableChildrenMap) {
+            return map;
+        }
+        if (map instanceof ImmutableMap) {
+            return map;
+        }
+        if (map.size() == 0) {
+            return Collections.emptyMap();
+        }
+
+        return new UnmodifiableChildrenMap(map);
+    }
+
+    @Override
+    public int size() {
+        return delegate.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return delegate.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(final Object key) {
+        return delegate.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(final Object value) {
+        return delegate.containsValue(value);
+    }
+
+    @Override
+    public DataContainerChild<? extends PathArgument, ?> get(final Object key) {
+        return delegate.get(key);
+    }
+
+    @Override
+    public DataContainerChild<? extends PathArgument, ?> put(final PathArgument key,
+            final DataContainerChild<? extends PathArgument, ?> value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public DataContainerChild<? extends PathArgument, ?> remove(final Object key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void putAll(final Map<? extends PathArgument, ? extends DataContainerChild<? extends PathArgument, ?>> m) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Set<PathArgument> keySet() {
+        return Collections.unmodifiableSet(delegate.keySet());
+    }
+
+    @Override
+    public Collection<DataContainerChild<? extends PathArgument, ?>> values() {
+        if (values == null) {
+            values = Collections.unmodifiableCollection(delegate.values());
+        }
+        return values;
+    }
+
+    @Override
+    public Set<Entry<PathArgument, DataContainerChild<? extends PathArgument, ?>>> entrySet() {
+        LOG.warn("Invocation of inefficient entrySet()", new Throwable().fillInStackTrace());
+        return Collections.unmodifiableMap(delegate).entrySet();
+    }
+
+
+    @Override
+    public boolean equals(final Object o) {
+        return this == o || delegate.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return delegate.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return delegate.toString();
+    }
+}
