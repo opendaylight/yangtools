1e557178d08e9238405f4e2d9456769d48394318
pkajsa <pkajsa@cisco.com>
2014-11-13 14:51:20 +0100

    Bug 2333 - Java code generation error, when yang model contains Choice node in module body at top level
    BindingGeneratorImpl fails (NoSuchElementException) during java source code
    generation from yang model which contains Choice node directly in module body
    at top level. The parent path of Choice node is empty (because the parent
    is the module) and therefore iterator.next() on the path fails. For more
    information see Bug 2333.
    
    Change-Id: I6e617eca91bb5d8d2b91ad058a4df5c2793ec81d
    Signed-off-by: pkajsa <pkajsa@cisco.com>

---
+++ code-generator/binding-generator-impl/src/test/java/org/opendaylight/yangtools/sal/binding/generator/impl/BindingGeneratorImplTest.java
@@ -0,0 +1,116 @@
+/**
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ */
+package org.opendaylight.yangtools.sal.binding.generator.impl;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import org.junit.Test;
+import org.opendaylight.yangtools.sal.binding.model.api.GeneratedType;
+import org.opendaylight.yangtools.sal.binding.model.api.ParameterizedType;
+import org.opendaylight.yangtools.sal.binding.model.api.Type;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.parser.api.YangSyntaxErrorException;
+import org.opendaylight.yangtools.yang.parser.impl.YangParserImpl;
+
+public class BindingGeneratorImplTest {
+
+    @Test
+    public void choiceNodeGenerationTest() throws IOException, YangSyntaxErrorException, URISyntaxException {
+        File resourceFile = new File(getClass().getResource("/binding-generator-impl-test/choice-test.yang").toURI());
+        File resourceDir = resourceFile.getParentFile();
+
+        YangParserImpl parser = YangParserImpl.getInstance();
+        SchemaContext context = parser.parseFile(resourceFile, resourceDir);
+
+        List<Type> generateTypes = new BindingGeneratorImpl(false).generateTypes(context);
+
+        GeneratedType choiceTestData = null;
+        GeneratedType myRootContainer = null;
+        GeneratedType myList = null;
+        GeneratedType myContainer = null;
+        GeneratedType myList2 = null;
+        GeneratedType myContainer2 = null;
+
+        for (Type type : generateTypes) {
+            switch (type.getName()) {
+            case "ChoiceTestData":
+                choiceTestData = (GeneratedType) type;
+                break;
+            case "Myrootcontainer":
+                myRootContainer = (GeneratedType) type;
+                break;
+            case "Mylist":
+                myList = (GeneratedType) type;
+                break;
+            case "Mylist2":
+                myList2 = (GeneratedType) type;
+                break;
+            case "Mycontainer":
+                myContainer = (GeneratedType) type;
+                break;
+            case "Mycontainer2":
+                myContainer2 = (GeneratedType) type;
+                break;
+            }
+        }
+
+        assertNotNull(choiceTestData);
+        assertNotNull(myRootContainer);
+        assertNotNull(myList);
+        assertNotNull(myContainer);
+        assertNotNull(myList2);
+        assertNotNull(myContainer2);
+
+        List<Type> implements1 = myContainer.getImplements();
+        Type childOfParamType = null;
+        for (Type type : implements1) {
+            if (type.getName().equals("ChildOf")) {
+                childOfParamType = ((ParameterizedType) type).getActualTypeArguments()[0];
+                break;
+            }
+        }
+        assertNotNull(childOfParamType);
+        assertTrue(childOfParamType.getName().equals("ChoiceTestData"));
+
+        implements1 = myList.getImplements();
+        childOfParamType = null;
+        for (Type type : implements1) {
+            if (type.getName().equals("ChildOf")) {
+                childOfParamType = ((ParameterizedType) type).getActualTypeArguments()[0];
+                break;
+            }
+        }
+        assertNotNull(childOfParamType);
+        assertTrue(childOfParamType.getName().equals("ChoiceTestData"));
+
+        implements1 = myContainer2.getImplements();
+        childOfParamType = null;
+        for (Type type : implements1) {
+            if (type.getName().equals("ChildOf")) {
+                childOfParamType = ((ParameterizedType) type).getActualTypeArguments()[0];
+                break;
+            }
+        }
+        assertNotNull(childOfParamType);
+        assertTrue(childOfParamType.getName().equals("Myrootcontainer"));
+
+        implements1 = myList2.getImplements();
+        childOfParamType = null;
+        for (Type type : implements1) {
+            if (type.getName().equals("ChildOf")) {
+                childOfParamType = ((ParameterizedType) type).getActualTypeArguments()[0];
+                break;
+            }
+        }
+        assertNotNull(childOfParamType);
+        assertTrue(childOfParamType.getName().equals("Myrootcontainer"));
+
+    }
+
+}
