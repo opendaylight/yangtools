a241f3cd6062a538cb4e0b8c6ef727e279996e15
Tony Tkacik <ttkacik@cisco.com>
2014-11-24 15:19:41 +0100

    Bug 2365: Initial version of YIN Schema.
    Added yang-schema-export module, which contains
    API definition and utility classes for implementing
    exporting of YANG and YIN schemas from parsed SchemaContext.
    
    Current implementation of YIN export is in beta quality
    and may omit some statements (marked with FIXME in code).
    
    Yang export (reconstruction of Yang file) from schema context
    is currently unsupported as it could be seen, that no
    such public API for exporting YANG file exists.
    
    Change-Id: I1be0591cfc78de72592f192249d1897de4f07e80
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ yang/yang-model-export/src/main/java/org/opendaylight/yangtools/yang/model/export/YangModuleWriter.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.export;
+
+import com.google.common.primitives.UnsignedInteger;
+import java.net.URI;
+import java.util.Date;
+import java.util.List;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.RevisionAwareXPath;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.api.Status;
+
+interface YangModuleWriter {
+
+    void endNode();
+
+    void startModuleNode(String identifier);
+
+    void startOrganizationNode(String input);
+
+    void startContactNode(String input);
+
+    void startDescriptionNode(String input);
+
+    void startUnitsNode(String input);
+
+    void startYangVersionNode(String input);
+
+    void startNamespaceNode(URI uri);
+
+    void startKeyNode(List<QName> keyList);
+
+    void startPrefixNode(String input);
+
+    void startFeatureNode(QName qName);
+
+    void startExtensionNode(QName qName);
+
+    void startArgumentNode(String input);
+
+    void startStatusNode(Status status);
+
+    void startTypeNode(QName qName);
+
+    void startLeafNode(QName qName);
+
+    void startContainerNode(QName qName);
+
+    void startGroupingNode(QName qName);
+
+    void startRpcNode(QName qName);
+
+    void startInputNode();
+
+    void startOutputNode();
+
+    void startLeafListNode(QName qName);
+
+    void startListNode(QName qName);
+
+    void startChoiceNode(QName qName);
+
+    void startCaseNode(QName qName);
+
+    void startNotificationNode(QName qName);
+
+    void startIdentityNode(QName qName);
+
+    void startBaseNode(QName qName);
+
+    void startTypedefNode(QName qName);
+
+    void startRevisionNode(Date date);
+
+    void startDefaultNode(String string);
+
+    void startMustNode(RevisionAwareXPath xpath);
+
+    void startErrorMessageNode(String input);
+
+    void startErrorAppTagNode(String input);
+
+    void startPatternNode(String regularExpression);
+
+    void startValueNode(Integer integer);
+
+    void startEnumNode(String name);
+
+    void startRequireInstanceNode(boolean require);
+
+    void startPathNode(RevisionAwareXPath revisionAwareXPath);
+
+    void startBitNode(String name);
+
+    void startPositionNode(UnsignedInteger position);
+
+    void startReferenceNode(String input);
+
+    void startRevisionDateNode(Date date);
+
+    void startImportNode(String moduleName);
+
+    void startUsesNode(QName groupingName);
+
+    void startAugmentNode(SchemaPath targetPath);
+
+    void startConfigNode(boolean config);
+
+    void startLengthNode(String lengthString);
+
+    void startMaxElementsNode(Integer max);
+
+    void startMinElementsNode(Integer min);
+
+    void startPresenceNode(boolean presence);
+
+    void startOrderedByNode(String ordering);
+
+    void startRangeNode(String rangeString);
+
+    void startRefineNode(SchemaPath path);
+
+    void startMandatoryNode(boolean mandatory);
+
+    void startAnyxmlNode(QName qName);
+
+    void startUnknownNode(StatementDefinition def);
+
+    void startUnknownNode(StatementDefinition def, String nodeParameter);
+
+    void startFractionDigitsNode(Integer fractionDigits);
+
+    void startYinElementNode(boolean yinElement);
+
+    void startWhenNode(RevisionAwareXPath revisionAwareXPath);
+
+}
