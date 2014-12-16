313f4913bf92f5eb139363ebabd1c7c7c3a0ef0f
Jozef Gloncak <jgloncak@cisco.com>
2014-12-16 16:34:09 +0100

    BUG 2468 - leafrefs via import statement
    Adds method which search for base type of leafref in module specified via
    QName value.
    
    Change-Id: Ib1979b56604429fccac2d9955387085aa59ed01d
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

--- yang/yang-model-util/src/main/java/org/opendaylight/yangtools/yang/model/util/SchemaContextUtil.java
+++ yang/yang-model-util/src/main/java/org/opendaylight/yangtools/yang/model/util/SchemaContextUtil.java
@@ -16,17 +16,13 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
-
 import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.model.api.ChoiceCaseNode;
 import org.opendaylight.yangtools.yang.model.api.ChoiceNode;
-import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.DataNodeContainer;
 import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.GroupingDefinition;
 import org.opendaylight.yangtools.yang.model.api.LeafListSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
-import org.opendaylight.yangtools.yang.model.api.ListSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang.model.api.ModuleImport;
 import org.opendaylight.yangtools.yang.model.api.NotificationDefinition;
@@ -602,6 +598,36 @@ public final class SchemaContextUtil {
         final TypeDefinition<?> targetTypeDefinition = typeDefinition(dataSchemaNode);
 
         if(targetTypeDefinition instanceof LeafrefTypeDefinition) {
+            return getBaseTypeForLeafRef(((LeafrefTypeDefinition) targetTypeDefinition), schemaContext, dataSchemaNode);
+        } else {
+            return targetTypeDefinition;
+        }
+    }
+
+    /**
+     * Returns base type for {@code typeDefinition} which belongs to module specified via {@code qName}. This handle case
+     * when leafref type isn't specified as type substatement of leaf or leaf-list but is defined in other module as typedef
+     * which is then imported to referenced module.
+     *
+     * Because {@code typeDefinition} is definied via typedef statement, only absolute path is meaningful.
+     *
+     * @param typeDefinition
+     * @param schemaContext
+     * @param qName
+     * @return
+     */
+    public static TypeDefinition<?> getBaseTypeForLeafRef(final LeafrefTypeDefinition typeDefinition,
+            final SchemaContext schemaContext, final QName qName) {
+        final RevisionAwareXPath pathStatement = typeDefinition.getPathStatement();
+        final RevisionAwareXPath strippedPathStatement = new RevisionAwareXPathImpl(stripConditionsFromXPathString(pathStatement), pathStatement.isAbsolute());
+        if (!strippedPathStatement.isAbsolute()) {
+            return null;
+        }
+
+        final Module parentModule = schemaContext.findModuleByNamespaceAndRevision(qName.getNamespace(),qName.getRevision());
+        final DataSchemaNode dataSchemaNode = (DataSchemaNode) SchemaContextUtil.findDataSchemaNode(schemaContext, parentModule, strippedPathStatement);
+        final TypeDefinition<?> targetTypeDefinition = typeDefinition(dataSchemaNode);
+        if (targetTypeDefinition instanceof LeafrefTypeDefinition) {
             return getBaseTypeForLeafRef(((LeafrefTypeDefinition) targetTypeDefinition), schemaContext, dataSchemaNode);
         } else {
             return targetTypeDefinition;
