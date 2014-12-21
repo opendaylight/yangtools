157b5693fa9d527be699659b00cc913ca56460fd
Robert Varga <rovarga@cisco.com>
2014-12-21 01:53:08 +0100

    BUG-2350: improve performance of data tree merges
    Profiling has revealed that performing a merge on a large subtree
    results in all pre-existing nodes being verified. The MD-SAL performs an
    empty merge whenever the user asks for parents to be created --
    resulting in up to 75% overhead.
    
    Change-Id: I94310b2cb27c8585a8b8c4933ff0f0f658fda9be
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/OperationWithModification.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/OperationWithModification.java
@@ -45,8 +45,16 @@ final class OperationWithModification {
     }
 
     void merge(final NormalizedNode<?, ?> data) {
+        /*
+         * A merge operation will end up overwriting parts of the tree, retaining others.
+         * We want to make sure we do not validate the complete resulting structure, but
+         * rather just what was written. In order to do that, we first pretend the data
+         * was written, run verification and then perform the merge -- with the explicit
+         * assumption that adding the newly-validated data with the previously-validated
+         * data will not result in invalid data.
+         */
+        applyOperation.verifyStructure(modification.asNewlyWritten(data));
         mergeImpl(data);
-        applyOperation.verifyStructure(modification);
     }
 
     void delete() {
