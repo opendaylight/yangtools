7d4c17a42f95352eb5850e37d03244e78849d338
Peter Kajsa <pkajsa@cisco.com>
2015-01-08 13:50:02 +0100

    Bug 2528 - Binding Generator: Generated notification implements ChildOf interface
    Generated Notification interface implements ChildOf<DataObject> interface
    which is incorrect. Generated Notification should not implement ChildOf
    interface since it is not part of data tree. This could lead to use of
    Notification in wrong context and may confuse consumers of APIs.
    
    Notice: The solution replaces "implements ChildOf<DataObject>" with
    "implements DataObject" in generated Notification interface. Notification
    has structured content, so I think the generated Notification interface should
    implements at least DataObject interface in order to be able to construct
    InstanceIdentifier for the notification (analogously as it is by RPC's input
    and output).
    
    Change-Id: I55c7a8cc4ce450fd712c63f8e77881d4a18674e6
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

--- code-generator/binding-generator-impl/src/test/java/org/opendaylight/yangtools/sal/binding/generator/impl/BindingGeneratorImplTest.java
+++ code-generator/binding-generator-impl/src/test/java/org/opendaylight/yangtools/sal/binding/generator/impl/BindingGeneratorImplTest.java
@@ -8,6 +8,7 @@
 package org.opendaylight.yangtools.sal.binding.generator.impl;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.io.IOException;
@@ -121,6 +122,43 @@ public class BindingGeneratorImplTest {
         assertNotNull(childOfParamType);
         assertTrue(childOfParamType.getName().equals("Myrootcontainer"));
 
+    }
+
+    @Test
+    public void notificationGenerationTest() throws IOException, YangSyntaxErrorException, URISyntaxException {
+        File resourceFile = new File(getClass().getResource("/binding-generator-impl-test/notification-test.yang")
+                .toURI());
+        File resourceDir = resourceFile.getParentFile();
+
+        YangParserImpl parser = YangParserImpl.getInstance();
+        SchemaContext context = parser.parseFile(resourceFile, resourceDir);
+
+        List<Type> generateTypes = new BindingGeneratorImpl(false).generateTypes(context);
+
+        GeneratedType foo = null;
+        for (Type type : generateTypes) {
+            if (type.getName().equals("Foo")) {
+                foo = (GeneratedType) type;
+                break;
+            }
+        }
+
+        Type childOf = null;
+        Type dataObject = null;
+        List<Type> impl = foo.getImplements();
+        for (Type type : impl) {
+            switch (type.getName()) {
+            case "ChildOf":
+                childOf = type;
+                break;
+            case "DataObject":
+                dataObject = type;
+                break;
+            }
+        }
+
+        assertNull(childOf);
+        assertNotNull(dataObject);
     }
 
 }
