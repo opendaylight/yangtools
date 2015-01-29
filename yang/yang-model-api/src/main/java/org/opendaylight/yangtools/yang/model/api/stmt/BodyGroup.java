169e6953439ad706b65b9fa9d28cba26b62fb438
Tony Tkacik <ttkacik@cisco.com>
2015-01-29 17:39:25 +0100

    Bug 2444: Added concrete API definitions for YANG statements.
    Introduced type captures of DeclaredStatement for RFC6020 statments
    along with some convenience getters to decrease need of users
    to look up statements in collections.
    
    This getters conveys RFC6020 semantic, but does not prevent
    access to any statements which may be introduced in later
    versions of YANG.
    
    Change-Id: I7ca3265aa7fc141446515e27f02c37ff3b5903dc
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/BodyGroup.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.api.stmt;
+
+import java.util.Collection;
+import javax.annotation.Nonnull;
+
+
+@Rfc6020AbnfRule("body-stmts")
+public interface BodyGroup extends DataDefinitionContainer.WithReusableDefinitions {
+
+    @Nonnull Collection<? extends ExtensionStatement> getExtensions();
+
+    @Nonnull Collection<? extends FeatureStatement> getFeatures();
+
+    @Nonnull Collection<? extends IdentityStatement> getIdentities();
+
+    @Nonnull Collection<? extends AugmentStatement> getAugments();
+
+    @Nonnull Collection<? extends RpcStatement> getRpcs();
+
+    @Nonnull Collection<? extends NotificationStatement> getNotifications();
+
+    @Nonnull Collection<? extends DeviationStatement> getDeviations();
+
+}
