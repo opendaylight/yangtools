b2389b744b28f3c401eeef0039685d16a6f4b010
Tony Tkacik <ttkacik@cisco.com>
2015-01-29 17:37:09 +0100

    Bug 2512: Initial design of YANG statement meta-model.
    Analysis of several new functional requirements for YANG parser,
    showed  need to define meta-model of YANG model, which
    could be used for transformation and processing of different YANG model
    serialization formats (YANG text and YIN).
    
    This new model introduces additional concept from YANG:
    
      - Statement
         - DeclaredStatement
         - EffectiveStatement
      - StatementDefinition
      - IdentifierNamespace
    
    This basic building blocks allow us to describe with these APIs
    any YANG statement model, which may be introduced down the line
    in form of YANG extensions or updates to YANG specification,
    which does not affect statement principle.
    
    Change-Id: I9d42eaeba41fc388ed84dc7071d8e38755daab5d
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/meta/IdentifierNamespace.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.api.meta;
+
+import javax.annotation.Nullable;
+/**
+ *
+ * Model specific namespace which allows access to specific
+ *
+ * {@link IdentifierNamespace} serves as common superclass for YANG model
+ * namespaces, which are type-captured subclasses. This type capture
+ * of namespace allows for handy type-safe reading methods
+ * such as {@link EffectiveStatement#get(Class, Object)} and still
+ * allows introduction of new namespaces without need to change
+ * model APIs.
+ *
+ * @param <K> Identifier type
+ * @param <V> Value type
+ */
+public interface IdentifierNamespace<K,V> {
+
+    @Nullable V get(K identifier);
+
+}
---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/meta/ModelStatement.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.api.meta;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ *
+ * Model statement
+ *
+ * There are two base types of model statements:
+ * <ul>
+ * <li>{@link DeclaredStatement} - Statement representation as was defined in original
+ * source. This representation could be used during computation of effective model
+ * or during transforming YANG model from one serialization format to other.
+ * </li>
+ * <li>
+ * {@link EffectiveStatement} - Representation of effective statement - this
+ * statement may be different from declared, in such way, that it contains additional
+ * substatements, provides access to model namespaces. Some effective statements may be not
+ * directly declared in YANG source, but could be inferred by semantic processing of
+ * other statements (eg. uses, augment).
+ * </li>
+ * </ul>
+ *
+ * @param <A> Argument type ({@link Void} if statement does not have argument.)
+ */
+public interface ModelStatement<A> {
+
+    /**
+     * Statement Definition of this statement.
+     *
+     * @return definition of this statement.
+     */
+    @Nonnull StatementDefinition statementDefinition();
+
+    /**
+     *
+     * Returns statement argument
+     *
+     * @return statement argument or null if statement does not have argument.
+     */
+    @Nullable A argument();
+
+    /**
+     * Returns statement source, which denotes if statement was
+     * explicitly declared in original model or inferred during
+     * semantic processing of model.
+     *
+     * @return statement source.
+     */
+    @Nonnull StatementSource getStatementSource();
+
+}
