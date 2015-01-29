0a6a13dc0760545808691831a7eadad23e6b08e5
Tony Tkacik <ttkacik@cisco.com>
2015-01-29 17:37:09 +0100

    Bug 2512: Initial design of YANG statement meta-model.
    Analysis of several new functional requirements for YANG parser,
    leaded to need to define meta-model of YANG model, which
    could be used for transformation and processing of different YANG model
    serialization formats (YANG text and YIN).
    
    This new model introduces additional concept from YANG:
    
      - Statement
      - StatementDefinition
      - IdentifierNamespace
    
    Change-Id: I9d42eaeba41fc388ed84dc7071d8e38755daab5d
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/meta/DeclaredStatement.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.api.meta;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+/**
+ * Represents declared statement
+ *
+ * @param <A> Argument type ({@link Void} if statement does not have argument.)
+ */
+public interface DeclaredStatement<A> extends ModelStatement<A> {
+
+    @Nullable String rawArgument();
+
+    /**
+     *
+     * Returns collection of explicitly declared child statements, while preserving its original
+     * ordering from original source.
+     *
+     * @return Collection of statements, which were explicitly declared in
+     *         source of model.
+     */
+    @Nonnull Iterable<? extends DeclaredStatement<?>> declaredSubstatements();
+
+}
