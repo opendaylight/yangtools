ba80c6099600545eef29f58d3966af427a51c267
Marian Dubai <mdubai@cisco.com>
2015-01-29 10:29:51 +0100

    BUG 2307 - Filtering proxy for Schema context functionality
    Change-Id: If4c2231993ae70144f677625df100fa078695863
    Signed-off-by: Marian Dubai <mdubai@cisco.com>

---
+++ yang/yang-model-util/src/main/java/org/opendaylight/yangtools/yang/model/util/FilteringSchemaContextProxy.java
@@ -0,0 +1,270 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.html
+ */
+
+package org.opendaylight.yangtools.yang.model.util;
+
+import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+import com.google.common.base.Strings;
+import com.google.common.collect.*;
+import com.google.common.collect.ImmutableSet.Builder;
+import java.net.URI;
+import java.util.*;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.Immutable;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.ModuleIdentifier;
+import org.opendaylight.yangtools.yang.model.api.ModuleImport;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+@Immutable
+public final class FilteringSchemaContextProxy extends AbstractSchemaContext {
+
+    //collection to be filled with filtered modules
+    private final Set<Module> filteredModules;
+    
+    //collections to be filled in with filtered data
+    private final Map<ModuleIdentifier, String> identifiersToSources;
+    private final SetMultimap<URI, Module> namespaceToModules;
+    private final SetMultimap<String, Module> nameToModules;
+
+    /**
+     * Filters SchemaContext for yang modules
+     *
+     * @param delegate original SchemaContext
+     * @param rootModules modules (yang schemas) to be available and all their dependencies (modules importing rootModule and whole chain of their imports)
+     * @param additionalModuleIds (additional) modules (yang schemas) to be available and whole chain of their imports
+     *
+     * @return SchemaContext with filtered yang modules
+     */
+
+
+    public FilteringSchemaContextProxy(final SchemaContext delegate, final Collection<ModuleId> rootModules, final Set<ModuleId> additionalModuleIds) {
+
+        Preconditions.checkArgument(rootModules!=null,"Base modules cannot be null." );
+        Preconditions.checkArgument(additionalModuleIds!=null,"Additional modules cannot be null." );
+
+        final Builder<Module> filteredModulesBuilder = new Builder<>();
+
+         /*
+         * The most common lookup is from Namespace->Module.delegate.getModules()
+         *
+         * RESTCONF performs lookups based on module name only, where it wants
+         * to receive the latest revision
+         *
+         * Invest some quality time in building up lookup tables for both.
+         */
+        final SetMultimap<URI, Module> nsMap = Multimaps.newSetMultimap(
+                new TreeMap<URI, Collection<Module>>(), MODULE_SET_SUPPLIER);
+        final SetMultimap<String, Module> nameMap = Multimaps.newSetMultimap(
+                new TreeMap<String, Collection<Module>>(), MODULE_SET_SUPPLIER);
+
+        ImmutableMap.Builder<ModuleIdentifier, String> identifiersToSourcesBuilder = ImmutableMap.builder();
+
+        //preparing map to get all modules with one name but difference in revision
+        final TreeMultimap <String, Module> nameToModulesAll = TreeMultimap.create(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        }, REVISION_COMPARATOR);
+
+        nameToModulesAll.putAll(getStringModuleMap(delegate));
+
+        //in case there is a particular dependancy to view filteredModules/yang models
+        //dependancy is checked for module name and imports
+        filteredModulesBuilder.addAll(Collections2.filter(delegate.getModules(), new Predicate<Module>() {
+            @Override
+            public boolean apply(@Nullable Module module) {
+                return checkModuleDependency(module, rootModules);
+            }
+        }));
+
+        //adding additional modules
+        filteredModulesBuilder.addAll(Collections2.filter(delegate.getModules(), new Predicate<Module>() {
+            @Override
+            public boolean apply(@Nullable Module module) {
+                return selectAdditionalModules(module, additionalModuleIds);
+            }
+        }));
+
+        filteredModulesBuilder.addAll(getImportedModules(
+                Maps.uniqueIndex(delegate.getModules(), ModuleId.MODULE_TO_MODULE_ID), filteredModulesBuilder.build(), nameToModulesAll));
+
+        /**
+         * Instead of doing this on each invocation of getModules(), pre-compute
+         * it once and keep it around -- better than the set we got in.
+         */
+        this.filteredModules = filteredModulesBuilder.build();
+
+        for (final Module module :filteredModules) {
+
+            nameMap.put(module.getName(), module);
+            nsMap.put(module.getNamespace(), module);
+
+            identifiersToSourcesBuilder.put(module, module.getSource());
+        }
+
+        namespaceToModules = ImmutableSetMultimap.copyOf(nsMap);
+        nameToModules = ImmutableSetMultimap.copyOf(nameMap);
+        identifiersToSources = identifiersToSourcesBuilder.build();
+    }
+
+    private Multimap<String, Module> getStringModuleMap(SchemaContext delegate) {
+        return Multimaps.index(delegate.getModules(), new Function<Module, String>() {
+            @Override
+            public String apply(Module input) {
+                return input.getName();
+            }
+        });
+    }
+
+    //dealing with imported module other than root and directly importing root
+    private static Collection<Module> getImportedModules(Map<ModuleId, Module> allModules, Set<Module> baseModules, TreeMultimap<String, Module> nameToModulesAll) {
+
+        List<Module> relatedModules = Lists.newLinkedList();
+
+        for (Module module : baseModules) {
+            for (ModuleImport moduleImport : module.getImports()) {
+
+                Date revisionDate = moduleImport.getRevision() == null ?
+                        nameToModulesAll.get(moduleImport.getModuleName()).first().getRevision() : moduleImport.getRevision();
+
+                ModuleId key = new ModuleId(moduleImport.getModuleName(),revisionDate);
+                Module importedModule = allModules.get(key);
+
+                Preconditions.checkArgument(importedModule != null,  "Invalid schema, cannot find imported module: %s from module: %s, %s, modules:%s", key, module.getQNameModule(), module.getName() );
+                relatedModules.add(importedModule);
+
+                //calling imports recursive
+                relatedModules.addAll(getImportedModules(allModules, Collections.singleton(importedModule), nameToModulesAll));
+
+            }
+        }
+
+        return relatedModules;
+    }
+
+    @Override
+    protected Map<ModuleIdentifier, String> getIdentifiersToSources() {
+        return identifiersToSources;
+    }
+
+    public Set<Module> getModules() {
+        return filteredModules;
+    }
+
+    @Override
+    protected SetMultimap<URI, Module> getNamespaceToModules() {
+        return namespaceToModules;
+    }
+
+    @Override
+    protected SetMultimap<String, Module> getNameToModules() {
+        return nameToModules;
+    }
+
+    private boolean selectAdditionalModules(Module module, Set<ModuleId> additionalModules){
+
+        if(additionalModules.contains(new ModuleId(module.getName(), module.getRevision()))){
+
+            return true;
+        }
+
+        return false;
+    };
+
+    //check for any dependency regarding given string
+    private boolean checkModuleDependency(Module module, Collection<ModuleId> rootModules) {
+
+        for (ModuleId rootModule : rootModules) {
+
+            if(rootModule.equals(new ModuleId(module.getName(), module.getRevision()))) {
+                return true;
+            }
+
+            //handling/checking imports regarding root modules
+            for (ModuleImport moduleImport : module.getImports()) {
+
+                if(moduleImport.getModuleName().equals(rootModule.getName())) {
+
+                    if(moduleImport.getRevision() != null && !moduleImport.getRevision().equals(rootModule.getRev())) {
+                        return false;
+                    }
+
+                    return true;
+                }
+            }
+
+            //submodules handling
+            for (Module moduleSub : module.getSubmodules()) {
+                return checkModuleDependency(moduleSub, rootModules);
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("SchemaContextProxyImpl{filteredModules=%s}", filteredModules);
+    }
+
+    public static final class ModuleId {
+        private final String name;
+        private final Date rev;
+
+        public ModuleId(String name, Date rev) {
+            Preconditions.checkArgument(!Strings.isNullOrEmpty(name), "No module dependency name given. Nothing to do.");
+            this.name = name;
+            this.rev = Preconditions.checkNotNull(rev, "No revision date given. Nothing to do.");
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Date getRev() {
+            return rev;
+        }
+
+        public static final Function<Module, ModuleId> MODULE_TO_MODULE_ID = new Function<Module, ModuleId>() {
+            @Override
+            public ModuleId apply(Module input) {
+                return new ModuleId(input.getName(), input.getRevision());
+            }
+        };
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            ModuleId moduleId = (ModuleId) o;
+
+            if (name != null ? !name.equals(moduleId.name) : moduleId.name != null) return false;
+            if (rev != null ? !rev.equals(moduleId.rev) : moduleId.rev != null) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = name != null ? name.hashCode() : 0;
+            result = 31 * result + (rev != null ? rev.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+
+            return String.format("ModuleId{name='%s', rev=%s}",name,rev);
+        }
+    }
+}
--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/impl/SchemaContextImpl.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/impl/SchemaContextImpl.java
@@ -7,80 +7,33 @@
  */
 package org.opendaylight.yangtools.yang.parser.impl;
 
-import com.google.common.base.Optional;
-import com.google.common.base.Supplier;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.ImmutableSetMultimap;
-import com.google.common.collect.Multimaps;
-import com.google.common.collect.SetMultimap;
+import com.google.common.collect.*;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.*;
 import javax.annotation.concurrent.Immutable;
-import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.model.api.AugmentationSchema;
-import org.opendaylight.yangtools.yang.model.api.ConstraintDefinition;
-import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
-import org.opendaylight.yangtools.yang.model.api.ExtensionDefinition;
-import org.opendaylight.yangtools.yang.model.api.GroupingDefinition;
 import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang.model.api.ModuleIdentifier;
-import org.opendaylight.yangtools.yang.model.api.NotificationDefinition;
-import org.opendaylight.yangtools.yang.model.api.RpcDefinition;
-import org.opendaylight.yangtools.yang.model.api.SchemaContext;
-import org.opendaylight.yangtools.yang.model.api.SchemaPath;
-import org.opendaylight.yangtools.yang.model.api.Status;
-import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.UnknownSchemaNode;
-import org.opendaylight.yangtools.yang.model.api.UsesNode;
+import org.opendaylight.yangtools.yang.model.util.AbstractSchemaContext;
 import org.opendaylight.yangtools.yang.parser.util.ModuleDependencySort;
 
 @Immutable
-final class SchemaContextImpl implements SchemaContext {
-    private static final Comparator<Module> REVISION_COMPARATOR = new Comparator<Module>() {
-        @Override
-        public int compare(final Module o1, final Module o2) {
-            if (o2.getRevision() == null) {
-                return -1;
-            }
+final class SchemaContextImpl extends AbstractSchemaContext {
 
-            return o2.getRevision().compareTo(o1.getRevision());
-        }
-    };
-
-    private static final Supplier<TreeSet<Module>> MODULE_SET_SUPPLIER = new Supplier<TreeSet<Module>>() {
-        @Override
-        public TreeSet<Module> get() {
-            return new TreeSet<>(REVISION_COMPARATOR);
-        }
-    };
-
-    private final Map<ModuleIdentifier, String> identifiersToSources;
-    private final SetMultimap<URI, Module> namespaceToModules;
-    private final SetMultimap<String, Module> nameToModules;
-    private final Set<Module> modules;
+    private  final Map<ModuleIdentifier, String> identifiersToSources;
+    private  final SetMultimap<URI, Module> namespaceToModules;
+    private  final SetMultimap<String, Module> nameToModules;
+    private  final Set<Module> modules;
 
     SchemaContextImpl(final Set<Module> modules, final Map<ModuleIdentifier, String> identifiersToSources) {
         this.identifiersToSources = ImmutableMap.copyOf(identifiersToSources);
 
-        /*
+         /*
          * Instead of doing this on each invocation of getModules(), pre-compute
          * it once and keep it around -- better than the set we got in.
          */
         this.modules = ImmutableSet.copyOf(ModuleDependencySort.sort(modules.toArray(new Module[modules.size()])));
 
-        /*
+         /*
          * The most common lookup is from Namespace->Module.
          *
          * RESTCONF performs lookups based on module name only, where it wants
@@ -92,6 +45,7 @@ final class SchemaContextImpl implements SchemaContext {
                 new TreeMap<URI, Collection<Module>>(), MODULE_SET_SUPPLIER);
         final SetMultimap<String, Module> nameMap = Multimaps.newSetMultimap(
                 new TreeMap<String, Collection<Module>>(), MODULE_SET_SUPPLIER);
+
         for (Module m : modules) {
             nameMap.put(m.getName(), m);
             nsMap.put(m.getNamespace(), m);
@@ -102,210 +56,32 @@ final class SchemaContextImpl implements SchemaContext {
     }
 
     @Override
-    public Set<DataSchemaNode> getDataDefinitions() {
-        final Set<DataSchemaNode> dataDefs = new HashSet<>();
-        for (Module m : modules) {
-            dataDefs.addAll(m.getChildNodes());
-        }
-        return dataDefs;
+    protected Map<ModuleIdentifier, String> getIdentifiersToSources(){
+
+        return identifiersToSources;
     }
 
     @Override
-    public Set<Module> getModules() {
+    public Set<Module> getModules(){
+
         return modules;
     }
 
     @Override
-    public Set<NotificationDefinition> getNotifications() {
-        final Set<NotificationDefinition> notifications = new HashSet<>();
-        for (Module m : modules) {
-            notifications.addAll(m.getNotifications());
-        }
-        return notifications;
+    protected SetMultimap<URI, Module> getNamespaceToModules() {
+
+        return namespaceToModules;
     }
 
     @Override
-    public Set<RpcDefinition> getOperations() {
-        final Set<RpcDefinition> rpcs = new HashSet<>();
-        for (Module m : modules) {
-            rpcs.addAll(m.getRpcs());
-        }
-        return rpcs;
-    }
+    protected SetMultimap<String, Module> getNameToModules() {
 
-    @Override
-    public Set<ExtensionDefinition> getExtensions() {
-        final Set<ExtensionDefinition> extensions = new HashSet<>();
-        for (Module m : modules) {
-            extensions.addAll(m.getExtensionSchemaNodes());
-        }
-        return extensions;
-    }
-
-    @Override
-    public Module findModuleByName(final String name, final Date revision) {
-        for (final Module module : nameToModules.get(name)) {
-            if (revision == null || revision.equals(module.getRevision())) {
-                return module;
-            }
-        }
-
-        return null;
-    }
-
-    @Override
-    public Set<Module> findModuleByNamespace(final URI namespace) {
-        final Set<Module> ret = namespaceToModules.get(namespace);
-        return ret == null ? Collections.<Module>emptySet() : ret;
-    }
-
-    @Override
-    public Module findModuleByNamespaceAndRevision(final URI namespace, final Date revision) {
-        if (namespace == null) {
-            return null;
-        }
-        for (Module module : findModuleByNamespace(namespace)) {
-            if (revision == null || revision.equals(module.getRevision())) {
-                return module;
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public boolean isAugmenting() {
-        return false;
-    }
-
-    @Override
-    public boolean isAddedByUses() {
-        return false;
-    }
-
-    @Override
-    public boolean isConfiguration() {
-        return false;
-    }
-
-    @Override
-    public ConstraintDefinition getConstraints() {
-        return null;
-    }
-
-    @Override
-    public QName getQName() {
-        return SchemaContext.NAME;
-    }
-
-    @Override
-    public SchemaPath getPath() {
-        return SchemaPath.ROOT;
-    }
-
-    @Override
-    public String getDescription() {
-        return null;
-    }
-
-    @Override
-    public String getReference() {
-        return null;
-    }
-
-    @Override
-    public Status getStatus() {
-        return Status.CURRENT;
-    }
-
-    @Override
-    public List<UnknownSchemaNode> getUnknownSchemaNodes() {
-        final List<UnknownSchemaNode> result = new ArrayList<>();
-        for (Module module : modules) {
-            result.addAll(module.getUnknownSchemaNodes());
-        }
-        return Collections.unmodifiableList(result);
-    }
-
-    @Override
-    public Set<TypeDefinition<?>> getTypeDefinitions() {
-        final Set<TypeDefinition<?>> result = new LinkedHashSet<>();
-        for (Module module : modules) {
-            result.addAll(module.getTypeDefinitions());
-        }
-        return Collections.unmodifiableSet(result);
-    }
-
-    @Override
-    public Set<DataSchemaNode> getChildNodes() {
-        final Set<DataSchemaNode> result = new LinkedHashSet<>();
-        for (Module module : modules) {
-            result.addAll(module.getChildNodes());
-        }
-        return Collections.unmodifiableSet(result);
-    }
-
-    @Override
-    public Set<GroupingDefinition> getGroupings() {
-        final Set<GroupingDefinition> result = new LinkedHashSet<>();
-        for (Module module : modules) {
-            result.addAll(module.getGroupings());
-        }
-        return Collections.unmodifiableSet(result);
-    }
-
-    @Override
-    public DataSchemaNode getDataChildByName(final QName name) {
-        for (Module module : modules) {
-            final DataSchemaNode result = module.getDataChildByName(name);
-            if (result != null) {
-                return result;
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public DataSchemaNode getDataChildByName(final String name) {
-        for (Module module : modules) {
-            final DataSchemaNode result = module.getDataChildByName(name);
-            if (result != null) {
-                return result;
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public Set<UsesNode> getUses() {
-        return Collections.emptySet();
-    }
-
-    @Override
-    public boolean isPresenceContainer() {
-        return false;
-    }
-
-    @Override
-    public Set<AugmentationSchema> getAvailableAugmentations() {
-        return Collections.emptySet();
-    }
-
-    //FIXME: should work for submodules too
-    @Override
-    public Set<ModuleIdentifier> getAllModuleIdentifiers() {
-        return identifiersToSources.keySet();
-    }
-
-    @Override
-    public Optional<String> getModuleSource(final ModuleIdentifier moduleIdentifier) {
-        String maybeSource = identifiersToSources.get(moduleIdentifier);
-        return Optional.fromNullable(maybeSource);
+        return nameToModules;
     }
 
     @Override
     public String toString() {
-        return "SchemaContextImpl{" +
-                "modules=" + modules +
-                '}';
+
+        return String.format("SchemaContextImpl{modules=%s}", modules);
     }
 }
