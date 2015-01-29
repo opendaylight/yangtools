7307b98ce7d36f08a74a33ddf9704ca165ba5184
Tony Tkacik <ttkacik@cisco.com>
2015-01-29 17:39:25 +0100

    Bug 2444: Added concrete API definitions for YANG statements.
    Introduced type captures of DeclaredStatement for RFC6020 statments
    along with some convenience getters to decrease need of users
    to look up statements in collections.
    
    This getters conveys RFC6020 semantic, but does not prevent
    access to any statements which may be introduced in later
    versions of YANG.
    
    Change-Id: I7ca3265aa7fc141446515e27f02c37ff3b5903dc
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/Rfc6020AbnfRule.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.api.stmt;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+*
+* References ABNF rule defined in RFC6020 - YANG Specification.
+*
+* <p>
+* An interface / class annotated with this annotation
+* is Java representation of data represented by ABNF rule
+* provided as {@link #value()}. Java representation
+* does not need to be direct,
+* but must retain all information in some, publicly
+* accessible form for consumers.
+* </p>
+* <p>
+* Note that this annotation is used currently only for documentation
+* and does not affect any runtime behaviour.
+* </p>
+*
+*/
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+public @interface Rfc6020AbnfRule {
+
+    String[] value();
+}
