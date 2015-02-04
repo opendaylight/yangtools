076df49d6ed9c6734302833322124ce3a091dc2c
Peter Kajsa <pkajsa@cisco.com>
2015-02-04 10:06:09 +0100

    Bug 2363, Bug 2205. Beta version of LeafRefContext tree computation, beta version of DataTree leafrefs values validation
    Currently works (Bug 2363):
    
    - LeafRefContext tree build with all relations:
              - isReferencing/ hasReferencingChild
              - isReferencedBy/ hasReferencedByChild
              - leafref target node -> leafref source nodes
    - validation of LeafRef Path and Predicates syntax according to RFC6020
      specification
    - parsing of all LeafRef Path components (i.e.: LeafRef absolute/relative
      target path, path Predicates, key path expressions) into LeafRefPath
      instance
    - pack of utils methods over LeafRefContext tree
    - a couple of simple tests
    
    Currently works (Bug 2205):
    
    - validation of LeafRefs values and LeafRefs targets for WRITE, MERGE and
      DELETE operations
    - leafref path predicates evaluation (also multiple)
    
    Currently known issues (Bug 2363):
    
    - improve leafref identification, also for cases when type of the leaf is
      typedef -> typedef -> etc.. -> leafref
    - handle also choices without case
    - questionable issue is, whether it is necessary to merge referencing and
      referencedBy subtrees under the root context (currently are not)
    - in equals method check also for predicates equality
    - test the solution
    
    Currently known issues (Bug 2205):
    - handle also UnkeyedLists
    - perfrom further testing
    - increase unit tests coverage
    - add tests for special cases.
    
    Change-Id: Id873fb446fe40855993fe00e6a56f82d3daee491
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/leafrefcontext/impl/LeafRefContextImpl.java
@@ -0,0 +1,151 @@
+/**
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.leafrefcontext.impl;
+
+
+import org.opendaylight.yangtools.leafrefcontext.builder.LeafRefContextBuilder;
+import org.opendaylight.yangtools.leafrefcontext.api.LeafRefPath;
+import org.opendaylight.yangtools.leafrefcontext.api.LeafRefContext;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import java.util.HashMap;
+import org.opendaylight.yangtools.yang.common.QName;
+import java.util.Map;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+
+public class LeafRefContextImpl implements LeafRefContext {
+
+    private QName currentNodeQName;
+    private SchemaPath currentNodePath;
+    private SchemaContext schemaContext;
+    private Module module;
+
+    private LeafRefPath leafRefTargetPath = null;
+    private LeafRefPath absoluteLeafRefTargetPath = null;
+    private String leafRefTargetPathString = "";
+
+    private boolean isReferencedBy = false;
+    private boolean isReferencing = false;
+
+    private Map<QName, LeafRefContext> referencingChilds = new HashMap<QName, LeafRefContext>();
+    private Map<QName, LeafRefContext> referencedByChilds = new HashMap<QName, LeafRefContext>();
+    private Map<QName, LeafRefContext> referencedByLeafRefCtx = new HashMap<QName, LeafRefContext>();
+
+
+    public LeafRefContextImpl(QName currentNodeQName, SchemaPath currentNodePath, SchemaContext schemaContext) {
+        this.currentNodeQName = currentNodeQName;
+        this.currentNodePath = currentNodePath;
+        this.schemaContext = schemaContext;
+    }
+
+    public LeafRefContextImpl(LeafRefContextBuilder leafRefContextBuilder) {
+        this.currentNodeQName = leafRefContextBuilder.getCurrentNodeQName();
+        this.currentNodePath = leafRefContextBuilder.getCurrentNodePath();
+        this.schemaContext = leafRefContextBuilder.getSchemaContext();
+        this.leafRefTargetPath = leafRefContextBuilder.getLeafRefTargetPath();
+        this.absoluteLeafRefTargetPath = leafRefContextBuilder.getAbsoluteLeafRefTargetPath();
+        this.leafRefTargetPathString = leafRefContextBuilder.getLeafRefTargetPathString();
+        this.isReferencedBy = leafRefContextBuilder.isReferencedBy();
+        this.isReferencing = leafRefContextBuilder.isReferencing();
+        this.referencingChilds = leafRefContextBuilder.getReferencingChilds();
+        this.referencedByChilds = leafRefContextBuilder.getReferencedByChilds();
+        this.referencedByLeafRefCtx = leafRefContextBuilder.getAllReferencedByLeafRefCtxs();
+        this.module = leafRefContextBuilder.getLeafRefContextModule();
+    }
+
+    @Override
+    public boolean hasLeafRefContextChild() {
+        return hasReferencedByChild() || hasReferencingChild();
+    }
+
+    @Override
+    public boolean hasReferencedByChild() {
+        return !referencedByChilds.isEmpty();
+    }
+
+    @Override
+    public boolean hasReferencingChild() {
+        return !referencingChilds.isEmpty();
+    }
+
+    @Override
+    public boolean isReferencedBy() {
+        return isReferencedBy;
+    }
+
+    @Override
+    public boolean isReferencing() {
+        return isReferencing;
+    }
+
+    @Override
+    public LeafRefContext getReferencingChildByName(QName name) {
+        return referencingChilds.get(name);
+    }
+
+    @Override
+    public Map<QName, LeafRefContext> getReferencingChilds() {
+        return referencingChilds;
+    }
+
+    @Override
+    public LeafRefContext getReferencedByChildByName(QName name) {
+        return referencedByChilds.get(name);
+    }
+
+    @Override
+    public Map<QName, LeafRefContext> getReferencedByChilds() {
+        return referencedByChilds;
+    }
+
+    @Override
+    public SchemaPath getCurrentNodePath() {
+        return currentNodePath;
+    }
+
+    @Override
+    public LeafRefPath getLeafRefTargetPath() {
+        return leafRefTargetPath;
+    }
+
+    @Override
+    public String getLeafRefTargetPathString() {
+        return leafRefTargetPathString;
+    }
+
+    @Override
+    public QName getCurrentNodeQName() {
+        return currentNodeQName;
+    }
+
+    @Override
+    public SchemaContext getSchemaContext() {
+        return schemaContext;
+    }
+
+    @Override
+    public LeafRefPath getAbsoluteLeafRefTargetPath() {
+        return absoluteLeafRefTargetPath;
+    }
+
+    @Override
+    public Module getLeafRefContextModule() {
+        return module;
+    }
+
+    @Override
+    public LeafRefContext getReferencedByLeafRefCtxByName(QName qname) {
+        return referencedByLeafRefCtx.get(qname);
+    }
+
+    @Override
+    public Map<QName, LeafRefContext> getAllReferencedByLeafRefCtxs() {
+        return referencedByLeafRefCtx;
+    }
+
+}
