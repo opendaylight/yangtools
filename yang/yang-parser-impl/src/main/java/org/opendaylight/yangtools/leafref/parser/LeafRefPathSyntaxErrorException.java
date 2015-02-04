efc15e3fb5997940f546ae6ed2d3026ccdb3ac8a
Peter Kajsa <pkajsa@cisco.com>
2015-02-04 10:06:09 +0100

    Bug 2363, Bug 2205. Beta version of LeafRefContext tree computation, beta version of DataTree leafrefs values validation
    Currently works (Bug 2363):
    
    - LeafRefContext tree build with all relations:
              - isReferencing/ hasReferencingChild
              - isReferencedBy/ hasReferencedByChild
              - leafref target node -> leafref source nodes
    - validation of LeafRef Path and Predicates syntax according to RFC6020
      specification
    - parsing of all LeafRef Path components (i.e.: LeafRef absolute/relative
      target path, path Predicates, key path expressions) into LeafRefPath
      instance
    - pack of utils methods over LeafRefContext tree
    - a couple of simple tests
    
    Currently works (Bug 2205):
    
    - validation of LeafRefs values and LeafRefs targets for WRITE, MERGE and
      DELETE operations
    - leafref path predicates evaluation (also multiple)
    
    Currently known issues (Bug 2363):
    
    - improve leafref identification, also for cases when type of the leaf is
      typedef -> typedef -> etc.. -> leafref
    - handle also choices without case
    - questionable issue is, whether it is necessary to merge referencing and
      referencedBy subtrees under the root context (currently are not)
    - in equals method check also for predicates equality
    - test the solution
    
    Currently known issues (Bug 2205):
    - handle also UnkeyedLists
    - perfrom further testing
    - increase unit tests coverage
    - add tests for special cases
    
    Change-Id: Id873fb446fe40855993fe00e6a56f82d3daee491
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/leafref/parser/LeafRefPathSyntaxErrorException.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.leafref.parser;
+
+import org.opendaylight.yangtools.yang.model.parser.api.YangSyntaxErrorException;
+
+public class LeafRefPathSyntaxErrorException extends YangSyntaxErrorException{
+
+    private static final long serialVersionUID = 1L;
+
+    public LeafRefPathSyntaxErrorException(final String module, final int line, final int charPositionInLine,
+            final String message) {
+        super(module, line, charPositionInLine, message, null);
+    }
+
+    public LeafRefPathSyntaxErrorException(final String module, final int line, final int charPositionInLine,
+            final String message, final Throwable cause) {
+        super(module,line,charPositionInLine,message,cause);
+    }
+}
