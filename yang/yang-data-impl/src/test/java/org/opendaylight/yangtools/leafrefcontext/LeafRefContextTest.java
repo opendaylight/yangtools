7b9f09d297664e0538dfab6715f619d0ee741704
Peter Kajsa <pkajsa@cisco.com>
2015-02-04 10:06:09 +0100

    Bug 2363, Bug 2205. Beta version of LeafRefContext tree computation, beta version of DataTree leafrefs values validation
    Currently works (Bug 2363):
    
    - LeafRefContext tree build with all relations:
              - isReferencing/ hasReferencingChild
              - isReferencedBy/ hasReferencedByChild
              - leafref target node -> leafref source nodes
    - validation of LeafRef Path and Predicates syntax according to RFC6020
      specification
    - parsing of all LeafRef Path components (i.e.: LeafRef absolute/relative
      target path, path Predicates, key path expressions) into LeafRefPath
      instance
    - pack of utils methods over LeafRefContext tree
    - a couple of simple tests
    
    Currently works (Bug 2205):
    
    - validation of LeafRefs values and LeafRefs targets for WRITE, MERGE and
      DELETE operations
    - leafref path predicates evaluation (also multiple)
    
    Currently known issues (Bug 2363):
    
    - improve leafref identification, also for cases when type of the leaf is
      typedef -> typedef -> etc.. -> leafref
    - handle also choices without case
    - questionable issue is, whether it is necessary to merge referencing and
      referencedBy subtrees under the root context (currently are not)
    - in equals method check also for predicates equality
    - test the solution
    
    Currently known issues (Bug 2205):
    - handle also UnkeyedLists
    - perfrom further testing
    - increase unit tests coverage
    - add tests for special cases.
    
    Change-Id: Id873fb446fe40855993fe00e6a56f82d3daee491
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-data-impl/src/test/java/org/opendaylight/yangtools/leafrefcontext/LeafRefContextTest.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.leafrefcontext;
+
+import static org.junit.Assert.*;
+
+import org.opendaylight.yangtools.leafref.parser.LeafRefYangSyntaxErrorException;
+
+import org.opendaylight.yangtools.yang.model.api.DataNodeContainer;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Map;
+import java.util.Set;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.opendaylight.yangtools.leafrefcontext.api.LeafRefContext;
+import org.opendaylight.yangtools.leafrefcontext.builder.LeafRefContextTreeBuilder;
+import org.opendaylight.yangtools.leafrefcontext.utils.LeafRefContextUtils;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.parser.api.YangSyntaxErrorException;
+import org.opendaylight.yangtools.yang.parser.impl.YangParserImpl;
+
+public class LeafRefContextTest {
+
+    private static SchemaContext context;
+    private static Module rootMod;
+    private static QNameModule root;
+    private static LeafRefContext rootLeafRefContext;
+
+    @BeforeClass
+    public static void init() throws URISyntaxException, IOException,
+            YangSyntaxErrorException, LeafRefYangSyntaxErrorException {
+
+        File resourceFile = new File(
+                LeafRefContextTreeBuilderTest.class
+                        .getResource(
+                                "/leafref-context-test/correct-modules/leafref-test2.yang")
+                        .toURI());
+
+        File resourceDir = resourceFile.getParentFile();
+
+        YangParserImpl parser = YangParserImpl.getInstance();
+        context = parser.parseFile(resourceFile, resourceDir);
+
+        Set<Module> modules = context.getModules();
+        for (Module module : modules) {
+            if (module.getName().equals("leafref-test2")) {
+                rootMod = module;
+            }
+        }
+
+        root = rootMod.getQNameModule();
+
+        LeafRefContextTreeBuilder leafRefContextTreeBuilder = new LeafRefContextTreeBuilder(
+                context);
+
+        rootLeafRefContext = leafRefContextTreeBuilder
+                .buildLeafRefContextTree();
+    }
+
+    @Test
+    public void test() {
+
+        QName q1 = QName.create(root, "ref1");
+        QName q2 = QName.create(root, "leaf1");
+        QName q3 = QName.create(root, "cont1");
+        QName q4 = QName.create(root, "cont2");
+        QName q5 = QName.create(root, "list1");
+        QName q6 = QName.create(root, "name");
+
+        DataSchemaNode leafRefNode = rootMod.getDataChildByName(q1);
+        DataSchemaNode targetNode = rootMod.getDataChildByName(q2);
+        DataSchemaNode cont1Node = rootMod.getDataChildByName(q3);
+        DataSchemaNode cont2Node = rootMod.getDataChildByName(q4);
+        DataSchemaNode name1Node = ((DataNodeContainer) ((DataNodeContainer) rootMod
+                .getDataChildByName(q3)).getDataChildByName(q5))
+                .getDataChildByName(q6);
+
+        assertTrue(LeafRefContextUtils.isLeafRef(leafRefNode,
+                rootLeafRefContext));
+        assertFalse(LeafRefContextUtils.isLeafRef(targetNode,
+                rootLeafRefContext));
+
+        assertTrue(LeafRefContextUtils.hasLeafRefChild(cont1Node,
+                rootLeafRefContext));
+        assertFalse(LeafRefContextUtils.hasLeafRefChild(leafRefNode,
+                rootLeafRefContext));
+
+        assertTrue(LeafRefContextUtils.isReferencedByLeafRef(targetNode,
+                rootLeafRefContext));
+        assertFalse(LeafRefContextUtils.isReferencedByLeafRef(leafRefNode,
+                rootLeafRefContext));
+
+        assertTrue(LeafRefContextUtils.hasChildReferencedByLeafRef(cont2Node,
+                rootLeafRefContext));
+        assertFalse(LeafRefContextUtils.hasChildReferencedByLeafRef(
+                leafRefNode, rootLeafRefContext));
+
+        Map<QName, LeafRefContext> leafRefs = LeafRefContextUtils
+                .getAllLeafRefsReferencingThisNode(name1Node,
+                        rootLeafRefContext);
+        assertEquals(4, leafRefs.size());
+        leafRefs = LeafRefContextUtils.getAllLeafRefsReferencingThisNode(
+                leafRefNode, rootLeafRefContext);
+        assertTrue(leafRefs.isEmpty());
+    }
+}
