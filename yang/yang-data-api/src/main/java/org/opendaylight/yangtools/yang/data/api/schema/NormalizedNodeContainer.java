4bada1ccf2b17f9870a6b23e1b10cfa57fb0a27e
Jan Hajnar <jhajnar@cisco.com>
2015-02-06 15:26:34 +0100

    Bug 2362 - Milestone: Basic constraints validation
    * added implementation of max-elements and min-elements for list and
    leaf-list.
    * added simple test model and tests for new functionality.
    
    Change-Id: I3f52367b2e4b02e91ff5f9aa3d2ea06dae3bcaf7
    Signed-off-by: Jan Hajnar <jhajnar@cisco.com>

--- yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/NormalizedNodeContainer.java
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/NormalizedNodeContainer.java
@@ -6,21 +6,20 @@
  */
 package org.opendaylight.yangtools.yang.data.api.schema;
 
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
-
 import com.google.common.base.Optional;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 
 /**
  * Node which is not leaf, but has child {@link NormalizedNode}s as its valzue.
- * 
- * 
+ *
+ *
  * NormalizedNodeContainer does not have a value, but it has a child
  * nodes. Definition of possible and valid child nodes is introduced
  * in subclasses of this interface.
- * 
+ *
  * This interface should not be used directly, but rather use of of derived subinterfaces
  * such as {@link DataContainerNode}, {@link MapNode}, {@link LeafSetNode}.
- * 
+ *
  * @param <I>
  *            Node Identifier type
  * @param <K>
@@ -36,18 +35,20 @@ public interface NormalizedNodeContainer<I extends PathArgument, K extends PathA
 
     /**
      * Returns immutable iteration of child nodes of this node.
-     * 
+     *
      */
     @Override
     Iterable<V> getValue();
 
     /**
      * Returns child node identified by provided key.
-     * 
+     *
      * @param child
      *            Path argument identifying child node
      * @return Optional with child node if child exists.
      *         {@link Optional#absent()} if child does not exists.
      */
     Optional<V> getChild(K child);
+
+
 }
