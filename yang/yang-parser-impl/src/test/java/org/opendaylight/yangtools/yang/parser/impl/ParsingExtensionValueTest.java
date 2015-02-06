1f7927a5c63c969ef2124f8cb17794cf30253089
Mayank Agarwal <mayagarw@cisco.com>
2015-02-05 16:09:18 -0800

    Adding a new extension for RESTConf Doc
    Signed-off-by: Mayank Agarwal <mayagarw@cisco.com>
    Change-Id: I123d684a24c90c3b77010b9002692f0557120f1b

--- yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/parser/impl/ParsingExtensionValueTest.java
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/parser/impl/ParsingExtensionValueTest.java
@@ -8,13 +8,18 @@
 package org.opendaylight.yangtools.yang.parser.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Collection;
 import java.util.Set;
 import org.junit.Before;
 import org.junit.Test;
+import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.UnknownSchemaNode;
 
 /**
  * Test for testing of extensions and their arguments.
@@ -31,10 +36,37 @@ public class ParsingExtensionValueTest {
 
         modules = TestUtils.loadModules(getClass().getResource("/extensions").toURI());
         assertEquals(2, modules.size());
+
+        for (Module module : modules) {
+          if(module.getName().equals("ext-use")){
+            boolean hideModule = isModuleHidden(module);
+            assertTrue(hideModule);
+          }
+        }
     }
 
     @Test
     public void parsingExtensionArgsTest() {
 
     }
+
+  private boolean isModuleHidden(Module module){
+    Collection<DataSchemaNode> modulesChildren = module.getChildNodes();
+
+    for (DataSchemaNode moduleChild : modulesChildren) {
+
+      if (moduleChild instanceof ContainerSchemaNode) {
+        Collection<UnknownSchemaNode> unknownNodes = moduleChild.getUnknownSchemaNodes();
+
+        for (UnknownSchemaNode unknownNode : unknownNodes) {
+
+          if (unknownNode.getQName().getLocalName().equals("module-scope-private")) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+
+  }
 }
