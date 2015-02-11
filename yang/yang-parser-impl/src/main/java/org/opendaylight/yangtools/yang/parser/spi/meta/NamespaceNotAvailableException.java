71cd4a3bbe49a257c595188515872599f825b254
Tony Tkacik <ttkacik@cisco.com>
2015-02-11 21:28:37 +0100

    Bug 2361: Added meta-level SPI for building any statement-based models.
    This meta-level SPI is designed for components, which introduces
    support for conrete statements, identifier namespaces or effective
    model inference to the YANG parser.
    
    Change-Id: Iff35085fa5a44568c449a5e1e34ee371768dd88b
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/spi/meta/NamespaceNotAvailableException.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.spi.meta;
+
+import com.google.common.base.Preconditions;
+
+public class NamespaceNotAvailableException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public NamespaceNotAvailableException(String message) {
+        super(Preconditions.checkNotNull(message));
+    }
+
+    public NamespaceNotAvailableException(String message, Throwable cause) {
+        super(Preconditions.checkNotNull(message), cause);
+    }
+}
