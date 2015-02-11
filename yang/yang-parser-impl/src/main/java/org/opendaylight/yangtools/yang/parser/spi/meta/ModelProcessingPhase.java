a808f96d29dbcfb341bfd84d488291a3494dba72
Tony Tkacik <ttkacik@cisco.com>
2015-02-11 21:28:37 +0100

    Bug 2361: Added meta-level SPI for building any statement-based models.
    This meta-level SPI is designed for components, which introduces
    support for conrete statements, identifier namespaces or effective
    model inference to the YANG parser.
    
    Change-Id: Iff35085fa5a44568c449a5e1e34ee371768dd88b
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/spi/meta/ModelProcessingPhase.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.spi.meta;
+
+import javax.annotation.Nullable;
+
+public enum ModelProcessingPhase {
+
+    /**
+     *
+     * Cross-source relationship resolution phase.
+     * <p>
+     * In this phase of processing only statements which affects
+     * cross-source relationship (e.g. imports / includes)
+     * are processed.
+     * <p>
+     * At end of this phase all source related contexts should
+     * be bind to their imports and includes to allow
+     * visibility of custom defined statements in following
+     * phases.
+     */
+    Linkage(null),
+    StatementDefinition(Linkage),
+    FullDeclaration(StatementDefinition),
+    SemanticInference(FullDeclaration);
+
+
+    private ModelProcessingPhase previousPhase;
+
+    private ModelProcessingPhase(@Nullable ModelProcessingPhase previous) {
+        this.previousPhase = previous;
+    }
+
+    public ModelProcessingPhase getPreviousPhase() {
+        return previousPhase;
+    }
+
+}
