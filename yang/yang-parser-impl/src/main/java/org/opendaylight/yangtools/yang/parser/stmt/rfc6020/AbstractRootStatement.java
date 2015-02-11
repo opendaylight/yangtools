68f0b94fafda5d614df6b96a2eaea70d0b7200d8
Tony Tkacik <ttkacik@cisco.com>
2015-02-11 21:35:12 +0100

    Bug 2361: Added prototype implementation using new SPI.
    This prototype level implementation of module, namespace,
    import, uses and container statement shows usage
    of meta-level SPI in order to introduce support for
    concrete statements.
    
    Note that this is same SPI, which is to be used
    to introduce new statement or extension support
    directly to the YANG parser.
    
    Change-Id: I5124a06669d48d3a32d585c4c75de5b2228f2eac
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/AbstractRootStatement.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.stmt.rfc6020;
+
+import java.util.Collection;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.AugmentStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.BodyGroup;
+import org.opendaylight.yangtools.yang.model.api.stmt.ContactStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.DataDefinitionStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.DescriptionStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.DeviationStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ExtensionStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.FeatureStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.GroupingStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.IdentityStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ImportStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.IncludeStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.LinkageGroup;
+import org.opendaylight.yangtools.yang.model.api.stmt.MetaGroup;
+import org.opendaylight.yangtools.yang.model.api.stmt.NotificationStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.OrganizationStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ReferenceStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.RevisionGroup;
+import org.opendaylight.yangtools.yang.model.api.stmt.RevisionStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.RpcStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.TypedefStatement;
+import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractDeclaredStatement;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+
+public abstract class AbstractRootStatement<T extends DeclaredStatement<String>> extends AbstractDeclaredStatement<String>
+        implements LinkageGroup, MetaGroup, RevisionGroup, BodyGroup {
+
+    protected AbstractRootStatement(StmtContext<String, T,?> context) {
+        super(context);
+    }
+
+    @Override
+    public final Collection<? extends ImportStatement> getImports() {
+        return allDeclared(ImportStatement.class);
+    }
+
+    @Override
+    public final Collection<? extends IncludeStatement> getIncludes() {
+        return allDeclared(IncludeStatement.class);
+    }
+
+    @Override
+    public DescriptionStatement getDescription() {
+        return firstDeclared(DescriptionStatement.class);
+    }
+
+    @Override
+    public ReferenceStatement getReference() {
+        return firstDeclared(ReferenceStatement.class);
+    }
+
+    @Override
+    public final OrganizationStatement getOrganization() {
+        return firstDeclared(OrganizationStatement.class);
+    }
+
+    @Override
+    public final ContactStatement getContact() {
+        return firstDeclared(ContactStatement.class);
+    }
+
+    @Override
+    public final Collection<? extends RevisionStatement> getRevisions() {
+        return allDeclared(RevisionStatement.class);
+    }
+
+    @Override
+    public final Collection<? extends ExtensionStatement> getExtensions() {
+        return allDeclared(ExtensionStatement.class);
+    }
+
+    @Override
+    public final Collection<? extends FeatureStatement> getFeatures() {
+        return allDeclared(FeatureStatement.class);
+    }
+
+    @Override
+    public final Collection<? extends IdentityStatement> getIdentities() {
+        return allDeclared(IdentityStatement.class);
+    }
+
+    @Override
+    public Collection<? extends TypedefStatement> getTypedefs() {
+        return allDeclared(TypedefStatement.class);
+    }
+
+    @Override
+    public Collection<? extends GroupingStatement> getGroupings() {
+        return allDeclared(GroupingStatement.class);
+    }
+
+    @Override
+    public Collection<? extends DataDefinitionStatement> getDataDefinitions() {
+        return allDeclared(DataDefinitionStatement.class);
+    }
+
+    @Override
+    public final Collection<? extends AugmentStatement> getAugments() {
+        return allDeclared(AugmentStatement.class);
+    }
+
+    @Override
+    public final Collection<? extends RpcStatement> getRpcs() {
+        return allDeclared(RpcStatement.class);
+    }
+
+    @Override
+    public final Collection<? extends NotificationStatement> getNotifications() {
+        return allDeclared(NotificationStatement.class);
+    }
+
+    @Override
+    public final Collection<? extends DeviationStatement> getDeviations() {
+        return allDeclared(DeviationStatement.class);
+    }
+}
