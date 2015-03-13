efdfb3676bb1c3dfef5643211b352d46aea3e96b
Martin Ciglan <mciglan@cisco.com>
2015-03-13 15:18:48 +0100

    Bug 2366 - new parser API impl - added support and implementation for various statements (work in progress) - added to test
    Bug 2366 - New parser impl
    
    - fix of phases switching
    - register support for container statement
    - add container statements into the body of yang module in the unit test
    
    Change-Id: I5d6acc69bb877b26403d69e276ed5c578e9bd4d9
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/ConfigStatementImpl.java
@@ -0,0 +1,45 @@
+package org.opendaylight.yangtools.yang.parser.stmt.rfc6020;
+
+import org.opendaylight.yangtools.yang.model.api.Rfc6020Mapping;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ConfigStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.DescriptionStatement;
+import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractDeclaredStatement;
+import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractStatementSupport;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+
+import javax.annotation.Nonnull;
+
+public class ConfigStatementImpl extends AbstractDeclaredStatement<Boolean> implements ConfigStatement {
+
+    protected ConfigStatementImpl(
+            StmtContext<Boolean, ConfigStatement, ?> context) {
+        super(context);
+    }
+
+    public static class Definition extends AbstractStatementSupport<Boolean,ConfigStatement,EffectiveStatement<Boolean,ConfigStatement>> {
+
+        public Definition() {
+            super(Rfc6020Mapping.Config);
+        }
+
+        @Override public Boolean parseArgumentValue(StmtContext<?, ?, ?> ctx,
+                String value) throws SourceException {
+            return Boolean.valueOf(value);
+        }
+
+        @Override public ConfigStatement createDeclared(StmtContext<Boolean, ConfigStatement, ?> ctx) {
+            return new ConfigStatementImpl(ctx);
+        }
+
+        @Override public EffectiveStatement<Boolean, ConfigStatement> createEffective(StmtContext<Boolean, ConfigStatement, EffectiveStatement<Boolean, ConfigStatement>> ctx) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    @Nonnull @Override
+    public Boolean getValue() {
+        return argument();
+    }
+}
---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/LeafStatementImpl.java
@@ -0,0 +1,114 @@
+package org.opendaylight.yangtools.yang.parser.stmt.rfc6020;
+
+import org.opendaylight.yangtools.yang.model.api.Rfc6020Mapping;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ConfigStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.DefaultStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.DescriptionStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.IfFeatureStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.LeafStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.MandatoryStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.MustStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ReferenceStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.StatusStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.TypeStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.UnitsStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.WhenStatement;
+import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractDeclaredStatement;
+import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractStatementSupport;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Collection;
+
+public class LeafStatementImpl extends AbstractDeclaredStatement<QName> implements LeafStatement {
+
+    protected LeafStatementImpl(StmtContext<QName, LeafStatement, ?> context) {
+        super(context);
+    }
+
+    public static class Definition extends AbstractStatementSupport<QName,LeafStatement,EffectiveStatement<QName,LeafStatement>> {
+
+        public Definition() {
+            super(Rfc6020Mapping.Leaf);
+        }
+
+        @Override public QName parseArgumentValue(StmtContext<?, ?, ?> ctx,
+                String value) throws SourceException {
+            return Utils.qNameFromArgument(ctx,value);
+        }
+
+        @Override public LeafStatement createDeclared(
+                StmtContext<QName, LeafStatement, ?> ctx) {
+            return new LeafStatementImpl(ctx);
+        }
+
+        @Override public EffectiveStatement<QName, LeafStatement> createEffective(
+                StmtContext<QName, LeafStatement, EffectiveStatement<QName, LeafStatement>> ctx) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    @Nullable @Override
+    public Collection<? extends MustStatement> getMusts() {
+        return allDeclared(MustStatement.class);
+    }
+
+    @Nullable @Override
+    public DefaultStatement getDefault() {
+        return firstDeclared(DefaultStatement.class);
+    }
+
+    @Nullable @Override
+    public ConfigStatement getConfig() {
+        return firstDeclared(ConfigStatement.class);
+    }
+
+    @Nullable @Override
+    public MandatoryStatement getMandatory() {
+        return firstDeclared(MandatoryStatement.class);
+    }
+
+    @Nonnull @Override
+    public QName getName() {
+        return argument();
+    }
+
+    @Override
+    public WhenStatement getWhenStatement() {
+        return firstDeclared(WhenStatement.class);
+    }
+
+    @Nonnull @Override
+    public Collection<? extends IfFeatureStatement> getIfFeatures() {
+        return allDeclared(IfFeatureStatement.class);
+    }
+
+    @Nullable @Override
+    public DescriptionStatement getDescription() {
+        return firstDeclared(DescriptionStatement.class);
+    }
+
+    @Nullable @Override
+    public ReferenceStatement getReference() {
+        return firstDeclared(ReferenceStatement.class);
+    }
+
+    @Nonnull @Override
+    public TypeStatement getType() {
+        return firstDeclared(TypeStatement.class);
+    }
+
+    @Nullable @Override
+    public UnitsStatement getUnits() {
+        return firstDeclared(UnitsStatement.class);
+    }
+
+    @Nullable @Override
+    public StatusStatement getStatus() {
+        return firstDeclared(StatusStatement.class);
+    }
+}
