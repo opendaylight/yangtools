5969203b848f80cc59106d8f275b42c954ab0a17
Martin Ciglan <mciglan@cisco.com>
2015-03-13 17:15:00 +0100

    Bug 2366 - new parser API impl - added support and implementation for various statements (work in progress) - added to test
    Bug 2366 - New parser impl
    
    - fix of phases switching
    - register support for container statement
    - add container statements into the body of yang module in the unit test
    
    Change-Id: I5d6acc69bb877b26403d69e276ed5c578e9bd4d9
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/TypeStatementImpl.java
@@ -0,0 +1,60 @@
+package org.opendaylight.yangtools.yang.parser.stmt.rfc6020;
+
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.Rfc6020Mapping;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ConfigStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.DefaultStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.DescriptionStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.IfFeatureStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.LeafStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.MandatoryStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.MustStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.RangeStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ReferenceStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.StatusStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.TypeStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.UnitsStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.WhenStatement;
+import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractDeclaredStatement;
+import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractStatementSupport;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Collection;
+
+public class TypeStatementImpl extends AbstractDeclaredStatement<QName> implements TypeStatement {
+
+    protected TypeStatementImpl(StmtContext<QName, TypeStatement, ?> context) {
+        super(context);
+    }
+
+    public static class Definition extends AbstractStatementSupport<QName,TypeStatement,EffectiveStatement<QName,TypeStatement>> {
+
+        public Definition() {
+            super(Rfc6020Mapping.Type);
+        }
+
+        @Override public QName parseArgumentValue(StmtContext<?, ?, ?> ctx,
+                String value) throws SourceException {
+            return Utils.qNameFromArgument(ctx,value);
+        }
+
+        @Override public TypeStatement createDeclared(
+                StmtContext<QName, TypeStatement, ?> ctx) {
+            return new TypeStatementImpl(ctx);
+        }
+
+        @Override public EffectiveStatement<QName, TypeStatement> createEffective(
+                StmtContext<QName, TypeStatement, EffectiveStatement<QName, TypeStatement>> ctx) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    @Nonnull @Override
+    public String getName() {
+        return rawArgument();
+    }
+}
