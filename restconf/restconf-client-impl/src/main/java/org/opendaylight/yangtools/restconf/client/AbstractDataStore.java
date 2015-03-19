7e94c3ee1a8b45978f9b9d7c3291d9f85008cca2
Tony Tkacik <ttkacik@cisco.com>
2015-03-19 13:21:53 +0100

    Bug 868: Made sure Restconf client POC does not use CompositeNodes.
    Change-Id: I64a940fa86cb0a0f4adccfde401b7073f7bbf9b8
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- restconf/restconf-client-impl/src/main/java/org/opendaylight/yangtools/restconf/client/AbstractDataStore.java
+++ restconf/restconf-client-impl/src/main/java/org/opendaylight/yangtools/restconf/client/AbstractDataStore.java
@@ -4,14 +4,16 @@ import com.google.common.base.Function;
 import com.google.common.base.Optional;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.sun.jersey.api.client.ClientResponse;
+import java.io.InputStream;
 import java.util.Map.Entry;
+import org.opendaylight.yangtools.binding.data.codec.impl.BindingNormalizedNodeCodecRegistry;
 import org.opendaylight.yangtools.restconf.client.api.data.Datastore;
 import org.opendaylight.yangtools.restconf.client.api.data.DefaultRetrievalStrategy;
 import org.opendaylight.yangtools.restconf.client.api.data.RetrievalStrategy;
 import org.opendaylight.yangtools.restconf.utils.RestconfUtils;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.data.impl.codec.BindingIndependentMappingService;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 
@@ -38,18 +40,19 @@ public abstract class AbstractDataStore implements Datastore {
             RetrievalStrategy strategy) {
 
         final SchemaContext schemaContext = client.getSchemaContext();
-        final BindingIndependentMappingService mappingService = client.getMappingService();
-        final Entry<String, DataSchemaNode> pathWithSchema = RestconfUtils.toRestconfIdentifier(path, mappingService, schemaContext);
+        final BindingNormalizedNodeCodecRegistry mappingService = client.getMappingService();
+        final YangInstanceIdentifier domPath = mappingService.toYangInstanceIdentifier(path);
+        final Entry<String, DataSchemaNode> pathWithSchema = RestconfUtils.toRestconfIdentifier(domPath,schemaContext);
         String restconfPath = getStorePrefix() + pathWithSchema.getKey();
 
         return client.get(restconfPath, "application/xml",new Function<ClientResponse, Optional<T>>() {
 
+            @SuppressWarnings("unchecked")
             @Override
             public com.google.common.base.Optional<T> apply(ClientResponse response) {
                 switch (response.getStatus()) {
                 case 200: // Status OK
-                    DataObject dataObject = RestconfUtils.dataObjectFromInputStream(path,
-                            response.getEntityInputStream(), schemaContext, mappingService,pathWithSchema.getValue());
+                    DataObject dataObject = deserialize(domPath,response.getEntityInputStream());
                     return (Optional<T>) Optional.of(dataObject);
                 case 404: // Status Not Found
                     return Optional.<T> absent();
@@ -57,7 +60,13 @@ public abstract class AbstractDataStore implements Datastore {
                     throw new IllegalStateException("Failed : HTTP error code : " + response.getStatus());
                 }
             }
+
+
         });
+    }
+
+    protected final DataObject deserialize(YangInstanceIdentifier domPath, InputStream entityInputStream) {
+        throw new UnsupportedOperationException("Not implemented yet.");
     }
 
     protected abstract String getStorePrefix();
