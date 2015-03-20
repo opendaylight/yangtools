2f0aaa2d5d5819299786acea3f8c358d459c5215
Robert Varga <rovarga@cisco.com>
2015-03-20 18:58:47 +0100

    Add DefaultDataTreeCandidate utility class
    We have DataTreeCandidate implementations cropping up in the system,
    this provides a simple class for reuse.
    
    Change-Id: I3e0b594fd67b194fc38d4d3a1e2643802772aec1
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/tree/spi/DefaultDataTreeCandidate.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.api.schema.tree.spi;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidateNode;
+
+/**
+ * Default utility implementation of the {@link DataTreeCandidate} contract.
+ */
+public final class DefaultDataTreeCandidate implements DataTreeCandidate {
+    private final YangInstanceIdentifier rootPath;
+    private final DataTreeCandidateNode rootNode;
+
+    public DefaultDataTreeCandidate(final YangInstanceIdentifier rootPath, final DataTreeCandidateNode rootNode) {
+        this.rootPath = Preconditions.checkNotNull(rootPath);
+        this.rootNode = Preconditions.checkNotNull(rootNode);
+    }
+
+    @Override
+    public DataTreeCandidateNode getRootNode() {
+        return rootNode;
+    }
+
+    @Override
+    public YangInstanceIdentifier getRootPath() {
+        return rootPath;
+    }
+
+    @Override
+    public final String toString() {
+        return MoreObjects.toStringHelper(this).add("rootPath", getRootPath()).add("rootNode", getRootNode()).toString();
+    }
+}
