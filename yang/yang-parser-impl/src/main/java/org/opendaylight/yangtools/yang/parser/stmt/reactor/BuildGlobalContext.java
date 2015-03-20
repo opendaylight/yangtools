f098d58359a140fa6fc578149caaf924070f02bb
Martin Ciglan <mciglan@cisco.com>
2015-03-20 13:36:18 +0100

    Bug 2366: Initial implementation of Statement Reactor
    Initial implementation of statement reactor,
    which supports:
       - loading linkage statements from models
       - modifiers
       - resolving of forward references
       - building final statments
    
    Implementation currently does not support:
    
    - export of PrefixToQNameModule and QNameToStatementDefinition
      namespaces during loading statement streams.
    - copying statement subtrees (useful for uses, augmentations)
    
    Added initial set of tests - which deals with
    RFC6020 import resolution.
    
    Change-Id: Idde3ce496228a157ba11c07bccf6f177c52a8fe9
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/BuildGlobalContext.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.stmt.reactor;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import javax.annotation.Nonnull;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.IdentifierNamespace;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ModelProcessingPhase;
+import org.opendaylight.yangtools.yang.parser.spi.meta.NamespaceBehaviour;
+import org.opendaylight.yangtools.yang.parser.spi.meta.NamespaceBehaviour.NamespaceStorageNode;
+import org.opendaylight.yangtools.yang.parser.spi.meta.NamespaceBehaviour.StorageNodeType;
+import org.opendaylight.yangtools.yang.parser.spi.meta.NamespaceNotAvailableException;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.spi.meta.SomeModifiersUnresolvedException;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StatementSupport;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StatementSupportBundle;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+import org.opendaylight.yangtools.yang.parser.spi.source.StatementStreamSource;
+import org.opendaylight.yangtools.yang.parser.stmt.reactor.SourceSpecificContext.PhaseCompletionProgress;
+
+class BuildGlobalContext extends NamespaceStorageSupport implements NamespaceBehaviour.Registry {
+
+    private static final List<ModelProcessingPhase> PHASE_EXECUTION_ORDER = ImmutableList.<ModelProcessingPhase>builder()
+            .add(ModelProcessingPhase.SourceLinkage)
+            .add(ModelProcessingPhase.StatementDefinition)
+            .add(ModelProcessingPhase.FullDeclaration)
+            .add(ModelProcessingPhase.EffectiveModel)
+            .build();
+
+    private final Map<QName,StatementDefinitionContext<?,?,?>> definitions = new HashMap<>();
+    private final Map<Class<?>,NamespaceBehaviourWithListeners<?, ?, ?>> namespaces = new HashMap<>();
+
+
+    private final Map<ModelProcessingPhase,StatementSupportBundle> supports;
+    private final Set<SourceSpecificContext> sources = new HashSet<>();
+
+    private ModelProcessingPhase currentPhase;
+    private ModelProcessingPhase finishedPhase;
+
+    public BuildGlobalContext(Map<ModelProcessingPhase, StatementSupportBundle> supports) {
+        super();
+        this.supports = supports;
+    }
+
+    public void addSource(@Nonnull StatementStreamSource source) {
+        sources.add(new SourceSpecificContext(this,source));
+    }
+
+    @Override
+    public StorageNodeType getStorageNodeType() {
+        return StorageNodeType.Global;
+    }
+
+    @Override
+    public NamespaceStorageNode getParentNamespaceStorage() {
+        return null;
+    }
+
+    @Override
+    public NamespaceBehaviour.Registry getBehaviourRegistry() {
+        return this;
+    }
+
+    @Override
+    public <K, V, N extends IdentifierNamespace<K, V>> NamespaceBehaviourWithListeners<K, V, N> getNamespaceBehaviour(Class<N> type) {
+        NamespaceBehaviourWithListeners<?, ?, ?> potential = namespaces.get(type);
+        if (potential == null) {
+            NamespaceBehaviour<K, V, N> potentialRaw = supports.get(currentPhase).getNamespaceBehaviour(type);
+            if(potentialRaw != null) {
+                potential = new NamespaceBehaviourWithListeners<>(potentialRaw);
+                namespaces.put(type, potential);
+            }
+        }
+        if (potential != null) {
+            Preconditions.checkState(type.equals(potential.getIdentifier()));
+
+            /*
+             * Safe cast, previous checkState checks equivalence of key from
+             * which type argument are derived
+             */
+            @SuppressWarnings("unchecked")
+            NamespaceBehaviourWithListeners<K, V, N> casted = (NamespaceBehaviourWithListeners<K, V, N>) potential;
+            return casted;
+        }
+        throw new NamespaceNotAvailableException("Namespace " + type + "is not available in phase " + currentPhase);
+    }
+
+    public StatementDefinitionContext<?, ?, ?> getStatementDefinition(QName name) {
+        StatementDefinitionContext<?, ?, ?> potential = definitions.get(name);
+        if(potential == null) {
+            StatementSupport<?, ?, ?> potentialRaw = supports.get(currentPhase).getStatementDefinition(name);
+            if(potentialRaw != null) {
+                potential = new StatementDefinitionContext<>(potentialRaw);
+                definitions.put(name, potential);
+            }
+        }
+        return potential;
+    }
+
+    public EffectiveModelContext build() throws SourceException, ReactorException {
+        for(ModelProcessingPhase phase : PHASE_EXECUTION_ORDER) {
+            startPhase(phase);
+            loadPhaseStatements();
+            completePhaseActions();
+            endPhase(phase);
+        }
+        return transform();
+    }
+
+    private EffectiveModelContext transform() {
+        Preconditions.checkState(finishedPhase == ModelProcessingPhase.EffectiveModel);
+        List<DeclaredStatement<?>> rootStatements = new ArrayList<>();
+        for(SourceSpecificContext source : sources) {
+            DeclaredStatement<?> root = source.getRoot().buildDeclared();
+            rootStatements.add(root);
+        }
+        return new EffectiveModelContext(rootStatements);
+    }
+
+    private void startPhase(ModelProcessingPhase phase) {
+        Preconditions.checkState(Objects.equals(finishedPhase, phase.getPreviousPhase()));
+        for(SourceSpecificContext source : sources) {
+            source.startPhase(phase);
+        }
+        currentPhase = phase;
+    }
+
+    private  void loadPhaseStatements() throws SourceException {
+        Preconditions.checkState(currentPhase != null);
+        for(SourceSpecificContext source : sources) {
+            source.loadStatements();
+        }
+    }
+
+    private  void completePhaseActions() throws ReactorException {
+        Preconditions.checkState(currentPhase != null);
+        ArrayList<SourceSpecificContext> sourcesToProgress = Lists.newArrayList(sources);
+        try {
+            boolean progressing = true;
+            while(progressing) {
+                // We reset progressing to false.
+                progressing = false;
+                Iterator<SourceSpecificContext> currentSource = sourcesToProgress.iterator();
+                while(currentSource.hasNext()) {
+                    PhaseCompletionProgress sourceProgress = currentSource.next().tryToCompletePhase(currentPhase);
+                    switch (sourceProgress) {
+                        case FINISHED:
+                            currentSource.remove();
+                        case PROGRESS:
+                            progressing = true;
+                        case NO_PROGRESS:
+                            // Noop;
+                    }
+                }
+            }
+        } catch (SourceException e) {
+            throw Throwables.propagate(e);
+        }
+        if(!sourcesToProgress.isEmpty()) {
+            SomeModifiersUnresolvedException buildFailure = new SomeModifiersUnresolvedException(currentPhase);
+                for(SourceSpecificContext failedSource : sourcesToProgress) {
+                    SourceException sourceEx = failedSource.failModifiers(currentPhase);
+                    buildFailure.addSuppressed(sourceEx);
+                }
+                throw buildFailure;
+        }
+    }
+
+    private  void endPhase(ModelProcessingPhase phase) {
+        Preconditions.checkState(currentPhase == phase);
+        finishedPhase = currentPhase;
+    }
+
+    public Set<SourceSpecificContext> getSources() {
+        return sources;
+    }
+
+}
---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/NamespaceBehaviourWithListeners.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.stmt.reactor;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import java.util.Iterator;
+import org.opendaylight.yangtools.yang.model.api.meta.IdentifierNamespace;
+import org.opendaylight.yangtools.yang.parser.spi.meta.NamespaceBehaviour;
+
+final class NamespaceBehaviourWithListeners<K,V, N extends IdentifierNamespace<K, V>> extends NamespaceBehaviour<K, V, N> {
+
+    static abstract class ValueAddedListener {
+
+        private org.opendaylight.yangtools.yang.parser.spi.meta.NamespaceBehaviour.NamespaceStorageNode ctxNode;
+
+        public ValueAddedListener(NamespaceStorageNode contextNode) {
+            this.ctxNode = contextNode;
+        }
+
+        abstract void onValueAdded(Object key, Object value);
+
+    }
+
+    private final NamespaceBehaviour<K, V, N> delegate;
+    private final Multimap<K, ValueAddedListener> listeners = HashMultimap.create();
+
+    protected NamespaceBehaviourWithListeners(NamespaceBehaviour<K, V, N> delegate) {
+        super(delegate.getIdentifier());
+        this.delegate = delegate;
+    }
+
+    @Override
+    public void addTo(NamespaceBehaviour.NamespaceStorageNode storage,
+            K key, V value) {
+        delegate.addTo(storage, key, value);
+
+        Iterator<ValueAddedListener> keyListeners = listeners.get(key).iterator();
+        while(keyListeners.hasNext()) {
+            ValueAddedListener listener = keyListeners.next();
+            if(listener.ctxNode == storage || hasIdentiticalValue(listener.ctxNode,key,value)) {
+                keyListeners.remove();
+                listener.onValueAdded(key, value);
+            }
+        }
+    }
+
+    private boolean hasIdentiticalValue(NamespaceBehaviour.NamespaceStorageNode ctxNode, K key, V value) {
+        return getFrom(ctxNode, key) == value;
+    }
+
+    void addValueListener(K key, ValueAddedListener listener) {
+        listeners.put(key, listener);
+    }
+
+    @Override
+    public V getFrom(NamespaceBehaviour.NamespaceStorageNode storage,
+            K key) {
+        return delegate.getFrom(storage, key);
+    }
+}
