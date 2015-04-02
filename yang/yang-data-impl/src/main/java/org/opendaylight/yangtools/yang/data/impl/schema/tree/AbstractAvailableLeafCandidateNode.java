0aadf3bcf74e6a11f9783092384be36153284442
Robert Varga <rovarga@cisco.com>
2015-04-02 19:20:18 +0200

    BUG-2939: fixup dynamic nodes to cover overwrites
    Promote inner classes to their proper equivalents. Perform proper lazy
    comparison to cover all possible scenarios where the user might
    want to examine terminal nodes in greater depth. This is done by
    tracking presence of before/after data and fudging appropriate
    modification types.
    
    We make sure to not run equals() on the data, as that may introduce very
    steep costs -- an overwrite with the same value is still reported as a
    full write, not as unmodified.
    
    Change-Id: I0df100a74cfa82ceddeefcfae0ca2a011c608e08
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/AbstractAvailableLeafCandidateNode.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.impl.schema.tree;
+
+import com.google.common.base.Optional;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+abstract class AbstractAvailableLeafCandidateNode extends AbstractLeafCandidateNode {
+    protected AbstractAvailableLeafCandidateNode(final NormalizedNode<?, ?> dataAfter) {
+        super(dataAfter);
+    }
+
+    @Override
+    public final Optional<NormalizedNode<?, ?>> getDataAfter() {
+        return dataOptional();
+    }
+}
