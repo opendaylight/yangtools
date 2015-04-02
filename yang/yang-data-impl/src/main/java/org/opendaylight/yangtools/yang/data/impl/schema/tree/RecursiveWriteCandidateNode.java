6e64a9514adf8cd9712f746227e07b66510a2732
Robert Varga <rovarga@cisco.com>
2015-04-02 19:20:18 +0200

    BUG-2939: fixup dynamic nodes to cover overwrites
    This fixes the recursive creation so it relies only on the physical
    change type.
    
    Change-Id: I0df100a74cfa82ceddeefcfae0ca2a011c608e08
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/RecursiveWriteCandidateNode.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.impl.schema.tree;
+
+import com.google.common.base.Optional;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodeContainer;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidateNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.ModificationType;
+
+final class RecursiveWriteCandidateNode extends AbstractRecursiveCandidateNode {
+
+    RecursiveWriteCandidateNode(final NormalizedNodeContainer<?, PathArgument, NormalizedNode<?, ?>> data) {
+        super(data);
+    }
+
+    @Override
+    public ModificationType getModificationType() {
+        return ModificationType.WRITE;
+    }
+
+    @Override
+    protected DataTreeCandidateNode createContainer(
+            final NormalizedNodeContainer<?, PathArgument, NormalizedNode<?, ?>> childData) {
+        return new RecursiveWriteCandidateNode(childData);
+    }
+
+    @Override
+    protected DataTreeCandidateNode createLeaf(final NormalizedNode<?, ?> childData) {
+        return new WriteLeafCandidateNode(childData);
+    }
+
+    @Override
+    public Optional<NormalizedNode<?, ?>> getDataAfter() {
+        return dataOptional();
+    }
+
+    @Override
+    public Optional<NormalizedNode<?, ?>> getDataBefore() {
+        return Optional.absent();
+    }
+}
