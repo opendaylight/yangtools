af6e88b3a9d2bc0c95bb80a37b52baa82b47904f
Robert Varga <rovarga@cisco.com>
2015-04-02 19:20:18 +0200

    BUG-2939: fixup dynamic nodes to cover overwrites
    This fixes the recursive creation so it relies only on the physical
    change type.
    
    Change-Id: I0df100a74cfa82ceddeefcfae0ca2a011c608e08
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/AbstractModifiedNodeBasedCandidateNode.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/AbstractModifiedNodeBasedCandidateNode.java
@@ -11,17 +11,45 @@ import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Verify;
 import com.google.common.collect.Collections2;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import java.util.ArrayList;
 import java.util.Collection;
-import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.Map.Entry;
 import javax.annotation.Nullable;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodeContainer;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidateNode;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.ModificationType;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.spi.TreeNode;
 
-
 abstract class AbstractModifiedNodeBasedCandidateNode implements DataTreeCandidateNode {
+    private static final Function<NormalizedNode<?, ?>, DataTreeCandidateNode> TO_DELETED_NODES = new Function<NormalizedNode<?, ?>, DataTreeCandidateNode>() {
+        @Override
+        public DataTreeCandidateNode apply(final NormalizedNode<?, ?> input) {
+            return AbstractRecursiveCandidateNode.deleteNode(input);
+        }
+    };
+    private static final Function<NormalizedNode<?, ?>, DataTreeCandidateNode> TO_UNMODIFIED_NODES = new Function<NormalizedNode<?, ?>, DataTreeCandidateNode>() {
+        @Override
+        public DataTreeCandidateNode apply(final NormalizedNode<?, ?> input) {
+            return AbstractRecursiveCandidateNode.unmodifiedNode(input);
+        }
+    };
+    private static final Function<NormalizedNode<?, ?>, DataTreeCandidateNode> TO_WRITTEN_NODES = new Function<NormalizedNode<?, ?>, DataTreeCandidateNode>() {
+        @Override
+        public DataTreeCandidateNode apply(final NormalizedNode<?, ?> input) {
+            return AbstractRecursiveCandidateNode.initialWriteNode(input);
+        }
+    };
+    private static final Function<NormalizedNode<?, ?>, PathArgument> GET_IDENTIFIER = new Function<NormalizedNode<?, ?>, PathArgument>() {
+        @Override
+        public PathArgument apply(final NormalizedNode<?, ?> input) {
+            return input.getIdentifier();
+        }
+    };
     private final ModifiedNode mod;
     private final TreeNode newMeta;
     private final TreeNode oldMeta;
@@ -53,29 +81,92 @@ abstract class AbstractModifiedNodeBasedCandidateNode implements DataTreeCandida
         }
     }
 
-    private DataTreeCandidateNode childNode(final ModifiedNode input) {
-        final PathArgument id = input.getIdentifier();
-        return createChildNode(input, childMeta(oldMeta, id), childMeta(newMeta, id));
+    private static boolean canHaveChildren(@Nullable final TreeNode oldMeta, @Nullable final TreeNode newMeta) {
+        if (oldMeta != null) {
+            return oldMeta.getData() instanceof NormalizedNodeContainer;
+        }
+        if (newMeta != null) {
+            return newMeta.getData() instanceof NormalizedNodeContainer;
+        }
+        return false;
     }
 
-    private static DataTreeCandidateNode createChildNode(@Nonnull final ModifiedNode input, @Nullable final TreeNode oldMeta, @Nullable final TreeNode newMeta) {
-        if(oldMeta == null) {
-            return RecursiveModificationCandidateNode.initialWriteNode(newMeta.getData());
+    @SuppressWarnings("unchecked")
+    private static NormalizedNodeContainer<?, PathArgument, NormalizedNode<?, ?>> getContainer(@Nullable final TreeNode meta) {
+        return (meta == null ? null : (NormalizedNodeContainer<?, PathArgument, NormalizedNode<?, ?>>)meta.getData());
+    }
+
+    private static Collection<DataTreeCandidateNode> deltaChildren(@Nullable final NormalizedNodeContainer<?, PathArgument, NormalizedNode<?, ?>> oldData,
+            @Nullable final NormalizedNodeContainer<?, PathArgument, NormalizedNode<?, ?>> newData) {
+        if (newData == null) {
+            return Collections2.transform(oldData.getValue(), TO_DELETED_NODES);
         }
-        if(newMeta == null) {
-            return RecursiveModificationCandidateNode.deleteNode(oldMeta.getData());
+        if (oldData == null) {
+            return Collections2.transform(newData.getValue(), TO_WRITTEN_NODES);
         }
-        return new ChildNode(input, oldMeta, newMeta);
+
+        // Create indices for fast cross-references. This could be speeded up if we could access those
+        // maps that reside in our (ImmutableNodes) implementations.
+        final ImmutableMap<PathArgument, NormalizedNode<?, ?>> deleted = Maps.uniqueIndex(oldData.getValue(), GET_IDENTIFIER);
+        final ImmutableMap<PathArgument, NormalizedNode<?, ?>> written = Maps.uniqueIndex(newData.getValue(), GET_IDENTIFIER);
+
+        final Collection<DataTreeCandidateNode> ret = new ArrayList<>(Math.max(deleted.size(), written.size()));
+        for (Entry<PathArgument, NormalizedNode<?, ?>> e : deleted.entrySet()) {
+            final NormalizedNode<?, ?> newValue = written.get(e.getKey());
+            final DataTreeCandidateNode child;
+            if (newValue != null) {
+                // FIXME: child = AbstractRecursiveCandidateNode.overWriteNode(e.getValue(), newValue);
+                child = null;
+            } else {
+                child = AbstractRecursiveCandidateNode.deleteNode(e.getValue());
+            }
+
+            ret.add(child);
+        }
+
+        for (Entry<PathArgument, NormalizedNode<?, ?>> e : written.entrySet()) {
+            if (!deleted.containsKey(e.getKey())) {
+                ret.add(AbstractRecursiveCandidateNode.initialWriteNode(e.getValue()));
+            }
+        }
+
+        return ret;
+    }
+
+    private ChildNode childNode(final ModifiedNode childMod) {
+        final PathArgument id = childMod.getIdentifier();
+        return new ChildNode(childMod, childMeta(oldMeta, id), childMeta(newMeta, id));
     }
 
     @Override
     public Collection<DataTreeCandidateNode> getChildNodes() {
-        return Collections2.transform(mod.getChildren(), new Function<ModifiedNode, DataTreeCandidateNode>() {
-            @Override
-            public DataTreeCandidateNode apply(final ModifiedNode input) {
-                return childNode(input);
+        switch (mod.getModificationType()) {
+        case SUBTREE_MODIFIED:
+            return Collections2.transform(mod.getChildren(), new Function<ModifiedNode, DataTreeCandidateNode>() {
+                @Override
+                public DataTreeCandidateNode apply(final ModifiedNode input) {
+                    return childNode(input);
+                }
+            });
+        case UNMODIFIED:
+            // Unmodified node, but we still need to resolve potential children
+            if (canHaveChildren(oldMeta, newMeta)) {
+                return Collections2.transform(getContainer(oldMeta != null ? oldMeta : newMeta).getValue(), TO_UNMODIFIED_NODES);
+            } else {
+                return Collections.emptyList();
             }
-        });
+        case DELETE:
+        case WRITE:
+            // This is unusual, the user is requesting we follow into an otherwise-terminal node.
+            // We need to fudge things based on before/after data to correctly fake the expectations.
+            if (canHaveChildren(oldMeta, newMeta)) {
+                return deltaChildren(getContainer(oldMeta), getContainer(newMeta));
+            } else {
+                return Collections.emptyList();
+            }
+        default:
+            throw new IllegalArgumentException("Unhandled modification type " + mod.getModificationType());
+        }
     }
 
     @Override
@@ -103,6 +194,7 @@ abstract class AbstractModifiedNodeBasedCandidateNode implements DataTreeCandida
 
     @Override
     public DataTreeCandidateNode getModifiedChild(final PathArgument identifier) {
+        // FIXME: this works only for SUBTREE_MODIFIED
         final Optional<ModifiedNode> childMod = mod.getChild(identifier);
         if (childMod.isPresent()) {
             return childNode(childMod.get());
@@ -110,8 +202,8 @@ abstract class AbstractModifiedNodeBasedCandidateNode implements DataTreeCandida
         return null;
     }
 
-    static final class ChildNode extends AbstractModifiedNodeBasedCandidateNode {
-        public ChildNode(final ModifiedNode mod, final TreeNode oldMeta, final TreeNode newMeta) {
+    private static final class ChildNode extends AbstractModifiedNodeBasedCandidateNode {
+        ChildNode(final ModifiedNode mod, final TreeNode oldMeta, final TreeNode newMeta) {
             super(mod, oldMeta, newMeta);
         }
 
