99197f3fd9279b2439f6ec2c2794c36bcddbd4f8
Tony Tkacik <ttkacik@cisco.com>
2015-04-08 09:46:21 +0200

    Extracted remaining codecs to protected classes.
    Change-Id: I1cf9413b8971ae8c2cd51e7995aae537f6705767
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/codec/UnionStringCodec.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse
+ * Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.impl.codec;
+
+import com.google.common.base.Optional;
+import org.opendaylight.yangtools.yang.data.api.codec.UnionCodec;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.UnionTypeDefinition;
+
+class UnionStringCodec extends TypeDefinitionAwareCodec<Object, UnionTypeDefinition> implements UnionCodec<String> {
+
+    protected UnionStringCodec(final Optional<UnionTypeDefinition> typeDef) {
+        super(typeDef, Object.class);
+    }
+
+    static TypeDefinitionAwareCodec<?, UnionTypeDefinition> from(final UnionTypeDefinition normalizedType) {
+        return new UnionStringCodec(Optional.fromNullable(normalizedType));
+    }
+
+    @Override
+    public final String serialize(final Object data) {
+        return data == null ? "" : data.toString();
+    }
+
+    @Override
+    public final Object deserialize(final String stringRepresentation) {
+
+        if (!getTypeDefinition().isPresent()) {
+            return stringRepresentation;
+        }
+
+        for (final TypeDefinition<?> type : getTypeDefinition().get().getTypes()) {
+            final TypeDefinitionAwareCodec<Object, ? extends TypeDefinition<?>> typeAwareCodec = from(type);
+            if (typeAwareCodec == null) {
+                /*
+                 * This is a type for which we have no codec (eg identity ref) so we'll say it's
+                 * valid
+                 */
+                return stringRepresentation;
+            }
+
+            try {
+                typeAwareCodec.deserialize(stringRepresentation);
+                return stringRepresentation;
+            } catch (final Exception e) {
+                // invalid - try the next union type.
+            }
+        }
+
+        throw new IllegalArgumentException("Invalid value \"" + stringRepresentation + "\" for union type.");
+    }
+}
