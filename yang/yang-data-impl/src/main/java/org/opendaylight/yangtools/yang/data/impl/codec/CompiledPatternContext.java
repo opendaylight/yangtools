4f7db0c13cf84d3878b50bb633633ee6f423b9b5
Tony Tkacik <ttkacik@cisco.com>
2015-04-08 11:01:31 +0200

    Bug 2362: Added pattern validation for string types.
    Change-Id: Icea2e7cfa87bd746e6da99e6908ab959d6356fd3
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/codec/CompiledPatternContext.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.impl.codec;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import java.util.regex.Pattern;
+import org.opendaylight.yangtools.yang.model.api.type.PatternConstraint;
+
+class CompiledPatternContext {
+
+    private final Pattern pattern;
+    private final String errorMessage;
+
+    CompiledPatternContext(final PatternConstraint yangConstraint) {
+        pattern = Pattern.compile("^"+yangConstraint.getRegularExpression()+"$");
+        final String yangMessage = yangConstraint.getErrorMessage();
+        if(Strings.isNullOrEmpty(yangMessage)) {
+            errorMessage = "Value %s does not match regular expression <" + pattern.pattern() + ">";
+        } else {
+            errorMessage = yangMessage;
+        }
+    }
+
+    public void validate(final String s) {
+        Preconditions.checkArgument(pattern.matcher(s).matches(),errorMessage,s);
+    }
+
+}
