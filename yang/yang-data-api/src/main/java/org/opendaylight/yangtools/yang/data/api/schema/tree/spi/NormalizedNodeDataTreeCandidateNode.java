d9c0fbf390803d7d009b45285d27976eb110a69d
Robert Varga <rovarga@cisco.com>
2015-04-18 20:36:18 +0200

    BUG-3018: introduce NormalizedNodeDataTreeCandidateNode
    This is a utility class for turning NormalizedNode instances into
    freshly-written DataTreeCandidateNodes. This is required in the
    controller project, but is generally useful for any user of
    yang-data-api.
    
    Change-Id: Id8126a68f3bf35092bd8630358eb258d65c61d36
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/tree/spi/NormalizedNodeDataTreeCandidateNode.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.api.schema.tree.spi;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Collections2;
+import java.util.Collection;
+import java.util.Collections;
+import javax.annotation.Nonnull;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodeContainer;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidateNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.ModificationType;
+
+/**
+ * Utility implementation of {@link DataTreeCandidateNode} which acts as if
+ * the {@link NormalizedNode} passed to it at creation time were freshly written.
+ */
+@Beta
+public final class NormalizedNodeDataTreeCandidateNode implements DataTreeCandidateNode {
+    /**
+     * Convenience function for functional transformation of {@link NormalizedNode} into
+     * a {@link DataTreeCandidateNode}.
+     */
+    public static final Function<NormalizedNode<?, ?>, DataTreeCandidateNode> FACTORY_FUNCTION = new Function<NormalizedNode<?, ?>, DataTreeCandidateNode>() {
+        @Override
+        public DataTreeCandidateNode apply(final NormalizedNode<?, ?> input) {
+            return input == null ? null : new NormalizedNodeDataTreeCandidateNode(input);
+        }
+    };
+    private final NormalizedNode<?, ?> data;
+
+    /**
+     * Create a new instance backed by supplied data.
+     *
+     * @param data Backing {@link NormalizedNode} data.
+     */
+    public NormalizedNodeDataTreeCandidateNode(@Nonnull final NormalizedNode<?, ?> data) {
+        this.data = Preconditions.checkNotNull(data);
+    }
+
+    @Override
+    public PathArgument getIdentifier() {
+        return data.getIdentifier();
+    }
+
+    @Override
+    public Collection<DataTreeCandidateNode> getChildNodes() {
+        if (data instanceof NormalizedNodeContainer) {
+            return Collections2.transform(((NormalizedNodeContainer<?, ?, ?>) data).getValue(), FACTORY_FUNCTION);
+        } else {
+            return Collections.emptyList();
+        }
+    }
+
+    @Override
+    public DataTreeCandidateNode getModifiedChild(final PathArgument identifier) {
+        if (data instanceof NormalizedNodeContainer) {
+            @SuppressWarnings({ "rawtypes", "unchecked" })
+            final Optional<? extends NormalizedNode<?, ?>> child = ((NormalizedNodeContainer)data).getChild(identifier);
+            return FACTORY_FUNCTION.apply(child.orNull());
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public ModificationType getModificationType() {
+        return ModificationType.WRITE;
+    }
+
+    @Override
+    public Optional<NormalizedNode<?, ?>> getDataAfter() {
+        return Optional.<NormalizedNode<?, ?>>of(data);
+    }
+
+    @Override
+    public Optional<NormalizedNode<?, ?>> getDataBefore() {
+        return Optional.absent();
+    }
+}
