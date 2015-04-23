47156b04493d95a29134225d835b93ee6be003bf
Tony Tkacik <ttkacik@cisco.com>
2015-04-23 17:13:36 +0200

    Bug 3051: Fixed pattern checks in generated DTOs
    Pattern checks in generated DTOs were generated as
    OR checks in case model type specified multiple
    patterns, but correct behaviour is to do AND.
    
    So in order string needs to verified against
    all patterns and match all of them.
    
    Change-Id: If82e282312c82dd71de79534c7fa599fcdcefab4
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- model/ietf/ietf-inet-types/src/test/java/org/opendaylight/yang/gen/v1/urn/ietf/params/xml/ns/yang/ietf/inet/types/rev100924/HostBuilderTest.java
+++ model/ietf/ietf-inet-types/src/test/java/org/opendaylight/yang/gen/v1/urn/ietf/params/xml/ns/yang/ietf/inet/types/rev100924/HostBuilderTest.java
@@ -16,24 +16,23 @@ public class HostBuilderTest {
 
     @Test
     public void testGetDefaultInstanceIpv4() throws Exception {
-        Host host = HostBuilder.getDefaultInstance("127.0.0.1");
+        final Host host = HostBuilder.getDefaultInstance("127.0.0.1");
         assertEquals(new Host(new IpAddress(new Ipv4Address("127.0.0.1"))), host);
     }
 
     @Test
     public void testGetDefaultInstanceIpv6() throws Exception {
-        testIpv6("2001:db8:8s5a3:0:0:8a2e:370:7334");
-        testIpv6("2001:db8:85a3::8a2e:370:7334");
+        testIpv6("1234:5678:9abc:def1:2345:6789:abcd:ef12");
     }
 
     private void testIpv6(final String ivp6string) {
-        Host host = HostBuilder.getDefaultInstance(ivp6string);
+        final Host host = HostBuilder.getDefaultInstance(ivp6string);
         assertEquals(new Host(new IpAddress(new Ipv6Address(ivp6string))), host);
     }
 
     @Test
     public void testGetDefaultInstanceDomain() throws Exception {
-        Host host = HostBuilder.getDefaultInstance("localhost");
+        final Host host = HostBuilder.getDefaultInstance("localhost");
         assertEquals(new Host(new DomainName("localhost")), host);
     }
 }
