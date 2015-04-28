cbb698c351f9f9c527af570573d255ecf8e01108
Gregor Zatko <gzatko@cisco.com>
2015-04-28 13:17:15 +0200

    implementation and tests of effective statements: Module Import Deviation + Submodule in progress
    Change-Id: Ic8ab95dcd10e9c8523a87358ad422a6374856935
    Signed-off-by: Gregor Zatko <gzatko@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/DeviateStatementImpl.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/DeviateStatementImpl.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.yangtools.yang.parser.stmt.rfc6020;
 
+import org.opendaylight.yangtools.yang.model.api.Deviation;
 import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.DeviateEffectiveStatementImpl;
 
 import org.opendaylight.yangtools.yang.model.api.Rfc6020Mapping;
@@ -16,39 +17,41 @@ import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractDeclaredStatement
 import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractStatementSupport;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+
 import javax.annotation.Nonnull;
 
-public class DeviateStatementImpl extends AbstractDeclaredStatement<String> implements DeviateStatement {
+public class DeviateStatementImpl extends AbstractDeclaredStatement<Deviation.Deviate> implements DeviateStatement {
 
     protected DeviateStatementImpl(
-            StmtContext<String, DeviateStatement, ?> context) {
+            StmtContext<Deviation.Deviate, DeviateStatement, ?> context) {
         super(context);
     }
 
-    public static class Definition extends AbstractStatementSupport<String,DeviateStatement,EffectiveStatement<String,DeviateStatement>> {
+    public static class Definition extends AbstractStatementSupport<Deviation.Deviate,DeviateStatement,EffectiveStatement<Deviation.Deviate,DeviateStatement>> {
 
         public Definition() {
             super(Rfc6020Mapping.DEVIATE);
         }
 
-        @Override public String parseArgumentValue(
+        @Override public Deviation.Deviate parseArgumentValue(
                 StmtContext<?, ?, ?> ctx, String value) throws SourceException {
-            return value;
+            return Utils.parseDeviateFromString(value);
         }
 
         @Override public DeviateStatement createDeclared(
-                StmtContext<String, DeviateStatement, ?> ctx) {
+                StmtContext<Deviation.Deviate, DeviateStatement, ?> ctx) {
             return new DeviateStatementImpl(ctx);
         }
 
-        @Override public EffectiveStatement<String, DeviateStatement> createEffective(
-                StmtContext<String, DeviateStatement, EffectiveStatement<String, DeviateStatement>> ctx) {
+        @Override public EffectiveStatement<Deviation.Deviate, DeviateStatement> createEffective(
+                StmtContext<Deviation.Deviate, DeviateStatement, EffectiveStatement<Deviation.Deviate, DeviateStatement>> ctx) {
             return new DeviateEffectiveStatementImpl(ctx);
         }
+
     }
 
     @Nonnull @Override
-    public String getValue() {
-        return rawArgument();
+    public Deviation.Deviate getValue() {
+        return argument();
     }
 }
