35ba0d6f276a52d91012c16c2f379038e14c3c77
Robert Varga <rovarga@cisco.com>
2015-05-02 06:09:52 +0200

    Optimize DataTreeCandidate.applyToModification()
    Both implementations use recursive calls to themselves. Instead of recursion,
    we can use a simplistic single-linked stack of operations and iterate over
    them.
    
    While this results in object allocation, these objects are short-lived
    and never leak from current thread, which means they are easily
    collected. Since the call is not recursive, JIT should be able to more
    easily inline and optimize the operations.
    
    Change-Id: I7d82a58c6be3e853ea5bcf9069876370804cb199
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/tree/DataTreeCandidateNodes.java
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/tree/DataTreeCandidateNodes.java
@@ -8,6 +8,8 @@
 package org.opendaylight.yangtools.yang.data.api.schema.tree;
 
 import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+import java.util.Iterator;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 
 @Beta
@@ -18,5 +20,67 @@ public final class DataTreeCandidateNodes {
 
     public static DataTreeCandidateNode fromNormalizedNode(final NormalizedNode<?, ?> node) {
         return new NormalizedNodeDataTreeCandidateNode(node);
+    }
+
+    public static void applyToCursor(final DataTreeModificationCursor cursor, final DataTreeCandidateNode node) {
+        switch (node.getModificationType()) {
+        case DELETE:
+            cursor.delete(node.getIdentifier());
+            break;
+        case SUBTREE_MODIFIED:
+            cursor.enter(node.getIdentifier());
+            NodeIterator iterator = new NodeIterator(null, node.getChildNodes().iterator());
+            do {
+                iterator = iterator.next(cursor);
+            } while (iterator != null);
+            break;
+        case UNMODIFIED:
+            // No-op
+            break;
+        case WRITE:
+            cursor.write(node.getIdentifier(), node.getDataAfter().get());
+            break;
+        default:
+            throw new IllegalArgumentException("Unsupported modification " + node.getModificationType());
+        }
+    }
+
+    private static final class NodeIterator {
+        private final Iterator<DataTreeCandidateNode> iterator;
+        private final NodeIterator parent;
+
+        NodeIterator(final NodeIterator parent, final Iterator<DataTreeCandidateNode> iterator) {
+            this.parent = Preconditions.checkNotNull(parent);
+            this.iterator = Preconditions.checkNotNull(iterator);
+        }
+
+        NodeIterator next(final DataTreeModificationCursor cursor) {
+            while (iterator.hasNext()) {
+                final DataTreeCandidateNode node = iterator.next();
+                switch (node.getModificationType()) {
+                case DELETE:
+                    cursor.delete(node.getIdentifier());
+                    break;
+                case SUBTREE_MODIFIED:
+                    final Iterator<DataTreeCandidateNode> childIterator = node.getChildNodes().iterator();
+                    if (childIterator.hasNext()) {
+                        cursor.enter(node.getIdentifier());
+                        return new NodeIterator(this, childIterator);
+                    }
+                    break;
+                case UNMODIFIED:
+                    // No-op
+                    break;
+                case WRITE:
+                    cursor.write(node.getIdentifier(), node.getDataAfter().get());
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unsupported modification " + node.getModificationType());
+                }
+            }
+
+            cursor.exit();
+            return parent;
+        }
     }
 }
