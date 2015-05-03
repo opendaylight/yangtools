e7cc8bba266afa3ce1934f8f2b64e5ae662434d0
Robert Varga <rovarga@cisco.com>
2015-05-03 09:43:18 +0200

    Fix benchmark to compile on JDK8
    Since we do not compile benchmarks by default, this part was skipped
    when we fixed javadocs.
    
    Change-Id: I22768608bae94f0ab8cecdbd86b661c8a58010ea
    Signed-off-by: Robert Varga <rovarga@cisco.com>
    (cherry picked from commit 37afff4ffbc6aa13b69a3d0f133a63194845fbdf)

--- benchmarks/src/main/java/org/opendaylight/yangtools/yang/data/impl/tree/BenchmarkModel.java
+++ benchmarks/src/main/java/org/opendaylight/yangtools/yang/data/impl/tree/BenchmarkModel.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.opendaylight.yangtools.yang.data.impl.tree;
 
 import java.io.InputStream;
@@ -10,7 +17,7 @@ import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.parser.impl.YangParserImpl;
 
 /**
- * @author Lukas Sedlak <lsedlak@cisco.com>
+ * @author Lukas Sedlak &lt;lsedlak@cisco.com&gt;
  */
 public class BenchmarkModel {
 
