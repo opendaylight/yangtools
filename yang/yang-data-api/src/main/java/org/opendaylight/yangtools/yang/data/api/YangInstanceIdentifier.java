6d57405a607561d3de81cd2c10923984e786587e
Robert Varga <rovarga@cisco.com>
2015-05-19 00:29:06 +0200

    BUG-3263: return a Collection for path arguments
    With the rework in place, we can now return a Collection from both
    getPathArguments() and getReversePathArguments() even in the stacked
    case.
    
    Change-Id: I51179320022f060d9fb2597f473d0919d391092c
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/YangInstanceIdentifier.java
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/YangInstanceIdentifier.java
@@ -14,6 +14,7 @@ import com.google.common.collect.Iterables;
 import java.io.Serializable;
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -83,7 +84,8 @@ public abstract class YangInstanceIdentifier implements Path<YangInstanceIdentif
     }
 
     @Nonnull abstract YangInstanceIdentifier createRelativeIdentifier(int skipFromRoot);
-    @Nonnull abstract Iterable<PathArgument> tryPathArguments();
+    @Nonnull abstract Collection<PathArgument> tryPathArguments();
+    @Nonnull abstract Collection<PathArgument> tryReversePathArguments();
 
     /**
      * Check if this instance identifier has empty path arguments, e.g. it is
@@ -115,7 +117,7 @@ public abstract class YangInstanceIdentifier implements Path<YangInstanceIdentif
      *
      * @return Immutable iteration of path arguments.
      */
-    public abstract Iterable<PathArgument> getPathArguments();
+    public abstract Collection<PathArgument> getPathArguments();
 
     /**
      * Returns an iterable of path arguments in reverse order. This is useful
@@ -123,7 +125,7 @@ public abstract class YangInstanceIdentifier implements Path<YangInstanceIdentif
      *
      * @return Immutable iterable of path arguments in reverse order.
      */
-    public abstract Iterable<PathArgument> getReversePathArguments();
+    public abstract Collection<PathArgument> getReversePathArguments();
 
     /**
      * Returns the last PathArgument. This is equivalent of iterating
