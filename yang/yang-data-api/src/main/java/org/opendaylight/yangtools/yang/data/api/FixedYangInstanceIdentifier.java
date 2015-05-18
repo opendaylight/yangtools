7f1694ae1b560de07fde7cc3b3e670c95130f0c0
Robert Varga <rovarga@cisco.com>
2015-05-18 21:03:09 +0200

    BUG-3263: Split off Fixed/Stacked YangInstanceIdentifier
    This patch makes YangInstanceIdentifier an abstract class, with two
    subclasses: FixedYangInstanceIdentifier and
    StackedYangInstanceIdentifier.
    
    The fixed version uses an internal ImmutableList and derives efficiency
    from it to the maximum extent allowed.
    
    The stacked version stores a reference to the parent identifier and the
    last path argument. It caches the support classes which provide
    getPathArguments and getReversePathArguments.
    
    Also introduce a getParent() method, as it can now be implemented
    efficiently.
    
    Change-Id: I0c4a771e74d031cae0b53a7f97221213222729f6
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/FixedYangInstanceIdentifier.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.api;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import java.io.ObjectStreamException;
+import java.util.List;
+import org.opendaylight.yangtools.util.HashCodeBuilder;
+
+final class FixedYangInstanceIdentifier extends YangInstanceIdentifier {
+    static final FixedYangInstanceIdentifier EMPTY_INSTANCE = new FixedYangInstanceIdentifier(ImmutableList.<PathArgument>of(), new HashCodeBuilder<>().build());
+    private static final long serialVersionUID = 1L;
+    private final ImmutableList<PathArgument> path;
+
+    private FixedYangInstanceIdentifier(final ImmutableList<PathArgument> path, final int hash) {
+        super(hash);
+        this.path = Preconditions.checkNotNull(path, "path must not be null.");
+    }
+
+    static FixedYangInstanceIdentifier create(final Iterable<? extends PathArgument> path, final int hash) {
+        return new FixedYangInstanceIdentifier(ImmutableList.copyOf(path), hash);
+    }
+
+    @Override
+    public YangInstanceIdentifier getParent() {
+        if (path.isEmpty()) {
+            return null;
+        }
+
+        return YangInstanceIdentifier.create(path.subList(0, path.size() - 1));
+    }
+
+    @Override
+    public List<PathArgument> getPath() {
+        return path;
+    }
+
+    @Override
+    public List<PathArgument> getPathArguments() {
+        return path;
+    }
+
+    @Override
+    public List<PathArgument> getReversePathArguments() {
+        return path.reverse();
+    }
+
+    @Override
+    List<PathArgument> tryPathArguments() {
+        return path;
+    }
+
+    @Override
+    public PathArgument getLastPathArgument() {
+        return path.isEmpty()? null : path.get(path.size() - 1);
+    }
+
+    @Override
+    YangInstanceIdentifier createRelativeIdentifier(final int skipFromRoot) {
+        if (skipFromRoot == path.size()) {
+            return EMPTY_INSTANCE;
+        }
+
+        final ImmutableList<PathArgument> newPath = path.subList(skipFromRoot, path.size());
+        final HashCodeBuilder<PathArgument> hash = new HashCodeBuilder<>();
+        for (PathArgument a : newPath) {
+            hash.addArgument(a);
+        }
+
+        return new FixedYangInstanceIdentifier(newPath, hash.build());
+    }
+
+    private Object readResolve() throws ObjectStreamException {
+        return path.isEmpty() ? EMPTY_INSTANCE : this;
+    }
+
+    @Override
+    boolean pathArgumentsEqual(final YangInstanceIdentifier other) {
+        if (other instanceof FixedYangInstanceIdentifier) {
+            return path.equals(((FixedYangInstanceIdentifier) other).path);
+        } else {
+            return super.pathArgumentsEqual(other);
+        }
+    }
+}
