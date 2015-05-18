ffce7ae68a5edc167b21acea6082c54d508e482d
Robert Varga <rovarga@cisco.com>
2015-05-19 01:05:42 +0200

    BUG-3263: return a List of PathArguments
    This is a further improvement to the interface, we return a List of path
    arguments for both getPathArguments() and getReversePathArguments().
    This makes it possible for StackedYangInstanceIdentifier to not retain a
    legacy list of path arguments.
    
    Change-Id: I98ebc2bec139a0e02b4a428bc783ea23981e3658
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/YangInstanceIdentifier.java
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/YangInstanceIdentifier.java
@@ -109,7 +109,7 @@ public abstract class YangInstanceIdentifier implements Path<YangInstanceIdentif
      *
      * @return Immutable iteration of path arguments.
      */
-    public abstract Collection<PathArgument> getPathArguments();
+    public abstract List<PathArgument> getPathArguments();
 
     /**
      * Returns an iterable of path arguments in reverse order. This is useful
@@ -117,7 +117,7 @@ public abstract class YangInstanceIdentifier implements Path<YangInstanceIdentif
      *
      * @return Immutable iterable of path arguments in reverse order.
      */
-    public abstract Collection<PathArgument> getReversePathArguments();
+    public abstract List<PathArgument> getReversePathArguments();
 
     /**
      * Returns the last PathArgument. This is equivalent of iterating
