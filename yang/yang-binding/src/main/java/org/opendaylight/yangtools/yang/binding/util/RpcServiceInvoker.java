aab5a09be514bc7622416bf2a6d4d6a7465f9d7c
Robert Varga <rovarga@cisco.com>
2015-05-27 12:54:03 +0200

    Allow instantiation of more efficient RpcServiceInvokers
    Current invoker derived from binding class has the downside of
    performing localName->methodName conversion on each invocation, which
    costs us in the lookup path, as deriving the binding method name is a
    costly operation.
    
    Add a new static factory method for creating a RpcServiceInvoker
    instance base on a QName->Method map. Provide two additional
    implementations: a generic one, which performs a direct lookup in the
    map and a specialized one, which performs lookup only on the localName
    part of the qname. The latter is used after we have made sure all QNames
    come from the same QNameModule.
    
    Change-Id: I6a4b33f0487838b3ae6c15470823e4bc01114aa5
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-binding/src/main/java/org/opendaylight/yangtools/yang/binding/util/RpcServiceInvoker.java
+++ yang/yang-binding/src/main/java/org/opendaylight/yangtools/yang/binding/util/RpcServiceInvoker.java
@@ -8,62 +8,62 @@
 package org.opendaylight.yangtools.yang.binding.util;
 
 import com.google.common.base.Preconditions;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableMap.Builder;
 import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.concurrent.Future;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import org.opendaylight.yangtools.yang.binding.BindingMapping;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.RpcService;
 import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Provides single method invocation of RPCs on supplied instance.
  *
  * Rpc Service invoker provides common invocation interface for any subtype of {@link RpcService}.
  * via {@link #invokeRpc(RpcService, QName, DataObject)} method.
- *
- *
- *
  */
-public final class RpcServiceInvoker {
-
-    private static final LoadingCache<Class<? extends RpcService>, RpcServiceInvoker> INVOKERS = CacheBuilder.newBuilder()
-            .weakKeys()
-            .build(new CacheLoader<Class<? extends RpcService>, RpcServiceInvoker>() {
-
-                @Override
-                public RpcServiceInvoker load(Class<? extends RpcService> key) throws Exception {
-                    return createInvoker(key);
-                }
-
-            });
-
-    private final Map<String, RpcMethodInvoker> methodInvokers;
-
-    private RpcServiceInvoker(Map<String, RpcMethodInvoker> methodInvokers) {
-        this.methodInvokers  = Preconditions.checkNotNull(methodInvokers);
-    }
+public abstract class RpcServiceInvoker {
+    private static final Logger LOG = LoggerFactory.getLogger(RpcServiceInvoker.class);
 
     /**
-     *
      * Creates RPCServiceInvoker for specified RpcService type
      *
      * @param type RpcService interface, which was generated from model.
      * @return Cached instance of {@link RpcServiceInvoker} for supplied RPC type.
      *
      */
-    public static RpcServiceInvoker from(Class<? extends RpcService> type) {
-        Preconditions.checkArgument(type.isInterface());
-        Preconditions.checkArgument(BindingReflections.isBindingClass(type));
-        return INVOKERS.getUnchecked(type);
+    public static RpcServiceInvoker from(final Class<? extends RpcService> type) {
+        return ClassBasedRpcServiceInvoker.instanceFor(type);
+    }
+
+    /**
+     * Creates an RPCServiceInvoker for specified QName-&lt;Method mapping.
+     *
+     * @param qnameToMethod translation mapping, must not be null nor empty.
+     * @return An {@link RpcMethodInvoker} instance.
+     */
+    public static RpcServiceInvoker from(final Map<QName, Method> qnameToMethod) {
+        Preconditions.checkArgument(!qnameToMethod.isEmpty());
+        QNameModule module = null;
+
+        for (QName qname : qnameToMethod.keySet()) {
+            if (module != null) {
+                if (!module.equals(qname.getModule())) {
+                    LOG.debug("QNames from different modules {} and {}, falling back to QName map", module, qname.getModule());
+                    return QNameRpcServiceInvoker.instanceFor(qnameToMethod);
+                }
+            } else {
+                module = qname.getModule();
+            }
+        }
+
+        // All module are equal, which means we can use localName only
+        return LocalNameRpcServiceInvoker.instanceFor(module, qnameToMethod);
     }
 
     /**
@@ -74,29 +74,5 @@ public final class RpcServiceInvoker {
      * @param input Input data for RPC.
      * @return Future which will complete once rpc procesing is finished.
      */
-    public Future<RpcResult<?>> invokeRpc(@Nonnull RpcService impl, @Nonnull QName rpcName,@Nullable DataObject input ) {
-        Preconditions.checkNotNull(impl, "implemetation must be supplied");
-        return invoke(impl,BindingMapping.getMethodName(rpcName),input);
-    }
-
-    private static RpcServiceInvoker createInvoker(Class<? extends RpcService> key) {
-        return new RpcServiceInvoker(createInvokerMap(key));
-    }
-
-    private static Map<String, RpcMethodInvoker> createInvokerMap(Class<? extends RpcService> key) {
-        Builder<String, RpcMethodInvoker> ret = ImmutableMap.<String, RpcMethodInvoker>builder();
-        for(Method method : key.getMethods()) {
-            if(BindingReflections.isRpcMethod(method)) {
-                ret.put(method.getName(), RpcMethodInvoker.from(method));
-            }
-
-        }
-        return ret.build();
-    }
-
-    private Future<RpcResult<?>> invoke(RpcService impl, String methodName, DataObject input) {
-        RpcMethodInvoker invoker = methodInvokers.get(methodName);
-        Preconditions.checkArgument(invoker != null,"Supplied rpc is not valid for implementation %s",impl);
-        return invoker.invokeOn(impl, input);
-    }
+    public abstract Future<RpcResult<?>> invokeRpc(@Nonnull final RpcService impl, @Nonnull final QName rpcName, @Nullable final DataObject input);
 }
