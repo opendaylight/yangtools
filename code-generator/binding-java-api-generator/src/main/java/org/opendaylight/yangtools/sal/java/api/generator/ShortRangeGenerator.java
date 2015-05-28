295716868acead6218bed9d1a6ae9b6d03451ac7
Robert Varga <rovarga@cisco.com>
2015-05-29 01:50:23 +0200

    BUG-1485: optimize primitive range checks
    When we are dealing with a primitive type, we can force unboxing of the
    type by declaring the range checker method's argument as the primitive
    type. This will result in the stack not containing references for the
    fast path (e.g. successful check) as well as reduce the number of
    virtual calls to one. Checks will then be performed using primitive
    manipulation instructions, which should result in better fast-path
    execution.
    
    The slow path will take a hit, as for throwing the exception, we will
    end up re-boxing the primitive type.
    
    Change-Id: I7e6b0e25790f9a43dc99f2fc7426bb9fe25c505c
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- code-generator/binding-java-api-generator/src/main/java/org/opendaylight/yangtools/sal/java/api/generator/ShortRangeGenerator.java
+++ code-generator/binding-java-api-generator/src/main/java/org/opendaylight/yangtools/sal/java/api/generator/ShortRangeGenerator.java
@@ -9,7 +9,7 @@ package org.opendaylight.yangtools.sal.java.api.generator;
 
 final class ShortRangeGenerator extends AbstractSubIntegerRangeGenerator<Short> {
     ShortRangeGenerator() {
-        super(Short.class, Short.MIN_VALUE, Short.MAX_VALUE, short.class.getName());
+        super(Short.class, short.class.getName(), Short.MIN_VALUE, Short.MAX_VALUE);
     }
 
     @Override
