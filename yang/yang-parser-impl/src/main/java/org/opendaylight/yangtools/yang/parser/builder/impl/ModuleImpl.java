95b648513583f031c43a26c1273838b08e8b1c2f
Robert Varga <rovarga@cisco.com>
2015-05-28 12:21:55 +0200

    Make sure we cache QNameModule instances
    Parser should be taking advantege of cached QNameModules, as multiple
    generations/independent instances within a JVM should still be sharing
    them.
    
    Change-Id: I3c670519056456f8c93591b1e8e24f095131fb9a
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/builder/impl/ModuleImpl.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/builder/impl/ModuleImpl.java
@@ -66,8 +66,8 @@ public final class ModuleImpl extends AbstractDocumentedDataNodeContainer implem
         this.submodules = ImmutableSet.<Module> copyOf(builder.submodules);
         this.prefix = builder.getPrefix();
 
-        this.qnameModule = QNameModule.create(builder.getNamespace(),
-                builder.getRevision() == null ? null : new Date(builder.getRevision().getTime()));
+        this.qnameModule = QNameModule.cachedReference(QNameModule.create(builder.getNamespace(),
+                builder.getRevision() == null ? null : new Date(builder.getRevision().getTime())));
         this.yangVersion = builder.getYangVersion();
         this.organization = builder.getOrganization();
         this.contact = builder.getContact();
