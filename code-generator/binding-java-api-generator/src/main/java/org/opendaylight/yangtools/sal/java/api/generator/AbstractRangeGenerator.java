589e02fd42cf63b305dc35e85f4c839d08eb06d2
Robert Varga <rovarga@cisco.com>
2015-05-28 16:57:20 +0200

    BUG-1485: make AbstractRangeGenerator deal with GTOs
    For enforcement of ranges in builders, we will need to deal with types
    other than ConcreteType.
    
    Change-Id: I8f3ba96c104fb28f6d2cfc4b05b5d24bb7f80aa1
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- code-generator/binding-java-api-generator/src/main/java/org/opendaylight/yangtools/sal/java/api/generator/AbstractRangeGenerator.java
+++ code-generator/binding-java-api-generator/src/main/java/org/opendaylight/yangtools/sal/java/api/generator/AbstractRangeGenerator.java
@@ -14,6 +14,10 @@ import java.util.Collection;
 import java.util.Map;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import org.opendaylight.yangtools.sal.binding.model.api.ConcreteType;
+import org.opendaylight.yangtools.sal.binding.model.api.GeneratedProperty;
+import org.opendaylight.yangtools.sal.binding.model.api.GeneratedTransferObject;
+import org.opendaylight.yangtools.sal.binding.model.api.Type;
 import org.opendaylight.yangtools.yang.model.api.type.RangeConstraint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,8 +47,34 @@ abstract class AbstractRangeGenerator<T extends Number & Comparable<T>> {
         this.type = Preconditions.checkNotNull(typeClass);
     }
 
-    static AbstractRangeGenerator<?> getInstance(final String canonicalName) {
-        return GENERATORS.get(canonicalName);
+    // We need to walk up the
+    private static Type javaTypeForGTO(final GeneratedTransferObject gto) {
+        GeneratedTransferObject rootGto = gto;
+        while (rootGto.getSuperType() != null) {
+            rootGto = rootGto.getSuperType();
+        }
+
+        LOG.debug("Root GTO of {} is {}", rootGto, gto);
+        for (GeneratedProperty s : rootGto.getProperties()) {
+            if ("value".equals(s.getName())) {
+                return s.getReturnType();
+            }
+        }
+
+        throw new IllegalArgumentException(String.format("Failed to resolve GTO {} root {} to a Java type, properties are {}", gto, rootGto));
+    }
+
+    static AbstractRangeGenerator<?> forType(@Nonnull final Type type) {
+        final Type javaType;
+        if (type instanceof GeneratedTransferObject) {
+            javaType = javaTypeForGTO((GeneratedTransferObject) type);
+            LOG.debug("Resolved GTO {} to concrete type {}", type, javaType);
+        } else {
+            javaType = type;
+        }
+
+        Preconditions.checkArgument(javaType instanceof ConcreteType, "Unsupported type %s", type);
+        return GENERATORS.get(type.getFullyQualifiedName());
     }
 
     /**
