6397b5c1773f5ea825e592a0c27259b5e8631c06
Robert Varga <rovarga@cisco.com>
2015-06-02 10:29:20 +0200

    Introduce NormalizedNodes.findDuplicates()
    This is an analytics utility method to find duplicate entries within a
    NormalizedNode subtree.
    
    Change-Id: Iedb3c9f71848c546f6e303b66e3f129b5f4da54f
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/DuplicateEntry.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.api.schema;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.opendaylight.yangtools.concepts.Identifiable;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+
+/**
+ * Entry describing a duplicate found in a {@link NormalizedNode} tree. Note this
+ * class leaks mutable collections, so users are advised either not to share it.
+ */
+public final class DuplicateEntry implements Identifiable<YangInstanceIdentifier> {
+    private final List<YangInstanceIdentifier> hardLinks = new ArrayList<>(1);
+    private List<YangInstanceIdentifier> duplicates = Collections.emptyList();
+
+    DuplicateEntry(final YangInstanceIdentifier path) {
+        hardLinks.add(path);
+    }
+
+    void addDuplicate(final YangInstanceIdentifier path) {
+        if (duplicates.isEmpty()) {
+            duplicates = new ArrayList<>();
+        }
+        duplicates.add(path);
+    }
+
+    void addHardLink(final YangInstanceIdentifier path) {
+        hardLinks.add(path);
+    }
+
+    @Override
+    public YangInstanceIdentifier getIdentifier() {
+        return hardLinks.get(0);
+    }
+
+    public List<YangInstanceIdentifier> getHardLinks() {
+        return hardLinks;
+    }
+
+    public List<YangInstanceIdentifier> getDuplicates() {
+        return duplicates;
+    }
+}
---
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/DuplicateFinder.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.api.schema;
+
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+import java.util.Map;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+
+final class DuplicateFinder {
+    private final Map<NormalizedNode<?, ?>, DuplicateEntry> identities = new IdentityHashMap<>();
+    private final Map<NormalizedNode<?, ?>, DuplicateEntry> duplicates = new HashMap<>();
+
+    private DuplicateFinder() {
+        // Hidden on purpose
+    }
+
+    private void findDuplicates(final YangInstanceIdentifier path, final NormalizedNode<?, ?> node) {
+        final DuplicateEntry i = identities.get(node);
+        if (i == null) {
+            final DuplicateEntry d = duplicates.get(node);
+            if (d == null) {
+                final DuplicateEntry n = new DuplicateEntry(path);
+                identities.put(node, n);
+                duplicates.put(node, n);
+            } else {
+                d.addDuplicate(path);
+            }
+
+            if (node instanceof NormalizedNodeContainer<?, ?, ?>) {
+                final NormalizedNodeContainer<?, ?, ?> container = (NormalizedNodeContainer<?, ?, ?>) node;
+
+                for (NormalizedNode<?, ?> c : container.getValue()) {
+                    findDuplicates(path.node(c.getIdentifier()), c);
+                }
+            }
+        } else {
+            i.addHardLink(path);
+        }
+    }
+
+    /**
+     * Recursively scan a {@link NormalizedNode} instance and its children and
+     * produce a collection of {@link DuplicateEntry} objects. Each holds the
+     * original definition path and a list of hard/softlinks.
+     *
+     * @param node Root node, may not be null.
+     * @return List of entries
+     */
+    static Map<NormalizedNode<?, ?>, DuplicateEntry> findDuplicates(final NormalizedNode<?, ?> node) {
+        final DuplicateFinder finder = new DuplicateFinder();
+        finder.findDuplicates(YangInstanceIdentifier.EMPTY, node);
+        return finder.identities;
+    }
+}
