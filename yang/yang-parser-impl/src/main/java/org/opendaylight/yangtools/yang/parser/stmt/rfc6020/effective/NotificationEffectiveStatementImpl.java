27d9ea28aa698c7a65eaf4d7bad6812051141461
Peter Kajsa <pkajsa@cisco.com>
2015-06-02 09:33:37 +0200

    Bug 2366 - Effective statments impl merge
    - uses getRefine fix
    - load with context retest
    - ordering of statements and substatements fix
    - augment into unknown statement body fix
    - uses or augment in unknown statement body fix
    - extension identifier correctly passed to unknown statement for effective resolution phase
    - base of derived types implementation
    - substatements of included submodules appendation fix
    - node name collision validation
    - refine target validation
    - added statement copy type by both Augment and Uses (when augmenting in uses statement)
    - some of negative scenarios (arguments/statements validation) fix
    - fix of module imports, revision
    - augmentation namespace and revision initialization fix
    - available augmentation impl fix
    - Case shorthand schema path fix
    - Case shorthand fix
    - Unsupported augment targets fix
    - Multiple augments to the same target - reactor fix
    - StackOverflowException on cyclic identity hierarchy fix
    - include vs. belongsTo linkage resolution fix
    - introduces replacement of old YANG parser by new statement parser in
    set of existing tests
    - some test methods adjusted for new parser data structure
    
    - Types & TypeDefs support & resolution implemented
    - BelongsTo namespace added to be able to resolve typedefs from parent
      modules - Identity, IdentityRef support & resolution preparation
    - Minor sonar issues fixed
    - Effective statements related to types implemented
    
    Bug 2366 - Effective statements implementation (2nd patch)
    
    - added further implementation of Module and Submodule Effective statements and
      initialization of submodules collection in Module effective statement
    - implementation of uses-refine
    - implementation of Constraints, etc...
    - effective statement fields initialization
    - added unit tests
    - fix of some issues in the parser (ModuleQName default revision date,
      Generics, etc..)
    
    Bug 2366 - Identity, Refine, ... effective statements impl. (3th patch)
    
    - implementation of Identity effective statement
    - implementation of Refine effective statement
    - initialization of some unitialized effective statments fields
    - augmentation process fix
    - unit tests
    
    Change-Id: I95cda640136c966bd1b1fd3b0bcb1e143675eac1
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>
    Signed-off-by: Gregor Zatko <gzatko@cisco.com>
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>
    Signed-off-by: Gregor Zatko <gzatko@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/NotificationEffectiveStatementImpl.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/NotificationEffectiveStatementImpl.java
@@ -10,7 +10,6 @@ package org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedList;
-
 import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.Utils;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.NotificationStatement;
---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/retest/AugmentToExtensionTest.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ */
+package org.opendaylight.yangtools.yang.stmt.retest;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.opendaylight.yangtools.yang.stmt.test.StmtTestUtils;
+import org.opendaylight.yangtools.yang.parser.spi.meta.SomeModifiersUnresolvedException;
+import java.net.URISyntaxException;
+import java.util.Set;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.model.api.AugmentationSchema;
+import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.UsesNode;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+
+public class AugmentToExtensionTest {
+    private Set<Module> modules;
+
+    @Test(expected = SomeModifiersUnresolvedException.class)
+    public void testIncorrectPath() throws URISyntaxException, SourceException, ReactorException {
+        modules = TestUtils.loadModules(getClass().getResource("/augment-to-extension-test/incorrect-path").toURI());
+
+    }
+
+    @Test
+    public void testCorrectPathIntoUnsupportedTarget() throws URISyntaxException, SourceException, ReactorException {
+
+        try {
+        modules = TestUtils.loadModules(getClass().getResource(
+                "/augment-to-extension-test/correct-path-into-unsupported-target").toURI());
+        } catch (Exception e) {
+            StmtTestUtils.log(e, "     ");
+        }
+
+        Module devicesModule = TestUtils.findModule(modules, "augment-module");
+
+        ContainerSchemaNode devicesContainer = (ContainerSchemaNode) devicesModule.getDataChildByName("my-container");
+        Set<UsesNode> uses = devicesContainer.getUses();
+
+        boolean augmentationIsInContainer = false;
+        for (UsesNode usesNode : uses) {
+            Set<AugmentationSchema> augmentations = usesNode.getAugmentations();
+            for (AugmentationSchema augmentationSchema : augmentations) {
+                augmentationIsInContainer = true;
+            }
+        }
+
+        assertFalse(augmentationIsInContainer);
+    }
+
+    @Test
+    public void testCorrectAugment() throws URISyntaxException, SourceException, ReactorException {
+        modules = TestUtils.loadModules(getClass().getResource("/augment-to-extension-test/correct-augment").toURI());
+
+        Module devicesModule = TestUtils.findModule(modules, "augment-module");
+
+        ContainerSchemaNode devicesContainer = (ContainerSchemaNode) devicesModule.getDataChildByName("my-container");
+        Set<UsesNode> uses = devicesContainer.getUses();
+
+        boolean augmentationIsInContainer = false;
+        for (UsesNode usesNode : uses) {
+            Set<AugmentationSchema> augmentations = usesNode.getAugmentations();
+            for (AugmentationSchema augmentationSchema : augmentations) {
+                augmentationIsInContainer = true;
+            }
+        }
+
+        assertTrue(augmentationIsInContainer);
+    }
+
+}
