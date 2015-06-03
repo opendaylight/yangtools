620a5d7012685caca049b9f5c31ca103abaa6ee3
Gregor Zatko <gzatko@cisco.com>
2015-06-03 08:13:28 +0200

    Bug 2366 - Effective statments impl merge
    - fixed tests dealing with unknown stmts
    - uses getRefine fix
    - load with context retest
    - ordering of statements and substatements fix
    - augment into unknown statement body fix
    - uses or augment in unknown statement body fix
    - extension identifier correctly passed to unknown statement for effective resolution phase
    - base of derived types implementation
    - substatements of included submodules appendation fix
    - node name collision validation
    - refine target validation
    - added statement copy type by both Augment and Uses (when augmenting in uses statement)
    - some of negative scenarios (arguments/statements validation) fix
    - fix of module imports, revision
    - augmentation namespace and revision initialization fix
    - available augmentation impl fix
    - Case shorthand schema path fix
    - Case shorthand fix
    - Unsupported augment targets fix
    - Multiple augments to the same target - reactor fix
    - StackOverflowException on cyclic identity hierarchy fix
    - include vs. belongsTo linkage resolution fix
    - introduces replacement of old YANG parser by new statement parser in
    set of existing tests
    - some test methods adjusted for new parser data structure
    
    - Types & TypeDefs support & resolution implemented
    - BelongsTo namespace added to be able to resolve typedefs from parent
      modules - Identity, IdentityRef support & resolution preparation
    - Minor sonar issues fixed
    - Effective statements related to types implemented
    
    Bug 2366 - Effective statements implementation (2nd patch)
    
    - added further implementation of Module and Submodule Effective statements and
      initialization of submodules collection in Module effective statement
    - implementation of uses-refine
    - implementation of Constraints, etc...
    - effective statement fields initialization
    - added unit tests
    - fix of some issues in the parser (ModuleQName default revision date,
      Generics, etc..)
    
    Bug 2366 - Identity, Refine, ... effective statements impl. (3th patch)
    
    - implementation of Identity effective statement
    - implementation of Refine effective statement
    - initialization of some unitialized effective statments fields
    - augmentation process fix
    - unit tests
    
    Change-Id: I95cda640136c966bd1b1fd3b0bcb1e143675eac1
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>
    Signed-off-by: Gregor Zatko <gzatko@cisco.com>
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/TypeDefEffectiveStatementImpl.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective;
+
+import java.util.Collections;
+import java.util.List;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.api.Status;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.UnknownSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.TypedefStatement;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+import org.opendaylight.yangtools.yang.parser.spi.source.QNameToDerivedType;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.TypeUtils;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.Utils;
+
+public class TypeDefEffectiveStatementImpl extends EffectiveStatementBase<QName, TypedefStatement> implements
+        TypeDefinition {
+
+    private final QName qName;
+    private final SchemaPath path;
+
+    private TypeDefinition<?> baseType;
+
+    private String defaultValue;
+    private String units;
+
+    private String description;
+    private String reference;
+
+    private Status status;
+
+    public TypeDefEffectiveStatementImpl(StmtContext<QName, TypedefStatement, ?> ctx) {
+        super(ctx);
+
+        qName = ctx.getStatementArgument();
+        path = Utils.getSchemaPath(ctx);
+
+        for (final EffectiveStatement<?, ?> effectiveStatement : effectiveSubstatements()) {
+            if (effectiveStatement instanceof DefaultEffectiveStatementImpl) {
+                defaultValue = ((DefaultEffectiveStatementImpl) effectiveStatement).argument();
+            }
+            if (effectiveStatement instanceof UnitsEffectiveStatementImpl) {
+                units = ((UnitsEffectiveStatementImpl) effectiveStatement).argument();
+            }
+            if (effectiveStatement instanceof DescriptionEffectiveStatementImpl) {
+                description = ((DescriptionEffectiveStatementImpl) effectiveStatement).argument();
+            }
+            if (effectiveStatement instanceof ReferenceEffectiveStatementImpl) {
+                reference = ((ReferenceEffectiveStatementImpl) effectiveStatement).argument();
+            }
+            if (effectiveStatement instanceof StatusEffectiveStatementImpl) {
+                status = ((StatusEffectiveStatementImpl) effectiveStatement).argument();
+            }
+        }
+
+        final QName baseTypeQName = firstSubstatementOfType(TypeDefinition.class).getQName();
+        if (TypeUtils.isYangBaseTypeString(baseTypeQName.getLocalName())) {
+            baseType = TypeUtils.getYangBaseTypeFromString(baseTypeQName.getLocalName());
+        } else {
+            baseType = ctx.getFromNamespace(QNameToDerivedType.class, baseTypeQName);
+        }
+    }
+
+    @Override
+    public TypeDefinition<?> getBaseType() {
+        return baseType;
+    }
+
+    @Override
+    public String getUnits() {
+        return units;
+    }
+
+    @Override
+    public Object getDefaultValue() {
+        return defaultValue;
+    }
+
+    @Override
+    public QName getQName() {
+        return qName;
+    }
+
+    @Override
+    public SchemaPath getPath() {
+        return path;
+    }
+
+    @Override
+    public List<UnknownSchemaNode> getUnknownSchemaNodes() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String getReference() {
+        return reference;
+    }
+
+    @Override
+    public Status getStatus() {
+        return status;
+    }
+}
---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/type/base/UnionBaseType.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.type.base;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.YangConstants;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.api.Status;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.UnknownSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.type.LengthConstraint;
+import org.opendaylight.yangtools.yang.model.api.type.PatternConstraint;
+import org.opendaylight.yangtools.yang.model.api.type.RangeConstraint;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.TypeUtils;
+
+public class UnionBaseType implements YangBaseType {
+
+    private static final UnionBaseType INSTANCE = new UnionBaseType();
+
+    private static final QName QNAME = QName.create(YangConstants.RFC6020_YANG_MODULE, TypeUtils.UNION);
+    private static final SchemaPath SCHEMA_PATH = SchemaPath.create(true, QNAME);
+
+    private static final String UNITS = null;
+    private static final Object DEFAULT_VALUE = null;
+
+    private static final String DESCRIPTION = "The union built-in type represents a value that corresponds to one of its member types.";
+    private static final String REFERENCE = "https://tools.ietf.org/html/rfc6020#section-9.12";
+
+    private static final List<RangeConstraint> RANGE_CONSTRAINTS = Collections.emptyList();
+    private static final List<LengthConstraint> LENGTH_CONSTRAINTS = Collections.emptyList();
+    private static final List<PatternConstraint> PATTERN_CONSTRAINTS = Collections.emptyList();
+    private static final List<UnknownSchemaNode> UNKNOWN_SCHEMA_NODES = Collections.emptyList();
+
+    private static final Integer FRACTION_DIGITS = null;
+
+    private static final Status STATUS = Status.CURRENT;
+
+    private UnionBaseType() {
+    }
+
+    public static YangBaseType getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public QName getQName() {
+        return QNAME;
+    }
+
+    @Override
+    public SchemaPath getPath() {
+        return SCHEMA_PATH;
+    }
+
+    @Override
+    public TypeDefinition<?> getBaseType() {
+        return null;
+    }
+
+    @Override
+    public String getUnits() {
+        return UNITS;
+    }
+
+    @Override
+    public Object getDefaultValue() {
+        return DEFAULT_VALUE;
+    }
+
+    @Override
+    public List<UnknownSchemaNode> getUnknownSchemaNodes() {
+        return UNKNOWN_SCHEMA_NODES;
+    }
+
+    @Override
+    public String getDescription() {
+        return DESCRIPTION;
+    }
+
+    @Override
+    public String getReference() {
+        return REFERENCE;
+    }
+
+    @Override
+    public Status getStatus() {
+        return STATUS;
+    }
+
+    @Override
+    public List<RangeConstraint> getRangeConstraints() {
+        return RANGE_CONSTRAINTS;
+    }
+
+    @Override
+    public List<LengthConstraint> getLengthConstraints() {
+        return LENGTH_CONSTRAINTS;
+    }
+
+    @Override
+    public List<PatternConstraint> getPatternConstraints() {
+        return PATTERN_CONSTRAINTS;
+    }
+
+    @Override
+    public Integer getFractionDigits() {
+        return FRACTION_DIGITS;
+    }
+}
