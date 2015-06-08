7ec5cb356ab1b851957d6275db40de51a229f30d
Gregor Zatko <gzatko@cisco.com>
2015-06-08 11:09:22 +0200

    Bug 2366 - Effective statments impl merge
    - clean up of SourceSpecificContext & YangStatementParserListenerImpl classes, rebased on master
    - types, extended types and type definitions impl and bugfix
    - declared types resolution
    - statementSourceReference support implemented
    - decimal64 draft
    - basicType impl proposal
    - typeDefEffStmt impl as builder of ExtendedType
    - few decl. types resolution fixes done
    - identityRefs validation implemented
    - fixed tests dealing with unknown stmts
    - uses getRefine fix
    - load with context retest
    - ordering of statements and substatements fix
    - augment into unknown statement body fix
    - uses or augment in unknown statement body fix
    - extension identifier correctly passed to unknown statement for effective resolution phase
    - substatements of included submodules appendation fix
    - node name collision validation
    - refine target validation
    - added statement copy type by both Augment and Uses (when augmenting in uses statement)
    - some of negative scenarios (arguments/statements validation) fix
    - fix of module imports, revision
    - augmentation namespace and revision initialization fix
    - available augmentation impl fix
    - Case shorthand schema path fix
    - Case shorthand fix
    - Unsupported augment targets fix
    - Multiple augments to the same target - reactor fix
    - StackOverflowException on cyclic identity hierarchy fix
    - include vs. belongsTo linkage resolution fix
    - introduces replacement of old YANG parser by new statement parser in
    set of existing tests
    - some test methods adjusted for new parser data structure
    
    - Types & TypeDefs support & resolution implemented
    - BelongsTo namespace added to be able to resolve typedefs from parent
      modules - Identity, IdentityRef support & resolution preparation
    - Minor sonar issues fixed
    - Effective statements related to types implemented
    
    Bug 2366 - Effective statements implementation (2nd patch)
    
    - added further implementation of Module and Submodule Effective statements and
      initialization of submodules collection in Module effective statement
    - implementation of uses-refine
    - implementation of Constraints, etc...
    - effective statement fields initialization
    - added unit tests
    - fix of some issues in the parser (ModuleQName default revision date,
      Generics, etc..)
    
    Bug 2366 - Identity, Refine, ... effective statements impl. (3th patch)
    
    - implementation of Identity effective statement
    - implementation of Refine effective statement
    - initialization of some unitialized effective statments fields
    - augmentation process fix
    - unit tests
    
    Change-Id: I95cda640136c966bd1b1fd3b0bcb1e143675eac1
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>
    Signed-off-by: Gregor Zatko <gzatko@cisco.com>
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/BelongsToStatementImpl.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/BelongsToStatementImpl.java
@@ -1,21 +1,33 @@
-/**
+/*
  * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
- *
+ * <p/>
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 package org.opendaylight.yangtools.yang.parser.stmt.rfc6020;
 
-import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.BelongsEffectiveToStatementImpl;
-
-import org.opendaylight.yangtools.yang.model.api.stmt.PrefixStatement;
+import com.google.common.base.Optional;
+import java.net.URI;
+import java.util.Collection;
+import org.opendaylight.yangtools.yang.common.SimpleDateFormatUtil;
+import org.opendaylight.yangtools.yang.model.api.ModuleIdentifier;
 import org.opendaylight.yangtools.yang.model.api.Rfc6020Mapping;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.BelongsToStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.PrefixStatement;
+import org.opendaylight.yangtools.yang.parser.builder.impl.ModuleIdentifierImpl;
 import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractDeclaredStatement;
 import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractStatementSupport;
+import org.opendaylight.yangtools.yang.parser.spi.meta.InferenceException;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ModelActionBuilder;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ModelActionBuilder.InferenceAction;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ModelProcessingPhase;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+import org.opendaylight.yangtools.yang.parser.spi.source.BelongsToModuleContext;
+import org.opendaylight.yangtools.yang.parser.spi.source.ModuleNamespaceForBelongsTo;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.BelongsEffectiveToStatementImpl;
 
 public class BelongsToStatementImpl extends AbstractDeclaredStatement<String>
         implements BelongsToStatement {
@@ -50,6 +62,37 @@ public class BelongsToStatementImpl extends AbstractDeclaredStatement<String>
             return new BelongsEffectiveToStatementImpl(ctx);
         }
 
+        @Override
+        public void onLinkageDeclared(final StmtContext.Mutable<String, BelongsToStatement, EffectiveStatement<String, BelongsToStatement>> belongsToCtx) throws SourceException {
+            ModelActionBuilder belongsToAction = belongsToCtx.newInferenceAction(ModelProcessingPhase.SOURCE_LINKAGE);
+
+            final ModuleIdentifier belongsToModuleIdentifier = getModuleIdentifier(belongsToCtx);
+            final ModelActionBuilder.Prerequisite<StmtContext<?, ?, ?>> belongsToPrereq = belongsToAction.requiresCtx(belongsToCtx, ModuleNamespaceForBelongsTo.class, belongsToModuleIdentifier.getName(), ModelProcessingPhase.SOURCE_LINKAGE);
+
+            belongsToAction.apply(new InferenceAction() {
+
+                @Override
+                public void apply() throws InferenceException {
+                    StmtContext<?, ?, ?> belongsToModuleCtx = belongsToPrereq.get();
+
+                    belongsToCtx.addToNs(BelongsToModuleContext.class, belongsToModuleIdentifier,
+                            belongsToModuleCtx);
+                }
+
+                @Override
+                public void prerequisiteFailed(Collection<? extends ModelActionBuilder.Prerequisite<?>> failed) throws InferenceException {
+                    if (failed.contains(belongsToPrereq)) {
+                        throw new InferenceException("Module from belongs-to was not found: " + belongsToCtx.getStatementArgument(), belongsToCtx
+                                .getStatementSourceReference());
+                    }
+                }
+            });
+        }
+
+        private ModuleIdentifier getModuleIdentifier(StmtContext.Mutable<String, BelongsToStatement, EffectiveStatement<String, BelongsToStatement>> belongsToCtx) {
+            String moduleName = belongsToCtx.getStatementArgument();
+            return new ModuleIdentifierImpl(moduleName, Optional.<URI> absent(), Optional.of(SimpleDateFormatUtil.DEFAULT_BELONGS_TO_DATE));
+        }
     }
 
     @Override
