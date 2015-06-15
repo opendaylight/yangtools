0d909b9d0b1b45109bfbf7071958150aae7c25db
Peter Kajsa <pkajsa@cisco.com>
2015-06-15 11:36:29 +0200

    Bug 3669 - ietf-restconf binding is incomplete
    - added implementation of ModuleRevisionBuilder.java
    
    Change-Id: If50041c1b0befcb5fb8678cf95f915ef4b079bfb
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ model/ietf/ietf-restconf/src/main/java/org/opendaylight/yang/gen/v1/urn/ietf/params/xml/ns/yang/ietf/restconf/rev131019/restconf/restconf/modules/ModuleRevisionBuilder.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.restconf.rev131019.restconf.restconf.modules;
+
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.restconf.rev131019.restconf.restconf.modules.Module.Revision;
+
+/**
+ * The purpose of generated class in src/main/java for Union types is to create
+ * new instances of unions from a string representation. In some cases it is
+ * very difficult to automate it since there can be unions such as (uint32 -
+ * uint16), or (string - uint32).
+ *
+ * The reason behind putting it under src/main/java is: This class is generated
+ * in form of a stub and needs to be finished by the user. This class is
+ * generated only once to prevent loss of user code.
+ *
+ */
+public class ModuleRevisionBuilder {
+
+    public static Revision getDefaultInstance(java.lang.String defaultValue) {
+        return RevisionBuilder.getDefaultInstance(defaultValue);
+    }
+
+}
