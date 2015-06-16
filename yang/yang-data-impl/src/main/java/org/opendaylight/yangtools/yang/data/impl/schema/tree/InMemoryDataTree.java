9485ab4b26230cebcf9fcc550e8124b3cb4a7a95
Robert Varga <rovarga@cisco.com>
2015-06-17 01:08:50 +0200

    Allow instantiation of rooted DataTrees
    We are not restricted to instantiation of data trees only at the root,
    but can also bind them to a particular SchemaPath. Accessing such a data
    tree uses relative instance identifiers, thus increasing performance.
    
    Change-Id: I065d224e851c723d775f2b5711f818747592fc74
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/InMemoryDataTree.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/InMemoryDataTree.java
@@ -7,15 +7,25 @@
  */
 package org.opendaylight.yangtools.yang.data.impl.schema.tree;
 
+import com.google.common.base.Function;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Collections2;
+import java.util.Collection;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.AugmentationIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodes;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.TipProducingDataTree;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.TreeType;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.spi.TreeNode;
+import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.SchemaNode;
+import org.opendaylight.yangtools.yang.model.util.SchemaContextUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,15 +36,28 @@ final class InMemoryDataTree extends AbstractDataTreeTip implements TipProducing
     private static final AtomicReferenceFieldUpdater<InMemoryDataTree, DataTreeState> STATE_UPDATER =
             AtomicReferenceFieldUpdater.newUpdater(InMemoryDataTree.class, DataTreeState.class, "state");
     private static final Logger LOG = LoggerFactory.getLogger(InMemoryDataTree.class);
+    private static final Function<PathArgument, QName> EXTRACT_QNAME = new Function<PathArgument, QName>() {
+        @Override
+        public QName apply(final PathArgument input) {
+            if (input instanceof AugmentationIdentifier) {
+                return ((AugmentationIdentifier) input).getPossibleChildNames().iterator().next();
+            } else {
+                return input.getNodeType();
+            }
+        }
+    };
+
+    private final YangInstanceIdentifier rootPath;
+    private final TreeType treeType;
 
     /**
      * Current data store state generation.
      */
     private volatile DataTreeState state;
-    private final TreeType treeType;
 
-    public InMemoryDataTree(final TreeNode rootNode, final TreeType treeType, final SchemaContext schemaContext) {
-        this.treeType = Preconditions.checkNotNull(treeType,treeType);
+    public InMemoryDataTree(final TreeNode rootNode, final TreeType treeType, final YangInstanceIdentifier rootPath, final SchemaContext schemaContext) {
+        this.treeType = Preconditions.checkNotNull(treeType, "treeType");
+        this.rootPath = Preconditions.checkNotNull(rootPath, "rootPath");
         state = DataTreeState.createInitial(rootNode);
         if (schemaContext != null) {
             setSchemaContext(schemaContext);
@@ -51,7 +74,18 @@ final class InMemoryDataTree extends AbstractDataTreeTip implements TipProducing
 
         LOG.debug("Following schema contexts will be attempted {}", newSchemaContext);
 
-        final SchemaAwareApplyOperation operation = SchemaAwareApplyOperation.from(newSchemaContext,treeType);
+        final Collection<QName> schemaPath = Collections2.transform(rootPath.getPathArguments(), EXTRACT_QNAME);
+        final SchemaNode newNode = SchemaContextUtil.findNodeInSchemaContext(newSchemaContext, schemaPath);
+        if (newNode == null) {
+            LOG.debug("Could not find root {} in new schema context, not upgrading", rootPath);
+            return;
+        }
+        if (!(newNode instanceof ContainerSchemaNode)) {
+            LOG.warn("Root {} resolves to non-container type {}, not upgrading", rootPath, newNode);
+            return;
+        }
+
+        final SchemaAwareApplyOperation operation = SchemaAwareApplyOperation.from((ContainerSchemaNode) newNode, treeType);
 
         DataTreeState currentState, newState;
         do {
@@ -93,8 +127,17 @@ final class InMemoryDataTree extends AbstractDataTreeTip implements TipProducing
     }
 
     @Override
+    public YangInstanceIdentifier getRootPath() {
+        return rootPath;
+    }
+
+    @Override
     public String toString() {
-        return MoreObjects.toStringHelper(this).add("object", super.toString()).add("state", state).toString();
+        return MoreObjects.toStringHelper(this).
+                add("object", super.toString()).
+                add("rootPath", rootPath).
+                add("state", state).
+                toString();
     }
 
     @Override
