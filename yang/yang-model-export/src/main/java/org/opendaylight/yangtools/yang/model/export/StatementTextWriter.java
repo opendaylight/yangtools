67be783913ccc76c21b9a691d573010678fc398e
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-08-05 23:43:21 -0400

    Fix license header violations in yang-model-export
    Change-Id: I14acb8a5c0adb44046346683938bb0d2464067ab
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- yang/yang-model-export/src/main/java/org/opendaylight/yangtools/yang/model/export/StatementTextWriter.java
+++ yang/yang-model-export/src/main/java/org/opendaylight/yangtools/yang/model/export/StatementTextWriter.java
@@ -4,7 +4,9 @@
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
- */package org.opendaylight.yangtools.yang.model.export;
+ */
+
+package org.opendaylight.yangtools.yang.model.export;
 
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.model.api.RevisionAwareXPath;
