d601adb1fca809e80454c97a38f25a4cbc4fb513
Peter Kajsa <pkajsa@cisco.com>
2015-08-20 12:51:25 +0200

    Bug 3670 (part 3/5): Use of new statement parser in yang-maven-plugin (original header: Yangtools changes-fixes necessary for parser switch in Controller)
    Parser fixes:
    - bug4136 fix
    - enhanced javadoc comments
    - mapped effective build implementation and test
    - Module source and sourcePath implementation and test
    - EffectiveSchemaContext IdentifierToSources implementation and test
    - EffectiveSchemaContext resolveSchemaContext from modules implementation
    - Bug3799 test
    - Antlr4 identifier vs. string fix
    sal-dom-broker fix
    - QName parsing for unknown nodes
    - QName prefix parsing for unknown nodes from augmentation
    - uses nodes reusing
    - uses nodes reusing skip on top level
    - uses nodes dont copy description and reference of grouping to target node (TODO? add other not to copy)
    
    Change-Id: Iab104439f9b03977850c4d05b9ab933382919a09
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/spi/source/DeclarationInTextSource.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/spi/source/DeclarationInTextSource.java
@@ -20,12 +20,12 @@ import org.opendaylight.yangtools.yang.model.api.meta.StatementSource;
  *
  *  To create source reference use one of this static factories:
  *  <ul>
- *  <li>{@link #atPosition(String, int, int)} - provides most specific reference of statement location,
+ *  <li>{@link #atPosition(String, String, int, int)} - provides most specific reference of statement location,
  *  this is most prefered since it provides most context to debug YANG model.
  *  </li>
- *  <li>{@link #atLine(String, int)}- provides source and line of statement location.
+ *  <li>{@link #atLine(String, String, int)}- provides source and line of statement location.
  *  </li>
- *  <li>{@link #inSource(String)} - least specific reference, should be used only if any of previous
+ *  <li>{@link #inSource(String, String)} - least specific reference, should be used only if any of previous
  *  references are unable to create / derive from source.
  *  </li>
  *  </ul>
@@ -33,14 +33,20 @@ import org.opendaylight.yangtools.yang.model.api.meta.StatementSource;
  */
 public abstract class DeclarationInTextSource implements StatementSourceReference {
 
-    private final String source;
+    private final String sourceText;
+    private final String sourceName;
 
-    DeclarationInTextSource(String source) {
-        this.source = source;
+    DeclarationInTextSource(String sourceName, String sourceText) {
+        this.sourceName = sourceName;
+        this.sourceText = sourceText;
     }
 
     public String getSourceName() {
-        return source;
+        return sourceName;
+    }
+
+    public String getSourceText() {
+        return sourceText;
     }
 
     @Override
@@ -51,22 +57,22 @@ public abstract class DeclarationInTextSource implements StatementSourceReferenc
     @Override
     public abstract String toString();
 
-    public static final DeclarationInTextSource inSource(String sourceName) {
-        return new InSource(sourceName);
+    public static final DeclarationInTextSource inSource(String sourceName, String sourceText) {
+        return new InSource(sourceName, sourceText);
     }
 
-    public static final DeclarationInTextSource atLine(String sourceName, int line) {
-        return new AtLine(sourceName,line);
+    public static final DeclarationInTextSource atLine(String sourceName, String sourceText, int line) {
+        return new AtLine(sourceName, sourceText, line);
     }
 
-    public static final DeclarationInTextSource atPosition(String sourceName, int line, int position) {
-        return new AtPosition(sourceName,line,position);
+    public static final DeclarationInTextSource atPosition(String sourceName, String sourceText, int line, int position) {
+        return new AtPosition(sourceName, sourceText, line,position);
     }
 
     private static class InSource extends DeclarationInTextSource {
 
-        public InSource(String source) {
-            super(source);
+        public InSource(String sourceName, String sourceText) {
+            super(sourceName, sourceText);
         }
 
         @Override
@@ -80,8 +86,8 @@ public abstract class DeclarationInTextSource implements StatementSourceReferenc
 
         private final int line;
 
-        public AtLine(String source,int line) {
-            super(source);
+        public AtLine(String sourceName, String sourceText, int line) {
+            super(sourceName, sourceText);
             this.line = line;
         }
 
@@ -97,8 +103,8 @@ public abstract class DeclarationInTextSource implements StatementSourceReferenc
         private int line;
         private int character;
 
-        public AtPosition(String source, int line, int character) {
-            super(source);
+        public AtPosition(String sourceName, String sourceText, int line, int character) {
+            super(sourceName, sourceText);
             this.line = line;
             this.character = character;
         }
