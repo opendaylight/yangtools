676e705125dba7120c0a3e905fd36bcac915884a
Robert Varga <rovarga@cisco.com>
2015-08-25 11:06:56 +0200

    BUG-4158: use ImmutableOffsetMap
    In order to improve memory efficiency of maps holding children of
    container nodes, use ImmutableOffsetMap instead of wrapping constructed
    children.
    
    Change-Id: Ib11b8d5adc4d5509ecbbba59348638f78fc2e62b
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/nodes/AbstractImmutableDataContainerNode.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/nodes/AbstractImmutableDataContainerNode.java
@@ -11,6 +11,7 @@ import com.google.common.base.Optional;
 import java.util.Collection;
 import java.util.Map;
 import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.util.ImmutableOffsetMap;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
 import org.opendaylight.yangtools.yang.data.api.schema.DataContainerNode;
@@ -22,7 +23,7 @@ public abstract class AbstractImmutableDataContainerNode<K extends PathArgument>
             final Map<PathArgument, DataContainerChild<? extends PathArgument, ?>> children, final K nodeIdentifier) {
         super(nodeIdentifier);
 
-        this.children = UnmodifiableChildrenMap.create(children);
+        this.children = ImmutableOffsetMap.copyOf(children);
     }
 
     @Override
