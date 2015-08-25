9c1b0747a9441c7fb0c5d601d98b3a4ecd859c12
Robert Varga <rovarga@cisco.com>
2015-08-25 10:18:13 +0200

    BUG-4158: Introduce Modifiable/UnmodifiableMapPhase
    Our code frequently uses an access pattern, where we create a modifiable
    map, then turn it into an unmodifiable one and then we want to create a
    modifiable map containing all the mappings in the unmodifiable one (and
    then turn it again into unodifiable).
    
    This patch introduces interfaces which mark the modifiable/unmodifiable
    phases of such mapping. They also define the interface contract
    implementation must follow to be usable with this usabe pattern.
    
    Change-Id: I08b3077739a3df89e57c54a6937744f2394701bc
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ common/util/src/main/java/org/opendaylight/yangtools/util/ModifiableMapPhase.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.util;
+
+import com.google.common.annotations.Beta;
+import com.google.common.collect.ImmutableMap;
+import java.util.Map;
+import javax.annotation.Nonnull;
+
+/**
+ * A {@link Map} which can be modified and supports efficient conversion to an unmodifiable map. This interface is the
+ * logical counterpart to {@link UnmodifiableMapPhase}, but it does not require implementations of {@link #toUnmodifiableMap()}
+ * to return an implementation of that interface. The reason for that empty and singleton mappings are efficiently
+ * represented as {@link ImmutableMap}, which does not implement {@link UnmodifiableMapPhase}.
+ *
+ * @param <K> the type of keys maintained by this map
+ * @param <V> the type of mapped values
+ */
+@Beta
+public interface ModifiableMapPhase<K, V> extends Map<K, V> {
+    /**
+     * Return an isolated unmodifiable version of this map. Returned object must not allow removal, addition or changing
+     * of mappings. Its mappings must match the mappings currently present in this map, but must not be affected by any
+     * subsequent changes to this map.
+     *
+     * @return An unmodifiable version of this map.
+     */
+    @Nonnull Map<K, V> toUnmodifiableMap();
+}
---
+++ common/util/src/main/java/org/opendaylight/yangtools/util/UnmodifiableMapPhase.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.util;
+
+import com.google.common.annotations.Beta;
+import java.util.Map;
+import javax.annotation.Nonnull;
+
+/**
+ * A {@link Map} which cannot be modified and supports efficient conversion to a {@link ModifiableMapPhase}.
+ *
+ * @param <K> the type of keys maintained by this map
+ * @param <V> the type of mapped values
+ */
+@Beta
+public interface UnmodifiableMapPhase<K, V> extends Map<K, V> {
+    /**
+     * Return an isolated modifiable version of this map. Its mappings must match the mappings present in this map. Any
+     * modification of the returned map must not be affect the contents of this map.
+     *
+     * @return An modifiable version of this map.
+     */
+    @Nonnull ModifiableMapPhase<K, V> toModifiableMap();
+}
