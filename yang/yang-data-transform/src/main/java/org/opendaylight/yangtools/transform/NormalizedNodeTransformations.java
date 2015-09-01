de12adb2247271097ff4e739b5d09f1e61a56b20
Tony Tkacik <ttkacik@cisco.com>
2015-09-01 16:06:54 +0200

    Bug 3875: Introduced transforming stream writer
    Introduced simple QName transforming stream writer
    which could be used to re-stream and rebuild
    YANG Data with replacement of QNames.
    
    QNameTransformingStreamWriter currently supports
    custom mapping, replacement of specified QNameModules
    and replacement of specified QNames
    
    Change-Id: I98b253a0f184e1996d78b99fa8cfb20a7d588b96
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ yang/yang-data-transform/src/main/java/org/opendaylight/yangtools/transform/NormalizedNodeTransformations.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.transform;
+
+import com.google.common.base.Function;
+import com.google.common.base.Throwables;
+import java.io.IOException;
+import java.util.Map;
+import javax.annotation.Nonnull;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeWriter;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.impl.schema.NormalizedNodeResult;
+
+public class NormalizedNodeTransformations {
+
+    private NormalizedNodeTransformations() {
+        throw new UnsupportedOperationException("Utility class.");
+    }
+
+    public static NormalizedNode<?, ?> transformQNames(NormalizedNode<?, ?> original, Function<QName, QName> mapping) {
+        NormalizedNodeResult result = new NormalizedNodeResult();
+        NormalizedNodeStreamWriter nodeWriter = ImmutableNormalizedNodeStreamWriter.from(result);
+        NormalizedNodeStreamWriter transformWriter = QNameTransformingStreamWriter.fromFunction(nodeWriter, mapping);
+        try {
+            NormalizedNodeWriter.forStreamWriter(transformWriter).write(original);
+            return result.getResult();
+        } catch (IOException e) {
+            throw Throwables.propagate(e);
+        }
+    }
+
+    /**
+     *
+     * Returns a {@link NormalizedNode} with QNames replaced by supplied mapping.
+     *
+     * @param original Original Normalized Node
+     * @param mapping Map of QNames to transform. Not listed QNames are preserved.
+     * @return Normalized Node with replaced QNames.
+     */
+    public static NormalizedNode<?, ?> replaceQNames(@Nonnull NormalizedNode<?, ?> original,
+            @Nonnull Map<QName, QName> mapping) {
+        return transformQNames(original, new QNameReplacementFunction(mapping));
+    }
+
+    /**
+     *
+     * Returns a {@link NormalizedNode} with QNameModules replaced by supplied mapping.
+     *
+     * @param original Original Normalized Node
+     * @param mapping Map of QNameModules to transform. Not listed QNameModules are preserved.
+     * @return Normalized Node with replaced QNameModules.
+     */
+
+    public static NormalizedNode<?, ?> replaceQNameModules(@Nonnull NormalizedNode<?, ?> original,
+            @Nonnull Map<QNameModule, QNameModule> mapping) {
+        return transformQNames(original, new QNameModuleReplacementFunction(mapping));
+    }
+
+}
---
+++ yang/yang-data-transform/src/main/java/org/opendaylight/yangtools/transform/QNameTransformingStreamWriter.java
@@ -0,0 +1,195 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.transform;
+
+import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableSet;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Nonnull;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.AugmentationIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
+
+/**
+ *
+ * Stateless Normalized Node Stream Writer decorator, which performs QName translation.
+ *
+ * This class serves as base for Normalized Node Stream Writer decorators with option to transform
+ * QNames by user-implemented {@link #transform(QName)} function.
+ *
+ */
+public abstract class QNameTransformingStreamWriter implements NormalizedNodeStreamWriter {
+
+    // FIXME: Probably use loading cache to decrease memory
+    private final NormalizedNodeStreamWriter delegate;
+
+    protected QNameTransformingStreamWriter(NormalizedNodeStreamWriter delegate) {
+        this.delegate = Preconditions.checkNotNull(delegate);
+    }
+
+    /**
+     * Returns decorator, which uses supplied function to transform QNames.
+     *
+     * @param delegate Underlying normalized node stream writer
+     * @param transformation Transformation function, function is required to return non-null
+     *        values.
+     * @return decorator, which uses supplied function to transform QNames.
+     */
+    public static NormalizedNodeStreamWriter fromFunction(NormalizedNodeStreamWriter delegate,
+            final Function<QName, QName> transformation) {
+        return new QNameTransformingStreamWriter(delegate) {
+
+            @Override
+            protected QName transform(QName key) {
+                return transformation.apply(key);
+            }
+
+        };
+    }
+
+    /**
+     * Returns decorator, which uses supplied map to transform QNames.
+     *
+     * QNames not present in map are left unchanged.
+     *
+     * @param delegate Underlying normalized node stream writer
+     * @param mapping Immutable map which represent mapping from original to new values.
+     * @return decorator, which uses supplied mapping to transform QNames.
+     */
+    public static NormalizedNodeStreamWriter createQNameReplacing(NormalizedNodeStreamWriter delegate,
+            final Map<QName, QName> mapping) {
+        return fromFunction(delegate, new QNameReplacementFunction(mapping));
+    }
+
+    /**
+     * Returns decorator, which uses supplied map to transform QNameModules.
+     *
+     * QNameModules not present in map are left unchanged.
+     *
+     * @param delegate Underlying normalized node stream writer
+     * @param mapping Immutable map which represent mapping from original to new values.
+     * @return decorator, which uses supplied mapping to transform QNameModules.
+     */
+    public static NormalizedNodeStreamWriter createQNameModuleReplacing(NormalizedNodeStreamWriter delegate,
+            final Map<QNameModule, QNameModule> mapping) {
+        return fromFunction(delegate, new QNameModuleReplacementFunction(mapping));
+    }
+
+    @Override
+    public void leafNode(NodeIdentifier name, Object value) throws IOException, IllegalArgumentException {
+        delegate.leafNode(transform(name), value);
+    }
+
+    /**
+     * Transforms a QName to new mapping.
+     *
+     * NOTE: If QName should be unchanged implemention needs to return original QName.
+     *
+     * @param key QName to transform.
+     * @return Returns new value of QName.
+     */
+    protected abstract @Nonnull QName transform(@Nonnull QName key);
+
+    @Override
+    public void startLeafSet(NodeIdentifier name, int childSizeHint) throws IOException, IllegalArgumentException {
+        delegate.startLeafSet(transform(name), childSizeHint);
+    }
+
+    @Override
+    public void leafSetEntryNode(Object value) throws IOException, IllegalArgumentException {
+        delegate.leafSetEntryNode(value);
+    }
+
+    @Override
+    public void startContainerNode(NodeIdentifier name, int childSizeHint) throws IOException, IllegalArgumentException {
+        delegate.startContainerNode(transform(name), childSizeHint);
+    }
+
+    @Override
+    public void startUnkeyedList(NodeIdentifier name, int childSizeHint) throws IOException, IllegalArgumentException {
+        delegate.startUnkeyedList(transform(name), childSizeHint);
+    }
+
+    @Override
+    public void startUnkeyedListItem(NodeIdentifier name, int childSizeHint) throws IOException, IllegalStateException {
+        delegate.startUnkeyedListItem(transform(name), childSizeHint);
+    }
+
+    @Override
+    public void startMapNode(NodeIdentifier name, int childSizeHint) throws IOException, IllegalArgumentException {
+        delegate.startMapNode(transform(name), childSizeHint);
+    }
+
+    @Override
+    public void startMapEntryNode(NodeIdentifierWithPredicates identifier, int childSizeHint) throws IOException,
+            IllegalArgumentException {
+        delegate.startMapEntryNode(transform(identifier), childSizeHint);
+    }
+
+    @Override
+    public void startOrderedMapNode(NodeIdentifier name, int childSizeHint) throws IOException,
+            IllegalArgumentException {
+        delegate.startOrderedMapNode(transform(name), childSizeHint);
+    }
+
+    @Override
+    public void startChoiceNode(NodeIdentifier name, int childSizeHint) throws IOException, IllegalArgumentException {
+        delegate.startChoiceNode(transform(name), childSizeHint);
+    }
+
+    @Override
+    public void startAugmentationNode(AugmentationIdentifier identifier) throws IOException, IllegalArgumentException {
+        delegate.startAugmentationNode(transform(identifier));
+    }
+
+    @Override
+    public void anyxmlNode(NodeIdentifier name, Object value) throws IOException, IllegalArgumentException {
+        delegate.anyxmlNode(transform(name), value);
+    }
+
+    @Override
+    public void endNode() throws IOException, IllegalStateException {
+        delegate.endNode();
+    }
+
+    @Override
+    public void close() throws IOException {
+        delegate.close();
+    }
+
+    @Override
+    public void flush() throws IOException {
+        delegate.flush();
+    }
+
+    private NodeIdentifier transform(NodeIdentifier name) {
+        return new NodeIdentifier(transform(name.getNodeType()));
+    }
+
+    private AugmentationIdentifier transform(AugmentationIdentifier identifier) {
+        ImmutableSet.Builder<QName> builder = ImmutableSet.builder();
+        for (QName original : identifier.getPossibleChildNames()) {
+            builder.add(transform(original));
+        }
+        return new AugmentationIdentifier(builder.build());
+    }
+
+    private NodeIdentifierWithPredicates transform(NodeIdentifierWithPredicates identifier) {
+        Map<QName, Object> keyValues = new HashMap<>();
+        for (Map.Entry<QName, Object> original : identifier.getKeyValues().entrySet()) {
+            keyValues.put(transform(original.getKey()), original.getValue());
+        }
+        return new NodeIdentifierWithPredicates(transform(identifier.getNodeType()), keyValues);
+    }
+}
