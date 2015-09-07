f5356d239cd236624aa3cc6e9ea7819d18084540
Peter Kajsa <pkajsa@cisco.com>
2015-09-07 17:12:35 +0200

    Bug 3799: NPE when including a submodule of a more recent revision than the module
    Change-Id: Ieaff77303879d9f9eee4f4ae4840a517ada66a91
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/parser/impl/Bug3799Test.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+import java.util.Set;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.NotificationDefinition;
+
+public class Bug3799Test {
+
+    @Test
+    public void test() throws Exception {
+
+        Set<Module> modules = TestUtils.loadModules(getClass().getResource(
+                "/bugs/bug3799").toURI());
+        assertNotNull(modules);
+        assertEquals(1, modules.size());
+
+        Module testModule = modules.iterator().next();
+        Set<Module> subModules = testModule.getSubmodules();
+        assertNotNull(subModules);
+        assertEquals(1, subModules.size());
+
+        Module testSubmodule = subModules.iterator().next();
+
+        Set<NotificationDefinition> notifications = testSubmodule
+                .getNotifications();
+        assertNotNull(notifications);
+        assertEquals(1, notifications.size());
+
+        NotificationDefinition bazNotification = notifications.iterator()
+                .next();
+        Collection<DataSchemaNode> childNodes = bazNotification.getChildNodes();
+        assertNotNull(childNodes);
+        assertEquals(1, childNodes.size());
+
+        DataSchemaNode child = childNodes.iterator().next();
+        assertTrue(child instanceof LeafSchemaNode);
+
+        LeafSchemaNode leafBar = (LeafSchemaNode) child;
+        String bar = leafBar.getQName().getLocalName();
+        assertEquals("bar", bar);
+    }
+
+}
