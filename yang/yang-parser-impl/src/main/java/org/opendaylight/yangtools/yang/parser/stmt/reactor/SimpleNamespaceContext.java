5aa289d8ed3fd8ced4a86a9413edf403eaf9f5a6
Tony Tkacik <ttkacik@cisco.com>
2015-10-01 19:23:11 +0200

    Updated SchemaNodeIdentifier namespace handling.
    Introduced notion of derived namespaces and SchemaNodeIdentifier
    namespace for lookups of refine / augment and other statements
    
    - Fixed AugmentUtils and AugmentStatementImpl, UsesStatementImpl
      to use declarative way to get needed artefacts instead of
      manual search in statement context.
    - Removed utility methods, which are not needed anymore.
    
    Change-Id: Ifb9c6dffa948cf265f81f2d8cd8615318b1447d0
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/SimpleNamespaceContext.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.stmt.reactor;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.opendaylight.yangtools.yang.model.api.meta.IdentifierNamespace;
+import org.opendaylight.yangtools.yang.parser.spi.meta.NamespaceBehaviour;
+
+final class SimpleNamespaceContext<K, V, N extends IdentifierNamespace<K, V>>
+        extends NamespaceBehaviourWithListeners<K, V, N> {
+
+    // FIXME: Change this to Multimap, once issue with modules
+    // is resolved.
+    private final List<NamespaceBehaviourWithListeners.ValueAddedListener<K>> listeners = new ArrayList<>();
+    public SimpleNamespaceContext(NamespaceBehaviour<K, V, N> delegate) {
+        super(delegate);
+    }
+
+    protected boolean isRequestedValue(NamespaceBehaviourWithListeners.ValueAddedListener<K> listener, NamespaceStorageNode storage, V value) {
+        NamespaceStorageNode listenerCtx = listener.getCtxNode();
+        return value == getFrom(listenerCtx, listener.getKey());
+    }
+
+    @Override
+    protected void addListener(K key, NamespaceBehaviourWithListeners.ValueAddedListener<K> listener) {
+        listeners.add(listener);
+    }
+
+    @Override
+    protected Iterator<NamespaceBehaviourWithListeners.ValueAddedListener<K>> getMutableListeners(K key) {
+        return listeners.iterator();
+    }
+}
