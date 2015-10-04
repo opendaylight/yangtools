8f7c79722251314ad48e61b9b6f6ed3be5c447bd
Robert Varga <rovarga@cisco.com>
2015-10-04 23:19:33 +0200

    Create utility EnumPairBuilder
    EnumPair is a DTO, it would be nice to have a builder.
    
    Change-Id: Ib2c2fb69196fff1ae255c48737c85f756d083994
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/type/EnumTypeDefinition.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/type/EnumTypeDefinition.java
@@ -8,21 +8,20 @@
 package org.opendaylight.yangtools.yang.model.api.type;
 
 import java.util.List;
-
 import org.opendaylight.yangtools.yang.model.api.SchemaNode;
 import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
 
 /**
- * 
+ *
  * Makes is possible to access to the individual enumeration values of this
  * type.
- * 
+ *
  */
 public interface EnumTypeDefinition extends TypeDefinition<EnumTypeDefinition> {
 
     /**
      * Returns all enumeration values.
-     * 
+     *
      * @return list of <code>EnumPair</code> type instastances which contain the
      *         data about all individual enumeration pairs of
      *         <code>enumeration</code> YANG built-in type
@@ -30,15 +29,14 @@ public interface EnumTypeDefinition extends TypeDefinition<EnumTypeDefinition> {
     List<EnumPair> getValues();
 
     /**
-     * 
+     *
      * Contains the methods for accessing the data about the concrete
      * enumeration item which represents <code>enum</code> YANG type.
      */
     interface EnumPair extends SchemaNode {
-
         /**
          * The name to specify each assigned name of an enumeration type.
-         * 
+         *
          * @return name of each assigned name of an enumeration type.
          */
         String getName();
@@ -48,7 +46,7 @@ public interface EnumTypeDefinition extends TypeDefinition<EnumTypeDefinition> {
          * integer value with the assigned name for the enum. This integer value
          * MUST be in the range -2147483648 to 2147483647, and it MUST be unique
          * within the enumeration type.
-         * 
+         *
          * @return integer value assigned to enumeration
          */
         Integer getValue();
