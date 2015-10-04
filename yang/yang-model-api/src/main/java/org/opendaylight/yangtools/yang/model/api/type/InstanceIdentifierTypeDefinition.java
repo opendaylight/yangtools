4796d51102a4081851c5be50db99bbcb31154322
Robert Varga <rovarga@cisco.com>
2015-10-04 07:14:55 +0200

    Deprecate InstanceIdentifierTypeDefinition.getRevisionAwareXPath()
    This method has been added due to mis-reading of RFC6020. YANG actually
    does not define this property, but rather specifies how
    instance-identifier types are to be interpreted.
    
    Change-Id: I484b655475f2817277b62dae5fc7462442ebf874
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/type/InstanceIdentifierTypeDefinition.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/type/InstanceIdentifierTypeDefinition.java
@@ -11,27 +11,26 @@ import org.opendaylight.yangtools.yang.model.api.RevisionAwareXPath;
 import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
 
 /**
- * 
  * Contains methods for getting data from the <code>instance-identifier</code>
  * YANG built-in type.
  */
 public interface InstanceIdentifierTypeDefinition extends TypeDefinition<InstanceIdentifierTypeDefinition> {
 
     /**
-     * Returns XPath for instance of
-     * <code>InstanceIdentifierTypeDefinition</code>.
-     * 
+     * Returns XPath for instance of <code>InstanceIdentifierTypeDefinition</code>.
+     *
      * @return instance of type <code>RevisionAwareXPath</code>
+     * @deprecated This method is deprecated because it is not defined in YANG metamodel. It is scheduled for removal
+     *             in Boron release cycle.
      */
+    @Deprecated
     RevisionAwareXPath getPathStatement();
 
     /**
-     * Returns true|false which represents argument of
-     * <code>require-instance</code> statement. This statement is the
+     * Returns true|false which represents argument of <code>require-instance</code> statement. This statement is the
      * substatement of the <code>type</code> statement.
-     * 
-     * @return boolean value which is true if the <code>require-instance</code>
-     *         statement is true and vice versa
+     *
+     * @return boolean value which is true if the <code>require-instance</code> statement is true and vice versa
      */
     boolean requireInstance();
 }
