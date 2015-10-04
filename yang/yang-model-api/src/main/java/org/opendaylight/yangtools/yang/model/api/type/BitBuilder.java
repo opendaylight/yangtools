bf87fff2a8e61b6e8a2e377b614723c3ad630209
Robert Varga <rovarga@cisco.com>
2015-10-04 23:19:33 +0200

    Create utility EnumPairBuilder
    EnumPair is a DTO, it would be nice to have a builder.
    
    Change-Id: Ib2c2fb69196fff1ae255c48737c85f756d083994
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/type/BitBuilder.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.api.type;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+import java.util.List;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.api.Status;
+import org.opendaylight.yangtools.yang.model.api.UnknownSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.type.BitsTypeDefinition.Bit;
+
+@Beta
+public final class BitBuilder extends AbstractSchemaNodeBuilder<Bit> {
+    private Long position;
+
+    public BitBuilder setPosition(final Long position) {
+        Preconditions.checkArgument(position >= 0 && position <= 4294967295L,
+                "Position %s is not in valid range [0..4294967295]", position);
+
+        this.position = Preconditions.checkNotNull(position);
+        return this;
+    }
+
+    @Override
+    protected Bit buildNode(final SchemaPath path, final Status status, final String description, final String reference,
+            final List<UnknownSchemaNode> unknownSchemaNodes) {
+        Preconditions.checkState(position != null, "Position has not been set");
+        return new BitImpl(path, description, reference, status, unknownSchemaNodes, position);
+    }
+}
---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/type/BitImpl.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.api.type;
+
+import com.google.common.base.Preconditions;
+import java.util.List;
+import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.yang.model.api.AbstractSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.api.Status;
+import org.opendaylight.yangtools.yang.model.api.UnknownSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.type.BitsTypeDefinition.Bit;
+
+final class BitImpl extends AbstractSchemaNode implements Bit, Immutable {
+    private final Long position;
+
+    protected BitImpl(final SchemaPath path, final String description, final String reference, final Status status,
+            final List<UnknownSchemaNode> unknownNodes, final Long position) {
+        super(path, description, reference, status, unknownNodes);
+        this.position = Preconditions.checkNotNull(position);
+    }
+
+    @Override
+    public Long getPosition() {
+        return position;
+    }
+
+    @Override
+    public String getName() {
+        return getQName().getLocalName();
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + position.hashCode();
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (!super.equals(obj)) {
+            return false;
+        }
+        BitImpl other = (BitImpl) obj;
+        return position.equals(other.position);
+    }
+
+    @Override
+    public String toString() {
+        return BitImpl.class.getSimpleName() + "[name=" + getName() + ", position="
+                + position + "]";
+    }
+}
