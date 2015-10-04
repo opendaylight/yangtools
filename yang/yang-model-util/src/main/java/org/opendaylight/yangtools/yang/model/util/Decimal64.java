e20ed3ad5e5ef3303048da9e21322f92ee79eb93
Robert Varga <rovarga@cisco.com>
2015-10-04 04:32:00 +0200

    BUG-4407: Fix Decimal64 range enforcement
    Baseline ranges did not take into account the number of fraction digits,
    but rather instantiated the most permissive range. Fix this by
    pre-calculating all ranges specified in RFC6020 Section 9.3.4.
    
    Also enforce SchemaPath presence, as that is the unique identifier of
    the type.
    
    Change-Id: I5e8799da618398ba458606226e71e434f5214f33
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-model-util/src/main/java/org/opendaylight/yangtools/yang/model/util/Decimal64.java
+++ yang/yang-model-util/src/main/java/org/opendaylight/yangtools/yang/model/util/Decimal64.java
@@ -8,11 +8,12 @@
 package org.opendaylight.yangtools.yang.model.util;
 
 import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableList.Builder;
 import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.model.api.SchemaPath;
 import org.opendaylight.yangtools.yang.model.api.Status;
@@ -23,65 +24,70 @@ import org.opendaylight.yangtools.yang.model.api.type.RangeConstraint;
 /**
  * The <code>default</code> implementation of Decimal Type Definition interface.
  *
- *
  * @see DecimalTypeDefinition
  */
 public final class Decimal64 implements DecimalTypeDefinition {
-    private static final String UNITS = "";
-    private static final BigDecimal DEFAULT_VALUE = null;
-
     private static final String DESCRIPTION = "The decimal64 type represents a subset of the real numbers, which can "
             + "be represented by decimal numerals. The value space of decimal64 is the set of numbers that can "
             + "be obtained by multiplying a 64-bit signed integer by a negative power of ten, i.e., expressible as "
             + "'i x 10^-n' where i is an integer64 and n is an integer between 1 and 18, inclusively.";
-
     private static final String REFERENCE = "https://tools.ietf.org/html/rfc6020#section-9.3";
-    private static final int MAX_NUMBER_OF_FRACTION_DIGITS = 18;
-
-    /**
-     * Immutable List with default definition of Range Statements.
-     */
-    private static final List<RangeConstraint> DEFAULT_RANGE_STATEMENTS;
+    private static final List<List<RangeConstraint>> IMPLICIT_RANGE_STATEMENTS;
     static {
-        final BigDecimal min = new BigDecimal("-922337203685477580.8");
-        final BigDecimal max = new BigDecimal("922337203685477580.7");
-        final String rangeDescription = "Integer values between " + min + " and " + max + ", inclusively.";
+        final Builder<List<RangeConstraint>> b = ImmutableList.builder();
+        b.add(createRangeConstraint("-922337203685477580.8", "922337203685477580.7"));
+        b.add(createRangeConstraint("-92233720368547758.08", "92233720368547758.07"));
+        b.add(createRangeConstraint("-9223372036854775.808", "9223372036854775.807"));
+        b.add(createRangeConstraint("-922337203685477.5808", "922337203685477.5807"));
+        b.add(createRangeConstraint("-92233720368547.75808", "92233720368547.75807"));
+        b.add(createRangeConstraint("-9223372036854.775808", "9223372036854.775807"));
+        b.add(createRangeConstraint("-922337203685.4775808", "922337203685.4775807"));
+        b.add(createRangeConstraint("-92233720368.54775808", "92233720368.54775807"));
+        b.add(createRangeConstraint("-9223372036.854775808", "9223372036.854775807"));
+        b.add(createRangeConstraint("-922337203.6854775808", "922337203.6854775807"));
+        b.add(createRangeConstraint("-9223372.036854775808", "92233720.36854775807"));
+        b.add(createRangeConstraint("-922337.2036854775808", "9223372.036854775807"));
+        b.add(createRangeConstraint("-92233.72036854775808", "922337.2036854775807"));
+        b.add(createRangeConstraint("-9223.372036854775808", "9223.372036854775807"));
+        b.add(createRangeConstraint("-922.3372036854775808", "922.3372036854775807"));
+        b.add(createRangeConstraint("-92.23372036854775808", "92.23372036854775807"));
+        b.add(createRangeConstraint("-9.223372036854775808", "9.223372036854775807"));
+        IMPLICIT_RANGE_STATEMENTS = b.build();
+    }
 
-        DEFAULT_RANGE_STATEMENTS = ImmutableList.of(BaseConstraints.newRangeConstraint(min, max,
-            Optional.of(rangeDescription), Optional.of("https://tools.ietf.org/html/rfc6020#section-9.2.4")));
+    private static List<RangeConstraint> createRangeConstraint(final String min, final String max) {
+        final String description = "Decimal values between " + min + " and " + max +", inclusively";
+
+        return ImmutableList.of(BaseConstraints.newRangeConstraint(new BigDecimal(min), new BigDecimal(max),
+            Optional.of(description), Optional.of("https://tools.ietf.org/html/rfc6020#section-9.3.4")));
     }
 
     private final List<RangeConstraint> rangeStatements;
-    private final Integer fractionDigits;
+    private final int fractionDigits;
     private final SchemaPath path;
 
     /**
      * Default Decimal64 Type Constructor. <br>
      * <br>
-     * The initial range statements are set to Decimal64
-     * <code>min=-922337203685477580.8</code> and
-     * <code>max=922337203685477580.7</code> <br>
-     * The fractions digits MUST be defined as integer between 1 and
-     * {@link #MAX_NUMBER_OF_FRACTION_DIGITS} inclusively as defined interface
-     * {@link DecimalTypeDefinition} <br>
+     * The initial range statements are set to implicit ranges for Decimal64 implied by the number of fraction
+     * digits. Fraction digits MUST be defined as integer between 1 and 18 inclusively as defined interface
+     * {@link DecimalTypeDefinition}<br>
      * If the fraction digits are not defined inner the definition boundaries
      * the constructor will throw {@link IllegalArgumentException}
      *
      * @param path
-     * @param fractionDigits
-     *            integer between 1 and 18 inclusively
+     * @param fractionDigits integer between 1 and 18 inclusively
+     * @throws IllegalArgumentException
      *
      * @see DecimalTypeDefinition
-     * @exception IllegalArgumentException
      */
-    private Decimal64(final SchemaPath path, final Integer fractionDigits) {
-        if (!((fractionDigits.intValue() >= 1) && (fractionDigits.intValue() <= MAX_NUMBER_OF_FRACTION_DIGITS))) {
-            throw new IllegalArgumentException(
-                    "The fraction digits outside of boundaries. Fraction digits MUST be integer between 1 and 18 inclusively");
-        }
+    private Decimal64(final SchemaPath path, final int fractionDigits) {
+        Preconditions.checkArgument(fractionDigits >= 1 && fractionDigits <= 18,
+                "The number of fraction digits %s is outside of allowed range [1, 18]", fractionDigits);
+
+        this.path = Preconditions.checkNotNull(path);
         this.fractionDigits = fractionDigits;
-        rangeStatements = DEFAULT_RANGE_STATEMENTS;
-        this.path = path;
+        this.rangeStatements = IMPLICIT_RANGE_STATEMENTS.get(fractionDigits - 1);
     }
 
     public static Decimal64 create(final SchemaPath path, final Integer fractionDigits) {
@@ -95,12 +101,12 @@ public final class Decimal64 implements DecimalTypeDefinition {
 
     @Override
     public String getUnits() {
-        return UNITS;
+        return "";
     }
 
     @Override
     public Object getDefaultValue() {
-        return DEFAULT_VALUE;
+        return null;
     }
 
     @Override
@@ -148,7 +154,7 @@ public final class Decimal64 implements DecimalTypeDefinition {
         final int prime = 31;
         int result = 1;
         result = prime * result + BaseTypes.DECIMAL64_QNAME.hashCode();
-        result = prime * result + Objects.hashCode(path);
+        result = prime * result + path.hashCode();
         return result;
     }
 
@@ -164,7 +170,7 @@ public final class Decimal64 implements DecimalTypeDefinition {
             return false;
         }
         Decimal64 other = (Decimal64) obj;
-        return Objects.equals(path, other.path);
+        return path.equals(other.path);
     }
 
     @Override
