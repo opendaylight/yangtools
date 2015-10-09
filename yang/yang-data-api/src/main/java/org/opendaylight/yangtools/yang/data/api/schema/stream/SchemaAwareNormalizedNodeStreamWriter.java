b7adebbf2f5e71a51b5429ffd6ab3ebd2a4e5fde
Robert Varga <rovarga@cisco.com>
2015-10-09 18:31:02 +0200

    BUG-4261: introduce DataSchemaNodeAware NormalizedNodeStreamWriter
    This variant allows passing a DataSchemaNode from the user.
    Implementations can take advantage of this information to optimize their
    result. Utility no-op adaptors are provided, too.
    
    Change-Id: I990197baf3a80f9826cd35d3bb3155d1b7a10cc3
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/stream/SchemaAwareNormalizedNodeStreamWriter.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.api.schema.stream;
+
+import com.google.common.annotations.Beta;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+
+/**
+ * Marker interface for {@link NormalizedNodeStreamWriter}s which can take advantage of {@link DataSchemaNode}
+ * information when writing the nodes.
+ */
+@Beta
+public interface SchemaAwareNormalizedNodeStreamWriter extends NormalizedNodeStreamWriter, DataSchemaNodeAware {
+
+}
