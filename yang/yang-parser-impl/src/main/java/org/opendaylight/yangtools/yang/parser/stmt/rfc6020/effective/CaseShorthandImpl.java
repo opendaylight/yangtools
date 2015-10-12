6f8ad0930fbb9a9d58259df5a5fe29a9a76e0e93
Martin Ciglan <mciglan@cisco.com>
2015-10-12 09:40:46 +0200

    Bug 4414: Shorthand case statement not working
    Original for shorthand-case was not set at all,
    therefore definition in grouping causes
    IncorrectNestingException: Supplied class is not valid case
    
    Change-Id: I6941eea52a07967824d2653fbfaedebc5f35ddc9
    Signed-off-by: Martin Ciglan <mciglan@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/CaseShorthandImpl.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/CaseShorthandImpl.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
@@ -55,6 +55,12 @@ public class CaseShorthandImpl implements ChoiceCaseNode, DerivableSchemaNode {
         this.addedByUses = caseShorthandNode.isAddedByUses();
         this.constraints = caseShorthandNode.getConstraints();
         this.unknownNodes = ImmutableList.copyOf(caseShorthandNode.getUnknownSchemaNodes());
+
+        if (caseShorthandNode instanceof DerivableSchemaNode && ((DerivableSchemaNode) caseShorthandNode).getOriginal
+                ().isPresent()) {
+            this.original = new CaseShorthandImpl((DataSchemaNode) ((DerivableSchemaNode) caseShorthandNode).
+                    getOriginal().get());
+        }
     }
 
     @Override
