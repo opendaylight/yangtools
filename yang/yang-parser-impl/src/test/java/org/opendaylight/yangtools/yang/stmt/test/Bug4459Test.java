a42db32efaf39c1a5ad6fb627de77bc2e9d46338
Peter Kajsa <pkajsa@cisco.com>
2015-10-13 17:29:24 +0200

    Bug 4459 - Parser fails when enum contains illegal characters for QName.
    RFC6020 defines enum argument as string, but we need to parse QName from it
    due to binding part of yangtools. However, parsing of the enum containing
    illegal characters for QName causes IllegalArgumentException.
    
    Change-Id: Iccd5523e12ca9bdfa44efd0fab09ef764990b837
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/test/Bug4459Test.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.EnumTypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.EnumTypeDefinition.EnumPair;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+
+public class Bug4459Test {
+
+    @Test
+    public void test() throws IOException, URISyntaxException, SourceException, ReactorException {
+        SchemaContext schema = StmtTestUtils.parseYangSources("/bugs/bug4459");
+        assertNotNull(schema);
+
+        DataSchemaNode dataChildByName = schema.getDataChildByName("my-leaf");
+        assertTrue(dataChildByName instanceof LeafSchemaNode);
+        LeafSchemaNode myLeaf = (LeafSchemaNode) dataChildByName;
+
+        TypeDefinition<?> type = myLeaf.getType();
+        assertTrue(type instanceof EnumTypeDefinition);
+        EnumTypeDefinition myEnum = (EnumTypeDefinition) type;
+
+        QName expectedEnumQName = QName.create("foo", "1970-01-01", "QuestionMark-Ampersand-LeftParenthesis-RightParenthesis-QuestionMark-QuestionMark-Ampersand");
+        List<EnumPair> values = myEnum.getValues();
+        for (EnumPair enumPair : values) {
+            if (enumPair.getName().equals("?-&-(-)-?-?-&")) {
+                assertEquals(expectedEnumQName, enumPair.getQName());
+            }
+        }
+    }
+}
