f3612c50f17cad8764938433e3c70a525dc6d636
Peter Kajsa <pkajsa@cisco.com>
2015-10-19 15:22:42 +0200

    Bug 4410: Loop in typedefs causes stack overflow
    The following kind of loop in the typedefs causes stack overflow error during the compilation of the yang file:
    
        typedef foo {
        	type bar;
        }
    
        typedef bar {
        	type foo;
        }
    
    The compiler should provide descriptive error message instead.
    
    Change-Id: Ia752a7de4a6e1ec1c7018fadc1428845d820e390
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/test/Bug4410Test.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt.test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import com.google.common.base.Throwables;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.parser.spi.meta.InferenceException;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.spi.meta.SomeModifiersUnresolvedException;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+
+public class Bug4410Test {
+
+    @Test
+    public void test() throws SourceException, FileNotFoundException, ReactorException, URISyntaxException {
+        try {
+            StmtTestUtils.parseYangSources("/bugs/bug4410");
+            fail("SomeModifiersUnresolvedException should be thrown.");
+        } catch (SomeModifiersUnresolvedException e) {
+            Throwable rootCause = Throwables.getRootCause(e);
+            assertTrue(rootCause instanceof InferenceException);
+            final String message = rootCause.getMessage();
+            assertTrue(message.startsWith("Type [(foo?revision=1970-01-01)"));
+            assertTrue(message.endsWith("was not found."));
+        }
+    }
+}
