924693c23db8c1ddb3f06f5db78e73c67693c2c4
Robert Varga <rovarga@cisco.com>
2015-10-20 11:47:46 +0200

    BUG-4638: Split out Number comparator
    Comapring two Numbers of the same class is useful also for comparing
    lengths. Split it out into utility methods.
    
    Change-Id: I0203d191a7849b7485419a5ef9e32c0fef442ed3
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-model-util/src/main/java/org/opendaylight/yangtools/yang/model/util/type/NumberUtil.java
+++ yang/yang-model-util/src/main/java/org/opendaylight/yangtools/yang/model/util/type/NumberUtil.java
@@ -8,12 +8,38 @@
 package org.opendaylight.yangtools.yang.model.util.type;
 
 import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Comparator;
 import java.util.Map;
 
 final class NumberUtil {
+    private static final Comparator<Number> NUMBER_COMPARATOR = new Comparator<Number>() {
+        @Override
+        public int compare(final Number o1, final Number o2) {
+            Preconditions.checkArgument(o1.getClass().equals(o2.getClass()), "Incompatible Number classes %s and %s",
+                o1.getClass(), o2.getClass());
+
+            if (o1 instanceof Byte) {
+                return ((Byte)o1).compareTo((Byte) o2);
+            } else if (o1 instanceof Short) {
+                return ((Short)o1).compareTo((Short) o2);
+            } else if (o1 instanceof Integer) {
+                return ((Integer)o1).compareTo((Integer) o2);
+            } else if (o1 instanceof Long) {
+                return ((Long)o1).compareTo((Long) o2);
+            } else if (o1 instanceof BigDecimal) {
+                return ((BigDecimal)o1).compareTo((BigDecimal) o2);
+            } else if (o1 instanceof BigInteger) {
+                return ((BigInteger)o1).compareTo((BigInteger) o2);
+            } else {
+                throw new IllegalArgumentException("Unsupported Number class " + o1.getClass());
+            }
+        }
+    };
+
     private static final Map<Class<? extends Number>, Function<Number, Number>> CONVERTERS;
     static {
         final ImmutableMap.Builder<Class<? extends Number>, Function<Number, Number>> b = ImmutableMap.builder();
@@ -103,5 +129,10 @@ final class NumberUtil {
 
     static Function<Number, Number> converterTo(final Class<? extends Number> clazz) {
         return CONVERTERS.get(clazz);
+    }
+
+    static boolean isRangeCovered(final Number min, final Number max, final Number superMin, final Number superMax) {
+        return NumberUtil.NUMBER_COMPARATOR.compare(min, superMin) >= 0 &&
+                NumberUtil.NUMBER_COMPARATOR.compare(max, superMax) <= 0;
     }
 }
