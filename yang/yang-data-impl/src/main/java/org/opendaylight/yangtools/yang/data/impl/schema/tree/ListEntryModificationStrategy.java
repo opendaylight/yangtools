c51d0d9b29b27ca8a9b0460b35b104578528e03b
Robert Varga <rovarga@cisco.com>
2015-10-21 23:21:53 +0200

    BUG-4355: mandatory node presence enforcement
    Introduce mandatory leaf validation as specified in RFC6020. Mandatory
    node presence is enforced whenever a presence container, a list entry,
    and a case node.
    
    The presence of mandatory leaves which do not have any ancestors other
    than structural containers are not enforced as mandatory. The reason for
    this is that the first transaction on such a DataTree would have to
    introduce all mandatory leaves, as their non-presence would make other
    transactions fail consistency checks.
    
    Change-Id: I19df441026854955fd37f6e1ff7b4ea3c48c60b8
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/ListEntryModificationStrategy.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/ListEntryModificationStrategy.java
@@ -8,16 +8,44 @@
 package org.opendaylight.yangtools.yang.data.impl.schema.tree;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import com.google.common.base.Optional;
 import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.TreeType;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.spi.TreeNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.spi.Version;
 import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.DataContainerNodeBuilder;
 import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableMapEntryNodeBuilder;
 import org.opendaylight.yangtools.yang.model.api.ListSchemaNode;
 
 final class ListEntryModificationStrategy extends AbstractDataNodeContainerModificationStrategy<ListSchemaNode> {
+    private final MandatoryLeafEnforcer enforcer;
+
     ListEntryModificationStrategy(final ListSchemaNode schema, final TreeType treeType) {
         super(schema, MapEntryNode.class, treeType);
+        enforcer = MandatoryLeafEnforcer.forContainer(schema, treeType);
+    }
+
+    @Override
+    protected TreeNode applyMerge(final ModifiedNode modification, final TreeNode currentMeta, final Version version) {
+        final TreeNode ret = super.applyMerge(modification, currentMeta, version);
+        enforcer.enforceOnTreeNode(ret);
+        return ret;
+    }
+
+    @Override
+    protected TreeNode applyWrite(final ModifiedNode modification, final Optional<TreeNode> currentMeta,
+            final Version version) {
+        final TreeNode ret = super.applyWrite(modification, currentMeta, version);
+        enforcer.enforceOnTreeNode(ret);
+        return ret;
+    }
+
+    @Override
+    protected TreeNode applyTouch(final ModifiedNode modification, final TreeNode currentMeta, final Version version) {
+        final TreeNode ret = super.applyTouch(modification, currentMeta, version);
+        enforcer.enforceOnTreeNode(ret);
+        return ret;
     }
 
     @Override
