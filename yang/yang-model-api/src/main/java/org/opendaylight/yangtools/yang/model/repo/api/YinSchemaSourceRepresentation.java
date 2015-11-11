9b81bd24ce5de6b55a5aab78720f464877592e3f
Robert Varga <robert.varga@pantheon.sk>
2015-11-11 22:34:53 +0100

    BUG-4629: cleanup YinSchemaSource
    This patch introduces Yang/YinSchemaSourceRepresentation, which mark
    YANG and YIN schema sources irrespective of the format in which they are
    available.
    
    It then adds YimXmlSchemaSource, which allows access to YIN
    representation as a javax.xml.transform.Source. Finally it deprecates
    YinSchemaSource and provides a utility YinDomSchemaSource, which can
    translate a YinXmlSchemaSource in the DOM representation.
    
    Change-Id: I832b2f14637eeb3183c6fe3254f8129bccc0a7ba
    Signed-off-by: Robert Varga <robert.varga@pantheon.sk>

---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/repo/api/YinSchemaSourceRepresentation.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.repo.api;
+
+import com.google.common.annotations.Beta;
+
+/**
+ * A YIN {@link SchemaSourceRepresentation}.
+ */
+@Beta
+public interface YinSchemaSourceRepresentation extends SchemaSourceRepresentation {
+    /**
+     * Return the concrete representation type.
+     *
+     * @return The type of representation.
+     */
+    @Override
+    Class<? extends YinSchemaSourceRepresentation> getType();
+}
---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/repo/api/YinXmlSchemaSource.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.repo.api;
+
+import com.google.common.annotations.Beta;
+import javax.annotation.Nonnull;
+import javax.xml.transform.Source;
+
+/**
+ * YIN text schema source representation. Exposes an RFC6020 XML representation as an XML {@link Source}.
+ */
+@Beta
+public interface YinXmlSchemaSource extends YinSchemaSourceRepresentation {
+    @Override
+    Class<? extends YinXmlSchemaSource> getType();
+
+    /**
+     * Return an XML {@link Source} of the YIN document.
+     *
+     * @return An XML {@link Source} instance.
+     */
+    @Nonnull Source getSource();
+}
