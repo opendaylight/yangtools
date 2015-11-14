97bcd6718ea4d93ffc88103283d02a17f4beeeb4
Robert Varga <robert.varga@pantheon.sk>
2015-11-14 01:03:19 +0100

    BUG-4638: Convert to using new types
    This patch switches the Lithium parser to use yang.model.util.type as
    the underlying implementation of TypeDefinitions.
    
    Change-Id: Idf18a95f268b41b7a6832db74289fa7dcd86bf06
    Signed-off-by: Robert Varga <robert.varga@pantheon.sk>

--- yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/retest/YangParserNegativeTest.java
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/retest/YangParserNegativeTest.java
@@ -10,6 +10,7 @@ package org.opendaylight.yangtools.yang.stmt.retest;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import com.google.common.base.Throwables;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -24,7 +25,6 @@ import org.opendaylight.yangtools.yang.parser.spi.meta.SomeModifiersUnresolvedEx
 import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
 import org.opendaylight.yangtools.yang.parser.util.YangParseException;
 import org.opendaylight.yangtools.yang.parser.util.YangValidationException;
-import com.google.common.base.Throwables;
 
 public class YangParserNegativeTest {
 
@@ -52,9 +52,9 @@ public class YangParserNegativeTest {
                 TestUtils.loadModule(stream);
                 fail("IllegalArgumentException should be thrown");
             }
-        } catch (IllegalStateException e) {
+        } catch (SourceException e) {
             assertTrue(e.getMessage().startsWith(
-                    "Type '(urn:simple.types.data.demo?revision=2013-02-27)int-ext' was not found"));
+                    "Type '(urn:simple.types.data.demo?revision=2013-02-27)int-ext' not found"));
         }
     }
 
