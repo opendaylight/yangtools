851a0bc3b4371a33606ea253bcc0e1f34e4fcc94
Robert Varga <robert.varga@pantheon.sk>
2015-11-15 02:18:34 +0100

    BUG-4638: Introduce LeafTypeBuilders
    Leaves need to override units/default value of their type, but unlike
    typedefs do not need retain their SchemaPath.
    
    Change-Id: I4c0539983b0f5023bd57a98c57aca75ebc9975fb
    Signed-off-by: Robert Varga <robert.varga@pantheon.sk>

---
+++ yang/yang-model-util/src/main/java/org/opendaylight/yangtools/yang/model/util/type/LeafTypeBuilder.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2015 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.util.type;
+
+import com.google.common.base.Verify;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+
+/**
+ * Builder of {@link TypeDefinitions} for use in leaf statements. While similar to {@link DerivedTypeBuilder}, this
+ * builder does not support adding of unknown nodes and will return the base type if the type is not modified, hence
+ * not preserving the schema path.
+ *
+ * @param <T> Resulting {@link TypeDefinition}
+ */
+public abstract class LeafTypeBuilder<T extends TypeDefinition<T>> extends DerivedTypeBuilder<T> {
+    LeafTypeBuilder(final T baseType, final SchemaPath path) {
+        super(baseType, path);
+
+        if (baseType.getDefaultValue() != null) {
+            setDefaultValue(baseType.getDefaultValue());
+        }
+        if (baseType.getDescription() != null) {
+            setDescription(baseType.getDescription());
+        }
+        if (baseType.getReference() != null) {
+            setReference(baseType.getReference());
+        }
+        if (baseType.getStatus() != null) {
+            setStatus(baseType.getStatus());
+        }
+        if (baseType.getUnits() != null) {
+            setUnits(baseType.getUnits());
+        }
+    }
+
+    /**
+     * Build the resulting type.
+     *
+     * @return A new type instance
+     */
+    @Nonnull abstract T buildType();
+
+    @Override
+    public final T build() {
+        final T base = getBaseType();
+        if (Objects.equals(getDefaultValue(), base.getDefaultValue()) && Objects.equals(getUnits(), base.getUnits())) {
+            return base;
+        }
+
+        return Verify.verifyNotNull(buildType());
+    }
+}
