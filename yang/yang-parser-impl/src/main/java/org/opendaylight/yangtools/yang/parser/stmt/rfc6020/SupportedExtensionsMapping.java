ef0eb0ec83673d2eecc1d8077e8c0219c3f4b2e1
Peter Kajsa <pkajsa@cisco.com>
2015-11-16 10:31:59 +0100

    Bug 3874: Support of yang modeled AnyXML - XML deserialization
    - added support of yang modeled anyXml deserialization from XML
    - basic unit tests
    - added anyxml-schema-location extension support and removed
      hardcoded extension name from AnyxmlStatementImpl
    
    Change-Id: I2add4bd5613417514d84b52be786074e2cc75f4f
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/SupportedExtensionsMapping.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.stmt.rfc6020;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.AnyxmlSchemaLocationEffectiveStatementImpl;
+
+@Beta
+public enum SupportedExtensionsMapping implements StatementDefinition {
+    ANYXML_SCHEMA_LOCATION("urn:opendaylight:yang:extension:yang-ext", "2013-07-09", AnyxmlSchemaLocationStatementImpl.class,
+            AnyxmlSchemaLocationEffectiveStatementImpl.class, "anyxml-schema-location", "target-node");
+
+    private final Class<? extends DeclaredStatement<?>> type;
+    private final Class<? extends EffectiveStatement<?, ?>> effectiveType;
+    private final QName name;
+    private final QName argument;
+
+    SupportedExtensionsMapping(final String namespace, final String revision, final Class<? extends DeclaredStatement<?>> declared,
+            Class<? extends EffectiveStatement<?, ?>> effective, final String nameStr, final String argumentStr) {
+        type = Preconditions.checkNotNull(declared);
+        effectiveType = Preconditions.checkNotNull(effective);
+        name = createQName(namespace, revision, nameStr);
+        argument = createQName(namespace, revision, argumentStr);
+    }
+
+    @Nonnull
+    private static QName createQName(final String namespace,final String revision,final String localName) {
+        return QName.create(namespace, revision, localName);
+    }
+
+    @Override
+    public QName getStatementName() {
+        return name;
+    }
+
+    @Override
+    @Nullable
+    public QName getArgumentName() {
+        return argument;
+    }
+
+    @Override
+    @Nonnull
+    public Class<? extends DeclaredStatement<?>> getDeclaredRepresentationClass() {
+        return type;
+    }
+
+    @Override
+    public Class<? extends EffectiveStatement<?, ?>> getEffectiveRepresentationClass() {
+        return effectiveType;
+    }
+}
