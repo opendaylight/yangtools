d609e793b6fb85e70d64efe62aceda4da6833e44
Robert Varga <rovarga@cisco.com>
2015-11-18 15:49:46 +0100

    BUG-4638: implement bits validation
    BitsType needs to validate
    
    Change-Id: Ib648b33302bef5e475f3b26edd9f242fbfdb4560
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-model-util/src/main/java/org/opendaylight/yangtools/yang/model/util/type/InvalidBitDefinitionException.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2015 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.util.type;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.Preconditions;
+import org.opendaylight.yangtools.yang.model.api.type.BitsTypeDefinition.Bit;
+
+@Beta
+public class InvalidBitDefinitionException extends IllegalArgumentException {
+    private static final long serialVersionUID = 1L;
+    private final Bit offendingBit;
+
+    protected InvalidBitDefinitionException(final Bit offendingBit, final String message) {
+        super(message);
+        this.offendingBit = Preconditions.checkNotNull(offendingBit);
+    }
+
+    public InvalidBitDefinitionException(final Bit offendingBit, final String format,
+            final Object... args) {
+        this(offendingBit, String.format(format, args));
+    }
+
+    public Bit getOffendingBit() {
+        return offendingBit;
+    }
+}
