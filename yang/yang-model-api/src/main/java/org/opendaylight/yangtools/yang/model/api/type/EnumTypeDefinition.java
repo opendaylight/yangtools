01a3817e16f62cf9ead70918e6843166a5444f25
Tony Tkacik <ttkacik@cisco.com>
2015-12-08 14:30:22 +0100

    BUG-865 Deprecated getQName in EnumPair.
    Change-Id: I9f2ed25b2000c2a28051fe06cba2e63c02afcc14
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/type/EnumTypeDefinition.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/type/EnumTypeDefinition.java
@@ -8,21 +8,22 @@
 package org.opendaylight.yangtools.yang.model.api.type;
 
 import java.util.List;
-
+import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.model.api.SchemaNode;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
 import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
 
 /**
- * 
+ *
  * Makes is possible to access to the individual enumeration values of this
  * type.
- * 
+ *
  */
 public interface EnumTypeDefinition extends TypeDefinition<EnumTypeDefinition> {
 
     /**
      * Returns all enumeration values.
-     * 
+     *
      * @return list of <code>EnumPair</code> type instastances which contain the
      *         data about all individual enumeration pairs of
      *         <code>enumeration</code> YANG built-in type
@@ -30,15 +31,36 @@ public interface EnumTypeDefinition extends TypeDefinition<EnumTypeDefinition> {
     List<EnumPair> getValues();
 
     /**
-     * 
+     *
      * Contains the methods for accessing the data about the concrete
      * enumeration item which represents <code>enum</code> YANG type.
      */
     interface EnumPair extends SchemaNode {
 
         /**
+         *
+         * @deprecated Enum pair SHOULD NOT have schema path, since it's argument is only string and
+         *             not QName.
+         *
+         */
+        @Deprecated
+        @Override
+        SchemaPath getPath();
+
+        /**
+         *
+         * @deprecated Enum pair SHOULD NOT have QName, since it's argument is only string (allows
+         *             characters not allowed in QName) and not QName. Use {@link #getName()}
+         *             instead.
+         *
+         */
+        @Deprecated
+        @Override
+        QName getQName();
+
+        /**
          * The name to specify each assigned name of an enumeration type.
-         * 
+         *
          * @return name of each assigned name of an enumeration type.
          */
         String getName();
@@ -48,7 +70,7 @@ public interface EnumTypeDefinition extends TypeDefinition<EnumTypeDefinition> {
          * integer value with the assigned name for the enum. This integer value
          * MUST be in the range -2147483648 to 2147483647, and it MUST be unique
          * within the enumeration type.
-         * 
+         *
          * @return integer value assigned to enumeration
          */
         Integer getValue();
