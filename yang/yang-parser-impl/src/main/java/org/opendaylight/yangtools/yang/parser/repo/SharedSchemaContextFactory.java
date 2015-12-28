17c783274680ccab3be81201aa7e3bba85ac5a15
Robert Varga <robert.varga@pantheon.sk>
2015-12-28 10:46:21 +0100

    Clean SharedSchemaContextFactory up
    Use ImmutableList, order imports, use explicit collection instantiation.
    
    Change-Id: I037e0ee192d5e670bbbf4996a6a731efb7773df2
    Signed-off-by: Robert Varga <robert.varga@pantheon.sk>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/repo/SharedSchemaContextFactory.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/repo/SharedSchemaContextFactory.java
@@ -7,37 +7,30 @@
  */
 package org.opendaylight.yangtools.yang.parser.repo;
 
-import org.opendaylight.yangtools.yang.parser.impl.util.YangModelDependencyInfo;
-
-import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
-import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
-import org.opendaylight.yangtools.antlrv4.code.gen.YangStatementParser;
-import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.YangStatementSourceImpl;
-import org.opendaylight.yangtools.yang.parser.stmt.reactor.CrossSourceStatementReactor;
-import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.YangInferencePipeline;
 import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Predicate;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.collect.Collections2;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.AsyncFunction;
 import com.google.common.util.concurrent.CheckedFuture;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import javax.annotation.Nullable;
 import org.antlr.v4.runtime.ParserRuleContext;
+import org.opendaylight.yangtools.antlrv4.code.gen.YangStatementParser.StatementContext;
 import org.opendaylight.yangtools.util.concurrent.ExceptionMapper;
 import org.opendaylight.yangtools.util.concurrent.ReflectiveExceptionMapper;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
@@ -45,6 +38,12 @@ import org.opendaylight.yangtools.yang.model.repo.api.SchemaContextFactory;
 import org.opendaylight.yangtools.yang.model.repo.api.SchemaResolutionException;
 import org.opendaylight.yangtools.yang.model.repo.api.SchemaSourceFilter;
 import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
+import org.opendaylight.yangtools.yang.parser.impl.util.YangModelDependencyInfo;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+import org.opendaylight.yangtools.yang.parser.stmt.reactor.CrossSourceStatementReactor;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.YangInferencePipeline;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.YangStatementSourceImpl;
 import org.opendaylight.yangtools.yang.parser.util.ASTSchemaSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -78,23 +77,15 @@ final class SharedSchemaContextFactory implements SchemaContextFactory {
                         res.getResolvedSources(), res.getUnsatisfiedImports());
             }
 
-            final Map<SourceIdentifier, ParserRuleContext> asts =
-                    Maps.transformValues(srcs, ASTSchemaSource.GET_AST);
+            final Map<SourceIdentifier, ParserRuleContext> asts = Maps.transformValues(srcs, ASTSchemaSource.GET_AST);
+            final CrossSourceStatementReactor.BuildAction reactor = YangInferencePipeline.RFC6020_REACTOR.newBuild();
 
-            CrossSourceStatementReactor.BuildAction reactor = YangInferencePipeline.RFC6020_REACTOR
-                    .newBuild();
+            for (final Entry<SourceIdentifier, ParserRuleContext> e : asts.entrySet()) {
+                final ParserRuleContext parserRuleCtx = e.getValue();
+                Preconditions.checkArgument(parserRuleCtx instanceof StatementContext,
+                    "Unsupported context class %s for source %s", parserRuleCtx.getClass(), e.getKey());
 
-            for (final Entry<SourceIdentifier, ParserRuleContext> entry : asts
-                    .entrySet()) {
-                ParserRuleContext parserRuleCtx = entry.getValue();
-                if (parserRuleCtx instanceof YangStatementParser.StatementContext) {
-                    YangStatementSourceImpl source = new YangStatementSourceImpl(
-                            entry.getKey(),
-                            (YangStatementParser.StatementContext) parserRuleCtx);
-                    reactor.addSource(source);
-                } else {
-                    throw new IllegalArgumentException("Only YangStatementParser.StatementContext is supported.");
-                }
+                reactor.addSource(new YangStatementSourceImpl(e.getKey(), (StatementContext) parserRuleCtx));
             }
 
             SchemaContext schemaContext = reactor.buildEffective();
@@ -155,28 +146,32 @@ final class SharedSchemaContextFactory implements SchemaContextFactory {
      * @return set (preserving ordering) from the input collection
      */
     private static List<SourceIdentifier> deDuplicateSources(final Collection<SourceIdentifier> requiredSources) {
-        final Set<SourceIdentifier> uniqueSourceIdentifiers = Collections.unmodifiableSet(Sets.newLinkedHashSet(requiredSources));
-        if(uniqueSourceIdentifiers.size() != requiredSources.size()) {
-            LOG.warn("Duplicate sources requested for schema context, removed duplicate sources: {}", Collections2.filter(uniqueSourceIdentifiers, new Predicate<SourceIdentifier>() {
+        final Set<SourceIdentifier> uniqueSourceIdentifiers = new LinkedHashSet<>(requiredSources);
+        if (uniqueSourceIdentifiers.size() == requiredSources.size()) {
+            // Can potentially reuse input
+            return ImmutableList.copyOf(requiredSources);
+        }
+
+        LOG.warn("Duplicate sources requested for schema context, removed duplicate sources: {}",
+            Collections2.filter(uniqueSourceIdentifiers, new Predicate<SourceIdentifier>() {
                 @Override
                 public boolean apply(@Nullable final SourceIdentifier input) {
                     return Iterables.frequency(requiredSources, input) > 1;
                 }
             }));
-        }
-        return Lists.newArrayList(uniqueSourceIdentifiers);
+        return ImmutableList.copyOf(uniqueSourceIdentifiers);
     }
 
     private static final class SourceIdMismatchDetector implements Function<List<ASTSchemaSource>, List<ASTSchemaSource>> {
         private final List<SourceIdentifier> sourceIdentifiers;
 
         public SourceIdMismatchDetector(final List<SourceIdentifier> sourceIdentifiers) {
-            this.sourceIdentifiers = sourceIdentifiers;
+            this.sourceIdentifiers = Preconditions.checkNotNull(sourceIdentifiers);
         }
 
         @Override
         public List<ASTSchemaSource> apply(final List<ASTSchemaSource> input) {
-            final Map<SourceIdentifier, ASTSchemaSource> filtered = Maps.newLinkedHashMap();
+            final Map<SourceIdentifier, ASTSchemaSource> filtered = new LinkedHashMap<>();
 
             for (int i = 0; i < input.size(); i++) {
 
@@ -185,7 +180,8 @@ final class SharedSchemaContextFactory implements SchemaContextFactory {
                 final SourceIdentifier realSId = astSchemaSource.getIdentifier();
 
                 if (!expectedSId.equals(realSId)) {
-                    LOG.warn("Source identifier mismatch for module \"{}\", requested as {} but actually is {}. Using actual id", expectedSId.getName(), expectedSId, realSId);
+                    LOG.warn("Source identifier mismatch for module \"{}\", requested as {} but actually is {}. Using actual id",
+                        expectedSId.getName(), expectedSId, realSId);
                 }
 
                 if (filtered.containsKey(realSId)) {
@@ -195,7 +191,7 @@ final class SharedSchemaContextFactory implements SchemaContextFactory {
                 filtered.put(realSId, astSchemaSource);
 
             }
-            return Lists.newArrayList(filtered.values());
+            return ImmutableList.copyOf(filtered.values());
         }
     }
 }
