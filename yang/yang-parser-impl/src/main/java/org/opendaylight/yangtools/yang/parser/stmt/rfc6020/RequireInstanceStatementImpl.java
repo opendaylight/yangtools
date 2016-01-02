b2feb30eeef3456a8381bad61c7ff8de048082af
Robert Varga <robert.varga@pantheon.sk>
2016-01-02 15:49:32 +0100

    Make RequireInstanceStatement#getValue() return boolean
    @Nonnull with Boolean means we can use a primitive boolean, make users'
    life easier.
    
    Change-Id: I42ec575484dcfd9c92781608b614634a03bc3939
    Signed-off-by: Robert Varga <robert.varga@pantheon.sk>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/RequireInstanceStatementImpl.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/RequireInstanceStatementImpl.java
@@ -14,22 +14,18 @@ import org.opendaylight.yangtools.yang.parser.spi.SubstatementValidator;
 import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractDeclaredStatement;
 import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractStatementSupport;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
-import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
 import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.RequireInstanceEffectiveStatementImpl;
 
 public class RequireInstanceStatementImpl extends
         AbstractDeclaredStatement<Boolean> implements RequireInstanceStatement {
-    private static final SubstatementValidator SUBSTATEMENT_VALIDATOR = SubstatementValidator.builder(Rfc6020Mapping
-            .REQUIRE_INSTANCE)
-            .build();
+    private static final SubstatementValidator SUBSTATEMENT_VALIDATOR = SubstatementValidator.builder(
+        Rfc6020Mapping.REQUIRE_INSTANCE).build();
 
-    protected RequireInstanceStatementImpl(
-            StmtContext<Boolean, RequireInstanceStatement, ?> context) {
+    protected RequireInstanceStatementImpl(final StmtContext<Boolean, RequireInstanceStatement, ?> context) {
         super(context);
     }
 
-    public static class Definition
-            extends
+    public static class Definition extends
             AbstractStatementSupport<Boolean, RequireInstanceStatement, EffectiveStatement<Boolean, RequireInstanceStatement>> {
 
         public Definition() {
@@ -37,33 +33,31 @@ public class RequireInstanceStatementImpl extends
         }
 
         @Override
-        public Boolean parseArgumentValue(StmtContext<?, ?, ?> ctx, String value) {
+        public Boolean parseArgumentValue(final StmtContext<?, ?, ?> ctx, final String value) {
             return Boolean.valueOf(value);
         }
 
         @Override
-        public RequireInstanceStatement createDeclared(
-                StmtContext<Boolean, RequireInstanceStatement, ?> ctx) {
+        public RequireInstanceStatement createDeclared(final StmtContext<Boolean, RequireInstanceStatement, ?> ctx) {
             return new RequireInstanceStatementImpl(ctx);
         }
 
         @Override
         public EffectiveStatement<Boolean, RequireInstanceStatement> createEffective(
-                StmtContext<Boolean, RequireInstanceStatement, EffectiveStatement<Boolean, RequireInstanceStatement>> ctx) {
+                final StmtContext<Boolean, RequireInstanceStatement, EffectiveStatement<Boolean, RequireInstanceStatement>> ctx) {
             return new RequireInstanceEffectiveStatementImpl(ctx);
         }
 
         @Override
-        public void onFullDefinitionDeclared(StmtContext.Mutable<Boolean, RequireInstanceStatement,
-                EffectiveStatement<Boolean, RequireInstanceStatement>> stmt) throws SourceException {
+        public void onFullDefinitionDeclared(final StmtContext.Mutable<Boolean, RequireInstanceStatement,
+                EffectiveStatement<Boolean, RequireInstanceStatement>> stmt) {
             super.onFullDefinitionDeclared(stmt);
             SUBSTATEMENT_VALIDATOR.validate(stmt);
         }
     }
 
     @Override
-    public Boolean getValue() {
+    public boolean getValue() {
         return argument();
     }
-
 }
