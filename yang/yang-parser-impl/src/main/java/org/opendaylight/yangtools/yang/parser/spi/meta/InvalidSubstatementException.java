0f81b81b3f6d6ea8ca3e932dd3b40a7321e9770a
Robert Varga <robert.varga@pantheon.sk>
2016-01-05 00:46:14 +0100

    Introduce formatting methods for SourceException
    - check() is similar to Preconditions.checkArgument()
    - checkNotNull() is similar to Preconditions.checkNotNull()
    - new constructors with string formatting
    - fix {Invalid,Missing}SubstatementException not having a version
    
    Change-Id: I95d15fb6a591f77b0be17d9ca50f51d3636c80ed
    Signed-off-by: Robert Varga <robert.varga@pantheon.sk>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/spi/meta/InvalidSubstatementException.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/spi/meta/InvalidSubstatementException.java
@@ -12,16 +12,22 @@ import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
 import org.opendaylight.yangtools.yang.parser.spi.source.StatementSourceReference;
 
 /**
- *
  * Thrown when there was Invalid element in yang file
- *
  */
 public class InvalidSubstatementException extends SourceException {
-    public InvalidSubstatementException(@Nonnull String message, @Nonnull StatementSourceReference source, Throwable cause) {
+    private static final long serialVersionUID = 1L;
+
+    public InvalidSubstatementException(@Nonnull final String message, @Nonnull final StatementSourceReference source,
+            final Throwable cause) {
         super(message, source, cause);
     }
 
-    public InvalidSubstatementException(@Nonnull String message, @Nonnull StatementSourceReference source) {
+    public InvalidSubstatementException(@Nonnull final String message, @Nonnull final StatementSourceReference source) {
         super(message, source);
+    }
+
+    public InvalidSubstatementException(@Nonnull final StatementSourceReference source, @Nonnull final String format,
+            final Object... args) {
+        this(String.format(format, args), source);
     }
 }
