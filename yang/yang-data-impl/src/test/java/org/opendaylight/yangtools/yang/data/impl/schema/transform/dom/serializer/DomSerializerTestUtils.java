6ecdf9b00bdaf47345d803af42c55f492eec7121
Filip Gregor <fgregor@cisco.com>
2016-02-01 16:25:12 +0100

    Bug 3899: Milestone Increase test coverage for YangTools
    Tests for Dom serializer
    
    Change-Id: Id60b0f8572a64e01a8a0ff3b7792a0e38cbdce65
    Signed-off-by: Filip Gregor <fgregor@cisco.com>

---
+++ yang/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/schema/transform/dom/serializer/DomSerializerTestUtils.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.data.impl.schema.transform.dom.serializer;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.text.ParseException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.mockito.Mockito;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.impl.codec.TypeDefinitionAwareCodec;
+import org.opendaylight.yangtools.yang.data.impl.codec.xml.XmlCodecProvider;
+import org.opendaylight.yangtools.yang.data.impl.codec.xml.XmlDocumentUtils;
+import org.opendaylight.yangtools.yang.data.impl.schema.transform.base.serializer.NodeSerializerDispatcher;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.parser.api.YangSyntaxErrorException;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.spi.source.StatementStreamSource;
+import org.opendaylight.yangtools.yang.parser.stmt.reactor.CrossSourceStatementReactor;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.YangInferencePipeline;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.YangStatementSourceImpl;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.EffectiveSchemaContext;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+public class DomSerializerTestUtils {
+    static final Document doc = XmlDocumentUtils.getDocument();
+    static final Element data = doc.createElement("data");
+    static final NodeSerializerDispatcher mockDispatcher = Mockito.mock(NodeSerializerDispatcher.class);
+    static final XmlCodecProvider codecProvider = new XmlCodecProvider() {
+        @Override
+        public TypeDefinitionAwareCodec<Object, ? extends TypeDefinition<?>> codecFor(TypeDefinition<?> baseType) {
+            return TypeDefinitionAwareCodec.from(baseType);
+        }
+    };
+    static Element element;
+
+    private DomSerializerTestUtils() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    public static EffectiveSchemaContext createTestContext(String sourcePath) throws IOException,
+            YangSyntaxErrorException,
+            ReactorException {
+        StatementStreamSource source = new YangStatementSourceImpl(sourcePath, false);
+        CrossSourceStatementReactor.BuildAction reactor = YangInferencePipeline.RFC6020_REACTOR.newBuild();
+        reactor.addSources(source);
+        return reactor.buildEffective();
+    }
+
+    public static SchemaContext getSchemaContext() throws ReactorException, IOException, YangSyntaxErrorException {
+        EffectiveSchemaContext schemaContext = createTestContext("/dom-serializer-test/serializer-test.yang");
+        assertNotNull("Schema context must not be null.", schemaContext);
+        return schemaContext;
+    }
+
+    public static YangInstanceIdentifier.NodeIdentifier getNodeIdentifier(final String localName, final String namespace,
+                                                                           final String revision) throws ParseException {
+        return new YangInstanceIdentifier.NodeIdentifier(QName.create(namespace, revision, localName));
+    }
+
+    public static String toString(final Node xml) {
+        try {
+            final Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+            final StreamResult result = new StreamResult(new StringWriter());
+            final DOMSource source = new DOMSource(xml);
+            transformer.transform(source, result);
+
+            return result.getWriter().toString();
+        } catch (IllegalArgumentException | TransformerFactoryConfigurationError | TransformerException e) {
+            throw new RuntimeException("Unable to serialize xml element " + xml, e);
+        }
+    }
+
+    public static QName generateQname(String name) {
+        return QName.create("dom-serializer-test", "2016-01-01", name);
+    }
+
+    public static void testResults(String node) {
+        data.appendChild(element);
+        String tempXml = toString(data);
+        assertNotNull(tempXml);
+        assertTrue(tempXml.contains(node));
+    }
+}
---
+++ yang/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/schema/transform/dom/serializer/LeafNodeDomSerializerTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.data.impl.schema.transform.dom.serializer;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.text.ParseException;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableLeafNodeBuilder;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
+import org.opendaylight.yangtools.yang.model.parser.api.YangSyntaxErrorException;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.ContainerEffectiveStatementImpl;
+import org.w3c.dom.Element;
+
+public class LeafNodeDomSerializerTest {
+
+    @Test
+    public void LeafNodeDomSerializerTest() throws ReactorException, IOException, YangSyntaxErrorException, ParseException {
+        DataSchemaNode currentContainer = DomSerializerTestUtils.getSchemaContext().findModuleByName("serializer-test", null)
+                .getDataChildByName(DomSerializerTestUtils.generateQname("root"));
+        LeafSchemaNode currentLeaf = (LeafSchemaNode) ((ContainerEffectiveStatementImpl) currentContainer).getDataChildByName
+                (DomSerializerTestUtils.generateQname("first-leaf"));
+        assertNotNull(currentLeaf.getType());
+
+        LeafNode<String> tempLeafNode = new ImmutableLeafNodeBuilder<String>()
+                .withValue("testing data")
+                .withNodeIdentifier(DomSerializerTestUtils.getNodeIdentifier("first-leaf", "dom-serializer-test", "2016-01-01"))
+                .build();
+
+        LeafNodeDomSerializer temp = new LeafNodeDomSerializer(DomSerializerTestUtils.doc, DomSerializerTestUtils.codecProvider);
+        DomSerializerTestUtils.element = temp.serializeLeaf(currentLeaf, tempLeafNode);
+        Iterable<Element> elementsFromAbstractClass = temp.serialize(currentLeaf, tempLeafNode);
+        Element singleElementFromAbstractClass = elementsFromAbstractClass.iterator().next();
+        assertTrue(DomSerializerTestUtils.element.getLocalName().equals(singleElementFromAbstractClass.getLocalName()));
+        assertTrue(DomSerializerTestUtils.element.getNamespaceURI().equals(singleElementFromAbstractClass.getNamespaceURI()));
+        DomSerializerTestUtils.testResults("<first-leaf xmlns=\"dom-serializer-test\">testing data</first-leaf>");
+    }
+}
