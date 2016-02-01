825f2f9bc50628b45b480bd392247048ff7d4553
Filip Gregor <fgregor@cisco.com>
2016-02-01 16:25:12 +0100

    Bug 3899: Milestone Increase test coverage for YangTools
    Tests for Dom serializer, builders
    
    Change-Id: Id60b0f8572a64e01a8a0ff3b7792a0e38cbdce65
    Signed-off-by: Filip Gregor <fgregor@cisco.com>

---
+++ yang/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/schema/transform/dom/serializer/MapUnkeyedListTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.data.impl.schema.transform.dom.serializer;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MapUnkeyedListTest {
+
+    @Test
+    public void mapUnkeyedListTest () {
+        final MapEntryNodeDomSerializer mapEntryNodeDomSerializer = new MapEntryNodeDomSerializer
+                (DomSerializerTestUtils.doc, DomSerializerTestUtils.mockDispatcher);
+        final UnkeyedListEntryNodeDomSerializer unkeyedListEntryNodeDomSerializer = new UnkeyedListEntryNodeDomSerializer
+                (DomSerializerTestUtils.doc, DomSerializerTestUtils.mockDispatcher);
+        final MapNodeDomSerializer mapNodeDomSerializer = new MapNodeDomSerializer(mapEntryNodeDomSerializer);
+        final UnkeyedListNodeDomSerializer unkeyedListNodeDomSerializer = new UnkeyedListNodeDomSerializer
+                (unkeyedListEntryNodeDomSerializer);
+
+        assertTrue(mapEntryNodeDomSerializer.equals(mapNodeDomSerializer.getListEntryNodeSerializer()));
+        assertTrue(unkeyedListEntryNodeDomSerializer.equals(unkeyedListNodeDomSerializer.getListEntryNodeSerializer()));
+    }
+}
