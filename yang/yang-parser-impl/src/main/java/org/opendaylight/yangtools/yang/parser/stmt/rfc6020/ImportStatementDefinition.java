776003994a529889a24a31d66df71055e912752b
Peter Kajsa <pkajsa@cisco.com>
2016-02-02 18:09:17 +0100

    (Draft) Semantic versioning - in progress.
    - definition of semantic version extension
    - parsing of semantic version from yang sources
    - basic unit test
    - added pre-linkage-phase (without revision, NS only) (in progress)
    - semantic version onLinkagePhase invoke (in progress)
    - rebased, conflicts fix, javadocs (in progress)
    - unit test fixs
    - added unit tests for import resolution (in progress)
    - import resolution process (in progress, needs refactoring.)
    
    Change-Id: I521d17d1453148eb678089b0fc5d6add6f0ac7ae
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/ImportStatementDefinition.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/ImportStatementDefinition.java
@@ -8,13 +8,17 @@
 package org.opendaylight.yangtools.yang.parser.stmt.rfc6020;
 
 import static org.opendaylight.yangtools.yang.parser.spi.meta.ModelProcessingPhase.SOURCE_LINKAGE;
+import static org.opendaylight.yangtools.yang.parser.spi.meta.ModelProcessingPhase.SOURCE_PRE_LINKAGE;
 import static org.opendaylight.yangtools.yang.parser.spi.meta.StmtContextUtils.firstAttributeOf;
+
 import com.google.common.base.Optional;
+import com.google.common.base.Verify;
 import java.net.URI;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Map;
 import java.util.Map.Entry;
+import org.opendaylight.yangtools.concepts.SemVer;
 import org.opendaylight.yangtools.yang.common.SimpleDateFormatUtil;
 import org.opendaylight.yangtools.yang.model.api.ModuleIdentifier;
 import org.opendaylight.yangtools.yang.model.api.Rfc6020Mapping;
@@ -26,22 +30,27 @@ import org.opendaylight.yangtools.yang.model.api.stmt.PrefixStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.RevisionDateStatement;
 import org.opendaylight.yangtools.yang.parser.builder.impl.ModuleIdentifierImpl;
 import org.opendaylight.yangtools.yang.parser.spi.ModuleNamespace;
+import org.opendaylight.yangtools.yang.parser.spi.PreLinkageModuleNamespace;
 import org.opendaylight.yangtools.yang.parser.spi.SubstatementValidator;
 import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractStatementSupport;
 import org.opendaylight.yangtools.yang.parser.spi.meta.InferenceException;
 import org.opendaylight.yangtools.yang.parser.spi.meta.ModelActionBuilder;
 import org.opendaylight.yangtools.yang.parser.spi.meta.ModelActionBuilder.InferenceAction;
 import org.opendaylight.yangtools.yang.parser.spi.meta.ModelActionBuilder.Prerequisite;
+import org.opendaylight.yangtools.yang.parser.spi.meta.SemanticVersionNamespace;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext.Mutable;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContextUtils;
 import org.opendaylight.yangtools.yang.parser.spi.source.ImpPrefixToModuleIdentifier;
+import org.opendaylight.yangtools.yang.parser.spi.source.ImpPrefixToNamespace;
+import org.opendaylight.yangtools.yang.parser.spi.source.ModuleNameToNamespace;
 import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.ImportEffectiveStatementImpl;
 
-public class ImportStatementDefinition
-        extends AbstractStatementSupport<String, ImportStatement, EffectiveStatement<String, ImportStatement>> {
-    private static final SubstatementValidator SUBSTATEMENT_VALIDATOR =
-            SubstatementValidator.builder(Rfc6020Mapping.IMPORT)
-            .add(Rfc6020Mapping.PREFIX, 1, 1).add(Rfc6020Mapping.REVISION_DATE, 0, 1).build();
+public class ImportStatementDefinition extends
+        AbstractStatementSupport<String, ImportStatement, EffectiveStatement<String, ImportStatement>> {
+    private static final SubstatementValidator SUBSTATEMENT_VALIDATOR = SubstatementValidator
+            .builder(Rfc6020Mapping.IMPORT).add(Rfc6020Mapping.PREFIX, 1, 1).add(Rfc6020Mapping.REVISION_DATE, 0, 1)
+            .add(SupportedExtensionsMapping.SEMANTIC_VERSION, 0, 1).build();
 
     public ImportStatementDefinition() {
         super(Rfc6020Mapping.IMPORT);
@@ -71,81 +80,221 @@ public class ImportStatementDefinition
     }
 
     @Override
-    public void onLinkageDeclared(
-            final Mutable<String, ImportStatement, EffectiveStatement<String, ImportStatement>> stmt) {
-        final ModuleIdentifier impIdentifier = getImportedModuleIdentifier(stmt);
-        final ModelActionBuilder importAction = stmt.newInferenceAction(SOURCE_LINKAGE);
-        final Prerequisite<StmtContext<?, ?, ?>> imported = importAction.requiresCtx(stmt, ModuleNamespace.class,
-            impIdentifier, SOURCE_LINKAGE);
-        final Prerequisite<Mutable<?, ?, ?>> linkageTarget = importAction.mutatesCtx(stmt.getRoot(), SOURCE_LINKAGE);
+    public void onPreLinkageDeclared(Mutable<String, ImportStatement, EffectiveStatement<String, ImportStatement>> stmt) {
+        final String moduleName = stmt.getStatementArgument();
+        final ModelActionBuilder importAction = stmt.newInferenceAction(SOURCE_PRE_LINKAGE);
+        final Prerequisite<StmtContext<?, ?, ?>> imported = importAction.requiresCtx(stmt,
+                PreLinkageModuleNamespace.class, moduleName, SOURCE_PRE_LINKAGE);
+        final Prerequisite<Mutable<?, ?, ?>> linkageTarget = importAction
+                .mutatesCtx(stmt.getRoot(), SOURCE_PRE_LINKAGE);
 
         importAction.apply(new InferenceAction() {
             @Override
             public void apply() {
-                StmtContext<?, ?, ?> importedModule = null;
-                ModuleIdentifier importedModuleIdentifier = null;
-                if (impIdentifier.getRevision() == SimpleDateFormatUtil.DEFAULT_DATE_IMP) {
-                    Entry<ModuleIdentifier, StmtContext<?, ModuleStatement, EffectiveStatement<String, ModuleStatement>>> recentModuleEntry =
-                            findRecentModule(impIdentifier, stmt.getAllFromNamespace(ModuleNamespace.class));
-                    if (recentModuleEntry != null) {
-                        importedModuleIdentifier = recentModuleEntry.getKey();
-                        importedModule = recentModuleEntry.getValue();
-                    }
-                }
-
-                if (importedModule == null || importedModuleIdentifier == null) {
-                    importedModule = imported.get();
-                    importedModuleIdentifier = impIdentifier;
-                }
-
-                linkageTarget.get().addToNs(ImportedModuleContext.class, importedModuleIdentifier, importedModule);
-                String impPrefix = firstAttributeOf(stmt.declaredSubstatements(), PrefixStatement.class);
-                stmt.addToNs(ImpPrefixToModuleIdentifier.class, impPrefix, importedModuleIdentifier);
-
-                final URI modNs = firstAttributeOf(importedModule.declaredSubstatements(), NamespaceStatement.class);
-                stmt.addToNs(URIStringToImpPrefix.class, modNs.toString(), impPrefix);
-            }
-
-            private Entry<ModuleIdentifier, StmtContext<?, ModuleStatement, EffectiveStatement<String, ModuleStatement>>> findRecentModule(
-                    final ModuleIdentifier impIdentifier,
-                    final Map<ModuleIdentifier, StmtContext<?, ModuleStatement, EffectiveStatement<String, ModuleStatement>>> allModules) {
-
-                ModuleIdentifier recentModuleIdentifier = impIdentifier;
-                Entry<ModuleIdentifier, StmtContext<?, ModuleStatement, EffectiveStatement<String, ModuleStatement>>> recentModuleEntry = null;
-
-                for (Entry<ModuleIdentifier, StmtContext<?, ModuleStatement, EffectiveStatement<String, ModuleStatement>>> moduleEntry : allModules.entrySet()) {
-                    final ModuleIdentifier id = moduleEntry.getKey();
-
-                    if (id.getName().equals(impIdentifier.getName())
-                            && id.getRevision().compareTo(recentModuleIdentifier.getRevision()) > 0) {
-                        recentModuleIdentifier = id;
-                        recentModuleEntry = moduleEntry;
-                    }
-                }
-
-                return recentModuleEntry;
+                StmtContext<?, ?, ?> importedModuleContext = imported.get();
+                Verify.verify(moduleName.equals(importedModuleContext.getStatementArgument()));
+                final URI importedModuleNamespace = importedModuleContext.getFromNamespace(ModuleNameToNamespace.class,
+                        moduleName);
+                Verify.verify(importedModuleNamespace != null);
+                final String impPrefix = firstAttributeOf(stmt.declaredSubstatements(), PrefixStatement.class);
+                Verify.verify(impPrefix != null);
+                stmt.addToNs(ImpPrefixToNamespace.class, impPrefix, importedModuleNamespace);
             }
 
             @Override
-            public void prerequisiteFailed(final Collection<? extends Prerequisite<?>> failed)  {
+            public void prerequisiteFailed(final Collection<? extends Prerequisite<?>> failed) {
                 if (failed.contains(imported)) {
                     throw new InferenceException(stmt.getStatementSourceReference(),
-                        "Imported module [%s] was not found.", impIdentifier);
+                            "Imported module [%s] was not found.", moduleName);
                 }
             }
         });
-
-
     }
 
-    private static ModuleIdentifier getImportedModuleIdentifier(final Mutable<String, ImportStatement, ?> stmt) {
-        Date revision = firstAttributeOf(stmt.declaredSubstatements(), RevisionDateStatement.class);
-        if (revision == null) {
-            revision = SimpleDateFormatUtil.DEFAULT_DATE_IMP;
+    @Override
+    public void onLinkageDeclared(
+            final Mutable<String, ImportStatement, EffectiveStatement<String, ImportStatement>> stmt) {
+        if (stmt.isEnabledSemanticVersioning()) {
+            SemanticVersionImport.onLinkageDeclared(stmt);
+        } else {
+            RevisionImport.onLinkageDeclared(stmt);
+        }
+    }
+
+    private static class RevisionImport {
+
+        private RevisionImport() {
+            throw new UnsupportedOperationException("Utility class");
         }
 
-        return new ModuleIdentifierImpl(stmt.getStatementArgument(), Optional.<URI> absent(),
-                Optional.<Date> of(revision));
+        private static void onLinkageDeclared(
+                final Mutable<String, ImportStatement, EffectiveStatement<String, ImportStatement>> stmt) {
+            final ModuleIdentifier impIdentifier = getImportedModuleIdentifier(stmt);
+            final ModelActionBuilder importAction = stmt.newInferenceAction(SOURCE_LINKAGE);
+            final Prerequisite<StmtContext<?, ?, ?>> imported = importAction.requiresCtx(stmt, ModuleNamespace.class,
+                    impIdentifier, SOURCE_LINKAGE);
+            final Prerequisite<Mutable<?, ?, ?>> linkageTarget = importAction
+                    .mutatesCtx(stmt.getRoot(), SOURCE_LINKAGE);
+
+            importAction.apply(new InferenceAction() {
+                @Override
+                public void apply() {
+                    StmtContext<?, ?, ?> importedModule = null;
+                    ModuleIdentifier importedModuleIdentifier = null;
+                    if (impIdentifier.getRevision() == SimpleDateFormatUtil.DEFAULT_DATE_IMP) {
+                        Entry<ModuleIdentifier, StmtContext<?, ModuleStatement, EffectiveStatement<String, ModuleStatement>>> recentModuleEntry = findRecentModule(
+                                impIdentifier, stmt.getAllFromNamespace(ModuleNamespace.class));
+                        if (recentModuleEntry != null) {
+                            importedModuleIdentifier = recentModuleEntry.getKey();
+                            importedModule = recentModuleEntry.getValue();
+                        }
+                    }
+
+                    if (importedModule == null || importedModuleIdentifier == null) {
+                        importedModule = imported.get();
+                        importedModuleIdentifier = impIdentifier;
+                    }
+
+                    linkageTarget.get().addToNs(ImportedModuleContext.class, importedModuleIdentifier, importedModule);
+                    String impPrefix = firstAttributeOf(stmt.declaredSubstatements(), PrefixStatement.class);
+                    stmt.addToNs(ImpPrefixToModuleIdentifier.class, impPrefix, importedModuleIdentifier);
+
+                    final URI modNs = firstAttributeOf(importedModule.declaredSubstatements(), NamespaceStatement.class);
+                    stmt.addToNs(URIStringToImpPrefix.class, modNs.toString(), impPrefix);
+                }
+
+                @Override
+                public void prerequisiteFailed(final Collection<? extends Prerequisite<?>> failed) {
+                    if (failed.contains(imported)) {
+                        throw new InferenceException(stmt.getStatementSourceReference(),
+                                "Imported module [%s] was not found.", impIdentifier);
+                    }
+                }
+            });
+
+        }
+
+        private static Entry<ModuleIdentifier, StmtContext<?, ModuleStatement, EffectiveStatement<String, ModuleStatement>>> findRecentModule(
+                final ModuleIdentifier impIdentifier,
+                final Map<ModuleIdentifier, StmtContext<?, ModuleStatement, EffectiveStatement<String, ModuleStatement>>> allModules) {
+
+            ModuleIdentifier recentModuleIdentifier = impIdentifier;
+            Entry<ModuleIdentifier, StmtContext<?, ModuleStatement, EffectiveStatement<String, ModuleStatement>>> recentModuleEntry = null;
+
+            for (Entry<ModuleIdentifier, StmtContext<?, ModuleStatement, EffectiveStatement<String, ModuleStatement>>> moduleEntry : allModules
+                    .entrySet()) {
+                final ModuleIdentifier id = moduleEntry.getKey();
+
+                if (id.getName().equals(impIdentifier.getName())
+                        && id.getRevision().compareTo(recentModuleIdentifier.getRevision()) > 0) {
+                    recentModuleIdentifier = id;
+                    recentModuleEntry = moduleEntry;
+                }
+            }
+
+            return recentModuleEntry;
+        }
+
+        private static ModuleIdentifier getImportedModuleIdentifier(final Mutable<String, ImportStatement, ?> stmt) {
+            Date revision = firstAttributeOf(stmt.declaredSubstatements(), RevisionDateStatement.class);
+            if (revision == null) {
+                revision = SimpleDateFormatUtil.DEFAULT_DATE_IMP;
+            }
+
+            return new ModuleIdentifierImpl(stmt.getStatementArgument(), Optional.<URI> absent(),
+                    Optional.<Date> of(revision));
+        }
+    }
+
+    private static class SemanticVersionImport {
+        private SemanticVersionImport() {
+            throw new UnsupportedOperationException("Utility class");
+        }
+
+        private static void onLinkageDeclared(
+                final Mutable<String, ImportStatement, EffectiveStatement<String, ImportStatement>> stmt) {
+            //:TODO change from ModuleIdentifier to module name (String) only
+            final ModuleIdentifier impIdentifier = getImportedModuleIdentifier(stmt);
+            final ModelActionBuilder importAction = stmt.newInferenceAction(SOURCE_LINKAGE);
+            final Prerequisite<StmtContext<?, ?, ?>> imported = importAction.requiresCtx(stmt, ModuleNamespace.class,
+                    impIdentifier, SOURCE_LINKAGE);
+            final Prerequisite<Mutable<?, ?, ?>> linkageTarget = importAction
+                    .mutatesCtx(stmt.getRoot(), SOURCE_LINKAGE);
+
+            importAction.apply(new InferenceAction() {
+                @Override
+                public void apply() {
+                    Entry<ModuleIdentifier, StmtContext<?, ModuleStatement, EffectiveStatement<String, ModuleStatement>>> recentCompatibleModuleEntry = findRecentCompatibleModule(impIdentifier, stmt);
+
+                    StmtContext<?, ?, ?> importedModule = null;
+                    ModuleIdentifier importedModuleIdentifier = null;
+                    if (recentCompatibleModuleEntry != null) {
+                        importedModuleIdentifier = recentCompatibleModuleEntry.getKey();
+                        importedModule = recentCompatibleModuleEntry.getValue();
+                    } else {
+                        throw new InferenceException(stmt.getStatementSourceReference(),
+                                "Unable to find module compatible with requested import [%s(%s)].", impIdentifier.getName(), getRequestedImportVersion(stmt));
+                    }
+
+                    linkageTarget.get().addToNs(ImportedModuleContext.class, importedModuleIdentifier, importedModule);
+                    String impPrefix = firstAttributeOf(stmt.declaredSubstatements(), PrefixStatement.class);
+                    stmt.addToNs(ImpPrefixToModuleIdentifier.class, impPrefix, importedModuleIdentifier);
+
+                    final URI modNs = firstAttributeOf(importedModule.declaredSubstatements(), NamespaceStatement.class);
+                    stmt.addToNs(URIStringToImpPrefix.class, modNs.toString(), impPrefix);
+                }
+
+                @Override
+                public void prerequisiteFailed(final Collection<? extends Prerequisite<?>> failed) {
+                    if (failed.contains(imported)) {
+                        throw new InferenceException(stmt.getStatementSourceReference(),
+                                "Unable to find module compatible with requested import [%s(%s)].", impIdentifier.getName(), getRequestedImportVersion(stmt));
+                    }
+                }
+            });
+        }
+
+        private static SemVer getRequestedImportVersion(Mutable<?, ?, ?> impStmt) {
+            SemVer requestedImportVersion = StmtContextUtils.firstAttributeOf(impStmt.declaredSubstatements(),
+                    SemanticVersionStatementImpl.class);
+            if (requestedImportVersion == null) {
+                requestedImportVersion = SemVer.valueOf("0.0.0");
+            }
+            return requestedImportVersion;
+        }
+
+        private static Entry<ModuleIdentifier, StmtContext<?, ModuleStatement, EffectiveStatement<String, ModuleStatement>>> findRecentCompatibleModule(
+                final ModuleIdentifier impIdentifier,
+                final Mutable<String, ImportStatement, EffectiveStatement<String, ImportStatement>> impStmt) {
+            final Map<ModuleIdentifier, StmtContext<?, ModuleStatement, EffectiveStatement<String, ModuleStatement>>> allModules = impStmt
+                    .getAllFromNamespace(ModuleNamespace.class);
+            final Map<StmtContext<?, ?, ?>, SemVer> semanticVersionsMap = impStmt
+                    .getAllFromNamespace(SemanticVersionNamespace.class);
+            final String requestedImportModuleName = impIdentifier.getName();
+            final SemVer requestedImportVersion = getRequestedImportVersion(impStmt);
+
+            //:TODO change the namespaces in order this look will be working more efficient
+            //:TODO use default version 0.0.0 for modules without semantic version
+            SemVer recentCompatibleVersion = requestedImportVersion;
+            Entry<ModuleIdentifier, StmtContext<?, ModuleStatement, EffectiveStatement<String, ModuleStatement>>> recentCompatibleModuleEntry = null;
+            for (Entry<ModuleIdentifier, StmtContext<?, ModuleStatement, EffectiveStatement<String, ModuleStatement>>> moduleEntry : allModules
+                    .entrySet()) {
+                final String moduleName = moduleEntry.getKey().getName();
+                final SemVer moduleVersion = semanticVersionsMap.get(moduleEntry.getValue());
+                if (moduleName.equals(requestedImportModuleName)
+                        && moduleVersion.isCompatibleWith(requestedImportVersion)
+                        && moduleVersion.compareTo(recentCompatibleVersion) >= 0) {
+                    recentCompatibleVersion = moduleVersion;
+                    recentCompatibleModuleEntry = moduleEntry;
+                }
+            }
+            return recentCompatibleModuleEntry;
+        }
+
+        private static ModuleIdentifier getImportedModuleIdentifier(final Mutable<String, ImportStatement, ?> impStmt) {
+            return new ModuleIdentifierImpl(impStmt.getStatementArgument(), Optional.<URI> absent(),
+                    Optional.<Date> of(SimpleDateFormatUtil.DEFAULT_DATE_IMP));
+        }
     }
 
 }
