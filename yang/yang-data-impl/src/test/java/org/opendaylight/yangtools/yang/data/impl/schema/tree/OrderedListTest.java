058a97277192056c0fe679fa08e56599af5939fd
Igor Foltin <ifoltin@cisco.com>
2016-02-05 13:58:34 +0100

    Bug 3899: Milestone: Increase test coverage for Yangtools
    Added unit tests for data tree operations with ordered list.
    
    Change-Id: I82314d98fbb04ad336de9e7a15a433ab78454180
    Signed-off-by: Igor Foltin <ifoltin@cisco.com>

---
+++ yang/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/OrderedListTest.java
@@ -0,0 +1,282 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.data.impl.schema.tree;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import com.google.common.collect.ImmutableMap;
+import java.io.File;
+import java.net.URI;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.common.SimpleDateFormatUtil;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.OrderedMapNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.ConflictingModificationAppliedException;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeModification;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataValidationFailedException;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.TipProducingDataTree;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.TreeType;
+import org.opendaylight.yangtools.yang.data.impl.RetestUtils;
+import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OrderedListTest {
+    private Logger LOG = LoggerFactory.getLogger(OrderedListTest.class);
+
+    private TipProducingDataTree inMemoryDataTree;
+    private SchemaContext context;
+
+    private QNameModule testModule;
+    private QName parentContainer;
+    private QName childContainer;
+    private QName parentOrderedList;
+    private QName childOrderedList;
+    private QName parentKeyLeaf;
+    private QName parentOrdinaryLeaf;
+    private QName childKeyLeaf;
+    private QName childOrdinaryLeaf;
+
+    @Before
+    public void setup() throws Exception {
+        final File resourceFile = new File(Bug4295Test.class.getResource("/ordered-list-modification-test.yang")
+                .toURI());
+        context = RetestUtils.parseYangSources(resourceFile);
+        testModule = QNameModule.create(new URI("ordered-list-modification-test"),
+                SimpleDateFormatUtil.getRevisionFormat().parse("1970-01-01"));
+        parentContainer = QName.create(testModule, "parent-container");
+        childContainer = QName.create(testModule, "child-container");
+        parentOrderedList = QName.create(testModule, "parent-ordered-list");
+        childOrderedList = QName.create(testModule, "child-ordered-list");
+        parentKeyLeaf = QName.create(testModule, "parent-key-leaf");
+        childKeyLeaf = QName.create(testModule, "child-key-leaf");
+        parentOrdinaryLeaf = QName.create(testModule, "parent-ordinary-leaf");
+        childOrdinaryLeaf = QName.create(testModule, "child-ordinary-leaf");
+        inMemoryDataTree = InMemoryDataTreeFactory.getInstance().create(TreeType.OPERATIONAL);
+        inMemoryDataTree.setSchemaContext(context);
+    }
+
+    @Test
+    public void testsequentialModifications() throws DataValidationFailedException {
+        System.out.println(inMemoryDataTree);
+        modification1();
+        System.out.println(inMemoryDataTree);
+        // este treba spravit update jednej childOrderedListEntry v ramci druhej parentOrderedListEntry
+        modification2();
+        System.out.println(inMemoryDataTree);
+        // deletnem najprv jednu childOrderedListEntry v druhom parentOrderedListEntry a potom deletnem celu druhu
+        // parentOrderedListEntry
+        delete1();
+        System.out.println(inMemoryDataTree);
+        delete2();
+        System.out.println(inMemoryDataTree);
+        modification3();
+        System.out.println(inMemoryDataTree);
+        modification4();
+        System.out.println(inMemoryDataTree);
+
+    }
+
+    public void modification1() throws DataValidationFailedException {
+        OrderedMapNode parentOrderedListNode = Builders.orderedMapBuilder().withNodeIdentifier(
+                new NodeIdentifier(parentOrderedList))
+                .withChild(createParentOrderedListEntry("pkval1", "plfval1"))
+                .withChild(createParentOrderedListEntry("pkval2", "plfval2"))
+                .withChild(createParentOrderedListEntry("pkval3", "plfval3")).build();
+
+        ContainerNode parentContainerNode = Builders.containerBuilder().withNodeIdentifier(
+                new NodeIdentifier(parentContainer)).withChild(Builders.containerBuilder()
+                .withNodeIdentifier(new NodeIdentifier(childContainer)).withChild(parentOrderedListNode).build())
+                .build();
+
+        YangInstanceIdentifier path = YangInstanceIdentifier.of(parentContainer);
+
+        DataTreeModification treeModification = inMemoryDataTree.takeSnapshot().newModification();
+        treeModification.write(path, parentContainerNode);
+
+        OrderedMapNode childOrderedListNode = Builders.orderedMapBuilder().withNodeIdentifier(
+                new NodeIdentifier(childOrderedList))
+                .withChild(createChildOrderedListEntry("chkval1", "chlfval1"))
+                .withChild(createChildOrderedListEntry("chkval2", "chlfval2")).build();
+
+        path = YangInstanceIdentifier.of(parentContainer).node(childContainer).node(parentOrderedList)
+                .node(createParentOrderedListEntryPath("pkval2")).node(childOrderedList);
+
+        treeModification.write(path, childOrderedListNode);
+        treeModification.ready();
+        inMemoryDataTree.validate(treeModification);
+        inMemoryDataTree.commit(inMemoryDataTree.prepare(treeModification));
+    }
+
+    public void modification2() throws DataValidationFailedException {
+        OrderedMapNode parentOrderedListNode = Builders.orderedMapBuilder().withNodeIdentifier(
+                new NodeIdentifier(parentOrderedList))
+                .withChild(createParentOrderedListEntry("pkval3", "plfval3updated"))
+                .withChild(createParentOrderedListEntry("pkval4", "plfval4"))
+                .withChild(createParentOrderedListEntry("pkval5", "plfval5")).build();
+
+        ContainerNode parentContainerNode = Builders.containerBuilder().withNodeIdentifier(
+                new NodeIdentifier(parentContainer)).withChild(Builders.containerBuilder()
+                .withNodeIdentifier(new NodeIdentifier(childContainer)).withChild(parentOrderedListNode).build())
+                .build();
+
+        DataTreeModification treeModification = inMemoryDataTree.takeSnapshot().newModification();
+
+        YangInstanceIdentifier path = YangInstanceIdentifier.of(parentContainer);
+        treeModification.merge(path, parentContainerNode);
+
+        OrderedMapNode childOrderedListNode = Builders.orderedMapBuilder().withNodeIdentifier(
+                new NodeIdentifier(childOrderedList))
+                .withChild(createChildOrderedListEntry("chkval1", "chlfval1updated"))
+                .withChild(createChildOrderedListEntry("chkval2", "chlfval2updated"))
+                .withChild(createChildOrderedListEntry("chkval3", "chlfval3")).build();
+
+        path = YangInstanceIdentifier.of(parentContainer).node(childContainer).node(parentOrderedList)
+                .node(createParentOrderedListEntryPath("pkval2")).node(childOrderedList);
+        treeModification.merge(path, childOrderedListNode);
+
+        treeModification.ready();
+        inMemoryDataTree.validate(treeModification);
+        inMemoryDataTree.commit(inMemoryDataTree.prepare(treeModification));
+    }
+
+    public void modification3() throws DataValidationFailedException {
+        OrderedMapNode parentOrderedListNode = Builders.orderedMapBuilder().withNodeIdentifier(
+                new NodeIdentifier(parentOrderedList))
+                .withChild(createParentOrderedListEntry("pkval1", "plfval1")).build();
+
+        ContainerNode parentContainerNode = Builders.containerBuilder().withNodeIdentifier(
+                new NodeIdentifier(parentContainer)).withChild(Builders.containerBuilder()
+                .withNodeIdentifier(new NodeIdentifier(childContainer)).withChild(parentOrderedListNode).build())
+                .build();
+
+        YangInstanceIdentifier path = YangInstanceIdentifier.of(parentContainer);
+
+        DataTreeModification treeModification = inMemoryDataTree.takeSnapshot().newModification();
+        treeModification.write(path, parentContainerNode);
+
+        OrderedMapNode childOrderedListNode = Builders.orderedMapBuilder().withNodeIdentifier(
+                new NodeIdentifier(childOrderedList))
+                .withChild(createChildOrderedListEntry("chkval1", "chlfval1new")).build();
+
+        path = YangInstanceIdentifier.of(parentContainer).node(childContainer).node(parentOrderedList)
+                .node(createParentOrderedListEntryPath("pkval4")).node(childOrderedList);
+
+        treeModification.merge(path, childOrderedListNode);
+
+        try {
+            treeModification.ready();
+            fail("Exception should have been thrown.");
+            inMemoryDataTree.validate(treeModification);
+            inMemoryDataTree.commit(inMemoryDataTree.prepare(treeModification));
+        } catch (final IllegalArgumentException ex) {
+            LOG.debug("IllegalArgumentException was thrown as expected: {}", ex);
+            assertTrue(ex.getMessage().contains("Metadata not available for modification NodeModification"));
+        }
+    }
+
+    public void modification4() throws DataValidationFailedException {
+        DataTreeModification treeModification1 = inMemoryDataTree.takeSnapshot().newModification();
+        DataTreeModification treeModification2 = inMemoryDataTree.takeSnapshot().newModification();
+
+        OrderedMapNode parentOrderedListNode = Builders.orderedMapBuilder().withNodeIdentifier(new
+                NodeIdentifier(parentOrderedList)).withChild(createParentOrderedListEntry("pkval1",
+                "plfval1")).build();
+
+        OrderedMapNode parentOrderedListNode2 = Builders.orderedMapBuilder().withNodeIdentifier(new
+                NodeIdentifier(parentOrderedList)).withChild(createParentOrderedListEntry("pkval2",
+                "plfval2")).build();
+
+        ContainerNode parentContainerNode = Builders.containerBuilder().withNodeIdentifier(
+                new NodeIdentifier(parentContainer)).withChild(Builders.containerBuilder()
+                .withNodeIdentifier(new NodeIdentifier(childContainer)).withChild(parentOrderedListNode).build()).build();
+
+        ContainerNode parentContainerNode2 = Builders.containerBuilder().withNodeIdentifier(
+                new NodeIdentifier(parentContainer)).withChild(Builders.containerBuilder()
+                .withNodeIdentifier(new NodeIdentifier(childContainer)).withChild(parentOrderedListNode2).build())
+                .build();
+
+        YangInstanceIdentifier path = YangInstanceIdentifier.of(parentContainer);
+
+        treeModification1.write(path, parentContainerNode);
+        treeModification2.write(path, parentContainerNode2);
+        treeModification1.ready();
+        treeModification2.ready();
+
+        inMemoryDataTree.validate(treeModification1);
+        inMemoryDataTree.commit(inMemoryDataTree.prepare(treeModification1));
+
+        try {
+            inMemoryDataTree.validate(treeModification2);
+            fail("Exception should have been thrown.");
+            inMemoryDataTree.commit(inMemoryDataTree.prepare(treeModification2));
+        } catch (ConflictingModificationAppliedException ex) {
+            LOG.debug("ConflictingModificationAppliedException was thrown as expected: {}", ex);
+            assertTrue(ex.getMessage().contains("Node was replaced by other transaction"));
+        }
+    }
+
+    public void delete1() throws DataValidationFailedException {
+        YangInstanceIdentifier path = YangInstanceIdentifier.of(parentContainer).node(childContainer)
+                .node(parentOrderedList).node(createParentOrderedListEntryPath("pkval2")).node(childOrderedList)
+                .node(createChildOrderedListEntryPath("chkval1"));
+
+        DataTreeModification treeModification = inMemoryDataTree.takeSnapshot().newModification();
+        treeModification.delete(path);
+        treeModification.ready();
+        inMemoryDataTree.validate(treeModification);
+        inMemoryDataTree.commit(inMemoryDataTree.prepare(treeModification));
+    }
+
+    public void delete2() throws DataValidationFailedException {
+        YangInstanceIdentifier path = YangInstanceIdentifier.of(parentContainer).node(childContainer)
+                .node(parentOrderedList).node(createParentOrderedListEntryPath("pkval2"));
+
+        DataTreeModification treeModification = inMemoryDataTree.takeSnapshot().newModification();
+        treeModification.delete(path);
+        treeModification.ready();
+        inMemoryDataTree.validate(treeModification);
+        inMemoryDataTree.commit(inMemoryDataTree.prepare(treeModification));
+    }
+
+    private MapEntryNode createParentOrderedListEntry(String keyValue, String leafValue) {
+        return Builders.mapEntryBuilder().withNodeIdentifier(new NodeIdentifierWithPredicates(parentOrderedList,
+                parentKeyLeaf, keyValue))
+                .withChild(Builders.leafBuilder().withNodeIdentifier(NodeIdentifier.create(parentOrdinaryLeaf)).withValue
+                        (leafValue).build()).build();
+    }
+
+    private MapEntryNode createChildOrderedListEntry(String keyValue, String leafValue) {
+        return Builders.mapEntryBuilder().withNodeIdentifier(new NodeIdentifierWithPredicates(childOrderedList,
+                childKeyLeaf, keyValue))
+                .withChild(Builders.leafBuilder().withNodeIdentifier(NodeIdentifier.create(childOrdinaryLeaf)).withValue
+                        (leafValue).build()).build();
+    }
+
+    private NodeIdentifierWithPredicates createParentOrderedListEntryPath(String keyValue) {
+        ImmutableMap.Builder<QName, Object> builder = ImmutableMap.builder();
+        ImmutableMap<QName, Object> keys = builder.put(parentKeyLeaf, keyValue).build();
+        return new NodeIdentifierWithPredicates(parentOrderedList, keys);
+    }
+
+    private NodeIdentifierWithPredicates createChildOrderedListEntryPath(String keyValue) {
+        ImmutableMap.Builder<QName, Object> builder = ImmutableMap.builder();
+        ImmutableMap<QName, Object> keys = builder.put(childKeyLeaf, keyValue).build();
+        return new NodeIdentifierWithPredicates(childOrderedList, keys);
+    }
+}
