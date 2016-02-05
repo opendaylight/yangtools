e1aa397ca1776e98a4c7fd180ab48ad38fb36ea9
Jakub Toth <jatoth@cisco.com>
2016-02-05 17:02:14 +0100

    test XmlDocumentUtilsTest
    Change-Id: I8d4958b048e1bf392182b409d4998429712781b6
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

--- yang/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/codec/xml/XmlDocumentUtilsTest.java
+++ yang/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/codec/xml/XmlDocumentUtilsTest.java
@@ -9,24 +9,56 @@
 package org.opendaylight.yangtools.yang.data.impl.codec.xml;
 
 import com.google.common.base.Charsets;
+import com.google.common.base.Optional;
 import com.google.common.collect.Lists;
 import com.google.common.io.ByteSource;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.ModifyAction;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.impl.codec.TypeDefinitionAwareCodec;
+import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.DataContainerNodeBuilder;
+import org.opendaylight.yangtools.yang.model.api.ChoiceCaseNode;
+import org.opendaylight.yangtools.yang.model.api.ChoiceSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang.model.api.RpcDefinition;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.BinaryTypeDefinition;
 import org.opendaylight.yangtools.yang.parser.impl.YangParserImpl;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
+@RunWith(MockitoJUnitRunner.class)
 public class XmlDocumentUtilsTest {
-
+    private final String NS = "urn:opendaylight:controller:rpc:test";
+    private final String NS2 = "urn:opendaylight:controller:rpc:test2";
+    private final String NS3 = "urn:opendaylight:controller:rpc:test3";
+    private final String REVISION = "2014-07-28";
     private static final DocumentBuilderFactory BUILDERFACTORY;
 
     static {
@@ -60,9 +92,16 @@ public class XmlDocumentUtilsTest {
                 return XmlDocumentUtilsTest.this.getClass().getResourceAsStream("rpc-test.yang");
             }
         };
-        schema = new YangParserImpl().parseSources(Lists.newArrayList(byteSource));
-        final Module rpcTestModule = schema.getModules().iterator().next();
-        testRpc = rpcTestModule.getRpcs().iterator().next();
+        final ByteSource byteSource2 = new ByteSource() {
+
+            @Override
+            public InputStream openStream() throws IOException {
+                return XmlDocumentUtilsTest.this.getClass().getResourceAsStream("rpc-test2.yang");
+            }
+        };
+        this.schema = new YangParserImpl().parseSources(Lists.newArrayList(byteSource, byteSource2));
+        final Module rpcTestModule = this.schema.getModules().iterator().next();
+        this.testRpc = rpcTestModule.getRpcs().iterator().next();
     }
 
     public static Document readXmlToDocument(final String xmlContent) throws SAXException, IOException {
@@ -80,5 +119,126 @@ public class XmlDocumentUtilsTest {
 
         doc.getDocumentElement().normalize();
         return doc;
+    }
+
+    @Test
+    public void xmlDocCreateElementForContWithoutAttrTest() throws Exception {
+        final Document doc = readXmlToDocument(XML_CONTENT);
+        final YangInstanceIdentifier.NodeIdentifier container = new YangInstanceIdentifier.NodeIdentifier(
+                QName.create(this.NS, this.REVISION, "cont"));
+
+        final NormalizedNode<?, ?> data = ImmutableNodes.fromInstanceId(this.schema,
+                YangInstanceIdentifier.create(container));
+        Assert.assertNotNull(data);
+
+        final Element element = XmlDocumentUtils.createElementFor(doc, data);
+        Assert.assertNotNull(element);
+        Assert.assertEquals(element.getTagName(), "cont");
+    }
+
+    @Test
+    public void xmlDocCreateElementForContiWithAttrTest() throws Exception {
+        final Document doc = readXmlToDocument(XML_CONTENT);
+        final YangInstanceIdentifier.NodeIdentifier container = new YangInstanceIdentifier.NodeIdentifier(
+                QName.create(this.NS, this.REVISION, "cont"));
+
+        final Map<QName, String> attributes = new HashMap<>();
+        attributes.put(QName.create(this.NS, this.REVISION, "l"), "list");
+
+        final ContainerNode node = Builders.containerBuilder().withNodeIdentifier(container).withAttributes(attributes)
+                .build();
+        final NormalizedNode<?, ?> data = node;
+        Assert.assertNotNull(data);
+
+        final Element element = XmlDocumentUtils.createElementFor(doc, data);
+        Assert.assertNotNull(element);
+        Assert.assertEquals("cont", element.getTagName());
+        Assert.assertEquals("list", element.getAttribute("l"));
+    }
+
+    @Test
+    public void xmlDocgetModifyOperationWithAttributesTest() throws Exception {
+        final Document doc = readXmlToDocument(XML_CONTENT);
+        final YangInstanceIdentifier.NodeIdentifier container = new YangInstanceIdentifier.NodeIdentifier(
+                QName.create(this.NS, this.REVISION, "cont"));
+
+        final Map<QName, String> attributes = new HashMap<>();
+        attributes.put(QName.create(this.NS, this.REVISION, "l"), "list");
+
+        final ContainerNode node = Builders.containerBuilder().withNodeIdentifier(container).withAttributes(attributes)
+                .build();
+        final NormalizedNode<?, ?> data = node;
+        Assert.assertNotNull(data);
+
+        final Element element = XmlDocumentUtils.createElementFor(doc, data);
+        final Optional<ModifyAction> modifyOperationFromAttributes = XmlDocumentUtils
+                .getModifyOperationFromAttributes(element);
+    }
+
+    @Test
+    public void xmlDocgetModifyOperationWithoutAttributesTest() {
+        final Element element = Mockito.mock(Element.class);
+        Mockito.when(element.getAttributeNS(Mockito.anyString(), Mockito.anyString())).thenReturn("hello");
+
+        final Optional<ModifyAction> modifyOperationFromAttributes = XmlDocumentUtils
+                .getModifyOperationFromAttributes(element);
+        Assert.assertFalse(modifyOperationFromAttributes.isPresent());
+    }
+
+    @Test
+    public void xmlDocFindFirstSchemaContTest() {
+        final QName ID = QName.create(this.NS, this.REVISION, "cont");
+        final Optional<DataSchemaNode> findFirstSchema = XmlDocumentUtils.findFirstSchema(ID,
+                this.schema.getChildNodes());
+        Assert.assertNotNull(findFirstSchema);
+        Assert.assertEquals(ID, findFirstSchema.get().getQName());
+    }
+
+    @Test
+    public void xmlDocFindFirstSchemaCont2Test() {
+        final QName ID = QName.create(this.NS2, this.REVISION, "cont2");
+        final Optional<DataSchemaNode> findFirstSchema = XmlDocumentUtils.findFirstSchema(ID,
+                this.schema.getChildNodes());
+        final DataSchemaNode dataSchemaNode = findFirstSchema.get();
+        Assert.assertNotNull(findFirstSchema);
+        Assert.assertEquals(ID, dataSchemaNode.getQName());
+    }
+
+    @Test
+    public void xmlDocFindFirstSchemaNullParamsTest() {
+        Optional<DataSchemaNode> findFirstSchema = XmlDocumentUtils.findFirstSchema(null,
+                this.schema.getChildNodes());
+        Assert.assertFalse(findFirstSchema.isPresent());
+
+        final QName ID = QName.create(this.NS2, this.REVISION, "cont2");
+        findFirstSchema = XmlDocumentUtils.findFirstSchema(ID, null);
+        Assert.assertFalse(findFirstSchema.isPresent());
+    }
+
+    @Test
+    public void xmlDocFindFirstSchemaContChoicTest() {
+        final QName ID = QName.create(this.NS3, this.REVISION, "cont");
+        final DataContainerNodeBuilder<YangInstanceIdentifier.NodeIdentifier, ContainerNode> builder = Builders
+                .containerBuilder();
+        final DataSchemaNode sch = Mockito.mock(ChoiceSchemaNode.class);
+        final Set<ChoiceCaseNode> setCases = new HashSet<>();
+        final ChoiceCaseNode choice = Mockito.mock(ChoiceCaseNode.class);
+        setCases.add(choice);
+        Mockito.when(((ChoiceSchemaNode) sch).getCases()).thenReturn(setCases);
+        Mockito.when(sch.getQName()).thenReturn(QName.create("badNamespace", "badLocalName"));
+        final Iterable<DataSchemaNode> colls = Lists.newArrayList(sch);
+        final Optional<DataSchemaNode> findFirstSchema = XmlDocumentUtils.findFirstSchema(ID, colls);
+        Assert.assertNotNull(findFirstSchema);
+    }
+
+    @Test
+    public void codecProviderTest() {
+        final XmlCodecProvider provider = XmlDocumentUtils.defaultValueCodecProvider();
+        Assert.assertNotNull(provider);
+        Assert.assertEquals(XmlUtils.DEFAULT_XML_CODEC_PROVIDER, provider);
+        
+        final TypeDefinition<?> baseType = Mockito.mock(BinaryTypeDefinition.class);
+        final TypeDefinitionAwareCodec<Object, ? extends TypeDefinition<?>> codec = provider.codecFor(baseType);
+        Assert.assertNotNull(codec);
     }
 }
