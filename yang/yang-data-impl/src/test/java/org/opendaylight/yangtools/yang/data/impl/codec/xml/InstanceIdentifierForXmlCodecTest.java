0482df2006d9dc0762794455025ee9de58677585
Jakub Toth <jatoth@cisco.com>
2016-02-08 16:08:11 +0100

    Bug 3899: Milestone: Increase test coverage for Yangtools
    test InstanceIdentifierForXmlCodecTest
    
    Change-Id: I3dc7f58309ae900825a10d5ea0a878cf037c24e5
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ yang/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/codec/xml/InstanceIdentifierForXmlCodecTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.data.impl.codec.xml;
+
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.impl.RetestUtils;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+@RunWith(MockitoJUnitRunner.class)
+public class InstanceIdentifierForXmlCodecTest {
+
+    @Mock
+    private Element element;
+
+    private SchemaContext schemaContext;
+
+    public static final String XML_CONTENT = "<input xmlns=\"urn:opendaylight:controller:iid:test\">" + "<a>value</a>"
+            + "</input>";
+
+    private final String NS = "urn:opendaylight:controller:iid:test";
+
+    private final String REVISION = "2014-07-28";
+
+    @Before
+    public void setup() throws Exception {
+        final File rpcTestYang = new File(getClass().getResource("iid-test.yang").toURI());
+        this.schemaContext = RetestUtils.parseYangSources(rpcTestYang);
+    }
+
+    @Test
+    public void deserializeTest() {
+        Mockito.when(this.element.getTextContent()).thenReturn("");
+        final YangInstanceIdentifier deserialize = InstanceIdentifierForXmlCodec.deserialize(this.element,
+                this.schemaContext);
+        Assert.assertEquals("/", deserialize.toString());
+    }
+
+    @Test
+    public void serializeTest() throws Exception {
+        final Document doc = XmlDocumentUtilsTest.readXmlToDocument(XML_CONTENT);
+        final YangInstanceIdentifier.NodeIdentifier container = new YangInstanceIdentifier.NodeIdentifier(
+                QName.create("", this.REVISION, "cont"));
+        final NormalizedNode<?, ?> data = ImmutableNodes.fromInstanceId(this.schemaContext,
+                YangInstanceIdentifier.create(container));
+        Assert.assertNotNull(data);
+
+        final Element element = XmlDocumentUtils.createElementFor(doc, data);
+        final QName name = QName.create(this.NS, this.REVISION, "cont");
+        final YangInstanceIdentifier id = YangInstanceIdentifier.builder().node(name).build();
+        
+        final Element el = InstanceIdentifierForXmlCodec.serialize(id, element, this.schemaContext);
+        Assert.assertEquals(element, el);
+    }
+}
