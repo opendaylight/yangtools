48077a8d276134fda6c5c4b219518ab848d1719e
Jakub Toth <jatoth@cisco.com>
2016-02-09 16:19:01 +0100

    Bug 3899: Milestone: Increase test coverage for Yangtools
    SchemaSourceTransformerTest
    
    Change-Id: I12892be51e74a37faaa7cc513385229f208018e6
    Signed-off-by: Jakub Toth <jatoth@cisco.com>

---
+++ yang/yang-model-util/src/test/java/org/opendaylight/yangtools/yang/model/repo/util/SchemaSourceTransformerTest.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.repo.util;
+
+import com.google.common.util.concurrent.AsyncFunction;
+import com.google.common.util.concurrent.CheckedFuture;
+import java.util.Arrays;
+import java.util.concurrent.Future;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.yangtools.yang.model.repo.api.SchemaContextFactory;
+import org.opendaylight.yangtools.yang.model.repo.api.SchemaRepository;
+import org.opendaylight.yangtools.yang.model.repo.api.SchemaSourceException;
+import org.opendaylight.yangtools.yang.model.repo.api.SchemaSourceFilter;
+import org.opendaylight.yangtools.yang.model.repo.api.SchemaSourceRepresentation;
+import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
+import org.opendaylight.yangtools.yang.model.repo.api.YangSchemaSourceRepresentation;
+import org.opendaylight.yangtools.yang.model.repo.api.YinXmlSchemaSource;
+import org.opendaylight.yangtools.yang.model.repo.spi.PotentialSchemaSource;
+import org.opendaylight.yangtools.yang.model.repo.spi.PotentialSchemaSource.Costs;
+import org.opendaylight.yangtools.yang.model.repo.spi.SchemaSourceListener;
+import org.opendaylight.yangtools.yang.model.repo.spi.SchemaSourceProvider;
+import org.opendaylight.yangtools.yang.model.repo.spi.SchemaSourceRegistry;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SchemaSourceTransformerTest {
+
+    private SchemaSourceTransformer<YangSchemaSourceRepresentation, YinXmlSchemaSource> schema;
+    private final Class<YangSchemaSourceRepresentation> srcClass = YangSchemaSourceRepresentation.class;
+    private final Class<YinXmlSchemaSource> dstClass = YinXmlSchemaSource.class;
+    
+    @Mock
+    private SchemaRepository provider;
+
+    @Mock
+    private SchemaSourceRegistry consumer;
+
+    @Mock
+    private AsyncFunction<YangSchemaSourceRepresentation, YinXmlSchemaSource> function;
+
+    @Test
+    public void schemaSourceTransformerTest() {
+        this.schema = new SchemaSourceTransformer<YangSchemaSourceRepresentation, YinXmlSchemaSource>(
+                this.provider, this.srcClass, this.consumer, this.dstClass, this.function);
+        Assert.assertNotNull(this.schema);
+    }
+
+    @Test
+    public void schemaSourceTransformerGetSourceTest() throws Exception {
+        final Provider p = new Provider();
+        final Registrator reg = new Registrator(p, this.srcClass, PotentialSchemaSource.Costs.IMMEDIATE);
+        final SourceIdentifier sourceIdentifier = new SourceIdentifier("source");
+        reg.register(sourceIdentifier);
+        this.schema = new SchemaSourceTransformer<YangSchemaSourceRepresentation, YinXmlSchemaSource>(p,
+                this.srcClass, this.consumer, this.dstClass, this.function);
+        final SchemaSourceProvider<YinXmlSchemaSource> provider = this.schema;
+        final Future<? extends YinXmlSchemaSource> source = provider.getSource(sourceIdentifier);
+        Assert.assertNotNull(source);
+        source.cancel(true);
+        Assert.assertTrue(source.isDone());
+    }
+
+    @Test
+    public void schemaSourceRegAndUnregSchemaSourceTest() throws Exception {
+        final SourceIdentifier sourceIdentifier = new SourceIdentifier("source");
+        final Foo<YangSchemaSourceRepresentation> foo = new Foo<>(sourceIdentifier, this.srcClass,
+                PotentialSchemaSource.Costs.COMPUTATION);
+        final Provider p = new Provider();
+
+        final Registrator reg = new Registrator(p, this.srcClass, PotentialSchemaSource.Costs.IMMEDIATE);
+        reg.register(sourceIdentifier);
+
+        final Consumer c = new Consumer();
+        this.schema = new SchemaSourceTransformer<YangSchemaSourceRepresentation, YinXmlSchemaSource>(p, this.srcClass,
+                c, this.dstClass, this.function);
+
+        final SchemaSourceListener listener = this.schema;
+        p.registerSchemaSourceListener(listener);
+
+        final PotentialSchemaSource<?>[] potList = { foo.getPotentialSchemSource() };
+        final Iterable<PotentialSchemaSource<?>> sources = Arrays.asList(potList);
+        listener.schemaSourceRegistered(sources);
+        final CheckedFuture<YinXmlSchemaSource, SchemaSourceException> source = this.schema.getSource(sourceIdentifier);
+        Assert.assertNotNull(source);
+
+        listener.schemaSourceUnregistered(foo.getPotentialSchemSource());
+        final CheckedFuture<YinXmlSchemaSource, SchemaSourceException> source2 = this.schema
+                .getSource(sourceIdentifier);
+        Assert.assertNotNull(source2);
+    }
+
+    private class Foo<T extends SchemaSourceRepresentation> {
+        
+        final PotentialSchemaSource<T> src;
+
+        public Foo(final SourceIdentifier sourceIdentifier, final Class<T> representation, final Costs cost) {
+            this.src = PotentialSchemaSource.create(sourceIdentifier, representation,
+                    cost.getValue());
+        }
+
+        public PotentialSchemaSource<T> getPotentialSchemSource() {
+            return this.src;
+        }
+
+    }
+
+
+    private class Registrator extends AbstractSchemaSourceCache<YangSchemaSourceRepresentation> {
+
+        protected Registrator(final SchemaSourceRegistry consumer, final Class<YangSchemaSourceRepresentation> srcClass,
+                final Costs cost) {
+            super(consumer, srcClass, cost);
+        }
+
+        @Override
+        protected void offer(final YangSchemaSourceRepresentation source) {
+
+        }
+
+        @Override
+        public CheckedFuture<? extends YangSchemaSourceRepresentation, SchemaSourceException> getSource(
+                final SourceIdentifier sourceIdentifier) {
+            return Mockito.mock(CheckedFuture.class);
+        }
+
+    }
+
+    private class Provider extends AbstractSchemaRepository {
+
+        @Override
+        public SchemaContextFactory createSchemaContextFactory(final SchemaSourceFilter filter) {
+            return null;
+        }
+
+    }
+
+    private class Consumer extends AbstractSchemaRepository {
+
+        @Override
+        public SchemaContextFactory createSchemaContextFactory(final SchemaSourceFilter filter) {
+            return null;
+        }
+
+    }
+}
