8ca68826d8fc80bff49c9efed7b6c82faba95f6e
Filip Gregor <fgregor@cisco.com>
2016-02-15 14:48:14 +0100

    Bug 3899: Milestone Increase test coverage for YangTools - Path
    added tests for pathargument, yang-common
    
    Change-Id: I84b6b3f8e9b495b49241d983168846314569aa02
    Signed-off-by: Filip Gregor <fgregor@cisco.com>

--- yang/yang-common/src/test/java/org/opendaylight/yangtools/yang/common/QNameTest.java
+++ yang/yang-common/src/test/java/org/opendaylight/yangtools/yang/common/QNameTest.java
@@ -8,11 +8,14 @@
 package org.opendaylight.yangtools.yang.common;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import com.google.common.util.concurrent.ListenableFuture;
 import java.net.URI;
-
+import java.net.URISyntaxException;
+import java.util.Date;
 import org.junit.Test;
 
 public class QNameTest {
@@ -110,13 +113,77 @@ public class QNameTest {
         assertTrue(b.compareTo(a) == 0);
     }
 
+    @Test
+    public void testQName() {
+        final QName qName = QName.create(namespace, revision, localName);
+        final QName qName1 = QName.create(namespace, localName);
+        final QName qName2 = QName.create(qName1, localName);
+        assertEquals(qName1, qName.withoutRevision());
+        assertEquals(qName1, qName2);
+        assertTrue(qName.isEqualWithoutRevision(qName1));
+        assertNotNull(QName.formattedRevision(new Date()));
+        assertNotNull(qName.hashCode());
+        assertEquals(qName, qName.intern());
+    }
+
+    @Test
+    public void testQNameModule() {
+        final QNameModule qNameModule = QNameModule.create(ns, new Date());
+        assertNotNull(qNameModule.toString());
+        assertNotNull(qNameModule.getRevisionNamespace());
+    }
+
+    @Test
+    public void testYangConstants() {
+        final URI uriYang = YangConstants.RFC6020_YANG_NAMESPACE;
+        final URI uriYin = YangConstants.RFC6020_YIN_NAMESPACE;
+        assertEquals(URI.create("urn:ietf:params:xml:ns:yang:1"), uriYang);
+        assertEquals(URI.create("urn:ietf:params:xml:ns:yang:yin:1"), uriYin);
+        assertEquals(QNameModule.create(uriYang, null).intern(), YangConstants.RFC6020_YANG_MODULE);
+        assertEquals(QNameModule.create(uriYin, null).intern(), YangConstants.RFC6020_YIN_MODULE);
+    }
+
+    @Test
+    public void testRpcResultBuilder () {
+        final RpcResultBuilder<Object> rpcResultBuilder = RpcResultBuilder.status(true);
+        final RpcError rpcErrorShort = RpcResultBuilder.newError(RpcError.ErrorType.RPC, "tag", "msg");
+        final RpcError rpcErrorLong = RpcResultBuilder.newError(RpcError.ErrorType.RPC, "tag", "msg", "applicationTag",
+                "info", null);
+        final RpcError rpcErrorShortWarn = RpcResultBuilder.newWarning(RpcError.ErrorType.RPC, "tag", "msg");
+        final RpcError rpcErrorLongWarn = RpcResultBuilder.newWarning(RpcError.ErrorType.RPC, "tag", "msg",
+                "applicationTag",
+                "info", null);
+        rpcResultBuilder.withRpcError(rpcErrorShort);
+        final RpcResult<Object> rpcResult = rpcResultBuilder.build();
+        final RpcResultBuilder<RpcResult<Object>> rpcResultRpcResultBuilder1 = RpcResultBuilder.success
+                (rpcResultBuilder);
+        final RpcResultBuilder<RpcResult<Object>> rpcResultRpcResultBuilder2 = rpcResultRpcResultBuilder1.withResult
+                (rpcResultBuilder);
+
+        assertEquals(rpcErrorShort.getErrorType(), rpcErrorShortWarn.getErrorType());
+        assertEquals(rpcErrorLong.getErrorType(), rpcErrorLongWarn.getErrorType());
+        assertEquals(rpcResultRpcResultBuilder1, rpcResultRpcResultBuilder2);
+        assertTrue(rpcResultBuilder.buildFuture() instanceof ListenableFuture);
+        assertEquals("RpcResult [successful=true, result=null, errors=[RpcError [message=msg, severity=ERROR, " +
+                "errorType=RPC, tag=tag, applicationTag=null, info=null, cause=null]]]", rpcResult.toString());
+    }
+
+    @Test
+    public void testOperationFailedException() {
+        final Throwable cause = new Throwable( "mock cause" );
+        final RpcError rpcErrorShort = RpcResultBuilder.newError(RpcError.ErrorType.RPC, "tag", "msg");
+        final OperationFailedException operationFailedException1 = new OperationFailedException("error msg", cause,
+                rpcErrorShort);
+        final OperationFailedException operationFailedException2 = new OperationFailedException("error msg", rpcErrorShort);
+        assertEquals(operationFailedException1.getErrorList(), operationFailedException2.getErrorList());
+        assertTrue(operationFailedException1.toString().contains("error msg"));
+    }
+
     private void assertLocalNameFails(final String localName) {
         try {
             new QName((URI)null, localName);
             fail("Local name should fail:" + localName);
         } catch (IllegalArgumentException e) {
-
         }
     }
-
 }
