aef4f3fc7f9061aa8d21d828ee0ffd43231ff1dc
Igor Foltin <ifoltin@cisco.com>
2016-02-25 16:52:51 +0100

    Bug 5410: Issue while communicating with netconf device
    Regex anchors in yang pattern statement are now trimmed.
    
    Unescaped dollar signs in yang pattern statement are now escaped
    
    Change-Id: I6e1274addc4d5f51da6acac68b0dd63abce0b3cd
    Signed-off-by: Igor Foltin <ifoltin@cisco.com>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/parser/impl/Bug5410Test.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.parser.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.PatternConstraint;
+import org.opendaylight.yangtools.yang.model.util.ExtendedType;
+
+public class Bug5410Test {
+
+    @Test
+    public void testAnchorsFix() throws IOException, URISyntaxException {
+
+        Set<Module> modules = TestUtils.loadModules(getClass().getResource("/bugs/bug5410").toURI());
+        assertNotNull(modules);
+        Module module = modules.iterator().next();
+
+        LeafSchemaNode leaf = (LeafSchemaNode) module.getDataChildByName(
+                QName.create(module.getQNameModule(), "test-leaf"));
+        assertNotNull(leaf);
+
+        TypeDefinition<?> type = leaf.getType();
+        assertTrue(type instanceof ExtendedType);
+
+        List<PatternConstraint> patterns = ((ExtendedType) type).getPatternConstraints();
+        assertNotNull(patterns);
+        assertEquals(1, patterns.size());
+
+        PatternConstraint patternConstraint = patterns.iterator().next();
+        String fixedPattern = patternConstraint.getRegularExpression();
+        assertNotNull(Pattern.compile(fixedPattern));
+        assertEquals("^[^a-z]?\\$\\$[^A-Z]?$", fixedPattern);
+
+        String testString = "H$$d";
+        assertTrue(testString.matches(fixedPattern));
+    }
+}
