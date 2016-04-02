73b72d34da4691c1cedd0537410a4ea12de260e8
Igor Foltin <ifoltin@cisco.com>
2016-04-02 13:06:34 +0200

    Bug 1441: Implement XML Stream Reader to Normalized Node Writer
    This patch introduces new XML Parser which walks the DOM tree
    and emits events into a NormalizedNodeStreamWriter.
    
    The parser is found in the newly created module yang-data-codec-xml.
    
    Classes which are shared by XML and JSON Parser were moved to module yang-data-util.
    
    Added documentation for the XML parser
    
    Change-Id: I5b7de341333f94ad75bc2ff93b0f47f2c007e793
    Signed-off-by: Igor Foltin <ifoltin@cisco.com>

---
+++ yang/yang-data-codec-xml/src/main/java/org/opendaylight/yangtools/yang/data/codec/xml/XmlStringInstanceIdentifierCodec.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.data.codec.xml;
+
+import com.google.common.base.Preconditions;
+import java.net.URI;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.util.AbstractModuleStringInstanceIdentifierCodec;
+import org.opendaylight.yangtools.yang.data.util.DataSchemaContextTree;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+final class XmlStringInstanceIdentifierCodec  extends AbstractModuleStringInstanceIdentifierCodec
+        implements XmlCodec<YangInstanceIdentifier> {
+
+    private final DataSchemaContextTree dataContextTree;
+    private final XmlCodecFactory codecFactory;
+    private final SchemaContext context;
+
+    XmlStringInstanceIdentifierCodec(final SchemaContext context, final XmlCodecFactory jsonCodecFactory) {
+        this.context = Preconditions.checkNotNull(context);
+        this.dataContextTree = DataSchemaContextTree.from(context);
+        this.codecFactory = Preconditions.checkNotNull(jsonCodecFactory);
+    }
+
+    @Override
+    protected Module moduleForPrefix(final String prefix) {
+        return context.findModuleByName(prefix, null);
+    }
+
+    @Override
+    protected String prefixForNamespace(final URI namespace) {
+        final Module module = context.findModuleByNamespaceAndRevision(namespace, null);
+        return module == null ? null : module.getName();
+    }
+
+    @Override
+    protected DataSchemaContextTree getDataContextTree() {
+        return dataContextTree;
+    }
+
+    @Override
+    protected Object deserializeKeyValue(final DataSchemaNode schemaNode, final String value) {
+        Preconditions.checkNotNull(schemaNode, "schemaNode cannot be null");
+        Preconditions.checkArgument(schemaNode instanceof LeafSchemaNode, "schemaNode must be of type LeafSchemaNode");
+        final XmlCodec<Object> objectXmlCodec = codecFactory.codecFor(schemaNode);
+        return objectXmlCodec.deserialize(value);
+    }
+
+    /**
+     * Serialize YangInstanceIdentifier with specified XMLStreamWriter.
+     *
+     * @param writer XMLStreamWriter
+     * @param value YangInstanceIdentifier
+     */
+    @Override
+    public void serializeToWriter(final XMLStreamWriter writer, final YangInstanceIdentifier value)
+            throws XMLStreamException {
+        writer.writeCharacters(serialize(value));
+    }
+
+}
--- yang/yang-data-util/src/main/java/org/opendaylight/yangtools/yang/data/util/AnyXmlNodeDataWithSchema.java
+++ yang/yang-data-util/src/main/java/org/opendaylight/yangtools/yang/data/util/AnyXmlNodeDataWithSchema.java
@@ -1,28 +1,25 @@
 /*
- * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.yangtools.yang.data.codec.gson;
+package org.opendaylight.yangtools.yang.data.util;
 
 import java.io.IOException;
 import org.opendaylight.yangtools.yang.data.api.schema.stream.SchemaAwareNormalizedNodeStreamWriter;
 import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
 
-class ContainerNodeDataWithSchema extends CompositeNodeDataWithSchema {
+public class AnyXmlNodeDataWithSchema extends SimpleNodeDataWithSchema {
 
-    public ContainerNodeDataWithSchema(final DataSchemaNode schema) {
-        super(schema);
+    public AnyXmlNodeDataWithSchema(final DataSchemaNode dataSchemaNode) {
+        super(dataSchemaNode);
     }
 
     @Override
     public void write(final SchemaAwareNormalizedNodeStreamWriter writer) throws IOException {
         writer.nextDataSchemaNode(getSchema());
-        writer.startContainerNode(provideNodeIdentifier(), childSizeHint());
-        super.write(writer);
-        writer.endNode();
+        writer.leafNode(provideNodeIdentifier(), getValue());
     }
-
 }
