c2143b3d205ac15dbb751313dd7262ac6343e228
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2016-04-11 11:29:25 +0200

    Test for extension argument names
    Yin and Yang tests. Yin test is failing.
    
    Change-Id: I094113cf2f4bd7b0eb258c26bf315a0490318dd9
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/test/YangExtensionArgumentTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.stmt.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Set;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.model.api.ExtensionDefinition;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.UnknownSchemaNode;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.stmt.retest.TestUtils;
+
+public class YangExtensionArgumentTest {
+
+    private Set<Module> modules;
+
+    @Before
+    public void init() throws URISyntaxException, ReactorException {
+        modules = TestUtils.loadModules(getClass().getResource("/semantic-statement-parser/extension-argument-name")
+                .toURI());
+    }
+
+    @Test
+    public void ExtensionDefinitionTest() {
+        Module testModule = TestUtils.findModule(modules, "semantic-version");
+        assertNotNull(testModule);
+
+        List<ExtensionDefinition> extensions = testModule.getExtensionSchemaNodes();
+        assertEquals(1, extensions.size());
+
+        ExtensionDefinition extension = extensions.get(0);
+
+        assertEquals("semantic-version", extension.getQName().getLocalName());
+        assertEquals("semantic-version", extension.getArgument());
+    }
+
+    @Test
+    public void ExtensionParameterTest() {
+        Module testModule = TestUtils.findModule(modules, "foo");
+        assertNotNull(testModule);
+
+        UnknownSchemaNode node = testModule.getUnknownSchemaNodes().get(0);
+        assertEquals("0.1.1", node.getNodeParameter());
+    }
+}
