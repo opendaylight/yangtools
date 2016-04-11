92219dc89f319fa0deb35fb34c02950c8d3db735
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2016-04-11 11:29:25 +0200

    Test for extension argument names
    Yin and Yang tests. Yin test is failing.
    
    Change-Id: I094113cf2f4bd7b0eb258c26bf315a0490318dd9
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/test/yin/YinExtensionArgumentTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.stmt.test.yin;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Set;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.model.api.ExtensionDefinition;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.UnknownSchemaNode;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.stmt.retest.TestUtils;
+
+public class YinExtensionArgumentTest {
+
+    private List<ExtensionDefinition> extensions;
+    private List<UnknownSchemaNode> unknownSchemaNodes;
+
+    @Before
+    public void init() throws URISyntaxException, ReactorException {
+        Set<Module> modules = TestUtils.loadYinModules(getClass().getResource
+                ("/semantic-statement-parser/yin/extension-argument-name")
+                .toURI());
+
+        assertEquals(2, modules.size());
+
+        Module semanticVersion = TestUtils.findModule(modules, "semantic-version");
+        assertNotNull(semanticVersion);
+
+        extensions = semanticVersion.getExtensionSchemaNodes();
+        assertEquals(2, extensions.size());
+
+        Module foo = TestUtils.findModule(modules, "foo");
+        assertNotNull(foo);
+
+        unknownSchemaNodes = foo.getUnknownSchemaNodes();
+        assertEquals(2, unknownSchemaNodes.size());
+    }
+
+    @Test
+    public void ExtensionDefinitionDefaultTest() {
+        ExtensionDefinition extension = extensions.get(0);
+        assertEquals("extension", extension.getQName().getLocalName());
+        assertEquals("name", extension.getArgument());
+    }
+
+    @Test
+    public void ExtensionDefinitionCustomTest() {
+        ExtensionDefinition extension = extensions.get(1);
+        assertEquals("semantic-version", extension.getQName().getLocalName());
+        assertEquals("semantic-version", extension.getArgument());
+    }
+
+    @Test
+    public void ExtensionParameterDefaultTest() {
+        UnknownSchemaNode node = unknownSchemaNodes.get(0);
+        assertEquals("1.0.0-value", node.getNodeParameter());
+    }
+
+    @Test
+    public void ExtensionParameterCustomTest() {
+        UnknownSchemaNode node = unknownSchemaNodes.get(1);
+        assertEquals("0.0.1", node.getNodeParameter());
+    }
+}
