944aa3c5b10898b1e81b6638258bfc91afd23d73
Peter Kajsa <pkajsa@cisco.com>
2016-04-14 11:28:29 +0200

    Bug 4662: Introduce a SemanticVersion concept - SchemaContextFactory
    Introducing of semantic version concept into SchemaContextFactory.
    
    Change-Id: I382d6d6107852cd39a8e4023bbf2ae4d079782c7
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/parser/repo/SemVerSharedSchemaRepositoryTest.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.parser.repo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.CheckedFuture;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.repo.api.SchemaContextFactory;
+import org.opendaylight.yangtools.yang.model.repo.api.SchemaResolutionException;
+import org.opendaylight.yangtools.yang.model.repo.api.SchemaSourceException;
+import org.opendaylight.yangtools.yang.model.repo.api.SchemaSourceFilter;
+import org.opendaylight.yangtools.yang.parser.util.ASTSchemaSource;
+import org.opendaylight.yangtools.yang.parser.util.TextToASTTransformer;
+
+public class SemVerSharedSchemaRepositoryTest {
+
+    @Test
+    public void testSemVerSharedSchemaRepository() throws Exception {
+        final SharedSchemaRepository sharedSchemaRepository = new SharedSchemaRepository(
+                "sem-ver-shared-schema-repo-test");
+
+        final SettableSchemaProvider<ASTSchemaSource> bar = getImmediateYangSourceProviderFromResource("/semantic-version/semver-shared-schema-repository/bar@2016-01-01.yang");
+        bar.register(sharedSchemaRepository);
+        bar.setResult();
+        final SettableSchemaProvider<ASTSchemaSource> foo = getImmediateYangSourceProviderFromResource("/semantic-version/semver-shared-schema-repository/foo.yang");
+        foo.register(sharedSchemaRepository);
+        foo.setResult();
+        final SettableSchemaProvider<ASTSchemaSource> semVer = getImmediateYangSourceProviderFromResource("/semantic-version/semver-shared-schema-repository/semantic-version.yang");
+        semVer.register(sharedSchemaRepository);
+        semVer.setResult();
+
+        final SchemaContextFactory fact = sharedSchemaRepository
+                .createSchemaContextFactory(SchemaSourceFilter.ALWAYS_ACCEPT);
+
+        final CheckedFuture<SchemaContext, SchemaResolutionException> inetAndTopologySchemaContextFuture = fact
+                .createSchemaContext(Lists.newArrayList(bar.getId(), foo.getId(), semVer.getId()), true);
+        assertTrue(inetAndTopologySchemaContextFuture.isDone());
+        assertSchemaContext(inetAndTopologySchemaContextFuture.checkedGet(), 3);
+
+        final CheckedFuture<SchemaContext, SchemaResolutionException> barSchemaContextFuture = fact
+                .createSchemaContext(Lists.newArrayList(bar.getId(), semVer.getId()), true);
+        assertTrue(barSchemaContextFuture.isDone());
+        assertSchemaContext(barSchemaContextFuture.checkedGet(), 2);
+    }
+
+    @Test
+    public void testSharedSchemaRepository() throws Exception {
+        final SharedSchemaRepository sharedSchemaRepository = new SharedSchemaRepository("shared-schema-repo-test");
+
+        final SettableSchemaProvider<ASTSchemaSource> bar = getImmediateYangSourceProviderFromResource("/semantic-version/shared-schema-repository/bar@2016-01-01.yang");
+        bar.register(sharedSchemaRepository);
+        bar.setResult();
+        final SettableSchemaProvider<ASTSchemaSource> foo = getImmediateYangSourceProviderFromResource("/semantic-version/shared-schema-repository/foo.yang");
+        foo.register(sharedSchemaRepository);
+        foo.setResult();
+        final SettableSchemaProvider<ASTSchemaSource> semVer = getImmediateYangSourceProviderFromResource("/semantic-version/shared-schema-repository/semantic-version.yang");
+        semVer.register(sharedSchemaRepository);
+        semVer.setResult();
+
+        final SchemaContextFactory fact = sharedSchemaRepository
+                .createSchemaContextFactory(SchemaSourceFilter.ALWAYS_ACCEPT);
+
+        final CheckedFuture<SchemaContext, SchemaResolutionException> inetAndTopologySchemaContextFuture = fact
+                .createSchemaContext(Lists.newArrayList(bar.getId(), foo.getId(), semVer.getId()));
+        assertTrue(inetAndTopologySchemaContextFuture.isDone());
+        assertSchemaContext(inetAndTopologySchemaContextFuture.checkedGet(), 3);
+
+        final CheckedFuture<SchemaContext, SchemaResolutionException> barSchemaContextFuture = fact
+                .createSchemaContext(Lists.newArrayList(bar.getId(), semVer.getId()));
+        assertTrue(barSchemaContextFuture.isDone());
+        assertSchemaContext(barSchemaContextFuture.checkedGet(), 2);
+    }
+
+    private static void assertSchemaContext(final SchemaContext schemaContext, final int moduleSize) {
+        assertNotNull(schemaContext);
+        assertEquals(moduleSize, schemaContext.getModules().size());
+    }
+
+    static SettableSchemaProvider<ASTSchemaSource> getImmediateYangSourceProviderFromResource(final String resourceName)
+            throws Exception {
+        final ResourceYangSource yangSource = new ResourceYangSource(resourceName);
+        final CheckedFuture<ASTSchemaSource, SchemaSourceException> aSTSchemaSource = TextToASTTransformer.TRANSFORMATION
+                .apply(yangSource);
+        return SettableSchemaProvider.createImmediate(aSTSchemaSource.get(), ASTSchemaSource.class);
+    }
+}
