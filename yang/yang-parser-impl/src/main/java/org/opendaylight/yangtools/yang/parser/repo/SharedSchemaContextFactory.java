4eda1e7a3ac0a8d0aac6c01f676cdf7aea096453
Peter Kajsa <pkajsa@cisco.com>
2016-04-14 11:28:29 +0200

    Bug 4662: Introduce a SemanticVersion concept - SchemaContextFactory
    Introducing of semantic version concept into SchemaContextFactory.
    
    Change-Id: I382d6d6107852cd39a8e4023bbf2ae4d079782c7
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/repo/SharedSchemaContextFactory.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/repo/SharedSchemaContextFactory.java
@@ -39,6 +39,7 @@ import org.opendaylight.yangtools.yang.model.repo.api.SchemaContextFactory;
 import org.opendaylight.yangtools.yang.model.repo.api.SchemaResolutionException;
 import org.opendaylight.yangtools.yang.model.repo.api.SchemaSourceFilter;
 import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
+import org.opendaylight.yangtools.yang.model.repo.api.StatementParserMode;
 import org.opendaylight.yangtools.yang.parser.impl.util.YangModelDependencyInfo;
 import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
 import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
@@ -60,6 +61,42 @@ final class SharedSchemaContextFactory implements SchemaContextFactory {
         }
     };
     private final Cache<Collection<SourceIdentifier>, SchemaContext> cache = CacheBuilder.newBuilder().weakValues().build();
+    private final Cache<Collection<SourceIdentifier>, SchemaContext> semVerCache = CacheBuilder.newBuilder().weakValues().build();
+
+    private final AsyncFunction<List<ASTSchemaSource>, SchemaContext> assembleSemVerSources = new AsyncFunction<List<ASTSchemaSource>, SchemaContext>() {
+        @Override
+        public ListenableFuture<SchemaContext> apply(final List<ASTSchemaSource> sources) throws SchemaResolutionException, SourceException, ReactorException {
+            final Map<SourceIdentifier, ASTSchemaSource> srcs =
+                    Maps.uniqueIndex(sources, ASTSchemaSource.GET_SEMVER_IDENTIFIER);
+            final Map<SourceIdentifier, YangModelDependencyInfo> deps =
+                    Maps.transformValues(srcs, ASTSchemaSource.GET_DEPINFO);
+
+            LOG.debug("Resolving dependency reactor {}", deps);
+
+            final DependencyResolver res = SemVerDependencyResolver.create(deps);
+            if (!res.getUnresolvedSources().isEmpty()) {
+                LOG.debug("Omitting models {} due to unsatisfied imports {}", res.getUnresolvedSources(), res.getUnsatisfiedImports());
+                throw new SchemaResolutionException("Failed to resolve required models",
+                        res.getResolvedSources(), res.getUnsatisfiedImports());
+            }
+
+            final Map<SourceIdentifier, ParserRuleContext> asts = Maps.transformValues(srcs, ASTSchemaSource.GET_AST);
+            final CrossSourceStatementReactor.BuildAction reactor = YangInferencePipeline.RFC6020_REACTOR.newBuild(StatementParserMode.SEMVER_MODE);
+
+            for (final Entry<SourceIdentifier, ParserRuleContext> e : asts.entrySet()) {
+                final ParserRuleContext parserRuleCtx = e.getValue();
+                Preconditions.checkArgument(parserRuleCtx instanceof StatementContext,
+                    "Unsupported context class %s for source %s", parserRuleCtx.getClass(), e.getKey());
+
+                reactor.addSource(new YangStatementSourceImpl(e.getKey(), (StatementContext) parserRuleCtx));
+            }
+
+            SchemaContext schemaContext = reactor.buildEffective();
+
+            return Futures.immediateCheckedFuture(schemaContext);
+        }
+    };
+
     private final SharedSchemaRepository repository;
     // FIXME: ignored right now
     private final SchemaSourceFilter filter;
@@ -72,7 +109,14 @@ final class SharedSchemaContextFactory implements SchemaContextFactory {
 
     @Override
     public CheckedFuture<SchemaContext, SchemaResolutionException> createSchemaContext(
-            final Collection<SourceIdentifier> requiredSources, java.util.function.Predicate<QName> isFeatureSupported) {
+            final Collection<SourceIdentifier> requiredSources, final StatementParserMode statementParserMode,
+            final java.util.function.Predicate<QName> isFeatureSupported) {
+        return createSchemaContext(requiredSources,
+                statementParserMode == StatementParserMode.SEMVER_MODE ? this.semVerCache : this.cache,
+                new AssembleSources(isFeatureSupported, statementParserMode));
+    }
+
+    private CheckedFuture<SchemaContext, SchemaResolutionException> createSchemaContext(final Collection<SourceIdentifier> requiredSources, final Cache<Collection<SourceIdentifier>, SchemaContext> cache, AsyncFunction<List<ASTSchemaSource>, SchemaContext> assembleSources) {
         // Make sources unique
         final List<SourceIdentifier> uniqueSourceIdentifiers = deDuplicateSources(requiredSources);
 
@@ -91,7 +135,6 @@ final class SharedSchemaContextFactory implements SchemaContextFactory {
         sf = Futures.transform(sf, new SourceIdMismatchDetector(uniqueSourceIdentifiers));
 
         // Assemble sources into a schema context
-        final AssembleSources assembleSources = new AssembleSources(isFeatureSupported);
         final ListenableFuture<SchemaContext> cf = Futures.transform(sf, assembleSources);
 
         // Populate cache when successful
@@ -166,22 +209,26 @@ final class SharedSchemaContextFactory implements SchemaContextFactory {
     private static final class AssembleSources implements AsyncFunction<List<ASTSchemaSource>, SchemaContext> {
 
         private final java.util.function.Predicate<QName> isFeatureSupported;
+        private final StatementParserMode statementParserMode;
 
-        private AssembleSources(final java.util.function.Predicate<QName> isFeatureSupported) {
+        private AssembleSources(final java.util.function.Predicate<QName> isFeatureSupported, final StatementParserMode statementParserMode) {
             this.isFeatureSupported = Preconditions.checkNotNull(isFeatureSupported);
+            this.statementParserMode = Preconditions.checkNotNull(statementParserMode);
         }
 
         @Override
         public ListenableFuture<SchemaContext> apply(List<ASTSchemaSource> sources) throws SchemaResolutionException,
                 SourceException, ReactorException {
-            final Map<SourceIdentifier, ASTSchemaSource> srcs =
-                    Maps.uniqueIndex(sources, ASTSchemaSource.GET_IDENTIFIER);
+            final Map<SourceIdentifier, ASTSchemaSource> srcs = this.statementParserMode == StatementParserMode.SEMVER_MODE ? Maps
+                    .uniqueIndex(sources, ASTSchemaSource.GET_SEMVER_IDENTIFIER) : Maps.uniqueIndex(sources,
+                    ASTSchemaSource.GET_IDENTIFIER);
             final Map<SourceIdentifier, YangModelDependencyInfo> deps =
                     Maps.transformValues(srcs, ASTSchemaSource.GET_DEPINFO);
 
             LOG.debug("Resolving dependency reactor {}", deps);
 
-            final DependencyResolver res = DependencyResolver.create(deps);
+            final DependencyResolver res = this.statementParserMode == StatementParserMode.SEMVER_MODE ? SemVerDependencyResolver
+                    .create(deps) : RevisionDependencyResolver.create(deps);
             if (!res.getUnresolvedSources().isEmpty()) {
                 LOG.debug("Omitting models {} due to unsatisfied imports {}", res.getUnresolvedSources(), res.getUnsatisfiedImports());
                 throw new SchemaResolutionException("Failed to resolve required models",
@@ -190,7 +237,7 @@ final class SharedSchemaContextFactory implements SchemaContextFactory {
 
             final Map<SourceIdentifier, ParserRuleContext> asts = Maps.transformValues(srcs, ASTSchemaSource.GET_AST);
             final CrossSourceStatementReactor.BuildAction reactor =
-                    YangInferencePipeline.RFC6020_REACTOR.newBuild(isFeatureSupported);
+                    YangInferencePipeline.RFC6020_REACTOR.newBuild(statementParserMode, isFeatureSupported);
 
             for (final Entry<SourceIdentifier, ParserRuleContext> e : asts.entrySet()) {
                 final ParserRuleContext parserRuleCtx = e.getValue();
