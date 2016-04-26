63308544520d6ac21ec2066d2f61f3e7e8577e1e
Filip Gregor <fgregor@cisco.com>
2016-04-26 10:54:54 +0200

    Bug 5788: enum used as a key does not work
    fixed Qname check for enum as key from grouping in
    bindingGeneratorImpl
    
    Change-Id: Id6521d28a4b9aebcd1dfca2d33e8dfed68e9a269
    Signed-off-by: Filip Gregor <fgregor@cisco.com>

--- binding/mdsal-binding-java-api-generator/src/test/java/org/opendaylight/yangtools/sal/java/api/generator/stmt/parser/retest/CompilationTest.java
+++ binding/mdsal-binding-java-api-generator/src/test/java/org/opendaylight/yangtools/sal/java/api/generator/stmt/parser/retest/CompilationTest.java
@@ -589,6 +589,21 @@ public class CompilationTest extends BaseCompilationTest {
         cleanUp(sourcesOutputDir, compiledOutputDir);
     }
 
+    @Test
+    public void testBug5788() throws Exception {
+        final File sourcesOutputDir = new File(GENERATOR_OUTPUT_PATH + FS + "bug5788");
+        assertTrue("Failed to create test file '" + sourcesOutputDir + "'", sourcesOutputDir.mkdir());
+        final File compiledOutputDir = new File(COMPILER_OUTPUT_PATH + FS + "bug5788");
+        assertTrue("Failed to create test file '" + compiledOutputDir + "'", compiledOutputDir.mkdir());
+
+        generateTestSources("/compilation/bug5788", sourcesOutputDir);
+
+        // Test if sources are compilable
+        testCompilation(sourcesOutputDir, compiledOutputDir);
+
+        cleanUp(sourcesOutputDir, compiledOutputDir);
+    }
+
     /**
      * Test if class generated for node from grouping implements ChildOf.
      *
