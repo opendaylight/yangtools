77277ee2bd0de5bd1eca7caa50861b6019d6e94e
Robert Varga <rovarga@cisco.com>
2016-05-10 11:48:48 +0200

    BUG-5626: introduce utility identifiers
    Wrapping String and UUID into an Identifier is useful for bridging worlds
    with and without Identifiers. Introduce dedicated wrappers for these two
    common cases.
    
    Change-Id: Ib020c34512a826c23099601e3a57d558aa324976
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ common/util/src/main/java/org/opendaylight/yangtools/util/StringIdentifier.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.util;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+import org.opendaylight.yangtools.concepts.Identifier;
+
+/**
+ * Utility {@link Identifier} backed by a {@link String}.
+ */
+@Beta
+public final class StringIdentifier implements Identifier, Comparable<StringIdentifier> {
+    private static final long serialVersionUID = 1L;
+    private final String string;
+
+    public StringIdentifier(final String string) {
+        this.string = Preconditions.checkNotNull(string);
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    @Override
+    public int hashCode() {
+        return string.hashCode();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        return this == o || (o instanceof StringIdentifier && string.equals(((StringIdentifier)o).string));
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(StringIdentifier.class).add("string", string).toString();
+    }
+
+    @Override
+    public int compareTo(final StringIdentifier o) {
+        return string.compareTo(o.string);
+    }
+}
---
+++ common/util/src/main/java/org/opendaylight/yangtools/util/UUIDIdentifier.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.util;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+import java.util.UUID;
+import org.opendaylight.yangtools.concepts.Identifier;
+
+/**
+ * Utility {@link Identifier} backed by a {@link UUID}.
+ */
+@Beta
+public final class UUIDIdentifier implements Identifier, Comparable<UUIDIdentifier> {
+    private static final long serialVersionUID = 1L;
+    private final UUID uuid;
+
+    public UUIDIdentifier(final UUID uuid) {
+        this.uuid = Preconditions.checkNotNull(uuid);
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid.hashCode();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        return this == o || (o instanceof UUIDIdentifier && uuid.equals(((UUIDIdentifier)o).uuid));
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(UUIDIdentifier.class).add("uuid", uuid).toString();
+    }
+
+    @Override
+    public int compareTo(final UUIDIdentifier o) {
+        return uuid.compareTo(o.uuid);
+    }
+}
