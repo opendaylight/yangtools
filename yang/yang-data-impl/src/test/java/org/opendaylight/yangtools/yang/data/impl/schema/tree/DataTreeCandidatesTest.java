8640ecdd3000191c0db0697dfeb3c686a88aa787
Tomas Cere <tcere@cisco.com>
2016-05-13 17:43:22 +0200

    Add application of RootedDataTreeCandidates
    Allows applications of DataTreeCandidates that are rooted in one
    DataTree to another DataTree(comes in handy with sharded datastore
    when building notifications)
    
    Change-Id: I78ad88aed4453924c1594c7e8e47ffddd71c6282
    Signed-off-by: Tomas Cere <tcere@cisco.com>

---
+++ yang/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/DataTreeCandidatesTest.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.data.impl.schema.tree;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.TestCase.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTree;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidates;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeModification;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeModificationCursor;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.TreeType;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableContainerNodeBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableLeafNodeBuilder;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DataTreeCandidatesTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DataTreeCandidates.class);
+
+    private static SchemaContext SCHEMA_CONTEXT = null;
+
+    static {
+        try {
+            SCHEMA_CONTEXT = TestModel.createTestContext();
+        } catch (ReactorException e) {
+            LOG.error("Failed to setup test schema context", e);
+        }
+    }
+
+    private DataTree dataTree;
+
+    @Before
+    public void setUp() throws Exception {
+        dataTree = InMemoryDataTreeFactory.getInstance().create(TreeType.OPERATIONAL);
+        dataTree.setSchemaContext(SCHEMA_CONTEXT);
+
+        final ContainerNode testContainer = ImmutableContainerNodeBuilder.create()
+                .withNodeIdentifier(new NodeIdentifier(TestModel.TEST_QNAME))
+                .withChild(ImmutableContainerNodeBuilder.create()
+                        .withNodeIdentifier(new NodeIdentifier(SchemaContext.NAME))
+                        .build())
+                .build();
+
+        final InMemoryDataTreeModification modification = (InMemoryDataTreeModification) dataTree.takeSnapshot().newModification();
+        final DataTreeModificationCursor cursor = modification.createCursor(YangInstanceIdentifier.EMPTY);
+        cursor.write(TestModel.TEST_PATH.getLastPathArgument(), testContainer);
+        modification.ready();
+
+        dataTree.validate(modification);
+        final DataTreeCandidate candidate = dataTree.prepare(modification);
+        dataTree.commit(candidate);
+    }
+
+    @Test
+    public void testRootedCandidate() throws Exception {
+        final DataTree innerDataTree = InMemoryDataTreeFactory.getInstance().create(TreeType.OPERATIONAL, TestModel.INNER_CONTAINER_PATH);
+        innerDataTree.setSchemaContext(SCHEMA_CONTEXT);
+
+        final LeafNode<String> leaf = ImmutableLeafNodeBuilder.<String>create()
+                .withNodeIdentifier(new NodeIdentifier(TestModel.VALUE_QNAME))
+                .withValue("testing-value")
+                .build();
+
+        final InMemoryDataTreeModification modification = (InMemoryDataTreeModification) innerDataTree.takeSnapshot().newModification();
+        modification.write(TestModel.VALUE_PATH, leaf);
+
+        modification.ready();
+        dataTree.validate(modification);
+        final DataTreeCandidate candidate = dataTree.prepare(modification);
+        dataTree.commit(candidate);
+
+        final DataTreeModification newModification = dataTree.takeSnapshot().newModification();
+        final DataTreeCandidate newCandidate = DataTreeCandidates.newDataTreeCandidate(TestModel.INNER_CONTAINER_PATH, candidate.getRootNode());
+
+        try {
+            // lets see if getting the identifier of the root node throws an exception
+            newCandidate.getRootNode().getIdentifier();
+            fail();
+        } catch (IllegalStateException e) {
+            LOG.debug("Cannot get identifier of root node candidate which is correct", e);
+        }
+
+        // lets see if we can apply this rooted candidate to a new dataTree
+        DataTreeCandidates.applyToModification(newModification,
+                newCandidate);
+
+        final LeafNode<String> readLeaf = (LeafNode<String>) newModification.readNode(TestModel.INNER_VALUE_PATH).get();
+        assertEquals(readLeaf, leaf);
+    }
+}
