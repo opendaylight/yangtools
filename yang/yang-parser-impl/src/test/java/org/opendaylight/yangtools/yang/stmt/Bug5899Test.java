594ac3953ed56d9fb2fa2c0a6129baf1f892bcc1
Filip Gregor <fgregor@cisco.com>
2016-05-19 13:54:45 +0200

    Bug 5899: Cardinality check incorrectly limited to 1 for "must"
    updated cardinality for must statement in
    refine statement, added test
    
    Change-Id: I3c31cc902d05a87259970a856a8f0e7efc20c67c
    Signed-off-by: Filip Gregor <fgregor@cisco.com>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug5899Test.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+import org.opendaylight.yangtools.yang.stmt.test.StmtTestUtils;
+
+public class Bug5899Test {
+
+    @Test
+    public void testBug5899() throws ReactorException, SourceException, FileNotFoundException,
+            URISyntaxException {
+        final SchemaContext schemaContext = StmtTestUtils.parseYangSources("/bugs/bug5899");
+        assertNotNull(schemaContext);
+    }
+}
