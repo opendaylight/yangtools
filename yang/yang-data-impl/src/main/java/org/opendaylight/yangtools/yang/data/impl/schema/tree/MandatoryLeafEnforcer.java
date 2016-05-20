e7f9280699d02a3c902d95be11a54196143e3f72
Peter Kajsa <pkajsa@cisco.com>
2016-05-20 09:46:37 +0200

    Bug 5830: Mandatory leaf enforcement is not correct with presence container
    Fix of mandatory leaf enforcement under presence container.
    
    Change-Id: I6f5900f555e8f94916a84b747fbd895592606f38
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/MandatoryLeafEnforcer.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/MandatoryLeafEnforcer.java
@@ -11,7 +11,13 @@ import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableCollection.Builder;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import org.opendaylight.yangtools.concepts.Immutable;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
@@ -23,6 +29,7 @@ import org.opendaylight.yangtools.yang.model.api.ConstraintDefinition;
 import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.DataNodeContainer;
 import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -42,9 +49,12 @@ abstract class MandatoryLeafEnforcer implements Immutable {
 
     private static final class Strict extends MandatoryLeafEnforcer {
         private final Collection<YangInstanceIdentifier> mandatoryNodes;
+        private final Map<YangInstanceIdentifier, List<YangInstanceIdentifier>> presenceContMandatoryNodes;
 
-        private Strict(final Collection<YangInstanceIdentifier> mandatoryNodes) {
+        private Strict(final Collection<YangInstanceIdentifier> mandatoryNodes,
+                final Map<YangInstanceIdentifier, List<YangInstanceIdentifier>> presenceContMandatoryNodes) {
             this.mandatoryNodes = Preconditions.checkNotNull(mandatoryNodes);
+            this.presenceContMandatoryNodes = Preconditions.checkNotNull(presenceContMandatoryNodes);
         }
 
         @Override
@@ -54,12 +64,32 @@ abstract class MandatoryLeafEnforcer implements Immutable {
 
         @Override
         protected void enforceOnTreeNode(final NormalizedNode<?, ?> data) {
+            enforceMandatoryNodes(data);
+            enforcePresenceContainerMandatoryNodes(data);
+        }
+
+        private void enforceMandatoryNodes(final NormalizedNode<?, ?> data) {
             for (YangInstanceIdentifier id : mandatoryNodes) {
                 final Optional<NormalizedNode<?, ?>> descandant = NormalizedNodes.findNode(data, id);
                 Preconditions.checkArgument(descandant.isPresent(), "Node %s is missing mandatory descendant %s",
                         data.getIdentifier(), id);
             }
         }
+
+        private void enforcePresenceContainerMandatoryNodes(NormalizedNode<?, ?> data) {
+            for (Entry<YangInstanceIdentifier, List<YangInstanceIdentifier>> entry : presenceContMandatoryNodes
+                    .entrySet()) {
+                final Optional<NormalizedNode<?, ?>> presenceContainer = NormalizedNodes.findNode(data, entry.getKey());
+                if (presenceContainer.isPresent()) {
+                    for (YangInstanceIdentifier id : entry.getValue()) {
+                        final Optional<NormalizedNode<?, ?>> mandatoryNode = NormalizedNodes.findNode(data, id);
+                        Preconditions.checkArgument(mandatoryNode.isPresent(),
+                                "Node %s is missing mandatory descendant %s under presence container %s", data.getIdentifier(), id, entry.getKey());
+                    }
+                }
+            }
+        }
+
     }
 
     private static final Logger LOG = LoggerFactory.getLogger(MandatoryLeafEnforcer.class);
@@ -70,14 +100,31 @@ abstract class MandatoryLeafEnforcer implements Immutable {
     protected abstract void enforceOnTreeNode(final NormalizedNode<?, ?> normalizedNode);
 
     private static void findMandatoryNodes(final Builder<YangInstanceIdentifier> builder,
-            final YangInstanceIdentifier id, final DataNodeContainer schema, final TreeType type) {
+            final YangInstanceIdentifier id, final DataNodeContainer schema, final TreeType type,
+            final Map<YangInstanceIdentifier, List<YangInstanceIdentifier>> presenceContainerMandatoryNodes,
+            final YangInstanceIdentifier presenceContainerId) {
         for (DataSchemaNode child : schema.getChildNodes()) {
             if (SchemaAwareApplyOperation.belongsToTree(type, child)) {
                 if (child instanceof ContainerSchemaNode) {
                     final ContainerSchemaNode container = (ContainerSchemaNode) child;
                     if (!container.isPresenceContainer()) {
-                        findMandatoryNodes(builder, id.node(NodeIdentifier.create(child.getQName())), container, type);
+                        findMandatoryNodes(builder, id.node(NodeIdentifier.create(child.getQName())), container, type,
+                                presenceContainerMandatoryNodes, presenceContainerId);
+                    } else {
+                        findMandatoryNodes(builder, id.node(NodeIdentifier.create(child.getQName())), container, type,
+                                presenceContainerMandatoryNodes, id.node(NodeIdentifier.create(container.getQName())));
                     }
+                } else if (presenceContainerId != null && child instanceof LeafSchemaNode
+                        && child.getConstraints().isMandatory()) {
+                    final YangInstanceIdentifier childId = id.node(NodeIdentifier.create(child.getQName()));
+                    LOG.debug("Adding mandatory child {} under presence container {}", childId, presenceContainerId);
+                    List<YangInstanceIdentifier> mandatoryNodes = presenceContainerMandatoryNodes
+                            .get(presenceContainerId);
+                    if (mandatoryNodes == null) {
+                        mandatoryNodes = new ArrayList<>();
+                    }
+                    mandatoryNodes.add(childId.toOptimized());
+                    presenceContainerMandatoryNodes.put(presenceContainerId.toOptimized(), mandatoryNodes);
                 } else {
                     final ConstraintDefinition constraints = child.getConstraints();
                     final Integer minElements = constraints.getMinElements();
@@ -95,9 +142,11 @@ abstract class MandatoryLeafEnforcer implements Immutable {
         switch (type) {
         case CONFIGURATION:
             final Builder<YangInstanceIdentifier> builder = ImmutableList.builder();
-            findMandatoryNodes(builder, YangInstanceIdentifier.EMPTY, schema, type);
+            final Map<YangInstanceIdentifier, List<YangInstanceIdentifier>> presenceContMandatoryNodes = new HashMap<>();
+            findMandatoryNodes(builder, YangInstanceIdentifier.EMPTY, schema, type, presenceContMandatoryNodes, null);
             final Collection<YangInstanceIdentifier> mandatoryNodes = builder.build();
-            return mandatoryNodes.isEmpty() ? NOOP_ENFORCER : new Strict(mandatoryNodes);
+            return mandatoryNodes.isEmpty() && presenceContMandatoryNodes.isEmpty() ? NOOP_ENFORCER : new Strict(
+                    mandatoryNodes, ImmutableMap.copyOf(presenceContMandatoryNodes));
         case OPERATIONAL:
             return NOOP_ENFORCER;
         default:
