b6d2ccb032d7d583f104612477ae812f6bdb011e
Michael Vorburger <vorburger@redhat.com>
2016-05-25 14:58:03 +0200

    New LogMessageExtractorCheck which writes out TXT report of all loggers
    Change-Id: I853c742901131293c58c78bea821fe0d12957f07
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- common/checkstyle-logging/src/main/java/org/opendaylight/yangtools/checkstyle/CheckLoggingUtil.java
+++ common/checkstyle-logging/src/main/java/org/opendaylight/yangtools/checkstyle/CheckLoggingUtil.java
@@ -8,15 +8,13 @@
 
 package org.opendaylight.yangtools.checkstyle;
 
-import java.util.List;
-
-import org.slf4j.Logger;
-
 import com.google.common.collect.Lists;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.FullIdent;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 import com.puppycrawl.tools.checkstyle.utils.CheckUtils;
+import java.util.List;
+import org.slf4j.Logger;
 
 public final class CheckLoggingUtil {
 
@@ -49,6 +47,13 @@ public final class CheckLoggingUtil {
         return ast.getParent().getType() == TokenTypes.OBJBLOCK;
     }
 
+    /**
+     * Returns the name the method (and the enclosing class) at a given point specified by the
+     * passed-in abstract syntax tree (AST).
+     *
+     * @param ast an abstract syntax tree (AST) pointing to method call
+     * @return the name of the method being called
+     */
     public static String getMethodName(final DetailAST ast) {
         if (ast.getFirstChild().getLastChild() != null) {
             return ast.getFirstChild().getFirstChild().getText() + "." + ast.getFirstChild().getLastChild().getText();
@@ -60,6 +65,13 @@ public final class CheckLoggingUtil {
         return LOG_METHODS.contains(methodName);
     }
 
+    /**
+     * Returns the name of the closest enclosing class of the point by the passed-in abstract syntax
+     * tree (AST).
+     *
+     * @param ast an abstract syntax tree (AST)
+     * @return the name of the closest enclosign class
+     */
     public static String getClassName(final DetailAST ast) {
         DetailAST parent = ast.getParent();
         while (parent.getType() != TokenTypes.CLASS_DEF && parent.getType() != TokenTypes.ENUM_DEF) {
