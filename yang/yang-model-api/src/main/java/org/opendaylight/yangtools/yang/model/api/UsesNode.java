6aa5801b6b1a46161c8037d1a24cc92f5c31dc12
Peter Kajsa <pkajsa@cisco.com>
2016-06-02 16:31:06 +0200

    Bug 5942: When condition of uses node is not exposed by the YANG parser
    Interface UsesNode does not provide any method to obtain When condition,
    Description, Status, Reference and UnknownNodes defined in yang source.
    This patch provides API definition and its implementation necessary to get
    these substatements of UsesNode.
    
    Change-Id: I51e182de247a9375e4e28d76d7fecb421721cfc4
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/UsesNode.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/UsesNode.java
@@ -7,6 +7,9 @@
  */
 package org.opendaylight.yangtools.yang.model.api;
 
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -15,7 +18,7 @@ import java.util.Set;
  * <code>uses</code> substatement.
  *
  */
-public interface UsesNode {
+public interface UsesNode extends DocumentedNode {
 
     /**
      * Returns the schema path to used grouping.
@@ -58,4 +61,29 @@ public interface UsesNode {
      *         refined node
      */
     Map<SchemaPath, SchemaNode> getRefines();
+
+    /**
+     * Returns when statement
+     *
+     * If when condition is present node defined by the parent data definition
+     * statement is only valid when the returned XPath expression conceptually
+     * evaluates to "true" for a particular instance, then the node defined by
+     * the parent data definition statement is valid; otherwise, it is not.
+     *
+     * @return Optional of XPath condition
+     */
+    default Optional<RevisionAwareXPath> getWhenCondition() {
+        return Optional.absent();
+    }
+
+    /**
+     *
+     * Returns unknown schema nodes which belongs to this instance of the type
+     * <code>UsesNode</code>.
+     *
+     * @return list of unknown schema nodes defined under this uses node.
+     */
+    default List<UnknownSchemaNode> getUnknownSchemaNodes() {
+        return ImmutableList.of();
+    }
 }
