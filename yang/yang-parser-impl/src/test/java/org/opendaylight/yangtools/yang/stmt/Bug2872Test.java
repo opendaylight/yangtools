2da00471d1986ec623d280e56aced9b0ca05551f
Ryan Goulding <ryandgoulding@gmail.com>
2016-06-02 13:57:23 -0400

    Bug 2872 - Generated Java Enumerations should contain mapping to the string counter part
    The definition of EnumEffectiveStatementImpl.getName() was changed to return
    the "raw" name attribute from the input yang file.  The mapping is moved to
    mdsal through a corresponding change:
    
    https://git.opendaylight.org/gerrit/#/c/39734/
    
    A test is added to ensure that the name attribute is provided in its raw,
    unadulterated context.
    
    Change-Id: I2f2690251f21b9a3f8f4960f2f8c2ab1cdce6321
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug2872Test.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2016 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.common.SimpleDateFormatUtil;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.EnumTypeDefinition;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+
+public class Bug2872Test {
+
+    @Test
+    public void test() throws ReactorException, FileNotFoundException, URISyntaxException, ParseException {
+        final SchemaContext schema = StmtTestUtils.parseYangSources("/bugs/bug2872");
+        assertNotNull(schema);
+
+        final Date revision = SimpleDateFormatUtil.getRevisionFormat().parse("2016-06-08");
+        final QNameModule bug2872module = QNameModule.create(new URI("bug2872"), revision);
+        final QName foo = QName.create(bug2872module, "bar");
+
+        final DataSchemaNode dataSchemaNode = schema.getDataChildByName(foo);
+        assertTrue(dataSchemaNode instanceof LeafSchemaNode);
+        final LeafSchemaNode myLeaf = (LeafSchemaNode) dataSchemaNode;
+
+        final TypeDefinition<?> type = myLeaf.getType();
+        assertTrue(type instanceof EnumTypeDefinition);
+        final EnumTypeDefinition myEnum = (EnumTypeDefinition) type;
+
+        final List<EnumTypeDefinition.EnumPair> values = myEnum.getValues();
+        assertEquals(2, values.size());
+
+        final List<String> valueNames = new ArrayList<>();
+        for (EnumTypeDefinition.EnumPair pair : values) {
+            valueNames.add(pair.getName());
+        }
+        assertTrue(valueNames.contains("value-one"));
+        assertTrue(valueNames.contains("value-two"));
+    }
+}
