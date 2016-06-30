aabc3acfdb9ca46369480aa6cd93d37524487c79
Peter Kajsa <pkajsa@cisco.com>
2016-06-30 14:29:07 +0200

    (WIP) Bug 4955: Support unique statement for enforcement and secondary indices
    - introducing of TreeNodeIndex concept and providing one of its
      implementation i.e. UniqueIndex
    - implementation of unique constraints enforcing
    - unit test for unique constraint enforcing
    - introducing of IndexKey concept and providing one of its
      implementation i.e. UniqueIndexKey
    - introducing of data lookup in index
    
    - fix of comments from code review
    - reducing of copying of index maps to only once during tree node sealing
    - indexes map is maintained inside TreeNode
    - index modifications are performed at once during tree node sealing
    - unit test for values switch of two map entries
    
    - still in progress ...
    
    Change-Id: If3b94a085be034de28e341ac900142b021cd2a88
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/tree/spi/AbstractMutableContainerNode.java
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/tree/spi/AbstractMutableContainerNode.java
@@ -10,23 +10,29 @@ package org.opendaylight.yangtools.yang.data.api.schema.tree.spi;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Verify;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import org.opendaylight.yangtools.util.MapAdaptor;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodeContainer;
 
 /**
- * Abstract base for container-based {@link MutableTreeNode}s. It tracks modified nodes in a map and deals with
- * correctly implementing {@link #seal()}.
+ * Abstract base for container-based {@link MutableTreeNode}s. It tracks modified nodes in a map and deals
+ * with correctly implementing {@link #seal()}.
  */
 abstract class AbstractMutableContainerNode implements MutableTreeNode {
     private final Version version;
     private Map<PathArgument, TreeNode> children;
     private NormalizedNode<?, ?> data;
     private Version subtreeVersion;
+    private Map<Set<YangInstanceIdentifier>, TreeNodeIndex<?, ?>> indexes;
 
-    protected AbstractMutableContainerNode(final AbstractContainerNode parent, final Map<PathArgument, TreeNode> children) {
+    protected AbstractMutableContainerNode(final AbstractContainerNode parent,
+            final Map<PathArgument, TreeNode> children) {
         this.data = parent.getData();
+        this.indexes = MapAdaptor.getDefaultInstance().takeSnapshot(parent.getIndexes());
         this.version = parent.getVersion();
         this.subtreeVersion = parent.getSubtreeVersion();
         this.children = Preconditions.checkNotNull(children);
@@ -56,8 +62,20 @@ abstract class AbstractMutableContainerNode implements MutableTreeNode {
     }
 
     @Override
+    public final void addChild(final TreeNode child, final IndexStrategy strategy) {
+        addChild(child);
+        strategy.updateIndexes(indexes, child.getData());
+    }
+
+    @Override
     public final void removeChild(final PathArgument id) {
         children.remove(id);
+    }
+
+    @Override
+    public final void removeChild(final PathArgument id, final IndexStrategy strategy) {
+        removeChild(id);
+        strategy.removeFromIndexes(indexes, id);
     }
 
     @Override
@@ -77,22 +95,39 @@ abstract class AbstractMutableContainerNode implements MutableTreeNode {
          *    into data will not happen,
          * => more materialization can happen
          */
+        sealMutableIndexes();
         if (!version.equals(subtreeVersion)) {
             final Map<PathArgument, TreeNode> newChildren = MapAdaptor.getDefaultInstance().optimize(children);
             final int dataSize = getData().getValue().size();
             if (dataSize != newChildren.size()) {
                 Verify.verify(dataSize > newChildren.size(), "Detected %s modified children, data has only %s",
-                    newChildren.size(), dataSize);
-                ret = new LazyContainerNode(data, version, newChildren, subtreeVersion);
+                        newChildren.size(), dataSize);
+                ret = new LazyContainerNode(data, version, newChildren, subtreeVersion, indexes);
             } else {
-                ret = new MaterializedContainerNode(data, version, newChildren, subtreeVersion);
+                ret = new MaterializedContainerNode(data, version, newChildren, subtreeVersion, indexes);
             }
         } else {
-            ret = new SimpleContainerNode(data, version);
+            ret = new SimpleContainerNode(data, version, indexes);
         }
 
-        // This forces a NPE if this class is accessed again. Better than corruption.
+        // This forces a NPE if this class is accessed again. Better than
+        // corruption.
         children = null;
         return ret;
+    }
+
+    @Override
+    public void buildIndexes(final IndexStrategy strategy) {
+        this.indexes = strategy.createIndexesFromData(data);
+    }
+
+    private void sealMutableIndexes() {
+        for (final Entry<Set<YangInstanceIdentifier>, TreeNodeIndex<?, ?>> treeNodeIndexEntry : indexes.entrySet()) {
+            final TreeNodeIndex<?, ?> treeNodeIndex = treeNodeIndexEntry.getValue();
+            if (treeNodeIndex instanceof MutableTreeNodeIndex) {
+                final TreeNodeIndex<?, ?> sealed = ((MutableTreeNodeIndex<?, ?>) treeNodeIndex).seal();
+                indexes.put(treeNodeIndexEntry.getKey(), sealed);
+            }
+        }
     }
 }
