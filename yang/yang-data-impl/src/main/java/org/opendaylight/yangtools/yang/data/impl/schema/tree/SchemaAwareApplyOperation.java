7df61ffeefbb954227801de12693d32665fa344b
Igor Foltin <ifoltin@cisco.com>
2016-07-12 14:41:30 +0200

    Bug-5382: Structural lifecycle for lists and choices
    In addition to structural lifecycle management for non-presence
    container nodes, keyed lists(MapNode) and choices(ChoiceNode) are now
    managed in the same way.
    
    The behavior of these nodes is now different inside DataTree.
    They are no longer kept empty and are created on the fly when child
    node is submitted.
    
    Change-Id: I68220043c0f335324e5a8788b2872f57b750aae9
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>
    Signed-off-by: Igor Foltin <ifoltin@cisco.com>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/SchemaAwareApplyOperation.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/SchemaAwareApplyOperation.java
@@ -14,6 +14,7 @@ import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.AugmentationIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.ConflictingModificationAppliedException;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeConfiguration;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataValidationFailedException;
@@ -21,6 +22,7 @@ import org.opendaylight.yangtools.yang.data.api.schema.tree.ModificationType;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.TreeType;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.spi.TreeNode;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.spi.Version;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
 import org.opendaylight.yangtools.yang.model.api.AugmentationSchema;
 import org.opendaylight.yangtools.yang.model.api.AugmentationTarget;
 import org.opendaylight.yangtools.yang.model.api.ChoiceSchemaNode;
@@ -45,12 +47,16 @@ abstract class SchemaAwareApplyOperation extends ModificationApplyOperation {
             if (containerSchema.isPresenceContainer()) {
                 return new PresenceContainerModificationStrategy(containerSchema, treeConfig);
             } else {
-                return new StructuralContainerModificationStrategy(containerSchema, treeConfig);
+                return new StructuralModificationStrategyWrapper(
+                    new ContainerModificationStrategy(containerSchema, treeConfig),
+                    ImmutableNodes.containerNode(containerSchema.getQName()));
             }
         } else if (schemaNode instanceof ListSchemaNode) {
             return fromListSchemaNode((ListSchemaNode) schemaNode, treeConfig);
         } else if (schemaNode instanceof ChoiceSchemaNode) {
-            return new ChoiceModificationStrategy((ChoiceSchemaNode) schemaNode, treeConfig);
+            return new StructuralModificationStrategyWrapper(
+                new ChoiceModificationStrategy((ChoiceSchemaNode) schemaNode, treeConfig),
+                ImmutableNodes.choiceNode(schemaNode.getQName()));
         } else if (schemaNode instanceof LeafListSchemaNode) {
             return fromLeafListSchemaNode((LeafListSchemaNode) schemaNode, treeConfig);
         } else if (schemaNode instanceof LeafSchemaNode) {
@@ -79,23 +85,40 @@ abstract class SchemaAwareApplyOperation extends ModificationApplyOperation {
         }
     }
 
-    private static SchemaAwareApplyOperation fromListSchemaNode(final ListSchemaNode schemaNode, final DataTreeConfiguration treeConfig) {
+    private static ModificationApplyOperation fromListSchemaNode(final ListSchemaNode schemaNode, final DataTreeConfiguration treeConfig) {
+        final ModificationApplyOperation op;
+        final NormalizedNode<?, ?> emptyNode;
+
+        // Structural strategy wrapper is used to enable "structural" behaviour for auto create/delete mixin, parent
+        // list-like nodes
         final List<QName> keyDefinition = schemaNode.getKeyDefinition();
-        final SchemaAwareApplyOperation op;
-        if (keyDefinition == null || keyDefinition.isEmpty()) {
+        final boolean hasNoKey = keyDefinition == null || keyDefinition.isEmpty();
+
+        if (hasNoKey) {
+            // Unkeyed lists are not wrapped since they are not addressable (yet) and it's not allowed to modify
+            // unkeyed list entries. That's why auto create/delete is not needed
             op = new UnkeyedListModificationStrategy(schemaNode, treeConfig);
+            emptyNode = null;
         } else if (schemaNode.isUserOrdered()) {
-            op =  new OrderedMapModificationStrategy(schemaNode, treeConfig);
+            op = new OrderedMapModificationStrategy(schemaNode, treeConfig);
+            emptyNode = ImmutableNodes.orderedMapNode(schemaNode.getQName());
         } else {
             op = new UnorderedMapModificationStrategy(schemaNode, treeConfig);
+            emptyNode = ImmutableNodes.mapNode(schemaNode.getQName());
         }
-        return MinMaxElementsValidation.from(op, schemaNode);
+
+        final ModificationApplyOperation minMaxWrapper = MinMaxElementsValidation.from(op, schemaNode);
+
+        // It's important that StructuralModificationStrategyWrapper is on top of minMax, because minMax might perform
+        // validation on top of just deleted empty list parent node. This way, StructuralModificationStrategyWrapper
+        // can prevent it
+        return hasNoKey ? minMaxWrapper : new StructuralModificationStrategyWrapper(minMaxWrapper, emptyNode);
     }
 
-    private static SchemaAwareApplyOperation fromLeafListSchemaNode(final LeafListSchemaNode schemaNode, final DataTreeConfiguration treeConfig) {
+    private static ModificationApplyOperation fromLeafListSchemaNode(final LeafListSchemaNode schemaNode, final DataTreeConfiguration treeConfig) {
         final SchemaAwareApplyOperation op;
         if (schemaNode.isUserOrdered()) {
-            op =  new OrderedLeafSetModificationStrategy(schemaNode, treeConfig);
+            op = new OrderedLeafSetModificationStrategy(schemaNode, treeConfig);
         } else {
             op = new UnorderedLeafSetModificationStrategy(schemaNode, treeConfig);
         }
@@ -133,7 +156,7 @@ abstract class SchemaAwareApplyOperation extends ModificationApplyOperation {
         case NONE:
             break;
         default:
-            throw new UnsupportedOperationException("Suplied modification type "+ modification.getOperation()+ " is not supported.");
+            throw new UnsupportedOperationException("Supplied modification type "+ modification.getOperation()+ " is not supported.");
         }
     }
 
@@ -276,4 +299,5 @@ abstract class SchemaAwareApplyOperation extends ModificationApplyOperation {
     static boolean belongsToTree(final TreeType treeType, final DataSchemaNode node) {
         return treeType == TreeType.OPERATIONAL || node.isConfiguration();
     }
+
 }
