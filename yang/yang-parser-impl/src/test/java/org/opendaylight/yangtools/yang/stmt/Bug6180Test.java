4069117df5a9611328affa7c0bb85ea12d5e28e4
Peter Kajsa <pkajsa@cisco.com>
2016-07-14 12:46:14 +0200

    Bug 6180 - Parser: Backslash double-quote in double-quoted string not recognized
    Statement parser removes all double or single quotes from strings,
    what is incorrect. This fix removes only first and last quotes if they are
    present. All other quotes in the string should be escaped already, since the
    ANTLR parser recognizes only correctly enclosed strings.
    
    Change-Id: I2f596c73be05178dc5cbebaed12e25e428454219
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug6180Test.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.RevisionAwareXPath;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.PatternConstraint;
+import org.opendaylight.yangtools.yang.model.api.type.StringTypeDefinition;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+
+public class Bug6180Test {
+
+    @Test
+    public void test() throws ReactorException, SourceException, FileNotFoundException, URISyntaxException {
+        final SchemaContext schemaContext = StmtTestUtils.parseYangSources("/bugs/bug6180");
+        assertNotNull(schemaContext);
+
+        final DataSchemaNode dataNodeBar = schemaContext.getDataChildByName(QName.create("foo", "2016-07-11", "bar"));
+        assertTrue(dataNodeBar instanceof ContainerSchemaNode);
+        final ContainerSchemaNode bar = (ContainerSchemaNode) dataNodeBar;
+        final RevisionAwareXPath whenCondition = bar.getConstraints().getWhenCondition();
+        assertEquals("/foo != \\\"bar\\\"", whenCondition.toString());
+
+        final Set<TypeDefinition<?>> typeDefinitions = schemaContext.getTypeDefinitions();
+        assertEquals(1, typeDefinitions.size());
+        final TypeDefinition<?> type = typeDefinitions.iterator().next();
+        assertTrue(type instanceof StringTypeDefinition);
+        final List<PatternConstraint> patternConstraints = ((StringTypeDefinition) type).getPatternConstraints();
+        assertEquals(1, patternConstraints.size());
+        final PatternConstraint pattern = patternConstraints.iterator().next();
+        assertEquals("^\\\".*\\\"$", pattern.getRegularExpression());
+        assertTrue(Pattern.compile(pattern.getRegularExpression()).matcher("\"enclosed string in quotes\"").matches());
+    }
+}
