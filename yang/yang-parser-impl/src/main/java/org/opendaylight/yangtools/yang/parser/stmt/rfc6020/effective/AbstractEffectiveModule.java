c1c44d32f409452ab38c9ded293152506bddbb37
Robert Varga <rovarga@cisco.com>
2016-07-18 18:47:51 +0200

    BUG-865: remove SourceStreamAware
    This interface is a direct violation of the model.api contract.
    It is used only by the YangToSourceProcessor, which can implement
    the suggested fix.
    
    Change-Id: I90928a273503ee21e9663c115b7135cf5e1f0608
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/AbstractEffectiveModule.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/AbstractEffectiveModule.java
@@ -48,7 +48,6 @@ import org.opendaylight.yangtools.yang.model.api.stmt.SubmoduleStatement;
 import org.opendaylight.yangtools.yang.parser.spi.SubmoduleNamespace;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext.Mutable;
-import org.opendaylight.yangtools.yang.parser.spi.source.DeclarationInTextSource;
 import org.opendaylight.yangtools.yang.parser.spi.source.IncludedSubmoduleNameToIdentifier;
 import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.Utils;
 
@@ -56,7 +55,6 @@ abstract class AbstractEffectiveModule<D extends DeclaredStatement<String>> exte
         AbstractEffectiveDocumentedNode<String, D> implements Module, Immutable {
 
     private final String name;
-    private final String sourcePath;
     private final String prefix;
     private final YangVersion yangVersion;
     private final String organization;
@@ -97,12 +95,6 @@ abstract class AbstractEffectiveModule<D extends DeclaredStatement<String>> exte
 
         ContactEffectiveStatementImpl contactStmt = firstEffective(ContactEffectiveStatementImpl.class);
         this.contact = (contactStmt == null) ? null : contactStmt.argument();
-
-        if (ctx.getStatementSourceReference() instanceof DeclarationInTextSource) {
-            this.sourcePath = ((DeclarationInTextSource) ctx.getStatementSourceReference()).getSourceName();
-        } else {
-            this.sourcePath = null;
-        }
 
         // init submodules and substatements of submodules
         final List<EffectiveStatement<?, ?>> substatementsOfSubmodules;
@@ -252,11 +244,6 @@ abstract class AbstractEffectiveModule<D extends DeclaredStatement<String>> exte
             }
         }
         return resolvedModuleImports;
-    }
-
-    @Override
-    public String getModuleSourcePath() {
-        return sourcePath;
     }
 
     @Override
