5a946ea92f212e5f504a8d90a96331392f8be60b
Robert Varga <rovarga@cisco.com>
2016-07-18 18:32:41 +0200

    BUG-865: make EnumPair getQName/getSchemaPath unimplemented
    EnumPairs by definition do not have SchemaPath, which makes
    them non-SchemaNodes. As a first step make the methods
    unimplemented so we can catch offenders.
    
    Change-Id: I19cb47368ea5b575fa2ce89bb0c4b6a934bdaae3
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/EffectiveStatementTypeTest.java
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/EffectiveStatementTypeTest.java
@@ -224,9 +224,7 @@ public class EffectiveStatementTypeTest {
         assertFalse(enumSpecEff.equals("test"));
         assertTrue(enumSpecEff.equals(enumSpecEff));
 
-        assertEquals("zero", enumEff.getQName().getLocalName());
         assertEquals("zero", enumEff.getName());
-        assertNotNull(enumEff.getPath());
         assertNotNull(enumEff.getUnknownSchemaNodes());
         assertEquals("test enum", enumEff.getDescription());
         assertEquals("test enum ref", enumEff.getReference());
