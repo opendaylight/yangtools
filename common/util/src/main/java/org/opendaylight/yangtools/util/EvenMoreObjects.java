c63bbca92612f9ac235c58a27f89bfd467611006
Michael Vorburger <vorburger@redhat.com>
2016-07-26 01:26:14 +0200

    equalsHelper (MoreObjects2)
    Moving this class "downwards" from mdsalutil, because this really is a
    generic utility helper class, which probably best fits into
    yangtools.utils.
    
    Change-Id: I0d8c6dd44790a85f7efccd6f83d42e6bd7e084bb
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ common/util/src/main/java/org/opendaylight/yangtools/util/EvenMoreObjects.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.util;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.MoreObjects;
+import java.util.function.BiFunction;
+
+/**
+ * Utility helping to implement readable equals() methods.
+ *
+ * <p>See the example equals() of class Thing in the MoreObjects2Test for how to use this.
+ *
+ * <p>See <a href="https://github.com/google/guava/issues/2521">Guava issue proposing contributing this</a>.
+ *
+ * @see MoreObjects
+ *
+ * @author Michael Vorburger, Red Hat
+ */
+@Beta
+public final class EvenMoreObjects {
+
+    @SuppressWarnings("unchecked")
+    public static <T> boolean equalsHelper(T self, Object other, BooleanEqualsFunction<T> equals) {
+        if (other == null) {
+            return false;
+        }
+        if (other == self) {
+            return true;
+        }
+        if (self.getClass() != other.getClass()) {
+            return false;
+        }
+        return equals.apply(self, (T) other);
+    }
+
+    @FunctionalInterface
+    public interface BooleanEqualsFunction<T> extends BiFunction<T, T, Boolean> { }
+
+    private EvenMoreObjects() { }
+}
