f07653736cdf6665a0ab8e39fe5427a095c1730b
Michael Vorburger <vorburger@redhat.com>
2016-07-26 05:46:08 +0200

    Intro. new yangtools.testutils artifacts (incl. Mikito)
    Seeding new project with proposed Mikito (new), and a
    MockitoUnstubbedMethodExceptionAnswer (alternative to
    yangtools.mockito-configuration; see the package-info.java in
    mockito-configuration about why this is not being proposed in that
    project).
    
    Change-Id: I78237936924e0befc0ff17e7604e0fb33262d7cd
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ common/testutils/src/main/java/org/opendaylight/yangtools/testutils/mockito/ThrowsMethodExceptionAnswer.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.testutils.mockito;
+
+import com.google.common.annotations.Beta;
+import java.io.Serializable;
+import org.mockito.Mockito;
+import org.mockito.internal.stubbing.answers.ThrowsException;
+import org.mockito.internal.stubbing.answers.ThrowsExceptionClass;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+/**
+ * Mockito Answer which for un-stubbed methods throws an
+ * UnstubbedMethodException (instead of Mockito's default of returning null).
+ *
+ * <p>
+ * Usage:
+ *
+ * <pre>
+ * import static ...testutils.mockito.MoreAnswers.exception;
+ *
+ * Mockito.mock(YourInterface.class, exception())
+ * </pre>
+ *
+ * @see Mockito#mock(Class, Answer)
+ *
+ * @see ThrowsException
+ * @see ThrowsExceptionClass
+ *
+ * @author Michael Vorburger
+ */
+@Beta
+public class ThrowsMethodExceptionAnswer implements Answer<Object>, Serializable {
+    private static final long serialVersionUID = -7316574192253912318L;
+
+    /**
+     * Use {@link MoreAnswers} to obtain an instance.
+     */
+    ThrowsMethodExceptionAnswer() {
+    }
+
+    @Override
+    public Void answer(InvocationOnMock invocation) throws Throwable {
+        throw new UnstubbedMethodException(invocation.getMethod());
+    }
+
+}
