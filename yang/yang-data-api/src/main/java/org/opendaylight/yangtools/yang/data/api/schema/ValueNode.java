061de46858541547ff61921b0bfac47e44b31c7c
Robert Varga <rovarga@cisco.com>
2016-07-26 23:38:12 +0200

    Introduce common interface for getValue() method
    LeafNode and LeafSetEntryNode both share this method. Codec-type users
    benefit from being able to call on a common interface bridging the two
    otherwise unrelated classes.
    
    Change-Id: I5ef86c6859cf6cb7620aa3e4477941f248640c4f
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/ValueNode.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.api.schema;
+
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+
+/**
+ * Interface holding the common trait of {@link LeafSetEntryNode} and {@link LeafNode}, which both hold a value.
+ *
+ * @author Robert Varga
+ *
+ * <T> value type
+ */
+public interface ValueNode<I extends PathArgument, T> extends NormalizedNode<I, T> {
+    /**
+     * Returns value of held by this node.
+     *
+     * <h3>Implementation notes</h3> Invocation of {@link #getValue()} must
+     * provides same value as value in {@link #getIdentifier()}.
+     * <code>true == this.getIdentifier().getValue().equals(this.getValue())</code>
+     *
+     * @return Returned value of this node. Value SHOULD meet criteria
+     *         defined by schema.
+     *
+     */
+    @Override
+    T getValue();
+}
