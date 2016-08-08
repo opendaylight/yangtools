2681bac35517ac91a941874012b69c931b7ffa3c
Peter Kajsa <pkajsa@cisco.com>
2016-08-08 08:32:06 +0200

    Bug 4955: Unique constraints performance tests
    Change-Id: I5fd5a46dac175cf3ab152cc607918d68af03bae4
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/UniqueConstraintPerformanceTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.impl.schema.tree;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataValidationFailedException;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+
+public class UniqueConstraintPerformanceTest {
+    private static final int INITIAL_WRITE_COUNT = 100000;
+    private static final int SECOND_WRITE_COUNT = 100;
+
+    @Test
+    public void mapEntryTest() throws ReactorException, DataValidationFailedException {
+        final SchemaContext schemaContext = TestModel.createTestContext("/bug4955/foo.yang");
+        assertNotNull("Schema context must not be null.", schemaContext);
+
+        final InMemoryDataTree inMemoryDataTree = UniqueConstraintPerformanceTestUtils.initDataTree(schemaContext,
+                true, INITIAL_WRITE_COUNT);
+        UniqueConstraintPerformanceTestUtils.writeMapEntriesInSequence(inMemoryDataTree, SECOND_WRITE_COUNT,
+                INITIAL_WRITE_COUNT);
+    }
+}
