46ae4a706d07d2056ff2e6bd7323cc07bf3e20f5
Igor Foltin <ifoltin@cisco.com>
2016-08-16 10:10:32 +0200

    Bug 6410: Fixed initialization of typedefs in rpc
    Typedef substatements in RpcEffectiveStatementImpl are now initialized correctly.
    
    Change-Id: Ic9a02727ffb4b6e6c798360e2266dc99d77c79e7
    Signed-off-by: Igor Foltin <ifoltin@cisco.com>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug6410Test.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Set;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.RpcDefinition;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.YangStatementSourceImpl;
+
+public class Bug6410Test {
+
+    @Test
+    public void testTypedefsInRpc() throws ReactorException {
+        final SchemaContext schemaContext = StmtTestUtils.parseYangSources(
+                new YangStatementSourceImpl("/bugs/bug6410/foo.yang", false));
+
+        final Set<Module> modules = schemaContext.getModules();
+        assertEquals(1, modules.size());
+        final Module module = modules.iterator().next();
+
+        final Set<RpcDefinition> rpcs = module.getRpcs();
+        assertEquals(1, rpcs.size());
+        final RpcDefinition rpc = rpcs.iterator().next();
+
+        final Set<TypeDefinition<?>> typeDefs = rpc.getTypeDefinitions();
+        assertEquals(2, typeDefs.size());
+    }
+
+    @Test
+    public void shouldFailOnDuplicateTypedefs() {
+        try {
+            final SchemaContext schemaContext = StmtTestUtils.parseYangSources(
+                    new YangStatementSourceImpl("/bugs/bug6410/bar.yang", false));
+            fail("A ReactorException should have been thrown.");
+        } catch (ReactorException ex) {
+            assertTrue(ex.getCause() instanceof SourceException);
+            assertTrue(ex.getCause().getMessage().contains("Node name collision"));
+        }
+    }
+}
