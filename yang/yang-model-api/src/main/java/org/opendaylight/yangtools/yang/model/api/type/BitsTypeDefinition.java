c9a6ed1bb97f246319aec053599f66f0bd5721a3
Igor Foltin <ifoltin@cisco.com>
2016-08-16 15:06:58 +0200

    BUG-6316: Fix Bit and EnumPair's position/value types
    yang.model.api.type defines the effective model of the world,
    where these attributes cannot ever be null, which is in contrast
    to yang.model.api.stmt, which is the declared model -- where they
    can in fact be null.
    
    Fix this discrepancy by forcing the methods to return simple types
    
    Change-Id: I27b9d6d283cd14b2044890d9ccd19bf92647f27e
    Signed-off-by: Robert Varga <rovarga@cisco.com>
    Signed-off-by: Igor Foltin <ifoltin@cisco.com>
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/type/BitsTypeDefinition.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/type/BitsTypeDefinition.java
@@ -13,9 +13,7 @@ import org.opendaylight.yangtools.yang.model.api.SchemaNode;
 import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
 
 /**
- *
  * Makes is possible to access to the individual bits values of this type.
- *
  */
 public interface BitsTypeDefinition extends TypeDefinition<BitsTypeDefinition> {
     /**
@@ -33,18 +31,18 @@ public interface BitsTypeDefinition extends TypeDefinition<BitsTypeDefinition> {
      */
     interface Bit extends SchemaNode {
         /**
-         * The position value MUST be in the range 0 to 4294967295, and it MUST
-         * be unique within the bits type.
-         *
-         * @return The position value of bit in range from 0 to 4294967295.
-         */
-        Long getPosition();
-
-        /**
          * Returns the name of the concrete bit.
          *
          * @return string with the name of the concrete bit
          */
         @Nonnull String getName();
+
+        /**
+         * The position value MUST be in the range 0 to 4294967295, and it MUST
+         * be unique within the bits type.
+         *
+         * @return The position value of bit in range from 0 to 4294967295.
+         */
+        long getPosition();
     }
 }
