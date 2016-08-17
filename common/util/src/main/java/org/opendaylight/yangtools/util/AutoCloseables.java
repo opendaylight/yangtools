fbea06bf7463496fdd0d87073ec4087f66941d2b
Michael Vorburger <vorburger@redhat.com>
2016-08-17 14:39:24 +0200

    AutoCloseables closeAndLog() & closeQuietly()
    Change-Id: I37284657aff5da0f7a684f884834e8eaa8f77cc5
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ common/util/src/main/java/org/opendaylight/yangtools/util/AutoCloseables.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.util;
+
+import com.google.common.annotations.Beta;
+import com.google.common.io.Closeables;
+import javax.annotation.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Utility methods for working with {@link AutoCloseable} objects.
+ *
+ * <p>Neither Google Guava nor Apache Commons IO appears to have this.
+ *
+ * @see Closeables
+ *
+ * @author Michael Vorburger
+ */
+@Beta
+public final class AutoCloseables {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AutoCloseables.class);
+
+    private AutoCloseables() { }
+
+    /**
+     * Close, and log any exception that occurred as a warning.
+     * This method variant should be used in 99% of cases, just to be sure.
+     *
+     * @param closeable an AutoCloseable to close()
+     */
+    @SuppressWarnings("checkstyle:IllegalCatch")
+    public static void closeAndLog(@Nullable AutoCloseable closeable) {
+        if (closeable == null) {
+            return;
+        }
+        try {
+            closeable.close();
+        } catch (Exception e) {
+            LOG.warn("Failed to close() AutoCloseable: " + closeable.toString(), e);
+        }
+    }
+
+    /**
+     * Close, and log any exception that occurred as a warning. This method
+     * variant should be used only in rare exceptional (pun intended) cases,
+     * when a close() is known to cause completely harmless Exceptions (are you
+     * sure?) which when shown in logging would certainly only confuse
+     * end-users, and so must be 100% suppressed. This is rare.
+     *
+     * @param closeable an AutoCloseable to close()
+     */
+    @SuppressWarnings("checkstyle:IllegalCatch")
+    public static void closeQuietly(@Nullable AutoCloseable closeable) {
+        if (closeable == null) {
+            return;
+        }
+        try {
+            closeable.close();
+        } catch (Exception e) {
+            // Silently ignore
+        }
+    }
+}
