6d051fb473374c133474d2e90aa68663ae948763
Robert Varga <rovarga@cisco.com>
2016-08-30 18:41:48 +0200

    BUG-6497: Do not lose augmentation statement order
    Using an interim HashSet can cause unpredictable order
    of augmentations, which means the binding spec will not
    assign consistent mapping at runtime.
    
    Fix this by using a LinkedHashSet, which retains insertion
    order.
    
    Change-Id: I503102d8b61453cc9c8d35b07158cd97138cda42
    Signed-off-by: Robert Varga <rovarga@cisco.com>
    (cherry picked from commit 3c4bdfac3a8b04ecd1d6eab2cdadbb365b54664e)

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/ChoiceEffectiveStatementImpl.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/ChoiceEffectiveStatementImpl.java
@@ -11,7 +11,7 @@ import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.SortedSet;
@@ -51,7 +51,7 @@ public final class ChoiceEffectiveStatementImpl extends AbstractEffectiveDataSch
 
         // initSubstatementCollectionsAndFields
         Collection<? extends EffectiveStatement<?, ?>> effectiveSubstatements = effectiveSubstatements();
-        Set<AugmentationSchema> augmentationsInit = new HashSet<>();
+        Set<AugmentationSchema> augmentationsInit = new LinkedHashSet<>();
         SortedSet<ChoiceCaseNode> casesInit = new TreeSet<>(Comparators.SCHEMA_NODE_COMP);
 
         for (EffectiveStatement<?, ?> effectiveStatement : effectiveSubstatements) {
