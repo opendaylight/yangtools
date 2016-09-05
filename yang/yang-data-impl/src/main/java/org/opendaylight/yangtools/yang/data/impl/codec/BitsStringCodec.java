0e08c0b558d501593311c6016df7c71dd694ddd0
Robert Varga <rovarga@cisco.com>
2016-09-05 15:18:54 +0200

    BUG-5561: use canonical Bits order
    This patch makes BitsTypeBuilder order the bit definitions
    according to their position. It also adjusts BitsStringCodec
    to normalize user input strings to the canonical ordering.
    
    Change-Id: Ia4123a6275130619f9ef91d24b7340520707a67c
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/codec/BitsStringCodec.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/codec/BitsStringCodec.java
@@ -11,8 +11,9 @@ import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Splitter;
+import com.google.common.collect.Collections2;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Sets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import org.opendaylight.yangtools.yang.data.api.codec.BitsCodec;
@@ -25,20 +26,15 @@ final class BitsStringCodec extends TypeDefinitionAwareCodec<Set<String>, BitsTy
     private static final Joiner JOINER = Joiner.on(" ").skipNulls();
     private static final Splitter SPLITTER = Splitter.on(' ').omitEmptyStrings().trimResults();
 
-    private final Set<String> bits;
+    private final Set<String> validBits;
 
     @SuppressWarnings("unchecked")
     private BitsStringCodec(final Optional<BitsTypeDefinition> typeDef) {
         super(typeDef, (Class<Set<String>>) ((Class<?>) Set.class));
         if (typeDef.isPresent()) {
-            final List<Bit> yangBits = typeDef.get().getBits();
-            final Set<String> bitsBuilder = Sets.newHashSet();
-            for (final Bit bit : yangBits) {
-                bitsBuilder.add(bit.getName());
-            }
-            bits = ImmutableSet.copyOf(bitsBuilder);
+            validBits = ImmutableSet.copyOf(Collections2.transform(typeDef.get().getBits(), Bit::getName));
         } else {
-            bits = null;
+            validBits = null;
         }
     }
 
@@ -57,17 +53,30 @@ final class BitsStringCodec extends TypeDefinitionAwareCodec<Set<String>, BitsTy
             return ImmutableSet.of();
         }
 
-        final Iterable<String> strings = SPLITTER.split(stringRepresentation);
-        validate(strings);
-        return ImmutableSet.copyOf(strings);
-    }
+        final Set<String> strings = ImmutableSet.copyOf(SPLITTER.split(stringRepresentation));
+        if (validBits == null) {
+            // No schema available, use what we have
+            return strings;
+        }
 
-    private void validate(final Iterable<String> strings) {
-        if (bits != null) {
-            for (final String bit : strings) {
-                Preconditions.checkArgument(bits.contains(bit),
-                        "Invalid value '%s' for bits type. Allowed values are: %s", bit, bits);
+        // Normalize strings to schema first, retaining definition order
+        final List<String> sorted = new ArrayList<>(strings.size());
+        for (final String bit : validBits) {
+            if (strings.contains(bit)) {
+                sorted.add(bit);
             }
         }
+
+        // Check sizes, if the normalized set does not match non-normalized size, non-normalized strings contain
+        // an invalid bit.
+        if (sorted.size() != strings.size()) {
+            for (final String bit : strings) {
+                Preconditions.checkArgument(validBits.contains(bit),
+                    "Invalid value '%s' for bits type. Allowed values are: %s", bit, validBits);
+            }
+        }
+
+        // In case all valid bits have been specified, retain the set we have created for this codec
+        return sorted.size() == validBits.size() ? validBits : ImmutableSet.copyOf(sorted);
     }
 }
