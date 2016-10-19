cb76fee3e4e5257b6f0b48d9fd91ee3c91fd76ad
Filip Gregor <fgregor@cisco.com>
2016-10-19 15:57:33 +0200

    Bug 6491: Some imports are not exposed in Module.getImports
    in case of no revision in module and imported module, method
    getImports returns empty Set.
    added tests, fixed Set with imports in AbstractEffectiveModule
    
    Change-Id: Ie23196d9cbcca98266580d11a85d66c06ac8561c
    Signed-off-by: Filip Gregor <fgregor@cisco.com>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug6491Test.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Set;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.SimpleDateFormatUtil;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.ModuleImport;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+public class Bug6491Test {
+
+    @Test
+    public void testBug6491SchemaContext() throws Exception {
+        final SchemaContext context = StmtTestUtils.parseYangSources("/bugs/bug6491/");
+        assertNotNull(context);
+        final Module module = context.findModuleByName("bar", SimpleDateFormatUtil.DEFAULT_DATE_REV);
+        assertNotNull(module);
+        final Set<ModuleImport> imports = module.getImports();
+        assertNotNull(imports);
+        assertEquals(1, imports.size());
+        assertEquals(SimpleDateFormatUtil.DEFAULT_DATE_REV, imports.iterator().next().getRevision());
+    }
+}
