a9aa900b8c5d1dbe16011a0c6c0f00cbb2ec105e
Filip Gregor <fgregor@cisco.com>
2016-10-19 15:57:33 +0200

    Bug 6491: Some imports are not exposed in Module.getImports
    in case of no revision in module and imported module, method
    getImports returns empty Set.
    added tests, fixed Set with imports in AbstractEffectiveModule
    
    Change-Id: Ie23196d9cbcca98266580d11a85d66c06ac8561c
    Signed-off-by: Filip Gregor <fgregor@cisco.com>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug6491Test.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.opendaylight.yangtools.yang.common.SimpleDateFormatUtil.DEFAULT_DATE_REV;
+import static org.opendaylight.yangtools.yang.common.SimpleDateFormatUtil.getRevisionFormat;
+
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Set;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.ModuleImport;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+
+public class Bug6491Test {
+    private Date date;
+
+    @Before
+    public void setup() throws ParseException {
+        date = getRevisionFormat().parse("2016-01-01");
+    }
+
+    @Test
+    public void tetststs() throws ReactorException, FileNotFoundException, URISyntaxException {
+        testRevision("withoutRevision", DEFAULT_DATE_REV, DEFAULT_DATE_REV);
+        testRevision("withRevision", date, date);
+        testRevision("importedModuleRevisionOnly", DEFAULT_DATE_REV, date);
+        testRevision("moduleRevisionOnly", date, DEFAULT_DATE_REV);
+    }
+
+    private void testRevision(final String path, final Date moduleRevision, final Date importedRevision) throws
+            ReactorException, FileNotFoundException, URISyntaxException {
+        final SchemaContext context = StmtTestUtils.parseYangSources("/bugs/bug6491/".concat(path));
+        assertNotNull(context);
+        final Module module = context.findModuleByName("bar", moduleRevision);
+        assertNotNull(module);
+        final Set<ModuleImport> imports = module.getImports();
+        assertNotNull(imports);
+        assertEquals(1, imports.size());
+        assertEquals(importedRevision, imports.iterator().next().getRevision());
+    }
+}
