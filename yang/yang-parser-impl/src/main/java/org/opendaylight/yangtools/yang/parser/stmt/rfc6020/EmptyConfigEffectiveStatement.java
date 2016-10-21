77404039e6c805f0102fe15c334fd791a813a38d
Robert Varga <rovarga@cisco.com>
2016-10-21 11:00:15 +0200

    BUG-6522: share instances of Config(Effective)Statement
    Config statement has low cardinality, use specialized classes
    for its representation.
    
    Change-Id: I7b5dc7e7069e5e7cfa4808d44a95f147f6b7073d
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/EmptyConfigEffectiveStatement.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.stmt.rfc6020;
+
+import com.google.common.collect.ImmutableList;
+import java.util.Collection;
+import java.util.Map;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.IdentifierNamespace;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementSource;
+import org.opendaylight.yangtools.yang.model.api.stmt.ConfigStatement;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.ConfigEffectiveStatement;
+
+abstract class EmptyConfigEffectiveStatement implements ConfigEffectiveStatement {
+    static final EmptyConfigEffectiveStatement FALSE = new EmptyConfigEffectiveStatement() {
+        @Override
+        public ConfigStatement getDeclared() {
+            return EmptyConfigStatement.FALSE;
+        }
+    };
+
+    static final EmptyConfigEffectiveStatement TRUE = new EmptyConfigEffectiveStatement() {
+        @Override
+        public ConfigStatement getDeclared() {
+            return EmptyConfigStatement.TRUE;
+        }
+    };
+
+    private EmptyConfigEffectiveStatement() {
+        // Hidden
+    }
+
+    @Override
+    public final StatementDefinition statementDefinition() {
+        return getDeclared().statementDefinition();
+    }
+
+    @Override
+    public final Boolean argument() {
+        return getDeclared().argument();
+    }
+
+    @Override
+    public final StatementSource getStatementSource() {
+        return getDeclared().getStatementSource();
+    }
+
+    @Override
+    public final <K, V, N extends IdentifierNamespace<K, V>> V get(final Class<N> namespace, final K identifier) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    @Override
+    public final <K, V, N extends IdentifierNamespace<K, V>> Map<K, V> getAll(final Class<N> namespace) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    @Override
+    public final Collection<? extends EffectiveStatement<?, ?>> effectiveSubstatements() {
+        return ImmutableList.of();
+    }
+}
