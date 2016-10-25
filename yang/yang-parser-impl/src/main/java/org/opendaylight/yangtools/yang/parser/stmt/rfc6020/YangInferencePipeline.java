476d9db7e894088855af36daba92da5b47d7fd23
Robert Varga <rovarga@cisco.com>
2016-10-26 01:24:15 +0200

    WIP: BUG-6522: create a dedecated extensions map
    SourceSpecificContext already calls out to BuildGlobalContext
    to lookup up support statements, hence the potential extension
    lookup will be slower.
    
    This lookup occurs before we check for type arguments, hence
    we want to keep it fast. Therefore we take a snapshot of all
    extensions defined in the global context.
    
    XXX: this needs further work, as the extensions should be really
         resolved via imports, so that extensions which are not
         imported are not visible...
    
    Change-Id: I0c50de89cee5d4297e8ccd0cb61203c3cfe9a2f3
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/YangInferencePipeline.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/YangInferencePipeline.java
@@ -144,7 +144,7 @@ public final class YangInferencePipeline {
             .addSupport(new FractionDigitsStatementImpl.Definition())
             .addSupport(new BaseStatementImpl.Definition())
             .addSupport(global(DerivedIdentitiesNamespace.class))
-            .addSupport(global(StatementDefinitionNamespace.class))
+            .addSupport(sourceLocal(StatementDefinitionNamespace.class))
             .build();
 
     public static final StatementSupportBundle FULL_DECL_BUNDLE = StatementSupportBundle
