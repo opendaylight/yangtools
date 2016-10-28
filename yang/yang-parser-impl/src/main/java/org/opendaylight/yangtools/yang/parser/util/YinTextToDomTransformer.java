ca99400dacc10bcd332566178677073de5476915
Robert Varga <rovarga@cisco.com>
2016-10-28 11:08:18 +0200

    BUG-7054: add a YinTextToDomTransformer
    This is needed to turn a YIN text document into an in-memory
    DOM document. That will be then processed by the parser.
    
    Change-Id: I5d3ced18c99f1298b8385247cbfc8fef32e927bb
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/util/YinTextToDomTransformer.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.util;
+
+import com.google.common.annotations.Beta;
+import com.google.common.util.concurrent.Futures;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.dom.DOMSource;
+import org.opendaylight.yangtools.yang.model.repo.api.SchemaRepository;
+import org.opendaylight.yangtools.yang.model.repo.api.YinDomSchemaSource;
+import org.opendaylight.yangtools.yang.model.repo.api.YinTextSchemaSource;
+import org.opendaylight.yangtools.yang.model.repo.spi.SchemaSourceRegistry;
+import org.opendaylight.yangtools.yang.model.repo.util.SchemaSourceTransformer;
+import org.w3c.dom.Document;
+
+/**
+ * A {@link SchemaSourceTransformer} which handles translation of models from
+ * {@link YinTextSchemaSource} representation into {@link YinDomSchemaSource}.
+ *
+ * @author Robert Varga
+ */
+@Beta
+public final class YinTextToDomTransformer extends SchemaSourceTransformer<YinTextSchemaSource, YinDomSchemaSource> {
+    private static final DocumentBuilderFactory DBF;
+    static {
+        final DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
+        f.setCoalescing(true);
+        f.setExpandEntityReferences(false);
+        f.setIgnoringElementContentWhitespace(true);
+        f.setIgnoringComments(true);
+        f.setNamespaceAware(true);
+        f.setXIncludeAware(false);
+        try {
+            f.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            f.setFeature("http://xml.org/sax/features/external-general-entities", false);
+            f.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+        } catch (final ParserConfigurationException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+        DBF = f;
+    }
+
+    private YinTextToDomTransformer(final SchemaRepository provider, final SchemaSourceRegistry consumer) {
+        super(provider, YinTextSchemaSource.class, consumer, YinDomSchemaSource.class, input -> {
+            final Document doc = DBF.newDocumentBuilder().parse(input.openStream());
+            return Futures.immediateCheckedFuture(YinDomSchemaSource.create(input.getIdentifier(), new DOMSource(doc)));
+        });
+    }
+
+    public static YinTextToDomTransformer create(final SchemaRepository provider, final SchemaSourceRegistry consumer) {
+        return new YinTextToDomTransformer(provider, consumer);
+    }
+}
