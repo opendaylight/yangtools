70d0e77da31d92d060376043cc0e313d2f118fc1
Robert Varga <rovarga@cisco.com>
2016-10-28 12:21:46 +0200

    BUG-7057: introduce UntrustedXML class
    This adds the basic newDocumentBuilder() method. The class
    is expected to grow more methods in future.
    
    Change-Id: I019891af5f9e8be15eda158bb6e40243dcec0614
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ common/util/src/main/java/org/opendaylight/yangtools/util/xml/UntrustedXML.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.util.xml;
+
+import com.google.common.annotations.Beta;
+import javax.annotation.Nonnull;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+/**
+ * Set of utility methods for instantiating parser that deal with untrusted XML sources.
+ *
+ * @author Robert Varga
+ */
+@Beta
+public final class UntrustedXML {
+    private static final DocumentBuilderFactory DBF;
+    static {
+        final DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
+        f.setCoalescing(true);
+        f.setExpandEntityReferences(false);
+        f.setIgnoringElementContentWhitespace(true);
+        f.setIgnoringComments(true);
+        f.setNamespaceAware(true);
+        f.setXIncludeAware(false);
+        try {
+            f.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            f.setFeature("http://xml.org/sax/features/external-general-entities", false);
+            f.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+        } catch (final ParserConfigurationException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+        DBF = f;
+    }
+
+    /**
+     * Create a new {@link DocumentBuilder} for dealing with untrusted XML data. This method is equivalent to
+     * {@link DocumentBuilderFactory#newDocumentBuilder()}, except it does not throw a checked exception.
+     *
+     * @return A new DocumentBuilder
+     * @throws UnsupportedOperationException if the runtime fails to instantiate a good enough builder
+     */
+    public static @Nonnull DocumentBuilder newDocumentBuilder() {
+        try {
+            return DBF.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            throw new UnsupportedOperationException("Failed to instantiate a DocumentBuilder", e);
+        }
+    }
+}
