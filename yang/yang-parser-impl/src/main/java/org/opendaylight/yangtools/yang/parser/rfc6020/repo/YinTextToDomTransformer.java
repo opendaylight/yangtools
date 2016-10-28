a39c0b5a72240f7f3e896157004359d4eb3cf1af
Robert Varga <rovarga@cisco.com>
2016-10-28 14:42:04 +0200

    BUG-7054: add YinStatementStreamSource
    This is a basic stream source, which traverses a DOM node
    and its children, emitting stream events.
    
    Change-Id: I536bd97c4b94a818cdf366b0028251f1100d9667
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/rfc6020/repo/YinTextToDomTransformer.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/rfc6020/repo/YinTextToDomTransformer.java
@@ -9,7 +9,6 @@ package org.opendaylight.yangtools.yang.parser.rfc6020.repo;
 
 import com.google.common.annotations.Beta;
 import com.google.common.util.concurrent.Futures;
-import java.io.IOException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.transform.dom.DOMSource;
 import org.opendaylight.yangtools.util.xml.UntrustedXML;
@@ -19,7 +18,6 @@ import org.opendaylight.yangtools.yang.model.repo.api.YinTextSchemaSource;
 import org.opendaylight.yangtools.yang.model.repo.spi.SchemaSourceRegistry;
 import org.opendaylight.yangtools.yang.model.repo.util.SchemaSourceTransformer;
 import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 /**
@@ -30,21 +28,23 @@ import org.xml.sax.helpers.DefaultHandler;
  */
 @Beta
 public final class YinTextToDomTransformer extends SchemaSourceTransformer<YinTextSchemaSource, YinDomSchemaSource> {
+
+    @Deprecated
+    public static final Transformation<YinTextSchemaSource, YinDomSchemaSource> TRANSFORMATION = input -> {
+        final Document doc = UntrustedXML.newDocumentBuilder().newDocument();
+        final SAXParser parser = UntrustedXML.newSAXParser();
+        final DefaultHandler handler = new StatementSourceReferenceHandler(doc, null);
+        parser.parse(input.openStream(), handler);
+
+        return Futures.immediateCheckedFuture(YinDomSchemaSource.create(input.getIdentifier(), new DOMSource(doc)));
+    };
+
     private YinTextToDomTransformer(final SchemaRepository provider, final SchemaSourceRegistry consumer) {
-        super(provider, YinTextSchemaSource.class, consumer, YinDomSchemaSource.class,
-            input -> Futures.immediateCheckedFuture(parseDocument(input)));
+        super(provider, YinTextSchemaSource.class, consumer, YinDomSchemaSource.class, TRANSFORMATION);
     }
 
     public static YinTextToDomTransformer create(final SchemaRepository provider, final SchemaSourceRegistry consumer) {
         return new YinTextToDomTransformer(provider, consumer);
     }
 
-    private static YinDomSchemaSource parseDocument(final YinTextSchemaSource source) throws IOException, SAXException {
-        final Document doc = UntrustedXML.newDocumentBuilder().newDocument();
-        final SAXParser parser = UntrustedXML.newSAXParser();
-        final DefaultHandler handler = new StatementSourceReferenceHandler(doc, null);
-        parser.parse(source.openStream(), handler);
-
-        return YinDomSchemaSource.create(source.getIdentifier(), new DOMSource(doc));
-    }
 }
