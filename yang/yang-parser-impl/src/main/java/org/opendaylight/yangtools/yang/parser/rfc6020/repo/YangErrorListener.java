9e17ee71f3acfc60b8fdc5afde0844439d81147a
Robert Varga <rovarga@cisco.com>
2016-10-29 11:27:01 +0200

    BUG-7052: move YangErrorListener
    This class is used by the YANG-specific parser, move it
    out of generic 'impl' package to rfc6020.repo.
    
    Change-Id: I402851a4e6aa85090d6f736014ea440c8c79dc5e
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/rfc6020/repo/YangErrorListener.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/rfc6020/repo/YangErrorListener.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.yangtools.yang.parser.impl;
+package org.opendaylight.yangtools.yang.parser.rfc6020.repo;
 
 import java.util.ArrayList;
 import java.util.List;
