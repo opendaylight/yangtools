91130c16519b92b1cc21b979bbad9bbbfbfb322d
Robert Varga <rovarga@cisco.com>
2016-10-31 00:03:53 +0100

    WIP: BUG-7038: lookup statements by argument
    This patch reworks the lookup process to take statement
    argument into account.
    
    Change-Id: If95fec255136b9b1caad937c091d21295e208578
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/BuildGlobalContext.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/BuildGlobalContext.java
@@ -163,6 +163,11 @@ class BuildGlobalContext extends NamespaceStorageSupport implements NamespaceBeh
         return new SimpleNamespaceContext<>(potentialRaw);
     }
 
+    /**
+     *
+     * @deprecated See {@link #getStatementDefinition(StatementSupport)}.
+     */
+    @Deprecated
     StatementDefinitionContext<?, ?, ?> getStatementDefinition(final YangVersion version, final QName name) {
         StatementDefinitionContext<?, ?, ?> potential = definitions.get(version, name);
         if (potential == null) {
@@ -174,6 +179,15 @@ class BuildGlobalContext extends NamespaceStorageSupport implements NamespaceBeh
             }
         }
         return potential;
+    }
+
+    StatementDefinitionContext<?, ?, ?> getStatementDefinition(final StatementSupport<?, ?, ?> support) {
+        StatementDefinitionContext<?, ?, ?> ret = definitions.get(support.getStatementName());
+        if (ret == null) {
+            ret = new StatementDefinitionContext<>(support);
+            definitions.put(ret.getStatementName(), ret);
+        }
+        return ret;
     }
 
     EffectiveModelContext build() throws SourceException, ReactorException {
