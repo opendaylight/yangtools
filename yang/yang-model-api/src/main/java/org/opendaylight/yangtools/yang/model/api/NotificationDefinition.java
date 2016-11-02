d86753cbcb34be08b0c44d5cfce0661d6730f357
Igor Foltin <ifoltin@cisco.com>
2016-11-02 12:26:10 +0100

    Bug 6871: [YANG 1.1] Allow "must" in "input", "output" and "notification statements"
    API changes in yang-model-api.
    
    Change-Id: I79a2ff10dc79bc18094209426676e60849905580
    Signed-off-by: Igor Foltin <ifoltin@cisco.com>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/NotificationDefinition.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/NotificationDefinition.java
@@ -7,10 +7,24 @@
  */
 package org.opendaylight.yangtools.yang.model.api;
 
+import javax.annotation.Nullable;
+
 /**
  * Interface describing YANG 'notification' statement. The notification
  * statement is used to define a NETCONF notification.
  */
 public interface NotificationDefinition extends SchemaNode, DataNodeContainer, AugmentationTarget {
 
+    /**
+     * All implementations should override this method.
+     * The default definition of this method is used only in YANG 1.0 (RFC6020) implementation of
+     * NotificationDefinition which does not support any constraints.
+     * YANG notification statement has been changed in YANG 1.1 (RFC7950) and now allows must constraints.
+     *
+     * @return the constraints associated with this NotificationDefinition
+     */
+     // FIXME: version 2.0.0: make this method non-default
+    @Nullable default ConstraintDefinition getConstraints() {
+        return null;
+    }
 }
