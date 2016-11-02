edaa67f7427ae8b86bdb0301e57ebd8897ad40e7
Igor Foltin <ifoltin@cisco.com>
2016-11-02 12:26:10 +0100

    Bug 6871: [YANG 1.1] Allow "must" in "input", "output" and "notification statements"
    API changes in yang-model-api.
    
    Change-Id: I79a2ff10dc79bc18094209426676e60849905580
    Signed-off-by: Igor Foltin <ifoltin@cisco.com>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/NotificationStatement.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/NotificationStatement.java
@@ -7,6 +7,8 @@
  */
 package org.opendaylight.yangtools.yang.model.api.stmt;
 
+import com.google.common.collect.ImmutableList;
+import java.util.Collection;
 import javax.annotation.Nonnull;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
@@ -14,4 +16,17 @@ import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 public interface NotificationStatement extends DeclaredStatement<QName>, DataDefinitionContainer.WithReusableDefinitions, DocumentationGroup.WithStatus {
 
     @Nonnull QName getName();
+
+    /**
+     * All implementations should override this method.
+     * The default definition of this method is used only in YANG 1.0 (RFC6020) implementation of
+     * NotificationStatement which does not support must statements.
+     * YANG notification statement has been changed in YANG 1.1 (RFC7950) and now allows must statements.
+     *
+     * @return collection of must statements
+     */
+     // FIXME: version 2.0.0: make this method non-default
+    @Nonnull default Collection<? extends MustStatement> getMusts() {
+        return ImmutableList.of();
+    }
 }
