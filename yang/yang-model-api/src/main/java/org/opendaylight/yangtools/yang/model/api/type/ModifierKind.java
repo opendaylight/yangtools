0b0edb71c162f343203eedcfae5c40ac528fb8b6
Igor Foltin <ifoltin@cisco.com>
2016-11-02 10:43:32 +0100

    Bug 6870: [YANG 1.1] Support for new substatement "modifier" in "pattern" statement
    API changes in yang-model-api.
    
    Change-Id: I3ef2dc64b68d17d85e2a6c3ea0da0aece34d2fe7
    Signed-off-by: Igor Foltin <ifoltin@cisco.com>

---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/type/ModifierKind.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.model.api.type;
+
+/**
+ * Enum describing the effect of a YANG modifier statement.
+ *
+ * As of YANG 1.1 (RFC7950) there is only one modifier value
+ * available and that is "invert-match".
+ * If there are more possible values added in the future,
+ * this enum can be extended with more enum constants.
+ */
+public enum ModifierKind {
+
+    INVERT_MATCH("invert-match");
+
+    private final String keyword;
+
+    ModifierKind(final String keyword) {
+        this.keyword = keyword;
+    }
+
+    /**
+     * @return String that corresponds to the yang keyword.
+     */
+    public String getKeyword() {
+        return keyword;
+    }
+}
