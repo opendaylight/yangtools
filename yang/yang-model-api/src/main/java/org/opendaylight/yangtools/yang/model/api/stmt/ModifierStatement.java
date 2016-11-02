d46566a0ca182b42d784c0708a76d08fc86d2477
Igor Foltin <ifoltin@cisco.com>
2016-11-02 10:43:32 +0100

    Bug 6870: [YANG 1.1] Support for new substatement "modifier" in "pattern" statement
    API changes in yang-model-api.
    
    Change-Id: I3ef2dc64b68d17d85e2a6c3ea0da0aece34d2fe7
    Signed-off-by: Igor Foltin <ifoltin@cisco.com>

---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/ModifierStatement.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.model.api.stmt;
+
+import com.google.common.annotations.Beta;
+import javax.annotation.Nonnull;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.type.ModifierKind;
+
+/**
+ * Represents YANG modifier statement.
+ *
+ * The "modifier" statement, which is an optional substatement
+ * to the "pattern" statement, takes as an argument the string "invert-match".
+ * If a pattern has the "invert-match" modifier present, the type is
+ * restricted to values that do not match the pattern.
+ */
+@Beta
+public interface ModifierStatement extends DeclaredStatement<ModifierKind> {
+
+    @Nonnull ModifierKind getValue();
+}
