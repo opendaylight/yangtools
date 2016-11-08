f96244ec92fac3ac47e935b5762e844bd46b3e90
Igor Foltin <ifoltin@cisco.com>
2016-11-08 10:12:15 +0100

    Bug 6883: [YANG 1.1] Add support for anydata statement
    API changes in yang-model-api.
    
    Change-Id: Ic5afed07a487b7311b83c9bd01a077592f1359ae
    Signed-off-by: Igor Foltin <ifoltin@cisco.com>

---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/AnyDataSchemaNode.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.model.api;
+
+import com.google.common.annotations.Beta;
+import javax.annotation.Nullable;
+
+/**
+ * The "anydata" statement defines an interior node in the schema tree.
+ * It takes one argument, which is an identifier, followed by a block of
+ * substatements that holds detailed anydata information.
+ *
+ * The "anydata" statement is used to represent an unknown set of nodes
+ * that can be modeled with YANG, except anyxml, but for which the data
+ * model is not known at module design time.  It is possible, though not
+ * required, for the data model for anydata content to become known
+ * through protocol signaling or other means that are outside the scope
+ * of this document.
+ */
+@Beta
+public interface AnyDataSchemaNode extends DataSchemaNode {
+
+    /**
+     * @return schema of contained XML data or null if it is not provided
+     */
+    @Nullable ContainerSchemaNode getSchemaOfAnyXmlData();
+}
