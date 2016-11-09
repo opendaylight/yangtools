720f04ccfcc1347485efed92a7f56f3d4b49c7ee
Peter Kajsa <pkajsa@cisco.com>
2016-11-09 15:00:43 +0100

    Bug 6867: Extend yang statement parser to support different yang versions
    This patch extends statement parser in order to support different versions
    of yang, yin or other statement based languages. Statement parser is able
    to distinguish version of each yang, yin or other source and it performs
    parsing of the source in according to the specific version.
    Mixed-version schemacontext (i.e. imports across versions) is supported too.
    
    Change-Id: I06bbcbfee091a0f54480d5f5f588a9ed18a84b55
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/spi/meta/StatementSupportBundle.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/spi/meta/StatementSupportBundle.java
@@ -12,44 +12,65 @@ import com.google.common.collect.ImmutableMap;
 import java.util.HashMap;
 import java.util.Map;
 import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.concepts.SemVer;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.model.api.meta.IdentifierNamespace;
 
-public final class StatementSupportBundle implements Immutable,NamespaceBehaviour.Registry {
+public final class StatementSupportBundle implements Immutable, NamespaceBehaviour.Registry {
 
-    private static final StatementSupportBundle EMPTY = new StatementSupportBundle(null, ImmutableMap.of(), ImmutableMap.of());
+    private static final StatementSupportBundle EMPTY = new StatementSupportBundle(null, null, ImmutableMap.of(),
+            ImmutableMap.of(), ImmutableMap.of());
 
     private final StatementSupportBundle parent;
-    private final ImmutableMap<QName, StatementSupport<?,?,?>> definitions;
+    private final ImmutableMap<QName, StatementSupport<?, ?, ?>> commonDefinitions;
+    private final ImmutableMap<SemVer, ImmutableMap<QName, StatementSupport<?, ?, ?>>> versionSpecificDefinitions;
     private final ImmutableMap<Class<?>, NamespaceBehaviour<?, ?, ?>> namespaceDefinitions;
+    private final SupportedVersionsBundle supportedVersionBundle;
 
     private StatementSupportBundle(final StatementSupportBundle parent,
-                                   final ImmutableMap<QName, StatementSupport<?, ?, ?>> statements,
-                                   final ImmutableMap<Class<?>, NamespaceBehaviour<?, ?, ?>> namespaces) {
+            final SupportedVersionsBundle supportedVersionBundle,
+            final ImmutableMap<QName, StatementSupport<?, ?, ?>> commonStatements,
+            final ImmutableMap<Class<?>, NamespaceBehaviour<?, ?, ?>> namespaces,
+            final ImmutableMap<SemVer, ImmutableMap<QName, StatementSupport<?, ?, ?>>> versionSpecificStatements) {
         this.parent = parent;
-        this.definitions = statements;
+        this.supportedVersionBundle = supportedVersionBundle;
+        this.commonDefinitions = commonStatements;
         this.namespaceDefinitions = namespaces;
+        this.versionSpecificDefinitions = versionSpecificStatements;
     }
 
-    public ImmutableMap<QName, StatementSupport<?, ?, ?>> getDefinitions() {
-        return definitions;
+    public ImmutableMap<QName, StatementSupport<?, ?, ?>> getCommonDefinitions() {
+        return commonDefinitions;
+    }
+
+    public ImmutableMap<QName, StatementSupport<?, ?, ?>> getDefinitionsSpecificForVersion(final SemVer version) {
+        return versionSpecificDefinitions.get(version);
+    }
+
+    public ImmutableMap<SemVer, ImmutableMap<QName, StatementSupport<?, ?, ?>>> getAllVersionSpecificDefinitions() {
+        return versionSpecificDefinitions;
     }
 
     public ImmutableMap<Class<?>, NamespaceBehaviour<?, ?, ?>> getNamespaceDefinitions() {
         return namespaceDefinitions;
     }
 
-    public static Builder builder() {
-        return new Builder(EMPTY);
+    public static Builder builder(final SupportedVersionsBundle supportedVersionBundle) {
+        return new Builder(supportedVersionBundle, EMPTY);
     }
 
     public static Builder derivedFrom(final StatementSupportBundle parent) {
-        return new Builder(parent);
+        Preconditions.checkNotNull(parent);
+        return new Builder(parent.getSupportedVersionBundle(), parent);
+    }
+
+    public SupportedVersionsBundle getSupportedVersionBundle() {
+        return supportedVersionBundle;
     }
 
     @Override
-    public <K, V, N extends IdentifierNamespace<K, V>> NamespaceBehaviour<K, V, N> getNamespaceBehaviour(final Class<N> namespace)
-            throws NamespaceNotAvailableException {
+    public <K, V, N extends IdentifierNamespace<K, V>> NamespaceBehaviour<K, V, N> getNamespaceBehaviour(
+            final Class<N> namespace) throws NamespaceNotAvailableException {
         final NamespaceBehaviour<?, ?, ?> potential = namespaceDefinitions.get(namespace);
         if (potential != null) {
             Preconditions.checkState(namespace.equals(potential.getIdentifier()));
@@ -76,33 +97,83 @@ public final class StatementSupportBundle implements Immutable,NamespaceBehaviou
         return false;
     }
 
-    public StatementSupport<?, ?,?> getStatementDefinition(final QName stmtName) {
-        final StatementSupport<?,?, ?> potential = definitions.get(stmtName);
+    public StatementSupport<?, ?, ?> getStatementDefinition(final SemVer version, final QName stmtName) {
+        StatementSupport<?, ?, ?> result = getVersionSpecificStatementDefinition(version, stmtName);
+        if (result == null) {
+            result = getCommonStatementDefinition(stmtName);
+        }
+
+        return result;
+    }
+
+    public StatementSupport<?, ?, ?> getCommonStatementDefinition(final QName stmtName) {
+        final StatementSupport<?, ?, ?> potential = commonDefinitions.get(stmtName);
         if (potential != null) {
             return potential;
         }
         if (parent != null) {
-            return parent.getStatementDefinition(stmtName);
+            return parent.getCommonStatementDefinition(stmtName);
+        }
+        return null;
+    }
+
+    public StatementSupport<?, ?, ?> getVersionSpecificStatementDefinition(final SemVer version, final QName stmtName) {
+        final ImmutableMap<QName, StatementSupport<?, ?, ?>> stmtDefsForVersion = versionSpecificDefinitions
+                .get(version);
+        if (stmtDefsForVersion != null) {
+            final StatementSupport<?, ?, ?> potential = stmtDefsForVersion.get(stmtName);
+            if (potential != null) {
+                return potential;
+            }
+        }
+
+        if (parent != null) {
+            return parent.getVersionSpecificStatementDefinition(version, stmtName);
         }
         return null;
     }
 
     public static class Builder implements org.opendaylight.yangtools.concepts.Builder<StatementSupportBundle> {
-        private final Map<QName, StatementSupport<?, ?, ?>> statements = new HashMap<>();
+        private final Map<QName, StatementSupport<?, ?, ?>> commonStatements = new HashMap<>();
+        private final Map<SemVer, Map<QName, StatementSupport<?, ?, ?>>> versionSpecificStatements = new HashMap<>();
         private final Map<Class<?>, NamespaceBehaviour<?, ?, ?>> namespaces = new HashMap<>();
 
         private StatementSupportBundle parent;
+        private final SupportedVersionsBundle supportedVersionBundle;
 
-        Builder(final StatementSupportBundle parent) {
-            this.parent = parent;
+        Builder(final SupportedVersionsBundle supportedVersionBundle, final StatementSupportBundle parent) {
+            this.parent = Preconditions.checkNotNull(parent);
+            this.supportedVersionBundle = Preconditions.checkNotNull(supportedVersionBundle);
+            for (final SemVer version : supportedVersionBundle.getAll()) {
+                versionSpecificStatements.put(version, new HashMap<>());
+            }
         }
 
         public Builder addSupport(final StatementSupport<?, ?, ?> definition) {
             final QName identifier = definition.getStatementName();
-            Preconditions.checkState(!statements.containsKey(identifier), "Statement %s already defined.", identifier);
-            Preconditions.checkState(parent.getStatementDefinition(identifier) == null,
+            Preconditions.checkState(!commonStatements.containsKey(identifier),
+                    "Statement %s already defined in common statement bundle.", identifier);
+            Preconditions.checkState(parent.getCommonStatementDefinition(identifier) == null,
                     "Statement %s already defined.", identifier);
-            statements.put(identifier, definition);
+            commonStatements.put(identifier, definition);
+            return this;
+        }
+
+        public Builder addVersionSpecificSupport(final SemVer version, final StatementSupport<?, ?, ?> definition) {
+            Preconditions.checkNotNull(version);
+            Preconditions.checkNotNull(definition);
+            Preconditions.checkArgument(supportedVersionBundle.contains(version));
+
+            final QName identifier = definition.getStatementName();
+            Preconditions.checkState(!commonStatements.containsKey(identifier),
+                    "Statement %s already defined in common statement bundle.", identifier);
+            Preconditions.checkState(!versionSpecificStatements.get(version).containsKey(identifier),
+                    "Statement %s already defined for version %s.", identifier, version);
+            Preconditions.checkState(parent.getCommonStatementDefinition(identifier) == null,
+                    "Statement %s already defined in parent's common statement bundle.", identifier);
+            Preconditions.checkState(parent.getVersionSpecificStatementDefinition(version, identifier) == null,
+                    "Statement %s already defined for version %s in parent's statement bundle.", identifier, version);
+            versionSpecificStatements.get(version).put(identifier, definition);
             return this;
         }
 
@@ -115,6 +186,10 @@ public final class StatementSupportBundle implements Immutable,NamespaceBehaviou
             return this;
         }
 
+        public SupportedVersionsBundle getSupportedVersionBundle() {
+            return supportedVersionBundle;
+        }
+
         public Builder setParent(final StatementSupportBundle parent) {
             this.parent = parent;
             return this;
@@ -122,8 +197,14 @@ public final class StatementSupportBundle implements Immutable,NamespaceBehaviou
 
         @Override
         public StatementSupportBundle build() {
-            return new StatementSupportBundle(parent, ImmutableMap.copyOf(statements), ImmutableMap.copyOf(namespaces));
+            Preconditions.checkState(parent != null, "Parent must not be null");
+            final Map<SemVer, ImmutableMap<QName, StatementSupport<?, ?, ?>>> immutableVersionSpecificStmts = new HashMap<>();
+            for (final SemVer version : supportedVersionBundle.getAll()) {
+                immutableVersionSpecificStmts.put(version, ImmutableMap.copyOf(versionSpecificStatements.get(version)));
+            }
+
+            return new StatementSupportBundle(parent, supportedVersionBundle, ImmutableMap.copyOf(commonStatements),
+                    ImmutableMap.copyOf(namespaces), ImmutableMap.copyOf(immutableVersionSpecificStmts));
         }
     }
-
 }
