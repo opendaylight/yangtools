7379e79440045c641a8adb5a4fe58ad627c1b5b8
Vratko Polak <vrpolak@cisco.com>
2016-11-10 13:02:06 +0100

    Bug 6150: Add unit tests
    Change-Id: I8363a2ebff1fb46a55bd9726ff6949946ecfbb12
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug6150Test.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+import org.opendaylight.yangtools.yang.parser.stmt.reactor.CrossSourceStatementReactor.BuildAction;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.EffectiveSchemaContext;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.YangInferencePipeline;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.YangStatementSourceImpl;
+
+public class AugmentArgumentParsingTest {
+
+    private static final YangStatementSourceImpl TARGET = new YangStatementSourceImpl(
+            "/semantic-statement-parser/bug6150/target.yang", false);
+    private static YangStatementSourceImpl AUGMENT_FIRST = new YangStatementSourceImpl(
+            "/semantic-statement-parser/bug6150/aug-first.yang", false);
+    private static YangStatementSourceImpl AUGMENT_SECOND = new YangStatementSourceImpl(
+            "/semantic-statement-parser/bug6150/aug-second.yang", false);
+
+    private static void addSources(final BuildAction reactor, final YangStatementSourceImpl... sources) {
+        for (YangStatementSourceImpl source : sources) {
+            reactor.addSource(source);
+        }
+    }
+
+    @Test
+    public void effectiveAugmentFirstTest() throws SourceException, ReactorException {
+
+        BuildAction reactor = YangInferencePipeline.RFC6020_REACTOR.newBuild();
+        addSources(reactor, TARGET, AUGMENT_FIRST);
+
+        final EffectiveSchemaContext result = reactor.buildEffective();
+        assertNotNull(result);
+    }
+
+    @Test
+    public void effectiveAugmentSecondTest() throws SourceException, ReactorException {
+
+        BuildAction reactor = YangInferencePipeline.RFC6020_REACTOR.newBuild();
+        addSources(reactor, TARGET, AUGMENT_SECOND);
+
+        final EffectiveSchemaContext result = reactor.buildEffective();
+        assertNotNull(result);
+    }
+
+    @Test
+    public void effectiveAugmentBothTest() throws SourceException, ReactorException {
+
+        BuildAction reactor = YangInferencePipeline.RFC6020_REACTOR.newBuild();
+        addSources(reactor, TARGET, AUGMENT_FIRST, AUGMENT_SECOND);
+
+        final EffectiveSchemaContext result = reactor.buildEffective();
+        assertNotNull(result);
+    }
+}
