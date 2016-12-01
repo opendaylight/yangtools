2bb134934d944506937d69a8c9386369f49d3852
Peter Kajsa <pkajsa@cisco.com>
2016-12-01 14:57:24 +0100

    Bug 6867: Extend yang statement parser to support different yang versions
    This patch extends statement parser in order to support different versions
    of yang, yin or other statement based languages. Statement parser is able
    to distinguish version of each yang, yin or other source and it performs
    parsing of the source according to the specific version. Mixed-version
    schemacontext (i.e. imports across versions) is supported too.
    
    More in detail, StatementSupportBundle must be initialized by
    SupportedVersionBundle, which defines set of supported versions.
    In consequence, StatementSupportBundle allows to add version specific
    StatementSupport into bundle. Furthermore each RootStatementContext
    allows to set its version based on which corresponding StatementSupports
    are invoked. For yang models, RootStatementContext version is set based
    on a value of yang version statement.
    
    Change-Id: I06bbcbfee091a0f54480d5f5f588a9ed18a84b55
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc7950/AnydataStatementImpl.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc7950/AnydataStatementImpl.java
@@ -1,102 +1,87 @@
-/*
+/**
  * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.yangtools.yang.parser.stmt.rfc6020;
+package org.opendaylight.yangtools.yang.parser.stmt.rfc7950;
 
 import java.util.Collection;
-import javax.annotation.Nonnull;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.model.api.Rfc6020Mapping;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.AnydataStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.ConfigStatement;
-import org.opendaylight.yangtools.yang.model.api.stmt.ContainerStatement;
-import org.opendaylight.yangtools.yang.model.api.stmt.DataDefinitionStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.DescriptionStatement;
-import org.opendaylight.yangtools.yang.model.api.stmt.GroupingStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.IfFeatureStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.MandatoryStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.MustStatement;
-import org.opendaylight.yangtools.yang.model.api.stmt.PresenceStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.ReferenceStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.StatusStatement;
-import org.opendaylight.yangtools.yang.model.api.stmt.TypedefStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.WhenStatement;
 import org.opendaylight.yangtools.yang.parser.spi.SubstatementValidator;
 import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractDeclaredStatement;
 import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractStatementSupport;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext.Mutable;
-import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.ContainerEffectiveStatementImpl;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.ChildSchemaNodes;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.Utils;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc7950.effective.AnyDataEffectiveStatementImpl;
 
-public class ContainerStatementImpl extends AbstractDeclaredStatement<QName> implements ContainerStatement {
+public class AnydataStatementImpl extends AbstractDeclaredStatement<QName> implements AnydataStatement {
     private static final SubstatementValidator SUBSTATEMENT_VALIDATOR = SubstatementValidator.builder(Rfc6020Mapping
-            .CONTAINER)
-            .addAny(Rfc6020Mapping.ANYXML)
-            .addAny(Rfc6020Mapping.CHOICE)
+            .ANYDATA)
             .addOptional(Rfc6020Mapping.CONFIG)
-            .addAny(Rfc6020Mapping.CONTAINER)
             .addOptional(Rfc6020Mapping.DESCRIPTION)
-            .addAny(Rfc6020Mapping.GROUPING)
             .addAny(Rfc6020Mapping.IF_FEATURE)
-            .addAny(Rfc6020Mapping.LEAF)
-            .addAny(Rfc6020Mapping.LEAF_LIST)
-            .addAny(Rfc6020Mapping.LIST)
+            .addOptional(Rfc6020Mapping.MANDATORY)
             .addAny(Rfc6020Mapping.MUST)
-            .addOptional(Rfc6020Mapping.PRESENCE)
             .addOptional(Rfc6020Mapping.REFERENCE)
             .addOptional(Rfc6020Mapping.STATUS)
-            .addAny(Rfc6020Mapping.TYPEDEF)
-            .addAny(Rfc6020Mapping.USES)
             .addOptional(Rfc6020Mapping.WHEN)
             .build();
 
-    protected ContainerStatementImpl(final StmtContext<QName, ContainerStatement,?> context) {
+    protected AnydataStatementImpl(final StmtContext<QName, AnydataStatement, ?> context) {
         super(context);
     }
 
-    public static class Definition extends AbstractStatementSupport<QName,ContainerStatement,EffectiveStatement<QName,ContainerStatement>> {
+    public static class Definition extends
+            AbstractStatementSupport<QName, AnydataStatement, EffectiveStatement<QName, AnydataStatement>> {
 
         public Definition() {
-            super(Rfc6020Mapping.CONTAINER);
+            super(Rfc6020Mapping.ANYDATA);
         }
 
         @Override
-        public QName parseArgumentValue(final StmtContext<?,?,?> ctx, final String value) {
-            return Utils.qNameFromArgument(ctx,value);
+        public QName parseArgumentValue(final StmtContext<?, ?, ?> ctx, final String value) {
+            return Utils.qNameFromArgument(ctx, value);
         }
 
         @Override
-        public void onStatementAdded(
-                final Mutable<QName, ContainerStatement, EffectiveStatement<QName, ContainerStatement>> stmt) {
+        public void onStatementAdded(final Mutable<QName, AnydataStatement, EffectiveStatement<QName, AnydataStatement>> stmt) {
             stmt.getParentContext().addToNs(ChildSchemaNodes.class, stmt.getStatementArgument(), stmt);
         }
 
         @Override
-        public ContainerStatement createDeclared(final StmtContext<QName, ContainerStatement,?> ctx) {
-            return new ContainerStatementImpl(ctx);
+        public AnydataStatement createDeclared(final StmtContext<QName, AnydataStatement, ?> ctx) {
+            return new AnydataStatementImpl(ctx);
         }
 
         @Override
-        public EffectiveStatement<QName,ContainerStatement> createEffective(final StmtContext<QName,ContainerStatement,EffectiveStatement<QName,ContainerStatement>> ctx) {
-           return new ContainerEffectiveStatementImpl(ctx);
+        public EffectiveStatement<QName, AnydataStatement> createEffective(
+                final StmtContext<QName, AnydataStatement, EffectiveStatement<QName, AnydataStatement>> ctx) {
+            return new AnyDataEffectiveStatementImpl(ctx);
         }
 
         @Override
-        public void onFullDefinitionDeclared(final Mutable<QName, ContainerStatement,
-                EffectiveStatement<QName, ContainerStatement>> stmt) {
+        public void onFullDefinitionDeclared(final Mutable<QName, AnydataStatement,
+                EffectiveStatement<QName, AnydataStatement>> stmt) {
             super.onFullDefinitionDeclared(stmt);
-            getSubstatementValidator().validate(stmt);
-        }
-
-        protected SubstatementValidator getSubstatementValidator() {
-            return SUBSTATEMENT_VALIDATOR;
+            SUBSTATEMENT_VALIDATOR.validate(stmt);
         }
     }
 
-    @Nonnull
     @Override
     public QName getName() {
         return argument();
@@ -107,7 +92,6 @@ public class ContainerStatementImpl extends AbstractDeclaredStatement<QName> imp
         return firstDeclared(WhenStatement.class);
     }
 
-    @Nonnull
     @Override
     public Collection<? extends IfFeatureStatement> getIfFeatures() {
         return allDeclared(IfFeatureStatement.class);
@@ -116,11 +100,6 @@ public class ContainerStatementImpl extends AbstractDeclaredStatement<QName> imp
     @Override
     public Collection<? extends MustStatement> getMusts() {
         return allDeclared(MustStatement.class);
-    }
-
-    @Override
-    public PresenceStatement getPresence() {
-        return firstDeclared(PresenceStatement.class);
     }
 
     @Override
@@ -143,22 +122,9 @@ public class ContainerStatementImpl extends AbstractDeclaredStatement<QName> imp
         return firstDeclared(ReferenceStatement.class);
     }
 
-    @Nonnull
     @Override
-    public Collection<? extends TypedefStatement> getTypedefs() {
-        return allDeclared(TypedefStatement.class);
-    }
-
-    @Nonnull
-    @Override
-    public Collection<? extends GroupingStatement> getGroupings() {
-        return allDeclared(GroupingStatement.class);
-    }
-
-    @Nonnull
-    @Override
-    public Collection<? extends DataDefinitionStatement> getDataDefinitions() {
-        return allDeclared(DataDefinitionStatement.class);
+    public MandatoryStatement getMandatory() {
+        return firstDeclared(MandatoryStatement.class);
     }
 
 }
