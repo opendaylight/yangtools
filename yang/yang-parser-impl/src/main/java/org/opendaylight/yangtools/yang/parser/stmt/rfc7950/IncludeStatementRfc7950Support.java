ec8d57ef3a18214d0f142871e026374b5a93e5b6
Rashmi Pujar <rpujar@inocybe.com>
2016-12-07 16:52:47 -0500

    Bug 6874 - [Yang 1.1] Allow "description" and "reference" in "import" and "include"
    Implementation in yang-parser-impl bundle
    
    Change-Id: Iaa1573144c0939b6954d855d43c0a58379216d75
    Signed-off-by: Rashmi Pujar <rpujar@inocybe.com>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc7950/IncludeStatementRfc7950Support.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2016 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.stmt.rfc7950;
+
+import org.opendaylight.yangtools.yang.model.api.YangStmtMapping;
+import org.opendaylight.yangtools.yang.parser.spi.SubstatementValidator;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.IncludeStatementImpl;
+
+public class IncludeStatementRfc7950Support extends IncludeStatementImpl.Definition {
+
+    private static final SubstatementValidator SUBSTATEMENT_VALIDATOR = SubstatementValidator
+            .builder(YangStmtMapping.INCLUDE)
+            .addOptional(YangStmtMapping.REVISION_DATE)
+            .addOptional(YangStmtMapping.DESCRIPTION)
+            .addOptional(YangStmtMapping.REFERENCE).build();
+
+    @Override
+    protected SubstatementValidator getSubstatementValidator() {
+        return SUBSTATEMENT_VALIDATOR;
+    }
+}
