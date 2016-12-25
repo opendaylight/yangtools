c2d4152af49ac4298c4651fde4771012983eae50
Robert Varga <rovarga@cisco.com>
2016-12-25 13:06:07 +0100

    Return specialized TypeDefinitions
    BuiltinEffectiveStatements should declare specialized return
    rather than generic TypeDefinition.
    
    Change-Id: I6b5f74c7c13002eb27c37054ed88e2d87ba70a05
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/type/BuiltinEffectiveStatements.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/type/BuiltinEffectiveStatements.java
@@ -8,9 +8,15 @@
 package org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.type;
 
 import javax.annotation.Nonnull;
-import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
 import org.opendaylight.yangtools.yang.model.api.stmt.TypeEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.TypeStatement;
+import org.opendaylight.yangtools.yang.model.api.type.BinaryTypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.BooleanTypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.EmptyTypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.InstanceIdentifierTypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.IntegerTypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.StringTypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.UnsignedIntegerTypeDefinition;
 import org.opendaylight.yangtools.yang.model.util.type.BaseTypes;
 
 public final class BuiltinEffectiveStatements {
@@ -21,91 +27,91 @@ public final class BuiltinEffectiveStatements {
     public static final TypeEffectiveStatement<TypeStatement> BINARY = new AbstractBuiltinEffectiveStatement() {
         @Nonnull
         @Override
-        public TypeDefinition<?> getTypeDefinition() {
+        public BinaryTypeDefinition getTypeDefinition() {
             return BaseTypes.binaryType();
         }
     };
     public static final TypeEffectiveStatement<TypeStatement> BOOLEAN = new AbstractBuiltinEffectiveStatement() {
         @Nonnull
         @Override
-        public TypeDefinition<?> getTypeDefinition() {
+        public BooleanTypeDefinition getTypeDefinition() {
             return BaseTypes.booleanType();
         }
     };
     public static final TypeEffectiveStatement<TypeStatement> EMPTY = new AbstractBuiltinEffectiveStatement() {
         @Nonnull
         @Override
-        public TypeDefinition<?> getTypeDefinition() {
+        public EmptyTypeDefinition getTypeDefinition() {
             return BaseTypes.emptyType();
         }
     };
     public static final TypeEffectiveStatement<TypeStatement> INSTANCE_IDENTIFIER = new AbstractBuiltinEffectiveStatement() {
         @Nonnull
         @Override
-        public TypeDefinition<?> getTypeDefinition() {
+        public InstanceIdentifierTypeDefinition getTypeDefinition() {
             return BaseTypes.instanceIdentifierType();
         }
     };
     public static final TypeEffectiveStatement<TypeStatement> INT8 = new AbstractBuiltinEffectiveStatement() {
         @Nonnull
         @Override
-        public TypeDefinition<?> getTypeDefinition() {
+        public IntegerTypeDefinition getTypeDefinition() {
             return BaseTypes.int8Type();
         }
     };
     public static final TypeEffectiveStatement<TypeStatement> INT16 = new AbstractBuiltinEffectiveStatement() {
         @Nonnull
         @Override
-        public TypeDefinition<?> getTypeDefinition() {
+        public IntegerTypeDefinition getTypeDefinition() {
             return BaseTypes.int16Type();
         }
     };
     public static final TypeEffectiveStatement<TypeStatement> INT32 = new AbstractBuiltinEffectiveStatement() {
         @Nonnull
         @Override
-        public TypeDefinition<?> getTypeDefinition() {
+        public IntegerTypeDefinition getTypeDefinition() {
             return BaseTypes.int32Type();
         }
     };
     public static final TypeEffectiveStatement<TypeStatement> INT64 = new AbstractBuiltinEffectiveStatement() {
         @Nonnull
         @Override
-        public TypeDefinition<?> getTypeDefinition() {
+        public IntegerTypeDefinition getTypeDefinition() {
             return BaseTypes.int64Type();
         }
     };
     public static final TypeEffectiveStatement<TypeStatement> STRING = new AbstractBuiltinEffectiveStatement() {
         @Nonnull
         @Override
-        public TypeDefinition<?> getTypeDefinition() {
+        public StringTypeDefinition getTypeDefinition() {
             return BaseTypes.stringType();
         }
     };
     public static final TypeEffectiveStatement<TypeStatement> UINT8 = new AbstractBuiltinEffectiveStatement() {
         @Nonnull
         @Override
-        public TypeDefinition<?> getTypeDefinition() {
+        public UnsignedIntegerTypeDefinition getTypeDefinition() {
             return BaseTypes.uint8Type();
         }
     };
     public static final TypeEffectiveStatement<TypeStatement> UINT16 = new AbstractBuiltinEffectiveStatement() {
         @Nonnull
         @Override
-        public TypeDefinition<?> getTypeDefinition() {
+        public UnsignedIntegerTypeDefinition getTypeDefinition() {
             return BaseTypes.uint16Type();
         }
     };
     public static final TypeEffectiveStatement<TypeStatement> UINT32 = new AbstractBuiltinEffectiveStatement() {
         @Nonnull
         @Override
-        public TypeDefinition<?> getTypeDefinition() {
+        public UnsignedIntegerTypeDefinition getTypeDefinition() {
             return BaseTypes.uint32Type();
         }
     };
     public static final TypeEffectiveStatement<TypeStatement> UINT64 = new AbstractBuiltinEffectiveStatement() {
         @Nonnull
         @Override
-        public TypeDefinition<?> getTypeDefinition() {
+        public UnsignedIntegerTypeDefinition getTypeDefinition() {
             return BaseTypes.uint64Type();
         }
     };
