3721ad008b140c437fb5d92fd42f6e48507c6dab
Robert Varga <rovarga@cisco.com>
2016-12-29 13:25:51 +0100

    Update pom.xml
    Change-Id: I0554be2c99a6aa42ca1e25bf290693117a5b8920
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ third-party/triemap/src/test/java/org/opendaylight/yangtools/triemap/TestHelper.java
@@ -0,0 +1,27 @@
+package org.opendaylight.yangtools.triemap;
+
+import org.junit.Assert;
+
+public class TestHelper {
+
+    public static void assertEquals (long expected, long found) {
+        Assert.assertEquals (expected, found);
+    }
+
+    public static void assertEquals (int expected, int found) {
+        Assert.assertEquals (expected, found);
+    }
+
+    public static void assertEquals (Object expected, Object found) {
+        Assert.assertEquals (expected, found);
+    }
+
+    public static void assertTrue (boolean found) {
+        Assert.assertTrue (found);
+    }
+
+    public static void assertFalse (boolean found) {
+        Assert.assertFalse (found);
+    }
+
+}
