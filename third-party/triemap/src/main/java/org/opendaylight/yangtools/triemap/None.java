c9982aa60775a3773740a53582e15dfa24228a97
Robert Varga <rovarga@cisco.com>
2016-12-29 13:25:51 +0100

    BUG-7464: Update pom.xml
    Update pom.xml and move source to yangtools-specific
    package.
    
    Change-Id: I0554be2c99a6aa42ca1e25bf290693117a5b8920
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ third-party/triemap/src/main/java/org/opendaylight/yangtools/triemap/None.java
@@ -0,0 +1,12 @@
+package org.opendaylight.yangtools.triemap;
+
+/**
+ * Mimic None in Scala
+ *  
+ * @author Roman Levenstein <romixlev@gmail.com>
+ *
+ * @param <V>
+ */
+class None<V> extends Option<V>{
+
+}
