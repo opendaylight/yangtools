db5e581098d3d4507cf55cef14726fdfdbbcca28
Robert Varga <rovarga@cisco.com>
2016-12-30 10:54:41 +0100

    BYG-7464: Update junit import
    Assert has been moved to org.junit, import from new location.
    
    Change-Id: Id0be53f8c13319897492ea99a90405634fa42ccd
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- third-party/triemap/src/test/java/org/opendaylight/yangtools/triemap/TestSerialization.java
+++ third-party/triemap/src/test/java/org/opendaylight/yangtools/triemap/TestSerialization.java
@@ -6,8 +6,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestSerialization {
