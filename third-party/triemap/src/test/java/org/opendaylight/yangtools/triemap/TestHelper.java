5a20ad4f1a3181367250f56b9d1cffbc0f3929ef
Robert Varga <rovarga@cisco.com>
2016-12-30 11:18:13 +0100

    BUG-7464: Reformat source code
    This is an automatic reformat, performing the following:
    - remove trailing whitespace
    - organize imports
    - make arguments final
    - add explicit braces
    - use diamond for instantiation
    
    Change-Id: I08f578bf40b72c0a592f3adf5add4a275c376f3d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- third-party/triemap/src/test/java/org/opendaylight/yangtools/triemap/TestHelper.java
+++ third-party/triemap/src/test/java/org/opendaylight/yangtools/triemap/TestHelper.java
@@ -4,23 +4,23 @@ import org.junit.Assert;
 
 public class TestHelper {
 
-    public static void assertEquals (long expected, long found) {
+    public static void assertEquals (final long expected, final long found) {
         Assert.assertEquals (expected, found);
     }
 
-    public static void assertEquals (int expected, int found) {
+    public static void assertEquals (final int expected, final int found) {
         Assert.assertEquals (expected, found);
     }
 
-    public static void assertEquals (Object expected, Object found) {
+    public static void assertEquals (final Object expected, final Object found) {
         Assert.assertEquals (expected, found);
     }
 
-    public static void assertTrue (boolean found) {
+    public static void assertTrue (final boolean found) {
         Assert.assertTrue (found);
     }
 
-    public static void assertFalse (boolean found) {
+    public static void assertFalse (final boolean found) {
         Assert.assertFalse (found);
     }
 
