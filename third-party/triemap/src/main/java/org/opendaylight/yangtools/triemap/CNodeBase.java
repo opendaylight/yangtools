f0db330e20d43ea225cbd48007b1f1a3fbe740e5
Robert Varga <rovarga@cisco.com>
2016-12-30 11:30:23 +0100

    BUG-7464: Apply copyright headers
    Source code lacks explicit copyright headers, make sure we mark
    all sources as licensed by APL2.
    
    Change-Id: I218a55e3ea7e32a3e9d38ffb869b78523e97d077
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- third-party/triemap/src/main/java/org/opendaylight/yangtools/triemap/CNodeBase.java
+++ third-party/triemap/src/main/java/org/opendaylight/yangtools/triemap/CNodeBase.java
@@ -1,18 +1,21 @@
-/*                     __                                               *\
-**     ________ ___   / /  ___     Scala API                            **
-**    / __/ __// _ | / /  / _ |    (c) 2003-2012, LAMP/EPFL             **
-**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
-** /____/\___/_/ |_/____/_/ | |                                         **
-**                          |/                                          **
-\*                                                                      */
-
+/*
+ * (C) Copyright 2016 Pantheon Technologies, s.r.o. and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.opendaylight.yangtools.triemap;
 
-
-
 import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
-
-
 
 abstract class CNodeBase<K, V> extends MainNode<K, V> {
 
@@ -21,15 +24,15 @@ abstract class CNodeBase<K, V> extends MainNode<K, V> {
     public volatile int csize = -1;
 
     public boolean CAS_SIZE(final int oldval, final int nval) {
-    return updater.compareAndSet(this, oldval, nval);
+        return updater.compareAndSet(this, oldval, nval);
     }
 
     public void WRITE_SIZE(final int nval) {
-    updater.set(this, nval);
+        updater.set(this, nval);
     }
 
     public int READ_SIZE() {
-    return updater.get(this);
+        return updater.get(this);
     }
 
 }
