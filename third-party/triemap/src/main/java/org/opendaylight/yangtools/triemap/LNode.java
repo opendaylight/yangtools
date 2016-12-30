4e20f62e1eb367c2c2dc5a213c2a7f3195af74a7
Robert Varga <rovarga@cisco.com>
2016-12-30 11:33:03 +0100

    BUG-7464: Remove inner classes
    These classes form the internals of TrieMap, but there is no need
    to keep them as inner classes. Move them to their own files, which
    allows more precise control over access into their internals.
    
    Change-Id: I3b79a285e186af09a7f0c0e0ece43dc455c40de3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ third-party/triemap/src/main/java/org/opendaylight/yangtools/triemap/LNode.java
@@ -0,0 +1,64 @@
+/*
+ * (C) Copyright 2016 Pantheon Technologies, s.r.o. and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.opendaylight.yangtools.triemap;
+
+import java.util.Map.Entry;
+
+final class LNode<K, V> extends MainNode<K, V> {
+    final ListMap<K, V> listmap;
+
+    LNode(final ListMap<K, V> listmap) {
+        this.listmap = listmap;
+    }
+
+    LNode(final K k, final V v) {
+        this(ListMap.map(k, v));
+    }
+
+    LNode(final K k1, final V v1, final K k2, final V v2) {
+        this(ListMap.map(k1, v1, k2, v2));
+    }
+
+    LNode<K, V> inserted(final K k, final V v) {
+        return new LNode<> (listmap.add (k, v));
+    }
+
+    MainNode<K, V> removed (final K k, final TrieMap<K, V> ct) {
+        ListMap<K, V> updmap = listmap.remove(k);
+        if (updmap.size () > 1) {
+            return new LNode<> (updmap);
+        }
+
+        final Entry<K, V> kv = updmap.iterator().next();
+        // create it tombed so that it gets compressed on subsequent accesses
+        return new TNode<>(kv.getKey(), kv.getValue(), ct.computeHash(kv.getKey()));
+    }
+
+    Option<V> get(final K k) {
+        return listmap.get(k);
+    }
+
+    @Override
+    public int cachedSize(final Object ct) {
+        return listmap.size();
+    }
+
+    @Override
+    String string(final int lev) {
+        // (" " * lev) + "LNode(%s)".format(listmap.mkString(", "))
+        return "LNode";
+    }
+}
