6fceb3f7286e575781d00af9b92c558a3a0c8300
Robert Varga <rovarga@cisco.com>
2016-12-31 17:52:12 +0100

    BUG-7464: Fix StackOverflowError on hash collisions
    If LNodes get saturated due to poor key hash function, the ListMap
    chain can grow large. Since ListMap.get() was implemented using recursion
    this can easily trigger StackOverflowError. The same problem impacts
    ListMap.contains(), which is triggered in the add() path.
    
    Turn recursion into a loop, which provides defence against the error,
    while not alleviating the performance impact.
    
    Change-Id: Id04d30b9cb4d75dc4535ff20bd11a16ed96ab0ff
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ third-party/triemap/src/test/java/org/opendaylight/yangtools/triemap/ListMapTest.java
@@ -0,0 +1,40 @@
+/*
+ * (C) Copyright 2016 Pantheon Technologies, s.r.o. and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.opendaylight.yangtools.triemap;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class ListMapTest {
+
+    /**
+     * Test if Listmap.get() does not cause stack overflow.
+     */
+    @Test
+    public void testGetOverflow() {
+        ListMap<Integer, Boolean> map = ListMap.map(1, Boolean.TRUE, 2, Boolean.TRUE);
+
+        // 30K seems to be enough to trigger the problem locally
+        for (int i = 3; i < 30000; ++i) {
+            map = map.add(i, Boolean.TRUE);
+        }
+
+        final Option<Boolean> ret = map.get(0);
+        assertFalse(ret.nonEmpty());
+    }
+
+}
