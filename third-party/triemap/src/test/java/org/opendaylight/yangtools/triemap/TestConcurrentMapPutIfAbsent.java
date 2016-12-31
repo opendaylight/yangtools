62f6b314ee1c5ae6fd49d9133eed2105886e560c
Robert Varga <rovarga@cisco.com>
2016-12-31 17:30:53 +0100

    BUG-7464: Fix original porting damage in conditional operations
    Scala version of INode returns a correct empty Option
    in case an LNode does not contain the expected key, whereas
    this implementation happily falls through to a RuntimeException,
    this this porting damage, adding an appropriate comment.
    
    Removal of values through Map.remove(Object, Object) was
    inconsistent in that the SNode case uses v.equals() whereas
    the LNode case uses identity check. This is inconsistent
    and runs contrary to the interface specitication, which calls
    for using Objects.equals() -- which boils down to v.equals()
    just as the SNode case does.
    
    Change-Id: I33dd986e59211aa4c0be3648ae35e2818ebce8ad
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- third-party/triemap/src/test/java/org/opendaylight/yangtools/triemap/TestConcurrentMapPutIfAbsent.java
+++ third-party/triemap/src/test/java/org/opendaylight/yangtools/triemap/TestConcurrentMapPutIfAbsent.java
@@ -15,6 +15,10 @@
  */
 package org.opendaylight.yangtools.triemap;
 
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+
+import java.util.Map;
 import java.util.concurrent.ConcurrentMap;
 import org.junit.Test;
 
@@ -29,5 +33,27 @@ public class TestConcurrentMapPutIfAbsent {
             TestHelper.assertTrue (null == map.putIfAbsent (i, i));
             TestHelper.assertTrue (Integer.valueOf (i).equals (map.putIfAbsent (i, i)));
         }
+    }
+
+    @Test
+    public void testConflictingHash() {
+        final ZeroHashInt k1 = new ZeroHashInt(1);
+        final ZeroHashInt k2 = new ZeroHashInt(2);
+        final ZeroHashInt k3 = new ZeroHashInt(3);
+        final ZeroHashInt k3dup = new ZeroHashInt(3);
+        final ZeroHashInt v1 = new ZeroHashInt(4);
+        final ZeroHashInt v2 = new ZeroHashInt(5);
+        final ZeroHashInt v3 = new ZeroHashInt(6);
+
+        final Map<ZeroHashInt, ZeroHashInt> map = new TrieMap<>();
+        // Pre-populate an LNode
+        assertNull(map.putIfAbsent(k1, v1));
+        assertNull(map.putIfAbsent(k2, v2));
+        assertNull(map.putIfAbsent(k3, v3));
+
+        // Check with identical key
+        assertSame(v3, map.putIfAbsent(k3, v3));
+        // Check with equivalent key
+        assertSame(v3, map.putIfAbsent(k3dup, v3));
     }
 }
