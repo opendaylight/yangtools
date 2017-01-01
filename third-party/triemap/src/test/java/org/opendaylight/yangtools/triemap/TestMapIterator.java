cf6392981801cb3e87911a9b8416639af8001ba1
Robert Varga <rovarga@cisco.com>
2017-01-01 14:07:14 +0100

    BUG-7464: remove TestHelper
    TestHelper is an unneeded wrapper around Assert's methods,
    use them directly as appropriate instead of the constraining
    abstraction.
    
    Change-Id: I8648d3dc731100b562c64b657f4e78e8787d9f61
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- third-party/triemap/src/test/java/org/opendaylight/yangtools/triemap/TestMapIterator.java
+++ third-party/triemap/src/test/java/org/opendaylight/yangtools/triemap/TestMapIterator.java
@@ -15,6 +15,12 @@
  */
 package org.opendaylight.yangtools.triemap;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
@@ -26,45 +32,47 @@ import org.junit.Test;
 public class TestMapIterator {
     @Test
     public void testMapIterator () {
-        for (int i = 0; i < 60 * 1000; i+= 400 + new Random ().nextInt (400)) {
-            System.out.println (i);
-            final Map<Integer, Integer> bt = new TrieMap <> ();
+        final Random random = new Random();
+
+        for (int i = 0; i < 60 * 1000; i+= 400 + random.nextInt(400)) {
+            final Map<Integer, Integer> bt = new TrieMap <>();
             for (int j = 0; j < i; j++) {
-                TestHelper.assertEquals (null, bt.put (Integer.valueOf (j), Integer.valueOf (j)));
+                assertNull(bt.put(Integer.valueOf(j), Integer.valueOf(j)));
             }
             int count = 0;
-            final Set<Integer> set = new HashSet<> ();
-            for (final Map.Entry<Integer, Integer> e : bt.entrySet ()) {
-                set.add (e.getKey ());
+            final Set<Integer> set = new HashSet<>();
+            for (final Entry<Integer, Integer> e : bt.entrySet()) {
+                set.add(e.getKey());
                 count++;
             }
             for (final Integer j : set) {
-                TestHelper.assertTrue (bt.containsKey (j));
+                assertTrue(bt.containsKey(j));
             }
-            for (final Integer j : bt.keySet ()) {
-                TestHelper.assertTrue (set.contains (j));
-            }
-
-            TestHelper.assertEquals (i, count);
-            TestHelper.assertEquals (i, bt.size ());
-
-            for (Entry<Integer, Integer> e : bt.entrySet ()) {
-                TestHelper.assertTrue (e.getValue () == bt.get (e.getKey ()));
-                e.setValue (e.getValue () + 1);
-                TestHelper.assertTrue (e.getValue () == e.getKey () + 1);
-                TestHelper.assertTrue (e.getValue () == bt.get (e.getKey ()));
-                e.setValue (e.getValue () - 1);
+            for (final Integer j : bt.keySet()) {
+                assertTrue(set.contains(j));
             }
 
-            for (final Iterator<Integer> iter = bt.keySet ().iterator (); iter.hasNext ();) {
-                final Integer k = iter.next ();
-                TestHelper.assertTrue (bt.containsKey (k));
-                iter.remove ();
-                TestHelper.assertFalse (bt.containsKey (k));
+            assertEquals(i, count);
+            assertEquals(i, bt.size());
+
+            for (Entry<Integer, Integer> e : bt.entrySet()) {
+                assertSame(e.getValue(), bt.get(e.getKey()));
+                e.setValue(e.getValue() + 1);
+                assertEquals((Object)e.getValue(), e.getKey () + 1);
+                assertEquals(e.getValue(), bt.get(e.getKey()));
+                e.setValue(e.getValue() - 1);
             }
 
-            TestHelper.assertEquals (0, bt.size ());
-            TestHelper.assertTrue (bt.isEmpty ());
+            final Iterator<Integer> it = bt.keySet().iterator();
+            while(it.hasNext()) {
+                final Integer k = it.next ();
+                assertTrue(bt.containsKey(k));
+                it.remove();
+                assertFalse(bt.containsKey(k));
+            }
+
+            assertEquals(0, bt.size ());
+            assertTrue(bt.isEmpty ());
         }
     }
 }
