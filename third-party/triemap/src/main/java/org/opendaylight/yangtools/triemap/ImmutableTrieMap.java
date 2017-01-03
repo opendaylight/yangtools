cb992b5413f37bce45e4de7d804dca09d8d06057
Robert Varga <rovarga@cisco.com>
2017-01-03 12:39:11 +0100

    BUG-7464: Add a dedicated ImmutableEntrySet
    Having immutability as a specialization is useful, as we can make explicit
    assumptions about the map not moving. This will be important as we implement
    specialized interators and spliterators.
    
    Change-Id: I00a36a5bfc48dc31b088a46b07545f0442679b49
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- third-party/triemap/src/main/java/org/opendaylight/yangtools/triemap/ImmutableTrieMap.java
+++ third-party/triemap/src/main/java/org/opendaylight/yangtools/triemap/ImmutableTrieMap.java
@@ -20,6 +20,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import com.google.common.annotations.Beta;
 import com.google.common.base.Equivalence;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.Function;
@@ -120,6 +121,11 @@ public final class ImmutableTrieMap<K, V> extends TrieMap<K, V> {
     }
 
     @Override
+    ImmutableEntrySet<K, V> createEntrySet() {
+        return new ImmutableEntrySet<>(this);
+    }
+
+    @Override
     boolean isReadOnly() {
         return true;
     }
@@ -129,7 +135,12 @@ public final class ImmutableTrieMap<K, V> extends TrieMap<K, V> {
         return root;
     }
 
-    private static UnsupportedOperationException unsupported() {
+    static UnsupportedOperationException unsupported() {
         return new UnsupportedOperationException("Attempted to modify a read-only view");
+    }
+
+    @Override
+    Iterator<Entry<K, V>> iterator() {
+        return immutableIterator();
     }
 }
