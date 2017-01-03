62650d7cd931fa7e6415e644db57b56a4300c65c
Robert Varga <rovarga@cisco.com>
2017-01-03 22:16:17 +0100

    BUG-7464: Add dedicated key set classes
    These are slightly more efficient versions of the default
    key set and react to Set.remove().
    
    Change-Id: If67d4c75210d22dafbd2cd2dabeebc8554505a5e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ third-party/triemap/src/main/java/org/opendaylight/yangtools/triemap/AbstractKeySet.java
@@ -0,0 +1,65 @@
+/*
+ * (C) Copyright 2016 Pantheon Technologies, s.r.o. and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.opendaylight.yangtools.triemap;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.AbstractSet;
+import java.util.Collection;
+
+/**
+ * Abstract base class for key set views of a TrieMap
+ *
+ * @author Robert Varga
+ *
+ * @param <K> the type of keys
+ */
+abstract class AbstractKeySet<K> extends AbstractSet<K> {
+    private final TrieMap<K, ?> map;
+
+    AbstractKeySet(final TrieMap<K, ?> map) {
+        this.map = checkNotNull(map);
+    }
+
+    final TrieMap<K, ?> map() {
+        return map;
+    }
+
+    @Override
+    public final boolean add(final K e) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public final boolean addAll(final Collection<? extends K> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public final boolean contains(final Object o) {
+        return map.containsKey(o);
+    }
+
+    @Override
+    public final boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public final int size() {
+        return map.size();
+    }
+}
