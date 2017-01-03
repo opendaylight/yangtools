0680091c3e96a7487be6a5609b3a052de77f9334
Robert Varga <rovarga@cisco.com>
2017-01-03 22:16:17 +0100

    BUG-7464: Add dedicated key set classes
    These are slightly more efficient versions of the default
    key set and react to Set.remove().
    
    Change-Id: If67d4c75210d22dafbd2cd2dabeebc8554505a5e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ third-party/triemap/src/main/java/org/opendaylight/yangtools/triemap/ImmutableKeySet.java
@@ -0,0 +1,60 @@
+/*
+ * (C) Copyright 2016 Pantheon Technologies, s.r.o. and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.opendaylight.yangtools.triemap;
+
+import static org.opendaylight.yangtools.triemap.ImmutableTrieMap.unsupported;
+
+import com.google.common.collect.Iterators;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+/**
+ * An immutable view of a TrieMap's key set.
+ *
+ * @author Robert Varga
+ *
+ * @param <K> the type of keys
+ */
+final class ImmutableKeySet<K> extends AbstractKeySet<K> {
+    ImmutableKeySet(final TrieMap<K, ?> map) {
+        super(map);
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return Iterators.transform(map().immutableIterator(), Entry::getKey);
+    }
+
+    @Override
+    public void clear() {
+        throw unsupported();
+    }
+
+    @Override
+    public boolean remove(final Object o) {
+        throw unsupported();
+    }
+    @Override
+    public boolean retainAll(final Collection<?> c) {
+        throw unsupported();
+    }
+
+    @Override
+    public boolean removeAll(final Collection<?> c) {
+        throw unsupported();
+    }
+}
