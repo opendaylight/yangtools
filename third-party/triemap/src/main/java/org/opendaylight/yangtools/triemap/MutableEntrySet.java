d766ae0a79583f652f5ba80d7f171856b95c040d
Robert Varga <rovarga@cisco.com>
2017-01-03 12:39:11 +0100

    BUG-7464: Add a dedicated ImmutableEntrySet
    Having immutability as a specialization is useful, as we can make explicit
    assumptions about the map not moving. This will be important as we implement
    specialized interators and spliterators.
    
    Change-Id: I00a36a5bfc48dc31b088a46b07545f0442679b49
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- third-party/triemap/src/main/java/org/opendaylight/yangtools/triemap/MutableEntrySet.java
+++ third-party/triemap/src/main/java/org/opendaylight/yangtools/triemap/MutableEntrySet.java
@@ -15,9 +15,8 @@
  */
 package org.opendaylight.yangtools.triemap;
 
-import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkArgument;
 
-import java.util.AbstractSet;
 import java.util.Iterator;
 import java.util.Map.Entry;
 
@@ -27,30 +26,30 @@ import java.util.Map.Entry;
  * @param <K> the type of keys
  * @param <V> the type of values
  */
-final class EntrySet<K, V> extends AbstractSet<Entry<K, V>> {
-    private final TrieMap<K, V> map;
+final class MutableEntrySet<K, V> extends AbstractEntrySet<K, V> {
+    MutableEntrySet(final TrieMap<K, V> map) {
+        super(map);
+    }
 
-    EntrySet(final TrieMap<K, V> map) {
-        this.map = checkNotNull(map);
+    @Override
+    public boolean add(final Entry<K, V> e) {
+        final K k = e.getKey();
+        checkArgument(k != null);
+        final V v = e.getValue();
+        checkArgument(v != null);
+
+        final V prev = map().putIfAbsent(k, v);
+        return prev == null || !v.equals(prev);
+    }
+
+    @Override
+    public void clear() {
+        map().clear();
     }
 
     @Override
     public Iterator<Entry<K, V>> iterator() {
-        return map.iterator();
-    }
-
-    @Override
-    public boolean contains(final Object o) {
-        if (!(o instanceof Entry)) {
-            return false;
-        }
-
-        final Entry<?, ?> e = (Entry<?, ?>) o;
-        if (e.getKey() == null) {
-            return false;
-        }
-        final V v = map.get(e.getKey());
-        return v != null && v.equals(e.getValue());
+        return map().iterator();
     }
 
     @Override
@@ -69,16 +68,6 @@ final class EntrySet<K, V> extends AbstractSet<Entry<K, V>> {
             return false;
         }
 
-        return map.remove(key, value);
-    }
-
-    @Override
-    public final int size() {
-        return map.size();
-    }
-
-    @Override
-    public final void clear() {
-        map.clear();
+        return map().remove(key, value);
     }
 }
