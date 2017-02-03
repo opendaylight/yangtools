fdd95ac1071bd11968b27817c28e9dfe14226ffe
Peter Kajsa <pkajsa@cisco.com>
2017-02-03 14:25:27 +0100

    BUG-7424: Disallow child namespace collisions
    All leafs, leaf-lists, lists, containers, choices, rpcs,
    notifications, and anyxmls defined (directly or through a uses
    statement) within a parent node or at the top level of the module
    or its submodules share the same identifier namespace.
    
    Change-Id: I1852d2ca5ebd07ba6c1ed21a5ceded3e87d67467
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug7424Test.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.parser.spi.meta.SomeModifiersUnresolvedException;
+
+public class Bug7424Test {
+    @Test
+    public void testRpc() throws Exception {
+        try {
+            StmtTestUtils.parseYangSource("/bugs/bug7424/foo-rpc.yang");
+            fail("Test should fail due to invalid yang model.");
+        } catch (final SomeModifiersUnresolvedException e) {
+            assertTrue(e
+                    .getCause()
+                    .getMessage()
+                    .startsWith(
+                            "Error in module 'foo': cannot add '(foo?revision=1970-01-01)name'. "
+                                    + "Node name collision: '(foo?revision=1970-01-01)name' already declared."));
+        }
+    }
+
+    @Test
+    public void testNotification() throws Exception {
+        try {
+            StmtTestUtils.parseYangSource("/bugs/bug7424/foo-notification.yang");
+            fail("Test should fail due to invalid yang model.");
+        } catch (final SomeModifiersUnresolvedException e) {
+            assertTrue(e
+                    .getCause()
+                    .getMessage()
+                    .startsWith(
+                            "Error in module 'foo': cannot add '(foo?revision=1970-01-01)name'. "
+                                    + "Node name collision: '(foo?revision=1970-01-01)name' already declared."));
+        }
+    }
+
+    @Test
+    public void testData() throws Exception {
+        try {
+            StmtTestUtils.parseYangSource("/bugs/bug7424/foo-data.yang");
+            fail("Test should fail due to invalid yang model.");
+        } catch (final SomeModifiersUnresolvedException e) {
+            assertTrue(e
+                    .getCause()
+                    .getMessage()
+                    .startsWith(
+                            "Error in module 'foo': cannot add '(foo?revision=1970-01-01)name'. "
+                                    + "Node name collision: '(foo?revision=1970-01-01)name' already declared."));
+        }
+    }
+
+    @Test
+    public void testRpcUses() throws Exception {
+        try {
+            StmtTestUtils.parseYangSource("/bugs/bug7424/foo-rpc-uses.yang");
+            fail("Test should fail due to invalid yang model.");
+        } catch (final SomeModifiersUnresolvedException e) {
+            assertTrue(e
+                    .getCause()
+                    .getMessage()
+                    .startsWith(
+                            "Error in module 'foo': cannot add '(foo?revision=1970-01-01)name'. "
+                                    + "Node name collision: '(foo?revision=1970-01-01)name' already declared."));
+        }
+    }
+}
