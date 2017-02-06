efe4663509662247256595e730231270db267c17
Igor Foltin <ifoltin@cisco.com>
2017-02-06 08:40:48 +0100

    Bug 6887: [Yang 1.1] Allow enumerations and bits to be subtyped
    Starting with YANG 1.1, enumeration and bits types
    can be restricted
    
    An enumeration type can be restricted with one or more
    "enum" statements, which enumerate a subset of the values
    for the base type.
    
    A bits type can be restricted with one or more "bit"
    statements, which enumerate a subset of the values for
    the base type.
    
    Change-Id: If46991b858a2a9153a60de85645f6ec301fcb33f
    Signed-off-by: Igor Foltin <ifoltin@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/type/BitsTypeEffectiveStatementImpl.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/type/BitsTypeEffectiveStatementImpl.java
@@ -8,17 +8,75 @@
 
 package org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.type;
 
+import javax.annotation.Nonnull;
+import org.opendaylight.yangtools.yang.common.YangVersion;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.TypeEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.TypeStatement;
 import org.opendaylight.yangtools.yang.model.api.type.BitsTypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.BitsTypeDefinition.Bit;
+import org.opendaylight.yangtools.yang.model.util.type.BitBuilder;
+import org.opendaylight.yangtools.yang.model.util.type.BitsTypeBuilder;
+import org.opendaylight.yangtools.yang.model.util.type.InvalidBitDefinitionException;
 import org.opendaylight.yangtools.yang.model.util.type.RestrictedTypes;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
-import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.TypeUtils;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.DeclaredEffectiveStatementBase;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.UnknownEffectiveStatementImpl;
 
-public final class BitsTypeEffectiveStatementImpl extends AbstractTypeEffectiveStatement<BitsTypeDefinition> {
+public final class BitsTypeEffectiveStatementImpl extends DeclaredEffectiveStatementBase<String, TypeStatement>
+        implements TypeEffectiveStatement<TypeStatement> {
+
+    private final BitsTypeDefinition typeDefinition;
+
     public BitsTypeEffectiveStatementImpl(
             final StmtContext<String, TypeStatement, EffectiveStatement<String, TypeStatement>> ctx,
             final BitsTypeDefinition baseType) {
-        super(ctx, RestrictedTypes.newBitsBuilder(baseType, TypeUtils.typeEffectiveSchemaPath(ctx)));
+        super(ctx);
+
+        final BitsTypeBuilder builder = RestrictedTypes.newBitsBuilder(baseType, ctx.getSchemaPath().get());
+
+        for (final EffectiveStatement<?, ?> stmt : effectiveSubstatements()) {
+            if (stmt instanceof BitEffectiveStatementImpl) {
+                SourceException.throwIf(ctx.getRootVersion() != YangVersion.VERSION_1_1,
+                        ctx.getStatementSourceReference(), "Restricted bits type is allowed only in YANG 1.1 version.");
+                final BitEffectiveStatementImpl bitSubStmt = (BitEffectiveStatementImpl) stmt;
+
+                final long effectivePos;
+                if (bitSubStmt.getDeclaredPosition() == null) {
+                    effectivePos = getBaseTypeBitPosition(bitSubStmt.getName(), baseType);
+                } else {
+                    effectivePos = bitSubStmt.getDeclaredPosition();
+                }
+
+                final Bit b = BitBuilder.create(bitSubStmt.getPath(), effectivePos)
+                        .setDescription(bitSubStmt.getDescription()).setReference(bitSubStmt.getReference())
+                        .setStatus(bitSubStmt.getStatus()).setUnknownSchemaNodes(bitSubStmt.getUnknownSchemaNodes())
+                        .build();
+
+                builder.addBit(b);
+            } else if (stmt instanceof UnknownEffectiveStatementImpl) {
+                builder.addUnknownSchemaNode((UnknownEffectiveStatementImpl) stmt);
+            }
+        }
+
+        typeDefinition = builder.build();
+    }
+
+    private static long getBaseTypeBitPosition(final String bitName, final BitsTypeDefinition baseType) {
+        for (Bit baseTypeBit : baseType.getBits()) {
+            if (bitName.equals(baseTypeBit.getName())) {
+                return baseTypeBit.getPosition();
+            }
+        }
+
+        throw new InvalidBitDefinitionException("Bit '%s' is not a subset of its base bits type %s.", bitName,
+                baseType.getQName());
+    }
+
+    @Nonnull
+    @Override
+    public BitsTypeDefinition getTypeDefinition() {
+        return typeDefinition;
     }
 }
