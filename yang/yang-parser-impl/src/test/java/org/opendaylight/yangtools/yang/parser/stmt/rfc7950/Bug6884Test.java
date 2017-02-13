8225f7421f980f10bc28162a08c3d59d73f0c854
Peter Kajsa <pkajsa@cisco.com>
2017-02-13 09:23:17 +0100

    Bug 6884 - [Yang 1.1] Submodule can reference all definitions in all submodules
    Since Yang 1.1, it has been allowed a submodule to reference all definitions
    in all submodules that belong to the same module, without using the "include"
    statement. This patch also deals with possible circular chains of includes
    between submodules.
    
    Change-Id: I2b3d19fc2c867882fc89f042bbe5febaf89bc2ea
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/parser/stmt/rfc7950/Bug6884Test.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.stmt.rfc7950;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import com.google.common.collect.ImmutableList;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.SchemaNode;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.util.SchemaContextUtil;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+import org.opendaylight.yangtools.yang.stmt.StmtTestUtils;
+
+public class Bug6884Test {
+    private static final String FOO_NS = "foo";
+    private static final String FOO_REV = "1970-01-01";
+
+    @Test
+    public void testYang11() throws SourceException, FileNotFoundException, ReactorException, URISyntaxException {
+        final SchemaContext schemaContext = StmtTestUtils.parseYangSources("/rfc7950/bug6884/yang1-1");
+        assertNotNull(schemaContext);
+
+        assertTrue(findNode(schemaContext, ImmutableList.of(foo("sub-root"), foo("sub-foo-2-con"))) instanceof ContainerSchemaNode);
+    }
+
+    @Test
+    public void testCircularIncludesYang10() throws SourceException, FileNotFoundException, ReactorException,
+            URISyntaxException {
+        final SchemaContext schemaContext = StmtTestUtils.parseYangSources("/rfc7950/bug6884/circular-includes");
+
+        assertNotNull(schemaContext);
+        assertTrue(findNode(schemaContext, ImmutableList.of(foo("sub-root"), foo("sub-foo-2-con"))) instanceof ContainerSchemaNode);
+        assertTrue(findNode(schemaContext, ImmutableList.of(foo("sub-root-2"), foo("sub-foo-con"))) instanceof ContainerSchemaNode);
+    }
+
+    private static SchemaNode findNode(final SchemaContext context, final Iterable<QName> qNames) {
+        return SchemaContextUtil.findDataSchemaNode(context, SchemaPath.create(qNames, true));
+    }
+
+    private static QName foo(final String localName) {
+        return QName.create(FOO_NS, FOO_REV, localName);
+    }
+}
