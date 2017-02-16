b1426618356b312e8970c510a797d086a6049a29
Igor Foltin <ifoltin@cisco.com>
2017-02-16 09:43:15 +0100

    Bug 6878: Add support for parsing yang-specific XPath functions
    YANG 1.1 (RFC7950) introduces a set of yang-specific XPath functions.
    YANG statement parser can now parse YANG 1.1 models which contain
    these functions.
    
    Change-Id: Icfbf95db0d04a3f077133c38341f250cde4e56eb
    Signed-off-by: Igor Foltin <ifoltin@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/Utils.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/Utils.java
@@ -13,6 +13,7 @@ import com.google.common.base.CharMatcher;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableBiMap;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
 import com.google.common.collect.ImmutableSet;
@@ -37,6 +38,8 @@ import org.opendaylight.yangtools.antlrv4.code.gen.YangStatementParser;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.common.SimpleDateFormatUtil;
+import org.opendaylight.yangtools.yang.common.YangConstants;
+import org.opendaylight.yangtools.yang.common.YangVersion;
 import org.opendaylight.yangtools.yang.model.api.DeviateKind;
 import org.opendaylight.yangtools.yang.model.api.ModuleIdentifier;
 import org.opendaylight.yangtools.yang.model.api.RevisionAwareXPath;
@@ -68,6 +71,7 @@ import org.slf4j.LoggerFactory;
 
 public final class Utils {
     private static final int UNICODE_SCRIPT_FIX_COUNTER = 30;
+    private static final String YANG_XPATH_FUNCTIONS_PREFIX = "yang";
     private static final Logger LOG = LoggerFactory.getLogger(Utils.class);
     private static final CharMatcher LEFT_PARENTHESIS_MATCHER = CharMatcher.is('(');
     private static final CharMatcher RIGHT_PARENTHESIS_MATCHER = CharMatcher.is(')');
@@ -305,6 +309,9 @@ public final class Utils {
         }
     };
 
+    private static final Set<String> YANG_XPATH_FUNCTIONS = ImmutableSet.of("re-match", "deref",
+            "derived-from", "derived-from-or-self", "enum-value", "bit-is-set");
+
     private Utils() {
         throw new UnsupportedOperationException();
     }
@@ -358,17 +365,37 @@ public final class Utils {
 
     static RevisionAwareXPath parseXPath(final StmtContext<?, ?, ?> ctx, final String path) {
         final XPath xPath = XPATH_FACTORY.get().newXPath();
-        xPath.setNamespaceContext(StmtNamespaceContext.create(ctx));
+        xPath.setNamespaceContext(StmtNamespaceContext.create(ctx,
+                ImmutableBiMap.of(YangConstants.RFC6020_YANG_NAMESPACE.toString(), YANG_XPATH_FUNCTIONS_PREFIX)));
 
         final String trimmed = trimSingleLastSlashFromXPath(path);
         try {
+            // XPath extension functions have to be prefixed
+            // yang-specific XPath functions are in fact extended functions, therefore we have to add
+            // "yang" prefix to them so that they can be properly validated with the XPath.compile() method
+            // the "yang" prefix is bound to RFC6020 YANG namespace
+            final String prefixedXPath = addPrefixToYangXPathFunctions(trimmed, ctx);
             // TODO: we could capture the result and expose its 'evaluate' method
-            xPath.compile(trimmed);
+            xPath.compile(prefixedXPath);
         } catch (final XPathExpressionException e) {
             LOG.warn("Argument \"{}\" is not valid XPath string at \"{}\"", path, ctx.getStatementSourceReference(), e);
         }
 
         return new RevisionAwareXPathImpl(path, PATH_ABS.matcher(path).matches());
+    }
+
+    private static String addPrefixToYangXPathFunctions(final String path, final StmtContext<?, ?, ?> ctx) {
+        if (ctx.getRootVersion() == YangVersion.VERSION_1_1) {
+            String result = path;
+            final String prefix = YANG_XPATH_FUNCTIONS_PREFIX + ":";
+            for (final String xpathFunction : YANG_XPATH_FUNCTIONS) {
+                result = result.replaceAll(xpathFunction + "\\(", prefix + xpathFunction + "(");
+            }
+
+            return result;
+        }
+
+        return path;
     }
 
     public static QName trimPrefix(final QName identifier) {
---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/parser/stmt/rfc7950/Bug6878Test.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.parser.stmt.rfc7950;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.stmt.StmtTestUtils;
+
+public class Bug6878Test {
+
+    @Test
+    public void testParsingXPathWithYang11Functions() throws Exception {
+        final PrintStream stdout = System.out;
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        final String testLog;
+
+        System.setOut(new PrintStream(output, true, "UTF-8"));
+
+        final SchemaContext schemaContext = StmtTestUtils.parseYangSource("/rfc7950/bug6878/foo.yang");
+        assertNotNull(schemaContext);
+
+        testLog = output.toString();
+        assertFalse(testLog.contains("Could not find function: "));
+        System.setOut(stdout);
+    }
+
+    @Test
+    public void shouldLogInvalidYang10XPath() throws Exception {
+        final PrintStream stdout = System.out;
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        final String testLog;
+
+        System.setOut(new PrintStream(output, true, "UTF-8"));
+
+        StmtTestUtils.parseYangSource("/rfc7950/bug6878/foo10-invalid.yang");
+
+        testLog = output.toString();
+        assertTrue(testLog.contains("Could not find function: re-match"));
+        System.setOut(stdout);
+    }
+
+    @Test
+    public void shouldLogInvalidYang10XPath2() throws Exception {
+        final PrintStream stdout = System.out;
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        final String testLog;
+
+        System.setOut(new PrintStream(output, true, "UTF-8"));
+
+        StmtTestUtils.parseYangSource("/rfc7950/bug6878/foo10-invalid-2.yang");
+
+        testLog = output.toString();
+        assertTrue(testLog.contains("Could not find function: deref"));
+        System.setOut(stdout);
+    }
+}
