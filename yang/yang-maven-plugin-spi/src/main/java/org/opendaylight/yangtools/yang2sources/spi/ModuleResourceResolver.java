39584243fc19671baf97223b2ae7d4171fc5355b
Robert Varga <rovarga@cisco.com>
2017-02-18 12:44:17 +0100

    Add generalized FileGenerator interface
    This patch adds a maven-independent interface for code generators,
    which captures enough semantics to make mapping usable. We will be
    implementing this interface in place of the original SPI.
    
    JIRA: YANGTOOLS-1146
    Change-Id: I3daef73d3fa71e74a9678979fd00417579f60695
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-maven-plugin-spi/src/main/java/org/opendaylight/yangtools/yang2sources/spi/ModuleResourceResolver.java
+++ yang/yang-maven-plugin-spi/src/main/java/org/opendaylight/yangtools/yang2sources/spi/ModuleResourceResolver.java
@@ -8,9 +8,6 @@
 package org.opendaylight.yangtools.yang2sources.spi;
 
 import com.google.common.annotations.Beta;
-import java.util.Optional;
-import org.opendaylight.yangtools.yang.model.api.ModuleLike;
-import org.opendaylight.yangtools.yang.model.repo.api.SchemaSourceRepresentation;
 
 /**
  * An SPI-level interface to find the schema source for a particular YANG module, as packaged in the final artifact.
@@ -18,16 +15,6 @@ import org.opendaylight.yangtools.yang.model.repo.api.SchemaSourceRepresentation
  */
 @Beta
 @FunctionalInterface
-public interface ModuleResourceResolver {
-    /**
-     * Find the path of the packaged resource which corresponds to the specified module in the specified representation.
-     *
-     * @param module Requested module
-     * @param representation Requested representation
-     * @return Path to packaged resource
-     * @throws NullPointerException if any argument is null
-     * @throws IllegalArgumentException if the requested representation is not supported by this resolver
-     */
-    Optional<String> findModuleResourcePath(ModuleLike module,
-        Class<? extends SchemaSourceRepresentation> representation);
+public interface ModuleResourceResolver extends org.opendaylight.yangtools.plugin.generator.api.ModuleResourceResolver {
+
 }
