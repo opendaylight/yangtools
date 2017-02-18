2ced03ec80a744947d4ba435f870ff2e642e8d12
Robert Varga <rovarga@cisco.com>
2017-02-18 12:44:17 +0100

    Add generalized FileGenerator interface
    This patch adds a maven-independent interface for code generators,
    which captures enough semantics to make mapping usable. We will be
    implementing this interface in place of the original SPI.
    
    JIRA: YANGTOOLS-1146
    Change-Id: I3daef73d3fa71e74a9678979fd00417579f60695
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ plugin/plugin-generator-api/src/main/java/org/opendaylight/yangtools/plugin/generator/api/AbstractGeneratedFile.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.plugin.generator.api;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.MoreObjects.ToStringHelper;
+import java.io.IOException;
+import java.io.OutputStream;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Abstract base class for {@link GeneratedFile}s. This class is suitable for binary files. For text files use
+ * {@link AbstractGeneratedTextFile}.
+ *
+ * @author Robert Varga
+ */
+@Beta
+@NonNullByDefault
+public abstract class AbstractGeneratedFile implements GeneratedFile {
+    private final GeneratedFileLifecycle lifecycle;
+
+    protected AbstractGeneratedFile(final GeneratedFileLifecycle lifecycle) {
+        this.lifecycle = requireNonNull(lifecycle);
+    }
+
+    /**
+     * Return the lifecycle governing this file.
+     *
+     * @return Governing lifecycle
+     */
+    @Override
+    public final GeneratedFileLifecycle getLifecycle() {
+        return lifecycle;
+    }
+
+    /**
+     * Write the body of this file into specified {@link OutputStream}.
+     *
+     * @param output stream where to write the output
+     * @throws IOException when the stream reports an IOException
+     */
+    @Override
+    public abstract void writeBody(OutputStream output) throws IOException;
+
+    @Override
+    public final String toString() {
+        return addToStringAttributes(MoreObjects.toStringHelper(this).omitNullValues()).toString();
+    }
+
+    protected ToStringHelper addToStringAttributes(final ToStringHelper helper) {
+        return helper.add("lifecycle", lifecycle);
+    }
+}
---
+++ plugin/plugin-generator-api/src/main/java/org/opendaylight/yangtools/plugin/generator/api/AbstractGeneratedTextFile.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.plugin.generator.api;
+
+import com.google.common.annotations.Beta;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * The contents of a generated file and its {@link GeneratedFileLifecycle}. This class is suitable for text files,
+ * for binary files see {@link AbstractGeneratedFile}. Text files are encoded in {@link StandardCharsets#UTF_8}.
+ *
+ * @author Robert Varga
+ */
+@Beta
+@NonNullByDefault
+public abstract class AbstractGeneratedTextFile extends AbstractGeneratedFile {
+    protected AbstractGeneratedTextFile(final GeneratedFileLifecycle lifecycle) {
+        super(lifecycle);
+    }
+
+    @Override
+    public final void writeBody(final OutputStream output) throws IOException {
+        try (Writer writer = new OutputStreamWriter(output, StandardCharsets.UTF_8)) {
+            try (BufferedWriter buffered = new BufferedWriter(writer)) {
+                writeBody(buffered);
+            }
+        }
+    }
+
+    /**
+     * Write the body of this file into specified {@link Writer}.
+     *
+     * @param output writer where to write the output
+     * @throws IOException when the stream reports an IOException
+     */
+    protected abstract void writeBody(Writer output) throws IOException;
+}
