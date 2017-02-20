8a1c40a4a06b27800df6ca9e19e004e18349f14f
Robert Varga <robert.varga@pantheon.tech>
2017-02-20 21:46:34 +0100

    BUG-6315: Use YangTextSchemaSource to emit schema files
    To properly address the issue of META-INF/yang files not having their
    revisions we need to encapsulate minimal project initialization state,
    which gives us properly-resolved revisions.
    
    We then refactor the execution flow to first inspect files to
    determine whan needs to happen and then execute.
    
    Change-Id: I3257475f0b267adf0e4d5cd469829a267cfc68aa
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-maven-plugin/src/main/java/org/opendaylight/yangtools/yang2sources/plugin/YangProvider.java
+++ yang/yang-maven-plugin/src/main/java/org/opendaylight/yangtools/yang2sources/plugin/YangProvider.java
@@ -7,58 +7,56 @@
  */
 package org.opendaylight.yangtools.yang2sources.plugin;
 
+import com.google.common.io.Files;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
 import org.apache.maven.model.Resource;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
+import org.opendaylight.yangtools.yang.model.repo.api.YangTextSchemaSource;
 import org.opendaylight.yangtools.yang2sources.plugin.ConfigArg.CodeGeneratorArg;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-class YangProvider {
-    private static final Logger LOG = LoggerFactory.getLogger(YangProvider.class);
+abstract class YangProvider {
+    private static final class Default extends YangProvider {
+        private static final Logger LOG = LoggerFactory.getLogger(YangProvider.class);
 
-    void addYangsToMetaInf(final MavenProject project, final File yangFilesRootDir,
-            final Collection<File> excludedFiles) throws MojoFailureException {
+        @Override
+        void addYangsToMetaInf(final MavenProject project, final Collection<YangTextSchemaSource> modelsInProject)
+                throws IOException {
 
-        // copy project's src/main/yang/*.yang to target/generated-sources/yang/META-INF/yang/*.yang
-
-        File generatedYangDir = new File(project.getBasedir(), CodeGeneratorArg.YANG_GENERATED_DIR);
-        addYangsToMetaInf(project, yangFilesRootDir, excludedFiles, generatedYangDir);
-
-        // Also copy to the actual build output dir if different than "target". When running in
-        // Eclipse this can differ (eg "target-ide").
-
-        File actualGeneratedYangDir = new File(project.getBuild().getDirectory(),
+            final File generatedYangDir = new File(project.getBuild().getDirectory(),
                 CodeGeneratorArg.YANG_GENERATED_DIR.replace("target" + File.separator, ""));
-        if (!actualGeneratedYangDir.equals(generatedYangDir)) {
-            addYangsToMetaInf(project, yangFilesRootDir, excludedFiles, actualGeneratedYangDir);
-        }
-    }
 
-    private static void addYangsToMetaInf(final MavenProject project, final File yangFilesRootDir,
-            final Collection<File> excludedFiles, final File generatedYangDir) throws MojoFailureException {
+            LOG.debug("Generated dir {}", generatedYangDir);
 
-        File withMetaInf = new File(generatedYangDir, YangToSourcesProcessor.META_INF_YANG_STRING);
-        withMetaInf.mkdirs();
+            // copy project's src/main/yang/*.yang to ${project.builddir}/generated-sources/yang/META-INF/yang/
+            // This honors setups like a Eclipse-profile derived one
+            final File withMetaInf = new File(generatedYangDir, YangToSourcesProcessor.META_INF_YANG_STRING);
+            withMetaInf.mkdirs();
 
-        try {
-            Collection<File> files = Util.listFiles(yangFilesRootDir, excludedFiles);
-            for (File file : files) {
-                org.apache.commons.io.FileUtils.copyFile(file, new File(withMetaInf, file.getName()));
+            for (YangTextSchemaSource source : modelsInProject) {
+                final String fileName = source.getIdentifier().toYangFilename();
+                final File file = new File(withMetaInf, fileName);
+
+                source.copyTo(Files.asByteSink(file));
+                LOG.debug("Created file {} for {}", file, source.getIdentifier());
             }
-        } catch (IOException e) {
-            LOG.warn("Failed to generate files into root {}", yangFilesRootDir, e);
-            throw new MojoFailureException("Unable to list yang files into resource folder", e);
+
+            setResource(generatedYangDir, project);
+            LOG.debug("{} YANG files marked as resources: {}", YangToSourcesProcessor.LOG_PREFIX, generatedYangDir);
         }
-
-        setResource(generatedYangDir, project);
-
-        LOG.debug("{} Yang files from: {} marked as resources: {}", YangToSourcesProcessor.LOG_PREFIX, yangFilesRootDir,
-                YangToSourcesProcessor.META_INF_YANG_STRING_JAR);
     }
+
+    private static final YangProvider DEFAULT = new Default();
+
+    static YangProvider getInstance() {
+        return DEFAULT;
+    }
+
+    abstract void addYangsToMetaInf(MavenProject project, Collection<YangTextSchemaSource> modelsInProject)
+            throws IOException;
 
     static void setResource(final File targetYangDir, final MavenProject project) {
         Resource res = new Resource();
