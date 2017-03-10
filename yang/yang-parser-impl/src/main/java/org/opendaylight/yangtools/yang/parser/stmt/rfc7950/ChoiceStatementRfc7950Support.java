57f7690df88165e1c301fb839a484cb6bef2c0c5
Peter Kajsa <pkajsa@cisco.com>
2017-03-10 14:10:56 +0100

    Bug 6183 - Shorthand case should create case node in schema tree
    According to Rfc7950, if a case statement is omitted and implicit case
    node is created, schema node identifiers MUST always explicitly include
    the implicit case node identifiers.
    
    This patch deals with both Yang 1.1 and Yang 1 in the same way.
    
    Change-Id: I64fef2aee85855cb6300318f7c97e694b3105a0a
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc7950/ChoiceStatementRfc7950Support.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc7950/ChoiceStatementRfc7950Support.java
@@ -10,6 +10,7 @@ package org.opendaylight.yangtools.yang.parser.stmt.rfc7950;
 import com.google.common.annotations.Beta;
 import org.opendaylight.yangtools.yang.model.api.YangStmtMapping;
 import org.opendaylight.yangtools.yang.parser.spi.SubstatementValidator;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StatementSupport;
 import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.ChoiceStatementImpl;
 
 /**
@@ -35,6 +36,13 @@ public final class ChoiceStatementRfc7950Support extends ChoiceStatementImpl.Def
             .addOptional(YangStmtMapping.STATUS)
             .addOptional(YangStmtMapping.WHEN)
             .build();
+
+    private static final StatementSupport<?, ?, ?> IMPLICIT_CASE = new CaseStatementRfc7950Support();
+
+    @Override
+    protected StatementSupport<?, ?, ?> implictCase() {
+        return IMPLICIT_CASE;
+    }
 
     @Override
     protected SubstatementValidator getSubstatementValidator() {
