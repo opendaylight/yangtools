f8a1d897db58389f1e1b4365e4674f91bf197a6c
Robert Varga <rovarga@cisco.com>
2017-03-14 23:24:45 +0100

    Make sure we do not redefine prefixes
    When writing out an instance identifier make sure we do not override
    prefixes that are already defined in the writer's namespace context.
    
    Change-Id: Idca9b4b365d66f9be0a05acd73c05bab14ef0282
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- yang/yang-data-codec-xml/src/main/java/org/opendaylight/yangtools/yang/data/codec/xml/RandomPrefixInstanceIdentifierSerializer.java
+++ yang/yang-data-codec-xml/src/main/java/org/opendaylight/yangtools/yang/data/codec/xml/RandomPrefixInstanceIdentifierSerializer.java
@@ -10,18 +10,19 @@ package org.opendaylight.yangtools.yang.data.codec.xml;
 import java.net.URI;
 import java.util.Map.Entry;
 import javax.annotation.Nonnull;
+import javax.xml.namespace.NamespaceContext;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.util.AbstractStringInstanceIdentifierCodec;
 import org.opendaylight.yangtools.yang.data.util.DataSchemaContextTree;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 
 final class RandomPrefixInstanceIdentifierSerializer extends AbstractStringInstanceIdentifierCodec {
-    private final RandomPrefix prefixes = new RandomPrefix();
     private final DataSchemaContextTree schemaTree;
+    private final RandomPrefix prefixes;
 
-
-    RandomPrefixInstanceIdentifierSerializer(SchemaContext ctx) {
+    RandomPrefixInstanceIdentifierSerializer(final SchemaContext ctx, final NamespaceContext namespaceContext) {
         schemaTree = DataSchemaContextTree.from(ctx);
+        prefixes = new RandomPrefix(namespaceContext);
     }
 
     Iterable<Entry<URI, String>> getPrefixes() {
