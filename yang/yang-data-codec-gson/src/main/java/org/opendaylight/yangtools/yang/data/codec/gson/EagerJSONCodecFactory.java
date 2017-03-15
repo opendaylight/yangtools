fb197d1a7964287b95432fd2fabf91578a7c7ce7
Robert Varga <rovarga@cisco.com>
2017-03-15 19:10:18 +0100

    Split JSONCodec into multiple implementations
    Change-Id: I15342e6b4ed68ccbac9c099f63654d7456913934
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ yang/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/EagerJSONCodecFactory.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.codec.gson;
+
+import com.google.common.base.Preconditions;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import java.util.Map;
+import javax.annotation.concurrent.ThreadSafe;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.TypedSchemaNode;
+
+/**
+ * Pre-computed JSONCodecFactory. All possible codecs are created upfront at instantiation time, after which they
+ * are available for the cost of a constant lookup.
+ *
+ * @author Robert Varga
+ */
+@ThreadSafe
+final class EagerJSONCodecFactory extends JSONCodecFactory {
+    // Weak keys to retire the entry when SchemaContext goes away
+    // Soft values to keep unreferenced factories around for a bit
+    private static final LoadingCache<SchemaContext, EagerJSONCodecFactory> CACHE = CacheBuilder.newBuilder()
+            .weakKeys().softValues().build(new CacheLoader<SchemaContext, EagerJSONCodecFactory>() {
+                @Override
+                public EagerJSONCodecFactory load(final SchemaContext key) {
+                    return new EagerJSONCodecFactory(key);
+                }
+            });
+
+    private final Map<TypedSchemaNode, JSONCodec<?>> codecs;
+
+    EagerJSONCodecFactory(final SchemaContext context) {
+        super(context);
+        this.codecs = constructCodecs(context);
+    }
+
+    static EagerJSONCodecFactory getIfPresent(final SchemaContext context) {
+        return CACHE.getIfPresent(context);
+    }
+
+    static EagerJSONCodecFactory get(final SchemaContext context) {
+        return CACHE.getUnchecked(context);
+    }
+
+    @Override
+    JSONCodec<?> codecFor(final TypedSchemaNode schema) {
+        final JSONCodec<?> ret = codecs.get(schema);
+        Preconditions.checkArgument(ret != null, "No codec available for schema %s", schema);
+        return ret;
+    }
+
+    private static Map<TypedSchemaNode, JSONCodec<?>> constructCodecs(final SchemaContext context) {
+        final LazyJSONCodecFactory lazy = new LazyJSONCodecFactory(context);
+
+        // FIXME: walk all schema nodes
+
+        return lazy.getCodecs();
+    }
+}
---
+++ yang/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/SharedJSONCodecFactory.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.codec.gson;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.ThreadSafe;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.TypedSchemaNode;
+
+/**
+ * A thread-safe lazily-populated codec factory. Instances are cached in an internal weak/soft cache.
+ *
+ * @author Robert Varga
+ */
+@ThreadSafe
+final class SharedJSONCodecFactory extends JSONCodecFactory {
+    // Weak keys to retire the entry when SchemaContext goes away and to force identity-based lookup
+    private static final LoadingCache<SchemaContext, SharedJSONCodecFactory> CACHE = CacheBuilder.newBuilder()
+            .weakKeys().build(new CacheLoader<SchemaContext, SharedJSONCodecFactory>() {
+                @Override
+                public SharedJSONCodecFactory load(final SchemaContext key) {
+                    return new SharedJSONCodecFactory(key);
+                }
+            });
+
+    // Weak keys to force identity lookup
+    // Soft values to keep unreferenced codecs around for a bit, but eventually we want them to go away
+    private final LoadingCache<TypedSchemaNode, JSONCodec<?>> codecs = CacheBuilder.newBuilder().weakKeys().softValues()
+            .build(new CacheLoader<TypedSchemaNode, JSONCodec<?>>() {
+                @Override
+                public JSONCodec<?> load(@Nonnull final TypedSchemaNode key) {
+                    return createCodec(key, key.getType());
+                }
+            });
+
+    SharedJSONCodecFactory(final SchemaContext context) {
+        super(context);
+    }
+
+    static SharedJSONCodecFactory getIfPresent(final SchemaContext context) {
+        return CACHE.getIfPresent(context);
+    }
+
+    static SharedJSONCodecFactory get(final SchemaContext context) {
+        return CACHE.getUnchecked(context);
+    }
+
+    @Override
+    JSONCodec<?> codecFor(final TypedSchemaNode schema) {
+        return codecs.getUnchecked(schema);
+    }
+}
