952f203317ed07699f0d4bd6df91f6bb9c545a27
Peter Kajsa <pkajsa@cisco.com>
2017-03-23 17:12:33 +0100

    (WIP) Bug 7038 - Rework 'type decimal64' lookup
    Fix of hack in SourceSpecificContext, which attempts to lookup specific
    complex types by considering only a QName's localName and routes
    it via BUILTIN_TYPE_SUPPORTS to specific statement support.
    
    In progress ...
    
    Change-Id: I602c0a919107a633f594a4393299f3f6623bd1c5
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug7038Test.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+public class Bug7038Test {
+    @Test
+    public void test() throws Exception {
+        final SchemaContext context = StmtTestUtils.parseYangSources("/bugs/bug7038");
+        assertNotNull(context);
+        assertEquals(1, context.getUnknownSchemaNodes().size());
+    }
+}
