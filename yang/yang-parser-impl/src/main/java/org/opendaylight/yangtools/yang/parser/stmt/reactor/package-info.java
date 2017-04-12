4fd48320a17781bbe573b14c854b943222513dd3
Peter Kajsa <pkajsa@cisco.com>
2017-04-12 10:27:57 +0200

    Bug 7050 - Document parser-impl package structure
    Added initial javadoc to yang statement parser packages.
    
    Change-Id: I2f18eb4ad01fee297f52953f2ef2aae24a69d253
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/package-info.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Contains implementation of parser reactor {link @CrossSourceStatementReactor}.
+ * Classes provided by this package are responsible for performing of essential
+ * parsing processes and building of schema context {@link SchemaContext} as result
+ * of parsing process.
+ *
+ * <p>
+ * This package also contains:
+ * <ul>
+ * <li> implementation of statement context which provides
+ * information necessary to creation of declared and effective statement </li>
+ * <li> custom statement parser builder {@link CustomStatementParserBuilder} which
+ * provides methods and implementation useful for building of custom statement
+ * parser</li>
+ * <li> entry point to parsing process provided by cross source statement reactor
+ * {link @CrossSourceStatementReactor}</li>
+ * </p>
+ */
+package org.opendaylight.yangtools.yang.parser.stmt.reactor;
+
