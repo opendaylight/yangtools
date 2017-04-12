a847a95135fdff660677a9a4b6cdc34ec2df9880
Peter Kajsa <pkajsa@cisco.com>
2017-04-12 10:27:57 +0200

    Bug 7050 - Document parser-impl package structure
    Added initial javadoc to yang statement parser packages.
    
    Change-Id: I2f18eb4ad01fee297f52953f2ef2aae24a69d253
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/spi/validation/package-info.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+/**
+ * Provides interfaces and enumerations necessary for validation processes.
+ * Currently, this package contains an interface which represents a namespace used
+ * by the validation process. This namespace contains different types of validation
+ * bundles or collections. Allowed types of validation bundles are defined by the
+ * enumeration {@link ValidationBundlesNamespace.ValidationBundleType} which is
+ * provided by this package.
+ */
+package org.opendaylight.yangtools.yang.parser.spi.validation;
