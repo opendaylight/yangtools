6999938e626196e590ec97bd4fbcdab4e07f0593
Peter Kajsa <pkajsa@cisco.com>
2017-05-25 15:42:57 +0200

    (WIP) Bug 2444 - in progress
    - yin export of action, anydata and modifier statements
    - must statement in notification, input and output
    - description and reference in import statements
    - notification statements in data nodes
    - unique statement in list statements
    - optional of organisation and contact
    - require-instance in leafref specification
    - length statement in binary specification
    
    in progress:
    - if-feautre statements
    - revision statement (desc and ref)
    - when statement (desc and ref)
    - units statement in leaflist staement
    - include statement
    - submodules
    
    Change-Id: I1e1662716caaf8fe118bb7db55148bc13ee81951
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-model-export/src/test/java/org/opendaylight/yangtools/yang/model/export/test/Bug2444Test.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.export.test;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.export.YinExportUtils;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+public class Bug2444Test {
+    @Test
+    public void test() throws Exception {
+        final SchemaContext schema = YangParserTestUtils.parseYangSources("/bugs/bug2444");
+        assertNotNull(schema);
+        final File outDir = new File("target/bug2444-export");
+        outDir.mkdirs();
+        for (final Module module : schema.getModules()) {
+            exportModule(schema, module, outDir);
+        }
+    }
+
+    private static File exportModule(final SchemaContext schemaContext, final Module module, final File outDir)
+            throws Exception {
+        final File outFile = new File(outDir, YinExportUtils.wellFormedYinName(module.getName(), module.getRevision()));
+        try (OutputStream output = new FileOutputStream(outFile)) {
+            YinExportUtils.writeModuleToOutputStream(schemaContext, module, output);
+        }
+        return outFile;
+    }
+}
