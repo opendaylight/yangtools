3e63bf3f2ad9fa4cfea7d4f97519018348b5542f
Peter Kajsa <pkajsa@cisco.com>
2017-05-25 15:42:57 +0200

    Bug 2444 - fix of several issues in yang-model-export
    Add Yin export of:
    - action, anydata and modifier statements
    - must statement in notification, input and output
    - description and reference in import statements
    - notification statements in data nodes
    - unique statement in list statements
    - optional of organisation and contact statements
    - require-instance statement in leafref specification
    - length statement in binary specification
    - multiple base identities in identity-ref type
      and identity definition
    - default values in leaf-list statement
    
    Change-Id: I1e1662716caaf8fe118bb7db55148bc13ee81951
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-model-export/src/test/java/org/opendaylight/yangtools/yang/model/export/test/Bug2444Test.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.export.test;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import org.custommonkey.xmlunit.Diff;
+import org.custommonkey.xmlunit.ElementNameAndAttributeQualifier;
+import org.custommonkey.xmlunit.XMLAssert;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.SimpleDateFormatUtil;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.export.YinExportUtils;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class Bug2444Test {
+    @Test
+    public void test() throws Exception {
+        final SchemaContext schema = YangParserTestUtils.parseYangSources("/bugs/bug2444/yang");
+        assertNotNull(schema);
+
+        final File outDir = new File("target/bug2444-export");
+        outDir.mkdirs();
+
+        for (final Module module : schema.getModules()) {
+            exportModule(schema, module, outDir);
+
+            final OutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            final BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(byteArrayOutputStream);
+            try {
+                YinExportUtils.writeModuleToOutputStream(schema, module, bufferedOutputStream);
+                final String output = byteArrayOutputStream.toString();
+                assertNotNull(output);
+                assertNotEquals(0, output.length());
+
+                final Document doc = YinExportTestUtils.loadDocument(String.format("/bugs/bug2444/yin/%s@%s.yin",
+                        module.getName(), SimpleDateFormatUtil.getRevisionFormat().format(module.getRevision())));
+                assertXMLEquals(doc, output);
+            } finally {
+                byteArrayOutputStream.close();
+                bufferedOutputStream.close();
+            }
+        }
+    }
+
+    private static void assertXMLEquals(final Document expectedXMLDoc, final String output)
+            throws SAXException, IOException {
+        final String expected = YinExportTestUtils.toString(expectedXMLDoc.getDocumentElement());
+
+        XMLUnit.setIgnoreWhitespace(true);
+        XMLUnit.setNormalize(true);
+        XMLUnit.setNormalizeWhitespace(true);
+
+        final Diff diff = new Diff(expected, output);
+        diff.overrideElementQualifier(new ElementNameAndAttributeQualifier());
+        XMLAssert.assertXMLEqual(diff, true);
+    }
+
+    private static File exportModule(final SchemaContext schemaContext, final Module module, final File outDir)
+            throws Exception {
+        final File outFile = new File(outDir, YinExportUtils.wellFormedYinName(module.getName(), module.getRevision()));
+        try (OutputStream output = new FileOutputStream(outFile)) {
+            YinExportUtils.writeModuleToOutputStream(schemaContext, module, output);
+        }
+        return outFile;
+    }
+}
---
+++ yang/yang-model-export/src/test/java/org/opendaylight/yangtools/yang/model/export/test/YinExportTestUtils.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.export.test;
+
+import com.google.common.base.Preconditions;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.opendaylight.yangtools.util.xml.UntrustedXML;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+public class YinExportTestUtils {
+
+    private YinExportTestUtils() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    static Document loadDocument(final String xmlPath) throws IOException, SAXException {
+        final InputStream resourceAsStream = SchemaContextEmitterTest.class.getResourceAsStream(xmlPath);
+        final Document currentConfigElement = readXmlToDocument(resourceAsStream);
+        Preconditions.checkNotNull(currentConfigElement);
+        return currentConfigElement;
+    }
+
+    static Document readXmlToDocument(final InputStream xmlContent) throws IOException, SAXException {
+        final Document doc = UntrustedXML.newDocumentBuilder().parse(xmlContent);
+        doc.getDocumentElement().normalize();
+        return doc;
+    }
+
+    static String toString(final Node xml) {
+        try {
+            final Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+            final StreamResult result = new StreamResult(new StringWriter());
+            final DOMSource source = new DOMSource(xml);
+            transformer.transform(source, result);
+
+            return result.getWriter().toString();
+        } catch (IllegalArgumentException | TransformerFactoryConfigurationError | TransformerException e) {
+            throw new RuntimeException("Unable to serialize xml element " + xml, e);
+        }
+    }
+}
