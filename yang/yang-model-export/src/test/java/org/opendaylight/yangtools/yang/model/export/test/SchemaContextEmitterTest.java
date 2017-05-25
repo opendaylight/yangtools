c70e4bd885c5ae2c12964c69e11981a5600fe389
Peter Kajsa <pkajsa@cisco.com>
2017-05-25 15:42:57 +0200

    Bug 2444 - fix of several issues in yang-model-export
    Add Yin export of:
    - action, anydata and modifier statements
    - must statement in notification, input and output
    - description and reference in import statements
    - notification statements in data nodes
    - unique statement in list statements
    - optional of organisation and contact statements
    - require-instance statement in leafref specification
    - length statement in binary specification
    - multiple base identities in identity-ref type
      and identity definition
    - default values in leaf-list statement
    
    Change-Id: I1e1662716caaf8fe118bb7db55148bc13ee81951
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

--- yang/yang-model-export/src/test/java/org/opendaylight/yangtools/yang/model/export/test/SchemaContextEmitterTest.java
+++ yang/yang-model-export/src/test/java/org/opendaylight/yangtools/yang/model/export/test/SchemaContextEmitterTest.java
@@ -12,35 +12,23 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
-import com.google.common.base.Preconditions;
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.StringWriter;
 import java.net.URISyntaxException;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.TransformerFactoryConfigurationError;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 import org.custommonkey.xmlunit.Diff;
 import org.custommonkey.xmlunit.ElementNameAndAttributeQualifier;
 import org.custommonkey.xmlunit.XMLAssert;
 import org.custommonkey.xmlunit.XMLUnit;
 import org.junit.Test;
-import org.opendaylight.yangtools.util.xml.UntrustedXML;
 import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.export.YinExportUtils;
 import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
 import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 public class SchemaContextEmitterTest {
@@ -55,7 +43,7 @@ public class SchemaContextEmitterTest {
         final OutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         final BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(byteArrayOutputStream);
 
-        for (Module module : schemaContext.getModules()) {
+        for (final Module module : schemaContext.getModules()) {
             YinExportUtils.writeModuleToOutputStream(schemaContext, module, bufferedOutputStream);
         }
 
@@ -63,8 +51,8 @@ public class SchemaContextEmitterTest {
         assertNotNull(output);
         assertNotEquals(0, output.length());
 
-        final Document doc = loadDocument("/schema-context-emitter-test/foo.yin");
-        final String expected = toString(doc.getDocumentElement());
+        final Document doc = YinExportTestUtils.loadDocument("/schema-context-emitter-test/foo.yin");
+        final String expected = YinExportTestUtils.toString(doc.getDocumentElement());
 
         XMLUnit.setIgnoreWhitespace(true);
         XMLUnit.setNormalize(true);
@@ -72,34 +60,5 @@ public class SchemaContextEmitterTest {
         final Diff diff = new Diff(expected, output);
         diff.overrideElementQualifier(new ElementNameAndAttributeQualifier());
         XMLAssert.assertXMLEqual(diff, true);
-    }
-
-    private static Document loadDocument(final String xmlPath) throws IOException, SAXException {
-        final InputStream resourceAsStream = SchemaContextEmitterTest.class.getResourceAsStream(xmlPath);
-        final Document currentConfigElement = readXmlToDocument(resourceAsStream);
-        Preconditions.checkNotNull(currentConfigElement);
-        return currentConfigElement;
-    }
-
-    private static Document readXmlToDocument(final InputStream xmlContent) throws IOException, SAXException {
-        final Document doc = UntrustedXML.newDocumentBuilder().parse(xmlContent);
-        doc.getDocumentElement().normalize();
-        return doc;
-    }
-
-    private static String toString(final Node xml) {
-        try {
-            final Transformer transformer = TransformerFactory.newInstance().newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-
-            final StreamResult result = new StreamResult(new StringWriter());
-            final DOMSource source = new DOMSource(xml);
-            transformer.transform(source, result);
-
-            return result.getWriter().toString();
-        } catch (IllegalArgumentException | TransformerFactoryConfigurationError | TransformerException e) {
-            throw new RuntimeException("Unable to serialize xml element " + xml, e);
-        }
     }
 }
