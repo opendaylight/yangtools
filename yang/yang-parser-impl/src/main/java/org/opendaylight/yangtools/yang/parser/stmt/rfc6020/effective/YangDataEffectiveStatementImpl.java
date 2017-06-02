2200a3d8bc6e15745c8e38e1787a13621d17ef07
Igor Foltin <igor.foltin@pantheon.tech>
2017-06-02 14:41:49 +0200

    Bug 8523: Add support for parsing restconf:yang-data extension
    Add support for yang-data extension to the YANG statement parser.
    This extension is defined in RFC8040:
    https://tools.ietf.org/html/rfc8040#section-8
    
    Config and if-feature statements are ignored
    when placed within a yang-data extension body.
    
    Change-Id: Iccc1ffc76cf1ba1032552e68a4591d44f586c889
    Signed-off-by: Igor Foltin <igor.foltin@pantheon.tech>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/YangDataEffectiveStatementImpl.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.api.YangDataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.stmt.UnknownStatement;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.Utils;
+
+/**
+ * Effective statement representation of 'yang-data' extension defined in https://tools.ietf.org/html/rfc8040#section-8
+ */
+@Beta
+public final class YangDataEffectiveStatementImpl extends UnknownEffectiveStatementBase<String>
+        implements YangDataSchemaNode {
+
+    private final SchemaPath path;
+    private final QName maybeQNameArgument;
+    private final ContainerSchemaNode containerSchemaNode;
+
+    public YangDataEffectiveStatementImpl(final StmtContext<String, UnknownStatement<String>, ?> ctx) {
+        super(ctx);
+
+        QName maybeQNameArgumentInit;
+        try {
+            maybeQNameArgumentInit = Utils.qNameFromArgument(ctx, argument());
+        } catch (IllegalArgumentException e) {
+            maybeQNameArgumentInit = getNodeType();
+        }
+        this.maybeQNameArgument = maybeQNameArgumentInit;
+
+        path = ctx.getParentContext().getSchemaPath().get().createChild(maybeQNameArgument);
+        containerSchemaNode = Preconditions.checkNotNull(firstEffective(ContainerEffectiveStatementImpl.class));
+    }
+
+    @Nonnull
+    @Override
+    public QName getQName() {
+        return maybeQNameArgument;
+    }
+
+    @Nonnull
+    @Override
+    public SchemaPath getPath() {
+        return path;
+    }
+
+    @Nonnull
+    @Override
+    public ContainerSchemaNode getContainer() {
+        return containerSchemaNode;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(maybeQNameArgument, path);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof YangDataEffectiveStatementImpl)) {
+            return false;
+        }
+
+        final YangDataEffectiveStatementImpl other = (YangDataEffectiveStatementImpl) obj;
+        return Objects.equals(maybeQNameArgument, other.maybeQNameArgument) && Objects.equals(path, other.path);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this).add("qname", maybeQNameArgument).add("path", path).toString();
+    }
+}
