dcea871e8edbbb4bdf5f9a00f234b2b25af7d827
Robert Varga <robert.varga@pantheon.tech>
2017-06-12 18:42:51 +0200

    BUG-7052: extract SimpleSchemaContext
    Some parts of EffectiveSchemaContext are not dependent on parser
    proper and just fill out the blanks left by AbstractSchemaContext.
    
    Extract these bits into SimpleSchemaContext for wider reuse.
    
    Change-Id: I72238ec67b958e3e7918e399e9643264b11860db
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/EffectiveSchemaContext.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/EffectiveSchemaContext.java
@@ -10,116 +10,47 @@ package org.opendaylight.yangtools.yang.parser.stmt.reactor;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Verify;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.ImmutableSetMultimap;
-import com.google.common.collect.Multimaps;
-import com.google.common.collect.SetMultimap;
-import java.net.URI;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
-import java.util.TreeMap;
 import org.opendaylight.yangtools.yang.model.api.Module;
-import org.opendaylight.yangtools.yang.model.api.ModuleIdentifier;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.ModuleStatement;
-import org.opendaylight.yangtools.yang.model.util.AbstractSchemaContext;
-import org.opendaylight.yangtools.yang.model.util.ModuleDependencySort;
-import org.opendaylight.yangtools.yang.model.util.ModuleIdentifierImpl;
+import org.opendaylight.yangtools.yang.model.util.SimpleSchemaContext;
 
 @VisibleForTesting
-public final class EffectiveSchemaContext extends AbstractSchemaContext {
-
-    private final SetMultimap<URI, Module> namespaceToModules;
-    private final SetMultimap<String, Module> nameToModules;
-    private final Set<Module> modules;
-
+public final class EffectiveSchemaContext extends SimpleSchemaContext {
     private final List<DeclaredStatement<?>> rootDeclaredStatements;
     private final List<EffectiveStatement<?, ?>> rootEffectiveStatements;
-    private final Set<ModuleIdentifier> moduleIdentifiers;
 
-    EffectiveSchemaContext(final List<DeclaredStatement<?>> rootDeclaredStatements,
+    private EffectiveSchemaContext(final Set<Module> modules, final List<DeclaredStatement<?>> rootDeclaredStatements,
             final List<EffectiveStatement<?, ?>> rootEffectiveStatements) {
+        super(modules);
         this.rootDeclaredStatements = ImmutableList.copyOf(rootDeclaredStatements);
         this.rootEffectiveStatements = ImmutableList.copyOf(rootEffectiveStatements);
+    }
 
-        final Set<Module> modulesInit = new HashSet<>();
+    static EffectiveSchemaContext create(final List<DeclaredStatement<?>> rootDeclaredStatements,
+            final List<EffectiveStatement<?, ?>> rootEffectiveStatements) {
+        final Set<Module> modules = new HashSet<>();
         for (EffectiveStatement<?, ?> stmt : rootEffectiveStatements) {
             if (stmt.getDeclared() instanceof ModuleStatement) {
                 Verify.verify(stmt instanceof Module);
-                modulesInit.add((Module) stmt);
+                modules.add((Module) stmt);
             }
         }
-        this.modules = ImmutableSet.copyOf(ModuleDependencySort.sort(modulesInit));
 
-        final SetMultimap<URI, Module> nsMap = Multimaps.newSetMultimap(new TreeMap<>(), MODULE_SET_SUPPLIER);
-        final SetMultimap<String, Module> nameMap = Multimaps.newSetMultimap(new TreeMap<>(), MODULE_SET_SUPPLIER);
-        final Set<ModuleIdentifier> modIdBuilder = new HashSet<>();
-        for (Module m : modulesInit) {
-            nameMap.put(m.getName(), m);
-            nsMap.put(m.getNamespace(), m);
-            modIdBuilder.add(ModuleIdentifierImpl.create(m.getName(), Optional.of(m.getNamespace()),
-                Optional.of(m.getRevision())));
-            resolveSubmoduleIdentifiers(m.getSubmodules(), modIdBuilder);
-        }
-
-        namespaceToModules = ImmutableSetMultimap.copyOf(nsMap);
-        nameToModules = ImmutableSetMultimap.copyOf(nameMap);
-        moduleIdentifiers = ImmutableSet.copyOf(modIdBuilder);
+        return new EffectiveSchemaContext(modules, rootDeclaredStatements, rootEffectiveStatements);
     }
 
-    private EffectiveSchemaContext(final Set<Module> modules) {
-
-         /*
-         * Instead of doing this on each invocation of getModules(), pre-compute
-         * it once and keep it around -- better than the set we got in.
-         */
-        this.modules = ImmutableSet.copyOf(ModuleDependencySort.sort(modules));
-
-         /*
-         * The most common lookup is from Namespace->Module.
-         *
-         * RESTCONF performs lookups based on module name only, where it wants
-         * to receive the latest revision
-         *
-         * Invest some quality time in building up lookup tables for both.
-         */
-        final SetMultimap<URI, Module> nsMap = Multimaps.newSetMultimap(
-                new TreeMap<>(), MODULE_SET_SUPPLIER);
-        final SetMultimap<String, Module> nameMap = Multimaps.newSetMultimap(
-                new TreeMap<>(), MODULE_SET_SUPPLIER);
-
-        Set<ModuleIdentifier> modIdBuilder = new HashSet<>();
-        for (Module m : modules) {
-            nameMap.put(m.getName(), m);
-            nsMap.put(m.getNamespace(), m);
-            modIdBuilder.add(ModuleIdentifierImpl.create(m.getName(), Optional.of(m.getNamespace()),
-                Optional.of(m.getRevision())));
-            resolveSubmoduleIdentifiers(m.getSubmodules(), modIdBuilder);
-        }
-
-        namespaceToModules = ImmutableSetMultimap.copyOf(nsMap);
-        nameToModules = ImmutableSetMultimap.copyOf(nameMap);
-        moduleIdentifiers = ImmutableSet.copyOf(modIdBuilder);
-
-        rootDeclaredStatements = ImmutableList.of();
-        rootEffectiveStatements = ImmutableList.of();
-    }
-
+    /**
+     * @deprecated Use {@link SimpleSchemaContext#forModules(Set)} instead.
+     */
+    @Deprecated
     public static SchemaContext resolveSchemaContext(final Set<Module> modules) {
-       return new EffectiveSchemaContext(modules);
-    }
-
-    private static void resolveSubmoduleIdentifiers(final Set<Module> submodules, final Set<ModuleIdentifier> modIdBuilder) {
-        for (Module submodule : submodules) {
-            modIdBuilder.add(ModuleIdentifierImpl.create(submodule.getName(),
-                Optional.of(submodule.getNamespace()), Optional.of(submodule.getRevision())));
-        }
+       return SimpleSchemaContext.forModules(modules);
     }
 
     @VisibleForTesting
@@ -130,35 +61,5 @@ public final class EffectiveSchemaContext extends AbstractSchemaContext {
     @VisibleForTesting
     public List<EffectiveStatement<?, ?>> getRootEffectiveStatements() {
         return rootEffectiveStatements;
-    }
-
-    @Override
-    protected Map<ModuleIdentifier, String> getIdentifiersToSources() {
-        return ImmutableMap.of();
-    }
-
-    @Override
-    public Set<Module> getModules() {
-        return modules;
-    }
-
-    @Override
-    protected SetMultimap<URI, Module> getNamespaceToModules() {
-        return namespaceToModules;
-    }
-
-    @Override
-    protected SetMultimap<String, Module> getNameToModules() {
-        return nameToModules;
-    }
-
-    @Override
-    public Set<ModuleIdentifier> getAllModuleIdentifiers() {
-        return moduleIdentifiers;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("EffectiveSchemaContext{modules=%s}", modules);
     }
 }
