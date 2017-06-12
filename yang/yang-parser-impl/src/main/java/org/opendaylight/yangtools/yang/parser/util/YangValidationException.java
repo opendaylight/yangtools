6b20951e04d8de5eb141aad8d0c06d52590de9bd
Robert Varga <robert.varga@pantheon.tech>
2017-06-12 18:05:10 +0200

    BUG-7052: Move ModuleDependencySort to yang-model-util
    This is a useful utility which has no dependencies on parser proper,
    hence should be part of yang-model-util.
    
    Change-Id: I8c584d61e57ec513261e59f0fa306f50747b7b5a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/util/YangValidationException.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/util/YangValidationException.java
@@ -9,17 +9,21 @@ package org.opendaylight.yangtools.yang.parser.util;
 
 /**
  * Unchecked exception thrown if yang definition is not valid according to
- * {YangModelBasicValidationListener}
+ * {YangModelBasicValidationListener}.
+ *
+ * @deprecated This exception is not used anywhere except {@link ModuleDependencySort} and is scheduled for removal
+ *             along with that class.
  */
+@Deprecated
 public final class YangValidationException extends RuntimeException {
 
     private static final long serialVersionUID = 7414330400390825381L;
 
-    public YangValidationException(String message, Throwable cause) {
+    public YangValidationException(final String message, final Throwable cause) {
         super(message, cause);
     }
 
-    public YangValidationException(String message) {
+    public YangValidationException(final String message) {
         super(message);
     }
 
