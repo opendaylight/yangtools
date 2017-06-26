1a79a27329b0875c81ea52edc21a5ac1c768d1bb
Peter Kajsa <pkajsa@cisco.com>
2017-06-26 11:34:49 +0200

    Bug 2444 - Yin export of all YANG statements
    Currently yang-export exports effective world of yang into yin,
    what is not correct, because lot of information is not accesible
    in effective world anymore.
    So the correct solution is to export declared world of yang,
    where all information is accessible and in raw form and that is
    exactly what we need.
    Export of effective world is still retained for cases when
    we don't have access to declared form of supplied module or
    we want to emit also instantiated statements (e.g. statements added
    by uses or augment)
    
    Change-Id: Ic2bc559623d9e72c59e2ed7f6a797cd67933ada6
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

--- yang/yang-model-export/src/main/java/org/opendaylight/yangtools/yang/model/export/YinExportUtils.java
+++ yang/yang-model-export/src/main/java/org/opendaylight/yangtools/yang/model/export/YinExportUtils.java
@@ -24,18 +24,18 @@ import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 
 public final class YinExportUtils {
 
-
     private YinExportUtils() {
         throw new UnsupportedOperationException("Utility class");
     }
-
 
     /**
      *
      * Returns well-formed file name of YIN file as defined in RFC6020.
      *
-     * @param name Module or submodule name
-     * @param revision Revision of module or submodule
+     * @param name
+     *            Module or submodule name
+     * @param revision
+     *            Revision of module or submodule
      * @return well-formed file name of YIN file as defined in RFC6020.
      */
     public static String wellFormedYinName(final String name, final Date revision) {
@@ -53,7 +53,7 @@ public final class YinExportUtils {
      * @return well-formed file name of YIN file as defined in RFC6020.
      */
     public static String wellFormedYinName(final String name, final String revision) {
-        return String.format("%s@%s.yin", Preconditions.checkNotNull(name),Preconditions.checkNotNull(revision));
+        return String.format("%s@%s.yin", Preconditions.checkNotNull(name), Preconditions.checkNotNull(revision));
     }
 
     /**
@@ -69,18 +69,42 @@ public final class YinExportUtils {
      *            written.
      * @throws XMLStreamException
      */
-    public static void writeModuleToOutputStream(final SchemaContext ctx, final Module module, final OutputStream str) throws XMLStreamException {
+    public static void writeModuleToOutputStream(final SchemaContext ctx, final Module module, final OutputStream str)
+            throws XMLStreamException {
+        writeModuleToOutputStream(ctx, module, str, false);
+    }
+
+    /**
+     * Writes YIN representation of supplied module to specified output stream.
+     *
+     * @param ctx
+     *            Schema Context which contains module and extension definitions
+     *            to be used during export of model.
+     * @param module
+     *            Module to be exported.
+     * @param str
+     *            Output stream to which YIN representation of model will be
+     *            written.
+     * @param emitInstantiated
+     *            Option to emit also instantiated statements (e.g. statements
+     *            added by uses or augment)
+     * @throws XMLStreamException
+     */
+    public static void writeModuleToOutputStream(final SchemaContext ctx, final Module module, final OutputStream str,
+            final boolean emitInstantiated) throws XMLStreamException {
         final XMLOutputFactory factory = XMLOutputFactory.newFactory();
         final XMLStreamWriter xmlStreamWriter = factory.createXMLStreamWriter(str);
-        writeModuleToOutputStream(ctx,module, xmlStreamWriter);
+        writeModuleToOutputStream(ctx, module, xmlStreamWriter, emitInstantiated);
         xmlStreamWriter.flush();
     }
 
-    private static void writeModuleToOutputStream(final SchemaContext ctx, final Module module, final XMLStreamWriter xmlStreamWriter) {
+    private static void writeModuleToOutputStream(final SchemaContext ctx, final Module module,
+            final XMLStreamWriter xmlStreamWriter, final boolean emitInstantiated) {
         final URI moduleNs = module.getNamespace();
-        final Map<String, URI> prefixToNs = prefixToNamespace(ctx,module);
-        final StatementTextWriter yinWriter = SingleModuleYinStatementWriter.create(xmlStreamWriter, moduleNs, prefixToNs);
-        SchemaContextEmitter.writeToStatementWriter(module, ctx,yinWriter);
+        final Map<String, URI> prefixToNs = prefixToNamespace(ctx, module);
+        final StatementTextWriter yinWriter = SingleModuleYinStatementWriter.create(xmlStreamWriter, moduleNs,
+                prefixToNs);
+        SchemaContextEmitter.writeToStatementWriter(module, ctx, yinWriter, emitInstantiated);
     }
 
     private static Map<String, URI> prefixToNamespace(final SchemaContext ctx, final Module module) {
@@ -88,7 +112,7 @@ public final class YinExportUtils {
         prefixMap.put(module.getPrefix(), module.getNamespace());
         for (final ModuleImport imp : module.getImports()) {
             final String prefix = imp.getPrefix();
-            final URI namespace = getModuleNamespace(ctx,imp.getModuleName());
+            final URI namespace = getModuleNamespace(ctx, imp.getModuleName());
             prefixMap.put(prefix, namespace);
         }
         return prefixMap;
