6bbc6496b151879e765f586e8b98ba7653bf6b30
Peter Kajsa <pkajsa@cisco.com>
2017-06-26 11:09:27 +0200

    Bug 2444 - add missing API of some declared statements
    Fix of missing API of some declared statements according to
    RFC7950. This API is necessary to proper export of declared statements
    to Yin.
    
    Change-Id: Icc0209656751b00c5cc7401f7b95d8979af0fa0d
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/ActionStatement.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/ActionStatement.java
@@ -22,6 +22,6 @@ import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
  * detailed action information.  The argument is the name of the action.
  */
 @Beta
-public interface ActionStatement extends DeclaredStatement<QName>, OperationGroup, ConditionalFeature {
+public interface ActionStatement extends DeclaredStatement<QName>, OperationGroup {
 
 }
