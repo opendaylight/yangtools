904c8455a45d895bb5621347e05db781df1030a7
Peter Kajsa <pkajsa@cisco.com>
2017-06-26 11:09:27 +0200

    Bug 2444 - add missing API of some declared statements
    Fix of missing API of some declared statements according to
    RFC7950. This API is necessary to proper export of declared statements
    to Yin.
    
    Change-Id: Icc0209656751b00c5cc7401f7b95d8979af0fa0d
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/RefineStatementImpl.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/RefineStatementImpl.java
@@ -13,8 +13,15 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.opendaylight.yangtools.yang.model.api.YangStmtMapping;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ConfigStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.DefaultStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.DescriptionStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.IfFeatureStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.MandatoryStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.MaxElementsStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.MinElementsStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.MustStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.PresenceStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.ReferenceStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.RefineStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
@@ -92,5 +99,47 @@ public class RefineStatementImpl extends AbstractDeclaredStatement<SchemaNodeIde
     @Override
     public ReferenceStatement getReference() {
         return firstDeclared(ReferenceStatement.class);
+    }
+
+    @Nonnull
+    @Override
+    public Collection<? extends MustStatement> getMusts() {
+        return allDeclared(MustStatement.class);
+    }
+
+    @Nonnull
+    @Override
+    public Collection<? extends DefaultStatement> getDefaults() {
+        return allDeclared(DefaultStatement.class);
+    }
+
+    @Nullable
+    @Override
+    public ConfigStatement getConfig() {
+        return firstDeclared(ConfigStatement.class);
+    }
+
+    @Nullable
+    @Override
+    public PresenceStatement getPresence() {
+        return firstDeclared(PresenceStatement.class);
+    }
+
+    @Nullable
+    @Override
+    public MandatoryStatement getMandatory() {
+        return firstDeclared(MandatoryStatement.class);
+    }
+
+    @Nullable
+    @Override
+    public MinElementsStatement getMinElements() {
+        return firstDeclared(MinElementsStatement.class);
+    }
+
+    @Nullable
+    @Override
+    public MaxElementsStatement getMaxElements() {
+        return firstDeclared(MaxElementsStatement.class);
     }
 }
