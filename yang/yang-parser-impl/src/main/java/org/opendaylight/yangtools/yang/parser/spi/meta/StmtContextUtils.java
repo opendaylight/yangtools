6126288b6562c8460d3780fb3a8e0b1fb45a8ea8
Peter Kajsa <pkajsa@cisco.com>
2017-07-06 10:05:01 +0200

    Bug 7051 - Refactoring of StmtContextUtils
    Refactoring of StmtContextUtils in order to remove undesired dependencies
    on stmt.rfc6020 package.
    
    Change-Id: I7ac36a021705dc65d8b01a784683904176aa3f21
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/spi/meta/StmtContextUtils.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/spi/meta/StmtContextUtils.java
@@ -19,6 +19,8 @@ import java.util.Date;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.common.SimpleDateFormatUtil;
@@ -37,6 +39,7 @@ import org.opendaylight.yangtools.yang.model.api.stmt.PresenceStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.RevisionStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
 import org.opendaylight.yangtools.yang.model.api.stmt.SubmoduleStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.UnknownStatement;
 import org.opendaylight.yangtools.yang.model.repo.api.RevisionSourceIdentifier;
 import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
 import org.opendaylight.yangtools.yang.parser.spi.source.BelongsToPrefixToModuleName;
@@ -45,11 +48,13 @@ import org.opendaylight.yangtools.yang.parser.spi.source.ModuleCtxToModuleQName;
 import org.opendaylight.yangtools.yang.parser.spi.source.ModuleIdentifierToModuleQName;
 import org.opendaylight.yangtools.yang.parser.spi.source.ModuleNameToModuleQName;
 import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
-import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.UnknownStatementImpl;
-import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.YangDataStatementImpl;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.SupportedExtensionsMapping;
 
 public final class StmtContextUtils {
     public static final Splitter LIST_KEY_SPLITTER = Splitter.on(' ').omitEmptyStrings().trimResults();
+    private static final Set<Class<? extends DeclaredStatement<?>>> SUPPORTED_EXTENSIONS = Stream
+            .of(SupportedExtensionsMapping.values()).map(e -> e.getDeclaredRepresentationClass())
+            .collect(Collectors.toSet());
 
     private StmtContextUtils() {
         throw new UnsupportedOperationException("Utility class");
@@ -210,7 +215,7 @@ public final class StmtContextUtils {
         StmtContext<?, ?, ?> current = stmtCtx;
         while (current.getParentContext().getParentContext() != null) {
             current = current.getParentContext();
-            if (producesDeclared(current, UnknownStatementImpl.class)) {
+            if (isUnknownStatement(current)) {
                 return true;
             }
         }
@@ -218,18 +223,11 @@ public final class StmtContextUtils {
         return false;
     }
 
-    /**
-     * Checks if the statement context has a 'yang-data' extension node as its parent.
-     *
-     * @param stmtCtx statement context to be checked
-     * @return true if the parent node is a 'yang-data' node, otherwise false
-     */
-    public static boolean hasYangDataExtensionParent(final StmtContext<?, ?, ?> stmtCtx) {
-        return producesDeclared(stmtCtx.getParentContext(), YangDataStatementImpl.class);
-    }
-
     public static boolean isUnknownStatement(final StmtContext<?, ?, ?> stmtCtx) {
-        return producesDeclared(stmtCtx, UnknownStatementImpl.class);
+        final Class<? extends DeclaredStatement<?>> declaredRepresentationClass = stmtCtx.getPublicDefinition()
+                .getDeclaredRepresentationClass();
+        return stmtCtx != null && UnknownStatement.class.isAssignableFrom(declaredRepresentationClass)
+                && !SUPPORTED_EXTENSIONS.contains(declaredRepresentationClass);
     }
 
     public static Collection<SchemaNodeIdentifier> replaceModuleQNameForKey(
@@ -507,9 +505,7 @@ public final class StmtContextUtils {
             localName = namesParts[1];
             qNameModule = StmtContextUtils.getModuleQNameByPrefix(ctx, prefix);
             // in case of unknown statement argument, we're not going to parse it
-            if (qNameModule == null
-                    && ctx.getPublicDefinition().getDeclaredRepresentationClass()
-                    .isAssignableFrom(UnknownStatementImpl.class)) {
+            if (qNameModule == null && isUnknownStatement(ctx)) {
                 localName = value;
                 qNameModule = StmtContextUtils.getRootModuleQName(ctx);
             }
@@ -601,10 +597,5 @@ public final class StmtContextUtils {
             }
         }
         return revision;
-    }
-
-    public static boolean isUnknownNode(final StmtContext<?, ?, ?> stmtCtx) {
-        return stmtCtx != null && stmtCtx.getPublicDefinition().getDeclaredRepresentationClass()
-                .isAssignableFrom(UnknownStatementImpl.class);
     }
 }
