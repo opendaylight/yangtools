42a2423076219b0b00858343cda2848a5de6ce52
Igor Foltin <igor.foltin@pantheon.tech>
2017-07-25 17:18:39 +0200

    Bug 8803: Add Unit test
    Add a unit test for investigation purposes.
    
    Change-Id: Ib8af7d03eb59b004f2fcb4d1806c0f4f256a974a
    Signed-off-by: Igor Foltin <igor.foltin@pantheon.tech>

---
+++ yang/yang-data-codec-xml/src/test/java/org/opendaylight/yangtools/yang/data/codec/xml/Bug8803Test.java
@@ -0,0 +1,72 @@
+package org.opendaylight.yangtools.yang.data.codec.xml;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeWriter;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.impl.schema.NormalizedNodeResult;
+import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.SchemaNode;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.util.SchemaContextUtil;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+public class Bug8803Test {
+
+    @Ignore("Beware, this test creates an infinite loop.")
+    @Test
+    public void test() throws Exception {
+        final SchemaContext schemaContext = YangParserTestUtils.parseYangSources("/bug8803");
+        final SchemaPath topContPath = SchemaPath.create(true, QName.create("foo-ns", "1970-01-01", "top-cont"));
+        final SchemaNode dataSchemaNode = SchemaContextUtil.findDataSchemaNode(schemaContext, topContPath);
+        assertTrue(dataSchemaNode instanceof ContainerSchemaNode);
+        final ContainerSchemaNode topContSchema = (ContainerSchemaNode) dataSchemaNode;
+
+        final InputStream resourceAsStream = Bug8803Test.class.getResourceAsStream("/bug8803/foo.xml");
+
+        // deserialization
+        final XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+        final XMLStreamReader reader = inputFactory.createXMLStreamReader(resourceAsStream);
+
+        final NormalizedNodeResult result = new NormalizedNodeResult();
+        final NormalizedNodeStreamWriter streamWriter = ImmutableNormalizedNodeStreamWriter.from(result);
+        final XmlParserStream xmlParser = XmlParserStream.create(streamWriter, schemaContext, topContSchema);
+        xmlParser.parse(reader);
+        final NormalizedNode<?, ?> transformedInput = result.getResult();
+        assertNotNull(transformedInput);
+
+        // serialization
+        final OutputStream outputStream = new ByteArrayOutputStream();
+        final XMLOutputFactory outputFactory = XMLOutputFactory.newFactory();
+        // switching NS repairing to false does not help
+        outputFactory.setProperty(XMLOutputFactory.IS_REPAIRING_NAMESPACES, false);
+        final XMLStreamWriter xmlStreamWriter = outputFactory.createXMLStreamWriter(outputStream,
+                StandardCharsets.UTF_8.name());
+
+        final NormalizedNodeStreamWriter xmlNormalizedNodeStreamWriter = XMLStreamNormalizedNodeStreamWriter.create(
+                xmlStreamWriter, schemaContext);
+
+        final NormalizedNodeWriter normalizedNodeWriter = NormalizedNodeWriter.forStreamWriter(
+                xmlNormalizedNodeStreamWriter);
+        normalizedNodeWriter.write(transformedInput);
+
+        final String serializedXml = outputStream.toString();
+        assertFalse(serializedXml.isEmpty());
+    }
+}
