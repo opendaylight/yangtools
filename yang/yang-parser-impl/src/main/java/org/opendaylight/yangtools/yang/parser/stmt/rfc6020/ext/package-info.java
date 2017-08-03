26cddc438dca2492303c6d5af7b0656017e5550b
Peter Kajsa <pkajsa@cisco.com>
2017-08-03 13:20:43 +0200

    Bug 7051 - moving of CustomStatementParserBuilder
    Moving of CustomStatementParserBuilder to new package rfc6020.ext
    in order to eliminate undesired dependency of
    org.opendaylight.yangtools.yang.parser.stmt.reactor
    on org.opendaylight.yangtools.yang.parser.stmt.rfc6020
    
    Change-Id: I255f2d568d6db915332884246a7b8ddda9e23431
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/ext/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Provides builders and classes necessary for customization of yang stmt parser
+ * (e.g. add support for an third-party extension etc.).
+ * {@link CustomStatementParserBuilder} is designed for building a custom yang
+ * statement parser with extended support for a custom statement or yang
+ * extension.
+ */
+package org.opendaylight.yangtools.yang.parser.stmt.rfc6020.ext;
