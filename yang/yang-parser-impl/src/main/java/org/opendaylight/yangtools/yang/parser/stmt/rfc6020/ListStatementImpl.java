cc5df2d9a6266d6113316aebd91b58b33186062b
Robert Varga <robert.varga@pantheon.tech>
2017-08-07 03:04:18 +0200

    Refactor argument adaptation
    Adapting arguments is really a StatementSupport-level operation, and
    should not be part of a specific reactor implementation.
    
    This patch creates the appropriate interfaces required to have
    StatementSupport classes a say when arguments are adapted.
    
    Change-Id: Ifa3e25b96d9f451a53a99a30be2573abe7ee744c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/ListStatementImpl.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/ListStatementImpl.java
@@ -32,7 +32,7 @@ import org.opendaylight.yangtools.yang.model.api.stmt.TypedefStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.UniqueStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.WhenStatement;
 import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractDeclaredStatement;
-import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractStatementSupport;
+import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractQNameStatementSupport;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext.Mutable;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContextUtils;
@@ -72,7 +72,7 @@ public class ListStatementImpl extends AbstractDeclaredStatement<QName>
 
     public static class Definition
             extends
-            AbstractStatementSupport<QName, ListStatement, EffectiveStatement<QName, ListStatement>> {
+            AbstractQNameStatementSupport<ListStatement, EffectiveStatement<QName, ListStatement>> {
 
         public Definition() {
             super(YangStmtMapping.LIST);
