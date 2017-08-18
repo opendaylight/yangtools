000956887188dafcc2e395c0606980fa21177372
Peter Kajsa <pkajsa@cisco.com>
2017-08-18 12:24:32 +0200

    (Draft) Bug 9005 - unit test Boron-SR3
    Change-Id: I473d1bf9ac36a7d55fbae0da0737400993deb327
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug9005Test.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+public class Bug9005Test {
+    @Test
+    public void fooTest() throws Exception {
+        for (int i = 0; i < 1000; i++) {
+            final SchemaContext context = StmtTestUtils.parseYangSources("/bugs/bug9005/foo-test");
+            assertNotNull(context);
+        }
+    }
+
+    @Test
+    public void fooTest2() throws Exception {
+        for (int i = 0; i < 1000; i++) {
+            final SchemaContext context = StmtTestUtils.parseYangSources("/bugs/bug9005/foo-test-2");
+            assertNotNull(context);
+        }
+    }
+
+    @Test
+    public void openconfigTest() throws Exception {
+        final SchemaContext context = StmtTestUtils.parseYangSources("/bugs/bug9005/openconfig");
+        assertNotNull(context);
+    }
+}
