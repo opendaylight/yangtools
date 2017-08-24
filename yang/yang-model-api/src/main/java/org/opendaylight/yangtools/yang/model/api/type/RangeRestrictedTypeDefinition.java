8a1f8e887d585152f75f29085c3e2005019c536f
Robert Varga <robert.varga@pantheon.tech>
2017-08-24 16:41:54 +0200

    BUG-8043: introduce RangeRestrictedTypeDefinition
    {Decimal,Integer,UnsignedInteger}TypeDefinition share the same method
    and semantics, capture them in RangeRestrictedTypeDefinition.
    
    Change-Id: I5fa6dbf5efdcc3797abc628a98e034602b8285ad
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/type/RangeRestrictedTypeDefinition.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.api.type;
+
+import java.util.List;
+import javax.annotation.Nonnull;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+
+/**
+ * Marker interface for {@link TypeDefinition}s which can have their values restricted to a set of allowed values.
+ *
+ * @param <T> Concrete {@link TypeDefinition} subinterface
+ */
+public interface RangeRestrictedTypeDefinition<T extends TypeDefinition<T>> extends TypeDefinition<T> {
+    /**
+     * Returns range constraints for instance of this type. These are the effective constraints, e.g. they include
+     * any range constraints imposed by base types.
+     *
+     * @return list of range constraints which are specified as the argument of the <code>range</code> which is
+     *         a substatement of the <code>type</code> statement
+     */
+    @Nonnull List<RangeConstraint> getRangeConstraints();
+}
