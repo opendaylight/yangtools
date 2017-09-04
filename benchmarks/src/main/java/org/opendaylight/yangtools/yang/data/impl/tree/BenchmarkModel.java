c167581de56960c7faaf86baa9e1998a04f39bc8
Robert Varga <robert.varga@pantheon.tech>
2017-09-04 09:51:29 +0200

    Fix benchmark compilation and runtime
    Benchmark has been broken for some time, fix it up.
    
    Change-Id: If2e1f3292a110947a1428a096de20e90591b0f6d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- benchmarks/src/main/java/org/opendaylight/yangtools/yang/data/impl/tree/BenchmarkModel.java
+++ benchmarks/src/main/java/org/opendaylight/yangtools/yang/data/impl/tree/BenchmarkModel.java
@@ -7,49 +7,43 @@
  */
 package org.opendaylight.yangtools.yang.data.impl.tree;
 
-import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
-import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
-
-import org.opendaylight.yangtools.yang.parser.stmt.reactor.CrossSourceStatementReactor;
-import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.YangInferencePipeline;
-import java.io.InputStream;
-import java.util.Collections;
+import java.io.IOException;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.parser.api.YangSyntaxErrorException;
+import org.opendaylight.yangtools.yang.model.repo.api.YangTextSchemaSource;
+import org.opendaylight.yangtools.yang.parser.rfc6020.repo.YangStatementStreamSource;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.stmt.reactor.CrossSourceStatementReactor;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.YangInferencePipeline;
 
 /**
  * @author Lukas Sedlak &lt;lsedlak@cisco.com&gt;
  */
 public class BenchmarkModel {
 
-    public static final QName TEST_QNAME = QName.create("urn:opendaylight:params:xml:ns:yang:controller:md:sal:dom:store:test", "2014-03-13",
-        "test");
+    public static final QName TEST_QNAME =
+            QName.create("urn:opendaylight:params:xml:ns:yang:controller:md:sal:dom:store:test", "2014-03-13", "test");
     public static final QName OUTER_LIST_QNAME = QName.create(TEST_QNAME, "outer-list");
     public static final QName INNER_LIST_QNAME = QName.create(TEST_QNAME, "inner-list");
     public static final QName OUTER_CHOICE_QNAME = QName.create(TEST_QNAME, "outer-choice");
     public static final QName ID_QNAME = QName.create(TEST_QNAME, "id");
     public static final QName NAME_QNAME = QName.create(TEST_QNAME, "name");
     public static final QName VALUE_QNAME = QName.create(TEST_QNAME, "value");
-    private static final String DATASTORE_TEST_YANG = "/odl-datastore-test.yang";
 
     public static final YangInstanceIdentifier TEST_PATH = YangInstanceIdentifier.of(TEST_QNAME);
-    public static final YangInstanceIdentifier OUTER_LIST_PATH = YangInstanceIdentifier.builder(TEST_PATH).node(OUTER_LIST_QNAME).build();
+    public static final YangInstanceIdentifier OUTER_LIST_PATH = YangInstanceIdentifier.builder(TEST_PATH)
+            .node(OUTER_LIST_QNAME).build();
 
-    public static final InputStream getDatastoreBenchmarkInputStream() {
-        return getInputStream(DATASTORE_TEST_YANG);
-    }
-
-    private static InputStream getInputStream(final String resourceName) {
-        return BenchmarkModel.class.getResourceAsStream(resourceName);
-    }
-
-    public static SchemaContext createTestContext() throws SourceException, ReactorException {
+    public static SchemaContext createTestContext() {
         CrossSourceStatementReactor.BuildAction reactor = YangInferencePipeline.RFC6020_REACTOR.newBuild();
-        SchemaContext schemaContext = reactor.buildEffective(Collections.singletonList(
-            getDatastoreBenchmarkInputStream()));
-
-        return schemaContext;
+        try {
+            reactor.addSource(YangStatementStreamSource.create(
+                YangTextSchemaSource.forResource("/odl-datastore-test.yang")));
+            return reactor.buildEffective();
+        } catch (IOException | YangSyntaxErrorException | ReactorException e) {
+            throw new IllegalStateException(e);
+        }
     }
-
 }
