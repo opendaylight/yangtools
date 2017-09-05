3161b22f66201205da0e956601eaca75fe638fa6
Robert Varga <robert.varga@pantheon.tech>
2017-09-05 23:51:57 +0200

    Correct model revision
    2017-16-05 is an invalid revision string, correct it to something
    sane.
    
    Change-Id: Ie2997888dd86ff94a89c4d0c0c6dc01f1ebe2290
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-test-model/src/main/java/org/opendaylight/yang/gen/v1/bug8449/rev170516/ContInt32RefUnionInt32Builder.java
@@ -0,0 +1,19 @@
+package org.opendaylight.yang.gen.v1.bug8449.rev170516;
+import org.opendaylight.yang.gen.v1.bug8449.rev170516.ContInt32.RefUnionInt32;
+
+/**
+ * The purpose of generated class in src/main/java for Union types is to create new instances of unions from a string representation.
+ * In some cases it is very difficult to automate it since there can be unions such as (uint32 - uint16), or (string - uint32).
+ *
+ * The reason behind putting it under src/main/java is:
+ * This class is generated in form of a stub and needs to be finished by the user. This class is generated only once to prevent
+ * loss of user code.
+ *
+ */
+public class ContInt32RefUnionInt32Builder {
+
+    public static RefUnionInt32 getDefaultInstance(java.lang.String defaultValue) {
+        throw new java.lang.UnsupportedOperationException("Not yet implemented");
+    }
+
+}
