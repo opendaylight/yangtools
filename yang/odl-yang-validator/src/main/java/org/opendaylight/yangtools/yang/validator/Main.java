29934881dde5a3f08116a405dd1904f7e4b468d4
Jakub Toth <jakub.toth@pantheon.tech>
2017-09-19 15:26:22 +0200

    Rename yang-system-test module to odl-yang-validator
    Change-Id: I7353c23afcfa7f53b37e428564f783565b41f4c6
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

--- yang/odl-yang-validator/src/main/java/org/opendaylight/yangtools/yang/validator/Main.java
+++ yang/odl-yang-validator/src/main/java/org/opendaylight/yangtools/yang/validator/Main.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.yangtools.yang.parser.system.test;
+package org.opendaylight.yangtools.yang.validator;
 
 import ch.qos.logback.classic.Level;
 import com.google.common.base.Stopwatch;
