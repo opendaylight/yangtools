fcb2be4f7ac644fae008b6cb541357d643ee5925
Jakub Toth <jakub.toth@pantheon.tech>
2017-09-22 15:47:19 +0200

    Add option to validate yang by module name
    Change-Id: If1aa92440cee4510758d78f5c2dc4d619474b7db
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

--- yang/odl-yang-validator/src/main/java/org/opendaylight/yangtools/yang/validator/SystemTestUtils.java
+++ yang/odl-yang-validator/src/main/java/org/opendaylight/yangtools/yang/validator/SystemTestUtils.java
@@ -13,11 +13,16 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.parser.api.YangSyntaxErrorException;
@@ -30,13 +35,18 @@ import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.YangInferencePipeline
 
 class SystemTestUtils {
 
+    private final static Pattern MODULE_PATTERN = Pattern.compile("module(.*?)\\{");
+    private final static Pattern WHITESPACES = Pattern.compile("\\s+");
+
+
     static final FileFilter YANG_FILE_FILTER = file -> {
         final String name = file.getName().toLowerCase();
         return name.endsWith(".yang") && file.isFile();
     };
 
     static SchemaContext parseYangSources(final List<String> yangLibDirs, final List<String> yangTestFiles,
-            final Set<QName> supportedFeatures, final boolean recursiveSearch) throws ReactorException, IOException,
+            final Set<QName> supportedFeatures, final boolean recursiveSearch)
+            throws ReactorException, IOException,
             YangSyntaxErrorException {
         /*
          * Current dir "." should be always present implicitly in the list of
@@ -51,19 +61,62 @@ class SystemTestUtils {
             libFiles.addAll(getYangFiles(yangLibDir, recursiveSearch));
         }
 
+
         final List<File> testFiles = new ArrayList<>();
         for (final String yangTestFile : yangTestFiles) {
-            testFiles.add(new File(yangTestFile));
+            if (!yangTestFile.endsWith(".yang")) {
+                testFiles.add(findInFiles(libFiles, yangTestFile));
+            } else {
+                testFiles.add(new File(yangTestFile));
+            }
         }
 
         return parseYangSources(supportedFeatures, testFiles, libFiles);
     }
 
+    private static File findInFiles(final List<File> libFiles, final String yangTestFile) throws IOException {
+        for (final File file : libFiles) {
+            if (getModelNameFromFile(file).replaceAll(WHITESPACES.pattern(), "").equals(yangTestFile)) {
+                return file;
+            }
+        }
+        throw new FileNotFoundException("File does not exist:" + yangTestFile);
+    }
+
+    private static String getModelNameFromFile(final File file) throws IOException {
+        final String fileAsString = readFile(file.getAbsolutePath());
+        final Matcher matcher = MODULE_PATTERN.matcher(fileAsString);
+        if (matcher.find()) {
+            return matcher.group(1);
+        } else {
+            return "";
+        }
+    }
+
+    private static String readFile(final String path) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(path)), StandardCharsets.UTF_8);
+    }
+
     static SchemaContext parseYangSources(final Set<QName> supportedFeatures, final List<File> testFiles,
-            final List<File> libFiles) throws ReactorException, IOException, YangSyntaxErrorException {
+            final List<File> libFiles)
+            throws ReactorException, IOException, YangSyntaxErrorException {
         final List<StatementStreamSource> testSources = getYangStatementSources(testFiles);
         final List<StatementStreamSource> libSources = getYangStatementSources(libFiles);
+
+        if (testSources.isEmpty()) {
+            throw new FileNotFoundException("Files does not exists:" + testFiles);
+        }
+
         return parseYangSources(testSources, libSources, supportedFeatures);
+    }
+
+    private static List<StatementStreamSource> getYangStatementSources(final List<File> yangFiles)
+            throws IOException, YangSyntaxErrorException {
+        final List<StatementStreamSource> yangSources = new ArrayList<>(yangFiles.size());
+        for (final File file : yangFiles) {
+            yangSources.add(YangStatementStreamSource.create(YangTextSchemaSource.forFile(file)));
+        }
+        return yangSources;
     }
 
     static SchemaContext parseYangSources(final List<StatementStreamSource> testSources,
@@ -71,23 +124,14 @@ class SystemTestUtils {
         Preconditions.checkArgument(testSources != null && !testSources.isEmpty(), "No yang sources");
 
         final CrossSourceStatementReactor.BuildAction reactor = YangInferencePipeline.RFC6020_REACTOR.newBuild();
-        reactor.addSources(testSources);
         reactor.addLibSources(libSources);
+        reactor.addSources(testSources);
 
         if (supportedFeatures != null) {
             reactor.setSupportedFeatures(supportedFeatures);
         }
 
         return reactor.buildEffective();
-    }
-
-    private static List<StatementStreamSource> getYangStatementSources(final List<File> yangFiles)
-            throws IOException, YangSyntaxErrorException {
-        final List<StatementStreamSource> yangSources = new ArrayList<>(yangFiles.size());
-        for (File file : yangFiles) {
-            yangSources.add(YangStatementStreamSource.create(YangTextSchemaSource.forFile(file)));
-        }
-        return yangSources;
     }
 
     private static Collection<File> getYangFiles(final String yangSourcesDirectoryPath, final boolean recursiveSearch)
