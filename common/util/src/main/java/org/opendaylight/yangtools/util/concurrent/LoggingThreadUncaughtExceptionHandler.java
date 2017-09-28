483823b6e240dc3963a3f343dd17bad0e631fe95
Michael Vorburger <vorburger@redhat.com>
2017-09-28 23:32:19 +0200

    SpecialExecutors with LoggingThreadUncaughtExceptionHandler
    Change-Id: I133bbf474a60681f061968a8f9ed1ed32671fd6a
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ common/util/src/main/java/org/opendaylight/yangtools/util/concurrent/LoggingThreadUncaughtExceptionHandler.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.util.concurrent;
+
+import com.google.common.base.Preconditions;
+import java.lang.Thread.UncaughtExceptionHandler;
+import org.slf4j.Logger;
+
+/**
+ * Thread's UncaughtExceptionHandler which logs to slf4j.
+ *
+ * @author Michael Vorburger.ch
+ */
+public final class LoggingThreadUncaughtExceptionHandler implements UncaughtExceptionHandler {
+
+    // This class is also available as in infrautils (but yangtools cannot depend on infrautils)
+    // as org.opendaylight.infrautils.utils.concurrent.LoggingThreadUncaughtExceptionHandler
+
+    /**
+     * Factory method to obtain an instance of this bound to the passed slf4j Logger.
+     */
+    public static UncaughtExceptionHandler toLogger(Logger logger) {
+        return new LoggingThreadUncaughtExceptionHandler(logger);
+    }
+
+    private final Logger logger;
+
+    private LoggingThreadUncaughtExceptionHandler(Logger logger) {
+        this.logger = Preconditions.checkNotNull(logger, "logger");
+    }
+
+    @Override
+    public void uncaughtException(Thread thread, Throwable throwable) {
+        logger.error("Thread terminated due to uncaught exception: {}", thread.getName(), throwable);
+    }
+}
---
+++ common/util/src/main/java/org/opendaylight/yangtools/util/concurrent/ThreadFactoryProvider.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.util.concurrent;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import java.util.Optional;
+import java.util.concurrent.ThreadFactory;
+import org.immutables.value.Value;
+import org.slf4j.Logger;
+
+/**
+ * Builder for {@link ThreadFactory}. Easier to use than the
+ * {@link com.google.common.util.concurrent.ThreadFactoryBuilder}, because it
+ * enforces setting all required properties through a staged builder.
+ *
+ * @author Michael Vorburger.ch
+ */
+@Value.Immutable
+@Value.Style(stagedBuilder = true)
+public abstract class ThreadFactoryProvider {
+
+    // This class is also available as in infrautils (but yangtools cannot depend on infrautils)
+    // as org.opendaylight.infrautils.utils.concurrent.ThreadFactoryProvider
+
+    public static ImmutableThreadFactoryProvider.NamePrefixBuildStage builder() {
+        return ImmutableThreadFactoryProvider.builder();
+    }
+
+    /**
+     * Prefix for threads from this factory. For example, "rpc-pool", to create
+     * "rpc-pool-1/2/3" named threads. Note that this is a prefix, not a format,
+     * so you pass just "rpc-pool" instead of e.g. "rpc-pool-%d".
+     */
+    @Value.Parameter public abstract String namePrefix();
+
+    /**
+     * Logger used to log uncaught exceptions from new threads created via this factory.
+     */
+    @Value.Parameter public abstract Logger logger();
+
+    /**
+     * Priority for new threads from this factory.
+     */
+    @Value.Parameter public abstract Optional<Integer> priority();
+
+    /**
+     * Daemon or not for new threads created via this factory.
+     * <b>NB: Defaults to true.</b>
+     */
+    @Value.Default public boolean daemon() {
+        return true;
+    }
+
+    public ThreadFactory get() {
+        ThreadFactoryBuilder guavaBuilder = new ThreadFactoryBuilder();
+        guavaBuilder.setNameFormat(namePrefix() + "-%d");
+        guavaBuilder.setUncaughtExceptionHandler(LoggingThreadUncaughtExceptionHandler.toLogger(logger()));
+        guavaBuilder.setDaemon(daemon());
+        priority().ifPresent(priority -> guavaBuilder.setPriority(priority));
+        logger().info("ThreadFactory created: {}", namePrefix());
+        return guavaBuilder.build();
+    }
+}
