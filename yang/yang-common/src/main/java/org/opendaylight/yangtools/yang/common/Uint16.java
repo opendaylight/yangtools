a455be77c71fb453f38c0edcb8e397a9cac8e8db
Robert Varga <robert.varga@pantheon.tech>
2017-10-10 18:01:39 +0200

    Introduce Decimal64, Empty, Uint{8,16,32,64}
    This introduces specialized classes for native YANG types.
    
    Change-Id: I356f134c27ea4c8e593da067c2a5e8f2c989bbea
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/Uint16.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright (c) 2015 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.common;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import com.google.common.annotations.Beta;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.opendaylight.yangtools.concepts.Immutable;
+
+/**
+ * Dedicated type for YANG's 'type uint16' type.
+ *
+ * @author Robert Varga
+ */
+@Beta
+public final class Uint16 extends Number implements Comparable<Uint16>, Immutable {
+    private static final long serialVersionUID = 1L;
+    private static final int MIN_VALUE = 0;
+    private static final int MAX_VALUE = 65535;
+
+    /**
+     * Cache of first 256 values.
+     */
+    private static final Uint16[] CACHE = new Uint16[Uint8.MAX_VALUE];
+
+    /**
+     * Commonly encountered values.
+     */
+    private static final Uint16[] COMMON = new Uint16[] {
+        new Uint16(Short.MAX_VALUE),
+        new Uint16((short)32768),
+        new Uint16((short)65535),
+    };
+
+    /**
+     * Tunable weak LRU cache for other values. By default it holds {@value #DEFAULT_LRU_SIZE} entries. This can be
+     * changed via {@value #LRU_SIZE_PROPERTY} system property.
+     */
+    private static final int DEFAULT_LRU_SIZE = 1024;
+    private static final String LRU_SIZE_PROPERTY = "org.opendaylight.yangtools.yang.common.Uint16.LRU.size";
+    private static final int MAX_LRU_SIZE = MAX_VALUE + 1;
+    private static final int LRU_SIZE;
+
+    static {
+        final int p = Integer.getInteger(LRU_SIZE_PROPERTY, DEFAULT_LRU_SIZE);
+        LRU_SIZE = p >= 0 ? Math.min(p, MAX_LRU_SIZE) : DEFAULT_LRU_SIZE;
+    }
+
+    private static final LoadingCache<Short, Uint16> LRU = CacheBuilder.newBuilder().weakValues().maximumSize(LRU_SIZE)
+            .build(new CacheLoader<Short, Uint16>() {
+                @Override
+                public Uint16 load(final Short key) {
+                    return new Uint16(key);
+                }
+            });
+
+    private final short value;
+
+    private Uint16(final short value) {
+        this.value = value;
+    }
+
+    private static Uint16 instanceFor(final short value) {
+        final int slot = Short.toUnsignedInt(value);
+        if (slot >= CACHE.length) {
+            for (Uint16 c : COMMON) {
+                if (c.value == value) {
+                    return c;
+                }
+            }
+
+            return LRU.getUnchecked(value);
+        }
+
+        Uint16 ret = CACHE[slot];
+        if (ret == null) {
+            synchronized (CACHE) {
+                ret = CACHE[slot];
+                if (ret == null) {
+                    ret = new Uint16(value);
+                    CACHE[slot] = ret;
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    public static Uint16 fromShortBits(final short bits) {
+        return instanceFor(bits);
+    }
+
+    public static Uint16 valueOf(final byte byteVal) {
+        checkArgument(byteVal >= MIN_VALUE, "Negative values are not allowed");
+        return instanceFor(byteVal);
+    }
+
+    public static Uint16 valueOf(final short shortVal) {
+        checkArgument(shortVal >= MIN_VALUE, "Negative values are not allowed");
+        return instanceFor(shortVal);
+    }
+
+    public static Uint16 valueOf(final int intVal) {
+        checkArgument(intVal >= MIN_VALUE && intVal <= MAX_VALUE, "Value %s is outside of allowed range", intVal);
+        return instanceFor((short)(intVal & 0xffff));
+    }
+
+    public static Uint16 valueOf(final long longVal) {
+        checkArgument(longVal >= MIN_VALUE && longVal <= MAX_VALUE, "Value %s is outside of allowed range", longVal);
+        return instanceFor((short)(longVal & 0xffff));
+    }
+
+    public static Uint16 valueOf(final Uint8 uint) {
+        return instanceFor(uint.shortValue());
+    }
+
+    public static Uint16 valueOf(final Uint32 uint) {
+        return valueOf(uint.longValue());
+    }
+
+    public static Uint16 valueOf(final String string) {
+        return valueOf(string, 10);
+    }
+
+    public static Uint16 valueOf(final String string, final int radix) {
+        // FIXME: implement this
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public short shortValue() {
+        return value;
+    }
+
+    @Override
+    public int intValue() {
+        return Short.toUnsignedInt(value);
+    }
+
+    @Override
+    public long longValue() {
+        return Short.toUnsignedLong(value);
+    }
+
+    @Override
+    public float floatValue() {
+        return intValue();
+    }
+
+    @Override
+    public double doubleValue() {
+        return intValue();
+    }
+
+    @Override
+    @SuppressWarnings("checkstyle:parameterName")
+    public int compareTo(final Uint16 o) {
+        return Integer.compare(intValue(), o.intValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Short.hashCode(value);
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        return obj instanceof Uint16 && value == ((Uint16)obj).value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(intValue());
+    }
+
+    private Object readResolve() {
+        return instanceFor(value);
+    }
+}
---
+++ yang/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/Uint8.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) 2015 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.common;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import com.google.common.annotations.Beta;
+import org.opendaylight.yangtools.concepts.Immutable;
+
+/**
+ * Dedicated type for YANG's 'type uint8' type.
+ *
+ * @author Robert Varga
+ */
+@Beta
+public final class Uint8 extends Number implements Comparable<Uint8>, Immutable {
+    static final short MIN_VALUE = 0;
+    static final short MAX_VALUE = 255;
+
+    private static final long serialVersionUID = 1L;
+    private static final Uint8[] CACHE = new Uint8[MAX_VALUE + 1];
+
+    private final byte value;
+
+    private Uint8(final byte value) {
+        this.value = value;
+    }
+
+    private static Uint8 instanceFor(final byte value) {
+        final int slot = Byte.toUnsignedInt(value);
+
+        Uint8 ret = CACHE[slot];
+        if (ret == null) {
+            synchronized (CACHE) {
+                ret = CACHE[slot];
+                if (ret == null) {
+                    ret = new Uint8(value);
+                    CACHE[slot] = ret;
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    public static Uint8 fromByteBits(final byte bits) {
+        return instanceFor(bits);
+    }
+
+    public static Uint8 valueOf(final byte byteVal) {
+        checkArgument(byteVal >= MIN_VALUE, "Negative values are not allowed");
+        return instanceFor(byteVal);
+    }
+
+    public static Uint8 valueOf(final short shortVal) {
+        checkArgument(shortVal >= MIN_VALUE && shortVal <= MAX_VALUE, "Value %s is outside of allowed range", shortVal);
+        return instanceFor((byte)(shortVal & 0xff));
+    }
+
+    public static Uint8 valueOf(final int intVal) {
+        checkArgument(intVal >= MIN_VALUE && intVal <= MAX_VALUE, "Value %s is outside of allowed range", intVal);
+        return instanceFor((byte)(intVal & 0xff));
+    }
+
+    public static Uint8 valueOf(final long longVal) {
+        checkArgument(longVal >= MIN_VALUE && longVal <= MAX_VALUE, "Value %s is outside of allowed range", longVal);
+        return instanceFor((byte)(longVal & 0xff));
+    }
+
+    public static Uint8 valueOf(final Uint16 uint) {
+        return valueOf(uint.intValue());
+    }
+
+    public static Uint8 valueOf(final String string) {
+        return valueOf(string, 10);
+    }
+
+    public static Uint8 valueOf(final String string, final int radix) {
+        // FIXME: implement this
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public byte byteValue() {
+        return value;
+    }
+
+    @Override
+    public int intValue() {
+        return Byte.toUnsignedInt(value);
+    }
+
+    @Override
+    public long longValue() {
+        return Byte.toUnsignedLong(value);
+    }
+
+    @Override
+    public float floatValue() {
+        return intValue();
+    }
+
+    @Override
+    public double doubleValue() {
+        return intValue();
+    }
+
+    @Override
+    @SuppressWarnings("checkstyle:parameterName")
+    public int compareTo(final Uint8 o) {
+        return intValue() - o.intValue();
+    }
+
+    @Override
+    public int hashCode() {
+        return Byte.hashCode(value);
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        return obj instanceof Uint8 && value == ((Uint8)obj).value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(intValue());
+    }
+
+    private Object readResolve() {
+        return instanceFor(value);
+    }
+}
