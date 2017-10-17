4b59bdfb4034d41512e4fd78932d70e4a3ebc0fd
Peter Kajsa <pkajsa@cisco.com>
2017-10-17 18:52:05 +0200

    Bug 9195 - Option to set supported deviations for SchemaContextFactory
    There was missing API to set supported deviations in SchemaContextFactory.
    This patch adds missing API and implementation for setting supported deviations
    in SchemaContextFactory.
    In order to avoid passing of numerous arguments into createSchemaContext(..) method
    a concept of SchemaContextFactoryConfiguration has been introduced in this patch,
    which encapsulates all options possible to set.
    
    Change-Id: I8eef54d729b7bfed4cee41ee6758d969fc375dcf
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>

---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/repo/api/SchemaContextFactoryConfiguration.java
@@ -0,0 +1,179 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.repo.api;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import javax.annotation.Nonnull;
+import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+
+/**
+ * SchemaContextFactory configuration class.
+ *
+ * <p>
+ * SchemaContextFactoryConfiguration supports currently the following options to
+ * be set:
+ * <ul>
+ * <li>schema source filter</li>
+ * <li>statement parser mode</li>
+ * <li>supported features</li>
+ * <li>supported deviations</li>
+ * </ul>
+ */
+@Beta
+public final class SchemaContextFactoryConfiguration implements Immutable {
+    public static final SchemaContextFactoryConfiguration DEFAULT_CONFIGURATION = new Builder().build();
+
+    private final SchemaSourceFilter filter;
+    private final StatementParserMode statementParserMode;
+    private final Optional<Set<QName>> supportedFeatures;
+    private final Optional<Map<QNameModule, Set<QNameModule>>> modulesDeviatedByModules;
+
+    private SchemaContextFactoryConfiguration(final SchemaSourceFilter filter,
+            final StatementParserMode statementParserMode, final Set<QName> supportedFeatures,
+            final Map<QNameModule, Set<QNameModule>> modulesDeviatedByModules) {
+        this.filter = requireNonNull(filter);
+        this.statementParserMode = requireNonNull(statementParserMode);
+        this.supportedFeatures = Optional.ofNullable(supportedFeatures);
+        this.modulesDeviatedByModules = Optional.ofNullable(modulesDeviatedByModules);
+    }
+
+    public SchemaSourceFilter getSchemaSourceFilter() {
+        return filter;
+    }
+
+    public StatementParserMode getStatementParserMode() {
+        return statementParserMode;
+    }
+
+    public Optional<Set<QName>> getSupportedFeatures() {
+        return supportedFeatures;
+    }
+
+    public Optional<Map<QNameModule, Set<QNameModule>>> getModulesDeviatedByModules() {
+        return modulesDeviatedByModules;
+    }
+
+    public static SchemaContextFactoryConfiguration getDefault() {
+        return DEFAULT_CONFIGURATION;
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(filter, statementParserMode, supportedFeatures, modulesDeviatedByModules);
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof SchemaContextFactoryConfiguration)) {
+            return false;
+        }
+        final SchemaContextFactoryConfiguration other = (SchemaContextFactoryConfiguration) obj;
+        return Objects.equals(filter, other.filter) && Objects.equals(statementParserMode, other.statementParserMode)
+                && Objects.equals(supportedFeatures, other.supportedFeatures)
+                && Objects.equals(modulesDeviatedByModules, other.modulesDeviatedByModules);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this).add("schemaSourceFilter", filter)
+                .add("statementParserMode", statementParserMode).add("supportedFeatures", supportedFeatures)
+                .add("modulesDeviatedByModules", modulesDeviatedByModules).toString();
+    }
+
+    public static class Builder
+            implements org.opendaylight.yangtools.concepts.Builder<SchemaContextFactoryConfiguration> {
+        private SchemaSourceFilter filter = SchemaSourceFilter.ALWAYS_ACCEPT;
+        private StatementParserMode statementParserMode = StatementParserMode.DEFAULT_MODE;
+        private Set<QName> supportedFeatures;
+        private Map<QNameModule, Set<QNameModule>> modulesDeviatedByModules;
+
+        /**
+         * Set schema source filter which will filter available schema sources
+         * using the provided filter.
+         *
+         * @param filter
+         *            schema source filter which acts as the gating function
+         *            before a schema source is considered by the factory for
+         *            inclusion in the SchemaContext it produces.
+         * @return this builder
+         */
+        public Builder setFilter(@Nonnull final SchemaSourceFilter filter) {
+            this.filter = requireNonNull(filter);
+            return this;
+        }
+
+        /**
+         * Set yang statement parser mode.
+         *
+         * @param statementParserMode
+         *            mode of yang statement parser
+         * @return this builder
+         */
+        public Builder setStatementParserMode(@Nonnull final StatementParserMode statementParserMode) {
+            this.statementParserMode = requireNonNull(statementParserMode);
+            return this;
+        }
+
+        /**
+         * Set supported features based on which all if-feature statements in
+         * the parsed YANG modules will be resolved.
+         *
+         * @param supportedFeatures
+         *            Set of supported features in the final SchemaContext. If
+         *            the set is empty, no features encountered will be
+         *            supported.
+         * @return this builder
+         */
+        public Builder setSupportedFeatures(final Set<QName> supportedFeatures) {
+            this.supportedFeatures = supportedFeatures != null ? ImmutableSet.copyOf(supportedFeatures) : null;
+            return this;
+        }
+
+        /**
+         * Set YANG modules which can be deviated by specified modules during
+         * the parsing process. Map key (QNameModule) denotes a module which can
+         * be deviated by the modules in the Map value.
+         *
+         * @param modulesDeviatedByModules
+         *            Map of YANG modules (Map key) which can be deviated by
+         *            specified modules (Map value) in the final SchemaContext.
+         *            If the map is empty, no deviations encountered will be
+         *            supported.
+         * @return this builder
+         */
+        public Builder setModulesDeviatedByModules(final Map<QNameModule, Set<QNameModule>> modulesDeviatedByModules) {
+            this.modulesDeviatedByModules = modulesDeviatedByModules != null
+                    ? ImmutableMap.copyOf(modulesDeviatedByModules) : null;
+            return this;
+        }
+
+        @Override
+        public SchemaContextFactoryConfiguration build() {
+            return new SchemaContextFactoryConfiguration(filter, statementParserMode, supportedFeatures,
+                    modulesDeviatedByModules);
+        }
+    }
+}
