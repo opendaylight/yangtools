6139b745e5dff50d6d897455e0cc4e592584e77f
Igor Foltin <igor.foltin@pantheon.tech>
2017-10-17 16:08:18 +0200

    Bug 9242: Reuse deviating statement contexts
    In the process of deviate add/replace resolution we make
    copies of deviated statement contexts which are then added
    to the deviation target context.
    
    However, making copies of these statement contexts is not
    necessary except the ones which represent unknown statements.
    
    Therefore, reuse them during the resolution of deviate add/replace.
    
    By doing this, we also solve the failing deviate replace with
    user-defined types as statement contexts that represent deviating
    user-defined types are now resolved within their original context.
    
    Change-Id: Ie61a38e270ef648fe61ea283805d149533c10f62
    Signed-off-by: Igor Foltin <igor.foltin@pantheon.tech>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug9242Test.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+
+import java.util.Date;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.SimpleDateFormatUtil;
+import org.opendaylight.yangtools.yang.model.api.Deviation;
+import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+
+public class Bug9242Test {
+
+    @Test
+    public void testDeviateReplaceWithUserDefinedTypes() throws Exception {
+        final SchemaContext schemaContext = StmtTestUtils.parseYangSources("/bugs/bug9242/");
+        assertNotNull(schemaContext);
+
+        final Date revision = SimpleDateFormatUtil.getRevisionFormat().parse("2017-10-13");
+
+        final Module rootModule = schemaContext.findModuleByName("root-module", revision);
+        final Module impModule = schemaContext.findModuleByName("imp-module", revision);
+        assertNotNull(impModule);
+
+        TypeDefinition<?> deviatedMyLeafType = null;
+        TypeDefinition<?> deviatedMyLeaf2Type = null;
+
+        for (final Deviation deviation : rootModule.getDeviations()) {
+            if (deviation.getTargetPath().getLastComponent().equals(QName.create(
+                    impModule.getQNameModule(), "my-leaf"))) {
+                deviatedMyLeafType = deviation.getDeviates().iterator().next().getDeviatedType();
+            }
+
+            if (deviation.getTargetPath().getLastComponent().equals(QName.create(
+                    impModule.getQNameModule(), "my-leaf-2"))) {
+                deviatedMyLeaf2Type = deviation.getDeviates().iterator().next().getDeviatedType();
+            }
+        }
+
+        assertNotNull(deviatedMyLeafType);
+        assertNotNull(deviatedMyLeaf2Type);
+
+        final LeafSchemaNode myLeaf = (LeafSchemaNode) impModule.getDataChildByName(QName.create(
+                impModule.getQNameModule(), "my-leaf"));
+        assertSame(deviatedMyLeafType, myLeaf.getType());
+
+        final LeafSchemaNode myLeaf2 = (LeafSchemaNode) impModule.getDataChildByName(QName.create(
+                impModule.getQNameModule(), "my-leaf-2"));
+        assertSame(deviatedMyLeaf2Type, myLeaf2.getType());
+    }
+}
