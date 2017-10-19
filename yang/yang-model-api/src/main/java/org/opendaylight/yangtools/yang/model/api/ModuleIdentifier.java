dea336e74d48d148357f595164f0e6111d58a5c8
Robert Varga <robert.varga@pantheon.tech>
2017-10-19 15:36:01 +0200

    Elimiante ModuleIdentifier.getQNameModule()
    This method should be part of Module only, remove it and its sole user,
    which happens to be a test.
    
    Change-Id: I481050cb9e8601a5cb294bf1f2493d1fa6adb040
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/ModuleIdentifier.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/ModuleIdentifier.java
@@ -5,13 +5,11 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.yangtools.yang.model.api;
 
 import java.net.URI;
 import java.util.Date;
 import org.opendaylight.yangtools.concepts.SemVer;
-import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
 
 /**
@@ -22,14 +20,6 @@ import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
  */
 @Deprecated
 public interface ModuleIdentifier {
-    /**
-     * Returns a {@link QNameModule}, which contains the namespace and
-     * the revision of the module.
-     *
-     * @return QNameModule identifier.
-     */
-    QNameModule getQNameModule();
-
     /**
      * Returns the name of the module which is specified as argument of YANG
      * {@link Module <b><font color="#FF0000">module</font></b>} keyword.
