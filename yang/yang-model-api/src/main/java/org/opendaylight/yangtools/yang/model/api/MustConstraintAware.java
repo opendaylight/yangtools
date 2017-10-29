93a11c9fd78cb0e9d14e59b243790a4c7fa0fb85
Robert Varga <robert.varga@pantheon.tech>
2017-10-29 22:48:41 +0100

    Fix NotificationDefinition
    NotificationDefinition should provide MustDefinitions directly, as
    it cannot have any other constraints.
    
    Change-Id: Icf38b5b832979fafac8c38b09b108509014fc48c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/MustConstraintAware.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.api;
+
+import java.util.Collection;
+
+/**
+ * Mix-in interface for nodes which can define must constraints.
+ *
+ * @author Robert Varga
+ */
+public interface MustConstraintAware {
+    /**
+     * Specifies the rules which the node which contains <code>must</code> YANG substatement has to match.
+     *
+     * @return collection of <code>MustDefinition</code> (XPath) instances which represents the concrete data
+     *         constraints
+     */
+    Collection<MustDefinition> getMustConstraints();
+}
