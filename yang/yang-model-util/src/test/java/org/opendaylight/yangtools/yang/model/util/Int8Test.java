9ef7c8a3af4fbe7ea953e0b82131327ae2c10819
Robert Varga <robert.varga@pantheon.tech>
2017-10-30 12:04:06 +0100

    Update TypeDefinition design
    Use Optional to communicate presence of default value and units.
    
    Change-Id: Icfffb752507e2810a6afb157faa09d07680292cf
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-model-util/src/test/java/org/opendaylight/yangtools/yang/model/util/Int8Test.java
+++ yang/yang-model-util/src/test/java/org/opendaylight/yangtools/yang/model/util/Int8Test.java
@@ -7,8 +7,11 @@
  */
 package org.opendaylight.yangtools.yang.model.util;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
 
+import java.util.Optional;
 import org.junit.Test;
 import org.opendaylight.yangtools.yang.model.api.type.IntegerTypeDefinition;
 import org.opendaylight.yangtools.yang.model.util.type.BaseTypes;
@@ -19,8 +22,9 @@ public class Int8Test {
     public void testInt8() {
         IntegerTypeDefinition int8 = BaseTypes.int8Type();
         IntegerTypeDefinition int8Second = BaseTypes.int8Type();
-        assertTrue("The method 'getInstance()' has to return the same instance", int8 == int8Second);
-        assertTrue("The method 'getBaseType()' is returning incorrect value", int8.getBaseType() == null);
-        assertTrue("The method 'getDefaultType()' is returning incorrect value", int8.getDefaultValue() == null);
+        assertSame("The method 'getInstance()' has to return the same instance", int8, int8Second);
+        assertNull("The method 'getBaseType()' is returning incorrect value", int8.getBaseType());
+        assertEquals("The method 'getDefaultType()' is returning incorrect value", Optional.empty(),
+            int8.getDefaultValue());
     }
 }
