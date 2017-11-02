c47f428aedb1a9555564c11d851f41d7d030f054
Robert Varga <robert.varga@pantheon.tech>
2017-11-02 14:17:26 +0100

    Enforce decimal64 fraction-digits in derived types
    Overriding fraction-digits to a different value than specified by
    base type is invalid -- throw a SourceException if we encounter such
    a thing.
    
    Change-Id: Ibb79203377ff41eefbf70100d6e86887e2fff185
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/type/DecimalTypeEffectiveStatementImpl.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/type/DecimalTypeEffectiveStatementImpl.java
@@ -16,16 +16,14 @@ import org.opendaylight.yangtools.yang.model.api.type.DecimalTypeDefinition;
 import org.opendaylight.yangtools.yang.model.util.type.RangeRestrictedTypeBuilder;
 import org.opendaylight.yangtools.yang.model.util.type.RestrictedTypes;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
 import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.TypeUtils;
 import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.DeclaredEffectiveStatementBase;
 import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.FractionDigitsEffectiveStatementImpl;
 import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.UnknownEffectiveStatementImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public final class DecimalTypeEffectiveStatementImpl extends DeclaredEffectiveStatementBase<String, TypeStatement>
         implements TypeEffectiveStatement<TypeStatement> {
-    private static final Logger LOG = LoggerFactory.getLogger(Decimal64SpecificationEffectiveStatementImpl.class);
     private final DecimalTypeDefinition typeDefinition;
 
     public DecimalTypeEffectiveStatementImpl(
@@ -46,15 +44,8 @@ public final class DecimalTypeEffectiveStatementImpl extends DeclaredEffectiveSt
             }
             if (stmt instanceof FractionDigitsEffectiveStatementImpl) {
                 final Integer digits = ((FractionDigitsEffectiveStatementImpl)stmt).argument();
-
-                if (!baseType.getFractionDigits().equals(digits)) {
-                    LOG.warn("Ignoring attempt to override fraction-digits to {} at {}, base type is {}", digits,
-                        ctx.getStatementSourceReference(), baseType);
-
-                    // FIXME: promote to a full error once our models are fixed
-                    // throw new SourceException(String.format("Cannot override fraction-digits from base type %s",
-                    // baseType), ctx.getStatementSourceReference());
-                }
+                SourceException.throwIf(!baseType.getFractionDigits().equals(digits), ctx.getStatementSourceReference(),
+                    "Cannot override fraction-digits from base type %s to %s", baseType, digits);
             }
         }
 
