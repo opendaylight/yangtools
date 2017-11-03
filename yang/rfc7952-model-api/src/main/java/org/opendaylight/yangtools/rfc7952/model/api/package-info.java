6c4493541e34049eee79fd974996bc0d7da006ff
Robert Varga <robert.varga@pantheon.tech>
2017-11-04 00:50:28 +0100

    Add support for RFC7952 extensions
    This patch adds support for md:annotation statement.
    
    Change-Id: I912645c0e01662594346e107766dd86f704f0cca
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/rfc7952-model-api/src/main/java/org/opendaylight/yangtools/rfc7952/model/api/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * YANG metamodel extensions to support metadata, as defined in
+ * <a href="https://tools.ietf.org/html/rfc7952">RFC7952</a>.
+ *
+ * @author Robert Varga
+ */
+package org.opendaylight.yangtools.rfc7952.model.api;
