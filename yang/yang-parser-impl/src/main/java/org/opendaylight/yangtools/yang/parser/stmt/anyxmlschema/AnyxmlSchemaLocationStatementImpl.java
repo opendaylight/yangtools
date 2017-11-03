8b81231016723f540c224c523d92dfa0a04e47aa
Robert Varga <robert.varga@pantheon.tech>
2017-11-03 18:15:30 +0100

    YANGTOOLS-706: split out rfc8040-model-api
    YangDataSchemaNode needs to live in a separate RFC8040-specific
    module, as it is not part of RFC7950 semantic model. Split it
    out.
    
    This flushes out the need to properly define YangDataStatement,
    which in turn cascades to discrepancies in the structure of our
    support classes. Those are cleaned up, with extensions getting
    their proper declared statement interfaces and packages.
    
    Change-Id: I84f7ad882172806a5aac8362837b85b50dc3a387
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/anyxmlschema/AnyxmlSchemaLocationStatementImpl.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.stmt.anyxmlschema;
+
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
+import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractDeclaredStatement;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+
+final class AnyxmlSchemaLocationStatementImpl extends AbstractDeclaredStatement<SchemaNodeIdentifier>
+        implements AnyxmlSchemaLocationStatement {
+    AnyxmlSchemaLocationStatementImpl(
+            final StmtContext<SchemaNodeIdentifier, AnyxmlSchemaLocationStatement, ?> context) {
+        super(context);
+    }
+
+    @Override
+    public SchemaNodeIdentifier getArgument() {
+        return argument();
+    }
+}
---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/thirdparty/plugin/ThirdPartyExtensionStatement.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.thirdparty.plugin;
+
+import org.opendaylight.yangtools.yang.model.api.stmt.UnknownStatement;
+
+/**
+ * Public declared statement definition that this plugin supports. This interface should generally depend only on
+ * concepts defined in yang-model-api.
+ */
+public interface ThirdPartyExtensionStatement extends UnknownStatement<String> {
+
+}
