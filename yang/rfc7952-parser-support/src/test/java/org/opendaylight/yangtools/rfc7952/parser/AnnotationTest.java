b3cf64cb9dd7390a14c592045e7734c02e109ae8
Robert Varga <robert.varga@pantheon.tech>
2017-11-04 00:50:28 +0100

    Add support for RFC7952 extensions
    This patch adds support for md:annotation statement.
    
    Change-Id: I912645c0e01662594346e107766dd86f704f0cca
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/rfc7952-parser-support/src/test/java/org/opendaylight/yangtools/rfc7952/parser/AnnotationTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc7952.parser;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.opendaylight.yangtools.rfc7952.model.api.AnnotationSchemaNode;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.parser.api.YangSyntaxErrorException;
+import org.opendaylight.yangtools.yang.model.repo.api.YangTextSchemaSource;
+import org.opendaylight.yangtools.yang.model.util.type.BaseTypes;
+import org.opendaylight.yangtools.yang.parser.rfc6020.repo.YangStatementStreamSource;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ModelProcessingPhase;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StatementSupport;
+import org.opendaylight.yangtools.yang.parser.stmt.reactor.CrossSourceStatementReactor;
+import org.opendaylight.yangtools.yang.parser.stmt.reactor.CrossSourceStatementReactor.BuildAction;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.ext.CustomStatementParserBuilder;
+
+public class AnnotationTest {
+    private static final QName LAST_MODIFIED_QNAME = QName.create("http://example.org/example-last-modified",
+            "last-modified");
+    private static CrossSourceStatementReactor reactor;
+
+    @BeforeClass
+    public static void createReactor() {
+        final CustomStatementParserBuilder builder = new CustomStatementParserBuilder().addDefaultRFC6020Bundles();
+        for (StatementSupport<?, ?, ?> support : Metadata.getStatements()) {
+            builder.addStatementSupport(ModelProcessingPhase.FULL_DECLARATION, support);
+        }
+
+        reactor = builder.build();
+    }
+
+    @Test
+    public void testAnnotationResolution() throws ReactorException, IOException, YangSyntaxErrorException {
+        final BuildAction build = reactor.newBuild();
+        build.addSource(YangStatementStreamSource.create(
+            YangTextSchemaSource.forResource("/ietf-yang-metadata@2016-08-05.yang")));
+        build.addSource(YangStatementStreamSource.create(
+            YangTextSchemaSource.forResource("/example-last-modified.yang")));
+        final SchemaContext context = build.buildEffective();
+
+        final Map<QName, AnnotationSchemaNode> annotations = AnnotationSchemaNode.findAll(context);
+        assertEquals(1, annotations.size());
+        final AnnotationSchemaNode annotation = annotations.get(LAST_MODIFIED_QNAME);
+        assertNotNull(annotation);
+
+        final Optional<AnnotationSchemaNode> findAnnotation = AnnotationSchemaNode.find(context, LAST_MODIFIED_QNAME);
+        assertTrue(findAnnotation.isPresent());
+        assertSame(annotation, findAnnotation.get());
+
+        assertEquals(BaseTypes.stringType(), annotation.getType());
+        assertEquals(Optional.empty(), annotation.getReference());
+        assertEquals(Optional.of("This annotation contains the date and time when the\n"
+                + "           annotated instance was last modified (or created)."), annotation.getDescription());
+    }
+}
