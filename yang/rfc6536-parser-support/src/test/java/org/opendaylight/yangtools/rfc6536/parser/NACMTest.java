6b155e8487cfe198f129da9a5a957024de6fbac2
Robert Varga <robert.varga@pantheon.tech>
2017-11-03 23:05:10 +0100

    Add support for RFC6536 extensions
    This patch adds support for parsing RFC6536 extensions and related
    concepts for end users.
    
    Change-Id: I41ae3192b74660be409c63bbec82a1a14c7de4af
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/rfc6536-parser-support/src/test/java/org/opendaylight/yangtools/rfc6536/parser/NACMTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc6536.parser;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.opendaylight.yangtools.rfc6536.model.api.DefaultDenyAllSchemaNode;
+import org.opendaylight.yangtools.rfc6536.model.api.DefaultDenyWriteSchemaNode;
+import org.opendaylight.yangtools.rfc6536.model.api.NACMConstants;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.parser.api.YangSyntaxErrorException;
+import org.opendaylight.yangtools.yang.model.repo.api.YangTextSchemaSource;
+import org.opendaylight.yangtools.yang.parser.rfc6020.repo.YangStatementStreamSource;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ModelProcessingPhase;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.stmt.reactor.CrossSourceStatementReactor;
+import org.opendaylight.yangtools.yang.parser.stmt.reactor.CrossSourceStatementReactor.BuildAction;
+import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.ext.CustomStatementParserBuilder;
+
+public class NACMTest {
+    private static CrossSourceStatementReactor reactor;
+
+    @BeforeClass
+    public static void createReactor() {
+        reactor = new CustomStatementParserBuilder()
+                .addDefaultRFC6020Bundles()
+                .addAllCommonStatementSupports(ModelProcessingPhase.FULL_DECLARATION, NACM.getStatements())
+                .build();
+    }
+
+    @Test
+    public void testResolution() throws ReactorException, IOException, YangSyntaxErrorException {
+        final BuildAction build = reactor.newBuild();
+        build.addSource(YangStatementStreamSource.create(
+            YangTextSchemaSource.forResource("/ietf-netconf-acm@2012-02-22.yang")));
+        build.addSource(YangStatementStreamSource.create(
+            YangTextSchemaSource.forResource("/ietf-yang-types@2013-07-15.yang")));
+        final SchemaContext context = build.buildEffective();
+
+        final Module module = context.findModule(NACMConstants.RFC6536_MODULE).get();
+        final DataSchemaNode nacm = module.findDataChildByName(QName.create(NACMConstants.RFC6536_MODULE, "nacm"))
+                .get();
+        assertTrue(DefaultDenyAllSchemaNode.findIn(nacm).isPresent());
+        assertFalse(DefaultDenyWriteSchemaNode.findIn(nacm).isPresent());
+    }
+}
