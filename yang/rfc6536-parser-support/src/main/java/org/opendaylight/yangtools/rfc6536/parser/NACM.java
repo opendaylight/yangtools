79a06982cd84ff99a7c7a9339e6b284286b5f01b
Robert Varga <robert.varga@pantheon.tech>
2017-11-03 23:05:10 +0100

    Add support for RFC6536 extensions
    This patch adds support for parsing RFC6536 extensions and related
    concepts for end users.
    
    Change-Id: I41ae3192b74660be409c63bbec82a1a14c7de4af
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/rfc6536-parser-support/src/main/java/org/opendaylight/yangtools/rfc6536/parser/NACM.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc6536.parser;
+
+import com.google.common.annotations.Beta;
+import com.google.common.collect.ImmutableList;
+import java.util.Collection;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StatementSupport;
+
+/**
+ * Primary entry point into this implementation. Use {@link #getStatements()} to acquire a collection of
+ * {@link StatementSupport} instances, which need to be registered with the parser.
+ *
+ * @author Robert Varga
+ */
+@Beta
+public final class NACM {
+
+    private NACM() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Return a collection of statements which need to be added into parser reactor to enable support for NACM.
+     *
+     * @return Collection of statements to be added.
+     */
+    public static Collection<StatementSupport<?, ?, ?>> getStatements() {
+        return ImmutableList.of(DefaultDenyAll.getInstance(), DefaultDenyWrite.getInstance());
+    }
+}
