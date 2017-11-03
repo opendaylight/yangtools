42e3c5fb3671957033567ef13de9802c5bec4d94
Robert Varga <robert.varga@pantheon.tech>
2017-11-04 00:50:28 +0100

    YANGTOOLS-619: Add support for RFC7952 extensions
    This patch adds support for md:annotation statement.
    
    Change-Id: I912645c0e01662594346e107766dd86f704f0cca
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/rfc7952-model-api/src/main/java/org/opendaylight/yangtools/rfc7952/model/api/MetadataConstants.java
@@ -0,0 +1,49 @@
+package org.opendaylight.yangtools.rfc7952.model.api;
+
+import com.google.common.collect.ImmutableList;
+import java.net.URI;
+import java.util.Collection;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.common.Revision;
+import org.opendaylight.yangtools.yang.model.repo.api.RevisionSourceIdentifier;
+import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
+
+/**
+ * Constants associated with RFC7952.
+ *
+ * @author Robert Varga
+ */
+public final class MetadataConstants {
+    private static final String MODULE_NAME = "ietf-yang-metadata";
+    private static final URI MODULE_NAMESPACE = URI.create("urn:ietf:params:xml:ns:yang:ietf-yang-metadata");
+    private static final Revision RFC7952_REVISION = Revision.of("2016-08-05");
+
+    /**
+     * Runtime RFC6536 identity.
+     */
+    public static final QNameModule RFC7952_MODULE = QNameModule.create(MODULE_NAMESPACE, RFC7952_REVISION).intern();
+
+    /**
+     * RFC6536 model source name.
+     */
+    public static final SourceIdentifier RFC7952_SOURCE = RevisionSourceIdentifier.create(MODULE_NAME,
+        RFC7952_REVISION);
+
+    /**
+     * Normative prefix to use when importing {@link #RFC7952_SOURCE}.
+     */
+    public static final String MODULE_PREFIX = "md";
+
+    private MetadataConstants() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Return identifiers of all sources known to define the metadata extension.
+     *
+     * @return Collection of identifiers.
+     */
+    public static Collection<SourceIdentifier> knownModelSources() {
+        return ImmutableList.of(RFC7952_SOURCE);
+    }
+}
