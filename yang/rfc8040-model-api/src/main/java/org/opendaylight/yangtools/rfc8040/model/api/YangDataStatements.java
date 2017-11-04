d2da08814902e909d3d8a71cfbde1add89029585
Robert Varga <robert.varga@pantheon.tech>
2017-11-04 16:44:58 +0100

    YANGTOOLS-706: separate out yang-data StametementDefinition
    SupportedExtensions mapping is too central for what we want to achieve.
    Split out yang-data StatementDefinition into its own model world, so
    others can point to it without accessing parser internals.
    
    Also co-locate the implementation of YangDataStatement with its support,
    so they can be easily transported.
    
    Change-Id: Ic7ddfed705da5a0e9e3b20eecb333bb69b3e831c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/rfc8040-model-api/src/main/java/org/opendaylight/yangtools/rfc8040/model/api/YangDataStatements.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8040.model.api;
+
+import static java.util.Objects.requireNonNull;
+
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
+
+/**
+ * {@link StatementDefinition}s for statements defined by RFC8040.
+ *
+ * @author Robert Varga
+ */
+public enum YangDataStatements implements StatementDefinition {
+    YANG_DATA(QName.create(YangDataConstants.RFC8040_MODULE, "yang-data"), "name", YangDataStatement.class,
+        YangDataEffectiveStatement.class);
+
+    private final Class<? extends EffectiveStatement<?, ?>> effectiveRepresentation;
+    private final Class<? extends DeclaredStatement<?>> declaredRepresentation;
+    private final QName statementName;
+    private final QName argumentName;
+
+    private YangDataStatements(final QName statementName, final String argumentName,
+            final Class<? extends DeclaredStatement<?>> declaredRepresentation,
+                    final Class<? extends EffectiveStatement<?, ?>> effectiveRepresentation) {
+        this.statementName = statementName.intern();
+        this.argumentName = QName.create(statementName, argumentName);
+        this.declaredRepresentation = requireNonNull(declaredRepresentation);
+        this.effectiveRepresentation = requireNonNull(effectiveRepresentation);
+    }
+
+    @Override
+    public QName getArgumentName() {
+        return argumentName;
+    }
+
+    @Override
+    public boolean isArgumentYinElement() {
+        return true;
+    }
+
+    @Override
+    public QName getStatementName() {
+        return statementName;
+    }
+
+    @Override
+    public Class<? extends EffectiveStatement<?, ?>> getEffectiveRepresentationClass() {
+        return effectiveRepresentation;
+    }
+
+    @Override
+    public Class<? extends DeclaredStatement<?>> getDeclaredRepresentationClass() {
+        return declaredRepresentation;
+    }
+}
