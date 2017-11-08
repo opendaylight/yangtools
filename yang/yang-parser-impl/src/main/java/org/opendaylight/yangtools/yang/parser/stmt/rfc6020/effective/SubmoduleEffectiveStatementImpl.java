45ed8a84b91c06c5b95350ec9c387fc479f1ef9d
Robert Varga <robert.varga@pantheon.tech>
2017-11-08 23:48:27 +0100

    Reduce inter-statement implementation coupling
    Now that we have proper model.api.stmt interfaces we can reduce
    reliance on effective statement implementation classes, leading
    a more streamlined implementation.
    
    This patch does not eliminate all of the reliance, as that requires
    updating model.api.stmt interfaces to carry enough information
    to allow cross-statement inference.
    
    Change-Id: Ib39d8578c954a6e8ecede53ce5a7420e7dd0f2aa
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/SubmoduleEffectiveStatementImpl.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/rfc6020/effective/SubmoduleEffectiveStatementImpl.java
@@ -13,6 +13,7 @@ import java.util.Objects;
 import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.BelongsToStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.RevisionEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.SubmoduleEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.SubmoduleStatement;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
@@ -30,7 +31,7 @@ public final class SubmoduleEffectiveStatementImpl extends AbstractEffectiveModu
         final String belongsToModuleName = firstAttributeOf(ctx.declaredSubstatements(), BelongsToStatement.class);
         final QNameModule belongsToModuleQName = ctx.getFromNamespace(ModuleNameToModuleQName.class,
                 belongsToModuleName);
-        final RevisionEffectiveStatementImpl submoduleRevision = firstEffective(RevisionEffectiveStatementImpl.class);
+        final RevisionEffectiveStatement submoduleRevision = firstEffective(RevisionEffectiveStatement.class);
         this.qnameModule = QNameModule.create(belongsToModuleQName.getNamespace(),
             submoduleRevision == null ? null : submoduleRevision.argument()).intern();
     }
