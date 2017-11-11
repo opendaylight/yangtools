2a03258afd2b0e2baa2010b4c7e77a1439f3dd49
Robert Varga <robert.varga@pantheon.tech>
2017-11-11 14:00:06 +0100

    Move String/NumericalRestrictions
    These 4 classes are not used anywhere, but if they are to be used,
    it will be in rfc7950.stmt.type package. Move them there, factor
    them out to follow package convensions and hide them.
    
    This eliminates the rfc6020.effective.type package.
    
    Change-Id: Ib2dae96b152108fb0550b6f07625bd6b7854154b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/type/StringRestrictionsEffectiveStatement.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/type/StringRestrictionsEffectiveStatement.java
@@ -5,16 +5,16 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.yangtools.yang.parser.stmt.rfc6020.effective.type;
+package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.type;
 
-import org.opendaylight.yangtools.yang.model.api.stmt.TypeStatement.NumericalRestrictions;
+import org.opendaylight.yangtools.yang.model.api.stmt.TypeStatement.StringRestrictions;
 import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.DeclaredEffectiveStatementBase;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 
-public class NumericalRestrictionsEffectiveStatementImpl
-        extends DeclaredEffectiveStatementBase<String, NumericalRestrictions> {
-
-    public NumericalRestrictionsEffectiveStatementImpl(final StmtContext<String, NumericalRestrictions, ?> ctx) {
+// FIXME: this class is not used anywhere, decide its future
+final class StringRestrictionsEffectiveStatement
+        extends DeclaredEffectiveStatementBase<String, StringRestrictions> {
+    StringRestrictionsEffectiveStatement(final StmtContext<String, StringRestrictions, ?> ctx) {
         super(ctx);
     }
 }
