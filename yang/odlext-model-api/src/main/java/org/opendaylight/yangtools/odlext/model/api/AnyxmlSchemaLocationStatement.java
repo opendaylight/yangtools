78a636d91db42c656c2fd110d143f8736bf01ad7
Robert Varga <robert.varga@pantheon.tech>
2017-11-11 16:36:33 +0100

    Split out OpenDaylight yang-ext metamodel
    AnyxmlSchemaLocation is an unpublished extensions, which should
    not be part of yang-model-api. Split it out into a separate metamodel
    extension.
    
    Also clean up parser support, which unfortunately needs to be integrated
    due to how the SchemaNode instance is overridden.
    
    Change-Id: Ied1688b82505cd34bfe3151f8686d697f81787ec
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/odlext-model-api/src/main/java/org/opendaylight/yangtools/odlext/model/api/AnyxmlSchemaLocationStatement.java
+++ yang/odlext-model-api/src/main/java/org/opendaylight/yangtools/odlext/model/api/AnyxmlSchemaLocationStatement.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.yangtools.yang.parser.stmt.anyxmlschema;
+package org.opendaylight.yangtools.odlext.model.api;
 
 import com.google.common.annotations.Beta;
 import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/odlext/stmt/AnyxmlSchemaLocationSupport.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/odlext/stmt/AnyxmlSchemaLocationSupport.java
@@ -5,26 +5,29 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.yangtools.yang.parser.stmt.anyxmlschema;
+package org.opendaylight.yangtools.yang.parser.odlext.stmt;
 
+import static org.opendaylight.yangtools.odlext.model.api.OpenDaylightExtensionsStatements.ANYXML_SCHEMA_LOCATION;
+
+import org.opendaylight.yangtools.odlext.model.api.AnyxmlSchemaLocationStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
+import org.opendaylight.yangtools.yang.parser.odlext.namespace.AnyxmlSchemaLocationNamespace;
 import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.ArgumentUtils;
 import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractStatementSupport;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext.Mutable;
 import org.opendaylight.yangtools.yang.parser.spi.meta.SubstatementValidator;
-import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.SupportedExtensionsMapping;
 
 public final class AnyxmlSchemaLocationSupport
         extends AbstractStatementSupport<SchemaNodeIdentifier, AnyxmlSchemaLocationStatement,
             EffectiveStatement<SchemaNodeIdentifier, AnyxmlSchemaLocationStatement>> {
     private static final SubstatementValidator SUBSTATEMENT_VALIDATOR = SubstatementValidator.builder(
-        SupportedExtensionsMapping.ANYXML_SCHEMA_LOCATION).build();
+        ANYXML_SCHEMA_LOCATION).build();
     private static final AnyxmlSchemaLocationSupport INSTANCE = new AnyxmlSchemaLocationSupport();
 
     private AnyxmlSchemaLocationSupport() {
-        super(SupportedExtensionsMapping.ANYXML_SCHEMA_LOCATION);
+        super(ANYXML_SCHEMA_LOCATION);
     }
 
     public static AnyxmlSchemaLocationSupport getInstance() {
@@ -40,8 +43,7 @@ public final class AnyxmlSchemaLocationSupport
     public void onFullDefinitionDeclared(final Mutable<SchemaNodeIdentifier, AnyxmlSchemaLocationStatement,
             EffectiveStatement<SchemaNodeIdentifier, AnyxmlSchemaLocationStatement>> stmt) {
         super.onFullDefinitionDeclared(stmt);
-        stmt.getParentContext().addToNs(AnyxmlSchemaLocationNamespace.class,
-                SupportedExtensionsMapping.ANYXML_SCHEMA_LOCATION, stmt);
+        stmt.getParentContext().addToNs(AnyxmlSchemaLocationNamespace.class, ANYXML_SCHEMA_LOCATION, stmt);
     }
 
     @Override
