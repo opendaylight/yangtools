924471a8a55fc18fb4d713e8314baabdf24e6359
Robert Varga <robert.varga@pantheon.tech>
2017-11-11 14:13:53 +0100

    Split out TypeUtils.parse{Length,Range}ListFromString
    Each of these has a natural place in the respective type's
    StatementSupport. Move them out, leaving common items in
    TypeUtils for now.
    
    Change-Id: Id879ab588170ec90a74196f8d0d16f37b54f85c2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/range/RangeStatementSupport.java
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/range/RangeStatementSupport.java
@@ -7,14 +7,22 @@
  */
 package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.range;
 
+import com.google.common.collect.Iterables;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import org.opendaylight.yangtools.yang.model.api.YangStmtMapping;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.RangeStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.UnresolvedNumber;
 import org.opendaylight.yangtools.yang.model.api.stmt.ValueRange;
 import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractStatementSupport;
+import org.opendaylight.yangtools.yang.parser.spi.meta.InferenceException;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 import org.opendaylight.yangtools.yang.parser.spi.meta.SubstatementValidator;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
 import org.opendaylight.yangtools.yang.parser.stmt.rfc6020.TypeUtils;
 
 public final class RangeStatementSupport extends AbstractStatementSupport<List<ValueRange>, RangeStatement,
@@ -32,8 +40,35 @@ public final class RangeStatementSupport extends AbstractStatementSupport<List<V
     }
 
     @Override
-    public List<ValueRange> parseArgumentValue(final StmtContext<?, ?, ?> ctx, final String value) {
-        return TypeUtils.parseRangeListFromString(ctx, value);
+    public List<ValueRange> parseArgumentValue(final StmtContext<?, ?, ?> ctx, final String rangeArgument) {
+        final List<ValueRange> ranges = new ArrayList<>();
+
+        for (final String singleRange : TypeUtils.PIPE_SPLITTER.split(rangeArgument)) {
+            final Iterator<String> boundaries = TypeUtils.TWO_DOTS_SPLITTER.split(singleRange).iterator();
+            final Number min = parseDecimalConstraintValue(ctx, boundaries.next());
+
+            final Number max;
+            if (boundaries.hasNext()) {
+                max = parseDecimalConstraintValue(ctx, boundaries.next());
+
+                // if min larger than max then error
+                SourceException.throwIf(TypeUtils.compareNumbers(min, max) == 1, ctx.getStatementSourceReference(),
+                        "Range constraint %s has descending order of boundaries; should be ascending", singleRange);
+                SourceException.throwIf(boundaries.hasNext(), ctx.getStatementSourceReference(),
+                    "Wrong number of boundaries in range constraint %s", singleRange);
+            } else {
+                max = min;
+            }
+
+            // some of intervals overlapping
+            InferenceException.throwIf(ranges.size() > 1
+                && TypeUtils.compareNumbers(min, Iterables.getLast(ranges).upperBound()) != 1,
+                ctx.getStatementSourceReference(),  "Some of the value ranges in %s are not disjoint",
+                rangeArgument);
+            ranges.add(ValueRange.of(min, max));
+        }
+
+        return ranges;
     }
 
     @Override
@@ -51,5 +86,21 @@ public final class RangeStatementSupport extends AbstractStatementSupport<List<V
     @Override
     protected SubstatementValidator getSubstatementValidator() {
         return SUBSTATEMENT_VALIDATOR;
+    }
+
+    private static Number parseDecimalConstraintValue(final StmtContext<?, ?, ?> ctx, final String value) {
+        if ("max".equals(value)) {
+            return UnresolvedNumber.max();
+        }
+        if ("min".equals(value)) {
+            return UnresolvedNumber.min();
+        }
+
+        try {
+            return value.indexOf('.') != -1 ? new BigDecimal(value) : new BigInteger(value);
+        } catch (final NumberFormatException e) {
+            throw new SourceException(String.format("Value %s is not a valid decimal number", value),
+                    ctx.getStatementSourceReference(), e);
+        }
     }
 }
