77e50131ecff6d66bd856947fb1f7cf06eb9d1fd
Robert Varga <robert.varga@pantheon.tech>
2017-11-11 23:27:22 +0100

    YANGTOOLS-829: add parser support for openconfig-hashed-value
    This patch adds metamodel and parsing support for openconfig-hashed-value
    and its predecessor, openconfig-encrypted-value.
    
    Change-Id: Iac44d2e34a9b4ccc931ddb69e3d6709b962e7378
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/openconfig-parser-support/src/main/java/org/opendaylight/yangtools/openconfig/parser/package-info.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * YANG parser support for metamodel extensions defined in
+ * <a href="https://github.com/openconfig/public/blob/master/release/models/openconfig-extensions.yang">OpenConfig</a>.
+ *
+ * <p>
+ * The openconfig-version extension is integrated in the parser, as it supports the semantic version import resoltion,
+ * for other extensions use {@link OpenConfig#getStatements()}.
+ *
+ * @author Robert Varga
+ */
+package org.opendaylight.yangtools.openconfig.parser;
