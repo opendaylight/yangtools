309defd2b34b240782b2b374ac83437ade2b2ee5
Robert Varga <robert.varga@pantheon.tech>
2017-11-12 21:18:12 +0100

    YANGTOOLS-706: move odlext parser support
    Use composition with a delegator pattern to implement
    YangModeledAnyxmlEffectiveStatementImpl. Also adjust the pattern
    used to bring in extensions. This forces them to be be aware
    of yang-parser-rfc7950, which is fine, as that is the reactor
    they can generally expect to be configured.
    
    Change-Id: I91aa95221efb76753d9ea82dc164c1fffc6755c7
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/odlext-parser-support/src/main/java/org/opendaylight/yangtools/odlext/parser/package-info.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * YANG parser support for OpenDaylight extensions as defined in yang-ext.yang.
+ *
+ * <p>
+ * To access this extension, add {@link org.opendaylight.yangtools.odlext.parser.AnyxmlSchemaLocationStatementSupport},
+ * {@link org.opendaylight.yangtools.odlext.parser.AnyxmlStatementSupportOverride} and
+ * {@link org.opendaylight.yangtools.odlext.parser.AnyxmlSchemaLocationNamespace#BEHAVIOR} to the reactor.
+ *
+ * @author Robert Varga
+ */
+package org.opendaylight.yangtools.odlext.parser;
