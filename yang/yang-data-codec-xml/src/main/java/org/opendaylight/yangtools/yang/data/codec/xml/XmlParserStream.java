55495dfa369d2fa7259afd5d9246972c7d3a2a6a
Robert Varga <robert.varga@pantheon.tech>
2017-11-20 14:13:31 +0100

    YANGTOOLS-813: add parent schemapath to error report
    It is not immediately clear in what context the search is being
    made -- make sure we report parent path, too.
    
    Change-Id: I86a15b409c6241d93f24ee75cc68ff78fc067ebc
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-data-codec-xml/src/main/java/org/opendaylight/yangtools/yang/data/codec/xml/XmlParserStream.java
+++ yang/yang-data-codec-xml/src/main/java/org/opendaylight/yangtools/yang/data/codec/xml/XmlParserStream.java
@@ -356,8 +356,8 @@ public final class XmlParserStream implements Closeable, Flushable {
                                     new URI(xmlElementNamespace));
 
                     if (childDataSchemaNodes.isEmpty()) {
-                        checkState(!strictParsing, "Schema for node with name %s and namespace %s doesn't exist.",
-                            xmlElementName, xmlElementNamespace);
+                        checkState(!strictParsing, "Schema for node with name %s and namespace %s doesn't exist at %s",
+                            xmlElementName, xmlElementNamespace, parentSchema.getPath());
                         skipUnknownNode(in);
                         continue;
                     }
