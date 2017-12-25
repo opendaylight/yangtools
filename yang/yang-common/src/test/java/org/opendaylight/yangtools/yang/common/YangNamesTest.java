a628259b96f120ec4dc9a8b9e95f786d3070a713
Robert Varga <robert.varga@pantheon.tech>
2017-12-25 15:29:17 +0100

    Add YangNames test
    This adds a simplistic test to ensure YangNames.parseFileName() works
    as expected.
    
    Change-Id: Ic619ebdce2a0641418283b4621be4c676341a053
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-common/src/test/java/org/opendaylight/yangtools/yang/common/YangNamesTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.common;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.AbstractMap.SimpleImmutableEntry;
+import org.junit.Test;
+
+public class YangNamesTest {
+    @Test
+    public void testParseFileName() {
+        assertEquals(new SimpleImmutableEntry<>("foo", null), YangNames.parseFilename("foo"));
+        assertEquals(new SimpleImmutableEntry<>("foo", "bar"), YangNames.parseFilename("foo@bar"));
+        assertEquals(new SimpleImmutableEntry<>("foo@bar", "baz"), YangNames.parseFilename("foo@bar@baz"));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testParseFileNameNull() {
+        YangNames.parseFilename(null);
+    }
+}
