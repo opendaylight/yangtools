7488666fb1146119ba8ce644d6b2cb12ad166518
Robert Varga <robert.varga@pantheon.tech>
2017-12-27 02:00:26 +0100

    YANGTOOLS-766: add RFC7951JSONInstanceIdentifierCodec
    RFC7951 non-present prefixes result in last argument namespace reuse,
    hence refactor AbstractStringInstanceIdentifierCodec to allow for QNames
    to be instantiated using a memoized QNameModule.
    
    The default implementation routes to createQName("", localName), which
    preserves backwards compatibility. A specialized subclass
    of JSONInstanceIdetifierCodec is created to use memoized QNameModule,
    and it is wired to RFC7951 codec supplier.
    
    A similar update is done in AbstractNamespaceCodec, where appendQName()
    alternative, which is forwarded the last encountered QNameModule is
    added -- the default routes to normal appendQName(), with RFC7951
    comparing last QNameModule with current QName's and skipping prefix
    serialization if they match.
    
    This is sufficient to make related unit test pass, so remove @Ignore
    from it and expand it to assert the decoded YangInstanceIdentifier.
    
    Change-Id: Iec7996d0e1d759623c3cabcbaf0164737afa1649
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 88ed3390d89256f1857d921ccb7766ffe4b04555)

--- yang/yang-data-util/src/main/java/org/opendaylight/yangtools/yang/data/util/XpathStringParsingPathArgumentBuilder.java
+++ yang/yang-data-util/src/main/java/org/opendaylight/yangtools/yang/data/util/XpathStringParsingPathArgumentBuilder.java
@@ -12,10 +12,10 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import org.opendaylight.yangtools.concepts.Builder;
 import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeWithValue;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
@@ -29,7 +29,7 @@ import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgum
  * is not correctly serialized or does not represent instance identifier valid
  * for associated schema context.
  */
-class XpathStringParsingPathArgumentBuilder implements Builder<Collection<PathArgument>> {
+final class XpathStringParsingPathArgumentBuilder implements Builder<List<PathArgument>> {
 
     /**
      * Matcher matching WSP YANG ABNF token.
@@ -57,12 +57,12 @@ class XpathStringParsingPathArgumentBuilder implements Builder<Collection<PathAr
     private static final char PRECONDITION_START = '[';
     private static final char PRECONDITION_END = ']';
 
+    private final List<PathArgument> product = new ArrayList<>();
     private final AbstractStringInstanceIdentifierCodec codec;
     private final String data;
 
-    private final List<PathArgument> product = new ArrayList<>();
-
     private DataSchemaContextNode<?> current;
+    private QNameModule lastModule;
     private int offset;
 
     XpathStringParsingPathArgumentBuilder(final AbstractStringInstanceIdentifierCodec codec, final String data) {
@@ -73,7 +73,7 @@ class XpathStringParsingPathArgumentBuilder implements Builder<Collection<PathAr
     }
 
     @Override
-    public Collection<PathArgument> build() {
+    public List<PathArgument> build() {
         while (!allCharactersConsumed()) {
             product.add(computeNextArgument());
         }
@@ -84,7 +84,9 @@ class XpathStringParsingPathArgumentBuilder implements Builder<Collection<PathAr
         checkValid(SLASH == currentChar(), "Identifier must start with '/'.");
         skipCurrentChar();
         checkValid(!allCharactersConsumed(), "Identifier cannot end with '/'.");
-        QName name = nextQName();
+        final QName name = nextQName();
+        // Memoize module
+        lastModule = name.getModule();
         if (allCharactersConsumed() || SLASH == currentChar()) {
             return computeIdentifier(name);
         }
@@ -168,18 +170,13 @@ class XpathStringParsingPathArgumentBuilder implements Builder<Collection<PathAr
     private QName nextQName() {
         // Consume prefix or identifier
         final String maybePrefix = nextIdentifier();
-        final String prefix;
-        final String localName;
-        if (COLON == currentChar()) {
-            // previous token is prefix;
-            prefix = maybePrefix;
+        if (!allCharactersConsumed() && COLON == currentChar()) {
+            // previous token is prefix
             skipCurrentChar();
-            localName = nextIdentifier();
-        } else {
-            prefix = "";
-            localName = maybePrefix;
+            return codec.createQName(maybePrefix, nextIdentifier());
         }
-        return codec.createQName(prefix, localName);
+
+        return codec.createQName(lastModule, maybePrefix);
     }
 
     /**
