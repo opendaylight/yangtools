d027a59601cfbbc912e00e351eeb35a27dcc3bc2
Robert Varga <robert.varga@pantheon.tech>
2018-01-02 11:23:44 +0100

    YANGTOOLS-841: Fix SchemaContextUtil.findNodeInSchemaContext()
    When performing recursive node lookup, consider
    {Action,Notification}NodeContainers, so that we correctly resolved
    nested actions and notifications -- which are new elements in RFC7950
    metamodel.
    
    Also generalized RPC input/output lookup to work on actions, too.
    
    Change-Id: I877061c7f0370caf1ed04eb9420a43af3b2d3915
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/stmt/YT841Test.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.net.URI;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.SchemaNode;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.util.SchemaContextUtil;
+
+public class YT841Test {
+    private static final QNameModule FOO = QNameModule.create(URI.create("foo"), QName.parseRevision("2018-01-02"));
+
+    @Test
+    public void testFindDataSchemaNode() throws Exception {
+        final SchemaContext context = StmtTestUtils.parseYangSources("/bugs/YT841/");
+        final Module foo = context.findModuleByNamespace(URI.create("foo")).iterator().next();
+        assertNotNull(foo);
+
+        final SchemaNode target = SchemaContextUtil.findDataSchemaNode(context, SchemaPath.create(true,
+            QName.create(FOO, "foo"),
+            QName.create(FOO, "foo"),
+            QName.create(FOO, "foo"),
+            QName.create(FOO, "input")));
+        assertNotNull(target);
+
+    }
+}
