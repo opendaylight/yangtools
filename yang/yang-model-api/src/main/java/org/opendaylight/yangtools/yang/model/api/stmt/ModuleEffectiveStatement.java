d4f4fa39c4b8b07a39d8575888e288d614da8edc
Robert Varga <robert.varga@pantheon.tech>
2018-02-03 13:35:24 +0100

    Expose EffectiveModuleStatement prefix mapping
    ModuleEffectiveStatement should expose mapping of prefixes
    to imported modules (and self) as well as reverse mapping of
    QNameModule to prefix, so that users can map properly map
    QNames encountered within a module.
    
    This patch exposes that information via two new namespaces,
    which are present only in ModuleEffectiveStatements. Furthermore
    EffectiveStatementBase is updated to allow subclasses to contain
    namespaces.
    
    JIRA: MDSAL-301
    Change-Id: I5139c32eff18c1fa703dd8d3c98882939f82b109
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/ModuleEffectiveStatement.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/ModuleEffectiveStatement.java
@@ -8,9 +8,39 @@
 package org.opendaylight.yangtools.yang.model.api.stmt;
 
 import com.google.common.annotations.Beta;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.IdentifierNamespace;
 
 @Beta
 public interface ModuleEffectiveStatement extends EffectiveStatement<String, ModuleStatement> {
+    /**
+     * Namespace mapping all known prefixes in a module to their modules. Note this namespace includes the module
+     * in which it is instantiated.
+     */
+    abstract class PrefixToEffectiveModuleNamespace implements IdentifierNamespace<String, ModuleEffectiveStatement> {
+        private PrefixToEffectiveModuleNamespace() {
+            // This class should never be subclassed
+        }
+    }
 
+    /**
+     * Namespace mapping all known {@link QNameModule}s to their encoding prefixes. This includes the declaration
+     * from prefix/namespace/revision and all imports as they were resolved.
+     */
+    abstract class QNameModuleToPrefixNamespace implements IdentifierNamespace<QNameModule, String> {
+        private QNameModuleToPrefixNamespace() {
+            // This class should never be subclassed
+        }
+    }
+
+    /**
+     * Get the local QNameModule of this module. All implementations need to override this default method.
+     *
+     * @return Local QNameModule
+     */
+    default @NonNull QNameModule localQNameModule() {
+        throw new UnsupportedOperationException(getClass() + " is missing an implementation");
+    }
 }
