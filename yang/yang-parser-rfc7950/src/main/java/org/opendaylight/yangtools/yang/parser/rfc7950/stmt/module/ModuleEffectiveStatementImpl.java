66e05815ac820e1b248511f9b9c5f295a32f2887
Robert Varga <robert.varga@pantheon.tech>
2018-02-03 13:35:24 +0100

    Expose EffectiveModuleStatement prefix mapping
    ModuleEffectiveStatement should expose mapping of prefixes
    to imported modules (and self) as well as reverse mapping of
    QNameModule to prefix, so that users can map properly map
    QNames encountered within a module.
    
    This patch exposes that information via two new namespaces,
    which are present only in ModuleEffectiveStatements. Furthermore
    EffectiveStatementBase is updated to allow subclasses to contain
    namespaces.
    
    Change-Id: I5139c32eff18c1fa703dd8d3c98882939f82b109
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/module/ModuleEffectiveStatementImpl.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/module/ModuleEffectiveStatementImpl.java
@@ -7,29 +7,77 @@
  */
 package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.module;
 
-import com.google.common.base.Verify;
+import static com.google.common.base.Verify.verifyNotNull;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+import com.google.common.collect.Maps;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Objects;
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.IdentifierNamespace;
+import org.opendaylight.yangtools.yang.model.api.stmt.ImportEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.ModuleEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.ModuleStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.PrefixEffectiveStatement;
 import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractEffectiveModule;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+import org.opendaylight.yangtools.yang.parser.spi.source.ImportPrefixToModuleCtx;
 import org.opendaylight.yangtools.yang.parser.spi.source.ModuleCtxToModuleQName;
 
 final class ModuleEffectiveStatementImpl extends AbstractEffectiveModule<ModuleStatement>
         implements ModuleEffectiveStatement {
-    private final QNameModule qnameModule;
+    private final Map<String, ModuleEffectiveStatement> prefixToModule;
+    private final Map<QNameModule, String> namespaceToPrefix;
+    private final @NonNull QNameModule qnameModule;
 
     ModuleEffectiveStatementImpl(
             final StmtContext<String, ModuleStatement, EffectiveStatement<String, ModuleStatement>> ctx) {
         super(ctx);
-        qnameModule = Verify.verifyNotNull(ctx.getFromNamespace(ModuleCtxToModuleQName.class, ctx));
+        qnameModule = verifyNotNull(ctx.getFromNamespace(ModuleCtxToModuleQName.class, ctx));
+
+        final String localPrefix = findFirstEffectiveSubstatementArgument(PrefixEffectiveStatement.class).get();
+        final Builder<String, ModuleEffectiveStatement> prefixToModuleBuilder = ImmutableMap.builder();
+        prefixToModuleBuilder.put(localPrefix, this);
+        streamEffectiveSubstatements(ImportEffectiveStatement.class).map(ImportEffectiveStatement::argument)
+            .forEach(prefix -> prefixToModuleBuilder.put(prefix, (ModuleEffectiveStatement) ctx.getFromNamespace(
+                ImportPrefixToModuleCtx.class, prefix).buildEffective()));
+
+        prefixToModule = prefixToModuleBuilder.build();
+
+        final Map<QNameModule, String> tmp = Maps.newLinkedHashMapWithExpectedSize(prefixToModule.size() + 1);
+        tmp.put(qnameModule, localPrefix);
+        for (Entry<String, ModuleEffectiveStatement> e : prefixToModule.entrySet()) {
+            tmp.putIfAbsent(e.getValue().localQNameModule(), e.getKey());
+        }
+        namespaceToPrefix = ImmutableMap.copyOf(tmp);
     }
 
     @Override
-    public QNameModule getQNameModule() {
+    public @NonNull QNameModule localQNameModule() {
         return qnameModule;
+    }
+
+    @Override
+    public @NonNull QNameModule getQNameModule() {
+        return qnameModule;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <K, V, N extends IdentifierNamespace<K, V>> Optional<? extends Map<K, V>> getNamespaceContents(
+            final @NonNull Class<N> namespace) {
+        if (PrefixToEffectiveModuleNamespace.class.equals(namespace)) {
+            return Optional.of((Map<K, V>) prefixToModule);
+        }
+        if (QNameModuleToPrefixNamespace.class.equals(namespace)) {
+            return Optional.of((Map<K, V>) namespaceToPrefix);
+        }
+        return super.getNamespaceContents(namespace);
     }
 
     @Override
@@ -62,5 +110,4 @@ final class ModuleEffectiveStatementImpl extends AbstractEffectiveModule<ModuleS
         }
         return true;
     }
-
 }
