dbef81155ed6a9f611c8e8adeb1813f20d5c045b
Robert Varga <robert.varga@pantheon.tech>
2018-02-22 15:01:52 +0100

    Validate parsed QName to identity
    We need to validate if parsed QName refers to an existing identity
    before allowing it in Identityref codecs. Introduce IdentityCodecUtil
    along with a test suite and migrate users over.
    
    JIRA: YANGTOOLS-846
    Change-Id: I673ae6df4406110202b86329121a3ca9673fb0ed
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit e7e0e48e6a79b08bb2c976ee3fd69582d84a7e4c)

---
+++ yang/yang-data-util/src/main/java/org/opendaylight/yangtools/yang/data/util/codec/IdentityCodecUtil.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.util.codec;
+
+import static com.google.common.base.Preconditions.checkState;
+
+import com.google.common.annotations.Beta;
+import java.util.function.Function;
+import javax.annotation.concurrent.ThreadSafe;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.model.api.IdentitySchemaNode;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+/**
+ * Utility methods for implementing string-to-identity codecs.
+ *
+ * @author Robert Varga
+ */
+@Beta
+@NonNullByDefault
+@ThreadSafe
+public final class IdentityCodecUtil {
+
+    private IdentityCodecUtil() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Parse a string into a QName using specified prefix-to-QNameModule mapping function, interpreting the result
+     * as an IdentitySchemaNode existing in specified SchemaContext.
+     *
+     * @param value string value to parse
+     * @param schemaContext Parent schema context
+     * @param prefixToModule prefix-to-QNameModule mapping function
+     * @return Corresponding IdentitySchemaNode.
+     * @throws IllegalArgumentException if the value is invalid or does not refer to an existing identity
+     */
+    public static IdentitySchemaNode parseIdentity(final String value, final SchemaContext schemaContext,
+            final Function<String, QNameModule> prefixToModule) {
+        final QName qname = QNameCodecUtil.decodeQName(value, prefixToModule);
+        final Module module = schemaContext.findModuleByNamespaceAndRevision(qname.getNamespace(),
+            qname.getRevision());
+        checkState(module != null, "Parsed QName %s refers to a non-existent module", qname);
+
+        for (IdentitySchemaNode identity : module.getIdentities()) {
+            if (qname.equals(identity.getQName())) {
+                return identity;
+            }
+        }
+
+        throw new IllegalArgumentException("Parsed QName " + qname + " does not refer to a valid identity");
+    }
+}
