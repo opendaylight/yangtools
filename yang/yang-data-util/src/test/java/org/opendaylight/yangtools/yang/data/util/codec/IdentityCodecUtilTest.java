af3dd286f6921c4173ae3ef974517fc7866c0fc2
Robert Varga <robert.varga@pantheon.tech>
2018-02-22 15:01:52 +0100

    Validate parsed QName to identity
    We need to validate if parsed QName refers to an existing identity
    before allowing it in Identityref codecs. Introduce IdentityCodecUtil
    along with a test suite and migrate users over.
    
    JIRA: YANGTOOLS-846
    Change-Id: I673ae6df4406110202b86329121a3ca9673fb0ed
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit e7e0e48e6a79b08bb2c976ee3fd69582d84a7e4c)

---
+++ yang/yang-data-util/src/test/java/org/opendaylight/yangtools/yang/data/util/codec/IdentityCodecUtilTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.util.codec;
+
+import static com.google.common.base.Preconditions.checkState;
+import static org.junit.Assert.assertNotNull;
+
+import java.net.URI;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+public class IdentityCodecUtilTest {
+    private static final QNameModule MODULE = QNameModule.create(URI.create("yangtools846"));
+    private static SchemaContext SCHEMA_CONTEXT;
+
+    @BeforeClass
+    public static void init() {
+        SCHEMA_CONTEXT = YangParserTestUtils.parseYangResource("/yangtools846.yang");
+    }
+
+    @AfterClass
+    public static void cleanup() {
+        SCHEMA_CONTEXT = null;
+    }
+
+    @Test
+    public void testCorrectInput() {
+        assertNotNull(IdentityCodecUtil.parseIdentity("yt846:foo", SCHEMA_CONTEXT,
+            IdentityCodecUtilTest::resolvePrefix));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNonExistent() {
+        IdentityCodecUtil.parseIdentity("yt846:bar", SCHEMA_CONTEXT, IdentityCodecUtilTest::resolvePrefix);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testEmptyLocalName() {
+        IdentityCodecUtil.parseIdentity("yt846:", SCHEMA_CONTEXT, IdentityCodecUtilTest::resolvePrefix);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testEmptyString() {
+        IdentityCodecUtil.parseIdentity("", SCHEMA_CONTEXT, IdentityCodecUtilTest::resolvePrefix);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testNoPrefix() {
+        IdentityCodecUtil.parseIdentity("foo", SCHEMA_CONTEXT, IdentityCodecUtilTest::resolvePrefix);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testEmptyPrefix() {
+        IdentityCodecUtil.parseIdentity(":foo", SCHEMA_CONTEXT, IdentityCodecUtilTest::resolvePrefix);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testColon() {
+        IdentityCodecUtil.parseIdentity(":", SCHEMA_CONTEXT, IdentityCodecUtilTest::resolvePrefix);
+    }
+
+
+    private static QNameModule resolvePrefix(final String prefix) {
+        // TODO: QNameCodecUtil should deal with some of the malformed stuff here by throwing IAE. We throw an ISE
+        //       to discern what is happening.
+        checkState("yt846".equals(prefix), "Unexpected prefix %s", prefix);
+        return MODULE;
+    }
+}
