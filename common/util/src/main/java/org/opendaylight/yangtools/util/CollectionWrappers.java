a5e14894c2423601b03f74347ca6f62b956b36b1
Robert Varga <robert.varga@pantheon.tech>
2018-02-26 16:27:17 +0100

    Add CollectionWrappers
    SchemaNode/SchemaContext has some unfortunate method contracts
    which require returning a Set, while they are represented as
    java.util.Map.values() in EffectiveStatement world, where they
    support keyed lookups, which is really what users require.
    
    To bridge these two worlds without duplicating the same information
    in two collections we need a memory-efficient proxy to act as an
    adaptor, as we know the primary storage (Map.values()) is known
    to be both immutable and conforming to Set interface requirements.
    
    This patch such a class.
    
    JIRA: YANGTOOLS-853
    Change-Id: I052e4f9d58aa1f054450840c2216b0fd2109bc61
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ common/util/src/main/java/org/opendaylight/yangtools/util/CollectionWrappers.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.util;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Iterators;
+import java.util.AbstractList;
+import java.util.AbstractSet;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.Spliterator;
+import java.util.stream.Stream;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.concepts.Immutable;
+
+/**
+ * Utility class for adapting a {@link Collection}s to {@link Set}s and {@link List}s.
+ *
+ * @author Robert Varga
+ */
+@Beta
+@NonNullByDefault
+public final class CollectionWrappers {
+    private static final class ListWrapper<E> extends AbstractList<E> implements Immutable {
+        private final Collection<E> delegate;
+
+        private ListWrapper(final Collection<E> delegate) {
+            this.delegate = requireNonNull(delegate);
+        }
+
+        @Override
+        public Iterator<E> iterator() {
+            return Iterators.unmodifiableIterator(delegate.iterator());
+        }
+
+        @Override
+        public int size() {
+            return delegate.size();
+        }
+
+        @Override
+        public Spliterator<E> spliterator() {
+            return delegate.spliterator();
+        }
+
+        @Override
+        public Stream<E> parallelStream() {
+            return delegate.parallelStream();
+        }
+
+        @Override
+        public Stream<E> stream() {
+            return delegate.stream();
+        }
+
+        @Override
+        public E get(final int index) {
+            return Iterables.get(delegate, index);
+        }
+    }
+
+    private static final class SetWrapper<E> extends AbstractSet<E> implements Immutable {
+        private final Collection<E> delegate;
+
+        private SetWrapper(final Collection<E> delegate) {
+            this.delegate = requireNonNull(delegate);
+        }
+
+        @Override
+        public Iterator<E> iterator() {
+            return Iterators.unmodifiableIterator(delegate.iterator());
+        }
+
+        @Override
+        public int size() {
+            return delegate.size();
+        }
+
+        @Override
+        public Spliterator<E> spliterator() {
+            return delegate.spliterator();
+        }
+
+        @Override
+        public Stream<E> parallelStream() {
+            return delegate.parallelStream();
+        }
+
+        @Override
+        public Stream<E> stream() {
+            return delegate.stream();
+        }
+    }
+
+    private CollectionWrappers() {
+
+    }
+
+    /**
+     * Wrap the specified {@link Collection} as a {@link List}. If the collection is already a List, it is wrapped in
+     * a {@link Collections#unmodifiableList(List)} to prevent mutability leaking. If the collection is determined
+     * to be empty, an empty list is returned instead. If the collection is a known-immutable implementation of List
+     * interface, it is returned unwrapped. Backing collection is required to be effectively immutable. If this
+     * requirement is violated, the returned object may may behave in unpredictable ways.
+     *
+     * @param collection Collection to be wrapped
+     * @return An effectively-immutable wrapper of the collection.
+     * @throws NullPointerException if collection is null
+     */
+    public static <E> List<E> wrapAsList(final Collection<E> collection) {
+        if (collection.isEmpty()) {
+            return ImmutableList.of();
+        }
+        if (collection instanceof List) {
+            final List<E> cast = (List<E>) collection;
+            if (collection instanceof ListWrapper || collection instanceof ImmutableList) {
+                return cast;
+            }
+            return Collections.unmodifiableList(cast);
+        }
+
+        return new ListWrapper<>(collection);
+    }
+
+    /**
+     * Wrap the specified {@link Collection} as a {@link Set}. If the collection is already a Set, it is wrapped in
+     * a {@link Collections#unmodifiableSet(Set)} to prevent mutability leaking. If the collection is determined
+     * to be empty, an empty set is returned instead. If the collection is a known-immutable implementation of Set
+     * interface, it is returned unwrapped. The collection is checked for duplicates at instantiation time, such that
+     * it effectively implements the Set contract. Backing collection is required to be effectively immutable. If this
+     * requirement is violated, the returned object may may behave in unpredictable ways.
+     *
+     * @param collection Collection to be wrapped
+     * @return An effectively-immutable wrapper of the collection.
+     * @throws NullPointerException if collection is null or any of its elements is null
+     * @throws IllegalArgumentException if the collection's contents do not conform to the Set contract
+     */
+    public static <E> Set<E> wrapAsSet(final Collection<E> collection) {
+        if (collection.isEmpty()) {
+            return ImmutableSet.of();
+        }
+        if (collection instanceof Set) {
+            final Set<E> cast = (Set<E>) collection;
+            if (collection instanceof SetWrapper || collection instanceof SingletonSet
+                    || collection instanceof ImmutableSet) {
+                return cast;
+            }
+            return Collections.unmodifiableSet(cast);
+        }
+
+        final Set<E> check = ImmutableSet.copyOf(collection);
+        checkArgument(collection.size() == check.size(), "Supplied collection %s has duplicate elements", collection);
+        return new SetWrapper<>(collection);
+    }
+}
