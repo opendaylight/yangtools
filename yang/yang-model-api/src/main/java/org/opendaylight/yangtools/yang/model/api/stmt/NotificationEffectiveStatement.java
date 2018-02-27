5a97a38b9881a5df759db3c1c516381c8f68e154
Robert Varga <robert.varga@pantheon.tech>
2018-02-27 09:25:29 +0100

    Add {Data,Schema}TreeAwareffectiveStatement
    YANG defines two distinct ways to look at a YANG model:
    - schema tree: The definition hierarchy specified within a module.
    - data tree: An instantiated tree of any data modeled with YANG,
                 e.g., configuration data, state data, combined configuration and
                 state data, RPC or action input, RPC or action output, or
                 notification.
    
    This means that there really are two notions of 'a child node',
    based on schema tree, or based on data tree. The data tree is
    a strict subset of schema tree.
    
    This patch introduces both these concepts and retrofits the
    appropriate nodes to expose both of these views.
    
    JIRA: YANGTOOLS-853
    Change-Id: Id2c41bb30c4376a846e4e974033b6d621ecfb155
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/NotificationEffectiveStatement.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/NotificationEffectiveStatement.java
@@ -9,9 +9,10 @@ package org.opendaylight.yangtools.yang.model.api.stmt;
 
 import com.google.common.annotations.Beta;
 import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 
 @Beta
-public interface NotificationEffectiveStatement extends EffectiveStatement<QName, NotificationStatement> {
+public interface NotificationEffectiveStatement extends SchemaTreeEffectiveStatement<NotificationStatement>,
+    DataTreeAwareEffectiveStatement<QName, NotificationStatement>,
+    SchemaTreeAwareEffectiveStatement<QName, NotificationStatement> {
 
 }
