aa9ad915e0cc66f75395265912fb66ef2c540a2b
Robert Varga <robert.varga@pantheon.tech>
2018-02-27 09:25:29 +0100

    Add DataTreeAwareffectiveStatement
    YANG defines two distinct ways to look at a YANG model:
    - schema tree: The definition hierarchy specified within a module.
    - data tree: An instantiated tree of any data modeled with YANG,
                 e.g., configuration data, state data, combined configuration and
                 state data, RPC or action input, RPC or action output, or
                 notification.
    
    This means that there really are two notions of 'a child node',
    based on schema tree, or based on data tree. The data tree is
    a strict subset of schema tree.
    
    This patch introduces DataTreeAwareEffectiveStatement
    and its companion DataTreeEffectiveStatement, updating extant
    statements to support it.
    
    Construction of this namespace has a side-effect of finding
    duplicate data nodes, thus fixing YANGTOOLS-857.
    
    JIRA: YANGTOOLS-853
    JIRA: YANGTOOLS-857
    Change-Id: Id02bb2557845000fe7fa90d1bf56c8425cce24fb
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Jie Han <han.jie@zte.com.cn>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/AbstractSchemaEffectiveDocumentedNode.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/AbstractSchemaEffectiveDocumentedNode.java
@@ -8,15 +8,25 @@
 package org.opendaylight.yangtools.yang.parser.rfc7950.stmt;
 
 import com.google.common.annotations.Beta;
+import com.google.common.base.Functions;
 import com.google.common.collect.ImmutableMap;
+import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.IdentifierNamespace;
+import org.opendaylight.yangtools.yang.model.api.stmt.CaseEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ChoiceEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.DataTreeAwareEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.DataTreeEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeAwareEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeEffectiveStatement;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+import org.opendaylight.yangtools.yang.parser.spi.source.StatementSourceReference;
 
 /**
  * An {@link AbstractEffectiveDocumentedNode} which can optionally support {@link SchemaTreeAwareEffectiveStatement}.
@@ -28,7 +38,7 @@ import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 @Beta
 public abstract class AbstractSchemaEffectiveDocumentedNode<A, D extends DeclaredStatement<A>>
         extends AbstractEffectiveDocumentedNode<A, D> {
-
+    private final Map<QName, DataTreeEffectiveStatement<?>> dataTreeNamespace;
     private final Map<QName, SchemaTreeEffectiveStatement<?>> schemaTreeNamespace;
 
     protected AbstractSchemaEffectiveDocumentedNode(final StmtContext<A, D, ?> ctx) {
@@ -36,10 +46,57 @@ public abstract class AbstractSchemaEffectiveDocumentedNode<A, D extends Declare
 
         if (this instanceof SchemaTreeAwareEffectiveStatement) {
             schemaTreeNamespace = streamEffectiveSubstatements(SchemaTreeEffectiveStatement.class).collect(
-                ImmutableMap.toImmutableMap(SchemaTreeEffectiveStatement::getIdentifier, stmt -> stmt));
+                ImmutableMap.toImmutableMap(SchemaTreeEffectiveStatement::getIdentifier, Functions.identity()));
+
+            if (this instanceof DataTreeAwareEffectiveStatement && !schemaTreeNamespace.isEmpty()) {
+                final Map<QName, DataTreeEffectiveStatement<?>> dataChildren = new LinkedHashMap<>();
+                final boolean sameAsSchema = recursiveDataTreeChildren(ctx.getStatementSourceReference(), dataChildren,
+                    schemaTreeNamespace.values());
+
+                // This is a mighty hack to lower memory usage: if we consumed all schema tree children as data nodes,
+                // the two maps are equal and hence we can share the instance.
+                try {
+                    dataTreeNamespace = sameAsSchema ? (Map) schemaTreeNamespace : ImmutableMap.copyOf(dataChildren);
+                } catch (IllegalArgumentException e) {
+                    throw new SourceException("Conflicting data children detected", ctx.getStatementSourceReference(),
+                        e);
+                }
+            } else {
+                dataTreeNamespace = ImmutableMap.of();
+            }
         } else {
+            dataTreeNamespace = ImmutableMap.of();
             schemaTreeNamespace = ImmutableMap.of();
         }
+    }
+
+    private static boolean recursiveDataTreeChildren(final StatementSourceReference ref,
+            final Map<QName, DataTreeEffectiveStatement<?>> dataChildren,
+            final Collection<? extends SchemaTreeEffectiveStatement<?>> statements) {
+        boolean sameAsSchema = true;
+
+        for (SchemaTreeEffectiveStatement<?> child : statements) {
+            if (child instanceof DataTreeEffectiveStatement) {
+                final QName id = child.getIdentifier();
+                final DataTreeEffectiveStatement<?> prev = dataChildren.putIfAbsent(id,
+                    (DataTreeEffectiveStatement<?>) child);
+                SourceException.throwIf(prev != null, ref,
+                        "Cannot add data child with name %s, a conflicting child already exists", id);
+                continue;
+            }
+
+            // Deal with other SchemaTreeEffectiveStatements, but we are already tainted
+            sameAsSchema = false;
+
+            // For choice statements go through all their cases and fetch their data children
+            if (child instanceof ChoiceEffectiveStatement) {
+                child.streamEffectiveSubstatements(CaseEffectiveStatement.class)
+                .map(stmt -> stmt.streamEffectiveSubstatements(SchemaTreeEffectiveStatement.class))
+                .forEach(strm -> recursiveDataTreeChildren(ref, dataChildren, strm.collect(Collectors.toList())));
+            }
+        }
+
+        return sameAsSchema;
     }
 
     @Override
@@ -49,6 +106,10 @@ public abstract class AbstractSchemaEffectiveDocumentedNode<A, D extends Declare
         if (this instanceof SchemaTreeAwareEffectiveStatement
                 && SchemaTreeAwareEffectiveStatement.Namespace.class.equals(namespace)) {
             return Optional.of((Map<K, V>) schemaTreeNamespace);
+        }
+        if (this instanceof DataTreeAwareEffectiveStatement
+                && DataTreeAwareEffectiveStatement.Namespace.class.equals(namespace)) {
+            return Optional.of((Map<K, V>) dataTreeNamespace);
         }
         return super.getNamespaceContents(namespace);
     }
