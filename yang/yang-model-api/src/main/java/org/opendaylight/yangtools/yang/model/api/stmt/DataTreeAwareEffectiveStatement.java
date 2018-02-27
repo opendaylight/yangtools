57e3e905622e0524059df62609a9c152fcf02554
Robert Varga <robert.varga@pantheon.tech>
2018-02-27 09:25:29 +0100

    Add {Data,Schema}TreeEffectiveStatementNamespace
    YANG defines two distinct ways to look at a YANG model:
    - schema tree: The definition hierarchy specified within a module.
    - data tree: An instantiated tree of any data modeled with YANG,
                 e.g., configuration data, state data, combined configuration and
                 state data, RPC or action input, RPC or action output, or
                 notification.
    
    This means that there really are two notions of 'a child node',
    based on schema tree, or based on data tree. The data tree is
    a strict subset of schema tree.
    
    This patch introduces both these concepts and retrofits the
    appropriate nodes to expose both of these views.
    
    JIRA: YANGTOOLS-853
    Change-Id: Id2c41bb30c4376a846e4e974033b6d621ecfb155
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/DataTreeAwareEffectiveStatement.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.api.stmt;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+
+/**
+ * Interface implemented by all {@link EffectiveStatement}s which can contain a {@code data tree} child.
+ *
+ * @param <A> Argument type
+ * @param <D> Class representing declared version of this statement.
+ * @author Robert Varga
+ */
+public interface DataTreeAwareEffectiveStatement<A, D extends DeclaredStatement<A>> extends EffectiveStatement<A, D> {
+    /**
+     * Find a {@code data tree} child {@link DataTreeEffectiveStatement}, as identified by its QName argument.
+     *
+     * @param qname Child identifier
+     * @return Data tree child, or empty
+     * @throws NullPointerException if {@code qname} is null
+     */
+    // FIXME: make sure this namespace is populated
+    default <E extends DataTreeEffectiveStatement<?>> @NonNull Optional<E> findDataTreeChild(
+            final @NonNull QName qname) {
+        return Optional.ofNullable(get(DataTreeEffectiveStatementNamespace.class, requireNonNull(qname)));
+    }
+}
