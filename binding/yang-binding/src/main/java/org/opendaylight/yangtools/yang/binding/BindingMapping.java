94705fdcb11767ada14e1f592c9a1143041f7e80
Robert Varga <robert.varga@pantheon.tech>
2018-02-28 16:03:09 +0100

    Invert enforcement patterns when needed
    RFC7950 and yang-model-api defines pattern modifier invert-match,
    which needs to be taken into account when validating incoming strings.
    
    Mutate the pattern we expose to the codegen such that it captures
    the inversion operation. This code is closely related to generated
    code and string formats used therein, hence the beef of the
    implementation lives in BindingMapping.
    
    JIRA: MDSAL-314
    Change-Id: Ie29745d3343f565ac6b1b5716b1ec38dd0f09bc9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/yang-binding/src/main/java/org/opendaylight/yangtools/yang/binding/BindingMapping.java
+++ binding/yang-binding/src/main/java/org/opendaylight/yangtools/yang/binding/BindingMapping.java
@@ -53,6 +53,9 @@ public final class BindingMapping {
     public static final String RPC_INPUT_SUFFIX = "Input";
     public static final String RPC_OUTPUT_SUFFIX = "Output";
 
+    private static final String NEGATED_PATTERN_PREFIX = "^(?!";
+    private static final String NEGATED_PATTERN_SUFFIX = ").*$";
+
     private static final Interner<String> PACKAGE_INTERNER = Interners.newWeakInterner();
 
     private BindingMapping() {
@@ -197,6 +200,68 @@ public final class BindingMapping {
         } else {
             return rawString;
         }
+    }
+
+    /**
+     * Create a {@link Pattern} expression which performs inverted match to the specified pattern. The input pattern
+     * is expected to be a valid regular expression passing {@link Pattern#compile(String)} and to have both start and
+     * end of string anchors as the first and last characters.
+     *
+     * @param pattern Pattern regular expression to negate
+     * @return Negated regular expression
+     * @throws IllegalArgumentException if the pattern does not conform to expected structure
+     * @throws NullPointerException if pattern is null
+     */
+    public static String negatePatternString(final String pattern) {
+        checkArgument(pattern.charAt(0) == '^' && pattern.charAt(pattern.length() - 1) == '$',
+                "Pattern '%s' does not have expected format", pattern);
+
+        /*
+         * Converting the expression into a negation is tricky. For example, when we have:
+         *
+         *   pattern "a|b" { modifier invert-match; }
+         *
+         * this gets escaped into either "^a|b$" or "^(?:a|b)$". Either format can occur, as the non-capturing group
+         * strictly needed only in some cases. From that we want to arrive at:
+         *   "^(?!(?:a|b)$).*$".
+         *
+         *           ^^^         original expression
+         *        ^^^^^^^^       tail of a grouped expression (without head anchor)
+         *    ^^^^        ^^^^   inversion of match
+         *
+         * Inversion works by explicitly anchoring at the start of the string and then:
+         * - specifying a negative lookahead until the end of string
+         * - matching any string
+         * - anchoring at the end of the string
+         */
+        final boolean hasGroup = pattern.startsWith("^(?:") && pattern.endsWith(")$");
+        final int len = pattern.length();
+        final StringBuilder sb = new StringBuilder(len + (hasGroup ? 7 : 11)).append(NEGATED_PATTERN_PREFIX);
+
+        if (hasGroup) {
+            sb.append(pattern, 1, len);
+        } else {
+            sb.append("(?:").append(pattern, 1, len - 1).append(")$");
+        }
+        return sb.append(NEGATED_PATTERN_SUFFIX).toString();
+    }
+
+    /**
+     * Check if the specified {@link Pattern} is the result of {@link #negatePatternString(String)}. This method
+     * assumes the pattern was not hand-coded but rather was automatically-generated, such that its non-automated
+     * parts come from XSD regular expressions. If this constraint is violated, this method may result false positives.
+     *
+     * @param pattern Pattern to check
+     * @return True if this pattern is a negation.
+     * @throws NullPointerException if pattern is null
+     * @throws IllegalArgumentException if the pattern does not conform to expected structure
+     */
+    public static boolean isNegatedPattern(final Pattern pattern) {
+        return isNegatedPattern(pattern.toString());
+    }
+
+    private static boolean isNegatedPattern(final String pattern) {
+        return pattern.startsWith(NEGATED_PATTERN_PREFIX) && pattern.endsWith(NEGATED_PATTERN_SUFFIX);
     }
 
     /**
