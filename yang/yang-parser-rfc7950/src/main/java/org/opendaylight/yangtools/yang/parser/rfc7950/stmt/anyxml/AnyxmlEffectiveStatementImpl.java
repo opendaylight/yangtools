922131a215aae1b459d8cfb1fc1816e5ab95c86c
Robert Varga <robert.varga@pantheon.tech>
2018-03-07 13:03:02 +0100

    Hide effective statement implementations
    Remove deprecated tags and address the 3.0.0 guidance instructions,
    hiding the implementation classes.
    
    Change-Id: I42d2e580a513e02657fd7f927b025e329e067104
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/anyxml/AnyxmlEffectiveStatementImpl.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/anyxml/AnyxmlEffectiveStatementImpl.java
@@ -21,12 +21,7 @@ import org.opendaylight.yangtools.yang.model.api.stmt.MandatoryEffectiveStatemen
 import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractEffectiveDataSchemaNode;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 
-/**
- * @deprecated This class is visible only for historical purposes and is going to be hidden.
- */
-// FIXME: 3.0.0: hide this class and make it final
-@Deprecated
-public class AnyxmlEffectiveStatementImpl extends AbstractEffectiveDataSchemaNode<AnyxmlStatement>
+final class AnyxmlEffectiveStatementImpl extends AbstractEffectiveDataSchemaNode<AnyxmlStatement>
         implements AnyxmlEffectiveStatement, AnyXmlSchemaNode, DerivableSchemaNode {
 
     private final Collection<MustDefinition> mustConstraints;
