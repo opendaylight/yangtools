9c2cc8470533ad8ec6e4766d63e0f16076e68e1f
Balaji Varadaraju <bvaradar@luminanetworks.com>
2018-03-12 21:26:59 -0500

    YANGTOOLS-804: Netconf payload fails to render in JSON for anyxml
    When a YANG model contains an anyxml element the output does not render correctly
    in JSON in Restconf. XML output works however. Upon investigation we found issue
    was with recognising and handling collections in XML. This patch addresses the
    same and also enhanced and added more unit tests.
    
    Change-Id: I524fcfea4f45f4ee9779181ce22ec38656fc0f09
    Signed-off-by: Balaji Varadaraju <bvaradar@luminanetworks.com>

--- yang/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/JSONNormalizedNodeStreamWriter.java
+++ yang/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/JSONNormalizedNodeStreamWriter.java
@@ -243,25 +243,66 @@ public final class JSONNormalizedNodeStreamWriter implements NormalizedNodeStrea
     private void writeXmlNode(final Node node) throws IOException {
         final Element firstChildElement = getFirstChildElement(node);
         if (firstChildElement == null) {
-            writeXmlValue(node);
-        } else if (ANYXML_ARRAY_ELEMENT_ID.equals(firstChildElement.getNodeName())) {
-            writer.beginArray();
-            writeArray(firstChildElement);
-            writer.endArray();
+        	if (!isArrayElement(node))
+         	   writeXmlValue(node);
+         	else {
+         		writeArrayContent(node);
+         	}
+        }
+        else if (isArrayElement(node)) {
+            writeArrayContent(node);
         } else {
-            writer.beginObject();
-            writeObject(firstChildElement);
-            writer.endObject();
+            writeObjectContent(firstChildElement);
         }
     }
 
-    private void writeArray(Node node) throws IOException {
-        while (node != null) {
-            if (ELEMENT_NODE == node.getNodeType()) {
-                writeXmlNode(node);
-            }
-            node = node.getNextSibling();
-        }
+	private void writeArrayContent(final Node node) throws IOException {
+		writer.beginArray();
+		handleArray(node);
+		writer.endArray();
+	}
+
+	private void writeObjectContent(final Element firstChildElement)
+			throws IOException {
+		writer.beginObject();
+		writeObject(firstChildElement);
+		writer.endObject();
+	}
+
+    private boolean isArrayElement(final Node node)
+    {
+		if(node.getNodeType() == Node.ELEMENT_NODE) {
+		   if(node.getParentNode().getNodeType() == Node.ELEMENT_NODE) {
+		   Element parentNode = (Element)node.getParentNode();
+		   NodeList elementsByTagName = parentNode.getElementsByTagName(node.getNodeName());
+		   if(elementsByTagName.getLength() >1)
+		   {
+				return true;
+		   }
+		  }
+		}
+		return false;
+    }
+
+    private void handleArray(final Node node) throws IOException
+    {
+    	Element parentNode = (Element)node.getParentNode();
+    	NodeList elementsList = parentNode.getElementsByTagName(node.getNodeName());
+    	for(int i = 0; i < elementsList.getLength(); i++)
+    	{
+    		Node arrayElement = elementsList.item(i);
+    		Element parent = (Element)arrayElement.getParentNode();
+    		if(parent != parentNode)
+    			continue;
+    		Element firstChildElement = getFirstChildElement(arrayElement);
+    		if(firstChildElement !=null ) {
+    		   writeObjectContent(firstChildElement);
+    		}
+    		else {
+    			//It may be scalar
+    			writeXmlValue(arrayElement);
+    		}
+    	}
     }
 
     private void writeObject(Node node) throws IOException {
