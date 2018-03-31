6673a1b4c9c7dbd4274238561384542e93e8376b
Robert Varga <robert.varga@pantheon.tech>
2018-04-01 01:04:47 +0200

    Add yang.common.DerivedString class
    DerivedString provides the baseline alternative for storing
    String-equivalent information. The base class provides to prototype
    using which interactions with Strings can be implemented.
    
    Each such type is supported by a DerivedStringSupport, potentially
    further validated via a DerivedStringValidator.
    
    JIRA: YANGTOOLS-418
    Change-Id: Ifa8c01723fdc43e71cc6ab48fca50963a131ccaa
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/AbstractDerivedStringValidator.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.common;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import javax.annotation.concurrent.ThreadSafe;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * Abstract base class for implementing validators.
+ *
+ * @param <R> string representation class
+ * @param <T> validated string representation class
+ * @author Robert Varga
+ */
+@Beta
+@NonNullByDefault
+@ThreadSafe
+public abstract class AbstractDerivedStringValidator<R extends DerivedString<R>, T extends R>
+        implements DerivedStringValidator<R, T> {
+    private final DerivedStringSupport<R> representationSupport;
+    private final Class<T> validatedClass;
+
+    protected AbstractDerivedStringValidator(final DerivedStringSupport<R> representationSupport,
+            final Class<T> validatedClass) {
+        this.representationSupport = requireNonNull(representationSupport);
+        this.validatedClass = DerivedString.validateRepresentationClass(validatedClass);
+    }
+
+    @Override
+    public final Class<R> getRepresentationClass() {
+        return representationSupport.getRepresentationClass();
+    }
+
+    @Override
+    public final Class<T> getValidatedRepresentationClass() {
+        return validatedClass;
+    }
+
+    @Override
+    public final T validateRepresentation(final R value) {
+        @Nullable T valid;
+        return (valid = castIfValid(value)) != null ? valid : validate(value);
+    }
+
+    @Override
+    public final T validateRepresentation(final R value, final String canonicalString) {
+        @Nullable T valid;
+        return (valid = castIfValid(value)) != null ? valid : validate(value, requireNonNull(canonicalString));
+    }
+
+    /**
+     * Validate a {@link DerivedString} representation. Subclasses should override this method if they can
+     * provide a validation algorithm which does not rely on canonical strings but works on representation state only.
+     *
+     * @param value Representation value
+     * @return Validated representation
+     * @throws NullPointerException if {@code value} is null
+     * @throws IllegalArgumentException if the value does not meet validation criteria.
+     */
+    protected T validate(final R value) {
+        return validate(value, value.toCanonicalString());
+    }
+
+    /**
+     * Validate a {@link DerivedString} representation. Subclasses can chose whether they operate on representation
+     * state or canonical string -- both are considered equivalent.
+     *
+     * @param value Representation value
+     * @param canonicalString Canonical string matching the representation value
+     * @return Validated representation
+     * @throws NullPointerException if {@code value} or {@code canonicalString} is null.
+     * @throws IllegalArgumentException if the value does not meet validation criteria.
+     */
+    protected abstract T validate(R value, String canonicalString);
+
+    private @Nullable T castIfValid(final R value) {
+        return validatedClass.isAssignableFrom(value.validator().getValidatedRepresentationClass())
+                ? validatedClass.cast(value) : null;
+    }
+}
