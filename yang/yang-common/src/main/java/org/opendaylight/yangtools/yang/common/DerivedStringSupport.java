80a74033b7cc6e6b14ff3a1e91cfaff0c3d902be
Robert Varga <robert.varga@pantheon.tech>
2018-04-01 01:04:47 +0200

    Add yang.common.DerivedString class
    DerivedString provides the baseline alternative for storing
    String-equivalent information. The base class provides to prototype
    using which interactions with Strings can be implemented.
    
    JIRA: YANGTOOLS-418
    Change-Id: Ifa8c01723fdc43e71cc6ab48fca50963a131ccaa
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/DerivedStringSupport.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.common;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import javax.annotation.concurrent.ThreadSafe;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Support for a {@link DerivedString} specialization. An implementation of this interface must be registered
+ * in the system and be available from each DerivedString object.
+ *
+ * <T> derived string type
+ * @author Robert Varga
+ */
+@Beta
+@NonNullByDefault
+@ThreadSafe
+public interface DerivedStringSupport<T extends DerivedString<T>> {
+    /**
+     * Returns the instantiated representation class.
+     *
+     * @return Instantiated representation class.
+     */
+    Class<T> getRepresentationClass();
+
+    /**
+     * Create a instance for a string representation.
+     *
+     * @param str String representation
+     * @return A {@link DerivedString} instance.
+     * @throws NullPointerException if str is null
+     * @throws IllegalArgumentException if str does not contain a valid representation
+     */
+    T forString(String str);
+
+    /**
+     * Create a instance for a string representation.
+     *
+     * @param str String representation
+     * @return A {@link DerivedString} instance.
+     * @throws NullPointerException if str is null
+     * @throws IllegalArgumentException if str does not contain canonical representation
+     */
+    default T forCanonicalString(final String str) {
+        return forString(requireNonNull(str));
+    }
+
+    /**
+     * Unsafe cast to a factory type.
+     *
+     * @return This instance cast to specified type
+     */
+    @SuppressWarnings("unchecked")
+    default <X extends DerivedString<X>> DerivedStringSupport<X> unsafe() {
+        return (DerivedStringSupport<X>) this;
+    }
+}
