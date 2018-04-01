d14d3288ed506a96d7c41e1efe24c14590021a69
Robert Varga <robert.varga@pantheon.tech>
2018-04-01 05:34:49 +0200

    WIP: add amend extension
    JIRA: YANGTOOLS-575
    Change-Id: Id6575036c53ef8add6d73da89be1fd67a38d7c9a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/amend-parser-support/src/main/java/org/opendaylight/yangtools/amend/parser/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * YANG parser support for OpenDaylight amend extension as defined in odl-amend.yang.
+ *
+ * <p>
+ * To access this extension, add {@link org.opendaylight.yangtools.amend.parser.AmendStatementSupport} to the reactor.
+ *
+ * @author Robert Varga
+ */
+package org.opendaylight.yangtools.amend.parser;
