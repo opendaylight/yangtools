097a423f5da1e5823872919083344db37240622a
Robert Varga <robert.varga@pantheon.tech>
2018-04-06 17:48:24 +0200

    WIP: Add ietf-string-types
    // FIXME: also need DateAndTimeString
    
    JIRA: YANGTOOLS-418
    Change-Id: Ic5d8341df44432e96922f84f4a224ac68c926ac8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/ietf-string-types/src/main/java/org/opendaylight/yangtools/ietf/stringtypes/Ipv4AddressStringSupport.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.ietf.stringtypes;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import com.google.common.annotations.Beta;
+import com.google.common.net.InetAddresses;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.util.regex.Pattern;
+import javax.annotation.RegEx;
+import javax.annotation.concurrent.ThreadSafe;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.common.AbstractDerivedStringSupport;
+
+@Beta
+@NonNullByDefault
+@ThreadSafe
+public final class Ipv4AddressStringSupport extends AbstractDerivedStringSupport<Ipv4AddressString> {
+    @RegEx
+    private static final String LETTER_NUMBER_REGEX = "[\\p{N}\\p{L}]+";
+    private static final Pattern LETTER_NUMBER_PATTERN = Pattern.compile(LETTER_NUMBER_REGEX);
+    private static final Ipv4AddressStringSupport INSTANCE = new Ipv4AddressStringSupport();
+
+    private Ipv4AddressStringSupport() {
+        super(Ipv4AddressString.class);
+    }
+
+    public static Ipv4AddressStringSupport getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Ipv4AddressString fromString(final String str) {
+        final InetAddress address;
+        final String zone;
+        final int percent = str.indexOf('%');
+        if (percent != -1) {
+            zone = str.substring(percent + 1);
+            checkArgument(LETTER_NUMBER_PATTERN.matcher(zone).matches(), "Value \"%s\" has invalid zone", str);
+            address = InetAddresses.forString(str.substring(0, percent));
+        } else {
+            address = InetAddresses.forString(str);
+            zone = "";
+        }
+
+        checkArgument(address instanceof Inet4Address, "Value \"%s\" is not a valid ipv4-address", str);
+        return Ipv4AddressString.valueOf((Inet4Address) address, zone);
+    }
+}
