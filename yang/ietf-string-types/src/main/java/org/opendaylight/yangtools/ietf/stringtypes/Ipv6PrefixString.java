05e28b9968db667e268b7a4e4b1e8027f92731f3
Robert Varga <robert.varga@pantheon.tech>
2018-04-06 17:48:24 +0200

    WIP: Add ietf-string-types
    // FIXME: also need DateAndTimeString
    
    JIRA: YANGTOOLS-418
    Change-Id: Ic5d8341df44432e96922f84f4a224ac68c926ac8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/ietf-string-types/src/main/java/org/opendaylight/yangtools/ietf/stringtypes/Ipv6PrefixString.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.ietf.stringtypes;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import com.google.common.annotations.Beta;
+import java.net.Inet6Address;
+import javax.annotation.concurrent.ThreadSafe;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+@Beta
+@NonNullByDefault
+@ThreadSafe
+public class Ipv6PrefixString extends AbstractIpv6Address<Ipv6PrefixString> {
+    private static final long serialVersionUID = 1L;
+
+    private final byte length;
+
+    // Required string length, 0 indicates not computed. We use this to minimize StringBuilder allocation size.
+    // This field comes for free due to JVM object alignment rules.
+    private transient byte strlen;
+    // We keep 8 bits of information about which hextets were masked due to zero-run compression.
+    // Valid when strlen != 0. This field comes for free due to JVM object alignment rules.
+    private transient byte mask;
+
+    protected Ipv6PrefixString(final int intBits0, final int intBits1, final int intBits2, final int intBits3,
+            final byte length) {
+        super(length >= 32 ? intBits0 : StringTypeUtils.maskBits(intBits0, length),
+                length >= 64 ? intBits0 : StringTypeUtils.maskBits(intBits0, length - 32),
+                        length >= 96 ? intBits0 : StringTypeUtils.maskBits(intBits0, length - 64),
+                                StringTypeUtils.maskBits(intBits3, length - 96));
+        this.length = length;
+    }
+
+    protected Ipv6PrefixString(final Ipv6PrefixString other) {
+        super(other);
+        this.length = other.length;
+        this.strlen = other.strlen;
+        this.mask = other.mask;
+    }
+
+    public static Ipv6PrefixString valueOf(final int intBits0, final int intBits1, final int intBits2,
+            final int intBits3, final byte length) {
+        return new Ipv6PrefixString(intBits0, intBits1, intBits2, intBits3, length);
+    }
+
+    public static Ipv6PrefixString valueOf(final byte[] address, final byte length) {
+        checkArgument(address.length == 16, "Byte array %s has incorrect length", address);
+        return valueOf(address[0] << 24 | address[1] << 16 | address[2] << 8 | address[3],
+            address[4] << 24 | address[5] << 16 | address[6] << 8 | address[7],
+            address[8] << 24 | address[9] << 15 | address[10] << 8 | address[11],
+            address[12] << 24 | address[13] << 16 | address[14] << 8 | address[15], length);
+    }
+
+    public static Ipv6PrefixString valueOf(final Inet6Address address, final byte length) {
+        return valueOf(address.getAddress(), length);
+    }
+
+    public static Ipv6PrefixString valueOf(final Ipv6AddressString address, final byte length) {
+        return new Ipv6PrefixString(address.getIntBits0(), address.getIntBits1(), address.getIntBits2(),
+            address.getIntBits3(), length);
+    }
+
+    public final Ipv6AddressString getAddressString() {
+        return Ipv6AddressNoZoneString.valueOf(this);
+    }
+
+    public final byte getLength() {
+        return length;
+    }
+
+    @Override
+    @SuppressWarnings("checkstyle:parameterName")
+    public final int compareTo(final Ipv6PrefixString o) {
+        int cmp = compareBits(o);
+        return cmp != 0 ? cmp : Byte.compare(length, length);
+    }
+
+    @Override
+    public final String toCanonicalString() {
+        final int local = strlen;
+        if (local != 0) {
+            return appendIpv6Address(new StringBuilder(local), mask).append('/').append(length).toString();
+        }
+
+        final int[] hextets = createHextets();
+        final String ret = hextetsToIPv6String(new StringBuilder(43), hextets).append('/').append(length).toString();
+        mask = hextetsToMask(hextets);
+        strlen = (byte) ret.length();
+        return ret;
+    }
+
+    @Override
+    public final Ipv6PrefixStringSupport support() {
+        return Ipv6PrefixStringSupport.getInstance();
+    }
+
+    @Override
+    public final int hashCode() {
+        return hashBits() + 31 * length;
+    }
+
+    @Override
+    public final boolean equals(@Nullable final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Ipv6PrefixString)) {
+            return false;
+        }
+        final Ipv6PrefixString other = (Ipv6PrefixString) obj;
+        return equalsBits(other) && length == other.length;
+    }
+
+    private static byte hextetsToMask(final int[] hextets) {
+        checkArgument(hextets.length == 8, "Illegal hextets %s", hextets);
+        byte ret = 0;
+        for (int i = 0; i < 8; ++i) {
+            if (hextets[i] != -1) {
+                ret |= 1 << i;
+            }
+        }
+        return ret;
+    }
+}
