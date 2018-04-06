7849c212f3a53fc35864f7d8441ec4286cef2c37
Robert Varga <robert.varga@pantheon.tech>
2018-04-06 17:48:24 +0200

    WIP: Add ietf-string-types
    // FIXME: also need DateAndTimeString
    
    JIRA: YANGTOOLS-418
    Change-Id: Ic5d8341df44432e96922f84f4a224ac68c926ac8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/ietf-string-types/src/main/java/org/opendaylight/yangtools/ietf/stringtypes/MacAddressStringSupport.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.ietf.stringtypes;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static org.opendaylight.yangtools.ietf.stringtypes.StringTypeUtils.hexValue;
+
+import com.google.common.annotations.Beta;
+import javax.annotation.concurrent.ThreadSafe;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.common.AbstractDerivedStringSupport;
+
+@Beta
+@NonNullByDefault
+@ThreadSafe
+public final class MacAddressStringSupport extends AbstractDerivedStringSupport<MacAddressString> {
+    private static final MacAddressStringSupport INSTANCE = new MacAddressStringSupport();
+
+    private MacAddressStringSupport() {
+        super(MacAddressString.class);
+    }
+
+    public static MacAddressStringSupport getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public MacAddressString fromCanonicalString(final String str) {
+        checkArgument(str.length() == 17, "Malformed string \"%s\"", str);
+        return new MacAddressString(hexValue(str.charAt(0)) << 28 | hexValue(str.charAt(1)) << 24
+            | hexValue(str.charAt(3)) << 20 | hexValue(str.charAt(4)) << 16 | hexValue(str.charAt(6)) << 12
+            | hexValue(str.charAt(7)) << 8 | hexValue(str.charAt(9)) << 4 | hexValue(str.charAt(10)),
+            (short) (hexValue(str.charAt(12)) << 12 | hexValue(str.charAt(13)) << 8 | hexValue(str.charAt(15)) << 4
+                    | hexValue(str.charAt(16))));
+    }
+
+    @Override
+    public MacAddressString fromString(final String str) {
+        checkArgument(str.length() == 17, "Malformed string \"%s\"", str);
+        for (int i = 2; i < 17; i += 3) {
+            checkColon(str, i);
+        }
+        return fromCanonicalString(str);
+    }
+
+    private static void checkColon(final String str, final int offset) {
+        final char ch = str.charAt(offset);
+        checkArgument(ch == ':', "Invalid character '%s' at offset %s", ch, offset);
+    }
+}
