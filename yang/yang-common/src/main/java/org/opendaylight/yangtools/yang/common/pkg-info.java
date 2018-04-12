f603c80fa9a7fefce7f065a3e034c52a1e6ddb3f
Robert Varga <robert.varga@pantheon.tech>
2018-04-13 01:26:11 +0200

    Provide some yangtools.yang.common javadoc
    This package deserves an introduction, provide one.
    
    Change-Id: I9dfab88255d53915ed0df8ca9d59208a1030824c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/pkg-info.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2015 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Common YANG concepts and constants expressed in terms of Java. This notably includes the concepts of
+ * <ul>
+ * <li>a {@link YangVersion}</li>
+ * <li>a {@link Revision}</li>
+ * <li>a {@link QNameModule}</li>
+ * <li>a {@link QName}</li>
+ * <li>a Java-native representation of a YANG string, {@link DerivedString}</li>
+ * <li>a Java-native representations of numeric YANG types, like {@link Uint64}, {@link Decimal64} and
+ *     {@link Empty}</li>
+ * </ul>
+ */
+package org.opendaylight.yangtools.yang.common;
