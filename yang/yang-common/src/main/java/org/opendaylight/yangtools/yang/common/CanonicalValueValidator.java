1d2fe1ada4e60b4b64f7a2f61766196e1ec8d391
Robert Varga <robert.varga@pantheon.tech>
2018-04-16 17:54:17 +0200

    Add generalized CanonicalValue YANG concept
    The parsing and validation concepts in DerivedString are really
    useful in expressing that a particular value has been validated
    to conform to some abstract contract.
    
    This facility will allow us to side-step validation when moving
    data between classes, especially when we can prove requried
    validation has already been done.
    
    This patch introduces CanonicalValue and support interfaces, so
    it can be used more widely.
    
    JIRA: YANGTOOLS-418
    Change-Id: Ib3f8723facd60c6737351d863ad0f9cef6c4a5da
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/CanonicalValueValidator.java
+++ yang/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/CanonicalValueValidator.java
@@ -11,39 +11,39 @@ import com.google.common.annotations.Beta;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 
 /**
- * YANG string representation validator. Implementations of this interface can perform further validation of
- * representation state such that it conforms to a YANG string type derived from a type with a {@link DerivedString}
- * representation class.
+ * {@link CanonicalValue} validator interface. Implementations of this interface can perform further validation of
+ * representation state such that it conforms to a YANG type derived from a type with a {@link CanonicalValue}
+ * representation.
  *
  * <p>
- * Note: this interface should not be directly implemented. Use {@link AbstractDerivedStringValidator} instead.
+ * Note: this interface should not be directly implemented. Use {@link AbstractCanonicalValueValidator} instead.
  *
- * @param <R> string representation class
- * @param <T> validated string representation class
+ * @param <T> canonical value type
+ * @param <V> validated canonical value type
  * @author Robert Varga
  */
 @Beta
 @NonNullByDefault
-public interface DerivedStringValidator<R extends DerivedString<R>, T extends R> {
+public interface CanonicalValueValidator<T extends CanonicalValue<T>, V extends T> {
     /**
-     * Returns the instantiated representation class. The representation class is a {@link DerivedString} which
-     * understands the semantics of modeled data and has some internal representation of it. All {@link DerivedString}s
+     * Returns the instantiated representation class. The representation class is a {@link CanonicalValue} which
+     * understands the semantics of modeled data and has some internal representation of it. All {@link CanonicalValue}s
      * which share the same representation class are considered equal if their internal state would result in the
      * same canonical string representation as defined by the YANG data model.
      *
-     * @return Representation {@link DerivedString} class.
+     * @return Representation {@link CanonicalValue} class.
      */
-    Class<R> getRepresentationClass();
+    Class<T> getRepresentationClass();
 
     /**
      * Return the class which captures the fact it was validated by this validator.
      *
      * @return Validated capture of the representation class.
      */
-    Class<T> getValidatedRepresentationClass();
+    Class<V> getValidatedRepresentationClass();
 
     /**
-     * Validate a {@link DerivedString} representation. Implementations should override this method if they can
+     * Validate a {@link CanonicalValue} representation. Implementations should override this method if they can
      * provide a validation algorithm which does not rely on canonical strings but works on representation state only.
      *
      * @param value Representation value
@@ -51,14 +51,14 @@ public interface DerivedStringValidator<R extends DerivedString<R>, T extends R>
      * @throws NullPointerException if {@code value} is null
      * @throws IllegalArgumentException if the value does not meet validation criteria.
      */
-    default T validateRepresentation(final R value) {
+    default V validateRepresentation(final T value) {
         return validateRepresentation(value, value.toCanonicalString());
     }
 
     /**
-     * Validate a {@link DerivedString} representation. Implementations can chose whether they operate on representation
-     * state or canonical string -- both are considered equivalent. Users should call this method if they have
-     * a representation readily available.
+     * Validate a {@link CanonicalValue} representation. Implementations can chose whether they operate on
+     * representation state or canonical string -- both are considered equivalent. Users should call this method if they
+     * have a representation readily available.
      *
      * @param value Representation value
      * @param canonicalString Canonical string matching the representation value
@@ -66,5 +66,5 @@ public interface DerivedStringValidator<R extends DerivedString<R>, T extends R>
      * @throws NullPointerException if {@code value} or {@code canonicalString} is null.
      * @throws IllegalArgumentException if the value does not meet validation criteria.
      */
-    T validateRepresentation(R value, String canonicalString);
+    V validateRepresentation(T value, String canonicalString);
 }
