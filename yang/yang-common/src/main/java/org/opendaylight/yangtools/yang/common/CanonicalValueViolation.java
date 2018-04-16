49dd92f02b3da56730148b21b080cd5c0e451897
Robert Varga <robert.varga@pantheon.tech>
2018-04-16 23:28:17 +0200

    Convert CanonicalValue{Support,Validator} to use Variant
    Variant is useful here, as we can return either a CanonicalValue
    or CanonicalValueViolatio (which holds error details). This allows
    us to side-step exception throws while retaining error information.
    
    Change-Id: Ie3d1d6ff21611391a912b6ccfb208c0d58fa9ab5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/CanonicalValueViolation.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.common;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.MoreObjects.ToStringHelper;
+import java.util.Objects;
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.concepts.Variant;
+
+/**
+ * A violation of a {@link CanonicalValue} validation. Contains details as mandated by RFC7950 Section 8.3.1.
+ *
+ * @author Robert Varga
+ */
+@Beta
+@NonNullByDefault
+public abstract class CanonicalValueViolation {
+    private static final class Regular extends CanonicalValueViolation {
+        private final @Nullable String appTag;
+        private final @Nullable String message;
+
+        Regular(final @Nullable String appTag, final @Nullable String message) {
+            this.appTag = appTag;
+            this.message = message;
+        }
+
+        @Override
+        @Nullable String appTag() {
+            return appTag;
+        }
+
+        @Override
+        @Nullable String message() {
+            return message;
+        }
+
+        @Override
+        public IllegalArgumentException toIllegalArgument() {
+            return new IllegalArgumentException(message);
+        }
+
+        @Override
+        ToStringHelper addToString(final ToStringHelper helper) {
+            return helper.omitNullValues().add("app-tag", appTag).add("message", message);
+        }
+    }
+
+    public static final class WithException extends CanonicalValueViolation {
+        private final Exception cause;
+
+        public WithException(final Exception cause) {
+            this.cause = requireNonNull(cause);
+        }
+
+        @Override
+        @Nullable String appTag() {
+            return null;
+        }
+
+        @Override
+        @Nullable String message() {
+            return cause.getMessage();
+        }
+
+        public Exception getCause() {
+            return cause;
+        }
+
+        @Override
+        public IllegalArgumentException toIllegalArgument() {
+            return new IllegalArgumentException(cause.getMessage(), cause);
+        }
+
+        @Override
+        ToStringHelper addToString(final ToStringHelper helper) {
+            return helper.add("cause", cause);
+        }
+    }
+
+    private static final CanonicalValueViolation EMPTY = new Regular(null, null);
+    private static final Variant<?, CanonicalValueViolation> EMPTY_VARIANT = Variant.ofSecond(EMPTY);
+
+    public static CanonicalValueViolation empty() {
+        return EMPTY;
+    }
+
+    public static CanonicalValueViolation of(final Exception cause) {
+        return new WithException(cause);
+    }
+
+    public static CanonicalValueViolation of(final @Nullable String appTag, final @Nullable String message) {
+        return appTag == null && message == null ? EMPTY : new Regular(appTag, message);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> Variant<T, CanonicalValueViolation> emptyVariant() {
+        return (Variant<T, CanonicalValueViolation>) EMPTY_VARIANT;
+    }
+
+    public static <T> Variant<T, CanonicalValueViolation> variantOf(final Exception cause) {
+        return Variant.ofSecond(CanonicalValueViolation.of(cause));
+    }
+
+    public static <T> Variant<T, CanonicalValueViolation> variantOf(final String message) {
+        return variantOf(null, message);
+    }
+
+    public static <T> Variant<T, CanonicalValueViolation> variantOf(final @Nullable String appTag,
+            final String message) {
+        return Variant.ofSecond(CanonicalValueViolation.of(appTag, message));
+    }
+
+    public final Optional<String> getAppTag() {
+        return nullableString(appTag());
+    }
+
+    public final Optional<String> getMessage() {
+        return nullableString(appTag());
+    }
+
+    public abstract IllegalArgumentException toIllegalArgument();
+
+    abstract @Nullable String appTag();
+
+    abstract @Nullable String message();
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(appTag(), message());
+    }
+
+    @Override
+    public final boolean equals(final @Nullable Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof CanonicalValueViolation)) {
+            return false;
+        }
+        final CanonicalValueViolation other = (CanonicalValueViolation) obj;
+        return Objects.equals(appTag(), other.appTag()) && Objects.equals(message(), other.message());
+    }
+
+    @Override
+    public final String toString() {
+        return addToString(MoreObjects.toStringHelper(this)).toString();
+    }
+
+    abstract ToStringHelper addToString(ToStringHelper helper);
+
+    private static Optional<String> nullableString(@Nullable final String str) {
+        return str != null ? Optional.of(str) : Optional.empty();
+    }
+}
