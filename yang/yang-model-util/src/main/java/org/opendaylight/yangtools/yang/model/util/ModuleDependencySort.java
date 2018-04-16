895e195697f2137f75a4ec088d0af9ef3aa0ed67
Robert Varga <robert.varga@pantheon.tech>
2018-04-16 12:24:39 +0200

    Consider submodule imports when sorting modules
    YANGTOOLS-806 started skipping import statements, hence
    a module's effective requirements are no longer expressed
    in their imports.
    
    Teach ModuleDependencySort about submodules and populate
    the graph based on submodule's imports, too.
    
    JIRA: YANGTOOLS-876
    Change-Id: I1aed32de034cf2e602e130afcbeafb171fac514b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-model-util/src/main/java/org/opendaylight/yangtools/yang/model/util/ModuleDependencySort.java
+++ yang/yang-model-util/src/main/java/org/opendaylight/yangtools/yang/model/util/ModuleDependencySort.java
@@ -12,6 +12,7 @@ import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Table;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
@@ -110,7 +111,7 @@ public final class ModuleDependencySort {
             }
 
             // no need to check if other Type of object, check is performed in process modules
-            for (final ModuleImport imprt : module.getImports()) {
+            for (final ModuleImport imprt : allImports(module)) {
                 final String toName = imprt.getModuleName();
                 final Optional<Revision> toRevision = imprt.getRevision();
 
@@ -137,6 +138,19 @@ public final class ModuleDependencySort {
                 from.addEdge(to);
             }
         }
+    }
+
+    private static Collection<ModuleImport> allImports(final Module mod) {
+        if (mod.getSubmodules().isEmpty()) {
+            return mod.getImports();
+        }
+
+        final List<ModuleImport> concat = new ArrayList<>();
+        concat.addAll(mod.getImports());
+        for (Module sub : mod.getSubmodules()) {
+            concat.addAll(sub.getImports());
+        }
+        return concat;
     }
 
     /**
