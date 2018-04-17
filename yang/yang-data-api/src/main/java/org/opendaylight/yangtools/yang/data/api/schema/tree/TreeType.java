de549c971e1d282e1ebcef420b9973a7082ba530
Robert Varga <robert.varga@pantheon.tech>
2018-04-17 03:27:14 +0200

    Add TreeType documentation
    Clarify TreeType's relationship with RFC6020, RFC7950 and RFC8342,
    add 2.1.0/3.0.0 design considerations.
    
    Change-Id: Ie74eba7244270015be87fdaf5e48d6d265f6e099
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/tree/TreeType.java
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/tree/TreeType.java
@@ -9,8 +9,28 @@ package org.opendaylight.yangtools.yang.data.api.schema.tree;
 
 import com.google.common.annotations.Beta;
 
+/**
+ * {@link DataTree} type, specifying which YANG modeled content is valid with a data tree. This reflects
+ * <a ref="https://tools.ietf.org/html/rfc6020#section-7.21.1">RFC6020</a>/
+ * <a ref="https://tools.ietf.org/html/rfc6020#section-7.19.1">RFC7950</a> data combinations based on {@code config}
+ * statement.
+ */
+// FIXME: 2.1.0: 3.0.0: Consider defining a 'config false'-only type
+// FIXME: 3.0.0: Consider renaming this enum
 @Beta
 public enum TreeType {
+    /**
+     * Only {@code config true} nodes are allowed. This corresponds, but is not limited, to @{code candidate},
+     * {@code startup} and {@code running} data stores defined in
+     * <a href="https://tools.ietf.org/html/rfc8342#section-4.1">RFC8342 section 4.1</a> as well as {@code intended}
+     * data store defined in <a href="https://tools.ietf.org/html/rfc8342#section-5">RFC8342 section 4.1</a>.
+     */
     CONFIGURATION,
+    /**
+     * Only {@code config true} and {@code config false} nodes are allowed. This corresponds, but is not limited, to
+     * @{code operational state} defined in
+     * <a href="https://tools.ietf.org/html/rfc8342#section-4.1">RFC8342 section 4.1</a> as well as {@code operational}
+     * data store defined in <a href="https://tools.ietf.org/html/rfc8342#section-5">RFC8342 section 4.1</a>.
+     */
     OPERATIONAL,
 }
