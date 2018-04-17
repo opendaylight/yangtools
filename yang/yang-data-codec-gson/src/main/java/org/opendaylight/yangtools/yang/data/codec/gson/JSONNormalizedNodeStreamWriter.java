5f251be1b1ccd80a5b7037194e0f6a79037fce42
Balaji Varadaraju <bvaradar@luminanetworks.com>
2018-04-17 18:08:32 -0500

    YANGTOOLS-804: Netconf payload fails to render in JSON for anyxml
    When a YANG model contains an anyxml element the output does not render correctly
    in JSON in Restconf. XML output works however. Upon investigation we found issue
    was with recognising and handling collections in XML. This patch addresses the
    same and also enhanced and added more unit tests.
    
    Change-Id: Icb238dd95a3b9af9c4d0f48ff673a7c285925364
    Signed-off-by: Balaji Varadaraju <bvaradar@luminanetworks.com>

--- yang/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/JSONNormalizedNodeStreamWriter.java
+++ yang/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/JSONNormalizedNodeStreamWriter.java
@@ -8,7 +8,6 @@
 package org.opendaylight.yangtools.yang.data.codec.gson;
 
 import static java.util.Objects.requireNonNull;
-import static org.opendaylight.yangtools.yang.data.codec.gson.JsonParserStream.ANYXML_ARRAY_ELEMENT_ID;
 import static org.w3c.dom.Node.ELEMENT_NODE;
 import static org.w3c.dom.Node.TEXT_NODE;
 
@@ -280,32 +279,77 @@ public abstract class JSONNormalizedNodeStreamWriter implements NormalizedNodeSt
     private void writeXmlNode(final Node node) throws IOException {
         final Element firstChildElement = getFirstChildElement(node);
         if (firstChildElement == null) {
-            writeXmlValue(node);
-        } else if (ANYXML_ARRAY_ELEMENT_ID.equals(firstChildElement.getNodeName())) {
-            writer.beginArray();
-            writeArray(firstChildElement);
-            writer.endArray();
+            if (!isArrayElement(node)) {
+                writeXmlValue(node);
+            } else {
+                writeArrayContent(node);
+            }
+        }
+        else if (isArrayElement(node)) {
+            writeArrayContent(node);
         } else {
-            writer.beginObject();
-            writeObject(firstChildElement);
-            writer.endObject();
+            writeObjectContent(firstChildElement);
         }
     }
 
-    private void writeArray(Node node) throws IOException {
-        while (node != null) {
-            if (ELEMENT_NODE == node.getNodeType()) {
-                writeXmlNode(node);
+    private void writeArrayContent(final Node node) throws IOException {
+        writer.beginArray();
+        handleArray(node);
+        writer.endArray();
+    }
+
+    private void writeObjectContent(final Element firstChildElement)
+         throws IOException {
+        writer.beginObject();
+        writeObject(firstChildElement);
+        writer.endObject();
+    }
+
+    private boolean isArrayElement(final Node node) {
+        if ((ELEMENT_NODE == node.getNodeType())) {
+            String nodeName = node.getNodeName();
+            Node nextNode = node.getNextSibling();
+            while (nextNode != null) {
+                if (ELEMENT_NODE == nextNode.getNodeType()) {
+                    if (nextNode.getNodeName().equals(nodeName)) {
+                        return true;
+                    }
+                }
+                nextNode = nextNode.getNextSibling();
             }
-            node = node.getNextSibling();
+        }
+        return false;
+    }
+
+    private void handleArray(final Node node) throws IOException {
+        Element parentNode = (Element)node.getParentNode();
+        NodeList elementsList = parentNode.getElementsByTagName(node.getNodeName());
+        for (int i = 0; i < elementsList.getLength(); i++) {
+            Node arrayElement = elementsList.item(i);
+            Element parent = (Element)arrayElement.getParentNode();
+            if (parent != parentNode) {
+                continue;
+            }
+            Element firstChildElement = getFirstChildElement(arrayElement);
+            if (firstChildElement != null) {
+                writeObjectContent(firstChildElement);
+            }
+            else {
+                //It may be scalar
+                writeXmlValue(arrayElement);
+            }
         }
     }
 
     private void writeObject(Node node) throws IOException {
+        String previousNodeName = "";
         while (node != null) {
             if (ELEMENT_NODE == node.getNodeType()) {
-                writer.name(node.getNodeName());
-                writeXmlNode(node);
+                if (!node.getNodeName().equals(previousNodeName)) {
+                    previousNodeName = node.getNodeName();
+                    writer.name(node.getNodeName());
+                    writeXmlNode(node);
+                }
             }
             node = node.getNextSibling();
         }
