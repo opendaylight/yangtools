dfaa57e1ad352486d37b8afbcde8b7f634e19a9c
Robert Varga <robert.varga@pantheon.tech>
2018-05-04 23:46:12 +0200

    WIP: yang-xpath-api
    This patch adds the basic model of a YANG-extended XPath 1.0
    expression.
    
    Change-Id: Id5f619e6acd956b7b43f9faba75f34051e3dc509
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-xpath-api/src/main/java/org/opendaylight/yangtools/yang/xpath/api/YangNaryOperator.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.xpath.api;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import com.google.common.collect.ImmutableSet;
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * YANG XPath binary operator.
+ *
+ * @author Robert Varga
+ */
+@Beta
+public enum YangNaryOperator {
+    /**
+     * Logical 'and' operator on operands.
+     *
+     * @see <a href="https://www.w3.org/TR/1999/REC-xpath-19991116/#NT-AndExpr">AndExpr</a>
+     */
+    AND("and"),
+    /**
+     * Logical 'or' operator on operands.
+     *
+     * @see <a href="https://www.w3.org/TR/1999/REC-xpath-19991116/#NT-OrExpr">OrExpr</a>
+     */
+    OR("or"),
+    /**
+     * Set union operator on operands.
+     *
+     * @see <a href="https://www.w3.org/TR/1999/REC-xpath-19991116/#NT-UnionExpr">UnionExpr</a>
+     */
+    UNION("|");
+
+    private final class Expr extends YangNaryExpr {
+        private static final long serialVersionUID = 1L;
+
+        Expr(final Set<YangExpr> exprs) {
+            super(exprs);
+        }
+
+        @Override
+        public YangNaryOperator getOperator() {
+            return YangNaryOperator.this;
+        }
+    }
+
+    private final String str;
+
+    YangNaryOperator(final String str) {
+        this.str = requireNonNull(str);
+    }
+
+    @Override
+    public String toString() {
+        return str;
+    }
+
+    public YangExpr exprWith(final Collection<YangExpr> exprs) {
+        final Set<YangExpr> set = ImmutableSet.copyOf(exprs);
+        return set.size() == 1 ? set.iterator().next() : new Expr(set);
+    }
+}
---
+++ yang/yang-xpath-api/src/main/java/org/opendaylight/yangtools/yang/xpath/api/YangPredicateAware.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.xpath.api;
+
+import com.google.common.annotations.Beta;
+import com.google.common.collect.ImmutableSet;
+import java.util.Set;
+import org.opendaylight.yangtools.yang.xpath.api.YangLocationPath.Step;
+
+/**
+ * Common interface for {@link YangFilterExpr} and {@link Step}, both of which can contain predicates. Predicates are
+ * expressed in terms of {@link YangExpr}.
+ *
+ * @author Robert Varga
+ */
+@Beta
+public interface YangPredicateAware {
+    default Set<YangExpr> getPredicates() {
+        return ImmutableSet.of();
+    }
+}
