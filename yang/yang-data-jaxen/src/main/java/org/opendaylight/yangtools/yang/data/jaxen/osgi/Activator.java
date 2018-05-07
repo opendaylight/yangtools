50c9641065f92bc822d3fda8f0dc7795f6d0fd3f
Robert Varga <robert.varga@pantheon.tech>
2018-05-07 13:24:56 +0200

    Add yang-data-jaxen OSGi activator
    While we are providing a ServiceLoader definition, which can be
    picked up by SPI Fly, provide an explicit OSGi activator, too.
    
    Change-Id: Id7a221b34c10aeb4f492c9ab71b7af431840d718
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-data-jaxen/src/main/java/org/opendaylight/yangtools/yang/data/jaxen/osgi/Activator.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.jaxen.osgi;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.data.api.schema.xpath.XPathSchemaContextFactory;
+import org.opendaylight.yangtools.yang.data.jaxen.JaxenSchemaContextFactory;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ * YANG Jaxen XPath implementation activator. Publishes a {@link XPathSchemaContextFactory} implementation on bundle
+ * start.
+ *
+ * @author Robert Varga
+ */
+public final class Activator implements BundleActivator {
+    private ServiceRegistration<@NonNull XPathSchemaContextFactory> registration;
+
+    @Override
+    public void start(final BundleContext context) throws Exception {
+        registration = context.registerService(XPathSchemaContextFactory.class, new JaxenSchemaContextFactory(), null);
+    }
+
+    @Override
+    public void stop(final BundleContext context) throws Exception {
+        if (registration != null) {
+            registration.unregister();
+            registration = null;
+        }
+    }
+}
