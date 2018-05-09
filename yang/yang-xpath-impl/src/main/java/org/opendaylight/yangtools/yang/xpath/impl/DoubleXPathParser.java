59854b0bd6bd8853a2cd3eb3e1bf8c9a1425cb6d
Robert Varga <robert.varga@pantheon.tech>
2018-05-09 20:54:22 +0200

    WIP: add yang-xpath-impl
    This adds an antlr-based XPath parser, which results in
    yang-xpath-api expressions.
    
    The grammar file (xpath.g4) is taken from
    https://github.com/antlr/grammars-v4/blob/master/xpath/xpath.g4
    at commit a3ff48932b0168d93d184a988fa40723b60f6c27.
    
    Change-Id: Ic7cfdb2d8d132577840f910786f72ed75b17c79e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-xpath-impl/src/main/java/org/opendaylight/yangtools/yang/xpath/impl/DoubleXPathParser.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.xpath.impl;
+
+import static com.google.common.base.Verify.verify;
+
+import java.util.Optional;
+import java.util.function.Function;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.xpath.api.YangBinaryOperator;
+import org.opendaylight.yangtools.yang.xpath.api.YangBooleanExpr;
+import org.opendaylight.yangtools.yang.xpath.api.YangExpr;
+import org.opendaylight.yangtools.yang.xpath.api.YangNumberExpr;
+import org.opendaylight.yangtools.yang.xpath.api.YangNumberExprBuilder;
+
+final class DoubleXPathParser extends XPathParser {
+
+    DoubleXPathParser(final QNameModule implicitNamespace, final Function<String, QNameModule> prefixes) {
+        super(implicitNamespace, prefixes);
+    }
+
+    @Override
+    public XPathParserNumberCompliance getNumberCompliance() {
+        return XPathParserNumberCompliance.IEEE754;
+    }
+
+    @Override
+    YangNumberExpr createNumber(final String str) {
+        return new YangNumberExprBuilder().number(Double.valueOf(str)).build();
+    }
+
+    @Override
+    YangNumberExpr negateNumber(final YangNumberExpr number) {
+        return new YangNumberExprBuilder().number(-coerceDouble(number)).build();
+    }
+
+    @Override
+    Optional<YangExpr> simplifyNumbers(final YangBinaryOperator operator, final YangNumberExpr left,
+            final YangNumberExpr right) {
+        final double l = coerceDouble(left);
+        final double r = coerceDouble(right);
+
+        final double result;
+        switch (operator) {
+            case DIV:
+                result = l / r;
+                break;
+            case EQUALS:
+                return of(l == r);
+            case GT:
+                return of(l > r);
+            case GTE:
+                return of(l >= r);
+            case LT:
+                return of(l < r);
+            case LTE:
+                return of(l <= r);
+            case MINUS:
+                result = l - r;
+                break;
+            case MOD:
+                result = l % r;
+                break;
+            case MUL:
+                result = l * r;
+                break;
+            case NOT_EQUALS:
+                return of(l != r);
+            case PLUS:
+                result = l + r;
+                break;
+            default:
+                throw new IllegalStateException("Unhandled operator " + operator);
+        }
+
+        return Optional.of(new YangNumberExprBuilder().number(result).build());
+    }
+
+    private static Optional<YangExpr> of(final boolean value) {
+        return Optional.of(YangBooleanExpr.of(value));
+    }
+
+    private static double coerceDouble(final YangNumberExpr expr) {
+        final Number number = expr.getNumber();
+        verify(number instanceof Double);
+        return number.doubleValue();
+    }
+}
