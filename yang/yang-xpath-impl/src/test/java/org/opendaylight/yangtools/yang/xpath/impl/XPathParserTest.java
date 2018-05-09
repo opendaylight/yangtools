db6bf16acad801350d0e86f9c109a97c51888bb6
Robert Varga <robert.varga@pantheon.tech>
2018-05-09 20:54:22 +0200

    WIP: add yang-xpath-impl
    This adds an antlr-based XPath parser, which results in
    yang-xpath-api expressions.
    
    The grammar file (xpath.g4) is taken from
    https://github.com/antlr/grammars-v4/blob/master/xpath/xpath.g4
    at commit a3ff48932b0168d93d184a988fa40723b60f6c27.
    
    Change-Id: Ic7cfdb2d8d132577840f910786f72ed75b17c79e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-xpath-impl/src/test/java/org/opendaylight/yangtools/yang/xpath/impl/XPathParserTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.xpath.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.common.collect.ImmutableMap;
+import java.net.URI;
+import java.util.Map;
+import javax.xml.xpath.XPathExpressionException;
+import org.eclipse.jdt.annotation.Nullable;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.xpath.api.YangBooleanConstantExpr;
+import org.opendaylight.yangtools.yang.xpath.api.YangExpr;
+
+@SuppressWarnings("null")
+public class XPathParserTest {
+    private static final QNameModule DEF_NS = QNameModule.create(URI.create("defaultns"));
+    private static final Map<String, QNameModule> NAMESPACES = ImmutableMap.of(
+        "foo", QNameModule.create(URI.create("foo")),
+        "bar", QNameModule.create(URI.create("bar")));
+
+    private @Nullable XPathParser<?> parser;
+
+    @Before
+    public void before() {
+        parser = new ExactXPathParser(DEF_NS, NAMESPACES::get);
+    }
+
+    @Test
+    public void testSmoke() throws XPathExpressionException {
+        parseExpr("3 + 5");
+        parseExpr("/a/b");
+        parseExpr("a/b");
+        parseExpr("./a/b");
+        parseExpr("../a/b");
+        parseExpr("foo:foo");
+        parseExpr("@foo");
+        parseExpr("@foo:foo");
+        parseExpr("current()");
+        parseExpr("foo:self()");
+        parseExpr("foo:comment()");
+        parseExpr("/a[foo = 2 and bar = 3]");
+        parseExpr("/a[foo = \"2\" and bar = '3']");
+        parseExpr("$foo:comment");
+        parseExpr("$comment");
+        parseExpr("$self");
+    }
+
+    @Test
+    public void testUnionSquashing() throws XPathExpressionException {
+        final YangExpr a = parseExpr("a");
+        assertEquals(a, parseExpr("a|a"));
+        assertEquals(a, parseExpr("a|a|a"));
+        assertEquals(a, parseExpr("a|a|a|a"));
+
+        final YangExpr ab = parseExpr("a|b");
+        assertEquals(ab, parseExpr("a|b|a"));
+        assertEquals(ab, parseExpr("a|b|a|b"));
+        assertEquals(ab, parseExpr("a|a|b|a|b"));
+        assertEquals(ab, parseExpr("a|b|b|a|b"));
+        assertEquals(ab, parseExpr("a|b|a|a|b"));
+    }
+
+    @Test
+    public void testNumberSquashing() throws XPathExpressionException {
+        final YangExpr two = parseExpr("2");
+        assertEquals(two, parseExpr("1 + 1"));
+        assertEquals(two, parseExpr("3 - 1"));
+        assertEquals(two, parseExpr("2 * 1"));
+        assertEquals(two, parseExpr("4 div 2"));
+        assertEquals(two, parseExpr("6 mod 4"));
+    }
+
+    @Test
+    public void testSameExprSquashing() throws XPathExpressionException {
+        // Expressions
+        assertEquals(YangBooleanConstantExpr.FALSE, parseExpr("/a != /a"));
+        assertEquals(YangBooleanConstantExpr.TRUE, parseExpr("/a = /a"));
+
+        // Numbers
+        assertEquals(YangBooleanConstantExpr.FALSE, parseExpr("2 != 2"));
+        assertEquals(YangBooleanConstantExpr.FALSE, parseExpr("2 != (1 + 1)"));
+        assertEquals(YangBooleanConstantExpr.TRUE, parseExpr("2 = 2"));
+        assertEquals(YangBooleanConstantExpr.TRUE, parseExpr("2 = (1 + 1)"));
+    }
+
+    private YangExpr parseExpr(final String xpath) throws XPathExpressionException {
+        return parser.parseExpression(xpath).getRootExpr();
+    }
+}
