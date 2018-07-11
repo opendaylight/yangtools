06a1586927f6330557048c1b3fe9d85e25bc3203
Robert Varga <robert.varga@pantheon.tech>
2018-07-11 22:31:26 +0200

    Update ListenerRegistry implementation
    ListenerRegistry needs some usability improvements in the future,
    as well as it can be written with better GC behavior.
    
    Change-Id: Ibcef7af65816a0f084b8614718bade64a3f0f11d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- common/util/src/main/java/org/opendaylight/yangtools/util/ListenerRegistry.java
+++ common/util/src/main/java/org/opendaylight/yangtools/util/ListenerRegistry.java
@@ -7,71 +7,111 @@
  */
 package org.opendaylight.yangtools.util;
 
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects;
 import java.util.Collections;
 import java.util.EventListener;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import org.opendaylight.yangtools.concepts.AbstractObjectRegistration;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+import javax.annotation.concurrent.ThreadSafe;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.concepts.AbstractListenerRegistration;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.concepts.Mutable;
 
-public class ListenerRegistry<T extends EventListener> implements Iterable<ListenerRegistration<T>> {
+@ThreadSafe
+//FIXME: 3.0.0: make final, do not implement Iterable
+public class ListenerRegistry<T extends EventListener> implements Iterable<ListenerRegistration<T>>, Mutable {
 
-    private final ConcurrentMap<ListenerRegistration<? extends T>,ListenerRegistration<? extends T>> listeners;
-    final Set<ListenerRegistration<T>> unmodifiableView;
+    private final Set<ListenerRegistration<? extends T>> listeners = ConcurrentHashMap.newKeySet();
+    // This conversion is known to be safe.
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    private final Set<ListenerRegistration<T>> unmodifiableView = (Set) Collections.unmodifiableSet(listeners);
 
-    @SuppressWarnings("unchecked")
-    public ListenerRegistry() {
-        listeners = new ConcurrentHashMap<>();
-        // This conversion is known to be safe.
-        @SuppressWarnings("rawtypes")
-        final Set rawSet = Collections.unmodifiableSet(listeners.keySet());
-        unmodifiableView = rawSet;
+    private final String name;
+
+    private ListenerRegistry(final String name) {
+        this.name = name;
     }
 
+    /**
+     * Default constructor.
+     *
+     * @deprecated This class will not be subclassable, use {@link #create()} instead.
+     */
+    @Deprecated
+    public ListenerRegistry() {
+        this(null);
+    }
+
+    public static <T extends EventListener> ListenerRegistry<T> create() {
+        return new ListenerRegistry<>(null);
+    }
+
+    public static <T extends EventListener> ListenerRegistry<T> create(final @NonNull String name) {
+        return new ListenerRegistry<>(requireNonNull(name));
+    }
+
+    // FIXME: 3.0.0: return Set<ListenerRegistration<? extends T>>
+    // FIXME: 3.0.0: rename to getRegistrations()
     public Iterable<ListenerRegistration<T>> getListeners() {
         return unmodifiableView;
     }
 
+    public boolean isEmpty() {
+        return listeners.isEmpty();
+    }
+
+    public Stream<? extends T> streamListeners() {
+        return listeners.stream().map(ListenerRegistration::getInstance);
+    }
+
+    // FIXME: 3.0.0: change return type to what registerWithType does
     public ListenerRegistration<T> register(final T listener) {
         if (listener == null) {
             throw new IllegalArgumentException("Listener should not be null.");
         }
-        final ListenerRegistrationImpl<T> ret = new ListenerRegistrationImpl<>(listener);
-        listeners.put(ret,ret);
-        return ret;
+        return registerWithType(listener);
     }
 
+    // FIXME: 3.0.0: remove this method
     public <L extends T> ListenerRegistration<L> registerWithType(final L listener) {
-        final ListenerRegistrationImpl<L> ret = new ListenerRegistrationImpl<>(listener);
-        listeners.put(ret,ret);
+        final ListenerRegistration<L> ret = new ListenerRegistrationImpl<>(listener, listeners::remove);
+        listeners.add(ret);
         return ret;
     }
 
     @Override
-    public java.util.Iterator<ListenerRegistration<T>> iterator() {
+    public Iterator<ListenerRegistration<T>> iterator() {
         return unmodifiableView.iterator();
     }
 
-    @SuppressWarnings("rawtypes")
-    private void remove(final ListenerRegistrationImpl registration) {
-        listeners.remove(registration);
+    @Override
+    public String toString() {
+        return name == null ? super.toString() : MoreObjects.toStringHelper(this)
+                .add("name", name)
+                .add("size", listeners.size())
+                .toString();
     }
 
-    private class ListenerRegistrationImpl<P extends EventListener> extends AbstractObjectRegistration<P> implements
-            ListenerRegistration<P> {
+    private static final class ListenerRegistrationImpl<T extends EventListener>
+            extends AbstractListenerRegistration<T> {
+        private Consumer<ListenerRegistration<? super T>> removeCall;
 
-        ListenerRegistrationImpl(final P instance) {
+        ListenerRegistrationImpl(final T instance, final Consumer<ListenerRegistration<? super T>> removeCall) {
             super(instance);
+            this.removeCall = requireNonNull(removeCall);
         }
 
         @Override
         protected void removeRegistration() {
-            ListenerRegistry.this.remove(this);
+            removeCall.accept(this);
+            // Do not retail reference to that state
+            removeCall = null;
         }
-    }
-
-    public static <T extends EventListener> ListenerRegistry<T> create() {
-        return new ListenerRegistry<>();
     }
 }
