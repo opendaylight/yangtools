a3ef4e2603890e276abed74f04b23b3f6ec126d3
Robert Varga <robert.varga@pantheon.tech>
2018-07-12 11:26:35 +0200

    Do not use SchemaNodeIdentifierBuildNamespace prerequisites
    Incremental lookups on ChildSchemaNodeNamespace allow us to react
    to childen being created, hence we do not have an assumption that
    the target node must be instantiated by someone else (like eager
    instantiation).
    
    This will also allow us better recovery if the target node is not
    going to appear due to being disabled by if-feature.
    
    JIRA: YANGTOOLS-859
    Change-Id: Ibc05e18037bb85d42ed7123293e7f83f896aad5d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/deviate/AbstractDeviateStatementSupport.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/deviate/AbstractDeviateStatementSupport.java
@@ -23,7 +23,7 @@ import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 import org.opendaylight.yangtools.yang.model.api.stmt.DeviateStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
-import org.opendaylight.yangtools.yang.parser.rfc7950.namespace.SchemaNodeIdentifierBuildNamespace;
+import org.opendaylight.yangtools.yang.parser.rfc7950.namespace.ChildSchemaNodeNamespace;
 import org.opendaylight.yangtools.yang.parser.rfc7950.reactor.YangValidationBundles;
 import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractStatementSupport;
 import org.opendaylight.yangtools.yang.parser.spi.meta.CopyType;
@@ -144,7 +144,7 @@ abstract class AbstractDeviateStatementSupport extends AbstractStatementSupport<
 
         final Prerequisite<Mutable<?, ?, EffectiveStatement<?, ?>>> targetCtxPrerequisite =
                 deviateAction.mutatesEffectiveCtx(deviateStmtCtx.getRoot(),
-                    SchemaNodeIdentifierBuildNamespace.class,  deviationTarget);
+                    ChildSchemaNodeNamespace.class, deviationTarget.getPathFromRoot());
 
         deviateAction.apply(new InferenceAction() {
             @Override
