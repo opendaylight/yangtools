fd3b0138ed5419975796ea35d7f8ed13a5a9a55d
Robert Varga <robert.varga@pantheon.tech>
2018-07-16 10:47:32 +0200

    Add AbstractEffectiveOperationContainerSchemaNode
    InputEffectiveStatementImpl and OutputEffectiveStatementImpl are
    pretty much exactly the same, add a common superclass to hold their
    implementation.
    
    Change-Id: I1b7b174a006da8430f233a1ac43591cdf6563912
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/AbstractEffectiveOperationContainerSchemaNode.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.stmt;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.ImmutableSet;
+import java.util.Objects;
+import java.util.Set;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.ActionDefinition;
+import org.opendaylight.yangtools.yang.model.api.NotificationDefinition;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+
+@Beta
+public abstract class AbstractEffectiveOperationContainerSchemaNode<D extends DeclaredStatement<QName>>
+        extends AbstractEffectiveContainerSchemaNode<D> {
+    protected AbstractEffectiveOperationContainerSchemaNode(final StmtContext<QName, D, ?> ctx) {
+        super(ctx);
+    }
+
+    @Override
+    public final Set<ActionDefinition> getActions() {
+        return ImmutableSet.of();
+    }
+
+    @Override
+    public final Set<NotificationDefinition> getNotifications() {
+        return ImmutableSet.of();
+    }
+
+    @Override
+    public final boolean isPresenceContainer() {
+        // FIXME: this should not really be here
+        return false;
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(getQName(), getPath());
+    }
+
+    @Override
+    public final boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        AbstractEffectiveOperationContainerSchemaNode<?> other = (AbstractEffectiveOperationContainerSchemaNode<?>) obj;
+        return Objects.equals(getQName(), other.getQName()) && Objects.equals(getPath(), other.getPath());
+    }
+
+    @Override
+    public final String toString() {
+        return MoreObjects.toStringHelper(this).add("path", getPath()).toString();
+    }
+}
--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/output/OutputEffectiveStatementImpl.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/output/OutputEffectiveStatementImpl.java
@@ -7,68 +7,17 @@
  */
 package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.output;
 
-import com.google.common.collect.ImmutableSet;
-import java.util.Objects;
-import java.util.Set;
 import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.model.api.ActionDefinition;
-import org.opendaylight.yangtools.yang.model.api.NotificationDefinition;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.OutputEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.OutputStatement;
-import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractEffectiveContainerSchemaNode;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractEffectiveOperationContainerSchemaNode;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 
-final class OutputEffectiveStatementImpl extends AbstractEffectiveContainerSchemaNode<OutputStatement>
+final class OutputEffectiveStatementImpl extends AbstractEffectiveOperationContainerSchemaNode<OutputStatement>
         implements OutputEffectiveStatement {
-
     OutputEffectiveStatementImpl(
             final StmtContext<QName, OutputStatement, EffectiveStatement<QName, OutputStatement>> ctx) {
         super(ctx);
-    }
-
-    @Override
-    public Set<ActionDefinition> getActions() {
-        return ImmutableSet.of();
-    }
-
-    @Override
-    public Set<NotificationDefinition> getNotifications() {
-        return ImmutableSet.of();
-    }
-
-    @Override
-    public boolean isPresenceContainer() {
-        // FIXME: this should not really be here
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + Objects.hashCode(getQName());
-        result = prime * result + Objects.hashCode(getPath());
-        return result;
-    }
-
-    @Override
-    public boolean equals(final Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        OutputEffectiveStatementImpl other = (OutputEffectiveStatementImpl) obj;
-        return Objects.equals(getQName(), other.getQName()) && Objects.equals(getPath(), other.getPath());
-    }
-
-    @Override
-    public String toString() {
-        return "RPC Output " + getQName().getLocalName();
     }
 }
