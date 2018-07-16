fe563f120ab8105bf2931bd91384d3671981e6d8
Robert Varga <robert.varga@pantheon.tech>
2018-07-16 10:47:32 +0200

    Add AbstractEffectiveOperationContainerSchemaNode
    InputEffectiveStatementImpl and OutputEffectiveStatementImpl are
    pretty much exactly the same, add a common superclass to hold their
    implementation.
    
    Change-Id: I1b7b174a006da8430f233a1ac43591cdf6563912
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/input/InputEffectiveStatementImpl.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/input/InputEffectiveStatementImpl.java
@@ -7,68 +7,17 @@
  */
 package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.input;
 
-import com.google.common.collect.ImmutableSet;
-import java.util.Objects;
-import java.util.Set;
 import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.model.api.ActionDefinition;
-import org.opendaylight.yangtools.yang.model.api.NotificationDefinition;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.InputEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.InputStatement;
-import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractEffectiveContainerSchemaNode;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractEffectiveOperationContainerSchemaNode;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 
-final class InputEffectiveStatementImpl extends AbstractEffectiveContainerSchemaNode<InputStatement>
+final class InputEffectiveStatementImpl extends AbstractEffectiveOperationContainerSchemaNode<InputStatement>
         implements InputEffectiveStatement {
-
     InputEffectiveStatementImpl(
             final StmtContext<QName, InputStatement, EffectiveStatement<QName, InputStatement>> ctx) {
         super(ctx);
-    }
-
-    @Override
-    public Set<ActionDefinition> getActions() {
-        return ImmutableSet.of();
-    }
-
-    @Override
-    public Set<NotificationDefinition> getNotifications() {
-        return ImmutableSet.of();
-    }
-
-    @Override
-    public boolean isPresenceContainer() {
-        // FIXME: this should not really be here
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + Objects.hashCode(getQName());
-        result = prime * result + Objects.hashCode(getPath());
-        return result;
-    }
-
-    @Override
-    public boolean equals(final Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        InputEffectiveStatementImpl other = (InputEffectiveStatementImpl) obj;
-        return Objects.equals(getQName(), other.getQName()) && Objects.equals(getPath(), other.getPath());
-    }
-
-    @Override
-    public String toString() {
-        return "RPC input " + getQName().getLocalName();
     }
 }
