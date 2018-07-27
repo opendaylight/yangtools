22b61227083dc01c7eb759fc31c2c6946726db3b
Stephen Kitt <skitt@redhat.com>
2018-07-27 17:50:25 +0200

    Build with Mockito 2.1
    This adjusts to Mockito changes, including most tests.
    
    Change-Id: I75bea532168c68a4a468f2b41a458d40429ccbe8
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- yang/yang-model-util/src/test/java/org/opendaylight/yangtools/yang/model/repo/util/RefcountedRegistrationTest.java
+++ yang/yang-model-util/src/test/java/org/opendaylight/yangtools/yang/model/repo/util/RefcountedRegistrationTest.java
@@ -17,7 +17,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.opendaylight.yangtools.yang.model.repo.spi.SchemaSourceRegistration;
 
 @RunWith(MockitoJUnitRunner.class)
--- yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/parser/rfc7950/repo/SchemaContextUtilTest.java
+++ yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/parser/rfc7950/repo/SchemaContextUtilTest.java
@@ -10,7 +10,7 @@ package org.opendaylight.yangtools.yang.parser.rfc7950.repo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 
 import java.io.IOException;
