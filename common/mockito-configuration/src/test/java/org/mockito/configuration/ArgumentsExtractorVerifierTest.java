b32f7bd42bf80ce09beb0e767040ef503ba9dc51
Stephen Kitt <skitt@redhat.com>
2018-07-27 17:50:25 +0200

    Build with Mockito 2.1
    This adjusts to Mockito changes, including most tests.
    
    Change-Id: I75bea532168c68a4a468f2b41a458d40429ccbe8
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- common/mockito-configuration/src/test/java/org/mockito/configuration/ArgumentsExtractorVerifierTest.java
+++ common/mockito-configuration/src/test/java/org/mockito/configuration/ArgumentsExtractorVerifierTest.java
@@ -8,7 +8,7 @@
 package org.mockito.configuration;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 
