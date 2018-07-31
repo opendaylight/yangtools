bceea69792a617fc98b4d9246065ac7f158e6e93
Robert Varga <robert.varga@pantheon.tech>
2018-07-31 02:10:03 +0200

    Make empty lists and choices disappear
    This achieves automatic lifecycle behavior, so that lists disappear
    as soon as they have zero elements.
    
    Bug4454 test is updated to account for the list disappearing under
    normal operation.
    
    JIRA: YANGTOOLS-585
    Change-Id: I68220043c0f335324e5a8788b2872f57b750aae9
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>
    Signed-off-by: Igor Foltin <ifoltin@cisco.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/StructuralContainerModificationStrategy.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/StructuralContainerModificationStrategy.java
@@ -8,15 +8,10 @@
 package org.opendaylight.yangtools.yang.data.impl.schema.tree;
 
 import java.util.Optional;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
-import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
-import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodeContainer;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeConfiguration;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataValidationFailedException;
-import org.opendaylight.yangtools.yang.data.api.schema.tree.ModificationType;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.spi.TreeNode;
-import org.opendaylight.yangtools.yang.data.api.schema.tree.spi.TreeNodeFactory;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.spi.Version;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
 import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
@@ -29,101 +24,24 @@ import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
  * it enforces presence of mandatory leaves, which is not something we want here, as structural containers are not
  * root anchors for that validation.
  */
-final class StructuralContainerModificationStrategy extends ModificationApplyOperation {
-    /**
-     * Fake TreeNode version used in
-     * {@link #checkApplicable(ModificationPath, NodeModification, Optional, Version)}.
-     * It is okay to use a global constant, as the delegate will ignore it anyway. For
-     * {@link #apply(ModifiedNode, Optional, Version)} we will use the appropriate version as provided to us.
-     */
-    private static final Version FAKE_VERSION = Version.initial();
-    private final ContainerModificationStrategy delegate;
+final class StructuralContainerModificationStrategy extends ContainerModificationStrategy {
+    private final ContainerNode emptyNode;
 
-    StructuralContainerModificationStrategy(final ContainerSchemaNode schemaNode,
-        final DataTreeConfiguration treeConfig) {
-        this.delegate = new ContainerModificationStrategy(schemaNode, treeConfig);
-    }
-
-    private Optional<TreeNode> fakeMeta(final Version version) {
-        final ContainerNode container = ImmutableNodes.containerNode(delegate.getSchema().getQName());
-        return Optional.of(TreeNodeFactory.createTreeNode(container, version));
+    StructuralContainerModificationStrategy(final ContainerSchemaNode schema, final DataTreeConfiguration treeConfig) {
+        super(schema, treeConfig);
+        this.emptyNode = ImmutableNodes.containerNode(schema.getQName());
     }
 
     @Override
     Optional<TreeNode> apply(final ModifiedNode modification, final Optional<TreeNode> storeMeta,
             final Version version) {
-        final Optional<TreeNode> ret;
-        if (modification.getOperation() == LogicalOperation.TOUCH && !storeMeta.isPresent()) {
-            // Container is not present, let's take care of the 'magically appear' part of our job
-            ret = delegate.apply(modification, fakeMeta(version), version);
-
-            // Fake container got removed: that is a no-op
-            if (!ret.isPresent()) {
-                modification.resolveModificationType(ModificationType.UNMODIFIED);
-                return ret;
-            }
-
-            // If the delegate indicated SUBTREE_MODIFIED, account for the fake and report APPEARED
-            if (modification.getModificationType() == ModificationType.SUBTREE_MODIFIED) {
-                modification.resolveModificationType(ModificationType.APPEARED);
-            }
-        } else {
-            // Container is present, run normal apply operation
-            ret = delegate.apply(modification, storeMeta, version);
-
-            // Container was explicitly deleted, no magic required
-            if (!ret.isPresent()) {
-                return ret;
-            }
-        }
-
-        /*
-         * At this point ret is guaranteed to be present. We need to take care of the 'magically disappear' part of
-         * our job. Check if there are any child nodes left. If there are none, remove this container and turn the
-         * modification into a DISAPPEARED.
-         */
-        if (((NormalizedNodeContainer<?, ?, ?>) ret.get().getData()).getValue().isEmpty()) {
-            modification.resolveModificationType(ModificationType.DISAPPEARED);
-            return Optional.empty();
-        }
-
-        return ret;
+        return AutomaticLifecycleMixin.apply(super::apply, emptyNode, modification, storeMeta, version);
     }
 
     @Override
     void checkApplicable(final ModificationPath path, final NodeModification modification,
             final Optional<TreeNode> current, final Version version) throws DataValidationFailedException {
-        if (modification.getOperation() == LogicalOperation.TOUCH && !current.isPresent()) {
-            // Structural containers are created as needed, so we pretend this container is here
-            delegate.checkApplicable(path, modification, fakeMeta(FAKE_VERSION), version);
-        } else {
-            delegate.checkApplicable(path, modification, current, version);
-        }
-    }
-
-    @Override
-    void verifyStructure(final NormalizedNode<?, ?> modification, final boolean verifyChildren) {
-        delegate.verifyStructure(modification, verifyChildren);
-    }
-
-    @Override
-    void recursivelyVerifyStructure(final NormalizedNode<?, ?> value) {
-        delegate.recursivelyVerifyStructure(value);
-    }
-
-    @Override
-    ChildTrackingPolicy getChildPolicy() {
-        return delegate.getChildPolicy();
-    }
-
-    @Override
-    void mergeIntoModifiedNode(final ModifiedNode modification, final NormalizedNode<?, ?> value,
-            final Version version) {
-        delegate.mergeIntoModifiedNode(modification, value, version);
-    }
-
-    @Override
-    public Optional<ModificationApplyOperation> getChild(final PathArgument child) {
-        return delegate.getChild(child);
+        AutomaticLifecycleMixin.checkApplicable(super::checkApplicable, emptyNode, path, modification, current,
+            version);
     }
 }
