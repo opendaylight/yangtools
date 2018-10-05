02b27647161e6859d3b47506fc96532cfa25a7c7
Robert Varga <robert.varga@pantheon.tech>
2018-10-05 11:16:46 +0200

    Migrate parser extensions to JDT annotations
    This removes run-time reliance on javax.annotation package.
    
    JIRA: YANGTOOLS-907
    Change-Id: I9cd870882d8c7edfcc44a50154d0e4148bd4eca5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/openconfig-model-api/src/main/java/org/opendaylight/yangtools/openconfig/model/api/OpenConfigStatements.java
+++ yang/openconfig-model-api/src/main/java/org/opendaylight/yangtools/openconfig/model/api/OpenConfigStatements.java
@@ -10,14 +10,16 @@ package org.opendaylight.yangtools.openconfig.model.api;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.annotations.Beta;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 
 @Beta
+@NonNullByDefault
 public enum OpenConfigStatements implements StatementDefinition {
     OPENCONFIG_ENCRYPTED_VALUE(QName.create(OpenConfigConstants.ENCRYPTED_VALUE_MODULE, "openconfig-encrypted-value"),
         null, OpenConfigHashedValueStatement.class, OpenConfigHashedValueEffectiveStatement.class),
@@ -29,8 +31,9 @@ public enum OpenConfigStatements implements StatementDefinition {
     private final Class<? extends EffectiveStatement<?, ?>> effectiveRepresentation;
     private final Class<? extends DeclaredStatement<?>> declaredRepresentation;
     private final QName statementName;
-    private final QName argumentName;
+    private final @Nullable QName argumentName;
 
+    @SuppressFBWarnings("NP_STORE_INTO_NONNULL_FIELD")
     OpenConfigStatements(final QName statementName, @Nullable final String argumentName,
             final Class<? extends DeclaredStatement<?>> declaredRepresentation,
             final Class<? extends EffectiveStatement<?, ?>> effectiveRepresentation) {
@@ -40,25 +43,21 @@ public enum OpenConfigStatements implements StatementDefinition {
         this.effectiveRepresentation = requireNonNull(effectiveRepresentation);
     }
 
-    @Nonnull
     @Override
     public QName getStatementName() {
         return statementName;
     }
 
     @Override
-    @Nullable
-    public QName getArgumentName() {
+    public @Nullable QName getArgumentName() {
         return argumentName;
     }
 
     @Override
-    @Nonnull
     public Class<? extends DeclaredStatement<?>> getDeclaredRepresentationClass() {
         return declaredRepresentation;
     }
 
-    @Nonnull
     @Override
     public Class<? extends EffectiveStatement<?, ?>> getEffectiveRepresentationClass() {
         return effectiveRepresentation;
