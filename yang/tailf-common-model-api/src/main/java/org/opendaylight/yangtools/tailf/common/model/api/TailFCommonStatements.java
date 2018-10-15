fb429274a279b5cc4e56ecf21e08036b46c7aa75
Robert Varga <robert.varga@pantheon.tech>
2018-10-15 18:48:24 +0200

    Add tailf-common support
    This adds minimal tailf-common.yang parser support, specifically
    support for mapping tailf:action to YANG 1.1 action.
    
    JIRA: YANGTOOLS-403
    Change-Id: I11b324f46ead73ba198f2b854390a1c7c47d3509
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/tailf-common-model-api/src/main/java/org/opendaylight/yangtools/tailf/common/model/api/TailFCommonStatements.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.tailf.common.model.api;
+
+import static java.util.Objects.requireNonNull;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
+import org.opendaylight.yangtools.yang.model.api.stmt.ActionEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ActionStatement;
+
+/**
+ * {@link StatementDefinition}s for statements defined by tailf-common.yang.
+ *
+ * @author Robert Varga
+ */
+@NonNullByDefault
+public enum TailFCommonStatements implements StatementDefinition {
+    ACTION(QName.create(TailFCommonConstants.MODULE, "action"), "name", ActionStatement.class,
+        ActionEffectiveStatement.class);
+
+    private final Class<? extends EffectiveStatement<?, ?>> effectiveRepresentation;
+    private final Class<? extends DeclaredStatement<?>> declaredRepresentation;
+    private final QName statementName;
+    private final QName argumentName;
+
+    TailFCommonStatements(final QName statementName, final String argumentName,
+            final Class<? extends DeclaredStatement<?>> declaredRepresentation,
+            final Class<? extends EffectiveStatement<?, ?>> effectiveRepresentation) {
+        this.statementName = statementName.intern();
+        this.argumentName = QName.create(statementName, argumentName).intern();
+        this.declaredRepresentation = requireNonNull(declaredRepresentation);
+        this.effectiveRepresentation = requireNonNull(effectiveRepresentation);
+    }
+
+    @Override
+    public QName getStatementName() {
+        return statementName;
+    }
+
+    @Override
+    public @NonNull QName getArgumentName() {
+        return argumentName;
+    }
+
+    @Override
+    public Class<? extends DeclaredStatement<?>> getDeclaredRepresentationClass() {
+        return declaredRepresentation;
+    }
+
+    @Override
+    public Class<? extends EffectiveStatement<?, ?>> getEffectiveRepresentationClass() {
+        return effectiveRepresentation;
+    }
+
+    @Override
+    public boolean isArgumentYinElement() {
+        return false;
+    }
+}
