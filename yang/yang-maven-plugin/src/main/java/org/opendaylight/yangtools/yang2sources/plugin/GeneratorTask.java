6dc9075447293b8282d23cb364b6c05db285cce1
Robert Varga <robert.varga@pantheon.tech>
2018-11-12 11:54:14 +0100

    WIP: Add internal wrapper tasks
    Rework plugin execution so that we can use both old-style and
    new-style plugins by introducing GeneratorTask. This is then
    specialized for both worlds.
    
    We rework the multi-version support so that we run reactor assemply
    twice if needed -- for each requested mode separately.
    
    // FIXME: reconcile commented-out code
    
    JIRA: YANGTOOLS-1147
    Change-Id: I494ad899fabfb065c91537019698fdb131e0f1f2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-maven-plugin/src/main/java/org/opendaylight/yangtools/yang2sources/plugin/GeneratorTask.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang2sources.plugin;
+
+import static java.util.Objects.requireNonNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import org.apache.maven.project.MavenProject;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.plugin.generator.api.FileGeneratorException;
+import org.opendaylight.yangtools.yang.model.repo.api.StatementParserMode;
+import org.sonatype.plexus.build.incremental.BuildContext;
+
+@NonNullByDefault
+abstract class GeneratorTask {
+    private final StatementParserMode parserMode;
+
+    GeneratorTask(final StatementParserMode parserMode) {
+        this.parserMode = requireNonNull(parserMode);
+    }
+
+    final StatementParserMode parserMode() {
+        return parserMode;
+    }
+
+    abstract void initialize(MavenProject project, ContextHolder context);
+
+    abstract Collection<File> execute(BuildContext buildContext) throws FileGeneratorException, IOException;
+}
