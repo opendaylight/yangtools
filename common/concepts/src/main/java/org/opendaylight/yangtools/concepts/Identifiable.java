89ddc0a57c389ff3feb403cbd0bd5709f01a8e14
Robert Varga <robert.varga@pantheon.tech>
2018-11-15 15:31:25 +0100

    Document Identifiable interface contract
    Somehow this core interface went undocumented, fix that and also
    clarify it's companion Identifier contract.
    
    Change-Id: I009ed90e548770b46d636e5e41c07877642682d5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- common/concepts/src/main/java/org/opendaylight/yangtools/concepts/Identifiable.java
+++ common/concepts/src/main/java/org/opendaylight/yangtools/concepts/Identifiable.java
@@ -9,6 +9,15 @@ package org.opendaylight.yangtools.concepts;
 
 import org.eclipse.jdt.annotation.NonNull;
 
+/**
+ * A generalized contract of an object which has an identifier. The identifier, unlike the object carrying it, must
+ * generally comply to the interface contract outlined in {@link Identifier}, but this is not enforced in this API's
+ * design, as there are external classes, such as {@link String}, which comply to the contract without implementing it.
+ *
+ * @param <T> Identifier class, must conform to API contract outlined by {@link Identifier}, even if it does not
+ *            implement it.
+ */
+// FIXME: 3.0.0: consider requiring "T extends Serializable"
 public interface Identifiable<T> {
     /**
      * Return this objects Identifier.
