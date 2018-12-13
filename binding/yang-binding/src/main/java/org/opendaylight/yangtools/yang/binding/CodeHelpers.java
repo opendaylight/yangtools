6bb343b9265961d8a8c2465c863d126a500a7612
Robert Varga <robert.varga@pantheon.tech>
2018-12-13 20:07:37 +0100

    Add CodeHelpers.throwInvalidRange() specializations
    Range-checking code can (and does) emit checks based on primitive
    types, hence it is forcing a boxing operation.
    
    Provide overloaded methods to allow generated code to bind to both
    int- and long-based specializations, resulting is less byte code
    being generated.
    
    Change-Id: If6d3cc04c3f7d3d3d5fb3ad3a10f3c3d8f813654
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/yang-binding/src/main/java/org/opendaylight/yangtools/yang/binding/CodeHelpers.java
+++ binding/yang-binding/src/main/java/org/opendaylight/yangtools/yang/binding/CodeHelpers.java
@@ -188,6 +188,30 @@ public final class CodeHelpers {
     /**
      * Throw an IllegalArgument exception describing a range violation.
      *
+     * @param expected String describing expected ranges
+     * @param actual Actual observed value
+     * @throws IllegalArgumentException always
+     */
+    public static void throwInvalidRange(final String expected, final int actual) {
+        // Not a code duplication: provides faster string concat via StringBuilder.append(int)
+        throw new IllegalArgumentException("Invalid range: " + actual + ", expected: " + expected + ".");
+    }
+
+    /**
+     * Throw an IllegalArgument exception describing a range violation.
+     *
+     * @param expected String describing expected ranges
+     * @param actual Actual observed value
+     * @throws IllegalArgumentException always
+     */
+    public static void throwInvalidRange(final String expected, final long actual) {
+        // Not a code duplication: provides faster string concat via StringBuilder.append(long)
+        throw new IllegalArgumentException("Invalid range: " + actual + ", expected: " + expected + ".");
+    }
+
+    /**
+     * Throw an IllegalArgument exception describing a range violation.
+     *
      * @param expected Objects describing expected ranges
      * @param actual Actual observed byte array
      * @throws IllegalArgumentException always
@@ -244,7 +268,7 @@ public final class CodeHelpers {
      * @param hash Wrapped object hash
      * @return Wrapper object hash
      */
-    private static int wrapHashCode(int hash) {
+    private static int wrapHashCode(final int hash) {
         return hash == 0 ? 31 : hash;
     }
 }
