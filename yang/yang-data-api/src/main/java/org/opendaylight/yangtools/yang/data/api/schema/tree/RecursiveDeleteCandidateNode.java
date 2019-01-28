92b0ef83235d999d8561782b9dc149140dbec165
Robert Varga <robert.varga@pantheon.tech>
2019-01-28 22:17:54 +0100

    Move NormalizedNode-based DataTreeCandidateNodes into API
    These utility classes are useful for downstreams, and we have
    an API point from where we can expose them in a relatively clean
    fashion.
    
    Move them to API package and expose them through DataTreeCandidateNodes
    utility class.
    
    Change-Id: Idaa276323f78baa273200f8d8c6b77e53fa60c6a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit e8ac6b68244f35fce04f421314259ec8683652aa)

--- yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/tree/RecursiveDeleteCandidateNode.java
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/tree/RecursiveDeleteCandidateNode.java
@@ -5,35 +5,28 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.yangtools.yang.data.impl.schema.tree;
+package org.opendaylight.yangtools.yang.data.api.schema.tree;
 
 import java.util.Optional;
-import javax.annotation.Nonnull;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodeContainer;
-import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidateNode;
-import org.opendaylight.yangtools.yang.data.api.schema.tree.ModificationType;
 
-final class RecursiveUnmodifiedCandidateNode extends AbstractRecursiveCandidateNode {
-    protected RecursiveUnmodifiedCandidateNode(
-            final NormalizedNodeContainer<?, PathArgument, NormalizedNode<?, ?>> data) {
+final class RecursiveDeleteCandidateNode extends AbstractRecursiveCandidateNode {
+    RecursiveDeleteCandidateNode(final NormalizedNodeContainer<?, PathArgument, NormalizedNode<?, ?>> data) {
         super(data);
     }
 
     @Override
-    @Nonnull
     public ModificationType getModificationType() {
-        return ModificationType.UNMODIFIED;
+        return ModificationType.DELETE;
     }
 
-    @Nonnull
     @Override
     public Optional<NormalizedNode<?, ?>> getDataAfter() {
-        return dataOptional();
+        return Optional.empty();
     }
 
-    @Nonnull
     @Override
     public Optional<NormalizedNode<?, ?>> getDataBefore() {
         return dataOptional();
@@ -42,11 +35,11 @@ final class RecursiveUnmodifiedCandidateNode extends AbstractRecursiveCandidateN
     @Override
     DataTreeCandidateNode createContainer(
             final NormalizedNodeContainer<?, PathArgument, NormalizedNode<?, ?>> childData) {
-        return new RecursiveUnmodifiedCandidateNode(childData);
+        return new RecursiveDeleteCandidateNode(childData);
     }
 
     @Override
     DataTreeCandidateNode createLeaf(final NormalizedNode<?, ?> childData) {
-        return new UnmodifiedLeafCandidateNode(childData);
+        return new DeleteLeafCandidateNode(childData);
     }
 }
