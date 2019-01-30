084e4389e5204e6ce55447a4b5c9954e0d01293c
Robert Varga <robert.varga@pantheon.tech>
2019-01-30 13:49:40 +0100

    Eliminate no-op MandatoryLeafEnforcer
    Running through a no-op enforcer, while efficient, still requires
    a bimorphic invocation, which results most of the time in a no-op.
    
    Eliminate the no-op implementation and refactor its two users, so
    that they provide dedicated subclasses which use enforcer. This has
    the benefit of not overriding SchemaAwareApplyOperation just to
    run a noop invocation.
    
    Change-Id: Iff44eb6efa048bb86a71b0d7753d3e58425b5e4d
    JIRA: YANGTOOLS-945
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/ContainerModificationStrategy.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/ContainerModificationStrategy.java
@@ -8,29 +8,80 @@
 package org.opendaylight.yangtools.yang.data.impl.schema.tree;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Objects.requireNonNull;
 
 import com.google.common.base.Preconditions;
+import java.util.Optional;
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeConfiguration;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.spi.TreeNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.spi.Version;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
 import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.DataContainerNodeBuilder;
 import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableContainerNodeBuilder;
 import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
 
 /**
- * General container modification strategy. This is used by {@link PresenceContainerModificationStrategy}, which needs
- * to tap into {@link SchemaAwareApplyOperation}'s operations, or wrapped through {@link AutomaticLifecycleMixin} when
- * the container is a structural one.
+ * General container modification strategy. This is used by {@link EnforcingMandatory} in case of presence containers
+ * with mandatory nodes, as it needs to tap into {@link SchemaAwareApplyOperation}'s operations, or wrapped through
+ * {@link AutomaticLifecycleMixin} when the container is a structural one.
  */
 class ContainerModificationStrategy extends AbstractDataNodeContainerModificationStrategy<ContainerSchemaNode> {
+    private static final class EnforcingMandatory extends ContainerModificationStrategy {
+        private final MandatoryLeafEnforcer enforcer;
+
+        EnforcingMandatory(final ContainerSchemaNode schemaNode, final DataTreeConfiguration treeConfig,
+                final MandatoryLeafEnforcer enforcer) {
+            super(schemaNode, treeConfig);
+            this.enforcer = requireNonNull(enforcer);
+        }
+
+        @Override
+        void verifyStructure(final NormalizedNode<?, ?> writtenValue, final boolean verifyChildren) {
+            super.verifyStructure(writtenValue, verifyChildren);
+            if (verifyChildren) {
+                enforcer.enforceOnData(writtenValue);
+            }
+        }
+
+        @Override
+        protected TreeNode applyMerge(final ModifiedNode modification, final TreeNode currentMeta,
+                final Version version) {
+            final TreeNode ret = super.applyMerge(modification, currentMeta, version);
+            enforcer.enforceOnTreeNode(ret);
+            return ret;
+        }
+
+        @Override
+        protected TreeNode applyWrite(final ModifiedNode modification, final NormalizedNode<?, ?> newValue,
+                final Optional<TreeNode> currentMeta, final Version version) {
+            final TreeNode ret = super.applyWrite(modification, newValue, currentMeta, version);
+            enforcer.enforceOnTreeNode(ret);
+            return ret;
+        }
+
+        @Override
+        protected TreeNode applyTouch(final ModifiedNode modification, final TreeNode currentMeta,
+                final Version version) {
+            final TreeNode ret = super.applyTouch(modification, currentMeta, version);
+            enforcer.enforceOnTreeNode(ret);
+            return ret;
+        }
+    }
+
     ContainerModificationStrategy(final ContainerSchemaNode schemaNode, final DataTreeConfiguration treeConfig) {
         super(schemaNode, ContainerNode.class, treeConfig);
     }
 
     static ModificationApplyOperation of(final ContainerSchemaNode schema, final DataTreeConfiguration treeConfig) {
-        return schema.isPresenceContainer() ? new PresenceContainerModificationStrategy(schema, treeConfig)
-                : new AutomaticLifecycleMixin(new ContainerModificationStrategy(schema, treeConfig),
+        if (schema.isPresenceContainer()) {
+            final Optional<MandatoryLeafEnforcer> enforcer = MandatoryLeafEnforcer.forContainer(schema, treeConfig);
+            return enforcer.isPresent() ? new EnforcingMandatory(schema, treeConfig, enforcer.get())
+                    : new ContainerModificationStrategy(schema, treeConfig);
+        }
+
+        return new AutomaticLifecycleMixin(new ContainerModificationStrategy(schema, treeConfig),
                     ImmutableNodes.containerNode(schema.getQName()));
     }
 
