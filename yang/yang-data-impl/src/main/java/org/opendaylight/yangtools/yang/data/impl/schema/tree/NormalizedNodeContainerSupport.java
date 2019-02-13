d367b8e984a53cb80a8c78f7efd3240cf331129d
Robert Varga <robert.varga@pantheon.tech>
2019-02-13 13:43:29 +0100

    WIP: rework checkTouchApplicable in terms of NormalizedNodeContainerSupport
    This is stepping stone towards full automatic integration, eliminating
    the need for explicit methods.
    
    Change-Id: Ifc3045938650ffc1b8604584d7507828198c82e5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/NormalizedNodeContainerSupport.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/NormalizedNodeContainerSupport.java
@@ -8,16 +8,90 @@
 package org.opendaylight.yangtools.yang.data.impl.schema.tree;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Verify.verify;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.base.MoreObjects;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.AugmentationIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.AugmentationNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.NormalizedNodeContainerBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableAugmentationNodeBuilder;
+import org.opendaylight.yangtools.yang.data.util.DataSchemaContextNode;
+import org.opendaylight.yangtools.yang.model.api.AugmentationSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.DocumentedNode.WithStatus;
+import org.opendaylight.yangtools.yang.model.api.SchemaNode;
 
-final class NormalizedNodeContainerSupport<K extends PathArgument, T extends NormalizedNode<K, ?>> {
+abstract class NormalizedNodeContainerSupport<K extends PathArgument, T extends NormalizedNode<K, ?>> {
+    static final class Augmentation extends NormalizedNodeContainerSupport<AugmentationIdentifier, AugmentationNode> {
+        Augmentation() {
+            super(AugmentationNode.class, ImmutableAugmentationNodeBuilder::create,
+                ImmutableAugmentationNodeBuilder::create);
+        }
+
+        @Override
+        AugmentationNode defaultEmptyValue(final WithStatus schemaNode) {
+            verify(schemaNode instanceof AugmentationSchemaNode, "Unexpected schema %s", schemaNode);
+            final AugmentationIdentifier identifier = DataSchemaContextNode.augmentationIdentifierFrom(
+                (AugmentationSchemaNode) schemaNode);
+            return ImmutableAugmentationNodeBuilder.create().withNodeIdentifier(identifier).build();
+        }
+    }
+
+    static final class Automatic<T extends NormalizedNode<NodeIdentifier, ?>, S extends SchemaNode>
+            extends NormalizedNodeContainerSupport<NodeIdentifier, T> {
+
+        private final Class<S> schemaClass;
+
+        Automatic(final Class<T> requiredClass, final Class<S> schemaClass,
+            final Function<T, NormalizedNodeContainerBuilder<NodeIdentifier, ?, ?, T>> copyBuilder,
+            final Supplier<NormalizedNodeContainerBuilder<NodeIdentifier, ?, ?, T>> emptyBuilder) {
+            super(requiredClass, copyBuilder, emptyBuilder);
+            this.schemaClass = requireNonNull(schemaClass);
+        }
+
+        Automatic(final Class<T> requiredClass, final Class<S> schemaClass, final ChildTrackingPolicy childPolicy,
+            final Function<T, NormalizedNodeContainerBuilder<NodeIdentifier, ?, ?, T>> copyBuilder,
+            final Supplier<NormalizedNodeContainerBuilder<NodeIdentifier, ?, ?, T>> emptyBuilder) {
+            super(requiredClass, childPolicy, copyBuilder, emptyBuilder);
+            this.schemaClass = requireNonNull(schemaClass);
+        }
+
+        @Override
+        T defaultEmptyValue(final WithStatus schemaNode) {
+            verify(schemaClass.isInstance(schemaNode), "Unexpected schema %s while expecting %s", schemaNode,
+                schemaClass);
+            final QName qname = schemaClass.cast(schemaNode).getQName();
+            return emptyBuilder.get().withNodeIdentifier(NodeIdentifier.create(qname)).build();
+        }
+    }
+
+    static final class Manual<K extends PathArgument, T extends NormalizedNode<K, ?>>
+        extends NormalizedNodeContainerSupport<K, T> {
+
+        Manual(final Class<T> requiredClass, final Function<T, NormalizedNodeContainerBuilder<K, ?, ?, T>> copyBuilder,
+            final Supplier<NormalizedNodeContainerBuilder<K, ?, ?, T>> emptyBuilder) {
+            super(requiredClass, copyBuilder, emptyBuilder);
+        }
+
+        Manual(final Class<T> requiredClass, final ChildTrackingPolicy childPolicy,
+            final Function<T, NormalizedNodeContainerBuilder<K, ?, ?, T>> copyBuilder,
+            final Supplier<NormalizedNodeContainerBuilder<K, ?, ?, T>> emptyBuilder) {
+            super(requiredClass, childPolicy, copyBuilder, emptyBuilder);
+        }
+
+        @Override
+        T defaultEmptyValue(final WithStatus schemaNode) {
+            return null;
+        }
+    }
+
     final Function<T, NormalizedNodeContainerBuilder<K, ?, ?, T>> copyBuilder;
     final Supplier<NormalizedNodeContainerBuilder<K, ?, ?, T>> emptyBuilder;
     final ChildTrackingPolicy childPolicy;
@@ -38,13 +112,15 @@ final class NormalizedNodeContainerSupport<K extends PathArgument, T extends Nor
         this(requiredClass, ChildTrackingPolicy.UNORDERED, copyBuilder, emptyBuilder);
     }
 
-    NormalizedNodeContainerBuilder<?, ?, ?, T> createBuilder(final NormalizedNode<?, ?> original) {
+    final NormalizedNodeContainerBuilder<?, ?, ?, T> createBuilder(final NormalizedNode<?, ?> original) {
         return copyBuilder.apply(cast(original));
     }
 
-    NormalizedNode<?, ?> createEmptyValue(final NormalizedNode<?, ?> original) {
+    final T createEmptyValue(final NormalizedNode<?, ?> original) {
         return emptyBuilder.get().withNodeIdentifier(cast(original).getIdentifier()).build();
     }
+
+    abstract @Nullable T defaultEmptyValue(WithStatus schemaNode);
 
     private T cast(final NormalizedNode<?, ?> original) {
         checkArgument(requiredClass.isInstance(original), "Require %s, got %s", requiredClass, original);
