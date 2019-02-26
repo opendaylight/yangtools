d3bd58ed1f89d2bc84c420b93529ef0f9182d277
Robert Varga <robert.varga@pantheon.tech>
2019-02-26 22:14:28 +0100

    Update yang-xpath-api design
    This is the first round of API design update, bringing the following
    changes:
    - MathMode is now a top-level enumeration
    - YangXPathMathSupport is an API interface providing common functions
    - Prefix resolution is done through YangNamespaceContext
    - YangNaryExpr is final
    - math-specific YangNumberExprs are hidden classes
    - YangFunctionCallExpr.NoArgs is folded into YangFunctionCallExpr
    - YangLiteralExpr is now final
    - YangXPathExpression exposes MathMode
    - Literal interpretation is lazy and works on subexpressions
    
    Change-Id: Ie721678b5513dc088c1a267b98f38d9b0643cb33
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-xpath-api/src/main/java/org/opendaylight/yangtools/yang/xpath/api/YangXPathMathSupport.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2019 Pantheon Technologies, s.r.o.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.xpath.api;
+
+import com.google.common.annotations.Beta;
+import java.util.Optional;
+
+@Beta
+public interface YangXPathMathSupport<N extends YangNumberExpr<N, ?>> {
+    /**
+     * Create a {@link YangNumberExpr} backed by specified string.
+     *
+     * @param str String
+     * @return number expression
+     * @throws NullPointerException if {@code str} is null
+     * @throws NumberFormatException if the string does not represent a valid number
+     */
+    N createNumber(String str);
+
+    /**
+     * Create a {@link YangNumberExpr} representing the negated value of a number.
+     *
+     * @param number input number
+     * @return negated number expression
+     * @throws NullPointerException if {@code number} is null
+     * @throws IllegalArgumentException if {@code number} has unrecognized type
+     */
+    N negateNumber(YangNumberExpr<?, ?> number);
+
+    /**
+     * Attempt to evaluate an  operator and its left- and right-handside.
+     *
+     * @param operator Operator to apply
+     * @param left Left hand-side
+     * @param right Right hand-side
+     * @return Evaluation result, if evaluation succeeded
+     * @throws NullPointerException if any of the arguments is null
+     */
+    Optional<YangExpr> tryEvaluate(YangBinaryOperator operator, YangNumberExpr<?, ?> left, YangNumberExpr<?, ?> right);
+}
