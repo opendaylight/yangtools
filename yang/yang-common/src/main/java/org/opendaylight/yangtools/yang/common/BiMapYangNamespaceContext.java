98877b53a98082aea263db1f17b01f4353f55a77
Robert Varga <robert.varga@pantheon.tech>
2019-02-27 09:35:18 +0100

    Add YangNamespaceContext
    We require prefix/QNameModule mapping in multiple contexts, some
    of which are bound to a SchemaContext, some of which are bound to
    a particular Module.
    
    This patch adds a generalized YangNamespaceContext to serve in these
    situations and provides a simple BiMap-based implementation.
    
    Change-Id: I3d44a55ea3569532395b3acb9dc7dedba70c1f4e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/BiMapYangNamespaceContext.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2019 Pantheon Technologies, s.r.o.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.common;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.ImmutableBiMap;
+import com.google.common.collect.ImmutableBiMap.Builder;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.Map.Entry;
+import java.util.Optional;
+import org.opendaylight.yangtools.concepts.WritableObject;
+
+/**
+ * A BiMap-based implementation of {@link YangNamespaceContext}. This implementation requires the default namespace
+ * to be present.
+ *
+ */
+@Beta
+public final class BiMapYangNamespaceContext implements YangNamespaceContext, WritableObject {
+    private static final long serialVersionUID = 1L;
+
+    private final ImmutableBiMap<String, QNameModule> mapping;
+    private final QNameModule defaultNamespace;
+
+    public BiMapYangNamespaceContext(final QNameModule defaultNamespace, final BiMap<String, QNameModule> mapping) {
+        this.defaultNamespace = requireNonNull(defaultNamespace);
+        checkArgument(mapping.containsValue(defaultNamespace), "Mapping %s does not contain default namespace %s",
+            mapping, defaultNamespace);
+        this.mapping = ImmutableBiMap.copyOf(mapping);
+    }
+
+    @Override
+    public Optional<QNameModule> getDefaultNamespace() {
+        return Optional.of(defaultNamespace);
+    }
+
+    @Override
+    public Optional<QNameModule> findNamespaceForPrefix(final String prefix) {
+        return Optional.ofNullable(mapping.get(requireNonNull(prefix)));
+    }
+
+    @Override
+    public Optional<String> findPrefixForNamespace(final QNameModule namespace) {
+        return Optional.ofNullable(mapping.inverse().get(requireNonNull(namespace)));
+    }
+
+    @Override
+    public void writeTo(final DataOutput out) throws IOException {
+        defaultNamespace.writeTo(out);
+        out.writeInt(mapping.size());
+        for (Entry<String, QNameModule> entry : mapping.entrySet()) {
+            out.writeUTF(entry.getKey());
+            entry.getValue().writeTo(out);
+        }
+    }
+
+    public static BiMapYangNamespaceContext readFrom(final DataInput in) throws IOException {
+        final QNameModule defaultNamespace = QNameModule.readFrom(in);
+        final int size = in.readInt();
+        final Builder<String, QNameModule> builder = ImmutableBiMap.builder();
+        for (int i = 0; i < size; ++i) {
+            final String prefix = in.readUTF();
+            final QNameModule namespace = QNameModule.readFrom(in);
+            builder.put(prefix, namespace);
+        }
+
+        return new BiMapYangNamespaceContext(defaultNamespace, builder.build());
+    }
+}
