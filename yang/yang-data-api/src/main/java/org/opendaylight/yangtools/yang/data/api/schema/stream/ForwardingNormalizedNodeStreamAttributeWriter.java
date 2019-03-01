b14694e456212c9655b87bb4e5718384173db67c
Robert Varga <robert.varga@pantheon.tech>
2019-03-01 14:05:52 +0100

    Rework NormalizedNodeStreamWriter
    This patch reworks NormalizedNodeStreamWriter to issue start/end
    events for even simple nodes, allowing attributes to be seamlessly
    integrated into open nodes.
    
    JIRA: YANGTOOLS-497
    Change-Id: I9aa0979b778f54ce77be365b3ad8ba6690138df2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/stream/ForwardingNormalizedNodeStreamAttributeWriter.java
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/stream/ForwardingNormalizedNodeStreamAttributeWriter.java
@@ -10,8 +10,6 @@ package org.opendaylight.yangtools.yang.data.api.schema.stream;
 import java.io.IOException;
 import java.util.Map;
 import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
 
 public abstract class ForwardingNormalizedNodeStreamAttributeWriter extends ForwardingNormalizedNodeStreamWriter
         implements NormalizedNodeStreamAttributeWriter {
@@ -19,38 +17,7 @@ public abstract class ForwardingNormalizedNodeStreamAttributeWriter extends Forw
     protected abstract NormalizedNodeStreamAttributeWriter delegate();
 
     @Override
-    public void leafNode(final NodeIdentifier name, final Object value, final Map<QName, String> attributes)
-            throws IOException {
-        delegate().leafNode(name, value, attributes);
-    }
-
-    @Override
-    public void leafSetEntryNode(final QName name, final Object value, final Map<QName, String> attributes)
-            throws IOException {
-        delegate().leafSetEntryNode(name, value, attributes);
-    }
-
-    @Override
-    public void startContainerNode(final NodeIdentifier name, final int childSizeHint,
-            final Map<QName, String> attributes) throws IOException {
-        delegate().startContainerNode(name, childSizeHint, attributes);
-    }
-
-    @Override
-    public void startYangModeledAnyXmlNode(final NodeIdentifier name, final int childSizeHint,
-            final Map<QName, String> attributes) throws IOException {
-        delegate().startYangModeledAnyXmlNode(name, childSizeHint, attributes);
-    }
-
-    @Override
-    public void startUnkeyedListItem(final NodeIdentifier name, final int childSizeHint,
-            final Map<QName, String> attributes) throws IOException {
-        delegate().startUnkeyedListItem(name, childSizeHint, attributes);
-    }
-
-    @Override
-    public void startMapEntryNode(final NodeIdentifierWithPredicates identifier, final int childSizeHint,
-            final Map<QName, String> attributes) throws IOException {
-        delegate().startMapEntryNode(identifier, childSizeHint, attributes);
+    public void attributes(final Map<QName, String> attributes) throws IOException {
+        delegate().attributes(attributes);
     }
 }
