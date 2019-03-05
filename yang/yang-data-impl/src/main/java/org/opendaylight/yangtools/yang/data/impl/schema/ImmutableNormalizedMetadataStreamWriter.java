f11bac0fe131e8084cc97065b52e7e79899cac14
Robert Varga <robert.varga@pantheon.tech>
2019-03-05 12:40:19 +0100

    Integrate ImmutableNormalizedNodeStreamWriter with metadata
    This adds basic immutable, builder-driven implementation of
    NormalizedMetadata.
    
    It also teaches ImmutableNormalizedNodeStreamWriter about
    metadata annotations by implementing NormalizedMetadataStreamWriter
    interface and exposing acquired metadata through
    NormalizedNodeMetadataResult.
    
    JIRA: YANGTOOLS-961
    Change-Id: Ifff34a8885a5ee0593520d6aae47c55adfcd66dd
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/ImmutableNormalizedMetadataStreamWriter.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.impl.schema;
+
+import com.google.common.annotations.Beta;
+import com.google.common.collect.ClassToInstanceMap;
+import com.google.common.collect.ImmutableClassToInstanceMap;
+import com.google.common.collect.ImmutableMap;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.rfc7952.data.api.NormalizedMetadataStreamWriter;
+import org.opendaylight.yangtools.rfc7952.data.util.ImmutableNormalizedMetadata;
+import org.opendaylight.yangtools.rfc7952.data.util.ImmutableNormalizedMetadata.Builder;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriterExtension;
+
+@Beta
+public class ImmutableNormalizedMetadataStreamWriter extends ImmutableNormalizedNodeStreamWriter
+        implements NormalizedMetadataStreamWriter {
+    private final Deque<Builder> builders = new ArrayDeque<>();
+
+    protected ImmutableNormalizedMetadataStreamWriter(final NormalizedNodeResult result) {
+        super(result);
+    }
+
+    @Override
+    public final ClassToInstanceMap<NormalizedNodeStreamWriterExtension> getExtensions() {
+        return ImmutableClassToInstanceMap.of(NormalizedMetadataStreamWriter.class, this);
+    }
+
+    @Override
+    protected void enter(final PathArgument name) {
+        super.enter(name);
+        builders.push(ImmutableNormalizedMetadata.builder().withIdentifier(name));
+    }
+
+    @Override
+    protected @Nullable NormalizedNode<PathArgument, ?> exit(final NormalizedNode<PathArgument, ?> product) {
+
+
+        return super.exit(product);
+    }
+
+    @Override
+    public final void metadata(final ImmutableMap<QName, Object> metadata) throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public final void nodeValue(final Object value) {
+
+    }
+}
