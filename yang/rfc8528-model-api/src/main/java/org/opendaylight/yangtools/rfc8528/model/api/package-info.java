02ecd82fc3064d209944bd3b1470cde7f9245975
Robert Varga <robert.varga@pantheon.tech>
2019-03-07 20:18:21 +0100

    Add YANG Schema Mount parser support
    This adds the necessary bits and pieces to properly support
    mount-point extension in the parser.
    
    JIRA: YANGTOOLS-965
    Change-Id: I7ca1b6c8c961e3033dc8023089d9169573dc3c4e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 18f08017dcdc114f5f7e22c171cac79b38ea0526)

---
+++ yang/rfc8528-model-api/src/main/java/org/opendaylight/yangtools/rfc8528/model/api/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * YANG metamodel extensions to support mount-poing, as defined in
+ * <a href="https://tools.ietf.org/html/rfc8528">RFC8528</a>. For SchemaNode world entry space is defined via
+ * {@link org.opendaylight.yangtools.rfc8528.model.api.MountPointSchemaNode}'s static methods.
+ *
+ * @author Robert Varga
+ */
+package org.opendaylight.yangtools.rfc8528.model.api;
