61f594baea187e23e0d0a8809d8548cdb72ec907
Robert Varga <robert.varga@pantheon.tech>
2019-03-20 23:15:16 +0100

    Introduce UnqualifiedQName and QualifiedQName
    For the purposes of XPath expressions we need the concept of a future
    node-identifier which is not bound to a concrete namespace (which
    is already covered by QName).
    
    This node identifier comes in two forms, qualified and unqualified,
    each of which undergoes different namespace binding lifecycle.
    
    This patch introduces AbstractQName, which serves as the base class
    for all QName constructs and two separate classes UnqualifiedQName
    and QualifiedQName, which have the ability to be be converted to
    a full QName.
    
    Note that due to Serializable contract, QName cannot be made a
    subclass of AbstractQName. This fact is marked up for a future
    API change.
    
    Change-Id: I87242dcab93ecdd1050630fd4b3c320419ae10d5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/UnqualifiedQName.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.common;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.Interner;
+import com.google.common.collect.Interners;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * An unresolved, unqualified {@link QName}. It is guaranteed to hold a valid {@link #getLocalName()}, in the default
+ * namespace, which is not resolved. A resolved {@link QName} can be constructed through {@link #bindTo(QNameModule)}.
+ */
+@Beta
+@NonNullByDefault
+public final class UnqualifiedQName extends AbstractQName implements Comparable<UnqualifiedQName> {
+    private static final long serialVersionUID = 1L;
+    private static final Interner<UnqualifiedQName> INTERNER = Interners.newWeakInterner();
+
+    private UnqualifiedQName(final String localName) {
+        super(localName);
+    }
+
+    public static UnqualifiedQName of(final String localName) {
+        return new UnqualifiedQName(checkLocalName(localName));
+    }
+
+    /**
+     * Read an UnboundQName from a DataInput. The format is expected to match the output format of
+     * {@link #writeTo(DataOutput)}.
+     *
+     * @param in DataInput to read
+     * @return An UnboundQName instance
+     * @throws IOException if I/O error occurs
+     */
+    public static UnqualifiedQName readFrom(final DataInput in) throws IOException {
+        return of(in.readUTF());
+    }
+
+    public QName bindTo(final QNameModule namespace) {
+        return new QName(namespace, getLocalName());
+    }
+
+    @Override
+    @SuppressFBWarnings(value = "ES_COMPARING_PARAMETER_STRING_WITH_EQ", justification = "Interning identity check")
+    public UnqualifiedQName intern() {
+        // Make sure to intern the string and check whether it refers to the same name as we are
+        final String name = getLocalName();
+        final String internedName = name.intern();
+        final UnqualifiedQName template = internedName == name ? this : new UnqualifiedQName(internedName);
+        return INTERNER.intern(template);
+    }
+
+    @Override
+    @SuppressWarnings("checkstyle:parameterName")
+    public int compareTo(final UnqualifiedQName o) {
+        return getLocalName().compareTo(o.getLocalName());
+    }
+
+    @Override
+    public void writeTo(final DataOutput out) throws IOException {
+        out.writeUTF(getLocalName());
+    }
+
+    @Override
+    public int hashCode() {
+        return getLocalName().hashCode();
+    }
+
+    @Override
+    public boolean equals(final @Nullable Object obj) {
+        return this == obj || obj instanceof UnqualifiedQName
+                && getLocalName().equals(((AbstractQName) obj).getLocalName());
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this).add("localName", getLocalName()).toString();
+    }
+
+    @Override
+    Object writeReplace() {
+        return new UQNv1(this);
+    }
+}
