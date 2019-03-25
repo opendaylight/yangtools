9c17d319f699c6abdec0b7911de5bc31702725ed
Robert Varga <robert.varga@pantheon.tech>
2019-03-25 18:21:26 +0100

    Add RFC6241 parser support
    This adds extensions to identify get-config/get filter.
    
    JIRA: YANGTOOLS-831
    Change-Id: Ib38c31f1b2dd82793cebe72f0ba8e7300f1b6404
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/rfc6241-parser-support/src/main/java/org/opendaylight/yangtools/rfc6241/parser/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * YANG parser support for metamodel extensions defined in <a href="https://tools.ietf.org/html/rfc6241">RFC6241</a>.
+ * Add {@link org.opendaylight.yangtools.rfc6241.parser.GetFilterElementAttributesStatementSupport} to your reactor to
+ * add support for this extension.
+ *
+ * @author Robert Varga
+ */
+package org.opendaylight.yangtools.rfc6241.parser;
