82f842c0061cb7722ec6e21960f85227509547e8
Robert Varga <robert.varga@pantheon.tech>
2019-03-26 11:46:21 +0100

    Address DataTreeTip 3.0.0 FIXMEs
    This documents exceptions being thrown and switches to
    @NonNullByDefault.
    
    Change-Id: Ibb8df7bb87fbf29888bc95dc978def3229bbcc06
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/tree/DataTreeTip.java
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/tree/DataTreeTip.java
@@ -8,21 +8,23 @@
 package org.opendaylight.yangtools.yang.data.api.schema.tree;
 
 import com.google.common.annotations.Beta;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 
 /**
  * Tip of a data tree instance. It acts as a point to which modifications can be applied.
  */
-// FIXME: 3.0.0: Use @NonNullByDefault
 @Beta
+@NonNullByDefault
 public interface DataTreeTip {
     /**
      * Validate whether a particular modification can be applied to the data tree.
      *
      * @param modification Data tree modification.
      * @throws DataValidationFailedException If modification data is not valid.
+     * @throws NullPointerException if modification is null
+     * @throws IllegalArgumentException if modification is unrecognized
      */
-    // FIXME: 3.0.0: document NullPointerException being thrown
-    // FIXME: 3.0.0: document IllegalArgumentException being thrown
     void validate(DataTreeModification modification) throws DataValidationFailedException;
 
     /**
@@ -30,10 +32,10 @@ public interface DataTreeTip {
      *
      * @param modification Data tree modification.
      * @return candidate data tree
+     * @throws NullPointerException if modification is null
+     * @throws IllegalArgumentException if modification is unrecognized
      */
-    // FIXME: 3.0.0: document NullPointerException being thrown
-    // FIXME: 3.0.0: document IllegalArgumentException being thrown
-    // FIXME: 3.0.0: throw DataValidationFailedException or similar
+    // FIXME: 4.0.0: throw DataValidationFailedException or similar
     DataTreeCandidateTip prepare(DataTreeModification modification);
 
     /**
@@ -51,5 +53,5 @@ public interface DataTreeTip {
      * equals implementation must result in identity comparison.
      */
     @Override
-    boolean equals(Object obj);
+    boolean equals(@Nullable Object obj);
 }
