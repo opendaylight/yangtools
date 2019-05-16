9ceb4a0648ae6db7e701b136d110ea6a2f55fd7b
Robert Varga <robert.varga@pantheon.tech>
2019-05-16 14:27:44 +0200

    Add OpaqueData structure
    anydata requires us to model its content, which we know effectively
    nothing about. Futhermore its content cannot be transported across
    serialization formats, notably from XML to JSON.
    
    This adds the prerequisite OpaqueData interfaces and their builders.
    
    JIRA: YANGTOOLS-978
    Change-Id: I62f214f09b7d8f58a5eb7f047b1ba67e250d7933
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/opaque/OpaqueDataValueBuilder.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.impl.schema.opaque;
+
+import static com.google.common.base.Preconditions.checkState;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.opaque.OpaqueDataValue;
+
+@Beta
+public final class OpaqueDataValueBuilder extends AbstractOpaqueDataNodeBuilder<OpaqueDataValue> {
+    private Object value;
+
+    @Override
+    public OpaqueDataValueBuilder withIdentifier(final NodeIdentifier identifier) {
+        super.withIdentifier(identifier);
+        return this;
+    }
+
+    public OpaqueDataValueBuilder withValue(final Object value) {
+        checkState(this.value == null, "Value has already been set");
+        this.value = requireNonNull(value);
+        return this;
+    }
+
+    @Override
+    public OpaqueDataValue build(final NodeIdentifier identifier) {
+        checkState(value != null, "Value has not been set");
+        return new ImmutableOpaqueDataValue(identifier, value);
+    }
+}
