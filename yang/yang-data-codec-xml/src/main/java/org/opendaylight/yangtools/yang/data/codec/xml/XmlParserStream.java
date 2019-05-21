1645e760d098ea69536ee7e68171e3295a9843a7
Robert Varga <robert.varga@pantheon.tech>
2019-05-21 11:54:47 +0200

    Tighten XmlParserStream.setValue()
    The argument type to setValue() is required to be a
    SimpleNodeDataWithSchema, make sure to express that in declaration
    and have callers perform cast -- which is already guarded by
    instanceof checks.
    
    Change-Id: I65c668c4dba8855d2e79014254182d2069a1d6cc
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit e77cc4f4f00c23195cc82e701153acc675637a44)

--- yang/yang-data-codec-xml/src/main/java/org/opendaylight/yangtools/yang/data/codec/xml/XmlParserStream.java
+++ yang/yang-data-codec-xml/src/main/java/org/opendaylight/yangtools/yang/data/codec/xml/XmlParserStream.java
@@ -304,7 +304,7 @@ public final class XmlParserStream implements Closeable, Flushable {
 
         if (parent instanceof LeafNodeDataWithSchema || parent instanceof LeafListEntryNodeDataWithSchema) {
             parent.setAttributes(getElementAttributes(in));
-            setValue(parent, in.getElementText().trim(), in.getNamespaceContext());
+            setValue((SimpleNodeDataWithSchema) parent, in.getElementText().trim(), in.getNamespaceContext());
             if (isNextEndDocument(in)) {
                 return;
             }
@@ -334,7 +334,7 @@ public final class XmlParserStream implements Closeable, Flushable {
         }
 
         if (parent instanceof AnyXmlNodeDataWithSchema) {
-            setValue(parent, readAnyXmlValue(in), in.getNamespaceContext());
+            setValue((AnyXmlNodeDataWithSchema) parent, readAnyXmlValue(in), in.getNamespaceContext());
             if (isNextEndDocument(in)) {
                 return;
             }
@@ -465,15 +465,12 @@ public final class XmlParserStream implements Closeable, Flushable {
         in.nextTag();
     }
 
-    private void setValue(final AbstractNodeDataWithSchema parent, final Object value,
+    private void setValue(final SimpleNodeDataWithSchema parent, final Object value,
             final NamespaceContext nsContext) {
-        checkArgument(parent instanceof SimpleNodeDataWithSchema, "Node %s is not a simple type",
-                parent.getSchema().getQName());
-        final SimpleNodeDataWithSchema parentSimpleNode = (SimpleNodeDataWithSchema) parent;
-        checkArgument(parentSimpleNode.getValue() == null, "Node '%s' has already set its value to '%s'",
-                parentSimpleNode.getSchema().getQName(), parentSimpleNode.getValue());
-
-        parentSimpleNode.setValue(translateValueByType(value, parentSimpleNode.getSchema(), nsContext));
+        final DataSchemaNode schema = parent.getSchema();
+        final Object prev = parent.getValue();
+        checkArgument(prev == null, "Node '%s' has already set its value to '%s'", schema.getQName(), prev);
+        parent.setValue(translateValueByType(value, schema, nsContext));
     }
 
     private Object translateValueByType(final Object value, final DataSchemaNode node,
