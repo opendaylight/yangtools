7d85229f1f8213f8ea89adff75d4ed1339023569
Robert Varga <robert.varga@pantheon.tech>
2019-06-06 12:23:49 +0200

    Expose ReusableResultingStreamWriter
    ReusableImmutableNormalizedNodeStreamWriter has two properties
    which are not inherently tied to it being bound to immutable nodes:
    - it can have its state reset
    - it exposes a NormalizedNode result of a streaming session
    
    Expose these traits as ReusableResultingStreamWriter, so that users
    can use the API rather than the implementation.
    
    Change-Id: I841211c89520c4a5c67d436c8a49d719e8f7f1b7
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/ReusableImmutableNormalizedNodeStreamWriter.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/ReusableImmutableNormalizedNodeStreamWriter.java
@@ -12,13 +12,15 @@ import static java.util.Objects.requireNonNull;
 import com.google.common.annotations.Beta;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.ReusableResultingStreamWriter;
 
 /**
  * A reusable variant of {@link ImmutableNormalizedNodeStreamWriter}. It can be reset into its base state and used for
  * multiple streaming sessions.
  */
 @Beta
-public final class ReusableImmutableNormalizedNodeStreamWriter extends ImmutableNormalizedNodeStreamWriter {
+public final class ReusableImmutableNormalizedNodeStreamWriter extends ImmutableNormalizedNodeStreamWriter
+        implements ReusableResultingStreamWriter {
     private final NormalizedNodeResultBuilder builder;
 
     private ReusableImmutableNormalizedNodeStreamWriter(final NormalizedNodeResultBuilder builder) {
@@ -30,11 +32,13 @@ public final class ReusableImmutableNormalizedNodeStreamWriter extends Immutable
         return new ReusableImmutableNormalizedNodeStreamWriter(new NormalizedNodeResultBuilder());
     }
 
+    @Override
     public void reset() {
         builder.result().reset();
         reset(builder);
     }
 
+    @Override
     public NormalizedNode<?, ?> getResult() {
         return builder.result().getResult();
     }
