e987f4bb25b987a373ff7500c9dd8107e9555dfa
Robert Varga <robert.varga@pantheon.tech>
2019-06-06 12:23:49 +0200

    Expose ReusableStreamReceiver
    ReusableImmutableNormalizedNodeStreamWriter has two properties
    which are not inherently tied to it being bound to immutable nodes:
    - it can have its state reset
    - it exposes a NormalizedNode result of a streaming session
    
    Expose these traits as ReusableStreamReceiver, so that users
    can use the API rather than the implementation.
    
    Change-Id: I841211c89520c4a5c67d436c8a49d719e8f7f1b7
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 9e66147b4a8167007e6d3479c449ab9944cce5f9)
    (cherry picked from commit 721a100e4ee9042e8ccdc43d09805b88ed94c4da)

---
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/stream/ReusableStreamReceiver.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.api.schema.stream;
+
+import com.google.common.annotations.Beta;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+/**
+ * Utility marker interface for {@link NormalizedNodeStreamWriter} implementations which can be reused multiple times
+ * and can expose a {@link NormalizedNode} result of each complete streaming use.
+ *
+ * <p>
+ * An example of use would be:
+ * <pre>
+ *   ReusableStreamReceiver writer;
+ *   final NormalizedNode result;
+ *
+ *   try {
+ *       // pipe events into writer:
+ *       writer.startContainer(...);
+ *       ...
+ *
+ *       result = writer.getResult();
+ *   } finally {
+ *       writer.reset();
+ *   }
+ * </pre>
+ *
+ * <p>
+ * Note the writer should always be {@link #reset()} in a {@code finally} block, so that any streaming state is
+ * properly discarded.
+ */
+@Beta
+public interface ReusableStreamReceiver extends NormalizedNodeStreamWriter {
+    /**
+     * Acquire the result of the last streaming session.
+     *
+     * @return Result of streaming.
+     */
+    NormalizedNode<?, ?> getResult();
+
+    /**
+     * Reset this writer to initial state.
+     */
+    void reset();
+}
