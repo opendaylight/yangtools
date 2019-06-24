b63925f54d5d5dd543ea03285f66ef4bf1dd5330
Robert Varga <robert.varga@pantheon.tech>
2019-06-24 18:01:16 +0200

    Allow Errata 5617 expressions in leafref
    This updates PathExpression interface to allow expressing
    leafref paths which start with a deref(...) function call,
    as proposed by https://www.rfc-editor.org/errata/eid5617.
    
    This unfortunately necessitates breaking the API contract of this
    interface, as the original object model could not express these.
    
    JIRA: YANGTOOLS-968
    Change-Id: I3a193d338de5a82b95d2dd6cc5200e77032e67d9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/path/UnparsedPathExpression.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/path/UnparsedPathExpression.java
@@ -12,7 +12,6 @@ import static java.util.Objects.requireNonNull;
 import com.google.common.base.MoreObjects.ToStringHelper;
 import org.opendaylight.yangtools.yang.model.util.AbstractPathExpression;
 import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.ArgumentUtils;
-import org.opendaylight.yangtools.yang.xpath.api.YangLocationPath;
 
 final class UnparsedPathExpression extends AbstractPathExpression {
     private final RuntimeException cause;
@@ -30,7 +29,7 @@ final class UnparsedPathExpression extends AbstractPathExpression {
     }
 
     @Override
-    public YangLocationPath getLocation() {
+    public Steps getSteps() {
         throw new UnsupportedOperationException("Expression '" + getOriginalString() + "' was not parsed", cause);
     }
 
