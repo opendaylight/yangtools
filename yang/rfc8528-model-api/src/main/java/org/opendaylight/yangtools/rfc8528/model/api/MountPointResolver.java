d5fab172d6e866613132500ec76a79328866b216
Robert Varga <robert.varga@pantheon.tech>
2019-06-26 15:06:12 +0200

    Add preliminary XML support for RFC8528
    This patch adds the rfc8528-model-api interface to support parsing
    of shared schema mount points and wires XmlParserStream to use it,
    so that the mount point internals are directly appended to the
    NormalizedNode structure.
    
    JIRA: YANGTOOLS-1007
    Change-Id: I1e595fafcc0aa1cd41c2cc6afc469d5152953dc6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/rfc8528-model-api/src/main/java/org/opendaylight/yangtools/rfc8528/model/api/MountPointResolver.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8528.model.api;
+
+import com.google.common.annotations.Beta;
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.common.QName;
+
+/**
+ * Interface provider resolving a mount point label, as defined by the effective module statement. This interface
+ * support shared schema mount points, i.e. those which are fully defined by the label.
+ */
+@Beta
+@NonNullByDefault
+public interface MountPointResolver {
+    /**
+     * Resolve the SchemaContext for specified shared schema mount point label.
+     *
+     * @param label Mount point label, as defined in the effective model
+     * @return Optional SchemaContext, empty if the label is not defined
+     * @throws NullPointerException if label is null
+     */
+    // FIXME: 5.0.0: require EffectiveModelContext here
+    Optional<? extends MountPointStrategy> resolveLabel(QName label);
+
+    /**
+     * Return a {@link MountPointResolver} which reports every label as undefined.
+     *
+     * @return A resolver instance
+     */
+    static MountPointResolver noop() {
+        return NoopSharedMountPointResolver.INSTANCE;
+    }
+}
