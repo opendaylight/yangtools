3fc16068c3df55d0f4c99148e1fe106f11da5522
Robert Varga <robert.varga@pantheon.tech>
2019-06-26 15:06:12 +0200

    WIP: Add XML support for parsing schema-mount data
    When we cannot find a direct descendant of a particular schema,
    we can still interpret it as a schema-mounted element, if the
    corresponding schema allows for that.
    
    // FIXME: add a simple unit test
    
    JIRA: YANGTOOLS-1007
    Change-Id: I1e595fafcc0aa1cd41c2cc6afc469d5152953dc6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-data-codec-xml/src/main/java/org/opendaylight/yangtools/yang/data/codec/xml/DOMSourceMountPointChild.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.codec.xml;
+
+import static java.util.Objects.requireNonNull;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.transform.dom.DOMSource;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.impl.schema.AbstractMountPointChild;
+import org.opendaylight.yangtools.yang.data.util.MountPointChild;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.xml.sax.SAXException;
+
+/**
+ * Internal MountPointChild implementation, reusing data bits from {@link DOMSourceAnydata}.
+ */
+@NonNullByDefault
+final class DOMSourceMountPointChild extends AbstractMountPointChild implements MountPointChild {
+    private final DOMSource source;
+
+    DOMSourceMountPointChild(final DOMSource source) {
+        this.source = requireNonNull(source);
+    }
+
+    @Override
+    public void writeTo(final NormalizedNodeStreamWriter writer, final SchemaContext schemaContext)
+            throws IOException {
+
+        final XmlParserStream xmlParser;
+        try {
+            xmlParser = XmlParserStream.create(writer, schemaContext, schemaContext);
+        } catch (IllegalArgumentException e) {
+            throw new IOException("Failed to instantiate XML parser", e);
+        }
+
+        try {
+            final XMLStreamReader reader = new DOMSourceXMLStreamReader(source);
+            xmlParser.parse(reader).flush();
+        } catch (XMLStreamException | URISyntaxException | SAXException e) {
+            throw new IOException("Failed to parse payload", e);
+        }
+    }
+}
