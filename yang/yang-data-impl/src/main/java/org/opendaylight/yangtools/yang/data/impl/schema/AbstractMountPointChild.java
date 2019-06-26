fafd2bc208fd8e27ad5e1199ab074a00220e2f85
Robert Varga <robert.varga@pantheon.tech>
2019-06-26 15:06:12 +0200

    WIP: Add XML support for parsing schema-mount data
    When we cannot find a direct descendant of a particular schema,
    we can still interpret it as a schema-mounted element, if the
    corresponding schema allows for that.
    
    // FIXME: add a simple unit test
    
    JIRA: YANGTOOLS-1007
    Change-Id: I1e595fafcc0aa1cd41c2cc6afc469d5152953dc6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/AbstractMountPointChild.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.impl.schema;
+
+import com.google.common.annotations.Beta;
+import java.io.IOException;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.util.MountPointChild;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+/**
+ * Utility base class for implementing {@link MountPointChild} classes.
+ */
+@Beta
+public abstract class AbstractMountPointChild implements MountPointChild {
+    @Override
+    public final NormalizedNode<?, ?> normalizeTo(final SchemaContext schemaContext) throws IOException {
+        final NormalizedNodeResult result = new NormalizedNodeResult();
+        final NormalizedNodeStreamWriter streamWriter = ImmutableNormalizedNodeStreamWriter.from(result);
+        streamTo(streamWriter, schemaContext);
+        return result.getResult();
+    }
+}
