4e14fbc7c3f811595486ed549bd5398af0348d67
Robert Varga <robert.varga@pantheon.tech>
2019-07-09 11:30:43 +0200

    Add YangLibraryConstants
    RFC8528's data representation rules require components dealing with
    mountpoints on both schema and data levels to recognize
    ietf-yang-library data for the purposes of interpretation of other
    contained data. This patch adds the minimum information required
    to identify the data items required.
    
    Change-Id: I9be2e1d6527d316a475dc1e47d7670e75383810b
    JIRA: YANGTOOLS-1007
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/rfc8528-model-api/src/main/java/org/opendaylight/yangtools/rfc8528/model/api/YangLibraryConstants.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8528.model.api;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import java.net.URI;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Constants related to {@code ietf-yang-library.yang}. As schema-mount works in concert with yang-library, we need
+ * these constants to interpret correctly categorize incoming data and present them to schema resolution process.
+ *
+ * <p>
+ * While RFC7895 and RFC8525 are not strictly required by YANG, RFC7950 contains a weak reference to it when dealing
+ * with capability negotiation on protocol layers. Moreover RFC8528 makes it explicit that an instance of yang-library
+ * is mounted underneath both {@code inline} and {@code shared-schema} types of mount.
+ *
+ * <p>
+ * While we could mandate use of either RFC7895 or RFC8525 across the board, this is not feasible, as mount points may
+ * be nested and point to external systems -- hence it is completely possible to encounter both old and new information
+ * in a single mount point tree.
+ */
+@Beta
+@NonNullByDefault
+public final class YangLibraryConstants {
+    /**
+     * The namespace assigned to {@code ietf-yang-library}. This constant is required for XML-like parsers, using
+     * XML namespaces to reference modules.
+     */
+    public static final URI MODULE_NAMESPACE = URI.create("urn:ietf:params:xml:ns:yang:ietf-yang-library");
+    /**
+     * The module name assigned to {@code ietf-yang-library}. This constant is required for JSON-like parsers, using
+     * module names to reference modules.
+     */
+    public static final String MODULE_NAME = "ietf-yang-library";
+
+    /**
+     * Top-level containers which hold YANG Library information.
+     */
+    public enum ContainerName {
+        /**
+         * Container in RFC7895 (pre-NMDA) YANG Library.
+         */
+        RFC7895("modules-state"),
+        /**
+         * Container in RFC8525 (NMDA) YANG Library.
+         */
+        RFC8525("yang-library");
+
+        private final String localName;
+
+        ContainerName(final String localName) {
+            this.localName = requireNonNull(localName);
+        }
+
+        public String getLocalName() {
+            return localName;
+        }
+    }
+
+    private YangLibraryConstants() {
+        // Hidden
+    }
+}
