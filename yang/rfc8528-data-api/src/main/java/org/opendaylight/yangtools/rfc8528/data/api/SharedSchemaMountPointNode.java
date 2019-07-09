72adabb20173aeb9a3e73d28dd79a6244768099c
Robert Varga <robert.varga@pantheon.tech>
2019-07-09 22:29:38 +0200

    Add MountPointNode and its specializations
    MountPointNode is a utility construct to carry a NormalizedNode
    tree with mount point content attached.
    
    JIRA: YANGTOOLS-1010
    Change-Id: Ie33cd6e566584191d20f942eeb87ce6145e30717
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/rfc8528-data-api/src/main/java/org/opendaylight/yangtools/rfc8528/data/api/SharedSchemaMountPointNode.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8528.data.api;
+
+import com.google.common.annotations.Beta;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * A mount point with {@code schema-ref} shared-schema, as defined in RFC8528.
+ */
+@Beta
+@NonNullByDefault
+public interface SharedSchemaMountPointNode extends MountPointNode {
+    /*
+     * FIXME: consider whether this interface should contain some information based on 'parent-reference':
+     *        - List<YangXPathExpression.QualifiedBound> getParentReference()
+     *        - the node-set required to maintain referential integrity in the subtree of this node
+     */
+}
