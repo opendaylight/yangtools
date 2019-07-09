cde32391236e28135766a8db4269196ac8d4cfb8
Robert Varga <robert.varga@pantheon.tech>
2019-07-09 12:57:04 +0200

    Retrofit mount-point awareness into yang-data-util
    {Container,ListEntry}NodeDataWithSchema can potentially contain
    out-of-band mount point data, which needs intermediate processing
    once all data has been acquired.
    
    We introduce AbstractMountPointDataWithSchema as a common superclass,
    which optionally holds MountPointData.
    
    MountPointData contains enough information to resolve the data nodes
    in the context of a schema context supplied either statically or
    dynamically (based on the mount data).
    
    JIRA: YANGTOOLS-1007
    Change-Id: I08ba4dcf3aaf758f12b42e625d9d17a774d07825
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-data-util/src/main/java/org/opendaylight/yangtools/yang/data/util/MountPointChild.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.util;
+
+import com.google.common.annotations.Beta;
+import java.io.IOException;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizableAnydata;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+/**
+ * A raw child of {@link MountPointData}. This is similar in functionality to {@link NormalizableAnydata}, but
+ * rather than normalizing, the data is fed into a combination of a SchemaContext and NormalizedNodeStreamWriter.
+ */
+@Beta
+@NonNullByDefault
+public interface MountPointChild {
+    /**
+     * Stream this child into a writer, with the help of a SchemaContext.
+     *
+     * @param writer Writer to emit the child into
+     * @param schemaContext SchemaContext for normalization purposes
+     * @throws IOException if an underlying error occurs
+     * @throws NullPointerException if any of the arguments is null
+     */
+    void writeTo(NormalizedNodeStreamWriter writer, SchemaContext schemaContext) throws IOException;
+
+    /**
+     * Normalized this child to a particular SchemaContext.
+     *
+     * @param schemaContext SchemaContext for normalization purposes
+     * @return A NormalizedNode representation of this child
+     * @throws IOException if an underlying error occurs
+     * @throws NullPointerException if any of the arguments is null
+     */
+    NormalizedNode<?, ?> normalizeTo(SchemaContext schemaContext) throws IOException;
+}
