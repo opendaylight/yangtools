c0910f56cc9ce1ac58ac63639fa4ae7a9a23145e
Robert Varga <robert.varga@pantheon.tech>
2019-07-09 22:29:38 +0200

    WIP: add MountPointNode
    MountPointNode is a utility construct to carry a NormalizedNode
    tree with mount point content attached.
    
    Change-Id: Ie33cd6e566584191d20f942eeb87ce6145e30717
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/rfc8528-data-util/src/main/java/org/opendaylight/yangtools/rcf8528/data/util/InlineMountPointNodeImpl.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rcf8528.data.util;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.MoreObjects.ToStringHelper;
+import java.util.Collection;
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.rfc8528.data.api.InlineMountpointNode;
+import org.opendaylight.yangtools.rfc8528.data.api.MountPointIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.util.AbstractIdentifiableSchemaContextProvider;
+
+@Beta
+public final class InlineMountPointNodeImpl extends AbstractIdentifiableSchemaContextProvider<MountPointIdentifier>
+        implements InlineMountpointNode {
+    private final @NonNull ContainerNode delegate;
+
+    public InlineMountPointNodeImpl(final @NonNull SchemaContext schemaContext,
+            final @NonNull MountPointIdentifier identifier, final @NonNull ContainerNode delegate) {
+        super(schemaContext, identifier);
+        this.delegate = requireNonNull(delegate);
+    }
+
+    @Override
+    public Collection<DataContainerChild<? extends PathArgument, ?>> getValue() {
+        return delegate.getValue();
+    }
+
+    @Override
+    public Optional<DataContainerChild<? extends PathArgument, ?>> getChild(final PathArgument child) {
+        return delegate.getChild(child);
+    }
+
+    @Override
+    protected ToStringHelper addToStringAttributes(final ToStringHelper helper) {
+        return super.addToStringAttributes(helper).add("delegate", delegate);
+    }
+}
