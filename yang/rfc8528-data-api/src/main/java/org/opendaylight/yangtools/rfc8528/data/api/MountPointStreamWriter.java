39330a0d0598227efbbdf2da4565166ed0e846ce
Robert Varga <robert.varga@pantheon.tech>
2019-07-09 11:39:25 +0200

    Add rfc8528-data-api
    Mount points require a rather complex interaction with
    NormalizedNodeStreamWriter, which is defined in a new artifact.
    
    Change-Id: If3063151c7eca561b5382e2da6ddc339cba94e4f
    JIRA: YANGTOOLS-1007
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/rfc8528-data-api/src/main/java/org/opendaylight/yangtools/rfc8528/data/api/MountPointStreamWriter.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8528.data.api;
+
+import com.google.common.annotations.Beta;
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.rfc8528.model.api.MountPointSchemaResolver;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriterExtension;
+
+/**
+ * A {@link NormalizedNodeStreamWriterExtension} exposed by stream writers which can handle mount point data, notably
+ * providing the facilities to resolve a mount point schema and normalize mount point contents into a normalized
+ * structure.
+ */
+@Beta
+public interface MountPointStreamWriter extends NormalizedNodeStreamWriterExtension {
+    /**
+     * Attempt to acquire a {@link MountPointSchemaResolver} to resolve schemas for the purposes of interpreting this
+     * mount point. An empty result indicates the mount point is not attached.
+     *
+     * @param label Mount point label, as defined via the use of {@code mount-point} statement
+     * @return An optional handler for mount point data
+     * @throws NullPointerException if label is null
+     */
+    Optional<MountPointSchemaResolver> findMountPoint(@NonNull QName label);
+}
---
+++ yang/rfc8528-model-api/src/main/java/org/opendaylight/yangtools/rfc8528/model/api/StaticMountPointSchemaResolver.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8528.model.api;
+
+import com.google.common.annotations.Beta;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.api.SchemaContextProvider;
+
+/**
+ * A resolver which has static knowledge of the SchemaContext which should be used to interpret mount point data.
+ * Instances of this interface should be used in contexts where the mount point data is expected not to contain
+ * required {@code ietf-yang-library} data, for example due to filtering.
+ */
+@Beta
+public interface StaticMountPointSchemaResolver extends MountPointSchemaResolver, SchemaContextProvider {
+    @Override
+    @NonNull SchemaContext getSchemaContext();
+}
