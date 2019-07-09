d231cb9383fbe08b2bec3c6abade06fbf1344398
Robert Varga <robert.varga@pantheon.tech>
2019-07-09 22:29:38 +0200

    WIP: add MountPointNode
    MountPointNode is a utility construct to carry a NormalizedNode
    tree with mount point content attached.
    
    JIRA: YANGTOOLS-1010
    Change-Id: Ie33cd6e566584191d20f942eeb87ce6145e30717
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/rfc8528-data-api/src/main/java/org/opendaylight/yangtools/rfc8528/data/api/MountPointNode.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8528.data.api;
+
+import com.google.common.annotations.Beta;
+import java.util.Collection;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
+import org.opendaylight.yangtools.yang.data.api.schema.DataContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.MixinNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTree;
+
+/**
+ * Common NormalizedNode representation of a YANG mount point. This interface is not meant to be implemented directly,
+ * but rather used through its specializations like {@link InlineMountpointNode} and {@link SharedSchemaMountpointNode}.
+ *
+ * <p>
+ * Furthermore, these nodes are not meant to be stored in a {@link DataTree} and most NormalizedNode utilities will be
+ * confused when they see them. The purpose of this interface is making data interchange between mount point-aware
+ * components more seamless.
+ */
+@Beta
+public interface MountPointNode extends MixinNode, DataContainerNode<MountPointIdentifier>,
+        DataContainerChild<MountPointIdentifier, Collection<DataContainerChild<? extends PathArgument, ?>>> {
+
+    @Override
+    default QName getNodeType() {
+        return getIdentifier().getLabel();
+    }
+}
