99acb359026ee92bbef79a2e99bf334f00c60b68
Robert Varga <robert.varga@pantheon.tech>
2019-07-09 12:57:04 +0200

    WIP: retrofit mount-point awareness into yang-data-util
    {Container,ListEntry}NodeDataWithSchema can potentially contain
    out-of-band mount point data, which needs intermediate processing
    once all data has been acquired.
    
    We introduce AbstractMountPointDataWithSchema as a common superclass,
    which optionally holds MountPointData.
    
    MountPointData contains enough information to resolve the data nodes
    in the context of a schema context supplied either statically or
    dynamically (based on the mount data).
    
    JIRA: YANGTOOLS-1007
    Change-Id: I08ba4dcf3aaf758f12b42e625d9d17a774d07825
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-data-util/src/main/java/org/opendaylight/yangtools/yang/data/util/MountPointData.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.util;
+
+import static com.google.common.base.Preconditions.checkState;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.concepts.Identifiable;
+import org.opendaylight.yangtools.rfc8528.data.api.DynamicMountPointSchemaResolver;
+import org.opendaylight.yangtools.rfc8528.data.api.MountPointStreamWriter;
+import org.opendaylight.yangtools.rfc8528.model.api.MountPointSchemaResolver;
+import org.opendaylight.yangtools.rfc8528.model.api.StaticMountPointSchemaResolver;
+import org.opendaylight.yangtools.rfc8528.model.api.YangLibraryConstants.ContainerName;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizableAnydata;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * YANG Schema Mount-supported data attached to either a {@code list} item or a {@code container}.
+ */
+@Beta
+public final class MountPointData implements Identifiable<QName> {
+    private static final Logger LOG = LoggerFactory.getLogger(MountPointData.class);
+
+    private final Map<ContainerName, MountPointChild> yangLib = new EnumMap<>(ContainerName.class);
+    private final List<MountPointChild> children = new ArrayList<>();
+    private final @NonNull QName label;
+
+    MountPointData(final QName label) {
+        this.label = requireNonNull(label);
+    }
+
+    @Override
+    public QName getIdentifier() {
+        return label;
+    }
+
+    public void setContainer(final @NonNull ContainerName containerName, final @NonNull MountPointChild data) {
+        final NormalizableAnydata prev = yangLib.putIfAbsent(containerName, requireNonNull(data));
+        checkState(prev == null, "Attempted to duplicate container %s data %s with %s", containerName, prev, data);
+    }
+
+    public void addChild(final @NonNull MountPointChild data) {
+        children.add(requireNonNull(data));
+    }
+
+    void write(final @NonNull NormalizedNodeStreamWriter writer) {
+        final MountPointStreamWriter mountWriter = writer.getExtensions().getInstance(MountPointStreamWriter.class);
+        if (mountWriter == null) {
+            LOG.debug("Writer {} does not support mount points, ignoring data in {}", writer, label);
+            return;
+        }
+
+        final Optional<MountPointSchemaResolver> optResolver = mountWriter.findMountPoint(label);
+        if (!optResolver.isPresent()) {
+            LOG.debug("Mount point for {} is not present, ignoring it", label);
+            return;
+        }
+
+        final MountPointSchemaResolver resolver = optResolver.get();
+        if (resolver instanceof StaticMountPointSchemaResolver) {
+            writeStatic(writer, mountWriter, ((StaticMountPointSchemaResolver) resolver).getSchemaContext());
+        } else if (resolver instanceof DynamicMountPointSchemaResolver) {
+            writeDynamic(writer, mountWriter, (DynamicMountPointSchemaResolver) resolver);
+        } else {
+            throw new IllegalStateException("Unhandled resolver  ");
+        }
+    }
+
+    private void writeDynamic(final NormalizedNodeStreamWriter writer,
+            final @NonNull MountPointStreamWriter mountWriter, final DynamicMountPointSchemaResolver resolver) {
+        // FIXME: YANGTOOLS-1007: negotiate/emit data
+    }
+
+    private void writeStatic(final NormalizedNodeStreamWriter writer, final @NonNull MountPointStreamWriter mountWriter,
+            final @NonNull SchemaContext schemaContext) {
+        // FIXME: YANGTOOLS-1007: negotiate/emit data
+    }
+}
