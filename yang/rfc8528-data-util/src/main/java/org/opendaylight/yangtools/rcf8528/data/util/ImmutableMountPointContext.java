b475fc3ed09bab7982f650d835c08d7e956f27a9
Robert Varga <robert.varga@pantheon.tech>
2019-07-24 01:58:44 +0200

    Add AbstractMountPointContextFactory
    This factory has model-specific information about RFC8528, so that
    it is able to form an accurate view of mount points defined by inline
    data.
    
    Change-Id: I3350403c3fb3e1e074649536511eb7935f0ff6c9
    JIRA: YANGTOOLS-1010
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/rfc8528-data-util/src/main/java/org/opendaylight/yangtools/rcf8528/data/util/ImmutableMountPointContext.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rcf8528.data.util;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects.ToStringHelper;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import java.util.Optional;
+import java.util.function.Function;
+import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.rcf8528.data.util.AbstractMountPointContextFactory.MountPointDefinition;
+import org.opendaylight.yangtools.rfc8528.data.api.MountPointContext;
+import org.opendaylight.yangtools.rfc8528.data.api.MountPointContextFactory;
+import org.opendaylight.yangtools.rfc8528.data.api.MountPointIdentifier;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.model.util.AbstractSchemaContextProvider;
+
+final class ImmutableMountPointContext extends AbstractSchemaContextProvider implements Immutable, MountPointContext {
+    private final ImmutableMap<MountPointIdentifier, MountPointDefinition> mountPoints;
+    private final Function<MountPointDefinition, MountPointContextFactory> createFactory;
+
+    ImmutableMountPointContext(final SchemaContext schemaContext, final Iterable<MountPointDefinition> mountPoints,
+            final Function<MountPointDefinition, MountPointContextFactory> createFactory) {
+        super(schemaContext);
+        this.mountPoints = Maps.uniqueIndex(mountPoints, MountPointDefinition::getIdentifier);
+        this.createFactory = requireNonNull(createFactory);
+    }
+
+    @Override
+    public Optional<MountPointContextFactory> findMountPoint(final MountPointIdentifier label) {
+        final MountPointDefinition def = mountPoints.get(requireNonNull(label));
+        return def == null ? Optional.empty() : Optional.of(createFactory.apply(def));
+    }
+
+    @Override
+    protected ToStringHelper addToStringAttributes(final ToStringHelper helper) {
+        return super.addToStringAttributes(helper).add("mountPoints", mountPoints);
+    }
+}
