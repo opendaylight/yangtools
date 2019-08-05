444c32b12943dcfc82323a585301f63a67f615a6
Robert Varga <robert.varga@pantheon.tech>
2019-08-05 20:34:27 +0200

    Fix stmt.compat Java 11 compilation
    Java 11 inference does not like the implicit cast, although it is
    supposed to be safe. We do not really need it, remove it.
    
    Change-Id: Ic2b89a523eb91cb86cd669dc9670c6acba214dca
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/compat/ActionNodeContainerCompat.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/compat/ActionNodeContainerCompat.java
@@ -14,7 +14,6 @@ import org.opendaylight.yangtools.yang.model.api.ActionDefinition;
 import org.opendaylight.yangtools.yang.model.api.ActionNodeContainer;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeAwareEffectiveStatement;
-import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeEffectiveStatement;
 
 /**
  * Compatibility bridge between {@link ActionNodeContainer#findAction(QName)} and
@@ -28,7 +27,7 @@ public interface ActionNodeContainerCompat<A, D extends DeclaredStatement<A>>
     default Optional<ActionDefinition> findAction(final QName qname) {
         // 'action' identifier must never collide with another element, hence if we look it up and it ends up being
         // an ActionDefinition, we have found a match.
-        final SchemaTreeEffectiveStatement<?> child = get(Namespace.class, qname);
+        final Object child = get(Namespace.class, qname);
         return child instanceof ActionDefinition ? Optional.of((ActionDefinition) child) : Optional.empty();
     }
 }
