fad18359e68c04b754d67334480caadc57ce91ee
Andrii Mazurian <a.mazuryan@gmail.com>
2019-08-13 12:59:44 +0300

    Rename AnyXmlNode to DOMSourceAnyxmlNode
    Rename AnyXmlNode to DOMSourceAnyxmlNode, as that reflects its place in the object hierarchy.
    
    Jira: YANGTOOLS-976
    Signed-off-by: Andrii Mazurian <a.mazuryan@gmail.com>
    Change-Id: I962c910557a14dad5f88fa4e69146606e21fddc4

--- yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/AnyXmlNode.java
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/AnyXmlNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -7,24 +7,16 @@
  */
 package org.opendaylight.yangtools.yang.data.api.schema;
 
-import javax.xml.transform.dom.DOMSource;
+import com.google.common.annotations.Beta;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 
 /**
- * An AnyxmlNode with data in {@link DOMSource} format.
+ * A NormalizedNode holding the contents of an {@code anyxml} node in some object model. This interface is a common
+ * capture for all object model specializations.
+ *
+ * @param <V> Value type, uniquely identifying the object model used for values
  */
-// FIXME: 4.0.0: YANGTOOLS-976: rename to DOMSourceAnyxmlNode
-public interface AnyXmlNode extends AnyxmlNode<DOMSource> {
-    @Override
-    default Class<DOMSource> getValueObjectModel() {
-        return DOMSource.class;
-    }
+@Beta
+public interface AnyxmlNode<V> extends ForeignDataNode<NodeIdentifier, V> {
 
-    /**
-     * Return value represented as a DOMSource. Returned source contains top level element
-     * that duplicates the anyxml node.
-     *
-     * @return anyxml node value represented as DOMSource.
-     */
-    @Override
-    DOMSource getValue();
 }
