e01c45ac43cd24896b39ac103dc58098c507e114
Andrii Mazurian <a.mazuryan@gmail.com>
2019-08-13 12:59:44 +0300

    Rename AnyXmlNode to DOMSourceAnyxmlNode
    Rename AnyXmlNode to DOMSourceAnyxmlNode, as that reflects its place in the object hierarchy.
    
    Jira: YANGTOOLS-976
    Signed-off-by: Andrii Mazurian <a.mazuryan@gmail.com>
    Change-Id: I962c910557a14dad5f88fa4e69146606e21fddc4

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/builder/impl/ImmutableAnyXmlNodeBuilder.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/builder/impl/ImmutableAnyXmlNodeBuilder.java
@@ -10,24 +10,25 @@ package org.opendaylight.yangtools.yang.data.impl.schema.builder.impl;
 import javax.xml.transform.dom.DOMSource;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
-import org.opendaylight.yangtools.yang.data.api.schema.AnyXmlNode;
+import org.opendaylight.yangtools.yang.data.api.schema.DOMSourceAnyxmlNode;
 import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.NormalizedNodeBuilder;
 import org.opendaylight.yangtools.yang.data.impl.schema.nodes.AbstractImmutableNormalizedSimpleValueNode;
 
 public class ImmutableAnyXmlNodeBuilder
-        extends AbstractImmutableNormalizedNodeBuilder<NodeIdentifier, DOMSource, AnyXmlNode> {
+        extends AbstractImmutableNormalizedNodeBuilder<NodeIdentifier, DOMSource, DOMSourceAnyxmlNode> {
 
-    public static @NonNull NormalizedNodeBuilder<NodeIdentifier, DOMSource, AnyXmlNode> create() {
+    public static @NonNull NormalizedNodeBuilder<NodeIdentifier, DOMSource, DOMSourceAnyxmlNode> create() {
         return new ImmutableAnyXmlNodeBuilder();
     }
 
     @Override
-    public AnyXmlNode build() {
+    public DOMSourceAnyxmlNode build() {
         return new ImmutableXmlNode(getNodeIdentifier(), getValue());
     }
 
     private static final class ImmutableXmlNode
-            extends AbstractImmutableNormalizedSimpleValueNode<NodeIdentifier, DOMSource> implements AnyXmlNode {
+            extends AbstractImmutableNormalizedSimpleValueNode<NodeIdentifier, DOMSource>
+            implements DOMSourceAnyxmlNode {
 
         ImmutableXmlNode(final NodeIdentifier nodeIdentifier, final DOMSource value) {
             super(nodeIdentifier, value);
