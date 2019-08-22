6053b79bdd47b63cd333753f4ea03b7183bc5536
Robert Varga <robert.varga@pantheon.tech>
2019-08-22 20:23:17 +0200

    WIP: turn util into a JPMS module
    This creates an explicit module-info.java, making the policy explicit.
    
    Change-Id: I07d0d8d31e1f0673dd338cb97901e976127cd190
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ common/util/src/main/java/module-info.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.yangtools.util {
+    exports org.opendaylight.yangtools.util;
+    exports org.opendaylight.yangtools.util.concurrent;
+    exports org.opendaylight.yangtools.util.xml;
+
+    requires transitive java.xml;
+
+    requires org.opendaylight.yangtools.concepts;
+    requires tech.pantheon.triemap;
+
+    // FIXME: this is due to the accidental leak of slf4j in LoggingThreadUncaughtExceptionHandler
+    requires transitive org.slf4j;
+
+    // Annotations
+    requires static com.github.spotbugs.annotations;
+    requires static java.compiler;
+    requires static java.management;
+    requires static org.eclipse.jdt.annotation;
+    requires static org.checkerframework.checker.qual;
+    requires static org.gaul.modernizer_maven_annotations;
+    requires static org.immutables.value;
+}
