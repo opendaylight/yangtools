e9b26c7eab29bf086c661aea7c64d79e30a66964
Robert Varga <robert.varga@pantheon.tech>
2019-08-22 20:05:03 +0200

    Make concepts an explicit module
    This adds module-info, turning concepts into a full-pledged module.
    
    Change-Id: I3225cb148364818ff2ff1c0abac0ebf90c4f198c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ common/concepts/src/main/java/org.opendaylight.yangtools.concepts/module-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.yangtools.concepts {
+    exports org.opendaylight.yangtools.concepts;
+
+    requires com.github.spotbugs.annotations;
+    requires com.google.common;
+    requires org.eclipse.jdt.annotation;
+    requires org.checkerframework.checker.qual;
+}
