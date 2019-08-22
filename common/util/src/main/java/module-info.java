af8b0adddf71d876e3f2adf6e86013cef5bc3920
Robert Varga <robert.varga@pantheon.tech>
2019-08-22 20:23:17 +0200

    WIP: turn util into a JPMS module
    This creates an explicit module-info.java, making the policy explicit.
    
    // FIXME: modernizer is in our way
    // FIXME: immutables does not seem not quite work
    
    Change-Id: I07d0d8d31e1f0673dd338cb97901e976127cd190
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ common/util/src/main/java/module-info.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.yangtools.util {
+    exports org.opendaylight.yangtools.util;
+    exports org.opendaylight.yangtools.util.concurrent;
+    exports org.opendaylight.yangtools.util.xml;
+
+    requires java.xml;
+
+    requires com.google.common;
+    requires org.immutables.value;
+    requires org.opendaylight.yangtools.concepts;
+    requires org.slf4j;
+    requires tech.pantheon.triemap;
+
+    // Annotations
+    // FIXME: deal with these
+    requires static error.prone.annotations;
+
+    requires static com.github.spotbugs.annotations;
+    requires static java.compiler;
+    requires static java.management;
+    requires static org.eclipse.jdt.annotation;
+    requires static org.checkerframework.checker.qual;
+    requires static org.gaul.modernizer_maven_annotations;
+}
