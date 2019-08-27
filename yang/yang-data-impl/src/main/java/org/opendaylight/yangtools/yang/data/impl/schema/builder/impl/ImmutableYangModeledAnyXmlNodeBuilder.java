06fc6af1772d525de1e9ef32fed96002ad1bc2c5
Robert Varga <robert.varga@pantheon.tech>
2019-08-27 16:04:55 +0200

    WIP: do not retain leaf nodes in containers
    Leaf nodes are observed to dominate our memory footprint. This
    patch modifies immutable implementations of NormalizedNodeContainers
    such that they eliminate any leaf nodes when they are stored, retaining
    only the encapsulated value.
    
    Leaf nodes are then re-created on access as needed.
    
    Change-Id: I00fc3ac0b64290068e8a6e4c8972454729fa9011
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/builder/impl/ImmutableYangModeledAnyXmlNodeBuilder.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/builder/impl/ImmutableYangModeledAnyXmlNodeBuilder.java
@@ -14,7 +14,6 @@ import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.odlext.model.api.YangModeledAnyXmlSchemaNode;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
-import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
 import org.opendaylight.yangtools.yang.data.api.schema.YangModeledAnyXmlNode;
 import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.DataContainerNodeBuilder;
 import org.opendaylight.yangtools.yang.data.impl.schema.nodes.AbstractImmutableDataContainerNode;
@@ -59,8 +58,7 @@ public final class ImmutableYangModeledAnyXmlNodeBuilder extends
 
         private final @NonNull ContainerSchemaNode contentSchema;
 
-        ImmutableYangModeledAnyXmlNode(final NodeIdentifier nodeIdentifier,
-                final Map<PathArgument, DataContainerChild<? extends PathArgument, ?>> value,
+        ImmutableYangModeledAnyXmlNode(final NodeIdentifier nodeIdentifier, final Map<PathArgument, Object> value,
                 final ContainerSchemaNode contentSchema) {
             super(value, nodeIdentifier);
             this.contentSchema = requireNonNull(contentSchema, "Schema of yang modeled anyXml content cannot be null.");
