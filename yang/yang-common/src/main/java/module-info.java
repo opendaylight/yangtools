008e8a98ac409fdc470e4735bf437b1799acf1e0
Robert Varga <robert.varga@pantheon.tech>
2019-08-27 23:03:20 +0200

    Convert yang-common to a JPMS module
    This drops plugin configuration in favor of declaring a module. It
    allows us to ditch metainf-services as ServiceLoader bindings are
    now explicit.
    
    Change-Id: Ie160bbf551f4bbca424978215400c78d4b305334
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-common/src/main/java/module-info.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+import org.opendaylight.yangtools.yang.common.CanonicalValueSupport;
+import org.opendaylight.yangtools.yang.common.Decimal64;
+import org.opendaylight.yangtools.yang.common.Uint16;
+import org.opendaylight.yangtools.yang.common.Uint32;
+import org.opendaylight.yangtools.yang.common.Uint64;
+import org.opendaylight.yangtools.yang.common.Uint8;
+
+module org.opendaylight.yangtools.yang.common {
+    exports org.opendaylight.yangtools.yang.common;
+
+    provides CanonicalValueSupport with
+        Decimal64.Support,
+        Uint8.Support,
+        Uint16.Support,
+        Uint32.Support,
+        Uint64.Support;
+
+    requires transitive org.opendaylight.yangtools.concepts;
+    requires org.slf4j;
+
+    // Annotations
+    requires static com.github.spotbugs.annotations;
+    requires static org.eclipse.jdt.annotation;
+    requires static org.checkerframework.checker.qual;
+}
