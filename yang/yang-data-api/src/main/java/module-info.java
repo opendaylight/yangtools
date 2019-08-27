a9e04b7f157b924e9e07933f03dcc1daec117700
Robert Varga <robert.varga@pantheon.tech>
2019-08-27 23:11:10 +0200

    Convert yang-data-api to a JPMS module
    This drops plugin configuration in favor of declaring a module.
    
    JIRA: YANGTOOLS-934
    Change-Id: Id96ce4f0a85d9c957f694ca0125f38979f4654e6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-data-api/src/main/java/module-info.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.yangtools.yang.data.api {
+    exports org.opendaylight.yangtools.yang.data.api;
+    exports org.opendaylight.yangtools.yang.data.api.codec;
+    exports org.opendaylight.yangtools.yang.data.api.schema;
+    exports org.opendaylight.yangtools.yang.data.api.schema.stream;
+    exports org.opendaylight.yangtools.yang.data.api.schema.tree;
+    exports org.opendaylight.yangtools.yang.data.api.schema.tree.spi;
+
+    requires transitive org.opendaylight.yangtools.yang.model.api;
+
+    requires org.opendaylight.yangtools.util;
+}
