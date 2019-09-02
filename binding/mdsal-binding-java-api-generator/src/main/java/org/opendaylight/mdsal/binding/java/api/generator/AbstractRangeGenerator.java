c7168c7ffb84fff77962e0761b9c9e43593b54bf
Robert Varga <robert.varga@pantheon.tech>
2019-09-02 16:28:48 +0200

    Remove BigIntegerRangeGenerator
    We do not generate BigIntegers anymore, this removes related
    generator.
    
    JIRA: MDSAL-330
    Change-Id: I29be15b970e8392f474136c8dd6a948fcbd4054c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-java-api-generator/src/main/java/org/opendaylight/mdsal/binding/java/api/generator/AbstractRangeGenerator.java
+++ binding/mdsal-binding-java-api-generator/src/main/java/org/opendaylight/mdsal/binding/java/api/generator/AbstractRangeGenerator.java
@@ -36,7 +36,6 @@ abstract class AbstractRangeGenerator<T extends Number & Comparable<T>> {
         addGenerator(b, new IntegerRangeGenerator());
         addGenerator(b, new LongRangeGenerator());
         addGenerator(b, new BigDecimalRangeGenerator());
-        addGenerator(b, new BigIntegerRangeGenerator());
         addGenerator(b, new Uint8RangeGenerator());
         addGenerator(b, new Uint16RangeGenerator());
         addGenerator(b, new Uint32RangeGenerator());
