7a23619f5149978496263b1a57071cef6fe52b08
Mario Abraham <mario.abraham@pantheon.tech>
2019-09-27 11:00:08 +0200

    Represent large number types as strings
    A value of the int64, uint64 and decimal64 type is
    represented as a JSON string related to rfc 7951.
    
    Issue-ID: YANGTOOLS-1027
    Change-Id: Ic6aea0a0f97e042ed40d2e0be323557750401621
    Signed-off-by: Mario Abraham <mario.abraham@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit af02c59d72a4f6af9361ea2842c35e634f6454e4)

---
+++ yang/yang-data-codec-gson/src/test/java/org/opendaylight/yangtools/yang/data/codec/gson/RFC7951YT1027Test.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.codec.gson;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class RFC7951YT1027Test extends AbstractYT1027Test {
+    private static JSONCodecFactory CODEC_FACTORY;
+
+    @BeforeClass
+    public static void createFactory() {
+        CODEC_FACTORY = JSONCodecFactorySupplier.RFC7951.getShared(SCHEMA_CONTEXT);
+    }
+
+    @AfterClass
+    public static void destroyFactory() {
+        CODEC_FACTORY = null;
+    }
+
+    @Test
+    public void testDecimalUnquotedParsing() throws IOException {
+        assertEquals(DECIMAL_DATA, fromJSON(UNQUOTED_DECIMAL));
+    }
+
+    @Test
+    public void testInt64UnquotedParsing() throws IOException {
+        assertEquals(INT64_DATA, fromJSON(UNQUOTED_INT64));
+    }
+
+    @Test
+    public void testUint64UnquotedParsing() throws IOException {
+        assertEquals(UINT64_DATA, fromJSON(UNQUOTED_UINT64));
+    }
+
+    @Override
+    JSONCodecFactory codecFactory() {
+        return CODEC_FACTORY;
+    }
+
+    @Override
+    Class<?> wrapperClass() {
+        return QuotedJSONCodec.class;
+    }
+
+    @Override
+    String expectedDecimal() {
+        return "{\n"
+                + "  \"yt1027:decimal\": \"1.1\"\n"
+                + "}";
+    }
+
+    @Override
+    String expectedInt64() {
+        return "{\n"
+                + "  \"yt1027:int64\": \"2\"\n"
+                + "}";
+    }
+
+    @Override
+    String expectedUint64() {
+        return "{\n"
+                + "  \"yt1027:uint64\": \"1\"\n"
+                + "}";
+    }
+}
