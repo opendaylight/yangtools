3a3534cb84a1c2440ae60c1a951032556068c66b
Mario Abraham <mario.abraham@pantheon.tech>
2019-09-27 11:00:08 +0200

    Represent large number types as strings
    A value of the int64, uint64 and decimal64 type is
    represented as a JSON string related to rfc 7951.
    
    Issue-ID: YANGTOOLS-1027
    Change-Id: Ic6aea0a0f97e042ed40d2e0be323557750401621
    Signed-off-by: Mario Abraham <mario.abraham@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-data-codec-gson/src/test/java/org/opendaylight/yangtools/yang/data/codec/gson/Lhotka02YT1027Test.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.codec.gson;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+public class Lhotka02YT1027Test extends AbstractYT1027Test {
+    private static JSONCodecFactory CODEC_FACTORY;
+
+    @BeforeClass
+    public static void createFactory() {
+        CODEC_FACTORY = JSONCodecFactorySupplier.DRAFT_LHOTKA_NETMOD_YANG_JSON_02.getShared(SCHEMA_CONTEXT);
+    }
+
+    @AfterClass
+    public static void destroyFactory() {
+        CODEC_FACTORY = null;
+    }
+
+    @Override
+    JSONCodecFactory codecFactory() {
+        return CODEC_FACTORY;
+    }
+
+    @Override
+    Class<?> wrapperClass() {
+        return NumberJSONCodec.class;
+    }
+
+    @Override
+    String expectedDecimal() {
+        return UNQUOTED_DECIMAL;
+    }
+
+    @Override
+    String expectedInt64() {
+        return UNQUOTED_INT64;
+    }
+
+    @Override
+    String expectedUint64() {
+        return UNQUOTED_UINT64;
+    }
+}
