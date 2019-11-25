d972561a5435ea88946eecfaae4dd1b9e4540399
Robert Varga <robert.varga@pantheon.tech>
2019-11-25 13:54:17 +0100

    Centralize unknownNodes
    We have a number of statements which collect unknown nodes, which
    are specified by DocumentedNode. Centralize them as close as possible
    in the utility class hierarchy.
    
    JIRA: YANGTOOLS-1040
    Change-Id: I337fb684b557269926e641d86d6afd609b2dce57
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 805ce8dc5e98b90427de0cc49fad70d7054635a6)

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/AbstractEffectiveDocumentedNode.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/AbstractEffectiveDocumentedNode.java
@@ -7,16 +7,22 @@
  */
 package org.opendaylight.yangtools.yang.parser.rfc7950.stmt;
 
+import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
+import java.util.List;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.yang.model.api.DocumentedNode;
 import org.opendaylight.yangtools.yang.model.api.Status;
+import org.opendaylight.yangtools.yang.model.api.UnknownSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.StatusEffectiveStatement;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 
 // FIXME: 5.0.0: rename to AbstractEffectiveDocumentedNodeWithStatus
 public abstract class AbstractEffectiveDocumentedNode<A, D extends DeclaredStatement<A>>
         extends AbstractEffectiveDocumentedNodeWithoutStatus<A, D> implements DocumentedNode.WithStatus {
+    private final @NonNull ImmutableList<UnknownSchemaNode> unknownNodes;
     private final @NonNull Status status;
 
     /**
@@ -28,10 +34,23 @@ public abstract class AbstractEffectiveDocumentedNode<A, D extends DeclaredState
     protected AbstractEffectiveDocumentedNode(final StmtContext<A, D, ?> ctx) {
         super(ctx);
         status = findFirstEffectiveSubstatementArgument(StatusEffectiveStatement.class).orElse(Status.CURRENT);
+
+        final List<UnknownSchemaNode> unknownNodesInit = new ArrayList<>();
+        for (final EffectiveStatement<?, ?> stmt : effectiveSubstatements()) {
+            if (stmt instanceof UnknownSchemaNode) {
+                unknownNodesInit.add((UnknownSchemaNode) stmt);
+            }
+        }
+        unknownNodes = ImmutableList.copyOf(unknownNodesInit);
     }
 
     @Override
     public final Status getStatus() {
         return status;
+    }
+
+    @Override
+    public final ImmutableList<UnknownSchemaNode> getUnknownSchemaNodes() {
+        return unknownNodes;
     }
 }
