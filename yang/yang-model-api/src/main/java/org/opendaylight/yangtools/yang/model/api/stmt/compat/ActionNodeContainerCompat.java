0bd33ecb379e6226f971931cd98db2cf1ae04e11
Robert Varga <robert.varga@pantheon.tech>
2019-12-11 21:34:09 +0100

    Fix JDK11 compilation
    This fixes up a javac inference problem, which is completely fine
    as we can drop down to java.lang.Object, which we'll be casting
    anyway.
    
    Change-Id: I5ce990970607a917d20130a0ecf71a107b3cc0ac
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/compat/ActionNodeContainerCompat.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/compat/ActionNodeContainerCompat.java
@@ -14,7 +14,6 @@ import org.opendaylight.yangtools.yang.model.api.ActionDefinition;
 import org.opendaylight.yangtools.yang.model.api.ActionNodeContainer;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeAwareEffectiveStatement;
-import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeEffectiveStatement;
 
 /**
  * Compatibility bridge between {@link ActionNodeContainer#findAction(QName)} and
@@ -28,7 +27,10 @@ public interface ActionNodeContainerCompat<A, D extends DeclaredStatement<A>>
     default Optional<ActionDefinition> findAction(final QName qname) {
         // 'action' identifier must never collide with another element, hence if we look it up and it ends up being
         // an ActionDefinition, we have found a match.
-        final SchemaTreeEffectiveStatement<?> child = get(Namespace.class, qname);
+        //
+        // Note: JDK 11 does not like the statement constraint here, hence we go through object (which is just as fine)
+        @SuppressWarnings("unchecked")
+        final Object child = get(Namespace.class, qname);
         return child instanceof ActionDefinition ? Optional.of((ActionDefinition) child) : Optional.empty();
     }
 }
