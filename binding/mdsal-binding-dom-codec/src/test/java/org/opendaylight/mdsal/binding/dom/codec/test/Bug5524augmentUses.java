905ba778e0a7403d9e5d47e6a5e81fac4f1fc989
Robert Varga <robert.varga@pantheon.tech>
2019-12-12 23:12:39 +0100

    Move Bug5224/Bug5845 tests
    These tests are in reality testing BindingNormalizedNodeCodecRegistry,
    move them to mdsal-binding-dom-codec, eliminating their dependency
    on AbstractDataBrokerTest.
    
    JIRA: MDSAL-393
    Change-Id: I05fd3129efffeac5564096c34f7170a578caa380
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-dom-codec/src/test/java/org/opendaylight/mdsal/binding/dom/codec/test/Bug5524augmentUses.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.dom.codec.test;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Collections;
+import org.junit.Test;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingCodecTree;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingDataObjectCodecTreeNode;
+import org.opendaylight.yang.gen.v1.urn.test.opendaylight.bug._5524.module1.rev160101.grouping.module1.ListModule11Builder;
+import org.opendaylight.yang.gen.v1.urn.test.opendaylight.bug._5524.module1.rev160101.grouping.module1.list.module1._1.ListModule12Builder;
+import org.opendaylight.yang.gen.v1.urn.test.opendaylight.bug._5524.module1.rev160101.grouping.module1.list.module1._1.list.module1._2.ContainerModule1Builder;
+import org.opendaylight.yang.gen.v1.urn.test.opendaylight.bug._5524.module3.rev160101.grouping.module3.ContainerManualListModule11Builder;
+import org.opendaylight.yang.gen.v1.urn.test.opendaylight.bug._5524.module3.rev160101.grouping.module3.ContainerModule11;
+import org.opendaylight.yang.gen.v1.urn.test.opendaylight.bug._5524.module3.rev160101.grouping.module3.ContainerModule11Builder;
+import org.opendaylight.yang.gen.v1.urn.test.opendaylight.bug._5524.module3.rev160101.grouping.module3.ManualListModule11Builder;
+import org.opendaylight.yang.gen.v1.urn.test.opendaylight.bug._5524.module3.rev160101.grouping.module3.container.manual.list.module1._1.ContainerManualListModule12Builder;
+import org.opendaylight.yang.gen.v1.urn.test.opendaylight.bug._5524.module3.rev160101.grouping.module3.container.manual.list.module1._1.container.manual.list.module1._2.ContainerManualContainerModule1Builder;
+import org.opendaylight.yang.gen.v1.urn.test.opendaylight.bug._5524.module3.rev160101.grouping.module3.container.manual.list.module1._1.container.manual.list.module1._2.container.manual.container.module1.ContainerManualContainerModule2Builder;
+import org.opendaylight.yang.gen.v1.urn.test.opendaylight.bug._5524.module3.rev160101.grouping.module3.manual.list.module1._1.ManualListModule12Builder;
+import org.opendaylight.yang.gen.v1.urn.test.opendaylight.bug._5524.module3.rev160101.grouping.module3.manual.list.module1._1.manual.list.module1._2.ManualContainerModule1Builder;
+import org.opendaylight.yang.gen.v1.urn.test.opendaylight.bug._5524.module4.rev160101.Module4Main;
+import org.opendaylight.yang.gen.v1.urn.test.opendaylight.bug._5524.module4.rev160101.Module4MainBuilder;
+import org.opendaylight.yang.gen.v1.urn.test.opendaylight.bug._5524.module4.rev160101.module4.main.ContainerModule4Builder;
+import org.opendaylight.yang.gen.v1.urn.test.opendaylight.bug._5524.module4.rev160101.module4.main.container.module._4.ManualContainerModule11;
+import org.opendaylight.yang.gen.v1.urn.test.opendaylight.bug._5524.module4.rev160101.module4.main.container.module._4.ManualContainerModule11Builder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+public class Bug5524augmentUses extends AbstractBindingCodecTest {
+    @Test
+    public void testBug5224() throws Exception {
+        final Module4Main module4Main = new Module4MainBuilder().setContainerModule4(
+                new ContainerModule4Builder().setListModule11(Collections.singletonList(
+                        new ListModule11Builder().setListModule12(Collections.singletonList(
+                                new ListModule12Builder().setContainerModule1(
+                                        new ContainerModule1Builder().addAugmentation(ContainerModule11.class,
+                                                new ContainerModule11Builder().build()).build())
+                                                    .build())).build())).build()).build();
+
+        final Module4Main manualModule4Main = new Module4MainBuilder().setContainerModule4(
+                new ContainerModule4Builder().setManualListModule11(Collections.singletonList(
+                        new ManualListModule11Builder().setManualListModule12(Collections.singletonList(
+                                new ManualListModule12Builder().setManualContainerModule1(
+                                        new ManualContainerModule1Builder().addAugmentation(
+                                                ManualContainerModule11.class, new ManualContainerModule11Builder()
+                                                    .build()).build()).build())).build())).build()).build();
+
+        final Module4Main contManualModule4Main = new Module4MainBuilder().setContainerModule4(
+                new ContainerModule4Builder().setContainerManualListModule11(Collections.singletonList(
+                        new ContainerManualListModule11Builder().setContainerManualListModule12(
+                                Collections.singletonList(
+                                new ContainerManualListModule12Builder().setContainerManualContainerModule1(
+                                        new ContainerManualContainerModule1Builder().setContainerManualContainerModule2(
+                                                new ContainerManualContainerModule2Builder().build()).build())
+                                        .build())).build())).build()).build();
+
+        final BindingCodecTree codecContext = registry.getCodecContext();
+        final BindingDataObjectCodecTreeNode<Module4Main> subtreeCodec = codecContext.getSubtreeCodec(
+                InstanceIdentifier.create(Module4Main.class));
+        final NormalizedNode<?, ?> serialized = subtreeCodec.serialize(module4Main);
+        final NormalizedNode<?, ?> manualSerialized = subtreeCodec.serialize(manualModule4Main);
+        final NormalizedNode<?, ?> containerManualSerialized = subtreeCodec.serialize(contManualModule4Main);
+        assertNotNull(serialized);
+        assertNotNull(manualSerialized);
+        assertNotNull(containerManualSerialized);
+    }
+}
---
+++ binding/mdsal-binding-dom-codec/src/test/java/org/opendaylight/mdsal/binding/dom/codec/test/Bug5845booleanKeyTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.dom.codec.test;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Collections;
+import org.junit.Test;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingCodecTree;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingDataObjectCodecTreeNode;
+import org.opendaylight.yang.gen.v1.urn.yang.foo.rev160101.BooleanContainer;
+import org.opendaylight.yang.gen.v1.urn.yang.foo.rev160101.BooleanContainerBuilder;
+import org.opendaylight.yang.gen.v1.urn.yang.foo.rev160101._boolean.container.BooleanListBuilder;
+import org.opendaylight.yang.gen.v1.urn.yang.foo.rev160101._boolean.container.BooleanListIntBuilder;
+import org.opendaylight.yang.gen.v1.urn.yang.foo.rev160101._boolean.container.BooleanListIntKey;
+import org.opendaylight.yang.gen.v1.urn.yang.foo.rev160101._boolean.container.BooleanListKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+public class Bug5845booleanKeyTest extends AbstractBindingCodecTest {
+    @Test
+    public void testBug5845() throws Exception {
+        final BooleanContainer booleanContainer = new BooleanContainerBuilder().setBooleanList(Collections
+                .singletonList(new BooleanListBuilder()
+                        .withKey(new BooleanListKey(true, true))
+                        .setBooleanLeaf1(true)
+                        .setBooleanLeaf2(true)
+                        .build()))
+                .build();
+
+        final BooleanContainer booleanContainerInt = new BooleanContainerBuilder().setBooleanListInt(Collections
+                .singletonList(new BooleanListIntBuilder()
+                        .withKey(new BooleanListIntKey((byte) 1))
+                        .setBooleanLeafInt((byte) 1)
+                        .build()))
+                .build();
+
+        final BindingCodecTree codecContext = registry.getCodecContext();
+        final BindingDataObjectCodecTreeNode<BooleanContainer> subtreeCodec = codecContext.getSubtreeCodec(
+                InstanceIdentifier.create(BooleanContainer.class));
+        final NormalizedNode<?, ?> serializedInt = subtreeCodec.serialize(booleanContainerInt);
+        assertNotNull(serializedInt);
+        final NormalizedNode<?, ?> serialized = subtreeCodec.serialize(booleanContainer);
+        assertNotNull(serialized);
+    }
+}
