6e9d90e3de5056aa6b95c576595cee81eade861e
Robert Varga <robert.varga@pantheon.tech>
2019-12-23 20:06:56 +0100

    Correct deref() leafref handling
    deref() invocation requires the target node to actually be examined,
    derefenced and any subsequent path evaluated on top of it.
    
    We correct the implementation to account for correct derefence and
    add a UT for a confusing test case. This is implemented using
    YangParserTestUtils in a separate internal artifact, hence future
    test cases are easy to implement.
    
    JIRA: YANGTOOLS-1050
    Change-Id: I0777da339577f93ecc2786f5beab40b29554fe0e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 2ef2f65082021eab8213c4a9e0d71a2cc42a6962)

---
+++ yang/yang-model-util-ut/src/test/java/org/opendaylight/yangtools/yang/model/util/ut/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Additional unit test suite for {@code yang-model-util}, which are using YANG parser to construct the testing data
+ * and thus cannot live directly in that artifact.
+ */
+// FIXME: YANTTOOLS-1052: this should be eliminated
+package org.opendaylight.yangtools.yang.model.util.ut;
