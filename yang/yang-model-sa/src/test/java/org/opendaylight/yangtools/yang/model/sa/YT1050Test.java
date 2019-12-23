0f73d08139d5d1cff17042fc3abef9c903bb56e1
Robert Varga <robert.varga@pantheon.tech>
2019-12-23 20:06:56 +0100

    WIP: add a testcase
    This adds a proper test case to validate that deref() correctly
    derefences data. This is seeding a new yang-model-sa component, which
    provides methods related to static analysis of a SchemaContext.
    
    // FIXME: handle grouping/uses transition w.r.t. SchemaPath
    
    JIRA: YANGTOOLS-1050
    JIRA: YANGTOOLS-1052
    Change-Id: I0777da339577f93ecc2786f5beab40b29554fe0e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-model-sa/src/test/java/org/opendaylight/yangtools/yang/model/sa/YT1050Test.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.sa;
+
+import static org.hamcrest.Matchers.isA;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.opendaylight.yangtools.yang.model.api.GroupingDefinition;
+import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.ListSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.SchemaNode;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.LeafrefTypeDefinition;
+import org.opendaylight.yangtools.yang.model.util.SchemaContextUtil;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+public class YT1050Test {
+    private static final QName SECONDARY = QName.create("yt1050", "secondary").intern();
+    private static final QName TYPE = QName.create(SECONDARY, "type");
+
+    private EffectiveModelContext context;
+    private LeafSchemaNode secondaryType;
+    private LeafSchemaNode primaryType;
+    private Module module;
+
+    @Before
+    public void before() {
+        context = YangParserTestUtils.parseYangResource("/yt1050.yang");
+        module = context.getModules().iterator().next();
+
+        final GroupingDefinition grp = module.getGroupings().iterator().next();
+        primaryType = (LeafSchemaNode) grp.findDataChildByName(TYPE).get();
+        secondaryType = (LeafSchemaNode) ((ListSchemaNode) grp.findDataChildByName(SECONDARY).get())
+                .findDataChildByName(TYPE).get();
+    }
+
+    @Test
+    public void testFindDataSchemaNodeForRelativeXPathWithDeref() {
+        final TypeDefinition<?> typeNodeType = secondaryType.getType();
+        assertThat(typeNodeType, isA(LeafrefTypeDefinition.class));
+
+        final SchemaNode found =  SchemaContextUtil.findDataSchemaNodeForRelativeXPath(context, module, secondaryType,
+            ((LeafrefTypeDefinition) typeNodeType).getPathStatement());
+        assertSame(primaryType, found);
+    }
+}
