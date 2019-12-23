f2f269d590a349648f8cfd42256da3168e97f5cb
Robert Varga <robert.varga@pantheon.tech>
2019-12-23 20:06:56 +0100

    WIP: add a testcase
    JIRA: YANGTOOLS-1050
    Change-Id: I0777da339577f93ecc2786f5beab40b29554fe0e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-model-util-ut/src/test/java/org/opendaylight/yangtools/yang/model/util/ut/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Additional unit tests to cover utilities in {@code org.opendaylight.yangtools.yang.model.util}. These unit tests
+ * are split out to allow use of YANG parser to create the testing context.
+ */
+package org.opendaylight.yangtools.yang.model.util.ut;
