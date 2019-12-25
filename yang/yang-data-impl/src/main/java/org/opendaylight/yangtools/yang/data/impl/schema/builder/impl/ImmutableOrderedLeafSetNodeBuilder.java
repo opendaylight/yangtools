469fe0830f7926e6f7c51527403b2a5f1cc6d9a1
Robert Varga <robert.varga@pantheon.tech>
2019-12-25 12:24:25 +0100

    Add spotbugs/811 suppression
    JDK11 incurs a spotbugs warning which is not correct, supress it.
    
    Change-Id: Ib4262bfd0aa3728fe7eb94eb970e4b59035cc0b8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/builder/impl/ImmutableOrderedLeafSetNodeBuilder.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/builder/impl/ImmutableOrderedLeafSetNodeBuilder.java
@@ -8,6 +8,7 @@
 package org.opendaylight.yangtools.yang.data.impl.schema.builder.impl;
 
 import com.google.common.collect.Iterables;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
@@ -129,6 +130,8 @@ public class ImmutableOrderedLeafSetNodeBuilder<T> implements ListNodeBuilder<T,
             return children.hashCode();
         }
 
+        @SuppressFBWarnings(value = "UPM_UNCALLED_PRIVATE_METHOD",
+                justification = "https://github.com/spotbugs/spotbugs/issues/811")
         private Map<NodeWithValue, LeafSetEntryNode<T>> getChildren() {
             return Collections.unmodifiableMap(children);
         }
