2384981c0f65b865be2d45e353bf9f96817e14fb
Robert Varga <robert.varga@pantheon.tech>
2019-12-26 23:58:04 +0100

    WIP: add yang-file-xml
    Change-Id: I23739cb709a53ded04ee711eea852a06dc3fdbaa
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-file-xml/src/main/java/module-info.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+/**
+ * Utility interfaces for dealing with YANG/YIN files.
+ */
+module org.opendaylight.yangtools.yang.file.xml {
+    requires org.opendaylight.yangtools.yang.file.api;
+
+//    requires transitive org.opendaylight.yangtools.concepts;
+//    requires org.slf4j;
+//
+//    // Annotations
+//    requires static com.github.spotbugs.annotations;
+    requires static org.eclipse.jdt.annotation;
+//    requires static org.checkerframework.checker.qual;
+}
