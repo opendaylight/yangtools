aec9619f52adafa4634c984748a60bb30c127389
Robert Varga <robert.varga@pantheon.tech>
2019-12-26 13:51:40 +0100

    Split out RFC7950 ANTLR grammars
    ANTLR-generated code does not pass SpotBugs validation, leaving a
    lot of code uncovered by SpotBugs enforcement. This patch splits
    the grammars into a separate artifact, allowing the rest of the
    parser to be cleaned up.
    
    JIRA: YANGTOOLS-1057
    Change-Id: If65bf28e1add5085cb43a2fc2ad71cafc4cd6adb
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-antlr/src/main/java/org/opendaylight/yangtools/yang/parser/antlr/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * ANTLR grammars for parsing RFC7950 constructs. This package is considered an implementation detail and does not
+ * fall under API stability contract. Use at your own risk.
+ */
+package org.opendaylight.yangtools.yang.parser.antlr;
