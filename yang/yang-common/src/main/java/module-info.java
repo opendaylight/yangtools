a9120af9bc331b9f1b7bc7253e22100ca7e8004a
Robert Varga <robert.varga@pantheon.tech>
2019-12-26 22:38:19 +0100

    Add yang-common JMPS javadoc
    This adds Module-level documentation, at least its bare bones.
    
    Change-Id: I08c98f111063d3dffb1f2812aab69a20de289ea5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-common/src/main/java/module-info.java
+++ yang/yang-common/src/main/java/module-info.java
@@ -12,6 +12,11 @@ import org.opendaylight.yangtools.yang.common.Uint32;
 import org.opendaylight.yangtools.yang.common.Uint64;
 import org.opendaylight.yangtools.yang.common.Uint8;
 
+/**
+ * Mapping of YANG language constructs to Java.
+ *
+ * @provides CanonicalValueSupport for core YANG types
+ */
 module org.opendaylight.yangtools.yang.common {
     exports org.opendaylight.yangtools.yang.common;
 
