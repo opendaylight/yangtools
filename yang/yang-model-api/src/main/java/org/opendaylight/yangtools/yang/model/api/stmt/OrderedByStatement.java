cbd7efc63cd21fe68ad9e6958a37934eb8cc7f01
Robert Varga <robert.varga@pantheon.tech>
2019-12-28 11:22:28 +0100

    Add OrderedByStatement.Ordering
    This adds a utility enum to express parsed argument value, allowing
    us to remove some string duplication.
    
    JIRA: YANGTOOLS-1058
    Change-Id: I147f51fbeb9140d6cd43a80d205218a73d827a21
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/OrderedByStatement.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/OrderedByStatement.java
@@ -10,10 +10,38 @@ package org.opendaylight.yangtools.yang.model.api.stmt;
 import static com.google.common.base.Verify.verifyNotNull;
 
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 
-public interface OrderedByStatement extends DeclaredStatement<String> {
-    default @NonNull String getValue() {
+public interface OrderedByStatement extends DeclaredStatement<OrderedByStatement.Ordering> {
+    @NonNullByDefault
+    public enum Ordering {
+        SYSTEM("system"),
+        USER("user");
+
+        private String argumentString;
+
+        Ordering(final String argumentString) {
+            this.argumentString = argumentString;
+        }
+
+        public String getArgumentString() {
+            return argumentString;
+        }
+
+        public static Ordering forArgumentString(final String argumentString) {
+            switch (argumentString) {
+                case "system":
+                    return SYSTEM;
+                case "user":
+                    return USER;
+                default:
+                    throw new IllegalArgumentException("Invalid ordering string '" + argumentString + "'");
+            }
+        }
+    }
+
+    default @NonNull Ordering getValue() {
         // FIXME: YANGTOOLS-908: verifyNotNull() should not be needed here
         return verifyNotNull(argument());
     }
--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/ordered_by/AbstractOrderedByEffectiveStatement.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/ordered_by/AbstractOrderedByEffectiveStatement.java
@@ -9,10 +9,11 @@ package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.ordered_by;
 
 import org.opendaylight.yangtools.yang.model.api.stmt.OrderedByEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.OrderedByStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.OrderedByStatement.Ordering;
 import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractDeclaredEffectiveStatement;
 
 abstract class AbstractOrderedByEffectiveStatement
-        extends AbstractDeclaredEffectiveStatement.DefaultArgument<String, OrderedByStatement>
+        extends AbstractDeclaredEffectiveStatement.DefaultArgument<Ordering, OrderedByStatement>
         implements OrderedByEffectiveStatement {
     AbstractOrderedByEffectiveStatement(final OrderedByStatement declared) {
         super(declared);
