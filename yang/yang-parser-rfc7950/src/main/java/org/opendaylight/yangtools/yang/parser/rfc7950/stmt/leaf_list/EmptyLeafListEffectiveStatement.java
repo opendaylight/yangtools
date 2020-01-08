7de56a8f72ed38a170b5d410bac7eb06fdfafb2b
Robert Varga <robert.varga@pantheon.tech>
2020-01-09 00:22:32 +0100

    Refactor Leaf(List)EffectiveStatementImpl
    Leaf(List)EffectiveStatementImpl costs 88 bytes each instance in common
    Linux deployment. This turns out to be unnecessary, as be can easily
    drop the size to 32/40 (48 worst) bytes by splitting out well-known
    constants. This can represent as much as 10% savings on retained heap.
    
    JIRA: YANGTOOLS-1065
    Change-Id: Icdafc4a196d86d2a493a44523bc81b1024854acb
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/leaf_list/EmptyLeafListEffectiveStatement.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.leaf_list;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import java.util.Collection;
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.model.api.ElementCountConstraint;
+import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.MustDefinition;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.LeafListStatement;
+
+final class EmptyLeafListEffectiveStatement extends AbstractLeafListEffectiveStatement {
+    EmptyLeafListEffectiveStatement(final LeafListStatement declared, final SchemaPath path, final int flags,
+            final ImmutableList<? extends EffectiveStatement<?, ?>> substatements) {
+        super(declared, path, flags, substatements);
+    }
+
+    @Override
+    public Optional<LeafSchemaNode> getOriginal() {
+        return Optional.empty();
+    }
+
+    @Override
+    public ImmutableList<MustDefinition> getMustConstraints() {
+        return ImmutableList.of();
+    }
+
+    @Override
+    public @NonNull Collection<? extends Object> getDefaults() {
+        return ImmutableSet.of();
+    }
+
+    @Override
+    public Optional<ElementCountConstraint> getElementCountConstraint() {
+        return Optional.empty();
+    }
+}
