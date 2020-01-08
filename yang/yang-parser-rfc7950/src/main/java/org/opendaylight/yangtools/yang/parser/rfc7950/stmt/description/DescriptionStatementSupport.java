cc370e6bce6fc00983709ba35ee422d94e3a4e15
Robert Varga <robert.varga@pantheon.tech>
2020-01-08 21:21:16 +0100

    WIP: refactor simple effective statements
    Currently DescriptionEffectiveStatementImpl takes 32 bytes in typical
    Linux environment, which is not much, but for well-documented models this
    can amount to 8% of the cost of a SchemaContext.
    
    The primary cause is of course is inlining of groupings, but even if that
    is fixed we will end up with at least one instance for each description,
    so sizing here is important.
    
    We can get rid of this cost by decomposing the implementation by known
    DescriptionEffectiveStatement invariants, so that we end up with using
    16 (typical) or 24 bytes in the same environment. Worst case size is
    reduced to 24 (typical) or 32 bytes (from 48). The overall benefit is
    expected to be around 4% SchemaContext size.
    
    Apply the same refactor to reference, contact,
    
    Change-Id: Ic8da0c38cd89ae10c66ebb70719cf582cb0f627b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/description/DescriptionStatementSupport.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/description/DescriptionStatementSupport.java
@@ -7,9 +7,12 @@
  */
 package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.description;
 
+import com.google.common.collect.ImmutableList;
 import org.opendaylight.yangtools.yang.model.api.YangStmtMapping;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.DescriptionStatement;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractDeclaredEffectiveStatement;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractEffectiveStatement;
 import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractStatementSupport;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 import org.opendaylight.yangtools.yang.parser.spi.meta.SubstatementValidator;
@@ -41,7 +44,11 @@ public final class DescriptionStatementSupport extends AbstractStatementSupport<
     @Override
     public EffectiveStatement<String, DescriptionStatement> createEffective(
             final StmtContext<String, DescriptionStatement, EffectiveStatement<String, DescriptionStatement>> ctx) {
-        return new DescriptionEffectiveStatementImpl(ctx);
+        final DescriptionStatement declared = AbstractDeclaredEffectiveStatement.buildDeclared(ctx);
+        final ImmutableList<? extends EffectiveStatement<?, ?>> substatements =
+                AbstractEffectiveStatement.buildEffectiveSubstatements(ctx);
+        return substatements.isEmpty() ? new EmptyDescriptionEffectiveStatement(declared)
+                    : new RegularDescriptionEffectiveStatement(declared, substatements);
     }
 
     @Override
