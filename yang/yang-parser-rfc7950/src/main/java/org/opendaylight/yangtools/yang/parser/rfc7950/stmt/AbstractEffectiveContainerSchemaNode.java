3d87feb6583e3f294f2405188faca38520e78e90
Robert Varga <robert.varga@pantheon.tech>
2020-01-13 16:02:40 +0100

    Deprecate AbstractEffectiveContainerSchemaNode
    With ContainerEffectiveStatementImpl refactored, this class does not
    have a direct user and can thus be folded into
    AbstractEffectiveOperationContainerSchemaNode.
    
    Deprecate it for removal, so that it does not accidentally pick up
    any new users.
    
    JIRA: YANGTOOLS-1065
    Change-Id: Ie078b2de0161798a7580e125b19955cebeb1d525
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/AbstractEffectiveContainerSchemaNode.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/AbstractEffectiveContainerSchemaNode.java
@@ -14,6 +14,7 @@ import org.opendaylight.yangtools.yang.model.api.stmt.compat.ActionNodeContainer
 import org.opendaylight.yangtools.yang.model.api.stmt.compat.NotificationNodeContainerCompat;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 
+@Deprecated(forRemoval = true)
 public abstract class AbstractEffectiveContainerSchemaNode<D extends DeclaredStatement<QName>>
         extends AbstractEffectiveMustConstraintAwareSimpleDataNodeContainer<D>
         implements ContainerSchemaNode, ActionNodeContainerCompat<QName, D>, NotificationNodeContainerCompat<QName, D> {
