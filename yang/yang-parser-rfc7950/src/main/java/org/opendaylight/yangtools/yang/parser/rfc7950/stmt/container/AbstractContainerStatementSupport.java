d64bd664a3aee612500bacc187d804034943298c
Robert Varga <robert.varga@pantheon.tech>
2020-01-13 13:42:04 +0100

    WIP: refactor ContainerEffectiveStatementImpl
    The story here is very similar to ListEffectiveStatementImpl,
    with the additional simplifications that containers are pretty
    much same.
    
    JIRA: YANGTOOLS-1065
    Change-Id: I1050c4e8f82f60274c78e810fc31896daccba389
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/container/AbstractContainerStatementSupport.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/container/AbstractContainerStatementSupport.java
@@ -7,19 +7,27 @@
  */
 package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.container;
 
+import com.google.common.collect.ImmutableList;
 import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.api.Status;
 import org.opendaylight.yangtools.yang.model.api.YangStmtMapping;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ContainerEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.ContainerStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.PresenceEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.StatusEffectiveStatement;
 import org.opendaylight.yangtools.yang.parser.rfc7950.namespace.ChildSchemaNodeNamespace;
-import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractQNameStatementSupport;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.BaseQNameStatementSupport;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.EffectiveStatementMixins.EffectiveStatementWithFlags.FlagsBuilder;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext.Mutable;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContextUtils;
+import org.opendaylight.yangtools.yang.parser.spi.source.StatementSourceReference;
 
 abstract class AbstractContainerStatementSupport
-        extends AbstractQNameStatementSupport<ContainerStatement, EffectiveStatement<QName, ContainerStatement>> {
-
+        extends BaseQNameStatementSupport<ContainerStatement, ContainerEffectiveStatement> {
     AbstractContainerStatementSupport() {
         super(YangStmtMapping.CONTAINER);
     }
@@ -30,8 +38,7 @@ abstract class AbstractContainerStatementSupport
     }
 
     @Override
-    public final void onStatementAdded(
-            final Mutable<QName, ContainerStatement, EffectiveStatement<QName, ContainerStatement>> stmt) {
+    public final void onStatementAdded(final Mutable<QName, ContainerStatement, ContainerEffectiveStatement> stmt) {
         stmt.coerceParentContext().addToNs(ChildSchemaNodeNamespace.class, stmt.coerceStatementArgument(), stmt);
     }
 
@@ -41,8 +48,27 @@ abstract class AbstractContainerStatementSupport
     }
 
     @Override
-    public final EffectiveStatement<QName,ContainerStatement> createEffective(
-            final StmtContext<QName, ContainerStatement, EffectiveStatement<QName,ContainerStatement>> ctx) {
-        return new ContainerEffectiveStatementImpl(ctx);
+    protected final ContainerEffectiveStatement createEffective(
+            final StmtContext<QName, ContainerStatement, ContainerEffectiveStatement> ctx,
+            final ContainerStatement declared, final ImmutableList<? extends EffectiveStatement<?, ?>> substatements) {
+
+        final StatementSourceReference ref = ctx.getStatementSourceReference();
+        final SchemaPath path = ctx.getSchemaPath().get();
+        final ContainerSchemaNode original = (ContainerSchemaNode) ctx.getOriginalCtx().map(StmtContext::buildEffective)
+                .orElse(null);
+        final int flags = new FlagsBuilder()
+                .setHistory(ctx.getCopyHistory())
+                .setStatus(findFirstArgument(substatements, StatusEffectiveStatement.class, Status.CURRENT))
+                .setConfiguration(ctx.isConfiguration())
+                .setPresence(findFirstStatement(substatements, PresenceEffectiveStatement.class) != null)
+                .toFlags();
+        return new ContainerEffectiveStatementImpl(declared, path, flags, ref, substatements, original);
+    }
+
+    @Override
+    protected final ContainerEffectiveStatement createEmptyEffective(
+            final StmtContext<QName, ContainerStatement, ContainerEffectiveStatement> ctx,
+            final ContainerStatement declared) {
+        return createEffective(ctx, declared, ImmutableList.of());
     }
 }
