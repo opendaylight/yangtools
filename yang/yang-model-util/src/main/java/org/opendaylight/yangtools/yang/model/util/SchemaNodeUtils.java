fe7595fdd895350d14a68fc0b9a337e9cc96f486
Robert Varga <robert.varga@pantheon.tech>
2020-01-14 15:56:18 +0100

    Deprecate DataNodeIterator
    This class is used in two places only, where each of the places
    only needs specific type of nodes. Expose utility methods for
    those two cases and a general tranverser method from SchemaNodeUtils
    and deprecate DataNodeIterator.
    
    Change-Id: I4fdc47e16fe2ea7e6f042bb80fe869657ee93fac
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit adf41b25c82b005f66a0de86a8e3c8a8a455cf76)

--- yang/yang-model-util/src/main/java/org/opendaylight/yangtools/yang/model/util/SchemaNodeUtils.java
+++ yang/yang-model-util/src/main/java/org/opendaylight/yangtools/yang/model/util/SchemaNodeUtils.java
@@ -9,14 +9,28 @@ package org.opendaylight.yangtools.yang.model.util;
 
 import static java.util.Objects.requireNonNull;
 
+import com.google.common.annotations.Beta;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.CaseSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.ChoiceSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.DataNodeContainer;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.DerivableSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.GroupingDefinition;
+import org.opendaylight.yangtools.yang.model.api.ListSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.NotificationDefinition;
 import org.opendaylight.yangtools.yang.model.api.RpcDefinition;
 import org.opendaylight.yangtools.yang.model.api.SchemaNode;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
 
 public final class SchemaNodeUtils {
     private SchemaNodeUtils() {
@@ -60,6 +74,103 @@ public final class SchemaNodeUtils {
             default:
                 throw new IllegalArgumentException("Supplied qname " + qname
                         + " does not represent rpc input or output.");
+        }
+    }
+
+    @Beta
+    public static @NonNull Collection<? extends TypeDefinition<?>> getAllTypeDefinitions(
+            final DataNodeContainer parent) {
+        final List<TypeDefinition<?>> typedefs = new ArrayList<>();
+        traverse(new DataNodeAggregator() {
+            @Override
+            protected void addTypedefs(final Collection<? extends TypeDefinition<?>> typeDefs) {
+                typedefs.addAll(typeDefs);
+            }
+        }, requireNonNull(parent));
+        return typedefs;
+    }
+
+    @Beta
+    public static @NonNull Collection<? extends ContainerSchemaNode> getAllContainers(final DataNodeContainer parent) {
+        final List<ContainerSchemaNode> containers = new ArrayList<>();
+        traverse(new DataNodeAggregator() {
+            @Override
+            protected void addContainer(final ContainerSchemaNode containerNode) {
+                containers.add(containerNode);
+            }
+        }, requireNonNull(parent));
+        return containers;
+    }
+
+    @Beta
+    public static void traverse(final @NonNull DataNodeAggregator aggregator, final DataNodeContainer dataNode) {
+        if (dataNode == null) {
+            return;
+        }
+
+        final Iterable<DataSchemaNode> childNodes = dataNode.getChildNodes();
+        if (childNodes != null) {
+            for (DataSchemaNode childNode : childNodes) {
+                if (childNode.isAugmenting()) {
+                    continue;
+                }
+                aggregator.addChild(childNode);
+                if (childNode instanceof ContainerSchemaNode) {
+                    final ContainerSchemaNode containerNode = (ContainerSchemaNode) childNode;
+                    aggregator.addContainer(containerNode);
+                    traverse(aggregator, containerNode);
+                } else if (childNode instanceof ListSchemaNode) {
+                    final ListSchemaNode list = (ListSchemaNode) childNode;
+                    aggregator.addList(list);
+                    traverse(aggregator, list);
+                } else if (childNode instanceof ChoiceSchemaNode) {
+                    final ChoiceSchemaNode choiceNode = (ChoiceSchemaNode) childNode;
+                    aggregator.addChoice(choiceNode);
+                    for (final CaseSchemaNode caseNode : choiceNode.getCases().values()) {
+                        traverse(aggregator, caseNode);
+                    }
+                }
+            }
+        }
+
+        aggregator.addTypedefs(dataNode.getTypeDefinitions());
+
+        traverseModule(aggregator, dataNode);
+        traverseGroupings(aggregator, dataNode);
+    }
+
+    private static void traverseModule(final DataNodeAggregator aggregator, final DataNodeContainer dataNode) {
+        final Module module;
+        if (dataNode instanceof Module) {
+            module = (Module) dataNode;
+        } else {
+            return;
+        }
+
+        for (NotificationDefinition notificationDefinition : module.getNotifications()) {
+            traverse(aggregator, notificationDefinition);
+        }
+
+        for (RpcDefinition rpcDefinition : module.getRpcs()) {
+            aggregator.addTypedefs(rpcDefinition.getTypeDefinitions());
+            ContainerSchemaNode input = rpcDefinition.getInput();
+            if (input != null) {
+                traverse(aggregator, input);
+            }
+            ContainerSchemaNode output = rpcDefinition.getOutput();
+            if (output != null) {
+                traverse(aggregator, output);
+            }
+        }
+    }
+
+    private static void traverseGroupings(final DataNodeAggregator aggregator, final DataNodeContainer dataNode) {
+        final Set<GroupingDefinition> groupings = dataNode.getGroupings();
+        if (groupings != null) {
+            for (GroupingDefinition grouping : groupings) {
+                aggregator.addGrouping(grouping);
+                traverse(aggregator, grouping);
+            }
         }
     }
 }
