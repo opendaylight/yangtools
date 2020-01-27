21df6999573b02c4a71eb60f1d711db49e09d66b
Robert Varga <robert.varga@pantheon.tech>
2020-01-27 21:26:14 +0100

    Further optimize OrderedBy effective statement dispatch
    Comparing both argument and declared instance is not necessary.
    We are using identity comparison, so just do that.
    
    Change-Id: I6e28b9013ae2d056907b52f87cd2813bbcdd23c9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/ordered_by/OrderedByStatementSupport.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/ordered_by/OrderedByStatementSupport.java
@@ -105,23 +105,13 @@ public final class OrderedByStatementSupport
             final OrderedByStatement declared) {
         // Aggressively reuse effective instances which are backed by the corresponding empty declared instance, as this
         // is the case unless there is a weird extension in use.
-        final Ordering argument = declared.getValue();
-        switch (argument) {
-            case SYSTEM:
-                if (EMPTY_SYSTEM_DECL.equals(declared)) {
-                    return EMPTY_SYSTEM_EFF;
-                }
-                break;
-            case USER:
-                if (EMPTY_USER_DECL.equals(declared)) {
-                    return EMPTY_USER_EFF;
-                }
-                break;
-            default:
-                throw new IllegalStateException("Unhandled argument " + argument);
+        if (EMPTY_USER_DECL.equals(declared)) {
+            // Most likely to be seen (as system is the default)
+            return EMPTY_USER_EFF;
+        } else if (EMPTY_SYSTEM_DECL.equals(declared)) {
+            return EMPTY_SYSTEM_EFF;
+        } else {
+            return new EmptyOrderedByEffectiveStatement(declared);
         }
-
-        // Declared instance was non-empty, which can happen with extensions
-        return new EmptyOrderedByEffectiveStatement(declared);
     }
 }
