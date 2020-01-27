7d038f119186fc3b776988bab24582166d2d7e08
Robert Varga <robert.varga@pantheon.tech>
2020-01-28 00:40:42 +0100

    Optimize Status statement declarations
    As Status has low cardinality we can quite easily improve things
    by sharing common instances.
    
    JIRA: YANGTOOLS-1065
    Change-Id: Ie404a65bc4a43b0d51e72f011a2aa7a5980c30df
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/status/AbstractStatusEffectiveStatement.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/status/AbstractStatusEffectiveStatement.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -10,12 +10,12 @@ package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.status;
 import org.opendaylight.yangtools.yang.model.api.Status;
 import org.opendaylight.yangtools.yang.model.api.stmt.StatusEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.StatusStatement;
-import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.DeclaredEffectiveStatementBase;
-import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractDeclaredEffectiveStatement;
 
-final class StatusEffectiveStatementImpl extends DeclaredEffectiveStatementBase<Status, StatusStatement>
+abstract class AbstractStatusEffectiveStatement
+        extends AbstractDeclaredEffectiveStatement.DefaultArgument<Status, StatusStatement>
         implements StatusEffectiveStatement {
-    StatusEffectiveStatementImpl(final StmtContext<Status, StatusStatement, ?> ctx) {
-        super(ctx);
+    AbstractStatusEffectiveStatement(final StatusStatement declared) {
+        super(declared);
     }
 }
