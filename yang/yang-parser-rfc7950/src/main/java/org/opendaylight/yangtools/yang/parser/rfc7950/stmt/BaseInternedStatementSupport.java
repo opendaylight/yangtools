3ee65e880e77d93cffcffe87d3b6fda890997f10
Robert Varga <robert.varga@pantheon.tech>
2020-01-28 01:10:47 +0100

    Optimize {Position,Value}StatementSupport
    These statements can be interned and are mostly independent, add
    baseline implementations which intern empty statements on JVM level.
    
    JIRA: YANGTOOLS-1065
    Change-Id: I1fafda7403d743ffa6c5279bd149c161ad78ed33
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/BaseInternedStatementSupport.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.stmt;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.collect.ImmutableList;
+import java.util.concurrent.ExecutionException;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+
+/**
+ * A {@link BaseStatementSupport} specialized for global interning. This base class is useful when the argument can be
+ * reasonably interned and it dominates the {@link EffectiveStatement} implementation. Typical examples include
+ * {@code position} and {@code value} statements, which typically do not have substatements and are based on simple
+ * types.
+ */
+public abstract class BaseInternedStatementSupport<A, D extends DeclaredStatement<A>,
+        E extends EffectiveStatement<A, D>> extends BaseStatementSupport<A, D, E> {
+    private final LoadingCache<A, D> declaredCache = CacheBuilder.newBuilder().weakValues()
+            .build(new CacheLoader<A, D>() {
+                @Override
+                public D load(final A key) {
+                    return createEmptyDeclared(key);
+                }
+            });
+    private final LoadingCache<D, E> effectiveCache = CacheBuilder.newBuilder().weakValues()
+            .build(new CacheLoader<D, E>() {
+                @Override
+                public E load(final D key) throws ExecutionException {
+                    return createEmptyEffective(key);
+                }
+            });
+
+    protected BaseInternedStatementSupport(final StatementDefinition publicDefinition) {
+        super(publicDefinition);
+    }
+
+    @Override
+    protected final D createEmptyDeclared(final StmtContext<A, D, ?> ctx) {
+        return declaredCache.getUnchecked(ctx.coerceStatementArgument());
+    }
+
+    protected abstract @NonNull D createEmptyDeclared(@NonNull A argument);
+
+    @Override
+    protected final E createEmptyEffective(final StmtContext<A, D, E> ctx, final D declared) {
+        return effectiveCache.getUnchecked(declared);
+    }
+
+    protected abstract @NonNull E createEmptyEffective(@NonNull D declared);
+
+    @Override
+    protected final D createDeclared(final StmtContext<A, D, ?> ctx,
+            final ImmutableList<? extends DeclaredStatement<?>> substatements) {
+        return createDeclared(ctx.coerceStatementArgument(), substatements);
+    }
+
+    protected abstract @NonNull D createDeclared(@NonNull A argument,
+            ImmutableList<? extends DeclaredStatement<?>> substatements);
+
+    protected abstract @NonNull D createEmptyDeclared(@NonNull A argument);
+
+    protected abstract @NonNull E createEmptyEffective(@NonNull D declared);
+}
---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/value/EmptyValueStatement.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.value;
+
+final class EmptyValueStatement extends AbstractValueStatement {
+    EmptyValueStatement(final Integer argument) {
+        super(argument);
+    }
+}
