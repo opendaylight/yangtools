b8a0082dda2731f9c27aad93f05d3c3e21915aa3
Robert Varga <robert.varga@pantheon.tech>
2020-01-30 15:42:35 +0100

    Convert yang-version statement support
    yang-version statements are few and far between, but that is not
    a reason to be wasteful. This reworks declared and effective
    implementations to lower their footprint as well as share only
    a few instances typically.
    
    JIRA: YANGTOOLS-1065
    Change-Id: Ia5485b3dff972fc41721a3ec72be9b7f2ea67088
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/yang_version/YangVersionStatementSupport.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/yang_version/YangVersionStatementSupport.java
@@ -7,22 +7,35 @@
  */
 package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.yang_version;
 
+import com.google.common.collect.ImmutableList;
+import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.yang.common.YangVersion;
 import org.opendaylight.yangtools.yang.model.api.YangStmtMapping;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.YangVersionEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.YangVersionStatement;
-import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractStatementSupport;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.BaseStatementSupport;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext.Mutable;
 import org.opendaylight.yangtools.yang.parser.spi.meta.SubstatementValidator;
 import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
 
 public final class YangVersionStatementSupport
-        extends AbstractStatementSupport<YangVersion, YangVersionStatement, YangVersionEffectiveStatement> {
+        extends BaseStatementSupport<YangVersion, YangVersionStatement, YangVersionEffectiveStatement> {
     private static final SubstatementValidator SUBSTATEMENT_VALIDATOR = SubstatementValidator.builder(YangStmtMapping
         .YANG_VERSION)
         .build();
     private static final YangVersionStatementSupport INSTANCE = new YangVersionStatementSupport();
+
+    private static final @NonNull EmptyYangVersionStatement EMPTY_VER1_DECL =
+            new EmptyYangVersionStatement(YangVersion.VERSION_1);
+    private static final @NonNull EmptyYangVersionStatement EMPTY_VER1_1_DECL =
+            new EmptyYangVersionStatement(YangVersion.VERSION_1_1);
+    private static final @NonNull EmptyYangVersionEffectiveStatement EMPTY_VER1_EFF =
+            new EmptyYangVersionEffectiveStatement(EMPTY_VER1_DECL);
+    private static final @NonNull EmptyYangVersionEffectiveStatement EMPTY_VER1_1_EFF =
+            new EmptyYangVersionEffectiveStatement(EMPTY_VER1_1_DECL);
 
     private YangVersionStatementSupport() {
         super(YangStmtMapping.YANG_VERSION);
@@ -39,24 +52,53 @@ public final class YangVersionStatementSupport
     }
 
     @Override
-    public YangVersionStatement createDeclared(final StmtContext<YangVersion, YangVersionStatement, ?> ctx) {
-        return new YangVersionStatementImpl(ctx);
-    }
-
-    @Override
     public void onPreLinkageDeclared(
             final Mutable<YangVersion, YangVersionStatement, YangVersionEffectiveStatement> stmt) {
         stmt.setRootVersion(stmt.getStatementArgument());
     }
 
     @Override
-    public YangVersionEffectiveStatement createEffective(
-            final StmtContext<YangVersion, YangVersionStatement, YangVersionEffectiveStatement> ctx) {
-        return new YangVersionEffectiveStatementImpl(ctx);
+    protected SubstatementValidator getSubstatementValidator() {
+        return SUBSTATEMENT_VALIDATOR;
     }
 
     @Override
-    protected SubstatementValidator getSubstatementValidator() {
-        return SUBSTATEMENT_VALIDATOR;
+    protected YangVersionStatement createDeclared(final StmtContext<YangVersion, YangVersionStatement, ?> ctx,
+            final ImmutableList<? extends DeclaredStatement<?>> substatements) {
+        return new RegularYangVersionStatement(ctx.coerceStatementArgument(), substatements);
+    }
+
+    @Override
+    protected YangVersionStatement createEmptyDeclared(final StmtContext<YangVersion, YangVersionStatement, ?> ctx) {
+        final YangVersion argument = ctx.coerceStatementArgument();
+        switch (argument) {
+            case VERSION_1:
+                return EMPTY_VER1_DECL;
+            case VERSION_1_1:
+                return EMPTY_VER1_1_DECL;
+            default:
+                throw new IllegalStateException("Unhandled version " + argument);
+        }
+    }
+
+    @Override
+    protected YangVersionEffectiveStatement createEffective(
+            final StmtContext<YangVersion, YangVersionStatement, YangVersionEffectiveStatement> ctx,
+            final YangVersionStatement declared,
+            final ImmutableList<? extends EffectiveStatement<?, ?>> substatements) {
+        return new RegularYangVersionEffectiveStatement(declared, substatements);
+    }
+
+    @Override
+    protected YangVersionEffectiveStatement createEmptyEffective(
+            final StmtContext<YangVersion, YangVersionStatement, YangVersionEffectiveStatement> ctx,
+            final YangVersionStatement declared) {
+        if (EMPTY_VER1_DECL.equals(declared)) {
+            return EMPTY_VER1_EFF;
+        } else if (EMPTY_VER1_1_DECL.equals(declared)) {
+            return EMPTY_VER1_1_EFF;
+        } else {
+            return new EmptyYangVersionEffectiveStatement(declared);
+        }
     }
 }
