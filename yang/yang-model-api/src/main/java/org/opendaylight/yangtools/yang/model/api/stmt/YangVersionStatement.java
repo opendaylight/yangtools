757c641865a99323d90637520250e4c110647306
Robert Varga <robert.varga@pantheon.tech>
2020-01-30 15:42:35 +0100

    Convert yang-version statement support
    yang-version statements are few and far between, but that is not
    a reason to be wasteful. This reworks declared and effective
    implementations to lower their footprint as well as share only
    a few instances typically.
    
    JIRA: YANGTOOLS-1065
    Change-Id: Ia5485b3dff972fc41721a3ec72be9b7f2ea67088
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/YangVersionStatement.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/YangVersionStatement.java
@@ -11,9 +11,16 @@ import static com.google.common.base.Verify.verifyNotNull;
 
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.yang.common.YangVersion;
+import org.opendaylight.yangtools.yang.model.api.YangStmtMapping;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 
 public interface YangVersionStatement extends DeclaredStatement<YangVersion> {
+    @Override
+    default StatementDefinition statementDefinition() {
+        return YangStmtMapping.YANG_VERSION;
+    }
+
     default @NonNull YangVersion getValue() {
         // FIXME: YANGTOOLS-908: verifyNotNull() should not be needed here
         return verifyNotNull(argument());
