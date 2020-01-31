54d387b0ec6413ae0f23948c598bf3ecb029e6ba
Robert Varga <robert.varga@pantheon.tech>
2020-01-31 14:20:15 +0100

    Refactor fraction-digits implementation classes
    Refactor our implementation classes, so that we end up sharing
    declared/effective instances as much as possible.
    
    JIRA: YANGTOOLS-1065
    Change-Id: I2bdf8d30f9d2fd734a536001906b1f6f0af09f41
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/FractionDigitsStatement.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/FractionDigitsStatement.java
@@ -7,9 +7,16 @@
  */
 package org.opendaylight.yangtools.yang.model.api.stmt;
 
+import org.opendaylight.yangtools.yang.model.api.YangStmtMapping;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 
 public interface FractionDigitsStatement extends DeclaredStatement<Integer> {
+    @Override
+    default StatementDefinition statementDefinition() {
+        return YangStmtMapping.FRACTION_DIGITS;
+    }
+
     default int getValue() {
         return argument().intValue();
     }
