4663b76ab20dcc1831d8395b3e3e3f627311837b
Robert Varga <robert.varga@pantheon.tech>
2020-01-31 14:20:15 +0100

    Refactor fraction-digits implementation classes
    Refactor our implementation classes, so that we end up sharing
    declared/effective instances as much as possible.
    
    JIRA: YANGTOOLS-1065
    Change-Id: I2bdf8d30f9d2fd734a536001906b1f6f0af09f41
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/fraction_digits/FractionDigitsStatementSupport.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/fraction_digits/FractionDigitsStatementSupport.java
@@ -7,22 +7,45 @@
  */
 package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.fraction_digits;
 
-import com.google.common.collect.Range;
+import static com.google.common.base.Verify.verifyNotNull;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
 import org.opendaylight.yangtools.yang.model.api.YangStmtMapping;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.FractionDigitsEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.FractionDigitsStatement;
-import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractStatementSupport;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.BaseStatementSupport;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 import org.opendaylight.yangtools.yang.parser.spi.meta.SubstatementValidator;
 import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
 
 public final class FractionDigitsStatementSupport
-        extends AbstractStatementSupport<Integer, FractionDigitsStatement, FractionDigitsEffectiveStatement> {
-    private static final Range<Integer> FRAC_DIGITS_ALLOWED = Range.closed(1, 18);
-    private static final SubstatementValidator SUBSTATEMENT_VALIDATOR = SubstatementValidator.builder(
-        YangStmtMapping.FRACTION_DIGITS)
-        .build();
+        extends BaseStatementSupport<Integer, FractionDigitsStatement, FractionDigitsEffectiveStatement> {
+    private static final SubstatementValidator SUBSTATEMENT_VALIDATOR =
+            SubstatementValidator.builder(YangStmtMapping.FRACTION_DIGITS).build();
     private static final FractionDigitsStatementSupport INSTANCE = new FractionDigitsStatementSupport();
+
+    private static final ImmutableMap<Integer, EmptyFractionDigitsStatement> EMPTY_DECLS;
+    private static final ImmutableMap<EmptyFractionDigitsStatement, EmptyFractionDigitsEffectiveStatement> EMPTY_EFF;
+
+    static {
+        final Builder<Integer, EmptyFractionDigitsStatement> declBuilder = ImmutableMap.builder();
+        final Builder<EmptyFractionDigitsStatement, EmptyFractionDigitsEffectiveStatement> effBuilder =
+                ImmutableMap.builder();
+
+        for (int i = 1; i <= 18; ++i) {
+            final Integer argument = i;
+            final EmptyFractionDigitsStatement decl = new EmptyFractionDigitsStatement(argument);
+            declBuilder.put(argument, decl);
+            effBuilder.put(decl, new EmptyFractionDigitsEffectiveStatement(decl));
+        }
+
+        EMPTY_DECLS = declBuilder.build();
+        EMPTY_EFF = effBuilder.build();
+    }
 
     private FractionDigitsStatementSupport() {
         super(YangStmtMapping.FRACTION_DIGITS);
@@ -34,32 +57,50 @@ public final class FractionDigitsStatementSupport
 
     @Override
     public Integer parseArgumentValue(final StmtContext<?, ?, ?> ctx, final String value) {
-        final Integer fractionDigits;
+        final int fractionDigits;
         try {
-            fractionDigits = Integer.valueOf(value);
+            fractionDigits = Integer.parseInt(value);
         } catch (NumberFormatException e) {
             throw new SourceException(ctx.getStatementSourceReference(), e,
                 "%s is not valid fraction-digits integer argument", value);
         }
-
-        SourceException.throwIf(!FRAC_DIGITS_ALLOWED.contains(fractionDigits), ctx.getStatementSourceReference(),
-            "fraction-digits argument should be integer within %s", FRAC_DIGITS_ALLOWED);
+        if (fractionDigits < 1 || fractionDigits > 18) {
+            throw new SourceException("fraction-digits argument should be integer within [1..18]",
+                ctx.getStatementSourceReference());
+        }
         return fractionDigits;
-    }
-
-    @Override
-    public FractionDigitsStatement createDeclared(final StmtContext<Integer, FractionDigitsStatement, ?> ctx) {
-        return new FractionDigitsStatementImpl(ctx);
-    }
-
-    @Override
-    public FractionDigitsEffectiveStatement createEffective(
-            final StmtContext<Integer, FractionDigitsStatement, FractionDigitsEffectiveStatement> ctx) {
-        return new FractionDigitsEffectiveStatementImpl(ctx);
     }
 
     @Override
     protected SubstatementValidator getSubstatementValidator() {
         return SUBSTATEMENT_VALIDATOR;
+    }
+
+    @Override
+    protected FractionDigitsStatement createDeclared(final StmtContext<Integer, FractionDigitsStatement, ?> ctx,
+            final ImmutableList<? extends DeclaredStatement<?>> substatements) {
+        return new RegularFractionDigitsStatement(ctx.coerceStatementArgument(), substatements);
+    }
+
+    @Override
+    protected FractionDigitsStatement createEmptyDeclared(final StmtContext<Integer, FractionDigitsStatement, ?> ctx) {
+        final Integer argument = ctx.coerceStatementArgument();
+        return verifyNotNull(EMPTY_DECLS.get(argument), "No declared instance for %s", argument);
+    }
+
+    @Override
+    protected FractionDigitsEffectiveStatement createEffective(
+            final StmtContext<Integer, FractionDigitsStatement, FractionDigitsEffectiveStatement> ctx,
+            final FractionDigitsStatement declared,
+            final ImmutableList<? extends EffectiveStatement<?, ?>> substatements) {
+        return new RegularFractionDigitsEffectiveStatement(declared, substatements);
+    }
+
+    @Override
+    protected FractionDigitsEffectiveStatement createEmptyEffective(
+            final StmtContext<Integer, FractionDigitsStatement, FractionDigitsEffectiveStatement> ctx,
+            final FractionDigitsStatement declared) {
+        final EmptyFractionDigitsEffectiveStatement shared = EMPTY_EFF.get(declared);
+        return shared != null ? shared :  new EmptyFractionDigitsEffectiveStatement(declared);
     }
 }
---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/fraction_digits/RegularFractionDigitsStatement.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.fraction_digits;
+
+import com.google.common.collect.ImmutableList;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.FractionDigitsStatement;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractDeclaredStatement.ArgumentToString.WithSubstatements;
+
+final class RegularFractionDigitsStatement extends WithSubstatements<Integer> implements FractionDigitsStatement {
+    RegularFractionDigitsStatement(final Integer argument,
+            final ImmutableList<? extends DeclaredStatement<?>> substatements) {
+        super(argument, substatements);
+    }
+}
