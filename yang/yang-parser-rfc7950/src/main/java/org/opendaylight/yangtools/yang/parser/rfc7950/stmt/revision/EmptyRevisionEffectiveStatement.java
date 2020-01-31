73ba037c0873f2993708e631eb36afd30c065038
Robert Varga <robert.varga@pantheon.tech>
2020-01-31 17:02:16 +0100

    WIP: refactor Revision statement implementations
    // FIXME: decide what to do about description et al.
    
    Change-Id: Ibc74c0c43c0f0fa704f2b63c0a736c8c0994abc1
    JIRA: YANGTOOLS-1065
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/revision/EmptyRevisionEffectiveStatement.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.revision;
+
+import org.opendaylight.yangtools.yang.common.Revision;
+import org.opendaylight.yangtools.yang.model.api.stmt.RevisionEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.RevisionStatement;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractDeclaredEffectiveStatement.DefaultArgument;
+
+final class EmptyRevisionEffectiveStatement extends DefaultArgument<Revision, RevisionStatement>
+        implements RevisionEffectiveStatement {
+    EmptyRevisionEffectiveStatement(final RevisionStatement declared) {
+        super(declared);
+    }
+}
