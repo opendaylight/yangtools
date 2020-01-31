8260abbf3ddc54e996faf714791ddaecf7a9aeb8
Robert Varga <robert.varga@pantheon.tech>
2020-01-31 14:20:15 +0100

    Refactor fraction-digits implementation classes
    Refactor our implementation classes, so that we end up sharing
    declared/effective instances as much as possible.
    
    JIRA: YANGTOOLS-1065
    Change-Id: I2bdf8d30f9d2fd734a536001906b1f6f0af09f41
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/fraction_digits/EmptyFractionDigitsEffectiveStatement.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/fraction_digits/EmptyFractionDigitsEffectiveStatement.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -9,13 +9,11 @@ package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.fraction_digits;
 
 import org.opendaylight.yangtools.yang.model.api.stmt.FractionDigitsEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.FractionDigitsStatement;
-import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.DeclaredEffectiveStatementBase;
-import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractDeclaredEffectiveStatement.DefaultArgument;
 
-final class FractionDigitsEffectiveStatementImpl
-        extends DeclaredEffectiveStatementBase<Integer, FractionDigitsStatement>
+final class EmptyFractionDigitsEffectiveStatement extends DefaultArgument<Integer, FractionDigitsStatement>
         implements FractionDigitsEffectiveStatement {
-    FractionDigitsEffectiveStatementImpl(final StmtContext<Integer, FractionDigitsStatement, ?> ctx) {
-        super(ctx);
+    EmptyFractionDigitsEffectiveStatement(final FractionDigitsStatement declared) {
+        super(declared);
     }
 }
