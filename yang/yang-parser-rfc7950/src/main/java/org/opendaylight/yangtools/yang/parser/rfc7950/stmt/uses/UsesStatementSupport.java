239434566a5fc443940bd5a6c2c6713cf8bc50d9
Robert Varga <robert.varga@pantheon.tech>
2020-02-02 21:02:29 +0100

    Do not copy grouping/uses statements
    All of the effects of grouping/uses should be accounted for when
    copying occurs and these statements cannot be otherwise targeted.
    
    Make sure we ignore them during copy operations.
    
    Change-Id: I1f3d3fc25c43f29948f21d454330ed8a3ce6932f
    JIRA: YANGTOOLS-1078
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/uses/UsesStatementSupport.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/uses/UsesStatementSupport.java
@@ -60,7 +60,7 @@ public final class UsesStatementSupport
     private static final UsesStatementSupport INSTANCE = new UsesStatementSupport();
 
     private UsesStatementSupport() {
-        super(YangStmtMapping.USES);
+        super(YangStmtMapping.USES, CopyPolicy.IGNORE);
     }
 
     public static UsesStatementSupport getInstance() {
