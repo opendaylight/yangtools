30b0e813ca4322a4500d69d5f84713c8c9df6a1d
Robert Varga <robert.varga@pantheon.tech>
2020-02-03 11:27:54 +0100

    Refactor min-elements statement implementations
    Migrate MinElements(Effective)Statement to improve their memory
    footprint.
    
    JIRA: YANGTOOLS-652
    Change-Id: Iddf0d2d61d304eadefbf4433d4bd8f731aa4cefa
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/min_elements/RegularMinElementsEffectiveStatement.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.min_elements;
+
+import com.google.common.collect.ImmutableList;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.MinElementsEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.MinElementsStatement;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractDeclaredEffectiveStatement.DefaultArgument.WithSubstatements;
+
+final class RegularMinElementsEffectiveStatement extends WithSubstatements<Integer, MinElementsStatement>
+        implements MinElementsEffectiveStatement {
+    RegularMinElementsEffectiveStatement(final MinElementsStatement declared,
+            final ImmutableList<? extends EffectiveStatement<?, ?>> substatements) {
+        super(declared, substatements);
+    }
+}
