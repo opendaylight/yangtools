af31f417f2ed0f24509c4b9b04d1615f478d55ee
Robert Varga <robert.varga@pantheon.tech>
2020-02-03 10:58:29 +0100

    Refactor organization statement implementations
    Migrate Organization(Effective)Statement to improve their memory
    footprint.
    
    JIRA: YANGTOOLS-652
    Change-Id: If677ade7943fa9dd1b2cc5b9ee216049a9c04145
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/organization/RegularOrganizationEffectiveStatement.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.organization;
+
+import com.google.common.collect.ImmutableList;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.OrganizationEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.OrganizationStatement;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractDeclaredEffectiveStatement.DefaultArgument.WithSubstatements;
+
+final class RegularOrganizationEffectiveStatement extends WithSubstatements<String, OrganizationStatement>
+        implements OrganizationEffectiveStatement {
+    RegularOrganizationEffectiveStatement(final OrganizationStatement declared,
+            final ImmutableList<? extends EffectiveStatement<?, ?>> substatements) {
+        super(declared, substatements);
+    }
+}
