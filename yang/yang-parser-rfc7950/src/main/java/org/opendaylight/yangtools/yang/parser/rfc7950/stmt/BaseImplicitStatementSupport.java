34baa134c02f7993d0dfb3b1728a338777b8d537
Robert Varga <robert.varga@pantheon.tech>
2020-02-04 13:34:49 +0100

    Migrate case statement
    Optimize declared/effective implementations to save memory and
    share more codepaths. As a side-effect of this refactor,
    implicitly-created case statements accurately do not retain
    declared instances.
    
    JIRA: YANGTOOLS-1065
    Change-Id: Iaf4a7171c006d4f3818c1f5a021ceb0c35ea0366
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/BaseImplicitStatementSupport.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.stmt;
+
+import com.google.common.annotations.Beta;
+import com.google.common.collect.ImmutableList;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementSource;
+import org.opendaylight.yangtools.yang.parser.rfc7950.namespace.ChildSchemaNodeNamespace;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext.Mutable;
+
+/**
+ * A massively-misnamed superclass for statements which are both schema tree participants and can be created as implicit
+ * nodes. This covers {@code case}, {@code input} and {@code output} statements.
+ *
+ * @param <D> Declared Statement representation
+ * @param <E> Effective Statement representation
+ */
+@Beta
+public abstract class BaseImplicitStatementSupport<D extends DeclaredStatement<QName>,
+        E extends EffectiveStatement<QName, D>> extends BaseQNameStatementSupport<D, E> {
+    protected BaseImplicitStatementSupport(final StatementDefinition publicDefinition) {
+        super(publicDefinition);
+    }
+
+    @Override
+    public final void onStatementAdded(final Mutable<QName, D, E> stmt) {
+        stmt.coerceParentContext().addToNs(ChildSchemaNodeNamespace.class, stmt.coerceStatementArgument(), stmt);
+    }
+
+    @Override
+    protected final E createEffective(
+            final StmtContext<QName, D, E> ctx,
+            final D declared, final ImmutableList<? extends EffectiveStatement<?, ?>> substatements) {
+        final StatementSource source = ctx.getStatementSource();
+        switch (ctx.getStatementSource()) {
+            case CONTEXT:
+                return createUndeclaredEffective(ctx, substatements);
+            case DECLARATION:
+                return createDeclaredEffective(ctx, substatements, declared);
+            default:
+                throw new IllegalStateException("Unhandled statement source " + source);
+        }
+    }
+
+    @Override
+    protected final E createEmptyEffective(final StmtContext<QName, D, E> ctx, final D declared) {
+        return createEffective(ctx, declared, ImmutableList.of());
+    }
+
+    protected abstract @NonNull E createDeclaredEffective(@NonNull StmtContext<QName, D, E> ctx,
+            @NonNull ImmutableList<? extends EffectiveStatement<?, ?>> substatements, @NonNull D declared);
+
+    protected abstract @NonNull E createUndeclaredEffective(@NonNull StmtContext<QName, D, E> ctx,
+            @NonNull ImmutableList<? extends EffectiveStatement<?, ?>> substatements);
+
+}
