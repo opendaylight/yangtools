53f9d2936dbc5d24a8653166e039cd3034831cee
Robert Varga <robert.varga@pantheon.tech>
2020-02-05 16:01:13 +0100

    Refactor Feature statement implementations
    While these are not common, implementation classes are holding
    down an abstract superclass. Improve memory footprint by switching
    to more efficient class hierarchy.
    
    JIRA: YANGTOOLS-1065
    Change-Id: I8f19c217e9a08d71d47a3afbe5ad8ea035b8a02a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/feature/EmptyFeatureEffectiveStatement.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.feature;
+
+import static java.util.Objects.requireNonNull;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.FeatureDefinition;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.api.stmt.FeatureEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.FeatureStatement;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractDeclaredEffectiveStatement.Default;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.EffectiveStatementMixins.SchemaNodeMixin;
+
+class EmptyFeatureEffectiveStatement extends Default<QName, FeatureStatement>
+        implements FeatureDefinition, FeatureEffectiveStatement, SchemaNodeMixin<QName, FeatureStatement> {
+    private final @NonNull SchemaPath path;
+    private final int flags;
+
+    EmptyFeatureEffectiveStatement(final FeatureStatement declared, final SchemaPath path, final int flags) {
+        super(declared);
+        this.path = requireNonNull(path);
+        this.flags = flags;
+    }
+
+    @Override
+    public final int flags() {
+        return flags;
+    }
+
+    @Override
+    public final @NonNull QName argument() {
+        return getQName();
+    }
+
+    @Override
+    public final SchemaPath getPath() {
+        return path;
+    }
+
+    @Override
+    public final String toString() {
+        return getClass().getSimpleName() + "[name=" + getQName() + "]";
+    }
+}
