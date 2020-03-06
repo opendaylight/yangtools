79e7d581b91152249f9c8129812314fed2435f9a
vladyslav.marchenko <Vladyslav.Marchenko@pantheon.tech>
2020-03-06 15:29:22 +0200

    YANGTOOLS-1079 Audit, optimize and explicitly test YANG string unescaping
    Signed-off-by: vladyslav.marchenko <Vladyslav.Marchenko@pantheon.tech>
    Change-Id: I3b8bd13f260f13796492d19aea35c4c3f27760fc

--- yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/parser/rfc7950/repo/AugmentContextUtilsTest.java
+++ yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/parser/rfc7950/repo/AugmentContextUtilsTest.java
@@ -8,9 +8,16 @@
 package org.opendaylight.yangtools.yang.parser.rfc7950.repo;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.opendaylight.yangtools.yang.parser.rfc7950.repo.ArgumentContextUtils.trimWhitespace;
+import static org.opendaylight.yangtools.yang.parser.rfc7950.repo.ArgumentContextUtils.unescape;
 
+import java.io.File;
+import java.util.Optional;
 import org.junit.Test;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.stmt.StmtTestUtils;
 
 public class AugmentContextUtilsTest {
 
@@ -36,5 +43,76 @@ public class AugmentContextUtilsTest {
         assertEquals("   \ta\n", trimWhitespace("   \ta\n   ", 3));
         assertEquals("   \ta\n", trimWhitespace("   \ta\n    ", 3));
         assertEquals("   \ta\n ", trimWhitespace("   \ta\n     ", 3));
+    }
+
+    @Test
+    public void testUnescapeNew() {
+//      a\b -----> a\b  (invalid for 7950)
+        assertEquals("\\abc", unescape("\\abc", 0));
+        assertEquals("abc\\", unescape("abc\\", 3));
+        assertEquals("abc\\def", unescape("abc\\def", 3));
+        //      a\\b -----> a\b
+        assertEquals("\\abc", unescape("\\\\abc", 0));
+        assertEquals("abc\\", unescape("abc\\\\", 3));
+        assertEquals("abc\\def", unescape("abc\\\\def", 3));
+        //      a\\\b -----> a\\b (invalid for 7950)
+        assertEquals("\\\\abc", unescape("\\\\\\abc", 0));
+        assertEquals("abc\\\\", unescape("abc\\\\\\", 3));
+        assertEquals("abc\\\\def", unescape("abc\\\\\\def", 3));
+        //      a"b -----> a"b (not passible)
+        assertEquals("\"abc", unescape("\"abc", 0));
+        assertEquals("abc\"", unescape("abc\"", 3));
+        assertEquals("abc\"def", unescape("abc\"def", 3));
+        //      a\"b -----> a"b
+        assertEquals("\"abc", unescape("\\\"abc", 0));
+        assertEquals("abc\"", unescape("abc\\\"", 3));
+        assertEquals("abc\"def", unescape("abc\\\"def", 3));
+        //      a\\"b -----> a\"b (not passible)
+        assertEquals("\\\"abc", unescape("\\\\\"abc", 0));
+        assertEquals("abc\\\"", unescape("abc\\\\\"", 3));
+        assertEquals("abc\\\"def", unescape("abc\\\\\"def", 3));
+        //      a\\\"b -----> a\"b
+        assertEquals("\\\"abc", unescape("\\\\\\\"abc", 0));
+        assertEquals("abc\\\"", unescape("abc\\\\\\\"", 3));
+        assertEquals("abc\\\"def", unescape("abc\\\\\\\"def", 3));
+        //      a\tb -----> a   b
+        assertEquals("\tabc", unescape("\\tabc", 0));
+        assertEquals("abc\t", unescape("abc\\t", 3));
+        assertEquals("abc\tdef", unescape("abc\\tdef", 3));
+        //      a\\tb -----> a\tb
+        assertEquals("\\tabc", unescape("\\\\tabc", 0));
+        assertEquals("abc\\t", unescape("abc\\\\t", 3));
+        assertEquals("abc\\tdef", unescape("abc\\\\tdef", 3));
+        //      a\\\tb -----> a\    b
+        assertEquals("\\\tabc", unescape("\\\\\\tabc", 0));
+        assertEquals("abc\\\t", unescape("abc\\\\\\t", 3));
+        assertEquals("abc\\\tdef", unescape("abc\\\\\\tdef", 3));
+        //      a\nb -----> a
+        //                  b
+        assertEquals("\nabc", unescape("\\nabc", 0));
+        assertEquals("abc\n", unescape("abc\\n", 3));
+        assertEquals("abc\ndef", unescape("abc\\ndef", 3));
+        //      a\\nb -----> a\nb
+        assertEquals("\\nabc", unescape("\\\\nabc", 0));
+        assertEquals("abc\\n", unescape("abc\\\\n", 3));
+        assertEquals("abc\\ndef", unescape("abc\\\\ndef", 3));
+        //      a\\\nb -----> a\
+        //                    b
+        assertEquals("\\\nabc", unescape("\\\\\\nabc", 0));
+        assertEquals("abc\\\n", unescape("abc\\\\\\n", 3));
+        assertEquals("abc\\\ndef", unescape("abc\\\\\\ndef", 3));
+
+        assertEquals("\\\nabc abc\\n\nabc abc\t", unescape("\\\\\\nabc abc\\\\n\\nabc abc\\t", 0));
+    }
+
+    @Test
+    public void stringTestUnescape() throws Exception {
+        final SchemaContext schemaContext = StmtTestUtils.parseYangSources(new File(getClass()
+                .getResource("/unescape/string-test.yang").toURI()));
+        assertNotNull(schemaContext);
+        assertEquals(1, schemaContext.getModules().size());
+        final Module module = schemaContext.getModules().iterator().next();
+        assertEquals(Optional.of("  Unescaping examples: \\,\n,\t  \"string enclosed in double quotes\" end\n"
+                + "abc \\\\\\ \\t \\\nnn"), module.getDescription());
     }
 }
