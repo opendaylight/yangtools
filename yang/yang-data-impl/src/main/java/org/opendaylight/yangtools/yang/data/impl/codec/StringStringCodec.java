c45723bf1b14babbf045c746d41458c26992f5e3
Tadei Bilan <tadei.bilan@pantheon.tech>
2020-03-27 13:48:06 +0200

    YANGTOOLS-763 yang-data-codecs: improve validation error reporting
    Change-Id: Ibee7484737ec75238f712ddde49941e4639c2fa8
    Signed-off-by: tadei.bilan <tadei.bilan@pantheon.tech>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/codec/StringStringCodec.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/codec/StringStringCodec.java
@@ -7,10 +7,11 @@
  */
 package org.opendaylight.yangtools.yang.data.impl.codec;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.annotations.Beta;
+import org.opendaylight.yangtools.yang.common.RpcError;
+import org.opendaylight.yangtools.yang.data.api.codec.IllegalYangValueException;
 import org.opendaylight.yangtools.yang.data.api.codec.StringCodec;
 import org.opendaylight.yangtools.yang.model.api.type.LengthConstraint;
 import org.opendaylight.yangtools.yang.model.api.type.StringTypeDefinition;
@@ -46,9 +47,13 @@ public class StringStringCodec extends TypeDefinitionAwareCodec<String, StringTy
 
     void validate(final String str) {
         if (lengthConstraint != null) {
-            // FIXME: YANGTOOLS-763: throw a dedicated exception
-            checkArgument(lengthConstraint.getAllowedRanges().contains(str.length()),
-                    "String '%s' does not match allowed length constraint %s", lengthConstraint);
+            if (!lengthConstraint.getAllowedRanges().contains(str.length())) {
+                throw new IllegalYangValueException(
+                        RpcError.ErrorSeverity.ERROR,
+                        RpcError.ErrorType.PROTOCOL,
+                        "String " + str + " does not match allowed length constraint " + lengthConstraint,
+                        "bad-element");
+            }
         }
     }
 }
