8f245549b7dee96f128be1cc6eaa28c1ff22b883
Robert Varga <robert.varga@pantheon.tech>
2020-06-30 23:30:55 +0200

    Convert BaseStatementSupport
    Convert the trivial case of base statement, which can be
    easily minimized.
    
    JIRA: YANGTOOLS-1065
    Change-Id: If575ee91c1266b500655a96321e938c2abb849cb
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/BaseEffectiveStatement.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/BaseEffectiveStatement.java
@@ -9,9 +9,14 @@ package org.opendaylight.yangtools.yang.model.api.stmt;
 
 import com.google.common.annotations.Beta;
 import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.YangStmtMapping;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 
 @Beta
 public interface BaseEffectiveStatement extends EffectiveStatement<QName, BaseStatement> {
-
+    @Override
+    default StatementDefinition statementDefinition() {
+        return YangStmtMapping.BASE;
+    }
 }
