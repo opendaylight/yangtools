722c65168aefde350e9fa3cc975adc06a29c21d4
Robert Varga <robert.varga@pantheon.tech>
2020-06-30 13:19:35 +0200

    Migrate WhenStatementSupport to BaseStatementSupport
    This allows us to reduce the footprint of individual declared
    and effective instances.
    
    JIRA: YANGTOOLS-1065
    Change-Id: I6a5dcba7fdbb2a6cdeba54c99627a41423683e05
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/when/EmptyWhenStatement.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/when/EmptyWhenStatement.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -9,11 +9,15 @@ package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.when;
 
 import org.opendaylight.yangtools.yang.model.api.RevisionAwareXPath;
 import org.opendaylight.yangtools.yang.model.api.stmt.WhenStatement;
-import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractDeclaredStatement;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractDeclaredStatement.WithArgument;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 
-final class WhenStatementImpl extends AbstractDeclaredStatement<RevisionAwareXPath> implements WhenStatement {
-    WhenStatementImpl(final StmtContext<RevisionAwareXPath, WhenStatement, ?> context) {
+/**
+ * @author Robert Varga
+ *
+ */
+final class EmptyWhenStatement extends WithArgument<RevisionAwareXPath> implements WhenStatement {
+    EmptyWhenStatement(final StmtContext<RevisionAwareXPath, ?, ?> context) {
         super(context);
     }
 }
