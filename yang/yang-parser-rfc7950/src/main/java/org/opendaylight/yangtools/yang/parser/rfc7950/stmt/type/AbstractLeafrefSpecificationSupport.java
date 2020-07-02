df059357c84ac57a4bbe7055a2e7d4e8bfe44154
Robert Varga <robert.varga@pantheon.tech>
2020-07-02 23:56:30 +0200

    Migrate LeafrefSpecificationSupport
    LeafreefSpecificationSupport can use memory-efficient representation
    through BaseStatementSupport. Furthermore we can reuse
    TypeEffectiveStatementImpl instead of brewing a separate effective
    implementation.
    
    JIRA: YANGTOOLS-1065
    Change-Id: I735563730e8c382b49ea01fccc00191ffcfdfb14
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/type/AbstractLeafrefSpecificationSupport.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/type/AbstractLeafrefSpecificationSupport.java
@@ -7,15 +7,21 @@
  */
 package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.type;
 
+import com.google.common.collect.ImmutableList;
 import org.opendaylight.yangtools.yang.model.api.YangStmtMapping;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.PathEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.RequireInstanceEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.TypeStatement.LeafrefSpecification;
-import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractStatementSupport;
+import org.opendaylight.yangtools.yang.model.util.type.BaseTypes;
+import org.opendaylight.yangtools.yang.model.util.type.LeafrefTypeBuilder;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.BaseStatementSupport;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
 
 abstract class AbstractLeafrefSpecificationSupport
-        extends
-        AbstractStatementSupport<String, LeafrefSpecification, EffectiveStatement<String, LeafrefSpecification>> {
+        extends BaseStatementSupport<String, LeafrefSpecification, EffectiveStatement<String, LeafrefSpecification>> {
     AbstractLeafrefSpecificationSupport() {
         super(YangStmtMapping.TYPE);
     }
@@ -26,13 +32,48 @@ abstract class AbstractLeafrefSpecificationSupport
     }
 
     @Override
-    public final LeafrefSpecification createDeclared(final StmtContext<String, LeafrefSpecification, ?> ctx) {
-        return new LeafrefSpecificationImpl(ctx);
+    protected final LeafrefSpecification createDeclared(final StmtContext<String, LeafrefSpecification, ?> ctx,
+            final ImmutableList<? extends DeclaredStatement<?>> substatements) {
+        return new LeafrefSpecificationImpl(ctx, substatements);
     }
 
     @Override
-    public final EffectiveStatement<String, LeafrefSpecification> createEffective(
-            final StmtContext<String, LeafrefSpecification, EffectiveStatement<String, LeafrefSpecification>> ctx) {
-        return new LeafrefSpecificationEffectiveStatement(ctx);
+    protected final LeafrefSpecification createEmptyDeclared(final StmtContext<String, LeafrefSpecification, ?> ctx) {
+        throw noPath(ctx);
+    }
+
+    @Override
+    protected final EffectiveStatement<String, LeafrefSpecification> createEffective(
+            final StmtContext<String, LeafrefSpecification, EffectiveStatement<String, LeafrefSpecification>> ctx,
+            final LeafrefSpecification declared,
+            final ImmutableList<? extends EffectiveStatement<?, ?>> substatements) {
+        final LeafrefTypeBuilder builder = BaseTypes.leafrefTypeBuilder(ctx.getSchemaPath().get());
+
+        for (final EffectiveStatement<?, ?> stmt : substatements) {
+            if (stmt instanceof PathEffectiveStatement) {
+                builder.setPathStatement(((PathEffectiveStatement) stmt).argument());
+            } else if (stmt instanceof RequireInstanceEffectiveStatement) {
+                builder.setRequireInstance(((RequireInstanceEffectiveStatement)stmt).argument());
+            }
+        }
+
+        return new TypeEffectiveStatementImpl<>(declared, substatements, builder);
+    }
+
+    @Override
+    protected final EffectiveStatement<String, LeafrefSpecification> createEmptyEffective(
+            final StmtContext<String, LeafrefSpecification, EffectiveStatement<String, LeafrefSpecification>> ctx,
+            final LeafrefSpecification declared) {
+        throw noPath(ctx);
+    }
+
+    private static SourceException noPath(final StmtContext<?, ?, ?> ctx) {
+        /*
+         *  https://tools.ietf.org/html/rfc7950#section-9.12
+         *
+         *     When the type is "union", the "type" statement (Section 7.4) MUST be
+         *     present.
+         */
+        return new SourceException("A path statement has to be present", ctx.getStatementSourceReference());
     }
 }
