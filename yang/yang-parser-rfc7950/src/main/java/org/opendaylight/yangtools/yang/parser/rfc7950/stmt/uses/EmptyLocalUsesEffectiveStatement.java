954f358ee3688cb6f68a3ab84d78f55f71bc3efd
Robert Varga <robert.varga@pantheon.tech>
2020-07-02 13:37:42 +0200

    Convert UsesStatementSupport to BaseQNameStatementSupport
    Uses statement is a bit more complicated in its effective form,
    hence we end up with four different implementations when we optimize
    for size.
    
    JIRA: YANGTOOLS-1065
    Change-Id: I7f01ecd8277a042821cb5b37c0d9406a1e9204be
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/uses/EmptyLocalUsesEffectiveStatement.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.uses;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Collection;
+import java.util.Map;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.AugmentationSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.GroupingDefinition;
+import org.opendaylight.yangtools.yang.model.api.SchemaNode;
+import org.opendaylight.yangtools.yang.model.api.UsesNode;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier.Descendant;
+import org.opendaylight.yangtools.yang.model.api.stmt.UsesEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.UsesStatement;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractDeclaredEffectiveStatement.DefaultArgument;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.EffectiveStatementMixins.CopyableMixin;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.EffectiveStatementMixins.DocumentedNodeMixin.WithStatus;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.EffectiveStatementMixins.WhenConditionMixin;
+
+/**
+ * Empty {@link UsesEffectiveStatement}, at its very simplest form. This is appropriate when all of these are true:
+ * <ul>
+ *   <li>This statement takes effect in the same module as where it was declared, i.e. the arguments match</li>
+ *   <li>It has no substatements</li>
+ * </ul>
+ *
+ * <p>
+ * This provides minimum footprint, as we share the argument from the declared instance and just keep the flags and
+ * source grouping pointer.
+ */
+class EmptyLocalUsesEffectiveStatement extends DefaultArgument<QName, UsesStatement>
+        implements UsesEffectiveStatement, UsesNode, CopyableMixin<QName, UsesStatement>,
+            WhenConditionMixin<QName, UsesStatement>, WithStatus<QName, UsesStatement> {
+    private final @NonNull GroupingDefinition sourceGrouping;
+    private final int flags;
+
+    EmptyLocalUsesEffectiveStatement(final UsesStatement declared, final GroupingDefinition sourceGrouping,
+            final int flags) {
+        super(declared);
+        this.sourceGrouping = requireNonNull(sourceGrouping);
+        this.flags = flags;
+    }
+
+    @Override
+    public final GroupingDefinition getSourceGrouping() {
+        return sourceGrouping;
+    }
+
+    @Override
+    public final int flags() {
+        return flags;
+    }
+
+    @Override
+    public Collection<? extends AugmentationSchemaNode> getAugmentations() {
+        return filterEffectiveStatements(AugmentationSchemaNode.class);
+    }
+
+    @Override
+    public Map<Descendant, SchemaNode> getRefines() {
+        return ImmutableMap.of();
+    }
+
+    @Override
+    // FIXME: 6.0.0: remove this method
+    public final String toString() {
+        return "UsesEffectiveStatementImpl[sourceGrouping=" + sourceGrouping.getQName() + "]";
+    }
+}
---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/uses/RegularLocalUsesEffectiveStatement.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.uses;
+
+import com.google.common.collect.ImmutableList;
+import java.util.Collection;
+import java.util.Map;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.model.api.AugmentationSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.GroupingDefinition;
+import org.opendaylight.yangtools.yang.model.api.SchemaNode;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier.Descendant;
+import org.opendaylight.yangtools.yang.model.api.stmt.UsesStatement;
+
+/**
+ * An extension of {@link EmptyLocalUsesEffectiveStatement}, which adds substatements to the mix. Since this means we
+ * can also have refine statements, we keep a lazily-populated map of those.
+ */
+final class RegularLocalUsesEffectiveStatement extends EmptyLocalUsesEffectiveStatement {
+    private final Object substatements;
+
+    private volatile Map<Descendant, SchemaNode> refines;
+
+    RegularLocalUsesEffectiveStatement(final UsesStatement declared, final GroupingDefinition sourceGrouping,
+            final int flags, final ImmutableList<? extends EffectiveStatement<?, ?>> substatements) {
+        super(declared, sourceGrouping, flags);
+        this.substatements = maskList(substatements);
+    }
+
+    @Override
+    public ImmutableList<? extends EffectiveStatement<?, ?>> effectiveSubstatements() {
+        return unmaskList(substatements);
+    }
+
+    @Override
+    public Collection<? extends AugmentationSchemaNode> getAugmentations() {
+        return filterEffectiveStatements(AugmentationSchemaNode.class);
+    }
+
+    @Override
+    public Map<Descendant, SchemaNode> getRefines() {
+        final Map<Descendant, SchemaNode> local;
+        return (local = refines) != null ? local : loadRefines();
+    }
+
+    private synchronized @NonNull Map<Descendant, SchemaNode> loadRefines() {
+        Map<Descendant, SchemaNode> local = refines;
+        if (local == null) {
+            refines = local = UsesStatementSupport.indexRefines(effectiveSubstatements());
+        }
+        return local;
+    }
+}
