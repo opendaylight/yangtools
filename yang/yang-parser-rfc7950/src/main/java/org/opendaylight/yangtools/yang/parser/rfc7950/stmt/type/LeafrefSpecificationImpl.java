8d78601a4346cdc16efd13e1d812a9e6c7496da4
Robert Varga <robert.varga@pantheon.tech>
2020-07-02 23:56:30 +0200

    Migrate LeafrefSpecificationSupport
    LeafrefSpecificationSupport can use memory-efficient representation
    through BaseStatementSupport. Furthermore we can reuse
    TypeEffectiveStatementImpl instead of brewing a separate effective
    implementation.
    
    JIRA: YANGTOOLS-1065
    Change-Id: I735563730e8c382b49ea01fccc00191ffcfdfb14
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/type/LeafrefSpecificationImpl.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/type/LeafrefSpecificationImpl.java
@@ -7,12 +7,15 @@
  */
 package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.type;
 
+import com.google.common.collect.ImmutableList;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.TypeStatement.LeafrefSpecification;
-import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractDeclaredStatement;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractDeclaredStatement.WithRawStringArgument.WithSubstatements;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 
-final class LeafrefSpecificationImpl extends AbstractDeclaredStatement<String> implements LeafrefSpecification {
-    LeafrefSpecificationImpl(final StmtContext<String, LeafrefSpecification, ?> context) {
-        super(context);
+final class LeafrefSpecificationImpl extends WithSubstatements implements LeafrefSpecification {
+    LeafrefSpecificationImpl(final StmtContext<String, ?, ?> context,
+            final ImmutableList<? extends DeclaredStatement<?>> substatements) {
+        super(context, substatements);
     }
 }
