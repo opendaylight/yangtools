30c0380ca7b3dcffbeecd06506e868247f6aee34
Robert Varga <robert.varga@pantheon.tech>
2020-07-02 23:44:23 +0200

    Migrate UnionSpecificationSupport
    UnionSpecificationSupport can use memory-efficient representation
    through BaseStatementSupport. Furthermore we can reuse
    TypeEffectiveStatementImpl instead of brewing a separate effective
    implementation.
    
    JIRA: YANGTOOLS-1065
    Change-Id: I93ad3126e000c489776ea92aed58e9183e93da42
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/type/UnionSpecificationSupport.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/type/UnionSpecificationSupport.java
@@ -7,15 +7,21 @@
  */
 package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.type;
 
+import com.google.common.collect.ImmutableList;
 import org.opendaylight.yangtools.yang.model.api.YangStmtMapping;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.TypeEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.TypeStatement.UnionSpecification;
-import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractStatementSupport;
+import org.opendaylight.yangtools.yang.model.util.type.BaseTypes;
+import org.opendaylight.yangtools.yang.model.util.type.UnionTypeBuilder;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.BaseStatementSupport;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 import org.opendaylight.yangtools.yang.parser.spi.meta.SubstatementValidator;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
 
-final class UnionSpecificationSupport extends
-    AbstractStatementSupport<String, UnionSpecification, EffectiveStatement<String, UnionSpecification>> {
+final class UnionSpecificationSupport
+        extends BaseStatementSupport<String, UnionSpecification, EffectiveStatement<String, UnionSpecification>> {
     private static final SubstatementValidator SUBSTATEMENT_VALIDATOR = SubstatementValidator.builder(YangStmtMapping
         .TYPE)
         .addMultiple(YangStmtMapping.TYPE)
@@ -31,18 +37,50 @@ final class UnionSpecificationSupport extends
     }
 
     @Override
-    public UnionSpecification createDeclared(final StmtContext<String, UnionSpecification, ?> ctx) {
-        return new UnionSpecificationImpl(ctx);
-    }
-
-    @Override
-    public EffectiveStatement<String, UnionSpecification> createEffective(
-            final StmtContext<String, UnionSpecification, EffectiveStatement<String, UnionSpecification>> ctx) {
-        return new UnionSpecificationEffectiveStatement(ctx);
-    }
-
-    @Override
     protected SubstatementValidator getSubstatementValidator() {
         return SUBSTATEMENT_VALIDATOR;
+    }
+
+    @Override
+    protected UnionSpecification createDeclared(final StmtContext<String, UnionSpecification, ?> ctx,
+            final ImmutableList<? extends DeclaredStatement<?>> substatements) {
+        return new UnionSpecificationImpl(ctx, substatements);
+    }
+
+    @Override
+    protected UnionSpecification createEmptyDeclared(final StmtContext<String, UnionSpecification, ?> ctx) {
+        throw noType(ctx);
+    }
+
+    @Override
+    protected EffectiveStatement<String, UnionSpecification> createEffective(
+            final StmtContext<String, UnionSpecification, EffectiveStatement<String, UnionSpecification>> ctx,
+            final UnionSpecification declared, final ImmutableList<? extends EffectiveStatement<?, ?>> substatements) {
+        final UnionTypeBuilder builder = BaseTypes.unionTypeBuilder(ctx.getSchemaPath().get());
+
+        for (final EffectiveStatement<?, ?> stmt : substatements) {
+            if (stmt instanceof TypeEffectiveStatement) {
+                builder.addType(((TypeEffectiveStatement<?>)stmt).getTypeDefinition());
+            }
+        }
+
+        return new TypeEffectiveStatementImpl<>(declared, substatements, builder);
+    }
+
+    @Override
+    protected EffectiveStatement<String, UnionSpecification> createEmptyEffective(
+            final StmtContext<String, UnionSpecification, EffectiveStatement<String, UnionSpecification>> ctx,
+            final UnionSpecification declared) {
+        throw noType(ctx);
+    }
+
+    private static SourceException noType(final StmtContext<?, ?, ?> ctx) {
+        /*
+         *  https://tools.ietf.org/html/rfc7950#section-9.12
+         *
+         *     When the type is "union", the "type" statement (Section 7.4) MUST be
+         *     present.
+         */
+        return new SourceException("At least one type statement has to be present", ctx.getStatementSourceReference());
     }
 }
