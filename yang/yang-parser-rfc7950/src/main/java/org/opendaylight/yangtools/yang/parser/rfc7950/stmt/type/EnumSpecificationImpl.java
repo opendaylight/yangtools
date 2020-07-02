ec565b55de1cd89c86bae2af2495e1e40b98fc4d
Robert Varga <robert.varga@pantheon.tech>
2020-07-02 23:08:20 +0200

    Migrate EnumSpecificationSupport
    EnumSpecificationSupport can use memory-efficient representation
    through BaseStatementSupport. Furthermore we can reuse
    TypeEffectiveStatementImpl instead of brewing a separate effective
    implementation.
    
    Change-Id: I0b211e2cfd09cae8e69ca614b190a562ba85e090
    JIRA: YANGTOOLS-1065
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/type/EnumSpecificationImpl.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/type/EnumSpecificationImpl.java
@@ -7,12 +7,15 @@
  */
 package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.type;
 
+import com.google.common.collect.ImmutableList;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.TypeStatement.EnumSpecification;
-import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractDeclaredStatement;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractDeclaredStatement.WithRawStringArgument.WithSubstatements;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 
-final class EnumSpecificationImpl extends AbstractDeclaredStatement<String> implements EnumSpecification {
-    EnumSpecificationImpl(final StmtContext<String, EnumSpecification, ?> context) {
-        super(context);
+final class EnumSpecificationImpl extends WithSubstatements implements EnumSpecification {
+    EnumSpecificationImpl(final StmtContext<String, ?, ?> context,
+            final ImmutableList<? extends DeclaredStatement<?>> substatements) {
+        super(context, substatements);
     }
 }
