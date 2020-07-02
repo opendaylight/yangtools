d693701b368eeb1e9e9aabfe3be39b0c9ff59fe6
Robert Varga <robert.varga@pantheon.tech>
2020-07-03 00:08:34 +0200

    Migrate InstanceIdentifierSpecificationSupport
    InstanceIdentifierSpecificationSupport can use memory-efficient
    representation through BaseStatementSupport. Furthermore we can reuse
    TypeEffectiveStatementImpl instead of brewing a separate effective
    implementation.
    
    JIRA: YANGTOOLS-1065
    Change-Id: I821172baf33bd639fd52e02160db7122b4f53367
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/type/EmptyIdentifierSpecification.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/type/EmptyIdentifierSpecification.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -8,12 +8,11 @@
 package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.type;
 
 import org.opendaylight.yangtools.yang.model.api.stmt.TypeStatement.InstanceIdentifierSpecification;
-import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractDeclaredStatement;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractDeclaredStatement.WithRawStringArgument;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 
-final class InstanceIdentifierSpecificationImpl extends AbstractDeclaredStatement<String>
-        implements InstanceIdentifierSpecification {
-    InstanceIdentifierSpecificationImpl(final StmtContext<String, InstanceIdentifierSpecification, ?> ctx) {
+final class EmptyIdentifierSpecification extends WithRawStringArgument implements InstanceIdentifierSpecification {
+    EmptyIdentifierSpecification(final StmtContext<String, ?, ?> ctx) {
         super(ctx);
     }
 }
