aac90eae99cf552f8d926b6dea9e36e96bf63d4b
Robert Varga <robert.varga@pantheon.tech>
2020-08-10 14:07:00 +0200

    WIP: add specialized token factory
    Our baseline parsing footprint is rather large. We can improve it
    by looking at token component sizes and using distinct token classes.
    
    Aside from common tokens, which are using as fallbacks for text
    handling, we introduce 4 specialized classes, three of them seeing
    typical use.
    
    Since these classes use smaller fields to hold lazy state, as well
    as eliminate typical invariants, we end up saving around 39MiB (12%)
    in a memory-hungry use case.
    
    // FIXME: evaluate performance impact of this change
    
    JIRA: YANGTOOLS-1128
    Change-Id: I600ae6cbe755212e2d1a15ae3e6b369ed0b3b962
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/antlr/Token12140.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.antlr;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.TokenSource;
+import org.antlr.v4.runtime.misc.Pair;
+
+class Token12140 extends AbstractLazyToken {
+    private final byte type;
+    private final short line;
+    private final byte charPositionInLine;
+
+    private final int startIndex;
+
+    Token12140(final Pair<TokenSource, CharStream> source, final int type, final int line, final int charPositionInLine,
+            final int startIndex) {
+        super(source);
+        this.type = (byte) type;
+        this.line = (short) line;
+        this.charPositionInLine = (byte) charPositionInLine;
+        this.startIndex = startIndex;
+    }
+
+    @Override
+    public final int getType() {
+        return type;
+    }
+
+    @Override
+    public final int getLine() {
+        return Short.toUnsignedInt(line);
+    }
+
+    @Override
+    public final int getCharPositionInLine() {
+        return Byte.toUnsignedInt(charPositionInLine);
+    }
+
+    @Override
+    public final int getStartIndex() {
+        return startIndex;
+    }
+
+    @Override
+    public int getStopIndex() {
+        return startIndex;
+    }
+}
