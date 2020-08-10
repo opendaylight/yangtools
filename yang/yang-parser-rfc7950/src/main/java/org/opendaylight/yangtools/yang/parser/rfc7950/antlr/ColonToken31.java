6f1a35461502fe29b80c2a9d5cf64d540a862689
Robert Varga <robert.varga@pantheon.tech>
2020-08-10 14:56:29 +0200

    WIP: Add explicit character tokens
    We have a couple of single-character tokens. Specializing holder classes,
    for which we can easily specialize. This saves further 11MiB (4%) of memory
    retained by AST schema source.
    
    // FIXME: re-qualify memory improvements
    
    JIRA: YANGTOOLS-1128
    Change-Id: I5f6b345a5425deec9be07bc9a22ef417e80a6302
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/antlr/ColonToken31.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.antlr;
+
+final class ColonToken31 extends AbstractColonToken {
+    private final int value;
+
+    ColonToken31(final int line, final int charPositionInLine) {
+        value = value31(line, charPositionInLine);
+    }
+
+    @Override
+    public int getLine() {
+        return getLine(value);
+    }
+
+    @Override
+    public int getCharPositionInLine() {
+        return getCharPositionInLine(value);
+    }
+}
