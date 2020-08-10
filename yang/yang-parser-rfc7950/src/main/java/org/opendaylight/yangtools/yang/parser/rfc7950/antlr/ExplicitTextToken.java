2a10346d2692037a9c5fb01a816ca6397e7983bd
Robert Varga <robert.varga@pantheon.tech>
2020-08-10 14:07:00 +0200

    Add a specialized token factory
    Our baseline parsing footprint is rather large. We can improve it
    by looking at token component sizes and using distinct token classes.
    
    Aside from CommonToken, which are using as fallbacks for text
    handling, we introduce 4 specialized classes, three of them seeing
    typical use.
    
    Since these classes use smaller fields to hold lazy state, as well
    as eliminate typical invariants, we end up saving around 39MiB (12%)
    of AST size in a typical case from the field.
    
    JIRA: YANGTOOLS-1128
    Change-Id: I600ae6cbe755212e2d1a15ae3e6b369ed0b3b962
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/antlr/ExplicitTextToken.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.antlr;
+
+import static java.util.Objects.requireNonNull;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.TokenSource;
+
+final class ExplicitTextToken extends AbstractToken {
+    private final int type;
+    private final String text;
+
+    ExplicitTextToken(final int type, final String text) {
+        this.type = type;
+        this.text = requireNonNull(text);
+    }
+
+    @Override
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public int getType() {
+        return type;
+    }
+
+    @Override
+    public int getLine() {
+        // TODO: this mimics CommonToken, but is probably not right
+        return 0;
+    }
+
+    @Override
+    public int getCharPositionInLine() {
+        return -1;
+    }
+
+    @Override
+    public int getStartIndex() {
+        // TODO: this mimics CommonToken, but is probably not right
+        return 0;
+    }
+
+    @Override
+    public int getStopIndex() {
+        // TODO: this mimics CommonToken, but is probably not right
+        return 0;
+    }
+
+    @Override
+    public TokenSource getTokenSource() {
+        return null;
+    }
+
+    @Override
+    public CharStream getInputStream() {
+        return null;
+    }
+}
