8be2ddcfe70caf5bedaae11022a159050625e572
Robert Varga <robert.varga@pantheon.tech>
2020-08-10 18:11:57 +0200

    Fix YangDataStatementSupport declaration
    We should not be operating on parameterized EffectiveStatement,
    but rather of YangDataEffectiveStatement itself.
    
    Change-Id: I02797816e712b9a484dca694c77f4074ceb2b0da
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 32595bd5e0cc5db2866ef3333df1682b32f2aea6)

--- yang/rfc8040-parser-support/src/main/java/org/opendaylight/yangtools/rfc8040/parser/YangDataStatementSupport.java
+++ yang/rfc8040-parser-support/src/main/java/org/opendaylight/yangtools/rfc8040/parser/YangDataStatementSupport.java
@@ -8,10 +8,10 @@
 package org.opendaylight.yangtools.rfc8040.parser;
 
 import com.google.common.annotations.Beta;
+import org.opendaylight.yangtools.rfc8040.model.api.YangDataEffectiveStatement;
 import org.opendaylight.yangtools.rfc8040.model.api.YangDataStatement;
 import org.opendaylight.yangtools.rfc8040.model.api.YangDataStatements;
 import org.opendaylight.yangtools.yang.model.api.YangStmtMapping;
-import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractDeclaredStatement;
 import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractStatementSupport;
@@ -20,8 +20,8 @@ import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext.Mutable;
 import org.opendaylight.yangtools.yang.parser.spi.meta.SubstatementValidator;
 
 @Beta
-public final class YangDataStatementSupport extends AbstractStatementSupport<String, YangDataStatement,
-        EffectiveStatement<String, YangDataStatement>> {
+public final class YangDataStatementSupport
+        extends AbstractStatementSupport<String, YangDataStatement, YangDataEffectiveStatement> {
     /**
      * Declared statement representation of 'yang-data' extension defined in
      * <a href="https://tools.ietf.org/html/rfc8040#section-8">RFC 8040</a>.
@@ -69,8 +69,8 @@ public final class YangDataStatementSupport extends AbstractStatementSupport<Str
     }
 
     @Override
-    public EffectiveStatement<String, YangDataStatement> createEffective(final StmtContext<String,
-            YangDataStatement, EffectiveStatement<String, YangDataStatement>> ctx) {
+    public YangDataEffectiveStatement createEffective(
+            final StmtContext<String, YangDataStatement, YangDataEffectiveStatement> ctx) {
         // in case of yang-data node we need to perform substatement validation at the point when we have
         // effective substatement contexts already available - if the node has only a uses statement declared in it,
         // one top-level container node may very well be added to the yang-data as an effective statement
@@ -79,8 +79,7 @@ public final class YangDataStatementSupport extends AbstractStatementSupport<Str
     }
 
     @Override
-    public void onFullDefinitionDeclared(final Mutable<String, YangDataStatement,
-            EffectiveStatement<String, YangDataStatement>> ctx) {
+    public void onFullDefinitionDeclared(final Mutable<String, YangDataStatement, YangDataEffectiveStatement> ctx) {
         // as per https://tools.ietf.org/html/rfc8040#section-8,
         // yang-data is ignored unless it appears as a top-level statement
         if (ctx.coerceParentContext().getParentContext() != null) {
