fc65ada0a33174bac1650970cea1b228c9aa9a42
Robert Varga <robert.varga@pantheon.tech>
2020-08-10 14:56:29 +0200

    WIP: Add explicit character tokens
    We have a couple of single-character tokens. Specializing holder classes,
    for which we can easily specialize. This saves further 5MiB (2%) of memory.
    
    // FIXME: refactor these so that we have common getType/getText() methods
    //        as location info is not typically used
    
    // FIXME: add generic single-char tokens? These would be more effective
    //        as they could also cover SEP and other tokens.
    
    JIRA: YANGTOOLS-1128
    Change-Id: I5f6b345a5425deec9be07bc9a22ef417e80a6302
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/antlr/RightBraceToken44.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.antlr;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.TokenSource;
+import org.antlr.v4.runtime.misc.Pair;
+import org.opendaylight.yangtools.yang.parser.antlr.YangStatementLexer;
+
+final class RightBraceToken44 extends AbstractCharToken44 {
+    RightBraceToken44(final Pair<TokenSource, CharStream> source, final int line, final int charPositionInLine) {
+        super(source, line, charPositionInLine);
+    }
+
+    @Override
+    public int getType() {
+        return YangStatementLexer.RIGHT_BRACE;
+    }
+
+    @Override
+    public String getText() {
+        return "}";
+    }
+}
