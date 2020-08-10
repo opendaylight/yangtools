e141fa94698bb61a7e42957187c00199a759a8f4
Robert Varga <robert.varga@pantheon.tech>
2020-08-10 14:07:00 +0200

    Add a specialized token factory
    Our baseline parsing footprint is rather large. We can improve it
    by looking at token component sizes and using distinct token classes.
    
    Aside from CommonToken, which are using as fallbacks for text
    handling, we introduce 4 specialized classes, three of them seeing
    typical use.
    
    Since these classes use smaller fields to hold lazy state, as well
    as eliminate typical invariants, we end up saving around 39MiB (12%)
    of AST size in a typical case from the field.
    
    JIRA: YANGTOOLS-1128
    Change-Id: I600ae6cbe755212e2d1a15ae3e6b369ed0b3b962
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/antlr/AbstractToken.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.antlr;
+
+import static com.google.common.base.Preconditions.checkState;
+
+import org.antlr.v4.runtime.WritableToken;
+
+abstract class AbstractToken implements WritableToken {
+    private int tokenIndex = -1;
+
+    @Override
+    public final int getChannel() {
+        return DEFAULT_CHANNEL;
+    }
+
+    @Override
+    public final int getTokenIndex() {
+        return tokenIndex;
+    }
+
+    @Override
+    public final void setTokenIndex(final int index) {
+        checkState(tokenIndex == -1);
+        tokenIndex = index;
+    }
+
+    @Override
+    public final void setText(final String text) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public final void setType(final int ttype) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public final void setLine(final int line) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public final void setCharPositionInLine(final int pos) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public final void setChannel(final int channel) {
+        throw new UnsupportedOperationException();
+    }
+}
