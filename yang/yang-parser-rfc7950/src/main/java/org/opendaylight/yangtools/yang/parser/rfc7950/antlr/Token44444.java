128bd8d5fbf16bb338c7e07d60ecea2b3ac0c27d
Robert Varga <robert.varga@pantheon.tech>
2020-08-10 14:07:00 +0200

    Add a specialized token factory
    Our baseline parsing footprint is rather large. We can improve it
    by looking at token component sizes and using distinct token classes.
    
    Aside from CommonToken, which are using as fallbacks for text
    handling, we introduce 4 specialized classes, three of them seeing
    typical use.
    
    Since these classes use smaller fields to hold lazy state, as well
    as eliminate typical invariants, we end up saving around 39MiB (12%)
    of AST size in a typical case from the field.
    
    JIRA: YANGTOOLS-1128
    Change-Id: I600ae6cbe755212e2d1a15ae3e6b369ed0b3b962
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit f65682b440f3a2767a7852c9b1b3f0d0ea25f95d)
    (cherry picked from commit e66e724b7eece352212ddb274d417ffc1c9b49f0)

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/antlr/Token44444.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.antlr;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.TokenSource;
+import org.antlr.v4.runtime.misc.Pair;
+
+/**
+ * Large general token implementation. This cuts {@code channel}, {@code text} fields completely, as we typically
+ * we do not use them. All other fields are retained..
+ *
+ * <p>
+ * This class ends up costing 40/56/40/48 bytes instead of 48/64/48/48 bytes, a saving of 12-33%, while still being
+ * applicable in all situations.
+ */
+final class Token44444 extends AbstractSourceToken {
+    private final int type;
+    private final int line;
+    private final int charPositionInLine;
+    private final int startIndex;
+    private final int stopIndex;
+
+    Token44444(final Pair<TokenSource, CharStream> source, final int type, final int line, final int charPositionInLine,
+            final int startIndex, final int stopIndex) {
+        super(source);
+        this.type = type;
+        this.line = line;
+        this.charPositionInLine = charPositionInLine;
+        this.startIndex = startIndex;
+        this.stopIndex = stopIndex;
+    }
+
+    @Override
+    public int getType() {
+        return type;
+    }
+
+    @Override
+    public int getLine() {
+        return line;
+    }
+
+    @Override
+    public int getCharPositionInLine() {
+        return charPositionInLine;
+    }
+
+    @Override
+    public int getStartIndex() {
+        return startIndex;
+    }
+
+    @Override
+    public int getStopIndex() {
+        return stopIndex;
+    }
+}
