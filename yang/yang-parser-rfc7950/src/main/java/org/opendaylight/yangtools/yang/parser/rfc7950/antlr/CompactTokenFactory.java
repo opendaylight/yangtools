90e19ffab9b5ada9e007bd60769f853e91d63d38
Robert Varga <robert.varga@pantheon.tech>
2020-08-10 14:07:00 +0200

    WIP: add specialized token factory
    Our baseline parsing footprint is rather large. We can improve it
    by looking at token component sizes and using distinct token classes.
    
    Aside from common tokens, which are using as fallbacks for text
    handling, we introduce 4 specialized classes, three of them seeing
    typical use.
    
    Since these classes use smaller fields to hold lazy state, as well
    as eliminate typical invariants, we end up saving around 39MiB (12%)
    in a memory-hungry use case.
    
    // FIXME: evaluate performance impact of this change
    
    JIRA: YANGTOOLS-1128
    Change-Id: I600ae6cbe755212e2d1a15ae3e6b369ed0b3b962
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/antlr/CompactTokenFactory.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.antlr;
+
+import com.google.common.annotations.Beta;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenFactory;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.TokenFactory;
+import org.antlr.v4.runtime.TokenSource;
+import org.antlr.v4.runtime.misc.Pair;
+import org.eclipse.jdt.annotation.NonNull;
+
+@Beta
+public final class CompactTokenFactory implements TokenFactory<Token> {
+    public static final @NonNull CompactTokenFactory INSTANCE = new CompactTokenFactory();
+
+    private CompactTokenFactory() {
+        // Hidden on purpose
+    }
+
+    @Override
+    public Token create(final Pair<TokenSource, CharStream> source, final int type, final String text,
+            final int channel, final int start, final int stop, final int line, final int charPositionInLine) {
+        if (channel != Token.DEFAULT_CHANNEL || text != null) {
+            // Non-default channel or text present, defer to common token factory
+            return CommonTokenFactory.DEFAULT.create(source, type, text, channel, start, stop, line,
+                charPositionInLine);
+        }
+
+        // Can we fit token type into a single byte? This should always be true
+        if (type >= Byte.MIN_VALUE && type <= Byte.MAX_VALUE) {
+            // Can we fit line in an unsigned short? This is usually be true
+            if (line >= 0 && line <= 65535) {
+                // Can we fit position in line into an unsigned byte? This is usually true
+                if (charPositionInLine >= 0 && charPositionInLine <= 255) {
+                    // Can we fit start/stop into an an unsigned short?
+                    if (start >= 0 && start <= 65535 && stop >= 0 && stop <= 65535) {
+                        return new Token12122(source, type, line, charPositionInLine, start, stop);
+                    }
+                    return new Token12144(source, type, line, charPositionInLine, start, stop);
+                }
+            }
+        }
+
+        // FIXME: add more specialized classes
+        return new Token44444(source, type, line, charPositionInLine, start, stop);
+    }
+
+    @Override
+    public Token create(final int type, final String text) {
+        return new ExplicitTextToken(type, text);
+    }
+}
