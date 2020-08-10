e66e724b7eece352212ddb274d417ffc1c9b49f0
Robert Varga <robert.varga@pantheon.tech>
2020-08-10 14:07:00 +0200

    Add a specialized token factory
    Our baseline parsing footprint is rather large. We can improve it
    by looking at token component sizes and using distinct token classes.
    
    Aside from CommonToken, which are using as fallbacks for text
    handling, we introduce 4 specialized classes, three of them seeing
    typical use.
    
    Since these classes use smaller fields to hold lazy state, as well
    as eliminate typical invariants, we end up saving around 39MiB (12%)
    of AST size in a typical case from the field.
    
    JIRA: YANGTOOLS-1128
    Change-Id: I600ae6cbe755212e2d1a15ae3e6b369ed0b3b962
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit f65682b440f3a2767a7852c9b1b3f0d0ea25f95d)

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/antlr/AbstractSourceToken.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.antlr;
+
+import static java.util.Objects.requireNonNull;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.TokenSource;
+import org.antlr.v4.runtime.misc.Interval;
+import org.antlr.v4.runtime.misc.Pair;
+
+abstract class AbstractSourceToken extends AbstractToken {
+    private final Pair<TokenSource, CharStream> source;
+
+    AbstractSourceToken(final Pair<TokenSource, CharStream> source) {
+        this.source = requireNonNull(source);
+    }
+
+    @Override
+    public final TokenSource getTokenSource() {
+        return source.a;
+    }
+
+    @Override
+    public final CharStream getInputStream() {
+        return source.b;
+    }
+
+    @Override
+    public final String getText() {
+        final CharStream input = getInputStream();
+        if (input == null) {
+            return null;
+        }
+
+        final int n = input.size();
+        final int start = getStartIndex();
+        final int stop = getStopIndex();
+        return start < n && stop < n ? input.getText(Interval.of(start, stop)) : "<EOF>";
+    }
+}
