0575ccbaea68384ebc2ea5d9bbdf195ccfe77c1f
Robert Varga <robert.varga@pantheon.tech>
2020-08-11 14:41:37 +0200

    Remove YangStatementStreamSource.getYangAST()
    This method has been deprecated, remove it now.
    
    Change-Id: I6e8c90388887224e2112c48136dd4e8e00428426
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/repo/YangStatementStreamSource.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/repo/YangStatementStreamSource.java
@@ -157,11 +157,6 @@ public final class YangStatementStreamSource extends AbstractIdentifiable<Source
         }.visit(context);
     }
 
-    @Deprecated(forRemoval = true)
-    public ParserRuleContext getYangAST() {
-        return statementContext();
-    }
-
     StatementContext statementContext() {
         return context;
     }
