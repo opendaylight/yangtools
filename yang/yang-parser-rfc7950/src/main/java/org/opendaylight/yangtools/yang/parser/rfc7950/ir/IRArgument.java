0659134567040e135659d8198a544893bae46972
Robert Varga <robert.varga@pantheon.tech>
2020-08-14 12:55:12 +0200

    WIP: Add IR
    Change-Id: Idf057bbb1e29280a8e378529401023fb87d18923
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/ir/IRArgument.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.ir;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import com.google.common.collect.ImmutableList;
+import java.util.Iterator;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.concepts.Immutable;
+
+@Beta
+public abstract class IRArgument implements Immutable {
+    public static final class Concatenation extends IRArgument {
+        private final @NonNull ImmutableList<IRArgument> parts;
+
+        Concatenation(final ImmutableList<IRArgument> parts) {
+            this.parts = requireNonNull(parts);
+        }
+
+        public @NonNull ImmutableList<IRArgument> parts() {
+            return parts;
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder();
+            final Iterator<IRArgument> it = parts.iterator();
+            sb.append(it.next());
+            while (it.hasNext()) {
+                sb.append(" + ").append(it.next());
+            }
+            return sb.toString();
+        }
+    }
+
+    public static final class DoubleQuoted extends AbstractSimple {
+        DoubleQuoted(final String string) {
+            super(string);
+        }
+
+        @Override
+        public String toString() {
+            return '"' + string() + '"';
+        }
+    }
+
+    public abstract static class Literal extends AbstractSimple {
+        Literal(final String string) {
+            super(string);
+        }
+    }
+
+    private abstract static class AbstractSimple extends IRArgument {
+        private final @NonNull String string;
+
+        AbstractSimple(final String string) {
+            this.string = requireNonNull(string);
+        }
+
+        public final @NonNull String string() {
+            return string;
+        }
+    }
+
+    static final class SingleQuoted extends Literal {
+        SingleQuoted(final String string) {
+            super(string);
+        }
+
+        @Override
+        public String toString() {
+            return "'" + string() + "'";
+        }
+    }
+
+    static final class Unquoted extends Literal {
+        Unquoted(final String string) {
+            super(string);
+        }
+
+        @Override
+        public String toString() {
+            return string();
+        }
+    }
+
+    IRArgument() {
+        // Hidden on purpose
+    }
+
+    @Override
+    public abstract String toString();
+}
---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/ir/StatementFactory.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.ir;
+
+import static com.google.common.base.Verify.verify;
+
+import com.google.common.base.VerifyException;
+import com.google.common.collect.ImmutableList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.parser.antlr.YangStatementParser;
+import org.opendaylight.yangtools.yang.parser.antlr.YangStatementParser.ArgumentContext;
+import org.opendaylight.yangtools.yang.parser.antlr.YangStatementParser.KeywordContext;
+import org.opendaylight.yangtools.yang.parser.antlr.YangStatementParser.QuotedStringContext;
+import org.opendaylight.yangtools.yang.parser.antlr.YangStatementParser.StatementContext;
+import org.opendaylight.yangtools.yang.parser.antlr.YangStatementParser.UnquotedStringContext;
+import org.opendaylight.yangtools.yang.parser.rfc7950.ir.IRArgument.Concatenation;
+import org.opendaylight.yangtools.yang.parser.rfc7950.ir.IRArgument.DoubleQuoted;
+import org.opendaylight.yangtools.yang.parser.rfc7950.ir.IRArgument.SingleQuoted;
+import org.opendaylight.yangtools.yang.parser.rfc7950.ir.IRArgument.Unquoted;
+import org.opendaylight.yangtools.yang.parser.rfc7950.ir.IRKeyword.Qualified;
+import org.opendaylight.yangtools.yang.parser.rfc7950.ir.IRKeyword.Unqualified;
+
+final class StatementFactory {
+    private final Map<String, DoubleQuoted> dquotArguments = new HashMap<>();
+    private final Map<String, SingleQuoted> squotArguments = new HashMap<>();
+    private final Map<String, Unquoted> uquotArguments = new HashMap<>();
+    private final Map<String, IRKeyword> keywords = new HashMap<>();
+    private final Map<String, String> strings = new HashMap<>();
+
+    @NonNull IRStatement createStatement(final StatementContext stmt) {
+        final ParseTree firstChild = stmt.getChild(0);
+        verify(firstChild instanceof KeywordContext, "Unexpected shape of %s", stmt);
+
+        final ParseTree keywordStart = firstChild.getChild(0);
+        verify(keywordStart instanceof TerminalNode, "Unexpected keyword start %s", keywordStart);
+        final Token keywordToken = ((TerminalNode) keywordStart).getSymbol();
+
+        final IRKeyword keyword;
+        switch (firstChild.getChildCount()) {
+            case 1:
+                keyword = keywords.computeIfAbsent(strOf(keywordToken), Unqualified::new);
+                break;
+            case 3:
+                keyword = new Qualified(strOf(keywordToken), strOf(firstChild.getChild(2)));
+                break;
+            default:
+                throw new VerifyException("Unexpected keyword " + firstChild);
+        }
+
+        final IRArgument argument = createArgument(stmt);
+        final ImmutableList<IRStatement> statements = createStatements(stmt);
+        final int line = keywordToken.getLine();
+        final int column = keywordToken.getCharPositionInLine();
+
+        switch (statements.size()) {
+            case 0:
+                if (fits22(line, column)) {
+                    return new IRStatement022(keyword, argument, line, column);
+                }
+                return fits31(line, column) ? new IRStatement031(keyword, argument, line, column)
+                        : new IRStatement044(keyword, argument, line, column);
+            case 1:
+                return new IRStatement144(keyword, argument, statements.get(0), line, column);
+            default:
+                return new IRStatementL44(keyword, argument, statements, line, column);
+        }
+    }
+
+    private IRArgument createArgument(final StatementContext stmt) {
+        final ArgumentContext argCtx = stmt.argument();
+        if (argCtx == null) {
+            return null;
+        }
+        if (argCtx.getChildCount() == 1) {
+            final ParseTree child = argCtx.getChild(0);
+            if (child instanceof UnquotedStringContext) {
+                return uquotArguments.computeIfAbsent(strOf(child), Unquoted::new);
+            }
+
+            verify(child instanceof QuotedStringContext, "Unexpected child %s", child);
+            return createArgument((QuotedStringContext) child);
+        }
+
+        return new Concatenation(argCtx.quotedString().stream().map(this::createArgument)
+            .collect(ImmutableList.toImmutableList()));
+    }
+
+    private IRArgument createArgument(final QuotedStringContext argument) {
+        final ParseTree literal = argument.getChild(1);
+        verify(literal instanceof TerminalNode, "Unexpected literal %s", literal);
+        final Token token = ((TerminalNode) literal).getSymbol();
+        switch (token.getType()) {
+            case YangStatementParser.DQUOT_END:
+                return dquotArguments.computeIfAbsent("", DoubleQuoted::new);
+            case YangStatementParser.DQUOT_STRING:
+                return dquotArguments.computeIfAbsent(strOf(token), DoubleQuoted::new);
+            case YangStatementParser.SQUOT_END:
+                return squotArguments.computeIfAbsent("", SingleQuoted::new);
+            case YangStatementParser.SQUOT_STRING:
+                return squotArguments.computeIfAbsent(strOf(token), SingleQuoted::new);
+            default:
+                throw new VerifyException("Unexpected token " + token);
+        }
+    }
+
+    private ImmutableList<IRStatement> createStatements(final StatementContext stmt) {
+        final List<StatementContext> statements = stmt.statement();
+        return statements.isEmpty() ? ImmutableList.of()
+                : statements.stream().map(this::createStatement).collect(ImmutableList.toImmutableList());
+    }
+
+    private String strOf(final ParseTree tree) {
+        return strings.computeIfAbsent(tree.getText(), Function.identity());
+    }
+
+    private String strOf(final Token token) {
+        return strings.computeIfAbsent(token.getText(), Function.identity());
+    }
+
+    private static boolean fits22(final int line, final int column) {
+        return line >= 0 && line <= 65535 && column >= 0 && column <= 65535;
+    }
+
+    private static boolean fits31(final int line, final int column) {
+        return line >= 0 && line <= 16777215 && column >= 0 && column <= 255;
+    }
+}
