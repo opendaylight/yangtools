971072cbea01bd4869f06c6230dab245e53830a4
Robert Varga <robert.varga@pantheon.tech>
2020-08-14 12:55:12 +0200

    Add an explicit intermediate YANG representation
    ANTLR classes are geared towards parsing and raw speed, without much
    regards to memory usage. Most notably using ParseTree as our AST ends
    up retaining separators and various metadata which we do not really
    need.
    
    Add a simplistic intermediate representation for our AST, which where
    we pre-process ANTLR trees and de-duplicate strings. This leads to
    elimination of 90% of objects, with corresponding reduction of memory
    footprint.
    
    JIRA: YANGTOOLS-1130
    Change-Id: Idf057bbb1e29280a8e378529401023fb87d18923
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/ir/AbstractIRObject.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.ir;
+
+import com.google.common.base.MoreObjects;
+import org.opendaylight.yangtools.concepts.Immutable;
+
+abstract class AbstractIRObject implements Immutable {
+    @Override
+    public final String toString() {
+        return MoreObjects.toStringHelper(this).add("fragment", toYangFragment(new StringBuilder())).toString();
+    }
+
+    abstract StringBuilder toYangFragment(StringBuilder sb);
+}
