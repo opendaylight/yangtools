4756cc2bc249a7abdb5f8394a7734e49519b4836
Robert Varga <robert.varga@pantheon.tech>
2020-08-14 12:55:12 +0200

    Add an explicit intermediate YANG representation
    ANTLR classes are geared towards parsing and raw speed, without much
    regards to memory usage. Most notably using ParseTree as our AST ends
    up retaining separators and various metadata which we do not really
    need.
    
    Add a simplistic intermediate representation for our AST, which where
    we pre-process ANTLR trees and de-duplicate strings. This leads to
    elimination of 90% of objects, with corresponding reduction of memory
    footprint.
    
    JIRA: YANGTOOLS-1130
    Change-Id: Idf057bbb1e29280a8e378529401023fb87d18923
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/ir/package-info.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Intermediate representation of a YANG file. This is an Abstract Syntax Tree equivalent to ParseTree we get from
+ * ANTLR, except it is immutable and has a denser in-memory representation due to it not containing any metadata which
+ * is not required for the purposes of statement inference.
+ *
+ * <p>
+ * The main entrypoint into this package is {@link org.opendaylight.yangtools.yang.parser.rfc7950.ir.IRStatement}, which
+ * represents a single YANG statement. Every YANG file is required to contain exactly one top-level statement,
+ * {@code module} or {@code submodule}, hence an IRStatement also represents the significant contents of a YANG file.
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.ir;
