2f00d5a8baffd14832546ebafad3034e2ab87b32
Robert Varga <robert.varga@pantheon.tech>
2020-08-24 18:36:00 +0200

    Add YANG IR I/O support
    Persisting the intermediate representation and restoring it is much
    faster and memory-efficient than parsing YANG files from scratch.
    
    This patch adds the smarts to persist/restore an IRStatement to/from
    an arbitrary DataOutput/DataInput.
    
    We also complete AbstractIRObject definitions by adding appropriate
    hashCode()/equals() methods.
    
    JIRA: YANGTOOLS-1461
    Change-Id: Iff10b91c8439d3323ae8c263480f4dfd6c1f1c00
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/parser/rfc7950/antlr/IOSupportTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.antlr;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.opendaylight.yangtools.yang.ir.IOSupport;
+import org.opendaylight.yangtools.yang.ir.IRStatement;
+import org.opendaylight.yangtools.yang.model.repo.api.YangIRSchemaSource;
+import org.opendaylight.yangtools.yang.parser.rfc7950.repo.TextToIRTransformer;
+import org.opendaylight.yangtools.yang.stmt.TestUtils;
+
+class IOSupportTest {
+    private static YangIRSchemaSource FOO;
+
+    @BeforeAll
+    static void beforeClass() throws Exception {
+        FOO = TextToIRTransformer.transformText(TestUtils.assertSchemaSource("/bugs/YT1089/foo.yang"));
+    }
+
+    @Test
+    void testSerializedSize() throws IOException {
+        final byte[] bytes = serialize(FOO.getRootStatement());
+        assertEquals(485, bytes.length);
+    }
+
+    @Test
+    void testSerdes() throws IOException {
+        final var orig = FOO.getRootStatement();
+        final var copy = deserialize(serialize(orig));
+        assertEquals(orig, copy);
+    }
+
+    private static byte[] serialize(final IRStatement stmt) throws IOException {
+        final var baos = new ByteArrayOutputStream();
+
+        try (var dos = new DataOutputStream(baos)) {
+            IOSupport.writeStatement(dos, stmt);
+        }
+
+        return baos.toByteArray();
+    }
+
+    private static IRStatement deserialize(final byte[] bytes) throws IOException {
+        try (var dis = new DataInputStream(new ByteArrayInputStream(bytes))) {
+            final var stmt = IOSupport.readStatement(dis);
+            assertEquals(0, dis.available());
+            return stmt;
+        }
+    }
+}
