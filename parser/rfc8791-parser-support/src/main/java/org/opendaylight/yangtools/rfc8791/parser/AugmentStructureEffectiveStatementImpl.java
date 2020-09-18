70a8f7728f7cf8f11b40fb7d368303aa12bb76f3
Robert Varga <robert.varga@pantheon.tech>
2020-09-19 00:49:35 +0200

    WIP: Add parser support for RFC8791 statements
    // FIXME: finish augment-structure inference
    
    JIRA: YANGTOOLS-1113
    Change-Id: Ib0a33a14dbd4af05c58184943b86bf19a9dcc833
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

---
+++ parser/rfc8791-parser-support/src/main/java/org/opendaylight/yangtools/rfc8791/parser/AugmentStructureEffectiveStatementImpl.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8791.parser;
+
+import com.google.common.collect.ImmutableList;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.rfc8791.model.api.AugmentStructureArgument;
+import org.opendaylight.yangtools.rfc8791.model.api.AugmentStructureEffectiveStatement;
+import org.opendaylight.yangtools.rfc8791.model.api.AugmentStructureStatement;
+import org.opendaylight.yangtools.yang.model.api.DataNodeContainer;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.spi.meta.AbstractDeclaredEffectiveStatement.DefaultWithSchemaTree;
+import org.opendaylight.yangtools.yang.parser.spi.meta.EffectiveStmtCtx;
+
+final class AugmentStructureEffectiveStatementImpl
+        extends DefaultWithSchemaTree<AugmentStructureArgument, @NonNull AugmentStructureStatement>
+        implements AugmentStructureEffectiveStatement {
+    private final @NonNull AugmentStructureArgument argument;
+
+    AugmentStructureEffectiveStatementImpl(
+            final EffectiveStmtCtx.Current<AugmentStructureArgument, AugmentStructureStatement> stmt,
+            final ImmutableList<? extends EffectiveStatement<?,?>> substatements) {
+        super(stmt.declared(), substatements);
+        argument = stmt.getArgument();
+    }
+
+    @Override
+    public AugmentStructureArgument argument() {
+        return argument;
+    }
+
+    @Override
+    public @NonNull DataNodeContainer toDataNodeContainer() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
