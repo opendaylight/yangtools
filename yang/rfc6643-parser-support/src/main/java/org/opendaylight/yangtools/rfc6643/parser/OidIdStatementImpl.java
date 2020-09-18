a4a486e43bf3a755cf7462322758c8bf847bb29d
Robert Varga <robert.varga@pantheon.tech>
2020-09-18 17:54:47 +0200

    Add RFC6643 parser support
    RFC6643 defines a way to map SMIv2 to YANG. This mapping retains
    the original SMI details using YANG extensions, so that the resulting
    model can be mapped back.
    
    This patch adds semantic support for these extensions, so that the
    semantics are supported out of the box.
    
    JIRA: YANGTOOLS-1134
    Change-Id: I494b2537e89e5a104debe486a9012d22d5c7b3aa
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 4223bf8c4eae4a003b71554fbcc98b5377ddce2b)

---
+++ yang/rfc6643-parser-support/src/main/java/org/opendaylight/yangtools/rfc6643/parser/OidIdStatementImpl.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016, 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc6643.parser;
+
+import static com.google.common.base.Verify.verifyNotNull;
+
+import org.opendaylight.yangtools.rfc6643.model.api.ObjectIdentifier;
+import org.opendaylight.yangtools.rfc6643.model.api.OidStatement;
+import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractDeclaredStatement;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+
+final class OidIdStatementImpl extends AbstractDeclaredStatement<ObjectIdentifier> implements OidStatement {
+    OidIdStatementImpl(final StmtContext<ObjectIdentifier, OidStatement, ?> context) {
+        super(context);
+    }
+
+    @Override
+    public ObjectIdentifier getOid() {
+        return verifyNotNull(argument());
+    }
+
+    @Override
+    public ObjectIdentifier getArgument() {
+        return null;
+    }
+}
