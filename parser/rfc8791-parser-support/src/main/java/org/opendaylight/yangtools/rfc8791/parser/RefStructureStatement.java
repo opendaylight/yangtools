7f4ea34d650ea7007ab3093ddadc12d0bdb83b35
Robert Varga <robert.varga@pantheon.tech>
2020-09-19 00:49:35 +0200

    WIP: Add parser support for RFC8791 statements
    // FIXME: finish augment-structure inference
    
    JIRA: YANGTOOLS-1113
    Change-Id: Ib0a33a14dbd4af05c58184943b86bf19a9dcc833
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

---
+++ parser/rfc8791-parser-support/src/main/java/org/opendaylight/yangtools/rfc8791/parser/RefStructureStatement.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8791.parser;
+
+import org.opendaylight.yangtools.rfc8791.model.api.StructureStatement;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclarationReference;
+import org.opendaylight.yangtools.yang.model.spi.meta.AbstractRefStatement;
+
+final class RefStructureStatement extends AbstractRefStatement<QName, StructureStatement>
+        implements StructureStatement {
+    RefStructureStatement(final StructureStatement delegate, final DeclarationReference ref) {
+        super(delegate, ref);
+    }
+}
