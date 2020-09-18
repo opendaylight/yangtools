d6a62a354f07b0f142093a92134e30f2565c39da
Robert Varga <robert.varga@pantheon.tech>
2020-09-19 00:06:18 +0200

    Define MaxAccess enumeration
    RFC2578 specifies which strings are actually valid as max-access
    arguments. Make sure we properly validate them and use an enum.
    
    JIRA: YANGTOOLS-1134
    Change-Id: I9e22dccb115f34ab50aaa8cf84e09d12713c69fa
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/rfc6643-parser-support/src/main/java/org/opendaylight/yangtools/rfc6643/parser/MaxAccessStatementImpl.java
+++ yang/rfc6643-parser-support/src/main/java/org/opendaylight/yangtools/rfc6643/parser/MaxAccessStatementImpl.java
@@ -7,14 +7,38 @@
  */
 package org.opendaylight.yangtools.rfc6643.parser;
 
+import static java.util.Objects.requireNonNull;
+
 import com.google.common.collect.ImmutableList;
+import java.util.Collection;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.rfc6643.model.api.MaxAccess;
 import org.opendaylight.yangtools.rfc6643.model.api.MaxAccessStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
-import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractDeclaredStatement.WithRawStringArgument.WithSubstatements;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractDeclaredStatement;
 
-final class MaxAccessStatementImpl extends WithSubstatements implements MaxAccessStatement {
-    MaxAccessStatementImpl(final String rawArgument,
+final class MaxAccessStatementImpl extends AbstractDeclaredStatement<MaxAccess> implements MaxAccessStatement {
+    private final @NonNull Object substatements;
+    private final MaxAccess argument;
+
+    MaxAccessStatementImpl(final MaxAccess argument,
             final ImmutableList<? extends DeclaredStatement<?>> substatements) {
-        super(rawArgument, substatements);
+        this.argument = requireNonNull(argument);
+        this.substatements = maskList(substatements);
+    }
+
+    @Override
+    public MaxAccess argument() {
+        return argument;
+    }
+
+    @Override
+    public String rawArgument() {
+        return argument.stringLiteral();
+    }
+
+    @Override
+    public Collection<? extends DeclaredStatement<?>> declaredSubstatements() {
+        return unmaskList(substatements);
     }
 }
