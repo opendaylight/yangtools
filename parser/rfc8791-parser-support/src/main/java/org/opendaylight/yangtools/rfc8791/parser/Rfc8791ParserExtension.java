8616a50a09231b2057440aef478d541f73107904
Robert Varga <robert.varga@pantheon.tech>
2020-09-19 00:49:35 +0200

    WIP: Add parser support for RFC8791 statements
    // FIXME: finish augment-structure inference
    
    JIRA: YANGTOOLS-1113
    Change-Id: Ib0a33a14dbd4af05c58184943b86bf19a9dcc833
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

---
+++ parser/rfc8791-parser-support/src/main/java/org/opendaylight/yangtools/rfc8791/parser/Rfc8791ParserExtension.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8791.parser;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.kohsuke.MetaInfServices;
+import org.opendaylight.yangtools.rfc8791.model.api.AugmentStructureStatement;
+import org.opendaylight.yangtools.rfc8791.model.api.StructureStatement;
+import org.opendaylight.yangtools.yang.parser.api.YangParserConfiguration;
+import org.opendaylight.yangtools.yang.parser.spi.AbstractParserExtension;
+import org.opendaylight.yangtools.yang.parser.spi.ParserExtension;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StatementSupportBundle;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Parser support for {@code ietf-yang-structure-ext.yang}.
+ *
+ * @since 14.0.21
+ */
+@NonNullByDefault
+@MetaInfServices(ParserExtension.class)
+@Component(service = ParserExtension.class)
+public final class Rfc8791ParserExtension extends AbstractParserExtension {
+    /**
+     * Default constructor.
+     */
+    public Rfc8791ParserExtension() {
+        super(AugmentStructureStatement.DEF, StructureStatement.DEF);
+    }
+
+    @Override
+    public StatementSupportBundle configureBundle(final YangParserConfiguration config) {
+        return StatementSupportBundle.builder(StatementSupportBundle.VERSIONS_RFC7950)
+            .addSupport(StructureStatementSupport.BEHAVIOUR)
+            .addSupport(StructureStatementSupport.rfc6020(config), StructureStatementSupport.rfc7950(config))
+            .addSupport(AugmentStructureStatementSupport.rfc6020(config),
+                AugmentStructureStatementSupport.rfc7950(config))
+            .build();
+    }
+}
---
+++ parser/rfc8791-parser-support/src/main/java/org/opendaylight/yangtools/rfc8791/parser/StructureStatementSupport.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8791.parser;
+
+import com.google.common.collect.ImmutableList;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.rfc8791.model.api.StructureEffectiveStatement;
+import org.opendaylight.yangtools.rfc8791.model.api.StructureStatement;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.Status;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclarationReference;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.AnydataStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.AnyxmlStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ChoiceStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ContainerStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.DescriptionStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.GroupingStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.LeafListStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.LeafStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ListStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.MustStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ReferenceStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.StatusEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.StatusStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.TypedefStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.UsesStatement;
+import org.opendaylight.yangtools.yang.model.spi.meta.EffectiveStatementMixins;
+import org.opendaylight.yangtools.yang.parser.api.YangParserConfiguration;
+import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractQNameStatementSupport;
+import org.opendaylight.yangtools.yang.parser.spi.meta.BoundStmtCtx;
+import org.opendaylight.yangtools.yang.parser.spi.meta.EffectiveStmtCtx.Current;
+import org.opendaylight.yangtools.yang.parser.spi.meta.NamespaceBehaviour;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ParserNamespace;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext.Mutable;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContextUtils;
+import org.opendaylight.yangtools.yang.parser.spi.meta.SubstatementValidator;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+
+public final class StructureStatementSupport
+        extends AbstractQNameStatementSupport<StructureStatement, StructureEffectiveStatement> {
+    static final ParserNamespace.@NonNull Writable<QName,
+        StmtContext<QName, StructureStatement, StructureEffectiveStatement>> NAMESPACE =
+            ParserNamespace.writable("structure");
+
+    static final @NonNull NamespaceBehaviour<QName,
+        StmtContext<QName, StructureStatement, StructureEffectiveStatement>> BEHAVIOUR =
+            NamespaceBehaviour.global(NAMESPACE);
+
+    private StructureStatementSupport(final YangParserConfiguration config, final SubstatementValidator validator) {
+        super(StructureStatement.DEF, StatementPolicy.reject(), config, validator);
+    }
+
+    static StructureStatementSupport rfc6020(final YangParserConfiguration config) {
+        return new StructureStatementSupport(config,
+            SubstatementValidator.builder(StructureStatement.DEF)
+                .addAny(MustStatement.DEF)
+                .addOptional(StatusStatement.DEF)
+                .addOptional(DescriptionStatement.DEF)
+                .addOptional(ReferenceStatement.DEF)
+                .addAny(TypedefStatement.DEF)
+                .addAny(GroupingStatement.DEF)
+                .addAny(ContainerStatement.DEF)
+                .addAny(LeafStatement.DEF)
+                .addAny(LeafListStatement.DEF)
+                .addAny(ListStatement.DEF)
+                .addAny(ChoiceStatement.DEF)
+                .addAny(AnyxmlStatement.DEF)
+                .addAny(UsesStatement.DEF)
+                .build());
+    }
+
+    static StructureStatementSupport rfc7950(final YangParserConfiguration config) {
+        return new StructureStatementSupport(config,
+            SubstatementValidator.builder(StructureStatement.DEF)
+                .addAny(MustStatement.DEF)
+                .addOptional(StatusStatement.DEF)
+                .addOptional(DescriptionStatement.DEF)
+                .addOptional(ReferenceStatement.DEF)
+                .addAny(TypedefStatement.DEF)
+                .addAny(GroupingStatement.DEF)
+                .addAny(ContainerStatement.DEF)
+                .addAny(LeafStatement.DEF)
+                .addAny(LeafListStatement.DEF)
+                .addAny(ListStatement.DEF)
+                .addAny(ChoiceStatement.DEF)
+                .addAny(AnydataStatement.DEF)
+                .addAny(AnyxmlStatement.DEF)
+                .addAny(UsesStatement.DEF)
+                .build());
+    }
+
+    @Override
+    public QName parseArgumentValue(final StmtContext<?, ?, ?> ctx, final String value) {
+        return StmtContextUtils.parseIdentifier(ctx, value);
+    }
+
+    @Override
+    protected StructureStatement createDeclared(final BoundStmtCtx<QName> ctx,
+            final ImmutableList<DeclaredStatement<?>> substatements) {
+        return new StructureStatementImpl(ctx.getArgument(), substatements);
+    }
+
+    @Override
+    protected StructureStatement attachDeclarationReference(final StructureStatement stmt,
+            final DeclarationReference reference) {
+        return new RefStructureStatement(stmt, reference);
+    }
+
+    @Override
+    public boolean isIgnoringConfig() {
+        return true;
+    }
+
+    @Override
+    public void onStatementAdded(final Mutable<QName, StructureStatement, StructureEffectiveStatement> stmt) {
+        final var parent = stmt.coerceParentContext();
+        if (parent.getParentContext() != null) {
+            throw new SourceException(stmt, "Structure may only be used as top-level statement");
+        }
+
+        final var name = stmt.getArgument();
+        final var prev = parent.namespaceItem(NAMESPACE, name);
+        if (prev != null) {
+            throw new SourceException(stmt,
+                "Error in module '%s': cannot add '%s'. Node name collision: '%s' already declared at %s",
+                stmt.getRoot().rawArgument(), name, prev.argument(), prev.sourceReference());
+        }
+        parent.addToNs(NAMESPACE, name, stmt);
+    }
+
+    @Override
+    protected StructureEffectiveStatement createEffective(final Current<QName, StructureStatement> stmt,
+            final ImmutableList<? extends EffectiveStatement<?, ?>> substatements) {
+        final int flags = new EffectiveStatementMixins.EffectiveStatementWithFlags.FlagsBuilder()
+            .setHistory(stmt.history())
+            .setStatus(findFirstArgument(substatements, StatusEffectiveStatement.class, Status.CURRENT))
+            .setConfiguration(stmt.effectiveConfig().asNullable())
+            .toFlags();
+        return new StructureEffectiveStatementImpl(stmt, substatements, flags);
+    }
+}
