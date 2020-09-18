2f32f8c26e7963e02060bc4e847d8f4953bba411
Robert Varga <robert.varga@pantheon.tech>
2020-09-19 00:49:35 +0200

    WIP: Add parser support for RFC8791 statements
    // FIXME: figure out ArgumentUtils packaging
    // FIXME: finish augment-structure inference
    
    JIRA: YANGTOOLS-1113
    Change-Id: Ib0a33a14dbd4af05c58184943b86bf19a9dcc833
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

---
+++ parser/rfc8791-parser-support/src/main/java/org/opendaylight/yangtools/rfc8791/parser/dagger/package-info.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Dagger modules for supporting extensions introduced in
+ * <a href="https://www.rfc-editor.org/rfc/rfc8791">RFC8791</a>. There is a single module, {@link Rfc8791Module}, which
+ * provides a static factory method returning the appropriate
+ * {@link org.opendaylight.yangtools.yang.parser.spi.ParserExtension}. This method can be used to build bindings to
+ * other injection frameworks, like Guice or Spring Framework.
+ *
+ * <p>This package is not exposed to OSGi runtime.
+ *
+ * @since 14.0.21
+ */
+package org.opendaylight.yangtools.rfc8791.parser.dagger;
