ceca893c01a7e00a586ea8192e8121be9c0db148
Robert Varga <robert.varga@pantheon.tech>
2020-09-19 00:06:18 +0200

    Define MaxAccess enumeration
    RFC2578 specifies which strings are actually valid as max-access
    arguments. Make sure we properly validate them and use an enum.
    
    JIRA: YANGTOOLS-1134
    Change-Id: I9e22dccb115f34ab50aaa8cf84e09d12713c69fa
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/rfc6643-parser-support/src/main/java/org/opendaylight/yangtools/rfc6643/parser/MaxAccessStatementSupport.java
+++ yang/rfc6643-parser-support/src/main/java/org/opendaylight/yangtools/rfc6643/parser/MaxAccessStatementSupport.java
@@ -10,17 +10,19 @@ package org.opendaylight.yangtools.rfc6643.parser;
 import com.google.common.annotations.Beta;
 import com.google.common.collect.ImmutableList;
 import org.opendaylight.yangtools.rfc6643.model.api.IetfYangSmiv2ExtensionsMapping;
+import org.opendaylight.yangtools.rfc6643.model.api.MaxAccess;
 import org.opendaylight.yangtools.rfc6643.model.api.MaxAccessEffectiveStatement;
 import org.opendaylight.yangtools.rfc6643.model.api.MaxAccessStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
-import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.BaseStringStatementSupport;
+import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.BaseStatementSupport;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 import org.opendaylight.yangtools.yang.parser.spi.meta.SubstatementValidator;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
 
 @Beta
 public final class MaxAccessStatementSupport
-        extends BaseStringStatementSupport<MaxAccessStatement, MaxAccessEffectiveStatement> {
+        extends BaseStatementSupport<MaxAccess, MaxAccessStatement, MaxAccessEffectiveStatement> {
     private static final SubstatementValidator SUBSTATEMENT_VALIDATOR =
             SubstatementValidator.builder(IetfYangSmiv2ExtensionsMapping.MAX_ACCESS).build();
     private static final MaxAccessStatementSupport INSTANCE = new MaxAccessStatementSupport();
@@ -34,31 +36,40 @@ public final class MaxAccessStatementSupport
     }
 
     @Override
+    public MaxAccess parseArgumentValue(final StmtContext<?, ?, ?> ctx, final String value) {
+        try {
+            return MaxAccess.forStringLiteral(value);
+        } catch (IllegalArgumentException e) {
+            throw new SourceException(ctx.getStatementSourceReference(), e, "Invalid max-access value '%s'", value);
+        }
+    }
+
+    @Override
     protected SubstatementValidator getSubstatementValidator() {
         return SUBSTATEMENT_VALIDATOR;
     }
 
     @Override
-    protected MaxAccessStatement createDeclared(final StmtContext<String, MaxAccessStatement, ?> ctx,
+    protected MaxAccessStatement createDeclared(final StmtContext<MaxAccess, MaxAccessStatement, ?> ctx,
             final ImmutableList<? extends DeclaredStatement<?>> substatements) {
-        return new MaxAccessStatementImpl(ctx.coerceRawStatementArgument(), substatements);
+        return new MaxAccessStatementImpl(ctx.coerceStatementArgument(), substatements);
     }
 
     @Override
-    protected MaxAccessStatement createEmptyDeclared(final StmtContext<String, MaxAccessStatement, ?> ctx) {
+    protected MaxAccessStatement createEmptyDeclared(final StmtContext<MaxAccess, MaxAccessStatement, ?> ctx) {
         return createDeclared(ctx, ImmutableList.of());
     }
 
     @Override
     protected MaxAccessEffectiveStatement createEffective(
-            final StmtContext<String, MaxAccessStatement, MaxAccessEffectiveStatement> ctx,
+            final StmtContext<MaxAccess, MaxAccessStatement, MaxAccessEffectiveStatement> ctx,
             final MaxAccessStatement declared, final ImmutableList<? extends EffectiveStatement<?, ?>> substatements) {
         return new MaxAccessEffectiveStatementImpl(ctx, substatements);
     }
 
     @Override
     protected MaxAccessEffectiveStatement createEmptyEffective(
-            final StmtContext<String, MaxAccessStatement, MaxAccessEffectiveStatement> ctx,
+            final StmtContext<MaxAccess, MaxAccessStatement, MaxAccessEffectiveStatement> ctx,
             final MaxAccessStatement declared) {
         return createEffective(ctx, declared, ImmutableList.of());
     }
