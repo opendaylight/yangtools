6b8758cce6cbdf94628c358238eeb7f584e331dc
Robert Varga <robert.varga@pantheon.tech>
2020-09-19 00:49:35 +0200

    WIP: Add parser support for RFC8791 statements
    // FIXME: augmentation resolving not working
    
    JIRA: YANGTOOLS-1113
    Change-Id: Ib0a33a14dbd4af05c58184943b86bf19a9dcc833
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

---
+++ parser/rfc8791-parser-support/src/main/java/org/opendaylight/yangtools/rfc8791/parser/AugmentStructureStatementImpl.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8791.parser;
+
+import com.google.common.collect.ImmutableList;
+import org.opendaylight.yangtools.rfc8791.model.api.AugmentStructureArgument;
+import org.opendaylight.yangtools.rfc8791.model.api.AugmentStructureStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.spi.meta.AbstractDeclaredStatement.WithArgument.WithSubstatements;
+import org.opendaylight.yangtools.yang.parser.spi.meta.BoundStmtCtx;
+
+final class AugmentStructureStatementImpl extends WithSubstatements<AugmentStructureArgument>
+        implements AugmentStructureStatement {
+    AugmentStructureStatementImpl(final BoundStmtCtx<AugmentStructureArgument> ctx,
+            final ImmutableList<? extends DeclaredStatement<?>> substatements) {
+        super(ctx.getRawArgument(), ctx.getArgument(), substatements);
+    }
+}
