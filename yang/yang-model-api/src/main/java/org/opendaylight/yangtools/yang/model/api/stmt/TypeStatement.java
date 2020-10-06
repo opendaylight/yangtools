dc08c3034b234d295a56fe2b8798d19c6fe7d644
Robert Varga <robert.varga@pantheon.tech>
2020-10-06 07:26:31 +0200

    Fix NumericalRestrictions.getRange() definition
    While the interface is not implented, we want the default method
    to be accurate.
    
    Change-Id: Iede28e13911437d10808a55be0cff348675cd7a5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/TypeStatement.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/TypeStatement.java
@@ -32,9 +32,8 @@ public interface TypeStatement extends DeclaredStatement<String> {
     @Rfc6020AbnfRule("numerical-restrictions")
     // FIXME: 6.0.0: this interface does not have an implementation
     interface NumericalRestrictions extends TypeStatement {
-        // FIXME: 6.0.0: this is not accurate, 'range' is an optional statement
-        default @NonNull RangeStatement getRange() {
-            return findFirstDeclaredSubstatement(RangeStatement.class).get();
+        default @Nullable RangeStatement getRange() {
+            return findFirstDeclaredSubstatement(RangeStatement.class).orElse(null);
         }
     }
 
