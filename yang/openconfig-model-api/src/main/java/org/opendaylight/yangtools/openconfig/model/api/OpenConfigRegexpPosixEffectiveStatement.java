afd58107afefb4028d72b5e581efdbd040cab23d
Martin Bobak <martin.bobak@pantheon.tech>
2020-10-07 08:42:54 +0200

    Add openconfig 'posix-pattern' and 'regexp-posix' extensions statements
    JIRA: https://jira.opendaylight.org/browse/YANGTOOLS-1132
    Change-Id: I78896e898a7f5edaf7418d957bb646e70c147c0d
    Signed-off-by: Martin Bobak <martin.bobak@pantheon.tech>

---
+++ yang/openconfig-model-api/src/main/java/org/opendaylight/yangtools/openconfig/model/api/OpenConfigRegexpPosixEffectiveStatement.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.openconfig.model.api;
+
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+
+public interface OpenConfigRegexpPosixEffectiveStatement extends
+        EffectiveStatement<Void, OpenConfigRegexpPosixStatement> {
+
+}
