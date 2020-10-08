cc45b4a44eef460b657c1606d6dab8ec0fb56b25
Robert Varga <robert.varga@pantheon.tech>
2020-10-08 16:52:54 +0200

    Make yang-model-api a JPMS module
    This converts yang-model-api to an explicit module, expressing what
    it really does. This unfornately exposes the state of affairs re.
    JPMS testing and requires us to use explicit opens to get mockito
    working.
    
    Change-Id: I80de90ea903300624e64c57859dc451a59381644
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-data-api/src/main/java/module-info.java
+++ yang/yang-data-api/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -12,15 +12,9 @@ module org.opendaylight.yangtools.yang.data.api {
     exports org.opendaylight.yangtools.yang.data.api.schema.stream;
     exports org.opendaylight.yangtools.yang.data.api.schema.tree;
     exports org.opendaylight.yangtools.yang.data.api.schema.tree.spi;
-    exports org.opendaylight.yangtools.yang.data.api.schema.xpath;
 
-    // FIXME: Audit whether we get these as transitive
-    requires transitive java.xml;
-    requires transitive org.opendaylight.yangtools.concepts;
-    requires transitive org.opendaylight.yangtools.yang.common;
     requires transitive org.opendaylight.yangtools.yang.model.api;
 
-    // FIXME: Audit whether we should make these transitive
     requires org.opendaylight.yangtools.util;
 
     // Annotations
