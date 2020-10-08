ac9056013cbbe648f4520e54cd1f2478e26b25b8
Robert Varga <robert.varga@pantheon.tech>
2020-10-08 16:52:54 +0200

    Make yang-model-api a JPMS module
    This converts yang-model-api to an explicit module, expressing what
    it really does. This unfornately exposes the state of affairs re.
    JPMS testing and requires us to use explicit opens to get mockito
    working.
    
    Change-Id: I80de90ea903300624e64c57859dc451a59381644
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-model-api/src/main/java/module-info.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.yangtools.yang.model.api {
+    exports org.opendaylight.yangtools.yang.model.api;
+    exports org.opendaylight.yangtools.yang.model.api.meta;
+    exports org.opendaylight.yangtools.yang.model.api.stmt;
+    exports org.opendaylight.yangtools.yang.model.api.stmt.compat;
+    exports org.opendaylight.yangtools.yang.model.api.type;
+    exports org.opendaylight.yangtools.yang.model.repo.api;
+    exports org.opendaylight.yangtools.yang.model.repo.spi;
+
+    // FIXME: it would be nice if we could live without java.xml here
+    requires transitive java.xml;
+    requires transitive com.google.common;
+    requires transitive org.opendaylight.yangtools.yang.common;
+    requires transitive org.opendaylight.yangtools.yang.xpath.api;
+
+    requires org.opendaylight.yangtools.util;
+    requires org.slf4j;
+
+    // Annotations
+    requires static org.eclipse.jdt.annotation;
+}
