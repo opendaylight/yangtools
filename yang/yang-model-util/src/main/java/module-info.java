81e1a259f1ebfe24da3d77cb387b4ed4ce416614
Robert Varga <robert.varga@pantheon.tech>
2020-10-18 18:54:44 +0200

    Convert rfc8528-model-util to JPMS
    This is one of our core dependencies, convert it so that we can
    proceed further. Note that UTs require a block of pom.xml magic
    to work.
    
    JIRA: YANGTOOLS-934
    Change-Id: Ifdd94495717d19138a1cf2580cde1dd0e9418e10
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-model-util/src/main/java/module-info.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.yangtools.yang.model.util {
+    exports org.opendaylight.yangtools.yang.model.util;
+    exports org.opendaylight.yangtools.yang.model.util.type;
+    exports org.opendaylight.yangtools.yang.model.repo.util;
+
+    requires transitive org.opendaylight.yangtools.yang.model.api;
+    requires transitive org.opendaylight.yangtools.rfc7952.model.api;
+
+    requires org.opendaylight.yangtools.util;
+    requires org.slf4j;
+}
