3a1af86fe9e422cf1cc4eaa9075e5bbf4b9fc5ec
Robert Varga <robert.varga@pantheon.tech>
2020-10-18 18:02:31 +0200

    Convert rfc7952-data-api to JPMS
    This is extremely simple artifact, convert it to JPMS. Note that
    this also shows unneded dependency on rfc7952-model-api, which is
    removed.
    
    JIRA: YANGTOOLS-934
    Change-Id: I384b83ee85ff786821fdcf85d489e17417dc7bae
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/rfc7952-data-api/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+// 'rfc7952' in the name ends with a digit
+@SuppressWarnings("module")
+module org.opendaylight.yangtools.rfc7952.data.api {
+    exports org.opendaylight.yangtools.rfc7952.data.api;
+
+    requires transitive org.opendaylight.yangtools.yang.data.api;
+}
