e53abdae5e966d5ff2a35ca3aa9be6bf9ca404bf
Robert Varga <robert.varga@pantheon.tech>
2020-10-18 22:29:45 +0200

    Add a test reproducer
    This issue does not reproduce on any stream, add a test case just
    to prove it.
    
    JIRA: YANGTOOLS-1039
    Change-Id: Idce40f02d1311db4eef6642e0c3281da058523b1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/YT1039Test.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class YT1039Test {
+    @Test
+    public void testUsesRefineAnyxml() throws Exception {
+        assertEquals(2, StmtTestUtils.parseYangSources("/bugs/YT1039").getModuleStatements().size());
+    }
+}
