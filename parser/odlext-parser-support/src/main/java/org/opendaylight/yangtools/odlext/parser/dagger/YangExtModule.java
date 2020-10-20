bae189029ec8b27babf8dc6de7575e3fe7eadf5e
Robert Varga <robert.varga@pantheon.tech>
2020-10-20 20:52:49 +0200

    WIP: Add dagger
    This adds a quick-and-dirty prototype.
    
    The idea is: we use Dagger modules with @Provide @Singleton annotations
    and deprecate the fugly subclasses @Singletons.
    
    // FIXME: the annotation story for binding-generator and
              yang-parser-impl needs to be figured out:
              - having 'requires' transitive breaks binding-runtime-spi,
                which needs 'parser.repo', and since we attempt to wire
                those non-existing modules :(
    
    // FIXME: binding-generator
    // FIXME: yang-data-tree-ri
    // FIXME: try to wire yang-model-validator using dagger
    // FIXME: add tests
    
    Change-Id: I973d9544a008ead947b9775f6ca036f3b3bffad0
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/odlext-parser-support/src/main/java/org/opendaylight/yangtools/odlext/parser/dagger/YangExtModule.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.odlext.parser.dagger;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.multibindings.IntoSet;
+import jakarta.inject.Singleton;
+import org.opendaylight.yangtools.odlext.parser.impl.YangExtParserExtension;
+import org.opendaylight.yangtools.yang.parser.spi.ParserExtension;
+
+/**
+ * A Dagger module binding {@code yang-ext.yang} extension.
+ *
+ * @since 14.0.21
+ */
+@Module
+public interface YangExtModule {
+    @Provides
+    @Singleton
+    @IntoSet
+    static ParserExtension provideParserExtension() {
+        return new YangExtParserExtension();
+    }
+}
--- parser/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/inject/InjectYangLibResolver.java
+++ parser/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/inject/InjectYangLibResolver.java
@@ -12,14 +12,18 @@ import javax.inject.Singleton;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.yangtools.yang.parser.api.YangLibResolver;
 import org.opendaylight.yangtools.yang.parser.impl.DefaultYangLibResolver;
+import org.opendaylight.yangtools.yang.parser.ri.dagger.YangLibResolverModule;
 import org.opendaylight.yangtools.yang.xpath.api.YangXPathParserFactory;
 
 /**
  * Reference {@link YangLibResolver} implementation.
+ *
+ * @since 14.0.20
+ * @deprecated Use {@link YangLibResolverModule#provideYangLibResolver(YangXPathParserFactory)} instead.
  */
 @Singleton
 @NonNullByDefault
-@SuppressWarnings("exports")
+@Deprecated(since = "14.0.21", forRemoval = true)
 public final class InjectYangLibResolver extends DefaultYangLibResolver {
     @Inject
     public InjectYangLibResolver(final YangXPathParserFactory xpathFactory) {
