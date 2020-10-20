e461e5b38778cfdb9611bab850a491da9a05da7f
Robert Varga <robert.varga@pantheon.tech>
2020-10-20 20:52:49 +0200

    WIP: Add dagger provider to yang-xpath-impl
    Quick&dirty prototype, but seems to work okay.
    
    // FIXME: figure out the {javax/jakarta} split
    
    Change-Id: I973d9544a008ead947b9775f6ca036f3b3bffad0
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- xml/yang-xpath-impl/src/main/java/module-info.java
+++ xml/yang-xpath-impl/src/main/java/module-info.java
@@ -23,7 +23,10 @@ module org.opendaylight.yangtools.yang.xpath.impl {
     requires org.slf4j;
 
     // Annotations
+    requires static transitive dagger;
+    requires static transitive java.compiler;
     requires static transitive javax.inject;
+    requires static transitive jakarta.inject;
     requires static com.github.spotbugs.annotations;
     requires static org.eclipse.jdt.annotation;
     requires static org.kohsuke.metainf_services;
