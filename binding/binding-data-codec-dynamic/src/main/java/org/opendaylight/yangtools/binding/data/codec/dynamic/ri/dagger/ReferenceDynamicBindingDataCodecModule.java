cfb63137d86a53dd0900898ebe4cf75e30189460
Robert Varga <robert.varga@pantheon.tech>
2020-10-20 20:52:49 +0200

    WIP: Daggerize binding-data-codec-dynamic
    // FIXME: figure out service wiring
    
    JIRA: YANGTOOLS-1727
    Change-Id: I973d9544a008ead947b9775f6ca036f3b3bffad0
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/binding-data-codec-dynamic/src/main/java/org/opendaylight/yangtools/binding/data/codec/dynamic/ri/dagger/ReferenceDynamicBindingDataCodecModule.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.binding.data.codec.dynamic.ri.dagger;
+
+import dagger.Module;
+import dagger.Provides;
+import jakarta.inject.Singleton;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.binding.data.codec.dynamic.DynamicBindingDataCodec;
+import org.opendaylight.yangtools.binding.data.codec.impl.BindingCodecContext;
+import org.opendaylight.yangtools.binding.runtime.api.BindingRuntimeContext;
+
+/**
+ * A Dagger module providing reference {@link DynamicBindingDataCodec} implementation.
+ */
+@Module
+@NonNullByDefault
+@SuppressWarnings("exports")
+public interface ReferenceDynamicBindingDataCodecModule {
+    @Provides
+    @Singleton
+    static DynamicBindingDataCodec provideDynamicBindingDataCodec(final BindingRuntimeContext context) {
+        return new BindingCodecContext(context);
+    }
+}
