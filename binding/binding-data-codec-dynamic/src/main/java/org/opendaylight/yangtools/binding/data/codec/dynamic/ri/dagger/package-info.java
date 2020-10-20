4d2df5f0fa4a7a3f59c38db6b46e57a1eb67f6d7
Robert Varga <robert.varga@pantheon.tech>
2020-10-20 20:52:49 +0200

    WIP: Daggerize binding-data-codec-dynamic
    // FIXME: figure out service wiring
    
    JIRA: YANGTOOLS-1727
    Change-Id: I973d9544a008ead947b9775f6ca036f3b3bffad0
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/binding-data-codec-dynamic/src/main/java/org/opendaylight/yangtools/binding/data/codec/dynamic/ri/dagger/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Projection of the reference implementation into Dagger world.
+ *
+ * <p>This package is not exposed to OSGi runtime.
+ *
+ * @since 14.0.21
+ */
+package org.opendaylight.yangtools.binding.data.codec.dynamic.ri.dagger;
