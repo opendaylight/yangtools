de4bd0eb8cc0f198ac800268fa462248f40acceb
Robert Varga <robert.varga@pantheon.tech>
2020-10-20 20:52:49 +0200

    WIP: Enable Dagger profile
    This adds a quick-and-dirty prototype.
    
    The idea is: we use Dagger modules with @Provide @Singleton annotations
    and deprecate the fugly subclasses @Singletons.
    
    // FIXME: move parser-impl implementations and module
    // FIXME: the annotation story for binding-generator and
              yang-parser-impl needs to be figured out:
              - having 'requires' transitive breaks binding-runtime-spi,
                which needs 'parser.repo', and since we attempt to wire
                those non-existing modules :(
    // FIXME: try to wire yang-model-validator using dagger
    // FIXME: add tests for all Dagger modules
    
    JIRA: YANGTOOLS-1727
    Change-Id: I973d9544a008ead947b9775f6ca036f3b3bffad0
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/rfc6241-parser-support/src/main/java/org/opendaylight/yangtools/rfc6241/parser/dagger/Rfc6241Module.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc6241.parser.dagger;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.multibindings.IntoSet;
+import jakarta.inject.Singleton;
+import org.opendaylight.yangtools.rfc6241.parser.impl.Rfc6241ParserExtension;
+import org.opendaylight.yangtools.yang.parser.spi.ParserExtension;
+
+/**
+ * A Dagger module binding RFC6241 extension.
+ *
+ * @since 14.0.21
+ */
+@Module
+public interface Rfc6241Module {
+    @Provides
+    @Singleton
+    @IntoSet
+    static ParserExtension provideParserExtension() {
+        return new Rfc6241ParserExtension();
+    }
+}
