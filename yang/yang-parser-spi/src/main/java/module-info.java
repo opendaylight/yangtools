e7ffec246371a3b14285fd1950c8565384900f5f
Robert Varga <robert.varga@pantheon.tech>
2020-10-21 18:20:52 +0200

    Convert yang-parser-spi to JPMS
    This is simple enough, and even if we need to move packages, that
    is something we can do later.
    
    JIRA: YANGTOOLS-1151
    Change-Id: I2f31980b00c59de77b15e36fa39b39c0cbf943a2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-spi/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.yangtools.yang.parser.spi {
+    exports org.opendaylight.yangtools.yang.parser.spi;
+    exports org.opendaylight.yangtools.yang.parser.spi.meta;
+    exports org.opendaylight.yangtools.yang.parser.spi.source;
+    exports org.opendaylight.yangtools.yang.parser.spi.validation;
+
+    requires transitive org.opendaylight.yangtools.yang.model.api;
+}
