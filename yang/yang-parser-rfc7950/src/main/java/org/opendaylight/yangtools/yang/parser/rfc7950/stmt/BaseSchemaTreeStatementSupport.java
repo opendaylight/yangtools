a8563f6427915753c5be2329702b4fe3cdce38cb
miroslav.kovac <miroslav.kovac@pantheon.tech>
2020-10-29 11:26:47 +0100

    Move implementation of OnStatementAdded to single class
    Create BaseSchemaTreeStatementSupport that implements
    onStatementAdded and migrate all the usage from
    StatementSupport classes.
    
    JIRA: YANGTOOLS-1154
    Change-Id: I3815d69f8621b9559470fea35fa4d427a717acc4
    Signed-off-by: miroslav.kovac <miroslav.kovac@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit ca57ae28c6974bf6c5faa866667e52c8f458e325)

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/BaseSchemaTreeStatementSupport.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.stmt;
+
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeEffectiveStatement;
+import org.opendaylight.yangtools.yang.parser.rfc7950.namespace.ChildSchemaNodeNamespace;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext.Mutable;
+
+/**
+ * Specialization of {@link BaseQNameStatementSupport} for stating the onStatementAdded method.
+ *
+ * @param <D> Declared Statement representation
+ * @param <E> Effective Statement representation
+ */
+public abstract class BaseSchemaTreeStatementSupport<D extends DeclaredStatement<QName>,
+        E extends SchemaTreeEffectiveStatement<D>> extends BaseQNameStatementSupport<D, E> {
+    protected BaseSchemaTreeStatementSupport(final StatementDefinition publicDefinition) {
+        super(publicDefinition);
+    }
+
+    protected BaseSchemaTreeStatementSupport(final StatementDefinition publicDefinition, final CopyPolicy copyPolicy) {
+        super(publicDefinition, copyPolicy);
+    }
+
+    @Override
+    public final void onStatementAdded(final Mutable<QName, D, E> stmt) {
+        stmt.coerceParentContext().addToNs(ChildSchemaNodeNamespace.class, stmt.coerceStatementArgument(), stmt);
+    }
+
+}
