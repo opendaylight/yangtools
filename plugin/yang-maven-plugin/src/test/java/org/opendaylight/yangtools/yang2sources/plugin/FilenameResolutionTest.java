2e9ba2bb54da853f6eb1b1597dae3eac08c17b84
Robert Varga <robert.varga@pantheon.tech>
2020-11-02 17:09:48 +0100

    WIP: Remove remove yang-maven-plugin interfaces
    // FIXME: fixup unit tests
    // FIXME: probably needs some amount of mocking rework
    
    JIRA: YANGTOOLS-747
    Change-Id: I5e3acf0df97a33e7507841a549fa7109b48c16c3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- plugin/yang-maven-plugin/src/test/java/org/opendaylight/yangtools/yang2sources/plugin/FilenameResolutionTest.java
+++ plugin/yang-maven-plugin/src/test/java/org/opendaylight/yangtools/yang2sources/plugin/FilenameResolutionTest.java
@@ -9,39 +9,45 @@ package org.opendaylight.yangtools.yang2sources.plugin;
 
 import static org.junit.Assert.assertEquals;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableTable;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Table;
 import com.google.common.io.Resources;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import org.apache.maven.plugin.AbstractMojoExecutionException;
 import org.junit.Test;
+import org.kohsuke.MetaInfServices;
+import org.opendaylight.yangtools.plugin.generator.api.FileGenerator;
+import org.opendaylight.yangtools.plugin.generator.api.FileGeneratorException;
+import org.opendaylight.yangtools.plugin.generator.api.FileGeneratorFactory;
+import org.opendaylight.yangtools.plugin.generator.api.GeneratedFile;
+import org.opendaylight.yangtools.plugin.generator.api.GeneratedFilePath;
+import org.opendaylight.yangtools.plugin.generator.api.GeneratedFileType;
+import org.opendaylight.yangtools.plugin.generator.api.ModuleResourceResolver;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang.model.repo.api.YangTextSchemaSource;
-import org.opendaylight.yangtools.yang2sources.plugin.ConfigArg.CodeGeneratorArg;
-import org.opendaylight.yangtools.yang2sources.spi.BasicCodeGenerator;
-import org.opendaylight.yangtools.yang2sources.spi.ModuleResourceResolver;
 
 public class FilenameResolutionTest extends AbstractCodeGeneratorTest {
     @Test
     public void testResolveSubmoduleResource() throws URISyntaxException, AbstractMojoExecutionException, IOException {
         setupMojo(new YangToSourcesProcessor(
-            new File(Resources.getResource(FilenameResolutionTest.class, "/filename").toURI()), ImmutableList.of(),
-            List.of(new CodeGeneratorArg(Generator.class.getName(), "outputDir")), project, false, yangProvider))
+            new File(Resources.getResource(FilenameResolutionTest.class, "/filename").toURI()), List.of(),
+            List.of(new FileGeneratorArg(Generator.class.getSimpleName())), project, false, yangProvider))
             .execute();
     }
 
-    public static final class Generator implements BasicCodeGenerator {
+    public static final class Generator implements FileGenerator {
         @Override
-        public Collection<File> generateSources(final EffectiveModelContext context, final File outputBaseDir,
-                final Set<Module> currentModules, final ModuleResourceResolver moduleResourcePathResolver)  {
+        public Table<GeneratedFileType, GeneratedFilePath, GeneratedFile> generateFiles(
+                final EffectiveModelContext context, final Set<Module> localModules,
+                final ModuleResourceResolver moduleResourcePathResolver) throws FileGeneratorException {
             final var module = Iterables.getOnlyElement(context.getModules());
             assertEquals(Optional.of("/META-INF/yang/foo@2020-10-13.yang"),
                 moduleResourcePathResolver.findModuleResourcePath(module, YangTextSchemaSource.class));
@@ -49,17 +55,20 @@ public class FilenameResolutionTest extends AbstractCodeGeneratorTest {
             final var submodule = Iterables.getOnlyElement(module.getSubmodules());
             assertEquals(Optional.of("/META-INF/yang/foo-submodule@2020-10-12.yang"),
                 moduleResourcePathResolver.findModuleResourcePath(submodule, YangTextSchemaSource.class));
-            return List.of();
+            return ImmutableTable.of();
+        }
+    }
+
+    @MetaInfServices
+    public static final class GeneratorFactory implements FileGeneratorFactory {
+        @Override
+        public String getIdentifier() {
+            return Generator.class.getSimpleName();
         }
 
         @Override
-        public void setAdditionalConfig(final Map<String, String> additionalConfiguration) {
-            // Noop
-        }
-
-        @Override
-        public void setResourceBaseDir(final File resourceBaseDir) {
-            // Noop
+        public FileGenerator newFileGenerator(final Map<String, String> configuration) {
+            return new Generator();
         }
     }
 }
