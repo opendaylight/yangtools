546ea13e3eb2b444011f65485e220b23447bb4d9
Robert Varga <robert.varga@pantheon.tech>
2020-11-06 20:01:32 +0100

    Convert yang-binding to a JPMS module
    The code itself is simple, just export it. UTs need an override
    to allow use of Whitebox.
    
    JIRA: MDSAL-624
    Change-Id: I1d1114473563fe9237df5855ef95c45318cd2ec9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/yang-binding/src/main/java/module-info.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.yangtools.yang.binding {
+    exports org.opendaylight.yangtools.yang.binding;
+    exports org.opendaylight.yangtools.yang.binding.annotations;
+    exports org.opendaylight.yangtools.yang.binding.util;
+
+    requires transitive org.opendaylight.yangtools.yang.common;
+    requires org.opendaylight.yangtools.util;
+
+    // Annotations
+    requires static transitive org.eclipse.jdt.annotation;
+    requires static com.github.spotbugs.annotations;
+}
