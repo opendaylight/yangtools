2fa7ba91118b545058f864ea7d04b54c5cdd8ec1
Robert Varga <robert.varga@pantheon.tech>
2020-11-07 10:35:54 +0100

    Convert mdsal-binding-dom-codec-api to JPMS
    This is a very simplistic API component, convert it to JPMS.
    
    JIRA: MDSAL-632
    Change-Id: Ie3d7e264f750d9d0f373e334f18cb6294cced9d0
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-dom-codec-api/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.mdsal.binding.dom.codec.api {
+    exports org.opendaylight.mdsal.binding.dom.codec.api;
+
+    requires transitive org.opendaylight.mdsal.binding.runtime.api;
+    requires org.opendaylight.mdsal.binding.spec.util;
+
+    // Annotations
+    requires static transitive org.eclipse.jdt.annotation;
+    requires static com.github.spotbugs.annotations;
+}
