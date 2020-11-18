ca2b3e313b46e41f2cafba24d8ae9e0592156f06
Robert Varga <robert.varga@pantheon.tech>
2020-11-18 13:33:10 +0100

    Fix get{Min,Max}Elements() usage
    These return a nullable, UTs should be using
    assertEquals(Object, Object) to compare them.
    
    Change-Id: I6d6aefc94695c5118b4ec4552f64bb45b45dcdc8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/EffectiveUsesRefineAndConstraintsTest.java
+++ yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/EffectiveUsesRefineAndConstraintsTest.java
@@ -106,8 +106,8 @@ public class EffectiveUsesRefineAndConstraintsTest {
         assertFalse(listSchemaNode.isConfiguration());
 
         ElementCountConstraint listConstraints = listSchemaNode.getElementCountConstraint().get();
-        assertEquals(10, listConstraints.getMinElements().intValue());
-        assertEquals(20, listConstraints.getMaxElements().intValue());
+        assertEquals(10, listConstraints.getMinElements());
+        assertEquals(20, listConstraints.getMaxElements());
         assertEquals(1, listSchemaNode.getMustConstraints().size());
     }
 
@@ -141,8 +141,8 @@ public class EffectiveUsesRefineAndConstraintsTest {
         assertTrue(listSchemaNode.isConfiguration());
 
         ElementCountConstraint listConstraints = listSchemaNode.getElementCountConstraint().get();
-        assertEquals(5, listConstraints.getMinElements().intValue());
-        assertEquals(7, listConstraints.getMaxElements().intValue());
+        assertEquals(5, listConstraints.getMinElements());
+        assertEquals(7, listConstraints.getMaxElements());
         assertEquals(2, listSchemaNode.getMustConstraints().size());
     }
 }
