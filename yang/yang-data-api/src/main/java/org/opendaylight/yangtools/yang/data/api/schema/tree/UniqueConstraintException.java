efa6b50d0488c7c6a838204509a659c4c302b89a
Robert Varga <robert.varga@pantheon.tech>
2020-11-18 15:41:30 +0100

    Add UniqueValidation
    This is a quick prototype of a stateless external enforcer. The test
    suite is split out of If3b94a085be034de28e341ac900142b021cd2a88 and
    adapted a bit.
    
    JIRA: YANGTOOLS-1177
    Change-Id: I3914497981db0281ab8f32dee12a102ce729022e
    Signed-off-by: Peter Kajsa <pkajsa@cisco.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/tree/UniqueConstraintException.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.api.schema.tree;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.util.Collections;
+import java.util.Map;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier.Descendant;
+
+/**
+ * Exception thrown when a {@code unique} statement restrictions are violated.
+ *
+ * @author Robert Varga
+ */
+@Beta
+@NonNullByDefault
+public class UniqueConstraintException extends DataValidationFailedException {
+    private static final long serialVersionUID = 1L;
+
+    // Note: this cannot be an ImmutableMap because we must support null values
+    @SuppressFBWarnings(value = "SE_BAD_FIELD", justification = "Best effort on serialization")
+    private final Map<Descendant, @Nullable Object> values;
+
+    public UniqueConstraintException(final YangInstanceIdentifier path, final Map<Descendant, @Nullable Object> values,
+            final String message) {
+        super(path, message);
+        this.values = requireNonNull(values);
+    }
+
+    public final Map<Descendant, @Nullable Object> values() {
+        return Collections.unmodifiableMap(values);
+    }
+}
