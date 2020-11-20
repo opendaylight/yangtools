d440dacb87fbe22b88f5c13f8bdef71f4c984f25
Robert Varga <robert.varga@pantheon.tech>
2020-11-20 18:12:47 +0100

    WIP: Add Forwarding(Mutable)TreeNode and their unique parts
    // FIXME: split out UniqueConstraintValidationFailedException
    // FIXME: expand functionality
    
    JIRA: YANGTOOLS-1178
    Change-Id: Ia1be35244884993adce77a3fd0e9315013352dd0
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/ForwardingMutableTreeNode.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.impl.schema.tree;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.MoreObjects.ToStringHelper;
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.spi.MutableTreeNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.spi.TreeNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.spi.Version;
+
+abstract class ForwardingMutableTreeNode implements MutableTreeNode {
+    private final MutableTreeNode delegate;
+
+    ForwardingMutableTreeNode(final MutableTreeNode delegate) {
+        this.delegate = requireNonNull(delegate);
+    }
+
+    @Override
+    public void setData(final NormalizedNode<?, ?> data) {
+        delegate.setData(data);
+    }
+
+    @Override
+    public void addChild(final TreeNode child) {
+        delegate.addChild(child);
+    }
+
+    @Override
+    public void removeChild(final PathArgument id) {
+        delegate.removeChild(id);
+    }
+
+    @Override
+    public final Optional<? extends TreeNode> getChild(final PathArgument child) {
+        return delegate.getChild(child);
+    }
+
+    @Override
+    public final void setSubtreeVersion(final Version subtreeVersion) {
+        delegate.setSubtreeVersion(subtreeVersion);
+    }
+
+    @Override
+    public final TreeNode seal() {
+        return seal(delegate.seal());
+    }
+
+    abstract @NonNull TreeNode seal(@NonNull TreeNode delegate);
+
+    @Override
+    public final String toString() {
+        return addToStringAttributes(MoreObjects.toStringHelper(this)).toString();
+    }
+
+    ToStringHelper addToStringAttributes(final ToStringHelper helper) {
+        return helper.add("delegate", delegate);
+    }
+}
