04417c2b2c69c020ddee9f5ddedf7868d9d4d97b
Robert Varga <robert.varga@pantheon.tech>
2020-11-26 13:39:02 +0100

    Add a refcount mechanism for substatements
    We need to defer cleaning substatements until there is no way
    they could be referenced again. This can happen, for example,
    when we make a copy through uses: source grouping can be built
    independently, but needs to retain its effective statements
    until all its copies are built as well.
    
    We fuse the two remaining booleans in StatementContextBase to
    allow up to 16 bits for refcount, which should be enough for
    most scenarios.
    
    // FIXME: trim down ReplicaStatementContext
    // FIXME: examine refcount/effective checks for simplification
    
    JIRA: YANGTOOLS-1184
    Change-Id: I5d26846ee1a5ce705671d9e66d45137a3d1084cf
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/StatementContextBase.java
+++ yang/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/StatementContextBase.java
@@ -16,6 +16,7 @@ import static java.util.Objects.requireNonNull;
 import com.google.common.annotations.Beta;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.MoreObjects.ToStringHelper;
+import com.google.common.base.VerifyException;
 import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMultimap;
@@ -146,11 +147,73 @@ public abstract class StatementContextBase<A, D extends DeclaredStatement<A>, E 
     private @Nullable ModelProcessingPhase completedPhase;
     private @Nullable E effectiveInstance;
 
+    /**
+     * Substatement refcount tracking. This mechanics deals with retaining substatements for the purposes of
+     * instantiating their lazy copies in InferredStatementContext. It works in concert with {@link #loadEffective()}
+     * and {@link #buildDeclared()}/{@link #builtDeclared()}: declared/effective statement views hold an implicit
+     * reference and refcount-based sweep is not activated until they are done (or this statement is not
+     * {@link #isSupportedToBuildEffective}).
+     *
+     * <p>
+     * Reference count is hierarchical in that parent references also pin down their child statements and do not allow
+     * them to be swept.
+     *
+     * <p>
+     * The counter's positive values are tracking incoming references via {@link #incRef()}/{@link #decRef()} methods.
+     * Once we transition to sweeping, this value becomes negative counting upwards to {@link #REFCOUNT_NONE} based on
+     * {@link #sweepOnChildDone()}. Once we reach that, we transition to {@link #REFCOUNT_SWEPT}.
+     */
+    private int refcount = REFCOUNT_NONE;
+    /**
+     * No outstanding references, this statement is a potential candidate for sweeping, provided it has populated its
+     * declared and effective views and {@link #parentRef} is known to be absent.
+     */
+    private static final int REFCOUNT_NONE = 0;
+    /**
+     * Reference count overflow or some other recoverable logic error. Do not rely on refcounts and do not sweep
+     * anything.
+     *
+     * <p>
+     * Note on value assignment:
+     * This allow our incRef() to naturally progress to being saturated. Others jump there directly.
+     * It also makes it  it impossible to observe {@code Interger.MAX_VALUE} children, which we take advantage of for
+     * {@link #REFCOUNT_SWEEPING}.
+     */
+    private static final int REFCOUNT_DEFUNCT = Integer.MAX_VALUE;
+    /**
+     * This statement is being actively swept. This is a transient value set when we are sweeping our children, so that
+     * we prevent re-entering this statement.
+     *
+     * <p>
+     * Note on value assignment:
+     * The value is lower than any legal child refcount due to {@link #REFCOUNT_DEFUNCT} while still being higher than
+     * {@link #REFCOUNT_SWEPT}.
+     */
+    private static final int REFCOUNT_SWEEPING = -Integer.MAX_VALUE;
+    /**
+     * This statement, along with its entire subtree has been swept and we positively know all our children have reached
+     * this state. We {@link #sweepNamespaces()} upon reaching this state.
+     *
+     * <p>
+     * Note on value assignment:
+     * This is the lowest value observable, making it easier on checking others on equality.
+     */
+    private static final int REFCOUNT_SWEPT = Integer.MIN_VALUE;
+
+    /**
+     * Local knowledge of {@link #refcount} values up to statement root. We use this field to prevent recursive lookups
+     * in {@link #noParentRefs(StatementContextBase)} -- once we discover a parent reference once, we keep that
+     * knowledge and update it when {@link #sweep()} is invoked.
+     */
+    private final byte parentRef = PARENTREF_UNKNOWN;
+    private static final byte PARENTREF_UNKNOWN = 0;
+    private static final byte PARENTREF_ABSENT  = 1;
+    private static final byte PARENTREF_PRESENT = 2;
+
     // Master flag controlling whether this context can yield an effective statement
     // FIXME: investigate the mechanics that are being supported by this, as it would be beneficial if we can get rid
     //        of this flag -- eliminating the initial alignment shadow used by below gap-filler fields.
     private boolean isSupportedToBuildEffective = true;
-
     // Flag for use with AbstractResumedStatement. This is hiding in the alignment shadow created by above boolean
     private boolean fullyDefined;
 
@@ -556,8 +619,22 @@ public abstract class StatementContextBase<A, D extends DeclaredStatement<A>, E 
     }
 
     private E loadEffective() {
-        return effectiveInstance = definition.getFactory().createEffective(new BaseCurrentEffectiveStmtCtx<>(this),
-            streamDeclared(), streamEffective());
+        // Always imply declared statement
+        buildDeclared();
+
+        final E ret = effectiveInstance = createEffective();
+        // we have called createEffective(), substatements are no longer guarded by us. Let's see if we can clear up
+        // some residue.
+        if (refcount == REFCOUNT_NONE) {
+            sweepOnDecrement();
+        }
+        return ret;
+    }
+
+    // Exposed for ReplicaStatementContext
+    E createEffective() {
+        return definition.getFactory().createEffective(new BaseCurrentEffectiveStmtCtx<>(this), streamDeclared(),
+            streamEffective());
     }
 
     abstract Stream<? extends StmtContext<?, ?, ?>> streamDeclared();
@@ -911,7 +988,11 @@ public abstract class StatementContextBase<A, D extends DeclaredStatement<A>, E 
     @Override
     public final StatementContextBase<A, D, E> replicaAsChildOf(final Mutable<?, ?, ?> parent) {
         checkArgument(parent instanceof StatementContextBase, "Unsupported parent %s", parent);
-        return this;
+        return replicaAsChildOf((StatementContextBase<?, ?, ?>) parent);
+    }
+
+    final @NonNull StatementContextBase<A, D, E> replicaAsChildOf(final StatementContextBase<?, ?, ?> stmt) {
+        return new ReplicaStatementContext<>(stmt, this);
     }
 
     private static void checkEffectiveModelCompleted(final StmtContext<?, ?, ?> stmt) {
@@ -951,6 +1032,184 @@ public abstract class StatementContextBase<A, D extends DeclaredStatement<A>, E 
      * @return True if {@link #allSubstatements()} and {@link #allSubstatementsStream()} would return an empty stream.
      */
     abstract boolean hasEmptySubstatements();
+
+    abstract boolean builtDeclared();
+
+    /**
+     * Acquire a reference on this context. As long as there is at least one reference outstanding,
+     * {@link #buildEffective()} will not result in {@link #effectiveSubstatements()} being discarded.
+     *
+     * @throws VerifyException if {@link #effectiveSubstatements()} has already been discarded
+     */
+    final void incRef() {
+        final int current = refcount;
+        verify(current >= REFCOUNT_NONE, "Attempted to access reference count of %s", this);
+        if (current != REFCOUNT_DEFUNCT) {
+            // Note: can end up becoming REFCOUNT_DEFUNCT on overflow
+            refcount = current + 1;
+        } else {
+            LOG.debug("Disabled refcount increment of {}", this);
+        }
+    }
+
+    /**
+     * Release a reference on this context. This call may result in {@link #effectiveSubstatements()} becoming
+     * unavailable.
+     */
+    final void decRef() {
+        final int current = refcount;
+        if (current == REFCOUNT_DEFUNCT) {
+            // no-op
+            LOG.debug("Disabled refcount decrement of {}", this);
+            return;
+        }
+        if (current <= REFCOUNT_NONE) {
+            // Underflow, become defunct
+            LOG.warn("Statement refcount underflow, reference counting disabled for {}", this, new Throwable());
+            refcount = REFCOUNT_DEFUNCT;
+            return;
+        }
+
+        refcount = current - 1;
+        LOG.trace("Refcount {} on {}", refcount, this);
+        if (refcount == REFCOUNT_NONE && canBeSwept()) {
+            // We are no longer guarded by effective instance
+            sweepOnDecrement();
+        }
+    }
+
+    private boolean canBeSwept() {
+        return builtDeclared() && effectiveInstance != null || !isSupportedToBuildEffective();
+    }
+
+    private void sweepOnDecrement() {
+        LOG.trace("Sweeping on decrement {}", this);
+        final StatementContextBase<?, ?, ?> parent = getParentContext();
+        if (parent == null) {
+            // We are the top-level object and have lost a reference. Trigger sweep if possible and we are done.
+            sweepState();
+            return;
+        }
+
+        // Check whether there the parent needs to know any references to any of our parents
+        int refs = parent.refcount;
+        if (refs > REFCOUNT_SWEEPING && refs < REFCOUNT_NONE) {
+            // We are a child for which our parent is waiting. Notify it and we are done.
+            parent.sweepOnChildDone();
+            return;
+        } else if (refs > REFCOUNT_NONE || refs <= REFCOUNT_SWEEPING || !parent.canBeSwept()) {
+            // No-op
+            return;
+        }
+
+        // parent is potentially reclaimable
+        if (parent.noParentRefs()) {
+            LOG.trace("Cleanup {} from {} to parent {}", parent.refcount, this, parent);
+            parent.sweepState();
+        }
+    }
+
+    // FIXME: cache the resolution of this
+    private boolean noParentRefs() {
+        final StatementContextBase<?, ?, ?> parent = getParentContext();
+        if (parent != null) {
+            final int refs = parent.refcount;
+            if (refs > REFCOUNT_NONE || !parent.canBeSwept()) {
+                // parent with refcount or protected by views
+                return false;
+            }
+            if (refs < REFCOUNT_NONE) {
+                // parent is being swept already
+                return true;
+            }
+            // REFCOUNT_NONE and reclaimable, look forward
+            return parent.noParentRefs();
+        }
+        return true;
+    }
+
+
+    private void sweepOnChildDone() {
+        LOG.trace("Sweeping on child done {}", this);
+        final int current = refcount;
+        if (current >= REFCOUNT_NONE) {
+            // no-op, perhaps we want to handle some cases differently?
+            LOG.trace("Ignoring child sweep of {} for {}", this, current);
+            return;
+        }
+        verify(current != REFCOUNT_SWEPT, "Attempt to sweep a child of swept %s", this);
+
+        refcount = current + 1;
+        LOG.trace("Child refcount {}", refcount);
+        if (refcount == REFCOUNT_NONE) {
+            sweepDone();
+            sweepParent();
+        }
+    }
+
+    private void sweepParent() {
+        final StatementContextBase<?, ?, ?> parent = getParentContext();
+        LOG.trace("Propagating to parent {}", parent);
+        if (parent != null && parent.refcount > REFCOUNT_SWEEPING && parent.refcount < REFCOUNT_NONE) {
+            parent.sweepOnChildDone();
+        }
+    }
+
+    private void sweepDone() {
+        LOG.trace("Sweep done for {}", this);
+        refcount = REFCOUNT_SWEPT;
+        sweepNamespaces();
+    }
+
+    private boolean sweepState() {
+        refcount = REFCOUNT_SWEEPING;
+        final int childRefs = sweepEffective();
+        if (childRefs == 0) {
+            sweepDone();
+            return true;
+        }
+        if (childRefs < 0 || childRefs >= REFCOUNT_DEFUNCT) {
+            LOG.warn("Negative child refcount {} cannot be stored, reference counting disabled for {}", childRefs, this,
+                new Throwable());
+            refcount = REFCOUNT_DEFUNCT;
+        } else {
+            LOG.trace("Still {} outstanding children of {}", childRefs, this);
+            refcount = -childRefs;
+        }
+        return false;
+    }
+
+    /**
+     * Sweep this statement context as a result of {@link #sweepEffective()}, i.e. when parent is also being swept.
+     */
+    final void sweep() {
+        if (refcount == REFCOUNT_NONE && canBeSwept()) {
+            LOG.trace("Releasing {}", this);
+            sweepState();
+        }
+    }
+
+    static final int countUnswept(final Collection<? extends StatementContextBase<?, ?, ?>> substatements) {
+        int result = 0;
+        for (StatementContextBase<?, ?, ?> stmt : substatements) {
+            if (stmt.refcount > REFCOUNT_NONE || !stmt.canBeSwept()) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Implementation-specific sweep action. This is expected to perform a recursive {@link #sweep()} on all
+     * {@link #declaredSubstatements()} and {@link #effectiveSubstatements()} and report the result of the sweep
+     * operation.
+     *
+     * <p>
+     * {@link #effectiveSubstatements()} as well as namespaces may become inoperable as a result of this operation.
+     *
+     * @return True if the entire tree has been completely swept, false otherwise.
+     */
+    abstract int sweepEffective();
 
     /**
      * Config statements are not all that common which means we are performing a recursive search towards the root
---
+++ yang/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/SweptNamespace.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.stmt.reactor;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.VerifyException;
+import java.util.AbstractMap;
+import java.util.Set;
+
+/**
+ * Placeholder namespace map which does not allow access and acts as a sentinel for namespaces which have been
+ * explicitly removed from {@link NamespaceStorageSupport}.
+ */
+final class SweptNamespace extends AbstractMap<Object, Object> {
+    private final Class<?> name;
+
+    SweptNamespace(final Class<?> name) {
+        this.name = requireNonNull(name);
+    }
+
+    @Override
+    public Set<Entry<Object, Object>> entrySet() {
+        throw new VerifyException("Attempted to access swept namespace " + name);
+    }
+}
