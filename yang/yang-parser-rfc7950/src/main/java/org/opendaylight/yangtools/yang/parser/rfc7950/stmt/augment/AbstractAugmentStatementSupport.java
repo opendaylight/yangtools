48d852e425ab4b2089cb9548c0656182ad6083a0
Robert Varga <robert.varga@pantheon.tech>
2020-11-26 13:39:02 +0100

    Add a refcount mechanism for substatements
    We need to defer cleaning substatements until there is no way
    they could be referenced again. This can happen, for example,
    when we make a copy through uses: source grouping can be built
    independently, but needs to retain its effective statements
    until all its copies are built as well.
    
    We fuse the two remaining booleans in StatementContextBase to
    allow up to 16 bits for refcount, which should be enough for
    most scenarios.
    
    Since AbstractAugmentSupport is implanting its source statement
    to the target, effectively duplicating the statement, we introduce
    ReplicaStatementContext to handle the reference counting between
    the two sites.
    
    JIRA: YANGTOOLS-1184
    Change-Id: I5d26846ee1a5ce705671d9e66d45137a3d1084cf
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/augment/AbstractAugmentStatementSupport.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/augment/AbstractAugmentStatementSupport.java
@@ -127,6 +127,8 @@ abstract class AbstractAugmentStatementSupport
                 // We are targeting a context which is creating implicit nodes. In order to keep things consistent,
                 // we will need to circle back when creating effective statements.
                 if (augmentTargetCtx.hasImplicitParentSupport()) {
+                    // FIXME: YANGTOOLS-1184: so we are putting this in the source node ... which is okay, but we also
+                    //        need to deal with the target copy (below)
                     augmentNode.addToNs(AugmentImplicitHandlingNamespace.class, augmentNode, augmentTargetCtx);
                 }
 
@@ -135,7 +137,11 @@ abstract class AbstractAugmentStatementSupport
                 //        which we do not handle. This needs to be reworked in terms of unknown schema nodes.
                 try {
                     copyFromSourceToTarget(augmentSourceCtx, augmentTargetCtx);
-                    augmentTargetCtx.addEffectiveSubstatement(augmentSourceCtx);
+                    // FIXME: YANGTOOLS-1184: this add is violating statement lifecycle: it suddenly has two parents.
+                    //        Since there is no copy operation, this statement appears out of thin air, i.e. this
+                    //        support does not run onStatementAdded(). Furthermore it has an original copy type.
+                    //        Finally it would
+                    augmentTargetCtx.addEffectiveReplicaSubstatement(augmentSourceCtx);
                 } catch (final SourceException e) {
                     LOG.warn("Failed to add augmentation {} defined at {}", augmentTargetCtx.sourceReference(),
                             augmentSourceCtx.sourceReference(), e);
