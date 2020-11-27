45a307c61877d720d624a744374a95f6a7de42c2
Robert Varga <robert.varga@pantheon.tech>
2020-11-27 03:16:04 +0100

    Split IfFeatureStatementSupport
    We are using version-dependent argument parsing, which is indicating
    this support should have two subclasses, one for each version.
    
    This speeds up processing, as we do not have to lookup the version
    and can rely on reactor statement binding to bind the correct support.
    
    Change-Id: I9257760f333af44d063143a95bc4898f63120005
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/if_feature/IfFeatureStatementRFC7950Support.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.if_feature;
+
+import org.opendaylight.yangtools.yang.model.api.stmt.IfFeatureExpr;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+
+public final class IfFeatureStatementRFC7950Support extends AbstractIfFeatureStatementSupport {
+    private static final IfFeatureStatementRFC7950Support INSTANCE = new IfFeatureStatementRFC7950Support();
+
+    private IfFeatureStatementRFC7950Support() {
+        // Hidden on purpose
+    }
+
+    public static IfFeatureStatementRFC7950Support getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public IfFeatureExpr parseArgumentValue(final StmtContext<?, ?, ?> ctx, final String value) {
+        return IfFeaturePredicateVisitor.parseIfFeatureExpression(ctx, value);
+    }
+}
