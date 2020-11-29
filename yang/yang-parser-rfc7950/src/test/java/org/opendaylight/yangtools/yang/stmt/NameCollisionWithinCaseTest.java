c828181c27ab5349fff52e5a01bd92ff176f9783
Robert Varga <robert.varga@pantheon.tech>
2020-11-30 00:04:56 +0100

    Clean up NameCollisionWithinCaseTest
    Use assertThrows() to properly catch exceptions, cleaning up related
    Sonar complaints.
    
    Change-Id: I23a956f008db6ca53329627ef190fc74b0055a33
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/NameCollisionWithinCaseTest.java
+++ yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/NameCollisionWithinCaseTest.java
@@ -10,7 +10,7 @@ package org.opendaylight.yangtools.yang.stmt;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.startsWith;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertThrows;
 
 import org.junit.Test;
 import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
@@ -18,43 +18,32 @@ import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
 
 public class NameCollisionWithinCaseTest {
     @Test
-    public void testChildNameCollisionOfAugmentCase() throws Exception {
-        try {
-            StmtTestUtils.parseYangSource("/bugs/name-collision-within-case/foo.yang");
-            fail("Expected failure due to node name collision");
-        } catch (ReactorException e) {
-            final Throwable cause = e.getCause();
-            assertThat(cause, instanceOf(SourceException.class));
-            assertThat(cause.getMessage(), startsWith(
-                "Cannot add data tree child with name (foo?revision=2018-02-11)bar, a conflicting child already exists "
-                        + "[at "));
-        }
+    public void testChildNameCollisionOfAugmentCase() {
+        final ReactorException ex = assertThrows(ReactorException.class,
+            () -> StmtTestUtils.parseYangSource("/bugs/name-collision-within-case/foo.yang"));
+        final Throwable cause = ex.getCause();
+        assertThat(cause, instanceOf(SourceException.class));
+        assertThat(cause.getMessage(), startsWith("Cannot add data tree child with name (foo?revision=2018-02-11)bar, "
+            + "a conflicting child already exists [at "));
     }
 
     @Test
-    public void testChildNameCollisionOfAugmentChoice() throws Exception {
-        try {
-            StmtTestUtils.parseYangSource("/bugs/name-collision-within-case/bar.yang");
-            fail("Expected failure due to node name collision");
-        } catch (ReactorException e) {
-            final Throwable cause = e.getCause();
-            assertThat(cause, instanceOf(SourceException.class));
-            assertThat(cause.getMessage(), startsWith(
-                "Cannot add data tree child with name (bar?revision=2018-02-11)bar, a conflicting child already exists "
-                        + "[at "));
-        }
+    public void testChildNameCollisionOfAugmentChoice() {
+        final ReactorException ex = assertThrows(ReactorException.class,
+            () -> StmtTestUtils.parseYangSource("/bugs/name-collision-within-case/bar.yang"));
+        final Throwable cause = ex.getCause();
+        assertThat(cause, instanceOf(SourceException.class));
+        assertThat(cause.getMessage(), startsWith("Cannot add data tree child with name (bar?revision=2018-02-11)bar, "
+            + "a conflicting child already exists [at "));
     }
 
     @Test
     public void testChildNameCollisionNormal() throws Exception {
-        try {
-            StmtTestUtils.parseYangSource("/bugs/name-collision-within-case/baz.yang");
-            fail("Expected failure due to node name collision");
-        } catch (ReactorException e) {
-            final Throwable cause = e.getCause();
-            assertThat(cause, instanceOf(SourceException.class));
-            assertThat(cause.getMessage(), startsWith(
-                "Error in module 'baz': cannot add '(baz?revision=2018-02-28)bar'. Node name collision: "));
-        }
+        final ReactorException ex = assertThrows(ReactorException.class,
+            () -> StmtTestUtils.parseYangSource("/bugs/name-collision-within-case/baz.yang"));
+        final Throwable cause = ex.getCause();
+        assertThat(cause, instanceOf(SourceException.class));
+        assertThat(cause.getMessage(), startsWith(
+            "Error in module 'baz': cannot add '(baz?revision=2018-02-28)bar'. Node name collision: "));
     }
 }
