7a82616b46a9d0b645ec1c1c860f3b78fddcc593
Robert Varga <robert.varga@pantheon.tech>
2020-11-30 17:30:24 +0100

    WIP: Expose localName checking method from AbstractQNAME
    JIRA: YANGTOOLS-1191
    Change-Id: Iecea9f446730e4e7840b962a95844ce5128dfb9e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/AbstractQName.java
+++ yang/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/AbstractQName.java
@@ -72,12 +72,19 @@ public abstract class AbstractQName implements Identifier, WritableObject {
         return new QName(namespace, getLocalName());
     }
 
+    public static final boolean isValidLocalName(final String localName) {
+        return !localName.isEmpty() && checkContent(localName);
+    }
+
     abstract Object writeReplace();
 
     static final String checkLocalName(final @Nullable String localName) {
         checkArgument(!localName.isEmpty(), "Parameter 'localName' must be a non-empty string.");
-        checkArgument(IDENTIFIER_START.matches(localName.charAt(0)) && NOT_IDENTIFIER_PART.indexIn(localName, 1) == -1,
-                "String '%s' is not a valid identifier", localName);
+        checkArgument(checkContent(localName), "String '%s' is not a valid identifier", localName);
         return localName;
+    }
+
+    private static boolean checkContent(final String localName) {
+        return IDENTIFIER_START.matches(localName.charAt(0)) && NOT_IDENTIFIER_PART.indexIn(localName, 1) == -1;
     }
 }
