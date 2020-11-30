df779136640431fc3a3a7a937e150576a0ccf825
Robert Varga <robert.varga@pantheon.tech>
2020-11-30 08:50:33 +0100

    Search effective statements instaead of StmtContexts
    When we are searching for the appropriate namespace/prefix, we want
    to be referring to the effective statements we have at hand rather
    than dropping back to StmtContext.
    
    JIRA: YANGTOOLS-1186
    Change-Id: Ic2c3c94498ef11f807a56e3dd533b31c28c97f26
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/module/ModuleEffectiveStatementImpl.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/module/ModuleEffectiveStatementImpl.java
@@ -61,7 +61,7 @@ final class ModuleEffectiveStatementImpl extends AbstractEffectiveModule<ModuleS
     ModuleEffectiveStatementImpl(final Current<UnqualifiedQName, ModuleStatement> stmt,
             final ImmutableList<? extends EffectiveStatement<?, ?>> substatements,
             final Collection<? extends Submodule> submodules) {
-        super(stmt, substatements, findPrefix(stmt.caerbannog(), "module", stmt.getRawArgument()));
+        super(stmt, substatements, findPrefix(stmt.sourceReference(), substatements, "module", stmt.getRawArgument()));
 
         qnameModule = verifyNotNull(stmt.getFromNamespace(ModuleCtxToModuleQName.class, stmt.caerbannog()));
         this.submodules = ImmutableList.copyOf(submodules);
