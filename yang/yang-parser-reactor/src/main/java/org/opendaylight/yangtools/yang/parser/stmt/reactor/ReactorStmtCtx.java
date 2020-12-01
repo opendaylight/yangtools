4ee591d072653a55ad8823e6844809b94dc8ab91
Robert Varga <robert.varga@pantheon.tech>
2020-12-01 23:33:10 +0100

    WIP: Sweep child->parent relationships more quickly
    Sweep logic currently does not kick in until the root statement of
    a particular source is completely built. This leads to us retaining
    all of the state until that moment, at which point we mass-release
    everything.
    
    This is caused by us being needlessly conservative in deciding when
    to start sweeping our parents.
    
    Replace ReactorStmtCtx.noParentRefs() with noParentRefcount(), which
    disregards noImplicitRef().
    
    JIRA: YANGTOOLS-1184
    Change-Id: I97feeb65c270b1e7abceb99b94938593cf2e9abc
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/ReactorStmtCtx.java
+++ yang/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/ReactorStmtCtx.java
@@ -196,28 +196,23 @@ abstract class ReactorStmtCtx<A, D extends DeclaredStatement<A>, E extends Effec
         }
 
         // parent is potentially reclaimable
-        if (noParentRefs()) {
+        if (noParentRefcount()) {
             LOG.trace("Cleanup {} of parent {}", refcount, this);
             sweepState();
         }
     }
 
     // FIXME: cache the resolution of this
-    private boolean noParentRefs() {
+    private boolean noParentRefcount() {
         final ReactorStmtCtx<?, ?, ?> parent = parentStmtCtx();
         if (parent != null) {
+            // There are three possibilities:
+            // - REFCOUNT_NONE, in which case we need to search next parent
+            // - negative (< REFCOUNT_NONE), meaning parent is in some stage of sweeping, hence it does not have
+            //   a reference to us
+            // - positive (> REFCOUNT_NONE), meaning parent has an explicit refcount which is holding us down
             final int refs = parent.refcount;
-            // FIXME: 'noImplicitRef' is too strict here?
-            if (refs > REFCOUNT_NONE || !parent.noImplictRef()) {
-                // parent with refcount or protected by views
-                return false;
-            }
-            if (refs < REFCOUNT_NONE) {
-                // parent is being swept already
-                return true;
-            }
-            // REFCOUNT_NONE and reclaimable, look forward
-            return parent.noParentRefs();
+            return refs == REFCOUNT_NONE ? parent.noParentRefcount() : refs < REFCOUNT_NONE;
         }
         return true;
     }
