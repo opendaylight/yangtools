fdd2269d7a52b33e39fc47b3c72c65c1864c3c0f
miroslav.kovac <miroslav.kovac@pantheon.tech>
2020-12-03 18:05:43 +0100

    Add support for disabling SchemaPath generation
    Add a simple system property, which instructs SchemaNode.getPath()
    parser implementations to report UnsupportedOperationException
    instead of retaining the SchemaPath.
    
    JIRA: YANGTOOLS-1070
    Change-Id: I07b609c8952fcfcc4c87bbf6a2aaa155afefb4bc
    Signed-off-by: Michal Banik <michal.banik@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: miroslav.kovac <miroslav.kovac@pantheon.tech>

--- yang/yang-parser-spi/src/main/java/org/opendaylight/yangtools/yang/parser/spi/meta/SchemaPathSupport.java
+++ yang/yang-parser-spi/src/main/java/org/opendaylight/yangtools/yang/parser/spi/meta/SchemaPathSupport.java
@@ -13,10 +13,29 @@ import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.yangtools.concepts.Immutable;
 import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Beta
 // FIXME: remove this class once we ditch SchemaPath.getPath()
 public abstract class SchemaPathSupport implements Immutable {
+    private static final class Disabled extends SchemaPathSupport {
+        @Override
+        QName effectivePath(final SchemaPath path) {
+            return verifyNotNull(path.getLastComponent());
+        }
+
+        @Override
+        SchemaPath optionalPath(final SchemaPath path) {
+            return null;
+        }
+
+        @Override
+        boolean equalPaths(final SchemaPath first, final SchemaPath second) {
+            return true;
+        }
+    }
+
     private static final class Enabled extends SchemaPathSupport {
         @Override
         SchemaPath effectivePath(final SchemaPath path) {
@@ -34,7 +53,21 @@ public abstract class SchemaPathSupport implements Immutable {
         }
     }
 
-    private static final SchemaPathSupport DEFAULT = new Enabled();
+    private static final Logger LOG = LoggerFactory.getLogger(SchemaPathSupport.class);
+    private static final String ENABLE_PROPERTY = "org.opendaylight.yangtools.schemanode.getpath";
+
+    private static final SchemaPathSupport DEFAULT;
+
+    static {
+        // Forbid creating the SchemaPath in SchemaNode if property "schemanode.getpath.forbid" is set to "enabled"
+        // schemanode.getpath.forbid=enabled
+        if (System.getProperty(ENABLE_PROPERTY, "enabled").equals("disabled")) {
+            LOG.info("SchemaNode.getPath() support disabled");
+            DEFAULT = new Disabled();
+        } else {
+            DEFAULT = new Enabled();
+        }
+    }
 
     private SchemaPathSupport() {
         // Hidden on purpose
