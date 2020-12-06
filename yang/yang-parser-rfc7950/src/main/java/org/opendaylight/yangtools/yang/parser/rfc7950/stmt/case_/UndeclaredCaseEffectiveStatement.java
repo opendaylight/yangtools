ff8f4273db36a1da2281216b5774bc0823eab9d5
Robert Varga <robert.varga@pantheon.tech>
2020-12-06 17:19:41 +0100

    Make DataNodeContainer.dataChildByName() non-default
    Switch implementations, so that we provide findDataChildByName()
    as a default method and require a nullable lookup.
    
    JIRA: YANGTOOLS-1183
    Change-Id: I0e5ba2373b4ec6a4e1d6c8e7923e05954b2275c7
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/case_/UndeclaredCaseEffectiveStatement.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/case_/UndeclaredCaseEffectiveStatement.java
@@ -51,8 +51,8 @@ final class UndeclaredCaseEffectiveStatement extends WithSubstatements<QName, Ca
     }
 
     @Override
-    public Optional<DataSchemaNode> findDataChildByName(final QName name) {
-        return findDataSchemaNode(name);
+    public DataSchemaNode dataChildByName(final QName name) {
+        return dataSchemaNode(name);
     }
 
     @Override
