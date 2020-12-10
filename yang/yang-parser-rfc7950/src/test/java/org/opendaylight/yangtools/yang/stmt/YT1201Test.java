62e56229ba32a8ed6a14b4d0a6d6b1c85f442ec8
Robert Varga <robert.varga@pantheon.tech>
2020-12-10 12:48:16 +0100

    Make sure SmtmtNamespaceContext uses belongs-to prefix
    For xpath parsing we nee to make sure we take into account the
    prefix under which 'belongs-to' module is known to the submodule.
    
    JIRA: YANGTOOLS-1201
    Change-Id: Id38d76443d23eecef09f1f927c44218363a3a1d1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit bb9c8c8751e338d2e683063144d1d5810b22eee7)

---
+++ yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/YT1201Test.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.RevisionAwareXPath;
+import org.opendaylight.yangtools.yang.model.api.RevisionAwareXPath.WithExpression;
+import org.opendaylight.yangtools.yang.xpath.api.YangExpr;
+import org.opendaylight.yangtools.yang.xpath.api.YangLocationPath.Relative;
+import org.opendaylight.yangtools.yang.xpath.api.YangXPathAxis;
+
+public class YT1201Test {
+    private static final QName FOO = QName.create("foo", "foo");
+    private static final QName BAR = QName.create("foo", "bar");
+
+    @Test
+    public void testWhenPrefixes() throws Exception {
+        final DataSchemaNode bar = StmtTestUtils.parseYangSources("/bugs/YT1201/").getDataChildByName(BAR);
+        assertThat(bar, instanceOf(ContainerSchemaNode.class));
+        final RevisionAwareXPath when = ((ContainerSchemaNode) bar).getWhenCondition().get();
+        assertThat(when, instanceOf(WithExpression.class));
+        final YangExpr expr = ((WithExpression) when).getXPathExpression().getRootExpr();
+        assertThat(expr, instanceOf(Relative.class));
+        assertEquals(List.of(YangXPathAxis.CHILD.asStep(FOO)), ((Relative) expr).getSteps());
+    }
+}
