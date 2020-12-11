bf5ebc4d88502ec51da2fbd9892f60c51fe9fc38
Robert Varga <robert.varga@pantheon.tech>
2020-12-11 01:27:45 +0100

    Convert yang-data-jaxen to OSGi DS
    We do not need a dependency on osgi.core here, just use component
    annotations.
    
    Change-Id: If19fd9722e7c09088a5dc8839ef774813a4a217d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- attic/yang-data-jaxen/src/main/java/org/opendaylight/yangtools/yang/data/jaxen/JaxenSchemaContextFactory.java
+++ attic/yang-data-jaxen/src/main/java/org/opendaylight/yangtools/yang/data/jaxen/JaxenSchemaContextFactory.java
@@ -7,17 +7,45 @@
  */
 package org.opendaylight.yangtools.yang.data.jaxen;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.kohsuke.MetaInfServices;
 import org.opendaylight.yangtools.yang.data.jaxen.api.XPathSchemaContext;
 import org.opendaylight.yangtools.yang.data.jaxen.api.XPathSchemaContextFactory;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @MetaInfServices
+@Singleton
+@Component
 @NonNullByDefault
 public final class JaxenSchemaContextFactory implements XPathSchemaContextFactory {
+    private static final Logger LOG = LoggerFactory.getLogger(JaxenSchemaContextFactory.class);
+
+    @Inject
+    public JaxenSchemaContextFactory() {
+        // For DI
+    }
+
     @Override
     public XPathSchemaContext createContext(final EffectiveModelContext context) {
         return new JaxenSchemaContext(context);
+    }
+
+    @Activate
+    @SuppressWarnings("static-method")
+    void activate() {
+        LOG.info("Jaxen XPathSchemaContextFactory enabled");
+    }
+
+    @Deactivate
+    @SuppressWarnings("static-method")
+    void deactivate() {
+        LOG.info("Jaxen XPathSchemaContextFactory disabled");
     }
 }
