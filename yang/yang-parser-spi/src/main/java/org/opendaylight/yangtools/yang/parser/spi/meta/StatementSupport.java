16ed8e05eb2943ba795f64de805b6c4c47a44f21
Robert Varga <robert.varga@pantheon.tech>
2021-01-11 10:49:31 +0100

    Clarify StatementSupport.applyCopyPolicy() contract
    Target module and other parts are not well documented, change
    that.
    
    Change-Id: I77fa352ac50dea84bb373d71bf33146d726482a1
    JIRA: YANGTOOLS-1163
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-spi/src/main/java/org/opendaylight/yangtools/yang/parser/spi/meta/StatementSupport.java
+++ yang/yang-parser-spi/src/main/java/org/opendaylight/yangtools/yang/parser/spi/meta/StatementSupport.java
@@ -145,13 +145,23 @@ public interface StatementSupport<A, D extends DeclaredStatement<A>, E extends E
      * their operations with regard to their statements being replicated into different contexts, so that
      * {@link Mutable} instances are not created when it is evident they are superfluous.
      *
-     * @param stmt Context of statement to be copied statement.
+     * <p>
+     * The copy operation has three intrinsic parts:
+     * <ul>
+     *   <li>target {@code parent}, i.e. new parent statement for the copy. This determines things like default value
+     *       of the {@code config} statement and similar</li>
+     *   <li>copy operation type</li>
+     *   <li>{@code target module}, which defines the default namespace for the statement copy. This might not be always
+     *       present, in which case the namespace is retained from the source. As an example, {@code uses} changes
+     *       the default namespace to parent's namespace, whereas {@code augment} does not.</li>
+     * </ul>
+     *
+     * @param stmt Context of statement to be copied statement
      * @param parent Parent statement context
      * @param copyType Type of copy being performed
      * @param targetModule Target module, if present
      * @return Policy that needs to be applied to the copy operation of this statement.
      */
-    // FIXME: YANGTOOLS-694: clarify targetModule semantics (does null mean 'same as declared'?)
     default @NonNull CopyPolicy applyCopyPolicy(final Mutable<?, ?, ?> stmt, final Mutable<?, ?, ?> parent,
             final CopyType copyType, @Nullable final QNameModule targetModule) {
         // Most of statement supports will just want to copy the statement
