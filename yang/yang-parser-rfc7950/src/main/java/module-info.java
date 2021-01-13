1d66b4efc4a25b7086198f4fa229e04f8c2c70ec
Robert Varga <robert.varga@pantheon.tech>
2021-01-13 17:03:03 +0100

    Update module-info.java
    Add explicit requires, as Eclipse does not trust these coming from
    upstreams.
    
    Change-Id: I8ea7bd2c9b8b930f4069fbece86df40f76dbb370
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/module-info.java
+++ yang/yang-parser-rfc7950/src/main/java/module-info.java
@@ -20,7 +20,9 @@ module org.opendaylight.yangtools.yang.parser.rfc7950 {
     requires transitive org.opendaylight.yangtools.yang.parser.reactor;
     requires transitive org.opendaylight.yangtools.yang.parser.spi;
 
+    requires org.opendaylight.yangtools.concepts;
     requires org.opendaylight.yangtools.openconfig.model.api;
+    requires org.opendaylight.yangtools.yang.common;
     requires org.opendaylight.yangtools.yang.model.api;
     requires org.opendaylight.yangtools.yang.model.util;
     requires org.opendaylight.yangtools.yang.parser.antlr;
