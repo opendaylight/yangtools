354070b8e1451f804f8a8131c7abd12123178dde
Robert Varga <robert.varga@pantheon.tech>
2021-01-22 14:59:01 +0100

    WIP: Refactor statement reuse
    It seems we still have a few issues in the area, as we get confused
    as to who goes where and how.
    
    JIRA: YANGTOOLS-1212
    Change-Id: Ic50e21e793d16d35170ce95561ec4e3f68cc6ef1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/ReplicaStatementContext.java
+++ yang/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/ReplicaStatementContext.java
@@ -36,14 +36,28 @@ final class ReplicaStatementContext<A, D extends DeclaredStatement<A>, E extends
     private final StatementContextBase<?, ?, ?> parent;
     private final ReactorStmtCtx<A, D, E> source;
 
-    ReplicaStatementContext(final StatementContextBase<?, ?, ?> parent, final ReactorStmtCtx<A, D, E> source) {
+    private ReplicaStatementContext(final StatementContextBase<?, ?, ?> parent, final ReactorStmtCtx<A, D, E> source) {
         super(source);
         this.parent = requireNonNull(parent);
         this.source = requireNonNull(source);
+        verify(!source.isSupportedToBuildEffective() || source.fullyDefined(),
+            "Source %s is not fully defined", source);
+    }
+
+    static <A, D extends DeclaredStatement<A>, E extends EffectiveStatement<A, D>>
+            @NonNull ReplicaStatementContext<A, D, E> noRef(final StatementContextBase<?, ?, ?> parent,
+                final ReactorStmtCtx<A, D, E> source) {
+        return new ReplicaStatementContext<>(parent, source);
+    }
+
+    static <A, D extends DeclaredStatement<A>, E extends EffectiveStatement<A, D>>
+            @NonNull ReplicaStatementContext<A, D, E> withRef(final StatementContextBase<?, ?, ?> parent,
+                final ReactorStmtCtx<A, D, E> source) {
+        final ReplicaStatementContext<A, D, E> ret = new ReplicaStatementContext<>(parent, source);
         if (source.isSupportedToBuildEffective()) {
-            verify(source.fullyDefined(), "Source %s is not fully defined", source);
             source.incRef();
         }
+        return ret;
     }
 
     @Override
@@ -107,7 +121,7 @@ final class ReplicaStatementContext<A, D extends DeclaredStatement<A>, E extends
     }
 
     @Override
-    public Mutable<A, D, E> replicaAsChildOf(final Mutable<?, ?, ?> newParent) {
+    ReplicaStatementContext<A, D, E> replicaAsChildOf(final StatementContextBase<?, ?, ?> newParent) {
         return source.replicaAsChildOf(newParent);
     }
 
