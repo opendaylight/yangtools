d213bc73c50c5d71f3b1d939680e54d851561af1
Robert Varga <robert.varga@pantheon.tech>
2021-01-25 13:32:35 +0100

    Cache substatement context-independence summary
    Individual statements expose their statically-known CopyPolicy,
    which governs how they react to being copied. This does not account
    the statement instance's substatements, as that is a
    partially-dynamic property.
    
    Even if a particular statement is affected by the copy operation,
    it is quite common for its substatement (tree) to not be affected --
    lending itself for reuse of the effective substatement list as well
    as any structures which are derived from it.
    
    Whenever a statement completes ModelProcessingPhase.EFFECTIVE_MODEL,
    examine all substatements and whether all of them are recursively
    context-independent.
    
    Expend our last remaining bit in ReactorStmtCtx.flags to remember
    the check's result -- preventing the need to recurse into a
    substatement's substatements.
    
    Also add an explicit unit test to show that 'when' with 'description'
    and 'reference' gets reused in its entirety.
    
    JIRA: YANGTOOLS-1209
    Change-Id: I0fda715da441038a1276678f49718df9bfacfb90
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/YT1209Test.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
+
+import java.net.URI;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.model.api.stmt.GroupingEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.LeafEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ModuleEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.WhenEffectiveStatement;
+
+public class YT1209Test {
+    @Test
+    public void testWhenStatementReuse() throws Exception {
+        final ModuleEffectiveStatement module = StmtTestUtils.parseYangSource("/bugs/YT1209/when.yang")
+            .getModuleStatements()
+            .get(QNameModule.create(URI.create("foo")));
+        assertNotNull(module);
+
+        final LeafEffectiveStatement grpFoo = module
+            .findFirstEffectiveSubstatement(GroupingEffectiveStatement.class).orElseThrow()
+            .findFirstEffectiveSubstatement(LeafEffectiveStatement.class).orElseThrow();
+        final LeafEffectiveStatement foo = module
+            .findFirstEffectiveSubstatement(LeafEffectiveStatement.class).orElseThrow();
+
+        // The statements should not be the same due SchemaPath being part of LeafSchemaNode
+        assertNotSame(foo, grpFoo);
+        // The statements are instantiated in the same module, hence they should have the same argument
+        assertSame(foo.argument(), grpFoo.argument());
+        // The statements' when substatement should be reused
+        assertSame(foo.findFirstEffectiveSubstatement(WhenEffectiveStatement.class).orElseThrow(),
+            grpFoo.findFirstEffectiveSubstatement(WhenEffectiveStatement.class).orElseThrow());
+    }
+}
