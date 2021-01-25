3281b9ce6ae7a66ee4d3b66179a227a04b2c1d2a
Robert Varga <robert.varga@pantheon.tech>
2021-01-25 13:32:35 +0100

    Cache substatement context-independence summary
    Individual statements expose their statically-known CopyPolicy,
    which governs how they react to being copied. This does not account
    the statement instance's substatements, as that is a
    partially-dynamic property.
    
    Even if a particular statement is affected by the copy operation,
    it is quite common for its substatement (tree) to not be affected --
    lending itself for reuse of the effective substatement list as well
    as any structures which are derived from it.
    
    Whenever a statement completes ModelProcessingPhase.EFFECTIVE_MODEL,
    examine all substatements and whether all of them are recursively
    context-independent.
    
    Expend our last remaining bit in ReactorStmtCtx.flags to remember
    the check's result -- preventing the need to recurse into a
    substatement's substatements.
    
    JIRA: YANGTOOLS-1209
    Change-Id: I0fda715da441038a1276678f49718df9bfacfb90
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/ReactorStmtCtx.java
+++ yang/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/ReactorStmtCtx.java
@@ -118,6 +118,13 @@ abstract class ReactorStmtCtx<A, D extends DeclaredStatement<A>, E extends Effec
     //        of this flag -- eliminating the initial alignment shadow used by below gap-filler fields.
     private boolean isSupportedToBuildEffective = true;
 
+    // EffectiveConfig mapping
+    private static final int MASK_CONFIG                = 0x03;
+    private static final int HAVE_CONFIG                = 0x04;
+    // Effective instantiation mechanics for StatementContextBase: if this flag is set all substatements are known not
+    // change when instantiated. This includes context-independent statements as well as any statements which are
+    // ignored during copy instantiation.
+    private static final int ALL_INDEPENDENT            = 0x08;
     // Flag bit assignments
     private static final int IS_SUPPORTED_BY_FEATURES   = 0x10;
     private static final int HAVE_SUPPORTED_BY_FEATURES = 0x20;
@@ -127,9 +134,6 @@ abstract class ReactorStmtCtx<A, D extends DeclaredStatement<A>, E extends Effec
     private static final int SET_SUPPORTED_BY_FEATURES  = HAVE_SUPPORTED_BY_FEATURES | IS_SUPPORTED_BY_FEATURES;
     private static final int SET_IGNORE_IF_FEATURE      = HAVE_IGNORE_IF_FEATURE | IS_IGNORE_IF_FEATURE;
 
-    // EffectiveConfig mapping
-    private static final int MASK_CONFIG = 0x03;
-    private static final int HAVE_CONFIG = 0x04;
     private static final EffectiveConfig[] EFFECTIVE_CONFIGS;
 
     static {
@@ -483,7 +487,7 @@ abstract class ReactorStmtCtx<A, D extends DeclaredStatement<A>, E extends Effec
         return false;
     }
 
-    // These two exists only due to memory optimization, should live in AbstractResumedStatement. We are also reusing
+    // These two exist only due to memory optimization, should live in AbstractResumedStatement. We are also reusing
     // this for ReplicaStatementContext's refcount tracking.
     final boolean fullyDefined() {
         return fullyDefined;
@@ -491,6 +495,17 @@ abstract class ReactorStmtCtx<A, D extends DeclaredStatement<A>, E extends Effec
 
     final void setFullyDefined() {
         fullyDefined = true;
+    }
+
+    // These two exist only for StatementContextBase. Since we are squeezed for size, with only a single bit available
+    // in flags, we default to 'false' and only set the flag to true when we are absolutely sure -- and all other cases
+    // err on the side of caution by taking the time to evaluate each substatement separately.
+    final boolean allSubstatementsContextIndependent() {
+        return (flags & ALL_INDEPENDENT) != 0;
+    }
+
+    final void setAllSubstatementsContextIndependent() {
+        flags |= ALL_INDEPENDENT;
     }
 
     //
