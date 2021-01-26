7b00331e640aed78b324624d564d25114711548f
miroslav.kovac <miroslav.kovac@pantheon.tech>
2021-01-26 14:55:23 +0100

    Remove SchemaPath from SchemaUtils class
    Signed-off-by: miroslav.kovac <miroslav.kovac@pantheon.tech>
    Change-Id: Ie6bbdef9fcb427df3e27072230f26dc00437940a

--- yang/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/schema/SchemaUtilsTest.java
+++ yang/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/schema/SchemaUtilsTest.java
@@ -10,6 +10,7 @@ package org.opendaylight.yangtools.yang.data.impl.schema;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import com.google.common.collect.ImmutableList;
 import java.util.Collection;
 import org.junit.Test;
 import org.opendaylight.yangtools.yang.common.QName;
@@ -19,7 +20,7 @@ import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.NotificationDefinition;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.api.SchemaNode;
-import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier.Absolute;
 import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
 
 public class SchemaUtilsTest {
@@ -29,11 +30,11 @@ public class SchemaUtilsTest {
     public void test() {
         final SchemaContext schemaContext = YangParserTestUtils.parseYangResource("/schema-utils-test/foo.yang");
         assertTrue(SchemaUtils.findDataParentSchemaOnPath(schemaContext,
-                SchemaPath.create(true, qN("my-name"), qN("my-name-a"))) instanceof ContainerSchemaNode);
+                Absolute.of(qN("my-name"), qN("my-name-a"))) instanceof ContainerSchemaNode);
         assertTrue(SchemaUtils.findDataParentSchemaOnPath(schemaContext,
-                SchemaPath.create(true, qN("my-name-2"), qN("my-name-b"))) instanceof NotificationDefinition);
+                Absolute.of(qN("my-name-2"), qN("my-name-b"))) instanceof NotificationDefinition);
         assertTrue(SchemaUtils.findDataParentSchemaOnPath(schemaContext,
-                SchemaPath.create(true, qN("my-name-2"), qN("my-name-2-b"))) instanceof ActionDefinition);
+                Absolute.of(qN("my-name-2"), qN("my-name-2-b"))) instanceof ActionDefinition);
     }
 
     @Test
@@ -42,31 +43,31 @@ public class SchemaUtilsTest {
                 .parseYangResource("/schema-utils-test/name-conflicts.yang");
         // test my-name conflicts
         assertEquals(8, SchemaUtils.findParentSchemaNodesOnPath(schemaContext,
-                SchemaPath.create(true, qN("my-name"), qN("my-name-nested"), qN("my-name-nested2"))).size());
+                ImmutableList.of(qN("my-name"), qN("my-name-nested"), qN("my-name-nested2"))).size());
 
         // test target container
         final Collection<SchemaNode> target = SchemaUtils.findParentSchemaNodesOnPath(schemaContext,
-                SchemaPath.create(true, qN("my-name-2"), qN("my-name-nested"), qN("target")));
+                ImmutableList.of(qN("my-name-2"), qN("my-name-nested"), qN("target")));
         assertEquals(1, target.size());
         assertTrue(target.iterator().next() instanceof ContainerSchemaNode);
 
         // test l schema nodes (i.e. container and two leafs)
         Collection<SchemaNode> schema = SchemaUtils.findParentSchemaNodesOnPath(schemaContext,
-                SchemaPath.create(true, qN("my-name-3"), qN("input"), qN("con-3"), qN("l")));
+                ImmutableList.of(qN("my-name-3"), qN("input"), qN("con-3"), qN("l")));
         assertEquals(1, schema.size());
         assertTrue(schema.iterator().next() instanceof ContainerSchemaNode);
 
         schema = SchemaUtils.findParentSchemaNodesOnPath(schemaContext,
-                SchemaPath.create(true, qN("my-name-3"), qN("input"), qN("con-1"), qN("l")));
+                ImmutableList.of(qN("my-name-3"), qN("input"), qN("con-1"), qN("l")));
         assertEquals(1, schema.size());
         assertTrue(schema.iterator().next() instanceof LeafSchemaNode);
 
         schema = SchemaUtils.findParentSchemaNodesOnPath(schemaContext,
-                SchemaPath.create(true, qN("my-name-3"), qN("input"), qN("con-2"), qN("l")));
+                ImmutableList.of(qN("my-name-3"), qN("input"), qN("con-2"), qN("l")));
         assertTrue(schema.isEmpty());
 
         schema = SchemaUtils.findParentSchemaNodesOnPath(schemaContext,
-                SchemaPath.create(true, qN("my-name-3"), qN("output"), qN("con-2"), qN("l")));
+                ImmutableList.of(qN("my-name-3"), qN("output"), qN("con-2"), qN("l")));
         assertEquals(1, schema.size());
         assertTrue(schema.iterator().next() instanceof LeafSchemaNode);
     }
