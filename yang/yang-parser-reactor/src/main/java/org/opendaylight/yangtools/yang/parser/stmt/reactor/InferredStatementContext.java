4f707c17dfd317df5ca14673be665e6ddb2df17a
Robert Varga <robert.varga@pantheon.tech>
2021-01-26 21:52:13 +0100

    WIP: short-circuit statement reuse
    // FIXME: add a unit test
    
    JIRA: YANGTOOLS-1212
    Change-Id: Ic50e21e793d16d35170ce95561ec4e3f68cc6ef1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/InferredStatementContext.java
+++ yang/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/InferredStatementContext.java
@@ -209,6 +209,14 @@ final class InferredStatementContext<A, D extends DeclaredStatement<A>, E extend
             return origEffective;
         }
 
+        // Quick check for independence
+        if (allSubstatementsContextIndependent()) {
+            LOG.debug("Reusing context-independent: {}", origEffective);
+            substatements = reusePrototypeReplicas();
+            prototype.decRef();
+            return origEffective;
+        }
+
         // We can reuse this statement let's see if all the statements agree
         final List<Entry<Mutable<?, ?, ?>, Mutable<?, ?, ?>>> declared = prototype.streamDeclared()
             .filter(StmtContext::isSupportedByFeatures)
