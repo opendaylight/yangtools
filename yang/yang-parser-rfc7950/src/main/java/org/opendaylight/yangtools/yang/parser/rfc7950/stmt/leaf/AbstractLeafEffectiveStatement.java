6daa12c3a15070504a729ab29dce0a6783873809
Robert Varga <robert.varga@pantheon.tech>
2021-01-30 14:29:46 +0100

    Reuse TypeDefinition across leaf copies
    Improve LeafStatementSupport to use TypeBuilder's ability to copy
    TypeDefinitions. This eliminates the need to search substatements
    and reuses internal TypeDefinition structures as well.
    
    JIRA: YANGTOOLS-1213
    Change-Id: I6e10877971902540bb448359baf0444d61993255
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/leaf/AbstractLeafEffectiveStatement.java
+++ yang/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/leaf/AbstractLeafEffectiveStatement.java
@@ -27,6 +27,7 @@ import org.opendaylight.yangtools.yang.model.api.stmt.TypeEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.UnitsEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.util.type.ConcreteTypeBuilder;
 import org.opendaylight.yangtools.yang.model.util.type.ConcreteTypes;
+import org.opendaylight.yangtools.yang.model.util.type.TypeBuilder;
 import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.AbstractDeclaredEffectiveStatement;
 import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.EffectiveStatementMixins.DataSchemaNodeMixin;
 import org.opendaylight.yangtools.yang.parser.rfc7950.stmt.EffectiveStatementMixins.MandatoryMixin;
@@ -49,6 +50,15 @@ abstract class AbstractLeafEffectiveStatement extends AbstractDeclaredEffectiveS
         this.type = buildType(argument);
         this.flags = flags;
         this.path = path;
+    }
+
+    AbstractLeafEffectiveStatement(final AbstractLeafEffectiveStatement original, final SchemaPath path,
+            final int flags) {
+        super(original);
+        this.substatements = original.substatements;
+        this.path = path;
+        this.flags = flags;
+        this.type = TypeBuilder.copyTypeDefinition(original.type, getQName());
     }
 
     @Override
