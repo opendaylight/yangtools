5fa88937eb5e22c77dd997e7f7966c2fa7a6108f
Robert Varga <robert.varga@pantheon.tech>
2021-02-01 12:28:48 +0100

    Add Guava requires
    Eclipse is still not happy, take another step.
    
    Change-Id: I0ba5aa38a9d07e87161e917caf3cf0064f2c753d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/main/java/module-info.java
+++ yang/yang-parser-rfc7950/src/main/java/module-info.java
@@ -20,6 +20,7 @@ module org.opendaylight.yangtools.yang.parser.rfc7950 {
     requires transitive org.opendaylight.yangtools.yang.parser.reactor;
     requires transitive org.opendaylight.yangtools.yang.parser.spi;
 
+    requires com.google.common;
     requires org.opendaylight.yangtools.concepts;
     requires org.opendaylight.yangtools.openconfig.model.api;
     requires org.opendaylight.yangtools.yang.common;
