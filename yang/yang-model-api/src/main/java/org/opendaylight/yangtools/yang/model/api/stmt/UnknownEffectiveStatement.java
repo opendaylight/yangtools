e1418800e5c7b34edbc0ac3347694e25ae7bcec8
Robert Varga <robert.varga@pantheon.tech>
2021-02-03 11:38:59 +0100

    Bridge UnknownSchemaNode to UnknownEffectiveStatement
    // FIXME: finish this up
    
    Change-Id: Ice955c1535cb487337a7f3b0aff19a9ce8d286d1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/UnknownEffectiveStatement.java
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/UnknownEffectiveStatement.java
@@ -11,6 +11,7 @@ import com.google.common.annotations.Beta;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 
 @Beta
+// FIXME: evaluate usefulness of this
 public interface UnknownEffectiveStatement<A> extends EffectiveStatement<A, UnknownStatement<A>> {
 
 }
