c2e209dc3a61f20bf707292c3a8dd23e7b769cc1
Robert Varga <robert.varga@pantheon.tech>
2021-02-08 14:34:13 +0100

    WIP: Add SchemaInferenceStack.resolvePathExpression()
    // FIXME: mold this patch into I64e4f4ef2062ab2a0d143a520a53e8972cf54b26
    
    Change-Id: I76425a6ed4c6d7648124086a490082e9189a8d1c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-model-util/src/main/java/org/opendaylight/yangtools/yang/model/util/LeafrefResolver.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.util;
+
+import com.google.common.annotations.Beta;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.LeafrefTypeDefinition;
+
+/**
+ * Helper interface aiding resolution of leafref chains.
+ */
+@Beta
+@FunctionalInterface
+public interface LeafrefResolver {
+    /**
+     * Resolve specified {@link LeafrefTypeDefinition} until a non-{@code leafref} type is found.
+     *
+     * @param type leafref definition
+     * @return Resolved type
+     * @throws NullPointerException if {@code type} is null
+     * @throws IllegalArgumentException if the type definition cannot be resolved
+     */
+    @NonNull TypeDefinition<?> resolveLeafref(@NonNull LeafrefTypeDefinition type);
+}
