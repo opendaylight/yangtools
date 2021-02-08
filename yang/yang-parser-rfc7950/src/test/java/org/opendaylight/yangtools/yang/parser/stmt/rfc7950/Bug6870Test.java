0c39c031f1a53bba511f381a2fdb30e8e51ed7aa
Robert Varga <robert.varga@pantheon.tech>
2021-02-08 20:19:05 +0100

    Modernize Bug6870Test
    Perform a long-overdue cleanup, improving assertions and eliminating
    the need for SchemaContextUtil.
    
    JIRA: YANGTOOLS-1052
    Change-Id: I24e8bfe62509e12a4158a99c673ff182bd5c2240
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/parser/stmt/rfc7950/Bug6870Test.java
+++ yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/parser/stmt/rfc7950/Bug6870Test.java
@@ -1,89 +1,72 @@
 /*
  * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.yangtools.yang.parser.stmt.rfc7950;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertThrows;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
 import java.util.List;
 import java.util.Optional;
 import org.junit.Test;
 import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
-import org.opendaylight.yangtools.yang.model.api.SchemaContext;
-import org.opendaylight.yangtools.yang.model.api.SchemaNode;
-import org.opendaylight.yangtools.yang.model.api.SchemaPath;
 import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
 import org.opendaylight.yangtools.yang.model.api.type.ModifierKind;
 import org.opendaylight.yangtools.yang.model.api.type.PatternConstraint;
 import org.opendaylight.yangtools.yang.model.api.type.StringTypeDefinition;
-import org.opendaylight.yangtools.yang.model.util.SchemaContextUtil;
-import org.opendaylight.yangtools.yang.parser.spi.meta.SomeModifiersUnresolvedException;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
 import org.opendaylight.yangtools.yang.stmt.StmtTestUtils;
 
 public class Bug6870Test {
-    private static final String FOO_NS = "foo";
-
     @Test
     public void valid11Test() throws Exception {
-        final SchemaContext schemaContext = StmtTestUtils.parseYangSource("/rfc7950/bug6870/foo.yang");
-        assertNotNull(schemaContext);
+        final EffectiveModelContext schemaContext = StmtTestUtils.parseYangSource("/rfc7950/bug6870/foo.yang");
 
-        assertModifier(schemaContext, ModifierKind.INVERT_MATCH, ImmutableList.of("root", "my-leaf"));
-        assertModifier(schemaContext, null, ImmutableList.of("root", "my-leaf-2"));
+        assertModifier(schemaContext, ModifierKind.INVERT_MATCH, QName.create("foo", "root"),
+            QName.create("foo", "my-leaf"));
+        assertModifier(schemaContext, null, QName.create("foo", "root"), QName.create("foo", "my-leaf-2"));
     }
 
-    private static void assertModifier(final SchemaContext schemaContext, final ModifierKind expectedModifierKind,
-            final List<String> localNamePath) {
-        final SchemaNode findNode = findNode(schemaContext, localNamePath);
-        assertTrue(findNode instanceof LeafSchemaNode);
+    private static void assertModifier(final EffectiveModelContext schemaContext,
+            final ModifierKind expectedModifierKind, final QName... qnames) {
+        final DataSchemaNode findNode = schemaContext.findDataTreeChild(qnames).orElseThrow();
+        assertThat(findNode, instanceOf(LeafSchemaNode.class));
         final LeafSchemaNode myLeaf = (LeafSchemaNode) findNode;
 
         final TypeDefinition<? extends TypeDefinition<?>> type = myLeaf.getType();
-        assertTrue(type instanceof StringTypeDefinition);
-        final StringTypeDefinition stringType = (StringTypeDefinition) type;
-
-        final List<PatternConstraint> patternConstraints = stringType.getPatternConstraints();
+        assertThat(type, instanceOf(StringTypeDefinition.class));
+        final List<PatternConstraint> patternConstraints = ((StringTypeDefinition) type).getPatternConstraints();
         assertEquals(1, patternConstraints.size());
-
-        final PatternConstraint patternConstraint = patternConstraints.iterator().next();
-        assertEquals(Optional.ofNullable(expectedModifierKind), patternConstraint.getModifier());
-    }
-
-    private static SchemaNode findNode(final SchemaContext context, final Iterable<String> localNamesPath) {
-        final Iterable<QName> qNames = Iterables.transform(localNamesPath,
-            localName -> QName.create(FOO_NS, localName));
-        return SchemaContextUtil.findDataSchemaNode(context, SchemaPath.create(qNames, true));
+        assertEquals(Optional.ofNullable(expectedModifierKind), patternConstraints.iterator().next().getModifier());
     }
 
     @Test
-    public void invalid11Test() throws Exception {
-        try {
-            StmtTestUtils.parseYangSource("/rfc7950/bug6870/invalid11.yang");
-            fail("Test should fail due to invalid Yang 1.1");
-        } catch (final SomeModifiersUnresolvedException e) {
-            assertTrue(e.getCause().getMessage()
-                    .startsWith("'Invert-match' is not valid argument of modifier statement"));
-        }
+    public void invalid11Test() {
+        final ReactorException ex = assertThrows(ReactorException.class,
+            () -> StmtTestUtils.parseYangSource("/rfc7950/bug6870/invalid11.yang"));
+        final Throwable cause = ex.getCause();
+        assertThat(cause, instanceOf(SourceException.class));
+        assertThat(cause.getMessage(), startsWith("'Invert-match' is not valid argument of modifier statement"));
     }
 
     @Test
-    public void invalid10Test() throws Exception {
-        try {
-            StmtTestUtils.parseYangSource("/rfc7950/bug6870/invalid10.yang");
-            fail("Test should fail due to invalid Yang 1.0");
-        } catch (final SomeModifiersUnresolvedException e) {
-            assertTrue(e.getCause().getMessage().startsWith("modifier is not a YANG statement or use of extension"));
-        }
+    public void invalid10Test() {
+        final ReactorException ex = assertThrows(ReactorException.class,
+            () -> StmtTestUtils.parseYangSource("/rfc7950/bug6870/invalid10.yang"));
+        final Throwable cause = ex.getCause();
+        assertThat(cause, instanceOf(SourceException.class));
+        assertThat(cause.getMessage(), startsWith("modifier is not a YANG statement or use of extension"));
     }
 }
