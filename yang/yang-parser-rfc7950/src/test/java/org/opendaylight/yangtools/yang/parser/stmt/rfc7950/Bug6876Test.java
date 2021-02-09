dc96f7f93680fa42421fae3b607c9f0a66331fdd
Robert Varga <robert.varga@pantheon.tech>
2021-02-09 16:36:22 +0100

    Cleanup more tests
    Bug6876Test, Bug6884Test and Bug6335Test are very simple,
    modernize them, eliminating dependencies on SchemaContextUtil.
    
    JIRA: YANGTOOLS-1052
    Change-Id: If16de321a712996728df0236795d1d62ee6edd8b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/parser/stmt/rfc7950/Bug6876Test.java
+++ yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/parser/stmt/rfc7950/Bug6876Test.java
@@ -10,35 +10,25 @@ package org.opendaylight.yangtools.yang.parser.stmt.rfc7950;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.startsWith;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
 
-import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
-import org.opendaylight.yangtools.yang.model.api.SchemaContext;
-import org.opendaylight.yangtools.yang.model.api.SchemaNode;
-import org.opendaylight.yangtools.yang.model.api.SchemaPath;
-import org.opendaylight.yangtools.yang.model.util.SchemaContextUtil;
 import org.opendaylight.yangtools.yang.parser.spi.meta.InferenceException;
 import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
 import org.opendaylight.yangtools.yang.stmt.StmtTestUtils;
 
 public class Bug6876Test {
-    private static final String BAR_NS = "bar";
-    private static final String BAR_REV = "2017-01-11";
-    private static final String FOO_NS = "foo";
-
     @Test
     public void yang11Test() throws Exception {
-        final SchemaContext context = StmtTestUtils.parseYangSources("/rfc7950/bug6876/yang11");
-        assertNotNull(context);
-
-        assertThat(findNode(context, ImmutableList.of(bar("augment-target"), bar("my-leaf"))),
-            instanceOf(LeafSchemaNode.class));
-        assertThat(findNode(context, ImmutableList.of(bar("augment-target"), foo("mandatory-leaf"))),
-            instanceOf(LeafSchemaNode.class));
+        final EffectiveModelContext context = StmtTestUtils.parseYangSources("/rfc7950/bug6876/yang11");
+        DataSchemaNode node = context.findDataTreeChild(bar("augment-target"), bar("my-leaf")).orElse(null);
+        assertThat(node, instanceOf(LeafSchemaNode.class));
+        node = context.findDataTreeChild(bar("augment-target"), foo("mandatory-leaf")).orElse(null);
+        assertThat(node, instanceOf(LeafSchemaNode.class));
     }
 
     @Test
@@ -51,15 +41,11 @@ public class Bug6876Test {
             "An augment cannot add node 'mandatory-leaf' because it is mandatory and in module different than target"));
     }
 
-    private static SchemaNode findNode(final SchemaContext context, final Iterable<QName> qnames) {
-        return SchemaContextUtil.findDataSchemaNode(context, SchemaPath.create(qnames, true));
-    }
-
     private static QName foo(final String localName) {
-        return QName.create(FOO_NS, localName);
+        return QName.create("foo", localName);
     }
 
     private static QName bar(final String localName) {
-        return QName.create(BAR_NS, BAR_REV, localName);
+        return QName.create("bar", "2017-01-11", localName);
     }
 }
--- yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug5335Test.java
+++ yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug5335Test.java
@@ -10,17 +10,14 @@ package org.opendaylight.yangtools.yang.stmt;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.startsWith;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
-import org.opendaylight.yangtools.yang.model.api.SchemaNode;
-import org.opendaylight.yangtools.yang.model.api.SchemaPath;
-import org.opendaylight.yangtools.yang.model.util.SchemaContextUtil;
 import org.opendaylight.yangtools.yang.parser.spi.meta.InferenceException;
 import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
 
@@ -57,7 +54,7 @@ public class Bug5335Test {
     }
 
     @Test
-    public void incorrectTest3() throws Exception {
+    public void incorrectTest3() {
         final ReactorException ex = assertThrows(ReactorException.class,
             () -> StmtTestUtils.parseYangSources("/bugs/bug5335/incorrect/case-2"));
         final Throwable cause = ex.getCause();
@@ -68,43 +65,33 @@ public class Bug5335Test {
 
     @Test
     public void correctTest1() throws Exception {
-        final SchemaContext context = StmtTestUtils.parseYangSources("/bugs/bug5335/correct/case-1");
-        assertNotNull(context);
-
-        final SchemaPath schemaPath = SchemaPath.create(true, ROOT, PRESENCE_CONTAINER_B, MANDATORY_LEAF_B);
-        final SchemaNode mandatoryLeaf = SchemaContextUtil.findDataSchemaNode(context, schemaPath);
-        assertTrue(mandatoryLeaf instanceof LeafSchemaNode);
+        final EffectiveModelContext context = StmtTestUtils.parseYangSources("/bugs/bug5335/correct/case-1");
+        final DataSchemaNode mandatoryLeaf = context.findDataTreeChild(ROOT, PRESENCE_CONTAINER_B, MANDATORY_LEAF_B)
+            .orElse(null);
+        assertThat(mandatoryLeaf, instanceOf(LeafSchemaNode.class));
     }
 
     @Test
     public void correctTest2() throws Exception {
-        final SchemaContext context = StmtTestUtils.parseYangSources("/bugs/bug5335/correct/case-2");
-        assertNotNull(context);
-
-        final SchemaPath schemaPath = SchemaPath.create(true, ROOT, PRESENCE_CONTAINER_B, NON_PRESENCE_CONTAINER_B,
-                MANDATORY_LEAF_B);
-        final SchemaNode mandatoryLeaf = SchemaContextUtil.findDataSchemaNode(context, schemaPath);
-        assertTrue(mandatoryLeaf instanceof LeafSchemaNode);
+        final EffectiveModelContext context = StmtTestUtils.parseYangSources("/bugs/bug5335/correct/case-2");
+        final DataSchemaNode mandatoryLeaf = context.findDataTreeChild(ROOT, PRESENCE_CONTAINER_B,
+            NON_PRESENCE_CONTAINER_B, MANDATORY_LEAF_B).orElse(null);
+        assertThat(mandatoryLeaf, instanceOf(LeafSchemaNode.class));
     }
 
     @Test
     public void correctTest3() throws Exception {
-        final SchemaContext context = StmtTestUtils.parseYangSources("/bugs/bug5335/correct/case-3");
-        assertNotNull(context);
-
-        final SchemaPath schemaPath = SchemaPath.create(true, ROOT, PRESENCE_CONTAINER_B, NON_PRESENCE_CONTAINER_B,
-                MANDATORY_LEAF_B);
-        final SchemaNode mandatoryLeaf = SchemaContextUtil.findDataSchemaNode(context, schemaPath);
-        assertTrue(mandatoryLeaf instanceof LeafSchemaNode);
+        final EffectiveModelContext context = StmtTestUtils.parseYangSources("/bugs/bug5335/correct/case-3");
+        final DataSchemaNode mandatoryLeaf = context.findDataTreeChild(ROOT, PRESENCE_CONTAINER_B,
+            NON_PRESENCE_CONTAINER_B, MANDATORY_LEAF_B).orElse(null);
+        assertThat(mandatoryLeaf, instanceOf(LeafSchemaNode.class));
     }
 
     @Test
     public void correctTest4() throws Exception {
         final SchemaContext context = StmtTestUtils.parseYangSources("/bugs/bug5335/correct/case-4");
-        assertNotNull(context);
-
-        final SchemaPath schemaPath = SchemaPath.create(true, ROOT, NON_PRESENCE_CONTAINER_F, MANDATORY_LEAF_F);
-        final SchemaNode mandatoryLeaf = SchemaContextUtil.findDataSchemaNode(context, schemaPath);
-        assertTrue(mandatoryLeaf instanceof LeafSchemaNode);
+        final DataSchemaNode mandatoryLeaf = context.findDataTreeChild(ROOT, NON_PRESENCE_CONTAINER_F, MANDATORY_LEAF_F)
+            .orElse(null);
+        assertThat(mandatoryLeaf, instanceOf(LeafSchemaNode.class));
     }
 }
