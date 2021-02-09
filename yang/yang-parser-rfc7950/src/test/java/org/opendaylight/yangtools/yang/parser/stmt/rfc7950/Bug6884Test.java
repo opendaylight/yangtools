361ea3ed5ab6f1575eac6f8efa910338149b53a5
Robert Varga <robert.varga@pantheon.tech>
2021-02-09 16:36:22 +0100

    Cleanup more tests
    Bug6876Test, Bug6884Test and Bug6335Test are very simple,
    modernize them, eliminating dependencies on SchemaContextUtil.
    
    JIRA: YANGTOOLS-1052
    Change-Id: If16de321a712996728df0236795d1d62ee6edd8b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/parser/stmt/rfc7950/Bug6884Test.java
+++ yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/parser/stmt/rfc7950/Bug6884Test.java
@@ -7,47 +7,36 @@
  */
 package org.opendaylight.yangtools.yang.parser.stmt.rfc7950;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
-import org.opendaylight.yangtools.yang.model.api.SchemaContext;
-import org.opendaylight.yangtools.yang.model.api.SchemaNode;
-import org.opendaylight.yangtools.yang.model.api.SchemaPath;
-import org.opendaylight.yangtools.yang.model.util.SchemaContextUtil;
+import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.stmt.StmtTestUtils;
 
 public class Bug6884Test {
-    private static final String FOO_NS = "foo";
-
     @Test
     public void testYang11() throws Exception {
-        final SchemaContext schemaContext = StmtTestUtils.parseYangSources("/rfc7950/bug6884/yang1-1");
-        assertNotNull(schemaContext);
-
-        assertTrue(findNode(schemaContext, ImmutableList.of(foo("sub-root"), foo("sub-foo-2-con")))
-            instanceof ContainerSchemaNode);
+        final EffectiveModelContext schemaContext = StmtTestUtils.parseYangSources("/rfc7950/bug6884/yang1-1");
+        final DataSchemaNode node = schemaContext.findDataTreeChild(foo("sub-root"), foo("sub-foo-2-con")).orElse(null);
+        assertThat(node, instanceOf(ContainerSchemaNode.class));
     }
 
     @Test
     public void testCircularIncludesYang10() throws Exception {
-        final SchemaContext schemaContext = StmtTestUtils.parseYangSources("/rfc7950/bug6884/circular-includes");
+        final EffectiveModelContext schemaContext =
+            StmtTestUtils.parseYangSources("/rfc7950/bug6884/circular-includes");
+        DataSchemaNode node = schemaContext.findDataTreeChild(foo("sub-root"), foo("sub-foo-2-con")).orElse(null);
+        assertThat(node, instanceOf(ContainerSchemaNode.class));
 
-        assertNotNull(schemaContext);
-        assertTrue(findNode(schemaContext, ImmutableList.of(foo("sub-root"), foo("sub-foo-2-con")))
-            instanceof ContainerSchemaNode);
-        assertTrue(findNode(schemaContext, ImmutableList.of(foo("sub-root-2"), foo("sub-foo-con")))
-            instanceof ContainerSchemaNode);
-    }
-
-    private static SchemaNode findNode(final SchemaContext context, final Iterable<QName> qnames) {
-        return SchemaContextUtil.findDataSchemaNode(context, SchemaPath.create(qnames, true));
+        node = schemaContext.findDataTreeChild(foo("sub-root-2"), foo("sub-foo-con")).orElse(null);
+        assertThat(node, instanceOf(ContainerSchemaNode.class));
     }
 
     private static QName foo(final String localName) {
-        return QName.create(FOO_NS, localName);
+        return QName.create("foo", localName);
     }
 }
