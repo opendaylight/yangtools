e4d29b2545a81d6e6e8cb6df8003ed4451c18311
Robert Varga <robert.varga@pantheon.tech>
2021-02-10 14:51:13 +0100

    Fix missing location in error reports
    There are two places where we fail to propagate location:
    - one is a pure omission
    - one is a case of bad format
    Fix both sites, improving our error reporting.
    
    Change-Id: I65b52c69a6d6d7bb0fd8a9bc3956a2dab51d81c5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-data-codec-xml/src/main/java/org/opendaylight/yangtools/yang/data/codec/xml/XmlParserStream.java
+++ yang/yang-data-codec-xml/src/main/java/org/opendaylight/yangtools/yang/data/codec/xml/XmlParserStream.java
@@ -491,7 +491,8 @@ public final class XmlParserStream implements Closeable, Flushable {
                         } else if (parentSchema instanceof ListSchemaNode) {
                             optMount = MountPointSchemaNode.streamAll((ListSchemaNode) parentSchema).findFirst();
                         } else {
-                            throw new XMLStreamException("Unhandled mount-aware schema " + parentSchema);
+                            throw new XMLStreamException("Unhandled mount-aware schema " + parentSchema,
+                                in.getLocation());
                         }
 
                         if (optMount.isPresent()) {
@@ -517,7 +518,7 @@ public final class XmlParserStream implements Closeable, Flushable {
                     if (strictParsing) {
                         throw new XMLStreamException(String.format(
                             "Schema for node with name %s and namespace %s does not exist at %s", xmlElementName,
-                            elementNS, parentSchema.getPath(), in.getLocation()));
+                            elementNS, parentSchema.getPath()), in.getLocation());
                     }
 
                     LOG.debug("Skipping unknown node ns=\"{}\" localName=\"{}\" at path {}", elementNS, xmlElementName,
