c2b34e7b3e4af3870d6835d08eec1f1526713c2c
Robert Varga <robert.varga@pantheon.tech>
2021-02-10 15:15:45 +0100

    Add module-info for yang-data-codec-xml
    Add a a simple module-info.java, eliminating the need to configure
    maven-bundle-plugin.
    
    JIRA: YANGTOOLS-1266
    Change-Id: I0da9d85efcbaf909b62022dbbaa06210559cdb7a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-data-codec-xml/src/main/java/module-info.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.yangtools.yang.data.codec.xml {
+    exports org.opendaylight.yangtools.yang.data.codec.xml;
+
+    requires transitive org.opendaylight.yangtools.yang.data.api;
+    requires transitive org.opendaylight.yangtools.yang.data.util;
+    requires transitive org.opendaylight.yangtools.yang.model.api;
+    requires transitive org.opendaylight.yangtools.rfc7952.data.api;
+    requires transitive org.opendaylight.yangtools.rfc8528.data.api;
+
+    requires java.xml;
+    requires com.google.common;
+    requires org.codehaus.stax2;
+    requires org.opendaylight.yangtools.util;
+    requires org.opendaylight.yangtools.yang.model.util;
+    requires org.opendaylight.yangtools.yang.data.impl;
+    requires org.opendaylight.yangtools.rfc7952.model.api;
+    requires org.opendaylight.yangtools.rfc8528.model.api;
+    requires org.opendaylight.yangtools.rfc8528.data.util;
+    requires org.slf4j;
+
+    // Annotations
+    requires static org.eclipse.jdt.annotation;
+}
