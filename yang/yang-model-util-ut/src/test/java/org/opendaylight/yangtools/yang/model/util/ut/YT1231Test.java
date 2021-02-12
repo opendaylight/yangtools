a506cbc574a26e3edce40f8016692ba942b450b5
Robert Varga <robert.varga@pantheon.tech>
2021-02-12 01:16:51 +0100

    Add SchemaInferenceStack.enterChoice()
    yang-data-impl's SchemaTracker has a use case where it needs to track
    choice statements, but does not track case statements. This is quite
    usual for things interacting with NormalizedNodes -- and therefore we
    provide an adequate utility in SchemaInferenceStack.
    
    JIRA: YANGTOOLS-1235
    Change-Id: Ie0936f2dad16c938fa502efe250e368f6440279a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-model-util-ut/src/test/java/org/opendaylight/yangtools/yang/model/util/ut/YT1231Test.java
+++ yang/yang-model-util-ut/src/test/java/org/opendaylight/yangtools/yang/model/util/ut/YT1231Test.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -47,5 +47,27 @@ public class YT1231Test {
         assertThat(stack.enterSchemaTree(BAR), instanceOf(ChoiceEffectiveStatement.class));
         assertThat(stack.enterSchemaTree(BAR), instanceOf(CaseEffectiveStatement.class));
         assertEquals(Absolute.of(FOO, BAR, BAR), stack.toSchemaNodeIdentifier());
+    }
+
+    @Test
+    public void testEnterChoice() {
+        final EffectiveModelContext context = YangParserTestUtils.parseYangResource("/yt1231.yang");
+        final SchemaInferenceStack stack = new SchemaInferenceStack(context);
+
+        // Simple
+        assertThat(stack.enterDataTree(FOO), instanceOf(ContainerEffectiveStatement.class));
+        assertEquals(Absolute.of(FOO), stack.toSchemaNodeIdentifier());
+        assertThat(stack.enterChoice(BAR), instanceOf(ChoiceEffectiveStatement.class));
+        assertEquals(Absolute.of(FOO, BAR), stack.toSchemaNodeIdentifier());
+
+        // Has to cross choice -> case -> choice
+        assertThat(stack.enterChoice(BAZ), instanceOf(ChoiceEffectiveStatement.class));
+        assertEquals(Absolute.of(FOO, BAR, BAR, BAZ), stack.toSchemaNodeIdentifier());
+
+        // Now the same with just case -> choice
+        stack.exit();
+        assertThat(stack.enterSchemaTree(BAR), instanceOf(CaseEffectiveStatement.class));
+        assertThat(stack.enterChoice(BAZ), instanceOf(ChoiceEffectiveStatement.class));
+        assertEquals(Absolute.of(FOO, BAR, BAR, BAZ), stack.toSchemaNodeIdentifier());
     }
 }
