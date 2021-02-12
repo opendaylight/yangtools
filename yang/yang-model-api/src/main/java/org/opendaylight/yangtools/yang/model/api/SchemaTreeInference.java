0ef288db09753e0cc201206c1b5fdcd0459af1c5
Robert Varga <robert.varga@pantheon.tech>
2021-02-12 16:20:56 +0100

    Add SchemaTreeInference
    SchemaTreeInference is a nice inference point, firmly rooted in
    RFC7950 concepts. Also wire it to SchemaInferenceStack, which can
    support it quite easily.
    
    JIRA: YANGTOOLS-1240
    Change-Id: I746e5a4c17af3e4ca17c24dc15b7cb3533b48d8f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/SchemaTreeInference.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.api;
+
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeEffectiveStatement;
+
+/**
+ * An {@link EffectiveStatementInference} consisting purely of steps along the {@code schema tree} axis, so that it
+ * represents a {@code schema tree node} based on resolution of {@code absolute-schema-nodeid} as specified by
+ * <a href="https://tools.ietf.org/html/rfc7950#section-6.5">RFC7950 section 6.5</a>.
+ */
+public interface SchemaTreeInference extends EffectiveStatementInference {
+    /**
+     * {@inheritDoc}
+     *
+     * <p>
+     * The statement path is always composed on {@link SchemaTreeEffectiveStatement}s and contains at least one element.
+     * The path is ordered from conceptual YANG root, i.e. the first element corresponds to the first element in
+     * {@link SchemaNodeIdentifier.Absolute#firstNodeIdentifier()}.
+     */
+    @Override
+    List<@NonNull SchemaTreeEffectiveStatement<?>> statementPath();
+
+    /**
+     * Return the {@link SchemaNodeIdentifier.Absolute} which resulted in this inference.
+     *
+     * <p>
+     * Default implementation interprets {@link #statementPath()}'s arguments as the ordered source of
+     * {@link SchemaNodeIdentifier.Absolute} steps.
+     *
+     * @return An absolute SchemaNodeIdentifier
+     */
+    // FIXME: 8.0.0: get @implSpec working, see
+    //        https://stackoverflow.com/questions/38707030/maven-javadoc-plugin-and-inheritdoc-for-java-api-core-classes
+    default SchemaNodeIdentifier.Absolute toSchemaNodeIdentifier() {
+        return SchemaNodeIdentifier.Absolute.of(statementPath().stream()
+            .map(SchemaTreeEffectiveStatement::argument)
+            .collect(ImmutableList.toImmutableList()));
+    }
+}
