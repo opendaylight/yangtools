8fbd734d4830189ca41e5cc66f05fa1c20aeafed
Konstantin.Nosach <Kostiantyn.Nosach@pantheon.tech>
2021-02-12 14:31:57 +0200

    Improve NormalizedNode and modification formatting capabilities
    - provide NormalizedNode#prettyTree method to get namespace
    compressed pretty tree string representation of NormalizedNode
    objects
    
    - provide InMemoryDataTreeModification#prettyTree method to get
    namespace compressed pretty tree string representation
    of InMemoryDataTreeModification objects
    
    JIRA: YANGTOOLS-1203
    Change-Id: I6f2c98bc3048a086063846699ec73e0f2ac02b33
    Signed-off-by: Kostiantyn Nosach <kostiantyn.nosach@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ data/yang-data-spi/src/main/java/org/opendaylight/yangtools/yang/data/spi/node/NormalizedNodePrettyTree.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.spi.node;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Locale;
+import java.util.function.Supplier;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodeContainer;
+
+final class NormalizedNodePrettyTree implements Supplier<String> {
+    private static final int INDENT = 4;
+
+    private final @NonNull NormalizedNode node;
+    private final @NonNull Class<?> type;
+    private final @Nullable PathArgument parentIdentifier;
+    private final int initialOffset;
+
+    NormalizedNodePrettyTree(final @NonNull NormalizedNode node, final @NonNull Class<?> type) {
+        this.node = requireNonNull(node);
+        this.type = requireNonNull(type);
+        this.parentIdentifier = null;
+        this.initialOffset = 0;
+    }
+
+    NormalizedNodePrettyTree(final @NonNull NormalizedNode node, final @NonNull Class<?> type,
+            final @NonNull PathArgument parentIdentifier, final int initialOffset) {
+        this.node = requireNonNull(node);
+        this.type = requireNonNull(type);
+        this.parentIdentifier = requireNonNull(parentIdentifier);
+        this.initialOffset = initialOffset;
+    }
+
+    @Override
+    public String toString() {
+        return get();
+    }
+
+    @Override
+    public String get() {
+        final StringBuilder sb = new StringBuilder();
+        appendIndent(sb, initialOffset);
+        appendClassName(sb, type).append("{identifier=")
+                .append(node.getIdentifier().toRelativeString(parentIdentifier));
+        appendNodeValue(sb, node, initialOffset);
+        return sb.toString();
+    }
+
+    private static void appendIndent(final StringBuilder sb, final int offset) {
+        for (int i = 0; i < offset; i++) {
+            sb.append(' ');
+        }
+    }
+
+    private static StringBuilder appendClassName(final StringBuilder sb, final Class<?> type) {
+        final String simpleName = type.getSimpleName();
+        return sb.append(simpleName.toLowerCase(Locale.ROOT).charAt(0)).append(simpleName, 1, simpleName.length());
+    }
+
+    private static void appendNodeValue(final StringBuilder sb, final NormalizedNode node, final int offset) {
+        if (node instanceof NormalizedNodeContainer) {
+            sb.append(", value=[");
+            for (final NormalizedNode child : ((NormalizedNodeContainer<?>) node).body()) {
+                sb.append('\n').append(child.prettyTree(node.getIdentifier(), offset + INDENT));
+            }
+            sb.append("]}");
+        } else {
+            sb.append(", value=").append(node.body()).append('}');
+        }
+    }
+}
