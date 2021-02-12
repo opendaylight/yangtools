b1ba48e77901327d0698e1e92895b2865d4567b8
Konstantin.Nosach <Kostiantyn.Nosach@pantheon.tech>
2021-02-12 14:31:57 +0200

    Improve NormalizedNode formatting capabilities
    In case of data tree operation errors we end up dumping the
    NormalizedNode involved in the operation, which does contain a lot of
    information, but it is in a very unfriendly format.
    
    JIRA: YANGTOOLS-1203
    Change-Id: Iab76b7a88cd3e60bafea23eaff7636308867fb22
    Signed-off-by: Kostiantyn Nosach <kostiantyn.nosach@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- data/yang-data-spi/src/main/java/org/opendaylight/yangtools/yang/data/spi/node/AbstractNormalizedNode.java
+++ data/yang-data-spi/src/main/java/org/opendaylight/yangtools/yang/data/spi/node/AbstractNormalizedNode.java
@@ -10,11 +10,14 @@ package org.opendaylight.yangtools.yang.data.spi.node;
 
 import com.google.common.annotations.Beta;
 import com.google.common.base.MoreObjects.ToStringHelper;
+import java.util.function.Supplier;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.concepts.AbstractIdentifiable;
 import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.yang.common.XMLNamespace;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodeContainer;
 
 /**
  * Abstract base class for {@link NormalizedNode} implementations.
@@ -57,4 +60,60 @@ public abstract class AbstractNormalizedNode<I extends PathArgument, T extends N
     protected abstract int valueHashCode();
 
     protected abstract boolean valueEquals(@NonNull T other);
+
+    @Override
+    @NonNull public Supplier<String> prettyTree() {
+        return new NormalizedNodePrettyTree();
+    }
+
+    public final class NormalizedNodePrettyTree implements Supplier<String> {
+        private static final int INDENT = 4;
+
+        @Override
+        public String toString() {
+            return get();
+        }
+
+        @Override
+        public String get() {
+            final StringBuilder sb = new StringBuilder();
+            toStringTree(sb, AbstractNormalizedNode.this, null, 0);
+            return sb.toString();
+        }
+
+        private void toStringTree(final StringBuilder sb, final AbstractNormalizedNode<?, ?> node,
+                final PathArgument parentIdentifier, final int offset) {
+            final PathArgument identifier = node.getIdentifier();
+
+            final XMLNamespace namespace = identifier.getNodeType().getNamespace();
+            final XMLNamespace parentNamespace = parentIdentifier == null
+                ? null : parentIdentifier.getNodeType().getNamespace();
+
+            sb.append("\n");
+            sb.append(" ".repeat(offset));
+            sb.append(changeClassName(node));
+            sb.append("{identifier=");
+
+            if (namespace.equals(parentNamespace)) {
+                sb.append(identifier.toRelativeString(parentIdentifier));
+            } else {
+                sb.append(identifier);
+            }
+
+            if (node instanceof NormalizedNodeContainer) {
+                sb.append(", value=[");
+                for (final NormalizedNode child : ((NormalizedNodeContainer<?,?>)node).body()) {
+                    toStringTree(sb, (AbstractNormalizedNode<?, ?>) child, identifier, offset + INDENT);
+                }
+                sb.append("]}");
+            } else {
+                sb.append(", value=").append(node.body()).append('}');
+            }
+        }
+
+        private String changeClassName(final AbstractNormalizedNode<?, ?> node) {
+            final String simpleName = node.implementedType().getSimpleName();
+            return Character.toLowerCase(simpleName.charAt(0)) + simpleName.substring(1);
+        }
+    }
 }
