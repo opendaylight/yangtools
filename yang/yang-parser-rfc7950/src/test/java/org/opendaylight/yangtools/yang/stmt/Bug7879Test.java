f118c37784a98799e0608c0574321a4a5172a48b
Robert Varga <robert.varga@pantheon.tech>
2021-02-12 17:53:47 +0100

    Expand SchemaTreeAwareEffectiveStatement.findSchemaTreeNode()
    Default implementation lends itself towards performing the heavy
    lifting required. Allows us to ditch some SchemaContextUtils
    dependencies in tests. There are more to follow.
    
    JIRA: YANGTOOLS-1052
    Change-Id: I0fda81c4bf537327f7de51888bed2269857cd8e9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug7879Test.java
+++ yang/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug7879Test.java
@@ -7,37 +7,35 @@
  */
 package org.opendaylight.yangtools.yang.stmt;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import java.util.Optional;
 import org.junit.Test;
 import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.XMLNamespace;
 import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
-import org.opendaylight.yangtools.yang.model.api.SchemaContext;
-import org.opendaylight.yangtools.yang.model.api.SchemaNode;
-import org.opendaylight.yangtools.yang.model.api.SchemaPath;
-import org.opendaylight.yangtools.yang.model.util.SchemaContextUtil;
+import org.opendaylight.yangtools.yang.model.api.stmt.ModuleEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeEffectiveStatement;
 
 public class Bug7879Test {
-    private static final String NS = "my-model-ns";
+    private static final XMLNamespace NS = XMLNamespace.of("my-model-ns");
 
     @Test
     public void test() throws Exception {
-        final SchemaContext context = TestUtils.parseYangSources("/bugs/bug7879");
-        assertNotNull(context);
+        final ModuleEffectiveStatement module = TestUtils.parseYangSources("/bugs/bug7879")
+            .getModuleStatement(QName.create(NS, "my-model"));
 
-        assertTrue(findNode(context, qN("my-alarm"), qN("my-content"), qN("my-event-container"))
-                instanceof ContainerSchemaNode);
-        final SchemaNode myEventValueLeaf = findNode(context, qN("my-alarm"), qN("my-content"), qN("my-event-value"));
-        assertTrue(myEventValueLeaf instanceof LeafSchemaNode);
-        assertEquals(Optional.of("new description"), myEventValueLeaf.getDescription());
-    }
+        final SchemaTreeEffectiveStatement<?> container = module.findSchemaTreeNode(
+            qN("my-alarm"), qN("my-content"), qN("my-event-container")).orElse(null);
+        assertThat(container, instanceOf(ContainerSchemaNode.class));
 
-    private static SchemaNode findNode(final SchemaContext context, final QName... qnames) {
-        return SchemaContextUtil.findDataSchemaNode(context, SchemaPath.create(true, qnames));
+        final SchemaTreeEffectiveStatement<?> leaf = module.findSchemaTreeNode(
+            qN("my-alarm"), qN("my-content"), qN("my-event-value")).orElse(null);
+        assertThat(leaf, instanceOf(LeafSchemaNode.class));
+        assertEquals(Optional.of("new description"), ((LeafSchemaNode) leaf).getDescription());
     }
 
     private static QName qN(final String localName) {
