7016c9d31f68dcb9c99f04d43e62d81bef85085f
Konstantin.Nosach <Kostiantyn.Nosach@pantheon.tech>
2021-02-12 14:31:57 +0200

    Improve NormalizedNode formatting capabilities
    In case of data tree operation errors we end up dumping the
    NormalizedNode involved in the operation, which does contain a lot of
    information, but it is in a very unfriendly format.
    
    JIRA: YANGTOOLS-1203
    Signed-off-by: Kostiantyn Nosach <kostiantyn.nosach@pantheon.tech>
    Change-Id: Iab76b7a88cd3e60bafea23eaff7636308867fb22

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/nodes/AbstractImmutableNormalizedNode.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/nodes/AbstractImmutableNormalizedNode.java
@@ -8,11 +8,13 @@
 package org.opendaylight.yangtools.yang.data.impl.schema.nodes;
 
 import com.google.common.base.MoreObjects.ToStringHelper;
+import java.util.function.Supplier;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.concepts.AbstractIdentifiable;
 import org.opendaylight.yangtools.concepts.Immutable;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodeContainer;
 
 public abstract class AbstractImmutableNormalizedNode<K extends PathArgument, N extends NormalizedNode>
         extends AbstractIdentifiable<PathArgument, K> implements NormalizedNode, Immutable {
@@ -43,9 +45,50 @@ public abstract class AbstractImmutableNormalizedNode<K extends PathArgument, N 
         return super.addToStringAttributes(toStringHelper).add("body", body());
     }
 
+    @Override
+    public Supplier<String> prettyTree() {
+        return new NormalizedNodePrettyTree();
+    }
+
     protected abstract @NonNull Class<N> implementedType();
 
     protected abstract int valueHashCode();
 
     protected abstract boolean valueEquals(@NonNull N other);
+
+    public final class NormalizedNodePrettyTree implements Supplier<String> {
+
+        @Override
+        public String toString() {
+            return get();
+        }
+
+        @Override
+        public String get() {
+            final StringBuilder sb = new StringBuilder();
+            toStringTree(sb, AbstractImmutableNormalizedNode.this, 0);
+            return sb.toString();
+        }
+
+        private void toStringTree(final StringBuilder sb, final NormalizedNode node, final int offset) {
+            final String prefix = " ".repeat(offset);
+            sb.append("\n" + prefix + changeClassName(node) + "{identifier=" + node.getIdentifier().toString());
+            if (node instanceof NormalizedNodeContainer) {
+                sb.append(", body=[");
+                for (NormalizedNode child : ((NormalizedNodeContainer<?,?>)node).body()) {
+                    toStringTree(sb, child, offset + 4);
+                }
+                sb.append("]}");
+            } else {
+                sb.append(", body=").append(node.body()).append('}');
+            }
+        }
+
+        private String changeClassName(final NormalizedNode node) {
+            String name = node.getClass().getSimpleName().replace("Immutable", "");
+            char [] charArr = name.toCharArray();
+            charArr[0] += 32;
+            return new String(charArr);
+        }
+    }
 }
