cfc3ae38c22d0342dc188dff3347a5f13bfe7e1c
Robert Varga <robert.varga@pantheon.tech>
2021-02-12 13:47:50 +0100

    Add yang.model.api.EffectiveStatementInference
    A number of users of SchemaInferenceStack would benefit from
    the ability to have it transportable with yang.model.api constructs.
    
    Add EffectiveStatementInference which serves as a generic structure
    to express such a transportable construct and specialize it in
    SchemaInferenceStack.Inference.
    
    JIRA: YANGTOOLS-1237
    Change-Id: I74f3435b8952b70c21864f31fccf079f5e86dd58
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/EffectiveStatementInference.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.api;
+
+import com.google.common.annotations.Beta;
+import java.util.List;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+
+/**
+ * A base class of an {@link EffectiveModelContext} context reasoning with regards to what logical sequence of
+ * {@link EffectiveStatement}s were considered. Implementations of this class may provide additional facts which were
+ * derived from the line of reasoning.
+ */
+@Beta
+public interface EffectiveStatementInference extends EffectiveModelContextProvider, Immutable {
+    /**
+     * An {@code Unmodifiable} {@link List} of {@link EffectiveStatement}s, ordered in some meaningful way.
+     *
+     * @return A List of EffectiveStatements
+     */
+    @NonNull List<@NonNull EffectiveStatement<?, ?>> statementPath();
+}
