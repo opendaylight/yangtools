1e9c4172995050b1ac3d3856c93c51916bc641da
Robert Varga <robert.varga@pantheon.tech>
2021-02-13 16:12:19 +0100

    WIP: Add the ability to retain SourceReference
    // FIXME: proper yang.model.api.meta.SourceReference
    // FIXME: reactor configuration
    // FIXME: StatementSupport integrace
    // FIXME: more statement forwarders
    
    Change-Id: I4e4d9567c83c08aab9de2a2685470835fadcd0d7
    JIRA: YANGTOOLS-1193
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-model-spi/src/main/java/org/opendaylight/yangtools/yang/model/spi/meta/ForwardingDeclaredStatement.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.spi.meta;
+
+import com.google.common.annotations.Beta;
+import java.util.Collection;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+
+/**
+ * Common base class for forwarding implementations of {@link DeclaredStatement}.
+ */
+@Beta
+public abstract class ForwardingDeclaredStatement<A, D extends DeclaredStatement<A>>
+        extends ForwardingModelStatement<A, D> implements DeclaredStatement<A> {
+    @Override
+    public String rawArgument() {
+        return delegate().rawArgument();
+    }
+
+    @Override
+    public Collection<? extends DeclaredStatement<?>> declaredSubstatements() {
+        return delegate().declaredSubstatements();
+    }
+
+    @Override
+    protected abstract @NonNull D delegate();
+}
---
+++ yang/yang-model-spi/src/main/java/org/opendaylight/yangtools/yang/model/spi/meta/ForwardingModelStatement.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.spi.meta;
+
+import com.google.common.collect.ForwardingObject;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.model.api.meta.ModelStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementOrigin;
+
+/**
+ * Common base class for forwarding implementations of {@link ModelStatement}.
+ */
+public abstract class ForwardingModelStatement<A, S extends ModelStatement<A>> extends ForwardingObject
+        implements ModelStatement<A> {
+    @Override
+    public StatementDefinition statementDefinition() {
+        return delegate().statementDefinition();
+    }
+
+    @Override
+    public A argument() {
+        return delegate().argument();
+    }
+
+    @Override
+    public StatementOrigin statementOrigin() {
+        return delegate().statementOrigin();
+    }
+
+
+    @Override
+    protected abstract @NonNull S delegate();
+}
