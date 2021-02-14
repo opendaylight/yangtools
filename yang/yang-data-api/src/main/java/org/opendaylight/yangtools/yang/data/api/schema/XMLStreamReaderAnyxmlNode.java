dc89e4e2ad6e981f340f8c8e7b5346186859a1ef
Robert Varga <robert.varga@pantheon.tech>
2021-02-14 23:21:17 +0100

    WIP: Add XMLStreamReaderAnyxmlNode
    Change-Id: I5c8f83457790ea44c674c5f5349ac879b4e210ae
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/XMLStreamReaderAnyxmlNode.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.api.schema;
+
+import com.google.common.annotations.Beta;
+import javax.xml.stream.XMLStreamReader;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.yang.data.api.schema.XMLStreamReaderAnyxmlNode.Body;
+
+// FIXME: move this to yang.data.spi
+@Beta
+public interface XMLStreamReaderAnyxmlNode extends AnyxmlNode<Body> {
+    @Beta
+    @FunctionalInterface
+    public interface Body extends Immutable {
+        /**
+         * Return {@link XMLStreamReader} reporting events constituting this node. Returned the stream contains the
+         * {@code anyxml} node as the root element.
+         * @return A XMLStreamReader
+         */
+        @NonNull XMLStreamReader toXMLStreamReader();
+    }
+
+    @Override
+    default Class<Body> bodyObjectModel() {
+        return Body.class;
+    }
+}
