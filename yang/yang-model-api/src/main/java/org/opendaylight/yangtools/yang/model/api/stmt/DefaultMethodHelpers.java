96eb4e7ec26da01172eebf23331ae20657a3f70b
Robert Varga <robert.varga@pantheon.tech>
2021-02-17 13:04:12 +0100

    Update {Data,Schema}TreeAwareEffectiveStatemnt
    Use of wildcards here is dangerous, potentially leading to
    ClassCastExceptions due to how the APIs a designed. Update them
    with explicit Class references, so we end up filtering results
    properly.
    
    Change-Id: I804670a24ea0de4268605c7fcdd93e00d96bb422
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/DefaultMethodHelpers.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.api.stmt;
+
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNull;
+
+/**
+ * Simple helper methods used in default implementations.
+ */
+final class DefaultMethodHelpers {
+    private DefaultMethodHelpers() {
+        // Hidden on purpose
+    }
+
+    static <E> @NonNull Optional<E> filterOptional(final @NonNull Class<E> type, final @NonNull Optional<?> optional) {
+        return optional.filter(type::isInstance).map(type::cast);
+    }
+}
