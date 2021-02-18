110408e16ac1acdfae331af73198e9945d11459d
Robert Varga <robert.varga@pantheon.tech>
2021-02-18 09:18:41 +0100

    Remove InferredStatementContext.childCopyType
    This field is costing us object size in both 32bit and uncompressed
    64bit mode. Since the field only encodes 2 bits, move this storage
    to StatementContextBase.copyHistory, saving 8/8/0/0 bytes.
    
    // FIXME: consider CopyType routing, as we are performing a lookup
    //        before calling out, whereas we should be able to get away
    //        with passing a byte and have final recipient decode it
    
    JIRA: YANGTOOLS-1256
    Change-Id: Ifeb2cefd0449a4d9112f31a4c511a63dc5b0fb10
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/StatementContextBase.java
+++ yang/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/StatementContextBase.java
@@ -97,7 +97,7 @@ public abstract class StatementContextBase<A, D extends DeclaredStatement<A>, E 
     private static final Logger LOG = LoggerFactory.getLogger(StatementContextBase.class);
 
     //
-    // {@link CopyHistory} encoded as a single byte. We still have 4 bits unused.
+    // {@link CopyHistory} encoded as a single byte. We still have 2 bits unused, as 0x30 is used for childCopyType()
     //
     private static final byte COPY_LAST_TYPE_MASK        = 0x03;
     private static final byte COPY_ADDED_BY_USES         = 0x04;
@@ -150,9 +150,11 @@ public abstract class StatementContextBase<A, D extends DeclaredStatement<A>, E 
         this.copyHistory = (byte) copyFlags(copyType);
     }
 
-    StatementContextBase(final StatementContextBase<A, D, E> prototype, final CopyType copyType) {
+    StatementContextBase(final StatementContextBase<A, D, E> prototype, final CopyType copyType,
+        final CopyType childCopyType) {
         this.definition = prototype.definition;
-        this.copyHistory = (byte) (copyFlags(copyType) | prototype.copyHistory & ~COPY_LAST_TYPE_MASK);
+        this.copyHistory = (byte) (copyFlags(copyType)
+            | prototype.copyHistory & ~COPY_LAST_TYPE_MASK | childCopyType.ordinal() << 4);
     }
 
     private static int copyFlags(final CopyType copyType) {
@@ -213,6 +215,11 @@ public abstract class StatementContextBase<A, D extends DeclaredStatement<A>, E 
     @Override
     public final CopyType getLastOperation() {
         return CopyType.values()[copyHistory & COPY_LAST_TYPE_MASK];
+    }
+
+    // This method exists only for space optimization of InferredStatementContext
+    final CopyType childCopyType() {
+        return CopyType.values()[copyHistory >> 4 & COPY_LAST_TYPE_MASK];
     }
 
     //
