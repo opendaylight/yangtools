b33ceac32f8f51577288d0070639c120fbb811ec
Robert Varga <robert.varga@pantheon.tech>
2021-02-19 11:20:09 +0100

    Refactor SharedSchemaContextFactory
    Update SharedSchemaContextFactory design by implementing an explicit
    cache instead of attempting to use Guava. While this is a bit more
    code, it solves the problem of initial thundering heard causing
    concurrent EffectiveModelContext.
    
    JIRA: YANGTOOLS-1252
    Change-Id: If6e41a8611b650018a1b8961e9c945dfab1f1d88
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/repo/AssembleSources.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.repo;
+
+import static org.opendaylight.yangtools.util.concurrent.FluentFutures.immediateFluentFuture;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Maps;
+import com.google.common.util.concurrent.AsyncFunction;
+import com.google.common.util.concurrent.FluentFuture;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.concepts.SemVer;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.opendaylight.yangtools.yang.model.parser.api.YangParser;
+import org.opendaylight.yangtools.yang.model.parser.api.YangParserException;
+import org.opendaylight.yangtools.yang.model.parser.api.YangParserFactory;
+import org.opendaylight.yangtools.yang.model.parser.api.YangSyntaxErrorException;
+import org.opendaylight.yangtools.yang.model.repo.api.SchemaContextFactoryConfiguration;
+import org.opendaylight.yangtools.yang.model.repo.api.SchemaResolutionException;
+import org.opendaylight.yangtools.yang.model.repo.api.SemVerSourceIdentifier;
+import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
+import org.opendaylight.yangtools.yang.model.repo.api.StatementParserMode;
+import org.opendaylight.yangtools.yang.parser.rfc7950.ir.IRSchemaSource;
+import org.opendaylight.yangtools.yang.parser.rfc7950.repo.YangModelDependencyInfo;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+final class AssembleSources implements AsyncFunction<List<IRSchemaSource>, EffectiveModelContext> {
+    private static final Logger LOG = LoggerFactory.getLogger(SharedSchemaContextFactory.class);
+
+    private final @NonNull Function<IRSchemaSource, SourceIdentifier> getIdentifier;
+    private final @NonNull SchemaContextFactoryConfiguration config;
+    private final @NonNull YangParserFactory parserFactory;
+
+    AssembleSources(final @NonNull YangParserFactory parserFactory,
+            final @NonNull SchemaContextFactoryConfiguration config) {
+        this.parserFactory = parserFactory;
+        this.config = config;
+        switch (config.getStatementParserMode()) {
+            case SEMVER_MODE:
+                this.getIdentifier = AssembleSources::getSemVerIdentifier;
+                break;
+            default:
+                this.getIdentifier = IRSchemaSource::getIdentifier;
+        }
+    }
+
+    @Override
+    public FluentFuture<EffectiveModelContext> apply(final List<IRSchemaSource> sources)
+            throws SchemaResolutionException, ReactorException {
+        final Map<SourceIdentifier, IRSchemaSource> srcs = Maps.uniqueIndex(sources, getIdentifier);
+        final Map<SourceIdentifier, YangModelDependencyInfo> deps =
+                Maps.transformValues(srcs, YangModelDependencyInfo::forIR);
+
+        LOG.debug("Resolving dependency reactor {}", deps);
+
+        final StatementParserMode statementParserMode = config.getStatementParserMode();
+        final DependencyResolver res = statementParserMode == StatementParserMode.SEMVER_MODE
+                ? SemVerDependencyResolver.create(deps) : RevisionDependencyResolver.create(deps);
+        if (!res.getUnresolvedSources().isEmpty()) {
+            LOG.debug("Omitting models {} due to unsatisfied imports {}", res.getUnresolvedSources(),
+                res.getUnsatisfiedImports());
+            throw new SchemaResolutionException("Failed to resolve required models",
+                    res.getResolvedSources(), res.getUnsatisfiedImports());
+        }
+
+        final YangParser parser = parserFactory.createParser(statementParserMode);
+        config.getSupportedFeatures().ifPresent(parser::setSupportedFeatures);
+        config.getModulesDeviatedByModules().ifPresent(parser::setModulesWithSupportedDeviations);
+
+        for (final Entry<SourceIdentifier, IRSchemaSource> entry : srcs.entrySet()) {
+            try {
+                parser.addSource(entry.getValue());
+            } catch (YangSyntaxErrorException | IOException e) {
+                throw new SchemaResolutionException("Failed to add source " + entry.getKey(), e);
+            }
+        }
+
+        final EffectiveModelContext schemaContext;
+        try {
+            schemaContext = parser.buildEffectiveModel();
+        } catch (final YangParserException e) {
+            throw new SchemaResolutionException("Failed to resolve required models", e);
+        }
+
+        return immediateFluentFuture(schemaContext);
+    }
+
+    private static SemVerSourceIdentifier getSemVerIdentifier(final IRSchemaSource source) {
+        final SourceIdentifier identifier = source.getIdentifier();
+        final SemVer semver = YangModelDependencyInfo.findSemanticVersion(source.getRootStatement(), identifier);
+        if (identifier instanceof SemVerSourceIdentifier && semver == null) {
+            return (SemVerSourceIdentifier) identifier;
+        }
+
+        return SemVerSourceIdentifier.create(identifier.getName(), identifier.getRevision(), semver);
+    }
+}
---
+++ yang/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/repo/SourceIdMismatchDetector.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.repo;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.gaul.modernizer_maven_annotations.SuppressModernizer;
+import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
+import org.opendaylight.yangtools.yang.parser.rfc7950.ir.IRSchemaSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SuppressModernizer
+final class SourceIdMismatchDetector implements Function<List<IRSchemaSource>, List<IRSchemaSource>> {
+    private static final Logger LOG = LoggerFactory.getLogger(SourceIdMismatchDetector.class);
+
+    private final Set<SourceIdentifier> sourceIdentifiers;
+
+    SourceIdMismatchDetector(final Set<SourceIdentifier> sourceIdentifiers) {
+        this.sourceIdentifiers = requireNonNull(sourceIdentifiers);
+    }
+
+    @Override
+    public List<IRSchemaSource> apply(final List<IRSchemaSource> input) {
+        final Iterator<SourceIdentifier> srcIt = sourceIdentifiers.iterator();
+        final Iterator<IRSchemaSource> it = input.iterator();
+
+        final Map<SourceIdentifier, IRSchemaSource> filtered = new LinkedHashMap<>();
+        while (it.hasNext()) {
+            final IRSchemaSource irSchemaSource = it.next();
+            final SourceIdentifier realSId = irSchemaSource.getIdentifier();
+            if (srcIt.hasNext()) {
+                final SourceIdentifier expectedSId = srcIt.next();
+                if (!expectedSId.equals(realSId)) {
+                    LOG.warn("Source identifier mismatch for module \"{}\", requested as {} but actually is {}. "
+                        + "Using actual id", expectedSId.getName(), expectedSId, realSId);
+                }
+            }
+
+            final IRSchemaSource prev = filtered.put(realSId, irSchemaSource);
+            if (prev != null) {
+                LOG.warn("Duplicate source for module {} detected in reactor", realSId);
+            }
+        }
+
+        return ImmutableList.copyOf(filtered.values());
+    }
+}
