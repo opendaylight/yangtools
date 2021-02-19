072ccfc2a5193b2be20b97f5a439adc94325b915
Robert Varga <robert.varga@pantheon.tech>
2021-02-19 14:15:11 +0100

    WIP: Split out yang-data-spi
    NormalizedNode implementations share common components which enforce
    NormalizedNode contracts. Promote the (mostly-) stateless parts into
    yang.data.spi.node.
    
    Change-Id: I6a650d2f7df9a9e8621452cfbbe42be4d54834ae
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/nodes/AbstractImmutableDataContainerNode.java
+++ yang/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/nodes/AbstractImmutableDataContainerNode.java
@@ -13,9 +13,10 @@ import org.opendaylight.yangtools.util.ImmutableOffsetMap;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
 import org.opendaylight.yangtools.yang.data.api.schema.DataContainerNode;
+import org.opendaylight.yangtools.yang.data.spi.node.AbstractNormalizedNode;
 
 public abstract class AbstractImmutableDataContainerNode<K extends PathArgument, N extends DataContainerNode<K>>
-        extends AbstractImmutableNormalizedNode<K, N> implements DataContainerNode<K> {
+        extends AbstractNormalizedNode<K, N> implements DataContainerNode<K> {
     private final Map<PathArgument, Object> children;
 
     protected AbstractImmutableDataContainerNode(final Map<PathArgument, Object> children, final K nodeIdentifier) {
