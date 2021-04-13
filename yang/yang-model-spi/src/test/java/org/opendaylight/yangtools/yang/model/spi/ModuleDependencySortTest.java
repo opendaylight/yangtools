b220d8d175c09e11d65a30727d6a76f8c8189573
Robert Varga <robert.varga@pantheon.tech>
2021-04-13 11:24:18 +0200

    Ditch Guava from ModuleDependencySortTest
    List.of()/Set.of() are adequate replacements for what we are doing here,
    use them instead of Immutable{List,Set}.
    
    Change-Id: Iebf600a8a36c0501c834f2a62d12e9b5588efbc2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-model-spi/src/test/java/org/opendaylight/yangtools/yang/model/spi/ModuleDependencySortTest.java
+++ yang/yang-model-spi/src/test/java/org/opendaylight/yangtools/yang/model/spi/ModuleDependencySortTest.java
@@ -7,13 +7,12 @@
  */
 package org.opendaylight.yangtools.yang.model.spi;
 
-import static com.google.common.collect.ImmutableList.of;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.doReturn;
 
-import com.google.common.collect.ImmutableSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -48,8 +47,8 @@ public class ModuleDependencySortTest {
         doReturn("foo").when(fooNoRev).getName();
         doReturn(FOO_MODULE.getNamespace()).when(fooNoRev).getNamespace();
         doReturn(FOO_MODULE.getRevision()).when(fooNoRev).getRevision();
-        doReturn(ImmutableSet.of()).when(fooNoRev).getImports();
-        doReturn(ImmutableSet.of()).when(fooNoRev).getSubmodules();
+        doReturn(Set.of()).when(fooNoRev).getImports();
+        doReturn(Set.of()).when(fooNoRev).getSubmodules();
 
         doReturn("foo").when(fooNoRevImport).getModuleName();
         doReturn(Optional.empty()).when(fooNoRevImport).getRevision();
@@ -58,21 +57,21 @@ public class ModuleDependencySortTest {
         doReturn(BAR_MODULE.getNamespace()).when(bar).getNamespace();
         doReturn(BAR_MODULE.getRevision()).when(bar).getRevision();
         doReturn(YangVersion.VERSION_1).when(bar).getYangVersion();
-        doReturn(ImmutableSet.of()).when(bar).getImports();
-        doReturn(ImmutableSet.of(barSubmodule)).when(bar).getSubmodules();
+        doReturn(Set.of()).when(bar).getImports();
+        doReturn(Set.of(barSubmodule)).when(bar).getSubmodules();
 
-        doReturn(ImmutableSet.of(fooNoRevImport)).when(barSubmodule).getImports();
+        doReturn(Set.of(fooNoRevImport)).when(barSubmodule).getImports();
     }
 
     @Test
     public void testSimpleModules() {
-        assertSortedTo(of(fooNoRev), fooNoRev);
+        assertSortedTo(List.of(fooNoRev), fooNoRev);
     }
 
     @Test
     public void testSubmodules() {
-        assertSortedTo(of(fooNoRev, bar), bar, fooNoRev);
-        assertSortedTo(of(fooNoRev, bar), fooNoRev, bar);
+        assertSortedTo(List.of(fooNoRev, bar), bar, fooNoRev);
+        assertSortedTo(List.of(fooNoRev, bar), fooNoRev, bar);
     }
 
     private static void assertSortedTo(final List<Module> expected, final Module... modules) {
