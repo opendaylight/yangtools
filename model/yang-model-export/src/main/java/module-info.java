e52e1259cfad36c417fec098abe334fd6ae79eb2
Robert Varga <robert.varga@pantheon.tech>
2021-04-27 11:13:32 +0200

    Turn yang-model-export into a JPMS module
    This is a simple component, add a module-info and clean up dependencies.
    
    JIRA: YANGTOOLS-1281
    Change-Id: Ief7bd386198cef51b64a7a7ca11398f2e3c88ba2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ model/yang-model-export/src/main/java/module-info.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.yangtools.yang.model.export {
+    exports org.opendaylight.yangtools.yang.model.export;
+
+    requires transitive org.opendaylight.yangtools.concepts;
+    requires transitive org.opendaylight.yangtools.yang.common;
+    requires transitive org.opendaylight.yangtools.yang.model.api;
+    requires com.google.common;
+
+    // Annotations
+    requires static transitive org.eclipse.jdt.annotation;
+    requires static com.github.spotbugs.annotations;
+}
