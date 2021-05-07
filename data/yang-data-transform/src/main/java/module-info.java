a6975ca6871f2663f0a247a355d4e0ba0487157d
Robert Varga <robert.varga@pantheon.tech>
2021-05-07 12:51:52 +0200

    Convert yang-data-transform into an explicit module
    We do not rely on anything complex, just perform the straightforward
    transformation, adjusting dependencies in the process.
    
    JIRA: YANGTOOLS-1286
    Change-Id: I3e385958268937a11f04a693a9a145caba3422d7
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ data/yang-data-transform/src/main/java/module-info.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.yangtools.yang.data.transform {
+    // FIXME: 8.0.0: rename this to yangtools.yang.data.transform
+    exports org.opendaylight.yangtools.transform;
+
+    requires transitive com.google.common;
+    requires transitive org.opendaylight.yangtools.yang.common;
+    requires transitive org.opendaylight.yangtools.yang.data.api;
+    requires org.opendaylight.yangtools.yang.data.impl;
+
+    // Annotations
+    requires static transitive org.eclipse.jdt.annotation;
+}
