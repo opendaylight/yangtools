0c62072458f0f3f436e150031c227dacd7385f44
Robert Varga <robert.varga@pantheon.tech>
2021-05-07 13:07:59 +0200

    Convert yang-data-codec-binfmt to an explict module
    This is a simple component and conversion is straightforward. Switch
    from being an automatic module to being an explicit module.
    
    JIRA: YANGTOOLS-1287
    Change-Id: I131b3104c4d65632057f8367e5681fef358c9928
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ codec/yang-data-codec-binfmt/src/main/java/module-info.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.yangtools.yang.data.codec.binfmt {
+    exports org.opendaylight.yangtools.yang.data.codec.binfmt;
+
+    requires transitive com.google.common;
+    requires transitive org.opendaylight.yangtools.yang.common;
+    requires transitive org.opendaylight.yangtools.yang.model.api;
+    requires transitive org.opendaylight.yangtools.yang.data.api;
+    requires java.xml;
+    requires org.opendaylight.yangtools.yang.data.impl;
+    requires org.opendaylight.yangtools.rfc8528.data.api;
+    requires org.slf4j;
+
+    // Annotations
+    requires static com.github.spotbugs.annotations;
+    requires static org.eclipse.jdt.annotation;
+}
