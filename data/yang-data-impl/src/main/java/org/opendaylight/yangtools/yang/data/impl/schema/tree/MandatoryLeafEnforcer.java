3dc552d3a4f7c25f6b5852b26b42ef3e8d98269a
Robert Varga <robert.varga@pantheon.tech>
2021-05-18 10:28:13 +0200

    Fix mandatory enforcer failure on augmented nodes
    Changes the way mandatory nodes are extracted from the schema.
    Previously all mandatory nodes were approached as direct children of
    the parent node. Even if they came from an augmentation which caused
    "missing mandatory descendant" exceptions.
    This patch preserves both options of building data nodes containing
    augmented mandatory nodes.
    - setting the mandatory node as a direct child of the parent node
    - setting the mandatory node as an AugmentationNode to the parent node
    
    However the former option seems to be incorrect and might be removed
    in the future major release.
    
    JIRA: YANGTOOLS-1276
    Change-Id: Ifcc2e0d141e18f74173f15e293b16cbd297a5af6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- data/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/MandatoryLeafEnforcer.java
+++ data/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/MandatoryLeafEnforcer.java
@@ -7,20 +7,27 @@
  */
 package org.opendaylight.yangtools.yang.data.impl.schema.tree;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableList.Builder;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+import java.util.Collection;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.AugmentationIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodes;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeConfiguration;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.TreeType;
 import org.opendaylight.yangtools.yang.data.spi.tree.TreeNode;
+import org.opendaylight.yangtools.yang.model.api.AugmentationSchemaNode;
+import org.opendaylight.yangtools.yang.model.api.AugmentationTarget;
 import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.DataNodeContainer;
 import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
@@ -33,12 +40,14 @@ import org.slf4j.LoggerFactory;
 final class MandatoryLeafEnforcer implements Immutable {
     private static final Logger LOG = LoggerFactory.getLogger(MandatoryLeafEnforcer.class);
 
-    private final ImmutableList<YangInstanceIdentifier> mandatoryNodes;
+    // introduced Map instead of List to support the enforcing mechanism for both versions of mandatory leafs which
+    // were added via augmentation. Related issue: YANGTOOLS-1276
+    private final ImmutableMap<YangInstanceIdentifier, YangInstanceIdentifier> directToAugMandatoryNodes;
 
-    private MandatoryLeafEnforcer(final ImmutableList<YangInstanceIdentifier> mandatoryNodes) {
-        this.mandatoryNodes = requireNonNull(mandatoryNodes);
+    private MandatoryLeafEnforcer(final ImmutableMap<YangInstanceIdentifier, YangInstanceIdentifier>
+        directToAugMandatoryNodes) {
+        this.directToAugMandatoryNodes = requireNonNull(directToAugMandatoryNodes);
     }
-
 
     static Optional<MandatoryLeafEnforcer> forContainer(final DataNodeContainer schema,
             final DataTreeConfiguration treeConfig) {
@@ -46,16 +55,27 @@ final class MandatoryLeafEnforcer implements Immutable {
             return Optional.empty();
         }
 
-        final Builder<YangInstanceIdentifier> builder = ImmutableList.builder();
+        final Builder<YangInstanceIdentifier, YangInstanceIdentifier> builder = ImmutableMap.builder();
         findMandatoryNodes(builder, YangInstanceIdentifier.empty(), schema, treeConfig.getTreeType());
-        final ImmutableList<YangInstanceIdentifier> mandatoryNodes = builder.build();
+        final ImmutableMap<YangInstanceIdentifier, YangInstanceIdentifier> mandatoryNodes = builder.build();
         return mandatoryNodes.isEmpty() ? Optional.empty() : Optional.of(new MandatoryLeafEnforcer(mandatoryNodes));
     }
 
+    /**
+     * Due to an issue with augmented mandatory nodes(YANGTOOLS-1276), the enforcing is done in 2 steps here.
+     * - first seek the mandatory node as a direct child of the data node - using his NodeIdentifier
+     * - in case the mandatory node came from augmentation, try looking for his AugmentationIdentifier
+     */
     void enforceOnData(final NormalizedNode data) {
-        for (final YangInstanceIdentifier id : mandatoryNodes) {
-            checkArgument(NormalizedNodes.findNode(data, id).isPresent(),
-                "Node %s is missing mandatory descendant %s", data.getIdentifier(), id);
+        for (Map.Entry<YangInstanceIdentifier, YangInstanceIdentifier> id : directToAugMandatoryNodes.entrySet()) {
+            if (NormalizedNodes.findNode(data, id.getKey()).isEmpty()) {
+                YangInstanceIdentifier augmentedId = id.getValue();
+                if (!augmentedId.isEmpty() && NormalizedNodes.findNode(data, augmentedId).isPresent()) {
+                    return;
+                }
+                throw new IllegalArgumentException(String.format("Node %s is missing mandatory descendant %s",
+                    data.getIdentifier(), id.getKey()));
+            }
         }
     }
 
@@ -63,14 +83,16 @@ final class MandatoryLeafEnforcer implements Immutable {
         enforceOnData(tree.getData());
     }
 
-    private static void findMandatoryNodes(final Builder<YangInstanceIdentifier> builder,
-            final YangInstanceIdentifier id, final DataNodeContainer schema, final TreeType type) {
+    private static void findMandatoryNodes(final Builder<YangInstanceIdentifier, YangInstanceIdentifier>
+        directToAugmentedChildBuilder, final YangInstanceIdentifier id, final DataNodeContainer schema,
+        final TreeType type) {
         for (final DataSchemaNode child : schema.getChildNodes()) {
             if (SchemaAwareApplyOperation.belongsToTree(type, child)) {
                 if (child instanceof ContainerSchemaNode) {
                     final ContainerSchemaNode container = (ContainerSchemaNode) child;
                     if (!container.isPresenceContainer()) {
-                        findMandatoryNodes(builder, id.node(NodeIdentifier.create(child.getQName())), container, type);
+                        findMandatoryNodes(directToAugmentedChildBuilder,
+                            id.node(NodeIdentifier.create(child.getQName())), container, type);
                     }
                 } else {
                     boolean needEnforce = child instanceof MandatoryAware && ((MandatoryAware) child).isMandatory();
@@ -82,9 +104,29 @@ final class MandatoryLeafEnforcer implements Immutable {
                                 }).orElse(Boolean.FALSE).booleanValue();
                     }
                     if (needEnforce) {
-                        final YangInstanceIdentifier childId = id.node(NodeIdentifier.create(child.getQName()));
-                        LOG.debug("Adding mandatory child {}", childId);
-                        builder.add(childId.toOptimized());
+                        final YangInstanceIdentifier directChildId = id.node(NodeIdentifier.create(child.getQName()))
+                            .toOptimized();
+                        if (child.isAugmenting()) {
+                            Collection<? extends AugmentationSchemaNode> augmentations =
+                                ((AugmentationTarget) schema).getAvailableAugmentations();
+                            for (AugmentationSchemaNode augmentation : augmentations) {
+                                if (augmentation.findDataChildByName(child.getQName()).isPresent()) {
+                                    final Set<QName> augQnames =
+                                        augmentation.getChildNodes().stream().map(DataSchemaNode::getQName)
+                                            .collect(Collectors.toSet());
+
+                                    final YangInstanceIdentifier augmentedChildId = id.node(AugmentationIdentifier
+                                        .create(augQnames)).toOptimized();
+                                    LOG.debug("Adding both direct and augmented versions of the mandatory child: "
+                                            + "direct {}, augmented: {}", directChildId, augmentedChildId);
+                                    directToAugmentedChildBuilder.put(directChildId, augmentedChildId);
+                                    break;
+                                }
+                            }
+                        } else {
+                            LOG.debug("Adding direct mandatory child {}", directChildId);
+                            directToAugmentedChildBuilder.put(directChildId, YangInstanceIdentifier.empty());
+                        }
                     }
                 }
             }
