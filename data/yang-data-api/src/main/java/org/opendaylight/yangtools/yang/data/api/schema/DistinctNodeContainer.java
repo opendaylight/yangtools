14d75b85a76fb2d67ec425b5e681c226fcf63097
Robert Varga <robert.varga@pantheon.tech>
2021-06-15 19:30:06 +0200

    Add DistinctNodeContainer.getChildByArg()
    There are a number of places where there is an implied assertion
    that the child must exist, but the caller does not care about the
    exception being reported.  These are using
    findChildByArg().orElseThrows() today, could use a more expressive
    alternative.
    
    Add getChildByArg() as a default method, which wraps a call to
    childByArg() with a verifyNotNull(), neatly expressing the intent.
    
    Change-Id: I8dade860a0c615e987aa2c017473eb08769afee3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- data/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/DistinctNodeContainer.java
+++ data/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/DistinctNodeContainer.java
@@ -7,6 +7,9 @@
  */
 package org.opendaylight.yangtools.yang.data.api.schema;
 
+import static com.google.common.base.Verify.verifyNotNull;
+
+import com.google.common.base.VerifyException;
 import java.util.Collection;
 import java.util.Optional;
 import org.eclipse.jdt.annotation.NonNull;
@@ -59,5 +62,17 @@ public interface DistinctNodeContainer<K extends PathArgument, V extends Normali
      */
     default Optional<V> findChildByArg(final K key) {
         return Optional.ofNullable(childByArg(key));
+    }
+
+    /**
+     * Returns a child node identified by provided key, asserting its presence.
+     *
+     * @param key Path argument identifying child node
+     * @return Matching child node
+     * @throws NullPointerException if {@code key} is null
+     * @throws VerifyException if the child does not exist
+     */
+    default @NonNull V getChildByArg(final K key) {
+        return verifyNotNull(childByArg(key));
     }
 }
