0b2772d4deca8638018e1997d816b0d751055043
Robert Varga <robert.varga@pantheon.tech>
2021-06-25 18:43:41 +0200

    WIP: Improve missing key reporting
    Change-Id: I920ef4580898b094f552ca608c649539993d5c02
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ data/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/codec/YangMissingKeyException.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.api.codec;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import java.util.Optional;
+import java.util.Set;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.RpcError.ErrorSeverity;
+import org.opendaylight.yangtools.yang.common.RpcError.ErrorType;
+import org.opendaylight.yangtools.yang.common.YangError;
+
+/**
+ * Dedicated exception for reporting conditions where {@code error-tag} should be reported as {@code missing-element}.
+ * This is covered by <a href="https://tools.ietf.org/html/rfc7950#section-8.3.1">RFC7950 section 8.3.1</a>:
+ * <pre>
+ *   If a leaf data value does not match the type constraints for the
+ *   leaf, including those defined in the type's "range", "length", and
+ *   "pattern" properties, the server MUST reply with an
+ *   "invalid-value" &lt;error-tag&gt; in the &lt;rpc-error&gt;, and with the
+ *   error-app-tag (Section 7.5.4.2) and error-message
+ *   (Section 7.5.4.1) associated with the constraint, if any exist.
+ * </pre>
+ *
+ * <p>
+ * This error tag also references <a href="https://tools.ietf.org/html/rfc6241#appendix-A">RFC6241 Appendix A</a>,
+ * which defines the appropriate severity and adds more semantics.
+ */
+@Beta
+public class YangMissingKeyException extends IllegalArgumentException implements YangError {
+    private static final long serialVersionUID = 1L;
+
+    private final @NonNull ErrorType errorType;
+    private final @NonNull Set<QName> missingKeys;
+
+    public YangMissingKeyException(final ErrorType errorType, final Set<QName> missingKeys) {
+        super(requireNonNull("List entry is missing keys " + missingKeys));
+        this.errorType = requireNonNull(errorType);
+        this.missingKeys = Set.copyOf(missingKeys);
+    }
+
+    @Override
+    public final ErrorType getErrorType() {
+        return errorType;
+    }
+
+    @Override
+    public final ErrorSeverity getSeverity() {
+        return ErrorSeverity.ERROR;
+    }
+
+    @Override
+    public final String getTag() {
+        return "missing-element";
+    }
+
+    @Override
+    public final Optional<String> getErrorAppTag() {
+        return Optional.empty();
+    }
+
+    @Override
+    public final Optional<String> getErrorMessage() {
+        return Optional.empty();
+    }
+
+    public final Set<QName> getMissingKeys() {
+        return missingKeys;
+    }
+}
