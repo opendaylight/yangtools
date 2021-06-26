9e95e0ef704e00e144e80a23cb03863e42bc820a
Robert Varga <robert.varga@pantheon.tech>
2021-06-26 16:45:21 +0200

    Add yang.common.Netconf constructs
    There is a number of error-reporting constructs which are defined across
    RFC4741, RFC6020, RFC6241 and RFC8040. These originate from NETCONF, but
    are quite integrated in the YANG enforcement specification.
    
    Encapsulate these constructs in a top-level Netconf class, introducing:
    
    - Netconf.Layer to identify the NETCONF layer involved
    - Netconf.ErrorSeverity to discern errors and warnings
    - Netconf.ErrorType to express error-types reported
    - Netconf.ErrorTag to extensibly identify error-tag reported
    - Netconf.ErrorInfo to extensibly identify addition error-info elements
    
    Also add proper RFC4741/RFC6241 namespace anchors and use them as
    appropriate.
    
    // FIXME: allocate an issue
    
    Change-Id: Idbb0d7ac4a2b58f930b06d14522872a18e7037d8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ data/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/YangNetconfError.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.api;
+
+import com.google.common.annotations.Beta;
+import java.util.List;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.yang.common.Netconf.ErrorInfo;
+import org.opendaylight.yangtools.yang.common.Netconf.ErrorInfo.AbstractNonUnique;
+import org.opendaylight.yangtools.yang.common.Netconf.ErrorSeverity;
+import org.opendaylight.yangtools.yang.common.Netconf.ErrorTag;
+import org.opendaylight.yangtools.yang.common.Netconf.ErrorType;
+
+/**
+ * Baseline interface for metadata associated with a NETCONF notion of an 'error'. Except what NETCONF regards as
+ * an 'error', really means 'notable event' in that it can either be a warning or an error. No warnings were defined
+ * at the time this distinction was made.
+ */
+@Beta
+@NonNullByDefault
+public interface YangNetconfError {
+    /**
+     * Return this error's severity.
+     *
+     * @return Error severity.
+     */
+    ErrorSeverity severity();
+    /**
+     * Return this error's type.
+     *
+     * @return Error type.
+     */
+    ErrorType type();
+    /**
+     * Return this error's tag.
+     *
+     * @return Error tag.
+     */
+    ErrorTag tag();
+    /**
+     * Return this errors's {@code error-message}, if available. This value is expected to be defined in a YANG model
+     * through a {@code error-message} statement.
+     *
+     * @return Event message, or null.
+     */
+    @Nullable String message();
+    /**
+     * Return this error's {@code error-app-tag}, if available. This value is expected to be defined in a YANG model
+     * through a {@code error-app-tag} statement.
+     *
+     * @return Application tag, or null.
+     */
+    @Nullable String appTag();
+    /**
+     * Return the path which triggered this error, if available.
+     *
+     * @return Triggering path, or null.
+     */
+    @Nullable YangInstanceIdentifier path();
+    /**
+     * Return this error's additional info.
+     *
+     * @return Additional info.
+     */
+    List<? extends ErrorInfo<?>> info();
+
+    /**
+     * {@code non-unique} as defined in
+     * <a href="https://datatracker.ietf.org/doc/html/rfc6020#section-13.1">RFC6020, section 13.1</a>, specialized with
+     * {@link YangInstanceIdentifier} as the path representation.
+     */
+    public static final class NonUnique extends AbstractNonUnique<YangInstanceIdentifier> {
+        public NonUnique(final YangInstanceIdentifier value) {
+            super(value);
+        }
+    }
+}
