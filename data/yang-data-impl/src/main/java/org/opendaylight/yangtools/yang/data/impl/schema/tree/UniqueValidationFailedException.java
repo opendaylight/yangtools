3bef7a468c713a2a6a8ad34d4c67de5b1af4343e
Robert Varga <robert.varga@pantheon.tech>
2021-06-27 21:20:21 +0200

    Retrofit YangNetconfError into Unique*Exception
    Add a basic bridge to YangNetconfError. This allows NETCONF WG
    protocols, like NETCONF and RESTCONF, to report unified complete
    information about failure causes.
    
    JIRA: YANGTOOLS-1306
    Change-Id: Id8134275fe1b304db88466ad2c6733a48871503e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- data/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/UniqueValidationFailedException.java
+++ data/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/tree/UniqueValidationFailedException.java
@@ -7,15 +7,34 @@
  */
 package org.opendaylight.yangtools.yang.data.impl.schema.tree;
 
+import java.util.List;
+import org.opendaylight.yangtools.yang.common.ErrorSeverity;
+import org.opendaylight.yangtools.yang.common.ErrorTag;
+import org.opendaylight.yangtools.yang.common.ErrorType;
+import org.opendaylight.yangtools.yang.data.api.ImmutableYangNetconfError;
+import org.opendaylight.yangtools.yang.data.api.YangNetconfError;
+import org.opendaylight.yangtools.yang.data.api.YangNetconfErrorAware;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.UniqueConstraintException;
 
 /**
  * Exception thrown when unique constraints would be violated and we cannot throw a {@link UniqueConstraintException}.
  */
-final class UniqueValidationFailedException extends SchemaValidationFailedException {
+final class UniqueValidationFailedException extends SchemaValidationFailedException implements YangNetconfErrorAware {
     private static final long serialVersionUID = 1L;
 
     UniqueValidationFailedException(final String message) {
         super(message);
+    }
+
+    @Override
+    public List<YangNetconfError> getNetconfErrors() {
+        return List.of(ImmutableYangNetconfError.builder()
+            .severity(ErrorSeverity.ERROR)
+            .type(ErrorType.APPLICATION)
+            .tag(ErrorTag.OPERATION_FAILED)
+            .appTag("data-not-unique")
+            // FIXME: 8.0.0: we are missing path information which should be filled in here. Constructor call site needs
+            //               to provide that.
+            .build());
     }
 }
