6fc0abdb58af8c0442d64c8ea19dc68e2f3cf0b3
Robert Varga <robert.varga@pantheon.tech>
2021-06-28 20:06:04 +0200

    Require SCR from yang-xpath-impl
    The jar cannot work in OSGi without SCR, make sure annotate as such.
    
    Change-Id: I1281094adc161aed606ed1937ae6999f50f79e9f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- xpath/yang-xpath-impl/src/main/java/org/opendaylight/yangtools/yang/xpath/impl/package-info.java
+++ xpath/yang-xpath-impl/src/main/java/org/opendaylight/yangtools/yang/xpath/impl/package-info.java
@@ -8,8 +8,7 @@
 /**
  * XPath parsing which results in a {@link org.opendaylight.yangtools.yang.xpath.api.YangExpr}. This is based
  * on the XPath grammar from <a href="https://github.com/antlr/grammars-v4/blob/master/xpath/xpath.g4">grammars-v4</a>.
- *
- * @author Robert Varga
  */
 @org.eclipse.jdt.annotation.NonNullByDefault
+@org.osgi.service.component.annotations.RequireServiceComponentRuntime
 package org.opendaylight.yangtools.yang.xpath.impl;
