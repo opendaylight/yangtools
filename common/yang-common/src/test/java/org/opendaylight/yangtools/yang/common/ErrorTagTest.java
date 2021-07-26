4e56945f1d435aebfaf8682dc45353cf2357e849
Robert Varga <robert.varga@pantheon.tech>
2021-07-27 00:55:47 +0200

    ErrorTag should be Serializable
    We want to transport ErrorTag as a type-safe string. Add the infra
    to do that. Note that the serialization format places an upper bound
    of 64K -- and that is very reasonable for an ErrorTag.
    
    JIRA: YANGTOOLS-1309
    Change-Id: Ib0049fbddc92e9b8ec9e1b37851e0de6f6faaad6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ common/yang-common/src/test/java/org/opendaylight/yangtools/yang/common/ErrorTagTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.common;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import org.junit.Test;
+
+public class ErrorTagTest {
+    @Test
+    public void testSerialization() throws Exception {
+        final ErrorTag expected = new ErrorTag("test");
+
+        final byte[] bytes;
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+            try (ObjectOutputStream oos = new ObjectOutputStream(bos)) {
+                oos.writeObject(expected);
+            }
+            bytes = bos.toByteArray();
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(bytes))) {
+            assertEquals(expected, ois.readObject());
+        }
+    }
+}
