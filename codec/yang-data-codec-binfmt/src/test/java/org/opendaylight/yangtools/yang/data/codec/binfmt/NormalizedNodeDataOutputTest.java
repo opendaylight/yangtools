303aa270663bb750327af775b7b66d8e91ec115b
Robert Varga <robert.varga@pantheon.tech>
2021-08-17 13:01:59 +0200

    Add NormalizedNodeDataOutputTest
    We have a few lines which are not covered by tests, make sure we cover
    them.
    
    Change-Id: If36df82225381edeafaf1f4d0d935fe8cf78a5a3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ codec/yang-data-codec-binfmt/src/test/java/org/opendaylight/yangtools/yang/data/codec/binfmt/NormalizedNodeDataOutputTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.codec.binfmt;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+
+import java.io.IOException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
+
+@RunWith(MockitoJUnitRunner.StrictStubs.class)
+public class NormalizedNodeDataOutputTest {
+    @Mock
+    public NormalizedNodeDataOutput output;
+
+    @Before
+    public void before() throws IOException {
+        doCallRealMethod().when(output).writeOptionalNormalizedNode(any());
+    }
+
+    @Test
+    public void testWriteOptionalNormalizedNodeAbsent() throws IOException {
+        doNothing().when(output).writeBoolean(false);
+        output.writeOptionalNormalizedNode(null);
+        verify(output).writeBoolean(false);
+    }
+
+    @Test
+    public void testWriteOptionalNormalizedNodePresent() throws IOException {
+        final ContainerNode node = ImmutableNodes.containerNode(QName.create("test", "test"));
+        doNothing().when(output).writeBoolean(true);
+        doNothing().when(output).writeNormalizedNode(node);
+        output.writeOptionalNormalizedNode(node);
+        verify(output).writeBoolean(true);
+        verify(output).writeNormalizedNode(node);
+    }
+}
