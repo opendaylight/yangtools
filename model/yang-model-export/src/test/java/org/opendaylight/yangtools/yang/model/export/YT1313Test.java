d08e5552beb9180f582bf419fdcf120052876411
Robert Varga <robert.varga@pantheon.tech>
2021-08-26 17:04:48 +0200

    Fix YANG export with duplicate imports
    When we have two submodules not agreeing on the prefix used to import a
    module, we get a nasty splat from StatementPrefixResolver. Fix this by
    detecting when this is happens and skip to full resolution magic.
    
    JIRA: YANGTOOLS-1313
    Change-Id: I99cc3ebc2f560590d05b51287d08429aaf7d6582
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Dominik Vrbovsky <dominik.vrbovsky@pantheon.tech>

---
+++ model/yang-model-export/src/test/java/org/opendaylight/yangtools/yang/model/export/YT1313Test.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.export;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.common.XMLNamespace;
+import org.opendaylight.yangtools.yang.model.api.stmt.ModuleEffectiveStatement;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+public class YT1313Test {
+    @Test
+    public void testSubmoduleImportPrefixes() {
+        final ModuleEffectiveStatement bar = YangParserTestUtils.parseYangResourceDirectory("/bugs/yt1313")
+            .getModuleStatement(QNameModule.create(XMLNamespace.of("bar")));
+
+        final StatementPrefixResolver resolver = StatementPrefixResolver.forModule(bar);
+        assertNotNull(resolver);
+    }
+}
