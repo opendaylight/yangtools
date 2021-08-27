3794c5b83df0f4a006386ac86e8ce1debc9dc404
Robert Varga <robert.varga@pantheon.tech>
2021-08-27 14:18:29 +0200

    Add support for RFC8639 extensions
    We have an internal YANG extension defined in rfc8639. Teach the parser
    to correctly recognize it.
    
    JIRA: YANGTOOLS-1314
    Change-Id: I96c30c9728e61cadbc550b0b96d925719593c0d6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ model/rfc8639-model-api/src/main/java/org/opendaylight/yangtools/rfc8639/model/api/SubscribedNotificationsConstants.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8639.model.api;
+
+import com.google.common.collect.ImmutableList;
+import java.util.Collection;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.common.Revision;
+import org.opendaylight.yangtools.yang.common.XMLNamespace;
+import org.opendaylight.yangtools.yang.model.repo.api.RevisionSourceIdentifier;
+import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
+
+/**
+ * Constants associated with RFC8639.
+ */
+@NonNullByDefault
+public final class SubscribedNotificationsConstants {
+    private static final String MODULE_NAME = "ietf-subscribed-notifications";
+    private static final XMLNamespace MODULE_NAMESPACE =
+        XMLNamespace.of("urn:ietf:params:xml:ns:yang:ietf-subscribed-notifications").intern();
+    private static final Revision RFC8639_REVISION = Revision.of("2019-09-09");
+
+    /**
+     * Runtime RFC8639 identity.
+     */
+    public static final QNameModule RFC8639_MODULE = QNameModule.create(MODULE_NAMESPACE, RFC8639_REVISION).intern();
+
+    /**
+     * RFC8639 model source name.
+     */
+    public static final SourceIdentifier RFC8639_SOURCE = RevisionSourceIdentifier.create(MODULE_NAME,
+        RFC8639_REVISION);
+
+    /**
+     * Normative prefix to use when importing {@link #RFC8639_SOURCE}.
+     */
+    public static final String MODULE_PREFIX = "sn";
+
+    private SubscribedNotificationsConstants() {
+        // Hidden on purpose
+    }
+
+    /**
+     * Return identifiers of all sources known to define the metadata extension.
+     *
+     * @return Collection of identifiers.
+     */
+    public static Collection<SourceIdentifier> knownModelSources() {
+        return ImmutableList.of(RFC8639_SOURCE);
+    }
+}
---
+++ parser/rfc8639-parser-support/src/main/java/org/opendaylight/yangtools/rfc8639/parser/SubscriptionStateNotificationEffectiveStatementImpl.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8639.parser;
+
+import com.google.common.collect.ImmutableList;
+import org.opendaylight.yangtools.rfc8639.model.api.SubscriptionStateNotificationEffectiveStatement;
+import org.opendaylight.yangtools.rfc8639.model.api.SubscriptionStateNotificationStatement;
+import org.opendaylight.yangtools.yang.common.Empty;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.spi.meta.AbstractDeclaredEffectiveStatement.DefaultArgument.WithSubstatements;
+
+final class SubscriptionStateNotificationEffectiveStatementImpl
+        extends WithSubstatements<Empty, SubscriptionStateNotificationStatement>
+        implements SubscriptionStateNotificationEffectiveStatement {
+
+    SubscriptionStateNotificationEffectiveStatementImpl(final SubscriptionStateNotificationStatement declared,
+            final ImmutableList<? extends EffectiveStatement<?, ?>> substatements) {
+        super(declared, substatements);
+    }
+}
