bc26a1fe628aa89401dbab2227bcb860b60a9008
Robert Varga <robert.varga@pantheon.tech>
2021-08-27 14:18:29 +0200

    Add support for RFC8639 extensions
    We have an internal YANG extension defined in rfc8639. Teach the parser
    to correctly recognize it.
    
    JIRA: YANGTOOLS-1314
    Change-Id: I96c30c9728e61cadbc550b0b96d925719593c0d6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ model/rfc8639-model-api/src/main/java/org/opendaylight/yangtools/rfc8639/model/api/SubscribedNotificationsStatements.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8639.model.api;
+
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.meta.ArgumentDefinition;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
+
+/**
+ * {@link StatementDefinition}s for statements defined by RFC8639.
+ */
+@NonNullByDefault
+public enum SubscribedNotificationsStatements implements StatementDefinition {
+    SUBSCRIPTION_STATE_NOTIFICATION("subscription-state-notification") {
+        @Override
+        public Optional<ArgumentDefinition> getArgumentDefinition() {
+            return Optional.empty();
+        }
+
+        @Override
+        public Class<? extends DeclaredStatement<?>> getDeclaredRepresentationClass() {
+            return SubscriptionStateNotificationStatement.class;
+        }
+
+        @Override
+        public Class<? extends EffectiveStatement<?, ?>> getEffectiveRepresentationClass() {
+            return SubscriptionStateNotificationEffectiveStatement.class;
+        }
+    };
+
+    private final QName statementName;
+
+    SubscribedNotificationsStatements(final String statementName) {
+        this.statementName = QName.create(SubscribedNotificationsConstants.RFC8639_MODULE, statementName).intern();
+    }
+
+    @Override
+    public final QName getStatementName() {
+        return statementName;
+    }
+}
---
+++ parser/rfc8639-parser-support/src/test/java/org/opendaylight/yangtools/rfc8639/parser/SubscribedNotificationsTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8639.parser;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.stream.Collectors;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.opendaylight.yangtools.rfc8639.model.api.SubscribedNotificationsConstants;
+import org.opendaylight.yangtools.rfc8639.model.api.SubscriptionStateNotificationEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.NotificationEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.repo.api.YangTextSchemaSource;
+import org.opendaylight.yangtools.yang.parser.api.YangParserConfiguration;
+import org.opendaylight.yangtools.yang.parser.api.YangSyntaxErrorException;
+import org.opendaylight.yangtools.yang.parser.rfc7950.reactor.RFC7950Reactors;
+import org.opendaylight.yangtools.yang.parser.rfc7950.repo.YangStatementStreamSource;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ModelProcessingPhase;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.parser.stmt.reactor.CrossSourceStatementReactor;
+
+public class SubscribedNotificationsTest {
+    private static CrossSourceStatementReactor reactor;
+
+    @BeforeClass
+    public static void createReactor() {
+        reactor = RFC7950Reactors.vanillaReactorBuilder()
+                .addStatementSupport(ModelProcessingPhase.FULL_DECLARATION,
+                    new SubscriptionStateNotificationStatementSupport(YangParserConfiguration.DEFAULT))
+                .build();
+    }
+
+    @AfterClass
+    public static void freeReactor() {
+        reactor = null;
+    }
+
+    @Test
+    public void testSubscribedNotifications() throws ReactorException, IOException, YangSyntaxErrorException {
+        final var context = reactor.newBuild()
+            .addLibSources(
+                YangStatementStreamSource.create(YangTextSchemaSource.forResource("/ietf-inet-types@2013-07-15.yang")),
+                YangStatementStreamSource.create(YangTextSchemaSource.forResource("/ietf-interfaces@2018-02-20.yang")),
+                YangStatementStreamSource.create(YangTextSchemaSource.forResource("/ietf-ip@2018-02-22.yang")),
+                YangStatementStreamSource.create(YangTextSchemaSource.forResource("/ietf-netconf-acm@2018-02-14.yang")),
+                YangStatementStreamSource.create(YangTextSchemaSource.forResource(
+                    "/ietf-network-instance@2019-01-21.yang")),
+                YangStatementStreamSource.create(YangTextSchemaSource.forResource("/ietf-restconf@2017-01-26.yang")),
+                YangStatementStreamSource.create(YangTextSchemaSource.forResource(
+                    "/ietf-yang-schema-mount@2019-01-14.yang")),
+                YangStatementStreamSource.create(YangTextSchemaSource.forResource("/ietf-yang-types@2013-07-15.yang")))
+            .addSources(
+                YangStatementStreamSource.create(YangTextSchemaSource.forResource(
+                    "/ietf-subscribed-notifications@2019-09-09.yang")))
+            .buildEffective();
+
+        final var notifications = context.getModuleStatement(SubscribedNotificationsConstants.RFC8639_MODULE)
+            .streamEffectiveSubstatements(NotificationEffectiveStatement.class)
+            .collect(Collectors.toUnmodifiableList());
+
+        assertEquals(7, notifications.size());
+        for (NotificationEffectiveStatement notif : notifications) {
+            final var sub = notif.findFirstEffectiveSubstatement(SubscriptionStateNotificationEffectiveStatement.class);
+            assertTrue("No marker in " + notif.argument(), sub.isPresent());
+        }
+    }
+}
