01be80372f17d7e0232c2106733a4dae3502f206
Robert Varga <robert.varga@pantheon.tech>
2021-09-02 19:39:54 +0200

    Intern statements along copy axis
    When we are inferring statements through grouping/uses, we end up adding
    a few bits to record the copy history and effective status/config. This
    process does not feed back anything back to the original declaration
    site.
    
    This leads to a situation when we can have a number of inferred
    statements which are exactly equal, yet each is its own java object --
    hence unnecessarily using heap for duplicate objects.
    
    We can improve this by having a deduplication map at the site of
    original declaration and updating/consulting it when we find we have an
    otherwise-unmodified statement.
    
    This patch adds the prerequisite constructs to yang-parser-spi and
    the smarts to perform this deduplication to yang-parser-reactor.
    
    JIRA: YANGTOOLS-1214
    Change-Id: I957c6d69d16717759f22a78163c705390e602e22
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-spi/src/main/java/org/opendaylight/yangtools/yang/parser/spi/meta/EffectiveStatementState.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.spi.meta;
+
+import com.google.common.annotations.Beta;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.MoreObjects.ToStringHelper;
+import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+
+/**
+ * Significant state captured by an {@link EffectiveStatement} at its instantiation site. This can be used to compare
+ * statements which are instantiated through inference, for example through {@code uses} from {@code grouping}s, when
+ * the reactor establishes the two instances have the same substatements.
+ */
+@Beta
+public abstract class EffectiveStatementState implements Immutable {
+    @Override
+    public abstract int hashCode();
+
+    @Override
+    public abstract boolean equals(Object obj);
+
+    @Override
+    public final String toString() {
+        return addToStringAttributes(MoreObjects.toStringHelper(this)).toString();
+    }
+
+    protected abstract ToStringHelper addToStringAttributes(ToStringHelper helper);
+}
