90620d49be8cbef3b611155d3405078cea705794
Robert Varga <robert.varga@pantheon.tech>
2021-09-06 13:19:21 +0200

    WIP: Index TypedefNamespace
    Add yang-model-{ri,spi} support for indexing TypedefNamespace and
    retrofit it into current classes.
    
    // FIXME: add tests
    
    JIRA: YANGTOOLS-1262
    Change-Id: I95136d4fc204019678a53ddd318e2fca453630b9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-spi/src/main/java/org/opendaylight/yangtools/yang/model/spi/meta/AbstractUndeclaredEffectiveStatement.java
+++ model/yang-model-spi/src/main/java/org/opendaylight/yangtools/yang/model/spi/meta/AbstractUndeclaredEffectiveStatement.java
@@ -29,6 +29,8 @@ import org.opendaylight.yangtools.yang.model.api.stmt.DataTreeAwareEffectiveStat
 import org.opendaylight.yangtools.yang.model.api.stmt.DataTreeEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeAwareEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.TypedefEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.TypedefNamespace;
 
 @Beta
 public abstract class AbstractUndeclaredEffectiveStatement<A, D extends DeclaredStatement<A>>
@@ -156,6 +158,32 @@ public abstract class AbstractUndeclaredEffectiveStatement<A, D extends Declared
             E extends DataTreeAwareEffectiveStatement<A, D>> extends WithDataTree<A, D, E> {
         public abstract static class WithSubstatements<A, D extends DeclaredStatement<A>,
                 E extends DataTreeAwareEffectiveStatement<A, D>> extends DefaultWithDataTree<A, D, E> {
+            public abstract static class WithTypedefNamespace<A, D extends DeclaredStatement<A>,
+                    E extends DataTreeAwareEffectiveStatement<A, D>> extends WithSubstatements<A, D, E> {
+                private final @NonNull ImmutableMap<QName, TypedefEffectiveStatement> typedefNamespace;
+
+                protected WithTypedefNamespace(final ImmutableList<? extends EffectiveStatement<?, ?>> substatements) {
+                    super(substatements);
+                    this.typedefNamespace = createTypedefNamespace(substatements);
+
+                }
+
+                protected WithTypedefNamespace(final WithTypedefNamespace<A, D, E> original) {
+                    super(original);
+                    this.typedefNamespace = original.typedefNamespace;
+                }
+
+                @Override
+                @SuppressWarnings("unchecked")
+                protected <K, V, N extends IdentifierNamespace<K, V>>
+                        Optional<? extends Map<K, V>> getNamespaceContents(final Class<N> namespace) {
+                    if (TypedefNamespace.class.equals(namespace)) {
+                        return Optional.of((Map<K, V>) typedefNamespace);
+                    }
+                    return super.getNamespaceContents(namespace);
+                }
+            }
+
             private final @NonNull Object substatements;
 
             protected WithSubstatements(final ImmutableList<? extends EffectiveStatement<?, ?>> substatements) {
