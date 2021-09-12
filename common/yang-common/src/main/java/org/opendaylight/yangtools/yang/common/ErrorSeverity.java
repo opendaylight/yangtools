1d7d55e795c20d3c4c7bfecd8a59e187db23e1b1
Robert Varga <robert.varga@pantheon.tech>
2021-09-12 16:43:52 +0200

    Remove deprecated NETCONF error interfaces
    RpcError.Error{Severity,Type} as well as YangError have their
    replacements, hence remove them.
    
    Change-Id: Ib90f3d63ee85524fd03e1f81fd5c4e0fd74a1d78
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- common/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/ErrorSeverity.java
+++ common/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/ErrorSeverity.java
@@ -24,21 +24,19 @@ public enum ErrorSeverity {
     /**
      * An error preventing an operation from completing successfully.
      */
-    ERROR("error", RpcError.ErrorSeverity.ERROR),
+    ERROR("error"),
     /**
      * A warning not affecting an operation's ability to complete successfully.
      */
-    WARNING("warning", RpcError.ErrorSeverity.ERROR);
+    WARNING("warning");
 
     private static final Map<String, ErrorSeverity> BY_ELEMENT_BODY =
         Maps.uniqueIndex(Arrays.asList(values()), ErrorSeverity::elementBody);
 
-    private final RpcError.ErrorSeverity legacy;
     private final String elementBody;
 
-    ErrorSeverity(final String elementName, final RpcError.ErrorSeverity legacy) {
+    ErrorSeverity(final String elementName) {
         this.elementBody = requireNonNull(elementName);
-        this.legacy = requireNonNull(legacy);
     }
 
     /**
@@ -48,11 +46,6 @@ public enum ErrorSeverity {
      */
     public String elementBody() {
         return elementBody;
-    }
-
-    @Deprecated
-    public RpcError.ErrorSeverity toLegacy() {
-        return legacy;
     }
 
     public static @Nullable ErrorSeverity forElementBody(final String elementBody) {
