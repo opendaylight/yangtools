fd0019215bc44e2d2d52f7890c84b1042e71bc22
Robert Varga <robert.varga@pantheon.tech>
2021-09-30 19:30:07 +0200

    WIP: Populate ietf-restconf operations container
    The 'operations' container defined in ietf-restconf module has magic
    semantics: it needs to have an empty leaf for every RPC that is
    available in the EffectiveModelContext.
    
    WWe are hard-pressed here to make things work with our parser
    infrastructure, as we currently do not have hooks to do this kind of
    schema-tree specific magic. Luckily there is also a YANG extension in
    that same model, so we hijack that to hook onto to the container
    inference and modify it just in time and make the magic happen.
    
    // FIXME: add leaf/typedef support for undeclared statements
    // FIXME: finish test suite
    
    JIRA: YANGTOOLS-1338
    Change-Id: I9cf774a148f0e764940b9725f717fa0c60ef1bf9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/rfc8040-parser-support/src/test/java/org/opendaylight/yangtools/rfc8040/parser/AbstractYangDataTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8040.parser;
+
+import java.io.IOException;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.opendaylight.yangtools.yang.model.repo.api.YangTextSchemaSource;
+import org.opendaylight.yangtools.yang.parser.api.YangParserConfiguration;
+import org.opendaylight.yangtools.yang.parser.api.YangSyntaxErrorException;
+import org.opendaylight.yangtools.yang.parser.rfc7950.reactor.RFC7950Reactors;
+import org.opendaylight.yangtools.yang.parser.rfc7950.repo.YangStatementStreamSource;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ModelProcessingPhase;
+import org.opendaylight.yangtools.yang.parser.spi.source.StatementStreamSource;
+import org.opendaylight.yangtools.yang.parser.stmt.reactor.CrossSourceStatementReactor;
+
+public abstract class AbstractYangDataTest {
+    static final StatementStreamSource IETF_RESTCONF_MODULE = sourceForResource(
+        "/yang-data-extension-test/ietf-restconf.yang");
+
+    static CrossSourceStatementReactor REACTOR;
+
+    @BeforeClass
+    public static void createReactor() {
+        REACTOR = RFC7950Reactors.vanillaReactorBuilder()
+                .addNamespaceSupport(ModelProcessingPhase.FULL_DECLARATION, YangDataArgumentNamespace.BEHAVIOUR)
+                .addStatementSupport(ModelProcessingPhase.FULL_DECLARATION,
+                    new YangDataStatementSupport(YangParserConfiguration.DEFAULT))
+                .build();
+    }
+
+    @AfterClass
+    public static void freeReactor() {
+        REACTOR = null;
+    }
+
+    static StatementStreamSource sourceForResource(final String resourceName) {
+        try {
+            return YangStatementStreamSource.create(YangTextSchemaSource.forResource(resourceName));
+        } catch (IOException | YangSyntaxErrorException e) {
+            throw new IllegalArgumentException("Failed to create source", e);
+        }
+    }
+
+}
