9fc3c851c77c4b2543676c2454c48e8e1e16294e
Robert Varga <robert.varga@pantheon.tech>
2021-10-02 19:47:51 +0200

    Remove TestUtils.parseYangSources
    We have loadModules() used more widely, eliminate code duplication. Also
    cleanup callers with better assertions.
    
    Change-Id: Iad5a6121dba2c0ea7008a948923c23ce33ad548a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug394Test.java
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug394Test.java
@@ -13,11 +13,9 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.stream.Collectors;
 import org.junit.Test;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.model.api.ContainerSchemaNode;
-import org.opendaylight.yangtools.yang.model.api.ExtensionDefinition;
 import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.api.stmt.UnrecognizedStatement;
@@ -28,7 +26,7 @@ import org.opendaylight.yangtools.yang.model.api.stmt.UnrecognizedStatement;
 public class Bug394Test {
     @Test
     public void testParseList() throws Exception {
-        final SchemaContext context = TestUtils.loadModules(getClass().getResource("/bugs/bug394-retest").toURI());
+        final SchemaContext context = TestUtils.loadModules("/bugs/bug394-retest");
         final Module bug394 = context.findModules("bug394").iterator().next();
         final Module bug394_ext = context.findModules("bug394-ext").iterator().next();
 
@@ -40,9 +38,7 @@ public class Bug394Test {
             .declaredSubstatements(UnrecognizedStatement.class);
         assertEquals(2, nodes.size());
 
-        final Collection<QName> extensions = bug394_ext.getExtensionSchemaNodes()
-            .stream().map(ExtensionDefinition::getQName).collect(Collectors.toUnmodifiableList());
-        assertEquals(3, extensions.size());
+        assertEquals(3, bug394_ext.getExtensionSchemaNodes().size());
 
         final Iterator<? extends UnrecognizedStatement> it = nodes.iterator();
         assertTrue(extensions.contains(it.next().statementDefinition().getStatementName()));
