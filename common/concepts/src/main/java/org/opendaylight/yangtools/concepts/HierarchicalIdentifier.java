4d6b74384358af27dacb9c99e85b2f8dba1fec17
Robert Varga <robert.varga@pantheon.tech>
2021-10-02 17:04:00 +0200

    Introduce HierarchicalIdentifier to replace Path
    Path is always combined with Identifier, and it really is an extension
    of Identifier contract. Add a new interface to capture that contract
    and deprecate Path, so we get out of java.nio.file.Path's way.
    
    JIRA: YANGTOOLS-1344
    Change-Id: Ida2f28b1f2d349b67115611eac843e7da8628f42
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit ce393b58bbeebe85e7725f357f9eb73c8cfafe13)

---
+++ common/concepts/src/main/java/org/opendaylight/yangtools/concepts/HierarchicalIdentifier.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.concepts;
+
+/**
+ * An {@link Identifier} tied to some tree-like structure, similar to how {@link java.nio.file.Path} is tied to a
+ * conceptual file system. In addition to equivalence class implied by Identifier, the hierarchical nature of these
+ * identifiers also introduces a notion of containment: a HierarchicalIdentifier is said to contain another
+ * HierarchicalIdentifier if the former points to an ancestor node of the node pointed to by the latter in the same
+ * instance of the tree-like structure they are defined on. This property is expressed through
+ * {@link #contains(HierarchicalIdentifier)}.
+ */
+public interface HierarchicalIdentifier<T extends HierarchicalIdentifier<T>> extends Identifier, Path<T> {
+    /**
+     * Check if this identifier contains some other identifier. If we take HierarchicalIdentifier to be similar to a
+     * {@link java.nio.file.Path}, this is method is the equivalent of {@code other.startsWith(this)}.
+     *
+     * @param other Other identifier, may not be null
+     * @return True if this identifier contains the other identifier
+     * @throws NullPointerException if {@code other} is null
+     */
+    @Override
+    boolean contains(T other);
+}
