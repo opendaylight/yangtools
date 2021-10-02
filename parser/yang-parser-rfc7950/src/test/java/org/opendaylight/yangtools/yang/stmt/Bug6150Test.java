5b097e25aa9cd43ee854e022432e7631ae3c7fec
Robert Varga <robert.varga@pantheon.tech>
2021-10-02 21:04:28 +0200

    Cleanup TestUtils.parseYangSources()
    We are being needlessly indirect here, as we are always loading
    resources. Clean that up.
    
    Change-Id: I050f5255b50445d348a4e760c7dbedb9c5a33dd8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug6150Test.java
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/Bug6150Test.java
@@ -8,41 +8,23 @@
 package org.opendaylight.yangtools.yang.stmt;
 
 import static org.junit.Assert.assertNotNull;
-import static org.opendaylight.yangtools.yang.stmt.StmtTestUtils.sourceForResource;
 
 import org.junit.Test;
-import org.opendaylight.yangtools.yang.model.api.SchemaContext;
-import org.opendaylight.yangtools.yang.parser.rfc7950.reactor.RFC7950Reactors;
-import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
-import org.opendaylight.yangtools.yang.parser.spi.source.StatementStreamSource;
 
 public class Bug6150Test {
-
-    private static final StatementStreamSource TARGET = sourceForResource("/bugs/bug6150/target.yang");
-    private static final StatementStreamSource AUGMENT_FIRST = sourceForResource("/bugs/bug6150/aug-first.yang");
-    private static final StatementStreamSource AUGMENT_SECOND = sourceForResource("/bugs/bug6150/aug-second.yang");
-
     @Test
-    public void effectiveAugmentFirstTest() throws ReactorException {
-        final SchemaContext result = RFC7950Reactors.defaultReactor().newBuild()
-                .addSources(TARGET, AUGMENT_FIRST)
-                .buildEffective();
-        assertNotNull(result);
+    public void effectiveAugmentFirstTest() throws Exception {
+        assertNotNull(TestUtils.parseYangSource("/bugs/bug6150/target.yang", "/bugs/bug6150/aug-first.yang"));
     }
 
     @Test
-    public void effectiveAugmentSecondTest() throws ReactorException {
-        final SchemaContext result = RFC7950Reactors.defaultReactor().newBuild()
-                .addSources(TARGET, AUGMENT_SECOND)
-                .buildEffective();
-        assertNotNull(result);
+    public void effectiveAugmentSecondTest() throws Exception {
+        assertNotNull(TestUtils.parseYangSource("/bugs/bug6150/target.yang", "/bugs/bug6150/aug-second.yang"));
     }
 
     @Test
-    public void effectiveAugmentBothTest() throws ReactorException {
-        final SchemaContext result = RFC7950Reactors.defaultReactor().newBuild()
-                .addSources(TARGET, AUGMENT_FIRST, AUGMENT_SECOND)
-                .buildEffective();
-        assertNotNull(result);
+    public void effectiveAugmentBothTest() throws Exception {
+        assertNotNull(TestUtils.parseYangSource("/bugs/bug6150/target.yang", "/bugs/bug6150/aug-first.yang",
+            "/bugs/bug6150/aug-second.yang"));
     }
 }
