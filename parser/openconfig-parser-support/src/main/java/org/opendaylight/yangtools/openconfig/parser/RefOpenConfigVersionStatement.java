7a4ed9ba44edfdedd36fa1623a30b60aa6812704
Robert Varga <robert.varga@pantheon.tech>
2021-10-21 11:15:40 +0200

    Move OpenConfigVersion support
    We are no longer using semantic versions to resolve linkage. Move
    statement support to openconfig-parser-support.
    
    JIRA: YANGTOOLS-1432
    Change-Id: I144cd2a1040c264b9c6bfafceb95ef5a77e09d90
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/openconfig-parser-support/src/main/java/org/opendaylight/yangtools/openconfig/parser/RefOpenConfigVersionStatement.java
+++ parser/openconfig-parser-support/src/main/java/org/opendaylight/yangtools/openconfig/parser/RefOpenConfigVersionStatement.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.yangtools.yang.parser.openconfig.stmt;
+package org.opendaylight.yangtools.openconfig.parser;
 
 import org.opendaylight.yangtools.concepts.SemVer;
 import org.opendaylight.yangtools.openconfig.model.api.OpenConfigVersionStatement;
