11b82c0fa8b86365c256e30adde9b82516f31ffa
Robert Varga <robert.varga@pantheon.tech>
2021-10-27 13:26:04 +0200

    WIP: Introduce yang.common.NetconfError
    // FIXME: needs and update to UniqueValidation
    // FIXME: needs dealing with YangErrorInfos
    
    JIRA: YANGTOOLS-1360
    Change-Id: I83842dda676bd3d3d2f5fcd8e7e5c7871ab6b4da
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- common/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/ErrorInfo.java
+++ common/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/ErrorInfo.java
@@ -5,21 +5,22 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.yangtools.yang.data.api;
+package org.opendaylight.yangtools.yang.common;
 
 import com.google.common.annotations.Beta;
-import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.opendaylight.yangtools.yang.common.ErrorInfo;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.concepts.Immutable;
 
 /**
  * An element of {@code error-info} container, as modeled in {@code errorInfoType} of
  * <a href="https://datatracker.ietf.org/doc/html/rfc6241#appendix-B">RFC6241, Appendix B</a>.
  */
 @Beta
-@NonNullByDefault
-public interface YangErrorInfo<T extends YangErrorInfo<T, B>, B> extends ErrorInfo<T> {
-
-    Class<B> bodyRepresentation();
-
-    B body();
+public interface ErrorInfo<T extends ErrorInfo<T>> extends Immutable {
+    /**
+     * Return this object's representation class.
+     *
+     * @return This object's representation class.
+     */
+    @NonNull Class<T> representation();
 }
