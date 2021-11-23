94d88e36f36abca592db62c9d0239689d31c31e7
Robert Varga <robert.varga@pantheon.tech>
2021-11-23 13:45:27 +0100

    WIP: Add a reproducer
    Change-Id: Ib1f0bde83a90e4bb45a7c4fa1fc62af205f4ea6c

---
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/YT1370Test.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.util.Set;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.parser.api.YangParserConfiguration;
+
+public class YT1370Test {
+    @Test
+    public void testUsesAugmentUsesException() throws Exception {
+        assertNotNull(StmtTestUtils.parseYangSources(
+            YangParserConfiguration.DEFAULT, Set.of(),
+            new File(YT1370Test.class.getResource("/bugs/YT1370/ietf-subscribed-notifications.yang").toURI()),
+            new File(YT1370Test.class.getResource("/bugs/YT1370/ietf-yang-push.yang").toURI())));
+    }
+}
