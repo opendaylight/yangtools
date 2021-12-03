6c1ebf0dc9dda320fbdf6814b8d01414f976e6ee
Robert Varga <robert.varga@pantheon.tech>
2021-12-04 00:21:41 +0100

    Do not retain single-entry RangeSets
    ImmutableRangeSet with a single entry is a tad inefficient: we retain
    the set and also a singleton list. Rather than doing that, store only
    the single range -- reducing the number of objects we retain.
    
    JIRA: YANGTOOLS-1376
    Change-Id: I8a3b497fa4c4b8a0106136f056bb363d6e103930
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-ri/src/main/java/org/opendaylight/yangtools/yang/model/ri/type/ResolvedRangeConstraint.java
+++ model/yang-model-ri/src/main/java/org/opendaylight/yangtools/yang/model/ri/type/ResolvedRangeConstraint.java
@@ -7,47 +7,19 @@
  */
 package org.opendaylight.yangtools.yang.model.ri.type;
 
-import static java.util.Objects.requireNonNull;
-
-import com.google.common.collect.ImmutableRangeSet;
 import com.google.common.collect.RangeSet;
-import java.util.Optional;
 import org.eclipse.jdt.annotation.NonNull;
-import org.opendaylight.yangtools.concepts.Immutable;
 import org.opendaylight.yangtools.yang.model.api.ConstraintMetaDefinition;
 import org.opendaylight.yangtools.yang.model.api.type.RangeConstraint;
 
-final class ResolvedRangeConstraint<T extends Number & Comparable<T>> implements RangeConstraint<T>, Immutable {
-    private final ImmutableRangeSet<@NonNull T> ranges;
-    private final ConstraintMetaDefinition meta;
-
+final class ResolvedRangeConstraint<T extends Number & Comparable<T>>
+        extends AbstractConstraint<@NonNull T> implements RangeConstraint<T> {
     ResolvedRangeConstraint(final ConstraintMetaDefinition meta, final RangeSet<T> ranges) {
-        this.meta = requireNonNull(meta);
-        this.ranges = ImmutableRangeSet.copyOf(ranges);
-    }
-
-    @Override
-    public Optional<String> getDescription() {
-        return meta.getDescription();
-    }
-
-    @Override
-    public Optional<String> getErrorAppTag() {
-        return meta.getErrorAppTag();
-    }
-
-    @Override
-    public Optional<String> getErrorMessage() {
-        return meta.getErrorMessage();
-    }
-
-    @Override
-    public Optional<String> getReference() {
-        return meta.getReference();
+        super(meta, ranges);
     }
 
     @Override
     public RangeSet<@NonNull T> getAllowedRanges() {
-        return ranges;
+        return ranges();
     }
 }
