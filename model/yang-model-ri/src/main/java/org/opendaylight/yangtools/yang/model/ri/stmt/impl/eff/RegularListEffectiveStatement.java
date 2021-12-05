cf966ad440bd1a19a14c4aa45c5f3b0826241487
Robert Varga <robert.varga@pantheon.tech>
2021-12-05 18:56:49 +0100

    Optimize ListEffectiveStatement layout
    We can save a field in case the declared statement's argument matches
    the effective argument.
    
    JIRA: YANGTOOLS-1316
    Change-Id: I02842d9dbe741b2d26f3f75948103715707df3b3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-ri/src/main/java/org/opendaylight/yangtools/yang/model/ri/stmt/impl/eff/RegularListEffectiveStatement.java
+++ model/yang-model-ri/src/main/java/org/opendaylight/yangtools/yang/model/ri/stmt/impl/eff/RegularListEffectiveStatement.java
@@ -7,8 +7,11 @@
  */
 package org.opendaylight.yangtools.yang.model.ri.stmt.impl.eff;
 
+import static java.util.Objects.requireNonNull;
+
 import com.google.common.collect.ImmutableList;
 import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.model.api.ElementCountConstraint;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
@@ -16,24 +19,33 @@ import org.opendaylight.yangtools.yang.model.api.stmt.ListStatement;
 
 public final class RegularListEffectiveStatement extends AbstractListEffectiveStatement {
     private final ElementCountConstraint elementCountConstraint;
+    private final @NonNull QName argument;
 
     public RegularListEffectiveStatement(final ListStatement declared, final QName argument, final int flags,
             final ImmutableList<? extends EffectiveStatement<?, ?>> substatements,
             final ImmutableList<QName> keyDefinition, final ElementCountConstraint elementCountConstraint) {
-        super(declared, argument, flags, substatements, keyDefinition);
+        super(declared, flags, substatements, keyDefinition);
+        this.argument = requireNonNull(argument);
         this.elementCountConstraint = elementCountConstraint;
     }
 
     public RegularListEffectiveStatement(final RegularListEffectiveStatement originalEffective, final QName argument,
             final int flags) {
-        super(originalEffective, argument, flags);
+        super(originalEffective, flags);
+        this.argument = requireNonNull(argument);
         elementCountConstraint = originalEffective.elementCountConstraint;
     }
 
     public RegularListEffectiveStatement(final EmptyListEffectiveStatement originalEffective, final QName argument,
             final int flags) {
-        super(originalEffective, argument, flags);
+        super(originalEffective, flags);
+        this.argument = requireNonNull(argument);
         elementCountConstraint = null;
+    }
+
+    @Override
+    public QName argument() {
+        return argument;
     }
 
     @Override
