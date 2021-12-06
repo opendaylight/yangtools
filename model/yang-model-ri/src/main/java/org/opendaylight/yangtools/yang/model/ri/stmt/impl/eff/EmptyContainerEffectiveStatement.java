aa547b17a1d9387f8d2804bc5287f9e774046c24
Robert Varga <robert.varga@pantheon.tech>
2021-12-06 09:42:47 +0100

    Specialize ContainerEffectiveStatementImpl
    We can improve class layout by omitting the argument if it was unchanged
    relative to declaration. Split the implementation into two classes.
    
    JIRA: YANGTOOLS-1316
    Change-Id: I2852f35d9f22cbdad70fa3a4786cfba7cc9c5faf
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ model/yang-model-ri/src/main/java/org/opendaylight/yangtools/yang/model/ri/stmt/impl/eff/EmptyContainerEffectiveStatement.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.ri.stmt.impl.eff;
+
+import com.google.common.collect.ImmutableList;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ContainerStatement;
+
+public final class EmptyContainerEffectiveStatement extends AbstractContainerEffectiveStatement {
+    public EmptyContainerEffectiveStatement(final ContainerStatement declared,
+            final ImmutableList<? extends EffectiveStatement<?, ?>> substatements, final int flags) {
+        super(declared, substatements, flags);
+    }
+
+    public EmptyContainerEffectiveStatement(final AbstractContainerEffectiveStatement original, final int flags) {
+        super(original, flags);
+    }
+
+    @Override
+    public QName argument() {
+        return getDeclared().argument();
+    }
+}
