8a34748ecf8e4cc9cc4a8091d4583cb4369f3231
Robert Varga <robert.varga@pantheon.tech>
2021-12-15 07:58:58 +0100

    Remove DeclaredStatement.statementOrigin()
    DeclaredStatements are always declared and thus statementOrigin() should
    only be exposed from EffectiveStatements.
    
    JIRA: YANGTOOLS-1384
    Change-Id: I7b02b782dd25c5328d6c724d3ec08d54c9d9bfbb
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-export/src/main/java/org/opendaylight/yangtools/yang/model/export/YinXMLEventReader.java
+++ model/yang-model-export/src/main/java/org/opendaylight/yangtools/yang/model/export/YinXMLEventReader.java
@@ -42,7 +42,7 @@ final class YinXMLEventReader implements XMLEventReader {
 
         OpenElement(final Iterator<? extends DeclaredStatement<?>> children) {
             this.children = requireNonNull(children);
-            this.name = null;
+            name = null;
         }
 
         OpenElement(final QName name, final Iterator<? extends DeclaredStatement<?>> children) {
@@ -180,22 +180,9 @@ final class YinXMLEventReader implements XMLEventReader {
         }
 
         do {
-            while (current.children.hasNext()) {
-                // We have to mind child statement origin and not emit empty implicit children
-                final DeclaredStatement<?> child = current.children.next();
-                switch (child.statementOrigin()) {
-                    case CONTEXT:
-                        final Iterator<? extends DeclaredStatement<?>> it = child.declaredSubstatements().iterator();
-                        if (it.hasNext()) {
-                            current = new OpenElement(it);
-                        }
-                        break;
-                    case DECLARATION:
-                        addStatement(child);
-                        return;
-                    default:
-                        throw new IllegalStateException("Unhandled statement origin " + child.statementOrigin());
-                }
+            if (current.children.hasNext()) {
+                addStatement(current.children.next());
+                return;
             }
 
             if (current.name != null) {
