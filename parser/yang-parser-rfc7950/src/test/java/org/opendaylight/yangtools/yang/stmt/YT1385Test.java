01a4fb5796e0f446dcca573538ee89773ae7a1c5
Robert Varga <robert.varga@pantheon.tech>
2021-12-15 18:24:37 +0100

    WIP: Validate unique statement references
    Unique statement's argument needs to be validated against the effective
    model, so that we discover when we have mismatches in arguments.
    
    // FIXME: finish test suite with augments/uses
    // FIXME: audit fallout in existing tests
    
    JIRA: YANGTOOLS-1385
    Change-Id: I4e41563b40dae6a186d8763d9711ce0c54afdbc3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/YT1385Test.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.CoreMatchers.startsWith;
+
+import org.junit.Test;
+
+public class YT1385Test extends AbstractYangTest {
+    @Test
+    public void testSameModuleWrongUnique() {
+        assertSourceException(allOf(
+            startsWith("Following components of unique statement argument refer to non-existent nodes: "
+                + "[Descendant{qnames=[(foo)bar]}] [at "),
+            endsWith("YT1385/foo.yang:5:3]")),
+            "/bugs/YT1385/foo.yang");
+    }
+}
