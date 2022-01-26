b213ad29d6373e04b25bafe8f0646d97963040be
Robert Varga <robert.varga@pantheon.tech>
2022-01-26 11:04:39 +0100

    Add SoftSchemaSourceCache
    GuavaSchemaSourceCache has a rather ugly design, which cannot be easily
    scaled. Introduce a simpler SoftSchemaSourceCache as its replacement.
    
    JIRA: YANGTOOLS-1391
    Change-Id: I0de9092885ede3efff9d76d48ec14b85a69818b2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ yang/yang-repo-spi/src/test/java/org/opendaylight/yangtools/yang/model/repo/spi/SoftSchemaSourceCacheTest.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.repo.spi;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+
+import com.google.common.base.MoreObjects.ToStringHelper;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.opendaylight.yangtools.yang.common.Revision;
+import org.opendaylight.yangtools.yang.model.repo.api.RevisionSourceIdentifier;
+import org.opendaylight.yangtools.yang.model.repo.api.YangSchemaSourceRepresentation;
+import org.opendaylight.yangtools.yang.model.repo.api.YangTextSchemaSource;
+
+@RunWith(MockitoJUnitRunner.StrictStubs.class)
+public class SoftSchemaSourceCacheTest {
+    public static final Class<YangSchemaSourceRepresentation> REPRESENTATION = YangSchemaSourceRepresentation.class;
+    public static final long LIFETIME = 1000L;
+    public static final TimeUnit UNITS = TimeUnit.MILLISECONDS;
+
+    @Mock
+    public SchemaSourceRegistry registry;
+    @Mock
+    public SchemaSourceRegistration<?> registration;
+
+    @Before
+    public void setUp() {
+        doNothing().when(registration).close();
+        doReturn(registration).when(registry).registerSchemaSource(any(SchemaSourceProvider.class),
+            any(PotentialSchemaSource.class));
+    }
+
+    @Test
+    public void inMemorySchemaSourceCacheTest() {
+        try (var cache = new SoftSchemaSourceCache<>(registry, REPRESENTATION)) {
+            assertNotNull(cache);
+        }
+    }
+
+    @Test
+    public void inMemorySchemaSourceCacheOfferAndGetSourcestest() throws Exception {
+        try (var cache = new SoftSchemaSourceCache<>(registry, REPRESENTATION)) {
+            final String content = "content";
+            final YangTextSchemaSource source = new TestingYangSource("test", "2012-12-12", content);
+            cache.offer(source);
+            final var sourceIdentifier = RevisionSourceIdentifier.create("test", Revision.of("2012-12-12"));
+            final var checkedSource = cache .getSource(sourceIdentifier);
+            assertNotNull(checkedSource);
+            final var yangSchemaSourceRepresentation = checkedSource.get();
+            assertNotNull(yangSchemaSourceRepresentation);
+            assertEquals(sourceIdentifier, yangSchemaSourceRepresentation.getIdentifier());
+        }
+    }
+
+    @Test
+    public void inMemorySchemaSourceCacheNullGetSourcestest() throws Exception {
+        try (var cache = new SoftSchemaSourceCache<>(registry, REPRESENTATION)) {
+            final var sourceIdentifier = RevisionSourceIdentifier.create("test", Revision.of("2012-12-12"));
+            final var checkedSource = cache.getSource(sourceIdentifier);
+            assertNotNull(checkedSource);
+            assertThrows(ExecutionException.class, () -> checkedSource.get());
+        }
+    }
+
+    @Test
+    public void inMemorySchemaSourceCache3test() throws InterruptedException, ExecutionException {
+        try (var cache1 = new SoftSchemaSourceCache<>(registry, REPRESENTATION)) {
+            try (var cache2 = new SoftSchemaSourceCache<>(registry, REPRESENTATION)) {
+                final String content = "content";
+                final YangTextSchemaSource source = new TestingYangSource("test", "2012-12-12", content);
+                cache1.offer(source);
+                cache2.offer(source);
+
+                final var sourceIdentifier = RevisionSourceIdentifier.create("test", Revision.of("2012-12-12"));
+                final var checkedSource = cache1.getSource(sourceIdentifier);
+                final var checkedSource2 = cache2.getSource(sourceIdentifier);
+                assertNotNull(checkedSource);
+                assertNotNull(checkedSource2);
+
+                assertEquals(checkedSource.get(), checkedSource2.get());
+            }
+        }
+    }
+
+    private static class TestingYangSource extends YangTextSchemaSource {
+        private final String content;
+
+        TestingYangSource(final String name, final String revision, final String content) {
+            super(RevisionSourceIdentifier.create(name, Revision.ofNullable(revision)));
+            this.content = content;
+        }
+
+        @Override
+        public InputStream openStream() {
+            return new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8));
+        }
+
+        @Override
+        public Optional<String> getSymbolicName() {
+            return Optional.empty();
+        }
+
+        @Override
+        protected ToStringHelper addToStringAttributes(final ToStringHelper toStringHelper) {
+            return toStringHelper;
+        }
+    }
+}
