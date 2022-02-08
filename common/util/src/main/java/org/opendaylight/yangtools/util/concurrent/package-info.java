d715a54ff4dce3f7d6b254c3d59ee4eef3d31f5d
Robert Varga <robert.varga@pantheon.tech>
2022-02-08 14:38:45 +0100

    Migrate common/util to bnd-parent
    We have a better-integrated parent, use that instead of the old felix
    plugin.
    
    Change-Id: Id400abd10892fd350f3f50119f5247d09f6e5fe4
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ common/util/src/main/java/org/opendaylight/yangtools/util/concurrent/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Utility classes for dealing with asynchronous and concurrent tasks.
+ */
+@Export
+package org.opendaylight.yangtools.util.concurrent;
+
+import org.osgi.annotation.bundle.Export;
