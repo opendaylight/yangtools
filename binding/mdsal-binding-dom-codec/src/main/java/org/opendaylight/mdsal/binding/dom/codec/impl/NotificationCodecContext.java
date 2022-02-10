459c9dfbee88debe5ec3ecfe08d9ef6df0d5d0d0
Robert Varga <robert.varga@pantheon.tech>
2022-02-11 00:26:22 +0100

    Bump byte-buddy to 1.12.8
    https://github.com/raphw/byte-buddy/releases/tag/byte-buddy-1.12.2
    https://github.com/raphw/byte-buddy/releases/tag/byte-buddy-1.12.3
    https://github.com/raphw/byte-buddy/releases/tag/byte-buddy-1.12.4
    https://github.com/raphw/byte-buddy/releases/tag/byte-buddy-1.12.5
    https://github.com/raphw/byte-buddy/releases/tag/byte-buddy-1.12.6
    https://github.com/raphw/byte-buddy/releases/tag/byte-buddy-1.12.7
    https://github.com/raphw/byte-buddy/releases/tag/byte-buddy-1.12.8
    
    Change-Id: I660d0fff4a67b33202bdb70b56ee546d63ac15d3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/impl/NotificationCodecContext.java
+++ binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/impl/NotificationCodecContext.java
@@ -8,6 +8,7 @@
 package org.opendaylight.mdsal.binding.dom.codec.impl;
 
 import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.base.Verify.verifyNotNull;
 import static net.bytebuddy.implementation.bytecode.member.MethodVariableAccess.loadThis;
 import static org.opendaylight.mdsal.binding.dom.codec.impl.ByteBuddyUtils.getField;
 import static org.opendaylight.mdsal.binding.dom.codec.impl.ByteBuddyUtils.putField;
@@ -144,7 +145,7 @@ final class NotificationCodecContext<D extends DataObject & Notification>
         @Override
         public ByteCodeAppender appender(final Target implementationTarget) {
             final TypeDescription instrumentedType = implementationTarget.getInstrumentedType();
-            final InGenericShape superCtor = instrumentedType.getSuperClass().getDeclaredMethods()
+            final InGenericShape superCtor = verifyNotNull(instrumentedType.getSuperClass()).getDeclaredMethods()
                     .filter(ElementMatchers.isConstructor()).getOnly();
 
             return new ByteCodeAppender.Simple(
