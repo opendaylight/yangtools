3699660bae64e57acf3db1dd7466b3cbc4c5063f
Robert Varga <robert.varga@pantheon.tech>
2022-03-01 21:42:12 +0100

    Fix AugmentEffectiveStatement javadoc
    We have leading + signs here, remove them.
    
    Change-Id: Iff1c28e7b6fce061bbe10b1c50fedd21c3cfb71d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/AugmentEffectiveStatement.java
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/AugmentEffectiveStatement.java
@@ -13,8 +13,8 @@ import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 
 /**
-+ * {@link EffectiveStatement} representation of a {@code augment} statement as defined by
-+ * <a href="https://datatracker.ietf.org/doc/html/rfc7950#section-7.17">RFC7950</a>.
+ * {@link EffectiveStatement} representation of a {@code augment} statement as defined by
+ * <a href="https://datatracker.ietf.org/doc/html/rfc7950#section-7.17">RFC7950</a>.
  */
 @Beta
 public interface AugmentEffectiveStatement
