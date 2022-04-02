64e71b7c5d68f5cfd681f8deb4612ed6f487858b
Robert Varga <robert.varga@pantheon.tech>
2022-04-02 14:13:54 +0200

    Fix assertion import
    Let's not use jupieter.api just yet here.
    
    Change-Id: I4645f4c476f9d2cc544a39b0b1ad5ce6c93873fc
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- data/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/schema/nodes/YT1417Test.java
+++ data/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/schema/nodes/YT1417Test.java
@@ -7,8 +7,8 @@
  */
 package org.opendaylight.yangtools.yang.data.impl.schema.nodes;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
