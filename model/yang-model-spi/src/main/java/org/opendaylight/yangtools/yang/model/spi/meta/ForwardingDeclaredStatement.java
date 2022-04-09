0b9bafc30c20aa744ee80222999b2f57cbee468f
Robert Varga <robert.varga@pantheon.tech>
2022-04-09 21:47:48 +0200

    Seal ModelStatement
    ModelStatement can either be a DeclaredStatement or an
    EffectiveStatement. Make sure we enforce that invariant.
    
    Change-Id: Ib54d7136527b3c49fd36e813a9f1acc5b02a8894
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-spi/src/main/java/org/opendaylight/yangtools/yang/model/spi/meta/ForwardingDeclaredStatement.java
+++ model/yang-model-spi/src/main/java/org/opendaylight/yangtools/yang/model/spi/meta/ForwardingDeclaredStatement.java
@@ -8,18 +8,30 @@
 package org.opendaylight.yangtools.yang.model.spi.meta;
 
 import com.google.common.annotations.Beta;
+import com.google.common.collect.ForwardingObject;
 import java.util.List;
 import java.util.Optional;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclarationReference;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 
 /**
  * Common base class for forwarding implementations of {@link DeclaredStatement}.
  */
 @Beta
 public abstract class ForwardingDeclaredStatement<A, D extends DeclaredStatement<A>>
-        extends ForwardingModelStatement<A, D> implements DeclaredStatement<A> {
+        extends ForwardingObject implements DeclaredStatement<A> {
+    @Override
+    public StatementDefinition statementDefinition() {
+        return delegate().statementDefinition();
+    }
+
+    @Override
+    public A argument() {
+        return delegate().argument();
+    }
+
     @Override
     public String rawArgument() {
         return delegate().rawArgument();
