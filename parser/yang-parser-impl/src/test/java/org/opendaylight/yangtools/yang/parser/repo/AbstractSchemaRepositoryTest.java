a0c3cda5c9209e407e811c0b3d81003629eeb6dd
Robert Varga <robert.varga@pantheon.tech>
2022-05-03 19:27:59 +0200

    Fix failure source not being reported
    When the parser fails with a well-known exception, extract the
    SourceIdentifier of the failed YANG source.
    
    JIRA: YANGTOOLS-1428
    Change-Id: Ib40b01a977131ce1d244be5bd4e67c39e04ab626
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 6dd19d2f87b3391b95d8087a3ff025c5414fd4e9)
    (cherry picked from commit fd1fcd8b28b69bc86053ff47ddefcc42d12fdcee)

---
+++ parser/yang-parser-impl/src/test/java/org/opendaylight/yangtools/yang/parser/repo/AbstractSchemaRepositoryTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.repo;
+
+import com.google.common.collect.SetMultimap;
+import com.google.common.util.concurrent.ListenableFuture;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.opendaylight.yangtools.yang.model.repo.api.SchemaContextFactoryConfiguration;
+import org.opendaylight.yangtools.yang.model.repo.api.YangTextSchemaSource;
+import org.opendaylight.yangtools.yang.parser.api.YangSyntaxErrorException;
+import org.opendaylight.yangtools.yang.parser.rfc7950.ir.IRSchemaSource;
+import org.opendaylight.yangtools.yang.parser.rfc7950.repo.TextToIRTransformer;
+
+public abstract class AbstractSchemaRepositoryTest {
+    static ListenableFuture<EffectiveModelContext> createSchemaContext(
+            final SetMultimap<QNameModule, QNameModule> modulesWithSupportedDeviations, final String... resources) {
+        final var sharedSchemaRepository = new SharedSchemaRepository();
+        final var requiredSources = Arrays.stream(resources)
+            .map(resource -> {
+                final var yangSource = assertYangTextResource(resource);
+                yangSource.register(sharedSchemaRepository);
+                yangSource.setResult();
+                return yangSource.getId();
+            })
+            .collect(Collectors.toUnmodifiableList());
+
+        return sharedSchemaRepository
+            .createEffectiveModelContextFactory(SchemaContextFactoryConfiguration.builder()
+                .setModulesDeviatedByModules(modulesWithSupportedDeviations)
+                .build())
+            .createEffectiveModelContext(requiredSources);
+    }
+
+    private static SettableSchemaProvider<IRSchemaSource> assertYangTextResource(final String resourceName) {
+        final IRSchemaSource yangSource;
+        try {
+            yangSource = TextToIRTransformer.transformText(YangTextSchemaSource.forResource(resourceName));
+        } catch (YangSyntaxErrorException | IOException e) {
+            throw new AssertionError("Failed to parse " + resourceName, e);
+        }
+        return SettableSchemaProvider.createImmediate(yangSource, IRSchemaSource.class);
+    }
+}
