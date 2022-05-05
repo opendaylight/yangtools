06f1ed248d7d911ad5c06e932a7ca410298e8a7f
Robert Varga <robert.varga@pantheon.tech>
2022-05-05 02:21:56 +0200

    WIP: Add YangLibModule et al.
    JIRA: YANGTOOLS-837
    Change-Id: Iab12347ea065f7d6351b0943ff3416fdb23eb0e5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-api/src/main/java/org/opendaylight/yangtools/yang/parser/api/YangLibModule.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.api;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.common.UnresolvedQName.Unqualified;
+import org.opendaylight.yangtools.yang.common.XMLNamespace;
+import org.opendaylight.yangtools.yang.model.repo.api.SchemaSourceRepresentation;
+import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
+
+/**
+ * A single <a href="https://www.rfc-editor.org/rfc/rfc8525">RFC8525</a> {@code module} or {@code import-only-module}
+ * list entry. Note that the YANG definition has two dissimilar instances, but that really is an artifact of how indexes
+ * work in YANG.
+ *
+ * @param identifier {@link SourceIdentifier} of this module, e.g. the combination of {@code name} and {@code revision}
+ * @param namespace {@link XMLNamespace} of this module
+ * @param submodules Submodules of this module
+ * @param features The set of supported features in this module
+ * @param deviationModuleNames Names of modules containing {@code deviate} statements targetting this module
+ * @param source A {@link SchemaSourceRepresentation} of the module
+ */
+public record YangLibModule(@NonNull SourceIdentifier identifier, @NonNull XMLNamespace namespace,
+        @NonNull ImmutableMap<Unqualified, YangLibSubmodule> submodules, @NonNull ImmutableSet<Unqualified> features,
+        @NonNull ImmutableSet<Unqualified> deviationModuleNames, @NonNull SchemaSourceRepresentation source) {
+    public YangLibModule {
+        requireNonNull(identifier);
+        requireNonNull(namespace);
+        requireNonNull(submodules);
+        requireNonNull(features);
+        requireNonNull(deviationModuleNames);
+    }
+}
