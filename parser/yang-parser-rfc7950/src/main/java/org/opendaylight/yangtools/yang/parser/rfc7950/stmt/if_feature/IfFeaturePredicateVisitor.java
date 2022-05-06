cc392f0c12f3cbd7a895cfda22b8abc7df524a4c
Samuel Schneider <samuel.schneider@pantheon.tech>
2022-05-06 11:58:29 +0200

    Refactor IfFeaturePredicateVisitor
    Refactor IfFeaturePredicateVisitor to work purely on
    IfFeatureExpressionParser without use of ANTLR's visitors.
    
    JIRA: YANGTOOLS-1396
    Change-Id: I4ff988caa34a50da843fd35b8918e7b502bdf770
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/if_feature/IfFeaturePredicateVisitor.java
+++ parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/if_feature/IfFeaturePredicateVisitor.java
@@ -7,15 +7,16 @@
  */
 package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.if_feature;
 
-import static java.util.Objects.requireNonNull;
+import static com.google.common.base.Verify.verify;
+import static com.google.common.base.Verify.verifyNotNull;
 
+import com.google.common.base.VerifyException;
 import com.google.common.collect.ImmutableSet;
-import java.util.ArrayList;
-import java.util.List;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.yangtools.yang.model.api.stmt.IfFeatureExpr;
 import org.opendaylight.yangtools.yang.parser.antlr.IfFeatureExpressionLexer;
 import org.opendaylight.yangtools.yang.parser.antlr.IfFeatureExpressionParser;
@@ -23,68 +24,71 @@ import org.opendaylight.yangtools.yang.parser.antlr.IfFeatureExpressionParser.Id
 import org.opendaylight.yangtools.yang.parser.antlr.IfFeatureExpressionParser.If_feature_exprContext;
 import org.opendaylight.yangtools.yang.parser.antlr.IfFeatureExpressionParser.If_feature_factorContext;
 import org.opendaylight.yangtools.yang.parser.antlr.IfFeatureExpressionParser.If_feature_termContext;
-import org.opendaylight.yangtools.yang.parser.antlr.IfFeatureExpressionParserBaseVisitor;
 import org.opendaylight.yangtools.yang.parser.rfc7950.antlr.SourceExceptionParser;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContextUtils;
 import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
 
-// FIXME: YANGTOOLS-1396: refactor on top of vanilla IfFeatureExpressionParser
 @NonNullByDefault
-final class IfFeaturePredicateVisitor extends IfFeatureExpressionParserBaseVisitor<IfFeatureExpr> {
-    private final StmtContext<?, ?, ?> stmtCtx;
-
-    private IfFeaturePredicateVisitor(final StmtContext<?, ?, ?> ctx) {
-        this.stmtCtx = requireNonNull(ctx);
+final class IfFeaturePredicateVisitor {
+    private IfFeaturePredicateVisitor() {
     }
 
     static IfFeatureExpr parseIfFeatureExpression(final StmtContext<?, ?, ?> ctx, final String value) {
-        final IfFeatureExpressionLexer lexer = new IfFeatureExpressionLexer(CharStreams.fromString(value));
-        final IfFeatureExpressionParser parser = new IfFeatureExpressionParser(new CommonTokenStream(lexer));
-        final IfFeatureExpr ret = new IfFeaturePredicateVisitor(ctx).visit(SourceExceptionParser.parse(lexer, parser,
-            parser::if_feature_expr, ctx.sourceReference()));
-
-        return ret;
+        final var lexer = new IfFeatureExpressionLexer(CharStreams.fromString(value));
+        final var parser = new IfFeatureExpressionParser(new CommonTokenStream(lexer));
+        final var ifFeatureExprContext =
+                SourceExceptionParser.parse(lexer, parser, parser::if_feature_expr, ctx.sourceReference());
+        return parseIfFeatureExpr(ifFeatureExprContext, ctx);
     }
 
-    @Override
-    public IfFeatureExpr visitIf_feature_expr(final @Nullable If_feature_exprContext ctx) {
-        return IfFeatureExpr.or(ctx.if_feature_term().stream()
-            .map(this::visitIf_feature_term)
-            .collect(ImmutableSet.toImmutableSet()));
-    }
+    private static IfFeatureExpr parseIfFeatureExpr(final If_feature_exprContext expr, final StmtContext<?, ?, ?> ctx) {
+        final int count = expr.getChildCount();
+        verify(count % 4 == 1, "Unexpected number of children %s", count);
 
-    @Override
-    public IfFeatureExpr visitIf_feature_term(final @Nullable If_feature_termContext ctx) {
-        final IfFeatureExpr factor = visitIf_feature_factor(ctx.if_feature_factor());
-        final List<If_feature_termContext> terms = ctx.if_feature_term();
-        if (terms == null || terms.isEmpty()) {
-            return factor;
+        final var expressions = ImmutableSet.<IfFeatureExpr>builderWithExpectedSize(count / 4 + 1);
+        for (int i = 0; i < count; i += 4) {
+            expressions.add(parseIfFeatureTerm(getChild(expr, i, If_feature_termContext.class), ctx));
         }
-        final List<IfFeatureExpr> factors = new ArrayList<>(terms.size() + 1);
-        factors.add(factor);
-        for (If_feature_termContext term : terms) {
-            factors.add(visitIf_feature_term(term));
-        }
-        return IfFeatureExpr.and(ImmutableSet.copyOf(factors));
+        return IfFeatureExpr.or(expressions.build());
     }
 
-    @Override
-    public IfFeatureExpr visitIf_feature_factor(final @Nullable If_feature_factorContext ctx) {
-        if (ctx.if_feature_expr() != null) {
-            return visitIf_feature_expr(ctx.if_feature_expr());
-        } else if (ctx.if_feature_factor() != null) {
-            return visitIf_feature_factor(ctx.if_feature_factor()).negate();
-        } else if (ctx.identifier_ref_arg() != null) {
-            return visitIdentifier_ref_arg(ctx.identifier_ref_arg());
-        }
+    private static IfFeatureExpr parseIfFeatureTerm(final If_feature_termContext term, final StmtContext<?, ?, ?> ctx) {
+        final int count = term.getChildCount();
+        verify(count % 4 == 1, "Unexpected number of children %s", count);
 
-        throw new SourceException("Unexpected grammar context during parsing of IfFeature expression. "
-                + "Most probably IfFeature grammar has been changed.", stmtCtx);
+        final var expressions = ImmutableSet.<IfFeatureExpr>builderWithExpectedSize(count / 4 + 1);
+        expressions.add(parseIfFeatureFactor(getChild(term, 0, If_feature_factorContext.class), ctx));
+
+        for (int i = 4; i < count; i += 4) {
+            expressions.add(parseIfFeatureTerm(getChild(term, i, If_feature_termContext.class), ctx));
+        }
+        return IfFeatureExpr.and(expressions.build());
     }
 
-    @Override
-    public IfFeatureExpr visitIdentifier_ref_arg(final @Nullable Identifier_ref_argContext ctx) {
-        return IfFeatureExpr.isPresent(StmtContextUtils.parseNodeIdentifier(stmtCtx, ctx.getText()));
+    private static IfFeatureExpr parseIfFeatureFactor(final If_feature_factorContext factor,
+            final StmtContext<?, ?, ?> ctx) {
+        final var first = factor.getChild(0);
+        if (first instanceof Identifier_ref_argContext refArg) {
+            return IfFeatureExpr.isPresent(StmtContextUtils.parseNodeIdentifier(ctx, refArg.getText()));
+        } else if (first instanceof TerminalNode terminal) {
+            return switch (terminal.getSymbol().getType()) {
+                case IfFeatureExpressionParser.LP ->
+                    parseIfFeatureExpr(verifyNotNull(factor.getChild(If_feature_exprContext.class, 0)), ctx);
+                case IfFeatureExpressionParser.NOT ->
+                    parseIfFeatureFactor(getChild(factor, 2, If_feature_factorContext.class), ctx).negate();
+                default -> throw new VerifyException("Unexpected terminal " + terminal.getText());
+            };
+        } else {
+            throw new SourceException("Unexpected grammar context during parsing of IfFeature expression. "
+                + "Most probably IfFeature grammar has been changed.", ctx);
+        }
+    }
+
+    private static <T> T getChild(final ParseTree parent, final int offset, final Class<T> clazz) {
+        final var child = parent.getChild(offset);
+        verify(clazz.isInstance(child), "Unexpected child %s at offset %s of %s when expecting %s", child, offset,
+                parent, clazz);
+        return clazz.cast(child);
     }
 }
