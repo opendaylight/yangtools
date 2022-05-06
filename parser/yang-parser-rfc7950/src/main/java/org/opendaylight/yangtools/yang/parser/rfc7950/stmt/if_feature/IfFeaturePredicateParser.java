02fc9d259796da0e6dbcbb96ebe33017fccce30c
Samuel Schneider <samuel.schneider@pantheon.tech>
2022-05-06 11:58:29 +0200

    Refactor IfFeaturePredicateVisitor
    Refactor IfFeaturePredicateVisitor to work purely on
    IfFeatureExpressionParser without use of ANTLR's visitors.
    
    JIRA: YANGTOOLS-1396
    Change-Id: I4ff988caa34a50da843fd35b8918e7b502bdf770
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/if_feature/IfFeaturePredicateParser.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.if_feature;
+
+import static com.google.common.base.Verify.verify;
+import static com.google.common.base.Verify.verifyNotNull;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.VerifyException;
+import com.google.common.collect.ImmutableSet;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.model.api.stmt.IfFeatureExpr;
+import org.opendaylight.yangtools.yang.parser.antlr.IfFeatureExpressionLexer;
+import org.opendaylight.yangtools.yang.parser.antlr.IfFeatureExpressionParser;
+import org.opendaylight.yangtools.yang.parser.antlr.IfFeatureExpressionParser.Identifier_ref_argContext;
+import org.opendaylight.yangtools.yang.parser.antlr.IfFeatureExpressionParser.If_feature_exprContext;
+import org.opendaylight.yangtools.yang.parser.antlr.IfFeatureExpressionParser.If_feature_factorContext;
+import org.opendaylight.yangtools.yang.parser.antlr.IfFeatureExpressionParser.If_feature_termContext;
+import org.opendaylight.yangtools.yang.parser.rfc7950.antlr.SourceExceptionParser;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContextUtils;
+import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
+
+@NonNullByDefault
+final class IfFeaturePredicateParser {
+    private final StmtContext<?, ?, ?> stmt;
+
+    private IfFeaturePredicateParser(final StmtContext<?, ?, ?> stmt) {
+        this.stmt = requireNonNull(stmt);
+    }
+
+    static IfFeatureExpr parseIfFeatureExpression(final StmtContext<?, ?, ?> stmt, final String value) {
+        final var lexer = new IfFeatureExpressionLexer(CharStreams.fromString(value));
+        final var parser = new IfFeatureExpressionParser(new CommonTokenStream(lexer));
+        final var ifFeatureExprContext =
+                SourceExceptionParser.parse(lexer, parser, parser::if_feature_expr, stmt.sourceReference());
+        return new IfFeaturePredicateParser(stmt).parseIfFeatureExpr(ifFeatureExprContext);
+    }
+
+    private IfFeatureExpr parseIfFeatureExpr(final If_feature_exprContext expr) {
+        final int count = expr.getChildCount();
+        verify(count % 4 == 1, "Unexpected number of children %s", count);
+
+        final var expressions = ImmutableSet.<IfFeatureExpr>builderWithExpectedSize(count / 4 + 1);
+        for (int i = 0; i < count; i += 4) {
+            expressions.add(parseIfFeatureTerm(getChild(expr, i, If_feature_termContext.class)));
+        }
+        return IfFeatureExpr.or(expressions.build());
+    }
+
+    private IfFeatureExpr parseIfFeatureTerm(final If_feature_termContext term) {
+        final int count = term.getChildCount();
+        verify(count % 4 == 1, "Unexpected number of children %s", count);
+
+        final var expressions = ImmutableSet.<IfFeatureExpr>builderWithExpectedSize(count / 4 + 1);
+        expressions.add(parseIfFeatureFactor(getChild(term, 0, If_feature_factorContext.class)));
+
+        for (int i = 4; i < count; i += 4) {
+            expressions.add(parseIfFeatureTerm(getChild(term, i, If_feature_termContext.class)));
+        }
+        return IfFeatureExpr.and(expressions.build());
+    }
+
+    private IfFeatureExpr parseIfFeatureFactor(final If_feature_factorContext factor) {
+        final var first = factor.getChild(0);
+        if (first instanceof Identifier_ref_argContext refArg) {
+            return IfFeatureExpr.isPresent(StmtContextUtils.parseNodeIdentifier(stmt, refArg.getText()));
+        } else if (first instanceof TerminalNode terminal) {
+            return switch (terminal.getSymbol().getType()) {
+                case IfFeatureExpressionParser.LP ->
+                    parseIfFeatureExpr(verifyNotNull(factor.getChild(If_feature_exprContext.class, 0)));
+                case IfFeatureExpressionParser.NOT ->
+                    parseIfFeatureFactor(getChild(factor, 2, If_feature_factorContext.class)).negate();
+                default -> throw new VerifyException("Unexpected terminal " + terminal.getText());
+            };
+        } else {
+            throw new SourceException("Unexpected grammar context during parsing of IfFeature expression. "
+                + "Most probably IfFeature grammar has been changed.", stmt);
+        }
+    }
+
+    private static <T> T getChild(final ParseTree parent, final int offset, final Class<T> clazz) {
+        final var child = parent.getChild(offset);
+        verify(clazz.isInstance(child), "Unexpected child %s at offset %s of %s when expecting %s", child, offset,
+                parent, clazz);
+        return clazz.cast(child);
+    }
+}
