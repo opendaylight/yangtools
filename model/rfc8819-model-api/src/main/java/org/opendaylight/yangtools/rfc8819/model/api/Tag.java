08dcfcea7bef158ca35557c46f9130583546e84e
ivan.martiniak <ivan.martiniak@pantheon.tech>
2022-05-06 12:30:39 +0200

    Add support for RFC8819 "module-tag" extension
    Improved:
     - Build schema context successfully recognizes
       ModuleTagEffectiveStatementImpl object - raw representation in yang
       file (tags:module-tag "ietf:some-new-tag";)
     - Tag object
     - Java docs
    
    // FIXME:
    - model-api: add module constants
    - model-api: decide on prefix/value split
    - model-api: validation pattern needs to be split up
    - parser-rfc7950: should not be updated
    
    JIRA: YANGTOOLS-1315
    Change-Id: I5ce2ce7ae57febf80c71de68bc37422e5a5431f7
    Signed-off-by: ivan.martiniak <ivan.martiniak@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ model/rfc8819-model-api/src/main/java/org/opendaylight/yangtools/rfc8819/model/api/Tag.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2022 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8819.model.api;
+
+import com.google.common.annotations.Beta;
+import com.google.common.annotations.VisibleForTesting;
+import java.util.regex.Pattern;
+import org.eclipse.jdt.annotation.NonNull;
+
+@Beta
+public final class Tag {
+    private static final Pattern TAG_VALIDATION_PATTERN = Pattern.compile("[\\S ]+");
+
+    private final TagPrefix prefix;
+    private final String value;
+
+    public Tag(final TagPrefix prefix, final String value) {
+        this.prefix = prefix;
+        this.value = value;
+    }
+
+    public static @NonNull Tag create(final TagPrefix prefix, final String value) {
+        return new Tag(prefix, value);
+    }
+
+    public static @NonNull Tag create(final String prefix, final String value) {
+        return new Tag(new TagPrefix(prefix), value);
+    }
+
+    public static @NonNull Tag create(final String value) {
+        return new Tag(null, value);
+    }
+
+    public static @NonNull Tag valueOf(final @NonNull String str) {
+        if (isValidTag(str)) {
+            final int separatorIdx = str.indexOf(':');
+            if (isIetfTag(str)) {
+                return create(TagPrefix.IETF, str.substring(separatorIdx + 1));
+            } else if (isVendorTag(str)) {
+                return create(TagPrefix.VENDOR, str.substring(separatorIdx + 1));
+            } else if (isUserTag(str)) {
+                return create(TagPrefix.USER, str.substring(separatorIdx + 1));
+            } else {
+                if (separatorIdx == -1) {
+                    return create(str);
+                }
+                return create(str.substring(0, separatorIdx + 1), str.substring(separatorIdx + 1));
+            }
+        }
+        throw new IllegalArgumentException("Invalid tag value '" + str + "'.");
+    }
+
+    private static boolean isValidTag(final String str) {
+        return TAG_VALIDATION_PATTERN.matcher(str).matches();
+    }
+
+    private static boolean isIetfTag(final String str) {
+        return str.startsWith(TagPrefix.IETF.getPrefix());
+    }
+
+    private static boolean isVendorTag(final String str) {
+        return str.startsWith(TagPrefix.VENDOR.getPrefix());
+    }
+
+    private static boolean isUserTag(final String str) {
+        return str.startsWith(TagPrefix.USER.getPrefix());
+    }
+
+    @Override
+    public String toString() {
+        return (prefix != null ? prefix : "") + value;
+    }
+
+    @VisibleForTesting
+    public TagPrefix getPrefix() {
+        return prefix;
+    }
+
+    @VisibleForTesting
+    public String getValue() {
+        return value;
+    }
+}
