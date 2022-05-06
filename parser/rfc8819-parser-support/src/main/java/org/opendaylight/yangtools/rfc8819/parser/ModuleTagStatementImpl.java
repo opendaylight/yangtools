58eea61f9a3b793c7ab05be15bdce58d831790d2
ivan.martiniak <ivan.martiniak@pantheon.tech>
2022-05-06 12:30:39 +0200

    Add support for RFC8819 "module-tag" extension
    Schema context successfully recognizes ModuleTagEffectiveStatementImpl
    object, raw representation in the yang file: tags:module-tag "ietf:tag";
    Module-tag extension is fully supported.
    
    JIRA: YANGTOOLS-1315
    Change-Id: I5ce2ce7ae57febf80c71de68bc37422e5a5431f7
    Signed-off-by: Ivan Martiniak <ivan.martiniak@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/rfc8819-parser-support/src/main/java/org/opendaylight/yangtools/rfc8819/parser/ModuleTagStatementImpl.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8819.parser;
+
+import com.google.common.collect.ImmutableList;
+import org.opendaylight.yangtools.rfc8819.model.api.ModuleTagStatement;
+import org.opendaylight.yangtools.rfc8819.model.api.Tag;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.spi.meta.AbstractDeclaredStatement.ArgumentToString.WithSubstatements;
+
+final class ModuleTagStatementImpl extends WithSubstatements<Tag> implements ModuleTagStatement {
+    ModuleTagStatementImpl(final Tag argument, final ImmutableList<? extends DeclaredStatement<?>> substatements) {
+        super(argument, substatements);
+    }
+}
