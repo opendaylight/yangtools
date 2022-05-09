508abdacda12a78c292ef5ced6808d874ac9ee5e
Robert Varga <robert.varga@pantheon.tech>
2022-05-09 10:39:19 +0200

    Fix OpenConfigConstants
    The name of the module is not correct, it should be
    openconfig-extensions.
    
    Change-Id: If38676aa836e283349f60d9c630f60f4dba85116
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/openconfig-model-api/src/main/java/org/opendaylight/yangtools/openconfig/model/api/OpenConfigConstants.java
+++ model/openconfig-model-api/src/main/java/org/opendaylight/yangtools/openconfig/model/api/OpenConfigConstants.java
@@ -23,7 +23,7 @@ import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
  */
 @NonNullByDefault
 public final class OpenConfigConstants {
-    private static final Unqualified MODULE_NAME = Unqualified.of("yang-ext").intern();
+    private static final Unqualified MODULE_NAME = Unqualified.of("openconfig-extensions").intern();
 
     // Package-visible, because openconfig-version applies across all known revisions and needs to bind to all of them
     static final XMLNamespace MODULE_NAMESPACE = XMLNamespace.of("http://openconfig.net/yang/openconfig-ext").intern();
