32aeb53bf28c97c1a47b8e5c4eb2f211fdc92c58
Robert Varga <robert.varga@pantheon.tech>
2022-05-18 00:46:34 +0200

    Eliminate HelperMethods
    The only caller is DataNodeContainer. Since Java 9 allows interfaces
    to define private methods, move the methods there.
    
    Change-Id: If4316f0a41ee8cfeced2bcb85534b5b46faaa43d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/DataNodeContainer.java
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/DataNodeContainer.java
@@ -147,7 +147,7 @@ public interface DataNodeContainer {
     default Optional<DataSchemaNode> findDataTreeChild(final QName name) {
         // First we try to find a direct child and check if it is a data node (as per RFC7950)
         final Optional<DataSchemaNode> optDataChild = findDataChildByName(name);
-        if (HelperMethods.isDataNode(optDataChild)) {
+        if (isDataNode(optDataChild)) {
             return optDataChild;
         }
 
@@ -206,5 +206,15 @@ public interface DataNodeContainer {
             checkArgument(child instanceof DataNodeContainer, "Path %s extends beyond terminal child %s", path, child);
             parent = (DataNodeContainer) child;
         } while (true);
+    }
+
+    private static boolean isDataNode(final Optional<DataSchemaNode> optNode) {
+        return optNode.isPresent() && isDataNode(optNode.orElseThrow());
+    }
+
+    private static boolean isDataNode(final DataSchemaNode node) {
+        return node instanceof ContainerSchemaNode || node instanceof LeafSchemaNode
+                || node instanceof LeafListSchemaNode || node instanceof ListSchemaNode
+                || node instanceof AnydataSchemaNode || node instanceof AnyxmlSchemaNode;
     }
 }
