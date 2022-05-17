a257eeb5333f6719db8afed7258fa0ce3dbdc0ae
Robert Varga <robert.varga@pantheon.tech>
2022-05-17 12:57:32 +0200

    Fix statement prerequisites and materialization
    We are firing onStatementAdded() when copying statements, which means
    that:
    a) a new inference requirement may be created during inferred
       statemenent materialization
    b) the inference requirement may be immediately available in effective
       model
    
    In order to deal with this, ModifierImpl has to always go through a
    bootstrap, so that it prerequisites only trigger once an action is
    registered.
    
    Furthermore InferredStatementContext needs always instantiate the map
    for partial instantiations so as to deal with namespace-driven
    instantiations happening while full instantiation is going on.
    
    JIRA: YANGTOOLS-1434
    Change-Id: I86dae587c1fe5804cd983c194903e1975f257408
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/YT1434Test.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import org.junit.Test;
+
+public class YT1434Test extends AbstractYangTest {
+    @Test
+    public void testUniqueViaAugment() {
+        assertEffectiveModel("/bugs/YT1434/foo.yang");
+    }
+
+    @Test
+    public void testUniqueViaUses() {
+        assertEffectiveModel("/bugs/YT1434/bar.yang");
+    }
+}
