91692a843b49520475498595c042a0873fc37f8e
Robert Varga <robert.varga@pantheon.tech>
2022-05-23 20:19:57 +0200

    Normalize Decimal64 to fraction-digits
    When parsing a Decimal64, we need to scale it to fraction-digits
    to ensure the input matches the expected range.
    
    JIRA: YANGTOOLS-1437
    Change-Id: I535eab4a20d30a742e94c3a6342e113cb569d0bf
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 415dfd816cdbefe5088ee62046f372690ab89aa7)

---
+++ data/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/codec/YT1437Test.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.impl.codec;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+public class YT1437Test {
+    @Test
+    public void testDecimalFractionDigits() {
+        final var module = YangParserTestUtils.parseYangResource("/yt1437.yang").findModule("yt1437").orElseThrow();
+        final var foo = module.findDataChildByName(QName.create(module.getQNameModule(), "foo")).orElseThrow();
+        assertThat(foo, instanceOf(LeafSchemaNode.class));
+
+        final TypeDefinitionAwareCodec<?, ?> codec = TypeDefinitionAwareCodec.from(((LeafSchemaNode) foo).getType());
+        assertThat(codec, instanceOf(DecimalStringCodec.class));
+        final var cast = (DecimalStringCodec) codec;
+
+        final var one = cast.deserialize("20.0");
+        assertEquals(2, one.scale());
+        assertEquals("20.0", cast.serialize(one));
+
+        final var two = cast.deserialize("20.00");
+        assertEquals(2, two.scale());
+        assertEquals("20.0", cast.serialize(two));
+
+        final var three = cast.deserialize("20.000");
+        assertEquals(2, three.scale());
+        assertEquals("20.0", cast.serialize(three));
+    }
+}
