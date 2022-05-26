bd91edd7f806d2331c730243a1d876fa050d84b0
Robert Varga <robert.varga@pantheon.tech>
2022-05-26 23:06:16 +0200

    Modernize yang-data-codec-binfmt
    Use JDK17 features to make the code a bit more less verbose and more
    resilient.
    
    Change-Id: I8219ca1a6507940b6d2e24800ff29c1d350469e2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- codec/yang-data-codec-binfmt/src/main/java/org/opendaylight/yangtools/yang/data/codec/binfmt/VersionedNormalizedNodeDataInput.java
+++ codec/yang-data-codec-binfmt/src/main/java/org/opendaylight/yangtools/yang/data/codec/binfmt/VersionedNormalizedNodeDataInput.java
@@ -32,23 +32,13 @@ final class VersionedNormalizedNodeDataInput extends ForwardingNormalizedNodeDat
         }
 
         final short version = input.readShort();
-        final NormalizedNodeDataInput ret;
-        switch (version) {
-            case TokenTypes.LITHIUM_VERSION:
-                ret = new LithiumNormalizedNodeInputStreamReader(input);
-                break;
-            case TokenTypes.NEON_SR2_VERSION:
-                ret = new NeonSR2NormalizedNodeInputStreamReader(input);
-                break;
-            case TokenTypes.SODIUM_SR1_VERSION:
-                ret = new SodiumSR1DataInput(input);
-                break;
-            case TokenTypes.MAGNESIUM_VERSION:
-                ret = new MagnesiumDataInput(input);
-                break;
-            default:
-                throw defunct("Unhandled stream version %s", version);
-        }
+        final NormalizedNodeDataInput ret = switch (version) {
+            case TokenTypes.LITHIUM_VERSION -> new LithiumNormalizedNodeInputStreamReader(input);
+            case TokenTypes.NEON_SR2_VERSION -> new NeonSR2NormalizedNodeInputStreamReader(input);
+            case TokenTypes.SODIUM_SR1_VERSION -> new SodiumSR1DataInput(input);
+            case TokenTypes.MAGNESIUM_VERSION -> new MagnesiumDataInput(input);
+            default -> throw defunct("Unhandled stream version %s", version);
+        };
 
         setDelegate(ret);
         return ret;
