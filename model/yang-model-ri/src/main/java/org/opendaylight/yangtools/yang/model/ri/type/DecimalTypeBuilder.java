7b7d863fb008f6886a54285917217ab8db88dac1
OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
2022-07-07 13:08:47 +0200

    Normalize Decimal64 scale in range constraints
    Modified logic of DecimalTypeBuilder to ensure proper scale
    of fraction-digits.
    
    JIRA: YANGTOOLS-1441
    Change-Id: I01ac7c7d940119f76867b15a3aaff50b3cd983e0
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>

--- model/yang-model-ri/src/main/java/org/opendaylight/yangtools/yang/model/ri/type/DecimalTypeBuilder.java
+++ model/yang-model-ri/src/main/java/org/opendaylight/yangtools/yang/model/ri/type/DecimalTypeBuilder.java
@@ -9,9 +9,14 @@ package org.opendaylight.yangtools.yang.model.ri.type;
 
 import static com.google.common.base.Preconditions.checkState;
 
+import com.google.common.collect.ImmutableRangeSet;
+import com.google.common.collect.Range;
+import java.util.Set;
+import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.yang.common.Decimal64;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.model.api.type.DecimalTypeDefinition;
+import org.opendaylight.yangtools.yang.model.api.type.RangeConstraint;
 
 public final class DecimalTypeBuilder extends RangeRestrictedTypeBuilder<DecimalTypeDefinition, Decimal64> {
     private Integer fractionDigits;
@@ -29,7 +34,50 @@ public final class DecimalTypeBuilder extends RangeRestrictedTypeBuilder<Decimal
     @Override
     DecimalTypeDefinition buildType() {
         checkState(fractionDigits != null, "Fraction digits not defined");
-        return new BaseDecimalType(getQName(), getUnknownSchemaNodes(), fractionDigits,
-            calculateRangeConstraint(BaseDecimalType.constraintsForDigits(fractionDigits)));
+        BaseDecimalType resultRange = new BaseDecimalType(getQName(), getUnknownSchemaNodes(), fractionDigits,
+                calculateRangeConstraint(BaseDecimalType.constraintsForDigits(fractionDigits)));
+        BaseDecimalType resolvedRange = ensureResolvedScale(resultRange, fractionDigits);
+
+        return resolvedRange;
+    }
+
+    private BaseDecimalType ensureResolvedScale(BaseDecimalType decimalType, @NonNull int scale) {
+        if (!decimalType.getRangeConstraint().isPresent()) {
+            return decimalType;
+        }
+
+        RangeConstraint<Decimal64> constraint = decimalType.getRangeConstraint().get();
+
+        // Check if we need to resolve anything at all
+        for (Range<Decimal64> range : constraint.getAllowedRanges().asRanges()) {
+            if (range.lowerEndpoint().scale() != scale || range.upperEndpoint().scale() != scale) {
+                return resolveScale(decimalType, scale);
+            }
+        }
+
+        // Everything is good - return same decimal type
+        return decimalType;
+    }
+
+    private BaseDecimalType resolveScale(BaseDecimalType unresolved, int scale) {
+        Set<Range<@NonNull Decimal64>> unresolvedRanges = unresolved.getRangeConstraint().get().getAllowedRanges()
+                .asRanges();
+        final ImmutableRangeSet.Builder<Decimal64> builder = ImmutableRangeSet.builder();
+        for (Range<Decimal64> range : unresolvedRanges) {
+            Decimal64 lower = range.lowerEndpoint();
+            Decimal64 upper = range.upperEndpoint();
+            if (lower.scale() != scale) {
+                lower = lower.scaleTo(scale);
+            }
+            if (upper.scale() != scale) {
+                upper = upper.scaleTo(scale);
+            }
+            builder.add(Range.closed(lower, upper));
+        }
+
+        ImmutableRangeSet<Decimal64> scaledRangeSet = builder.build();
+        ResolvedRangeConstraint<Decimal64> resolved =
+                new ResolvedRangeConstraint<>(unresolved.getRangeConstraint().get(), scaledRangeSet);
+        return new BaseDecimalType(getQName(), getUnknownSchemaNodes(), scale, resolved);
     }
 }
