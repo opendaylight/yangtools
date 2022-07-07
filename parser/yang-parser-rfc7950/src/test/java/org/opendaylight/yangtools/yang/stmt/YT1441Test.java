831cdc29ff60aa14799df2aad775e13509ec9bf5
OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
2022-07-07 13:08:47 +0200

    Normalize Decimal64 scale in range constraints
    Modified logic of DecimalTypeBuilder to ensure proper scale
    of fraction-digits. Updated test in order to ensure that range
    constraints are correct.
    
    // FIXME: we retain unrestricted meta
    
    JIRA: YANGTOOLS-1441
    Change-Id: I01ac7c7d940119f76867b15a3aaff50b3cd983e0
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/YT1441Test.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.hamcrest.CoreMatchers.startsWith;
+
+import org.junit.Test;
+
+public class YT1441Test extends AbstractYangTest {
+    @Test
+    public void testInvalidRange() {
+        assertSourceException(startsWith(
+            "Range constraint does not match fraction-digits: Decreasing scale of 2.345 to 2 requires rounding [at "),
+            "/bugs/YT1441/foo.yang");
+    }
+}
