ea6764214d705dd1d9376e5e0c05eaf987761a36
Robert Varga <robert.varga@pantheon.tech>
2022-08-07 21:33:14 +0200

    Convert yang-data-codec-gson to JPMS
    This is an automatic module, jump the ship and become a full module.
    
    JIRA: YANGTOOLS-1449
    Change-Id: Ib1847be1e684e3a694f110d8a3d6b0166d52259c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ codec/yang-data-codec-gson/src/main/java/module-info.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+module org.opendaylight.yangtools.yang.data.codec.gson {
+    exports org.opendaylight.yangtools.yang.data.codec.gson;
+
+    requires transitive com.google.gson;
+    requires transitive org.opendaylight.yangtools.yang.data.api;
+    requires transitive org.opendaylight.yangtools.rfc7952.data.api;
+    requires transitive org.opendaylight.yangtools.rfc8528.data.api;
+
+    requires com.google.common;
+    requires java.xml;
+    requires org.opendaylight.yangtools.util;
+    requires org.opendaylight.yangtools.yang.common;
+    requires org.opendaylight.yangtools.yang.data.impl;
+    requires org.opendaylight.yangtools.yang.data.util;
+    requires org.opendaylight.yangtools.yang.model.api;
+    requires org.opendaylight.yangtools.yang.model.util;
+    requires org.slf4j;
+
+    // Annotations
+    requires static org.checkerframework.checker.qual;
+    requires static com.github.spotbugs.annotations;
+    requires static org.eclipse.jdt.annotation;
+}
