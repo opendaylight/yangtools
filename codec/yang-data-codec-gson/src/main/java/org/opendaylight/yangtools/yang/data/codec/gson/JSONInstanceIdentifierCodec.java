21b67fd3b3d5d6b3738af87323b9f5954ad2ef1b
Robert Varga <robert.varga@pantheon.tech>
2022-08-07 20:33:31 +0200

    Integrate JSONCodecFactory specializations
    There is no point in keeping simple subclasses separate, move them
    to be nest-mates and tighen them up. This also integrates
    JSONInstanceIdentifierCodec, as it provides some brevity.
    
    Change-Id: Iaea1cb529603976324516516a7f172b33f68e127
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- codec/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/JSONInstanceIdentifierCodec.java
+++ codec/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/JSONInstanceIdentifierCodec.java
@@ -13,6 +13,8 @@ import static java.util.Objects.requireNonNull;
 import com.google.gson.stream.JsonWriter;
 import java.io.IOException;
 import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.common.XMLNamespace;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.util.AbstractModuleStringInstanceIdentifierCodec;
@@ -23,8 +25,32 @@ import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang.model.util.LeafrefResolver;
 
-abstract class JSONInstanceIdentifierCodec extends AbstractModuleStringInstanceIdentifierCodec
+abstract sealed class JSONInstanceIdentifierCodec extends AbstractModuleStringInstanceIdentifierCodec
         implements JSONCodec<YangInstanceIdentifier> {
+    static final class Lhotka02 extends JSONInstanceIdentifierCodec {
+        Lhotka02(final EffectiveModelContext context, final JSONCodecFactory jsonCodecFactory) {
+            super(context, jsonCodecFactory);
+        }
+    }
+
+    static final class RFC7951 extends JSONInstanceIdentifierCodec {
+        RFC7951(final EffectiveModelContext context, final JSONCodecFactory jsonCodecFactory) {
+            super(context, jsonCodecFactory);
+        }
+
+        @Override
+        protected StringBuilder appendQName(final StringBuilder sb, final QName qname, final QNameModule lastModule) {
+            return qname.getModule().equals(lastModule) ? sb.append(qname.getLocalName())
+                : super.appendQName(sb, qname, lastModule);
+        }
+
+        @Override
+        protected QName createQName(final QNameModule lastModule, final String localName) {
+            checkArgument(lastModule != null, "Unprefixed leading name %s", localName);
+            return QName.create(lastModule, localName);
+        }
+    }
+
     private final @NonNull DataSchemaContextTree dataContextTree;
     private final JSONCodecFactory codecFactory;
     private final EffectiveModelContext context;
@@ -57,8 +83,7 @@ abstract class JSONInstanceIdentifierCodec extends AbstractModuleStringInstanceI
             final String value) {
         requireNonNull(schemaNode, "schemaNode cannot be null");
         checkArgument(schemaNode instanceof LeafSchemaNode, "schemaNode must be of type LeafSchemaNode");
-        final JSONCodec<?> objectJSONCodec = codecFactory.codecFor((LeafSchemaNode) schemaNode, resolver);
-        return objectJSONCodec.parseValue(null, value);
+        return codecFactory.codecFor((LeafSchemaNode) schemaNode, resolver).parseValue(null, value);
     }
 
     @Override
