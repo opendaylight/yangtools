b5cf79d8046c8251ca919015697defe2d1b1387e
Robert Varga <robert.varga@pantheon.tech>
2022-08-11 01:23:51 +0200

    Add YangFeatureProvider
    YangFeatureProvider is a counterpart to YangModuleInfoProvider,
    providing the runtime with the ability to introspect what features
    should be supported in an EffectiveModelContext.
    
    JIRA: MDSAL-767
    Change-Id: I6d08772c96eead79b4c88ae9ac27c57901dc70a8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/mdsal-binding-test-model/src/main/java/org/opendaylight/mdsal/binding/test/model/util/Mdsal767Support.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mdsal.binding.test.model.util;
+
+import java.util.Set;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.kohsuke.MetaInfServices;
+import org.opendaylight.yang.gen.v1.mdsal767.norev.Mdsal767Data;
+import org.opendaylight.yang.gen.v1.mdsal767.norev.One$F;
+import org.opendaylight.yangtools.yang.binding.YangFeature;
+import org.opendaylight.yangtools.yang.binding.YangFeatureProvider;
+
+@MetaInfServices
+@NonNullByDefault
+public final class Mdsal767Support implements YangFeatureProvider<Mdsal767Data> {
+    @Override
+    public Class<Mdsal767Data> boundModule() {
+        return Mdsal767Data.class;
+    }
+
+    @Override
+    public Set<? extends YangFeature<?, Mdsal767Data>> supportedFeatures() {
+        return Set.of(One$F.VALUE);
+    }
+}
