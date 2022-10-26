54601eb76a9ee7d14a39134476f4c839f56b1e1a
Robert Varga <robert.varga@pantheon.tech>
2022-10-26 17:36:47 +0200

    Seal OperationDeclaredStatement
    This is a common super-interface for ActionStatement and RpcStatement,
    make sure we seal it to prevent misuse elsewhere.
    
    Change-Id: I52b29e9f5b845ab19d045847183bf374f01f1fa8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/RpcStatement.java
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/RpcStatement.java
@@ -13,7 +13,7 @@ import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 /**
  * Declared representation of a {@code rpc} statement.
  */
-public interface RpcStatement extends OperationDeclaredStatement {
+public non-sealed interface RpcStatement extends OperationDeclaredStatement {
     @Override
     default StatementDefinition statementDefinition() {
         return YangStmtMapping.RPC;
