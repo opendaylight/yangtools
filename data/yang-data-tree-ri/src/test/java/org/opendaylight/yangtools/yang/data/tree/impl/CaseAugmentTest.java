6bba08625b533ac36f297adbc2032d4dcd67ded9
Robert Varga <robert.varga@pantheon.tech>
2022-10-27 23:25:28 +0200

    Remove Augmentation{Identifier,Node}
    These two are an ugly leak from binding world into yang.data.api. Purge
    them with fire.
    
    // FIXME: deal with codec-binfmt migration!
    
    JIRA: YANGTOOLS-568
    Change-Id: Ib3b9182253ec7d42598b6a174a67c48191d9ed5b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- data/yang-data-tree-ri/src/test/java/org/opendaylight/yangtools/yang/data/tree/impl/CaseAugmentTest.java
+++ data/yang-data-tree-ri/src/test/java/org/opendaylight/yangtools/yang/data/tree/impl/CaseAugmentTest.java
@@ -7,17 +7,13 @@
  */
 package org.opendaylight.yangtools.yang.data.tree.impl;
 
-import static org.junit.Assert.assertTrue;
 import static org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes.leafNode;
 
-import com.google.common.collect.ImmutableSet;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.AugmentationIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
-import org.opendaylight.yangtools.yang.data.api.schema.AugmentationNode;
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
 import org.opendaylight.yangtools.yang.data.tree.api.DataTree;
@@ -35,8 +31,6 @@ public class CaseAugmentTest {
     private static final QName C1L3_QNAME = QName.create(TestModel.TEST_QNAME, "case1-leaf3");
     private static final QName C2L1_QNAME = QName.create(TestModel.TEST_QNAME, "case2-leaf1");
     private static final NodeIdentifier CHOICE_ID = new NodeIdentifier(CHOICE1_QNAME);
-    private static final AugmentationIdentifier AUGMENT_ID = new AugmentationIdentifier(
-        ImmutableSet.of(C1L2_QNAME, C1L3_QNAME));
 
     private static EffectiveModelContext SCHEMA_CONTEXT;
 
@@ -60,17 +54,13 @@ public class CaseAugmentTest {
     public void testWriteAugment() throws DataValidationFailedException {
         final DataTree inMemoryDataTree = initDataTree();
 
-        AugmentationNode augmentationNode = Builders.augmentationBuilder()
-                .withNodeIdentifier(AUGMENT_ID)
-                .withChild(leafNode(C1L2_QNAME, "leaf-value"))
-                .build();
-
         final ContainerNode container = Builders.containerBuilder()
-                .withNodeIdentifier(new NodeIdentifier(TestModel.TEST_QNAME))
-                .withChild(
-                        Builders.choiceBuilder().withNodeIdentifier(CHOICE_ID)
-                                .withChild(augmentationNode)
-                                .build()).build();
+            .withNodeIdentifier(new NodeIdentifier(TestModel.TEST_QNAME))
+            .withChild(Builders.choiceBuilder()
+                .withNodeIdentifier(CHOICE_ID)
+                .withChild(leafNode(C1L2_QNAME, "leaf-value"))
+                .build())
+            .build();
         final DataTreeModification modificationTree = inMemoryDataTree.takeSnapshot().newModification();
         modificationTree.write(TestModel.TEST_PATH, container);
         modificationTree.ready();
@@ -84,19 +74,14 @@ public class CaseAugmentTest {
     public void testWriteCase1All() throws DataValidationFailedException {
         final DataTree inMemoryDataTree = initDataTree();
 
-        AugmentationNode augmentationNode = Builders.augmentationBuilder()
-                .withNodeIdentifier(AUGMENT_ID)
-                .withChild(leafNode(C1L2_QNAME, "leaf-value"))
-                .withChild(leafNode(C1L3_QNAME, "leaf-value"))
-                .build();
-
         final ContainerNode container = Builders
                 .containerBuilder()
                 .withNodeIdentifier(new NodeIdentifier(TestModel.TEST_QNAME))
                 .withChild(
                         Builders.choiceBuilder().withNodeIdentifier(CHOICE_ID)
                                 .withChild(leafNode(QName.create(TestModel.TEST_QNAME, "case1-leaf1"), "leaf-value"))
-                                .withChild(augmentationNode)
+                                .withChild(leafNode(C1L2_QNAME, "leaf-value"))
+                                .withChild(leafNode(C1L3_QNAME, "leaf-value"))
                                 .build()).build();
         final DataTreeModification modificationTree = inMemoryDataTree.takeSnapshot().newModification();
         modificationTree.write(TestModel.TEST_PATH, container);
@@ -107,34 +92,26 @@ public class CaseAugmentTest {
         inMemoryDataTree.commit(prepare);
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void testWriteConflict() throws DataValidationFailedException {
         final DataTree inMemoryDataTree = initDataTree();
-
-        AugmentationNode augmentationNode = Builders.augmentationBuilder()
-                .withNodeIdentifier(AUGMENT_ID)
-                .withChild(leafNode(C1L2_QNAME, "leaf-value"))
-                .build();
 
         final ContainerNode container = Builders.containerBuilder()
                 .withNodeIdentifier(new NodeIdentifier(TestModel.TEST_QNAME))
                 .withChild(
                         Builders.choiceBuilder().withNodeIdentifier(CHOICE_ID)
-                                .withChild(augmentationNode)
+                                .withChild(leafNode(C1L2_QNAME, "leaf-value"))
                                 .withChild(leafNode(QName.create(TestModel.TEST_QNAME, "case2-leaf1"), "leaf-value"))
                                 .build()).build();
 
-        try {
-            final DataTreeModification modificationTree = inMemoryDataTree.takeSnapshot().newModification();
-            modificationTree.write(TestModel.TEST_PATH, container);
-            modificationTree.ready();
+        final DataTreeModification modificationTree = inMemoryDataTree.takeSnapshot().newModification();
+        modificationTree.write(TestModel.TEST_PATH, container);
+        modificationTree.ready();
 
-            inMemoryDataTree.validate(modificationTree);
-            final DataTreeCandidate prepare = inMemoryDataTree.prepare(modificationTree);
-            inMemoryDataTree.commit(prepare);
-        } catch (IllegalArgumentException e) {
-            assertTrue(e.getMessage().contains("implies non-presence of child"));
-            throw e;
-        }
+        inMemoryDataTree.validate(modificationTree);
+        final DataTreeCandidate prepare = inMemoryDataTree.prepare(modificationTree);
+
+        inMemoryDataTree.commit(prepare);
+        //            assertTrue(e.getMessage().contains("implies non-presence of child"));
     }
 }
