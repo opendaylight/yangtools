d457632231eac2a70b4ba8689c7837f130717eac
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2022-11-08 11:39:35 +0200

    Introduce RebuildContext to support incremental build
    New artifacts aggregates logic to make a decision either full
    rebuild is necessary or execution can be skipped due to inputs
    and configuration remain the same, and all the outputs untouched
    since previous run.
    
    The artifact also takes full responsibility for interaction non-default
    with BuildContext instance which is part of integration with Eclipse IDE
    (buildContext update notification is not in scope for this change)
    
    JIRA: YANGTOOLS-1165
    JIRA: YANGTOOLS-1166
    Change-Id: I6f8cbf03a52542e8c20fd1383d459056272f951a
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- plugin/yang-maven-plugin/src/main/java/org/opendaylight/yangtools/yang2sources/plugin/FileGeneratorArg.java
+++ plugin/yang-maven-plugin/src/main/java/org/opendaylight/yangtools/yang2sources/plugin/FileGeneratorArg.java
@@ -11,14 +11,19 @@ import static com.google.common.base.Verify.verifyNotNull;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.base.MoreObjects;
+import com.google.common.collect.Maps;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.concepts.Identifiable;
+import org.opendaylight.yangtools.concepts.WritableObject;
 
-public final class FileGeneratorArg implements Identifiable<String> {
+public final class FileGeneratorArg implements Identifiable<String>, WritableObject {
     @Parameter
     private final Map<String, String> configuration = new HashMap<>();
 
@@ -26,7 +31,7 @@ public final class FileGeneratorArg implements Identifiable<String> {
     private String identifier;
 
     public FileGeneratorArg() {
-
+        // Default constructor
     }
 
     public FileGeneratorArg(final String identifier) {
@@ -50,5 +55,25 @@ public final class FileGeneratorArg implements Identifiable<String> {
     @Override
     public String toString() {
         return MoreObjects.toStringHelper(this).add("id", identifier).add("configuration", configuration).toString();
+    }
+
+    public static @NonNull FileGeneratorArg readFrom(final DataInput in) throws IOException {
+        final var identifier = in.readUTF();
+        final var size = in.readInt();
+        final var configuration = Maps.<String, String>newHashMapWithExpectedSize(size);
+        for (int i = 0; i < size; ++i) {
+            configuration.put(in.readUTF(), in.readUTF());
+        }
+        return new FileGeneratorArg(identifier, configuration);
+    }
+
+    @Override
+    public void writeTo(final DataOutput out) throws IOException {
+        out.writeUTF(identifier);
+        out.writeInt(configuration.size());
+        for (String key : configuration.keySet().stream().sorted().toList()) {
+            out.writeUTF(key);
+            out.writeUTF(configuration.getOrDefault(key, ""));
+        }
     }
 }
--- plugin/yang-maven-plugin/src/main/java/org/opendaylight/yangtools/yang2sources/plugin/YangToSourcesState.java
+++ plugin/yang-maven-plugin/src/main/java/org/opendaylight/yangtools/yang2sources/plugin/YangToSourcesState.java
@@ -9,29 +9,61 @@ package org.opendaylight.yangtools.yang2sources.plugin;
 
 import static java.util.Objects.requireNonNull;
 
+import com.google.common.collect.ImmutableMap;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.util.Collection;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.concepts.WritableObject;
 
 /**
  * State of the result of a {@link YangToSourcesMojo} execution run.
  */
-// FIXME: expand to capture:
-//        - input YANG files
-//        - code generators and their config
-record YangToSourcesState(@NonNull FileStateSet outputFiles) implements WritableObject {
+record YangToSourcesState(
+        @NonNull FileStateSet inputFiles,
+        @NonNull ImmutableMap<String, FileGeneratorArg> configurations,
+        @NonNull FileStateSet outputFiles) implements WritableObject {
     YangToSourcesState {
+        requireNonNull(inputFiles);
+        requireNonNull(configurations);
         requireNonNull(outputFiles);
     }
 
     static @NonNull YangToSourcesState readFrom(final DataInput in) throws IOException {
-        return new YangToSourcesState(FileStateSet.readFrom(in));
+        return new YangToSourcesState(FileStateSet.readFrom(in), readConfigurations(in), FileStateSet.readFrom(in));
+    }
+
+    static YangToSourcesState empty() {
+        return new YangToSourcesState(FileStateSet.empty(), ImmutableMap.of(), FileStateSet.empty());
     }
 
     @Override
     public void writeTo(final DataOutput out) throws IOException {
+        inputFiles.writeTo(out);
+        writeConfigurations(out, configurations.values());
         outputFiles.writeTo(out);
+    }
+
+    private static void writeConfigurations(final DataOutput out, final Collection<FileGeneratorArg> configurations)
+            throws IOException {
+        out.writeInt(configurations.size());
+        for (var arg : configurations) {
+            arg.writeTo(out);
+        }
+    }
+
+    private static @NonNull ImmutableMap<String, FileGeneratorArg> readConfigurations(final DataInput in)
+            throws IOException {
+        final int size = in.readInt();
+        if (size == 0) {
+            return ImmutableMap.of();
+        }
+        final var configurations = ImmutableMap.<String, FileGeneratorArg>builderWithExpectedSize(size);
+        for (int i = 0; i < size; ++i) {
+            final var arg = FileGeneratorArg.readFrom(in);
+            configurations.put(arg.getIdentifier(), arg);
+        }
+        return configurations.build();
     }
 }
