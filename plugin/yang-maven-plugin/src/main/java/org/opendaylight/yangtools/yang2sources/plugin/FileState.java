bcda03096a20136dd71f81a8c37fe2b0ef00b7ed
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2022-11-08 11:39:35 +0200

    Track META-INF/ files in state
    Yang resources copied into META-INF folder now taken into
    account as plugin output the same way as generated files.
    
    JIRA: YANGTOOLS-1166
    Change-Id: I815c1fc69c1220ce86efd319725470f11533c72b
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

--- plugin/yang-maven-plugin/src/main/java/org/opendaylight/yangtools/yang2sources/plugin/FileState.java
+++ plugin/yang-maven-plugin/src/main/java/org/opendaylight/yangtools/yang2sources/plugin/FileState.java
@@ -9,6 +9,10 @@ package org.opendaylight.yangtools.yang2sources.plugin;
 
 import static java.util.Objects.requireNonNull;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.attribute.BasicFileAttributes;
 import org.eclipse.jdt.annotation.NonNull;
 
@@ -16,7 +20,27 @@ import org.eclipse.jdt.annotation.NonNull;
  * Hash of a single file state. {@link #size()} corresponds to {@link BasicFileAttributes#size()}.
  */
 record FileState(@NonNull String path, long size, int crc32) {
+    /**
+     * A simple interface describing the intended content of a file.
+     */
+    @FunctionalInterface
+    interface FileContent {
+        /**
+         * Write the content of this file to specified {@link OutputStream}
+         * @param out OutputStream to write to, guaranteed to be non-null
+         * @throws IOException if any error occurs
+         */
+        void writeTo(@NonNull OutputStream out) throws IOException;
+    }
+
     FileState {
         requireNonNull(path);
+    }
+
+    static @NonNull FileState ofWrittenFile(final File file, final FileContent content) throws IOException {
+        try (var out = new CapturingOutputStream(Files.newOutputStream(file.toPath()))) {
+            content.writeTo(out);
+            return new FileState(file.getPath(), out.size(), out.crc32c());
+        }
     }
 }
