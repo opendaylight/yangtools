278f1038628c02fbd260994e3bdff69db0ab9723
OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
2022-11-21 14:11:37 +0100

    Compare results of XML and JSON parsers for augmented nodes
    Added two tests for XML and JSON parsers that reads same augmented node.
    Logic for both tests same as netconf uses for patch requests.
    
    JIRA: NETCONF-911
    Change-Id: I504e23d545e37babf7cd57fe44b0f50e67f0315e
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>

---
+++ codec/yang-data-codec-gson/src/test/java/org/opendaylight/yangtools/yang/data/codec/gson/NC747Test.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yangtools.yang.data.codec.gson;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.opendaylight.yangtools.yang.data.codec.gson.TestUtils.loadTextFile;
+import static org.opendaylight.yangtools.yang.data.impl.schema.Builders.containerBuilder;
+import static org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes.leafNode;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URISyntaxException;
+import com.google.gson.stream.JsonReader;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.impl.schema.NormalizedNodeResult;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+
+public class NC747Test {
+    static EffectiveModelContext SCHEMA_CONTEXT;
+    private static JSONCodecFactory CODEC_FACTORY;
+
+    @BeforeClass
+    public static void createFactory() {
+        SCHEMA_CONTEXT = YangParserTestUtils.parseYangResourceDirectory("/netconf747/yang");
+        CODEC_FACTORY = JSONCodecFactorySupplier.RFC7951.getShared(SCHEMA_CONTEXT);
+    }
+
+    @AfterClass
+    public static void destroyFactory() {
+        CODEC_FACTORY = null;
+    }
+
+    @Test
+    public void testNetconf747() throws IOException, URISyntaxException {
+        final String inputJson = loadTextFile("/netconf747/data/data.json");
+
+        var containerRoot = QName.create("test-ns", "container-root");
+        var containerLvl1 = QName.create("test-ns", "container-lvl1");
+        var containerAug = QName.create("test-ns-aug", "container-aug");
+
+        final NormalizedNode expectedData = containerBuilder().withNodeIdentifier(new YangInstanceIdentifier
+                .NodeIdentifier(containerAug))
+                .withChild(leafNode(QName.create("test-ns-aug", "leaf-aug"), "data"))
+                .build();
+
+        var inf = SchemaInferenceStack.Inference.ofDataTreePath(SCHEMA_CONTEXT, containerRoot,
+                containerLvl1);
+        final NormalizedNode data = fromJSON(inputJson, inf);
+
+        // This code will solve issue with wrong data, but we should already have it from JsonParserStream without this
+        // lines
+//        if (data instanceof AugmentationNode) {
+//            data = ((DataContainerNode) data).body().iterator().next();
+//        }
+
+        assertNotNull(data);
+        assertEquals(expectedData, data);
+    }
+
+    JSONCodecFactory codecFactory() {
+        return CODEC_FACTORY;
+    }
+
+    final NormalizedNode fromJSON(final String input, final SchemaInferenceStack.Inference parentNode) {
+        final NormalizedNodeResult result = new NormalizedNodeResult();
+        final NormalizedNodeStreamWriter streamWriter = ImmutableNormalizedNodeStreamWriter.from(result);
+        final JsonParserStream jsonParser = JsonParserStream.create(streamWriter, codecFactory(), parentNode);
+        jsonParser.parse(new JsonReader(new StringReader(input)));
+        return result.getResult();
+    }
+}
