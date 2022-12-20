752bd42060e786d73c537d85ab162cf95cbc8adf
matus.matok <matus.matok@pantheon.tech>
2022-12-20 14:30:31 +0100

    WIP: Add support for formatting of EffectiveModelContext to organization tree
    FIXME: separate augments, rpcs, notifications groupings and yang-datas into separate blocks
    
    JIRA: YANGTOOLS-1464
    Signed-off-by: matus.matok <matus.matok@pantheon.tech>
    Change-Id: Ib0c1fa95102cd5409a0daefe088a6b45d92e9817

---
+++ model/yang-model-export/src/main/java/org/opendaylight/yangtools/yang/model/export/YangOrganizationTreeIterator.java
@@ -0,0 +1,228 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.export;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.collect.AbstractIterator;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.Stack;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
+import org.opendaylight.yangtools.yang.model.api.stmt.CaseStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ChoiceStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ConfigStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ConfigStatementAwareDeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ContainerStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.DataDefinitionStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.DocumentedDeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.IfFeatureAwareDeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.InputStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.KeyStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.LeafListStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ListStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.MandatoryStatementAwareDeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ModuleStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.NotificationStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.OperationDeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.OutputStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.PresenceStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.StatusStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.SubmoduleStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.TypeAwareDeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.UsesStatement;
+
+public class YangOrganizationTreeIterator extends AbstractIterator<@NonNull String> {
+
+    private final Deque<Iterator<? extends DeclaredStatement<?>>> stack = new ArrayDeque<>(16);
+    private final Stack<String> indent = new Stack<>();
+    private final int depth;
+
+    YangOrganizationTreeIterator(final DeclaredStatement<?> stmt, final StatementPrefixResolver resolver,
+            final Set<StatementDefinition> ignoredStatements, final int depth) {
+        pushStatement(List.of(requireNonNull(stmt)).iterator());
+        this.depth = depth;
+    }
+
+    @Override
+    protected @NonNull String computeNext() {
+
+        while (!stack.isEmpty()) {
+            final Iterator<? extends DeclaredStatement<?>> it = pollStatement();
+
+            if (stack.size() > depth) {
+                if (it.hasNext()) {
+                    return String.join("", indent) + "...\n";
+                }
+                else {
+                    return "";
+                }
+            }
+
+            while (it.hasNext()) {
+                final DeclaredStatement<?> currentStatement = it.next();
+
+                if (currentStatement instanceof ModuleStatement) {
+                    pushStatement(it);
+                    pushStatement(getTrimmedSubstatements(currentStatement));
+                    return "module: " + currentStatement.rawArgument() + "\n";
+                } else if (currentStatement instanceof SubmoduleStatement) {
+                    pushStatement(it);
+                    pushStatement(getTrimmedSubstatements(currentStatement));
+                    return "submodule: " + currentStatement.rawArgument() + "\n";
+                } else if (currentStatement instanceof DocumentedDeclaredStatement.WithStatus<?> documentedDeclaredStatement) {
+                    final String result = String.join("", indent)
+                            + getStatementStatus(documentedDeclaredStatement)
+                            + "--"
+                            + getStatementFlags(documentedDeclaredStatement)
+                            + getName(documentedDeclaredStatement)
+                            + getOpts(documentedDeclaredStatement)
+                            + " "
+                            + getType(documentedDeclaredStatement)
+                            + " "
+                            + getIfFeatures(documentedDeclaredStatement)
+                            + "\n";
+                    pushStatement(it);
+                    pushStatement(getTrimmedSubstatements(documentedDeclaredStatement));
+                    return result;
+                }
+            }
+        }
+        endOfData();
+        return "";
+    }
+
+    private Iterator<? extends DeclaredStatement<?>> getTrimmedSubstatements(DeclaredStatement<?> stmt) {
+        return stmt.declaredSubstatements().stream()
+                .filter(statement -> statement instanceof DocumentedDeclaredStatement.WithStatus<?>)
+                .iterator();
+    }
+
+    private String getStatementStatus(DocumentedDeclaredStatement.WithStatus<?> stmt) {
+        final Optional<StatusStatement> statusStatement = stmt.getStatus();
+
+        if (statusStatement.isEmpty()) {
+            return "+";
+        }
+        else if (statusStatement.orElseThrow().rawArgument().equals("deprecated")) {
+            return "x";
+        }
+        else if (statusStatement.orElseThrow().rawArgument().equals("obsolete")) {
+            return "o";
+        }
+        return "+";
+    }
+
+    private String getStatementFlags(DocumentedDeclaredStatement.WithStatus<?> stmt) {
+        if (stmt instanceof DataDefinitionStatement) {
+            if (stmt instanceof ConfigStatementAwareDeclaredStatement<?> configAware) {
+                final Optional<ConfigStatement> optConfig = configAware.getConfig();
+
+                if (optConfig.isPresent()) {
+                    if (!optConfig.orElseThrow().argument()) {
+                        return "ro";
+                    }
+                    return "rw";
+                }
+                return "rw";
+
+            } else if (stmt instanceof UsesStatement) {
+                return "-u";
+            }
+            return "";
+        } else if (stmt instanceof OperationDeclaredStatement) {
+            return "-x";
+        } else if (stmt instanceof InputStatement) {
+            return "-w";
+        } else if (stmt instanceof OutputStatement) {
+            return "ro";
+        } else if (stmt instanceof NotificationStatement) {
+            return "-n";
+        }
+        return "UU";
+    }
+
+    private String getName(DocumentedDeclaredStatement.WithStatus<?> stmt) {
+        if (stmt instanceof ChoiceStatement) {
+            return " (" + stmt.rawArgument() + ")";
+        } else if (stmt instanceof CaseStatement) {
+            return ":(" + stmt.rawArgument() + ")";
+        }
+        return " " + stmt.rawArgument();
+    }
+
+    private String getOpts(DocumentedDeclaredStatement.WithStatus<?> stmt) {
+        if (stmt instanceof MandatoryStatementAwareDeclaredStatement<?> mandatoryAwareStatement) {
+            final var optionalMandatoryStatement = mandatoryAwareStatement.getMandatory();
+            if (optionalMandatoryStatement.isPresent()) {
+                if (!optionalMandatoryStatement.orElseThrow().argument()) {
+                    return "?";
+                }
+                return "";
+            }
+            return "";
+        }
+        else if (stmt instanceof ContainerStatement containerStatement) {
+            final PresenceStatement presenceStatement = containerStatement.getPresence();
+            if (presenceStatement == null) {
+                return "!";
+            } else if (presenceStatement.argument().equals("true")) {
+                return "!";
+            }
+            return "";
+        } else if (stmt instanceof LeafListStatement) {
+            return "*";
+        } else if (stmt instanceof ListStatement listStatement) {
+            final KeyStatement keyStatement = listStatement.getKey();
+            return "* [" + keyStatement.rawArgument() + "]";
+        }
+        return "";
+    }
+
+    private String getType(DocumentedDeclaredStatement.WithStatus<?> stmt) {
+        if (stmt instanceof TypeAwareDeclaredStatement typeAwareStatement) {
+            return typeAwareStatement.getType().rawArgument();
+        }
+        return "";
+    }
+
+    private String getIfFeatures(DocumentedDeclaredStatement.WithStatus<?> stmt) {
+        if (stmt instanceof IfFeatureAwareDeclaredStatement<?> ifFeatureAwareStatement) {
+            final var ifFeatures = ifFeatureAwareStatement.getIfFeatures()
+                    .stream().map((statement) ->
+                            statement.rawArgument() == null ? "" : statement.rawArgument())
+                    .toList();
+            if (!ifFeatures.isEmpty()) {
+                return "{" + String.join(" ", ifFeatures) + "}?";
+            }
+            return "";
+        }
+        return "";
+    }
+
+    private void pushStatement(Iterator<? extends DeclaredStatement<?>> stmt) {
+        if (stmt.hasNext()) {
+            indent.push("|  ");
+        }
+        else {
+            indent.push("   ");
+        }
+        stack.push(stmt);
+    }
+
+    private Iterator<? extends DeclaredStatement<?>> pollStatement() {
+        indent.pop();
+        return  stack.poll();
+    }
+}
