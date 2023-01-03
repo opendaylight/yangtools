c5ee0f2ff2152992b645d8dad693888bfdab2132
Robert Varga <robert.varga@pantheon.tech>
2023-01-03 16:39:04 +0100

    Correct instance-identifier escaping
    Dealing with single-quoted and double-quoted strings in
    instance-identifier differs quite a bit. In order to deal with these
    strings, we need to differentiate them in lexer, for which we create
    separate modes.
    
    This forces us to explicitly define lexer lokens and only match raw
    strings in their mode -- ensuring there are no surprises.
    
    JIRA: YANGTOOLS-1458
    Change-Id: I3f58c10f068da1128d8a7c1c5bed3917bfea0c78
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ xpath/yang-xpath-impl/src/test/java/org/opendaylight/yangtools/yang/xpath/impl/InstanceIdentifierParserTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.xpath.impl;
+
+import com.google.common.collect.ImmutableBiMap;
+import org.junit.jupiter.api.Test;
+import org.opendaylight.yangtools.yang.common.BiMapYangNamespaceContext;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.common.XMLNamespace;
+import org.opendaylight.yangtools.yang.common.YangNamespaceContext;
+import org.opendaylight.yangtools.yang.xpath.api.YangLiteralExpr;
+import org.opendaylight.yangtools.yang.xpath.api.YangLocationPath.Absolute;
+import org.opendaylight.yangtools.yang.xpath.api.YangXPathMathMode;
+
+class InstanceIdentifierParserTest {
+    private static final QNameModule DEFNS = QNameModule.create(XMLNamespace.of("defaultns"));
+    private static final YangNamespaceContext CONTEXT = new BiMapYangNamespaceContext(ImmutableBiMap.of(
+        "def", DEFNS,
+        "foo", QNameModule.create(XMLNamespace.of("foo")),
+        "bar", QNameModule.create(XMLNamespace.of("bar"))));
+
+    private static final InstanceIdentifierParser PARSER =
+        new InstanceIdentifierParser.Qualified(YangXPathMathMode.IEEE754, CONTEXT);
+
+    @Test
+    void smokeTests() throws Exception {
+        assertPath("/foo:foo");
+        assertPath("/foo:foo[.='abcd']");
+        assertPath("/foo:foo[.=\"abcd\"]");
+    }
+
+    private static Absolute assertPath(final String literal) throws Exception {
+        return PARSER.interpretAsInstanceIdentifier(YangLiteralExpr.of(literal));
+    }
+}
