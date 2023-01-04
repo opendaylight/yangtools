4005067996f7415546722ab9df0870adcd123694
Sangwook Ha <sangwook.ha@verizon.com>
2023-01-03 20:12:51 -0800

    Fix ReactorStmtCtx.calculateParentRefcount()
    If we have an empty grouping, which is referenced via augment-uses to a
    different module, then if that module is buildEffective()'d first, we
    end up with the grouping being swept.
    
    This turns out to be a bug in parentRef determination -- in this
    particular case we observe parent being REFCOUNT_NONE and invoke it
    recursively -- and since the parent is a module, it itself does not have
    a parentRef. This leads to us thinking the grouping does not have a
    parentRef, whereas it does.
    
    Update calculateParentRefcount() to recognize parent.refCount == 0 as
    parent holding us down.
    
    The problem manifests itself in the test when the models are processed
    in order foo.yang, bar.yang -- but not when bar.yang is processed before
    foo.yang.
    
    Unfortunately the order of processing is not predictable in
    BuildGlobalContext, hence we repeat the test 4 times.
    
    JIRA: YANGTOOLS-1474
    Change-Id: Ib3d55fb92d5a6d34230722ab1cd39e8f8831b6fe
    Signed-off-by: Sangwook Ha <sangwook.ha@verizon.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit ad55ca2b2e72f15d7963cfcb011e4f7610889fce)

---
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/YT1474Test.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2023 Verizon and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.RepeatedTest;
+
+public class YT1474Test extends AbstractYangTest {
+    // FIXME: YANGTOOLS-1475: do not repeat the test once we have a way to force predictable execution order
+    @RepeatedTest(4)
+    public void testEmptyGrouping() {
+        assertEffectiveModel(List.of("/bugs/YT1474/foo.yang", "/bugs/YT1474/bar.yang"), Set.of());
+    }
+}
