165f906804ed3989c345b9ddae2a62d9f4ad3a36
Robert Varga <robert.varga@pantheon.tech>
2023-01-06 16:30:27 +0100

    Key value serialization fix: part 2 - string values
    Refactor serialization to ensure we first encode the value and then
    decide whether to escape it or not.
    
    JIRA: YANGTOOLS-1473
    Change-Id: I0df9beaf720a78682b6bde606a1359e7f1a09df8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

--- codec/yang-data-codec-xml/src/test/java/org/opendaylight/yangtools/yang/data/codec/xml/YT1473Test.java
+++ codec/yang-data-codec-xml/src/test/java/org/opendaylight/yangtools/yang/data/codec/xml/YT1473Test.java
@@ -8,27 +8,42 @@
 package org.opendaylight.yangtools.yang.data.codec.xml;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.verify;
 
+import java.io.StringReader;
+import java.util.stream.Stream;
+import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLStreamWriter;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.yangtools.util.xml.UntrustedXML;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeWithValue;
+import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.impl.schema.NormalizedNodeResult;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.ListSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.type.InstanceIdentifierTypeDefinition;
+import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack.Inference;
 import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
 
 @ExtendWith(MockitoExtension.class)
@@ -48,6 +63,10 @@ public class YT1473Test {
     private static final QName BAR_BAR = QName.create(BAR_NS, "bar"); // leaf of type 'instance-identifier'
     private static final QName BAR_TWO = QName.create(BAR_NS, "two"); // identity inheriting 'foo:one'
 
+    private static final String XML_HEAD = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    private static final String XMLNS = "xmlns:foo=\"foons\" xmlns:bar=\"barns\"";
+
+    private static EffectiveModelContext MODEL_CONTEXT;
     private static XmlCodec<YangInstanceIdentifier> CODEC;
 
     @Mock
@@ -57,33 +76,56 @@ public class YT1473Test {
 
     @BeforeAll
     public static void beforeAll() {
-        final var modelContext = YangParserTestUtils.parseYangResourceDirectory("/yt1473");
-        final var baz = modelContext.getDataChildByName(FOO_BAZ);
+        MODEL_CONTEXT = YangParserTestUtils.parseYangResourceDirectory("/yt1473");
+        final var baz = MODEL_CONTEXT.getDataChildByName(FOO_BAZ);
         assertTrue(baz instanceof ListSchemaNode);
         final var id = ((ListSchemaNode) baz).getDataChildByName(FOO_ID);
         assertTrue(id instanceof LeafSchemaNode);
         final var type = ((LeafSchemaNode) id).getType();
         assertTrue(type instanceof InstanceIdentifierTypeDefinition);
-        CODEC = (XmlStringInstanceIdentifierCodec) XmlCodecFactory.create(modelContext)
+        CODEC = (XmlStringInstanceIdentifierCodec) XmlCodecFactory.create(MODEL_CONTEXT)
                 .instanceIdentifierCodec((InstanceIdentifierTypeDefinition) type);
     }
 
     @AfterAll
     public static void afterAll() {
+        MODEL_CONTEXT = null;
         CODEC = null;
     }
 
-    @Test
-    public void testSerializeSimple() throws Exception {
-        // No escaping needed, use single quotes
-        assertEquals("/foo:foo[foo:str='str\"']", write(buildYangInstanceIdentifier(FOO_FOO, FOO_STR, "str\"")));
+    @ParameterizedTest(name = "Serialize key value: {0}")
+    @MethodSource("testArgs")
+    void serialize(final String testDesc, final String output, final YangInstanceIdentifier input) throws Exception {
+        assertEquals(output, write(input));
     }
 
-    @Test
-    @Disabled("YT-1473: string escaping needs to work")
-    public void testSerializeEscaped() throws Exception {
-        // Escaping is needed, use double quotes and escape
-        assertEquals("/foo:foo[foo:str=\"str'\\\"\"]", write(buildYangInstanceIdentifier(FOO_FOO, FOO_STR, "str'\"")));
+    @ParameterizedTest(name = "Parse key value: {0}")
+    @MethodSource("testArgs")
+    void parse(final String testDesc, final String input, final YangInstanceIdentifier output) throws Exception {
+
+        // case 1: list with key of type 'instance-identifier'
+        final String xml1 = XML_HEAD + "<baz xmlns=\"foons\"><id " + XMLNS + ">" + input + "</id></baz>";
+        final NormalizedNode normalizedNode1 = xmlToNormalizedNode(xml1, FOO_BAZ);
+        assertEquals(new NodeIdentifier(FOO_BAZ), normalizedNode1.getIdentifier());
+        assertTrue(normalizedNode1 instanceof MapNode);
+        final MapNode mapNode = (MapNode) normalizedNode1;
+        assertEquals(1, mapNode.size());
+        assertNotNull(mapNode.childByArg(NodeIdentifierWithPredicates.of(FOO_BAZ, FOO_ID, output)));
+
+        // case 2: leaf of type 'instance-identifier'
+        final String xml2 = XML_HEAD + "<bar:bar " + XMLNS + ">" + input + "</bar:bar>";
+        final NormalizedNode normalizedNode2 = xmlToNormalizedNode(xml2, BAR_BAR);
+        assertEquals(new NodeIdentifier(BAR_BAR), normalizedNode2.getIdentifier());
+        assertEquals(output, normalizedNode2.body());
+    }
+
+    private static Stream<Arguments> testArgs() {
+        return Stream.of(
+                Arguments.of("string unescaped", "/foo:foo[foo:str='str\"']",
+                        buildYangInstanceIdentifier(FOO_FOO, FOO_STR, "str\"")),
+                Arguments.of("string escaped", "/foo:foo[foo:str=\"str'\\\"\"]",
+                        buildYangInstanceIdentifier(FOO_FOO, FOO_STR, "str'\""))
+        );
     }
 
     @Test
@@ -136,4 +178,15 @@ public class YT1473Test {
         verify(writer).writeCharacters(captor.capture());
         return captor.getValue();
     }
+
+    private static NormalizedNode xmlToNormalizedNode(final String xml, final QName qname) throws Exception {
+        final XMLStreamReader reader = UntrustedXML.createXMLStreamReader(new StringReader(xml));
+        final NormalizedNodeResult result = new NormalizedNodeResult();
+        final NormalizedNodeStreamWriter streamWriter = ImmutableNormalizedNodeStreamWriter.from(result);
+        final XmlParserStream xmlParser = XmlParserStream.create(streamWriter,
+                Inference.ofDataTreePath(MODEL_CONTEXT, qname));
+        xmlParser.parse(reader);
+        return result.getResult();
+    }
+
 }
