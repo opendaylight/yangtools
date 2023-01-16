9f43415928851878525851de764ef26dd484b65b
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2023-01-16 11:02:55 +0200

    Bits to hex string codec implementation
    New implementation of bits codec targeted to optimize storage space.
    Now byte array keeps the data, while name to bit index mapping
    remains in codec itself.
    
    Legacy codec usage is replaced with new one, related tests updated.
    
    JIRA: YANGTOOLS-1426
    Change-Id: Iba77f251ef9f9d850d6d4c09ccfd8c3b03d8b0ef
    Signed-off-by: Ivan Martiniak <ivan.martiniak@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

--- data/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/codec/BitsCodecStringTest.java
+++ data/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/codec/BitsCodecStringTest.java
@@ -7,66 +7,80 @@
  */
 package org.opendaylight.yangtools.yang.data.impl.codec;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.opendaylight.yangtools.yang.data.impl.codec.TypeDefinitionAwareCodecTestHelper.getCodec;
 
-import com.google.common.collect.ImmutableSet;
-import java.util.Collections;
-import org.junit.Test;
+import java.util.Set;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.common.Uint32;
 import org.opendaylight.yangtools.yang.data.api.codec.BitsCodec;
 import org.opendaylight.yangtools.yang.model.api.type.BitsTypeDefinition;
 import org.opendaylight.yangtools.yang.model.ri.type.BaseTypes;
+import org.opendaylight.yangtools.yang.model.ri.type.BitBuilder;
 import org.opendaylight.yangtools.yang.model.ri.type.BitsTypeBuilder;
 
-/**
- * Unit tests for BitsCodecString.
- *
- * @author Thomas Pantelis
- */
-public class BitsCodecStringTest {
-    private  static BitsTypeDefinition toBitsTypeDefinition(final String... bits) {
-        final BitsTypeBuilder b = BaseTypes.bitsTypeBuilder(QName.create("foo", "foo"));
+class BitsCodecStringTest {
 
-        long pos = 0;
-        for (String bit : bits) {
-            BitsTypeDefinition.Bit mockBit = mock(BitsTypeDefinition.Bit.class);
-            doReturn(bit).when(mockBit).getName();
-            doReturn(Uint32.valueOf(pos)).when(mockBit).getPosition();
-            b.addBit(mockBit);
-            ++pos;
-        }
+    @ParameterizedTest(name = "Deserialize \"{1}\" -> \"{2}\"")
+    @MethodSource("bitsSerializationArgs")
+    void deserialization(final int bitsTotal, final String input, final String output) {
+        final BitsCodec<String> codec = getCodec(buildBitsTypeDefinition(bitsTotal), BitsCodec.class);
+        assertEquals(output, codec.deserialize(input));
+    }
 
+    @ParameterizedTest(name = "Serialize \"{2}\" -> \"{1}\"")
+    @MethodSource("bitsSerializationArgs")
+    void serialization(final int bitsTotal, final String output, final String input) {
+        final BitsCodec<String> codec = getCodec(buildBitsTypeDefinition(bitsTotal), BitsCodec.class);
+        assertStringSetsAreEqual(output, codec.serialize(input));
+    }
+
+    private static Stream<Arguments> bitsSerializationArgs() {
+        // (number of bits, serialized value, deserialized value)
+        return Stream.of(
+                Arguments.of(1, "", ""),
+                Arguments.of(1, toBitNames(0), "01"),
+                Arguments.of(8, toBitNames(1, 2, 7), "86"),
+                Arguments.of(9, toBitNames(0, 7, 8), "8101"),
+                Arguments.of(64, toBitNames(0, 63), "0100000000000080")
+        );
+    }
+
+    private static String toBitNames(final int... selectedIndexes) {
+        return String.join(" ", IntStream.of(selectedIndexes).mapToObj(i -> "Bit" + i).toList());
+    }
+
+    @Test
+    void deserializationFailure() {
+        final BitsCodec<String> codec = getCodec(buildBitsTypeDefinition(8), BitsCodec.class);
+        assertThrows(IllegalArgumentException.class,
+                () -> codec.deserialize("Bit1 Bit10")); // non-existing bit name Bit10
+    }
+
+    @Test
+    void serializationFailure() {
+        final BitsCodec<String> codec = getCodec(buildBitsTypeDefinition(1), BitsCodec.class);
+        assertThrows(IllegalArgumentException.class, () -> codec.serialize("non-hex input"));
+    }
+
+    private static BitsTypeDefinition buildBitsTypeDefinition(final int numOfBits) {
+        final BitsTypeBuilder b = BaseTypes.bitsTypeBuilder(QName.create("foo", "bar"));
+        IntStream.range(0, numOfBits).forEach(i -> {
+            b.addBit(BitBuilder.create("Bit" + i, Uint32.valueOf(i * 2)).build());
+        });
         return b.build();
     }
 
-    @SuppressWarnings("unchecked")
-    @Test
-    public void testSerialize() {
-        BitsCodec<String> codec = TypeDefinitionAwareCodecTestHelper.getCodec(toBitsTypeDefinition("foo"),
-                BitsCodec.class);
-
-        String serialized = codec.serialize(ImmutableSet.of("foo", "bar"));
-        assertNotNull(serialized);
-        assertTrue(serialized.contains("foo"));
-        assertTrue(serialized.contains("bar"));
-
-        assertEquals("", codec.serialize(ImmutableSet.of()));
-    }
-
-    @SuppressWarnings("unchecked")
-    @Test
-    public void testDeserialize() {
-        BitsCodec<String> codec = TypeDefinitionAwareCodecTestHelper.getCodec(
-            toBitsTypeDefinition("bit1", "bit2"), BitsCodec.class);
-
-        assertEquals("deserialize", ImmutableSet.of("bit1", "bit2"), codec.deserialize("  bit1 bit2     "));
-        assertEquals("deserialize", Collections.emptySet(), codec.deserialize(""));
-
-        TypeDefinitionAwareCodecTestHelper.deserializeWithExpectedIllegalArgEx(codec, "bit1 bit3");
+    private static void assertStringSetsAreEqual(final String expected, final String actual) {
+        final Set<String> expectedSet = Set.of(expected.trim().split("\\s+"));
+        final Set<String> actualSet = actual == null ? null : Set.of(actual.trim().split("\\s+"));
+        assertEquals(expectedSet, actualSet);
     }
 }
