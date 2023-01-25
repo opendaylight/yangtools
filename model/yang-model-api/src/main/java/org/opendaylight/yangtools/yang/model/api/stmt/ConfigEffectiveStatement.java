2ff86e63a4f563884234c0f6884ea1efd2443d0e
Robert Varga <robert.varga@pantheon.tech>
2023-01-25 05:07:52 +0100

    Document {Config,Status}EffectiveStatement as exceptional
    These two statements have a counter-intuitive lifecycle dictated
    by scalability concerns. While we could go the other way and instantiate
    effective views, the information conveyed is not consulted often and
    lends itself to extenal caching. Document the object model behaviour of
    these two statements, providing explanation and hints to end users.
    
    JIRA: YANGTOOLS-1271
    JIRA: YANGTOOLS-1272
    Change-Id: I20ac9e3aae0d0cbae37c5671649cde7347016dd8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/ConfigEffectiveStatement.java
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/ConfigEffectiveStatement.java
@@ -13,6 +13,41 @@ import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 
 /**
  * Effective representation of a {@code config} statement.
+ *
+ * <p>
+ * Note that unlike almost all other representations, these statements are only ever a reflection of a declared
+ * {@code config} statement. The truly effective status of a particular statement within a tree depends on its parent
+ * statements. As an example, given this YANG module:
+ * <code>
+ *   <pre>
+ *     module foo {
+ *
+ *       grouping foo {
+ *         leaf baz {
+ *           type string;
+ *         }
+ *       }
+ *
+ *       container bar {
+ *         config false;
+ *         uses foo;
+ *       }
+ *
+ *       uses bar;
+ *     }
+ *   </pre>
+ * </code>
+ * The object model will only reflect the {@code config} statement in {@code container bar}, but will not be present in
+ * {@code bar}'s {@code baz} leaf. The real effective {@code config} of {@code leaf baz} is a tri-state value:
+ * <ol>
+ *   <li>within {@code grouping foo} it is not applicable, that is to say undefined</li>
+ *   <li>within {@code container bar} it is inherited, that is to say {@code false}</li>
+ *   <li>within {@code module foo} it is inherited from default, that is to say {@code true}</li>.
+ * </ol>
+ *
+ * <p>
+ * Users are advised to use utility classes related to statement inference which consider parent/child relationships
+ * of statements.
  */
 public interface ConfigEffectiveStatement extends EffectiveStatement<Boolean, ConfigStatement> {
     @Override
