9d7c33f0535d006ede5180a1cdf3be35dafa9288
Robert Varga <robert.varga@pantheon.tech>
2023-02-17 22:13:46 +0100

    WIP: Revise NormalizedNode interface hierarchy
    // FIXME: assign issue
    
    Change-Id: I74987432948cea9f6fc74eac1e51a09c93cc912f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- data/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/NormalizedNode.java
+++ data/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/NormalizedNode.java
@@ -10,42 +10,44 @@ package org.opendaylight.yangtools.yang.data.api.schema;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.concepts.Identifiable;
 import org.opendaylight.yangtools.concepts.PrettyTreeAware;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 
 /**
- * Node which is normalized according to the YANG schema
- * is identifiable by a {@link org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier}.
+ * Node which has been normalized according to the YANG schema and is identifiable by a {@link YangInstanceIdentifier}.
+ * These nodes form the interior of a {@link NormalizedTree}.
  *
  * <p>
  * See subinterfaces of this interface for concretization of node.
  */
 /*
- * FIXME: 8.0.0: NormalizedNode represents the perfectly-compliant view of the data, as evaluated by an implementation,
- *               which is currently singular, with respect of its interpretation of a SchemaContext. This includes
- *               leaf values, which are required to hold normalized representation for a particular implementation,
- *               which may be affected by its understanding of any YANG extensions present -- such as optional type
- *               handling hints and bindings.
+ * FIXME: 11.0.0: NormalizedNode represents the perfectly-compliant view of the data, as evaluated by an implementation,
+ *                which is currently singular, with respect of its interpretation of a SchemaContext. This includes
+ *                leaf values, which are required to hold normalized representation for a particular implementation,
+ *                which may be affected by its understanding of any YANG extensions present -- such as optional type
+ *                handling hints and bindings.
  *
- *               Implementations (i.e. the reference implementation and parsers) will need to start using
- *               yang.common.Uint8 and friends and, if possible, express data validation in terms
- *               of yang.common.CanonicalValue and yang.common.CanonicalValueValidator.
+ *                Implementations (i.e. the reference implementation and parsers) will need to start using
+ *                yang.common.Uint8 and friends and, if possible, express data validation in terms
+ *                of yang.common.CanonicalValue and yang.common.CanonicalValueValidator.
  *
- *               This notably means that to efficiently implement any sort of lenient parsing, we need a separate
- *               concept which contains an unverified, potentially non-conformant data tree, which the consumer needs
- *               to check/fixup if it wishes to use it as a NormalizedNode. Such a concept should be called
- *               "UnverifiedData".
+ *                This notably means that to efficiently implement any sort of lenient parsing, we need a separate
+ *                concept which contains an unverified, potentially non-conformant data tree, which the consumer needs
+ *                to check/fixup if it wishes to use it as a NormalizedNode. Such a concept should be called
+ *                "UnverifiedData".
  *
- * FIXME: 8.0.0: Once we have UnverifiedData, we should really rename this to "NormalizedData" or similar to unload
- *               some "Node" ambiguity. "Node" should be a generic term reserved for a particular domain -- hence 'node'
- *               can be used to refer to either a 'schema node' in context of yang.model.api, or to
- *               a 'normalized data node' in context of yang.data.api.
+ * FIXME: 11.0.0: Once we have UnverifiedData, we should really rename this to "NormalizedData" or similar to unload
+ *                some "Node" ambiguity. "Node" should be a generic term reserved for a particular domain -- hence 'node'
+ *                can be used to refer to either a 'schema node' in context of yang.model.api, or to
+ *                a 'normalized data node' in context of yang.data.api.
  *
- * FIXME: 8.0.0: Well, not quite. The structure of unverified data is really codec specific -- and JSON and XML
- *               do not agree on details. Furthermore things get way more complicated when we have a cross-schema
- *               boundary -- like RFC8528. Hence we cannot really have a reasonably-structured concept of unverified
- *               data. Nevertheless, this interface should be named 'NormalizedData'.
+ * FIXME: 11.0.0: Well, not quite. The structure of unverified data is really codec specific -- and JSON and XML
+ *                do not agree on details. Furthermore things get way more complicated when we have a cross-schema
+ *                boundary -- like RFC8528. Hence we cannot really have a reasonably-structured concept of unverified
+ *                data. Nevertheless, this interface should be named 'NormalizedData'.
+ * FIXME: 11.0.0: Seal this interface
  */
-public interface NormalizedNode extends Identifiable<PathArgument>, PrettyTreeAware {
+public non-sealed interface NormalizedNode extends NormalizedData, Identifiable<PathArgument>, PrettyTreeAware {
     @Override
     // We override here, so that NormalizedNode.getIdentifier() has fewer implementations
     PathArgument getIdentifier();
