3b24adf5597b0027f69279614ba991dc274bc4da
Robert Varga <robert.varga@pantheon.tech>
2023-02-17 22:13:46 +0100

    WIP: Revise NormalizedNode interface hierarchy
    // FIXME: assign issue
    
    Change-Id: I74987432948cea9f6fc74eac1e51a09c93cc912f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ data/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/NormalizedOutput.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.api.schema;
+
+/**
+ * The contents of an RPC or action output, as expressed through an {@code output} statement.
+ */
+public non-sealed interface NormalizedOutput extends NormalizedTree {
+
+}
