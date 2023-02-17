1f8051a1a3c7d723c794b0cba9f2c786382e4fd8
Robert Varga <robert.varga@pantheon.tech>
2023-02-17 22:13:46 +0100

    WIP: Revise NormalizedNode interface hierarchy
    // FIXME: assign issue
    
    Change-Id: I74987432948cea9f6fc74eac1e51a09c93cc912f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ data/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/NormalizedYangData.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.api.schema;
+
+/**
+ * The contents of a {@code yang-data} template instance, as defined in
+ * <a href="https://www.rfc-editor.org/rfc/rfc8040#page-80">RFC8040</a>'s {@code ietf-restconf} module.
+ */
+public non-sealed interface NormalizedYangData extends NormalizedTree {
+
+}
