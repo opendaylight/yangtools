b9d1cbef0d3744ee068cf21c437ae94fe4c2b864
Robert Varga <robert.varga@pantheon.tech>
2023-02-22 15:51:28 +0100

    Add NormalizedNodeDataInput.readLegacyIdentifier()
    This is a migration method for users for readPathArgument(), which
    has an indication of the fact that the identifier would have been an
    AugmentationIdentifier or a MountPointIdentifier.
    
    JIRA: YANGTOOLS-568
    Change-Id: I4f6e68d5decde9d57da9f0431d87e0d8db80a0d0
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ codec/yang-data-codec-binfmt/src/main/java/org/opendaylight/yangtools/yang/data/codec/binfmt/LegacyMountPointIdentifier.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.codec.binfmt;
+
+import static java.util.Objects.requireNonNull;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.rfc8528.data.api.MountPointIdentifier;
+import org.opendaylight.yangtools.yang.common.QName;
+
+/**
+ * Representation of legacy {@link MountPointIdentifier}.
+ */
+@Deprecated(since = "11.0.0")
+public record LegacyMountPointIdentifier(@NonNull QName qname) implements LegacyPathArgument {
+    public LegacyMountPointIdentifier {
+        requireNonNull(qname);
+    }
+}
