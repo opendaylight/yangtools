0bb9d758a7d7faabbc6cca057a166c997e1b5040
Robert Varga <robert.varga@pantheon.tech>
2023-03-13 18:00:48 +0100

    Use local variable type inference a bit more
    Reduce verbosity by using local variable type inference.
    
    Change-Id: I8e02d0642b6e28052332a5f5dd5e9601442af8db
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/ModifierImpl.java
+++ parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/ModifierImpl.java
@@ -65,9 +65,9 @@ final class ModifierImpl implements ModelActionBuilder {
     }
 
     private boolean removeSatisfied() {
-        final Iterator<AbstractPrerequisite<?>> it = unsatisfied.iterator();
+        final var it = unsatisfied.iterator();
         while (it.hasNext()) {
-            final AbstractPrerequisite<?> prereq = it.next();
+            final var prereq = it.next();
             if (prereq.isDone()) {
                 // We are removing current prerequisite from list.
                 LOG.trace("Modifier {} prerequisite {} satisfied", this, prereq);
@@ -466,7 +466,7 @@ final class ModifierImpl implements ModelActionBuilder {
         @Override
         public void namespaceItemAdded(final StatementContextBase<?, ?, ?> context,
                 final ParserNamespace<?, ?> namespace, final Object key, final Object value) {
-            StatementContextBase<?, ?, ?> targetCtx = contextImpl(value);
+            final var targetCtx = contextImpl(value);
             targetCtx.addMutation(modPhase, this);
             resolvePrereq((C) targetCtx);
         }
