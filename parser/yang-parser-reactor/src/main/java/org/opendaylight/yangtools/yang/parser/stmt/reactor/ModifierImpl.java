39402dbca1d1ae06be52a9027095fbefdbe42463
Robert Varga <robert.varga@pantheon.tech>
2023-03-13 15:41:58 +0100

    Eliminate ModelActionBuilder.requiresCtxPath()
    There is only a single user of this method, which specifies
    EFFECTIVE_MODEL argument, which the implementation hard-codes instead of
    using the argument.
    
    Introduce requiresEffectiveCtxPath() which makes the processing phase
    implicit and inline the phase to prerequisite implementations.
    
    JIRA: YANGTOOLS-1485
    Change-Id: I9072fe022600f0b24179aaad08c05cfcce2604e6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/ModifierImpl.java
+++ parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/ModifierImpl.java
@@ -179,11 +179,11 @@ final class ModifierImpl implements ModelActionBuilder {
     }
 
     @Override
-    public <K, C extends StmtContext<?, ?, ?>> Prerequisite<C> requiresCtxPath(final StmtContext<?, ?, ?> context,
-            final ParserNamespace<K, C> namespace, final Iterable<K> keys, final ModelProcessingPhase phase) {
+    public <K, C extends StmtContext<?, ?, ?>> Prerequisite<C> requiresEffectiveCtxPath(
+            final StmtContext<?, ?, ?> context, final ParserNamespace<K, C> namespace, final Iterable<K> keys) {
         checkNotRegistered();
 
-        final var ret = new PhaseRequirementInNamespacePath<C, K>(this, EFFECTIVE_MODEL, keys);
+        final var ret = new PhaseRequirementInNamespacePath<C, K>(this, keys);
         addReq(ret);
         addBootstrap(() -> ret.hookOnto(context, namespace));
         return ret;
@@ -250,7 +250,7 @@ final class ModifierImpl implements ModelActionBuilder {
             final Iterable<K> keys) {
         checkNotRegistered();
 
-        final var ret = new PhaseModificationInNamespacePath<Mutable<?, ?, E>, K>(this, EFFECTIVE_MODEL, keys);
+        final var ret = new PhaseModificationInNamespacePath<Mutable<?, ?, E>, K>(this, keys);
         addReq(ret);
         addMutation(ret);
         addBootstrap(() -> ret.hookOnto(context, namespace));
@@ -446,9 +446,8 @@ final class ModifierImpl implements ModelActionBuilder {
 
     private static final class PhaseRequirementInNamespacePath<C extends StmtContext<?, ?, ?>, K>
             extends AbstractPathPrerequisite<C, K> {
-        PhaseRequirementInNamespacePath(final ModifierImpl modifier, final ModelProcessingPhase phase,
-                final Iterable<K> keys) {
-            super(modifier, phase, keys);
+        PhaseRequirementInNamespacePath(final ModifierImpl modifier, final Iterable<K> keys) {
+            super(modifier, EFFECTIVE_MODEL, keys);
         }
 
         @Override
@@ -490,9 +489,8 @@ final class ModifierImpl implements ModelActionBuilder {
      */
     private static final class PhaseModificationInNamespacePath<C extends Mutable<?, ?, ?>, K>
             extends AbstractPathPrerequisite<C, K> implements ContextMutation {
-        PhaseModificationInNamespacePath(final ModifierImpl modifier, final ModelProcessingPhase phase,
-                final Iterable<K> keys) {
-            super(modifier, phase, keys);
+        PhaseModificationInNamespacePath(final ModifierImpl modifier, final Iterable<K> keys) {
+            super(modifier, EFFECTIVE_MODEL, keys);
         }
 
         @Override
