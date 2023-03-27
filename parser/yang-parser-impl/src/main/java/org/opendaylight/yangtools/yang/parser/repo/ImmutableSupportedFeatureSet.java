b3c555f6d48772673d1a86bfb003507b72287243
matus.matok <matus.matok@pantheon.tech>
2023-03-27 13:31:23 +0200

    YangTextSchemaContextResolver needs to we aware of supported features
    Added new method YangTextSchemaContextResolver.registeredFeatures() which registers supported features for YangTextSchemaContextResolver.
    The logic works as follows. If no features were registered, all features are supported. If a set of features for a module is registered, all the features contained are supported. If multiple sets are registered, a union of contained features is supported. A module which has no features registered has all features supported. If there is only an empty set for a module registered, no features are supported in that module.
    This method returns a Registration that upon closing removes a support for the given set of features on which the Registration was returned. If a single feature is registered by multiple method calls, removal of the support for such feature requires all the respective Registrations to close.
    
    JIRA: YANGTOOLS-1463
    Change-Id: I443d42a6859ce97f238117fabdb7bbdd42488ee5
    Signed-off-by: matus.matok <matus.matok@pantheon.tech>
    
    YangTextSchemaContextResolver needs to we aware of supported features
    
    Added new method YangTextSchemaContextResolver.registeredFeatures() which registers supported features for YangTextSchemaContextResolver.
    The logic works as follows. If no features were registered, all features are supported. If a set of features is registered, all the features contained are supported. If multiple sets are registered, a union of contained features is supported.
    This method returns a Registration that upon closing removes a support for the given set of features on which the Registration was returned. If a single feature is registered by multiple method calls, removal of the support for such feature requires all the respective Registrations to close.
    If there is only an empty set registered, no features are supported.
    
    JIRA: YANGTOOLS-1463
    Change-Id: I443d42a6859ce97f238117fabdb7bbdd42488ee5
    Signed-off-by: matus.matok <matus.matok@pantheon.tech>

---
+++ parser/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/repo/ImmutableSupportedFeatureSet.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.repo;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.model.repo.api.SupportedFeatureSet;
+
+final class ImmutableSupportedFeatureSet extends SupportedFeatureSet {
+    // Note: not a ImmutableSetMultimap because we need to distinguish non-presence vs. empty Set
+    private final ImmutableMap<QNameModule, ImmutableSet<String>> featuresByModule;
+
+    ImmutableSupportedFeatureSet(final ImmutableMap<QNameModule, ImmutableSet<String>> featuresByModule) {
+        this.featuresByModule = requireNonNull(featuresByModule);
+    }
+
+    @Override
+    public boolean contains(final QName qname) {
+        final var sets = featuresByModule.get(qname.getModule());
+        return sets == null || sets.contains(qname.getLocalName());
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this).add("features", featuresByModule).toString();
+    }
+}
