5fbbb4007f4e31536992235c56f665911351a67a
Robert Varga <robert.varga@pantheon.tech>
2023-04-06 16:42:35 +0200

    Remove DerivedNamespaceBehaviour
    DerivedNamespaceBehaviour is not used anywhere and is source of quite a
    bit of head-scratching. Remove it, which allows is to solidify
    NamespaceAccess to a great extent.
    
    JIRA: YANGTOOLS-1502
    Change-Id: Iba43d0f627d5df53f18e9ea4de79f0982b62d837
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/NamespaceAccess.java
+++ parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/NamespaceAccess.java
@@ -9,9 +9,6 @@ package org.opendaylight.yangtools.yang.parser.stmt.reactor;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import org.eclipse.jdt.annotation.NonNull;
@@ -20,47 +17,28 @@ import org.opendaylight.yangtools.yang.parser.spi.meta.NamespaceKeyCriterion;
 import org.opendaylight.yangtools.yang.parser.spi.meta.NamespaceStorage;
 
 abstract class NamespaceAccess<K, V> {
-    abstract static class ValueAddedListener<K> {
-        private final NamespaceStorage ctxNode;
-
-        ValueAddedListener(final NamespaceStorage ctxNode) {
-            this.ctxNode = requireNonNull(ctxNode);
-        }
-
-        final NamespaceStorage getCtxNode() {
-            return ctxNode;
-        }
-    }
-
-    abstract static class KeyedValueAddedListener<K> extends ValueAddedListener<K> {
-        private final K key;
+    abstract static class KeyedValueAddedListener<K> {
+        private final @NonNull NamespaceStorage contextNode;
+        private final @NonNull K key;
 
         KeyedValueAddedListener(final NamespaceStorage contextNode, final K key) {
-            super(contextNode);
+            this.contextNode = requireNonNull(contextNode);
             this.key = requireNonNull(key);
-        }
-
-        final K getKey() {
-            return key;
         }
 
         final <V> boolean isRequestedValue(final NamespaceAccess<K, ?> access, final NamespaceStorage storage,
                 final V value) {
-            return value == access.valueFrom(getCtxNode(), key);
+            return value == access.valueFrom(contextNode, key);
         }
 
         abstract void onValueAdded(Object value);
     }
 
-    abstract static class PredicateValueAddedListener<K, V> extends ValueAddedListener<K> {
-        PredicateValueAddedListener(final NamespaceStorage contextNode) {
-            super(contextNode);
-        }
+    @FunctionalInterface
+    interface PredicateValueAddedListener<K, V> {
 
-        abstract boolean onValueAdded(@NonNull K key, @NonNull V value);
+        boolean onValueAdded(@NonNull K key, @NonNull V value);
     }
-
-    private List<VirtualNamespaceContext<?, V, K>> derivedNamespaces;
 
     abstract @Nullable V valueFrom(@NonNull NamespaceStorage storage, K key);
 
@@ -74,34 +52,4 @@ abstract class NamespaceAccess<K, V> {
     abstract void addListener(KeyedValueAddedListener<K> listener);
 
     abstract void addListener(PredicateValueAddedListener<K, V> listener);
-
-    protected void notifyListeners(final NamespaceStorage storage,
-            final Iterator<? extends KeyedValueAddedListener<K>> keyListeners, final V value) {
-        List<KeyedValueAddedListener<K>> toNotify = new ArrayList<>();
-        while (keyListeners.hasNext()) {
-            final KeyedValueAddedListener<K> listener = keyListeners.next();
-            if (listener.isRequestedValue(this, storage, value)) {
-                keyListeners.remove();
-                toNotify.add(listener);
-            }
-        }
-        for (KeyedValueAddedListener<K> listener : toNotify) {
-            listener.onValueAdded(value);
-        }
-    }
-
-    protected void notifyDerivedNamespaces(final NamespaceStorage storage, final K key, final V value) {
-        if (derivedNamespaces != null) {
-            for (VirtualNamespaceContext<?, V, K> derived : derivedNamespaces) {
-                derived.addedToSourceNamespace(storage, key, value);
-            }
-        }
-    }
-
-    final void addDerivedNamespace(final VirtualNamespaceContext<?, V, K> namespace) {
-        if (derivedNamespaces == null) {
-            derivedNamespaces = new ArrayList<>();
-        }
-        derivedNamespaces.add(namespace);
-    }
 }
