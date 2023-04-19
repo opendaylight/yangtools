fae7481a929c8be0c535aa64b9dab51c6dbffea8
Robert Varga <robert.varga@pantheon.tech>
2023-04-19 18:05:30 +0200

    Fix yang-common-netty module warnings
    Explicitly require netty-common and document the module.
    
    Change-Id: I08c4762b12be7b65ce7d0088c8e842165bfb4c9f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 7df85ae3b5d8e08efa4ecfa2eb0e48d47571ece9)

--- common/yang-common-netty/src/main/java/module-info.java
+++ common/yang-common-netty/src/main/java/module-info.java
@@ -5,12 +5,16 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+/**
+ * Common utilities bridging common YANG constructs with Netty.
+ */
 module org.opendaylight.yangtools.yang.common.netty {
     exports org.opendaylight.yangtools.yang.common.netty;
 
     requires transitive io.netty.buffer;
     requires transitive org.opendaylight.yangtools.yang.common;
     requires com.google.common;
+    requires io.netty.common;
 
     // Annotations
     requires static transitive org.eclipse.jdt.annotation;
