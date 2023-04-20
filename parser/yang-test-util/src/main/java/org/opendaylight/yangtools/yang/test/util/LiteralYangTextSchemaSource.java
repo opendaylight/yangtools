4ad14a5a8c00ddc7055ee8500d301f945b8e006e
matus.matok <matus.matok@pantheon.tech>
2023-04-20 12:13:56 +0200

    Enrich YangParserTestUtils with parseYang(String)
    Added a new static method parseYang() to YangParserTestUtils which allows for
    parsing a yang file directly from s string input. This allows for higher
    flexibility as we do not need to deal with resource accessibility across
    modules. Additionally, it is sometimes more convenient to prefer this new form
    of input rather than the old one.
    
    JIRA: YANGTOOLS-1483
    Change-Id: I4865b73ba8770669a54dd5be167b2ca2d63f3585
    Signed-off-by: matus.matok <matus.matok@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit a0e551c041c426eecd6c6ee6456abe3fa6922a6f)

---
+++ parser/yang-test-util/src/main/java/org/opendaylight/yangtools/yang/test/util/LiteralYangTextSchemaSource.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.test.util;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.io.CharSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.common.UnresolvedQName;
+import org.opendaylight.yangtools.yang.model.repo.api.SourceIdentifier;
+import org.opendaylight.yangtools.yang.model.repo.api.YangTextSchemaSource;
+
+/**
+ * A {@link YangTextSchemaSource} backed by a string literal.
+ */
+final class LiteralYangTextSchemaSource extends YangTextSchemaSource {
+    private final @NonNull String sourceString;
+
+    private LiteralYangTextSchemaSource(final SourceIdentifier identifier, final String sourceString,
+            final String symbolicName) {
+        super(identifier);
+        this.sourceString = requireNonNull(sourceString);
+    }
+
+    /**
+     * Create a new {@link YangTextSchemaSource} backed by a String input.
+     *
+     * @param sourceString YANG file as a String
+     * @return A new instance.
+     * @throws NullPointerException if {@code sourceString} is {@code null}
+     * @throws IllegalArgumentException if {@code sourceString} does not a valid YANG body, given a rather restrictive
+     *         view of what is valid.
+     */
+    static @NonNull LiteralYangTextSchemaSource ofLiteral(final String sourceString) {
+        // First line of a YANG file looks as follows:
+        //   `module module-name {`
+        // therefore in order to extract the name of the module from a plain string, we are interested in the second
+        // word of the first line
+        final String[] firstLine = sourceString.substring(0, sourceString.indexOf("{")).strip().split(" ");
+        final String moduleOrSubmoduleString = firstLine[0].strip();
+        checkArgument(moduleOrSubmoduleString.equals("module") || moduleOrSubmoduleString.equals("submodule"));
+
+        final String arg = firstLine[1].strip();
+        final var localName = UnresolvedQName.tryLocalName(arg);
+        checkArgument(localName != null);
+        return new LiteralYangTextSchemaSource(new SourceIdentifier(localName), sourceString, arg);
+    }
+
+    @Override
+    public InputStream openStream() throws IOException {
+        return CharSource.wrap(sourceString).asByteSource(StandardCharsets.UTF_8).openStream();
+    }
+
+    @Override
+    public Optional<String> getSymbolicName() {
+        return Optional.of(getIdentifier().name().getLocalName());
+    }
+}
