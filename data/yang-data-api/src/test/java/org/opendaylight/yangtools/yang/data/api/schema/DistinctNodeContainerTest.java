e35234d80fadd11debb505bc6337b4156c0e5dbc
Robert Varga <robert.varga@pantheon.tech>
2023-04-24 13:04:00 +0200

    Add explicit test for getChildArg()
    Make sure the method is tested to assert.
    
    Change-Id: I3e0c3abf4b48e70fce63dd42647b4718cdcea77f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ data/yang-data-api/src/test/java/org/opendaylight/yangtools/yang/data/api/schema/DistinctNodeContainerTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.api.schema;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doCallRealMethod;
+
+import com.google.common.base.VerifyException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+
+@ExtendWith(MockitoExtension.class)
+class ContainerNodeTest {
+    @Mock
+    private ContainerNode container;
+
+    @Test
+    void getChildByArgHasIdentifier() {
+        final var key = new NodeIdentifier(QName.create("foo", "foo"));
+        doCallRealMethod().when(container).getChildByArg(key);
+
+        final var ex = assertThrows(VerifyException.class, () -> container.getChildByArg(key));
+        assertEquals("No child matching (foo)foo", ex.getMessage());
+    }
+}
