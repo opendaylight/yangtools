8be340b95feaf1396efcfcc392f9cdbb92f48402
Robert Varga <robert.varga@pantheon.tech>
2023-05-17 01:36:25 +0200

    WIP: Introduce NormalizedMountPoint(s)
    Switch mount points from being in-line to being out-of-line just as
    NormalizedMetadata is. This allows users to explicitly manage the
    lifecycle -- preventing accidental leaks to NormalizedNode hierarchy.
    
    JIRA: YANGTOOLS-1172
    Change-Id: I523afc166c5532be8db928a3b3288cac266ed3e6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ data/yang-data-util/src/main/java/org/opendaylight/yangtools/yang/data/util/ImmutableNormalizedMountPoint.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.util;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.collect.ImmutableMap;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.rfc8528.model.api.MountPointLabel;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
+import org.opendaylight.yangtools.yang.data.api.schema.MountPointContext;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedMountPoint;
+
+/**
+ * An immutable {@link NormalizedMountPoint}.
+ */
+@NonNullByDefault
+public record ImmutableNormalizedMountPoint(
+        MountPointLabel label,
+        MountPointContext context,
+        ImmutableMap<PathArgument, DataContainerChild> children) implements NormalizedMountPoint {
+    public ImmutableNormalizedMountPoint {
+        requireNonNull(label);
+        requireNonNull(context);
+        requireNonNull(children);
+    }
+}
