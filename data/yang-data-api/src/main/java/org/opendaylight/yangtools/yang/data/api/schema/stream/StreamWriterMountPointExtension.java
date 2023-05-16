b87aa7d94fd79e816b24926187f1820759cdc9b4
Robert Varga <robert.varga@pantheon.tech>
2023-05-16 18:41:09 +0200

    StreamWriterMountPointExtension operates on MountPointLabel
    Disconnect stream support from MountPointIdentifier, so that we can move
    it and evolve it.
    
    JIRA: YANGTOOLS-1172
    Change-Id: I7f6a190575bf4477795c1cc079091d644f966310
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- data/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/stream/StreamWriterMountPointExtension.java
+++ data/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/stream/StreamWriterMountPointExtension.java
@@ -5,14 +5,13 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.yangtools.rfc8528.data.api;
+package org.opendaylight.yangtools.yang.data.api.schema.stream;
 
 import com.google.common.annotations.Beta;
 import java.io.IOException;
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.rfc8528.model.api.MountPointLabel;
 import org.opendaylight.yangtools.yang.data.api.schema.MountPointContext;
-import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
-import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriterExtension;
 
 /**
  * A {@link NormalizedNodeStreamWriterExtension} exposed by stream writers which can handle mount point data, notably
@@ -28,11 +27,10 @@ public interface StreamWriterMountPointExtension extends NormalizedNodeStreamWri
      * returned writer will be {@link NormalizedNodeStreamWriter#close()}d, at which point the parent writer will be
      * used again to emit the rest of the tree.
      *
-     * @param mountId Mount point identifier
+     * @param mountId Mount point label
      * @param mountCtx Mount point context
      * @return A new NormalizedNodeStreamWriter
      * @throws IOException if an error occurs
      */
-    NormalizedNodeStreamWriter startMountPoint(MountPointIdentifier mountId, MountPointContext mountCtx)
-            throws IOException;
+    NormalizedNodeStreamWriter startMountPoint(MountPointLabel label, MountPointContext mountCtx) throws IOException;
 }
