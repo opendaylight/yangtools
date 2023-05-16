a6aeea2ba625b8099eeb68458f75458453d124ad
Robert Varga <robert.varga@pantheon.tech>
2023-05-16 18:55:41 +0200

    MountPointStatement should operate on MountPointLabel
    Having an opaque QName is not exactly friendly, make sure we operate on
    MountPointLabel, so it can be directly picked up.
    
    JIRA: YANGTOOLS-1172
    Change-Id: I1713c76b9919200d18b3390f085d68d9b12e4354
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/rfc8528-parser-support/src/main/java/org/opendaylight/yangtools/rfc8528/parser/MountPointEffectiveStatementImpl.java
+++ parser/rfc8528-parser-support/src/main/java/org/opendaylight/yangtools/rfc8528/parser/MountPointEffectiveStatementImpl.java
@@ -9,6 +9,7 @@ package org.opendaylight.yangtools.rfc8528.parser;
 
 import com.google.common.collect.ImmutableList;
 import org.opendaylight.yangtools.rfc8528.model.api.MountPointEffectiveStatement;
+import org.opendaylight.yangtools.rfc8528.model.api.MountPointLabel;
 import org.opendaylight.yangtools.rfc8528.model.api.MountPointSchemaNode;
 import org.opendaylight.yangtools.rfc8528.model.api.MountPointStatement;
 import org.opendaylight.yangtools.yang.common.QName;
@@ -16,16 +17,17 @@ import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.spi.meta.AbstractEffectiveUnknownSchmemaNode;
 import org.opendaylight.yangtools.yang.parser.spi.meta.EffectiveStmtCtx.Current;
 
-final class MountPointEffectiveStatementImpl extends AbstractEffectiveUnknownSchmemaNode<QName, MountPointStatement>
+final class MountPointEffectiveStatementImpl
+        extends AbstractEffectiveUnknownSchmemaNode<MountPointLabel, MountPointStatement>
         implements MountPointEffectiveStatement, MountPointSchemaNode {
-    MountPointEffectiveStatementImpl(final Current<QName, MountPointStatement> stmt,
+    MountPointEffectiveStatementImpl(final Current<MountPointLabel, MountPointStatement> stmt,
             final ImmutableList<? extends EffectiveStatement<?, ?>> substatements) {
         super(stmt.declared(), stmt.argument(), stmt.history(), substatements);
     }
 
     @Override
     public QName getQName() {
-        return argument();
+        return argument().qname();
     }
 
     @Override
