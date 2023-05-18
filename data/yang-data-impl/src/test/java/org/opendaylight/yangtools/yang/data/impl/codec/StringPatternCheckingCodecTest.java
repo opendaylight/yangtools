b9bf52e9f8b75a56f06b49a75f49bafbb86af3a6
matus.matok <matus.matok@pantheon.tech>
2023-05-18 15:57:18 +0200

    Migrate YANG inputs for yang-data-impl
    Migrated all YANG testfiles counting 50 lines or less.
    
    JIRA: YANGTOOLS-1506
    Change-Id: I0081dd4517f38e59d449170ff9135c8645265be4
    Signed-off-by: matus.matok <matus.matok@pantheon.tech>

--- data/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/codec/StringPatternCheckingCodecTest.java
+++ data/yang-data-impl/src/test/java/org/opendaylight/yangtools/yang/data/impl/codec/StringPatternCheckingCodecTest.java
@@ -22,9 +22,21 @@ import org.opendaylight.yangtools.yang.model.api.LeafSchemaNode;
 import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
 
 public class StringPatternCheckingCodecTest {
+
     @Test
     public void testStringPatternCheckingCodec() {
-        final var schemaContext = YangParserTestUtils.parseYangResource("/string-pattern-checking-codec-test.yang");
+        final var schemaContext = YangParserTestUtils.parseYang("""
+            module string-pattern-checking-codec-test {
+                namespace "string-pattern-checking-codec-test";
+                prefix "spcct";
+                container test-container {
+                    leaf string-leaf-with-valid-pattern {
+                        type string {
+                            pattern "[A-Z]+";
+                        }
+                    }
+                }
+            }""");
         assertNotNull(schemaContext);
 
         final var testModuleQName = QNameModule.create(XMLNamespace.of("string-pattern-checking-codec-test"));
