0d78de9128f282f981d1c266a8d01bbf7ef6b548
Robert Varga <robert.varga@pantheon.tech>
2023-05-18 19:12:31 +0200

    Add NormalizedYangData
    RFC8040 yang-data extension is unlike any other piece of data we
    normalize, as it has its own identifier -- which does not match
    UnresolvedQName.Unqualified, but rather is captured in YangDataName.
    
    This forms the first such piece of data, but realistically we have a
    number of others -- like datastore root, RPC input, RPC output,
    notification -- all of which are now overloaded to ContainerNode.
    
    For now introduce NormalizedData as the construct above NormalizedNode,
    which does not imply a PathArgument being present -- i.e. pulling
    contract(), name() and body() from NormalizedNode.
    
    Add NormalizedYangData, which has its own contract and has a single
    DataContainerChild as its body.
    
    JIRA: YANGTOOLS-1472
    Change-Id: I7ba989b181ac1bdab8117053233c58f5d40a084f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ data/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/NormalizedYangData.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.api.schema;
+
+import org.opendaylight.yangtools.yang.common.YangDataName;
+
+/**
+ * The contents of a {@code yang-data} template instance, as defined in
+ * <a href="https://www.rfc-editor.org/rfc/rfc8040#page-80">RFC8040</a>'s {@code ietf-restconf} module.
+ */
+public interface NormalizedYangData extends NormalizedData {
+    @Override
+    default Class<NormalizedYangData> contract() {
+        return NormalizedYangData.class;
+    }
+
+    @Override
+    YangDataName name();
+
+    @Override
+    DataContainerChild body();
+}
