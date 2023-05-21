fcf067d86bc6df40d70f02c752050ce716b3ade0
Robert Varga <robert.varga@pantheon.tech>
2023-05-21 17:04:04 +0200

    Clean up and deprecate ContainerSchemaNodes
    We have a internal class defined here in two parts. Merge them together.
    Also removing indexing of notification's children --
    NotificationDefinition already provides that functionality.
    
    Finally deprecate the entire class, as OperationDefinition and
    NotificationDefinition already provide the contracts here -- but make a
    note on compatibility.
    
    Change-Id: Ia8164a8900a827bb07a47b4899551e343cddc48b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- data/yang-data-util/src/main/java/org/opendaylight/yangtools/yang/data/util/ContainerSchemaNodes.java
+++ data/yang-data-util/src/main/java/org/opendaylight/yangtools/yang/data/util/ContainerSchemaNodes.java
@@ -9,11 +9,8 @@ package org.opendaylight.yangtools.yang.data.util;
 
 import static java.util.Objects.requireNonNull;
 
-import com.google.common.annotations.Beta;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Maps;
 import java.util.Collection;
 import java.util.Optional;
 import org.eclipse.jdt.annotation.NonNull;
@@ -25,8 +22,9 @@ import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.GroupingDefinition;
 import org.opendaylight.yangtools.yang.model.api.MustDefinition;
 import org.opendaylight.yangtools.yang.model.api.NotificationDefinition;
+import org.opendaylight.yangtools.yang.model.api.NotificationNodeContainer;
+import org.opendaylight.yangtools.yang.model.api.OperationDefinition;
 import org.opendaylight.yangtools.yang.model.api.RpcDefinition;
-import org.opendaylight.yangtools.yang.model.api.SchemaNode;
 import org.opendaylight.yangtools.yang.model.api.Status;
 import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
 import org.opendaylight.yangtools.yang.model.api.UsesNode;
@@ -36,26 +34,29 @@ import org.opendaylight.yangtools.yang.xpath.api.YangXPathExpression.QualifiedBo
  * Utility class for taking notification or rpc as ContainerSchemaNode.
  *
  * @author <a href="mailto:geng.xingyuan@zte.com.cn">Geng Xingyuan</a>
+ * @deprecated This class is deprecated for removal. Use {@link NotificationDefinition#toContainerLike()} and
+ *             {@link OperationDefinition#toContainerLike()} to can similar service. Note that the former differs from
+ *             {@link #forNotification(NotificationDefinition)} in that it does not report the source notification from
+ *             {@link NotificationNodeContainer#getNotifications()} -- i.e. it does not contain itself.
  */
+@Deprecated(since = "11.0.0", forRemoval = true)
 public final class ContainerSchemaNodes {
     private ContainerSchemaNodes() {
         // Hidden on purpose
     }
 
-    @Beta
     public static @NonNull ContainerLike forNotification(final NotificationDefinition notification) {
         return new NotificationContainerSchemaNode(notification);
     }
 
-    @Beta
     public static @NonNull ContainerLike forRPC(final RpcDefinition rpc) {
         return rpc.toContainerLike();
     }
 
-    private abstract static class AbstractContainerSchemaNode<T extends SchemaNode> implements ContainerLike {
-        final @NonNull T schemaNode;
+    private static final class NotificationContainerSchemaNode implements ContainerLike {
+        final @NonNull NotificationDefinition schemaNode;
 
-        AbstractContainerSchemaNode(final T schemaNode) {
+        NotificationContainerSchemaNode(final NotificationDefinition schemaNode) {
             this.schemaNode = requireNonNull(schemaNode);
         }
 
@@ -115,16 +116,6 @@ public final class ContainerSchemaNodes {
         public Optional<? extends QualifiedBound> getWhenCondition() {
             return Optional.empty();
         }
-    }
-
-    private static final class NotificationContainerSchemaNode
-            extends AbstractContainerSchemaNode<NotificationDefinition> {
-        private final ImmutableMap<QName, ? extends DataSchemaNode> mapNodes;
-
-        private NotificationContainerSchemaNode(final NotificationDefinition notification) {
-            super(notification);
-            mapNodes = Maps.uniqueIndex(notification.getChildNodes(), DataSchemaNode::getQName);
-        }
 
         @Override
         public Collection<? extends NotificationDefinition> getNotifications() {
@@ -153,7 +144,7 @@ public final class ContainerSchemaNodes {
 
         @Override
         public DataSchemaNode dataChildByName(final QName name) {
-            return mapNodes.get(requireNonNull(name));
+            return schemaNode.dataChildByName(name);
         }
     }
 }
