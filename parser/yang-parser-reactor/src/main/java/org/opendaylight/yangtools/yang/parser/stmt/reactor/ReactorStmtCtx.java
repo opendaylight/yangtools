708333366aa33fdf1679c83952b77ecfb28459fe
Robert Varga <robert.varga@pantheon.tech>
2023-06-03 09:58:53 +0200

    Enforce isSupportedByFeatures() availability
    The check for supported features is only available once the statement
    has been fully declared. Enforce that invariant.
    
    // FIXME: assign issue
    // FIXME: finish up
    
    Change-Id: Ic7f3646794fca4de0f883e531bd16a256cc78e15
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/ReactorStmtCtx.java
+++ parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/ReactorStmtCtx.java
@@ -473,6 +473,11 @@ abstract class ReactorStmtCtx<A, D extends DeclaredStatement<A>, E extends Effec
 
     @Override
     public final boolean isSupportedByFeatures() {
+        final byte order = executionOrder();
+        if (order < ExecutionOrder.FULL_DECLARATION) {
+            throw new VerifyException("Attempted to examine features during " + order);
+        }
+
         final int fl = flags & SET_SUPPORTED_BY_FEATURES;
         if (fl != 0) {
             return fl == SET_SUPPORTED_BY_FEATURES;
