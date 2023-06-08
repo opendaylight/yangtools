95fd15c0b0a0190f95db9da76d66546076328420
Robert Varga <robert.varga@pantheon.tech>
2023-06-08 16:49:49 +0200

    Split out NormalizedContainer
    In order to support a consistent surface between ContainerNode and
    NormalizedYangData (which really serve a similar purpose), we need to
    make sure we have a common concept capturing NormalizedNodeContainer
    without implying addressability by PathArgument.
    
    This introduces DataContainer as a superclass of DataContainerNode,
    along with NormalizedContainer, DistinctContainer and OrderedContainer
    to support the attributes.
    
    JIRA: YANGTOOLS-1472
    Change-Id: Ida94334c0e8b7a776befffce62096a659ff3110b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- data/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/NormalizedNodeContainer.java
+++ data/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/NormalizedNodeContainer.java
@@ -7,9 +7,6 @@
  */
 package org.opendaylight.yangtools.yang.data.api.schema;
 
-import java.util.Collection;
-import org.eclipse.jdt.annotation.NonNull;
-
 /**
  * Node which is not leaf, but has child {@link NormalizedNode}s as its value. It provides iteration over its child
  * nodes via {@link #body()}. More convenient access to child nodes are provided by {@link DistinctNodeContainer} and
@@ -17,32 +14,7 @@ import org.eclipse.jdt.annotation.NonNull;
  *
  * @param <V> Child Node type
  */
-public sealed interface NormalizedNodeContainer<V extends NormalizedNode> extends NormalizedNode, OrderingAware
+public sealed interface NormalizedNodeContainer<V extends NormalizedNode> extends NormalizedContainer<V>, NormalizedNode
         permits DistinctNodeContainer, OrderedNodeContainer {
-    /**
-     * {@inheritDoc}
-     *
-     * <p>
-     * Returns iteration of all child nodes. Order of returned child nodes may be defined by subinterfaces.
-     */
-    @Override
-    Collection<@NonNull V> body();
-
-    /**
-     * Return the logical size of this container body. The default implementation defers to {@code body().size()}.
-     *
-     * @return Size of this container's body.
-     */
-    default int size() {
-        return body().size();
-    }
-
-    /**
-     * Determine whether this container body is empty. The default implementation defers to {@code body().isEmpty()}.
-     *
-     * @return True if this container has an empty body.
-     */
-    default boolean isEmpty() {
-        return body().isEmpty();
-    }
+    // Just a composite of NormalizedContainer and NormalizedNode
 }
