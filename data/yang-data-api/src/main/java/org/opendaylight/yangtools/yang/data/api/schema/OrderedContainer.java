2c38fe71ba2234f6b0ee4b6bf8484ebd81a0588b
Robert Varga <robert.varga@pantheon.tech>
2023-06-08 16:49:49 +0200

    Split out NormalizedContainer
    In order to support a consistent surface between ContainerNode and
    NormalizedYangData (which really serve a similar purpose), we need to
    make sure we have a common concept capturing NormalizedNodeContainer
    without implying addressability by PathArgument.
    
    This introduces DataContainer as a superclass of DataContainerNode,
    along with NormalizedContainer, DistinctContainer and OrderedContainer
    to support the attributes.
    
    JIRA: YANGTOOLS-1472
    Change-Id: Ida94334c0e8b7a776befffce62096a659ff3110b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ data/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/OrderedContainer.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.api.schema;
+
+import org.eclipse.jdt.annotation.NonNull;
+
+/**
+ * A {@link NormalizedContainer} which preserves user supplied ordering and allows addressing of child elements by
+ * position/
+ *
+ * @param <V> child type
+ */
+public sealed interface OrderedContainer<V extends NormalizedNode> extends NormalizedContainer<V>
+        permits OrderedNodeContainer {
+    /**
+     * Returns child node by position.
+     *
+     * @param position Position of child node
+     * @return Child Node
+     * @throws IndexOutOfBoundsException Out of bound Exception
+     */
+    @NonNull V childAt(int position);
+
+    @Override
+    int hashCode();
+
+    @Override
+    boolean equals(Object obj);
+}
