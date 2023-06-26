807d21ed50daab788d7117fffdf088d9dd8eb596
Robert Varga <robert.varga@pantheon.tech>
2023-06-26 13:48:45 +0200

    Do not require spotbugs transitively
    Clean up dependencies, eliminating unused declared dependencies on
    spotbugs-annotations. Also clean up dependency confusion between
    mdsal-binding-dom-adapter and mdsal-binding-test-utils.
    
    Change-Id: Iaa6681c3e53ca8b9bd9e63abecf199b316fa9b4d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-runtime-osgi/src/main/java/module-info.java
+++ binding/mdsal-binding-runtime-osgi/src/main/java/module-info.java
@@ -18,7 +18,6 @@ module org.opendaylight.mdsal.binding.runtime.osgi {
     requires org.slf4j;
 
     // Annotations
-    requires static com.github.spotbugs.annotations;
     requires static org.checkerframework.checker.qual;
     requires static org.eclipse.jdt.annotation;
     requires static org.osgi.service.component.annotations;
