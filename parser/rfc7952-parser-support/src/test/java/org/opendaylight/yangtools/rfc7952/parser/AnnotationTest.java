547d8b4d1d273e5a9bd9a93a0d4bdcd1fbb88059
matus.matok <matus.matok@pantheon.tech>
2023-07-13 10:22:34 +0200

    Migrate rfc7952-parser-support to JUnit5
    Migrated all tests to use JUnit5 Assertions, using openrewrite:rewrite-testing-frameworks.
    
    JIRA: YANGTOOLS-1521
    Change-Id: I5bdd6507cb01198a5a57b9b553eaf59e679f6d71
    Signed-off-by: matus.matok <matus.matok@pantheon.tech>

--- parser/rfc7952-parser-support/src/test/java/org/opendaylight/yangtools/rfc7952/parser/AnnotationTest.java
+++ parser/rfc7952-parser-support/src/test/java/org/opendaylight/yangtools/rfc7952/parser/AnnotationTest.java
@@ -7,9 +7,9 @@
  */
 package org.opendaylight.yangtools.rfc7952.parser;
 
-import static org.junit.Assert.assertSame;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
