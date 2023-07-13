e8f4ee65200f225bf5d0f72dde266c2dab2044bd
matus.matok <matus.matok@pantheon.tech>
2023-07-13 10:03:57 +0200

    Migrate odlext-parser-support to JUnit5
    Migrated all tests to use JUnit5 Assertions, using
    openrewrite:rewrite-testing-frameworks. Also modernize the tests a bit
    and remove unnecessary initialization.
    
    JIRA: YANGTOOLS-1521
    Change-Id: Ibedf49de7440857c66393db20ec7f1fe6d0a0429
    Signed-off-by: matus.matok <matus.matok@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/odlext-parser-support/src/test/java/org/opendaylight/yangtools/odlext/parser/MountTest.java
+++ parser/odlext-parser-support/src/test/java/org/opendaylight/yangtools/odlext/parser/MountTest.java
@@ -7,58 +7,41 @@
  */
 package org.opendaylight.yangtools.odlext.parser;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Optional;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.opendaylight.yangtools.odlext.model.api.MountEffectiveStatement;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.common.XMLNamespace;
-import org.opendaylight.yangtools.yang.model.api.stmt.ModuleEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.repo.api.YangTextSchemaSource;
 import org.opendaylight.yangtools.yang.parser.api.YangParserConfiguration;
 import org.opendaylight.yangtools.yang.parser.rfc7950.reactor.RFC7950Reactors;
 import org.opendaylight.yangtools.yang.parser.rfc7950.repo.YangStatementStreamSource;
 import org.opendaylight.yangtools.yang.parser.spi.meta.ModelProcessingPhase;
-import org.opendaylight.yangtools.yang.parser.stmt.reactor.CrossSourceStatementReactor;
 
-public class MountTest {
+class MountTest {
     private static final QNameModule FOO = QNameModule.create(XMLNamespace.of("foo"));
 
-    private static CrossSourceStatementReactor reactor;
-
-    @BeforeClass
-    public static void createReactor() {
-        reactor = RFC7950Reactors.vanillaReactorBuilder()
+    @Test
+    void test() throws Exception {
+        final var reactor = RFC7950Reactors.vanillaReactorBuilder()
             .addStatementSupport(ModelProcessingPhase.FULL_DECLARATION,
                 new MountStatementSupport(YangParserConfiguration.DEFAULT))
             .build();
-    }
-
-    @AfterClass
-    public static void freeReactor() {
-        reactor = null;
-    }
-
-    @Test
-    public void test() throws Exception {
-        final ModuleEffectiveStatement foo = reactor.newBuild()
+        final var foo = reactor.newBuild()
             .addSource(YangStatementStreamSource.create(YangTextSchemaSource.forResource("/yang-ext.yang")))
             .addSource(YangStatementStreamSource.create(YangTextSchemaSource.forResource("/mount.yang")))
             .buildEffective()
             .getModuleStatements()
             .get(FOO);
 
-        final Optional<MountEffectiveStatement> fooMount = foo.findDataTreeNode(QName.create(FOO, "foo")).orElseThrow()
-            .findFirstEffectiveSubstatement(MountEffectiveStatement.class);
-        assertTrue(fooMount.isPresent());
+        assertTrue(foo.findDataTreeNode(QName.create(FOO, "foo")).orElseThrow()
+            .findFirstEffectiveSubstatement(MountEffectiveStatement.class)
+            .isPresent());
 
-        final Optional<MountEffectiveStatement> barMount = foo.findDataTreeNode(QName.create(FOO, "bar")).orElseThrow()
-            .findFirstEffectiveSubstatement(MountEffectiveStatement.class);
-        assertTrue(barMount.isPresent());
-
+        assertTrue(foo.findDataTreeNode(QName.create(FOO, "bar")).orElseThrow()
+            .findFirstEffectiveSubstatement(MountEffectiveStatement.class)
+            .isPresent());
     }
 }
