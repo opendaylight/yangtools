fbe2414e978c21be653c522217f455e99c77f6a1
matus.matok <matus.matok@pantheon.tech>
2023-07-13 10:15:12 +0200

    Migrate rfc6536-parser-support to JUnit5
    Migrated all tests to use JUnit5 Assertions, using openrewrite:rewrite-testing-frameworks.
    
    JIRA: YANGTOOLS-1521
    Change-Id: I6c0fe859120a3f8a4319b92cbd4114c522912c91
    Signed-off-by: matus.matok <matus.matok@pantheon.tech>

--- parser/rfc6536-parser-support/src/test/java/org/opendaylight/yangtools/rfc6536/parser/NACMTest.java
+++ parser/rfc6536-parser-support/src/test/java/org/opendaylight/yangtools/rfc6536/parser/NACMTest.java
@@ -7,8 +7,8 @@
  */
 package org.opendaylight.yangtools.rfc6536.parser;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
