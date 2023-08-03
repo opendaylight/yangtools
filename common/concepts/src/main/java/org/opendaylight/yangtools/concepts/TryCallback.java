6784025f6133cf28e13765c9c4d61eb19f77cc93
Robert Varga <robert.varga@pantheon.tech>
2023-08-04 00:18:15 +0200

    Add Try(Callback)
    Add a usability improvement to mirror Futures.addCallback().
    
    // FIXME: add an issue
    
    Change-Id: I302c50007ab7d5cba12c2cfafff6c72c183da2bf
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ common/concepts/src/main/java/org/opendaylight/yangtools/concepts/TryCallback.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.concepts;
+
+import com.google.common.annotations.Beta;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.concurrent.Executor;
+import org.eclipse.jdt.annotation.NonNull;
+
+/**
+ * A {@link FutureCallback} exposing a {@link Try}.
+ *
+ * @param <V> Value type
+ */
+@Beta
+@FunctionalInterface
+public interface TryCallback<V> extends FutureCallback<V> {
+    /**
+     * Add a {@link TryCallback} to a {@link ListenableFuture}. This provides a superior API to
+     * {@link Futures#addCallback(ListenableFuture, FutureCallback, Executor)}, as {@link TryCallback} offers a guarded
+     * API around {@link Try}: users have to check failure conditions before accessing the resulting value.
+     *
+     * <p>
+     * This method provides a safer alternative to
+     * {@link Futures#addCallback(ListenableFuture, FutureCallback, Executor)} while imposing minor runtime overhead,
+     * which should be discovered easily. Even if not, the additional access safety offered by {@link Try} in and of
+     * itself is a justification for its user.
+     *
+     * @apiNote
+     *     Apparent mis-order with {@link Futures#addCallback(ListenableFuture, FutureCallback, Executor)} is quite
+     *     intentional.
+     *
+     * @param future {@link ListenableFuture} potentially producing the value
+     * @param callback {@link TryCallback} to execute when {@code future} completes
+     * @param executor {@link Executor} to run {@code callback} on
+     * @throws NullPointerException if any argument is {@code null}
+     */
+    static <V> void addCallback(final ListenableFuture<V> future, final TryCallback<V> callback,
+            final Executor executor) {
+        Futures.addCallback(future, callback, executor);
+    }
+
+    void onComplete(@NonNull Try<V> tryResult);
+
+    /**
+     * {@inheritDoc}
+     *
+     * @implSpec Default implementation performs {@code onComplete(Try.of(result))} and should never be overridden.
+     */
+    @Override
+    default void onSuccess(final V result) {
+        onComplete(Try.of(result));
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @implSpec Default implementation performs {@code onComplete(Try.failed(cause))} and should never be overridden.
+     */
+    @Override
+    default void onFailure(final Throwable cause) {
+        onComplete(Try.failed(cause));
+    }
+}
