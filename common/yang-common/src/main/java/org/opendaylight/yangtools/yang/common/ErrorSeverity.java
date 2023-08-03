784e8ba06b994d4a03ac152dba7632f301efd2e0
Robert Varga <robert.varga@pantheon.tech>
2023-08-03 17:54:28 +0200

    Use switch expression in forElementBody()
    We have an utterly simple dispatch here. Eliminate the use of an
    ImmutableMap and just make it a switch expression.
    
    Change-Id: I3cfbfe2ed183b69cad35edc1abc129acb07c93a7
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- common/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/ErrorSeverity.java
+++ common/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/ErrorSeverity.java
@@ -9,9 +9,6 @@ package org.opendaylight.yangtools.yang.common;
 
 import static java.util.Objects.requireNonNull;
 
-import com.google.common.collect.Maps;
-import java.util.Arrays;
-import java.util.Map;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 
@@ -30,13 +27,10 @@ public enum ErrorSeverity {
      */
     WARNING("warning");
 
-    private static final Map<String, ErrorSeverity> BY_ELEMENT_BODY =
-        Maps.uniqueIndex(Arrays.asList(values()), ErrorSeverity::elementBody);
-
     private final String elementBody;
 
-    ErrorSeverity(final String elementName) {
-        this.elementBody = requireNonNull(elementName);
+    ErrorSeverity(final String elementBody) {
+        this.elementBody = requireNonNull(elementBody);
     }
 
     /**
@@ -49,6 +43,10 @@ public enum ErrorSeverity {
     }
 
     public static @Nullable ErrorSeverity forElementBody(final String elementBody) {
-        return BY_ELEMENT_BODY.get(requireNonNull(elementBody));
+        return switch (elementBody) {
+            case "error" -> ERROR;
+            case "warning" -> WARNING;
+            default -> null;
+        };
     }
 }
