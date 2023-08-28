d614f302532641396e54b12bd20a69fe5485bc9b
Robert Varga <robert.varga@pantheon.tech>
2023-08-28 13:15:55 +0200

    Hide XmlCodecFactory internals
    We are getting warnings about leaking XmlCodec. While this is not
    exactly true, mitigate these warnings by splitting the real logic
    into a package-private class while retaining the same external
    XmlCodecFactory facade.
    
    JIRA: YANGTOOLS-1534
    Change-Id: I719cf82f00d4580505d92d7126be6d2c09c26a4b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit db06703aa3c1c68d9481b5823c16a570a23dfb6e)

--- codec/yang-data-codec-xml/src/main/java/org/opendaylight/yangtools/yang/data/codec/xml/XmlCodecFactory.java
+++ codec/yang-data-codec-xml/src/main/java/org/opendaylight/yangtools/yang/data/codec/xml/XmlCodecFactory.java
@@ -8,58 +8,18 @@
 
 package org.opendaylight.yangtools.yang.data.codec.xml;
 
-import static java.util.Objects.requireNonNull;
-
 import com.google.common.annotations.Beta;
-import java.util.List;
 import org.opendaylight.yangtools.rfc8528.data.api.MountPointContext;
 import org.opendaylight.yangtools.rfc8528.data.util.EmptyMountPointContext;
-import org.opendaylight.yangtools.yang.common.QNameModule;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
-import org.opendaylight.yangtools.yang.data.impl.codec.AbstractIntegerStringCodec;
-import org.opendaylight.yangtools.yang.data.impl.codec.BinaryStringCodec;
-import org.opendaylight.yangtools.yang.data.impl.codec.BitsStringCodec;
-import org.opendaylight.yangtools.yang.data.impl.codec.BooleanStringCodec;
-import org.opendaylight.yangtools.yang.data.impl.codec.DecimalStringCodec;
-import org.opendaylight.yangtools.yang.data.impl.codec.EnumStringCodec;
-import org.opendaylight.yangtools.yang.data.impl.codec.StringStringCodec;
-import org.opendaylight.yangtools.yang.data.util.codec.AbstractCodecFactory;
-import org.opendaylight.yangtools.yang.data.util.codec.SharedCodecCache;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
-import org.opendaylight.yangtools.yang.model.api.type.BinaryTypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.BitsTypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.BooleanTypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.DecimalTypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.EmptyTypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.EnumTypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.IdentityrefTypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.InstanceIdentifierTypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.Int16TypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.Int32TypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.Int64TypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.Int8TypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.StringTypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.Uint16TypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.Uint32TypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.Uint64TypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.Uint8TypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.UnionTypeDefinition;
-import org.opendaylight.yangtools.yang.model.api.type.UnknownTypeDefinition;
 
 /**
- * A thread-safe factory for instantiating {@link XmlCodec}s.
+ * A thread-safe factory for instantiating XML codecs.
  */
 @Beta
-public final class XmlCodecFactory extends AbstractCodecFactory<XmlCodec<?>> {
-    private final MountPointContext mountCtx;
-
+public final class XmlCodecFactory extends CodecFactory {
     private XmlCodecFactory(final MountPointContext mountCtx) {
-        super(mountCtx.getEffectiveModelContext(), new SharedCodecCache<>());
-        this.mountCtx = requireNonNull(mountCtx);
-    }
-
-    MountPointContext mountPointContext() {
-        return mountCtx;
+        super(mountCtx);
     }
 
     /**
@@ -80,101 +40,5 @@ public final class XmlCodecFactory extends AbstractCodecFactory<XmlCodec<?>> {
      */
     public static XmlCodecFactory create(final EffectiveModelContext context) {
         return create(new EmptyMountPointContext(context));
-    }
-
-    @Override
-    protected XmlCodec<?> binaryCodec(final BinaryTypeDefinition type) {
-        return new QuotedXmlCodec<>(BinaryStringCodec.from(type));
-    }
-
-    @Override
-    protected XmlCodec<?> booleanCodec(final BooleanTypeDefinition type) {
-        return new BooleanXmlCodec(BooleanStringCodec.from(type));
-    }
-
-    @Override
-    protected XmlCodec<?> bitsCodec(final BitsTypeDefinition type) {
-        return new QuotedXmlCodec<>(BitsStringCodec.from(type));
-    }
-
-    @Override
-    protected XmlCodec<?> emptyCodec(final EmptyTypeDefinition type) {
-        return EmptyXmlCodec.INSTANCE;
-    }
-
-    @Override
-    protected XmlCodec<?> enumCodec(final EnumTypeDefinition type) {
-        return new QuotedXmlCodec<>(EnumStringCodec.from(type));
-    }
-
-    @Override
-    protected XmlCodec<?> identityRefCodec(final IdentityrefTypeDefinition type, final QNameModule module) {
-        return new IdentityrefXmlCodec(getEffectiveModelContext(), module);
-    }
-
-    @Override
-    protected XmlCodec<YangInstanceIdentifier> instanceIdentifierCodec(final InstanceIdentifierTypeDefinition type) {
-        return new XmlStringInstanceIdentifierCodec(getEffectiveModelContext(), this);
-    }
-
-    @Override
-    protected XmlCodec<?> int8Codec(final Int8TypeDefinition type) {
-        return new NumberXmlCodec<>(AbstractIntegerStringCodec.from(type));
-    }
-
-    @Override
-    protected XmlCodec<?> int16Codec(final Int16TypeDefinition type) {
-        return new NumberXmlCodec<>(AbstractIntegerStringCodec.from(type));
-    }
-
-    @Override
-    protected XmlCodec<?> int32Codec(final Int32TypeDefinition type) {
-        return new NumberXmlCodec<>(AbstractIntegerStringCodec.from(type));
-    }
-
-    @Override
-    protected XmlCodec<?> int64Codec(final Int64TypeDefinition type) {
-        return new NumberXmlCodec<>(AbstractIntegerStringCodec.from(type));
-    }
-
-    @Override
-    protected XmlCodec<?> decimalCodec(final DecimalTypeDefinition type) {
-        return new NumberXmlCodec<>(DecimalStringCodec.from(type));
-    }
-
-    @Override
-    protected XmlCodec<?> stringCodec(final StringTypeDefinition type) {
-        // FIXME: YANGTOOLS-1523: use QuotedXmlCodec
-        return new StringXmlCodec(StringStringCodec.from(type));
-    }
-
-    @Override
-    protected XmlCodec<?> uint8Codec(final Uint8TypeDefinition type) {
-        return new NumberXmlCodec<>(AbstractIntegerStringCodec.from(type));
-    }
-
-    @Override
-    protected XmlCodec<?> uint16Codec(final Uint16TypeDefinition type) {
-        return new NumberXmlCodec<>(AbstractIntegerStringCodec.from(type));
-    }
-
-    @Override
-    protected XmlCodec<?> uint32Codec(final Uint32TypeDefinition type) {
-        return new NumberXmlCodec<>(AbstractIntegerStringCodec.from(type));
-    }
-
-    @Override
-    protected XmlCodec<?> uint64Codec(final Uint64TypeDefinition type) {
-        return new NumberXmlCodec<>(AbstractIntegerStringCodec.from(type));
-    }
-
-    @Override
-    protected XmlCodec<?> unionCodec(final UnionTypeDefinition type, final List<XmlCodec<?>> codecs) {
-        return UnionXmlCodec.create(type, codecs);
-    }
-
-    @Override
-    protected XmlCodec<?> unknownCodec(final UnknownTypeDefinition type) {
-        return NullXmlCodec.INSTANCE;
     }
 }
