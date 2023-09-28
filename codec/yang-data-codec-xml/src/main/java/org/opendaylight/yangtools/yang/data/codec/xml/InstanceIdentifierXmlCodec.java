30626b853551808f356dc1e21bc891ecf9cf0296
Robert Varga <robert.varga@pantheon.tech>
2023-09-28 19:39:39 +0200

    Clean up XML instance identifier class naming
    Naming of things here is not great. We have two distinct implementations
    of a full Codec, one of which can handle serialization and the other can
    handle parsing.
    
    Clean up naming, by having a top-level, fixed,
    InstanceIdentifierXmlCodec, which is what we give users. We then have
    InstanceIdentifier{Deserializer,Serializer}, which are bound, single-use
    delegates.
    
    This allows us to ditch the thread local stack we have been keeping to
    deal with the fact that the implementation is directly internal.
    
    Furthermore we optimize instantiation, so that we minimize the number of
    calls to DataSchemaContextTree.from().
    
    JIRA: YANGTOOLS-1543
    Change-Id: I1ca884fe57086311111f12b3ba223d08f3676a93
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 99715f7b5483c08f7fe4ec9232281ea04a3941b2)

---
+++ codec/yang-data-codec-xml/src/main/java/org/opendaylight/yangtools/yang/data/codec/xml/InstanceIdentifierXmlCodec.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.codec.xml;
+
+import static java.util.Objects.requireNonNull;
+
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.util.DataSchemaContextTree;
+
+final class InstanceIdentifierXmlCodec implements XmlCodec<YangInstanceIdentifier> {
+    private final @NonNull XmlCodecFactory codecFactory;
+    private final DataSchemaContextTree dataContextTree;
+
+    InstanceIdentifierXmlCodec(final XmlCodecFactory codecFactory) {
+        this.codecFactory = requireNonNull(codecFactory);
+        dataContextTree = DataSchemaContextTree.from(codecFactory.getEffectiveModelContext());
+    }
+
+    @Override
+    public Class<YangInstanceIdentifier> getDataType() {
+        return YangInstanceIdentifier.class;
+    }
+
+    @Override
+    public YangInstanceIdentifier parseValue(final NamespaceContext ctx, final String str) {
+        // FIXME: YANGTOOLS-1523: do not trim()
+        return new InstanceIdentifierDeserializer(dataContextTree, codecFactory, ctx).deserialize(str.trim());
+    }
+
+    @Override
+    public void writeValue(final XMLStreamWriter ctx, final YangInstanceIdentifier value) throws XMLStreamException {
+        final var serializer = new InstanceIdentifierSerializer(dataContextTree, ctx.getNamespaceContext());
+
+        final String str;
+        try {
+            str = serializer.serialize(value);
+        } catch (IllegalArgumentException e) {
+            throw new XMLStreamException("Failed to encode instance-identifier", e);
+        }
+        for (var entry : serializer.getPrefixes()) {
+            ctx.writeNamespace(entry.getValue(), entry.getKey().toString());
+        }
+        ctx.writeCharacters(str);
+    }
+}
