c3b177c1bde065d8ccee8784f88ea6c23dac173a
Robert Varga <robert.varga@pantheon.tech>
2023-09-28 19:39:39 +0200

    Clean up XML instance identifier class naming
    Naming of things here is not great. We have two distinct implementations
    of a full Codec, one of which can handle serialization and the other can
    handle parsing.
    
    Clean up naming, by having a top-level, fixed,
    InstanceIdentifierXmlCodec, which is what we give users. We then have
    InstanceIdentifier{Deserializer,Serializer}, which are bound, single-use
    delegates.
    
    This allows us to ditch the thread local stack we have been keeping to
    deal with the fact that the implementation is directly internal.
    
    Furthermore we optimize instantiation, so that we minimize the number of
    calls to DataSchemaContextTree.from().
    
    JIRA: YANGTOOLS-1543
    Change-Id: I1ca884fe57086311111f12b3ba223d08f3676a93
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 99715f7b5483c08f7fe4ec9232281ea04a3941b2)
    (cherry picked from commit 3f51d57d3473a02afd1b7cbaf38190e2297fb156)

---
+++ codec/yang-data-codec-xml/src/main/java/org/opendaylight/yangtools/yang/data/codec/xml/AbstractInstanceIdentifierCodec.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.codec.xml;
+
+import static java.util.Objects.requireNonNull;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.data.util.AbstractModuleStringInstanceIdentifierCodec;
+import org.opendaylight.yangtools.yang.data.util.DataSchemaContextTree;
+
+abstract sealed class AbstractInstanceIdentifierCodec extends AbstractModuleStringInstanceIdentifierCodec
+        permits InstanceIdentifierDeserializer, InstanceIdentifierSerializer {
+    private final @NonNull DataSchemaContextTree dataContextTree;
+
+    AbstractInstanceIdentifierCodec(final DataSchemaContextTree dataContextTree) {
+        this.dataContextTree = requireNonNull(dataContextTree);
+    }
+
+    @Override
+    protected final DataSchemaContextTree getDataContextTree() {
+        return dataContextTree;
+    }
+}
---
+++ codec/yang-data-codec-xml/src/main/java/org/opendaylight/yangtools/yang/data/codec/xml/InstanceIdentifierSerializer.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.codec.xml;
+
+import java.util.Map.Entry;
+import javax.xml.namespace.NamespaceContext;
+import org.opendaylight.yangtools.yang.common.XMLNamespace;
+import org.opendaylight.yangtools.yang.data.util.DataSchemaContextTree;
+import org.opendaylight.yangtools.yang.model.api.Module;
+
+final class InstanceIdentifierSerializer extends AbstractInstanceIdentifierCodec {
+    private final RandomPrefix prefixes;
+
+    InstanceIdentifierSerializer(final DataSchemaContextTree dataContextTree, final NamespaceContext nsContext) {
+        super(dataContextTree);
+        prefixes = new RandomPrefix(nsContext);
+    }
+
+    Iterable<Entry<XMLNamespace, String>> getPrefixes() {
+        return prefixes.getPrefixes();
+    }
+
+    @Override
+    protected Module moduleForPrefix(final String prefix) {
+        // This is deserialize() path, we do not support that in this class
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    protected String prefixForNamespace(final XMLNamespace namespace) {
+        return prefixes.encodePrefix(namespace);
+    }
+}
