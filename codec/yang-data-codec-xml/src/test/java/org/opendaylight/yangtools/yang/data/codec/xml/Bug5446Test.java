1678a82af9cfcc419a486db87b7047633a83d7b8
Robert Varga <robert.varga@pantheon.tech>
2023-09-29 21:54:51 +0200

    Clean up Bug5446Test
    Remove boilerplate around loading of resources by just using a text
    block.
    
    Change-Id: I4d423e014279c86a1db0a710f30814fc451d7731
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- codec/yang-data-codec-xml/src/test/java/org/opendaylight/yangtools/yang/data/codec/xml/Bug5446Test.java
+++ codec/yang-data-codec-xml/src/test/java/org/opendaylight/yangtools/yang/data/codec/xml/Bug5446Test.java
@@ -7,62 +7,30 @@
  */
 package org.opendaylight.yangtools.yang.data.codec.xml;
 
-import static java.util.Objects.requireNonNull;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.StringWriter;
 import java.util.Base64;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamWriter;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.TransformerFactoryConfigurationError;
-import javax.xml.transform.dom.DOMResult;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import org.custommonkey.xmlunit.XMLAssert;
-import org.custommonkey.xmlunit.XMLUnit;
 import org.junit.Test;
-import org.opendaylight.yangtools.util.xml.UntrustedXML;
 import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.common.QNameModule;
-import org.opendaylight.yangtools.yang.common.Revision;
-import org.opendaylight.yangtools.yang.common.XMLNamespace;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
-import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
-import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
-import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
-import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
 import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeWriter;
 import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
-import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.xml.sax.SAXException;
+import org.xmlunit.builder.DiffBuilder;
 
 public class Bug5446Test {
-    private static final QNameModule FOO_MODULE = QNameModule.create(XMLNamespace.of("foo"), Revision.of("2015-11-05"));
-    private static final QName ROOT_QNAME = QName.create(FOO_MODULE, "root");
-    private static final QName IP_ADDRESS_QNAME = QName.create(FOO_MODULE, "ip-address");
-
     @Test
     public void test() throws Exception {
-        final EffectiveModelContext schemaContext = YangParserTestUtils.parseYang("""
+        final var modelContext = YangParserTestUtils.parseYang("""
             module foo {
               yang-version 1;
               namespace "foo";
               prefix "foo";
 
-              revision "2015-11-05" {
-              }
+              revision "2015-11-05"
 
               typedef ipv4-address-binary {
                 type binary {
@@ -89,98 +57,30 @@ public class Bug5446Test {
                 }
               }
             }""");
-        final Document doc = loadDocument("/bug5446/xml/foo.xml");
 
-        final ContainerNode docNode = createDocNode();
+        final var decoded = Base64.getDecoder().decode("fwAAAQ==");
+        assertEquals("fwAAAQ==", Base64.getEncoder().encodeToString(decoded));
 
-        DataContainerChild root = docNode.getChildByArg(new NodeIdentifier(ROOT_QNAME));
-        DataContainerChild child = ((ContainerNode) root).getChildByArg(new NodeIdentifier(IP_ADDRESS_QNAME));
-        LeafNode<?> ipAdress = (LeafNode<?>) child;
+        final var sw = new StringWriter();
+        try (var nnsw = XMLStreamNormalizedNodeStreamWriter.create(
+                TestFactories.DEFAULT_OUTPUT_FACTORY.createXMLStreamWriter(sw), modelContext)) {
+            NormalizedNodeWriter.forStreamWriter(nnsw)
+                .write(Builders.containerBuilder()
+                    .withNodeIdentifier(new NodeIdentifier(QName.create("foo", "2015-11-05", "root")))
+                    .withChild(ImmutableNodes.leafNode(QName.create("foo", "2015-11-05", "ip-address"), decoded))
+                    .build())
+                .flush();
+        }
 
-        Object value = ipAdress.body();
-        assertTrue(value instanceof byte[]);
-        assertEquals("fwAAAQ==", Base64.getEncoder().encodeToString((byte[]) value));
-
-        DOMResult serializationResult = writeNormalizedNode(docNode, schemaContext);
-        assertNotNull(serializationResult);
-
-        XMLUnit.setIgnoreWhitespace(true);
-        XMLUnit.setIgnoreComments(true);
-        XMLUnit.setIgnoreAttributeOrder(true);
-        XMLUnit.setNormalize(true);
-
-        String expectedXMLString = toString(doc.getDocumentElement());
-        String serializationResultXMLString = toString(serializationResult.getNode());
-
-        XMLAssert.assertXMLEqual(expectedXMLString, serializationResultXMLString);
-    }
-
-    private static ContainerNode createDocNode() {
-        return Builders.containerBuilder()
-            .withNodeIdentifier(new NodeIdentifier(ROOT_QNAME))
-            .withChild(Builders.containerBuilder()
-                .withNodeIdentifier(new NodeIdentifier(ROOT_QNAME))
-                .withChild(ImmutableNodes.leafNode(IP_ADDRESS_QNAME, Base64.getDecoder().decode("fwAAAQ==")))
-                .build())
+        final var diff = DiffBuilder.compare(sw.toString())
+            .withTest("""
+            <?xml version="1.0" encoding="UTF-8"?>
+            <root xmlns="foo">
+                <ip-address>fwAAAQ==</ip-address>
+            </root>""")
+            .ignoreWhitespace()
+            .checkForIdentical()
             .build();
-    }
-
-    private static DOMResult writeNormalizedNode(final ContainerNode normalized, final EffectiveModelContext context)
-            throws IOException, XMLStreamException {
-        final Document doc = UntrustedXML.newDocumentBuilder().newDocument();
-        final DOMResult result = new DOMResult(doc);
-        NormalizedNodeWriter normalizedNodeWriter = null;
-        NormalizedNodeStreamWriter normalizedNodeStreamWriter = null;
-        XMLStreamWriter writer = null;
-        try {
-            writer = TestFactories.DEFAULT_OUTPUT_FACTORY.createXMLStreamWriter(result);
-            normalizedNodeStreamWriter = XMLStreamNormalizedNodeStreamWriter.create(writer, context);
-            normalizedNodeWriter = NormalizedNodeWriter.forStreamWriter(normalizedNodeStreamWriter);
-
-            for (var child : normalized.body()) {
-                normalizedNodeWriter.write(child);
-            }
-
-            normalizedNodeWriter.flush();
-        } finally {
-            if (normalizedNodeWriter != null) {
-                normalizedNodeWriter.close();
-            }
-            if (normalizedNodeStreamWriter != null) {
-                normalizedNodeStreamWriter.close();
-            }
-            if (writer != null) {
-                writer.close();
-            }
-        }
-
-        return result;
-    }
-
-    private static Document loadDocument(final String xmlPath) throws IOException, SAXException {
-        final InputStream resourceAsStream = Bug5446Test.class.getResourceAsStream(xmlPath);
-        return requireNonNull(readXmlToDocument(resourceAsStream));
-    }
-
-    private static Document readXmlToDocument(final InputStream xmlContent) throws IOException, SAXException {
-        final Document doc = UntrustedXML.newDocumentBuilder().parse(xmlContent);
-        doc.getDocumentElement().normalize();
-        return doc;
-    }
-
-    private static String toString(final Node xml) {
-        try {
-            final Transformer transformer = TransformerFactory.newInstance().newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-
-            final StreamResult result = new StreamResult(new StringWriter());
-            final DOMSource source = new DOMSource(xml);
-            transformer.transform(source, result);
-
-            return result.getWriter().toString();
-        } catch (IllegalArgumentException | TransformerFactoryConfigurationError | TransformerException e) {
-            throw new RuntimeException("Unable to serialize xml element " + xml, e);
-        }
+        assertFalse(diff.toString(), diff.hasDifferences());
     }
 }
