24e8e100fcb7f8c681d24de92d037e59c5bbbf1b
Robert Varga <robert.varga@pantheon.tech>
2023-10-03 20:21:52 +0200

    WIP: Add minidom XML representation
    YANG has an 'anydata' escape hatch, which allows for transporting
    unknown, but known-to-be-YANG-modeled, part of XML Infoset to be
    transported inline.
    
    Historically we have relied on org.w3c.dom to model this data, but that
    object model is mutable and more generic than we need.
    
    This patch introduces yang-xml, which provides a more suitable object
    model, along with two implementations:
    - one wrapping a org.w3c.dom.Document
    - one built through Document.Builder et al.
    
    // FIXME: Element's model needs DOM/Stream reconciliation
    //        - default namespace is not used in element string content
    //        - XML streaming:
    //          - always starts at Document
    //          - needs namespace declarations/attributes separately
    //          - needs to know which 'startElement()' to call
    //          - needs prefix instead of namespace
    //            - can we reassign element prefix?
    //            - can we reassign attribute prefix?
    //        - document processing:
    //          - does not care about namespace declarations
    //          - does not care about prefixes
    //          - cares about namespace + localname for elems and attrs
    //        - W3C DOM:
    //          - has/wants attributes and namespaces
    //        The aim is to make:
    //        - XML streaming fast by doing minimum work
    //        - document processing efficient by calculating stuff and
    //          requiring users to remember/provide stuff
    //        - W3C compat bearable by via limited processing
    // FIXME: Document implements PrettyTreeAware, pretty tree is a valid
    //        XML document
    
    JIRA: YANGTOOLS-1547
    Change-Id: I2e50e03724b5bf5013d29ed79013aa4958fb7e75
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ common/yang-xml/src/main/java/org/opendaylight/yangtools/yang/xml/W3CTextElement.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.xml;
+
+import static java.util.Objects.requireNonNull;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+@NonNullByDefault
+record W3CTextElement(org.w3c.dom.Element node) implements TextElement, W3CElement {
+    W3CTextElement {
+        requireNonNull(node);
+    }
+
+    @Override
+    public String text() {
+        // FIXME: implement this
+        throw new UnsupportedOperationException();
+    }
+}
