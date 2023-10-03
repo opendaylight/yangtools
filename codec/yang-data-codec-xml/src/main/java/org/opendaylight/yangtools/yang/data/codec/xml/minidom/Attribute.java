e6381c5424c227f06872e65a00862f46e4269af1
Robert Varga <robert.varga@pantheon.tech>
2023-10-03 20:21:52 +0200

    WIP: Add minidom
    Change-Id: I2e50e03724b5bf5013d29ed79013aa4958fb7e75
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ codec/yang-data-codec-xml/src/main/java/org/opendaylight/yangtools/yang/data/codec/xml/minidom/Attribute.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.codec.xml.minidom;
+
+import org.eclipse.jdt.annotation.NonNull;
+
+/**
+ * A single XML attribute.
+ */
+public non-sealed interface Attribute extends Node {
+    /**
+     * This attribute's value.
+     *
+     * @return Value
+     */
+    @NonNull String value();
+}
---
+++ codec/yang-data-codec-xml/src/main/java/org/opendaylight/yangtools/yang/data/codec/xml/minidom/ContainerElement.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.codec.xml.minidom;
+
+import java.util.List;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * An XML element containing other elements.
+ */
+@NonNullByDefault
+public non-sealed interface ContainerElement extends Element {
+    /**
+     * Return the list of child elements
+     *
+     * @return Contained child elements
+     */
+    List<Element> children();
+}
