d13db106ae3c5dce7e11a3c3e261f5eeec5164b4
Robert Varga <robert.varga@pantheon.tech>
2023-12-07 22:55:17 +0100

    Deprecate SchemaContextListener for removal
    SchemaContext is superseded by EffectiveModelContext, guide users
    towards that by deprecating SchemaContextListeer.
    
    JIRA: YANGTOOLS-1553
    Change-Id: I91a821bc5651cdf2167820f549427200c9162c07
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit f346b461546efece1575d9d0963cdfdb4b728d72)

--- model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/SchemaContextListener.java
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/SchemaContextListener.java
@@ -10,11 +10,13 @@ package org.opendaylight.yangtools.yang.model.api;
 import java.util.EventListener;
 
 /**
- * Interface for listeners interested in updates of the global schema context.
- * The global schema context reflects the global view of the model world, and
- * all entities interacting at the global scale need to maintain a consistent
- * view of that world.
+ * Interface for listeners interested in updates of the global schema context. The global schema context reflects the
+ * global view of the model world, and all entities interacting at the global scale need to maintain a consistent view
+ * of that world.
+ *
+ * @deprecated Use {@link EffectiveModelContextListener} instead.
  */
+@Deprecated(since = "11.0.5", forRemoval = true)
 public interface SchemaContextListener extends EventListener {
     /**
      * The global schema context is being updated.
