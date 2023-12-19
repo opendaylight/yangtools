143d815ea578f580086c8ae3ff9882d8b784eb94
Robert Varga <robert.varga@pantheon.tech>
2023-12-19 13:49:28 +0100

    Move binding.model.api documentation
    Package documentation resides in the wrong file, move it.
    
    Change-Id: I77d1c5f3b411df7ebb855ccaeb6c7c8d9d1184cc
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/mdsal-binding-model-api/src/main/java/org/opendaylight/mdsal/binding/model/api/package-info.java
+++ binding/mdsal-binding-model-api/src/main/java/org/opendaylight/mdsal/binding/model/api/package-info.java
@@ -5,4 +5,23 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+/**
+ * Model of Java classes generated from YANG. Some information on naming:
+ *
+ * <ul>
+ * <li>
+ * {@link Type} refers to a Java class, interface or primitive type. This is something that is used as the type
+ * specifier of a variable declaration.
+ * </li>
+ * <li>
+ * {@link GeneratedType} refers to a Java class or interface. It can either be a top-level or a nested class or
+ * interface.
+ * </li>
+ * <li>
+ * {@link GeneratedTransferObject} refers to {@link GeneratedType}, which is a concrete class. These are generated to
+ * encapsulate the YANG type hierarchy as expressed by 'typedef' and 'type' statement use. If it has a superclass, it is
+ * also referred to as an {@code Extended Type}.
+ * </li>
+ * </ul>
+ */
 package org.opendaylight.mdsal.binding.model.api;
