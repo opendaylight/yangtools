7d9be9ba89a932faa28842da87bd91ad443f1931
Robert Varga <robert.varga@pantheon.tech>
2023-12-21 23:13:03 +0100

    Remove NormalizedNode.getIdentifier()
    This method has been deprecated for removal, remove it now.
    
    JIRA: YANGTOOLS-1559
    Change-Id: Ia8f839c0daa60fc64ce20084833f3516869b02a8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- data/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/LeafSetEntryNode.java
+++ data/yang-data-api/src/main/java/org/opendaylight/yangtools/yang/data/api/schema/LeafSetEntryNode.java
@@ -33,19 +33,4 @@ public non-sealed interface LeafSetEntryNode<T> extends ValueNode<T> {
      */
     @Override
     NodeWithValue<T> name();
-
-    /**
-     * {@inheritDoc}
-     *
-     * <p>
-     * <b>Implementation note</b>
-     * Invocation of {@link NodeWithValue#getValue()} on returned instance of {@link NodeWithValue} must return the
-     * same value as invocation of {@code #body()}, such as following condition is always met:
-     * {@code true == this.getIdentifier().getValue().equals(this.body())}.
-     */
-    @Override
-    @Deprecated(since = "11.0.0", forRemoval = true)
-    default NodeWithValue<T> getIdentifier() {
-        return name();
-    }
 }
