f9ce25d4edfea59a4921cbfe6e012c875d3dabda
Robert Varga <robert.varga@pantheon.tech>
2023-12-22 16:51:48 +0100

    WIP: Add AbstractNormalizedNode
    JIRA: YANGTOOLS-1259
    Change-Id: I0764278cff74a482c74381e6b5e4787ffc3f87e1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ data/yang-data-spi/src/main/java/org/opendaylight/yangtools/yang/data/spi/node/ForwardingContainerNode.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.data.spi.node;
+
+import com.google.common.collect.ForwardingObject;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+
+/**
+ *
+ */
+public class ForwardingContainerNode extends ForwardingObject implements ContainerNode {
+
+}
