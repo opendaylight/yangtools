1565c2e538fe5bea39408c0f07fec03afb4a4108
Robert Varga <robert.varga@pantheon.tech>
2023-12-23 01:49:26 +0100

    Split out ImmutableLeafNode
    Split this class out to a top-level contract.
    
    JIRA: NETCONF-1259
    Change-Id: I196e2a4b6a93bbb26ba5c2245e3ad3775d4e2482
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- data/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/ImmutableNodes.java
+++ data/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/ImmutableNodes.java
@@ -24,7 +24,7 @@ import org.opendaylight.yangtools.yang.data.api.schema.SystemMapNode;
 import org.opendaylight.yangtools.yang.data.api.schema.UnkeyedListNode;
 import org.opendaylight.yangtools.yang.data.api.schema.UserMapNode;
 import org.opendaylight.yangtools.yang.data.api.schema.stream.YangInstanceIdentifierWriter;
-import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableLeafNodeBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableLeafNode;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 
@@ -105,7 +105,7 @@ public final class ImmutableNodes {
      * @return Leaf node with supplied identifier and value
      */
     public static <T> @NonNull LeafNode<T> leafNode(final NodeIdentifier name, final T value) {
-        return ImmutableLeafNodeBuilder.createNode(name, value);
+        return ImmutableLeafNode.of(name, value);
     }
 
     /**
--- data/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/builder/impl/ImmutableLeafNodeBuilder.java
+++ data/yang-data-impl/src/main/java/org/opendaylight/yangtools/yang/data/impl/schema/builder/impl/ImmutableLeafNodeBuilder.java
@@ -7,49 +7,14 @@
  */
 package org.opendaylight.yangtools.yang.data.impl.schema.builder.impl;
 
-import com.google.common.annotations.Beta;
-import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
-import org.opendaylight.yangtools.yang.data.impl.schema.nodes.AbstractImmutableNormalizedSimpleValueNode;
 
 public final class ImmutableLeafNodeBuilder<T>
         extends AbstractImmutableNormalizedNodeBuilder<NodeIdentifier, T, LeafNode<T>>
         implements LeafNode.Builder<T> {
-    @Beta
-    @SuppressWarnings("unchecked")
-    public static <T> @NonNull LeafNode<T> createNode(final NodeIdentifier identifier, final T value) {
-        if (value instanceof byte[] bytes) {
-            return (LeafNode<T>) new ImmutableBinaryLeafNode(identifier, bytes);
-        }
-        return new ImmutableLeafNode<>(identifier, value);
-    }
-
     @Override
     public LeafNode<T> build() {
-        return createNode(getNodeIdentifier(), getValue());
-    }
-
-    private static class ImmutableLeafNode<T>
-            extends AbstractImmutableNormalizedSimpleValueNode<NodeIdentifier, LeafNode<?>, T> implements LeafNode<T> {
-        ImmutableLeafNode(final NodeIdentifier nodeIdentifier, final T value) {
-            super(nodeIdentifier, value);
-        }
-
-        @Override
-        protected final Class<LeafNode<?>> implementedType() {
-            return (Class) LeafNode.class;
-        }
-    }
-
-    private static final class ImmutableBinaryLeafNode extends ImmutableLeafNode<byte[]> {
-        ImmutableBinaryLeafNode(final NodeIdentifier nodeIdentifier, final byte[] value) {
-            super(nodeIdentifier, value);
-        }
-
-        @Override
-        protected byte[] wrapValue(final byte[] valueToWrap) {
-            return valueToWrap.clone();
-        }
+        return ImmutableLeafNode.of(getNodeIdentifier(), getValue());
     }
 }
