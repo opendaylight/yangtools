61a3a0e77afdb97f502d8476b771f6540a7ae5e1
Robert Varga <robert.varga@pantheon.tech>
2024-01-01 23:58:20 +0100

    Add SchemaSourceInfo
    We typically need to organize SchemaSourceRepresentations in some way
    and make consistency inferences against their linkage.
    
    The RFCSs do not specifically call this out, but the information is kept
    at the top of the YANG file, so that even a simple text parser can find
    it quickly.
    
    Our YANG text processing pipeline has this information available
    whenever we need to build a YangModelDependencyInfo -- which is an
    optional operation, but the implementation only needs IRSchemaSource.
    
    This patch introduces SchemaSourceInfo, which is a RFC-based opinionated
    version YangModelDependencyInfo.
    
    The parsing machinery from YangModelDependencyInfo is rehosted to
    YangIRSchemaSource, as it has just about all the tools it needs for the
    job.
    
    // FIXME: deal with string parsing error reporting
    // FIXME: where do we place the utility methods?
    
    JIRA: YANGTOOLS-XXX
    Change-Id: I5043f2be3138c6cf599cb8ca29c5bfb0c829625e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-repo-api/src/main/java/org/opendaylight/yangtools/yang/model/repo/api/YangIRSchemaSource.java
+++ yang/yang-repo-api/src/main/java/org/opendaylight/yangtools/yang/model/repo/api/YangIRSchemaSource.java
@@ -11,17 +11,34 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.annotations.Beta;
+import com.google.common.collect.ImmutableSet;
 import java.util.Optional;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.yangtools.concepts.AbstractSimpleIdentifiable;
+import org.opendaylight.yangtools.yang.common.Revision;
+import org.opendaylight.yangtools.yang.common.UnresolvedQName.Unqualified;
+import org.opendaylight.yangtools.yang.common.XMLNamespace;
+import org.opendaylight.yangtools.yang.common.YangVersion;
 import org.opendaylight.yangtools.yang.ir.IRKeyword;
-import org.opendaylight.yangtools.yang.ir.IRKeyword.Unqualified;
 import org.opendaylight.yangtools.yang.ir.IRStatement;
+import org.opendaylight.yangtools.yang.model.api.YangStmtMapping;
+import org.opendaylight.yangtools.yang.model.repo.api.SchemaSourceInfo.Import;
 
 @Beta
 public final class YangIRSchemaSource extends AbstractSimpleIdentifiable<SourceIdentifier>
         implements YangSchemaSourceRepresentation {
+    private static final String BELONGS_TO = YangStmtMapping.BELONGS_TO.getStatementName().getLocalName();
+    private static final String IMPORT = YangStmtMapping.IMPORT.getStatementName().getLocalName();
+    private static final String INCLUDE = YangStmtMapping.INCLUDE.getStatementName().getLocalName();
+    private static final String MODULE = YangStmtMapping.MODULE.getStatementName().getLocalName();
+    private static final String NAMESPACE = YangStmtMapping.NAMESPACE.getStatementName().getLocalName();
+    private static final String PREFIX = YangStmtMapping.PREFIX.getStatementName().getLocalName();
+    private static final String REVISION = YangStmtMapping.REVISION.getStatementName().getLocalName();
+    private static final String REVISION_DATE = YangStmtMapping.REVISION_DATE.getStatementName().getLocalName();
+    private static final String SUBMODULE = YangStmtMapping.SUBMODULE.getStatementName().getLocalName();
+    private static final String YANG_VERSION = YangStmtMapping.YANG_VERSION.getStatementName().getLocalName();
+
     private final @NonNull IRStatement rootStatement;
     private final @Nullable String symbolicName;
 
@@ -31,9 +48,10 @@ public final class YangIRSchemaSource extends AbstractSimpleIdentifiable<SourceI
         this.rootStatement = requireNonNull(rootStatement);
         this.symbolicName = symbolicName;
 
-        final IRKeyword rootKeyword = rootStatement.keyword();
-        checkArgument(rootKeyword instanceof Unqualified, "Root statement has invalid keyword %s", rootKeyword);
-        final String rootName = rootKeyword.identifier();
+        final var rootKeyword = rootStatement.keyword();
+        checkArgument(rootKeyword instanceof IRKeyword.Unqualified,
+                "Root statement has invalid keyword %s", rootKeyword);
+        final var rootName = rootKeyword.identifier();
         switch (rootName) {
             case "module":
             case "submodule":
@@ -43,6 +61,13 @@ public final class YangIRSchemaSource extends AbstractSimpleIdentifiable<SourceI
         }
 
         checkArgument(rootStatement.argument() != null, "Root statement does not have an argument");
+    }
+
+    public static @NonNull YangIRSchemaSource fromSource(final SourceIdentifier sourceId,
+            final @NonNull IRStatement rootStatement, final @Nullable String symbolicName) {
+        return new YangIRSchemaSource(new SourceIdentifier(
+            safeStringArgument(sourceId, rootStatement, "name"),
+            latestRevision(rootStatement, sourceId)), rootStatement, symbolicName);
     }
 
     @Override
@@ -62,5 +87,108 @@ public final class YangIRSchemaSource extends AbstractSimpleIdentifiable<SourceI
      */
     public @NonNull IRStatement getRootStatement() {
         return rootStatement;
+    }
+
+    public @NonNull SchemaSourceInfo extractInfo() {
+        final var keyword = rootStatement.keyword();
+        checkArgument(keyword instanceof IRKeyword.Unqualified, "Invalid root statement %s", keyword);
+
+        final var arg = keyword.identifier();
+        if (MODULE.equals(arg)) {
+            return new ModuleSourceInfo(Unqualified.of(safeStringArgument(rootStatement, "module name")),
+                extractYangVersion(), extractNamespace(), prefixIn(rootStatement), extractRevisions(), extractImports(),
+                extractIncludes());
+        } else if (SUBMODULE.equals(arg)) {
+            return new SubmoduleSourceInfo(Unqualified.of(safeStringArgument(rootStatement, "submodule name")),
+                extractYangVersion(), extractBelongsTo(), extractRevisions(), extractImports(), extractIncludes());
+        } else {
+            throw new IllegalArgumentException("Root of parsed AST must be either module or submodule");
+        }
+    }
+
+    private @NonNull YangVersion extractYangVersion() {
+        return rootStatement.statements().stream()
+            .filter(stmt -> isStatement(stmt, YANG_VERSION))
+            .findFirst()
+            .map(stmt -> YangVersion.ofString(safeStringArgument(stmt, "yang-version version")))
+            .orElse(YangVersion.VERSION_1);
+    }
+
+    private @NonNull XMLNamespace extractNamespace() {
+        return rootStatement.statements().stream()
+            .filter(stmt -> isStatement(stmt, NAMESPACE))
+            .findFirst()
+            .map(stmt -> XMLNamespace.of(safeStringArgument(stmt, "namespace string")))
+            .orElseThrow(() -> new IllegalArgumentException("Missing namespace in " + getIdentifier()));
+    }
+
+    private @NonNull Unqualified extractBelongsTo() {
+        return rootStatement.statements().stream()
+            .filter(stmt -> isStatement(stmt, BELONGS_TO))
+            .findFirst()
+            .map(stmt -> Unqualified.of(safeStringArgument(stmt, "belongs-to module name")))
+            .orElseThrow(() -> new IllegalArgumentException("Missing belongs-to in " + getIdentifier()));
+    }
+
+    private @NonNull ImmutableSet<SchemaSourceInfo.Import> extractImports() {
+        return rootStatement.statements().stream()
+            .filter(stmt -> isStatement(stmt, IMPORT))
+            .map(stmt -> new Import(
+                Unqualified.of(safeStringArgument(stmt, "imported module name")),
+                prefixIn(stmt),
+                revisionDateIn(stmt)))
+            .collect(ImmutableSet.toImmutableSet());
+    }
+
+    private @NonNull ImmutableSet<SchemaSourceInfo.Include> extractIncludes() {
+        return rootStatement.statements().stream()
+            .filter(stmt -> isStatement(stmt, INCLUDE))
+            .map(stmt -> new SchemaSourceInfo.Include(
+                Unqualified.of(safeStringArgument(stmt, "included submodule name")),
+                revisionDateIn(stmt)))
+            .collect(ImmutableSet.toImmutableSet());
+    }
+
+    private @NonNull ImmutableSet<Revision> extractRevisions() {
+        return rootStatement.statements().stream()
+            .filter(stmt -> isStatement(stmt, REVISION))
+            .map(stmt -> Revision.of(safeStringArgument(stmt, "revision date")))
+            .collect(ImmutableSet.toImmutableSet());
+    }
+
+    private @NonNull String prefixIn(final IRStatement parent) {
+        return parent.statements().stream()
+            .filter(stmt -> isStatement(stmt, PREFIX))
+            .findFirst()
+            .map(stmt -> safeStringArgument(stmt, "prefix string"))
+            .orElseThrow(() -> new IllegalArgumentException("No prefix in " + parent));
+    }
+
+    private @Nullable Revision revisionDateIn(final IRStatement parent) {
+        return parent.statements().stream()
+            .filter(stmt -> isStatement(stmt, REVISION_DATE))
+            .findFirst()
+            .map(stmt -> Revision.of(safeStringArgument(stmt, "imported module revision-date")))
+            .orElse(null);
+    }
+
+    private static boolean isStatement(final IRStatement stmt, final String localName) {
+        return stmt.keyword() instanceof IRKeyword.Unqualified keyword && localName.equals(keyword.identifier());
+    }
+
+    private String safeStringArgument(final IRStatement stmt, final String desc) {
+        return safeStringArgument(getIdentifier(), stmt, desc);
+    }
+
+    // SourceIdentifier, Ref, whatever works here
+    private static String safeStringArgument(final SourceIdentifier sourceId, final IRStatement stmt,
+            final String desc) {
+        final var arg = stmt.argument();
+        if (arg == null) {
+            throw new IllegalArgumentException("Missing " + desc + " at " + sourceId);
+        }
+
+        // TODO: we probably need to understand yang version first....
+        return ArgumentContextUtils.rfc6020().stringFromStringContext(arg, sourceId);
     }
 }
