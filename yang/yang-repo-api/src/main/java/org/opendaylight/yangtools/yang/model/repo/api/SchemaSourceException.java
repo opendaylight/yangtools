795e73cd59cc88ae91a8a32dae9079a7ddbbc35c
Robert Varga <robert.varga@pantheon.tech>
2024-01-02 06:37:25 +0100

    SchemaSourceException should carry SourceIdentifier
    We have two classes that already do this, codify this fact.
    
    JIRA: YANGTOOLS-1150
    Change-Id: I63bb778b21a063346bfa432734bc60fbb0bc1560
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-repo-api/src/main/java/org/opendaylight/yangtools/yang/model/repo/api/SchemaSourceException.java
+++ yang/yang-repo-api/src/main/java/org/opendaylight/yangtools/yang/model/repo/api/SchemaSourceException.java
@@ -7,20 +7,33 @@
  */
 package org.opendaylight.yangtools.yang.model.repo.api;
 
+import static java.util.Objects.requireNonNull;
+
 import com.google.common.annotations.Beta;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.model.spi.source.SourceIdentifier;
 
 /**
  * Exception thrown when a failure to acquire a schema source occurs.
  */
 @Beta
 public class SchemaSourceException extends Exception {
-    private static final long serialVersionUID = 1L;
+    @java.io.Serial
+    private static final long serialVersionUID = 2L;
 
-    public SchemaSourceException(final String message) {
+    private final @NonNull SourceIdentifier sourceId;
+
+    public SchemaSourceException(final SourceIdentifier sourceId, final String message) {
         super(message);
+        this.sourceId = requireNonNull(sourceId);
     }
 
-    public SchemaSourceException(final String message, final Throwable cause) {
+    public SchemaSourceException(final SourceIdentifier sourceId, final String message, final Throwable cause) {
         super(message, cause);
+        this.sourceId = requireNonNull(sourceId);
+    }
+
+    public final @NonNull SourceIdentifier sourceId() {
+        return sourceId;
     }
 }
