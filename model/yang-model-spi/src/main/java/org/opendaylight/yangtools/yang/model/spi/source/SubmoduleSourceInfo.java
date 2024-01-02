05f9ef539da2a08f5b832f4131b1bf0b4491b810
Robert Varga <robert.varga@pantheon.tech>
2024-01-02 20:01:02 +0100

    Add SchemaSourceInfo
    We typically need to organize SchemaSourceRepresentations in some way
    and make consistency inferences against their linkage.
    
    The RFCSs do not specifically call this out, but the information is kept
    at the top of the YANG file, so that even a simple text parser can find
    it quickly.
    
    Our YANG text processing pipeline has this information available
    whenever we need to build a YangModelDependencyInfo -- which is an
    optional operation, but the implementation only needs IRSchemaSource.
    
    This patch introduces SchemaSourceInfo, which is a RFC-based opinionated
    version YangModelDependencyInfo.
    
    The parsing machinery from YangModelDependencyInfo is rehosted to
    YangIRSchemaSource, as it has just about all the tools it needs for the
    job.
    
    // FIXME: deal with string parsing error reporting
    // FIXME: where do we place the utility methods?
    
    JIRA: YANGTOOLS-1150
    Change-Id: I5043f2be3138c6cf599cb8ca29c5bfb0c829625e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ model/yang-model-spi/src/main/java/org/opendaylight/yangtools/yang/model/spi/source/SubmoduleSourceInfo.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.spi.source;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.collect.ImmutableSet;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.common.Revision;
+import org.opendaylight.yangtools.yang.common.UnresolvedQName.Unqualified;
+import org.opendaylight.yangtools.yang.common.YangVersion;
+
+/**
+ * A {@link SourceInfo} about a {@code submodule}.
+ */
+@NonNullByDefault
+public record SubmoduleSourceInfo(
+        Unqualified name,
+        YangVersion yangVersion,
+        Unqualified belongsTo,
+        ImmutableSet<Revision> revisions,
+        ImmutableSet<Import> imports,
+        ImmutableSet<Include> includes) implements SourceInfo {
+    public SubmoduleSourceInfo {
+        requireNonNull(name);
+        requireNonNull(yangVersion);
+        requireNonNull(belongsTo);
+        requireNonNull(revisions);
+        requireNonNull(imports);
+        requireNonNull(includes);
+    }
+}
