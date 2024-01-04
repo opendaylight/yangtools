a4a2210864d8236de82b789bf9b4abcc81349459
Robert Varga <robert.varga@pantheon.tech>
2024-01-04 10:56:10 +0100

    Move YangIRSchemaSource
    Let's call this model.spi.source.YangIRSource and clean up its
    interface.
    
    As this brings things closer to where they should be, we can now use
    StatementSourceException to unify the layout of our error messages and
    have TextStatementReference a readily-available implementation.
    
    JIRA: YANGTOOLS-1150
    Change-Id: I8b43ada04065c5183e599d48a84912a36aa6e54b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/repo/TextToIRTransformer.java
+++ parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/repo/TextToIRTransformer.java
@@ -11,18 +11,18 @@ import com.google.common.annotations.Beta;
 import com.google.common.util.concurrent.Futures;
 import java.io.IOException;
 import org.eclipse.jdt.annotation.NonNull;
-import org.opendaylight.yangtools.yang.ir.YangIRSchemaSource;
 import org.opendaylight.yangtools.yang.model.repo.api.SchemaRepository;
 import org.opendaylight.yangtools.yang.model.repo.spi.SchemaSourceRegistry;
 import org.opendaylight.yangtools.yang.model.repo.spi.SchemaSourceTransformer;
+import org.opendaylight.yangtools.yang.model.spi.source.YangIRSource;
 import org.opendaylight.yangtools.yang.model.spi.source.YangTextSource;
 import org.opendaylight.yangtools.yang.parser.api.YangSyntaxErrorException;
 import org.opendaylight.yangtools.yang.parser.rfc7950.antlr.IRSupport;
 
 @Beta
-public final class TextToIRTransformer extends SchemaSourceTransformer<YangTextSource, YangIRSchemaSource> {
+public final class TextToIRTransformer extends SchemaSourceTransformer<YangTextSource, YangIRSource> {
     private TextToIRTransformer(final SchemaRepository provider, final SchemaSourceRegistry consumer) {
-        super(provider, YangTextSource.class, consumer, YangIRSchemaSource.class,
+        super(provider, YangTextSource.class, consumer, YangIRSource.class,
             input -> Futures.immediateFuture(transformText(input)));
     }
 
@@ -31,10 +31,10 @@ public final class TextToIRTransformer extends SchemaSourceTransformer<YangTextS
         return new TextToIRTransformer(provider, consumer);
     }
 
-    public static @NonNull YangIRSchemaSource transformText(final YangTextSource text)
+    public static @NonNull YangIRSource transformText(final YangTextSource text)
             throws YangSyntaxErrorException, IOException {
         final var rootStatement = IRSupport.createStatement(YangStatementStreamSource.parseYangSource(text));
         final var info = YangIRSourceInfoExtractor.forIR(rootStatement, text.sourceId());
-        return new YangIRSchemaSource(info.sourceId(), rootStatement, text.symbolicName());
+        return new YangIRSource(info.sourceId(), rootStatement, text.symbolicName());
     }
 }
