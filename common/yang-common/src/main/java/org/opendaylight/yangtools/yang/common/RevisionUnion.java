02bb16214f96859574f7d6e5645ada6490f7fc58
Robert Varga <robert.varga@pantheon.tech>
2024-01-04 06:04:50 +0100

    Add RevisionUnion
    The intrinsic nullness of Revision is quite problematic, especially when
    we are interfacing the world of ietf-yang-library.
    
    Introduce RevisionUnion, which models the ietf-yang-library union of a
    revision-identifier and an empty string.
    
    JIRA: YANGTOOLS-918
    Change-Id: I2641eba8f27d71f750079b2713474e07393fddf3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ common/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/RevisionUnion.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.common;
+
+import java.io.Serializable;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.concepts.Immutable;
+
+/**
+ * A capture of an optional {@code revision-date}. This is a replacement for {@code Optional<Revision>}, with the added
+ * benefit of having a non-null string representation in {@link #unionString()}, which is also conveniently returned
+ * from {@link #toString()}.
+ *
+ * <p>
+ * This contract is exactly the same as this fragment from {@code ietf-yang-library}:
+ * <pre>{@code
+ *   type union {
+ *     type revision-identifier;
+ *     type string {
+ *       length "0";
+ *     }
+ *   }
+ * }</pre>
+ */
+@NonNullByDefault
+public sealed interface RevisionUnion extends Comparable<RevisionUnion>, Immutable, Serializable
+        permits Revision, NotRevision {
+    /**
+     * Return empty {@link RevisionUnion}.
+     *
+     * @return empty {@link RevisionUnion}
+     */
+    static RevisionUnion none() {
+        return NotRevision.INSTANCE;
+    }
+
+    static RevisionUnion of(final String unionString) {
+        return unionString.isEmpty() ? none() : Revision.of(unionString);
+    }
+
+    /**
+     * A {@code revision-date}-compliant date, or an empty string ({@code ""}).
+     *
+     * @return A revision-date or empty string
+     */
+    String unionString();
+
+    @Override
+    @SuppressWarnings("checkstyle:parameterName")
+    default int compareTo(final RevisionUnion o) {
+        // Since all strings conform to the format, we can use their comparable property to do the correct thing
+        // with respect to temporal ordering.
+        return unionString().compareTo(o.unionString());
+    }
+
+    /**
+     * Return the {@link Revision}, if present.
+     *
+     * @return the revision, or {@code null} if not present
+     */
+    @Nullable Revision revision();
+
+    default Optional<Revision> findRevision() {
+        return Optional.ofNullable(revision());
+    }
+
+    default Revision getRevision() {
+        final var revision = revision();
+        if (revision == null) {
+            throw new NoSuchElementException();
+        }
+        return revision;
+    }
+
+    @Override
+    int hashCode();
+
+    @Override
+    boolean equals(@Nullable Object obj);
+
+    /**
+     * Returns {@link #unionString()}.
+     *
+     * @return {@link #unionString()}
+     */
+    @Override
+    String toString();
+}
