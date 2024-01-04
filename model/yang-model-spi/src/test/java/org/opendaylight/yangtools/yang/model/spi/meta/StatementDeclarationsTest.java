f51876898a6a9d62719c52d7a7f90c2cf9c8fb66
Robert Varga <robert.varga@pantheon.tech>
2024-01-04 17:00:06 +0100

    Refactor ExplicitStatement
    Rather than having a superclass, just create StatementDeclarations as a
    host for the two factory methods. Implementations are completely
    internal and do not leak outside of this class.
    
    We also optimize memory footprint by using two versions of each class --
    one storing file/line as ints, the other one as shorts. This ends up
    saving a few bytes of memory.
    
    Eliminate the InFile specialization, as it is not used anywhere really.
    Also renale atPosition() to inText() to more closely align naming.
    
    JIRA: YANGTOOLS-1150
    Change-Id: Ib5a20e45f645f00fac8d1d27c48dd4dcab16fb17
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-spi/src/test/java/org/opendaylight/yangtools/yang/model/spi/meta/StatementDeclarationsTest.java
+++ model/yang-model-spi/src/test/java/org/opendaylight/yangtools/yang/model/spi/meta/StatementDeclarationsTest.java
@@ -5,23 +5,23 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.yangtools.yang.parser.spi.source;
+package org.opendaylight.yangtools.yang.model.spi.meta;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementOrigin;
 
-class ExplicitStatementTest {
+class StatementDeclarationsTest {
     @Test
     void testStatementSource() {
-        assertEquals(StatementOrigin.DECLARATION, ExplicitStatement.inFile("foo").statementOrigin());
+        assertEquals(StatementOrigin.DECLARATION, StatementDeclarations.inText(1, 1, null).statementOrigin());
     }
 
     @Test
     void testToString() {
-        assertEquals("foo", ExplicitStatement.inFile("foo").toString());
-        assertEquals("<UNKNOWN>:5:10", ExplicitStatement.atPosition(5, 10).toString());
-        assertEquals("foo:5:10", ExplicitStatement.atPosition("foo", 5, 10).toString());
+        assertEquals("<UNKNOWN>:5:10", StatementDeclarations.inText(5, 10).toString());
+        assertEquals("foo:5:10", StatementDeclarations.inText(5, 10, "foo").toString());
+        assertEquals("<UNKNOWN>:1:1", StatementDeclarations.inText(1, 1, null).toString());
     }
 }
