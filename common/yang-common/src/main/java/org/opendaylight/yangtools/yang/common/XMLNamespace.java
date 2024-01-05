a6f21998db618a16a8935642911172b0040c1af5
Robert Varga <robert.varga@pantheon.tech>
2024-01-05 15:35:28 +0100

    WIP: Use RevisionUnion in QNameModule
    QNameModule holds an optional Revision, which is inconvenient. This
    patch retrofits RevisionUnion into it.
    
    Since we are in this area, improve instantiation api, introducing of()
    and ofRevision() convenience methods and deprecate create() methods.
    
    Since QNameModule is a WritableObject composed of XMLNamespace and
    RevisionUnion, make the two also WritableObjects and delegate to them.
    
    // FIXME: review/expand documentation
    // FIXME: migrate a few users
    
    JIRA: YANGTOOLS-XXX
    Change-Id: Ib5349ef324d73340b285e9634e579825e55ab95d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- common/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/XMLNamespace.java
+++ common/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/XMLNamespace.java
@@ -7,25 +7,32 @@
  */
 package org.opendaylight.yangtools.yang.common;
 
+import static com.google.common.base.Verify.verifyNotNull;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.collect.Interner;
 import com.google.common.collect.Interners;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
 import java.io.Serial;
 import java.io.Serializable;
 import java.net.URI;
 import java.net.URISyntaxException;
-import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.concepts.WritableObject;
 
 /**
  * A simple type capture of {@code namespace} statement's argument according to
  * <a href="https://www.rfc-editor.org/rfc/rfc6020#section-7.1.3">RFC6020</a>.
  */
-public final class XMLNamespace implements Comparable<XMLNamespace>, Immutable, Serializable {
-    private static final Interner<XMLNamespace> INTERNER = Interners.newWeakInterner();
-    @Serial
+@NonNullByDefault
+public final class XMLNamespace implements Comparable<XMLNamespace>, Immutable, Serializable, WritableObject {
+    @java.io.Serial
     private static final long serialVersionUID = 1L;
+    private static final Interner<XMLNamespace> INTERNER = Interners.newWeakInterner();
 
     private final String namespace;
 
@@ -34,10 +41,10 @@ public final class XMLNamespace implements Comparable<XMLNamespace>, Immutable, 
     }
 
     // FIXME: add documentation
-    public static @NonNull XMLNamespace of(final String namespace) {
+    public static XMLNamespace of(final String namespace) {
         try {
-            // Validation only
-            new URI(namespace);
+            // FIXME: we want this validation, can we get it without the object allocation?
+            verifyNotNull(new URI(namespace));
         } catch (final URISyntaxException e) {
             throw new IllegalArgumentException("Namespace '" + namespace + "' is not a valid URI", e);
         }
@@ -50,7 +57,7 @@ public final class XMLNamespace implements Comparable<XMLNamespace>, Immutable, 
      *
      * @return Interned reference, or this object if it was interned.
      */
-    public @NonNull XMLNamespace intern() {
+    public XMLNamespace intern() {
         return INTERNER.intern(this);
     }
 
@@ -61,12 +68,25 @@ public final class XMLNamespace implements Comparable<XMLNamespace>, Immutable, 
     }
 
     @Override
+    public void writeTo(final DataOutput out) throws IOException {
+        out.writeUTF(namespace);
+    }
+
+    public static XMLNamespace readFrom(final DataInput in) throws IOException {
+        try {
+            return of(in.readUTF());
+        } catch (IllegalArgumentException e) {
+            throw new IOException("Invalid namespace", e);
+        }
+    }
+
+    @Override
     public int hashCode() {
         return namespace.hashCode();
     }
 
     @Override
-    public boolean equals(final Object obj) {
+    public boolean equals(final @Nullable Object obj) {
         return this == obj || obj instanceof XMLNamespace other && namespace.equals(other.namespace);
     }
 
