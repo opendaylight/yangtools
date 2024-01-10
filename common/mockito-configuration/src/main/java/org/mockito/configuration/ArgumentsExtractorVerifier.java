0165eddd3df7a3c3c90b798a28074f78f20462be
Robert Varga <robert.varga@pantheon.tech>
2024-01-10 13:23:53 +0100

    Modernize ArgumentsExtractorVerifier
    Refactor methods to keep the flow simpler.
    
    Change-Id: I079aae8c738ace034bf01579f998cbbfcc879675
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- common/mockito-configuration/src/main/java/org/mockito/configuration/ArgumentsExtractorVerifier.java
+++ common/mockito-configuration/src/main/java/org/mockito/configuration/ArgumentsExtractorVerifier.java
@@ -7,31 +7,30 @@
  */
 package org.mockito.configuration;
 
-import java.util.List;
 import org.mockito.exceptions.base.MockitoException;
 import org.mockito.internal.invocation.InvocationsFinder;
 import org.mockito.internal.verification.VerificationModeFactory;
 import org.mockito.internal.verification.api.VerificationData;
-import org.mockito.invocation.Invocation;
 import org.mockito.verification.VerificationMode;
 
 /**
  * Verifier that extracts arguments from actual invocation. Useful when deeper validation of arguments is needed.
  */
-public class ArgumentsExtractorVerifier implements VerificationMode {
+public final class ArgumentsExtractorVerifier implements VerificationMode {
     private Object[] arguments = null;
 
     @Override
     public void verify(final VerificationData data) {
-        List<Invocation> actualInvocations =
-            InvocationsFinder.findInvocations(data.getAllInvocations(), data.getTarget());
-        if (actualInvocations.size() != 1) {
-            throw new MockitoException("This verifier can only be used with 1 invocation, got "
-                    + actualInvocations.size());
+        final var actualInvocations = InvocationsFinder.findInvocations(data.getAllInvocations(), data.getTarget());
+        final var size = actualInvocations.size();
+        switch (size) {
+            case 1 -> {
+                final var invocation = actualInvocations.get(0);
+                arguments = invocation.getArguments();
+                invocation.markVerified();
+            }
+            default -> throw new MockitoException("This verifier can only be used with 1 invocation, got " + size);
         }
-        Invocation invocation = actualInvocations.get(0);
-        arguments = invocation.getArguments();
-        invocation.markVerified();
     }
 
     @Override
@@ -39,6 +38,11 @@ public class ArgumentsExtractorVerifier implements VerificationMode {
         return VerificationModeFactory.description(this, description);
     }
 
+    /**
+     * Return verified invocation arguments, or {@code null} if not verified.
+     *
+     * @return verified invocation arguments, or {@code null} if not verified
+     */
     public Object[] getArguments() {
         return arguments == null ? null : arguments.clone();
     }
