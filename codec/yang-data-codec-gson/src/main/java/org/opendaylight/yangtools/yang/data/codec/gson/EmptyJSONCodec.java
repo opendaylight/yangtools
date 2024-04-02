8c4ee5e27d6ba7f6d6415ad9eb4398e75e168aa2
Robert Varga <robert.varga@pantheon.tech>
2024-04-02 10:29:14 +0200

    Use JsonWriter.nullValue()
    We are passing a null String, which ends up calling to nullValue().
    Short-circuit this.
    
    Change-Id: I75423bee13cb2d73ef1b817b975a9cb155721c02
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit c42dafd31ad302197718b5fde00914adbdd13fb5)
    (cherry picked from commit 871844d401e90434c0ce39ee15169a0883bf9a58)

--- codec/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/EmptyJSONCodec.java
+++ codec/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/EmptyJSONCodec.java
@@ -32,8 +32,6 @@ final class EmptyJSONCodec implements JSONCodec<Empty> {
 
     @Override
     public void writeValue(final JsonWriter ctx, final Empty value) throws IOException {
-        ctx.beginArray();
-        ctx.value((String) null);
-        ctx.endArray();
+        ctx.beginArray().nullValue().endArray();
     }
 }
