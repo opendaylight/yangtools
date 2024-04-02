764e14ff71af88155f1f7369ca1c44727f01d167
Robert Varga <robert.varga@pantheon.tech>
2024-04-02 10:56:42 +0200

    Specialize JSONCodec to JSONValueWriter
    Completely disconnect JSONCodec from JsonWriter.
    
    JIRA: XXX
    Change-Id: I41f7426a11e76e90d717a898dfc916141e05ebf4
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- codec/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/JSONCodec.java
+++ codec/yang-data-codec-gson/src/main/java/org/opendaylight/yangtools/yang/data/codec/gson/JSONCodec.java
@@ -16,7 +16,7 @@ import org.opendaylight.yangtools.yang.data.util.codec.TypeAwareCodec;
  *
  * @param <T> Normalized value type
  */
-public sealed interface JSONCodec<T> extends TypeAwareCodec<T, Object, JsonWriter>
+public sealed interface JSONCodec<T> extends TypeAwareCodec<T, Object, JSONValueWriter>
         permits AbstractJSONCodec, EmptyJSONCodec, IdentityrefJSONCodec, NullJSONCodec, UnionJSONCodec,
                 // FIXME: rename this guy
                 JSONInstanceIdentifierCodec {
@@ -24,21 +24,8 @@ public sealed interface JSONCodec<T> extends TypeAwareCodec<T, Object, JsonWrite
      * {@inheritDoc}.
      *
      * @throws IOException if the write fails
-     * @deprecated Use {@link #writeValue(JSONValueWriter, Object)} instead.
      */
     @Override
-    @Deprecated(since = "13.0.3", forRemoval = true)
-    default void writeValue(final JsonWriter writer, final T value) throws IOException {
-        writeValue(new DefaultJSONValueWriter(writer), value);
-    }
-
-    /**
-     * Serialize specified value with specified {@link JSONValueWriter}.
-     *
-     * @param ctx Write context
-     * @param value Value in native format
-     * @throws IOException if the write fails
-     */
     void writeValue(JSONValueWriter ctx, T value) throws IOException;
 
     /**
