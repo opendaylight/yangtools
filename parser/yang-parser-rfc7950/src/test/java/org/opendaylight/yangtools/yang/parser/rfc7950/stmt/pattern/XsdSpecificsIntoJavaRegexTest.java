7a84d7792b0e445987b58caebc57a9636a5fd53b
Peter Suna <peter.suna@pantheon.tech>
2024-04-12 10:22:16 +0200

    Add test for XSD pattern specifics
    Create a test to reproduce the issue with converting XSD patterns
    into Java-compatible regex.
    
    JIRA: YANGTOOLS-1571
    Change-Id: Iab302638c3a436058f6de1d5dd5abd6e76fc411c
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

---
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/pattern/XsdSpecificsIntoJavaRegexTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.pattern;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.regex.Pattern;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class XsdSpecificsIntoJavaRegexTest {
+
+    @Test
+    void characterSubtractionGroupTest() {
+        final var javaRegexFromXSD = RegexUtils.getJavaRegexFromXSD("[a-z-[c]]");
+        assertEquals("^[a-z&&[^c]]$", javaRegexFromXSD);
+        final var pattern = Pattern.compile(javaRegexFromXSD);
+        assertTrue(pattern.matcher("a").find());
+        assertFalse(pattern.matcher("c").find());
+    }
+
+    @ParameterizedTest
+    @ValueSource(chars = {'c', 'C', 'd', 'D', 'i', 'I', 's', 'S', 'w', 'W'})
+    void multiCharEscapeTest(final char ch) {
+        final var javaRegexFromXSD = RegexUtils.getJavaRegexFromXSD("\\" + ch);
+        assertEquals("^\\\\" + ch + "$", javaRegexFromXSD);
+        assertNotNull(Pattern.compile(javaRegexFromXSD));
+    }
+}
