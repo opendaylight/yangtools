c511b2daf0506fee99181ef782ae78b1493b7418
tobias.pobocik <tobias.pobocik@pantheon.tech>
2024-05-23 15:16:28 +0200

    Fix JSONNormalizedNodeStreamWriter MapEntryNode Processing
    This commit resolves the issue where the JSONNormalizedNodeStreamWriter
    was failing to process MapEntryNode. The reason was the getParent()
    method not returning root if schemaStack.peek() is null.
    
    Also add test where root is used.
    
    JIRA: YANGTOOLS-1575
    Change-Id: If4f4afa758d9edd358e9a8e7f1f7665b0156d8b2
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- codec/yang-data-codec-gson/src/test/java/org/opendaylight/yangtools/yang/data/codec/gson/StreamToNormalizedNodeTest.java
+++ codec/yang-data-codec-gson/src/test/java/org/opendaylight/yangtools/yang/data/codec/gson/StreamToNormalizedNodeTest.java
@@ -10,17 +10,25 @@ package org.opendaylight.yangtools.yang.data.codec.gson;
 import static org.opendaylight.yangtools.yang.data.codec.gson.TestUtils.loadTextFile;
 
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 import java.io.File;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.Objects;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
 import org.opendaylight.yangtools.yang.data.api.schema.stream.LoggingNormalizedNodeStreamWriter;
 import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeWriter;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNormalizedNodeStreamWriter;
 import org.opendaylight.yangtools.yang.data.impl.schema.NormalizationResultHolder;
+import org.opendaylight.yangtools.yang.data.spi.node.ImmutableNodes;
+import org.opendaylight.yangtools.yang.data.util.DataSchemaContextTree;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -103,4 +111,51 @@ class StreamToNormalizedNodeTest extends AbstractComplexJsonTest {
         // Just to put it somewhere
         LOG.debug("Serialized JSON: {}", writer.toString());
     }
+
+    @Test
+    void immutableMapNodeStreamWriterDemonstration() throws Exception {
+        var configId = new YangInstanceIdentifier.NodeIdentifier(QName.create("http://example.com/basic-model", "config"));
+        var interfacesQName = QName.create("http://example.com/basic-model", "interfaces");
+        var interfaceQName = QName.create("http://example.com/basic-model", "interface");
+        var nameQName = QName.create("http://example.com/basic-model", "name");
+
+        var mtu = ImmutableNodes.leafNode(QName.create("http://example.com/basic-model", "mtu"), 1500);
+        var name = ImmutableNodes.leafNode(QName.create("http://example.com/basic-model", "name"), "admin");
+        var loopbackMode = ImmutableNodes.leafNode(QName.create("http://example.com/basic-model", "loopback-mode"), false);
+        var enabled = ImmutableNodes.leafNode(QName.create("http://example.com/basic-model", "enabled"), false);
+        var type = ImmutableNodes.leafNode(QName.create("http://example.com/basic-model", "type"),"IF_ETHERNET");
+
+        var interfaceIdWithPredicates = YangInstanceIdentifier.NodeIdentifierWithPredicates.of(interfaceQName, nameQName, "eth3");
+
+        var path = YangInstanceIdentifier.builder()
+            .node(interfacesQName)
+            .node(interfaceQName)
+            .nodeWithKey(interfaceQName, interfaceIdWithPredicates.asMap())
+            .build();
+
+        var configNodeBuilder = ImmutableNodes.newContainerBuilder().withNodeIdentifier(configId);
+        configNodeBuilder.withChild(mtu).withChild(name).withChild(loopbackMode).withChild(enabled).withChild(type);
+
+        var interfaceNode = ImmutableNodes.newMapEntryBuilder().withNodeIdentifier(interfaceIdWithPredicates)
+            .withChild(configNodeBuilder.build())
+            .withChild(ImmutableNodes.leafNode(QName.create("http://example.com/basic-model", "name"), "eth3"))
+            .build();
+
+        EffectiveModelContext context = YangParserTestUtils.parseYangResourceDirectory("/complexjson/yang");
+
+        var inference = DataSchemaContextTree.from(context)
+            .enterPath(Objects.requireNonNull(path))
+            .orElseThrow()
+            .stack()
+            .toInference();
+
+        var writer = new StringWriter();
+        var jsonWriter = new JsonWriter(writer);
+        var namespace = interfaceNode.name().getNodeType().getNamespace();
+        var nodeWriter = JSONNormalizedNodeStreamWriter
+            .createNestedWriter(lhotkaCodecFactory, inference, namespace, jsonWriter);
+        var normalizedNodeWriter = NormalizedNodeWriter.forStreamWriter(nodeWriter);
+        normalizedNodeWriter.write(interfaceNode);
+    }
+
 }
