f72852224d30981bed04b4e11362d0fae74c629d
Robert Varga <robert.varga@pantheon.tech>
2024-05-28 16:54:56 +0200

    Remove unused @SuppressFBWarnings
    Upgraded SpotBugs no longer reports a false positive, fix that up,
    eliminating the dependency from yang-test-util.
    
    Change-Id: Ib3ad5125fac3d8b2c360a10bf0eb3572b87e7c35
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-test-util/src/main/java/org/opendaylight/yangtools/yang/test/util/YangParserTestUtils.java
+++ parser/yang-test-util/src/main/java/org/opendaylight/yangtools/yang/test/util/YangParserTestUtils.java
@@ -7,7 +7,6 @@
  */
 package org.opendaylight.yangtools.yang.test.util;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
@@ -257,7 +256,6 @@ public final class YangParserTestUtils {
      * @param config parser configuration
      * @return effective schema context
      */
-    @SuppressFBWarnings(value = "NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE", justification = "Wrong inferent on listFiles")
     public static @NonNull EffectiveModelContext parseYangResourceDirectory(final String resourcePath,
             final Set<QName> supportedFeatures, final YangParserConfiguration config) {
         final URI directoryPath;
@@ -383,7 +381,6 @@ public final class YangParserTestUtils {
             Arrays.stream(sources).map(YangParserTestUtils::createYangTextSource).toList());
     }
 
-    @SuppressFBWarnings(value = "NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE", justification = "Wrong inferent on listFiles")
     private static Collection<File> getYangFiles(final String resourcePath) {
         final URI directoryPath;
         try {
