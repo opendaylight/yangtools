0a51f7b05d4edbdd920ddfcbbf44f046359725e6
Robert Varga <robert.varga@pantheon.tech>
2024-05-29 12:13:13 +0200

    WIP: Integrate mapping
    Change-Id: I2cf606ac6c04d0810860224b92bcf3db1403b80c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- common/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/OperationFailedException.java
+++ common/yang-common/src/main/java/org/opendaylight/yangtools/yang/common/OperationFailedException.java
@@ -16,6 +16,9 @@ import java.io.Serial;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+import java.util.function.BiFunction;
 
 /**
  * A general base exception for an operation failure.
@@ -117,5 +120,35 @@ public class OperationFailedException extends Exception {
 
     protected ToStringHelper addToStringAttributes(final ToStringHelper helper) {
         return helper.add("message", getMessage()).add("errorList", getErrorList());
+    }
+
+    protected static <X extends OperationFailedException> X ofCaught(final Throwable caught, final Class<X> type,
+            final String operationName, final BiFunction<String, Throwable, X> toException) {
+        // If the cause is a TransactionCommitFailedException, return that
+        if (type.isInstance(caught)) {
+            return type.cast(caught);
+        }
+
+        // Unmap ExecutionException's cause if possible
+        if (caught instanceof ExecutionException e) {
+            final var cause = e.getCause();
+            if (type.isInstance(cause)) {
+                return type.cast(caught);
+            } else if (cause != null) {
+                return toException.apply(operationName + " execution failed", cause);
+            }
+        }
+
+        // Otherwise return an instance of the specified type with the original cause.
+        final String message;
+        if (caught instanceof InterruptedException) {
+            message = operationName + " was interupted";
+        } else if (caught instanceof CancellationException) {
+            message = operationName + " was cancelled";
+        } else {
+            // We really should not get here but need to cover it anyway for completeness.
+            message = operationName + " encountered unexpected failure";
+        }
+        return toException.apply(message, caught);
     }
 }
