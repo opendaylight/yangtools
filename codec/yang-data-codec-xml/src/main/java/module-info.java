f668458abbe16c4895819806506e27102db93d94
Robert Varga <robert.varga@pantheon.tech>
2024-05-31 19:08:55 +0200

    Clean up codec-xml's module-info
    Use proper order of requires.
    
    Change-Id: I21f66c9818629731cdc779f7e7e82bb51577370a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- codec/yang-data-codec-xml/src/main/java/module-info.java
+++ codec/yang-data-codec-xml/src/main/java/module-info.java
@@ -16,8 +16,10 @@ module org.opendaylight.yangtools.yang.data.codec.xml {
     requires com.google.common;
     requires org.codehaus.stax2;
     requires org.opendaylight.yangtools.util;
-    requires org.opendaylight.yangtools.yang.model.util;
+    requires org.opendaylight.yangtools.yang.common;
     requires org.opendaylight.yangtools.yang.data.impl;
+    requires org.opendaylight.yangtools.yang.data.spi;
+    requires org.opendaylight.yangtools.yang.model.util;
     requires org.opendaylight.yangtools.rfc7952.model.api;
     requires org.opendaylight.yangtools.rfc8528.model.api;
     requires org.slf4j;
@@ -25,5 +27,4 @@ module org.opendaylight.yangtools.yang.data.codec.xml {
     // Annotations
     requires static com.google.errorprone.annotations;
     requires static org.eclipse.jdt.annotation;
-    requires org.opendaylight.yangtools.yang.data.spi;
 }
