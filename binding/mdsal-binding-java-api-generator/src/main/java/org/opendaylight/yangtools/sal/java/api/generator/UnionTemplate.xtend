opendaylight/mdsal

Fix getValue() of bits in union
Call Arrays.toString instead of calling toString directly for bits in union,
since getValue() of bits returns boolean[].

Change-Id: I4b06c95f07ff6d5ff81dc17613ed972b453db184
Signed-off-by: Xiao Liang <shaw.leon@gmail.com>

binding/mdsal-binding-java-api-generator/src/main/java/org/opendaylight/yangtools/sal/java/api/generator/UnionTemplate.xtend
@@ -13,6 +13,7 @@ import java.beans.ConstructorProperties
 import org.opendaylight.yangtools.sal.binding.model.api.GeneratedProperty
 import org.opendaylight.yangtools.sal.binding.model.api.GeneratedTransferObject
 import org.opendaylight.yangtools.sal.binding.model.api.Enumeration
+import org.opendaylight.yangtools.yang.model.api.type.BitsTypeDefinition
 
 /**
  * Template for generating JAVA class.
@@ -159,6 +160,11 @@ class UnionTemplate extends ClassTemplate {
                                     && "byte[]".equals((propRet as GeneratedTransferObject).properties.get(0).returnType.name)»
                                 ««« generated byte[] typedef
                                 «field.fieldName» = BaseEncoding.base64().encode(«property.fieldName».getValue()).toCharArray();
+                            «ELSEIF propRet instanceof GeneratedTransferObject // Is it a GeneratedTransferObject
+                                    && (propRet as GeneratedTransferObject).typedef  // Is it a typedef
+                                    && (propRet as GeneratedTransferObject).getBaseType instanceof BitsTypeDefinition»
+                                ««« generated bits typedef
+                                «field.fieldName» = java.util.Arrays.toString(«property.fieldName».getValue()).toCharArray();
                             «ELSE»
                                 ««« generated type
                                 «field.fieldName» = «property.fieldName».getValue().toString().toCharArray();

opendaylight/yangtools

binding/mdsal-binding-java-api-generator/src/main/java/org/opendaylight/yangtools/sal/java/api/generator/UnionTemplate.xtend
@@ -13,6 +13,7 @@ import java.beans.ConstructorProperties
 import org.opendaylight.yangtools.sal.binding.model.api.GeneratedProperty
 import org.opendaylight.yangtools.sal.binding.model.api.GeneratedTransferObject
 import org.opendaylight.yangtools.sal.binding.model.api.Enumeration
+import org.opendaylight.yangtools.yang.model.api.type.BitsTypeDefinition
 
 /**
  * Template for generating JAVA class.
@@ -159,6 +160,11 @@ class UnionTemplate extends ClassTemplate {
                                     && "byte[]".equals((propRet as GeneratedTransferObject).properties.get(0).returnType.name)»
                                 ««« generated byte[] typedef
                                 «field.fieldName» = BaseEncoding.base64().encode(«property.fieldName».getValue()).toCharArray();
+                            «ELSEIF propRet instanceof GeneratedTransferObject // Is it a GeneratedTransferObject
+                                    && (propRet as GeneratedTransferObject).typedef  // Is it a typedef
+                                    && (propRet as GeneratedTransferObject).baseType instanceof BitsTypeDefinition»
+                                ««« generated bits typedef
+                                «field.fieldName» = java.util.Arrays.toString(«property.fieldName».getValue()).toCharArray();
                             «ELSE»
                                 ««« generated type
                                 «field.fieldName» = «property.fieldName».getValue().toString().toCharArray();