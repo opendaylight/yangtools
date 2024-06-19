opendaylight/mdsal

BUG-6028: check value types for encapsulation
This is a fast check to see if the argument matches expected
value class. If it does not it does not make sense to invoke
the serializer, as it will fail.

Change-Id: I4ca556fea057f2188fa1c34d303467565af38d5c
Signed-off-by: Robert Varga <rovarga@cisco.com>

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/yangtools/binding/data/codec/impl/UnionValueOptionContext.java
@@ -51,8 +51,12 @@ final class UnionValueOptionContext {
     }
 
     Object deserializeUnion(final Object input) {
-        final Object value;
+        // Side-step potential exceptions by checking the type if it is available
+        if (codec instanceof EncapsulatedValueCodec && !((EncapsulatedValueCodec) codec).acceptableTypeVlaue(input)) {
+            return null;
+        }
 
+        final Object value;
         try {
             value = codec.deserialize(input);
         } catch (Exception e) {

opendaylight/yangtools

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/yangtools/binding/data/codec/impl/UnionValueOptionContext.java
@@ -51,8 +51,12 @@ final class UnionValueOptionContext {
     }
 
     Object deserializeUnion(final Object input) {
-        final Object value;
+        // Side-step potential exceptions by checking the type if it is available
+        if (codec instanceof EncapsulatedValueCodec && !((EncapsulatedValueCodec) codec).canAcceptObject(input)) {
+            return null;
+        }
 
+        final Object value;
         try {
             value = codec.deserialize(input);
         } catch (Exception e) {