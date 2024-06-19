opendaylight/mdsal

Refactor augment generator linkage
The uses/augment and module/augment cases are quite different from
the perspective how we map their argument reference to generators.

The uses/augment case is quite simple, as it refers to grouping's nodes,
not augmentations. The only variation we need to account for is the
skip back to the defining grouping.

The module/augment case, on the other hand, is more complex, as it can
refer to augmentations -- hence it need to account for changes namespace
changes along uses/grouping as well as back from grouping child to
augmentations.

JIRA: MDSAL-715
Change-Id: I7425301afa6de566d985d19450ccfc64ea527119
Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/AbstractCompositeGenerator.java
@@ -23,6 +23,7 @@ import org.opendaylight.mdsal.binding.model.api.GeneratedType;
 import org.opendaylight.mdsal.binding.model.api.type.builder.GeneratedTypeBuilder;
 import org.opendaylight.mdsal.binding.model.ri.BindingTypes;
 import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.model.api.AddedByUsesAware;
 import org.opendaylight.yangtools.yang.model.api.CopyableNode;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
@@ -42,6 +43,7 @@ import org.opendaylight.yangtools.yang.model.api.stmt.ListEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.NotificationEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.OutputEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.RpcEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
 import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.TypedefEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.UsesEffectiveStatement;
@@ -140,21 +142,11 @@ abstract class AbstractCompositeGenerator<T extends EffectiveStatement<?, ?>> ex
     }
 
     final void linkUsesDependencies(final GeneratorContext context) {
-        // We are establishing two linkages here:
-        // - we are resolving 'uses' statements to their corresponding 'grouping' definitions
-        // - we propagate those groupings as anchors to any augment statements
+        // We are resolving 'uses' statements to their corresponding 'grouping' definitions
         final List<GroupingGenerator> tmp = new ArrayList<>();
         for (EffectiveStatement<?, ?> stmt : statement().effectiveSubstatements()) {
             if (stmt instanceof UsesEffectiveStatement) {
-                final UsesEffectiveStatement uses = (UsesEffectiveStatement) stmt;
-                final GroupingGenerator grouping = context.resolveTreeScoped(GroupingGenerator.class, uses.argument());
-                tmp.add(grouping);
-
-                for (Generator gen : this) {
-                    if (gen instanceof UsesAugmentGenerator) {
-                        ((UsesAugmentGenerator) gen).linkGroupingDependency(uses, grouping);
-                    }
-                }
+                tmp.add(context.resolveTreeScoped(GroupingGenerator.class, ((UsesEffectiveStatement) stmt).argument()));
             }
         }
         groupings = List.copyOf(tmp);
@@ -189,7 +181,7 @@ abstract class AbstractCompositeGenerator<T extends EffectiveStatement<?, ?>> ex
     }
 
     @Override
-    final @Nullable AbstractExplicitGenerator<?> findSchemaTreeGenerator(final QName qname) {
+    final AbstractExplicitGenerator<?> findSchemaTreeGenerator(final QName qname) {
         final AbstractExplicitGenerator<?> found = super.findSchemaTreeGenerator(qname);
         return found != null ? found : findInferredGenerator(qname);
     }
@@ -211,6 +203,56 @@ abstract class AbstractCompositeGenerator<T extends EffectiveStatement<?, ?>> ex
             }
         }
         return null;
+    }
+
+    final @NonNull AbstractExplicitGenerator<?> getSchemaTreeGenerator(final SchemaNodeIdentifier path) {
+        // This is not quite straightforward. 'path' works on top of schema tree, which is instantiated view. Since we
+        // do not generate duplicate instantiations along 'uses' path, findSchemaTreeGenerator() would satisfy our
+        // request by returning a child of the source 'grouping'.
+        //
+        // When that happens, our subsequent lookups need to adjust the namespace being looked up to the grouping's
+        // namespace... except for the case when the step is actually an augmentation, in which case we must not make
+        // that adjustment.
+        //
+        // Hence we deal with this lookup recursively, dropping namespace hints when we cross into groupings.
+        return getSchemaTreeGenerator(path.getNodeIdentifiers().iterator(), null);
+    }
+
+    private @NonNull AbstractExplicitGenerator<?> getSchemaTreeGenerator(final Iterator<QName> qnames,
+            final @Nullable QNameModule localNamespace) {
+        final QName qname = qnames.next();
+
+        // First try local augments, as those are guaranteed to match namespace exactly
+        for (AbstractAugmentGenerator augment : augments) {
+            final AbstractExplicitGenerator<?> gen = augment.findSchemaTreeGenerator(qname);
+            if (gen != null) {
+                return continueLookup(gen, qnames, null);
+            }
+        }
+
+        // Second try local groupings, as those perform their own adjustment
+        for (GroupingGenerator grouping : groupings) {
+            final QNameModule ns = grouping.statement().argument().getModule();
+            final AbstractExplicitGenerator<?> gen = grouping.findSchemaTreeGenerator(qname.bindTo(ns));
+            if (gen != null) {
+                return continueLookup(gen, qnames, ns);
+            }
+        }
+
+        // Lastly try local statements adjusted with namespace, if applicable
+        final QName lookup = localNamespace == null ? qname : qname.bindTo(localNamespace);
+        final AbstractExplicitGenerator<?> gen = verifyNotNull(super.findSchemaTreeGenerator(lookup),
+            "Failed to find %s as %s in %s", qname, lookup, this);
+        return continueLookup(gen, qnames, localNamespace);
+    }
+
+    private static @NonNull AbstractExplicitGenerator<?> continueLookup(final @NonNull AbstractExplicitGenerator<?> gen,
+            final Iterator<QName> qnames, final QNameModule localNamespace) {
+        if (qnames.hasNext()) {
+            verify(gen instanceof AbstractCompositeGenerator, "Unexpected generator %s", gen);
+            return ((AbstractCompositeGenerator<?>) gen).getSchemaTreeGenerator(qnames, localNamespace);
+        }
+        return gen;
     }
 
     /**
@@ -338,7 +380,7 @@ abstract class AbstractCompositeGenerator<T extends EffectiveStatement<?, ?>> ex
                 final UsesEffectiveStatement uses = (UsesEffectiveStatement) stmt;
                 for (EffectiveStatement<?, ?> usesSub : uses.effectiveSubstatements()) {
                     if (usesSub instanceof AugmentEffectiveStatement) {
-                        tmpAug.add(new UsesAugmentGenerator((AugmentEffectiveStatement) usesSub, this, uses));
+                        tmpAug.add(new UsesAugmentGenerator((AugmentEffectiveStatement) usesSub, this));
                     }
                 }
             } else {

binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/ModuleAugmentGenerator.java
@@ -8,6 +8,7 @@
 package org.opendaylight.mdsal.binding.generator.impl.reactor;
 
 import org.opendaylight.yangtools.yang.model.api.stmt.AugmentEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
 
 /**
  * Generator corresponding to a {@code augment} statement used as a child of a {@code module} statement.
@@ -23,19 +24,8 @@ final class ModuleAugmentGenerator extends AbstractAugmentGenerator {
     }
 
     void linkAugmentationTarget(final GeneratorContext context) {
-        // FIXME: do we need two-step resolution here? we probably have solved this somehow, or it's part of...
-        // FIXME: MDSAL-696: this looks like the sort of check which should be involved in replacing getOriginal()
-        //
-        //      if (targetSchemaNode instanceof DataSchemaNode && ((DataSchemaNode) targetSchemaNode).isAddedByUses()) {
-        //          if (targetSchemaNode instanceof DerivableSchemaNode) {
-        //              targetSchemaNode = ((DerivableSchemaNode) targetSchemaNode).getOriginal().orElse(null);
-        //          }
-        //          if (targetSchemaNode == null) {
-        //              throw new IllegalStateException("Failed to find target node from grouping in augmentation "
-        //                  + augSchema + " in module " + context.module().getName());
-        //          }
-        //      }
-
-        setTargetGenerator(context.resolveSchemaNode(statement().argument()));
+        final SchemaNodeIdentifier path = statement().argument();
+        final ModuleGenerator module = context.resolveModule(path.firstNodeIdentifier().getModule());
+        setTargetGenerator(module.getSchemaTreeGenerator(path));
     }
 }

binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/UsesAugmentGenerator.java
@@ -7,51 +7,26 @@
  */
 package org.opendaylight.mdsal.binding.generator.impl.reactor;
 
-import static com.google.common.base.Verify.verify;
-import static com.google.common.base.Verify.verifyNotNull;
-import static java.util.Objects.requireNonNull;
-
 import org.opendaylight.yangtools.yang.model.api.stmt.AugmentEffectiveStatement;
-import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
-import org.opendaylight.yangtools.yang.model.api.stmt.UsesEffectiveStatement;
 
 /**
  * Generator corresponding to a {@code augment} statement used as a child of a {@code uses} statement.
  */
 final class UsesAugmentGenerator extends AbstractAugmentGenerator {
-    private final UsesEffectiveStatement uses;
-
-    private GroupingGenerator grouping;
-
-    UsesAugmentGenerator(final AugmentEffectiveStatement statement, final AbstractCompositeGenerator<?> parent,
-            final UsesEffectiveStatement uses) {
+    UsesAugmentGenerator(final AugmentEffectiveStatement statement, final AbstractCompositeGenerator<?> parent) {
         super(statement, parent);
-        this.uses = requireNonNull(uses);
-    }
-
-    void linkGroupingDependency(final UsesEffectiveStatement checkUses, final GroupingGenerator resolvedGrouping) {
-        if (uses.equals(checkUses)) {
-            verify(grouping == null, "Attempted to relink %s from %s to %s", this, grouping, resolvedGrouping);
-            this.grouping = requireNonNull(resolvedGrouping);
-        }
     }
 
     @Override
     void loadTargetGenerator() {
-        final GroupingGenerator grp = verifyNotNull(grouping, "No grouping linked in %s", this);
-        final SchemaNodeIdentifier path = statement().argument();
-
-        /*
-         *  Here we are going in the opposite direction of RFC7950, section 3.13:
-         *
-         *        The effect of a "uses" reference to a grouping is that the nodes
-         *        defined by the grouping are copied into the current schema tree and
-         *        are then updated according to the "refine" and "augment" statements.
-         *
-         *  Our argument is composed of QNames in the current schema tree's namespace, but the grouping may have been
-         *  defined in a different module -- and therefore it knows those children under its namespace. Adjust the path
-         *  we are searching if that happens to be the case.
-         */
-        setTargetGenerator(grp.resolveSchemaNode(path, grp.statement().argument().getModule()));
+        // Here we are going in the opposite direction of RFC7950, section 7.13:
+        //
+        //    The effect of a "uses" reference to a grouping is that the nodes
+        //    defined by the grouping are copied into the current schema tree and
+        //    are then updated according to the "refine" and "augment" statements.
+        //
+        // Our parent here is *not* the uses statement, but rather the statement which contains uses -- and its
+        // getSchemaTreeGenerator() is well equipped to deal with the namespace hopping needed to perform the lookups
+        setTargetGenerator(getParent().getSchemaTreeGenerator(statement().argument()));
     }
 }

opendaylight/yangtools

binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/AbstractCompositeGenerator.java
@@ -23,6 +23,7 @@ import org.opendaylight.mdsal.binding.model.api.GeneratedType;
 import org.opendaylight.mdsal.binding.model.api.type.builder.GeneratedTypeBuilder;
 import org.opendaylight.mdsal.binding.model.ri.BindingTypes;
 import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.model.api.AddedByUsesAware;
 import org.opendaylight.yangtools.yang.model.api.CopyableNode;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
@@ -42,6 +43,7 @@ import org.opendaylight.yangtools.yang.model.api.stmt.ListEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.NotificationEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.OutputEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.RpcEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
 import org.opendaylight.yangtools.yang.model.api.stmt.SchemaTreeEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.TypedefEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.UsesEffectiveStatement;
@@ -140,21 +142,11 @@ abstract class AbstractCompositeGenerator<T extends EffectiveStatement<?, ?>> ex
     }
 
     final void linkUsesDependencies(final GeneratorContext context) {
-        // We are establishing two linkages here:
-        // - we are resolving 'uses' statements to their corresponding 'grouping' definitions
-        // - we propagate those groupings as anchors to any augment statements
+        // We are resolving 'uses' statements to their corresponding 'grouping' definitions
         final List<GroupingGenerator> tmp = new ArrayList<>();
         for (EffectiveStatement<?, ?> stmt : statement().effectiveSubstatements()) {
             if (stmt instanceof UsesEffectiveStatement) {
-                final UsesEffectiveStatement uses = (UsesEffectiveStatement) stmt;
-                final GroupingGenerator grouping = context.resolveTreeScoped(GroupingGenerator.class, uses.argument());
-                tmp.add(grouping);
-
-                for (Generator gen : this) {
-                    if (gen instanceof UsesAugmentGenerator) {
-                        ((UsesAugmentGenerator) gen).linkGroupingDependency(uses, grouping);
-                    }
-                }
+                tmp.add(context.resolveTreeScoped(GroupingGenerator.class, ((UsesEffectiveStatement) stmt).argument()));
             }
         }
         groupings = List.copyOf(tmp);
@@ -189,7 +181,7 @@ abstract class AbstractCompositeGenerator<T extends EffectiveStatement<?, ?>> ex
     }
 
     @Override
-    final @Nullable AbstractExplicitGenerator<?> findSchemaTreeGenerator(final QName qname) {
+    final AbstractExplicitGenerator<?> findSchemaTreeGenerator(final QName qname) {
         final AbstractExplicitGenerator<?> found = super.findSchemaTreeGenerator(qname);
         return found != null ? found : findInferredGenerator(qname);
     }
@@ -211,6 +203,56 @@ abstract class AbstractCompositeGenerator<T extends EffectiveStatement<?, ?>> ex
             }
         }
         return null;
+    }
+
+    final @NonNull AbstractExplicitGenerator<?> resolveSchemaNode(final SchemaNodeIdentifier path) {
+        // This is not quite straightforward. 'path' works on top of schema tree, which is instantiated view. Since we
+        // do not generate duplicate instantiations along 'uses' path, findSchemaTreeGenerator() would satisfy our
+        // request by returning a child of the source 'grouping'.
+        //
+        // When that happens, our subsequent lookups need to adjust the namespace being looked up to the grouping's
+        // namespace... except for the case when the step is actually an augmentation, in which case we must not make
+        // that adjustment.
+        //
+        // Hence we deal with this lookup recursively, dropping namespace hints when we cross into groupings.
+        return resolveSchemaNode(path.getNodeIdentifiers().iterator(), null);
+    }
+
+    private @NonNull AbstractExplicitGenerator<?> resolveSchemaNode(final Iterator<QName> qnames,
+            final @Nullable QNameModule localNamespace) {
+        final QName qname = qnames.next();
+
+        // First try local augments, as those are guaranteed to match namespace exactly
+        for (AbstractAugmentGenerator augment : augments) {
+            final AbstractExplicitGenerator<?> gen = augment.findSchemaTreeGenerator(qname);
+            if (gen != null) {
+                return resolveNext(gen, qnames, null);
+            }
+        }
+
+        // Second try local groupings, as those perform their own adjustment
+        for (GroupingGenerator grouping : groupings) {
+            final QNameModule ns = grouping.statement().argument().getModule();
+            final AbstractExplicitGenerator<?> gen = grouping.findSchemaTreeGenerator(qname.bindTo(ns));
+            if (gen != null) {
+                return resolveNext(gen, qnames, ns);
+            }
+        }
+
+        // Lastly try local statements adjusted with namespace, if applicable
+        final QName lookup = localNamespace == null ? qname : qname.bindTo(localNamespace);
+        final AbstractExplicitGenerator<?> gen = verifyNotNull(super.findSchemaTreeGenerator(lookup),
+            "Failed to find %s as %s in %s", qname, lookup, this);
+        return resolveNext(gen, qnames, localNamespace);
+    }
+
+    private static @NonNull AbstractExplicitGenerator<?> resolveNext(final @NonNull AbstractExplicitGenerator<?> gen,
+            final Iterator<QName> qnames, final QNameModule localNamespace) {
+        if (qnames.hasNext()) {
+            verify(gen instanceof AbstractCompositeGenerator, "Unexpected generator %s", gen);
+            return ((AbstractCompositeGenerator<?>) gen).resolveSchemaNode(qnames, localNamespace);
+        }
+        return gen;
     }
 
     /**
@@ -338,7 +380,7 @@ abstract class AbstractCompositeGenerator<T extends EffectiveStatement<?, ?>> ex
                 final UsesEffectiveStatement uses = (UsesEffectiveStatement) stmt;
                 for (EffectiveStatement<?, ?> usesSub : uses.effectiveSubstatements()) {
                     if (usesSub instanceof AugmentEffectiveStatement) {
-                        tmpAug.add(new UsesAugmentGenerator((AugmentEffectiveStatement) usesSub, this, uses));
+                        tmpAug.add(new UsesAugmentGenerator((AugmentEffectiveStatement) usesSub, this));
                     }
                 }
             } else {

binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/ModuleAugmentGenerator.java
@@ -8,6 +8,7 @@
 package org.opendaylight.mdsal.binding.generator.impl.reactor;
 
 import org.opendaylight.yangtools.yang.model.api.stmt.AugmentEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
 
 /**
  * Generator corresponding to a {@code augment} statement used as a child of a {@code module} statement.
@@ -23,19 +24,8 @@ final class ModuleAugmentGenerator extends AbstractAugmentGenerator {
     }
 
     void linkAugmentationTarget(final GeneratorContext context) {
-        // FIXME: do we need two-step resolution here? we probably have solved this somehow, or it's part of...
-        // FIXME: MDSAL-696: this looks like the sort of check which should be involved in replacing getOriginal()
-        //
-        //      if (targetSchemaNode instanceof DataSchemaNode && ((DataSchemaNode) targetSchemaNode).isAddedByUses()) {
-        //          if (targetSchemaNode instanceof DerivableSchemaNode) {
-        //              targetSchemaNode = ((DerivableSchemaNode) targetSchemaNode).getOriginal().orElse(null);
-        //          }
-        //          if (targetSchemaNode == null) {
-        //              throw new IllegalStateException("Failed to find target node from grouping in augmentation "
-        //                  + augSchema + " in module " + context.module().getName());
-        //          }
-        //      }
-
-        setTargetGenerator(context.resolveSchemaNode(statement().argument()));
+        final SchemaNodeIdentifier path = statement().argument();
+        final ModuleGenerator module = context.resolveModule(path.firstNodeIdentifier().getModule());
+        setTargetGenerator(module.resolveSchemaNode(path));
     }
 }

binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/UsesAugmentGenerator.java
@@ -7,51 +7,26 @@
  */
 package org.opendaylight.mdsal.binding.generator.impl.reactor;
 
-import static com.google.common.base.Verify.verify;
-import static com.google.common.base.Verify.verifyNotNull;
-import static java.util.Objects.requireNonNull;
-
 import org.opendaylight.yangtools.yang.model.api.stmt.AugmentEffectiveStatement;
-import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
-import org.opendaylight.yangtools.yang.model.api.stmt.UsesEffectiveStatement;
 
 /**
  * Generator corresponding to a {@code augment} statement used as a child of a {@code uses} statement.
  */
 final class UsesAugmentGenerator extends AbstractAugmentGenerator {
-    private final UsesEffectiveStatement uses;
-
-    private GroupingGenerator grouping;
-
-    UsesAugmentGenerator(final AugmentEffectiveStatement statement, final AbstractCompositeGenerator<?> parent,
-            final UsesEffectiveStatement uses) {
+    UsesAugmentGenerator(final AugmentEffectiveStatement statement, final AbstractCompositeGenerator<?> parent) {
         super(statement, parent);
-        this.uses = requireNonNull(uses);
-    }
-
-    void linkGroupingDependency(final UsesEffectiveStatement checkUses, final GroupingGenerator resolvedGrouping) {
-        if (uses.equals(checkUses)) {
-            verify(grouping == null, "Attempted to relink %s from %s to %s", this, grouping, resolvedGrouping);
-            this.grouping = requireNonNull(resolvedGrouping);
-        }
     }
 
     @Override
     void loadTargetGenerator() {
-        final GroupingGenerator grp = verifyNotNull(grouping, "No grouping linked in %s", this);
-        final SchemaNodeIdentifier path = statement().argument();
-
-        /*
-         *  Here we are going in the opposite direction of RFC7950, section 3.13:
-         *
-         *        The effect of a "uses" reference to a grouping is that the nodes
-         *        defined by the grouping are copied into the current schema tree and
-         *        are then updated according to the "refine" and "augment" statements.
-         *
-         *  Our argument is composed of QNames in the current schema tree's namespace, but the grouping may have been
-         *  defined in a different module -- and therefore it knows those children under its namespace. Adjust the path
-         *  we are searching if that happens to be the case.
-         */
-        setTargetGenerator(grp.resolveSchemaNode(path, grp.statement().argument().getModule()));
+        // Here we are going in the opposite direction of RFC7950, section 7.13:
+        //
+        //    The effect of a "uses" reference to a grouping is that the nodes
+        //    defined by the grouping are copied into the current schema tree and
+        //    are then updated according to the "refine" and "augment" statements.
+        //
+        // Our parent here is *not* the uses statement, but rather the statement which contains uses -- and its
+        // getSchemaTreeGenerator() is well equipped to deal with the namespace hopping needed to perform the lookups
+        setTargetGenerator(getParent().resolveSchemaNode(statement().argument()));
     }
 }