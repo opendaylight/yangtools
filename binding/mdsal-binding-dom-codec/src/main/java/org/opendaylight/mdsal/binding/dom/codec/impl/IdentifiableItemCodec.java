opendaylight/mdsal

Optimize IdentifiableItemCodec a bit
Do not force instantiation of the backing map, as for single-item
NodeIdentifierWithPredicates it is just wasteful.

Change-Id: I0cc13d0afe2f3329f4757e9da414500de132acf6
Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/impl/IdentifiableItemCodec.java
@@ -56,8 +56,8 @@ abstract class IdentifiableItemCodec
         }
 
         @Override
-        Identifier<?> deserializeIdentifier(final Map<QName, Object> keyValues) throws Throwable {
-            return (Identifier<?>) ctor.invokeExact(keyContext.deserialize(keyValues.get(keyName)));
+        Identifier<?> deserializeIdentifierImpl(final NodeIdentifierWithPredicates nip) throws Throwable {
+            return (Identifier<?>) ctor.invokeExact(keyContext.deserialize(nip.getValue(keyName)));
         }
 
         @Override
@@ -102,11 +102,11 @@ abstract class IdentifiableItemCodec
         }
 
         @Override
-        Identifier<?> deserializeIdentifier(final Map<QName, Object> keyValues) throws Throwable {
+        Identifier<?> deserializeIdentifierImpl(final NodeIdentifierWithPredicates nip) throws Throwable {
             final Object[] bindingValues = new Object[keysInBindingOrder.size()];
             int offset = 0;
             for (final QName key : keysInBindingOrder) {
-                bindingValues[offset++] = keyValueContexts.get(key).deserialize(keyValues.get(key));
+                bindingValues[offset++] = keyValueContexts.get(key).deserialize(nip.getValue(key));
             }
 
             return (Identifier<?>) ctor.invokeExact(bindingValues);
@@ -162,7 +162,7 @@ abstract class IdentifiableItemCodec
     @SuppressWarnings("checkstyle:illegalCatch")
     final @NonNull Identifier<?> deserializeIdentifier(final NodeIdentifierWithPredicates input) {
         try {
-            return deserializeIdentifier(input.asMap());
+            return deserializeIdentifier(input);
         } catch (Throwable e) {
             Throwables.throwIfUnchecked(e);
             throw new IllegalStateException("Failed to deserialize " + input, e);
@@ -170,7 +170,8 @@ abstract class IdentifiableItemCodec
     }
 
     @SuppressWarnings("checkstyle:illegalThrows")
-    abstract @NonNull Identifier<?> deserializeIdentifier(Map<QName, Object> keyValues) throws Throwable;
+    abstract @NonNull Identifier<?> deserializeIdentifierImpl(@NonNull NodeIdentifierWithPredicates keyValues)
+            throws Throwable;
 
     abstract @NonNull NodeIdentifierWithPredicates serializeIdentifier(QName qname, Identifier<?> key);
 

opendaylight/yangtools

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/impl/IdentifiableItemCodec.java
@@ -53,8 +53,8 @@ abstract class IdentifiableItemCodec
         }
 
         @Override
-        Identifier<?> deserializeIdentifier(final Map<QName, Object> keyValues) throws Throwable {
-            return (Identifier<?>) ctor.invokeExact(keyContext.deserialize(keyValues.get(keyName)));
+        Identifier<?> deserializeIdentifierImpl(final NodeIdentifierWithPredicates nip) throws Throwable {
+            return (Identifier<?>) ctor.invokeExact(keyContext.deserialize(nip.getValue(keyName)));
         }
 
         @Override
@@ -98,11 +98,11 @@ abstract class IdentifiableItemCodec
         }
 
         @Override
-        Identifier<?> deserializeIdentifier(final Map<QName, Object> keyValues) throws Throwable {
+        Identifier<?> deserializeIdentifierImpl(final NodeIdentifierWithPredicates nip) throws Throwable {
             final Object[] bindingValues = new Object[keysInBindingOrder.size()];
             int offset = 0;
             for (final QName key : keysInBindingOrder) {
-                bindingValues[offset++] = keyValueContexts.get(key).deserialize(keyValues.get(key));
+                bindingValues[offset++] = keyValueContexts.get(key).deserialize(nip.getValue(key));
             }
 
             return (Identifier<?>) ctor.invokeExact(bindingValues);
@@ -158,7 +158,7 @@ abstract class IdentifiableItemCodec
     @SuppressWarnings("checkstyle:illegalCatch")
     final @NonNull Identifier<?> deserializeIdentifier(final NodeIdentifierWithPredicates input) {
         try {
-            return deserializeIdentifier(input.asMap());
+            return deserializeIdentifierImpl(input);
         } catch (Throwable e) {
             Throwables.throwIfUnchecked(e);
             throw new IllegalStateException("Failed to deserialize " + input, e);
@@ -166,7 +166,8 @@ abstract class IdentifiableItemCodec
     }
 
     @SuppressWarnings("checkstyle:illegalThrows")
-    abstract @NonNull Identifier<?> deserializeIdentifier(Map<QName, Object> keyValues) throws Throwable;
+    abstract @NonNull Identifier<?> deserializeIdentifierImpl(@NonNull NodeIdentifierWithPredicates nip)
+            throws Throwable;
 
     abstract @NonNull NodeIdentifierWithPredicates serializeIdentifier(QName qname, Identifier<?> key);
 