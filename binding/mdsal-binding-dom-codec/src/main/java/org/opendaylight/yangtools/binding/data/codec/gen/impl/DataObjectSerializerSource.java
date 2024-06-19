opendaylight/mdsal

BUG-2062: handle ordered leaf-lists
This is forward-port on the Lithium fix, migrated to the mdsal project.

Change-Id: I46d94c281b904125fecb9636e513daa0c65c93cf
Signed-off-by: Robert Varga <rovarga@cisco.com>

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/yangtools/binding/data/codec/gen/impl/DataObjectSerializerSource.java
@@ -66,6 +66,10 @@ abstract class DataObjectSerializerSource extends AbstractSource {
         return invoke(STREAM, "startLeafSet", escape(localName),expected);
     }
 
+    protected final CharSequence startLeafSet(final String localName,final CharSequence expected) {
+        return invoke(STREAM, "startOrderedLeafSet", escape(localName),expected);
+    }
+
     protected final CharSequence leafSetEntryNode(final CharSequence value) {
         return invoke(STREAM, "leafSetEntryNode", value);
     }

opendaylight/yangtools

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/yangtools/binding/data/codec/gen/impl/DataObjectSerializerSource.java
@@ -66,6 +66,10 @@ abstract class DataObjectSerializerSource extends AbstractSource {
         return invoke(STREAM, "startLeafSet", escape(localName),expected);
     }
 
+    protected final CharSequence startOrderedLeafSet(final String localName, final CharSequence expected) {
+        return invoke(STREAM, "startOrderedLeafSet", escape(localName),expected);
+    }
+
     protected final CharSequence leafSetEntryNode(final CharSequence value) {
         return invoke(STREAM, "leafSetEntryNode", value);
     }