opendaylight/mdsal

Centralize code generation a bit more
BaseCompilationTest should be used as the baseline for creating
tests which create source code. Refactor it a bit, to allow for
wider reuse.

Change-Id: I69a9d779222439813efb70b25cf88e5b03ac4f7e
Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

binding/mdsal-binding-java-api-generator/src/test/java/org/opendaylight/mdsal/binding/java/api/generator/test/BaseCompilationTest.java
@@ -48,14 +48,20 @@ public abstract class BaseCompilationTest {
         bindingGenerator = new BindingGeneratorImpl();
     }
 
-    protected final void generateTestSources(final String resourceDirPath, final File sourcesOutputDir)
+    protected static final void generatedTestSources(final List<Type> types, final File sourcesOutputDir)
+            throws IOException {
+        Collections.sort(types, (o1, o2) -> o2.getName().compareTo(o1.getName()));
+
+        final GeneratorJavaFile generator = new GeneratorJavaFile(ImmutableSet.copyOf(types));
+        generator.generateToFile(sourcesOutputDir);
+    }
+
+    protected final List<Type> generateTestSources(final String resourceDirPath, final File sourcesOutputDir)
             throws IOException, URISyntaxException {
         final List<File> sourceFiles = CompilationTestUtils.getSourceFiles(resourceDirPath);
         final SchemaContext context = YangParserTestUtils.parseYangFiles(sourceFiles);
         final List<Type> types = bindingGenerator.generateTypes(context);
-        Collections.sort(types, (o1, o2) -> o2.getName().compareTo(o1.getName()));
-        final GeneratorJavaFile generator = new GeneratorJavaFile(ImmutableSet.copyOf(types));
-        generator.generateToFile(sourcesOutputDir);
+        generatedTestSources(types, sourcesOutputDir);
 
         // Also generate YangModuleInfo
         for (Module module : context.getModules()) {
@@ -67,5 +73,7 @@ public abstract class BaseCompilationTest {
                 BindingMapping.MODULE_INFO_CLASS_NAME + ".java");
             Files.asCharSink(file, StandardCharsets.UTF_8).write(template.generate());
         }
+
+        return types;
     }
 }

binding/mdsal-binding-java-api-generator/src/test/java/org/opendaylight/mdsal/binding/java/api/generator/test/GeneratorJavaFileTest.java
@@ -12,11 +12,8 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import org.junit.Test;
-import org.opendaylight.mdsal.binding.java.api.generator.GeneratorJavaFile;
 import org.opendaylight.mdsal.binding.model.api.GeneratedType;
 import org.opendaylight.mdsal.binding.model.api.JavaTypeName;
 import org.opendaylight.mdsal.binding.model.api.type.builder.GeneratedTypeBuilder;
@@ -24,28 +21,24 @@ import org.opendaylight.mdsal.binding.model.util.BindingTypes;
 import org.opendaylight.mdsal.binding.model.util.Types;
 import org.opendaylight.mdsal.binding.model.util.generated.type.builder.CodegenGeneratedTypeBuilder;
 
-public class GeneratorJavaFileTest {
+public class GeneratorJavaFileTest extends BaseCompilationTest {
     private static final String FS = File.separator;
     private static final String PATH = "target/test/test-dir";
 
     @Test
     public void test() throws IOException {
-        final Set<GeneratedType> types = new HashSet<>();
-        GeneratedType t1 = createGeneratedType("org.opendaylight.controller.gen", "Type1");
-        GeneratedType t2 = createGeneratedType("org.opendaylight.controller.gen", "Type2");
-        GeneratedType t3 = createGeneratedType("org.opendaylight.controller.gen", "Type3");
-        types.add(t1);
-        types.add(t2);
-        types.add(t3);
-        GeneratedTypeBuilder gtb = new CodegenGeneratedTypeBuilder(
-            JavaTypeName.create("org.opendaylight.controller.gen", "Type4"));
+        final GeneratedTypeBuilder gtb = new CodegenGeneratedTypeBuilder(JavaTypeName.create(
+            "org.opendaylight.controller.gen", "Type4"));
         gtb.addImplementsType(Types.augmentableTypeFor(gtb));
-        types.add(gtb.build());
-        GeneratorJavaFile generator = new GeneratorJavaFile(types);
-        generator.generateToFile(new File(PATH));
 
-        String[] files = new File(PATH + FS + "org" + FS + "opendaylight" + FS + "controller" + FS + "gen").list();
-        List<String> filesList = Arrays.asList(files);
+        generatedTestSources(Arrays.asList(
+            createGeneratedType("org.opendaylight.controller.gen", "Type1"),
+            createGeneratedType("org.opendaylight.controller.gen", "Type2"),
+            createGeneratedType("org.opendaylight.controller.gen", "Type3"),
+            gtb.build()), new File(PATH));
+
+        List<String> filesList = Arrays.asList(
+            new File(PATH + FS + "org" + FS + "opendaylight" + FS + "controller" + FS + "gen").list());
 
         // assertEquals(5, files.length);
         assertTrue(filesList.contains("Type1.java"));
@@ -60,5 +53,4 @@ public class GeneratorJavaFileTest {
         builder.addImplementsType(BindingTypes.DATA_OBJECT);
         return builder.build();
     }
-
 }

opendaylight/yangtools

binding/mdsal-binding-java-api-generator/src/test/java/org/opendaylight/mdsal/binding/java/api/generator/test/BaseCompilationTest.java
@@ -48,14 +48,20 @@ public abstract class BaseCompilationTest {
         bindingGenerator = new BindingGeneratorImpl();
     }
 
-    protected final void generateTestSources(final String resourceDirPath, final File sourcesOutputDir)
+    protected static final void generateTestSources(final List<Type> types, final File sourcesOutputDir)
+            throws IOException {
+        Collections.sort(types, (o1, o2) -> o2.getName().compareTo(o1.getName()));
+
+        final GeneratorJavaFile generator = new GeneratorJavaFile(ImmutableSet.copyOf(types));
+        generator.generateToFile(sourcesOutputDir);
+    }
+
+    protected final List<Type> generateTestSources(final String resourceDirPath, final File sourcesOutputDir)
             throws IOException, URISyntaxException {
         final List<File> sourceFiles = CompilationTestUtils.getSourceFiles(resourceDirPath);
         final SchemaContext context = YangParserTestUtils.parseYangFiles(sourceFiles);
         final List<Type> types = bindingGenerator.generateTypes(context);
-        Collections.sort(types, (o1, o2) -> o2.getName().compareTo(o1.getName()));
-        final GeneratorJavaFile generator = new GeneratorJavaFile(ImmutableSet.copyOf(types));
-        generator.generateToFile(sourcesOutputDir);
+        generateTestSources(types, sourcesOutputDir);
 
         // Also generate YangModuleInfo
         for (Module module : context.getModules()) {
@@ -67,5 +73,7 @@ public abstract class BaseCompilationTest {
                 BindingMapping.MODULE_INFO_CLASS_NAME + ".java");
             Files.asCharSink(file, StandardCharsets.UTF_8).write(template.generate());
         }
+
+        return types;
     }
 }

binding/mdsal-binding-java-api-generator/src/test/java/org/opendaylight/mdsal/binding/java/api/generator/test/GeneratorJavaFileTest.java
@@ -12,11 +12,8 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import org.junit.Test;
-import org.opendaylight.mdsal.binding.java.api.generator.GeneratorJavaFile;
 import org.opendaylight.mdsal.binding.model.api.GeneratedType;
 import org.opendaylight.mdsal.binding.model.api.JavaTypeName;
 import org.opendaylight.mdsal.binding.model.api.type.builder.GeneratedTypeBuilder;
@@ -24,28 +21,24 @@ import org.opendaylight.mdsal.binding.model.util.BindingTypes;
 import org.opendaylight.mdsal.binding.model.util.Types;
 import org.opendaylight.mdsal.binding.model.util.generated.type.builder.CodegenGeneratedTypeBuilder;
 
-public class GeneratorJavaFileTest {
+public class GeneratorJavaFileTest extends BaseCompilationTest {
     private static final String FS = File.separator;
     private static final String PATH = "target/test/test-dir";
 
     @Test
     public void test() throws IOException {
-        final Set<GeneratedType> types = new HashSet<>();
-        GeneratedType t1 = createGeneratedType("org.opendaylight.controller.gen", "Type1");
-        GeneratedType t2 = createGeneratedType("org.opendaylight.controller.gen", "Type2");
-        GeneratedType t3 = createGeneratedType("org.opendaylight.controller.gen", "Type3");
-        types.add(t1);
-        types.add(t2);
-        types.add(t3);
-        GeneratedTypeBuilder gtb = new CodegenGeneratedTypeBuilder(
-            JavaTypeName.create("org.opendaylight.controller.gen", "Type4"));
+        final GeneratedTypeBuilder gtb = new CodegenGeneratedTypeBuilder(JavaTypeName.create(
+            "org.opendaylight.controller.gen", "Type4"));
         gtb.addImplementsType(Types.augmentableTypeFor(gtb));
-        types.add(gtb.build());
-        GeneratorJavaFile generator = new GeneratorJavaFile(types);
-        generator.generateToFile(new File(PATH));
 
-        String[] files = new File(PATH + FS + "org" + FS + "opendaylight" + FS + "controller" + FS + "gen").list();
-        List<String> filesList = Arrays.asList(files);
+        generateTestSources(Arrays.asList(
+            createGeneratedType("org.opendaylight.controller.gen", "Type1"),
+            createGeneratedType("org.opendaylight.controller.gen", "Type2"),
+            createGeneratedType("org.opendaylight.controller.gen", "Type3"),
+            gtb.build()), new File(PATH));
+
+        List<String> filesList = Arrays.asList(
+            new File(PATH + FS + "org" + FS + "opendaylight" + FS + "controller" + FS + "gen").list());
 
         // assertEquals(5, files.length);
         assertTrue(filesList.contains("Type1.java"));
@@ -60,5 +53,4 @@ public class GeneratorJavaFileTest {
         builder.addImplementsType(BindingTypes.DATA_OBJECT);
         return builder.build();
     }
-
 }