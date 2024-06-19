opendaylight/mdsal

Clean up constant reference
We have two methods with the same name, make sure we use the right
constant when referencing it.

JIRA: MDSAL-852
Change-Id: I7628d2c997e3c62895984d6d85672c65ff9f0ce7
Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/ActionGenerator.java
@@ -50,7 +50,7 @@ final class ActionGenerator extends AbstractInvokableGenerator<ActionEffectiveSt
             if (keyGen != null) {
                 final var keyType = keyGen.getGeneratedType(builderFactory);
                 builder.addImplementsType(BindingTypes.keyedListAction(parentType, keyType, input, output));
-                builder.addMethod(BindingMapping.RPC_INVOKE_NAME).setAbstract(true)
+                builder.addMethod(BindingMapping.ACTION_INVOKE_NAME).setAbstract(true)
                     .addParameter(BindingTypes.keyedInstanceIdentifier(parentType, keyType), "path")
                     .addParameter(input, "input")
                     .setReturnType(Types.listenableFutureTypeFor(BindingTypes.rpcResult(output)))
@@ -59,7 +59,7 @@ final class ActionGenerator extends AbstractInvokableGenerator<ActionEffectiveSt
             }
         }
         builder.addImplementsType(BindingTypes.action(parentType, input, output));
-        builder.addMethod(BindingMapping.RPC_INVOKE_NAME).setAbstract(true)
+        builder.addMethod(BindingMapping.ACTION_INVOKE_NAME).setAbstract(true)
             .addParameter(BindingTypes.instanceIdentifier(parentType), "path")
             .addParameter(input, "input")
             .setReturnType(Types.listenableFutureTypeFor(BindingTypes.rpcResult(output)))

opendaylight/yangtools

(cherry picked from commit c112f99b13a8b337563a27761159f7f4bb9dbd94)
(cherry picked from commit edac194e791de33fd9f4a3338855113139ff9ad0)

binding/mdsal-binding-generator/src/main/java/org/opendaylight/mdsal/binding/generator/impl/reactor/ActionGenerator.java
@@ -51,7 +51,7 @@ final class ActionGenerator extends AbstractInvokableGenerator<ActionEffectiveSt
             if (keyGen != null) {
                 final var keyType = keyGen.getGeneratedType(builderFactory);
                 builder.addImplementsType(BindingTypes.keyedListAction(parentType, keyType, input, output));
-                builder.addMethod(Naming.RPC_INVOKE_NAME).setAbstract(true)
+                builder.addMethod(Naming.ACTION_INVOKE_NAME).setAbstract(true)
                     .addParameter(BindingTypes.keyedInstanceIdentifier(parentType, keyType), "path")
                     .addParameter(input, "input")
                     .setReturnType(Types.listenableFutureTypeFor(BindingTypes.rpcResult(output)))
@@ -60,7 +60,7 @@ final class ActionGenerator extends AbstractInvokableGenerator<ActionEffectiveSt
             }
         }
         builder.addImplementsType(BindingTypes.action(parentType, input, output));
-        builder.addMethod(Naming.RPC_INVOKE_NAME).setAbstract(true)
+        builder.addMethod(Naming.ACTION_INVOKE_NAME).setAbstract(true)
             .addParameter(BindingTypes.instanceIdentifier(parentType), "path")
             .addParameter(input, "input")
             .setReturnType(Types.listenableFutureTypeFor(BindingTypes.rpcResult(output)))