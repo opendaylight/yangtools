opendaylight/mdsal

Expand class customization capabilities
This expands CodecClassLoader.Customizer with the ability to
cross-reference generated classes, so a generated class can
depend on another generated class -- which can potentially live
in a different classloader. This capability is required to support
stream writers, as they delegate to each other.

JIRA: MDSAL-401
JIRA: MDSAL-443
Change-Id: I446dcfe10c742c670f2021123acab600a28c2514
Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/loader/LeafCodecClassLoader.java
@@ -10,7 +10,14 @@ package org.opendaylight.mdsal.binding.dom.codec.loader;
 import static com.google.common.base.Verify.verify;
 import static java.util.Objects.requireNonNull;
 
+import com.google.common.collect.ImmutableSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 import org.eclipse.jdt.annotation.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 // A leaf class loader, binding together a root class loader and some other class loader
 final class LeafCodecClassLoader extends CodecClassLoader {
@@ -18,8 +25,15 @@ final class LeafCodecClassLoader extends CodecClassLoader {
         verify(registerAsParallelCapable());
     }
 
-    private final @NonNull ClassLoader target;
+    private static final Logger LOG = LoggerFactory.getLogger(LeafCodecClassLoader.class);
+
     private final @NonNull RootCodecClassLoader root;
+    private final @NonNull ClassLoader target;
+
+    @SuppressWarnings("rawtypes")
+    private static final AtomicReferenceFieldUpdater<LeafCodecClassLoader, ImmutableSet> DEPENDENCIES_UPDATER =
+            AtomicReferenceFieldUpdater.newUpdater(LeafCodecClassLoader.class, ImmutableSet.class, "dependencies");
+    private volatile ImmutableSet<LeafCodecClassLoader> dependencies = ImmutableSet.of();
 
     LeafCodecClassLoader(final RootCodecClassLoader root, final ClassLoader target) {
         super(root);
@@ -29,12 +43,40 @@ final class LeafCodecClassLoader extends CodecClassLoader {
 
     @Override
     protected Class<?> findClass(final String name) throws ClassNotFoundException {
-        return target.loadClass(name);
+        try {
+            return target.loadClass(name);
+        } catch (ClassNotFoundException e) {
+            LOG.trace("Class {} not found in target, looking through dependencies", name);
+            for (LeafCodecClassLoader loader : dependencies) {
+                final Class<?> loaded = loader.findLoadedClass(name);
+                if (loaded != null) {
+                    LOG.trace("Class {} found in dependency {}", name, loader);
+                    return loaded;
+                }
+            }
+
+            throw e;
+        }
     }
 
     @Override
     CodecClassLoader findClassLoader(final Class<?> bindingClass) {
         final ClassLoader bindingTarget = bindingClass.getClassLoader();
         return target.equals(bindingTarget) ? this : root.findClassLoader(bindingClass);
+    }
+
+    @Override
+    void appendLoaders(final Set<LeafCodecClassLoader> loaders) {
+        while (true) {
+            final ImmutableSet<LeafCodecClassLoader> local = dependencies;
+            final List<LeafCodecClassLoader> builder = new ArrayList<>(local.size() + loaders.size());
+            builder.addAll(local);
+            builder.addAll(loaders);
+            final ImmutableSet<LeafCodecClassLoader> updated = ImmutableSet.copyOf(builder);
+            if (local.equals(updated) || DEPENDENCIES_UPDATER.compareAndSet(this, local, updated)) {
+                // No need for an update or the update was successful
+                return;
+            }
+        }
     }
 }

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/loader/RootCodecClassLoader.java
@@ -13,6 +13,7 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -78,6 +79,14 @@ final class RootCodecClassLoader extends CodecClassLoader {
                 return recheck;
             }
         }
+    }
+
+    @Override
+    void appendLoaders(final Set<LeafCodecClassLoader> loaders) {
+        // Root loader should never see the requirement for other loaders, as that would violate loop-free nature
+        // of generated code: if a binding class is hosted in root loader, all its references must be visible from
+        // the root loader and hence all the generated code ends up residing in the root loader, too.
+        throw new IllegalStateException("Attempted to extend root loader with " + loaders);
     }
 
     private boolean isOurClass(final Class<?> bindingClass) {

opendaylight/yangtools

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/loader/LeafCodecClassLoader.java
@@ -10,7 +10,14 @@ package org.opendaylight.mdsal.binding.dom.codec.loader;
 import static com.google.common.base.Verify.verify;
 import static java.util.Objects.requireNonNull;
 
+import com.google.common.collect.ImmutableSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 import org.eclipse.jdt.annotation.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 // A leaf class loader, binding together a root class loader and some other class loader
 final class LeafCodecClassLoader extends CodecClassLoader {
@@ -18,8 +25,15 @@ final class LeafCodecClassLoader extends CodecClassLoader {
         verify(registerAsParallelCapable());
     }
 
-    private final @NonNull ClassLoader target;
+    private static final Logger LOG = LoggerFactory.getLogger(LeafCodecClassLoader.class);
+
     private final @NonNull RootCodecClassLoader root;
+    private final @NonNull ClassLoader target;
+
+    @SuppressWarnings("rawtypes")
+    private static final AtomicReferenceFieldUpdater<LeafCodecClassLoader, ImmutableSet> DEPENDENCIES_UPDATER =
+            AtomicReferenceFieldUpdater.newUpdater(LeafCodecClassLoader.class, ImmutableSet.class, "dependencies");
+    private volatile ImmutableSet<LeafCodecClassLoader> dependencies = ImmutableSet.of();
 
     LeafCodecClassLoader(final RootCodecClassLoader root, final ClassLoader target) {
         super(root);
@@ -29,12 +43,40 @@ final class LeafCodecClassLoader extends CodecClassLoader {
 
     @Override
     protected Class<?> findClass(final String name) throws ClassNotFoundException {
-        return target.loadClass(name);
+        try {
+            return target.loadClass(name);
+        } catch (ClassNotFoundException e) {
+            LOG.trace("Class {} not found in target, looking through dependencies", name);
+            for (LeafCodecClassLoader loader : dependencies) {
+                final Class<?> loaded = loader.findLoadedClass(name);
+                if (loaded != null) {
+                    LOG.trace("Class {} found in dependency {}", name, loader);
+                    return loaded;
+                }
+            }
+
+            throw e;
+        }
     }
 
     @Override
     CodecClassLoader findClassLoader(final Class<?> bindingClass) {
         final ClassLoader bindingTarget = bindingClass.getClassLoader();
         return target.equals(bindingTarget) ? this : root.findClassLoader(bindingClass);
+    }
+
+    @Override
+    void appendLoaders(final Set<LeafCodecClassLoader> newLoaders) {
+        while (true) {
+            final ImmutableSet<LeafCodecClassLoader> local = dependencies;
+            final List<LeafCodecClassLoader> builder = new ArrayList<>(local.size() + newLoaders.size());
+            builder.addAll(local);
+            builder.addAll(newLoaders);
+            final ImmutableSet<LeafCodecClassLoader> updated = ImmutableSet.copyOf(builder);
+            if (local.equals(updated) || DEPENDENCIES_UPDATER.compareAndSet(this, local, updated)) {
+                // No need for an update or the update was successful
+                return;
+            }
+        }
     }
 }

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/loader/RootCodecClassLoader.java
@@ -13,6 +13,7 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -78,6 +79,14 @@ final class RootCodecClassLoader extends CodecClassLoader {
                 return recheck;
             }
         }
+    }
+
+    @Override
+    void appendLoaders(final Set<LeafCodecClassLoader> newLoaders) {
+        // Root loader should never see the requirement for other loaders, as that would violate loop-free nature
+        // of generated code: if a binding class is hosted in root loader, all its references must be visible from
+        // the root loader and hence all the generated code ends up residing in the root loader, too.
+        throw new IllegalStateException("Attempted to extend root loader with " + newLoaders);
     }
 
     private boolean isOurClass(final Class<?> bindingClass) {