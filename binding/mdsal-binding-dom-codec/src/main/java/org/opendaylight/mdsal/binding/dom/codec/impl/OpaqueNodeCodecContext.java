opendaylight/mdsal

Clean up BindingNormalizedNodeCodec.deserialize() implementations
We should consistently throw NPE/IAE for inputs. Add some basic infra
and make sure every implementation behaves similarly. Most notably we do
not want to dump the entire argument, as that could lead to an OOM.

Change-Id: I6aad6c587028637e0089684ba2305d3e7c20675e
Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/impl/OpaqueNodeCodecContext.java
@@ -116,8 +116,10 @@ abstract class OpaqueNodeCodecContext<T extends OpaqueObject<T>> extends ValueNo
 
     @Override
     public final T deserialize(final NormalizedNode data) {
-        checkArgument(data instanceof ForeignDataNode, "Unexpected value %s", data);
-        return deserialize((ForeignDataNode<?>) data);
+        if (data instanceof ForeignDataNode<?> foreign) {
+            return deserialize(foreign);
+        }
+        throw new IllegalArgumentException("Expected a ForeignDataNode, not " + data.getClass().getName());
     }
 
     T deserialize(final ForeignDataNode<?> foreignData) {
@@ -127,8 +129,7 @@ abstract class OpaqueNodeCodecContext<T extends OpaqueObject<T>> extends ValueNo
     @Override
     public final ForeignDataNode<?> serialize(final T data) {
         final OpaqueData<?> opaqueData = data.getValue();
-        return opaqueData instanceof ForeignOpaqueData ? ((ForeignOpaqueData<?>) opaqueData).domData()
-                : serializedData(opaqueData);
+        return opaqueData instanceof ForeignOpaqueData<?> foreign ? foreign.domData() : serializedData(opaqueData);
     }
 
     @Override

opendaylight/yangtools

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/impl/OpaqueNodeCodecContext.java
@@ -116,8 +116,10 @@ abstract class OpaqueNodeCodecContext<T extends OpaqueObject<T>> extends ValueNo
 
     @Override
     public final T deserialize(final NormalizedNode data) {
-        checkArgument(data instanceof ForeignDataNode, "Unexpected value %s", data);
-        return deserialize((ForeignDataNode<?>) data);
+        if (data instanceof ForeignDataNode<?> foreign) {
+            return deserialize(foreign);
+        }
+        throw new IllegalArgumentException("Expected a ForeignDataNode, not " + data.contract().getSimpleName());
     }
 
     T deserialize(final ForeignDataNode<?> foreignData) {
@@ -127,8 +129,7 @@ abstract class OpaqueNodeCodecContext<T extends OpaqueObject<T>> extends ValueNo
     @Override
     public final ForeignDataNode<?> serialize(final T data) {
         final OpaqueData<?> opaqueData = data.getValue();
-        return opaqueData instanceof ForeignOpaqueData ? ((ForeignOpaqueData<?>) opaqueData).domData()
-                : serializedData(opaqueData);
+        return opaqueData instanceof ForeignOpaqueData<?> foreign ? foreign.domData() : serializedData(opaqueData);
     }
 
     @Override