opendaylight/mdsal

Optimize range/length generators
This patch improves range/length generators so that share common utility
methods from CodeHelper, eliminating common literals in generated code.

It also adds the ability for generators to import classes, leading to much
more readable code.

Change-Id: I98f103e9c05a1d68f0f4bc4c6c33146cb654e5bb
Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

binding/mdsal-binding-java-api-generator/src/main/java/org/opendaylight/mdsal/binding/java/api/generator/AbstractRangeGenerator.java
@@ -11,6 +11,7 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
 import java.util.Map;
+import java.util.function.Function;
 import javax.annotation.Nonnull;
 import org.opendaylight.mdsal.binding.model.api.ConcreteType;
 import org.opendaylight.mdsal.binding.model.api.Type;
@@ -93,7 +94,7 @@ abstract class AbstractRangeGenerator<T extends Number & Comparable<T>> {
 
     // FIXME: Once BUG-3399 is fixed, we should never need this
     @Deprecated
-    protected abstract T convert(final Number value);
+    protected abstract T convert(Number value);
 
     /**
      * Format a value into a Java-compilable expression which results in the appropriate
@@ -102,7 +103,7 @@ abstract class AbstractRangeGenerator<T extends Number & Comparable<T>> {
      * @param value Number value
      * @return Java language string representation
      */
-    @Nonnull protected abstract String format(final T value);
+    @Nonnull protected abstract String format(T value);
 
     /**
      * Generate the checker method source code.
@@ -110,15 +111,16 @@ abstract class AbstractRangeGenerator<T extends Number & Comparable<T>> {
      * @param constraints Restrictions which need to be applied.
      * @return Method source code.
      */
-    @Nonnull protected abstract String generateRangeCheckerImplementation(@Nonnull final String checkerName,
-            @Nonnull final RangeConstraint<?> constraints);
+    @Nonnull protected abstract String generateRangeCheckerImplementation(@Nonnull String checkerName,
+            @Nonnull RangeConstraint<?> constraints, Function<Class<?>, String> classImporter);
 
     private static String rangeCheckerName(final String member) {
         return "check" + member + "Range";
     }
 
-    String generateRangeChecker(@Nonnull final String member, @Nonnull final RangeConstraint<?> constraints) {
-        return generateRangeCheckerImplementation(rangeCheckerName(member), constraints);
+    String generateRangeChecker(@Nonnull final String member, @Nonnull final RangeConstraint<?> constraints,
+            final BaseTemplate template) {
+        return generateRangeCheckerImplementation(rangeCheckerName(member), constraints, template::importedName);
     }
 
     String generateRangeCheckerCall(@Nonnull final String member, @Nonnull final String valueReference) {

binding/mdsal-binding-java-api-generator/src/main/java/org/opendaylight/mdsal/binding/java/api/generator/LengthGenerator.java
@@ -14,6 +14,7 @@ import java.util.Set;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.opendaylight.mdsal.binding.model.api.Type;
+import org.opendaylight.yangtools.yang.binding.CodeHelpers;
 import org.opendaylight.yangtools.yang.model.api.type.LengthConstraint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -63,7 +64,8 @@ final class LengthGenerator {
         return new ArrayList<>(constraint.getAllowedRanges().asRanges()).toString();
     }
 
-    private static String generateArrayLengthChecker(final String member, final LengthConstraint constraint) {
+    private static String generateArrayLengthChecker(final String member, final LengthConstraint constraint,
+            final BaseTemplate template) {
         final StringBuilder sb = new StringBuilder();
         final Collection<String> expressions = createExpressions(constraint);
 
@@ -78,16 +80,15 @@ final class LengthGenerator {
                 sb.append("    }\n");
             }
 
-            sb.append("    throw new IllegalArgumentException(String.format(\"Invalid length: %s, expected: ")
-              .append(createLengthString(constraint)).append(".\", java.util.Arrays.toString(value)));\n");
+            sb.append("    ").append(template.importedName(CodeHelpers.class)).append(".throwInvalidLength(\"")
+            .append(createLengthString(constraint)).append("\", value);\n");
         }
 
-        sb.append("}\n");
-
-        return sb.toString();
+        return sb.append("}\n").toString();
     }
 
-    private static String generateStringLengthChecker(final String member, final LengthConstraint constraint) {
+    private static String generateStringLengthChecker(final String member, final LengthConstraint constraint,
+            final BaseTemplate template) {
         final StringBuilder sb = new StringBuilder();
         final Collection<String> expressions = createExpressions(constraint);
 
@@ -102,19 +103,18 @@ final class LengthGenerator {
                 sb.append("    }\n");
             }
 
-            sb.append("    throw new IllegalArgumentException(String.format(\"Invalid length: %s, expected: ")
-              .append(createLengthString(constraint)).append(".\", value));\n");
+            sb.append("    ").append(template.importedName(CodeHelpers.class)).append(".throwInvalidLength(\"")
+            .append(createLengthString(constraint)).append("\", value);\n");
         }
 
-        sb.append("}\n");
-
-        return sb.toString();
+        return sb.append("}\n").toString();
     }
 
     static String generateLengthChecker(final String member, final Type type,
-            final LengthConstraint constraint) {
+            final LengthConstraint constraint, final BaseTemplate template) {
         return TypeUtils.getBaseYangType(type).getName().indexOf('[') != -1
-                ? generateArrayLengthChecker(member, constraint) : generateStringLengthChecker(member, constraint);
+                ? generateArrayLengthChecker(member, constraint, template)
+                        : generateStringLengthChecker(member, constraint, template);
     }
 
     static String generateLengthCheckerCall(@Nullable final String member, @Nonnull final String valueReference) {

opendaylight/yangtools

binding/mdsal-binding-java-api-generator/src/main/java/org/opendaylight/mdsal/binding/java/api/generator/AbstractRangeGenerator.java
@@ -11,6 +11,7 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
 import java.util.Map;
+import java.util.function.Function;
 import javax.annotation.Nonnull;
 import org.opendaylight.mdsal.binding.model.api.ConcreteType;
 import org.opendaylight.mdsal.binding.model.api.Type;
@@ -93,7 +94,7 @@ abstract class AbstractRangeGenerator<T extends Number & Comparable<T>> {
 
     // FIXME: Once BUG-3399 is fixed, we should never need this
     @Deprecated
-    protected abstract T convert(final Number value);
+    protected abstract T convert(Number value);
 
     /**
      * Format a value into a Java-compilable expression which results in the appropriate
@@ -102,7 +103,7 @@ abstract class AbstractRangeGenerator<T extends Number & Comparable<T>> {
      * @param value Number value
      * @return Java language string representation
      */
-    @Nonnull protected abstract String format(final T value);
+    @Nonnull protected abstract String format(T value);
 
     /**
      * Generate the checker method source code.
@@ -110,15 +111,16 @@ abstract class AbstractRangeGenerator<T extends Number & Comparable<T>> {
      * @param constraints Restrictions which need to be applied.
      * @return Method source code.
      */
-    @Nonnull protected abstract String generateRangeCheckerImplementation(@Nonnull final String checkerName,
-            @Nonnull final RangeConstraint<?> constraints);
+    @Nonnull protected abstract String generateRangeCheckerImplementation(@Nonnull String checkerName,
+            @Nonnull RangeConstraint<?> constraints, Function<Class<?>, String> classImporter);
 
     private static String rangeCheckerName(final String member) {
         return "check" + member + "Range";
     }
 
-    String generateRangeChecker(@Nonnull final String member, @Nonnull final RangeConstraint<?> constraints) {
-        return generateRangeCheckerImplementation(rangeCheckerName(member), constraints);
+    String generateRangeChecker(@Nonnull final String member, @Nonnull final RangeConstraint<?> constraints,
+            final JavaFileTemplate template) {
+        return generateRangeCheckerImplementation(rangeCheckerName(member), constraints, template::importedName);
     }
 
     String generateRangeCheckerCall(@Nonnull final String member, @Nonnull final String valueReference) {

binding/mdsal-binding-java-api-generator/src/main/java/org/opendaylight/mdsal/binding/java/api/generator/LengthGenerator.java
@@ -14,6 +14,7 @@ import java.util.Set;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.opendaylight.mdsal.binding.model.api.Type;
+import org.opendaylight.yangtools.yang.binding.CodeHelpers;
 import org.opendaylight.yangtools.yang.model.api.type.LengthConstraint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -63,7 +64,8 @@ final class LengthGenerator {
         return new ArrayList<>(constraint.getAllowedRanges().asRanges()).toString();
     }
 
-    private static String generateArrayLengthChecker(final String member, final LengthConstraint constraint) {
+    private static String generateArrayLengthChecker(final String member, final LengthConstraint constraint,
+            final JavaFileTemplate template) {
         final StringBuilder sb = new StringBuilder();
         final Collection<String> expressions = createExpressions(constraint);
 
@@ -78,16 +80,15 @@ final class LengthGenerator {
                 sb.append("    }\n");
             }
 
-            sb.append("    throw new IllegalArgumentException(String.format(\"Invalid length: %s, expected: ")
-              .append(createLengthString(constraint)).append(".\", java.util.Arrays.toString(value)));\n");
+            sb.append("    ").append(template.importedName(CodeHelpers.class)).append(".throwInvalidLength(\"")
+            .append(createLengthString(constraint)).append("\", value);\n");
         }
 
-        sb.append("}\n");
-
-        return sb.toString();
+        return sb.append("}\n").toString();
     }
 
-    private static String generateStringLengthChecker(final String member, final LengthConstraint constraint) {
+    private static String generateStringLengthChecker(final String member, final LengthConstraint constraint,
+            final JavaFileTemplate template) {
         final StringBuilder sb = new StringBuilder();
         final Collection<String> expressions = createExpressions(constraint);
 
@@ -102,19 +103,18 @@ final class LengthGenerator {
                 sb.append("    }\n");
             }
 
-            sb.append("    throw new IllegalArgumentException(String.format(\"Invalid length: %s, expected: ")
-              .append(createLengthString(constraint)).append(".\", value));\n");
+            sb.append("    ").append(template.importedName(CodeHelpers.class)).append(".throwInvalidLength(\"")
+            .append(createLengthString(constraint)).append("\", value);\n");
         }
 
-        sb.append("}\n");
-
-        return sb.toString();
+        return sb.append("}\n").toString();
     }
 
     static String generateLengthChecker(final String member, final Type type,
-            final LengthConstraint constraint) {
+            final LengthConstraint constraint, final JavaFileTemplate template) {
         return TypeUtils.getBaseYangType(type).getName().indexOf('[') != -1
-                ? generateArrayLengthChecker(member, constraint) : generateStringLengthChecker(member, constraint);
+                ? generateArrayLengthChecker(member, constraint, template)
+                        : generateStringLengthChecker(member, constraint, template);
     }
 
     static String generateLengthCheckerCall(@Nullable final String member, @Nonnull final String valueReference) {