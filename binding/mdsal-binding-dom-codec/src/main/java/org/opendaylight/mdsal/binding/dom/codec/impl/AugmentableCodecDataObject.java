opendaylight/mdsal

Align CodecDataObject augmentations toString()
Compile-time generated codecs are using only values() from
augmentation in their toString() method. Make sure
AugmentableCodecDataObjects are following the same pattern.

Change-Id: I876dd3f2e8b6e42685198c9e1d4304592da87334
Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/impl/AugmentableCodecDataObject.java
@@ -54,7 +54,7 @@ public abstract class AugmentableCodecDataObject<T extends DataObject & Augmenta
             return (A) aug.get(augmentationType);
         }
 
-        @SuppressWarnings({"unchecked","rawtypes"})
+        @SuppressWarnings("rawtypes")
         final Optional<DataContainerCodecContext<?, ?>> optAugCtx = codecContext().possibleStreamChild(
             (Class) augmentationType);
         if (optAugCtx.isPresent()) {
@@ -97,7 +97,7 @@ public abstract class AugmentableCodecDataObject<T extends DataObject & Augmenta
 
     @Override
     final ToStringHelper codecAugmentedFillToString(final ToStringHelper helper) {
-        return super.codecAugmentedFillToString(helper).add("augmentations", augmentations());
+        return super.codecAugmentedFillToString(helper).add("augmentations", augmentations().values());
     }
 
     private static Map<Class<? extends Augmentation<?>>, Augmentation<?>> getAllAugmentations(

opendaylight/yangtools

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/impl/AugmentableCodecDataObject.java
@@ -54,7 +54,7 @@ public abstract class AugmentableCodecDataObject<T extends DataObject & Augmenta
             return (A) aug.get(augmentationType);
         }
 
-        @SuppressWarnings({"unchecked","rawtypes"})
+        @SuppressWarnings("rawtypes")
         final Optional<DataContainerCodecContext<?, ?>> optAugCtx = codecContext().possibleStreamChild(
             (Class) augmentationType);
         if (optAugCtx.isPresent()) {
@@ -97,7 +97,7 @@ public abstract class AugmentableCodecDataObject<T extends DataObject & Augmenta
 
     @Override
     final ToStringHelper codecAugmentedFillToString(final ToStringHelper helper) {
-        return super.codecAugmentedFillToString(helper).add("augmentations", augmentations());
+        return super.codecAugmentedFillToString(helper).add("augmentation", augmentations().values());
     }
 
     private static Map<Class<? extends Augmentation<?>>, Augmentation<?>> getAllAugmentations(