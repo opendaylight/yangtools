opendaylight/mdsal

Specialize CompositeValueCodec
We have only two codec interactions, both of which have dom-codec-api
not tied to IllegalArgumentCodec. Specialize the class and use our codec
API.

JIRA: MDSAL-704
Change-Id: Ic4b219f3e328fe3244811a57037fe1d1780e141f
Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/impl/BindingCodecContext.java
@@ -442,9 +442,9 @@ public final class BindingCodecContext extends AbstractBindingNormalizedNodeSeri
     private IllegalArgumentCodec<Object, Object> getCodecForBindingClass(final Class<?> valueType,
             final TypeDefinition<?> typeDef) {
         if (typeDef instanceof IdentityrefTypeDefinition) {
-            return new CompositeValueCodec(valueType, identityCodec);
+            return new CompositeValueCodec.IdentityValueCodec(valueType, identityCodec);
         } else if (typeDef instanceof InstanceIdentifierTypeDefinition) {
-            return new CompositeValueCodec(valueType, instanceIdentifierCodec);
+            return new CompositeValueCodec.InstanceIdentifierValueCodec(valueType, instanceIdentifierCodec);
         } else if (typeDef instanceof UnionTypeDefinition) {
             try {
                 return UnionTypeCodec.of(valueType, (UnionTypeDefinition) typeDef, this);

opendaylight/yangtools

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/impl/BindingCodecContext.java
@@ -442,9 +442,9 @@ public final class BindingCodecContext extends AbstractBindingNormalizedNodeSeri
     private IllegalArgumentCodec<Object, Object> getCodecForBindingClass(final Class<?> valueType,
             final TypeDefinition<?> typeDef) {
         if (typeDef instanceof IdentityrefTypeDefinition) {
-            return new CompositeValueCodec(valueType, identityCodec);
+            return new CompositeValueCodec.OfIdentity(valueType, identityCodec);
         } else if (typeDef instanceof InstanceIdentifierTypeDefinition) {
-            return new CompositeValueCodec(valueType, instanceIdentifierCodec);
+            return new CompositeValueCodec.OfInstanceIdentifier(valueType, instanceIdentifierCodec);
         } else if (typeDef instanceof UnionTypeDefinition) {
             try {
                 return UnionTypeCodec.of(valueType, (UnionTypeDefinition) typeDef, this);