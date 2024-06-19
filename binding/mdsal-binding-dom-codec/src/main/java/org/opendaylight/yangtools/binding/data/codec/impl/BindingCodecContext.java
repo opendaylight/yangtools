opendaylight/mdsal

BUG-8004: handle implicit RPC input
RPC input is always defined implicitly, which is something we cannot
deal with in Binding Specification compatibly, as original generators
did not emit classes for such methods and implicitly map them to Void.

In order to deal with these, we need to recognize when an RPC input
is not declared and re-wire it to a null codec. This loses information,
but it is the best we can do (simply because there is no binding
representation).

Change-Id: I4edbcd0cc886396bdba79f4c0ccfd91ba3d6b2c7
Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/yangtools/binding/data/codec/impl/BindingCodecContext.java
@@ -213,7 +213,7 @@ final class BindingCodecContext implements CodecContextFactory, BindingCodecTree
         return root.getNotification(notification);
     }
 
-    ContainerNodeCodecContext<?> getRpcDataContext(final SchemaPath path) {
+    AbstractContainerNodeCodecContext<?> getRpcDataContext(final SchemaPath path) {
         return root.getRpc(path);
     }
 

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/yangtools/binding/data/codec/impl/BindingNormalizedNodeCodecRegistry.java
@@ -108,7 +108,7 @@ public class BindingNormalizedNodeCodecRegistry implements DataObjectSerializerR
             LOG.error("Unexpected failure while serializing path {} data {}", path, data, e);
             throw new IllegalStateException("Failed to create normalized node", e);
         }
-        return new SimpleEntry<YangInstanceIdentifier,NormalizedNode<?,?>>(writeCtx.getKey(),result.getResult());
+        return new SimpleEntry<>(writeCtx.getKey(),result.getResult());
     }
 
     @Override
@@ -189,7 +189,7 @@ public class BindingNormalizedNodeCodecRegistry implements DataObjectSerializerR
 
         final DataObject lazyObj = codec.deserialize(data);
         final InstanceIdentifier<?> bindingPath = InstanceIdentifier.create(builder);
-        return new SimpleEntry<InstanceIdentifier<?>, DataObject>(bindingPath, lazyObj);
+        return new SimpleEntry<>(bindingPath, lazyObj);
     }
 
     @Override
@@ -200,7 +200,7 @@ public class BindingNormalizedNodeCodecRegistry implements DataObjectSerializerR
 
     @Override
     public DataObject fromNormalizedNodeRpcData(final SchemaPath path, final ContainerNode data) {
-        final ContainerNodeCodecContext<?> codec = codecContext.getRpcDataContext(path);
+        final AbstractContainerNodeCodecContext<?> codec = codecContext.getRpcDataContext(path);
         return codec.deserialize(data);
     }
 
@@ -228,7 +228,7 @@ public class BindingNormalizedNodeCodecRegistry implements DataObjectSerializerR
 
     public <T extends DataObject> Function<Optional<NormalizedNode<?, ?>>, Optional<T>>  deserializeFunction(final InstanceIdentifier<T> path) {
         final DataObjectCodecContext<?,?> ctx = (DataObjectCodecContext<?,?>) codecContext.getCodecContextNode(path, null);
-        return new DeserializeFunction<T>(ctx);
+        return new DeserializeFunction<>(ctx);
     }
 
     @Override

opendaylight/yangtools

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/yangtools/binding/data/codec/impl/BindingCodecContext.java
@@ -213,7 +213,7 @@ final class BindingCodecContext implements CodecContextFactory, BindingCodecTree
         return root.getNotification(notification);
     }
 
-    ContainerNodeCodecContext<?> getRpcDataContext(final SchemaPath path) {
+    RpcInputCodec<?> getRpcInputCodec(final SchemaPath path) {
         return root.getRpc(path);
     }
 

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/yangtools/binding/data/codec/impl/BindingNormalizedNodeCodecRegistry.java
@@ -108,7 +108,7 @@ public class BindingNormalizedNodeCodecRegistry implements DataObjectSerializerR
             LOG.error("Unexpected failure while serializing path {} data {}", path, data, e);
             throw new IllegalStateException("Failed to create normalized node", e);
         }
-        return new SimpleEntry<YangInstanceIdentifier,NormalizedNode<?,?>>(writeCtx.getKey(),result.getResult());
+        return new SimpleEntry<>(writeCtx.getKey(),result.getResult());
     }
 
     @Override
@@ -189,7 +189,7 @@ public class BindingNormalizedNodeCodecRegistry implements DataObjectSerializerR
 
         final DataObject lazyObj = codec.deserialize(data);
         final InstanceIdentifier<?> bindingPath = InstanceIdentifier.create(builder);
-        return new SimpleEntry<InstanceIdentifier<?>, DataObject>(bindingPath, lazyObj);
+        return new SimpleEntry<>(bindingPath, lazyObj);
     }
 
     @Override
@@ -200,7 +200,7 @@ public class BindingNormalizedNodeCodecRegistry implements DataObjectSerializerR
 
     @Override
     public DataObject fromNormalizedNodeRpcData(final SchemaPath path, final ContainerNode data) {
-        final ContainerNodeCodecContext<?> codec = codecContext.getRpcDataContext(path);
+        final RpcInputCodec<?> codec = codecContext.getRpcInputCodec(path);
         return codec.deserialize(data);
     }
 
@@ -228,7 +228,7 @@ public class BindingNormalizedNodeCodecRegistry implements DataObjectSerializerR
 
     public <T extends DataObject> Function<Optional<NormalizedNode<?, ?>>, Optional<T>>  deserializeFunction(final InstanceIdentifier<T> path) {
         final DataObjectCodecContext<?,?> ctx = (DataObjectCodecContext<?,?>) codecContext.getCodecContextNode(path, null);
-        return new DeserializeFunction<T>(ctx);
+        return new DeserializeFunction<>(ctx);
     }
 
     @Override