e8f2462a2d158daf894c61e37e70c07f09837917
Robert Varga <robert.varga@pantheon.tech>
2024-07-16 13:15:58 +0200

    WIP: Add binding.model.Archetype
    An Archetype captures what construct are we about to generate. At some
    point it should be the lingua franca between GeneratedType and
    RuntimeType, capturing the intent.
    
    // FIXME: convert users
    
    JIRA: YANGTOOLS-1578
    Change-Id: I69353a33fae9b9b4216be9e55d80449445047979
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/binding-model/src/main/java/org/opendaylight/yangtools/binding/model/DataContainerArchetype.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.binding.model;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.binding.DataContainer;
+import org.opendaylight.yangtools.binding.model.api.JavaTypeName;
+
+/**
+ * An archetype for a {@link DataContainer} specialization.
+ */
+@NonNullByDefault
+public abstract sealed class DataContainerArchetype<T extends DataContainerArchetype<T>> extends Archetype<T>
+        permits ChoiceArchetype, DataObjectArchetype, GroupingArchetype, YangDataArchetype {
+    DataContainerArchetype(final JavaTypeName typeName) {
+        super(typeName);
+    }
+}
---
+++ binding/binding-model/src/main/java/org/opendaylight/yangtools/binding/model/RpcOutputArchetype.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.binding.model;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.binding.RpcOutput;
+import org.opendaylight.yangtools.binding.model.api.JavaTypeName;
+
+/**
+ * An archetype for an {@link RpcOutput}.
+ */
+@NonNullByDefault
+public final class RpcOutputArchetype extends DataObjectArchetype<RpcOutputArchetype> {
+    public RpcOutputArchetype(final JavaTypeName typeName) {
+        super(typeName);
+    }
+
+    @Override
+    public Class<RpcOutputArchetype> archetypeContract() {
+        return RpcOutputArchetype.class;
+    }
+}
