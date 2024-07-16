5394ed7a588b8d2ef2d997993710ba7a8646482f
Robert Varga <robert.varga@pantheon.tech>
2024-07-16 13:15:58 +0200

    WIP: Add binding.model.Archetype
    An Archetype captures what construct are we about to generate. At some
    point it should be the lingua franca between GeneratedType and
    RuntimeType, capturing the intent.
    
    // FIXME: internal implementations
    // FIXME: GeneratedType.archetype()
    
    JIRA: YANGTOOLS-1578
    Change-Id: I69353a33fae9b9b4216be9e55d80449445047979
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/binding-model/src/main/java/org/opendaylight/yangtools/binding/model/BitsTypeObjectArchetype.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.binding.model;
+
+import static java.util.Objects.requireNonNull;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.binding.BitsTypeObject;
+import org.opendaylight.yangtools.binding.model.api.JavaTypeName;
+
+/**
+ * An archetype for a {@link BitsTypeObject}.
+ */
+@NonNullByDefault
+public record BitsTypeObjectArchetype(JavaTypeName typeName) implements TypeObjectArchetype<BitsTypeObjectArchetype> {
+    public BitsTypeObjectArchetype {
+        requireNonNull(typeName);
+    }
+
+    @Override
+    public Class<BitsTypeObjectArchetype> archetypeContract() {
+        return BitsTypeObjectArchetype.class;
+    }
+}
---
+++ binding/binding-model/src/main/java/org/opendaylight/yangtools/binding/model/IdentityArchetype.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.binding.model;
+
+import static java.util.Objects.requireNonNull;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.binding.BaseIdentity;
+import org.opendaylight.yangtools.binding.model.api.JavaTypeName;
+
+/**
+ * An archetype for a {@link BaseIdentity}.
+ */
+@NonNullByDefault
+public record IdentityArchetype(JavaTypeName typeName) implements Archetype<IdentityArchetype> {
+    public IdentityArchetype {
+        requireNonNull(typeName);
+    }
+
+    @Override
+    public Class<IdentityArchetype> archetypeContract() {
+        return IdentityArchetype.class;
+    }
+}
