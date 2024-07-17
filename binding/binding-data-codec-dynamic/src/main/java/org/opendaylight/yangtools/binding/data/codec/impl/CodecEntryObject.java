9fa11e839ae421945c78437fe06bfd76040606ff
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2024-07-17 15:34:32 +0200

    WIP: Add CodecEntryObject
    TODO
    
    JIRA: YANGTOOLS-1629
    Change-Id: I71d5b32764b513dd34aebc3ef84147ce59c78600
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

---
+++ binding/binding-data-codec-dynamic/src/main/java/org/opendaylight/yangtools/binding/data/codec/impl/CodecEntryObject.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.binding.data.codec.impl;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+import org.opendaylight.yangtools.binding.EntryObject;
+import org.opendaylight.yangtools.binding.Key;
+import org.opendaylight.yangtools.yang.data.api.schema.DataContainerNode;
+
+public abstract class CodecEntryObject<T extends EntryObject<T, K>, K extends Key<T>>
+        extends AugmentableCodecDataObject<T> implements EntryObject<T, K> {
+    private static final VarHandle KEY;
+
+    static {
+        try {
+            KEY = MethodHandles.lookup().findVarHandle(CodecEntryObject.class, "key",
+                Key.class);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    // Used via VarHandle
+    @SuppressWarnings("unused")
+    @SuppressFBWarnings(value = "UUF_UNUSED_FIELD", justification = "https://github.com/spotbugs/spotbugs/issues/2749")
+    private volatile K key;
+
+    protected CodecEntryObject(CommonDataObjectCodecContext<T, ?> context, DataContainerNode data) {
+        super(context, data);
+    }
+
+    @Override
+    public final K key() {
+        return (K) codecKey(KEY);
+    }
+}
