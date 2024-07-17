4d47e3d09383f33344653c55c1ba82b4ce092f27
Oleksandr Zharov <oleksandr.zharov@pantheon.tech>
2024-07-17 15:34:32 +0200

    WIP: Add CodecEntryObject
    TODO
    
    JIRA: YANGTOOLS-1629
    Change-Id: I71d5b32764b513dd34aebc3ef84147ce59c78600
    Signed-off-by: Oleksandr Zharov <oleksandr.zharov@pantheon.tech>

--- binding/binding-data-codec-dynamic/src/main/java/org/opendaylight/yangtools/binding/data/codec/impl/CodecDataObjectGenerator.java
+++ binding/binding-data-codec-dynamic/src/main/java/org/opendaylight/yangtools/binding/data/codec/impl/CodecDataObjectGenerator.java
@@ -243,6 +243,7 @@ abstract class CodecDataObjectGenerator<T extends CodecDataObject<?>> implements
     private static final Generic BB_STRING = TypeDefinition.Sort.describe(String.class);
     private static final TypeDescription BB_CDO = ForLoadedType.of(CodecDataObject.class);
     private static final TypeDescription BB_ACDO = ForLoadedType.of(AugmentableCodecDataObject.class);
+    private static final TypeDescription BB_CEO = ForLoadedType.of(CodecEntryObject.class);
 
     private static final StackManipulation FIRST_ARG_REF = MethodVariableAccess.REFERENCE.loadFrom(1);
 
@@ -273,6 +274,13 @@ abstract class CodecDataObjectGenerator<T extends CodecDataObject<?>> implements
             new Reusable<>(BB_ACDO, simpleProperties, daoProperties, keyMethod));
     }
 
+    static <T extends CodecDataObject<T>> Class<T> generateEntry(final BindingClassLoader loader,
+            final Class<?> bindingInterface, final ImmutableMap<Method, ValueNodeCodecContext> simpleProperties,
+            final Map<Class<?>, PropertyInfo> daoProperties, final Method keyMethod) {
+        return CodecPackage.CODEC.generateClass(loader, bindingInterface,
+            new Reusable<>(BB_CEO, simpleProperties, daoProperties, keyMethod));
+    }
+
     @Override
     public final GeneratorResult<T> generateClass(final BindingClassLoader loader, final String fqcn,
             final Class<?> bindingInterface) {
@@ -280,18 +288,18 @@ abstract class CodecDataObjectGenerator<T extends CodecDataObject<?>> implements
 
         final Generic bindingDef = TypeDefinition.Sort.describe(bindingInterface);
         @SuppressWarnings("unchecked")
-        Builder<T> builder = (Builder<T>) BB.subclass(Generic.Builder.parameterizedType(superClass, bindingDef).build())
-            .name(fqcn).implement(bindingDef);
+        Builder<T> builder;
+        // TODO this is temporary and does not solve all test error - WIP
+        if (keyMethod == null) {
+            builder = (Builder<T>) BB.subclass(Generic.Builder.parameterizedType(superClass, bindingDef).build())
+                .name(fqcn).implement(bindingDef);
+        } else {
+            final Generic keyMethodType = TypeDefinition.Sort.describe(keyMethod.getReturnType());
+            builder = (Builder<T>) BB.subclass(Generic.Builder.parameterizedType(superClass, bindingDef, keyMethodType)
+                .build()).name(fqcn).implement(bindingDef);
+        }
 
         builder = generateGetters(builder);
-
-        if (keyMethod != null) {
-            LOG.trace("Generating for key {}", keyMethod);
-            final String methodName = keyMethod.getName();
-            final TypeDescription retType = ForLoadedType.of(keyMethod.getReturnType());
-            builder = builder.defineMethod(methodName, retType, PUB_FINAL).intercept(
-                new KeyMethodImplementation(methodName, retType));
-        }
 
         // Final bits:
         return GeneratorResult.of(builder
