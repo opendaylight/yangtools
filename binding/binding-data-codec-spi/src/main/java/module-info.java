c4ff35b0918bc1a513aee1bae86b3f4c72ed9b66
Robert Varga <robert.varga@pantheon.tech>
2024-08-01 00:06:18 +0200

    Remove BindingDOMCodec{Factory,Services}
    These two carry the implication of a BindingRuntimeContext, which may
    not be generally available -- making binding-data-codec-spi dependent
    on binding-runtime-api.
    
    Replacement services are available through binding-data-codec-dynamic,
    hence we remove them and the corresponding runtime dependency.
    
    Change-Id: I6f2d407318e2610ac21c7b274b6acef3e8b9358f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-data-codec-spi/src/main/java/module-info.java
+++ binding/binding-data-codec-spi/src/main/java/module-info.java
@@ -9,7 +9,6 @@ module org.opendaylight.yangtools.binding.data.codec.spi {
     exports org.opendaylight.yangtools.binding.data.codec.spi;
 
     requires transitive org.opendaylight.yangtools.binding.data.codec.api;
-    requires transitive org.opendaylight.yangtools.binding.runtime.api;
 
     // Annotations
     requires static transitive org.eclipse.jdt.annotation;
