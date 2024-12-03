983bc78b75a02fb0d2056a85aa78517b33419a09
Robert Varga <robert.varga@pantheon.tech>
2024-12-03 22:16:30 +0100

    Add XSD regex model and parser
    This patch introduces an object model matching the XSD regular
    expression definition.
    
    We also add the corresponding ANTLR-based parser, which can produce this
    model.
    
    JIRA: YANGTOOLS-887
    Change-Id: Icfebea3686d044737705afeb2e2b507e7d01f3a1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ xml/regex/src/main/java/org/opendaylight/yangtools/regex/NegativeCharacterClassExpression.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.regex;
+
+import static java.util.Objects.requireNonNull;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+@NonNullByDefault
+public record NegativeCharacterClassExpression(CharacterGroup charGroup) implements CharacterClassExpression {
+    public NegativeCharacterClassExpression {
+        requireNonNull(charGroup);
+    }
+
+    @Override
+    public PositiveCharacterClassExpression negate() {
+        return new PositiveCharacterClassExpression(charGroup);
+    }
+
+    @Override
+    public void appendPatternFragment(final StringBuilder sb) {
+        charGroup.appendPatternFragment(sb.append("[^"));
+        sb.append(']');
+    }
+}
