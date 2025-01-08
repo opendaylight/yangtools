a96c9a04162eff628a258a232c85677a1c23b1fc
Robert Varga <robert.varga@pantheon.tech>
2025-01-08 21:28:47 +0100

    Unify state transition error reporting
    We have Illegal{Argument,State}Exception reported inconsistently when
    we detect unhandled State.
    
    This patch introduces illegalState() utility method, which unifies the
    message reported and optionally propagates Defuct.cause.
    
    JIRA: YANGTOOLS-1651
    Change-Id: Idbc4dcb6fc3619596b11ec9989dea032d620d3dc
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- data/yang-data-tree-ri/src/main/java/org/opendaylight/yangtools/yang/data/tree/impl/InMemoryDataTreeModification.java
+++ data/yang-data-tree-ri/src/main/java/org/opendaylight/yangtools/yang/data/tree/impl/InMemoryDataTreeModification.java
@@ -217,7 +217,7 @@ final class InMemoryDataTreeModification extends AbstractCursorAware implements 
         final var rootNode = switch (local) {
             case Open(var root) -> root;
             case Ready(var root) -> root;
-            default -> throw new IllegalStateException("Cannot access data in state " + local);
+            default -> throw illegalState(local, "access data of");
         };
 
         /*
@@ -308,7 +308,7 @@ final class InMemoryDataTreeModification extends AbstractCursorAware implements 
             // Simple fast case: just use the underlying modification
             case Noop noop -> snapshot.newModification();
             case Ready(var root) -> newModification(root);
-            default -> throw new IllegalStateException("Attempted to chain on modification in state " + local);
+            default -> throw illegalState(local, "chain on");
         };
     }
 
@@ -384,7 +384,7 @@ final class InMemoryDataTreeModification extends AbstractCursorAware implements 
             }
             case Open(var root) -> applyChildren(cursor, root);
             case Ready(var root) -> applyChildren(cursor, root);
-            default -> throw new IllegalStateException("Cannot apply in state " + local);
+            default -> throw illegalState(local, "access contents of");
         }
     }
 
@@ -414,7 +414,7 @@ final class InMemoryDataTreeModification extends AbstractCursorAware implements 
     public void ready() {
         final var local = acquireState();
         if (!(local instanceof Open open)) {
-            throw new IllegalStateException("Attempted to ready " + this + " in state " + local);
+            throw illegalState(local, "ready");
         }
 
         // We want a full CAS with setVolatile() memory semantics, as we want to force happen-before for everything,
@@ -464,8 +464,7 @@ final class InMemoryDataTreeModification extends AbstractCursorAware implements 
                 // no-op
             }
             case Ready(var root) -> validate(root, path, current);
-            // FIXME: this should be an IllegalStateException
-            default -> throw new IllegalArgumentException("Attempted to validate modification in state " + local);
+            default -> throw illegalState(local, "validate");
         }
     }
 
@@ -492,8 +491,7 @@ final class InMemoryDataTreeModification extends AbstractCursorAware implements 
         return switch (local) {
             case Noop noop -> new NoopDataTreeCandidate(YangInstanceIdentifier.of(), current);
             case Ready(var root) -> prepare(root, path, current);
-            // FIXME: this should be an IllegalStateException
-            default -> throw new IllegalArgumentException("Attempted to prepare modification in state " + local);
+            default -> throw illegalState(local, "prepare");
         };
     }
 
@@ -524,5 +522,10 @@ final class InMemoryDataTreeModification extends AbstractCursorAware implements 
     @NonNullByDefault
     State acquireState() {
         return verifyNotNull((State) STATE.getAcquire(this));
+    }
+
+    @NonNullByDefault
+    private static IllegalStateException illegalState(final State state, final String operation) {
+        throw new IllegalStateException("Attempted to " + operation + " modification in state " + state);
     }
 }
