538a5857df94abc1e997d9f0fcbb309cda846203
Robert Varga <robert.varga@pantheon.tech>
2025-02-06 17:44:34 +0100

    Convert yang-data-util to bnd-parent
    The first part of this is obvious -- change pom.xml, update
    module-info.java, add package-info.java's to hold @Export.
    
    The second part is less obvious: since we now explicitly control
    exports, we can from 'impl' from 'impl.context'. This is a safe
    operation, as we do not expose this package in JPMS nor in OSGi.
    
    Change-Id: Iaea72ca0db8dc9deae2538e109e5b6668dcbe4b7
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ data/yang-data-util/src/main/java/org/opendaylight/yangtools/yang/data/util/codec/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Various utilities to deal with encoding/decoding scalar values carried in
+ * {@link org.opendaylight.yangtools.yang.data.api.schema.LeafNode}s and similar.
+ */
+@org.osgi.annotation.bundle.Export
+package org.opendaylight.yangtools.yang.data.util.codec;
