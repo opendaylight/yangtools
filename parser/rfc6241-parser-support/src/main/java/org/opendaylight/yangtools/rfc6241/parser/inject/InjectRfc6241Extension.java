e015171fb2ef5aa77f318122684ffee8ceed06c7
Robert Varga <robert.varga@pantheon.tech>
2025-02-17 20:26:03 +0100

    Add ParserExtension
    This patch adds (Abstract)ParserExtension and converts
    rfc6241-parser-support to expose it.
    
    JIRA: YANGTOOLS-1152
    Change-Id: I5ca3dc9fa7f3802e8f54e5577b49c6e5184c1659
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/rfc6241-parser-support/src/main/java/org/opendaylight/yangtools/rfc6241/parser/inject/InjectRfc6241Extension.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc6241.parser.inject;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.rfc6241.parser.impl.Rfc6241Extension;
+
+/**
+ * Our extension exposed into the {@code javax.inject} world.
+ *
+ * @since 14.0.20
+ */
+@Singleton
+@NonNullByDefault
+@SuppressWarnings("exports")
+public final class InjectRfc6241Extension extends Rfc6241Extension {
+    /**
+     * Default constructor.
+     */
+    @Inject
+    public InjectRfc6241Extension() {
+        // visible for DI
+    }
+}
---
+++ parser/yang-parser-spi/src/main/java/org/opendaylight/yangtools/yang/parser/spi/AbstractParserExtension.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.spi;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.MoreObjects.ToStringHelper;
+import com.google.common.collect.ImmutableSet;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
+
+/**
+ * Abstract base class for {@link ParserExtension} implementations.
+ *
+ * @since 14.0.20
+ */
+@NonNullByDefault
+public abstract class AbstractParserExtension implements ParserExtension {
+    private final ImmutableSet<StatementDefinition> supportedStatements;
+
+    /**
+     * Construct an instance supporting a single statement.
+     *
+     * @param supportedStatement the supported statement
+     */
+    protected AbstractParserExtension(final StatementDefinition supportedStatement) {
+        this(ImmutableSet.of(supportedStatement));
+    }
+
+    /**
+     * Construct an instance supporting multiple statements.
+     *
+     * @param supportedStatement the supported statement
+     * @throws IllegalArgumentException if {@code supportedStatements} is empty
+     */
+    protected AbstractParserExtension(final StatementDefinition... supportedStatements) {
+        this(ImmutableSet.copyOf(supportedStatements));
+    }
+
+    /**
+     * Construct an instance supporting multiple statements.
+     *
+     * @param supportedStatements the supported statements
+     * @throws IllegalArgumentException if {@code supportedStatements} is empty
+     */
+    protected AbstractParserExtension(final ImmutableSet<StatementDefinition> supportedStatements) {
+        if (supportedStatements.isEmpty()) {
+            throw new IllegalArgumentException("supportedStatements must not be empty");
+        }
+        this.supportedStatements = requireNonNull(supportedStatements);
+    }
+
+    @Override
+    public final ImmutableSet<StatementDefinition> supportedStatements() {
+        return supportedStatements;
+    }
+
+    @Override
+    public final int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public final boolean equals(final @Nullable Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public final String toString() {
+        return addToStringAttributes(MoreObjects.toStringHelper(this).omitNullValues()).toString();
+    }
+
+    /**
+     * Add any extension-specific attributes. The default implementation adds {@link #supportedStatements()}.
+     *
+     * @param helper the {@link ToStringHelper}, ignoring {@code null} values
+     * @return the helper
+     */
+    protected ToStringHelper addToStringAttributes(final ToStringHelper helper) {
+        return helper.add("supportedStatements", supportedStatements);
+    }
+}
