04b38bea0ac1e143cf01890ee03454b5672b456b
Robert Varga <robert.varga@pantheon.tech>
2025-03-02 21:50:29 +0100

    Factor out netconf.databind.Request
    This is the counterpart to yang.common.RpcRequest, except having a nice
    tie in with RequestException as the error reporting tool.
    
    Change-Id: Ic6c3df7351fe4c7fd3e33509d1fc5af208cc6c59
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ protocol/databind/src/main/java/org/opendaylight/netconf/databind/AbstractRequest.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.databind;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.MoreObjects.ToStringHelper;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+import java.util.UUID;
+import org.eclipse.jdt.annotation.NonNull;
+
+/**
+ * Abstract base class for {@link Request} implementations. Each instance is automatically assigned a
+ * <a href="https://www.rfc-editor.org/rfc/rfc4122#section-4.4">type 4 UUID</a>.
+ *
+ * @param <R> type of reported result
+ */
+public abstract class AbstractRequest<R> implements Request<R> {
+    private static final VarHandle UUID_VH;
+
+    static {
+        try {
+            UUID_VH = MethodHandles.lookup().findVarHandle(AbstractRequest.class, "uuid", UUID.class);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    @SuppressFBWarnings(value = "UUF_UNUSED_FIELD", justification = "https://github.com/spotbugs/spotbugs/issues/2749")
+    private volatile UUID uuid;
+
+    /**
+     * Default constructor.
+     */
+    protected AbstractRequest() {
+        // nothing here
+    }
+
+    @Override
+    public final UUID uuid() {
+        final var existing = (UUID) UUID_VH.getAcquire(this);
+        return existing != null ? existing : loadUuid();
+    }
+
+    private @NonNull UUID loadUuid() {
+        final var created = UUID.randomUUID();
+        final var witness = (UUID) UUID_VH.compareAndExchangeRelease(this, null, created);
+        return witness != null ? witness : created;
+    }
+
+    @Override
+    public final void completeWith(final R result) {
+        onSuccess(requireNonNull(result));
+    }
+
+    @Override
+    public final void completeWith(final RequestException failure) {
+        onFailure(requireNonNull(failure));
+    }
+
+    protected abstract void onSuccess(@NonNull R result);
+
+    protected abstract void onFailure(@NonNull RequestException failure);
+
+    @Override
+    public final int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public final boolean equals(final Object obj) {
+        return super.equals(this);
+    }
+
+    @Override
+    public final String toString() {
+        return addToStringAttributes(MoreObjects.toStringHelper(this)).toString();
+    }
+
+    /**
+     * Add attributes to a {@link ToStringHelper}.
+     *
+     * @param helper the helper
+     * @return the helper
+     */
+    protected @NonNull ToStringHelper addToStringAttributes(final @NonNull ToStringHelper helper) {
+        helper.add("uuid", uuid());
+
+        final var principal = principal();
+        if (principal != null) {
+            helper.add("principal", principal.getName());
+        }
+
+        return helper;
+    }
+}
---
+++ protocol/databind/src/main/java/org/opendaylight/netconf/databind/Request.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netconf.databind;
+
+import com.google.common.annotations.Beta;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import java.security.Principal;
+import java.util.UUID;
+import java.util.concurrent.Executor;
+import java.util.function.Function;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * A request which can be completed either {@link #completeWith(Object) successfully} or
+ * {@link #completeWith(RequestException) unsuccessfully}. Each request has two invariants:
+ * <ol>
+ *   <li>it has a unique identifier, {@link #uuid()}</li>
+ *   <li>it has a {@link #principal()}, potentially unknown, which on behalf of whom the request is being made</li>
+ * </ol>
+ * A request can be thought of as a {@link FutureCallback} with attached metadata, where it is guaranteed the failure
+ * cause reported to {@link FutureCallback#onFailure(Throwable)} is always a {@link RequestException}. It can be adapted
+ * via {@link #transform(Function)} to a request of a different result type, similar to what a
+ * {@link Futures#transform(ListenableFuture, com.google.common.base.Function, Executor)} would do.
+ *
+ * <p>Completion is always signalled in the calling thread. Callers of {@link #completeWith(Object)} and
+ * {@link #completeWith(RequestException)} need to ensure that all side effects of the request have been completed. It
+ * is recommended that callers do not perform any further operations and just unwind the stack.
+ *
+ * @param <R> type of reported result
+ */
+@NonNullByDefault
+public interface Request<R> {
+    /**
+     * Return the identifier of this request.
+     *
+     * @return a {@link UUID}
+     */
+    UUID uuid();
+
+    /**
+     * Returns the {@link Principal} making this request.
+     *
+     * @return the Principal making this request, {@code null} if unauthenticated
+     */
+    @Nullable Principal principal();
+
+    /**
+     * Returns a request requesting a result of {@code U}. Completion of returned request will result in this request
+     * completing. If the request completes successfully, supplied function will be used to transform the result.
+     *
+     * @param <I> new result type
+     * @param function result mapping function
+     * @return a new request
+     */
+    // FIXME: this needs a better name
+    @Beta
+    <I> Request<I> transform(Function<I, R> function);
+
+    void completeWith(R result);
+
+    void completeWith(RequestException failure);
+}
