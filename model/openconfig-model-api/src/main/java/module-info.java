bb6179f4f605fb058b3c09530419000e1b8d0b62
Robert Varga <robert.varga@pantheon.tech>
2025-03-27 15:08:45 +0100

    Clean up OpenConfigStatements
    We have @SuppressFBWarnings which can be easily rectified using
    switching annotations. Do that to reduce proliferation.
    
    Change-Id: I8da1838958008c5086044b090f672d6b02a74eb2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/openconfig-model-api/src/main/java/module-info.java
+++ model/openconfig-model-api/src/main/java/module-info.java
@@ -15,6 +15,5 @@ module org.opendaylight.yangtools.openconfig.model.api {
 
     // Annotations
     requires static transitive org.eclipse.jdt.annotation;
-    requires static com.github.spotbugs.annotations;
     requires static org.osgi.annotation.bundle;
 }
--- model/openconfig-model-api/src/main/java/org/opendaylight/yangtools/openconfig/model/api/OpenConfigStatements.java
+++ model/openconfig-model-api/src/main/java/org/opendaylight/yangtools/openconfig/model/api/OpenConfigStatements.java
@@ -10,9 +10,8 @@ package org.opendaylight.yangtools.openconfig.model.api;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.annotations.Beta;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.Optional;
-import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.model.api.meta.ArgumentDefinition;
@@ -21,7 +20,6 @@ import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 
 @Beta
-@NonNullByDefault
 public enum OpenConfigStatements implements StatementDefinition {
     OPENCONFIG_ENCRYPTED_VALUE(QName.create(OpenConfigConstants.ENCRYPTED_VALUE_MODULE, "openconfig-encrypted-value"),
         null, OpenConfigHashedValueStatement.class, OpenConfigHashedValueEffectiveStatement.class),
@@ -34,12 +32,11 @@ public enum OpenConfigStatements implements StatementDefinition {
     OPENCONFIG_VERSION(QName.create(OpenConfigConstants.MODULE_NAMESPACE, "openconfig-version"), "semver",
         OpenConfigVersionStatement.class, OpenConfigVersionEffectiveStatement.class);
 
-    private final Class<? extends EffectiveStatement<?, ?>> effectiveRepresentation;
-    private final Class<? extends DeclaredStatement<?>> declaredRepresentation;
-    private final QName statementName;
+    private final @NonNull Class<? extends EffectiveStatement<?, ?>> effectiveRepresentation;
+    private final @NonNull Class<? extends DeclaredStatement<?>> declaredRepresentation;
+    private final @NonNull QName statementName;
     private final @Nullable QName argumentName;
 
-    @SuppressFBWarnings("NP_STORE_INTO_NONNULL_FIELD")
     OpenConfigStatements(final QName statementName, @Nullable final String argumentName,
             final Class<? extends DeclaredStatement<?>> declaredRepresentation,
             final Class<? extends EffectiveStatement<?, ?>> effectiveRepresentation) {
