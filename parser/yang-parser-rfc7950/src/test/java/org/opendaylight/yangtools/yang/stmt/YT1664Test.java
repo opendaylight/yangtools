f802fb98696d6a4e05adf02e57255c7fe465d356
Robert Varga <robert.varga@pantheon.tech>
2025-04-30 12:27:29 +0200

    WIP: Test min-elements/max-elements mismatch
    JIRA: YANGTOOLS-1664
    Change-Id: I18f472abf0210d6545b0ec9017e6e8a3350632cf
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/YT1664Test.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import org.junit.jupiter.api.Test;
+
+class YT1664Test extends AbstractYangTest {
+    @Test
+    void testLeafListBadElements() {
+        assertEffectiveModel("/bugs/YT1664/bar.yang");
+    }
+
+    @Test
+    void testListBadElements() {
+        assertEffectiveModel("/bugs/YT1664/foo.yang");
+    }
+}
