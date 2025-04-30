6b76da93fdb216d9db5b663c88c1a087712a2a35
Robert Varga <robert.varga@pantheon.tech>
2025-04-30 12:27:29 +0200

    Test min-elements/max-elements mismatch
    While the mismatch between min-elements and max-elements is caught in
    ElementCountConstraint, it is reported via an IAE.
    
    This patch improves the logic to catch the mismatch early, pointing to
    the offending parent statement.
    
    JIRA: YANGTOOLS-1664
    Change-Id: I18f472abf0210d6545b0ec9017e6e8a3350632cf
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/YT1664Test.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.hamcrest.CoreMatchers.startsWith;
+
+import org.junit.jupiter.api.Test;
+
+class YT1664Test extends AbstractYangTest {
+    @Test
+    void testLeafListBadElements() {
+        assertSourceException(startsWith("Conflicting min-elements '2' and max-elements '1' [at "),
+            "/bugs/YT1664/bar.yang");
+    }
+
+    @Test
+    void testListBadElements() {
+        assertSourceException(startsWith("Conflicting min-elements '2' and max-elements '1' [at "),
+            "/bugs/YT1664/foo.yang");
+    }
+}
