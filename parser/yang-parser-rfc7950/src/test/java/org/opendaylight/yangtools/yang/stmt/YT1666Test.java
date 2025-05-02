a46b5c3d6a4f626204ee2cc4a12b64c5c6b1ceaa
Robert Varga <robert.varga@pantheon.tech>
2025-05-02 13:10:56 +0200

    Validate max-elements argument
    max-elements' argument has to be a positive value, enforce that. Also
    improve interning to cover common values (1, 2).
    
    JIRA: YANGTOOLS-1666
    Change-Id: Iebad35c7825aa218405c0d80406384f39429d239
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/YT1666Test.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.hamcrest.CoreMatchers.startsWith;
+
+import org.junit.jupiter.api.Test;
+
+class YT1666Test extends AbstractYangTest {
+    @Test
+    void testNegativeMaxElements() {
+        assertSourceException(startsWith("Invalid max-elements argument \"-1\" [at "), "/bugs/YT1666/foo.yang");
+    }
+
+    @Test
+    void testLeadingZeroMaxElements() {
+        assertSourceException(startsWith("Invalid max-elements argument \"01\" [at "), "/bugs/YT1666/bar.yang");
+    }
+
+    @Test
+    void testMalformedMaxElements() {
+        assertSourceException(startsWith("Invalid max-elements argument \"1a\" [at "), "/bugs/YT1666/baz.yang");
+    }
+
+    @Test
+    void testZeroMaxElements() {
+        assertSourceException(startsWith("Invalid max-elements argument \"0\" [at "), "/bugs/YT1666/xyzzy.yang");
+    }
+}
