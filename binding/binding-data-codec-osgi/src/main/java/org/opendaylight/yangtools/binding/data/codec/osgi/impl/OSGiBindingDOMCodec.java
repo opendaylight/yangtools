faa823f30ee16fc186e677b5c3d54fed95b740c0
Robert Varga <robert.varga@pantheon.tech>
2025-09-10 12:09:13 +0200

    Simplify OSGiBindingDOMCodec
    We have constructor injection available, hence we can eliminate the
    dance around active/inactive instances, as we simply get our basics
    in early.
    
    While we are here, we also improve shutdown a bit by not instantiating
    the Map's values, but using a forEach() instead. We also clear the map
    to make sure we do not retain any references.
    
    JIRA: YANGTOOLS-1679
    Change-Id: I65838fbf407b5c9d44059e636e94bb09e65b5428
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-data-codec-osgi/src/main/java/org/opendaylight/yangtools/binding/data/codec/osgi/impl/OSGiBindingDOMCodec.java
+++ binding/binding-data-codec-osgi/src/main/java/org/opendaylight/yangtools/binding/data/codec/osgi/impl/OSGiBindingDOMCodec.java
@@ -7,15 +7,9 @@
  */
 package org.opendaylight.yangtools.binding.data.codec.osgi.impl;
 
-import static com.google.common.base.Verify.verify;
 import static java.util.Objects.requireNonNull;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.IdentityHashMap;
-import java.util.Map;
-import java.util.Set;
-import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.binding.data.codec.osgi.OSGiBindingDOMCodecServices;
 import org.opendaylight.yangtools.binding.data.codec.spi.BindingDOMCodecFactory;
 import org.opendaylight.yangtools.binding.runtime.osgi.OSGiBindingRuntimeContext;
@@ -32,131 +26,45 @@ import org.slf4j.LoggerFactory;
 
 @Component(immediate = true)
 public final class OSGiBindingDOMCodec {
-    // TODO: can we get rid of this complexity?
-    private abstract static class AbstractInstances {
-
-        abstract void add(OSGiBindingRuntimeContext runtimeContext);
-
-        abstract void remove(OSGiBindingRuntimeContext runtimeContext);
-
-        abstract @NonNull AbstractInstances toActive(BindingDOMCodecFactory codecFactory,
-            ComponentFactory<OSGiBindingDOMCodecServices> factory);
-
-        abstract @NonNull AbstractInstances toInactive();
-    }
-
-    private static final class InactiveInstances extends AbstractInstances {
-        private final Set<OSGiBindingRuntimeContext> instances = Collections.newSetFromMap(new IdentityHashMap<>());
-
-        InactiveInstances() {
-
-        }
-
-        InactiveInstances(final Set<OSGiBindingRuntimeContext> keySet) {
-            instances.addAll(keySet);
-        }
-
-        @Override
-        void add(final OSGiBindingRuntimeContext runtimeContext) {
-            verify(instances.add(runtimeContext), "Duplicate instance %s?!", runtimeContext);
-        }
-
-        @Override
-        void remove(final OSGiBindingRuntimeContext runtimeContext) {
-            instances.remove(runtimeContext);
-        }
-
-        @Override
-        AbstractInstances toActive(final BindingDOMCodecFactory codecFactory,
-                final ComponentFactory<OSGiBindingDOMCodecServices> factory) {
-            final ActiveInstances active = new ActiveInstances(codecFactory, factory);
-            instances.stream()
-                .sorted(Comparator.comparing(OSGiBindingRuntimeContext::generation).reversed())
-                .forEach(active::add);
-            return active;
-        }
-
-        @Override
-        AbstractInstances toInactive() {
-            throw new IllegalStateException("Attempted to deactivate inactive instances");
-        }
-    }
-
-    private static final class ActiveInstances extends AbstractInstances {
-        private final Map<OSGiBindingRuntimeContext, ComponentInstance<OSGiBindingDOMCodecServices>> instances =
-            new IdentityHashMap<>();
-        private final ComponentFactory<OSGiBindingDOMCodecServices> factory;
-        private final BindingDOMCodecFactory codecFactory;
-
-        ActiveInstances(final BindingDOMCodecFactory codecFactory,
-                final ComponentFactory<OSGiBindingDOMCodecServices> factory) {
-            this.codecFactory = requireNonNull(codecFactory);
-            this.factory = requireNonNull(factory);
-        }
-
-        @Override
-        void add(final OSGiBindingRuntimeContext runtimeContext) {
-            final var context = runtimeContext.service();
-
-            instances.put(runtimeContext, factory.newInstance(OSGiBindingDOMCodecServicesImpl.props(
-                runtimeContext.generation(), runtimeContext.getServiceRanking(),
-                codecFactory.createBindingDOMCodec(context))));
-        }
-
-        @Override
-        void remove(final OSGiBindingRuntimeContext runtimeContext) {
-            final ComponentInstance<OSGiBindingDOMCodecServices> instance = instances.remove(runtimeContext);
-            if (instance != null) {
-                instance.dispose();
-            } else {
-                LOG.warn("Instance for generation {} not found", runtimeContext.generation());
-            }
-        }
-
-        @Override
-        AbstractInstances toActive(final BindingDOMCodecFactory ignoreCodecFactory,
-                final ComponentFactory<OSGiBindingDOMCodecServices> ignoreFactory) {
-            throw new IllegalStateException("Attempted to activate active instances");
-        }
-
-        @Override
-        AbstractInstances toInactive() {
-            instances.values().forEach(ComponentInstance::dispose);
-            return new InactiveInstances(instances.keySet());
-        }
-
-    }
-
     private static final Logger LOG = LoggerFactory.getLogger(OSGiBindingDOMCodec.class);
 
-    @Reference
-    BindingDOMCodecFactory codecFactory = null;
-
-    @Reference(target = "(component.factory=" + OSGiBindingDOMCodecServicesImpl.FACTORY_NAME + ")")
-    ComponentFactory<OSGiBindingDOMCodecServices> contextFactory = null;
-
-    private AbstractInstances instances = new InactiveInstances();
-
-    @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
-    synchronized void addModuleInfoSnapshot(final OSGiBindingRuntimeContext runtimeContext) {
-        instances.add(runtimeContext);
-    }
-
-    synchronized void removeModuleInfoSnapshot(final OSGiBindingRuntimeContext runtimeContext) {
-        instances.remove(runtimeContext);
-    }
+    private final IdentityHashMap<OSGiBindingRuntimeContext, ComponentInstance<OSGiBindingDOMCodecServices>> instances =
+        new IdentityHashMap<>();
+    private final ComponentFactory<OSGiBindingDOMCodecServices> contextFactory;
+    private final BindingDOMCodecFactory codecFactory;
 
     @Activate
-    synchronized void activate() {
-        LOG.info("Binding/DOM Codec activating");
-        instances = instances.toActive(codecFactory, contextFactory);
+    public OSGiBindingDOMCodec(@Reference final BindingDOMCodecFactory codecFactory,
+            @Reference(target = "(component.factory=" + OSGiBindingDOMCodecServicesImpl.FACTORY_NAME + ")")
+            ComponentFactory<OSGiBindingDOMCodecServices> contextFactory) {
+        this.codecFactory = requireNonNull(codecFactory);
+        this.contextFactory = requireNonNull(contextFactory);
         LOG.info("Binding/DOM Codec activated");
     }
 
     @Deactivate
     synchronized void deactivate() {
         LOG.info("Binding/DOM Codec deactivating");
-        instances = instances.toInactive();
+        instances.forEach((ctx, instance) -> instance.dispose());
+        instances.clear();
         LOG.info("Binding/DOM Codec deactivated");
+    }
+
+    @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
+    synchronized void addRuntimeContext(final OSGiBindingRuntimeContext runtimeContext) {
+        final var context = runtimeContext.service();
+
+        instances.put(runtimeContext, contextFactory.newInstance(OSGiBindingDOMCodecServicesImpl.props(
+            runtimeContext.generation(), runtimeContext.getServiceRanking(),
+            codecFactory.createBindingDOMCodec(context))));
+    }
+
+    synchronized void removeRuntimeContext(final OSGiBindingRuntimeContext runtimeContext) {
+        final var instance = instances.remove(runtimeContext);
+        if (instance != null) {
+            instance.dispose();
+        } else {
+            LOG.warn("Instance for generation {} not found", runtimeContext.generation());
+        }
     }
 }
