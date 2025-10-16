7c4f3b7f52edbbdf840f857239a5d1ed88c399f8
Robert Varga <robert.varga@pantheon.tech>
2025-10-16 10:39:38 +0200

    Seal ModuleLike
    This interface is not meant to be used by itself, make sure we restrict
    such use.
    
    Change-Id: I9a773aa9f05d3eb47c4350c38abe98185b7d1e84
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/Submodule.java
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/Submodule.java
@@ -12,6 +12,6 @@ import org.opendaylight.yangtools.yang.model.api.stmt.SubmoduleEffectiveStatemen
 /**
  * This interface contains the methods for getting the data from the YANG submodule.
  */
-public interface Submodule extends ModuleLike, EffectiveStatementEquivalent<SubmoduleEffectiveStatement> {
+public non-sealed interface Submodule extends ModuleLike, EffectiveStatementEquivalent<SubmoduleEffectiveStatement> {
 
 }
