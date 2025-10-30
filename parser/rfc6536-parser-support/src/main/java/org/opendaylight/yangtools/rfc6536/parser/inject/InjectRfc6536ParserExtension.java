d7ad75ef864c83f03c3cd2d893b2551c99d9a629
Robert Varga <robert.varga@pantheon.tech>
2025-10-30 13:56:04 +0100

    Expose ParserExtension from rfc6536-parser-support
    Expose ParserExtension to allow it to be discovered without explicitly
    depending on it.
    
    JIRA: YANGTOOLS-1702
    Change-Id: I4028a0300c94542a103cbaa5e01c76a6aabf9195
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/rfc6536-parser-support/src/main/java/org/opendaylight/yangtools/rfc6536/parser/inject/InjectRfc6536ParserExtension.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc6536.parser.inject;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.rfc6536.parser.impl.Rfc6536ParserExtension;
+
+/**
+ * Parser support for {@code ietf-netconf.yang} exposed into the {@code javax.inject} world.
+ *
+ * @since 14.0.20
+ */
+@Singleton
+@NonNullByDefault
+@SuppressWarnings("exports")
+public final class InjectRfc6536ParserExtension extends Rfc6536ParserExtension {
+    /**
+     * Default constructor.
+     */
+    @Inject
+    public InjectRfc6536ParserExtension() {
+        // visible for DI
+    }
+}
