3b45b1fef554a2c8d373545bd357da380bcbff53
Robert Varga <robert.varga@pantheon.tech>
2025-10-30 13:56:04 +0100

    Expose ParserExtension from rfc6536-parser-support
    Expose ParserExtension to allow it to be discovered without explicitly
    depending on it.
    
    JIRA: YANGTOOLS-1702
    Change-Id: I4028a0300c94542a103cbaa5e01c76a6aabf9195
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/rfc6536-parser-support/src/main/java/module-info.java
+++ parser/rfc6536-parser-support/src/main/java/module-info.java
@@ -5,8 +5,20 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+import org.opendaylight.yangtools.yang.parser.spi.ParserExtension;
+
+/**
+ * A {@link ParserExtension} providing support for extensions introduced in
+ * <a href="https://www.rfc-editor.org/rfc/rfc6536">RFC6536</a>.
+ *
+ * @provides ParserExtension
+ */
 module org.opendaylight.yangtools.rfc6536.parser.support {
+    // FIXME: do not export this package
     exports org.opendaylight.yangtools.rfc6536.parser;
+    exports org.opendaylight.yangtools.rfc6536.parser.inject;
+
+    provides ParserExtension with org.opendaylight.yangtools.rfc6536.parser.impl.Rfc6536ParserExtension;
 
     requires transitive com.google.common;
     requires transitive org.opendaylight.yangtools.yang.parser.api;
@@ -18,7 +30,9 @@ module org.opendaylight.yangtools.rfc6536.parser.support {
     requires org.slf4j;
 
     // Annotations
+    requires static transitive javax.inject;
     requires static transitive org.eclipse.jdt.annotation;
+    requires static org.kohsuke.metainf_services;
     requires static org.osgi.annotation.bundle;
+    requires static org.osgi.service.component.annotations;
 }
-
