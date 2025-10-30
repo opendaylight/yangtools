cf44b82f811e0789b75b02deb4eed46cb5caeda5
Robert Varga <robert.varga@pantheon.tech>
2025-10-30 18:35:08 +0100

    Expose ParserExtension from rfc8528-parser-support
    Expose ParserExtension to allow it to be discovered without explicitly
    depending on it.
    
    JIRA: YANGTOOLS-1702
    Change-Id: Ide1796718a57c8e2f9978819d32ecc459db5262f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/rfc8528-parser-support/src/main/java/org/opendaylight/yangtools/rfc8528/parser/impl/Rfc8528ParserExtension.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc8528.parser.impl;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.kohsuke.MetaInfServices;
+import org.opendaylight.yangtools.rfc8528.model.api.SchemaMountStatements;
+import org.opendaylight.yangtools.rfc8528.parser.MountPointStatementSupport;
+import org.opendaylight.yangtools.rfc8528.parser.inject.InjectRfc8528ParserExtension;
+import org.opendaylight.yangtools.yang.parser.api.YangParserConfiguration;
+import org.opendaylight.yangtools.yang.parser.spi.AbstractParserExtension;
+import org.opendaylight.yangtools.yang.parser.spi.ParserExtension;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StatementSupportBundle;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Parser support for {@code ietf-yang-schema-mount.yang}.
+ *
+ * @since 14.0.20
+ */
+@MetaInfServices
+@NonNullByDefault
+@Component(service = ParserExtension.class)
+public sealed class Rfc8528ParserExtension extends AbstractParserExtension permits InjectRfc8528ParserExtension {
+    /**
+     * Default constructor.
+     */
+    public Rfc8528ParserExtension() {
+        super(SchemaMountStatements.MOUNT_POINT);
+    }
+
+    @Override
+    public final StatementSupportBundle configureBundle(final YangParserConfiguration config) {
+        return StatementSupportBundle.builder()
+            .addSupport(new MountPointStatementSupport(config))
+            .build();
+    }
+}
