03a7d22fd110c892200f04c05030ec74420227a7
Robert Varga <robert.varga@pantheon.tech>
2025-10-30 11:35:42 +0100

    Add parser.spi.ParserExtension
    Add ParserExtension, which exposes the ability to configure a
    StatementSupportBundle based on a YangParserConfiguration.
    
    We also provide a base AbstractParserExtension, which makes implementing
    extensions quite simple.
    
    JIRA: YANGTOOLS-1152
    Change-Id: I4d9660807cfcf4bffd7c3d644f9c36382bf6edd5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-spi/src/main/java/org/opendaylight/yangtools/yang/parser/spi/ParserExtension.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.spi;
+
+import java.util.Set;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
+import org.opendaylight.yangtools.yang.parser.api.YangParserConfiguration;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StatementSupportBundle;
+
+/**
+ * Service discovery mechanism and instantiation for extensions to a parser reactor. Implementations are encouraged to
+ * take advantage of {@link AbstractParserExtension}.
+ *
+ * @since 14.0.20
+ */
+@NonNullByDefault
+public interface ParserExtension {
+    /**
+     * Configure this extension with {@link YangParserConfiguration}, returning the resultas a
+     * {@link StatementSupportBundle}.
+     *
+     * @param config requested configuration
+     * @return A {@link StatementSupportBundle}
+     */
+    StatementSupportBundle configureBundle(YangParserConfiguration config);
+
+    /**
+     * {@return the set {@link StatementDefinition}s supported by this extension. Guaranteed to contain at least one
+     * element.}
+     */
+    Set<StatementDefinition> supportedStatements();
+}
