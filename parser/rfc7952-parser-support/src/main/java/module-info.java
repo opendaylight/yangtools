e25ed4089b51d43c6f1cde07d8a4a12fa45d7244
Robert Varga <robert.varga@pantheon.tech>
2025-10-30 14:36:21 +0100

    Expose ParserExtension from rfc7952-parser-support
    Expose ParserExtension to allow it to be discovered without explicitly
    depending on it.
    
    JIRA: YANGTOOLS-1702
    Change-Id: Icb9c14f4f4d486bd2f4503b4abf6c4698ff9002c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/rfc7952-parser-support/src/main/java/module-info.java
+++ parser/rfc7952-parser-support/src/main/java/module-info.java
@@ -5,8 +5,19 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+import org.opendaylight.yangtools.yang.parser.spi.ParserExtension;
+
+/**
+ * YANG parser support for metamodel extensions defined in <a href="https://www.rfc-editor.org/rfc/rfc7952">RFC7952</a>.
+ *
+ * @provides ParserExtension
+ */
 module org.opendaylight.yangtools.rfc7952.parser.support {
+    // FIXME: do not export this package
     exports org.opendaylight.yangtools.rfc7952.parser;
+    exports org.opendaylight.yangtools.rfc7952.parser.inject;
+
+    provides ParserExtension with org.opendaylight.yangtools.rfc7952.parser.impl.Rfc7952ParserExtension;
 
     requires transitive com.google.common;
     requires transitive org.opendaylight.yangtools.yang.parser.api;
@@ -19,6 +30,9 @@ module org.opendaylight.yangtools.rfc7952.parser.support {
     requires org.slf4j;
 
     // Annotations
+    requires static transitive javax.inject;
     requires static transitive org.eclipse.jdt.annotation;
+    requires static org.kohsuke.metainf_services;
     requires static org.osgi.annotation.bundle;
+    requires static org.osgi.service.component.annotations;
 }
