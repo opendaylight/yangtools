881e3aaa0c625bd90e52f1fa55b3031bcdd12a2c
Robert Varga <robert.varga@pantheon.tech>
2025-10-30 12:25:33 +0100

    WIP: convert odlext-parser-support
    // FIXME: finish this up
    
    JIRA: YANGTOOLS-1152
    Change-Id: Ib28caf360f810618a79bc40643edc72cfbf820f5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/odlext-parser-support/src/main/java/org/opendaylight/yangtools/odlext/parser/impl/OdlCodegenParserExtension.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.odlext.parser.impl;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.kohsuke.MetaInfServices;
+import org.opendaylight.yangtools.odlext.model.api.AugmentIdentifierStatement;
+import org.opendaylight.yangtools.odlext.parser.AugmentIdentifierStatementSupport;
+import org.opendaylight.yangtools.odlext.parser.inject.InjectOdlCodegenParserExtension;
+import org.opendaylight.yangtools.yang.parser.api.YangParserConfiguration;
+import org.opendaylight.yangtools.yang.parser.spi.AbstractParserExtension;
+import org.opendaylight.yangtools.yang.parser.spi.ParserExtension;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StatementSupportBundle;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Parser support for {@code odl-codegen-extensions.yang}.
+ *
+ * @since 14.0.20
+ */
+@MetaInfServices
+@NonNullByDefault
+@Component(service = ParserExtension.class)
+public sealed class OdlCodegenParserExtension extends AbstractParserExtension permits InjectOdlCodegenParserExtension {
+    /**
+     * Default constructor.
+     */
+    public OdlCodegenParserExtension() {
+        super(AugmentIdentifierStatement.DEFINITION);
+    }
+
+    @Override
+    public final StatementSupportBundle configureBundle(final YangParserConfiguration config) {
+        return StatementSupportBundle.builder().addSupport(new AugmentIdentifierStatementSupport(config)).build();
+    }
+}
