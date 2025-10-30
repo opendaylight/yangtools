105d4c2f22f5bc8911b6e2a8db1958022770b08b
Robert Varga <robert.varga@pantheon.tech>
2025-10-30 14:36:21 +0100

    Expose ParserExtension from rfc7952-parser-support
    Expose ParserExtension to allow it to be discovered without explicitly
    depending on it.
    
    JIRA: YANGTOOLS-1702
    Change-Id: Icb9c14f4f4d486bd2f4503b4abf6c4698ff9002c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/rfc7952-parser-support/src/main/java/org/opendaylight/yangtools/rfc7952/parser/impl/Rfc7952ParserExtension.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc7952.parser.impl;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.kohsuke.MetaInfServices;
+import org.opendaylight.yangtools.rfc7952.model.api.MetadataStatements;
+import org.opendaylight.yangtools.rfc7952.parser.AnnotationStatementSupport;
+import org.opendaylight.yangtools.rfc7952.parser.inject.InjectRfc7952ParserExtension;
+import org.opendaylight.yangtools.yang.parser.api.YangParserConfiguration;
+import org.opendaylight.yangtools.yang.parser.spi.AbstractParserExtension;
+import org.opendaylight.yangtools.yang.parser.spi.ParserExtension;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StatementSupportBundle;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Parser support for {@code ietf-yang-metadata.yang}.
+ *
+ * @since 14.0.20
+ */
+@NonNullByDefault
+@MetaInfServices(ParserExtension.class)
+@Component(service = ParserExtension.class)
+public sealed class Rfc7952ParserExtension extends AbstractParserExtension permits InjectRfc7952ParserExtension {
+    /**
+     * Default constructor.
+     */
+    public Rfc7952ParserExtension() {
+        super(MetadataStatements.ANNOTATION);
+    }
+
+    @Override
+    public final StatementSupportBundle configureBundle(final YangParserConfiguration config) {
+        return StatementSupportBundle.builder().addSupport(new AnnotationStatementSupport(config)).build();
+    }
+}
---
+++ parser/rfc7952-parser-support/src/main/java/org/opendaylight/yangtools/rfc7952/parser/inject/InjectRfc7952ParserExtension.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc7952.parser.inject;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.rfc7952.parser.impl.Rfc7952ParserExtension;
+
+/**
+ * Parser support for {@code ietf-yang-metadata.yang} exposed into the {@code javax.inject} world.
+ *
+ * @since 14.0.20
+ */
+@Singleton
+@NonNullByDefault
+@SuppressWarnings("exports")
+public final class InjectRfc7952ParserExtension extends Rfc7952ParserExtension {
+    /**
+     * Default constructor.
+     */
+    @Inject
+    public InjectRfc7952ParserExtension() {
+        // visible for DI
+    }
+}
