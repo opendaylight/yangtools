6d0843f64608a2031c8690c6e561d5a6cf3a7b8c
Robert Varga <robert.varga@pantheon.tech>
2025-10-30 13:56:04 +0100

    Expose ParserExtension from rfc6536-parser-support
    Expose ParserExtension to allow it to be discovered without explicitly
    depending on it.
    
    JIRA: YANGTOOLS-1702
    Change-Id: I4028a0300c94542a103cbaa5e01c76a6aabf9195
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/rfc6536-parser-support/src/main/java/org/opendaylight/yangtools/rfc6536/parser/impl/Rfc6536ParserExtension.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.rfc6536.parser.impl;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.kohsuke.MetaInfServices;
+import org.opendaylight.yangtools.rfc6536.model.api.NACMStatements;
+import org.opendaylight.yangtools.rfc6536.parser.DefaultDenyAllStatementSupport;
+import org.opendaylight.yangtools.rfc6536.parser.DefaultDenyWriteStatementSupport;
+import org.opendaylight.yangtools.rfc6536.parser.inject.InjectRfc6536ParserExtension;
+import org.opendaylight.yangtools.yang.parser.api.YangParserConfiguration;
+import org.opendaylight.yangtools.yang.parser.spi.AbstractParserExtension;
+import org.opendaylight.yangtools.yang.parser.spi.ParserExtension;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StatementSupportBundle;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Parser support for {@code ietf-netconf-acm.yang}.
+ *
+ * @since 14.0.20
+ */
+@NonNullByDefault
+@MetaInfServices(ParserExtension.class)
+@Component(service = ParserExtension.class)
+public sealed class Rfc6536ParserExtension extends AbstractParserExtension permits InjectRfc6536ParserExtension {
+    /**
+     * Default constructor.
+     */
+    public Rfc6536ParserExtension() {
+        super(NACMStatements.values());
+    }
+
+    @Override
+    public final StatementSupportBundle configureBundle(final YangParserConfiguration config) {
+        return StatementSupportBundle.builder()
+            .addSupport(new DefaultDenyAllStatementSupport(config))
+            .addSupport(new DefaultDenyWriteStatementSupport(config))
+            .build();
+    }
+}
