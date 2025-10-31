2bd1af156a56679475e0baeef6e3d8ee52c6ce9f
Robert Varga <robert.varga@pantheon.tech>
2025-10-31 10:46:04 +0100

    Convert binding-codegen to bnd-parent
    This is another trivial conversion.
    
    Change-Id: Ib3860a6c4dbe658e18ec3bbe73eb10140187628f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-codegen/src/main/java/org/opendaylight/yangtools/binding/codegen/package-info.java
+++ binding/binding-codegen/src/main/java/org/opendaylight/yangtools/binding/codegen/package-info.java
@@ -5,4 +5,5 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+@org.osgi.annotation.bundle.Export
 package org.opendaylight.yangtools.binding.codegen;
