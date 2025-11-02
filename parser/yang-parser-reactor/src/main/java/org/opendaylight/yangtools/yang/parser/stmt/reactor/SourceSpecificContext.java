5650d98a69231599d418247bdfb7192a82f1395c
tibor.kral <tibor.kral@pantheon.tech>
2025-11-02 20:56:42 +0100

    Resolve involved sources before moving through ModelProcessingPhases
    Instead of processing all the main sources and library sources figure
    out which ones are actually referenced. Only those are then pushed
    through the ProcessingPhases. The rest is ignored.
    
    JIRA: YANGTOOLS-1112
    Change-Id: I8875cd44ece2bfe6a5b71aa8d6b6658199c74290
    Signed-off-by: tibor.kral <tibor.kral@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/SourceSpecificContext.java
+++ parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/SourceSpecificContext.java
@@ -34,6 +34,7 @@ import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementSourceException;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementSourceReference;
 import org.opendaylight.yangtools.yang.model.api.source.SourceIdentifier;
+import org.opendaylight.yangtools.yang.model.spi.source.SourceInfo;
 import org.opendaylight.yangtools.yang.parser.spi.ParserNamespaces;
 import org.opendaylight.yangtools.yang.parser.spi.meta.InferenceException;
 import org.opendaylight.yangtools.yang.parser.spi.meta.ModelActionBuilder;
@@ -147,6 +148,10 @@ final class SourceSpecificContext implements NamespaceStorage, Mutable {
         return inProgressPhase;
     }
 
+    public SourceInfo getSourceInfo() {
+        return source.getSourceInfo();
+    }
+    
     AbstractResumedStatement<?, ?, ?> createDeclaredChild(final AbstractResumedStatement<?, ?, ?> current,
             final int childId, final QName name, final String argument, final StatementSourceReference ref) {
         StatementDefinitionContext<?, ?, ?> def = globalContext.getStatementDefinition(getRootVersion(), name);
