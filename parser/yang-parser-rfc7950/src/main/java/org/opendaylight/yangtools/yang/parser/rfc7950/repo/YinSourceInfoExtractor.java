6699ad6d9d7282afd1d9e98b105ebd1bc7be4460
tibor.kral <tibor.kral@pantheon.tech>
2025-11-02 19:17:45 +0100

    Add YIN SourceInfoExtractor
    Just like with the old YangIRSourceInfoExtractor we will need
    the ability to extract SourceInfo from YIN sources as well.
    
    // FIXME: the unique map seems bogus
    // FIXME: we do not pay attention to element namespace
    // FIXME: we should be extracting arguments based on StatementDefinition
              to decide whether to use attributes or children
    // FIXME: figure out the StatementSourceReference story?
    // FIXME: fixup exceptions
    
    // FIXME: add tests coverage for YinSourceInfoExtractor
    
    JIRA: YANGTOOLS-1747
    Change-Id: I44ccf5bf1d2ef13b176b8493149471262479f1ef
    Signed-off-by: tibor.kral <tibor.kral@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/repo/YinSourceInfoExtractor.java
@@ -0,0 +1,324 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.repo;
+
+import static com.google.common.base.Verify.verify;
+import static java.util.Objects.requireNonNull;
+import static org.opendaylight.yangtools.yang.common.YangConstants.RFC6020_YIN_NAMESPACE_STRING;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.common.Revision;
+import org.opendaylight.yangtools.yang.common.UnresolvedQName.Unqualified;
+import org.opendaylight.yangtools.yang.common.XMLNamespace;
+import org.opendaylight.yangtools.yang.common.YangVersion;
+import org.opendaylight.yangtools.yang.model.api.YangStmtMapping;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementSourceReference;
+import org.opendaylight.yangtools.yang.model.api.source.SourceDependency;
+import org.opendaylight.yangtools.yang.model.api.source.SourceIdentifier;
+import org.opendaylight.yangtools.yang.model.spi.source.SourceInfo;
+import org.opendaylight.yangtools.yang.model.spi.source.SourceInfo.ExtractorException;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public abstract sealed class YinSourceInfoExtractor implements SourceInfo.Extractor {
+    private static final class ForModule extends YinSourceInfoExtractor {
+        private static final @NonNull String NAMESPACE = YangStmtMapping.NAMESPACE.getStatementName().getLocalName();
+        private static final @NonNull String NAMESPACE_ARG =
+            YangStmtMapping.NAMESPACE.getArgumentDefinition().orElseThrow().argumentName().getLocalName();
+
+        ForModule(final Element root, final SourceIdentifier rootIdentifier) {
+            super(root, rootIdentifier);
+        }
+
+        @Override
+        public SourceInfo.Module extractSourceInfo() throws ExtractorException {
+            final var builder = SourceInfo.Module.builder();
+            fillCommon(builder);
+            return builder
+                .setPrefix(extractPrefix(root))
+                .setNamespace(extractNamespace())
+                .build();
+        }
+
+        private @NonNull XMLNamespace extractNamespace() throws ExtractorException {
+            return getFirstElementArgument(root, NAMESPACE, NAMESPACE_ARG, (element, arg) -> {
+                try {
+                    return XMLNamespace.of(arg);
+                } catch (IllegalArgumentException e) {
+                    throw newInvalidArgument(element, e);
+                }
+            });
+        }
+    }
+
+    private static final class ForSubmodule extends YinSourceInfoExtractor {
+        ForSubmodule(final Element root, final SourceIdentifier rootIdentifier) {
+            super(root, rootIdentifier);
+        }
+
+        @Override
+        public SourceInfo.Submodule extractSourceInfo() throws ExtractorException {
+            final var builder = SourceInfo.Submodule.builder();
+            fillCommon(builder);
+            return builder
+                .setBelongsTo(extractBelongsTo())
+                .build();
+        }
+    }
+
+    @NonNullByDefault
+    @FunctionalInterface
+    private interface ArgumentFunction<T> {
+
+        T apply(Element parent, String argument) throws ExtractorException;
+    }
+
+    private static final @NonNull String MODULE = "module";
+    private static final @NonNull String SUBMODULE = "submodule";
+
+    static {
+        verify(MODULE.equals(YangStmtMapping.MODULE.getStatementName().getLocalName()));
+        verify(SUBMODULE.equals(YangStmtMapping.SUBMODULE.getStatementName().getLocalName()));
+    }
+
+    private static final @NonNull String BELONGS_TO = YangStmtMapping.BELONGS_TO.getStatementName().getLocalName();
+    private static final @NonNull String IMPORT = YangStmtMapping.IMPORT.getStatementName().getLocalName();
+    private static final @NonNull String INCLUDE = YangStmtMapping.INCLUDE.getStatementName().getLocalName();
+    private static final @NonNull String PREFIX = YangStmtMapping.PREFIX.getStatementName().getLocalName();
+    private static final @NonNull String PREFIX_ARG =
+        YangStmtMapping.PREFIX.getArgumentDefinition().orElseThrow().argumentName().getLocalName();
+    private static final @NonNull String REVISION = YangStmtMapping.REVISION.getStatementName().getLocalName();
+    private static final @NonNull String REVISION_DATE =
+        YangStmtMapping.REVISION_DATE.getStatementName().getLocalName();
+    private static final @NonNull String YANG_VERSION = YangStmtMapping.YANG_VERSION.getStatementName().getLocalName();
+
+    private final ImmutableMap<String, Node> uniqueRootElements;
+    private final @NonNull SourceIdentifier rootIdentifier;
+
+    final @NonNull Element root;
+
+    YinSourceInfoExtractor(final Element root, final SourceIdentifier rootIdentifier) {
+        this.root = requireNonNull(root);
+        this.rootIdentifier = requireNonNull(rootIdentifier);
+        uniqueRootElements = mapUniqueChildElementsOf(root);
+    }
+
+    private static ImmutableMap<String, Node> mapUniqueChildElementsOf(final Node root) {
+        final var childNodes = root.getChildNodes();
+        final var childMap = new HashMap<String, Node>();
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            final var child = childNodes.item(i);
+            if (child.getNodeType() == Node.ELEMENT_NODE) {
+                childMap.putIfAbsent(child.getLocalName(), child);
+            }
+        }
+        return ImmutableMap.copyOf(childMap);
+    }
+
+    public static SourceInfo.Extractor of(final Node root, final SourceIdentifier sourceId) throws ExtractorException {
+        if (!(root instanceof Element element)) {
+            throw new ExtractorException("Root node is not an element");
+        }
+        if (!RFC6020_YIN_NAMESPACE_STRING.equals(element.getNamespaceURI())) {
+            throw new ExtractorException("Root element does not have YIN namespace", refOf(element));
+        }
+        return switch (element.getLocalName()) {
+            case MODULE -> new ForModule(element, sourceId);
+            case SUBMODULE -> new ForSubmodule(element, sourceId);
+            default -> throw new ExtractorException("Root element needs to be a module or submodule", refOf(element));
+        };
+    }
+
+    final void fillCommon(final SourceInfo.Builder<?, ?> builder) {
+        builder.setName(extractName());
+        final var yangVersion = extractYangVersion();
+        if (yangVersion != null) {
+            builder.setYangVersion(yangVersion);
+        }
+        extractRevisions(builder);
+        extractIncludes(builder);
+        extractImports(builder);
+    }
+
+    final Unqualified extractModulePrefix() {
+        final Node node = uniqueRootElements.get(PREFIX);
+        if (node != null) {
+            return readNodeQName(node);
+        }
+        throw new IllegalArgumentException("No prefix statement in " + refOf(root));
+    }
+
+    final SourceDependency.BelongsTo extractBelongsTo() {
+        final Node belongsToNode = uniqueRootElements.get(BELONGS_TO);
+        if (belongsToNode != null) {
+            return readBelongsTo(belongsToNode);
+        }
+        throw new IllegalArgumentException("No belongs-to statement in " + refOf(root));
+    }
+
+    // FIXME: what is this?
+    private Unqualified extractName() {
+        return rootIdentifier.name();
+    }
+
+    private YangVersion extractYangVersion() {
+        final Node child = uniqueRootElements.get(YANG_VERSION);
+        return child != null ? readYangVersion(child) : null;
+    }
+
+    private void extractRevisions(final SourceInfo.Builder<?, ?> builder) {
+        for (final Node revisionNode : getChildrenOfType(root, REVISION)) {
+            builder.addRevision(extractRevision(revisionNode));
+        }
+    }
+
+    private void extractIncludes(final SourceInfo.Builder<?, ?> builder) {
+        for (final Node includeNode : getChildrenOfType(root, INCLUDE)) {
+            builder.addInclude(extractInclude(includeNode));
+        }
+    }
+
+    private void extractImports(final SourceInfo.Builder<?, ?> builder) {
+        for (final Node importNode : getChildrenOfType(root, IMPORT)) {
+            builder.addImport(extractImport(importNode));
+        }
+    }
+
+    private static List<Node> getChildrenOfType(final Node parent, final String type) {
+        final NodeList childNodes = parent.getChildNodes();
+        final LinkedList<Node> children = new LinkedList<>();
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            final Node child = childNodes.item(i);
+            if (child.getNodeType() == Node.ELEMENT_NODE) {
+                if (child.getLocalName().equals(type)) {
+                    children.add(child);
+                }
+            }
+        }
+        return children;
+    }
+
+    private static StatementSourceReference refOf(final Node node) {
+        return StatementSourceReferenceHandler.extractRef((Element) node);
+    }
+
+    private static Revision extractRevision(final Node revisionNode) {
+        return Revision.of(revisionNode.getAttributes().item(0).getNodeValue());
+    }
+
+    private static SourceDependency.Include extractInclude(final Node includeNode) {
+        final Unqualified name = readNodeQName(includeNode);
+        Revision revision = null;
+
+        final Node revisionNode = getChildNode(includeNode, REVISION_DATE);
+        if (revisionNode != null) {
+            revision = readRevisionDate(revisionNode);
+        }
+        return new SourceDependency.Include(name, revision);
+    }
+
+    private static Unqualified readNodeQName(final Node node) {
+        return Unqualified.of(node.getAttributes().item(0).getNodeValue());
+    }
+
+    private static SourceDependency.Import extractImport(final Node importNode) {
+        final Unqualified name = readNodeQName(importNode);
+        Unqualified prefix = null;
+        Revision revision = null;
+
+        final Node prefixNode = getChildNode(importNode, PREFIX);
+        if (prefixNode == null) {
+            throw new IllegalArgumentException("No prefix statement in " + refOf(importNode));
+        }
+        prefix = readNodeQName(prefixNode);
+
+        final Node revisionNode = getChildNode(importNode, REVISION_DATE);
+        if (revisionNode != null) {
+            revision = readRevisionDate(revisionNode);
+        }
+
+        return new SourceDependency.Import(name, prefix, revision);
+    }
+
+    private static Node getChildNode(final Node root, final String nodeName) {
+        final var childNodes = root.getChildNodes();
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            final var child = childNodes.item(i);
+            if (child.getNodeType() == Node.ELEMENT_NODE && child.getLocalName().equals(nodeName)) {
+                return child;
+            }
+        }
+        return null;
+    }
+
+    private static YangVersion readYangVersion(final Node node) {
+        return YangVersion.forString(node.getAttributes().item(0).getNodeValue());
+    }
+
+    private static Revision readRevisionDate(final Node node) {
+        return Revision.of(node.getAttributes().item(0).getNodeValue());
+    }
+
+    private static SourceDependency.BelongsTo readBelongsTo(final Node belongsToNode) {
+        final Unqualified name = readNodeQName(belongsToNode);
+        final Node prefixNode = getChildNode(belongsToNode, PREFIX);
+        if (prefixNode != null) {
+            return new SourceDependency.BelongsTo(name, readNodeQName(prefixNode));
+        }
+        throw new IllegalArgumentException("No prefix statement in " + refOf(belongsToNode));
+    }
+
+    @NonNullByDefault
+    final Unqualified extractPrefix(final Element parent) throws ExtractorException {
+        return getFirstElementArgument(parent, PREFIX, PREFIX_ARG, (element, arg) -> {
+            try {
+                return Unqualified.of(arg);
+            } catch (IllegalArgumentException e) {
+                throw newInvalidArgument(element, e);
+            }
+        });
+    }
+
+    @NonNullByDefault
+    final <T> T getFirstElementArgument(final Element parent, final String keyword, final String argument,
+            ArgumentFunction<T> function) throws ExtractorException {
+        final var childNodes = parent.getChildNodes();
+        for (int i = 0, length = childNodes.getLength(); i < length; ++i) {
+            if (childNodes.item(i) instanceof Element element && isYinElement(element, keyword)) {
+                final var arg = element.getAttributeNodeNS(null, argument);
+                if (arg != null && arg.getSpecified()) {
+                    return function.apply(element, arg.getValue());
+                }
+
+                throw new ExtractorMissingArgumentException(null, keyword);
+            }
+        }
+        throw newMissingSubstatement(parent, keyword);
+    }
+
+    @NonNullByDefault
+    private static ExtractorException newInvalidArgument(final Element stmt, final Exception cause) {
+        return new ExtractorException(
+            "Invalid argument to " + stmt.getLocalName() + ": " + cause.getMessage(), cause, refOf(stmt));
+    }
+
+    @NonNullByDefault
+    private static ExtractorException newMissingSubstatement(final Element parent, final String keyword) {
+        return new ExtractorException("Missing " + keyword + " substatement", refOf(parent));
+    }
+
+    private static boolean isYinElement(final Element element, final String keyword) {
+        return RFC6020_YIN_NAMESPACE_STRING.equals(element.getNamespaceURI()) && keyword.equals(element.getLocalName());
+    }
+}
