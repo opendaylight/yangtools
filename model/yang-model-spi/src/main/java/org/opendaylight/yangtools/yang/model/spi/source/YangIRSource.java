7444d8c46a3f75446b34184bab61d0cc4aa09676
Robert Varga <robert.varga@pantheon.tech>
2025-11-04 17:22:16 +0100

    Refactor YangIRSource
    Add two specializations, so that users can easily distinguish whether
    the source represents a module or a submodule.
    
    Instantiation is done via YangIRSource.of(), which performs the
    appropriate checks.
    
    JIRA: YANGTOOLS-1721
    Change-Id: Ia972e405ee431fee04e767a90080691ae3d107e6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-spi/src/main/java/org/opendaylight/yangtools/yang/model/spi/source/YangIRSource.java
+++ model/yang-model-spi/src/main/java/org/opendaylight/yangtools/yang/model/spi/source/YangIRSource.java
@@ -9,7 +9,6 @@ package org.opendaylight.yangtools.yang.model.spi.source;
 
 import static java.util.Objects.requireNonNull;
 
-import com.google.common.annotations.Beta;
 import com.google.common.base.MoreObjects;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
@@ -20,75 +19,81 @@ import org.opendaylight.yangtools.yang.model.api.meta.StatementSourceException;
 import org.opendaylight.yangtools.yang.model.api.source.SourceIdentifier;
 import org.opendaylight.yangtools.yang.model.api.source.YangSourceRepresentation;
 import org.opendaylight.yangtools.yang.model.spi.meta.StatementDeclarations;
-import org.opendaylight.yangtools.yang.model.spi.source.SourceInfo.ExtractorException;
 
 /**
  * A {@link YangSourceRepresentation} backed by an {@link IRStatement}.
  */
 @NonNullByDefault
-public final class YangIRSource implements YangSourceRepresentation, SourceInfo.Extractor {
+public abstract sealed class YangIRSource implements YangSourceRepresentation, SourceInfo.Extractor
+        permits YangIRModuleSource, YangIRSubmoduleSource {
     private final SourceIdentifier sourceId;
     private final IRStatement statement;
     private final @Nullable String symbolicName;
 
-    public YangIRSource(final SourceIdentifier sourceId, final IRStatement statement,
-            final @Nullable String symbolicName) {
-        final var rootKeyword = statement.keyword();
-        if (!(rootKeyword instanceof IRKeyword.Unqualified)) {
-            throw new StatementSourceException(refOf(sourceId, statement),
-                "Root statement has invalid keyword " + rootKeyword);
-        }
-        final var rootName = rootKeyword.identifier();
-        switch (rootName) {
-            case "module", "submodule" -> {
-                // Okay
-            }
-            default -> throw new StatementSourceException(refOf(sourceId, statement),
-                "Invalid root statement keyword " + rootName);
-        }
-        if (statement.argument() == null) {
-            throw new StatementSourceException(refOf(sourceId, statement), "Root statement does not have an argument");
-        }
+    YangIRSource(final SourceIdentifier sourceId, final IRStatement statement, final @Nullable String symbolicName) {
         this.sourceId = requireNonNull(sourceId);
         this.statement = statement;
         this.symbolicName = symbolicName;
     }
 
     /**
+     * Create a new {@link YangIRSource} encapsulating a root {@link IRStatement} with specified
+     * {@link SourceIdentifier} and an optional symbolic name.
+     *
+     * @param sourceId the {@link SourceIdentifier}
+     * @param statement the root {@link IRStatement}
+     * @param symbolicName optional symbolic name
+     * @return A {@link YangIRSource}
+     * @throws StatementSourceException if the {@code statement} is not a valid root
+     */
+    public static YangIRSource of(final SourceIdentifier sourceId, final IRStatement statement,
+            final @Nullable String symbolicName) {
+        final var keyword = statement.keyword();
+        if (!(keyword instanceof IRKeyword.Unqualified unqualified)) {
+            throw new StatementSourceException(refOf(sourceId, statement),
+                "Root statement has invalid keyword " + keyword);
+        }
+        if (statement.argument() == null) {
+            throw new StatementSourceException(refOf(sourceId, statement), "Root statement does not have an argument");
+        }
+
+        final var rootName = unqualified.identifier();
+        return switch (rootName) {
+            case "module" -> new YangIRModuleSource(sourceId, statement, symbolicName);
+            case "submodule" -> new YangIRSubmoduleSource(sourceId, statement, symbolicName);
+            default -> throw new StatementSourceException(refOf(sourceId, statement),
+                "Invalid root statement keyword " + rootName);
+        };
+    }
+
+    /**
      * {@return the root statement of this source}
      */
-    public IRStatement statement() {
+    public final IRStatement statement() {
         return statement;
     }
 
     @Override
-    public SourceIdentifier sourceId() {
+    public final SourceIdentifier sourceId() {
         return sourceId;
     }
 
     @Override
-    public @Nullable String symbolicName() {
+    public final @Nullable String symbolicName() {
         return symbolicName;
     }
 
     @Override
-    public Class<YangIRSource> getType() {
+    public final Class<YangIRSource> getType() {
         return YangIRSource.class;
     }
 
     @Override
-    public SourceInfo extractSourceInfo() throws ExtractorException {
-        return SourceInfoExtractors.forIR(statement, sourceId).extractSourceInfo();
-    }
-
-    @Override
-    public String toString() {
+    public final String toString() {
         return MoreObjects.toStringHelper(this).add("identifier", sourceId).toString();
     }
 
-    // FIXME: hide this method
-    @Beta
-    public static StatementDeclaration.InText refOf(final SourceIdentifier source, final IRStatement stmt) {
+    static final StatementDeclaration.InText refOf(final SourceIdentifier source, final IRStatement stmt) {
         return StatementDeclarations.inText(source.name().getLocalName(), stmt.startLine(), stmt.startColumn() + 1);
     }
 }
