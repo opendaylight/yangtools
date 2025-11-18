opendaylight/mdsal

WIP: Implement support for (Normalized)YangData
// FIXME: rework class hierarchy a bit
// FIXME: finish implementation
// FIXME: expands tests

JIRA: MDSAL-805
Change-Id: Ibd9109fc7a4a15d05687ae911d2b4890838ea88d
Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

binding/mdsal-binding-dom-codec/src/main/java/org/opendaylight/mdsal/binding/dom/codec/impl/BindingCodecContext.java
@@ -53,6 +53,7 @@ import org.opendaylight.mdsal.binding.dom.codec.spi.BindingSchemaMapping;
 import org.opendaylight.mdsal.binding.loader.BindingClassLoader;
 import org.opendaylight.mdsal.binding.runtime.api.BindingRuntimeContext;
 import org.opendaylight.mdsal.binding.runtime.api.ListRuntimeType;
+import org.opendaylight.mdsal.binding.runtime.api.YangDataRuntimeType;
 import org.opendaylight.mdsal.binding.spec.reflect.BindingReflections;
 import org.opendaylight.yangtools.concepts.Immutable;
 import org.opendaylight.yangtools.util.ClassLoaderUtils;
@@ -132,6 +133,24 @@ public final class BindingCodecContext extends AbstractBindingNormalizedNodeSeri
                 return new DataContainerSerializer(BindingCodecContext.this, streamers.get(key));
             }
         });
+    private final LoadingCache<Class<? extends YangData<?>>, YangDataCodecContext<?>> yangDataByClass =
+        CacheBuilder.newBuilder() .build(new CacheLoader<>() {
+            @Override
+            public YangDataCodecContext<?> load(final Class<? extends YangData<?>> key) {
+                final var schema = context.getSchemaDefinition(key);
+                if (schema instanceof YangDataRuntimeType yangData) {
+                    return new YangDataCodecContext(key, yangData, BindingCodecContext.this);
+                }
+                throw new IllegalArgumentException(key + " maps to non-YangData " + schema);
+            }
+        });
+    private final LoadingCache<YangDataName, BindingYangDataCodecTreeNode<?>> yangDataByName = CacheBuilder.newBuilder()
+        .build(new CacheLoader<>() {
+            @Override
+            public BindingYangDataCodecTreeNode<?> load(final YangDataName key) throws ExecutionException {
+                return yangDataByClass.get(context.getYangDataClass(key));
+            }
+        });
 
     private final @NonNull BindingClassLoader loader =
         BindingClassLoader.create(BindingCodecContext.class, BYTECODE_DIRECTORY);
@@ -173,13 +192,14 @@ public final class BindingCodecContext extends AbstractBindingNormalizedNodeSeri
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public <T extends YangData<T>> BindingYangDataCodecTreeNode<T> getYangDataCodec(final Class<T> yangDataClass) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return (BindingYangDataCodecTreeNode<T>) yangDataByClass.getUnchecked(requireNonNull(yangDataClass));
     }
 
     @Override
     public BindingYangDataCodecTreeNode<?> getYangDataCodec(final YangDataName yangDataName) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return yangDataByName.getUnchecked(requireNonNull(yangDataName));
     }
 
     @Override

opendaylight/yangtools

binding/binding-data-codec-dynamic/src/main/java/org/opendaylight/yangtools/binding/data/codec/impl/BindingCodecContext.java
@@ -91,6 +91,7 @@ import org.opendaylight.yangtools.binding.runtime.api.InputRuntimeType;
 import org.opendaylight.yangtools.binding.runtime.api.ListRuntimeType;
 import org.opendaylight.yangtools.binding.runtime.api.NotificationRuntimeType;
 import org.opendaylight.yangtools.binding.runtime.api.OutputRuntimeType;
+import org.opendaylight.yangtools.binding.runtime.api.YangDataRuntimeType;
 import org.opendaylight.yangtools.concepts.Immutable;
 import org.opendaylight.yangtools.util.ClassLoaderUtils;
 import org.opendaylight.yangtools.yang.common.QName;
@@ -363,6 +364,24 @@ public final class BindingCodecContext extends AbstractBindingNormalizedNodeSeri
                 return getRpc(container);
             }
         });
+    private final LoadingCache<Class<? extends YangData<?>>, YangDataCodecContext<?>> yangDataByClass =
+        CacheBuilder.newBuilder() .build(new CacheLoader<>() {
+            @Override
+            public YangDataCodecContext<?> load(final Class<? extends YangData<?>> key) {
+                final var schema = context.getSchemaDefinition(key);
+                if (schema instanceof YangDataRuntimeType yangData) {
+                    return new YangDataCodecContext(key, yangData, BindingCodecContext.this);
+                }
+                throw new IllegalArgumentException(key + " maps to non-YangData " + schema);
+            }
+        });
+    private final LoadingCache<YangDataName, BindingYangDataCodecTreeNode<?>> yangDataByName = CacheBuilder.newBuilder()
+        .build(new CacheLoader<>() {
+            @Override
+            public BindingYangDataCodecTreeNode<?> load(final YangDataName key) throws ExecutionException {
+                return yangDataByClass.get(context.getYangDataClass(key));
+            }
+        });
 
     private final @NonNull BindingClassLoader loader = BCL_BUILDER.build();
     private final @NonNull InstanceIdentifierCodec instanceIdentifierCodec;
@@ -422,13 +441,14 @@ public final class BindingCodecContext extends AbstractBindingNormalizedNodeSeri
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public <T extends YangData<T>> BindingYangDataCodecTreeNode<T> getYangDataCodec(final Class<T> yangDataClass) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return (BindingYangDataCodecTreeNode<T>) yangDataByClass.getUnchecked(requireNonNull(yangDataClass));
     }
 
     @Override
     public BindingYangDataCodecTreeNode<?> getYangDataCodec(final YangDataName yangDataName) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return yangDataByName.getUnchecked(requireNonNull(yangDataName));
     }
 
     @Override