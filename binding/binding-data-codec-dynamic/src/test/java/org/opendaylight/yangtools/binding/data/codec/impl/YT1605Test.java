cfce086159045cdf2ee36d7fa5ce321dfba18693
Robert Varga <robert.varga@pantheon.tech>
2025-11-19 20:22:26 +0100

    Update Mdsal805Test
    Rename to YT1605Test, in keeping with the still-to-be-delivered issue
    and use a real NormalizedYangData instance instead of mocking.
    
    JIRA: YANGTOOLS-1605
    Change-Id: Ia22c0bfd2bb3f54de87d880ec1c92199c76514bf
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/binding-data-codec-dynamic/src/test/java/org/opendaylight/yangtools/binding/data/codec/impl/YT1605Test.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.binding.data.codec.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.opendaylight.yang.gen.v1.urn.test.yang.data.demo.rev220222.YangDataWithContainer;
+import org.opendaylight.yang.gen.v1.urn.test.yang.data.demo.rev220222.YangDataWithContainerBuilder;
+import org.opendaylight.yang.gen.v1.urn.test.yang.data.demo.rev220222.yang.data.with.container.ContainerFromYangData;
+import org.opendaylight.yang.gen.v1.urn.test.yang.data.demo.rev220222.yang.data.with.container.ContainerFromYangDataBuilder;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedYangData;
+import org.opendaylight.yangtools.yang.data.spi.node.ImmutableNodes;
+
+class YT1605Test extends AbstractBindingCodecTest {
+    private static final NormalizedYangData NORMALIZED = ImmutableNodes.newYangDataBuilder(YangDataWithContainer.NAME)
+        .setChild(ImmutableNodes.newContainerBuilder()
+            .withNodeIdentifier(new NodeIdentifier(ContainerFromYangData.QNAME))
+            .withChild(ImmutableNodes.leafNode(QName.create(ContainerFromYangData.QNAME, "str"), "some string"))
+            .build())
+        .build();
+    private static final YangDataWithContainer BINDING = new YangDataWithContainerBuilder()
+        .setContainerFromYangData(new ContainerFromYangDataBuilder().setStr("some string").build())
+        .build();
+
+    @Disabled
+    @Test
+    void testFromBinding() {
+        assertEquals(NORMALIZED, codecContext.getYangDataCodec(BINDING.implementedInterface()).fromBinding(BINDING));
+    }
+
+    @Disabled
+    @Test
+    void testToBinding() {
+        final var fromCodec = assertInstanceOf(YangDataWithContainer.class,
+            codecContext.getYangDataCodec(YangDataWithContainer.NAME).toBinding(NORMALIZED));
+        assertEquals(BINDING, fromCodec);
+
+        // shortcut should work
+        assertSame(NORMALIZED, codecContext.getYangDataCodec(BINDING.implementedInterface()).fromBinding(fromCodec));
+    }
+}
