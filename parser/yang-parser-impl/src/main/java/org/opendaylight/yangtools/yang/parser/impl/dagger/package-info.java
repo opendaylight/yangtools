664105526be4ef3774ac4cbef319910e1928ba57
Robert Varga <robert.varga@pantheon.tech>
2025-11-24 23:06:53 +0100

    Daggerize yang-parser-impl
    Expose YangParserComponent and the consituent Dagger modules for
    integration.
    
    JIRA: YANGTOOLS-1727
    Change-Id: I88d1f5029747bc8a2b04392cbf6a1c84b1a4d9b6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/impl/dagger/package-info.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Dagger modules and components for parsing YANG and YIN files. There are two separate modules,
+ * {@link YangLibResolverModule} and {@link YangParserFactoryModule}, which provide static factory methods returning
+ * a singleton implementation of they underlying construct. These methods can be used to build bindings to other
+ * injection frameworks, like Guice or Spring Framework.
+ *
+ * <p>Additionally there is a single component, {@link YangParserComponent}, which combines the two modules
+ * with the reference XPath parser implementation and all production-ready parser extensions defined in YANGTOOLS.
+ *
+ * <p>This package is not exposed to OSGi runtime.
+ *
+ * @since 14.0.21
+ */
+package org.opendaylight.yangtools.yang.parser.impl.dagger;
