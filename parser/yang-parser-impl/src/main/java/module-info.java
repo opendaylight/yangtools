f19badac786cff3e0fe6a4a28660a85738cff8ec
Robert Varga <robert.varga@pantheon.tech>
2025-11-24 23:06:53 +0100

    Daggerize yang-parser-impl
    Expose YangParserComponent and the consituent Dagger modules for
    integration.
    
    JIRA: YANGTOOLS-1727
    Change-Id: I88d1f5029747bc8a2b04392cbf6a1c84b1a4d9b6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-impl/src/main/java/module-info.java
+++ parser/yang-parser-impl/src/main/java/module-info.java
@@ -11,11 +11,21 @@ import org.opendaylight.yangtools.yang.parser.api.YangParserFactory;
 import org.opendaylight.yangtools.yang.parser.spi.ParserExtension;
 import org.opendaylight.yangtools.yang.xpath.api.YangXPathParserFactory;
 
+/**
+ * Reference implementation of YANG parser.
+ *
+ * @provides SchemaRepository
+ * @provides YangLibResolver
+ * @provides YangParserFactory
+ */
 module org.opendaylight.yangtools.yang.parser.impl {
+    exports org.opendaylight.yangtools.yang.parser.repo;
+    exports org.opendaylight.yangtools.yang.parser.impl.dagger;
+
     // FIXME: do not export this package
     exports org.opendaylight.yangtools.yang.parser.impl;
+    // FIXME: remove this package
     exports org.opendaylight.yangtools.yang.parser.inject;
-    exports org.opendaylight.yangtools.yang.parser.repo;
 
     uses ParserExtension;
     uses YangXPathParserFactory;
@@ -27,6 +37,7 @@ module org.opendaylight.yangtools.yang.parser.impl {
     requires transitive com.google.common;
     requires transitive org.opendaylight.yangtools.yang.parser.api;
     requires transitive org.opendaylight.yangtools.yang.parser.rfc7950;
+    requires transitive org.opendaylight.yangtools.yang.parser.spi;
     requires org.opendaylight.yangtools.odlext.parser.support;
     requires org.opendaylight.yangtools.openconfig.parser.support;
     requires org.opendaylight.yangtools.rfc6241.parser.support;
@@ -41,11 +52,15 @@ module org.opendaylight.yangtools.yang.parser.impl {
     requires org.opendaylight.yangtools.yang.common;
     requires org.opendaylight.yangtools.yang.model.api;
     requires org.opendaylight.yangtools.yang.model.spi;
+    requires org.opendaylight.yangtools.yang.xpath.impl;
     requires org.slf4j;
 
     // Annotations
     requires static transitive org.eclipse.jdt.annotation;
     requires static com.github.spotbugs.annotations;
+    requires static dagger;
+    requires static jakarta.inject;
+    requires static java.compiler;
     requires static javax.inject;
     requires static org.checkerframework.checker.qual;
     requires static org.gaul.modernizer_maven_annotations;
