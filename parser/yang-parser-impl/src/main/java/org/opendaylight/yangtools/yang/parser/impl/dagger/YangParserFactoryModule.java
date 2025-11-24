57a40bb633cddf130f66c6dcdcad84a5bbeea9bc
Robert Varga <robert.varga@pantheon.tech>
2025-11-24 23:06:53 +0100

    Daggerize yang-parser-impl
    Expose YangParserComponent and the consituent Dagger modules for
    integration.
    
    JIRA: YANGTOOLS-1727
    Change-Id: I88d1f5029747bc8a2b04392cbf6a1c84b1a4d9b6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-impl/src/main/java/org/opendaylight/yangtools/yang/parser/impl/dagger/YangParserFactoryModule.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.impl.dagger;
+
+import dagger.Module;
+import dagger.Provides;
+import jakarta.inject.Singleton;
+import java.util.Set;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.parser.api.YangParserFactory;
+import org.opendaylight.yangtools.yang.parser.impl.DefaultYangParserFactory;
+import org.opendaylight.yangtools.yang.parser.spi.ParserExtension;
+import org.opendaylight.yangtools.yang.xpath.api.YangXPathParserFactory;
+
+/**
+ * A Dagger module providing {@link YangParserFactory}.
+ *
+ * @since 14.0.21
+ */
+@Module
+@NonNullByDefault
+@SuppressWarnings("exports")
+public interface YangParserFactoryModule {
+    @Provides
+    @Singleton
+    static YangParserFactory provideParserFactory(final YangXPathParserFactory xpathFactory,
+            final Set<ParserExtension> extensions) {
+        return new DefaultYangParserFactory(xpathFactory, extensions);
+    }
+}
