84dca321cea3fd0fc9b132f7d0df079793ef3e1b
Robert Varga <robert.varga@pantheon.tech>
2025-12-11 22:07:03 +0100

    Add yang.model.spi.SourceInfoExtractors
    This patch rehosts YangIRSourceInfoExtractor logic into
    yang.model.api.source, hiding it behind SourceInfoExtractors utility
    method.
    
    Since now have the extractor available, make YangIRSource implement
    SourceInfo.Extractor, routing through SourceInfoExtractors.forIR().
    
    JIRA: YANGTOOLS-1749
    Change-Id: Iad0754a68627adb1be4b6d05571e8d71240acf1b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ model/yang-model-spi/src/main/java/org/opendaylight/yangtools/yang/model/spi/source/YangIRSourceInfoExtractor.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.spi.source;
+
+import static java.util.Objects.requireNonNull;
+
+import java.text.ParseException;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.yang.common.Revision;
+import org.opendaylight.yangtools.yang.common.UnresolvedQName.Unqualified;
+import org.opendaylight.yangtools.yang.common.XMLNamespace;
+import org.opendaylight.yangtools.yang.common.YangVersion;
+import org.opendaylight.yangtools.yang.ir.IRKeyword;
+import org.opendaylight.yangtools.yang.ir.IRStatement;
+import org.opendaylight.yangtools.yang.ir.StringEscaping;
+import org.opendaylight.yangtools.yang.model.api.YangStmtMapping;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDeclaration;
+import org.opendaylight.yangtools.yang.model.api.source.SourceDependency.BelongsTo;
+import org.opendaylight.yangtools.yang.model.api.source.SourceDependency.Import;
+import org.opendaylight.yangtools.yang.model.api.source.SourceDependency.Include;
+import org.opendaylight.yangtools.yang.model.api.source.SourceIdentifier;
+import org.opendaylight.yangtools.yang.model.spi.meta.StatementDeclarations;
+import org.opendaylight.yangtools.yang.model.spi.source.SourceInfo.ExtractorException;
+import org.opendaylight.yangtools.yang.model.spi.source.SourceInfo.ExtractorInvalidArgumentException;
+import org.opendaylight.yangtools.yang.model.spi.source.SourceInfo.ExtractorMalformedArgumentException;
+import org.opendaylight.yangtools.yang.model.spi.source.SourceInfo.ExtractorMissingArgumentException;
+import org.opendaylight.yangtools.yang.model.spi.source.SourceInfo.ExtractorMissingStatementException;
+
+/**
+ * Utility class for extract {@link SourceInfo} from a {@link YangIRSource}.
+ */
+abstract sealed class YangIRSourceInfoExtractor implements SourceInfo.Extractor {
+    static final class ForModule extends YangIRSourceInfoExtractor {
+        private static final @NonNull String NAMESPACE = YangStmtMapping.NAMESPACE.getStatementName().getLocalName();
+
+        @NonNullByDefault
+        ForModule(final SourceIdentifier sourceId, final IRStatement root) {
+            super(sourceId, root);
+        }
+
+        @Override
+        public SourceInfo.Module extractSourceInfo() throws ExtractorException {
+            final var builder = SourceInfo.Module.builder();
+            fillBuilder(builder);
+            return builder
+                .setNamespace(extractNamespace())
+                .setPrefix(extractPrefix(root))
+                .build();
+        }
+
+        private @NonNull XMLNamespace extractNamespace() throws ExtractorException {
+            for (var stmt : root.statements()) {
+                if (isStatement(stmt, NAMESPACE)) {
+                    final var arg = stringArgument(stmt);
+                    try {
+                        return XMLNamespace.of(arg);
+                    } catch (IllegalArgumentException e) {
+                        throw new ExtractorInvalidArgumentException(refOf(stmt), NAMESPACE, e);
+                    }
+                }
+            }
+            throw new ExtractorMissingStatementException(refOf(root), NAMESPACE);
+        }
+    }
+
+    static final class ForSubmodule extends YangIRSourceInfoExtractor {
+        private static final @NonNull String BELONGS_TO = YangStmtMapping.BELONGS_TO.getStatementName().getLocalName();
+
+        @NonNullByDefault
+        ForSubmodule(final SourceIdentifier sourceId, final IRStatement root) {
+            super(sourceId, root);
+        }
+
+        @Override
+        public SourceInfo.Submodule extractSourceInfo() throws ExtractorException {
+            final var builder = SourceInfo.Submodule.builder();
+            fillBuilder(builder);
+            return builder
+                .setBelongsTo(extractBelongsTo())
+                .build();
+        }
+
+        private @NonNull BelongsTo extractBelongsTo() throws ExtractorException {
+            for (var stmt : root.statements()) {
+                if (isStatement(stmt, BELONGS_TO)) {
+                    return new BelongsTo(unqualifiedArgument(stmt), extractPrefix(stmt));
+                }
+            }
+            throw new ExtractorMissingStatementException(refOf(root), BELONGS_TO);
+        }
+    }
+
+    private static final @NonNull String IMPORT = YangStmtMapping.IMPORT.getStatementName().getLocalName();
+    private static final @NonNull String INCLUDE = YangStmtMapping.INCLUDE.getStatementName().getLocalName();
+    private static final @NonNull String PREFIX = YangStmtMapping.PREFIX.getStatementName().getLocalName();
+    private static final @NonNull String REVISION = YangStmtMapping.REVISION.getStatementName().getLocalName();
+    private static final @NonNull String REVISION_DATE =
+        YangStmtMapping.REVISION_DATE.getStatementName().getLocalName();
+    private static final @NonNull String YANG_VERSION = YangStmtMapping.YANG_VERSION.getStatementName().getLocalName();
+
+    private final @NonNull SourceIdentifier sourceId;
+    final @NonNull IRStatement root;
+
+    @NonNullByDefault
+    YangIRSourceInfoExtractor(final SourceIdentifier sourceId, final IRStatement root) {
+        this.sourceId = requireNonNull(sourceId);
+        this.root = requireNonNull(root);
+    }
+
+    final void fillBuilder(final SourceInfo.Builder<?, ?> builder) throws ExtractorException {
+        builder
+            .setYangVersion(extractYangVersion())
+            .setName(unqualifiedArgument(root));
+
+        for (var stmt : root.statements()) {
+            if (isStatement(stmt, IMPORT)) {
+                builder.addImport(new Import(unqualifiedArgument(stmt), extractPrefix(stmt),
+                    extractRevisionDate(stmt)));
+            } else if (isStatement(stmt, INCLUDE)) {
+                builder.addInclude(new Include(unqualifiedArgument(stmt), extractRevisionDate(stmt)));
+            } else if (isStatement(stmt, REVISION)) {
+                builder.addRevision(revisionArgument(stmt));
+            }
+        }
+    }
+
+    private @NonNull YangVersion extractYangVersion() throws ExtractorException {
+        for (var stmt : root.statements()) {
+            if (isStatement(stmt, YANG_VERSION)) {
+                final var arg = stringArgument(stmt);
+                try {
+                    return YangVersion.ofString(arg);
+                } catch (IllegalArgumentException e) {
+                    throw new ExtractorInvalidArgumentException(refOf(stmt), YANG_VERSION, e);
+                }
+            }
+        }
+        return YangVersion.VERSION_1;
+    }
+
+    final @NonNull Unqualified extractPrefix(final IRStatement stmt) throws ExtractorException {
+        for (var subStmt : stmt.statements()) {
+            if (isStatement(subStmt, PREFIX)) {
+                return unqualifiedArgument(subStmt);
+            }
+        }
+        throw new ExtractorMissingStatementException(refOf(stmt), PREFIX);
+    }
+
+    private @Nullable Revision extractRevisionDate(final IRStatement stmt) throws ExtractorException {
+        for (var subStmt : stmt.statements()) {
+            if (isStatement(subStmt, REVISION_DATE)) {
+                return revisionArgument(subStmt);
+            }
+        }
+        return null;
+    }
+
+    private static boolean isStatement(final IRStatement stmt, final String name) {
+        return stmt.keyword() instanceof IRKeyword.Unqualified keyword && name.equals(keyword.identifier());
+    }
+
+    final @NonNull Revision revisionArgument(final IRStatement stmt) throws ExtractorException {
+        final var arg = stringArgument(stmt);
+        try {
+            return Revision.of(arg);
+        } catch (IllegalArgumentException e) {
+            throw new ExtractorInvalidArgumentException(refOf(stmt), stmt.keyword().asStringDeclaration(), e);
+        }
+    }
+
+    final @NonNull String stringArgument(final IRStatement stmt) throws ExtractorException {
+        final var arg = stmt.argument();
+        if (arg == null) {
+            throw new ExtractorMissingArgumentException(refOf(stmt), stmt.keyword().asStringDeclaration());
+        }
+
+        try {
+            // TODO: we probably need to understand yang version first....
+            return arg.asString(StringEscaping.RFC6020);
+        } catch (ParseException e) {
+            throw new ExtractorMalformedArgumentException(refOf(stmt), stmt.keyword().asStringDeclaration(), e);
+        }
+    }
+
+    final @NonNull Unqualified unqualifiedArgument(final IRStatement stmt) throws ExtractorException {
+        final var arg = stringArgument(stmt);
+        try {
+            return Unqualified.of(arg);
+        } catch (IllegalArgumentException e) {
+            throw new ExtractorInvalidArgumentException(refOf(stmt), stmt.keyword().asStringDeclaration(), e);
+        }
+    }
+
+    final StatementDeclaration.@NonNull InText refOf(final IRStatement stmt) {
+        return StatementDeclarations.inText(sourceId.name().getLocalName(), stmt.startLine(), stmt.startColumn() + 1);
+    }
+}
