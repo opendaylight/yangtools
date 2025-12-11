1ca0e66d1df619feba78d128a3c5f641b01f02e5
Robert Varga <robert.varga@pantheon.tech>
2025-12-11 20:25:13 +0100

    Add (Unchecked)StatementException
    StatementSourceException is an unchecked exception, which is
    unfortunate, as it does not have a checked exception equivalent.
    
    Introduce StatementException as the baseline checked equivalent and
    UncheckedStatementException as the baseline replacement.
    
    Also add StatementSourceException/(Unchecked)SourceException
    interoperability constructors.
    
    JIRA: YANGTOOLS-1749
    Change-Id: I5891d8b106dda4e3f8c981e74c6c4b766581bcd9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ model/yang-model-api/src/test/java/org/opendaylight/yangtools/yang/model/api/meta/TestUncheckedStatementException.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.api.meta;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+@NonNullByDefault
+final class TestUncheckedStatementException extends UncheckedStatementException {
+    @java.io.Serial
+    private static final long serialVersionUID = 1L;
+
+    TestUncheckedStatementException(final TestStatementException cause) {
+        super(cause);
+    }
+
+    @Override
+    public @NonNull TestStatementException getCause() {
+        return TestStatementException.class.cast(super.getCause());
+    }
+}
