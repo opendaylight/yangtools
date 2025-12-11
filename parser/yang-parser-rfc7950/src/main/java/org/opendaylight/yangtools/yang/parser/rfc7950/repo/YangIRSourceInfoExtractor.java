ba7bac7ff944150408de1b7b2bd8d82a61cde2bd
Robert Varga <robert.varga@pantheon.tech>
2025-12-11 11:03:03 +0100

    Reduce use of streams in YangIRSourceInfoExtractor
    Use simple iteration instead of streams to reduce the number of times we
    traverse the sub-statements.
    
    JIRA: YANGTOOLS-1747
    Change-Id: I515d8c7cfe49a48b6d2a706556e579c69a1b67ea
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/repo/YangIRSourceInfoExtractor.java
+++ parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/repo/YangIRSourceInfoExtractor.java
@@ -37,6 +37,8 @@ import org.opendaylight.yangtools.yang.parser.api.YangSyntaxErrorException;
  */
 public abstract sealed class YangIRSourceInfoExtractor {
     private static final class ForModule extends YangIRSourceInfoExtractor {
+        private static final String NAMESPACE = YangStmtMapping.NAMESPACE.getStatementName().getLocalName();
+
         @NonNullByDefault
         ForModule(final SourceIdentifier sourceId, final IRStatement root) {
             super(sourceId, root);
@@ -59,6 +61,8 @@ public abstract sealed class YangIRSourceInfoExtractor {
     }
 
     private static final class ForSubmodule extends YangIRSourceInfoExtractor {
+        private static final String BELONGS_TO = YangStmtMapping.BELONGS_TO.getStatementName().getLocalName();
+
         @NonNullByDefault
         ForSubmodule(final SourceIdentifier sourceId, final IRStatement root) {
             super(sourceId, root);
@@ -79,11 +83,9 @@ public abstract sealed class YangIRSourceInfoExtractor {
         }
     }
 
-    private static final String BELONGS_TO = YangStmtMapping.BELONGS_TO.getStatementName().getLocalName();
     private static final String IMPORT = YangStmtMapping.IMPORT.getStatementName().getLocalName();
     private static final String INCLUDE = YangStmtMapping.INCLUDE.getStatementName().getLocalName();
     private static final String MODULE = YangStmtMapping.MODULE.getStatementName().getLocalName();
-    private static final String NAMESPACE = YangStmtMapping.NAMESPACE.getStatementName().getLocalName();
     private static final String PREFIX = YangStmtMapping.PREFIX.getStatementName().getLocalName();
     private static final String REVISION = YangStmtMapping.REVISION.getStatementName().getLocalName();
     private static final String REVISION_DATE = YangStmtMapping.REVISION_DATE.getStatementName().getLocalName();
@@ -163,38 +165,35 @@ public abstract sealed class YangIRSourceInfoExtractor {
             .map(YangVersion::forString)
             .ifPresent(builder::setYangVersion);
 
-        root.statements().stream()
-            .filter(stmt -> isStatement(stmt, REVISION))
-            .map(stmt -> Revision.of(safeStringArgument(stmt, "revision argument")))
-            .forEach(builder::addRevision);
-
-        root.statements().stream()
-            .filter(stmt -> isStatement(stmt, IMPORT))
-            .map(stmt -> new Import(Unqualified.of(safeStringArgument(stmt, "import argument")),
-                extractPrefix(stmt), extractRevisionDate(stmt)))
-            .forEach(builder::addImport);
-
-        root.statements().stream()
-            .filter(stmt -> isStatement(stmt, INCLUDE))
-            .map(stmt -> new Include(Unqualified.of(safeStringArgument(stmt, "include argument")),
-                extractRevisionDate(stmt)))
-            .forEach(builder::addInclude);
+        for (var stmt : root.statements()) {
+            if (isStatement(stmt, IMPORT)) {
+                builder.addImport(new Import(Unqualified.of(safeStringArgument(stmt, "import argument")),
+                    extractPrefix(stmt), extractRevisionDate(stmt)));
+            } else if (isStatement(stmt, INCLUDE)) {
+                builder.addInclude(new Include(Unqualified.of(safeStringArgument(stmt, "include argument")),
+                    extractRevisionDate(stmt)));
+            } else if (isStatement(stmt, REVISION)) {
+                builder.addRevision(Revision.of(safeStringArgument(stmt, "revision argument")));
+            }
+        }
     }
 
     final @NonNull Unqualified extractPrefix(final IRStatement stmt) {
-        return stmt.statements().stream()
-            .filter(subStmt -> isStatement(subStmt, PREFIX))
-            .findFirst()
-            .map(subStmt -> Unqualified.of(safeStringArgument(subStmt, "prefix argument")))
-            .orElseThrow(() -> new IllegalArgumentException("No prefix statement in " + refOf(stmt)));
+        for (var subStmt : stmt.statements()) {
+            if (isStatement(subStmt, PREFIX)) {
+                return Unqualified.of(safeStringArgument(subStmt, "prefix argument"));
+            }
+        }
+        throw new IllegalArgumentException("No prefix statement in " + refOf(stmt));
     }
 
     private @Nullable Revision extractRevisionDate(final IRStatement stmt) {
-        return stmt.statements().stream()
-            .filter(subStmt -> isStatement(subStmt, REVISION_DATE))
-            .findFirst()
-            .map(subStmt -> Revision.of(safeStringArgument(subStmt, "revision date argument")))
-            .orElse(null);
+        for (var subStmt : stmt.statements()) {
+            if (isStatement(subStmt, REVISION_DATE)) {
+                return Revision.of(safeStringArgument(subStmt, "revision date argument"));
+            }
+        }
+        return null;
     }
 
     private static boolean isStatement(final IRStatement stmt, final String name) {
