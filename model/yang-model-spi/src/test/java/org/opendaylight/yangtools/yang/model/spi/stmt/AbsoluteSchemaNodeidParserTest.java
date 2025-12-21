72e35be63e5215f01a509c979328c9a7edfb52ed
Robert Varga <robert.varga@pantheon.tech>
2025-12-21 10:56:57 +0100

    Add AbsoluteSchemaNodeidParser
    The absolute-schema-nodeid ABNF is used quite widely and is quite
    simple to implement. Add the corresponding parser.
    
    JIRA: YANGTOOLS-1754
    Change-Id: Id6961ca4311623384fd76f2939ae40c73ecf862c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ model/yang-model-spi/src/test/java/org/opendaylight/yangtools/yang/model/spi/stmt/AbsoluteSchemaNodeidParserTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.spi.stmt;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.UnresolvedQName.Unqualified;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier.Absolute;
+
+class AbsoluteSchemaNodeidParserTest extends AbstractNamespaceBindingTest<Absolute> {
+    private AbsoluteSchemaNodeidParser parser;
+
+    @Override
+    AbsoluteSchemaNodeidParser parser() {
+        return parser;
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        parser = new AbsoluteSchemaNodeidParser(new DescendantSchemaNodeidParser(new NodeIdentifierParser(
+            new IdentifierParser(namespaceBinding))));
+    }
+
+    @Test
+    void happyParseArgument() throws Exception {
+        doReturn(FOO).when(namespaceBinding).lookupModule(Unqualified.of("abc"));
+        doReturn(BAR).when(namespaceBinding).currentModule();
+        assertArgument(Absolute.of(QName.create(FOO, "foolocal"), QName.create(BAR, "barlocal")),
+            "/abc:foolocal/barlocal");
+    }
+
+    @Test
+    void emptyParseArgumentThrows() {
+        final var ex = assertSyntaxException("");
+        assertEquals("absolute-schema-nodeid cannot be empty", ex.getMessage());
+        assertEquals(0, ex.getPosition());
+    }
+
+    @Test
+    void notSlashParseArgumentThrows() {
+        final var ex = assertSyntaxException("a");
+        assertEquals("'a' is not '/' as required by absolute-schema-nodeid", ex.getMessage());
+        assertEquals(1, ex.getPosition());
+    }
+
+    @Test
+    void slashParseArgumentThrows() {
+        final var ex = assertSyntaxException("/");
+        assertEquals("node-identifier cannot be empty", ex.getMessage());
+        assertEquals(1, ex.getPosition());
+    }
+
+    @Test
+    void trailingSlashParseArgumentThrows() {
+        doReturn(BAR).when(namespaceBinding).currentModule();
+        final var ex = assertSyntaxException("/a/b/");
+        assertEquals("node-identifier cannot be empty", ex.getMessage());
+        assertEquals(5, ex.getPosition());
+    }
+
+    @Test
+    void toStringReportsNamespaceBinding() {
+        assertEquals("AbsoluteSchemaNodeidParser{namespaceBinding=namespaceBinding}", parser.toString());
+    }
+}
