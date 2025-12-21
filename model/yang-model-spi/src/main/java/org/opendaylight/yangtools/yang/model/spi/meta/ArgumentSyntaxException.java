b99fbd52e06b5607d305a6b5e47c2e7483c4b8fc
Robert Varga <robert.varga@pantheon.tech>
2025-12-21 10:54:31 +0100

    Add model.spi.meta.ArgumentParser
    Add the baseline interface to translating a raw string to a
    ModelStatement argument.
    
    Change-Id: I29eb86f4b3f15560da0dd159cd71e91e79fce90f
    JIRA: YANGTOOLS-1754
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ model/yang-model-spi/src/main/java/org/opendaylight/yangtools/yang/model/spi/meta/ArgumentSyntaxException.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.spi.meta;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * An {@link ArgumentParserException} indicating a problem with the syntactic structure of an argument string.
+ *
+ * @since 14.0.22
+ */
+@NonNullByDefault
+public non-sealed class ArgumentSyntaxException extends ArgumentParserException {
+    @java.io.Serial
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Construct an instance with specified {@code message} and {@code position} and an optional cause.
+     *
+     * @param message the message
+     * @param position the error offset, must be non-negative
+     * @param cause the cause, {@code null} if not present
+     */
+    public ArgumentSyntaxException(final String message, final int position, final @Nullable Exception cause) {
+        super(message, position, cause);
+    }
+
+    /**
+     * Construct an instance with specified {@code message} and {@code position}.
+     *
+     * @param message the message
+     * @param position the error offset, must be non-negative
+     */
+    public ArgumentSyntaxException(final String message, final int position) {
+        super(message, position);
+    }
+
+    /**
+     * Construct an instance with specified {@code message} and as caused by a {@link ArgumentParserException}.
+     *
+     * @param message the message
+     * @param cause the cause
+     */
+    public ArgumentSyntaxException(final String message, final ArgumentParserException cause) {
+        super(message, cause);
+    }
+}
