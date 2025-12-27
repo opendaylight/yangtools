adec54da91ca1d50ab7ea3c31c0d9d932082c071
Robert Varga <robert.varga@pantheon.tech>
2025-12-27 17:47:00 +0100

    Add model.spi.stmt.IdentifierParser
    This adds IdentifierParser, which is a replacement for
    StmtContextUtils.parseIdentifier().
    
    JIRA: YANGTOOLS-1754
    Change-Id: Ie04c4eff333045229c667ad7b59f825fcd0cf89c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ model/yang-model-spi/src/main/java/org/opendaylight/yangtools/yang/model/spi/stmt/NamespaceBinding.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.spi.stmt;
+
+import com.google.common.annotations.Beta;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.common.UnresolvedQName;
+
+/**
+ * Interface for binding {@code prefix}-based {code identifier} ABNF constructs -- for example as used in
+ * {@code identifier-ref}, {@code unknown-statement} and {@code node-identifier} ABNF productions.
+ *
+ * @since 14.0.22
+ */
+// FIXME: 15.0.0: This looks awfully like yang.common.YangNamespaceContext, except it is better and more encompassing.
+//                Try unify the two somehow, keeping in mind that YangXPathParserFactory is using both of these.
+@Beta
+@NonNullByDefault
+public interface NamespaceBinding {
+    /**
+     * {@return the current module, i.e. the {@link QNameModule} a plain {@code identifier}s are bound to}
+     */
+    QNameModule currentModule();
+
+    /**
+     * {@return the module for specified prefix, or {@code null} if no such module exists}
+     * @param prefix the prefix
+     */
+    @Nullable QNameModule lookupModule(UnresolvedQName.Unqualified prefix);
+}
