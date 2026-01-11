c79bb9c01c52aecc61a9867353ad8276931f6b65
Robert Varga <robert.varga@pantheon.tech>
2026-01-11 23:08:51 +0100

    Add ImmutableNamespaceBinding
    The reactor can easily provide an immutable implementation of
    NamespaceBinding.
    
    This introduces a simple record, model.spi.ImmutableNamespaceBinding and
    teaches RootStatementContext to instantiate it.
    
    The test suite is updated to reflect the immutable nature of returned
    context.
    
    JIRA: YANGTOOLS-1753
    Change-Id: I9af987649b7d5004be0622d984630f3772c9ebcd
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-reactor/src/test/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/RootStatementContextNamespaceBindingTest.java
+++ parser/yang-parser-reactor/src/test/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/RootStatementContextNamespaceBindingTest.java
@@ -11,10 +11,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import java.util.Map;
 import org.eclipse.jdt.annotation.NonNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -28,7 +30,7 @@ import org.opendaylight.yangtools.yang.parser.spi.ParserNamespaces;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 
 @ExtendWith(MockitoExtension.class)
-class StmtContextNamespaceBindingTest {
+class RootStatementContextNamespaceBindingTest {
     private static final @NonNull QNameModule FOO_NS = QNameModule.ofRevision("foo", "2025-12-27");
     private static final @NonNull Unqualified FOO = Unqualified.of("foo");
 
@@ -37,59 +39,56 @@ class StmtContextNamespaceBindingTest {
     @Mock
     private StmtContext<?, ?, ?> importedContext;
 
-    private StmtContextNamespaceBinding namespaceBinding;
-
     @BeforeEach
     void beforeEach() {
-        namespaceBinding = new StmtContextNamespaceBinding(context);
+        doReturn(FOO.bindTo(FOO_NS)).when(context).moduleName();
+        doCallRealMethod().when(context).newNamespaceBinding();
     }
 
     @Test
     void currentModuleInvokesDefiningModule() {
-        doReturn(FOO_NS).when(context).definingModule();
+        doReturn(Map.of()).when(context).namespace(ParserNamespaces.IMPORT_PREFIX_TO_MODULECTX);
+        doReturn(false).when(context).producesDeclared(SubmoduleStatement.class);
+
+        final var namespaceBinding = context.newNamespaceBinding();
         assertSame(FOO_NS, namespaceBinding.currentModule());
         assertSame(FOO_NS, namespaceBinding.currentModule());
-        verify(context).definingModule();
+        verify(context).namespace(any());
     }
 
     @Test
     void lookupModuleImported() {
-        doReturn(importedContext).when(context).namespaceItem(ParserNamespaces.IMPORT_PREFIX_TO_MODULECTX, "foo");
+        doReturn(Map.of("foo", importedContext)).when(context).namespace(ParserNamespaces.IMPORT_PREFIX_TO_MODULECTX);
         doReturn(FOO_NS).when(context).namespaceItem(ParserNamespaces.MODULECTX_TO_QNAME, importedContext);
+        doReturn(false).when(context).producesDeclared(SubmoduleStatement.class);
+
+        final var namespaceBinding = context.newNamespaceBinding();
         assertSame(FOO_NS, namespaceBinding.lookupModule(FOO));
         assertSame(FOO_NS, namespaceBinding.lookupModule(FOO));
-        verify(context, times(2)).namespaceItem(any(), any());
+        verify(context).namespace(any());
+        verify(context).namespaceItem(any(), any());
     }
 
     @Test
     void lookupModuleBelongsTo() {
-        doReturn(null).when(context).namespaceItem(ParserNamespaces.IMPORT_PREFIX_TO_MODULECTX, "foo");
-        doReturn(null).when(context).namespaceItem(ParserNamespaces.MODULECTX_TO_QNAME, null);
+        doReturn(Map.of()).when(context).namespace(ParserNamespaces.IMPORT_PREFIX_TO_MODULECTX);
         doReturn(true).when(context).producesDeclared(SubmoduleStatement.class);
-        doReturn(FOO).when(context).namespaceItem(ParserNamespaces.BELONGSTO_PREFIX_TO_MODULE_NAME, "foo");
+        doReturn(Map.of("foo", FOO)).when(context).namespace(ParserNamespaces.BELONGSTO_PREFIX_TO_MODULE_NAME);
         doReturn(FOO_NS).when(context).namespaceItem(ParserNamespaces.MODULE_NAME_TO_QNAME, FOO);
+
+        final var namespaceBinding = context.newNamespaceBinding();
         assertSame(FOO_NS, namespaceBinding.lookupModule(FOO));
         assertSame(FOO_NS, namespaceBinding.lookupModule(FOO));
-        verify(context, times(4)).namespaceItem(any(), any());
+        verify(context, times(2)).namespace(any());
+        verify(context).namespaceItem(any(), any());
     }
 
     @Test
     void lookupModuleNotFound() {
-        doReturn(null).when(context).namespaceItem(ParserNamespaces.IMPORT_PREFIX_TO_MODULECTX, "foo");
-        doReturn(null).when(context).namespaceItem(ParserNamespaces.MODULECTX_TO_QNAME, null);
+        doReturn(Map.of()).when(context).namespace(ParserNamespaces.IMPORT_PREFIX_TO_MODULECTX);
         doReturn(false).when(context).producesDeclared(SubmoduleStatement.class);
+
+        final var namespaceBinding = context.newNamespaceBinding();
         assertNull(namespaceBinding.lookupModule(FOO));
-    }
-
-    @Test
-    void toStringChanges() {
-        doReturn("xyzzy").when(context).toString();
-        assertEquals("StmtContextNamespaceBinding{context=xyzzy, cached=0}", namespaceBinding.toString());
-
-        currentModuleInvokesDefiningModule();
-        assertEquals("StmtContextNamespaceBinding{context=xyzzy, cached=1}", namespaceBinding.toString());
-
-        lookupModuleImported();
-        assertEquals("StmtContextNamespaceBinding{context=xyzzy, cached=2}", namespaceBinding.toString());
     }
 }
