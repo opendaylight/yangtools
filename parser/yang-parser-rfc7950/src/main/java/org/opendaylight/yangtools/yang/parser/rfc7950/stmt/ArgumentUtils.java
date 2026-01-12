4418908d3a836abaee2cddeed62036e655f7dbdc
Robert Varga <robert.varga@pantheon.tech>
2026-01-12 19:48:29 +0100

    Prune ArgumentUtils methods
    ArgumentUtils is not exposed to users, hence we can trim now-unused
    methods.
    
    Change-Id: I9e97403cf24d30448781188e01dfaf7ff65ec000
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/ArgumentUtils.java
+++ parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/ArgumentUtils.java
@@ -9,17 +9,7 @@ package org.opendaylight.yangtools.yang.parser.rfc7950.stmt;
 
 import com.google.common.base.Splitter;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Pattern;
-import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
-import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier.Absolute;
-import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier.Descendant;
 import org.opendaylight.yangtools.yang.model.api.stmt.UnresolvedNumber;
-import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
-import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContextUtils;
-import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
 
 /**
  * Utility class for dealing with arguments encountered by StatementSupport classes.
@@ -27,9 +17,6 @@ import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
 public final class ArgumentUtils {
     public static final Splitter PIPE_SPLITTER = Splitter.on('|').trimResults();
     public static final Splitter TWO_DOTS_SPLITTER = Splitter.on("..").trimResults();
-
-    private static final Pattern PATH_ABS = Pattern.compile("/[^/].*");
-    private static final Splitter SLASH_SPLITTER = Splitter.on('/').omitEmptyStrings().trimResults();
 
     // these objects are to compare whether range has MAX or MIN value
     // none of these values should appear as Yang number according to spec so they are safe to use
@@ -44,49 +31,6 @@ public final class ArgumentUtils {
         final var num1 = yangConstraintToBigDecimal(n1);
         final var num2 = yangConstraintToBigDecimal(n2);
         return new BigDecimal(num1.toString()).compareTo(new BigDecimal(num2.toString()));
-    }
-
-    @Deprecated(since = "15.0.0", forRemoval = true)
-    public static boolean isAbsoluteXPath(final String path) {
-        return PATH_ABS.matcher(path).matches();
-    }
-
-    @Deprecated(since = "15.0.0", forRemoval = true)
-    public static Absolute parseAbsoluteSchemaNodeIdentifier(final StmtContext<?, ?, ?> ctx, final String str) {
-        // FIXME: this does accept check for a leading slash
-        return Absolute.of(parseNodeIdentifiers(ctx, str));
-    }
-
-    @Deprecated(since = "15.0.0", forRemoval = true)
-    public static Descendant parseDescendantSchemaNodeIdentifier(final StmtContext<?, ?, ?> ctx, final String str) {
-        // FIXME: this does accept a leading slash
-        return Descendant.of(parseNodeIdentifiers(ctx, str));
-    }
-
-    @Deprecated(since = "15.0.0", forRemoval = true)
-    public static SchemaNodeIdentifier nodeIdentifierFromPath(final StmtContext<?, ?, ?> ctx, final String path) {
-        final var qnames = parseNodeIdentifiers(ctx, path);
-        return PATH_ABS.matcher(path).matches() ? Absolute.of(qnames) : Descendant.of(qnames);
-    }
-
-    @SuppressWarnings("checkstyle:illegalCatch")
-    private static List<QName> parseNodeIdentifiers(final StmtContext<?, ?, ?> ctx, final String path) {
-        // FIXME: is the path trimming really necessary??
-        final var qnames = new ArrayList<QName>();
-        for (var nodeName : SLASH_SPLITTER.split(trimSingleLastSlashFromXPath(path))) {
-            try {
-                qnames.add(StmtContextUtils.parseNodeIdentifier(ctx, nodeName));
-            } catch (RuntimeException e) {
-                throw new SourceException(ctx, e, "Failed to parse node '%s' in path '%s'", nodeName, path);
-            }
-        }
-
-        SourceException.throwIf(qnames.isEmpty(), ctx, "Schema node identifier must not be empty");
-        return qnames;
-    }
-
-    private static String trimSingleLastSlashFromXPath(final String path) {
-        return path.endsWith("/") ? path.substring(0, path.length() - 1) : path;
     }
 
     private static BigDecimal yangConstraintToBigDecimal(final Number number) {
