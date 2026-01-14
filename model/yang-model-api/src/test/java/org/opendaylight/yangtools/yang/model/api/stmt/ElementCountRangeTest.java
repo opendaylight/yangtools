9882083bf3cb708083c3e9779bda98f5046fbf9b
Robert Varga <robert.varga@pantheon.tech>
2026-01-14 22:30:48 +0100

    Add ElementCountRange
    Improve usefulness of ElementCountMatcher by allowing a combination
    of MinElementsArgument and MaxElementsArgument to form a combined
    range-based matcher.
    
    We also add utility methods to squash well-known no-ops down to a null
    ElementCountMatcher.
    
    JIRA: YANGTOOLS-1667
    Change-Id: I379465f4c060841c79f3e4d23547a419cd383b6e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ model/yang-model-api/src/test/java/org/opendaylight/yangtools/yang/model/api/stmt/ElementCountRangeTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.api.stmt;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.math.BigInteger;
+import org.junit.jupiter.api.Test;
+import org.opendaylight.yangtools.yang.model.api.stmt.ElementCountMatcher.TooFewElements;
+import org.opendaylight.yangtools.yang.model.api.stmt.ElementCountMatcher.TooManyElements;
+import org.opendaylight.yangtools.yang.model.api.stmt.ElementCountMatcher.Violation;
+
+class ElementCountRangeTest {
+    private final ElementCountRange range = new ElementCountRange(MinElementsArgument.of(2), MaxElementsArgument.of(5));
+
+    @Test
+    void testMatching() {
+        assertNull(range.matches(2));
+        assertNull(range.matches(2L));
+        assertNull(range.matches(BigInteger.TWO));
+
+        assertNull(range.matches(5));
+        assertNull(range.matches(5L));
+        assertNull(range.matches(BigInteger.valueOf(5L)));
+
+        assertEquals("2", assertTooFew(range.matches(0)));
+        assertEquals("2", assertTooFew(range.matches(0L)));
+        assertEquals("2", assertTooFew(range.matches(BigInteger.ONE)));
+
+        assertEquals("5", assertTooMany(range.matches(6)));
+        assertEquals("5", assertTooMany(range.matches(7L)));
+        assertEquals("5", assertTooMany(range.matches(BigInteger.TEN)));
+    }
+
+    @Test
+    void toStringWorks() {
+        assertEquals("[2..5]", range.toString());
+    }
+
+    private static String assertTooFew(final Violation violation) {
+        return assertInstanceOf(TooFewElements.class, violation).minElements().toString();
+    }
+
+    private static String assertTooMany(final Violation violation) {
+        return assertInstanceOf(TooManyElements.class, violation).maxElements().toString();
+    }
+}
