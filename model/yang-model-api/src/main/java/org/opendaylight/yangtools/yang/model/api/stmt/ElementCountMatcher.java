e30f5c07ad9f2e6318e5294c9d743dcf806b07cf
Robert Varga <robert.varga@pantheon.tech>
2026-01-14 01:16:24 +0100

    WIP: Eliminate ElementCountConstraint
    Generalize ElementCountMatcher to act as a general utility to match for
    {min,max}-elements-defined constraints.
    
    JIRA: YANGTOOLS-1667
    Change-Id: I68c9a6a08363b2ab4ef74f0d1297ba1aedad5410
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/ElementCountMatcher.java
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/ElementCountMatcher.java
@@ -7,9 +7,13 @@
  */
 package org.opendaylight.yangtools.yang.model.api.stmt;
 
+import static java.util.Objects.requireNonNull;
+
 import java.math.BigInteger;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.yang.model.api.stmt.MaxElementsArgument.Bounded;
+import org.opendaylight.yangtools.yang.model.api.stmt.MaxElementsArgument.Unbounded;
 
 /**
  * A matcher on element count in a container like {@code list}, as expressed via {@code min-elements} and
@@ -18,22 +22,98 @@ import org.opendaylight.yangtools.concepts.Immutable;
  * @since 15.0.0
  */
 @NonNullByDefault
-public sealed interface ElementCountMatcher extends Immutable permits MinElementsArgument, MaxElementsArgument {
+public sealed interface ElementCountMatcher extends Immutable
+        permits MinElementsArgument, MaxElementsArgument, ElementCountRange {
     /**
-     * {@return {@code true} if {@code elementCount} matches this matcher}
-     * @param elementCount the element count
+     * A result of an {@link ElementCountMatcher} match operation.
      */
-    boolean matches(int elementCount);
+    sealed interface Match permits Failure, Success {
+        // marker interface
+    }
+
+    /**
+     * A successful {@link Match}.
+     */
+    final class Success implements Match {
+        /**
+         * The singleton value.
+         */
+        public static final Success VALUE = new Success();
+
+        private Success() {
+            // Hidden on purpose
+        }
+    }
+
+    /**
+     * A failed {@link Match}.
+     */
+    sealed interface Failure extends Match permits TooFewElements, TooManyElements {
+        // marker interface
+    }
+
+    /**
+     * Proposed element count violates constraint imposed by {@code min-elements}, as illustrated in
+     * <a href="https://www.rfc-editor.org/rfc/rfc7950#section-15.3">RFC7950 section 15.3</a>.
+     *
+     * @param minElements minimum allowed elements
+     */
+    record TooFewElements(String minElements) implements Failure {
+        public TooFewElements {
+            requireNonNull(minElements);
+        }
+    }
+
+    /**
+     * Proposed element count violates constraint imposed by {@code max-elements}, as illustrated in
+     * <a href="https://www.rfc-editor.org/rfc/rfc7950#section-15.2">RFC7950 section 15.2</a>.
+     *
+     * @param maxElements maximum allowed elements
+     */
+    record TooManyElements(String maxElements) implements Failure {
+        public TooManyElements {
+            requireNonNull(maxElements);
+        }
+    }
 
     /**
      * {@return {@code true} if {@code elementCount} matches this matcher}
      * @param elementCount the element count
      */
-    boolean matches(long elementCount);
+    Match match(int elementCount);
 
     /**
      * {@return {@code true} if {@code elementCount} matches this matcher}
      * @param elementCount the element count
      */
-    boolean matches(BigInteger elementCount);
+    Match match(long elementCount);
+
+    /**
+     * {@return {@code true} if {@code elementCount} matches this matcher}
+     * @param elementCount the element count
+     */
+    Match match(BigInteger elementCount);
+
+    /**
+     * {@return an ElementCountMatcher matching specified {@ode min-elements} and {@code max-elements}}
+     * @param minElements the {@link MinElementsArgument}
+     * @param maxElements the {@link MaxElementsArgument}
+     */
+    static ElementCountMatcher of(final MinElementsArgument minElements, final MaxElementsArgument maxElements) {
+        return switch (maxElements) {
+            case Bounded bounded -> of(minElements, bounded);
+            case Unbounded unbouded -> requireNonNull(minElements);
+        };
+    }
+
+    /**
+     * {@return an ElementCountMatcher matching specified {@ode min-elements} and {@code max-elements}}
+     * @param minElements the {@link MinElementsArgument}
+     * @param maxElements the {@link Bounded}
+     */
+    static ElementCountMatcher of(final MinElementsArgument minElements, final Bounded maxElements) {
+        // 'min.lowerInt == -1' == 'min-elements == 0'
+        return minElements.lowerInt() == -1 ? requireNonNull(maxElements)
+            : new ElementCountRange(minElements, maxElements);
+    }
 }
