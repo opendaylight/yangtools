21465ea61e3c026f7b18f4b06f6b0fc6f211af9e
Robert Varga <robert.varga@pantheon.tech>
2026-01-14 22:30:48 +0100

    Add ElementCountRange
    Improve usefulness of ElementCountMatcher by allowing a combination
    of MinElementsArgument and MaxElementsArgument to form a combined
    range-based matcher.
    
    We also add utility methods to squash well-known no-ops down to a null
    ElementCountMatcher.
    
    JIRA: YANGTOOLS-1667
    Change-Id: I379465f4c060841c79f3e4d23547a419cd383b6e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/ElementCountRange.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.api.stmt;
+
+import static java.util.Objects.requireNonNull;
+
+import java.math.BigInteger;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.yang.model.api.stmt.MaxElementsArgument.Bounded;
+
+@NonNullByDefault
+record ElementCountRange(MinElementsArgument minElements, Bounded maxElements) implements ElementCountMatcher {
+    ElementCountRange {
+        requireNonNull(minElements);
+        requireNonNull(maxElements);
+    }
+
+    @Override
+    public @Nullable Violation matches(final int elementCount) {
+        final var min = minElements.matches(elementCount);
+        return min != null ? min : maxElements.matches(elementCount);
+    }
+
+    @Override
+    public @Nullable Violation matches(final long elementCount) {
+        final var min = minElements.matches(elementCount);
+        return min != null ? min : maxElements.matches(elementCount);
+    }
+
+    @Override
+    public @Nullable Violation matches(final BigInteger elementCount) {
+        final var min = minElements.matches(elementCount);
+        return min != null ? min : maxElements.matches(elementCount);
+    }
+
+    @Override
+    public String toString() {
+        return "[" + minElements + ".." + maxElements + "]";
+    }
+}
