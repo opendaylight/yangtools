d448df4c02aa8173d1a7f98638ccb26308ef25ab
Robert Varga <robert.varga@pantheon.tech>
2026-01-20 13:22:13 +0100

    Remove ModelDefinedStatementDefintion
    ModelDefinedStatementDefinition is a superfluous implementation: replace
    it with a call to StatementDefinition.of().
    
    JIRA: YANGTOOLS-1739
    Change-Id: I65afe6d5b1c67a438ecc32f39c1e873204f0b902
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/extension/UnrecognizedEffectiveStatement.java
+++ parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/extension/UnrecognizedEffectiveStatement.java
@@ -7,31 +7,16 @@
  */
 package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.extension;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 import org.opendaylight.yangtools.yang.model.api.stmt.UnrecognizedStatement;
-import org.opendaylight.yangtools.yang.model.spi.meta.AbstractDeclaredEffectiveStatement;
 
 /**
  * An 'implementation' of an effective UnrecognizedStatement. This class is actually never instantiated and exists
  * only as an implementation-private marker for {@link StatementDefinition#getEffectiveRepresentationClass()}.
  */
-final class UnrecognizedEffectiveStatement extends AbstractDeclaredEffectiveStatement<Object, UnrecognizedStatement> {
-    private UnrecognizedEffectiveStatement() {
-        // This should never be called
-    }
-
-    @Override
-    public StatementDefinition statementDefinition() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public String argument() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public UnrecognizedStatement declared() {
-        throw new UnsupportedOperationException();
-    }
+@NonNullByDefault
+interface UnrecognizedEffectiveStatement extends EffectiveStatement<Object, UnrecognizedStatement> {
+    // Nothing to see
 }
