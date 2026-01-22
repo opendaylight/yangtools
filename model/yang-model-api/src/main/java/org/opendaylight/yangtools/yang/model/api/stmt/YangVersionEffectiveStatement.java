b64d21f73bf42befa70f16debe88b10a04f9c99e
Robert Varga <robert.varga@pantheon.tech>
2026-01-22 06:45:36 +0100

    Remove YangStmtMapping.{YANG_VERSION,YIN_ELEMENT}
    Perform the trivial split of next two constants.
    
    JIRA: YANGTOOLS-1739
    Change-Id: I8082f72f2f344d55826f74c571a374724b7a72c3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/YangVersionEffectiveStatement.java
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/YangVersionEffectiveStatement.java
@@ -7,17 +7,17 @@
  */
 package org.opendaylight.yangtools.yang.model.api.stmt;
 
+import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.yang.common.YangVersion;
-import org.opendaylight.yangtools.yang.model.api.YangStmtMapping;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 
 /**
  * Effective representation of a {@code yang-version} statement.
  */
-public interface YangVersionEffectiveStatement extends EffectiveStatement<YangVersion, YangVersionStatement> {
+public interface YangVersionEffectiveStatement extends EffectiveStatement<YangVersion, @NonNull YangVersionStatement> {
     @Override
     default StatementDefinition statementDefinition() {
-        return YangStmtMapping.YANG_VERSION;
+        return YangVersionStatement.DEFINITION;
     }
 }
