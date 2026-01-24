f51634c84916973492711af1976277ceafabc053
Robert Varga <robert.varga@pantheon.tech>
2026-01-25 00:31:20 +0100

    Update TypeStatement design
    Seal the base interface and introduce sub-interfaces to match the
    possible type-body-stmts specializations.
    
    // FIXME: add documentation
    
    JIRA: YANGTOOLS-1768
    Change-Id: I51f4bbc8a2440bc96c1d7e709e77138b2f5bcb6a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-ri/src/main/java/org/opendaylight/yangtools/yang/model/ri/stmt/impl/decl/RegularLeafrefTypeStatement.java
+++ model/yang-model-ri/src/main/java/org/opendaylight/yangtools/yang/model/ri/stmt/impl/decl/RegularLeafrefTypeStatement.java
@@ -10,11 +10,11 @@ package org.opendaylight.yangtools.yang.model.ri.stmt.impl.decl;
 import com.google.common.collect.ImmutableList;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
-import org.opendaylight.yangtools.yang.model.api.stmt.TypeStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.TypeStatement.OfLeafref;
 import org.opendaylight.yangtools.yang.model.spi.meta.AbstractDeclaredStatement.WithArgument.WithSubstatements;
 
-public final class RegularTypeStatement extends WithSubstatements<QName> implements TypeStatement {
-    public RegularTypeStatement(final String rawArgument, final QName argument,
+public final class RegularLeafrefTypeStatement extends WithSubstatements<QName> implements OfLeafref {
+    public RegularLeafrefTypeStatement(final String rawArgument, final QName argument,
             final ImmutableList<? extends DeclaredStatement<?>> substatements) {
         super(rawArgument, argument, substatements);
     }
---
+++ model/yang-model-ri/src/main/java/org/opendaylight/yangtools/yang/model/ri/stmt/impl/decl/RegularUnionTypeStatement.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.ri.stmt.impl.decl;
+
+import com.google.common.collect.ImmutableList;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.TypeStatement.OfUnion;
+import org.opendaylight.yangtools.yang.model.spi.meta.AbstractDeclaredStatement.WithArgument.WithSubstatements;
+
+public final class RegularUnionTypeStatement extends WithSubstatements<QName> implements OfUnion {
+    public RegularUnionTypeStatement(final String rawArgument, final QName argument,
+            final ImmutableList<? extends DeclaredStatement<?>> substatements) {
+        super(rawArgument, argument, substatements);
+    }
+}
