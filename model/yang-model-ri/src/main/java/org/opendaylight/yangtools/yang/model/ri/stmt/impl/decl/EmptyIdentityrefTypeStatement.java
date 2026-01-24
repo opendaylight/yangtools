bd1bd91e1fb986c4a721fef85cbf02efb852f4d1
Robert Varga <robert.varga@pantheon.tech>
2026-01-25 00:31:20 +0100

    Update TypeStatement design
    Seal the base interface and introduce sub-interfaces to match the
    possible type-body-stmts specializations.
    
    // FIXME: add documentation
    
    JIRA: YANGTOOLS-1768
    Change-Id: I51f4bbc8a2440bc96c1d7e709e77138b2f5bcb6a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ model/yang-model-ri/src/main/java/org/opendaylight/yangtools/yang/model/ri/stmt/impl/decl/EmptyIdentityrefTypeStatement.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.ri.stmt.impl.decl;
+
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.stmt.TypeStatement;
+import org.opendaylight.yangtools.yang.model.spi.meta.AbstractDeclaredStatement.WithArgument;
+
+public final class EmptyIdentityrefTypeStatement extends WithArgument<QName> implements TypeStatement.OfIdentityref {
+    public EmptyIdentityrefTypeStatement(final String rawArgument, final QName argument) {
+        super(rawArgument, argument);
+    }
+}
