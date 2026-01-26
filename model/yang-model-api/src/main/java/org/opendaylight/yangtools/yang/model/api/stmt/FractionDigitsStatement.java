213d347d49dfaf29892571708e2a1405ea760966
Robert Varga <robert.varga@pantheon.tech>
2026-01-26 21:35:25 +0100

    Clean up TypeStatement subclasses
    Move all foreign accessors to their respective declared statements.
    
    Change-Id: I782572d3a1858b83900dd658dbed16964ee3a9e7
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/FractionDigitsStatement.java
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/FractionDigitsStatement.java
@@ -7,7 +7,12 @@
  */
 package org.opendaylight.yangtools.yang.model.api.stmt;
 
+import com.google.common.annotations.Beta;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.yang.common.Empty;
 import org.opendaylight.yangtools.yang.common.YangConstants;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
@@ -16,6 +21,45 @@ import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
  * Declared representation of a {@code fraction-digits} statement.
  */
 public interface FractionDigitsStatement extends DeclaredStatement<Integer> {
+    /**
+     * A {@link DeclaredStatement} that is a parent of a single {@link FractionDigitsStatement}.
+     * @param <A> Argument type ({@link Empty} if statement does not have argument.)
+     */
+    @Beta
+    interface OptionalIn<A> extends DeclaredStatement<A> {
+        /**
+         * {@return the {@code FractionDigitsStatement} or {@code null} if not present}
+         */
+        default @Nullable FractionDigitsStatement fractionDigitsStatement() {
+            for (var stmt : declaredSubstatements()) {
+                if (stmt instanceof FractionDigitsStatement fractionDigits) {
+                    return fractionDigits;
+                }
+            }
+            return null;
+        }
+
+        /**
+         * {@return an optional {@code FractionDigitsStatement}}
+         */
+        default @NonNull Optional<FractionDigitsStatement> findFractionDigitsStatement() {
+            return Optional.ofNullable(fractionDigitsStatement());
+        }
+
+        /**
+         * {@return the {@code FractionDigitsStatement}}
+         * @throws NoSuchElementException if not present
+         */
+        default @NonNull FractionDigitsStatement getConfigStatement() {
+            final var fractionDigits = fractionDigitsStatement();
+            if (fractionDigits == null) {
+                throw new NoSuchElementException("No fraction-digits statement present in " + this);
+            }
+            return fractionDigits;
+        }
+    }
+
+
     /**
      * The definition of {@code fraction-digits} statement.
      *
--- model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/PathStatement.java
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/PathStatement.java
@@ -7,7 +7,12 @@
  */
 package org.opendaylight.yangtools.yang.model.api.stmt;
 
+import com.google.common.annotations.Beta;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.yang.common.Empty;
 import org.opendaylight.yangtools.yang.common.YangConstants;
 import org.opendaylight.yangtools.yang.model.api.PathExpression;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
@@ -17,6 +22,44 @@ import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
  * Declared representation of a {@code path} statement.
  */
 public interface PathStatement extends DeclaredStatement<PathExpression> {
+    /**
+     * A {@link DeclaredStatement} that is a parent of a single {@link PathStatement}.
+     * @param <A> Argument type ({@link Empty} if statement does not have argument.)
+     */
+    @Beta
+    interface OptionalIn<A> extends DeclaredStatement<A> {
+        /**
+         * {@return the {@code PathStatement} or {@code null} if not present}
+         */
+        default @Nullable PathStatement pathStatement() {
+            for (var stmt : declaredSubstatements()) {
+                if (stmt instanceof PathStatement path) {
+                    return path;
+                }
+            }
+            return null;
+        }
+
+        /**
+         * {@return an optional {@code PathStatement}}
+         */
+        default @NonNull Optional<PathStatement> findPathStatement() {
+            return Optional.ofNullable(pathStatement());
+        }
+
+        /**
+         * {@return the {@code PathStatement}}
+         * @throws NoSuchElementException if not present
+         */
+        default @NonNull PathStatement getPathStatement() {
+            final var path = pathStatement();
+            if (path == null) {
+                throw new NoSuchElementException("No path statement present in " + this);
+            }
+            return path;
+        }
+    }
+
     /**
      * The definition of {@code path} statement.
      *
