d46d568c5a1e64c78aacb040dc7117885bf0e524
Robert Varga <robert.varga@pantheon.tech>
2026-01-26 14:00:38 +0100

    Add {Description,Reference}Statement.OptionalIn
    Change-Id: I8b46d896be0471ca9b782914a3b72827eb40ecd2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/DescriptionStatement.java
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/DescriptionStatement.java
@@ -7,15 +7,59 @@
  */
 package org.opendaylight.yangtools.yang.model.api.stmt;
 
+import com.google.common.annotations.Beta;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.yang.common.Empty;
 import org.opendaylight.yangtools.yang.common.YangConstants;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredHumanTextStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 
 /**
  * Declared representation of a {@code description} statement.
  */
 public interface DescriptionStatement extends DeclaredHumanTextStatement {
+    /**
+     * A {@link DeclaredStatement} that is a parent of a single {@link DescriptionStatement}.
+     * @param <A> Argument type ({@link Empty} if statement does not have argument.)
+     */
+    @Beta
+    interface OptionalIn<A> extends DeclaredStatement<A> {
+        /**
+         * {@return the {@code DescriptionStatement} or {@code null} if not present}
+         */
+        default @Nullable DescriptionStatement descriptionStatement() {
+            for (var stmt : declaredSubstatements()) {
+                if (stmt instanceof DescriptionStatement description) {
+                    return description;
+                }
+            }
+            return null;
+        }
+
+        /**
+         * {@return an optional {@code DescriptionStatement}}
+         */
+        default @NonNull Optional<DescriptionStatement> findDescriptionStatement() {
+            return Optional.ofNullable(descriptionStatement());
+        }
+
+        /**
+         * {@return the {@code DescriptionStatement}}
+         * @throws NoSuchElementException if not present
+         */
+        default @NonNull DescriptionStatement getDescriptionStatement() {
+            final var description = descriptionStatement();
+            if (description == null) {
+                throw new NoSuchElementException("No status statement present in " + this);
+            }
+            return description;
+        }
+    }
+
     /**
      * The definition of {@code description} statement.
      *
--- model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/ReferenceStatement.java
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/ReferenceStatement.java
@@ -7,15 +7,59 @@
  */
 package org.opendaylight.yangtools.yang.model.api.stmt;
 
+import com.google.common.annotations.Beta;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.yang.common.Empty;
 import org.opendaylight.yangtools.yang.common.YangConstants;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredHumanTextStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 
 /**
  * Declared representation of a {@code reference} statement.
  */
 public interface ReferenceStatement extends DeclaredHumanTextStatement {
+    /**
+     * A {@link DeclaredStatement} that is a parent of a single {@link ReferenceStatement}.
+     * @param <A> Argument type ({@link Empty} if statement does not have argument.)
+     */
+    @Beta
+    interface OptionalIn<A> extends DeclaredStatement<A> {
+        /**
+         * {@return the {@code ReferenceStatement} or {@code null} if not present}
+         */
+        default @Nullable ReferenceStatement referenceStatement() {
+            for (var stmt : declaredSubstatements()) {
+                if (stmt instanceof ReferenceStatement reference) {
+                    return reference;
+                }
+            }
+            return null;
+        }
+
+        /**
+         * {@return an optional {@code ReferenceStatement}}
+         */
+        default @NonNull Optional<ReferenceStatement> findReferenceStatement() {
+            return Optional.ofNullable(referenceStatement());
+        }
+
+        /**
+         * {@return the {@code ReferenceStatement}}
+         * @throws NoSuchElementException if not present
+         */
+        default @NonNull ReferenceStatement getReferenceStatement() {
+            final var reference = referenceStatement();
+            if (reference == null) {
+                throw new NoSuchElementException("No status statement present in " + this);
+            }
+            return reference;
+        }
+    }
+
     /**
      * The definition of {@code reference} statement.
      *
