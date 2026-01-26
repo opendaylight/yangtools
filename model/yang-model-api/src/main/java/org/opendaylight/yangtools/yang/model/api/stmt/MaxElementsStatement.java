b48a36ec5b8ce52d9def40f64ee9a01c3d0bd9a1
Robert Varga <robert.varga@pantheon.tech>
2026-01-26 14:53:02 +0100

    Add {Min,Max}ElementsStatement.OptionalIn
    Factor out the two accessor methods into a common pattern.
    
    Change-Id: I060bad3a17c8201e2b491fdce4221623b3615326
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/MaxElementsStatement.java
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/MaxElementsStatement.java
@@ -7,7 +7,12 @@
  */
 package org.opendaylight.yangtools.yang.model.api.stmt;
 
+import com.google.common.annotations.Beta;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.yang.common.Empty;
 import org.opendaylight.yangtools.yang.common.YangConstants;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
@@ -16,6 +21,44 @@ import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
  * Declared representation of a {@code max-elements} statement.
  */
 public interface MaxElementsStatement extends DeclaredStatement<MaxElementsArgument> {
+    /**
+     * A {@link DeclaredStatement} that is a parent of a single {@link MaxElementsStatement}.
+     * @param <A> Argument type ({@link Empty} if statement does not have argument.)
+     */
+    @Beta
+    interface OptionalIn<A> extends DeclaredStatement<A> {
+        /**
+         * {@return the {@code MaxElementsStatement} or {@code null} if not present}
+         */
+        default @Nullable MaxElementsStatement maxElementsStatement() {
+            for (var stmt : declaredSubstatements()) {
+                if (stmt instanceof MaxElementsStatement maxElements) {
+                    return maxElements;
+                }
+            }
+            return null;
+        }
+
+        /**
+         * {@return an optional {@code MaxElementsStatement}}
+         */
+        default @NonNull Optional<MaxElementsStatement> findMaxElementsStatement() {
+            return Optional.ofNullable(maxElementsStatement());
+        }
+
+        /**
+         * {@return the {@code MaxElementsStatement}}
+         * @throws NoSuchElementException if not present
+         */
+        default @NonNull MaxElementsStatement getMaxElementsStatement() {
+            final var maxElements = maxElementsStatement();
+            if (maxElements == null) {
+                throw new NoSuchElementException("No max-elements statement present in " + this);
+            }
+            return maxElements;
+        }
+    }
+
     /**
      * The definition of {@code max-elements} statement.
      *
