0152a259cacac8146ac97051c628ee85fc93f2a3
Robert Varga <robert.varga@pantheon.tech>
2026-01-26 21:35:25 +0100

    Clean up TypeStatement subclasses
    Move all foreign accessors to their respective declared statements.
    
    Change-Id: I782572d3a1858b83900dd658dbed16964ee3a9e7
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/RangeStatement.java
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/RangeStatement.java
@@ -7,8 +7,14 @@
  */
 package org.opendaylight.yangtools.yang.model.api.stmt;
 
+import com.google.common.annotations.Beta;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.yang.common.Empty;
 import org.opendaylight.yangtools.yang.common.YangConstants;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 
 /**
@@ -16,6 +22,44 @@ import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
  */
 public interface RangeStatement extends DocumentedDeclaredStatement<ValueRanges>,
         ErrorAppTagStatement.OptionalIn<ValueRanges>, ErrorMessageStatement.OptionalIn<ValueRanges> {
+    /**
+     * A {@link DeclaredStatement} that is a parent of a single {@link RangeStatement}.
+     * @param <A> Argument type ({@link Empty} if statement does not have argument.)
+     */
+    @Beta
+    interface OptionalIn<A> extends DeclaredStatement<A> {
+        /**
+         * {@return the {@code RangeStatement} or {@code null} if not present}
+         */
+        default @Nullable RangeStatement rangeStatement() {
+            for (var stmt : declaredSubstatements()) {
+                if (stmt instanceof RangeStatement range) {
+                    return range;
+                }
+            }
+            return null;
+        }
+
+        /**
+         * {@return an optional {@code RangeStatement}}
+         */
+        default @NonNull Optional<RangeStatement> findRangeStatement() {
+            return Optional.ofNullable(rangeStatement());
+        }
+
+        /**
+         * {@return the {@code RangeStatement}}
+         * @throws NoSuchElementException if not present
+         */
+        default @NonNull RangeStatement getRangeStatement() {
+            final var range = rangeStatement();
+            if (range == null) {
+                throw new NoSuchElementException("No range statement present in " + this);
+            }
+            return range;
+        }
+    }
+
     /**
      * The definition of {@code range} statement.
      *
