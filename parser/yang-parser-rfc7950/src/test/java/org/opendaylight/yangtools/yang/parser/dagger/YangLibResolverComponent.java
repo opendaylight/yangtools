6c130e988abe63ec265e39aa51bc29f0bf08e84b
Robert Varga <robert.varga@pantheon.tech>
2026-01-28 18:11:43 +0100

    Add test dagger components
    The modules in yang-parser-rfc7950 are not tested, add the test
    components and EmptyParserExtensionsModule which supplies no
    ParserExtensions.
    
    Change-Id: If598bca174d0a0dcb93c85bc16e42540be67a9db
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit c386a02974e8c4452228f40ede9fd89b28b29b0e)

---
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/parser/dagger/YangLibResolverComponent.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.dagger;
+
+import dagger.Component;
+import jakarta.inject.Singleton;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.parser.api.YangLibResolver;
+import org.opendaylight.yangtools.yang.xpath.dagger.YangXPathParserFactoryModule;
+
+@Singleton
+@Component(
+    modules = {
+        YangXPathParserFactoryModule.class,
+        YangLibResolverModule.class,
+        EmptyParserExtensionsModule.class,
+    })
+@NonNullByDefault
+interface YangLibResolverComponent {
+
+    YangLibResolver libResolver();
+}
