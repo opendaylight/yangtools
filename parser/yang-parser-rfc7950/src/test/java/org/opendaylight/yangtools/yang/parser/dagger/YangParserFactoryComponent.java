c386a02974e8c4452228f40ede9fd89b28b29b0e
Robert Varga <robert.varga@pantheon.tech>
2026-01-28 18:11:43 +0100

    Add test dagger components
    The modules in yang-parser-rfc7950 are not tested, add the test
    components and EmptyParserExtensionsModule which supplies no
    ParserExtensions.
    
    Change-Id: If598bca174d0a0dcb93c85bc16e42540be67a9db
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/parser/dagger/YangParserFactoryComponent.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.dagger;
+
+import dagger.Component;
+import jakarta.inject.Singleton;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.parser.api.YangParserFactory;
+import org.opendaylight.yangtools.yang.source.ir.dagger.YangIRSourceModule;
+import org.opendaylight.yangtools.yang.xpath.dagger.YangXPathParserFactoryModule;
+import org.opendaylight.yangtools.yin.source.dom.dagger.YinDOMSourceModule;
+
+@Singleton
+@Component(
+    modules = {
+        YangIRSourceModule.class,
+        YinDOMSourceModule.class,
+        YangXPathParserFactoryModule.class,
+        YangParserFactoryModule.class,
+        EmptyParserExtensionsModule.class,
+    })
+@NonNullByDefault
+interface YangParserFactoryComponent {
+
+    YangParserFactory parserFactory();
+}
