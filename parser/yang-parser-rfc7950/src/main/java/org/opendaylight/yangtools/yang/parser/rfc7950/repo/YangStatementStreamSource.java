1d7d519ab493d0ee4392199ac5860b99a5df81e8
Robert Varga <robert.varga@pantheon.tech>
2026-01-28 05:02:25 +0100

    WIP: Split out YangIRStatementStreamSource
    // FIXME: add an issue
    
    Change-Id: I509a787db4313b36d0493cb2cab42b9c3dd6792b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/repo/YangStatementStreamSource.java
+++ parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/repo/YangStatementStreamSource.java
@@ -7,44 +7,22 @@
  */
 package org.opendaylight.yangtools.yang.parser.rfc7950.repo;
 
-import static java.util.Objects.requireNonNull;
-
 import com.google.common.annotations.Beta;
 import java.io.IOException;
-import org.opendaylight.yangtools.concepts.AbstractSimpleIdentifiable;
-import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.common.QNameModule;
-import org.opendaylight.yangtools.yang.common.YangVersion;
-import org.opendaylight.yangtools.yang.ir.IRKeyword;
-import org.opendaylight.yangtools.yang.ir.IRStatement;
-import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
-import org.opendaylight.yangtools.yang.model.api.meta.StatementSourceReference;
-import org.opendaylight.yangtools.yang.model.api.source.SourceIdentifier;
 import org.opendaylight.yangtools.yang.model.api.source.YangTextSource;
 import org.opendaylight.yangtools.yang.model.spi.source.YangIRSource;
 import org.opendaylight.yangtools.yang.parser.antlr.YangTextParser;
 import org.opendaylight.yangtools.yang.parser.api.YangSyntaxErrorException;
-import org.opendaylight.yangtools.yang.parser.spi.source.PrefixResolver;
-import org.opendaylight.yangtools.yang.parser.spi.source.QNameToStatementDefinition;
-import org.opendaylight.yangtools.yang.parser.spi.source.SourceException;
-import org.opendaylight.yangtools.yang.parser.spi.source.StatementStreamSource;
-import org.opendaylight.yangtools.yang.parser.spi.source.StatementWriter;
+import org.opendaylight.yangtools.yang.parser.spi.source.YangIRStatementStreamSource;
 
 /**
  * This class represents implementation of StatementStreamSource in order to emit YANG statements using supplied
  * StatementWriter.
  */
 @Beta
-public final class YangStatementStreamSource extends AbstractSimpleIdentifiable<SourceIdentifier>
-        implements StatementStreamSource {
-    private final IRStatement rootStatement;
-    private final String sourceName;
-
-    private YangStatementStreamSource(final SourceIdentifier sourceId, final IRStatement rootStatement,
-            final String sourceName) {
-        super(sourceId);
-        this.rootStatement = requireNonNull(rootStatement);
-        this.sourceName = sourceName;
+public final class YangStatementStreamSource {
+    private YangStatementStreamSource() {
+        // Hidden on purpose
     }
 
     /**
@@ -55,67 +33,9 @@ public final class YangStatementStreamSource extends AbstractSimpleIdentifiable<
      * @throws IOException When we fail to read the source
      * @throws YangSyntaxErrorException If the source fails basic parsing
      */
-    public static YangStatementStreamSource create(final YangTextSource source)
+    public static YangIRStatementStreamSource create(final YangTextSource source)
             throws IOException, YangSyntaxErrorException {
-        return new YangStatementStreamSource(source.sourceId(), YangTextParser.parseToIR(source),
-            source.symbolicName());
-    }
-
-    /**
-     * Create a {@link YangStatementStreamSource} for a {@link YangIRSource}.
-     *
-     * @param source YangTextSchemaSource, must not be null
-     * @return A new {@link YangStatementStreamSource}
-     * @throws NullPointerException if {@code source} is null
-     */
-    public static YangStatementStreamSource create(final YangIRSource source) {
-        return create(source.sourceId(), source.statement(), source.symbolicName());
-    }
-
-    public static YangStatementStreamSource create(final SourceIdentifier identifier, final IRStatement rootStatement,
-            final String symbolicName) {
-        return new YangStatementStreamSource(identifier, rootStatement, symbolicName);
-    }
-
-    @Override
-    public void writePreLinkage(final StatementWriter writer, final QNameToStatementDefinition stmtDef) {
-        new IRStatementVisitor(sourceName, writer, stmtDef, null, YangVersion.VERSION_1).visit(rootStatement);
-    }
-
-    @Override
-    public void writeLinkage(final StatementWriter writer, final QNameToStatementDefinition stmtDef,
-            final PrefixResolver preLinkagePrefixes, final YangVersion yangVersion) {
-        new IRStatementVisitor(sourceName, writer, stmtDef, preLinkagePrefixes, yangVersion) {
-            @Override
-            StatementDefinition<?, ?, ?> resolveStatement(final QNameModule module, final String localName) {
-                return stmtDef.getByNamespaceAndLocalName(module.namespace(), localName);
-            }
-        }.visit(rootStatement);
-    }
-
-    @Override
-    public void writeLinkageAndStatementDefinitions(final StatementWriter writer,
-            final QNameToStatementDefinition stmtDef, final PrefixResolver prefixes, final YangVersion yangVersion) {
-        new IRStatementVisitor(sourceName, writer, stmtDef, prefixes, yangVersion).visit(rootStatement);
-    }
-
-    @Override
-    public void writeFull(final StatementWriter writer, final QNameToStatementDefinition stmtDef,
-            final PrefixResolver prefixes, final YangVersion yangVersion) {
-        new IRStatementVisitor(sourceName, writer, stmtDef, prefixes, yangVersion) {
-            @Override
-            QName getValidStatementDefinition(final IRKeyword keyword, final StatementSourceReference ref) {
-                final QName ret = super.getValidStatementDefinition(keyword, ref);
-                if (ret == null) {
-                    throw new SourceException(ref, "%s is not a YANG statement or use of extension.",
-                        keyword.asStringDeclaration());
-                }
-                return ret;
-            }
-        }.visit(rootStatement);
-    }
-
-    IRStatement rootStatement() {
-        return rootStatement;
+        final var statement = YangTextParser.parseToIR(source);
+        return new YangIRStatementStreamSource(YangIRSource.of(source.sourceId(), statement, source.symbolicName()));
     }
 }
---
+++ parser/yang-parser-spi/src/main/java/org/opendaylight/yangtools/yang/parser/spi/source/YangIRStatementStreamSource.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.spi.source;
+
+import static java.util.Objects.requireNonNull;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.common.YangVersion;
+import org.opendaylight.yangtools.yang.ir.IRKeyword;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementSourceReference;
+import org.opendaylight.yangtools.yang.model.api.source.SourceIdentifier;
+import org.opendaylight.yangtools.yang.model.spi.source.YangIRSource;
+
+/**
+ * A {@link StatementStreamSource} operating based on {@link YangIRSource}.
+ */
+public record YangIRStatementStreamSource(@NonNull YangIRSource source) implements StatementStreamSource {
+    public YangIRStatementStreamSource {
+        requireNonNull(source);
+    }
+
+    @Override
+    public SourceIdentifier getIdentifier() {
+        return source.sourceId();
+    }
+
+    private String symbolicName() {
+        return source.symbolicName();
+    }
+
+    @Override
+    public void writePreLinkage(final StatementWriter writer, final QNameToStatementDefinition stmtDef) {
+        new IRStatementVisitor(symbolicName(), writer, stmtDef, null, YangVersion.VERSION_1)
+            .visit(source.statement());
+    }
+
+    @Override
+    public void writeLinkage(final StatementWriter writer, final QNameToStatementDefinition stmtDef,
+            final PrefixResolver preLinkagePrefixes, final YangVersion yangVersion) {
+        new IRStatementVisitor(symbolicName(), writer, stmtDef, preLinkagePrefixes, yangVersion) {
+            @Override
+            StatementDefinition<?, ?, ?> resolveStatement(final QNameModule module, final String localName) {
+                return stmtDef.getByNamespaceAndLocalName(module.namespace(), localName);
+            }
+        }.visit(source.statement());
+    }
+
+    @Override
+    public void writeLinkageAndStatementDefinitions(final StatementWriter writer,
+            final QNameToStatementDefinition stmtDef, final PrefixResolver prefixes, final YangVersion yangVersion) {
+        new IRStatementVisitor(symbolicName(), writer, stmtDef, prefixes, yangVersion)
+            .visit(source.statement());
+    }
+
+    @Override
+    public void writeFull(final StatementWriter writer, final QNameToStatementDefinition stmtDef,
+            final PrefixResolver prefixes, final YangVersion yangVersion) {
+        new IRStatementVisitor(symbolicName(), writer, stmtDef, prefixes, yangVersion) {
+            @Override
+            QName getValidStatementDefinition(final IRKeyword keyword, final StatementSourceReference ref) {
+                final var ret = super.getValidStatementDefinition(keyword, ref);
+                if (ret == null) {
+                    throw new SourceException(ref, "%s is not a YANG statement or use of extension.",
+                        keyword.asStringDeclaration());
+                }
+                return ret;
+            }
+        }.visit(source.statement());
+    }
+}
