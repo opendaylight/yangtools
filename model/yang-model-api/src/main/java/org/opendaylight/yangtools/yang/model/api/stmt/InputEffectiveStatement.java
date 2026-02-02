8c1fe650f83406856a82b4ce8c7620238e13eef4
Robert Varga <robert.varga@pantheon.tech>
2026-02-02 16:07:22 +0100

    Remove EffectiveOperationStatement
    This is mostly a useless capture of common traits. Move the two methods
    into {Input,Output}EffectiveStatement.MandatoryIn and remove the common
    interface.
    
    Change-Id: Iea7b50846304bc23289c2b292869ec34c8d30cfa
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/InputEffectiveStatement.java
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/InputEffectiveStatement.java
@@ -7,8 +7,12 @@
  */
 package org.opendaylight.yangtools.yang.model.api.stmt;
 
+import com.google.common.annotations.Beta;
+import com.google.common.base.VerifyException;
 import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 
 /**
@@ -17,6 +21,28 @@ import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 public non-sealed interface InputEffectiveStatement extends DataTreeEffectiveStatement<@NonNull InputStatement>,
         DataTreeAwareEffectiveStatement<QName, @NonNull InputStatement>,
         TypedefEffectiveStatement.MultipleIn<QName, @NonNull InputStatement> {
+    /**
+     * An {@link EffectiveStatement} that is a parent of a single {@link InputEffectiveStatement}.
+     *
+     * @param <A> Argument type
+     * @param <D> Class representing declared version of this statement.
+     * @since 15.0.0
+     */
+    @Beta
+    interface MandatoryIn<A, D extends DeclaredStatement<A>> extends EffectiveStatement<A, D> {
+        /**
+         * Return this statement's {@code input} substatement.
+         *
+         * @implSpec
+         *      Default implementation uses {@link #findFirstEffectiveSubstatement(Class)} and throws a
+         *      {@link VerifyException} if a matching substatement is not found.
+         * @return An {@link InputEffectiveStatement}
+         */
+        default @NonNull InputEffectiveStatement inputStatement() {
+            return DefaultMethodHelpers.verifyInputSubstatement(this);
+        }
+    }
+
     @Override
     default StatementDefinition<QName, @NonNull InputStatement, ?> statementDefinition() {
         return InputStatement.DEF;
