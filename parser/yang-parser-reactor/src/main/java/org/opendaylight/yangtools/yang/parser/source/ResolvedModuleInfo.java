8e45a9b62ec9cbaf1ab4f1b20d9ee45d6a839d53
Robert Varga <robert.varga@pantheon.tech>
2026-02-03 22:38:07 +0100

    Introduce parser.source.ResolvedDependency
    This is the common interface for the three SourceDependencies we handle,
    populated with three specializations each of which carries its
    SourceDependency.
    
    JIRA: YANGTOOLS-1112
    Change-Id: I674f81c0c1b8a5fdec83ef1346966e8a380676dc
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/ResolvedModuleInfo.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.source;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.common.UnresolvedQName.Unqualified;
+import org.opendaylight.yangtools.yang.model.spi.source.SourceInfo.Module;
+import org.opendaylight.yangtools.yang.parser.source.ResolvedDependency.ResolvedImport;
+import org.opendaylight.yangtools.yang.parser.source.ResolvedDependency.ResolvedInclude;
+
+/**
+ * A {@link ResolvedSourceInfo} corresponding to a {@code module}.
+ */
+@NonNullByDefault
+public record ResolvedModuleInfo(
+        Module sourceInfo,
+        QNameModule qnameModule,
+        List<ResolvedImport> imports,
+        List<ResolvedInclude> includes) implements ResolvedSourceInfo {
+    public ResolvedModuleInfo {
+        requireNonNull(sourceInfo);
+        requireNonNull(qnameModule);
+        imports = List.copyOf(imports);
+        includes = List.copyOf(includes);
+    }
+
+    @Override
+    public Unqualified prefix() {
+        return sourceInfo.prefix();
+    }
+}
