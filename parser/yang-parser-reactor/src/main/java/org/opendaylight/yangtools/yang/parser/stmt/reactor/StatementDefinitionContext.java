c4e10914f53c1bda9ae24ecef148362867d762c1
Robert Varga <robert.varga@pantheon.tech>
2026-02-03 19:11:47 +0100

    Use ArgumentFactory in reactor
    Remove StatementDefinitionContext's indirection and talk to the
    ArgumentFactory directly.
    
    Change-Id: I58a04f55c25e93210d6730e3496aa8ccf76d8cc4
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/StatementDefinitionContext.java
+++ parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/StatementDefinitionContext.java
@@ -15,18 +15,17 @@ import java.util.Map;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.model.api.meta.ArgumentDefinition;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ArgumentFactory;
 import org.opendaylight.yangtools.yang.parser.spi.meta.ImplicitParentAwareStatementSupport;
 import org.opendaylight.yangtools.yang.parser.spi.meta.ModelProcessingPhase;
 import org.opendaylight.yangtools.yang.parser.spi.meta.NamespaceStmtCtx;
 import org.opendaylight.yangtools.yang.parser.spi.meta.OverrideChildStatementSupport;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StatementFactory;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StatementSupport;
-import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
 import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext.Mutable;
 
 final class StatementDefinitionContext<A, D extends DeclaredStatement<A>, E extends EffectiveStatement<A, D>> {
@@ -40,20 +39,24 @@ final class StatementDefinitionContext<A, D extends DeclaredStatement<A>, E exte
         argumentSpecificSubDefinitions = support.hasArgumentSpecificSupports() ? new HashMap<>() : null;
     }
 
-    @NonNull StatementFactory<A, D, E> getFactory() {
+    @Nullable ArgumentDefinition<A> argumentDefinition() {
+        return support.argumentDefinition();
+    }
+
+    @NonNull QName statementName() {
+        return support.statementName();
+    }
+
+    @NonNull StatementDefinition<A, D, E> publicView() {
+        return support.getPublicView();
+    }
+
+    @NonNull ArgumentFactory<A> argumentFactory() {
         return support;
     }
 
-    A parseArgumentValue(final @NonNull StmtContext<A, D, E> context, final String value) {
-        return support.parseArgumentValue(context, value);
-    }
-
-    A adaptArgumentValue(final @NonNull StmtContext<A, D, E> context, final QNameModule targetModule) {
-        return support.adaptArgumentValue(context, targetModule);
-    }
-
-    @NonNull StatementDefinition<A, D, E> getPublicView() {
-        return support.getPublicView();
+    @NonNull StatementFactory<A, D, E> statementFactory() {
+        return support;
     }
 
     @Nullable StatementSupport<?, ?, ?> implicitParentFor(final @NonNull NamespaceStmtCtx parent,
@@ -78,17 +81,9 @@ final class StatementDefinitionContext<A, D extends DeclaredStatement<A>, E exte
         }
     }
 
-    @Nullable ArgumentDefinition<A> argumentDefinition() {
-        return support.argumentDefinition();
-    }
-
-    @NonNull QName getStatementName() {
-        return support.statementName();
-    }
-
     @Override
     public String toString() {
-        return MoreObjects.toStringHelper(this).add("statement", getStatementName()).toString();
+        return MoreObjects.toStringHelper(this).add("statement", statementName()).toString();
     }
 
     @NonNull StatementDefinitionContext<?, ?, ?> getSubDefinitionSpecificForArgument(final String argument) {
@@ -130,7 +125,7 @@ final class StatementDefinitionContext<A, D extends DeclaredStatement<A>, E exte
             unknownStmtDefsOfYangStmts = new HashMap<>(4);
         }
 
-        final var override = overrideSupport.statementDefinitionOverrideOf(def.getPublicView());
+        final var override = overrideSupport.statementDefinitionOverrideOf(def.publicView());
         final StatementDefinitionContext<?, ?, ?> ret;
         if (override != null) {
             ret = new StatementDefinitionContext<>(override);
