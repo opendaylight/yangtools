4a79e0384dc4c19962cb7a68b1ddcb71c7b23cb2
Robert Varga <robert.varga@pantheon.tech>
2026-02-04 18:31:47 +0100

    Add model.api.meta.StatementGroup
    RFC7950 defines our terminology, add statement groups for easy matching
    of data node, schema node and data definition statement.
    
    Change-Id: I0ed9b80705950fa971dacac7e6afc87cc0cc6055
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/meta/StatementGroup.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.model.api.meta;
+
+import java.util.Set;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.yang.model.api.stmt.ActionStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.AnydataStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.AnyxmlStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.AugmentStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.CaseStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ChoiceStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ContainerStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.InputStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.LeafListStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.LeafStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ListStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.NotificationStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.OutputStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.RpcStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.UsesStatement;
+
+/**
+ * Utility class holding various groups as defined in RFC7950.
+ *
+ * @since 15.0.0
+ */
+@NonNullByDefault
+public final class StatementGroup {
+    /**
+     * The set of {@code data definition statement}s, as per
+     * <a href="https://www.rfc-editor.org/rfc/rfc7950#section-3">RFC7950, section 3</a>.
+     * <pre>{@code
+     *   data definition statement: A statement that defines new data
+     *   nodes.  One of "container", "leaf", "leaf-list", "list", "choice",
+     *   "case", "augment", "uses", "anydata", and "anyxml".
+     * }</pre>
+     */
+    public static final Set<StatementDefinition<?, ?, ?>> DATA_DEFINITION_STATEMENT = Set.of(
+        ContainerStatement.DEF, LeafStatement.DEF, LeafListStatement.DEF, ListStatement.DEF, ChoiceStatement.DEF,
+        CaseStatement.DEF, AugmentStatement.DEF, UsesStatement.DEF, AnydataStatement.DEF, AnyxmlStatement.DEF);
+    /**
+     * The set of {@code data node statement}s, as per
+     * <a href="https://www.rfc-editor.org/rfc/rfc7950#section-3">RFC7950, section 3</a>.
+     * <pre>{@code
+     *   data node: A node in the schema tree that can be instantiated in a
+     *   data tree.  One of container, leaf, leaf-list, list, anydata, and
+     *   anyxml.
+     * }</pre>
+     */
+    public static final Set<StatementDefinition<?, ?, ?>> DATA_NODE = Set.of(
+        ContainerStatement.DEF, LeafStatement.DEF, LeafListStatement.DEF, ListStatement.DEF, AnydataStatement.DEF,
+        AnyxmlStatement.DEF);
+
+    /**
+     * The set of {@code schema node statement}s, as per
+     * <a href="https://www.rfc-editor.org/rfc/rfc7950#section-3">RFC7950, section 3</a>.
+     * <pre>{@code
+     *   schema node: A node in the schema tree.  One of action, container,
+     *   leaf, leaf-list, list, choice, case, rpc, input, output,
+     *   notification, anydata, and anyxml.
+     * }</pre>
+     */
+    public static final Set<StatementDefinition<?, ?, ?>> SCHEMA_NODE = Set.of(
+        ActionStatement.DEF, ContainerStatement.DEF, LeafStatement.DEF, LeafListStatement.DEF, ListStatement.DEF,
+        ChoiceStatement.DEF, CaseStatement.DEF, RpcStatement.DEF, InputStatement.DEF, OutputStatement.DEF,
+        NotificationStatement.DEF, AnydataStatement.DEF, AnyxmlStatement.DEF);
+
+    private StatementGroup() {
+        // Hidden on purpose
+    }
+}
