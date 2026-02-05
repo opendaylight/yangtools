8f9891fed9a3db3caec503f53182f7536f1dcf2e
Robert Varga <robert.varga@pantheon.tech>
2026-02-05 01:50:33 +0100

    Remove UnknownSchemaNode
    UnknownSchemaNode is not referenced anywhere anymore: remove it.
    
    JIRA: YANGTOOLS-1771
    Change-Id: Idb5fce9cddafe4bfe8f12f6947a5a155060c6b20
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/UnknownEffectiveStatement.java
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/stmt/UnknownEffectiveStatement.java
@@ -7,18 +7,19 @@
  */
 package org.opendaylight.yangtools.yang.model.api.stmt;
 
+import com.google.common.annotations.Beta;
 import org.opendaylight.yangtools.yang.common.Empty;
-import org.opendaylight.yangtools.yang.model.api.UnknownSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
 
 /**
- * Effective counterpart to {@link UnknownStatement}. This interface exists only for the purposes of providing a bridge
- * from {@link UnknownSchemaNode} to {@link EffectiveStatement} via {@link UnknownSchemaNode#asEffectiveStatement()}.
+ * Effective counterpart to {@link UnknownStatement}. This interface is a remnant of {@code UnknownSchemaNode} migration
+ * and should be removed soon.
  *
  * @param <A> Argument type ({@link Empty} if statement does not have argument.)
  * @param <D> Class representing declared version of this statement.
  */
+@Beta
 // FIXME: remove this interface once UnknownSchemaNode is gone
 public interface UnknownEffectiveStatement<A, D extends UnknownStatement<A>> extends EffectiveStatement<A, D> {
-
+    // Nothing else
 }
