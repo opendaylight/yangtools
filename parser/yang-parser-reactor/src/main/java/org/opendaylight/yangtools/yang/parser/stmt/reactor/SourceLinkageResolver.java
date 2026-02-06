b43df5212214d0e6b536343178b9c2abf3c041b1
Robert Varga <robert.varga@pantheon.tech>
2026-02-06 02:34:14 +0100

    Refactor IdentifierBinding wiring
    IdentifierBinding requires only source linkage resolution all
    'currentModule', which is really more like 'currentSource', but let's
    not delve on the details.
    
    Since we have resolved linkage, construct the requisite NamespaceBinding
    in SourceLinkageResolver and pass it down to SourceSpecificContext,
    which then uses it to instantiate IdentifierBinding, which is then
    passed on to RootStatementContext.
    
    This reduces users of a number of parser namespaces:
    - IMPORT_PREFIX_TO_MODULECTX
    - MODULECTX_TO_QNAME
    - BELONGSTO_PREFIX_TO_MODULE_NAME
    - MODULE_NAME_TO_QNAME
    
    JIRA: YANGTOOLS-1112
    Change-Id: Idbbff131ba6172f901821c2265c2c93a2b7e8402
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/SourceLinkageResolver.java
+++ parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/stmt/reactor/SourceLinkageResolver.java
@@ -26,6 +26,7 @@ import java.util.TreeSet;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.common.Revision;
 import org.opendaylight.yangtools.yang.common.UnresolvedQName.Unqualified;
@@ -39,6 +40,7 @@ import org.opendaylight.yangtools.yang.model.api.source.SourceIdentifier;
 import org.opendaylight.yangtools.yang.model.api.source.SourceSyntaxException;
 import org.opendaylight.yangtools.yang.model.spi.source.SourceInfo;
 import org.opendaylight.yangtools.yang.model.spi.source.SourceInfo.Submodule;
+import org.opendaylight.yangtools.yang.model.spi.stmt.ImmutableNamespaceBinding;
 import org.opendaylight.yangtools.yang.parser.source.ResolvedSourceInfo;
 import org.opendaylight.yangtools.yang.parser.spi.meta.InferenceException;
 import org.opendaylight.yangtools.yang.parser.spi.meta.ModelProcessingPhase;
@@ -170,8 +172,8 @@ public final class SourceLinkageResolver {
         for (var entry : allResolved.entrySet()) {
             final var source = entry.getKey();
             final var resolved = entry.getValue();
-            final var prefixToModule = new HashMap<Unqualified, QNameModule>();
 
+            final var prefixToModule = new HashMap<Unqualified, QNameModule>();
             // all resolved imports
             for (var dep : resolved.imports()) {
                 putPrefix(prefixToModule, dep.source().prefix(), dep.qname());
@@ -179,21 +181,29 @@ public final class SourceLinkageResolver {
 
             // the module the source belongs to
             final QNameModule definingModule;
+            // the prefix under which the module is known as well as its claim to a namespace+revision
+            final QName currentModule;
             switch (source.sourceInfo()) {
                 case SourceInfo.Module info -> {
                     definingModule = resolved.qnameModule();
+                    // FIXME: info.moduleName() should and resolved.qnameModule() should always be the same
+                    currentModule = info.moduleName().intern();
                     putPrefix(prefixToModule, info.prefix(), definingModule);
                 }
                 case SourceInfo.Submodule info -> {
                     // FIXME: missing @NonNull: this should be ensured through class hierarchy
                     final var belongsTo = resolved.belongsTo();
                     definingModule = belongsTo.parentModuleQname();
+                    // FIXME: this should live in info, just like moduleName() does
+                    currentModule = info.sourceId().name().bindTo(
+                        QNameModule.ofRevision(definingModule.namespace(), info.latestRevision()));
                     putPrefix(prefixToModule, belongsTo.source().prefix(), definingModule);
                 }
             }
 
             result.add(new ResolvedSourceContext(new SourceSpecificContext(source.global(), source.sourceInfo(),
-                definingModule, source.toStreamSource(prefixToModule)), resolved));
+                definingModule, new ImmutableNamespaceBinding(currentModule, Map.copyOf(prefixToModule)),
+                source.toStreamSource(prefixToModule)), resolved));
         }
 
         return List.copyOf(result);
