59dd38dd56bd666773ddad10361d88d6ff998e00
Robert Varga <robert.varga@pantheon.tech>
2026-02-10 13:40:25 +0100

    Refactor AbstractCodecFactory
    Use TypeDefinitionCompat's currentModule() to ditch references to
    SchemaNode.
    
    JIRA: YANGTOOLS-1771
    Change-Id: I9d488e6f2acfc480232b9177c4e24551e17cbcf4
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- data/yang-data-util/src/main/java/org/opendaylight/yangtools/yang/data/util/codec/LazyCodecCache.java
+++ data/yang-data-util/src/main/java/org/opendaylight/yangtools/yang/data/util/codec/LazyCodecCache.java
@@ -10,7 +10,6 @@ package org.opendaylight.yangtools.yang.data.util.codec;
 import com.google.common.annotations.Beta;
 import java.util.IdentityHashMap;
 import java.util.Map;
-import org.opendaylight.yangtools.yang.model.api.SchemaNode;
 import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
 import org.opendaylight.yangtools.yang.model.api.meta.TypeDefinitionCompat;
 
@@ -20,16 +19,16 @@ import org.opendaylight.yangtools.yang.model.api.meta.TypeDefinitionCompat;
 */
 @Beta
 public final class LazyCodecCache<T> extends CodecCache<T> {
-    private final Map<SchemaNode, T> complexCodecs = new IdentityHashMap<>();
+    private final Map<TypeDefinitionCompat, T> complexCodecs = new IdentityHashMap<>();
     private final Map<TypeDefinition<?>, T> simpleCodecs = new IdentityHashMap<>();
 
     @Override
-    <S extends SchemaNode & TypeDefinitionCompat> T getComplex(final S schema, final T codec) {
+    T getComplex(final TypeDefinitionCompat schema, final T codec) {
         return complexCodecs.computeIfAbsent(schema, any -> codec);
     }
 
     @Override
-    <S extends SchemaNode & TypeDefinitionCompat> T lookupComplex(final S schema) {
+    T lookupComplex(final TypeDefinitionCompat schema) {
         return complexCodecs.get(schema);
     }
 
