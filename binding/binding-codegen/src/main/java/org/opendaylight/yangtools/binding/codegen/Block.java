e9cd6f37f0ac1f0fc27407fce9b2c52cff403a85
Robert Varga <robert.varga@pantheon.tech>
2026-02-13 01:41:34 +0100

    WIP: Add Block(Builder}
    // FIXME: add documentation
    // FIXME: add an issue
    // FIXME: try it out
    
    Change-Id: Ic9f802739515bab4b7263a3cc8a1aeb529fb4060
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/binding-codegen/src/main/java/org/opendaylight/yangtools/binding/codegen/Block.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.binding.codegen;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.concepts.Immutable;
+
+/**
+ * A basic text block. Every block is explicitly terminated by a newline ({@code '\n')).
+ */
+@NonNullByDefault
+sealed interface Block extends Immutable, BlockBuilder.Item permits BlockBuilder.BlockImpl {
+    /**
+     * {@return a new {@link BlockBuilder}}
+     */
+    static BlockBuilder builder() {
+        return new BlockBuilder();
+    }
+
+    default StringBuilder appendTo(final StringBuilder sb) {
+        return appendTo(sb, 0);
+    }
+
+    default StringBuilder appendTo(final StringBuilder sb, final int indent) {
+        try {
+            appendTo((Appendable) sb, indent);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+        return sb;
+    }
+
+    default Appendable appendTo(final Appendable to) throws IOException {
+        return appendTo(to, 0);
+    }
+
+    @Override
+    Appendable appendTo(Appendable to, int indent) throws IOException;
+}
