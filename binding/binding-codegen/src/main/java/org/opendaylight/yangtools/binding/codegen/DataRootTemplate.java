179a0fe3a8a6e5f6dfb308fd1dd46135527c8a66
Robert Varga <robert.varga@pantheon.tech>
2026-02-13 03:19:31 +0100

    WIP: Migrate DataRootTemplate
    Use BlockBuilder to reduce verbosity.
    
    Change-Id: Ib75fafff86910811c559808cef5f55e486687d84
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-codegen/src/main/java/org/opendaylight/yangtools/binding/codegen/DataRootTemplate.java
+++ binding/binding-codegen/src/main/java/org/opendaylight/yangtools/binding/codegen/DataRootTemplate.java
@@ -8,49 +8,56 @@
 package org.opendaylight.yangtools.binding.codegen;
 
 import com.google.common.base.VerifyException;
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.yangtools.binding.DataRoot;
 import org.opendaylight.yangtools.binding.contract.Naming;
 import org.opendaylight.yangtools.binding.model.api.GeneratedType;
 import org.opendaylight.yangtools.binding.model.api.JavaTypeName;
+import org.opendaylight.yangtools.binding.model.api.ParameterizedType;
 import org.opendaylight.yangtools.binding.model.ri.BindingTypes;
 
 /**
  * Template for {@link DataRoot} specializations.
  */
 final class DataRootTemplate extends InterfaceTemplate {
+    private final @NonNullByDefault ParameterizedType metaType;
+
     DataRootTemplate(final GeneratedType genType) {
         super(genType);
+        metaType = BindingTypes.rootMeta(genType);
     }
 
     @Override
     CharSequence generateConstants() {
+        // extract metadata: these should be just DataRootArchetype's invariants
         if (consts.size() != 1) {
             throw new VerifyException("Unexpected constants " + consts);
         }
-
-        final var meta = consts.getFirst();
-        if (!meta.getName().equals(Naming.META_STATIC_FIELD_NAME)) {
-            throw new VerifyException("Unexpected constant " + meta);
+        final var constant = consts.getFirst();
+        final var name = constant.getName();
+        if (!name.equals(Naming.META_STATIC_FIELD_NAME)) {
+            throw new VerifyException("Unexpected constant " + constant);
         }
-        if (!(meta.getValue() instanceof JavaTypeName infoProviderName)) {
-            throw new VerifyException("Malformed constant " + meta);
+        if (!(constant.getValue() instanceof JavaTypeName infoProviderName)) {
+            throw new VerifyException("Malformed constant " + constant);
         }
 
-        // pre-compute constants: split out for future isolation
+        // pre-compute constants: split out for future isolation, as these contribute to the file header
         final var nonNullByDefault = importedName(NONNULL_BY_DEFAULT);
-        final var rootMetaType = BindingTypes.rootMeta(type());
-        final var rootMetaRaw = importedName(rootMetaType.getRawType());
-        final var rootMeta = importedName(rootMetaType);
-        final var infoProvider = importedName(infoProviderName);
+        final var meta = importedName(metaType.getRawType());
+        final var metaT = importedName(metaType);
         final var type = importedName(type());
+        final var infoProvider = importedName(infoProviderName);
 
-        return new StringBuilder("""
+        // build a block and format it: this is the best we can do
+        return Block.builder().t("""
             /**
-             * YANG identifier of the statement represented by this class.
+             * {@link """).s(meta).s(" associated with this class.").eol()
+            .t("""
              */
-            """).append(nonNullByDefault).append('\n')
-            .append(rootMeta).append(" META = new ").append(rootMetaRaw).append("<>(")
-            .append(type).append(".class, ").append(infoProvider).append(");\n")
-            .toString();
+            @""").s(nonNullByDefault).eol()
+            .s(metaT).c(' ').w().s(name).s(" = new ").s(meta).s("<>(").s(type).s(".class, ")
+            .s(infoProvider).s(".getInstance().getModuleInfo());")
+            .build().appendTo(new StringBuilder()).toString();
     }
 }
