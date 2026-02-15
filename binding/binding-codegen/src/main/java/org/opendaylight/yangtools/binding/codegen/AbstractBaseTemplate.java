780501196eb54a36722d3e36b54f94f9aaf772e2
Robert Varga <robert.varga@pantheon.tech>
2026-02-15 14:31:53 +0100

    Migrate asArgumentsDeclaration()
    This is a simple method, replace it with Java implementation.
    
    JIRA: YANGTOOLS-1618
    Change-Id: I6df6d01ecc9cfdab20000e94b03fdfc35abc7d62
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-codegen/src/main/java/org/opendaylight/yangtools/binding/codegen/AbstractBaseTemplate.java
+++ binding/binding-codegen/src/main/java/org/opendaylight/yangtools/binding/codegen/AbstractBaseTemplate.java
@@ -100,6 +100,29 @@ abstract class AbstractBaseTemplate extends JavaFileTemplate {
         }
     }
 
+    /**
+     * Template method which generates method parameters with their types from {@code parameters}.
+     *
+     * @param parameters group of generated property instances which are transformed to the method parameters
+     * @return string with the list of the method parameters with their types in Java format
+     */
+    final @NonNull String asArgumentsDeclaration(final @NonNull Iterable<GeneratedProperty> parameters) {
+        final var it = parameters.iterator();
+        if (!it.hasNext()) {
+            return "";
+        }
+
+        final var sb = new StringBuilder();
+        while (true) {
+            final var parameter = it.next();
+            sb.append(importedName(parameter.getReturnType())).append(' ').append(fieldName(parameter));
+            if (!it.hasNext()) {
+                return sb.toString();
+            }
+            sb.append(", ");
+        }
+    }
+
     static final @NonNull String getterMethodName(final @NonNull String propName) {
         return Naming.GETTER_PREFIX + Naming.toFirstUpper(propName);
     }
