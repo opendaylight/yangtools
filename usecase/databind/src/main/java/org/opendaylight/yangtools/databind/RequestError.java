2f95891c134703f1d83e3d7c03612b85255e1667
Robert Varga <robert.varga@pantheon.tech>
2026-02-16 13:43:35 +0100

    Move ErrorMessage to yang.common
    This is a simple construct, which should really be living in
    yang-common, move it there.
    
    JIRA: YANGTOOLS-1764
    Change-Id: I75ea7b4ba81c239a49152b94eed76603365fccd7
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- usecase/databind/src/main/java/org/opendaylight/yangtools/databind/RequestError.java
+++ usecase/databind/src/main/java/org/opendaylight/yangtools/databind/RequestError.java
@@ -12,6 +12,7 @@ import static java.util.Objects.requireNonNull;
 import com.google.common.base.MoreObjects;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.yang.common.ErrorMessage;
 import org.opendaylight.yangtools.yang.common.ErrorTag;
 import org.opendaylight.yangtools.yang.common.ErrorType;
 import org.opendaylight.yangtools.yang.common.RpcError;
