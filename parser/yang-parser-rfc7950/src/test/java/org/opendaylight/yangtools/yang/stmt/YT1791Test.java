4427b9909eb5903af8f97e88e28844b86450648f
Robert Varga <robert.varga@pantheon.tech>
2026-02-20 11:46:42 +0100

    Correct range overlap detection
    Range overlap detection is kicking in only for 3 or more ranges due to
    bad precondition check: we should check for ranges not being empty, not
    having more than one item.
    
    JIRA: YANGTOOLS-1791
    Change-Id: Ia8909c41e1fe7d4ec01716a5655b093cd8814ee0
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 2c81a880058d65d600648eb6b9283601df1a2885)

---
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/YT1791Test.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class YT1791Test extends AbstractYangTest {
+    @Test
+    void testOverlappingTwo() {
+        assertThat(assertInferenceException("/bugs/YT1791/two.yang").getMessage())
+            .startsWith("Some of the value ranges in 120..150|140 are not disjoint [at ")
+            .endsWith("/two.yang:8:7]");
+    }
+}
