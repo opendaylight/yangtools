96dabe1f625226e07794ea9473c4c4337fd1af27
Robert Varga <robert.varga@pantheon.tech>
2026-02-21 07:13:22 +0100

    Refactor ConcreteType/BaseTypeWithRestrictions
    Add a better definition for ConcreteType and tie it together with
    BaseTypeWithRestrictions, now known as RestrictedType.
    
    JIRA: YANGTOOLS-1792
    
    Change-Id: Ia27376c485506d05657cb7f417fe32c3be41ff30
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-model/src/main/java/org/opendaylight/yangtools/binding/model/api/ConcreteType.java
+++ binding/binding-model/src/main/java/org/opendaylight/yangtools/binding/model/api/ConcreteType.java
@@ -7,10 +7,28 @@
  */
 package org.opendaylight.yangtools.binding.model.api;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
 /**
- * The Concrete Type is marker interface that encapsulates definition of java
- * concrete type.
+ * This interface represents a Java type which is expected to be available via  {@code binding-spec} dependencies. This
+ * includes all classes therein, but also {@code yang-common}'s {@code Decimal64}.
+ *
+ * <p>These types support binding and unbinding additional {@link Restrictions} via their {@link RestrictedType}
+ * specialization.
  */
-public interface ConcreteType extends Type {
-    // Nothing else
+@NonNullByDefault
+public sealed interface ConcreteType extends Type permits DefaultConcreteType, RestrictedType {
+    /**
+     * {@return this type's equivalent with specified {@link Restrictions}}
+     * @param newRestrictions the restrictions to apply
+     */
+    RestrictedType withRestrictions(Restrictions newRestrictions);
+
+    /**
+     * {@return a {@link ConcreteType} for specified type {@link Class}}
+     * @param typeClass the type class
+     */
+    static ConcreteType ofClass(final Class<?> typeClass) {
+        return new DefaultConcreteType(JavaTypeName.create(typeClass));
+    }
 }
