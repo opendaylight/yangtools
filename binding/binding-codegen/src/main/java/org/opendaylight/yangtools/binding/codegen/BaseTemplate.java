242875d1333ce02be24596ade43d4df2222f0a34
Robert Varga <robert.varga@pantheon.tech>
2026-02-23 07:28:54 +0100

    Use string concatenation in emitNameConstant()
    This method is straightforward to the point where use of StringBuilder
    disrupts the flow.
    
    Use a simple string concatenation, which makes the code much more
    readable and results in less bytecode.
    
    Change-Id: I2c4a874c17a47a3a810ac5f829c48b523d461667
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-codegen/src/main/java/org/opendaylight/yangtools/binding/codegen/BaseTemplate.java
+++ binding/binding-codegen/src/main/java/org/opendaylight/yangtools/binding/codegen/BaseTemplate.java
@@ -205,14 +205,11 @@ abstract class BaseTemplate extends JavaFileTemplate {
     @NonNullByDefault
     final String emitNameConstant(final String name, final Type type, final JavaTypeName yangModuleInfo,
             final String yangDataName) {
-        return new StringBuilder()
-            .append("/**\n")
-            .append(" * Yang Data template name of the statement represented by this class.\n")
-            .append(" */\n")
-            .append("public static final ").append(importedNonNull(type)).append(' ').append(name).append(" = ")
-                .append(importedName(yangModuleInfo)).append('.').append(Naming.MODULE_INFO_YANGDATANAMEOF_METHOD_NAME)
-                .append("(\"").append(yangDataName).append("\");\n")
-            .toString();
+        return "/**\n"
+            +  " * Yang Data template name of the statement represented by this class.\n"
+            +  " */\n"
+            +  "public static final " + importedNonNull(type) + ' ' + name + " = " + importedName(yangModuleInfo) + '.'
+                 + Naming.MODULE_INFO_YANGDATANAMEOF_METHOD_NAME + "(\"" + yangDataName + "\");\n";
     }
 
     @NonNullByDefault
