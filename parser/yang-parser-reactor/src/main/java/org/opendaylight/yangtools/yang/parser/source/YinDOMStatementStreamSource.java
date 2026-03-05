94e899ca4e08a886d2486c83087ad1a216ff2242
Robert Varga <robert.varga@pantheon.tech>
2026-03-05 10:55:31 +0100

    WIP: Refactor RootStatementContext instantiation
    JIRA: YANGTOOLS-1112
    Change-Id: Ia0207b2bc63f15a0a1219e57f64104210f720803
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/YinDOMStatementStreamSource.java
+++ parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/YinDOMStatementStreamSource.java
@@ -10,12 +10,17 @@ package org.opendaylight.yangtools.yang.parser.source;
 
 import static java.util.Objects.requireNonNull;
 
+import com.google.common.base.VerifyException;
 import java.util.Map;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.yangtools.yang.common.QNameModule;
 import org.opendaylight.yangtools.yang.common.UnresolvedQName.Unqualified;
+import org.opendaylight.yangtools.yang.model.spi.source.YinDOMModuleSource;
 import org.opendaylight.yangtools.yang.model.spi.source.YinDOMSource;
+import org.opendaylight.yangtools.yang.model.spi.source.YinDOMSubmoduleSource;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 /**
  * A {@link StatementStreamSource} based on a {@link YinDOMSource}.
@@ -39,6 +44,33 @@ record YinDOMStatementStreamSource(@NonNull YinDOMSource source) implements Stat
 
     YinDOMStatementStreamSource {
         requireNonNull(source);
+    }
+
+    @Override
+    public Root root() {
+        final var statement = source.statement();
+        final var nameAttr = statement.getAttributeNodeNS(null, "name");
+        if (nameAttr == null) {
+            throw new VerifyException("missing name");
+        }
+        final var rawArgument = nameAttr.getTextContent();
+        final var sourceRef = source.sourceRefOf(statement);
+        final var size = countElements(statement.getChildNodes());
+
+        return switch (source) {
+            case YinDOMModuleSource module -> new ModuleRoot(sourceRef, rawArgument, size);
+            case YinDOMSubmoduleSource submodule -> new SubmoduleRoot(sourceRef, rawArgument, size);
+        };
+    }
+
+    private static int countElements(final NodeList nodeList) {
+        int count = 0;
+        for (int i = 0, length = nodeList.getLength(); i < length; ++i) {
+            if (nodeList.item(i) instanceof Element) {
+                count++;
+            }
+        }
+        return count;
     }
 
     @Override
