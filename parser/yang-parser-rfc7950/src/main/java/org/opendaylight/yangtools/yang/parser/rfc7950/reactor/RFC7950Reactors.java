0e3b6b953ae4389813ae619dd5de7b314762c6d7
Robert Varga <robert.varga@pantheon.tech>
2026-03-06 11:29:27 +0100

    Refactor ParserNamespaces.INCLUDED_MODULE
    INCLUDED_MODULE is mis-named and mis-defined. Rename it to
    INCLUDED_SUBMODULE and change its definition to mirror that of
    IMPORTED_MODULE.
    
    JIRA: YANGTOOLS-1112
    Change-Id: Ie10e7e18f7158ec50af0cc8eb60d9760c0d118d2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/reactor/RFC7950Reactors.java
+++ parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/reactor/RFC7950Reactors.java
@@ -136,7 +136,7 @@ public final class RFC7950Reactors {
             .addSupport(new YangVersionStatementSupport(config))
             .addSupport(NamespaceBehaviours.IMPORT_PREFIX_TO_MODULECTX)
             .addSupport(NamespaceBehaviours.IMPORTED_MODULE)
-            .addSupport(NamespaceBehaviours.INCLUDED_MODULE)
+            .addSupport(NamespaceBehaviours.INCLUDED_SUBMODULE)
             .addSupport(NamespaceBehaviours.INCLUDED_SUBMODULE_NAME_TO_MODULECTX)
             .addSupport(NamespaceBehaviours.MODULE)
             .addSupport(NamespaceBehaviours.MODULE_FOR_BELONGSTO)
