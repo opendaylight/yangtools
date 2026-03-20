0ebe019af9f88066bbebff4f7a7349056065f95c
Robert Varga <robert.varga@pantheon.tech>
2026-03-20 23:23:01 +0100

    WIP: Bind to RFC8431 ietf-netconf-acm
    While we do have RFC8341 revision explicitly mentioned in NACMConstants,
    our parser support only binds to RFC6536 revision, leaving NACM
    unsupported with RFC8341.
    
    This patch adds explicit binding to RFC8341, so that the new revision of
    the model is covered through same support as the old revision.
    
    // FIXME: add an explicit test
    
    JIRA: YANGTOOLS-1825
    Change-Id: I7244ba9fccc959f519c69a2b85fc356dd086ade9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/rfc6536-parser-support/src/main/java/org/opendaylight/yangtools/rfc6536/parser/DefaultDenyWriteStatementSupport.java
+++ parser/rfc6536-parser-support/src/main/java/org/opendaylight/yangtools/rfc6536/parser/DefaultDenyWriteStatementSupport.java
@@ -7,26 +7,44 @@
  */
 package org.opendaylight.yangtools.rfc6536.parser;
 
+import static java.util.Objects.requireNonNull;
+
 import com.google.common.collect.ImmutableList;
+import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.rfc6536.model.api.DefaultDenyWriteEffectiveStatement;
 import org.opendaylight.yangtools.rfc6536.model.api.DefaultDenyWriteStatement;
 import org.opendaylight.yangtools.yang.common.Empty;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclarationReference;
 import org.opendaylight.yangtools.yang.model.api.meta.DeclaredStatement;
 import org.opendaylight.yangtools.yang.model.api.meta.EffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.meta.StatementDefinition;
 import org.opendaylight.yangtools.yang.parser.api.YangParserConfiguration;
 import org.opendaylight.yangtools.yang.parser.spi.meta.AbstractEmptyStatementSupport;
 import org.opendaylight.yangtools.yang.parser.spi.meta.BoundStmtCtx;
 import org.opendaylight.yangtools.yang.parser.spi.meta.EffectiveStmtCtx.Current;
 import org.opendaylight.yangtools.yang.parser.spi.meta.SubstatementValidator;
 
-public final class DefaultDenyWriteStatementSupport
+final class DefaultDenyWriteStatementSupport
         extends AbstractEmptyStatementSupport<DefaultDenyWriteStatement, DefaultDenyWriteEffectiveStatement> {
     private static final SubstatementValidator VALIDATOR =
         SubstatementValidator.builder(DefaultDenyWriteStatement.DEF).build();
 
-    public DefaultDenyWriteStatementSupport(final YangParserConfiguration config) {
+    private final @NonNull StatementDefinition<Empty, DefaultDenyWriteStatement, DefaultDenyWriteEffectiveStatement>
+        definition;
+
+    DefaultDenyWriteStatementSupport(final YangParserConfiguration config,
+            final StatementDefinition<Empty, DefaultDenyWriteStatement, DefaultDenyWriteEffectiveStatement> def) {
         super(DefaultDenyWriteStatement.DEF, StatementPolicy.contextIndependent(), config, VALIDATOR);
+        definition = requireNonNull(def);
+    }
+
+    DefaultDenyWriteStatementSupport(final YangParserConfiguration config) {
+        this(config, DefaultDenyWriteStatement.DEF);
+    }
+
+    @Override
+    public StatementDefinition<Empty, DefaultDenyWriteStatement, DefaultDenyWriteEffectiveStatement> definition() {
+        return definition;
     }
 
     // FIXME: not implemented:
