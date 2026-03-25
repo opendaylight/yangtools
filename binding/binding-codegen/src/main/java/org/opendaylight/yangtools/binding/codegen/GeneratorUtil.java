a524672d4a8498c89c9aa869c1ec770f1490e983
Robert Varga <robert.varga@pantheon.tech>
2026-03-25 23:36:22 +0100

    Remove GeneratorUtil.isConstantInTO()
    This method is only used by its test: remove both, allowing us to clean
    up the remainder of GeneratorUtilTest.
    
    Change-Id: I57fd0cd68d2dad2701f7cb75e182e017504eb67c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-codegen/src/main/java/org/opendaylight/yangtools/binding/codegen/GeneratorUtil.java
+++ binding/binding-codegen/src/main/java/org/opendaylight/yangtools/binding/codegen/GeneratorUtil.java
@@ -7,48 +7,15 @@
  */
 package org.opendaylight.yangtools.binding.codegen;
 
-import java.util.List;
-import org.opendaylight.yangtools.binding.model.api.Constant;
-import org.opendaylight.yangtools.binding.model.api.GeneratedTransferObject;
 import org.opendaylight.yangtools.binding.model.api.ParameterizedType;
 import org.opendaylight.yangtools.binding.model.api.Type;
 
-public final class GeneratorUtil {
+final class GeneratorUtil {
     private GeneratorUtil() {
         // Hidden on purpose
     }
 
-    /**
-     * Checks if the constant with the name <code>constName</code> is in the list of the constant definition for
-     * <code>genTO</code>.
-     *
-     * @param constName string with the name of constant which is sought
-     * @param genTO generated transfer object in which is <code>constName</code> sought
-     * @return boolean value
-     *         <ul>
-     *         <li>true - if <code>constName</code> is in the list of the
-     *         constant definition for <code>genTO</code></li>
-     *         <li>false - in other cases</li>
-     *         </ul>
-     * @throws IllegalArgumentException
-     *             <ul>
-     *             <li>if <code>constName</code> equals <code>null</code></li>
-     *             <li>if <code>genTO</code> equals <code>null</code></li>
-     *             </ul>
-     */
-    static boolean isConstantInTO(final String constName, final GeneratedTransferObject genTO) {
-        if (constName == null || genTO == null) {
-            throw new IllegalArgumentException();
-        }
-        List<Constant> consts = genTO.getConstantDefinitions();
-        for (Constant cons : consts) {
-            if (cons.getName().equals(constName)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
+    // FIXME: YANGTOOLS-1804: move to BuilderTemplate
     static boolean strictTypeEquals(final Type type1, final Type type2) {
         if (!type1.equals(type2)) {
             return false;
---
+++ binding/binding-codegen/src/test/java/org/opendaylight/yangtools/binding/codegen/AbstractClassTemplateTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.binding.codegen;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opendaylight.yangtools.binding.model.api.GeneratedProperty;
+import org.opendaylight.yangtools.binding.model.api.GeneratedTransferObject;
+
+@ExtendWith(MockitoExtension.class)
+class AbstractClassTemplateTest {
+    @Mock
+    private GeneratedTransferObject enclosedType;
+    @Mock
+    private GeneratedTransferObject superType;
+    @Mock
+    private GeneratedProperty property;
+
+    @Test
+    void getPropertiesOfAllParentsTest() {
+        doReturn(List.of(property)).when(enclosedType).getProperties();
+        doReturn(Boolean.TRUE).when(property).isReadOnly();
+        doReturn(enclosedType).when(superType).getSuperType();
+        assertThat(AbstractClassTemplate.propertiesOfAllParents(superType)).contains(property);
+    }
+}
