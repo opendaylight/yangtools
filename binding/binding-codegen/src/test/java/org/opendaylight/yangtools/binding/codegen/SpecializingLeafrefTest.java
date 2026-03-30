57d84f5cbc66b41350984d9e854f21835f29a8f5
Robert Varga <robert.varga@pantheon.tech>
2026-03-30 10:41:45 +0200

    Speed up SpecializingLeafrefTest
    Use @BeforeAll/@AfterAll to share the resource we are asserting on.
    
    Change-Id: Ia05828d5f9f3b2d54e4afbac3c563ed5577278ae
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-codegen/src/test/java/org/opendaylight/yangtools/binding/codegen/SpecializingLeafrefTest.java
+++ binding/binding-codegen/src/test/java/org/opendaylight/yangtools/binding/codegen/SpecializingLeafrefTest.java
@@ -23,8 +23,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.opendaylight.yangtools.binding.contract.Naming;
 import org.opendaylight.yangtools.binding.model.api.GeneratedType;
@@ -70,13 +70,13 @@ class SpecializingLeafrefTest extends BaseCompilationTest {
     private static final String TTAB_SET_IS_VALID_ARG_TRUE = TRIPLE_TAB + "isValidArg = true;";
     private static final String DTAB_INIT_IS_VALID_ARG_FALSE = DOUBLE_TAB + "boolean isValidArg = false;";
 
-    private Path sourcesOutputDir;
-    private Path compiledOutputDir;
-    private List<GeneratedType> types;
-    private Map<String, Path> files;
+    private static Path sourcesOutputDir;
+    private static Path compiledOutputDir;
+    private static List<GeneratedType> types;
+    private static Map<String, Path> files;
 
-    @BeforeEach
-    void before() {
+    @BeforeAll
+    static void before() {
         sourcesOutputDir = CompilationTestUtils.generatorOutput("mdsal426");
         compiledOutputDir = CompilationTestUtils.compilerOutput("mdsal426");
         types = generateTestSources("/compilation/mdsal426", sourcesOutputDir);
@@ -84,9 +84,13 @@ class SpecializingLeafrefTest extends BaseCompilationTest {
         files = getFiles(sourcesOutputDir);
     }
 
-    @AfterEach
-    void after() throws Exception {
+    @AfterAll
+    static void after() throws Exception {
         CompilationTestUtils.cleanUp(sourcesOutputDir, compiledOutputDir);
+        sourcesOutputDir = null;
+        compiledOutputDir = null;
+        types = null;
+        files = null;
     }
 
     @Test
@@ -166,7 +170,7 @@ class SpecializingLeafrefTest extends BaseCompilationTest {
         leafList1AndLeaf1Absence(BAZ_GRP);
     }
 
-    private void leafList1AndLeaf1Absence(final String typeName) throws Exception {
+    private static void leafList1AndLeaf1Absence(final String typeName) throws Exception {
         verifyMethodAbsence(typeName, GET_LEAF1_NAME);
         verifyMethodAbsence(typeName, GET_LEAFLIST1_NAME);
 
@@ -273,7 +277,7 @@ class SpecializingLeafrefTest extends BaseCompilationTest {
         return getJavaFileName(name + Naming.BUILDER_SUFFIX);
     }
 
-    private String getFileContent(final String fileName) throws Exception {
+    private static String getFileContent(final String fileName) throws Exception {
         final var file = files.get(getJavaFileName(fileName));
         try (var stream = Files.lines(file)) {
             assertTrue(stream.findFirst().isPresent());
@@ -283,17 +287,17 @@ class SpecializingLeafrefTest extends BaseCompilationTest {
         return content;
     }
 
-    private void verifyMethodAbsence(final String typeName, final String getterName) {
+    private static void verifyMethodAbsence(final String typeName, final String getterName) {
         verifyReturnType(typeName, getterName, null);
     }
 
-    private void verifyReturnType(final String typeName, final String getterName, final Type returnType) {
-        final var generated = typeByName(types, typeName);
+    private static void verifyReturnType(final String typeName, final String getterName, final Type returnType) {
+        final var generated = typeByName(typeName);
         assertNotNull(generated);
         assertEquals(returnType, returnTypeByMethodName(generated, getterName));
     }
 
-    private static GeneratedType typeByName(final List<GeneratedType> types, final String name) {
+    private static GeneratedType typeByName(final String name) {
         for (var type : types) {
             if (type.simpleName().equals(name)) {
                 return type;
