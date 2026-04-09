aff55bc1a3b42e873c0de468752db06ec5e6479f
Robert Varga <robert.varga@pantheon.tech>
2026-04-09 10:55:20 +0200

    Expose RootMeta.unsafeAccess()
    There are three parts to this patch:
    - we generate a new UNSAFE_ACCESS field in YangModuleInfoImpl
    - we update RootMeta to require an UnsafeAccess
    - we update DataRootTemplate to pass the singleton instance to RootMeta
    
    JIRA: YANGTOOLS-1843
    Change-Id: I501d790cee75827ab7c5da179ce7a7cfcc9b0bef
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-spec/src/test/java/org/opendaylight/yangtools/binding/test/mock/FooData.java
+++ binding/binding-spec/src/test/java/org/opendaylight/yangtools/binding/test/mock/FooData.java
@@ -10,24 +10,28 @@ package org.opendaylight.yangtools.binding.test.mock;
 import java.io.InputStream;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.yangtools.binding.DataRoot;
+import org.opendaylight.yangtools.binding.contract.Naming;
+import org.opendaylight.yangtools.binding.lib.DefaultUnsafeAccess;
 import org.opendaylight.yangtools.binding.meta.RootMeta;
 import org.opendaylight.yangtools.binding.meta.YangModuleInfo;
 import org.opendaylight.yangtools.yang.common.QName;
 
 public interface FooData extends DataRoot<FooData> {
     @NonNullByDefault
-    RootMeta<FooData> META = new RootMeta<>(FooData.class, new YangModuleInfo() {
+    RootMeta<FooData> META = new RootMeta<>(FooData.class,
         // Note: this usually supplied via YangModuleInfoProvider reference
-        @Override
-        public InputStream openYangTextStream() {
-            throw new UnsupportedOperationException();
-        }
+        new YangModuleInfo() {
+            @Override
+            public InputStream openYangTextStream() {
+                throw new UnsupportedOperationException();
+            }
 
-        @Override
-        public QName getName() {
-            throw new UnsupportedOperationException();
-        }
-    });
+            @Override
+            public QName getName() {
+                throw new UnsupportedOperationException();
+            }
+        },
+        new DefaultUnsafeAccess(Naming.PACKAGE_PREFIX + "mock", FooData.class.getModule()));
 
     @Override
     default Class<FooData> implementedInterface() {
