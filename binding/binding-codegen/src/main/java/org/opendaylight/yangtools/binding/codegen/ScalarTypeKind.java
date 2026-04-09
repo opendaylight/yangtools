a3fb9371ecc31e83b84fa3d60e2495bc656912b7
Robert Varga <robert.varga@pantheon.tech>
2026-04-09 18:04:25 +0200

    WIP: Integrate generated ScalarTypeObjects with UnsafeAccess
    JIRA: YANGTOOLS-1677
    Change-Id: If8a3e13c8b6da9accb1e4944a50fe7e17cebb6aa
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/binding-codegen/src/main/java/org/opendaylight/yangtools/binding/codegen/ScalarTypeKind.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.binding.codegen;
+
+import static org.opendaylight.yangtools.binding.model.ri.BindingTypes.SCALAR_TYPE_OBJECT;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.binding.ScalarTypeObject;
+import org.opendaylight.yangtools.binding.model.api.GeneratedTransferObject;
+
+/**
+ * The code generation shape of a {@link GeneratedTransferObject} for the purposes of generation of a
+ * {@link ClassTemplate}'s {@link ScalarTypeObject} output class.
+ */
+@NonNullByDefault
+enum ScalarTypeKind {
+    /**
+     * This template is not generating a class in a {@link ScalarTypeObject} hierarchy.
+     */
+    // FIXME: remove this member when we have a specialized ScalarTypeObjectTemplate
+    NONE(false, false),
+    /**
+     * This template generates the root class in a {@link ScalarTypeObject} hierarchy.
+     */
+    ROOT(true, false),
+    /**
+     * This template generates the root class in a {@link ScalarTypeObject} hierarchy and defines restrictions.
+     */
+    ROOT_RESTRICTING(true, true),
+    /**
+     * This template generates a subclass class in a {@link ScalarTypeObject} hierarchy and it, or any of its
+     * superclasses, defines restrictions.
+     */
+    SUBCLASS(false, true),
+    /**
+     * This template generates a subclass class in a {@link ScalarTypeObject} hierarchy and one of its superclasses
+     * defines restrictions.
+     */
+    SUBCLASS_INHERITING(false, true),
+    /**
+     * This template generates a subclass class in a {@link ScalarTypeObject} hierarchy and it defines the first
+     * restrictions. This implies its root is {@link #ROOT} and all intermediate superclasses are {@link #SUBCLASS}.
+     */
+    SUBCLASS_RESTRICTING(false, true);
+
+    private final boolean isRoot;
+    private final boolean hasRestrictions;
+
+    ScalarTypeKind(final boolean isRoot, final boolean hasRestrictions) {
+        this.isRoot = isRoot;
+        this.hasRestrictions = hasRestrictions;
+    }
+
+    /**
+     * {@return {@code true} if the class represents the root of a {@link ScalarTypeObject} hierarchy}
+     */
+    boolean isRoot() {
+        return isRoot;
+    }
+
+    /**
+     * {@return {@code true} if the class or any of its superclasses have restrictions}
+     */
+    boolean hasRestrictions() {
+        return hasRestrictions;
+    }
+
+    private static boolean hasRestrictions(final GeneratedTransferObject gto) {
+        final var restrictions = gto.getRestrictions();
+        return restrictions != null && !restrictions.isEmpty();
+    }
+
+    static ScalarTypeKind of(final GeneratedTransferObject gto) {
+        final var superType = gto.getSuperType();
+        if (superType != null) {
+            final var superClass = of(superType);
+            return superClass == NONE ? NONE : ofSubclass(gto, superClass.hasRestrictions);
+        }
+
+        // FIXME: a better check, for example 'gto instanceof ScalarTypeObjectArchetype'
+        if (gto.getImplements().stream().noneMatch(ifc -> SCALAR_TYPE_OBJECT.name().equals(ifc.name()))) {
+            return NONE;
+        }
+        return hasRestrictions(gto) ? ROOT_RESTRICTING : ROOT;
+    }
+
+    private static ScalarTypeKind ofSubclass(final GeneratedTransferObject gto, final boolean superRestricted) {
+        if (superRestricted) {
+            return SUBCLASS_INHERITING;
+        }
+        return hasRestrictions(gto) ? SUBCLASS_RESTRICTING : SUBCLASS;
+    }
+}
