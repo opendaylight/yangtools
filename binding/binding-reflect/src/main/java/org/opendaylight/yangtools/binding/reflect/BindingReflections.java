b066a9e6488fb6d95a5e50dc2d25ff9df1227f7c
Robert Varga <robert.varga@pantheon.tech>
2026-04-10 13:40:51 +0200

    Remove BindingReflections.findQName()
    There are no users of this method: remove it, leaving BindingReflections
    a holder of just two methods.
    
    JIRA: YANGTOOLS-1847
    Change-Id: I6578d732c071f6117b05c1e21e8f33c6561f3b96
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-reflect/src/main/java/org/opendaylight/yangtools/binding/reflect/BindingReflections.java
+++ binding/binding-reflect/src/main/java/org/opendaylight/yangtools/binding/reflect/BindingReflections.java
@@ -7,81 +7,18 @@
  */
 package org.opendaylight.yangtools.binding.reflect;
 
-import static com.google.common.base.Preconditions.checkArgument;
-
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import java.lang.reflect.Field;
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
-import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.binding.Augmentation;
 import org.opendaylight.yangtools.binding.ChildOf;
 import org.opendaylight.yangtools.binding.DataContainer;
 import org.opendaylight.yangtools.binding.DataObject;
 import org.opendaylight.yangtools.binding.Notification;
 import org.opendaylight.yangtools.binding.contract.Naming;
-import org.opendaylight.yangtools.binding.meta.YangModuleInfo;
-import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.common.YangConstants;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Deprecated(since = "14.0.16", forRemoval = true)
 public final class BindingReflections {
-    private static final Logger LOG = LoggerFactory.getLogger(BindingReflections.class);
-    private static final LoadingCache<Class<?>, Optional<QName>> CLASS_TO_QNAME = CacheBuilder.newBuilder()
-            .weakKeys()
-            .expireAfterAccess(60, TimeUnit.SECONDS)
-            .build(new ClassToQNameLoader());
-
     private BindingReflections() {
         // Hidden on purpose
-    }
-
-    /**
-     * Returns a QName associated to supplied type.
-     *
-     * @param dataType Data type class
-     * @return QName associated to supplied dataType. If dataType is Augmentation method does not return canonical
-     *         QName, but QName with correct namespace revision, but virtual local name, since augmentations do not
-     *         have name. May return null if QName is not present.
-     */
-    public static QName findQName(final Class<?> dataType) {
-        return CLASS_TO_QNAME.getUnchecked(dataType).orElse(null);
-    }
-
-    /**
-     * Returns instance of {@link YangModuleInfo} of declaring model for specific class.
-     *
-     * @param cls data object class
-     * @return Instance of {@link YangModuleInfo} associated with model, from which this class was derived.
-     */
-    private static @NonNull YangModuleInfo getModuleInfo(final Class<?> cls) {
-        final var potentialClassName = Naming.rootToServicePackageName(cls.getPackage().getName()) + '.'
-            + Naming.MODULE_INFO_CLASS_NAME;
-        final Class<?> moduleInfoClass;
-        try {
-            moduleInfoClass = cls.getClassLoader().loadClass(potentialClassName);
-        } catch (ClassNotFoundException e) {
-            throw new IllegalStateException("Failed to load " + potentialClassName, e);
-        }
-
-        final Object instanceObj;
-        try {
-            instanceObj = moduleInfoClass.getDeclaredField(Naming.MODULE_INFO_INSTANCE_FIELD_NAME).get(null);
-        } catch (IllegalAccessException e) {
-            throw new IllegalStateException("Failed to read instance field in " + moduleInfoClass, e);
-        } catch (NoSuchFieldException e) {
-            throw new IllegalStateException("Failed to find instance field in " + moduleInfoClass, e);
-        }
-
-        if (!(instanceObj instanceof YangModuleInfo moduleInfo)) {
-            throw new IllegalStateException("Unexpected instance " + instanceObj);
-        }
-        return moduleInfo;
     }
 
     /**
@@ -111,84 +48,5 @@ public final class BindingReflections {
                 && !ChildOf.class.isAssignableFrom(targetType)
                 && !Notification.class.isAssignableFrom(targetType)
                 && (targetType.getName().endsWith("Input") || targetType.getName().endsWith("Output"));
-    }
-
-    private static final class ClassToQNameLoader extends CacheLoader<Class<?>, Optional<QName>> {
-
-        @Override
-        public Optional<QName> load(@SuppressWarnings("NullableProblems") final Class<?> key) throws Exception {
-            return resolveQNameNoCache(key);
-        }
-
-        /**
-         * Tries to resolve QName for supplied class. Looks up for static field with name from constant
-         * {@link Naming#QNAME_STATIC_FIELD_NAME} and returns value if present. If field is not present uses
-         * {@link #computeQName(Class)} to compute QName for missing types.
-         */
-        private static Optional<QName> resolveQNameNoCache(final Class<?> key) {
-            try {
-                final Field field;
-                try {
-                    field = key.getField(Naming.QNAME_STATIC_FIELD_NAME);
-                } catch (NoSuchFieldException e) {
-                    LOG.debug("{} does not have a {} field, falling back to computation", key,
-                        Naming.QNAME_STATIC_FIELD_NAME, e);
-                    return Optional.of(computeQName(key));
-                }
-
-                final Object obj = field.get(null);
-                if (obj instanceof QName qname) {
-                    return Optional.of(qname);
-                }
-            } catch (SecurityException | IllegalArgumentException | IllegalAccessException e) {
-                /*
-                 * It is safe to log this this exception on debug, since this method should not fail. Only failures are
-                 * possible if the runtime / backing is inconsistent.
-                 */
-                LOG.debug("Unexpected exception during extracting QName for {}", key, e);
-            }
-            return Optional.empty();
-        }
-
-        /**
-         * Computes QName for supplied class. Namespace and revision are same as {@link YangModuleInfo} associated with
-         * supplied class.
-         *
-         * <p>If class is
-         * <ul>
-         * <li>rpc input: local name is "input".
-         * <li>rpc output: local name is "output".
-         * <li>augmentation: local name is "module name".
-         * </ul>
-         *
-         * <p>There is also fallback, if it is not possible to compute QName using following algorithm returns module
-         * QName.
-         *
-         * @throws IllegalStateException If YangModuleInfo could not be resolved
-         * @throws IllegalArgumentException If supplied class was not derived from YANG model.
-         */
-        // FIXME: Extend this algorithm to also provide QName for YANG modeled simple types.
-        @SuppressWarnings({ "rawtypes", "unchecked" })
-        private static QName computeQName(final Class key) {
-            checkArgument(isBindingClass(key), "Supplied class %s is not derived from YANG.", key);
-
-            final QName module = getModuleInfo(key).getName();
-            if (Augmentation.class.isAssignableFrom(key)) {
-                return module;
-            }
-            if (isRpcType(key)) {
-                final String className = key.getSimpleName();
-                if (className.endsWith(Naming.RPC_OUTPUT_SUFFIX)) {
-                    return YangConstants.operationOutputQName(module.getModule()).intern();
-                }
-
-                return YangConstants.operationInputQName(module.getModule()).intern();
-            }
-
-            /*
-             * Fallback for Binding types which do not have QNAME field
-             */
-            return module;
-        }
     }
 }
