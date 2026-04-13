50ada55cb8e2e6196ae5b71e4f4e19d47a7eb0b2
Robert Varga <robert.varga@pantheon.tech>
2026-04-13 09:08:59 +0200

    WIP: Add binding.lib.ToStringBuilder
    // FIXME: add test vectors and tests
    // FIXME: add documentation
    // FIXME: add issue
    // FIXME: finish conversion
    
    Change-Id: Idcc17f4113081355e818de068776436a9aa715ef
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-spec/src/main/java/org/opendaylight/yangtools/binding/lib/CodeHelpers.java
+++ binding/binding-spec/src/main/java/org/opendaylight/yangtools/binding/lib/CodeHelpers.java
@@ -11,7 +11,6 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Verify.verify;
 import static java.util.Objects.requireNonNull;
 
-import com.google.common.base.MoreObjects.ToStringHelper;
 import com.google.common.base.VerifyException;
 import java.util.Arrays;
 import java.util.Collection;
@@ -44,6 +43,11 @@ import org.opendaylight.yangtools.yang.common.Empty;
 public final class CodeHelpers {
     private CodeHelpers() {
         // Hidden
+    }
+
+    @NonNullByDefault
+    public static ToStringBuilder toStringBuilder(final String className) {
+        return new ToStringBuilder(className);
     }
 
     /**
@@ -114,66 +118,6 @@ public final class CodeHelpers {
         final var ret = requireValue(value);
         checkScale(ret, expectedScale);
         return ret;
-    }
-
-    /**
-     * Append a {@code bits} individual value. If the value is {@code false}, this method does nothing.
-     *
-     * @param helper Helper to append to
-     * @param name Name of the bit
-     * @param value Value to append
-     * @throws NullPointerException if any argument is {@code null}
-     */
-    public static void appendBit(final ToStringHelper helper, final @NonNull String name, final boolean value) {
-        if (value) {
-            helper.addValue(name);
-        }
-    }
-
-    /**
-     * Append a named value to a ToStringHelper. If the value is {@code null}, this method does nothing.
-     *
-     * @param helper Helper to append to
-     * @param name Name of the value
-     * @param value Value to append
-     * @throws NullPointerException if the name or helper is {@code null}
-     */
-    public static void appendValue(final ToStringHelper helper, final @NonNull String name,
-            final @Nullable Object value) {
-        if (value != null) {
-            helper.add(name, value);
-        }
-    }
-
-    /**
-     * Append a named value to a ToStringHelper. If the value is {@code null}, this method does nothing.
-     *
-     * @param helper Helper to append to
-     * @param name Name of the value
-     * @param value Value to append
-     * @throws NullPointerException if the name or helper is {@code null}
-     */
-    public static void appendValue(final ToStringHelper helper, final String name, final byte[] value) {
-        if (value != null) {
-            helper.add(name, HexFormat.of().formatHex(value));
-        }
-    }
-
-    /**
-     * Append augmentation map of an Augmentable to a ToStringHelper. If augmentations are {@code null} or empty, this
-     * method does nothing.
-     *
-     * @param helper Helper to append to
-     * @param name Name of the augmentation value
-     * @param augmentable Augmentable object to
-     * @throws NullPointerException if any argument is {@code null}
-     */
-    public static void appendAugmentations(final ToStringHelper helper, final String name,
-            final Augmentable<?> augmentable) {
-        final var augments = augmentable.augmentations();
-        if (!augments.isEmpty()) {
-            helper.add(name, augments.values());
-        }
     }
 
     /**
