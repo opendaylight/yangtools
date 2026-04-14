92d8bc4af124e7268e4ddd72814a927fd16fd55e
Robert Varga <robert.varga@pantheon.tech>
2026-04-14 13:36:58 +0200

    WIP: Refactor AbstractDataContainer hashCode cache
    // FIXME: describe
    // FIXME: add tests
    
    JIRA: YANGTOOLS-1864
    Change-Id: I32edb58f15393a066126e732137d8041c3515536
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-spec/src/main/java/org/opendaylight/yangtools/binding/lib/AbstractDataContainer.java
+++ binding/binding-spec/src/main/java/org/opendaylight/yangtools/binding/lib/AbstractDataContainer.java
@@ -7,7 +7,16 @@
  */
 package org.opendaylight.yangtools.binding.lib;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+import java.time.Duration;
+import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yangtools.binding.DataContainer;
+import org.opendaylight.yangtools.yang.common.Empty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Abstract base class for {@link DataContainer} implementations. It implements {@link #hashCode()} caching and common
@@ -18,20 +27,73 @@ import org.opendaylight.yangtools.binding.DataContainer;
  */
 public abstract class AbstractDataContainer<T extends DataContainer & JavaDataContainer<T>>
         implements JavaDataContainer<T> {
-    // TODO: single field when hashCode() is defined to be != 0
-    private int hashCode;
-    private volatile boolean hashCodeValid;
+    private static final class InconsistentHashCode {
+        static final @NonNull Cache<Class<?>, Empty> DAMPENED =
+            CacheBuilder.newBuilder().weakKeys().expireAfterWrite(DAMPEN_DURATION).build();
+    }
+
+    private static final class ZeroHashCode {
+        static final @NonNull Cache<Class<?>, Empty> DAMPENED =
+            CacheBuilder.newBuilder().weakKeys().expireAfterWrite(DAMPEN_DURATION).build();
+    }
+
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractDataContainer.class);
+    private static final Duration DAMPEN_DURATION = Duration.ofHours(1);
+    private static final VarHandle VH;
+
+    static {
+        try {
+            VH = MethodHandles.lookup().findVarHandle(AbstractDataContainer.class, "hashCode", int.class);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    @SuppressWarnings("unused")
+    private volatile int hashCode;
 
     @Override
     public final int hashCode() {
-        return hashCodeValid ? hashCode : loadHashCode();
+        final var local = (int) VH.getAcquire(this);
+        return local != 0 ? local : loadHashCode();
     }
 
     private int loadHashCode() {
         final var result = bindingHashCode();
-        hashCode = result;
-        hashCodeValid = true;
-        return result;
+        if (result == 0) {
+            warnZeroHashCode();
+        }
+        final var witness = (int) VH.compareAndExchangeRelease(this, 0, result);
+        return witness == 0 ? result : reconcileHashCode(witness, result);
+    }
+
+    private int reconcileHashCode(final int stored, final int computed) {
+        return stored == computed ? computed : warnInconsistentHashCode(stored, computed);
+    }
+
+    private int warnInconsistentHashCode(final int stored, final int computed) {
+        final var clazz = getClass();
+        if (recordWarning(InconsistentHashCode.DAMPENED, clazz)) {
+            LOG.warn("{} produces inconsistent bindingHashCode(): stored {} computed {}",
+                clazz.getCanonicalName(), Integer.toHexString(stored), Integer.toHexString(computed), new Throwable());
+        }
+        return stored;
+    }
+
+    private void warnZeroHashCode() {
+        final var clazz = getClass();
+        if (recordWarning(ZeroHashCode.DAMPENED, clazz)) {
+            LOG.warn("{} produces indingHashCode() == 0, violating the contract",
+                clazz.getCanonicalName(), new Throwable());
+        }
+    }
+
+    private static boolean recordWarning(final @NonNull Cache<Class<?>, Empty> cache, final @NonNull Class<?> clazz) {
+        if (cache.getIfPresent(clazz) != null) {
+            return false;
+        }
+        cache.put(clazz, Empty.value());
+        return true;
     }
 
     @Override
