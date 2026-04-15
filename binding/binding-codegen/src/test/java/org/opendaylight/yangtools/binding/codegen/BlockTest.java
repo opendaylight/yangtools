17fde92704675faaf3df792586eaa0b1f98a2022
Robert Varga <robert.varga@pantheon.tech>
2026-04-16 00:06:27 +0200

    Disconnect Block from BlockBuilder
    Expand coverage of Block.ofLines() methods and fix the discovered
    methods by ditching BlockBuilder and using a plain StringBuilder --
    improving efficiency in the process of doing so.
    
    JIRA: YANGTOOLS-1865
    Change-Id: I63ed4f9a3b007020d48602cefda6788bf9420a70
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 50caead1ae5dd8e9dbd080cd16e34f236164db55)

--- binding/binding-codegen/src/test/java/org/opendaylight/yangtools/binding/codegen/BlockTest.java
+++ binding/binding-codegen/src/test/java/org/opendaylight/yangtools/binding/codegen/BlockTest.java
@@ -8,7 +8,11 @@
 package org.opendaylight.yangtools.binding.codegen;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.google.common.base.VerifyException;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class BlockTest {
@@ -24,5 +28,62 @@ class BlockTest {
         final var bb = Block.builder();
         Block2.EMPTY.appendTo(bb);
         assertEquals("\n\n", bb.toRawString());
+    }
+
+    @Test
+    void ofLineEmpty() {
+        assertSame(Block1.EMPTY, Block.ofLine(""));
+    }
+
+    @Test
+    void ofLineFoo() {
+        assertEquals(new Block1("foo"), Block.ofLine("foo"));
+    }
+
+    @Test
+    void ofLinesEmptyEmpty() {
+        assertSame(Block2.EMPTY, Block.ofLines("", ""));
+    }
+
+    @Test
+    void ofLinesEmptyEmptyArrayEmpty() {
+        assertSame(Block2.EMPTY, Block.ofLines("", "", new String[0]));
+    }
+
+    @Test
+    void ofLinesEmptyFoo() {
+        assertEquals(new Block2("\nfoo", 0), Block.ofLines("", "foo"));
+        assertEquals(new Block2("foo\n", 3), Block.ofLines("foo", ""));
+    }
+
+    @Test
+    void ofLinesEmptyEmptyEmpty() {
+        assertEquals(new BlockN("\n\n"), Block.ofLines("", "", ""));
+    }
+
+    @Test
+    void ofLinesEmptyList() {
+        final var ex = assertThrows(VerifyException.class, () -> Block.ofLines(List.of()));
+        assertEquals("no lines", ex.getMessage());
+    }
+
+    @Test
+    void ofLinesListEmpty() {
+        assertSame(Block1.EMPTY, Block.ofLines(List.of("")));
+    }
+
+    @Test
+    void ofLinesListEmptyEmpty() {
+        assertSame(Block2.EMPTY, Block.ofLines(List.of("", "")));
+    }
+
+    @Test
+    void ofLinesListEmptyEmptyEmpty() {
+        assertEquals(new BlockN("\n\n"), Block.ofLines(List.of("", "", "")));
+    }
+
+    @Test
+    void ofLinesList() {
+        assertEquals(new BlockN("foo\nbar\nbaz"), Block.ofLines(List.of("foo", "bar", "baz")));
     }
 }
