81d243d6d50b6feeece9c06cb48680bf3fd43047
Robert Varga <robert.varga@pantheon.tech>
2026-04-16 12:36:32 +0200

    Inline sumPropHashCodes()
    Improve summing functions to remove data dependencies during
    multiplication for the fixed-size variants.
    
    JIRA: YANGTOOLS-1862
    Change-Id: I2d131248b27bc049880c64eded153a49227f8f24
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-spec/src/main/java/org/opendaylight/yangtools/binding/lib/CodeHelpers.java
+++ binding/binding-spec/src/main/java/org/opendaylight/yangtools/binding/lib/CodeHelpers.java
@@ -978,18 +978,18 @@ public final class CodeHelpers {
 
     @NonNullByDefault
     private static int sumPropHashCodes(final int hashCode0, final int hashCode1) {
-        return 31 * (31 + hashCode0) + hashCode1;
+        return 31 * 31 + 31 * hashCode0 + hashCode1;
     }
 
     @NonNullByDefault
     private static int sumPropHashCodes(final int hashCode0, final int hashCode1, final int hashCode2) {
-        return 31 * (31 * (31 + hashCode0) + hashCode1) + hashCode2;
+        return 31 * 31 * 31 + 31 * 31 * hashCode0 + 31 * hashCode1 + hashCode2;
     }
 
     @NonNullByDefault
     private static int sumPropHashCodes(final int hashCode0, final int hashCode1, final int hashCode2,
             final int hashCode3) {
-        return 31 * (31 * (31 * (31 + hashCode0) + hashCode1) + hashCode2) + hashCode3;
+        return 31 * 31 * 31 * 31 + 31 * 31 * 31 * hashCode0 + 31 * 31 * hashCode1 + 31 * hashCode2 + hashCode3;
     }
 
     /**
