4e50117018e28c8c1b9eb1e8058a3e8dd3a04f69
Robert Varga <robert.varga@pantheon.tech>
2026-04-29 00:14:34 +0200

    Split out SerialVersionBuilder
    SerialVersionUID calculation should really live in binding-codegen. This
    patch splits it out info binding-model for reuse with
    binding-generator's SerialVersionHelper.
    
    JIRA: YANGTOOLS-1798
    Change-Id: I4cce0fc4916f9eabaa96e909be24a9e00d2ad24a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/binding-model/src/main/java/org/opendaylight/yangtools/binding/model/api/SerialVersionBuilder.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.binding.model.api;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
+import java.util.ArrayList;
+import java.util.Comparator;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Generates a {@code serialVersionUID} as per
+ * <a href="https://docs.oracle.com/en/java/javase/25/docs/specs/serialization/class.html#stream-unique-identifiers">
+ * Java Object Serialization Specification, section 4.6</a>.
+ *
+ * @since 16.0.0
+ */
+// FIXME: this should live in binding-codegen
+@Beta
+@NonNullByDefault
+public final class SerialVersionBuilder {
+    private record MethodDesc(String name, AccessModifier accessModifier) {
+        MethodDesc {
+            requireNonNull(name);
+            requireNonNull(accessModifier);
+        }
+    }
+
+    private sealed interface DigestFactory {
+
+        MessageDigest newMD();
+    }
+
+    private record CloneableFactory(MessageDigest prototype) implements DigestFactory {
+        CloneableFactory {
+            requireNonNull(prototype);
+        }
+
+        @Override
+        public MessageDigest newMD() {
+            try {
+                return (MessageDigest) prototype.clone();
+            } catch (CloneNotSupportedException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+    }
+
+    private record FallbackFactory(String algorithm, Provider provider) implements DigestFactory {
+        FallbackFactory {
+            requireNonNull(algorithm);
+            requireNonNull(provider);
+        }
+
+        @Override
+        public MessageDigest newMD() {
+            try {
+                return MessageDigest.getInstance(algorithm, provider);
+            } catch (NoSuchAlgorithmException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+    }
+
+    private static final Comparator<JavaTypeName> IFACE_COMPARATOR = Comparator.comparing(JavaTypeName::canonicalName);
+    private static final Comparator<MethodDesc> METHOD_COMPARATOR = Comparator.comparing(MethodDesc::name);
+    private static final DigestFactory DIGEST_FACTORY;
+
+    static {
+        final MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA");
+        } catch (NoSuchAlgorithmException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+        DIGEST_FACTORY = chooseFactory(md);
+    }
+
+    private static DigestFactory chooseFactory(final MessageDigest md) {
+        try {
+            md.clone();
+        } catch (CloneNotSupportedException e) {
+            final var provider = md.getProvider();
+            final var log = LoggerFactory.getLogger(SerialVersionBuilder.class);
+            if (log.isDebugEnabled()) {
+                log.warn("SHA-1 provided by {} does not support clone()", provider.getName(), e);
+            } else {
+                log.warn("SHA-1 provided by {} does not support clone()", provider.getName());
+            }
+            return new FallbackFactory(md.getAlgorithm(), provider);
+        }
+
+        return new CloneableFactory(md);
+    }
+
+    private final ArrayList<JavaTypeName> interfaces = new ArrayList<>();
+    private final ArrayList<MethodDesc> methods = new ArrayList<>();
+    private final ArrayList<String> fields = new ArrayList<>();
+    private final JavaTypeName clazz;
+
+    private boolean isAbstract = true;
+
+    public SerialVersionBuilder(final JavaTypeName clazz) {
+        this.clazz = requireNonNull(clazz);
+    }
+
+    public SerialVersionBuilder setAbstract(final boolean newAbstract) {
+        isAbstract = newAbstract;
+        return this;
+    }
+
+    public SerialVersionBuilder addField(final String name) {
+        fields.add(requireNonNull(name));
+        return this;
+    }
+
+    public SerialVersionBuilder addInterface(final JavaTypeName name) {
+        interfaces.add(requireNonNull(name));
+        return this;
+    }
+
+    public SerialVersionBuilder addMethod(final String name, final AccessModifier accessModifier) {
+        if (accessModifier != AccessModifier.PRIVATE) {
+            methods.add(new MethodDesc(name, accessModifier));
+        }
+        return this;
+    }
+
+    public long build() {
+        final var baos = new ByteArrayOutputStream();
+        try (var dos = new DataOutputStream(baos)) {
+            dos.writeUTF(clazz.simpleName());
+            // FIXME: explain magic numbers
+            dos.writeInt(isAbstract ? 3 : 7);
+
+            interfaces.sort(IFACE_COMPARATOR);
+            for (var iface : interfaces) {
+                dos.writeUTF(iface.canonicalName());
+            }
+
+            fields.sort(Comparator.naturalOrder());
+            for (var field : fields) {
+                dos.writeUTF(field);
+            }
+
+            methods.sort(METHOD_COMPARATOR);
+            for (var method : methods) {
+                dos.writeUTF(method.name);
+                dos.write(method.accessModifier.ordinal());
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+
+        final var hashBytes = DIGEST_FACTORY.newMD().digest(baos.toByteArray());
+        long hash = 0;
+        for (int i = Math.min(hashBytes.length, 8) - 1; i >= 0; i--) {
+            hash = hash << 8 | hashBytes[i] & 0xFF;
+        }
+        return hash;
+    }
+}
