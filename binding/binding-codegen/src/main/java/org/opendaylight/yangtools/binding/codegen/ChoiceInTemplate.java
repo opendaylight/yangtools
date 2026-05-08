3f9f773b9c4b9d312a0c6caf369189486cd0e741
Robert Varga <robert.varga@pantheon.tech>
2026-05-08 22:04:46 +0200

    Introduce ChoiceIn{Archetype,Template}
    ChoiceIn interfaces are extremely simplistic: introduce a dedicated
    ChoiceInArchetype and its corresponding template.
    
    JIRA: YANGTOOLS-1875
    Change-Id: Ic105679db76c4601e2f3463f2f4da0541ff86c05
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/binding-codegen/src/main/java/org/opendaylight/yangtools/binding/codegen/ChoiceInTemplate.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.binding.codegen;
+
+import static java.util.Objects.requireNonNull;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.binding.ChoiceIn;
+import org.opendaylight.yangtools.binding.model.api.ChoiceInArchetype;
+import org.opendaylight.yangtools.binding.model.api.DataRootArchetype;
+import org.opendaylight.yangtools.binding.model.api.JavaTypeName;
+
+/**
+ * Template for a {@link ChoiceIn} interface generated for a {@code choice} statement.
+ */
+@NonNullByDefault
+final class ChoiceInTemplate extends ArchetypeTemplate<ChoiceInArchetype> {
+    record Builder(ChoiceInArchetype type, DataRootArchetype root) implements Template.Builder {
+        Builder {
+            requireNonNull(type);
+            requireNonNull(root);
+        }
+
+        @Override
+        public ChoiceInTemplate build() {
+            return new ChoiceInTemplate(type, root);
+        }
+    }
+
+    private static final JavaTypeName CHOICE_IN = JavaTypeName.create(ChoiceIn.class);
+
+    private ChoiceInTemplate(final ChoiceInArchetype archetype, final DataRootArchetype root) {
+        super(GeneratedClass.of(archetype), archetype, root);
+    }
+
+    @Override
+    BlockBuilder body() {
+        final var type = archetype();
+
+        return newBodyBuilder(type.statement().toDataSchemaNode())
+            .str("public interface ").str(type.simpleName()).str(" extends ")
+                .gen(importedName(CHOICE_IN), importedName(type.choiceIn())).oB()
+                .eol("/**")
+                .eol(" * The YANG identifier of the {@code choice} represented by this class.")
+                .eol(" */")
+                .frg(qnameConstant(type))
+            .cB();
+    }
+}
---
+++ binding/binding-model/src/main/java/org/opendaylight/yangtools/binding/model/api/ChoiceInArchetype.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.binding.model.api;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.yangtools.binding.ChoiceIn;
+import org.opendaylight.yangtools.yang.model.api.stmt.ChoiceEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.IdentityEffectiveStatement;
+
+/**
+ * An {@link Archetype} for a {@link ChoiceIn} generated for an {@link ChoiceEffectiveStatement}.
+ *
+ * @param name this type's {@link JavaTypeName}}
+ * @param statement the {@link IdentityEffectiveStatement}
+ * @since 16.0.0
+ */
+@NonNullByDefault
+public record ChoiceInArchetype(
+        JavaTypeName name,
+        ChoiceEffectiveStatement statement,
+        JavaTypeName choiceIn) implements Archetype.WithQName<ChoiceEffectiveStatement> {
+    public ChoiceInArchetype {
+        requireNonNull(name);
+        requireNonNull(statement);
+        requireNonNull(choiceIn);
+    }
+
+    @Override
+    @Deprecated(forRemoval = true)
+    public List<AnnotationType> getAnnotations() {
+        return List.of();
+    }
+
+    @Override
+    @Deprecated(forRemoval = true)
+    public List<Constant> getConstantDefinitions() {
+        return List.of();
+    }
+
+    @Override
+    @Deprecated(forRemoval = true)
+    public List<GeneratedType> getEnclosedTypes() {
+        return List.of();
+    }
+
+    @Override
+    @Deprecated(forRemoval = true)
+    public List<EnumTypeObjectArchetype> getEnumerations() {
+        return List.of();
+    }
+
+    @Override
+    @Deprecated(forRemoval = true)
+    public List<Type> getImplements() {
+        return List.of();
+    }
+
+    @Override
+    @Deprecated(forRemoval = true)
+    public List<MethodSignature> getMethodDefinitions() {
+        return List.of();
+    }
+
+    @Override
+    @Deprecated(forRemoval = true)
+    public List<GeneratedProperty> getProperties() {
+        return List.of();
+    }
+
+    @Override
+    @Deprecated(forRemoval = true)
+    public boolean isAbstract() {
+        return true;
+    }
+}
