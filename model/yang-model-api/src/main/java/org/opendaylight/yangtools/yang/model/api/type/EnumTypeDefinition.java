43b052449778d52814c7731ada23c65d6651a419
Robert Varga <robert.varga@pantheon.tech>
2026-05-21 14:48:01 +0200

    Clean up EnumTypeDefinition
    Match the design to BitsTypeDefinition, as the two are very much
    equivalent.
    
    Change-Id: I8677eaecf72dc6acbd8210d462ed900a2327ea93
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/type/EnumTypeDefinition.java
+++ model/yang-model-api/src/main/java/org/opendaylight/yangtools/yang/model/api/type/EnumTypeDefinition.java
@@ -9,7 +9,7 @@ package org.opendaylight.yangtools.yang.model.api.type;
 
 import java.util.List;
 import java.util.Objects;
-import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.yangtools.yang.model.api.DocumentedNode;
 import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
@@ -17,6 +17,7 @@ import org.opendaylight.yangtools.yang.model.api.TypeDefinition;
 /**
  * Makes is possible to access to the individual enumeration values of this type.
  */
+@NonNullByDefault
 public interface EnumTypeDefinition extends TypeDefinition<EnumTypeDefinition> {
     /**
      * Contains the methods for accessing the data about the concrete enumeration item which represents {@code enum}
@@ -40,14 +41,11 @@ public interface EnumTypeDefinition extends TypeDefinition<EnumTypeDefinition> {
     }
 
     /**
-     * Returns all enumeration values.
-     *
-     * @return list of {@code EnumPair} type instances which contain the data about all individual enumeration pairs
-     *         of {@code enumeration} YANG built-in type
+     * {@return the list of individual {@link EnumPair}s, in the order of increasing {@link EnumPair#getValue()}}
      */
-    @NonNull List<EnumPair> getValues();
+    List<EnumPair> getValues();
 
-    static boolean equals(final @NonNull EnumTypeDefinition type, final @Nullable Object obj) {
+    static boolean equals(final EnumTypeDefinition type, final @Nullable Object obj) {
         if (type == obj) {
             return true;
         }
@@ -56,12 +54,12 @@ public interface EnumTypeDefinition extends TypeDefinition<EnumTypeDefinition> {
         return other != null && type.getValues().equals(other.getValues());
     }
 
-    static int hashCode(final @NonNull EnumTypeDefinition type) {
+    static int hashCode(final EnumTypeDefinition type) {
         return Objects.hash(type.getQName(), type.getBaseType(), type.getUnits().orElse(null), type.getDefaultValue(),
             type.getValues());
     }
 
-    static String toString(final @NonNull EnumTypeDefinition type) {
+    static String toString(final EnumTypeDefinition type) {
         return TypeDefinitions.toStringHelper(type).add("values", type.getValues()).toString();
     }
 }
