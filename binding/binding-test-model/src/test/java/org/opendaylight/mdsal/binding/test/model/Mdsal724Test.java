8b3d8e72352521dbdc64477d15484900b2825587
Robert Varga <robert.varga@pantheon.tech>
2026-05-25 21:40:11 +0200

    Migrate Mdsal724Test
    Use DataObjectReference instead of InstanceIdentifier in the test.
    
    JIRA: YANGTOOLS-1759
    Change-Id: I33e9a36977e8923c34e78c7df245b8395cf4332e
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-test-model/src/test/java/org/opendaylight/mdsal/binding/test/model/Mdsal724Test.java
+++ binding/binding-test-model/src/test/java/org/opendaylight/mdsal/binding/test/model/Mdsal724Test.java
@@ -10,28 +10,34 @@ package org.opendaylight.mdsal.binding.test.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.test.bi.ba.notification.rev150205.OutOfPixieDustNotification;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.md.sal.knock.knock.rev180723.KnockKnockInput;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.binding.DataObjectReference;
+import org.opendaylight.yangtools.binding.DataObjectStep;
 
 class Mdsal724Test {
     @Test
     void testNotificationInstanceIdentifier() {
-        // An InstanceIdentifier pointing at a notification, unsafe to create
         @SuppressWarnings({ "rawtypes", "unchecked" })
-        final var ex = assertThrows(IllegalArgumentException.class,
-            () -> InstanceIdentifier.create((Class) OutOfPixieDustNotification.class));
+        final DataObjectStep<?> step = DataObjectStep.of((Class) OutOfPixieDustNotification.class);
+        final var steps = List.of(step);
+
+        // A DataObjectReference pointing at a notification, unsafe to create
+        final var ex = assertThrows(IllegalArgumentException.class, () -> DataObjectReference.ofUnsafeSteps(steps));
         assertEquals("interface org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.test"
             + ".bi.ba.notification.rev150205.OutOfPixieDustNotification is not a valid path argument", ex.getMessage());
     }
 
     @Test
     void testRpcInputInstanceIdentifier() {
-        // An InstanceIdentifier pointing at a notification, unsafe to create
         @SuppressWarnings({ "rawtypes", "unchecked" })
-        final var ex = assertThrows(IllegalArgumentException.class,
-            () -> InstanceIdentifier.create((Class) KnockKnockInput.class));
+        final DataObjectStep<?> step = DataObjectStep.of((Class) KnockKnockInput.class);
+        final var steps = List.of(step);
+
+        // A DataObjectReference pointing at a notification, unsafe to create
+        final var ex = assertThrows(IllegalArgumentException.class, () -> DataObjectReference.ofUnsafeSteps(steps));
         assertEquals("interface org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.md.sal.knock.knock"
             + ".rev180723.KnockKnockInput is not a valid path argument", ex.getMessage());
     }
