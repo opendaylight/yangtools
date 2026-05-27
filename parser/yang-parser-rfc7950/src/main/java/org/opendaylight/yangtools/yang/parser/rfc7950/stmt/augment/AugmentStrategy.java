cf683a5c46c019acd1681c894f998a81caf143c9
Robert Varga <robert.varga@pantheon.tech>
2026-05-27 14:44:08 +0200

    WIP: RFC7950 augment allows non-config mandatory nodes
    // FIXME: finish up
    
    JIRA: YANGTOOLS-1890
    Change-Id: I0463b349e3f5b002f8971f1a489fd9df144b6a7b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/augment/AugmentStrategy.java
+++ parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/augment/AugmentStrategy.java
@@ -27,6 +27,7 @@ import org.opendaylight.yangtools.yang.model.api.stmt.AnyxmlStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.AugmentEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.AugmentStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.ChoiceStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.ConfigStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.ContainerStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.DataDefinitionStatement;
 import org.opendaylight.yangtools.yang.model.api.stmt.DescriptionStatement;
@@ -79,7 +80,7 @@ abstract sealed class AugmentStrategy {
 
         @Override
         boolean computeRejectMandatory(final QNameModule augmentModule, final SchemaNodeIdentifier augmentArg,
-                final Mutable<?, ?, ?> target) {
+                final StmtContext<?, ?, ?> target) {
             return false;
         }
 
@@ -91,54 +92,29 @@ abstract sealed class AugmentStrategy {
     }
 
     /**
-     * Common semantics when the {@code augment} must not add mandatory nodes anywhere.
+     * Common superclass for strategies which are potentially rejecting mandatory nodes.
      */
     @NonNullByDefault
-    private static final class Reject extends AugmentStrategy {
-        static final Reject INSTANCE = new Reject();
-
-        private Reject() {
+    private abstract static sealed class Reject extends AugmentStrategy {
+        Reject() {
             super(CopyType.ADDED_BY_AUGMENTATION);
         }
 
         @Override
-        boolean computeRejectMandatory(final QNameModule augmentModule, final SchemaNodeIdentifier augmentArg,
-                final Mutable<?, ?, ?> target) {
-            final var lastArg = augmentArg.lastNodeIdentifier();
-            verifyArgument(lastArg, target);
-
-            // targetting a node introduced by another module
-            if (!augmentModule.equals(lastArg.getModule())) {
-                return true;
-            }
-
-            // targetting a node introduced by an augment statement defined with the same home module
+        final boolean computeRejectMandatory(final QNameModule augmentModule, final SchemaNodeIdentifier augmentArg,
+                final StmtContext<?, ?, ?> target) {
+            // we are walking from target towards root, looking for a statement which implies mandatory nodes can be
+            // introduced
             final var identifiers = augmentArg.getNodeIdentifiers().reversed().iterator();
-            identifiers.next();
-
+            var arg = identifiers.next();
+            verifyArgument(arg, target);
             var current = target;
-            while (true) {
-                // if target or one of the target's ancestors from the same namespace
-                //   - is a presence container, or
-                //   - is non-mandatory list, or
-                //   - is non-mandatory choice
-                // we can terminate early as it is not a mandatory node container as per RFC6020 section 3.1
-                if (current.produces(ContainerStatement.DEF)) {
-                    if (containsPresenceSubStmt(current)) {
-                        return false;
-                    }
-                } else if (current.produces(ListStatement.DEF)) {
-                    // FIXME: YANGTOOLS-1894: this check is unstable when deviations are in play
-                    final var minElements = firstSubstatementAttributeOf(current, MinElementsStatement.DEF);
-                    if (minElements == null || minElements.matchesAll()) {
-                        return false;
-                    }
-                } else if (current.produces(ChoiceStatement.DEF)) {
-                    // FIXME: YANGTOOLS-1894: this check is unstable when deviations are in play
-                    final var mandatory = firstSubstatementAttributeOf(current, MandatoryStatement.DEF);
-                    if (mandatory == null || !mandatory) {
-                        return false;
-                    }
+
+            // first deal with the case of this augment being layered on top of a previous augmentation from the same
+            // module
+            while (augmentModule.equals(arg.getModule())) {
+                if (impliesPermitMandatory(current)) {
+                    return false;
                 }
 
                 // This could be an augmentation stacked on top of a previous augmentation from the same module, which
@@ -163,22 +139,61 @@ abstract sealed class AugmentStrategy {
                 }
 
                 final var next = identifiers.next();
-                // if current is from another module we need to perform mandatory nodes validation
-                if (!augmentModule.equals(next.getModule())) {
-                    return true;
-                }
-
                 final var parent = current.coerceParentContext();
                 if (parent instanceof RootStmtContext) {
                     throw new VerifyException("reached root " + parent + " from " + target);
                 }
                 verifyArgument(next, parent);
+                arg = next;
                 current = parent;
+            }
+
+            // we have reached a node from a module that is not the augment's home module: defer to subclass to
+            // determine the policy
+            return computeRejectMandatory(current);
+        }
+
+        abstract boolean computeRejectMandatory(StmtContext<?, ?, ?> stmt);
+
+        boolean impliesPermitMandatory(final StmtContext<?, ?, ?> stmt) {
+            // if target or one of the target's ancestors from the same namespace
+            //   - is a presence container, or
+            //   - is non-mandatory list, or
+            //   - is non-mandatory choice
+            // we can terminate early as it is not a mandatory node container as per RFC6020 section 3.1
+            if (stmt.produces(ContainerStatement.DEF)) {
+                return containsPresenceSubStmt(stmt);
+            }
+            if (stmt.produces(ListStatement.DEF)) {
+                // FIXME: YANGTOOLS-1894: this check is unstable when deviations are in play
+                final var minElements = firstSubstatementAttributeOf(stmt, MinElementsStatement.DEF);
+                return minElements == null || minElements.matchesAll();
+            }
+            if (stmt.produces(ChoiceStatement.DEF)) {
+                // FIXME: YANGTOOLS-1894: this check is unstable when deviations are in play
+                final var mandatory = firstSubstatementAttributeOf(stmt, MandatoryStatement.DEF);
+                return mandatory == null || !mandatory;
+            }
+            return false;
+        }
+
+        private static StmtContext<?, ?, ?> getParentAugmentation(final StmtContext<?, ?, ?> child) {
+            var parent = child.coerceParentContext();
+            while (!parent.produces(AugmentStatement.DEF)) {
+                parent = parent.coerceParentContext();
+            }
+            return parent;
+        }
+
+        private static void verifyArgument(final QName expected, final BoundStmtCtx<?> stmt) {
+            final var targetArg = stmt.argument();
+            if (!expected.equals(targetArg)) {
+                throw new VerifyException(stmt + " does not match " + expected);
             }
         }
 
         @Override
-        Iterator<CommonStmtCtx> mandatoryNodesOf(final StmtContext<?, ?, ?> stmt) {
+        final Iterator<CommonStmtCtx> mandatoryNodesOf(final StmtContext<?, ?, ?> stmt) {
             final var nodes = recMandatoryNodesOf(stmt);
             return nodes != null ? nodes : Collections.emptyIterator();
         }
@@ -219,6 +234,79 @@ abstract sealed class AugmentStrategy {
             }
             return null;
         }
+
+        private static boolean containsPresenceSubStmt(final StmtContext<?, ?, ?> stmtCtx) {
+            return stmtCtx.hasSubstatement(PresenceEffectiveStatement.class);
+        }
+
+        private static <A, D extends DeclaredStatement<A>> @Nullable A firstSubstatementAttributeOf(
+                final StmtContext<?, ?, ?> ctx, final StatementDefinition<A, D, ?> def) {
+            return StmtContextUtils.firstAttributeOf(ctx.allSubstatements(), def.declaredRepresentation());
+        }
+    }
+
+    /**
+     * Common semantics when the {@code augment} must not add mandatory nodes anywhere.
+     */
+    @NonNullByDefault
+    private static final class RejectAll extends Reject {
+        static final RejectAll INSTANCE = new RejectAll();
+
+        private RejectAll() {
+            // Hidden on purpose
+        }
+
+        @Override
+        boolean computeRejectMandatory(final StmtContext<?, ?, ?> stmt) {
+            // if current is from another module we need to perform mandatory nodes validation
+            return true;
+        }
+    }
+
+    /**
+     * Semantics for RFC7950 unconditional {@code augment}, which may introduce non-configuration mandatory nodes.
+     */
+    @NonNullByDefault
+    private static final class RejectNonConfig extends Reject {
+        static final RejectNonConfig INSTANCE = new RejectNonConfig();
+
+        private RejectNonConfig() {
+            // Hidden on purpose
+        }
+
+        @Override
+        boolean impliesPermitMandatory(final StmtContext<?, ?, ?> stmt) {
+            return super.impliesPermitMandatory(stmt) || isNonConfig(stmt);
+        }
+
+        @Override
+        boolean computeRejectMandatory(final StmtContext<?, ?, ?> stmt) {
+            var current = stmt;
+            while (true) {
+                if (isNonConfig(current)) {
+                    return false;
+                }
+
+                final var parent = current.coerceParentContext();
+                if (parent instanceof RootStmtContext) {
+                    return true;
+                }
+                current = parent;
+            }
+        }
+
+        private static boolean isNonConfig(final StmtContext<?, ?, ?> stmt) {
+            if (stmt.inStructure()) {
+                return true;
+            }
+            for (var sub : stmt.allSubstatements()) {
+                final var cfg = sub.tryDeclaring(ConfigStatement.class);
+                if (cfg != null && cfg.isSupportedToBuildEffective()) {
+                    return !cfg.getArgument();
+                }
+            }
+            return false;
+        }
     }
 
     private static final Logger LOG = LoggerFactory.getLogger(AugmentStrategy.class);
@@ -240,7 +328,7 @@ abstract sealed class AugmentStrategy {
      */
     @NonNullByDefault
     static final AugmentStrategy rfc6020() {
-        return Reject.INSTANCE;
+        return RejectAll.INSTANCE;
     }
 
     /**
@@ -258,8 +346,7 @@ abstract sealed class AugmentStrategy {
      */
     @NonNullByDefault
     static final AugmentStrategy unconditional() {
-        // FIXME: YANGTOOLS-1890: a dedicated instance
-        return Reject.INSTANCE;
+        return RejectNonConfig.INSTANCE;
     }
 
     static final void apply(final @NonNull AugmentStrategyResolver strategyResolver,
@@ -356,7 +443,7 @@ abstract sealed class AugmentStrategy {
 
     @NonNullByDefault
     abstract boolean computeRejectMandatory(QNameModule augmentModule, SchemaNodeIdentifier augmentArg,
-        Mutable<?, ?, ?> target);
+        StmtContext<?, ?, ?> target);
 
     /**
      * {@return all statements which is causing specified statement to be considered mandatory}
@@ -370,30 +457,5 @@ abstract sealed class AugmentStrategy {
         return new InferenceException(mandatoryNode,
             "An augment cannot add node '%s' because it is mandatory and in module different than target",
             mandatoryNode.rawArgument());
-    }
-
-    @NonNullByDefault
-    private static void verifyArgument(final QName expected, final BoundStmtCtx<?> stmt) {
-        final var targetArg = stmt.argument();
-        if (!expected.equals(targetArg)) {
-            throw new VerifyException(stmt + " does not match " + expected);
-        }
-    }
-
-    private static boolean containsPresenceSubStmt(final StmtContext<?, ?, ?> stmtCtx) {
-        return stmtCtx.hasSubstatement(PresenceEffectiveStatement.class);
-    }
-
-    private static <A, D extends DeclaredStatement<A>> @Nullable A firstSubstatementAttributeOf(
-            final StmtContext<?, ?, ?> ctx, final StatementDefinition<A, D, ?> def) {
-        return StmtContextUtils.firstAttributeOf(ctx.allSubstatements(), def.declaredRepresentation());
-    }
-
-    private static StmtContext<?, ?, ?> getParentAugmentation(final StmtContext<?, ?, ?> child) {
-        var parent = child.coerceParentContext();
-        while (!parent.produces(AugmentStatement.DEF)) {
-            parent = parent.coerceParentContext();
-        }
-        return parent;
     }
 }
