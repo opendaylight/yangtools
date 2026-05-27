956230676c3e36555b4b9642ec433f3b440fceed
Robert Varga <robert.varga@pantheon.tech>
2026-05-27 14:44:08 +0200

    RFC7950 augment allows non-config mandatory nodes
    Our initial implementation of RFC7950 unconditional augments fails to
    account for explicit 'config false' and implicitly non-configuration
    items.
    
    Here we add the baseline test suite and introduce an AugmentStrategy
    which performs the additional checks in computeRejectMandatory() -- thus
    discovering when the augment target node implies non-config context.
    
    One test case, which is introduction of config=false mandatory nodes is
    left @Disabled and will be dealt with in a follow-up patch.
    
    JIRA: YANGTOOLS-1890
    Change-Id: I0463b349e3f5b002f8971f1a489fd9df144b6a7b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/YT1890Test.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class YT1890Test extends AbstractYangTest {
+    @Test
+    void allowNonConfigAugment() {
+        // plain augments to contexts which are either ignoring config or are config=false
+        assertEffectiveModel("/bugs/YT1890/foo.yang", "/bugs/YT1890/baz.yang");
+    }
+
+    @Test
+    void disallowConfigAugment() {
+        // plain augment to an implied config=true context
+        assertInferenceExceptionMessage("/bugs/YT1890/foo.yang", "/bugs/YT1890/bar.yang")
+            .startsWith("""
+                An augment cannot add node 'bar' because it is mandatory and in module different than target [at """)
+            .endsWith("/bar.yang:9:5]");
+    }
+
+    @Test
+    void allowConfigAugmentDeviated() {
+        // fun with deviations: plain augment to a nominally config=true context, which is deviated to be config=false
+        assertEffectiveModel("/bugs/YT1890/foo.yang", "/bugs/YT1890/baz.yang", "/bugs/YT1890/xyzzy.yang");
+    }
+
+    @Test
+    @Disabled("non-config support not complete")
+    void allowConfigAugmentNonConfig() {
+        // plain augment to an implied config=true context introducing a mandatory config=false statement
+        assertEffectiveModel("/bugs/YT1890/foo.yang", "/bugs/YT1890/qux.yang");
+    }
+}
