65365f8fc0fd6d80dc86e0991a9957a1012e9851
Robert Varga <robert.varga@pantheon.tech>
2026-05-29 13:45:46 +0200

    Rework YangModuleInfo methods
    Add module(), which directly returns QNameModule. Deprecate getName() in
    favor of new name() method.
    
    Change-Id: I76dd0ca8d89d88848d4c7ae2365169a143641fe8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-codegen/src/test/java/org/opendaylight/yangtools/binding/codegen/YangModuleInfoCompilationTest.java
+++ binding/binding-codegen/src/test/java/org/opendaylight/yangtools/binding/codegen/YangModuleInfoCompilationTest.java
@@ -131,7 +131,7 @@ class YangModuleInfoCompilationTest extends BaseCompilationTest {
                 }
 
                 @Override
-                public QName getName() {
+                public QName name() {
                     return NAME;
                 }
 
@@ -158,7 +158,7 @@ class YangModuleInfoCompilationTest extends BaseCompilationTest {
                     }
 
                     @Override
-                    public QName getName() {
+                    public QName name() {
                         return NAME;
                     }
 
