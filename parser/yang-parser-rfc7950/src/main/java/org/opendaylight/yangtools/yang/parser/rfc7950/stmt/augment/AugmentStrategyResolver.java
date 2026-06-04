bcf772a733ec0c281b80dd79387ed9c7dce2cc46
Robert Varga <robert.varga@pantheon.tech>
2026-06-05 00:44:37 +0200

    Split out AugmentStrategyResolver
    We three separate classes implement StatementSupport for augment only
    because we need to specialize strategyFor() to support
    AugmentInferenceAction.
    
    Split out AugmentStrategy to host this method, which in turn allows us
    to merge the three StatementSupport classes, making things a tad easier
    to navigate.
    
    JIRA: YANGTOOLS-1890
    Change-Id: I4a400d938093b2163df79e0df807afd1afadbddf
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit bc371d803c05e7694f8f1600fa36f7eed1afad2e)

---
+++ parser/yang-parser-rfc7950/src/main/java/org/opendaylight/yangtools/yang/parser/rfc7950/stmt/augment/AugmentStrategyResolver.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.parser.rfc7950.stmt.augment;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.model.api.stmt.AugmentEffectiveStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.AugmentStatement;
+import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
+import org.opendaylight.yangtools.yang.model.api.stmt.WhenEffectiveStatement;
+import org.opendaylight.yangtools.yang.parser.spi.meta.StmtContext;
+
+/**
+ * Version-specific support for determining the {@link AugmentStrategy} for a particular statement.
+ */
+enum AugmentStrategyResolver {
+    /**
+     * Companion to {@link AugmentStatementRFC6020Support}.
+     */
+    RFC6020() {
+        @Override
+        AugmentStrategy strategyFor(
+                final StmtContext<SchemaNodeIdentifier, AugmentStatement, AugmentEffectiveStatement> stmt) {
+            return AugmentStrategy.RFC6020;
+        }
+    },
+    /**
+     * Companion to {@link AugmentStatementRFC7950Support}.
+     */
+    RFC7950() {
+        @Override
+        AugmentStrategy strategyFor(
+                final StmtContext<SchemaNodeIdentifier, AugmentStatement, AugmentEffectiveStatement> stmt) {
+            // RFC7950, page 120:
+            //    If the augmentation adds mandatory nodes (see Section 3) that
+            //    represent configuration to a target node in another module, the
+            //    augmentation MUST be made conditional with a "when" statement.
+            return stmt.hasSubstatement(WhenEffectiveStatement.class) ? AugmentStrategy.RFC7950_CONDITIONAL
+                : AugmentStrategy.RFC7950_UNCONDITIONAL;
+        }
+    };
+
+    /**
+     * {@return the {@link AugmentStrategy} to use with specified statement}
+     * @param stmt the statement
+     */
+    abstract @NonNull AugmentStrategy strategyFor(
+        @NonNull StmtContext<SchemaNodeIdentifier, AugmentStatement, AugmentEffectiveStatement> stmt);
+}
