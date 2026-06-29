77c4343f09df494a81a740277366a7375d47fb68
Robert Varga <robert.varga@pantheon.tech>
2026-06-30 01:14:01 +0200

    Refactor ResolvedSourceInfo instantiation
    ResolvedSourceBuilder knows exactly what ResolvedSourceInfo kind it is
    instantiating:
    - ensure resolve*() methods verify their expected arguments
    - refactor access subclasses to defer to specializations
    
    JIRA: YANGTOOLS-1896
    Change-Id: I59cc654dddc3f0622e20f576a1223621a27393ac
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/ResolvedSourceInfo.java
+++ parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/ResolvedSourceInfo.java
@@ -24,20 +24,34 @@ import org.opendaylight.yangtools.yang.parser.source.ResolvedDependency.Resolved
  * DTO containing all the linkage information which needs to be supplied to a RootStatementContext. This info will be
  * used to construct linkage substatements like imports, includes, belongs-to etc...
  */
+// FIXME: specialize for module/submodule
 public record ResolvedSourceInfo(
         @NonNull SourceInfoRef infoRef,
+        // TODO: rename to 'definingModule'?
         @NonNull QNameModule qnameModule,
         @NonNull List<ResolvedImport> imports,
         @NonNull List<ResolvedInclude> includes,
-        // FIXME: should never be null
-        @Nullable Unqualified prefix,
+        @NonNull Unqualified prefix,
         @Nullable ResolvedBelongsTo belongsTo) {
-
     @NonNullByDefault
     public ResolvedSourceInfo {
         requireNonNull(infoRef);
         requireNonNull(qnameModule);
         imports = List.copyOf(imports);
         includes = List.copyOf(includes);
+        requireNonNull(prefix);
+    }
+
+    static @NonNull ResolvedSourceInfo ofModule(final SourceInfoRef.@NonNull OfModule module,
+            final @NonNull List<ResolvedImport> imports, final @NonNull List<ResolvedInclude> includes) {
+        final var info = module.info();
+        return new ResolvedSourceInfo(module, info.moduleName().getModule(), imports, includes, info.prefix(), null);
+    }
+
+    static @NonNull ResolvedSourceInfo ofSubmodule(final SourceInfoRef.@NonNull OfSubmodule submodule,
+            final @NonNull ResolvedBelongsTo belongsTo, final @NonNull List<ResolvedImport> imports,
+            final @NonNull List<ResolvedInclude> includes) {
+        return new ResolvedSourceInfo(submodule, belongsTo.parentModuleQname(), imports, includes,
+            belongsTo.dependency().prefix(), belongsTo);
     }
 }
