858660790e889a612fcb26befcabfa59a13d72b3
Robert Varga <robert.varga@pantheon.tech>
2026-07-15 11:44:34 +0200

    Update ResolverSourceBuilder.resolveBelongsTo()
    There is no need to require a ResolvedSourceBuilder, a plaing
    SourceInfoRef suffices.
    
    JIRA: YANGTOOLS-1896
    Change-Id: Ib8d255345d8c0a1e5daf2001b177aa5f27f0d3ec
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/SourceLinkageResolver.java
+++ parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/SourceLinkageResolver.java
@@ -603,7 +603,7 @@ public final class SourceLinkageResolver {
             // FIXME: better message and/or better exception
             verify(belongsTo.isSatisfiedBy(parentId));
 
-            resolvedSubmodule.resolveBelongsTo(belongsTo, resolvedParent);
+            resolvedSubmodule.resolveBelongsTo(belongsTo, resolvedParent.infoRef());
         }
 
         // all other dependencies have been resolved: it is now time to deal with circular includes among submodules
