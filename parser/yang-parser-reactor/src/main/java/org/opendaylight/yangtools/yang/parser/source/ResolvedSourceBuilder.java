7093bbadfc2fd9e2f0337ca5d637ef0084e0cb68
Robert Varga <robert.varga@pantheon.tech>
2026-07-23 20:36:28 +0200

    Move ResolvedSourceBuilder.Dependencies
    These are static classes for realization of ResolvedSourceInfo.Builder
    contract: move them there, along with a bunch of other stateless
    methods.
    
    Expose their construction via Builder.dependenciesOf() and broaden
    Dependencies.resolveMissing() return type so as not to leak
    implementation internals.
    
    Change-Id: Ic0b199807380f004e1fc466f4657a163cb796d30
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/ResolvedSourceBuilder.java
+++ parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/ResolvedSourceBuilder.java
@@ -8,39 +8,30 @@
 package org.opendaylight.yangtools.yang.parser.source;
 
 import static com.google.common.base.Verify.verify;
-import static com.google.common.base.Verify.verifyNotNull;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.base.MoreObjects;
 import com.google.common.base.VerifyException;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.function.BiFunction;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.yangtools.concepts.Mutable;
-import org.opendaylight.yangtools.yang.common.Revision;
 import org.opendaylight.yangtools.yang.common.RevisionUnion;
 import org.opendaylight.yangtools.yang.common.UnresolvedQName.Unqualified;
 import org.opendaylight.yangtools.yang.common.YangVersion;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementDeclaration;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementSourceReference;
 import org.opendaylight.yangtools.yang.model.api.source.DeclarationInSource;
-import org.opendaylight.yangtools.yang.model.api.source.SourceDependency;
-import org.opendaylight.yangtools.yang.model.api.source.SourceDependency.BelongsTo;
 import org.opendaylight.yangtools.yang.model.api.source.SourceDependency.Import;
 import org.opendaylight.yangtools.yang.model.api.source.SourceDependency.Include;
-import org.opendaylight.yangtools.yang.model.api.source.SourceIdentifier;
 import org.opendaylight.yangtools.yang.model.spi.source.SourceInfo;
 import org.opendaylight.yangtools.yang.model.spi.source.SourceInfoRef;
 import org.opendaylight.yangtools.yang.parser.source.ResolvedDependency.ResolvedBelongsTo;
@@ -441,360 +432,6 @@ abstract sealed class ResolvedSourceBuilder<R extends SourceInfoRef> extends Res
         }
     }
 
-    /**
-     * A set of {@link SourceDependency} objects that need to be resolved to their corresponding
-     * {@link ResolvedSourceBuilder}. This class is meant to track {@link Import} and {@link Include}, with
-     * {@link ForSubmodule} tracking {@link BelongsTo} separately.
-     *
-     * <p>That separation allows us to assume {@code 0..N} cardinality and shared interpretation
-     * of {@link SourceDependency#revision()}: it may or may not be a wildcard.
-     *
-     * @param <D> dependency type
-     * @param <B> {@link ResolvedSourceBuilder} type
-     */
-    @NonNullByDefault
-    private abstract static sealed class Dependencies<D extends SourceDependency, B extends ResolvedSourceBuilder<?>> {
-        /**
-         * {@return an instance for the specified set of initial dependencies}
-         * @param <D> dependency type
-         * @param <B> {@link ResolvedSourceBuilder} type
-         * @param dependencies the set of dependencies
-         */
-        static final <D extends SourceDependency, B extends ResolvedSourceBuilder<?>> Dependencies<D, B> of(
-                final Set<@NonNull D> dependencies) {
-            return dependencies.isEmpty() ? NoDependencies.of() : new NeededDependencies<>(dependencies);
-        }
-
-        /**
-         * {@return the unmodifiable iterator reporting all dependencies that remain unresolved}
-         */
-        abstract Iterator<D> missing();
-
-        /**
-         * {@return an unmodifiable iterator reporting all dependencies that have been resolved}
-         */
-        abstract Iterator<B> present();
-
-        /**
-         * Resolve a currently-missing dependency with a builder.
-         *
-         * @param dependency the dependency that is missing
-         * @param target the builder to use to resolve the dependency
-         * @return replacement {@link ResolvedDependencies}, if needed
-         */
-        final @Nullable ResolvedDependencies<D, B> resolveMissing(final @NonNull D dependency,
-                final @NonNull B target) {
-            // split to keep argument checking consistent
-            return doResolveMissing(requireNonNull(dependency), requireNonNull(target));
-        }
-
-        /**
-         * Implementation of the {@link #resolveMissing(SourceDependency, ResolvedSourceBuilder)} contract. All
-         * arguments are guaranteed to be non-{@code null} by the caller.
-         *
-         * @param dependency the dependency that is missing
-         * @param target the builder to use to resolve the dependency
-         * @return replacement {@link ResolvedDependencies}, if needed
-         */
-        abstract @Nullable ResolvedDependencies<D, B> doResolveMissing(@NonNull D dependency, @NonNull B target);
-
-        /**
-         * Build a list of objects, each representing a dependency. Implementations of this method assert that all
-         * dependencies have been satisfied and reports an exception if {@code #missing().isEmpty()} is known to be
-         * {@code false}.
-         *
-         * @param <R> per-dependency result type
-         * @param function the function to turn a dependency and its corresponding builder into the result type
-         * @return a list of results
-         */
-        final <R> List<R> buildResolved(final BiFunction<D, B, R> function) {
-            return doBuildResolved(requireNonNull(function));
-        }
-
-        /**
-         * Implementation of the {@link #buildResolved(BiFunction)} contract. All arguments are guaranteed to be
-         * non-{@code null} by the caller.
-         *
-         * @param <R> per-dependency result type
-         * @param function the function to turn a dependency and its corresponding builder into the result type
-         * @return a list of results
-         */
-        abstract <R> List<R> doBuildResolved(BiFunction<D, B, R> function);
-
-        @Override
-        public abstract String toString();
-
-        // common utility for doResolveMissing() implementations
-        static final VerifyException notMissingException(final SourceDependency dependency,
-                final ResolvedSourceBuilder<?> target, final @Nullable ResolvedSourceBuilder<?> existing) {
-            return existing == null
-                // replace failed because the dependency was not specified
-                ? new VerifyException("Attempted to resolve unspecified " + dependency)
-                    // replace failed because the dependency was already resolved
-                    : new VerifyException(
-                        "Attempted to override resolution of " + dependency + " from " + existing + " to " + target);
-        }
-    }
-
-    /**
-     * An implementation of {@link Dependencies} indicating there are no dependencies.
-     *
-     * @param <D> dependency type
-     * @param <B> {@link ResolvedSourceBuilder} type
-     */
-    @NonNullByDefault
-    private static final class NoDependencies<D extends SourceDependency, B extends ResolvedSourceBuilder<?>>
-            extends Dependencies<D, B> {
-        private static final NoDependencies<?, ?> INSTANCE = new NoDependencies<>();
-
-        private NoDependencies() {
-            // hidden on purpose
-        }
-
-        @SuppressWarnings("unchecked")
-        static <D extends SourceDependency, B extends ResolvedSourceBuilder<?>> NoDependencies<D, B> of() {
-            return (NoDependencies<D, B>) INSTANCE;
-        }
-
-        @Override
-        Iterator<D> missing() {
-            return Collections.emptyIterator();
-        }
-
-        @Override
-        Iterator<B> present() {
-            return Collections.emptyIterator();
-        }
-
-        @Override
-        @Nullable ResolvedDependencies<D, B> doResolveMissing(final D dependency, final B target) {
-            throw notMissingException(dependency, target, null);
-        }
-
-        @Override
-        <R> List<R> doBuildResolved(final BiFunction<D, B, R> function) {
-            return List.of();
-        }
-
-        @Override
-        public String toString() {
-            return "NoDependencies{}";
-        }
-    }
-
-    /**
-     * An implementation of {@link Dependencies} indicating there is at least one dependency.
-     *
-     * @param <D> dependency type
-     * @param <B> {@link ResolvedSourceBuilder} type
-     */
-    @NonNullByDefault
-    private abstract static sealed class SomeDependencies<
-            D extends SourceDependency,
-            B extends ResolvedSourceBuilder<?>> extends Dependencies<D, B> {
-        @Override
-        final <R> List<R> doBuildResolved(final BiFunction<D, B, R> function) {
-            final var entries = map().entrySet();
-            final var tmp = new ArrayList<R>(entries.size());
-            for (var entry : entries) {
-                final var dependency = entry.getKey();
-                final var resolved = entry.getValue();
-                if (resolved == null) {
-                    throw new VerifyException("Unresolved dependency " + dependency);
-                }
-                tmp.add(verifyNotNull(function.apply(dependency, resolved)));
-            }
-            return List.copyOf(tmp);
-        }
-
-        abstract Map<D, ? extends @Nullable B> map();
-
-        @Override
-        public final String toString() {
-            return MoreObjects.toStringHelper(this).add("dependencies", map()).toString();
-        }
-    }
-
-    /**
-     * An implementation of {@link SomeDependencies} indicating there is at least one dependency that needs to be
-     * resolved. It should eventually settle into {@link ResolvedDependencies}
-     *
-     * @param <D> dependency type
-     * @param <B> {@link ResolvedSourceBuilder} type
-     */
-    @NonNullByDefault
-    private static final class NeededDependencies<D extends SourceDependency, B extends ResolvedSourceBuilder<?>>
-            extends SomeDependencies<D, B> {
-        /**
-         * The map of dependencies. The iteration order matches the iteration order of specified dependencies
-         * and contains {@code null} for each dependency. This allows us to enforce that each dependency is resolved
-         * exactly once (by making a {@code null} to non-{@code null} transition), at which it becomes satisfied -- thus
-         * providing a combined capability to detect
-         * <ol>
-         *   <li>attempts to resolve a dependency more than once, as well as</li>
-         *   <li>attempts to resolve a dependency that was not specified</li>
-         * </ol>
-         */
-        private final LinkedHashMap<D, @Nullable B> map;
-
-        private int missing;
-
-        /**
-         * Default constructor. Should only be called from {@link Dependencies#of(Set)}.
-         *
-         * @param dependencies the set of dependencies
-         */
-        NeededDependencies(final Set<D> dependencies) {
-            map = LinkedHashMap.newLinkedHashMap(dependencies.size());
-            for (var dependency : dependencies) {
-                map.put(requireNonNull(dependency), null);
-            }
-            missing = map.size();
-        }
-
-        @Override
-        Map<D, @Nullable B> map() {
-            return map;
-        }
-
-        @Override
-        Iterator<D> missing() {
-            return map.entrySet().stream()
-                .filter(entry -> entry.getValue() == null)
-                .map(Map.Entry::getKey)
-                .iterator();
-        }
-
-        @Override
-        Iterator<B> present() {
-            return map.values().stream().filter(Objects::nonNull).iterator();
-        }
-
-        @Override
-        @Nullable ResolvedDependencies<D, B> doResolveMissing(final D dependency, final B target) {
-            if (!map.replace(dependency, null, target)) {
-                throw notMissingException(dependency, target, map.get(dependency));
-            }
-            final var local = missing;
-            return switch (local) {
-                case 0 -> throw new VerifyException("Mismatched " + local + " and " + map);
-                case 1 -> {
-                    final var resolved = new ResolvedDependencies<>(map);
-                    missing = 0;
-                    yield resolved;
-                }
-                default -> {
-                    missing = local - 1;
-                    yield null;
-                }
-            };
-        }
-    }
-
-    /**
-     * An implementation of {@link SomeDependencies} indicating there is at least one dependency that needs to be
-     * resolved. It should eventually settle into {@link ResolvedDependencies}
-     *
-     * @param <D> dependency type
-     * @param <B> {@link ResolvedSourceBuilder} type
-     */
-    @NonNullByDefault
-    private static final class ResolvedDependencies<D extends SourceDependency, B extends ResolvedSourceBuilder<?>>
-            extends SomeDependencies<D, B> {
-        /**
-         * The map of dependencies. The iteration order matches the iteration order of specified dependencies
-         * and contains {@code null} for each dependency. This allows us to enforce that each dependency is resolved
-         * exactly once (by making a {@code null} to non-{@code null} transition), at which it becomes satisfied -- thus
-         * providing a combined capability to detect
-         * <ol>
-         *   <li>attempts to resolve a dependency more than once, as well as</li>
-         *   <li>attempts to resolve a dependency that was not specified</li>
-         * </ol>
-         */
-        private final ImmutableMap<D, B> map;
-
-        ResolvedDependencies(final LinkedHashMap<D, @Nullable B> map) {
-            final var builder = ImmutableMap.<D, B>builderWithExpectedSize(map.size());
-            for (var entry : map.entrySet()) {
-                @SuppressWarnings("null")
-                final var value = (@NonNull B) verifyNotNull(entry.getValue());
-                builder.put(entry.getKey(), value);
-            }
-            this.map = builder.build();
-        }
-
-        @Override
-        ImmutableMap<D, B> map() {
-            return map;
-        }
-
-        @Override
-        Iterator<D> missing() {
-            return Collections.emptyIterator();
-        }
-
-        @Override
-        Iterator<B> present() {
-            return map.values().iterator();
-        }
-
-        @Override
-        @Nullable ResolvedDependencies<D, B> doResolveMissing(final D dependency, final B target) {
-            throw notMissingException(dependency, target, map.get(dependency));
-        }
-    }
-
-    /**
-     * A {@link Dependencies} implementation indicating resolution has been completed and consumed. The only
-     * interaction allowed is {@link #missing()}, which always indicates nothing is missing.
-     *
-     * @param <D> dependency type
-     * @param <B> {@link ResolvedSourceBuilder} type
-     */
-    @NonNullByDefault
-    private static final class TerminalDependencies<D extends SourceDependency, B extends ResolvedSourceBuilder<?>>
-            extends Dependencies<D, B> {
-        private static final TerminalDependencies<?, ?> INSTANCE = new TerminalDependencies<>();
-
-        private TerminalDependencies() {
-            // hidden on purpose
-        }
-
-        @SuppressWarnings("unchecked")
-        static <D extends SourceDependency, B extends ResolvedSourceBuilder<?>> TerminalDependencies<D, B> of() {
-            return (TerminalDependencies<D, B>) INSTANCE;
-        }
-
-        @Override
-        Iterator<D> missing() {
-            return Collections.emptyIterator();
-        }
-
-        @Override
-        Iterator<B> present() {
-            return Collections.emptyIterator();
-        }
-
-        @Override
-        @Nullable ResolvedDependencies<D, B> doResolveMissing(final D dependency, final B target) {
-            throw uoe();
-        }
-
-        @Override
-        <R> List<@NonNull R> doBuildResolved(final BiFunction<D, B, R> function) {
-            throw uoe();
-        }
-
-        @Override
-        public String toString() {
-            return "TerminalDependencies{}";
-        }
-
-        private static UnsupportedOperationException uoe() {
-            return new UnsupportedOperationException("should never be called");
-        }
-    }
-
     // the SourceInfoRef this object is attempting to resolve
     private final @NonNull R infoRef;
 
@@ -811,42 +448,13 @@ abstract sealed class ResolvedSourceBuilder<R extends SourceInfoRef> extends Res
         this.infoRef = requireNonNull(infoRef);
 
         final var info = infoRef.info();
-        imports = Dependencies.of(info.imports());
-        includes = Dependencies.of(info.includes());
+        imports = dependenciesOf(info.imports());
+        includes = dependenciesOf(info.includes());
     }
 
-    final @NonNull R infoRef() {
+    @Override
+    final R infoRef() {
         return infoRef;
-    }
-
-    @NonNullByDefault
-    final String humanName() {
-        final var sourceId = sourceId();
-        return humanName(sourceId.name(), sourceId.revision());
-    }
-
-    @NonNullByDefault
-    static final String humanName(final Unqualified name, final @Nullable Revision revision) {
-        final var localName = name.getLocalName();
-        return revision == null ? localName : localName + "@" + revision;
-    }
-
-    final @NonNull Unqualified name() {
-        return sourceId().name();
-    }
-
-    final @Nullable Revision revision() {
-        return sourceId().revision();
-    }
-
-    final @NonNull SourceIdentifier sourceId() {
-        return sourceInfo().sourceId();
-    }
-
-    abstract @NonNull SourceInfo sourceInfo();
-
-    final @NonNull YangVersion yangVersion() {
-        return sourceInfo().yangVersion();
     }
 
     /**
@@ -974,8 +582,6 @@ abstract sealed class ResolvedSourceBuilder<R extends SourceInfoRef> extends Res
             }),
             includes.buildResolved((requirement, target) -> new ResolvedInclude(requirement, target.infoRef().ref())));
         product = result;
-        imports = TerminalDependencies.of();
-        includes = TerminalDependencies.of();
         return result;
     }
 
@@ -991,6 +597,6 @@ abstract sealed class ResolvedSourceBuilder<R extends SourceInfoRef> extends Res
 
     @Override
     public final String toString() {
-        return MoreObjects.toStringHelper(this).add("ref", infoRef).toString();
+        return MoreObjects.toStringHelper(this).add("ref", infoRef()).toString();
     }
 }
