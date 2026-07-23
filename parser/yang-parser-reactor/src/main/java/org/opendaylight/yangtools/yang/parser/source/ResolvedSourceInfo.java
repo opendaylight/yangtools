51891be173037869a4cc08c57e67cccbc1f4b2cc
Robert Varga <robert.varga@pantheon.tech>
2026-07-23 20:36:28 +0200

    Promote ResolvedSourceBuilder.Dependencies
    These are static classes for realization of ResolvedSourceInfo.Builder
    contract: split them out to a top-level DependencyLinker class, properly
    hiding their internals.
    
    We also remove TerminalDependencies, as those are related to builder
    tracking: at the end of the day, the builder should not be opinioned.
    
    Finally we enrich ResolveSourceInfo.Builder with more stateless methods.
    
    JIRA: YANGTOOLS-1896
    Change-Id: Ic0b199807380f004e1fc466f4657a163cb796d30
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/ResolvedSourceInfo.java
+++ parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/ResolvedSourceInfo.java
@@ -18,7 +18,11 @@ import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.yangtools.concepts.Immutable;
 import org.opendaylight.yangtools.concepts.Mutable;
 import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.common.Revision;
 import org.opendaylight.yangtools.yang.common.UnresolvedQName.Unqualified;
+import org.opendaylight.yangtools.yang.common.YangVersion;
+import org.opendaylight.yangtools.yang.model.api.source.SourceIdentifier;
+import org.opendaylight.yangtools.yang.model.spi.source.SourceInfo;
 import org.opendaylight.yangtools.yang.model.spi.source.SourceInfoRef;
 import org.opendaylight.yangtools.yang.model.spi.source.SourceRef;
 import org.opendaylight.yangtools.yang.parser.source.ResolvedDependency.ResolvedBelongsTo;
@@ -129,10 +133,44 @@ public abstract sealed class ResolvedSourceInfo implements Immutable {
      * A builder of {@link ResolvedSourceInfo} instances.
      */
     abstract static sealed class Builder implements Mutable permits ResolvedSourceBuilder {
+
+        final String humanName() {
+            final var sourceId = sourceId();
+            return humanName(sourceId.name(), sourceId.revision());
+        }
+
+        static final String humanName(final Unqualified name, final @Nullable Revision revision) {
+            final var localName = name.getLocalName();
+            return revision == null ? localName : localName + "@" + revision;
+        }
+
+        final Unqualified name() {
+            return sourceId().name();
+        }
+
+        final @Nullable Revision revision() {
+            return sourceId().revision();
+        }
+
+        final SourceIdentifier sourceId() {
+            return sourceInfo().sourceId();
+        }
+
+        final YangVersion yangVersion() {
+            return sourceInfo().yangVersion();
+        }
+
+        abstract SourceInfoRef infoRef();
+
+        abstract SourceInfo sourceInfo();
+
         /**
          * {@return the {@link ResolvedSourceInfo} result of this builder}
          */
         abstract ResolvedSourceInfo build();
+
+        @Override
+        public abstract String toString();
     }
 
     private final List<ResolvedImport> imports;
