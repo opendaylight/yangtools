e7d2c48c461afc7d4c296db3c913881f1658d574
Robert Varga <robert.varga@pantheon.tech>
2026-07-23 15:37:37 +0200

    Allow multiple submodules to be resolved
    This adds a simple test for having two modules include two submodules
    with same name+revision combination.
    
    The test happens to use no revisions, hence it flushes out a problem
    with SourceLinkageResolver's include narrowing logic in case two or more
    distinct modules include the same submodule name without revision --
    and fix that problem.
    
    JIRA: YANGTOOLS-1355
    Change-Id: I67561ad9a995ea945c1076e1d9dcf60206e20ba1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/YT1355Test.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.opendaylight.yangtools.yang.common.UnresolvedQName.Unqualified;
+
+class YT1355Test extends AbstractYangTest {
+    @Test
+    void multipleSubmodulesCanBeResolved() {
+        final var modules = assertEffectiveModel(
+            "/bugs/YT1355/foo/foo.yang", "/bugs/YT1355/foo/xyzzy.yang",
+            "/bugs/YT1355/bar/bar.yang", "/bugs/YT1355/bar/xyzzy.yang").getModuleStatements();
+        assertEquals(2, modules.size());
+
+        for (var module : modules.values()) {
+            final var submodules = module.submodules();
+            assertEquals(1, submodules.size());
+            final var submodule = submodules.iterator().next();
+            assertEquals(Unqualified.of("xyzzy"), submodule.argument());
+            final var belongsTo = submodule.belongsTo();
+            assertEquals(module.argument(), belongsTo.argument());
+            assertEquals(module.prefixArgument(), belongsTo.prefixArgument());
+        }
+    }
+}
