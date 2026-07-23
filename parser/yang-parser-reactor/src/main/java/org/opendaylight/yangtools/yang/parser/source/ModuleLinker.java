6ed354a030e126ef72fdfd9ebcea3a630a97114c
Robert Varga <robert.varga@pantheon.tech>
2026-07-23 23:21:27 +0200

    Split out {Module,Submodule}Builder
    Promote common methods specialized for module/submodule into their own
    interfaces, allowing us to make Builder a more useful interface.
    
    JIRA: YANGTOOLS-1896
    Change-Id: I00fb12a21a797c708fc79845df0fa9d50bd0ed20
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/ModuleLinker.java
+++ parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/ModuleLinker.java
@@ -25,10 +25,10 @@ import org.opendaylight.yangtools.yang.model.api.meta.StatementDeclaration;
 import org.opendaylight.yangtools.yang.model.api.meta.StatementSourceReference;
 import org.opendaylight.yangtools.yang.model.api.source.DeclarationInSource;
 import org.opendaylight.yangtools.yang.model.api.source.SourceDependency.Include;
-import org.opendaylight.yangtools.yang.model.spi.source.SourceInfo;
 import org.opendaylight.yangtools.yang.model.spi.source.SourceInfoRef;
 import org.opendaylight.yangtools.yang.parser.source.ResolvedDependency.ResolvedImport;
 import org.opendaylight.yangtools.yang.parser.source.ResolvedDependency.ResolvedInclude;
+import org.opendaylight.yangtools.yang.parser.source.ResolvedSourceInfo.ModuleBuilder;
 import org.opendaylight.yangtools.yang.parser.spi.meta.InferenceException;
 import org.opendaylight.yangtools.yang.parser.spi.meta.ModelProcessingPhase;
 import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
@@ -40,7 +40,7 @@ import org.opendaylight.yangtools.yang.parser.spi.meta.SomeModifiersUnresolvedEx
  * requirement that {@code Multiple revisions of the same submodule MUST NOT be included.} are reliably reported.
  */
 @NonNullByDefault
-final class ModuleLinker extends SourceLinker<SourceInfoRef.OfModule> {
+final class ModuleLinker extends SourceLinker<SourceInfoRef.OfModule, ResolvedModuleInfo> implements ModuleBuilder {
     /**
      * The source of an {@link ExactRevision}.
      */
@@ -230,7 +230,7 @@ final class ModuleLinker extends SourceLinker<SourceInfoRef.OfModule> {
      * @param source the {@link SourceLinker} to the source of requirements
      * @throws ReactorException if a requirement conflicts with a previous requirement
      */
-    void requireIncludes(final SourceLinker<?> source) throws ReactorException {
+    void requireIncludes(final SourceLinker<?, ?> source) throws ReactorException {
         final var it = source.missingIncludes();
         while (it.hasNext()) {
             requireInclude(source, it.next());
@@ -244,7 +244,8 @@ final class ModuleLinker extends SourceLinker<SourceInfoRef.OfModule> {
      * @param dependency the {@link Include}
      * @throws ReactorException if the requirement conflicts with a previous requirement or cannot be added
      */
-    private void requireInclude(final SourceLinker<?> source, final Include dependency) throws ReactorException {
+    private void requireInclude(final SourceLinker<?, ?> source, final Include dependency)
+            throws ReactorException {
         final var name = dependency.name();
         final var revision = dependency.revision();
         if (revision == null) {
@@ -284,25 +285,6 @@ final class ModuleLinker extends SourceLinker<SourceInfoRef.OfModule> {
         }
     }
 
-    @Override
-    SourceInfo.Module sourceInfo() {
-        return infoRef().info();
-    }
-
-    @Override
-    ResolvedModuleInfo doBuild(final List<ResolvedImport> imports, final List<ResolvedInclude> includes) {
-        return new ResolvedModuleInfo(infoRef(), imports, includes);
-    }
-
-    /**
-     * {@return the reference to the first include-without-revision of specified submodule or this module}
-     * @param name submodule name
-     */
-    StatementSourceReference includeRefOf(final Unqualified name) {
-        return submoduleSpecs.get(requireNonNull(name)) instanceof AnyRevision spec ? spec.sourceRef
-            : sourceId().toReference();
-    }
-
     /**
      * Check that a source can add an {@link Include} dependency to this module.
      *
@@ -310,7 +292,8 @@ final class ModuleLinker extends SourceLinker<SourceInfoRef.OfModule> {
      * @param dependency the dependency being resolved
      * @throws ReactorException if the source cannot be add the dependency to this module
      */
-    private void checkInclude(final SourceLinker<?> source, final Include dependency) throws ReactorException {
+    private void checkInclude(final SourceLinker<?, ?> source, final Include dependency)
+                throws ReactorException {
         switch (source) {
             case ModuleLinker module -> verify(module == this);
             case SubmoduleLinker submodule -> {
@@ -325,5 +308,19 @@ final class ModuleLinker extends SourceLinker<SourceInfoRef.OfModule> {
                 }
             }
         }
+    }
+
+    @Override
+    ResolvedModuleInfo doBuild(final List<ResolvedImport> imports, final List<ResolvedInclude> includes) {
+        return new ResolvedModuleInfo(infoRef(), imports, includes);
+    }
+
+    /**
+     * {@return the reference to the first include-without-revision of specified submodule or this module}
+     * @param name submodule name
+     */
+    StatementSourceReference includeRefOf(final Unqualified name) {
+        return submoduleSpecs.get(requireNonNull(name)) instanceof AnyRevision(var sourceRef) ? sourceRef
+            : sourceId().toReference();
     }
 }
