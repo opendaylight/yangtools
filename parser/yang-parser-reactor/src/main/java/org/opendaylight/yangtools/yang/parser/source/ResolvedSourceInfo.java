298bba742497003b52d2ecd87e14dacd783ea586
Robert Varga <robert.varga@pantheon.tech>
2026-07-23 18:19:33 +0200

    Add ResolvedSourceInfo.Builder
    Extract ResolvedSourceBuilder.build() contract into
    ResolvedSourceInfo.Builder, adjusting naming a bit and promoting
    SourceInfoRef-based methods as well.
    
    JIRA: YANGTOOLS-1896
    Change-Id: Ib609e666b9e2afa1b8a960905147710d83fd8589
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/ResolvedSourceInfo.java
+++ parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/ResolvedSourceInfo.java
@@ -15,8 +15,14 @@ import com.google.common.base.VerifyException;
 import java.util.List;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.yangtools.concepts.Immutable;
+import org.opendaylight.yangtools.concepts.Mutable;
 import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.common.Revision;
 import org.opendaylight.yangtools.yang.common.UnresolvedQName.Unqualified;
+import org.opendaylight.yangtools.yang.common.YangVersion;
+import org.opendaylight.yangtools.yang.model.api.source.SourceIdentifier;
+import org.opendaylight.yangtools.yang.model.spi.source.SourceInfo;
 import org.opendaylight.yangtools.yang.model.spi.source.SourceInfoRef;
 import org.opendaylight.yangtools.yang.model.spi.source.SourceRef;
 import org.opendaylight.yangtools.yang.parser.source.ResolvedDependency.ResolvedBelongsTo;
@@ -28,7 +34,7 @@ import org.opendaylight.yangtools.yang.parser.source.ResolvedDependency.Resolved
  * used to construct linkage substatements like imports, includes, belongs-to etc...
 */
 @NonNullByDefault
-public abstract sealed class ResolvedSourceInfo {
+public abstract sealed class ResolvedSourceInfo implements Immutable {
     /**
      * A {@link ResolvedSourceInfo} for a {@link SourceInfoRef.OfModule}.
      */
@@ -121,6 +127,50 @@ public abstract sealed class ResolvedSourceInfo {
         ToStringHelper addToStringAttributes(final ToStringHelper helper) {
             return super.addToStringAttributes(helper).add("belongsTo", belongsTo);
         }
+    }
+
+    /**
+     * A builder of {@link ResolvedSourceInfo} instances.
+     */
+    abstract static sealed class Builder implements Mutable permits ResolvedSourceBuilder {
+
+        final String humanName() {
+            final var sourceId = sourceId();
+            return humanName(sourceId.name(), sourceId.revision());
+        }
+
+        static final String humanName(final Unqualified name, final @Nullable Revision revision) {
+            final var localName = name.getLocalName();
+            return revision == null ? localName : localName + "@" + revision;
+        }
+
+        final Unqualified name() {
+            return sourceId().name();
+        }
+
+        final @Nullable Revision revision() {
+            return sourceId().revision();
+        }
+
+        final SourceIdentifier sourceId() {
+            return sourceInfo().sourceId();
+        }
+
+        final YangVersion yangVersion() {
+            return sourceInfo().yangVersion();
+        }
+
+        abstract SourceInfoRef infoRef();
+
+        abstract SourceInfo sourceInfo();
+
+        /**
+         * {@return the {@link ResolvedSourceInfo} result of this builder}
+         */
+        abstract ResolvedSourceInfo build();
+
+        @Override
+        public abstract String toString();
     }
 
     private final List<ResolvedImport> imports;
