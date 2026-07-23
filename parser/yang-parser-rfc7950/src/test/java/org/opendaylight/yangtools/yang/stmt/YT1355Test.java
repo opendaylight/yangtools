1572e0b613d62d80d552bd372a47430fc39617dd
Robert Varga <robert.varga@pantheon.tech>
2026-07-23 15:37:37 +0200

    WIP: Allow multiple submodules to be resolved
    This adds a simple test for having two modules include two submodules
    with same name+revision combination.
    
    The test happens to use no revisions, hence it flushes out a problem
    with SourceLinkageResolver's include narrowing
    
    // FIXME: fix the issue
    
    JIRA: YANGTOOLS-1355
    Change-Id: I67561ad9a995ea945c1076e1d9dcf60206e20ba1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/YT1355Test.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.yang.stmt;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.opendaylight.yangtools.yang.common.UnresolvedQName.Unqualified;
+import org.opendaylight.yangtools.yang.model.api.stmt.ModuleEffectiveStatement;
+
+class YT1355Test extends AbstractYangTest {
+    @Test
+    void multipleSubmodulesCanBeResolved() {
+        final var modules = assertEffectiveModel(
+            "/bugs/YT1355/cna-smfcc/cna-smfcc.yang", "/bugs/YT1355/cna-smfcc/trace.yang",
+            "/bugs/YT1355/nrf-agent/nrf-agent.yang", "/bugs/YT1355/nrf-agent/trace.yang").getModuleStatements();
+        assertEquals(2, modules.size());
+
+        for (var module : modules.values()) {
+            assertTraceSubmodule(module);
+        }
+    }
+
+    private static void assertTraceSubmodule(final ModuleEffectiveStatement module) {
+        final var submodules = module.submodules();
+        assertEquals(1, submodules.size());
+        final var submodule = submodules.iterator().next();
+        assertEquals(Unqualified.of("trace"), submodule.argument());
+        assertEquals(module.argument(), submodule.belongsTo().argument());
+    }
+}
