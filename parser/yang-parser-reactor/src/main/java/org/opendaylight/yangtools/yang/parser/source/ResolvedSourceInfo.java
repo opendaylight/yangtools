730f18c863c28dda1dcda3d2e3286f14d80e9511
Robert Varga <robert.varga@pantheon.tech>
2026-07-23 23:21:27 +0200

    Split out {Module,Submodule}Builder
    Promote common methods specialized for module/submodule into their own
    interfaces, allowing us to make Builder a more useful interface.
    
    JIRA: YANGTOOLS-1896
    Change-Id: I00fb12a21a797c708fc79845df0fa9d50bd0ed20
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/ResolvedSourceInfo.java
+++ parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/ResolvedSourceInfo.java
@@ -70,47 +70,90 @@ public abstract sealed class ResolvedSourceInfo implements Immutable permits Res
     /**
      * A builder of {@link ResolvedSourceInfo} instances.
      */
-    abstract static sealed class Builder implements Mutable permits SourceLinker {
+    public sealed interface Builder extends Mutable permits SourceLinker {
+        /**
+         * {@return the {@link SourceInfoRef} for which this builder was instantiated}
+         */
+        SourceInfoRef infoRef();
 
-        final String humanName() {
-            final var sourceId = sourceId();
-            return humanName(sourceId.name(), sourceId.revision());
-        }
+        /**
+         * {@return the equivalent of {@code infoRef().info{}}}
+         */
+        SourceInfo sourceInfo();
 
-        static final String humanName(final Unqualified name, final @Nullable Revision revision) {
-            final var localName = name.getLocalName();
-            return revision == null ? localName : localName + "@" + revision;
-        }
-
-        final Unqualified name() {
+        /**
+         * {@return the equivalent of {@code sourceId().name{}}}
+         */
+        default Unqualified name() {
             return sourceId().name();
         }
 
-        final @Nullable Revision revision() {
+        /**
+         * {@return the equivalent of {@code sourceId().revision{}}}
+         */
+        default @Nullable Revision revision() {
             return sourceId().revision();
         }
 
-        final SourceIdentifier sourceId() {
+        /**
+         * {@return a human-friendly identifier composed of {@link #name()} and {@link #revision()}}
+         */
+        String humanName();
+
+        /**
+         * {@return the equivalent of {@code sourceInfo().sourceId{}}}
+         */
+        default SourceIdentifier sourceId() {
             return sourceInfo().sourceId();
         }
 
-        final YangVersion yangVersion() {
+        /**
+         * {@return the equivalent of {@code sourceInfo().yangVersion{}}}
+         */
+        default YangVersion yangVersion() {
             return sourceInfo().yangVersion();
         }
 
         /**
-         * {@return the {@link SourceInfoRef} for which this builder was instantiated}
-         */
-        abstract SourceInfoRef infoRef();
-
-        abstract SourceInfo sourceInfo();
-
-        /**
          * {@return the {@link ResolvedSourceInfo} result of this builder}
          */
-        abstract ResolvedSourceInfo build();
+        ResolvedSourceInfo build();
+    }
+
+    /**
+     * A {@link Builder} for {@link ResolvedModuleInfo}.
+     */
+    public abstract static non-sealed class ModuleBuilder
+            extends SourceLinker<SourceInfoRef.OfModule, ResolvedModuleInfo> {
+        protected ModuleBuilder(final SourceInfoRef.OfModule infoRef) {
+            super(infoRef);
+        }
 
         @Override
-        public abstract String toString();
+        public final SourceInfo.Module sourceInfo() {
+            return infoRef().info();
+        }
+    }
+
+    /**
+     * A {@link Builder} for {@link ResolvedSubmoduleInfo}.
+     */
+    public abstract static non-sealed class SubmoduleBuilder
+            extends SourceLinker<SourceInfoRef.OfSubmodule, ResolvedSubmoduleInfo> {
+        protected SubmoduleBuilder(final SourceInfoRef.OfSubmodule infoRef) {
+            super(infoRef);
+        }
+
+        @Override
+        public final SourceInfo.Submodule sourceInfo() {
+            return infoRef().info();
+        }
+
+        /**
+         * {@return the module name specified by this submodule through {@link SourceInfo.Submodule#belongsTo()}}
+         */
+        public final Unqualified parentName() {
+            return sourceInfo().belongsTo().name();
+        }
     }
 }
