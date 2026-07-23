49218bd87bd8894b20108ede631ed6fd03c66e09
Robert Varga <robert.varga@pantheon.tech>
2026-07-23 23:21:27 +0200

    Split out {Abstract,Module,Submodule}Builder
    Promote common methods specialized for module/submodule into their own
    interfaces, allowing us to make Builder a more useful interface.
    
    Change-Id: I00fb12a21a797c708fc79845df0fa9d50bd0ed20
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/ResolvedSourceInfo.java
+++ parser/yang-parser-reactor/src/main/java/org/opendaylight/yangtools/yang/parser/source/ResolvedSourceInfo.java
@@ -87,7 +87,92 @@ public abstract sealed class ResolvedSourceInfo implements Immutable permits Res
     /**
      * A builder of {@link ResolvedSourceInfo} instances.
      */
-    abstract static sealed class Builder implements Mutable permits SourceLinker {
+    sealed interface Builder extends Mutable permits AbstractBuilder, ModuleBuilder, SubmoduleBuilder {
+        /**
+         * {@return the {@link SourceInfoRef} for which this builder was instantiated}
+         */
+        SourceInfoRef infoRef();
+
+        /**
+         * {@return the equivalent of {@code infoRef().info{}}}
+         */
+        SourceInfo sourceInfo();
+
+        /**
+         * {@return the equivalent of {@code sourceId().name{}}}
+         */
+        default Unqualified name() {
+            return sourceId().name();
+        }
+
+        /**
+         * {@return the equivalent of {@code sourceId().revision{}}}
+         */
+        default @Nullable Revision revision() {
+            return sourceId().revision();
+        }
+
+        /**
+         * {@return a human-friendly identifier composed of {@link #name()} and {@link #revision()}}
+         */
+        String humanName();
+
+        /**
+         * {@return the equivalent of {@code sourceInfo().sourceId{}}}
+         */
+        default SourceIdentifier sourceId() {
+            return sourceInfo().sourceId();
+        }
+
+        /**
+         * {@return the equivalent of {@code sourceInfo().yangVersion{}}}
+         */
+        default YangVersion yangVersion() {
+            return sourceInfo().yangVersion();
+        }
+
+        /**
+         * {@return the {@link ResolvedSourceInfo} result of this builder}
+         */
+        ResolvedSourceInfo build();
+    }
+
+    /**
+     * A {@link Builder} for {@link ResolvedModuleInfo}.
+     */
+    sealed interface ModuleBuilder extends Builder permits ModuleLinker {
+        @Override
+        SourceInfoRef.OfModule infoRef();
+
+        @Override
+        default SourceInfo.Module sourceInfo() {
+            return infoRef().info();
+        }
+
+        @Override
+        ResolvedModuleInfo build();
+    }
+
+    /**
+     * A {@link Builder} for {@link ResolvedSubmoduleInfo}.
+     */
+    sealed interface SubmoduleBuilder extends Builder permits SubmoduleLinker {
+        @Override
+        SourceInfoRef.OfSubmodule infoRef();
+
+        @Override
+        default SourceInfo.Submodule sourceInfo() {
+            return infoRef().info();
+        }
+
+        @Override
+        ResolvedSubmoduleInfo build();
+    }
+
+    /**
+     * A builder of {@link ResolvedSourceInfo} instances.
+     */
+    abstract static sealed class AbstractBuilder implements Builder permits SourceLinker {
         /**
          * A set of {@link SourceDependency} objects that need to be resolved to their corresponding {@link Builder}.
          * This class is meant to track {@link Import} and {@link Include}.
@@ -363,7 +448,8 @@ public abstract sealed class ResolvedSourceInfo implements Immutable permits Res
             }
         }
 
-        final String humanName() {
+        @Override
+        public final String humanName() {
             final var sourceId = sourceId();
             return humanName(sourceId.name(), sourceId.revision());
         }
@@ -372,34 +458,6 @@ public abstract sealed class ResolvedSourceInfo implements Immutable permits Res
             final var localName = name.getLocalName();
             return revision == null ? localName : localName + "@" + revision;
         }
-
-        final Unqualified name() {
-            return sourceId().name();
-        }
-
-        final @Nullable Revision revision() {
-            return sourceId().revision();
-        }
-
-        final SourceIdentifier sourceId() {
-            return sourceInfo().sourceId();
-        }
-
-        final YangVersion yangVersion() {
-            return sourceInfo().yangVersion();
-        }
-
-        /**
-         * {@return the {@link SourceInfoRef} for which this builder was instantiated}
-         */
-        abstract SourceInfoRef infoRef();
-
-        abstract SourceInfo sourceInfo();
-
-        /**
-         * {@return the {@link ResolvedSourceInfo} result of this builder}
-         */
-        abstract ResolvedSourceInfo build();
 
         @Override
         public abstract String toString();
