fbcec3a5b7265aa19ac53957418b821a00034c02
Robert Varga <robert.varga@pantheon.tech>
2026-07-28 22:08:09 +0200

    Refactor InterfaceGeneratorTest
    Remove mocking of LegacyArchetype and promote the test to a full codegen
    integration, asserting the actual generated content.
    
    JIRA: YANGTOOLS-1883
    Change-Id: Ieb5d985489c8899d051a2cb0d9293d4bfdbe68c9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-codegen/src/test/java/org/opendaylight/yangtools/binding/codegen/InterfaceGeneratorTest.java
+++ binding/binding-codegen/src/test/java/org/opendaylight/yangtools/binding/codegen/InterfaceGeneratorTest.java
@@ -7,113 +7,95 @@
  */
 package org.opendaylight.yangtools.binding.codegen;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.opendaylight.yangtools.binding.codegen.FileSearchUtil.getFiles;
 
-import java.util.List;
-import org.eclipse.jdt.annotation.NonNull;
-import org.eclipse.jdt.annotation.NonNullByDefault;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import org.assertj.core.api.AbstractStringAssert;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.opendaylight.yangtools.binding.model.api.DataRootArchetype;
-import org.opendaylight.yangtools.binding.model.api.DeprecatedAnnotation;
 import org.opendaylight.yangtools.binding.model.api.JavaTypeName;
-import org.opendaylight.yangtools.binding.model.api.LegacyArchetype;
-import org.opendaylight.yangtools.binding.model.api.MethodSignature;
-import org.opendaylight.yangtools.binding.model.ri.Types;
 
-class InterfaceGeneratorTest {
+class InterfaceGeneratorTest extends BaseCompilationTest {
+    private static Path SOURCES;
+    private static Map<String, Path> FILES;
+
+    @BeforeAll
+    static void beforeAll() {
+        SOURCES = CompilationTestUtils.generatorOutput("status-means-deprecated");
+        assertEquals(4, generateTestSources("/compilation/bug586", SOURCES).size());
+        FILES = getFiles(SOURCES);
+        assertEquals(17, FILES.size());
+    }
+
+    @AfterAll
+    static void afterAll() throws Exception {
+        CompilationTestUtils.cleanUp(SOURCES);
+        SOURCES = null;
+        FILES = null;
+    }
+
+    private static AbstractStringAssert<?> assertFileContent(final String name) {
+        final var file = FILES.get(name);
+        assertNotNull(file, name + " not found");
+        return assertThat(assertDoesNotThrow(() -> Files.readString(file)));
+    }
+
     private static final String TEST = "test";
     private static final JavaTypeName TYPE_NAME = JavaTypeName.create(TEST, TEST);
 
     @Test
-    void builderTemplateListenerMethodTest() {
-        final var methSign = mockMethSign("on" + TEST);
-        final var genType = mockGenType(methSign);
-
-        assertInterface("""
-            package test;
-
-            import javax.annotation.processing.Generated;
-
-            @Generated("mdsal-binding-generator")
-            public interface test {
-
-
-                void ontest();
-            }
-            """, genType);
-    }
-
-    @Test
     void builderTemplateDeprecatedListenerMethodTest() {
-        final var methSign = mockMethSign("on" + TEST);
-        doReturn(List.of(DeprecatedAnnotation.OBSOLETE)).when(methSign).getAnnotations();
+        assertFileContent("Services.java").contains("""
 
-        assertInterface("""
-            package test;
-
-            import java.lang.Deprecated;
-            import javax.annotation.processing.Generated;
-
-            @Generated("mdsal-binding-generator")
-            public interface test {
-
-
+                /**
+                 * {@return {@code Map<ServiceKey, Service>} service, or {@code null} if it is not present}
+                 */
                 @Deprecated(forRemoval = true)
-                void ontest();
-            }
-            """, mockGenType(methSign));
+                @Nullable Map<ServiceKey, Service> getService();
+
+                /**
+                 * {@return {@code Map<ServiceKey, Service>} service, or an empty list if it is not present}
+                 */
+                @Deprecated(forRemoval = true)
+                default @NonNull Map<ServiceKey, Service> nonnullService() {
+                    return CodeHelpers.nonnull(getService());
+                }
+
+            """);
+        assertFileContent("Service.java").contains("""
+             */
+            @Deprecated(forRemoval = true)
+            @Generated("mdsal-binding-generator")
+            public interface Service
+                extends ChildOf<Services>,
+                        JavaDataContainer<Service>,
+                        EntryObject<Service, ServiceKey> {
+            """);
     }
 
     @Test
     void builderTemplateGenerateObsoleteListenerMethodTest() {
-        final var methSign = mockMethSign("on" + TEST);
-        doReturn(List.of(DeprecatedAnnotation.DEPRECATED)).when(methSign).getAnnotations();
-        doReturn(true).when(methSign).isDefault();
+        assertFileContent("FooData.java").endsWith("""
 
-        assertInterface("""
-            package test;
-
-            import java.lang.Deprecated;
-            import javax.annotation.processing.Generated;
-
-            @Generated("mdsal-binding-generator")
-            public interface test {
-
-
+                /**
+                 * {@return {@code Services} services, or {@code null} if it is not present}
+                 */
                 @Deprecated
-                default void ontest() {
-                    // No-op
-                }
+                Services getServices();
+
+                /**
+                 * {@return {@code Services} services, or an empty instance if it is not present}
+                 */
+                @Deprecated
+                @NonNull Services nonnullServices();
             }
-            """, mockGenType(methSign));
-    }
-
-    private static @NonNull LegacyArchetype<?> mockGenType(final MethodSignature methSign) {
-        final var genType = spy(LegacyArchetype.class);
-        doReturn(TYPE_NAME).when(genType).name();
-        doReturn(TEST).when(genType).simpleName();
-        doReturn(TEST).when(genType).packageName();
-        doReturn(List.of(methSign)).when(genType).getMethodDefinitions();
-        doReturn(List.of()).when(genType).getImplements();
-        return genType;
-    }
-
-    private static @NonNull MethodSignature mockMethSign(final String methodeName) {
-        final var methSign = mock(MethodSignature.class);
-        doReturn(methodeName).when(methSign).getName();
-        final var methType = Types.typeForClass(void.class);
-        doReturn(methType).when(methSign).getReturnType();
-        doReturn(MethodSignature.ValueMechanics.NORMAL).when(methSign).getMechanics();
-        return methSign;
-    }
-
-    @NonNullByDefault
-    private static void assertInterface(final String expected, final LegacyArchetype<?> genType) {
-        final var sb = new StringBuilder();
-        new InterfaceTemplate.Builder(genType, mock(DataRootArchetype.class)).build().generateTo(sb);
-        assertEquals(expected, sb.toString());
+            """);
     }
 }
