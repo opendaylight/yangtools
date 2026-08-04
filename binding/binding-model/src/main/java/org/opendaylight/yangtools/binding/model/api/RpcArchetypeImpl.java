3a97088c380c2ef706b28756b4ed3449dc40b5ea
Robert Varga <robert.varga@pantheon.tech>
2026-08-04 02:20:41 +0200

    Hide RpcArchetypeImpl
    The implementation is accidentally public, make sure it does not leak.
    
    JIRA: YANGTOOLS-1578
    Change-Id: Ib4e1c663eea55e2a5c360f41e9c76eae0c9077a0
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-model/src/main/java/org/opendaylight/yangtools/binding/model/api/RpcArchetypeImpl.java
+++ binding/binding-model/src/main/java/org/opendaylight/yangtools/binding/model/api/RpcArchetypeImpl.java
@@ -11,27 +11,15 @@ import static java.util.Objects.requireNonNull;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
-import org.opendaylight.yangtools.binding.Rpc;
-import org.opendaylight.yangtools.binding.RpcInput;
-import org.opendaylight.yangtools.binding.RpcOutput;
 import org.opendaylight.yangtools.yang.model.api.stmt.RpcEffectiveStatement;
 
-/**
- * An {@link Archetype} for a {@link Rpc} generated for an {@link RpcEffectiveStatement}.
- *
- * @param name this type's {@link JavaTypeName}}
- * @param statement the {@link RpcEffectiveStatement}
- * @param input this {@link JavaTypeName}} of the corresponding {@link RpcInput} interface
- * @param output this {@link JavaTypeName}} of the corresponding {@link RpcOutput} interface
- * @since 16.0.0
- */
 @NonNullByDefault
-public record RpcArchetypeImpl(
+record RpcArchetypeImpl(
         JavaTypeName name,
         RpcEffectiveStatement statement,
         RpcInputArchetype input,
         RpcOutputArchetype output) implements RpcArchetype {
-    public RpcArchetypeImpl {
+    RpcArchetypeImpl {
         requireNonNull(name);
         requireNonNull(statement);
         requireNonNull(input);
