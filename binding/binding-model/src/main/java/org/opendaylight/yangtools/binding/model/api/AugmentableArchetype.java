1ecea458cc8b8bd4f14de6534d2d957ff8fe33f0
Robert Varga <robert.varga@pantheon.tech>
2026-08-04 02:53:05 +0200

    WIP: Add AugmentableArchetype.augmentations()
    It is not obvious, but an Augmentable needs to link all its
    augmentations for the purposes of navigating the tree in relationship
    to the EffectiveStatement tree.
    
    This having this ability completes the toolkit we need to be able to
    make RuntimeType instances transient while maintaining full schema tree
    navigability.
    
    Change-Id: Iac367f98841e43e78f2ffe413c8eeccc24585984
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-model/src/main/java/org/opendaylight/yangtools/binding/model/api/AugmentableArchetype.java
+++ binding/binding-model/src/main/java/org/opendaylight/yangtools/binding/model/api/AugmentableArchetype.java
@@ -7,13 +7,23 @@
  */
 package org.opendaylight.yangtools.binding.model.api;
 
+import java.util.List;
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.yangtools.binding.Augmentable;
 
 /**
  * An {@link DataContainerArchetype} for an {@link Augmentable} interface.
+ *
+ * @since 16.0.0
  */
+@NonNullByDefault
 public sealed interface AugmentableArchetype extends DataContainerArchetype
     permits CaseObjectArchetype, ChildOfArchetype, DataContainerArchetype.OfNotification, RpcInputArchetype,
             RpcOutputArchetype {
-    // nothing else
+    /**
+     * {@return the list of {@link AugmentationArchetype}s applicable to this archetype}
+     */
+    default List<AugmentationArchetype> augmentations() {
+        return List.of();
+    }
 }
