8b50149b8dad3d5f68464746aad8713a1e131e46
Robert Varga <robert.varga@pantheon.tech>
2026-08-05 13:41:08 +0200

    WIP: Add DataContainerGetters
    // FIXME: more conversions
    // FIXME: remove TypeAnalysis
    
    JIRA: YANGTOOLS-1911
    Change-Id: I62e7024b618d0d8990769ffef39276b07f151892
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/binding-codegen/src/main/java/org/opendaylight/yangtools/binding/codegen/GetterShape.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.binding.codegen;
+
+import static java.util.Objects.requireNonNull;
+import static org.opendaylight.yangtools.binding.contract.Naming.GETTER_PREFIX;
+import static org.opendaylight.yangtools.binding.contract.Naming.toFirstLower;
+
+import org.opendaylight.yangtools.binding.model.api.MethodSignature;
+import org.opendaylight.yangtools.binding.model.api.Type;
+
+/**
+ * The code generation shape of a {@link MethodSignature}.
+ */
+record GetterShape(MethodSignature method, boolean hasOverride) implements Comparable<GetterShape> {
+    private static final int GETTER_PREFIX_LENGTH = GETTER_PREFIX.length();
+
+    GetterShape {
+        requireNonNull(method);
+    }
+
+    String name() {
+        return method.name();
+    }
+
+    Type type() {
+        return method.returnType();
+    }
+
+    boolean isBinary() {
+        // FIXME: compare to BaseYangTypes.BINARY_TYPE
+        return type().isArray();
+    }
+
+    String suffix() {
+        return name().substring(GETTER_PREFIX_LENGTH);
+    }
+
+    String fieldName() {
+        return "_" + propName();
+    }
+
+    String propName() {
+        return toFirstLower(suffix());
+    }
+
+    @Override
+    public int compareTo(final GetterShape other) {
+        return method.name().compareTo(other.method.name());
+    }
+}
