c5f1f1ef9621cf8d3c0333a3f596acc1857fa1e5
Robert Varga <robert.varga@pantheon.tech>
2026-08-07 18:05:49 +0200

    Seal binding.Augmentable
    Drop the '& DataContainer' and make Augmentable a sealed extension of
    DataContainer. This allows us to reason a bit more about the actual
    hierarchy and cuts down some verbosity from CodeHelpers.
    
    // FIXME: add an issue
    
    Change-Id: Ifec5f3a2ede0b629e103a204fe0bddac8f86c53d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-spec/src/main/java/org/opendaylight/yangtools/binding/RpcInput.java
+++ binding/binding-spec/src/main/java/org/opendaylight/yangtools/binding/RpcInput.java
@@ -16,7 +16,7 @@ import org.opendaylight.yangtools.binding.lib.JavaDataContainer;
  * @param <T> concrete {@link RpcOutput} type
  */
 // FIXME: YANGTOOLS-1921: extends ParentObject<T>
-public interface RpcInput<T extends RpcInput<T>> extends Augmentable<T>, JavaDataContainer<T>, DataObject {
+public non-sealed interface RpcInput<T extends RpcInput<T>> extends Augmentable<T>, JavaDataContainer<T>, DataObject {
     @Override
     Class<T> implementedInterface();
 }
