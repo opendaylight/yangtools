fbad7690ff657bd0bed252f9836aa6c17c75dec6
Robert Varga <robert.varga@pantheon.tech>
2026-08-08 01:28:43 +0200

    Split out ParserName.{ReadOnly,ReadWrite}
    Promote the two specializations as ReactorNamespace and UserNamespace
    top-level classes.
    
    JIRA: YANGTOOLS-1815
    Change-Id: I17e12bb42f5baf623b5805ae3bc2a2dbc273930a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-spi/src/main/java/org/opendaylight/yangtools/yang/parser/spi/meta/ParserNamespace.java
+++ parser/yang-parser-spi/src/main/java/org/opendaylight/yangtools/yang/parser/spi/meta/ParserNamespace.java
@@ -16,50 +16,12 @@ import org.opendaylight.yangtools.concepts.Immutable;
  * {@link NamespaceStmtCtx#namespaceItem(ParserNamespace, Object)} and still allows introduction of new namespaces
  * without need to change APIs.
  *
- * <p>Each namespace is either {@link ReadOnly} or {@link Writable}.
+ * <p>Each namespace is either {@link ReactorNamespace} or {@link UserNamespace}.
  *
  * @param <K> key type
  * @param <V> value type
  */
 @NonNullByDefault
-public sealed interface ParserNamespace<K, V> extends Immutable {
-    /**
-     * A read-only {@link ParserNamespace}, which has fixed contents over the course of an execution.
-     *
-     * @param <K> key type
-     * @param <V> value type
-     */
-    sealed interface ReadOnly<K, V> extends ParserNamespace<K, V> permits ReadOnlyParserNamespace {
-        // Nothing else
-    }
-
-    /**
-     * A writable {@link ParserNamespace}, which can be modified over the course of an execution.
-     *
-     * @param <K> key type
-     * @param <V> value type
-     */
-    sealed interface Writable<K, V> extends ParserNamespace<K, V> permits WritableParserNamespace {
-        // Nothing else
-    }
-
-    /**
-     * {@return a new {@link ReadOnly} namespace with specified name}
-     * @param <K> key type
-     * @param <V> value type
-     * @param name the name
-     */
-    static <K, V> ReadOnly<K, V> readOnly(final String name) {
-        return new ReadOnlyParserNamespace<>(name);
-    }
-
-    /**
-     * {@return a new {@link Writable} namespace with specified name}
-     * @param <K> key type
-     * @param <V> value type
-     * @param name the name
-     */
-    static <K, V> Writable<K, V> writable(final String name) {
-        return new WritableParserNamespace<>(name);
-    }
+public sealed interface ParserNamespace<K, V> extends Immutable permits ReactorNamespace, UserNamespace {
+    // nothing else
 }
