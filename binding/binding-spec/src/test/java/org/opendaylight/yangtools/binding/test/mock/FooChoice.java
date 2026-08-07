868262dfbc8735a0be5caf443bf4aa999aede4af
Robert Varga <robert.varga@pantheon.tech>
2026-08-07 13:24:44 +0200

    WIP: Update ChoiceIn/CaseObject integration
    ChoiceIn and CaseObject are tightly related: the former specifies a
    contract that can be implemented by multiple times by the latter.
    
    It is an error to implement ChoiceIn without implementing CaseObject, as
    is an error to implement multiple ChoiceIn contracts.
    
    The design here extends ChoiceIn to contain the usual self-capture
    formula, but also specifies implementedChoice(), which is provided
    through a default implementation in a ChoiceIn specialization.
    
    // FIXME: implementedChoice/implementedCase methods
    
    JIRA: YANGTOOLS-1928
    Change-Id: Idb2261d835a5aa841c497b58e3027dc2c7517638
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ binding/binding-spec/src/test/java/org/opendaylight/yangtools/binding/test/mock/FooChoice.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.binding.test.mock;
+
+import org.opendaylight.yangtools.binding.ChoiceIn;
+
+/**
+ * Example of a {@link ChoiceIn} specialization. Assuming the model
+ * {@snippet lang="yang" :
+ *    module foo {
+ *      choice foo {
+ *        case bar;
+ *      }
+ *    }
+ * }
+ * this class represents the {@code choice} and {@link BarCase} represents the {@code case}.
+ */
+public interface FooChoice extends ChoiceIn<FooData, FooChoice> {
+    @Override
+    default Class<FooChoice> implementedChoice() {
+        return FooChoice.class;
+    }
+}
