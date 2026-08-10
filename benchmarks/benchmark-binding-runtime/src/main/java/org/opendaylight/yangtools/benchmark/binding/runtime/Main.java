25a306aff9949661e475342f97115a93d1de1358
Robert Varga <robert.varga@pantheon.tech>
2026-08-10 10:32:23 +0200

    Add benchmark-binding-runtime for Vanadium SR1
    This adds a simplistic tool to load transportpce models into binding
    runtime for the purposes of evaluating the footprint.
    
    This version works with Vanadium SR1 artifacts:
    - yangtools-14.0.23
    - ietf-1.0.4
    - transportpce-models-23.1.0
    
    The artifact is not plugged into the build on purpose.
    
    Change-Id: Id335b3f0d41778c98c3476c1250880d4fc140d5d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ benchmarks/benchmark-binding-runtime/src/main/java/org/opendaylight/yangtools/benchmark/binding/runtime/Main.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yangtools.benchmark.binding.runtime;
+
+import com.google.common.base.Stopwatch;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.ref.Reference;
+import java.nio.charset.Charset;
+import java.util.HashSet;
+import org.opendaylight.yangtools.binding.generator.dagger.BindingRuntimeGeneratorModule;
+import org.opendaylight.yangtools.binding.runtime.spi.BindingRuntimeHelpers;
+import org.opendaylight.yangtools.yang.parser.api.YangParserException;
+import org.opendaylight.yangtools.yang.parser.impl.dagger.DaggerYangParserComponent;
+
+public final class Main {
+    private Main() {
+        // hidden on purpose
+    }
+
+    // console output
+    @SuppressWarnings("checkstyle:regexpSinglelineJava")
+    public static void main(final String[] args) throws InterruptedException, IOException, YangParserException {
+        final var classes = new HashSet<Class<?>>();
+
+        for (var arg : args) {
+            classes.addAll(switch (arg) {
+                case "openroadm" -> OpenRoadm1311.classes();
+                case "tapi" -> Tapi240.classes();
+                default -> throw new IllegalArgumentException("Unknown model set '" + arg + "'");
+            });
+        }
+
+        final var array = classes.toArray(Class<?>[]::new);
+        System.out.println("Constructing BindingRuntimeContext from " + classes.size() + " root models");
+        System.out.println("Hit enter when ready");
+        final var in = new BufferedReader(new InputStreamReader(System.in, Charset.defaultCharset()));
+        in.readLine();
+
+        final var sw = Stopwatch.createStarted();
+        final var runtimeContext = BindingRuntimeHelpers.createRuntimeContext(
+            DaggerYangParserComponent.create().parserFactory(),
+            BindingRuntimeGeneratorModule.provideBindingRuntimeGenerator(),
+            array);
+        System.out.println("BindingRuntimeContext created in " + sw.stop());
+
+        System.out.println("Hit enter to run GC");
+        in.readLine();
+        Runtime.getRuntime().gc();
+
+        System.out.println("Hit enter to exit");
+        in.readLine();
+
+        Reference.reachabilityFence(runtimeContext);
+    }
+}
