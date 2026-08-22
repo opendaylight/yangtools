4876fe4a6bc545c950717c380dd27349d7ab19ce
Robert Varga <robert.varga@pantheon.tech>
2026-08-22 21:59:43 +0200

    Fix MaterializedSourceRepresentation javadoc
    ErrorProne is correctly pointing out a non-javadoc comment: add the
    missing asterisk.
    
    Change-Id: Iebd7287830f09ed071d20521d91652941f7e5f52
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- model/yang-model-spi/src/main/java/org/opendaylight/yangtools/yang/model/spi/source/MaterializedSourceRepresentation.java
+++ model/yang-model-spi/src/main/java/org/opendaylight/yangtools/yang/model/spi/source/MaterializedSourceRepresentation.java
@@ -22,7 +22,7 @@ import org.opendaylight.yangtools.yang.model.api.source.SourceSyntaxException;
  */
 @NonNullByDefault
 public interface MaterializedSourceRepresentation<R extends SourceRepresentation, S> extends SourceInfo.Extractor {
-    /*
+    /**
      * @{return {@link SourceIdentifier} of this source}
      */
     SourceIdentifier sourceId();
