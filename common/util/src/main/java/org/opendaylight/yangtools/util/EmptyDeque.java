e0e03a001dee8e8ea73056d0a7615b658949fafe
Robert Varga <robert.varga@pantheon.tech>
2026-08-26 01:46:53 +0200

    Deprecate util.EmptyDeque for removal
    There are no users of this class anywhere in OpenDaylight proper.
    Deprecate it for removal.
    
    JIRA: YANGTOOLS-1952
    Change-Id: Icc63e1ec6c588c1b182e911cb27933ae99436377
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- common/util/src/main/java/org/opendaylight/yangtools/util/EmptyDeque.java
+++ common/util/src/main/java/org/opendaylight/yangtools/util/EmptyDeque.java
@@ -22,7 +22,9 @@ import org.opendaylight.yangtools.concepts.Immutable;
  * elements in its {@link #offer(Object)} method.
  *
  * @param <E> the type of elements held in this collection
+ * @deprecated This class is deprecated and scheduled for removal in the next major release.
  */
+@Deprecated(since = "16.0.0", forRemoval = true)
 public final class EmptyDeque<E> extends AbstractQueue<E> implements Deque<E>, Immutable {
     private static final EmptyDeque<?> INSTANCE = new EmptyDeque<>();
     private static final Object[] EMPTY_ARRAY = new Object[0];
@@ -31,140 +33,166 @@ public final class EmptyDeque<E> extends AbstractQueue<E> implements Deque<E>, I
         // No instances
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @SuppressWarnings("unchecked")
     public static <T> EmptyDeque<T> instance() {
         return (EmptyDeque<T>) INSTANCE;
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     @SuppressWarnings("checkstyle:parameterName")
     public boolean offer(final E e) {
         return false;
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     @SuppressWarnings("checkstyle:parameterName")
     public boolean offerFirst(final E e) {
         return false;
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     @SuppressWarnings("checkstyle:parameterName")
     public boolean offerLast(final E e) {
         return false;
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     public E poll() {
         return null;
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     public E pollFirst() {
         return null;
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     public E pollLast() {
         return null;
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     public E peek() {
         return null;
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     public E peekFirst() {
         return null;
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     public E peekLast() {
         return null;
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     public Iterator<E> iterator() {
         return Collections.emptyIterator();
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     public Spliterator<E> spliterator() {
         return Spliterators.emptySpliterator();
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     public int size() {
         return 0;
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     public Object[] toArray() {
         return EMPTY_ARRAY;
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     @SuppressWarnings("checkstyle:parameterName")
     public <T> T[] toArray(final T[] a) {
         return requireNonNull(a);
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     @SuppressWarnings("checkstyle:parameterName")
     public void addFirst(final E e) {
         add(e);
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     @SuppressWarnings("checkstyle:parameterName")
     public void addLast(final E e) {
         add(e);
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     public E removeFirst() {
         return remove();
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     public E removeLast() {
         return remove();
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     public E getFirst() {
         return element();
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     public E getLast() {
         return element();
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     @SuppressWarnings("checkstyle:parameterName")
     public boolean removeFirstOccurrence(final Object o) {
         return false;
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     @SuppressWarnings("checkstyle:parameterName")
     public boolean removeLastOccurrence(final Object o) {
         return false;
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     @SuppressWarnings("checkstyle:parameterName")
     public void push(final E e) {
         addFirst(e);
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     public E pop() {
         return removeFirst();
     }
 
+    @Deprecated(since = "16.0.0", forRemoval = true)
     @Override
     public Iterator<E> descendingIterator() {
         return Collections.emptyIterator();
