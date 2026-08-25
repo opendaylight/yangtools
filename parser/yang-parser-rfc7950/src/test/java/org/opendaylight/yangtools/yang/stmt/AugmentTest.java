9cf7087d915ad20805445f7f1a79abe77b2eda43
Robert Varga <robert.varga@pantheon.tech>
2026-08-25 07:44:37 +0200

    Modernize AugmentTest
    Sonar is pointing out a bad names: fix that, but also modernize the test
    with explicit assertions, local variable type inference and switch dispatch.
    
    Change-Id: Ia299c1778e5272b2d1b3d971487735244b70d773
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/AugmentTest.java
+++ parser/yang-parser-rfc7950/src/test/java/org/opendaylight/yangtools/yang/stmt/AugmentTest.java
@@ -12,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -67,12 +68,12 @@ class AugmentTest extends AbstractYangTest {
 
         final var ds0ChannelNumber = assertInstanceOf(LeafSchemaNode.class,
             augment.dataChildByName(QName.create(module1.getQNameModule(), "ds0ChannelNumber")));
-        final var interfaceId = (LeafSchemaNode) augment.getDataChildByName(QName.create(
-            module1.getQNameModule(), "interface-id"));
-        final var schemas = (ContainerSchemaNode) augment.getDataChildByName(QName.create(
-            module1.getQNameModule(), "schemas"));
-        final var odl = (ChoiceSchemaNode) augment.getDataChildByName(QName.create(
-            module1.getQNameModule(), "odl"));
+        final var interfaceId = assertInstanceOf(LeafSchemaNode.class,
+            augment.getDataChildByName(QName.create(module1.getQNameModule(), "interface-id")));
+        final var schemas = assertInstanceOf(ContainerSchemaNode.class,
+            augment.getDataChildByName(QName.create(module1.getQNameModule(), "schemas")));
+        final var odl = assertInstanceOf(ChoiceSchemaNode.class,
+            augment.getDataChildByName(QName.create(module1.getQNameModule(), "odl")));
 
         assertNotNull(ds0ChannelNumber);
         assertNotNull(interfaceId);
@@ -104,7 +105,7 @@ class AugmentTest extends AbstractYangTest {
         AugmentationSchemaNode augment1 = null;
         AugmentationSchemaNode augment2 = null;
         AugmentationSchemaNode augment3 = null;
-        for (final AugmentationSchemaNode as : augmentations) {
+        for (var as : augmentations) {
             if (as.getWhenCondition().isEmpty()) {
                 augment3 = as;
             } else if ("br:ifType='ds0'".equals(as.getWhenCondition().orElseThrow().toString())) {
@@ -123,40 +124,41 @@ class AugmentTest extends AbstractYangTest {
         assertNotNull(augmentHolder);
 
         assertEquals(1, augment2.getChildNodes().size());
-        final var augmentHolder2 = (ContainerSchemaNode) augment2.getDataChildByName(QName.create(
-            module3.getQNameModule(), "augment-holder2"));
+        final var augmentHolder2 = assertInstanceOf(ContainerSchemaNode.class,
+            augment2.getDataChildByName(QName.create(module3.getQNameModule(), "augment-holder2")));
         assertNotNull(augmentHolder2);
 
         assertEquals(1, augment3.getChildNodes().size());
-        final var pause = (CaseSchemaNode) augment3.getDataChildByName(QName.create(
-            module3.getQNameModule(), "pause"));
+        final var pause = assertInstanceOf(CaseSchemaNode.class,
+            augment3.getDataChildByName(QName.create(module3.getQNameModule(), "pause")));
         assertNotNull(pause);
     }
 
     @Test
     void testAugmentResolving() {
         final var module2 = AUGMENT_IN_AUGMENT.findModules("bar").iterator().next();
-        final var interfaces = (ContainerSchemaNode) module2.getDataChildByName(QName.create(
-            module2.getQNameModule(), "interfaces"));
-        final var ifEntry = (ListSchemaNode) interfaces.getDataChildByName(QName.create(
-            module2.getQNameModule(), "ifEntry"));
+        final var interfaces = assertInstanceOf(ContainerSchemaNode.class,
+            module2.getDataChildByName(QName.create(module2.getQNameModule(), "interfaces")));
+        final var ifEntry = assertInstanceOf(ListSchemaNode.class,
+            interfaces.getDataChildByName(QName.create(module2.getQNameModule(), "ifEntry")));
 
         // baz.yang
         // augment "/br:interfaces/br:ifEntry" {
-        final var augmentHolder = (ContainerSchemaNode) ifEntry.getDataChildByName(QName.create(BAZ,
-            "augment-holder"));
+        final var augmentHolder = assertInstanceOf(ContainerSchemaNode.class,
+            ifEntry.getDataChildByName(QName.create(BAZ, "augment-holder")));
         checkIsAugmenting(augmentHolder, true);
         assertEquals(Q2, augmentHolder.getQName());
 
         // foo.yang
         // augment "/br:interfaces/br:ifEntry/bz:augment-holder"
-        final var ds0ChannelNumber = (LeafSchemaNode) augmentHolder.getDataChildByName(QName.create(FOO,
-            "ds0ChannelNumber"));
-        final var interfaceId = (LeafSchemaNode) augmentHolder.getDataChildByName(QName.create(FOO,
-            "interface-id"));
-        final var schemas = (ContainerSchemaNode) augmentHolder.getDataChildByName(QName.create(FOO,
-            "schemas"));
-        final var odl = (ChoiceSchemaNode) augmentHolder.getDataChildByName(QName.create(FOO, "odl"));
+        final var ds0ChannelNumber = assertInstanceOf(LeafSchemaNode.class,
+            augmentHolder.getDataChildByName(QName.create(FOO, "ds0ChannelNumber")));
+        final var interfaceId = assertInstanceOf(LeafSchemaNode.class,
+            augmentHolder.getDataChildByName(QName.create(FOO, "interface-id")));
+        final var schemas = assertInstanceOf(ContainerSchemaNode.class,
+            augmentHolder.getDataChildByName(QName.create(FOO, "schemas")));
+        final var odl = assertInstanceOf(ChoiceSchemaNode.class,
+            augmentHolder.getDataChildByName(QName.create(FOO, "odl")));
 
         assertNotNull(ds0ChannelNumber);
         assertNotNull(interfaceId);
@@ -179,17 +181,18 @@ class AugmentTest extends AbstractYangTest {
     @Test
     void testAugmentedChoice() {
         final var module2 = AUGMENT_IN_AUGMENT.findModules("bar").iterator().next();
-        final var interfaces = (ContainerSchemaNode) module2.getDataChildByName(QName.create(
-            module2.getQNameModule(), "interfaces"));
-        final var ifEntry = (ListSchemaNode) interfaces.getDataChildByName(QName.create(
-            module2.getQNameModule(), "ifEntry"));
-        final var augmentedHolder = (ContainerSchemaNode) ifEntry.getDataChildByName(QName.create(
-            BAZ, "augment-holder"));
+        final var interfaces = assertInstanceOf(ContainerSchemaNode.class,
+            module2.getDataChildByName(QName.create(module2.getQNameModule(), "interfaces")));
+        final var ifEntry = assertInstanceOf(ListSchemaNode.class,
+            interfaces.getDataChildByName(QName.create(module2.getQNameModule(), "ifEntry")));
+        final var augmentedHolder = assertInstanceOf(ContainerSchemaNode.class,
+            ifEntry.getDataChildByName(QName.create(BAZ, "augment-holder")));
         checkIsAugmenting(augmentedHolder, true);
 
         // foo.yang
         // augment "/br:interfaces/br:ifEntry/bz:augment-holder"
-        final var odl = (ChoiceSchemaNode) augmentedHolder.getDataChildByName(QName.create(FOO, "odl"));
+        final var odl = assertInstanceOf(ChoiceSchemaNode.class,
+            augmentedHolder.getDataChildByName(QName.create(FOO, "odl")));
         assertNotNull(odl);
         final var cases = odl.getCases();
         assertEquals(4, cases.size());
@@ -199,15 +202,13 @@ class AugmentTest extends AbstractYangTest {
         CaseSchemaNode node2 = null;
         CaseSchemaNode node3 = null;
 
-        for (final CaseSchemaNode ccn : cases) {
-            if ("id".equals(ccn.getQName().getLocalName())) {
-                id = ccn;
-            } else if ("node1".equals(ccn.getQName().getLocalName())) {
-                node1 = ccn;
-            } else if ("node2".equals(ccn.getQName().getLocalName())) {
-                node2 = ccn;
-            } else if ("node3".equals(ccn.getQName().getLocalName())) {
-                node3 = ccn;
+        for (var ccn : cases) {
+            switch (ccn.getQName().getLocalName()) {
+                case "id" -> id = ccn;
+                case "node1" -> node1 = ccn;
+                case "node2" -> node2 = ccn;
+                case "node3" -> node3 = ccn;
+                default -> fail("Unexcpected case " + ccn);
             }
         }
 
@@ -238,12 +239,12 @@ class AugmentTest extends AbstractYangTest {
 
         // test cases
         // case id child
-        final LeafSchemaNode caseIdChild = (LeafSchemaNode) idChildren.iterator().next();
+        final var caseIdChild = assertInstanceOf(LeafSchemaNode.class, idChildren.iterator().next());
         assertNotNull(caseIdChild);
         assertEquals(QName.create(FOO, "id"), caseIdChild.getQName());
 
         // case node3 child
-        final ContainerSchemaNode caseNode3Child = (ContainerSchemaNode) node3Children.iterator().next();
+        final var caseNode3Child = assertInstanceOf(ContainerSchemaNode.class, node3Children.iterator().next());
         assertNotNull(caseNode3Child);
         assertEquals(QName.create(FOO, "node3"), caseNode3Child.getQName());
     }
@@ -251,8 +252,8 @@ class AugmentTest extends AbstractYangTest {
     @Test
     void testAugmentRpc() {
         final var context = assertEffectiveModelDir("/augment-test/rpc");
-        final var NS_BAR = XMLNamespace.of("urn:opendaylight:bar");
-        final var NS_FOO = XMLNamespace.of("urn:opendaylight:foo");
+        final var barNs = XMLNamespace.of("urn:opendaylight:bar");
+        final var fooNs = XMLNamespace.of("urn:opendaylight:foo");
         final var revision = Revision.of("2013-10-11");
         final var bar = context.findModules("bar").iterator().next();
         final var rpcs = bar.getRpcs();
@@ -267,14 +268,14 @@ class AugmentTest extends AbstractYangTest {
         }
         assertNotNull(submit);
 
-        final var submitQName = QName.create(NS_BAR, revision, "submit");
+        final var submitQName = QName.create(barNs, revision, "submit");
         assertEquals(submitQName, submit.getQName());
         final var input = submit.getInput();
-        final var inputQName = QName.create(NS_BAR, revision, "input");
+        final var inputQName = QName.create(barNs, revision, "input");
         assertEquals(inputQName, input.getQName());
-        final var arguments = (ChoiceSchemaNode) input.getDataChildByName(QName.create(NS_BAR, revision,
-            "arguments"));
-        final QName argumentsQName = QName.create(NS_BAR, revision, "arguments");
+
+        final QName argumentsQName = QName.create(barNs, revision, "arguments");
+        final var arguments = assertInstanceOf(ChoiceSchemaNode.class, input.getDataChildByName(argumentsQName));
         assertEquals(argumentsQName, arguments.getQName());
         assertFalse(arguments.isAugmenting());
         final var cases = arguments.getCases();
@@ -284,12 +285,13 @@ class AugmentTest extends AbstractYangTest {
         CaseSchemaNode create = null;
         CaseSchemaNode destroy = null;
         for (var child : cases) {
-            if ("attach".equals(child.getQName().getLocalName())) {
-                attach = child;
-            } else if ("create".equals(child.getQName().getLocalName())) {
-                create = child;
-            } else if ("destroy".equals(child.getQName().getLocalName())) {
-                destroy = child;
+            switch (child.getQName().getLocalName()) {
+                case "attach" -> attach = child;
+                case "create" -> create = child;
+                case "destroy" -> destroy = child;
+                default -> {
+                    // no-op
+                }
             }
         }
         assertNotNull(attach);
@@ -301,17 +303,17 @@ class AugmentTest extends AbstractYangTest {
         assertTrue(destroy.isAugmenting());
 
         // case attach
-        assertEquals(QName.create(NS_FOO, revision, "attach"), attach.getQName());
+        assertEquals(QName.create(fooNs, revision, "attach"), attach.getQName());
         final var attachChildren = attach.getChildNodes();
         assertEquals(1, attachChildren.size());
 
         // case create
-        assertEquals(QName.create(NS_FOO, revision, "create"), create.getQName());
+        assertEquals(QName.create(fooNs, revision, "create"), create.getQName());
         final var createChildren = create.getChildNodes();
         assertEquals(1, createChildren.size());
 
         // case attach
-        assertEquals(QName.create(NS_FOO, revision, "destroy"), destroy.getQName());
+        assertEquals(QName.create(fooNs, revision, "destroy"), destroy.getQName());
         final var destroyChildren = destroy.getChildNodes();
         assertEquals(1, destroyChildren.size());
     }
@@ -322,14 +324,19 @@ class AugmentTest extends AbstractYangTest {
         assertEquals(1, context.getModules().size());
 
         final var test = context.getModules().iterator().next();
-        final var links = (DataNodeContainer) test.getDataChildByName(QName.create(test.getQNameModule(), "links"));
-        final var link = (DataNodeContainer) links.getDataChildByName(QName.create(test.getQNameModule(), "link"));
-        final var nodes = (DataNodeContainer) link.getDataChildByName(QName.create(test.getQNameModule(), "nodes"));
-        final var node = (ContainerSchemaNode) nodes.getDataChildByName(QName.create(test.getQNameModule(), "node"));
+        final var links = assertInstanceOf(DataNodeContainer.class,
+            test.getDataChildByName(QName.create(test.getQNameModule(), "links")));
+        final var link = assertInstanceOf(DataNodeContainer.class,
+            links.getDataChildByName(QName.create(test.getQNameModule(), "link")));
+        final var nodes = assertInstanceOf(DataNodeContainer.class,
+            link.getDataChildByName(QName.create(test.getQNameModule(), "nodes")));
+        final var node = assertInstanceOf(ContainerSchemaNode.class,
+            nodes.getDataChildByName(QName.create(test.getQNameModule(), "node")));
         final var augments = node.getAvailableAugmentations();
         assertEquals(1, augments.size());
         assertEquals(1, node.getChildNodes().size());
-        final var id = (LeafSchemaNode) node.getDataChildByName(QName.create(test.getQNameModule(), "id"));
+        final var id = assertInstanceOf(LeafSchemaNode.class,
+            node.getDataChildByName(QName.create(test.getQNameModule(), "id")));
         assertTrue(id.isAugmenting());
     }
 
