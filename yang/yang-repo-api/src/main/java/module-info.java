840c81903052c05d437f086cc87c98ef0d7962de
Robert Varga <robert.varga@pantheon.tech>
2026-08-25 23:19:39 +0200

    Disconnect yang-repo-api from util
    The only use of util is FluentFutures.immediateTrueFluentFuture():
    define our own constant and ditch the dependency.
    
    JIRA: YANGTOOLS-1945
    Change-Id: I2ce9bac8322391489b3e6a9b8278e07961f2a22c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- yang/yang-repo-api/src/main/java/module-info.java
+++ yang/yang-repo-api/src/main/java/module-info.java
@@ -14,7 +14,6 @@ module org.opendaylight.yangtools.yang.repo.api {
     requires transitive org.opendaylight.yangtools.yang.common;
     requires transitive org.opendaylight.yangtools.yang.model.api;
 
-    requires org.opendaylight.yangtools.util;
     requires org.slf4j;
 
     // Annotations
