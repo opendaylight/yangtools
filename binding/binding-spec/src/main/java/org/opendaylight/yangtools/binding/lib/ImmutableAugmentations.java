4489c152fb2b0943d012f005a7c55c53ddccd12a
Robert Varga <robert.varga@pantheon.tech>
2026-09-12 19:41:50 +0200

    Integrate AbstractAugmentable with Augmentations
    JIRA: YANGTOOLS-1962
    Change-Id: I83325afeccd5069a2a11cd258a1e9d59d5393306
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- binding/binding-spec/src/main/java/org/opendaylight/yangtools/binding/lib/ImmutableAugmentations.java
+++ binding/binding-spec/src/main/java/org/opendaylight/yangtools/binding/lib/ImmutableAugmentations.java
@@ -27,15 +27,29 @@ import org.opendaylight.yangtools.concepts.Immutable;
 @Beta
 public abstract sealed class ImmutableAugmentations<C extends DataContainer & Augmentable<C>>
         extends AbstractMap<Class<? extends Augmentation<C, ?>>, Augmentation<C, ?>>
-        implements Augmentations<C>, Immutable permits EmptyAugmentations {
+        implements Augmentations<C>, Immutable permits ImmutableAugmentations0, ImmutableAugmentations1 {
     /**
-     * {@return an empty ImmutableAugmentationMap}
+     * {@return an empty ImmutableAugmentations}
      *
      * @param <C> the {@link Augmentable} {@link DataContainer} type
+     * @return an ImmutableAugmentations instance
      */
     @SuppressWarnings("unchecked")
     public static final <C extends DataContainer & Augmentable<C>> @NonNull ImmutableAugmentations<C> of() {
-        return (ImmutableAugmentations<C>) EmptyAugmentations.INSTANCE;
+        return (ImmutableAugmentations<C>) ImmutableAugmentations0.INSTANCE;
+    }
+
+    /**
+     * {@return an ImmutableAugmentations containing one augmentation}
+     *
+     * @param <C> the {@link Augmentable} {@link DataContainer} type
+     * @param augmentation the augmentation
+     * @return an ImmutableAugmentations instance
+     * @throws NullPointerException if augmentation is {@code null}
+     */
+    public static final <C extends DataContainer & Augmentable<C>> @NonNull ImmutableAugmentations<C> of(
+            final Augmentation<C, ?> augmentation) {
+        return new ImmutableAugmentations1<>(augmentation);
     }
 
     /**
@@ -74,6 +88,19 @@ public abstract sealed class ImmutableAugmentations<C extends DataContainer & Au
     }
 
     abstract @NonNull Augmentations<C> withoutImpl(Class<? extends Augmentation<C, ?>> key);
+
+    static final <C extends DataContainer & Augmentable<C>>
+            Entry<Class<? extends Augmentation<C, ?>>, Augmentation<C, ?>> entryOf(final Augmentation<C, ?> value) {
+        return Map.entry(keyOf(value), value);
+    }
+
+    static final <C extends DataContainer & Augmentable<C>> Class<? extends Augmentation<C, ?>> keyOf(
+            final Augmentation<C, ?> value) {
+        @SuppressWarnings("unchecked")
+        final var key = (@NonNull Class<? extends Augmentation<C, ?>>) value.implementedInterface()
+            .asSubclass(Augmentation.class);
+        return key;
+    }
 
     @Override
     public final void clear() {
@@ -157,7 +184,7 @@ public abstract sealed class ImmutableAugmentations<C extends DataContainer & Au
         throw uoe();
     }
 
-    private static UnsupportedOperationException uoe() {
+    static final @NonNull UnsupportedOperationException uoe() {
         return new UnsupportedOperationException();
     }
 }
